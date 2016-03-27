81969165e3565044e44d67153af0d808d3493587
Kyle Mathews <mathews.kyle@gmail.com>
2016-03-26 22:36:36 -0700

    Default to using the local install of gatsby and warn if there's not one

--- bin/gatsby-develop.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-
-require('babel-core/register')
-require('./develop')
--- bin/gatsby.js
+++ bin/gatsby.js
@@ -1,4 +1,50 @@
 #!/usr/bin/env node
 
+/*eslint-disable */
 require('babel-core/register')
-require('./index')
+
+global.appStartTime = Date.now()
+
+var sysPath = require('path')
+var fs = require('fs')
+var version = process.version
+var verDigit = parseInt(version.match(/^v(\d+)\./)[1], 10)
+
+if (verDigit < 0.12) {
+  console.error(
+    'Error: Gatsby 0.9+ requires node.js v0.12 or higher (you have ' + version + ') ' +
+    'Upgrade node to the latest stable release.'
+  )
+  process.exit()
+}
+
+var cwd = sysPath.resolve('.')
+var cliFile = sysPath.join('dist', 'bin', 'cli.js')
+var localPath = sysPath.join(cwd, 'node_modules', 'gatsby', cliFile)
+
+var loadGatsby = function (path) {
+  require(path)
+}
+
+var loadGlobalGatsby = function () {
+  fs.realpath(__dirname, function (err, real) {
+    if (err) throw err
+    loadGatsby(sysPath.join(real, '..', cliFile))
+  })
+}
+
+fs.access(localPath, function (error) {
+  if (error) {
+    loadGlobalGatsby()
+  } else {
+    try {
+      loadGatsby(localPath)
+    } catch(error) {
+      console.error(
+        'Gatsby: Local install exists but failed to load it. ' +
+        'Continuing with global install:', error
+      )
+      loadGlobalGatsby()
+    }
+  }
+})
