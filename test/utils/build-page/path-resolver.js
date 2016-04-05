cb5d14030345fd1119d10befc5260b8042f2bbdb
Benjamin Stepp <me@benstepp.com>
2016-04-04 23:10:52 -0500

    Separate building Page Data and Globbing Pages
    * Split up all the spaghetti in glob-pages up so that it can be unit
    tested.
    * Renamed `pathresolver` to `urlResolver`
    * Added tests to verify the structure of the data returned by the new
    pathResolver.
    * updated the urlResolver's tests with the new function name

---
+++ test/utils/build-page/path-resolver.js
@@ -0,0 +1,56 @@
+import test from 'ava'
+import path from 'path'
+import { startsWith } from 'lodash'
+import pathResolver from '../../../lib/utils/build-page/path-resolver'
+
+test('it returns an object', t => {
+  const pagePath = '/'
+  const result = pathResolver(pagePath)
+  t.ok(result)
+  t.is(typeof result, 'object')
+})
+
+test('it has a require path', t => {
+  const pagePath = '/a-blog-post/index.md'
+  const result = pathResolver(pagePath)
+  t.ok(result.requirePath)
+  t.is(typeof result.requirePath, 'string')
+})
+
+test('it does not has a path if the name start with _', t => {
+  t.is(pathResolver('/_metadata.js').path, undefined)
+  t.is(pathResolver('/_template.html.erb').path, undefined)
+  t.is(pathResolver('/_notapage.json').path, undefined)
+})
+
+test('it has a path if the name doesnt start with _', t => {
+  t.ok(pathResolver('/2015/back-to-the-future.md').path)
+  t.ok(pathResolver('/my-first-blog.md').path)
+  t.ok(pathResolver('/index.md').path)
+})
+
+test('it has a templatePath if the name is _template', t => {
+  const pagePath = '/_template.js'
+  const result = pathResolver(pagePath)
+  t.ok(result.templatePath)
+  t.is(typeof result.templatePath, 'string')
+})
+
+test('it does not have a templatePath if not a _template', t => {
+  t.is(pathResolver('/index.md').templatePath, undefined)
+  t.is(pathResolver('/another-page.toml').templatePath, undefined)
+  t.is(pathResolver('/something-else/good-stuff.html').templatePath, undefined)
+})
+
+test('the directory name has / slashes', t => {
+  const pagePath = '/2016/testing-middleman-sites-with-capybara/index.md'
+  const result = pathResolver(pagePath)
+
+  t.same(result.file.dirname, path.posix.normalize(result.file.dirname))
+  t.same(result.file.dirname, '/2016/testing-middleman-sites-with-capybara')
+})
+
+test('the ext doesnt have a leading .', t => {
+  const result = pathResolver('/index.md')
+  t.false(startsWith(result.file.ext, '.'))
+})
