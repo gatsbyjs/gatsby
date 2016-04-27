50793855474af4e15366843192f06305de4169c2
Benjamin Stepp <benstepp@users.noreply.github.com>
2016-04-27 14:05:54 -0500

    Add a test case for #259 (#260)
    This currently fails. The change in #259 will make it pass. Currently
    the image comes out at the wrong path (/hello-worldimage.jpg)
    
    ava doesnt have test.failing() yet, and our eslint config prevents the
    usage of test.todo() so this will most defininitely blow up travis.

---
+++ test/fixtures/path-not-ending-in-slash/pages/_template.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function template (props) {
+  return <div>{props.children}</div>
+}
+
+template.propTypes = {
+  children: React.PropTypes.any,
+}
---
+++ test/fixtures/path-not-ending-in-slash/pages/hello-world-2016/index.md
@@ -0,0 +1,7 @@
+---
+title: Hello World
+date: "2015-05-01T22:12:03.284Z"
+path: "/hello-world"
+---
+
+![An Image](./image.jpg)
