7537b0ac71f9ddcacfab0ed3e12dcf9462750393
Benjamin Stepp <me@benstepp.com>
2016-05-08 11:30:26 -0500

    Support user provided babel configs
    This change allows the user to provide a babelrc or a babel section in
    their package.json. It also allows gatsby to be used without having to
    specifiy a babelrc if the user doesn't need anything past what babel
    provides as a default.
    
    * It resolves all of the paths to become absolute such that the user can
    crawl upwards as in #239.
    
    * It allows the custom usage of other babel plugins so that decorators
    work as in #129.

---
+++ test/utils/babel-config.js
@@ -0,0 +1,61 @@
+import test from 'ava'
+import path from 'path'
+import includes from 'lodash/includes'
+import babelConfig from '../../lib/utils/babel-config'
+
+function programStub (fixture) {
+  const directory = path.resolve('..', 'fixtures', fixture)
+  return { directory }
+}
+
+test('it returns a default babel config for babel-loader query', t => {
+  const program = programStub('site-without-babelrc')
+  const config = babelConfig(program)
+
+  t.true(typeof config === 'object')
+  t.truthy(config.presets.length)
+  t.truthy(config.plugins.length)
+})
+
+test('all plugins are absolute paths to avoid babel lookups', t => {
+  const program = programStub('site-without-babelrc')
+  const config = babelConfig(program)
+
+  config.presets.forEach(preset => t.true(path.resolve(preset) === preset))
+  config.plugins.forEach(plugin => t.true(path.resolve(plugin) === plugin))
+})
+
+test('fixture can resolve plugins in gatsby directory (crawling up)', t => {
+  const program = programStub('site-with-valid-babelrc')
+
+  const config = babelConfig(program)
+  t.truthy(config.presets.length)
+  t.truthy(config.plugins.length)
+})
+
+test('throws error when babelrc is not parseable', t => {
+  const program = programStub('site-with-invalid-babelrc')
+
+  t.throws(() => babelConfig(program))
+})
+
+test('can read babel from packagejson', t => {
+  const program = programStub('site-with-valid-babelpackage')
+
+  const config = babelConfig(program)
+  t.truthy(config.presets.length)
+})
+
+test('when in development has hmre', t => {
+  const program = programStub('site-without-babelrc')
+  const config = babelConfig(program, 'develop')
+
+  // regex matches: babel followed by any amount of hyphen or word characters
+  const presetNames = config.presets.map(p => p.match(/babel[-|\w]+/)[0])
+  t.true(includes(presetNames, 'babel-preset-react-hmre'))
+})
+
+test('throws when a plugin is not available', t => {
+  const program = programStub('site-with-unresolvable-babelrc')
+  t.throws(() => babelConfig(program))
+})
