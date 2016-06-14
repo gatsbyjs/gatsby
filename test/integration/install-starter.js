7358d0371a77d06e6598ed9413b9f796321cbaa1
Kyle Mathews <mathews.kyle@gmail.com>
2016-06-13 19:51:29 -0700

    Remove console.log

--- test/integration/install-starter.js
+++ test/integration/install-starter.js
@@ -9,6 +9,5 @@ import { gatsby } from '../support'
 test('calling gatsby new succesfully creates new site from default starter', async t => {
   await remove(`${tmpdir}/gatsby-default-starter`)
   const noArgs = await gatsby(['new', `${tmpdir}/tmp/gatsby-default-starter`])
-  console.log(noArgs)
   t.is(noArgs.code, 0)
 })
