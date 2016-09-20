ae27d96f2e8a1e35bb411fbeddaa38db24080b4b
Kyle Mathews <mathews.kyle@gmail.com>
2016-09-20 15:08:16 -0700

    Validate at runtime gatsby-config.js and new pages being created

---
+++ interfaces/joi.js
@@ -0,0 +1,13 @@
+const Joi = require(`joi`)
+
+export const gatsbyConfigSchema = Joi.object().keys({
+  sources: Joi.string().required(),
+  siteMetadata: Joi.object(),
+  linkPrefix: Joi.string(),
+})
+
+export const pageSchema = Joi.object().keys({
+  path: Joi.string().required(),
+  component: Joi.string().required(),
+  data: Joi.object(),
+}).unknown()
