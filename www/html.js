2567cd19035ae093e4fb2e2788e7e57c31c4752a
Kyle Mathews <mathews.kyle@gmail.com>
2016-11-13 20:57:13 -0800

    Add favicons

--- www/html.js
+++ www/html.js
@@ -18,6 +18,10 @@ module.exports = React.createClass({
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
+          <link rel="icon" type="image/png" sizes="192x192" href={require("images/favicons/android-icon-192x192.png")} />
+          <link rel="icon" type="image/png" sizes="32x32" href={require("images/favicons/favicon-32x32.png")} />
+          <link rel="icon" type="image/png" sizes="96x96" href={require("images/favicons/favicon-96x96.png")} />
+          <link rel="icon" type="image/png" sizes="16x16" href={require("images/favicons/favicon-16x16.png")} />
           <GoogleFont typography={typography} />
           <TypographyStyle typography={typography} />
           <HTMLStyles />
