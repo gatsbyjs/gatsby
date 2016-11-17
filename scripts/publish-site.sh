4293fb580a3851710e356413d67775179b3427bf
Kyle Mathews <mathews.kyle@gmail.com>
2016-11-16 20:25:56 -0800

    Use stable-module-id-hash webpack plugin (#559)
    * Use NamedModuleIds for determanistic builds.
    
    * Remove checked-in webpack records from the website

--- scripts/publish-site.sh
+++ scripts/publish-site.sh
@@ -1,10 +1,12 @@
-echo "=== Installing npm packages for Gatsby"
-npm install
 echo "=== Building ES5 version of Gatsby"
 npm run build
 cd www
 echo "=== Installing the website dependencies"
 npm install
+ls ./node_modules | grep stable
+echo "=== Overwrite website node_modules with Gatsby's."
+cp -r ../node_modules/* ./node_modules/
+ls ./node_modules | grep stable
 echo "=== Copying built Gatsby to website."
 cp -r ../dist ./node_modules/gatsby/
 echo "=== Building website"
