b9ab3c42649b3f2de5100a68fee4fbe8611be155
Kyle Mathews <mathews.kyle@gmail.com>
2016-11-16 19:18:44 -0800

    Wait until after website finishes installing before copying code

--- scripts/publish-site.sh
+++ scripts/publish-site.sh
@@ -3,10 +3,9 @@ npm install
 echo "=== Building ES5 version of Gatsby"
 npm run build
 cd www
-echo "=== Copying built Gatsby to website."
-echo pwd
-cp -r ../dist ./node_modules/gatsby/
 echo "=== Installing the website dependencies"
 npm install
+echo "=== Copying built Gatsby to website."
+cp -r ../dist ./node_modules/gatsby/
 echo "=== Building website"
 ./node_modules/.bin/gatsby build
