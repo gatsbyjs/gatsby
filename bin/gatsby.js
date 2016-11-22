b45e0ebd4c4ffe525617861df55e9d1831a97379
Mike Nason <michael@nason.us>
2016-11-22 10:00:43 -0500

    Disable global install and verify folder structure
    Use react-dev-utils/checkRequiredFiles

--- bin/gatsby.js
+++ bin/gatsby.js
@@ -8,6 +8,7 @@ global.appStartTime = Date.now()
 
 var sysPath = require('path')
 var fs = require('fs')
+var checkRequiredFiles = require('react-dev-utils/checkRequiredFiles')
 var version = process.version
 var verDigit = Number(version.match(/\d+/)[0])
 
@@ -27,32 +28,49 @@ var loadGatsby = function (path) {
   require(path)
 }
 
-var useGlobalGatsby = function () {
-  var commandsToIgnore = ['new', '--help']
-  if (commandsToIgnore.indexOf(process.argv[2]) === -1) {
-    console.log(
-      "A local install of Gatsby was not found.\n" +
-      "You should save Gatsby as a site dependency e.g. npm install --save gatsby"
-    )
-    process.exit()
-  }
-  fs.realpath(__dirname, function (err, real) {
-    if (err) throw err
-    loadGatsby(sysPath.join(real, '..', cliFile))
-  })
-}
+/*
+  These commands can:
+  - Use a global install as a fallback if no local install is present
+  - Bypass the required file check
+ */
+const commandsToIgnore = [undefined, 'new', '--help', '-h', '--version', '-V']
+const isGlobal = commandsToIgnore.includes(process.argv[2])
+
+const requiredFiles = [
+  'html.js',
+  'pages/_template.js'
+].map(function(file) {
+  // Relative to cwd
+  return sysPath.join(cwd, file)
+})
 
 fs.access(localPath, function (error) {
   if (error) {
-    useGlobalGatsby()
+    if (!isGlobal) {
+      console.error(
+        "A local install of Gatsby was not found.\n" +
+        "You should save Gatsby as a site dependency e.g. npm install --save gatsby\n"
+      )
+      process.exit()
+    } else {
+      console.warn('Proceeding with global Gatsby package.\n');
+      fs.realpath(__dirname, function (err, real) {
+        if (err) throw err
+        loadGatsby(sysPath.join(real, '..', cliFile))
+      })
+    }
   } else {
+    if (!isGlobal && !checkRequiredFiles(requiredFiles)) {
+      process.exit()
+    }
+
     try {
       loadGatsby(localPath)
     } catch(error) {
-      console.log(
-        'Gatsby: Local install exists but failed to load it.'
+      console.error(
+        'Gatsby: Local install exists but failed to load it.',
+        error
       )
-      console.log(error)
     }
   }
 })
