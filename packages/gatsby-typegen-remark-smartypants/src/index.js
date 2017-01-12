fdcf888c5cc8a2235fea9e3658f1305b4c20d90b
Kyle Mathews <mathews.kyle@gmail.com>
2017-01-11 20:43:54 -0800

    Add smartypants plugin for remark

---
+++ packages/gatsby-typegen-remark-smartypants/src/index.js
@@ -0,0 +1,14 @@
+const retext = require(`retext`)
+const visit = require(`unist-util-visit`)
+const smartypants = require(`retext-smartypants`)
+
+module.exports = ({ markdownAST }) => {
+  visit(markdownAST, `text`, (node) => {
+    const processedText = String(
+      retext()
+      .use(smartypants)
+      .process(node.value)
+    )
+    node.value = processedText
+  })
+}
