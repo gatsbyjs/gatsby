6e55685f4cf96f067335f335f1c558ddfafeef4a
Kyle Mathews <mathews.kyle@gmail.com>
2017-01-11 19:28:34 -0800

    Add new remark plugin to make iframe embeds responsive

---
+++ packages/gatsby-typegen-remark-responsive-iframe/src/index.js
@@ -0,0 +1,55 @@
+const visit = require(`unist-util-visit`)
+const cheerio = require(`cheerio`)
+const Promise = require(`bluebird`)
+const _ = require(`lodash`)
+
+module.exports = ({ markdownAST, pluginOptions = {} }) => (
+  new Promise((resolve) => {
+    const defaults = {
+      wrapperStyle: ``,
+    }
+    const options = _.defaults(pluginOptions, defaults)
+    visit(markdownAST, `html`, (node) => {
+      const $ = cheerio.load(node.value)
+      const iframe = $(`iframe`)
+      if (iframe) {
+        const width = iframe.attr(`width`)
+        const height = iframe.attr(`height`)
+        const src = iframe.attr(`src`)
+        if (width && height) {
+          $(`iframe`).attr(`style`, `
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+          `)
+          $(`iframe`).attr(`width`, null).attr(`height`, null)
+          const newIframe = $.html()
+          const rawHTML = `
+          <div
+            class="gatsby-resp-iframe-wrapper"
+            style="padding-bottom: ${(height/width)*100}%; position: relative; height: 0; overflow: hidden;${options.wrapperStyle}"
+          >
+            ${newIframe}
+          </div>
+          `
+
+          node.data = {
+            hChildren: [{ type: `raw`, value: rawHTML }],
+          }
+          // Set type to unknown so mdast-util-to-hast will treat this node as a
+          // div not an iframe — it gets quite confused otherwise.
+          node.type = `unknown`
+
+          // Also apparently, for html node types, you have to delete the value
+          // in order for mdast-util-to-hast to use hChildren. If even if
+          // you change the node type to unknown...
+          delete node.value
+        }
+      }
+    })
+
+    return resolve()
+  })
+)
