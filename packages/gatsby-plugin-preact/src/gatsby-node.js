f19f649f80bb7ed4699d710a258e2a21ec45cb1d
Kyle Mathews <mathews.kyle@gmail.com>
2017-01-23 17:49:27 -0800

    Add gatsby-plugin-preact

---
+++ packages/gatsby-plugin-preact/src/gatsby-node.js
@@ -0,0 +1,14 @@
+exports.modifyWebpackConfig = ({ args }) => {
+  const { config, stage } = args
+  // Requiring the server version of React-dom is hardcoded right now
+  // in the development server. So we'll just avoid loading Preact there
+  // for now.
+  if (stage !== `develop-html`) {
+    config._config.resolve.alias = {
+      react: `preact-compat`,
+      'react-dom': `preact-compat`,
+    }
+  }
+
+  return config
+}
