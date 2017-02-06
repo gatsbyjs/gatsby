a014b2de6878cc63aca34d47803b103a9216d9ae
Kyle Mathews <mathews.kyle@gmail.com>
2017-02-06 13:06:54 -0800

    Stop trying to use inlining css component for now
    Getting it + Webpack to work together to require the styles.css file
    automatically is... problematic. Webpack wants everything defined
    statically. Probably easiest way to do this is make this a plugin where
    the plugin injects the component into the <head>.

--- www/html.js
+++ www/html.js
@@ -3,10 +3,22 @@ import React from 'react'
 import { prefixLink } from 'gatsby-helpers'
 import { TypographyStyle } from 'react-typography'
 import typography from './utils/typography'
-import InlineCSS from 'gatsby-inline-css'
+
+let stylesStr
+if (process.env.NODE_ENV === `production`) {
+  try {
+    stylesStr = require(`!raw-loader!./public/styles.css`)
+  } catch (e) {
+    console.log(e)
+  }
+}
 
 module.exports = React.createClass({
   render () {
+    let css
+    if (process.env.NODE_ENV === `production`) {
+      css = <style id="gatsby-inlined-css" dangerouslySetInnerHTML={{ __html: stylesStr }} />
+    }
     return (
       <html lang="en">
         <head>
@@ -23,7 +35,7 @@ module.exports = React.createClass({
           <link rel="icon" type="image/png" sizes="96x96" href={require("images/favicons/favicon-96x96.png")} />
           <link rel="icon" type="image/png" sizes="16x16" href={require("images/favicons/favicon-16x16.png")} />
           <TypographyStyle typography={typography} />
-          <InlineCSS />
+          {css}
         </head>
         <body>
           <div id="react-mount" dangerouslySetInnerHTML={{ __html: this.props.body }} />
