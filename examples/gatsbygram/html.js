206e2dec799212a0cb7a91d60285c7dceb012925
Kyle Mathews <mathews.kyle@gmail.com>
2017-02-08 16:54:23 -0800

    Add script to build gatsbygram site on Netlify (#667)
    * Add script to build gatsbygram site on Netlify
    
    * Update site name
    
    * Update loading css

--- examples/gatsbygram/html.js
+++ examples/gatsbygram/html.js
@@ -4,11 +4,28 @@ import DocumentTitle from 'react-document-title'
 //import { prefixLink } from 'gatsby-helpers'
 import { GoogleFont, TypographyStyle } from 'react-typography'
 import typography from './utils/typography'
-import HTMLStyles from '.intermediate-representation/html-styles'
+
+let stylesStr
+if (process.env.NODE_ENV === `production`) {
+  try {
+    stylesStr = require(`!raw-loader!./public/styles.css`)
+  } catch (e) {
+    console.log(e)
+  }
+}
 
 module.exports = React.createClass({
   render () {
     const title = DocumentTitle.rewind()
+    let css
+    if (process.env.NODE_ENV === `production`) {
+      css = (
+        <style
+          id="gatsby-inlined-css"
+          dangerouslySetInnerHTML={{ __html: stylesStr }}
+        />
+      )
+    }
 
     return (
       <html lang="en">
@@ -19,9 +36,9 @@ module.exports = React.createClass({
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
-          <title>Image Gallery Gatsby Example Site</title>
+          <title>GatsbyGram</title>
           <TypographyStyle typography={typography} />
-          <HTMLStyles />
+          {css}
           {this.props.headComponents}
         </head>
         <body>
