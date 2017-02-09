50799574aa6b6e60ba5042b4560d1c2d497c0d9a
Kyle Mathews <mathews.kyle@gmail.com>
2017-02-08 17:25:39 -0800

    Add favicon, meta description, preload fonts

--- examples/gatsbygram/html.js
+++ examples/gatsbygram/html.js
@@ -4,6 +4,7 @@ import DocumentTitle from 'react-document-title'
 //import { prefixLink } from 'gatsby-helpers'
 import { GoogleFont, TypographyStyle } from 'react-typography'
 import typography from './utils/typography'
+import logo from '!file-loader!./images/logo.png'
 
 let stylesStr
 if (process.env.NODE_ENV === `production`) {
@@ -30,8 +31,16 @@ module.exports = React.createClass({
     return (
       <html lang="en">
         <head>
+          <link rel="preload" href={`/static/space-mono-latin-700.eadcd2d5.woff2`} as="font" crossOrigin />
+          <link rel="preload" href={`/static/space-mono-latin-400.a8338881.woff2`} as="font" crossOrigin />
           <meta charSet="utf-8" />
+          <meta name="description" content="GatsbyGram: A clone of Instagram built with GatsbyJS" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <link
+            rel="icon"
+            type="image/png"
+            href={logo}
+          />
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1.0"
