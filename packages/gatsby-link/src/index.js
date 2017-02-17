4db449b17af938e29538d20ffeef2abc01bfcfc1
Kyle Mathews <mathews.kyle@gmail.com>
2017-02-17 14:21:54 -0800

    Preload components/data bundles for all links regardless of browser
    Gatsby core won't have SW precaching built-in in the next release so
    this is necessary for a fast site. And even if a browser is precaching
    bundles, pulling bundles from the SW and evaling them still adds latency
    to page transitions. Doing this work ahead of time in spare moments
    means that page transitions will be always be fast.

--- packages/gatsby-link/src/index.js
+++ packages/gatsby-link/src/index.js
@@ -13,30 +13,26 @@ const GatsbyLink = React.createClass({
   propTypes: {
     to: React.PropTypes.string.isRequired,
   },
+
   componentDidMount () {
-    // Only enable prefetching of Link resources in production and for browsers that
-    // don't support service workers *cough* Safari/IE *cough*.
-    if (
-      process.env.NODE_ENV === `production` && !(`serviceWorker` in navigator)
-    ) {
-      const routes = window.gatsbyRootRoute
-      const { createMemoryHistory } = require(`history`)
-      const matchRoutes = require(`react-router/lib/matchRoutes`)
-      const getComponents = require(`react-router/lib/getComponents`)
+    const routes = window.gatsbyRootRoute
+    const { createMemoryHistory } = require(`history`)
+    const matchRoutes = require(`react-router/lib/matchRoutes`)
+    const getComponents = require(`react-router/lib/getComponents`)
 
-      const createLocation = createMemoryHistory().createLocation
+    const createLocation = createMemoryHistory().createLocation
 
-      if (typeof routes !== `undefined`) {
-        matchRoutes([routes], createLocation(this.props.to), (
-          error,
-          nextState
-        ) => {
-          getComponents(nextState, () =>
-            console.log(`loaded assets for ${this.props.to}`))
-        })
-      }
+    if (typeof routes !== `undefined`) {
+      matchRoutes([routes], createLocation(this.props.to), (
+        error,
+        nextState
+      ) => {
+        getComponents(nextState, () =>
+          console.log(`loaded assets for ${this.props.to}`))
+      })
     }
   },
+
   render () {
     const to = linkPrefix + this.props.to
     return <Link {...this.props} to={to} />
