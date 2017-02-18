1a2e5b5071973bda8660fa075c1cf71a8c2f0be8
Kyle Mathews <mathews.kyle@gmail.com>
2017-02-17 16:05:53 -0800

    Revert gatsby-link change
    Overly expensive for pages with lots of links.

--- packages/gatsby-link/src/index.js
+++ packages/gatsby-link/src/index.js
@@ -13,26 +13,32 @@ const GatsbyLink = React.createClass({
   propTypes: {
     to: React.PropTypes.string.isRequired,
   },
-
   componentDidMount () {
-    const routes = window.gatsbyRootRoute
-    const { createMemoryHistory } = require(`history`)
-    const matchRoutes = require(`react-router/lib/matchRoutes`)
-    const getComponents = require(`react-router/lib/getComponents`)
+    // Only enable prefetching of Link resources in production and for browsers that
+    // don't support service workers *cough* Safari/IE *cough*.
+    if (
+      process.env.NODE_ENV === `production` &&
+        !(`serviceWorker` in navigator) ||
+      window.location.protocol !== `https:`
+    ) {
+      const routes = window.gatsbyRootRoute
+      const { createMemoryHistory } = require(`history`)
+      const matchRoutes = require(`react-router/lib/matchRoutes`)
+      const getComponents = require(`react-router/lib/getComponents`)
 
-    const createLocation = createMemoryHistory().createLocation
+      const createLocation = createMemoryHistory().createLocation
 
-    if (typeof routes !== `undefined`) {
-      matchRoutes([routes], createLocation(this.props.to), (
-        error,
-        nextState
-      ) => {
-        getComponents(nextState, () =>
-          console.log(`loaded assets for ${this.props.to}`))
-      })
+      if (typeof routes !== `undefined`) {
+        matchRoutes([routes], createLocation(this.props.to), (
+          error,
+          nextState,
+        ) => {
+          getComponents(nextState, () =>
+            console.log(`loaded assets for ${this.props.to}`))
+        })
+      }
     }
   },
-
   render () {
     const to = linkPrefix + this.props.to
     return <Link {...this.props} to={to} />
