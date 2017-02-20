583b1d86c93d18cbabf39d90a389e9e42cd58209
Kyle Mathews <mathews.kyle@gmail.com>
2017-02-20 09:59:19 -0800

    Add gatsby-plugin-offline
    This adds a basic setup for handling loading a website with a service
    worker generated by sw-precache. It uses the AppShell technique to load
    first a simple HTML shell of the website (which would generally show the
    header for most sites) which then loads the Javascript for the actual
    page and renders that.

---
+++ packages/gatsby-plugin-offline/src/gatsby-node.js
@@ -0,0 +1,50 @@
+const Promise = require(`bluebird`)
+const precache = require(`sw-precache`)
+const path = require(`path`)
+
+exports.createPages = () => [
+  {
+    path: `/offline-plugin-app-shell-fallback/`,
+    component: path.resolve(`./app-shell.js`),
+  },
+]
+
+exports.postBuild = () => new Promise((resolve, reject) => {
+  const rootDir = `public`
+
+  const options = {
+    staticFileGlobs: [
+      `${rootDir}/**/*.{js,woff2}`,
+      `${rootDir}/index.html`,
+      `${rootDir}/manifest.json`,
+      `${rootDir}/offline-plugin-app-shell-fallback/index.html`,
+    ],
+    stripPrefix: rootDir,
+    navigateFallback: `/offline-plugin-app-shell-fallback/index.html`,
+    cacheId: `gatsby-plugin-offline`,
+    dontCacheBustUrlsMatching: /(.*.woff2|.*.js)/,
+    runtimeCaching: [
+      {
+        urlPattern: /.*.png/,
+        handler: `fastest`,
+      },
+      {
+        urlPattern: /.*.jpg/,
+        handler: `fastest`,
+      },
+      {
+        urlPattern: /.*.jpeg/,
+        handler: `fastest`,
+      },
+    ],
+    skipWaiting: false,
+  }
+
+  precache.write(`public/sw.js`, options, err => {
+    if (err) {
+      reject(err)
+    } else {
+      resolve()
+    }
+  })
+})
---
+++ packages/gatsby-plugin-offline/src/gatsby-ssr.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+exports.modifyPostBodyComponents = () => [
+  (
+    <script
+    dangerouslySetInnerHTML={{
+        __html: (
+          `
+        if ('serviceWorker' in navigator) {
+          // Delay registration until after the page has loaded, to ensure that
+          // our precaching requests don't degrade the first visit experience.
+          // See https://developers.google.com/web/fundamentals/instant-and-offline/service-worker/registration
+          window.addEventListener('load', function() {
+            navigator.serviceWorker.register('/sw.js');
+          })
+        }
+      `
+        ),
+      }}
+  />
+  ),
+]
