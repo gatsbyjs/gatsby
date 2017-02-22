209952c28f7c4e46d11ae07bca76bf607874351f
Kyle Mathews <mathews.kyle@gmail.com>
2017-02-21 18:35:52 -0800

    WIP commit on 1st blog post

---
+++ docs/blog-posts/2017-02-21-1-0-progress-update-where-came-from-where-going/index.md
@@ -0,0 +1,12 @@
+---
+title: "Gatsby 1.0 progress update: where it came from and where it's going"
+date: "2017-02-21"
+authorId: "Kyle Mathews"
+---
+
+React & Webpack & static content are very very good. Built prototype and
+it worked!
+
+Pull data in, let components do whatever they want, very little
+configuration, intuitive conventions, put all the power in the
+components.
--- www/pages/blog/index.js
+++ www/pages/blog/index.js
@@ -1,14 +1,83 @@
 import React from "react"
 import Link from "gatsby-link"
+import { presets } from "glamor"
 
 import { rhythm } from "utils/typography"
 
 const IndexRoute = React.createClass({
   render () {
+    console.log(`blog posts`, this.props)
+    const blogPosts = this.props.data.allFile.edges.map((edge) => edge.node.children[0])
+    console.log(blogPosts)
     return (
-      <div>
+      <div
+        css={{
+          margin: 0,
+          maxWidth: `100%`,
+          [presets.Tablet]: {
+            margin: `0 auto`,
+            maxWidth: rhythm(26),
+          },
+        }}
+      >
         <h1>Blog</h1>
-        <p>Coming soon</p>
+        {blogPosts.map((post) => {
+          return (
+            <div>
+              <h2
+                css={{
+                  marginBottom: 0,
+                }}
+              >
+                {post.frontmatter.title}
+              </h2>
+              <p
+                css={{
+                  color: `#696861`,
+                }}
+              >
+                {post.excerpt}
+              </p>
+              <div>
+                <img
+                  alt="Gravatar for mathews.kyle@gmail.com"
+                  src="//www.gravatar.com/avatar/e567aa8adbd2d49cd9990ea1ed19d4eb?d=retro&amp;r=g&amp;s=50"
+                  srcSet="//www.gravatar.com/avatar/e567aa8adbd2d49cd9990ea1ed19d4eb?d=retro&amp;r=g&amp;s=100 2x"
+                  height="35" width="35"
+                  css={{
+                    borderRadius: `100%`,
+                    display: `inline-block`,
+                    marginRight: rhythm(1/2),
+                    marginBottom: 0,
+                    verticalAlign: `top`,
+                  }}
+                />
+                <div
+                  css={{
+                    display: `inline-block`,
+                  }}
+                >
+                  <div
+                    css={{
+                      color: `#696861`,
+                      lineHeight: 1.1,
+                    }}
+                  >
+                    <small>Kyle Mathews</small>
+                  </div>
+                  <div
+                    css={{
+                      color: `#696861`,
+                      lineHeight: 1.1,
+                    }}
+                  >
+                    <small><em>{post.frontmatter.date}</em></small>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )
+        })}
       </div>
     )
   },
@@ -18,9 +87,19 @@ export default IndexRoute
 
 export const pageQuery = `
 {
-  site {
-    siteMetadata {
-      title
+  allFile(relativePath: { regex: "/blog-posts.*/" }) {
+    edges {
+      node {
+        children {
+          ... on MarkdownRemark {
+            excerpt
+            frontmatter {
+              title
+              date(formatString: "DD MMMM, YYYY")
+            }
+          }
+        }
+      }
     }
   }
 }
