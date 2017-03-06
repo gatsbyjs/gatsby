352b344852a2183689e1f0a76bf5b86475dbc60a
Kyle Mathews <mathews.kyle@gmail.com>
2017-03-06 14:17:32 -0800

    Just use canary versions of the site until can get bootstrapping worked out

--- scripts/publish-site.sh
+++ scripts/publish-site.sh
@@ -1,9 +1,9 @@
-echo "=== Building ES5 version of Gatsby"
-./node_modules/.bin/lerna bootstrap --npm-client=yarn
-./node_modules/.bin/lerna run build
+#echo "=== Building ES5 version of Gatsby"
+#./node_modules/.bin/lerna bootstrap --npm-client=yarn
+#./node_modules/.bin/lerna run build
 cd www
-#echo "=== Installing the website dependencies"
-#yarn
+echo "=== Installing the website dependencies"
+yarn
 #echo "=== Copying built Gatsby to website."
 #cp -r ../packages/gatsby/dist ./node_modules/gatsby/dist
 echo "=== Building website"
