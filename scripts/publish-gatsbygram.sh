23f1999c124050ed5bdbf9fe1647bc54ae9540e3
Kyle Mathews <mathews.kyle@gmail.com>
2017-03-06 12:16:01 -0800

    Rum lerna bootstrap when publishing sites

--- scripts/publish-gatsbygram.sh
+++ scripts/publish-gatsbygram.sh
@@ -1,12 +1,11 @@
 echo "=== Building ES5 version of Gatsby"
+./node_modules/.bin/lerna bootstrap
 ./node_modules/.bin/lerna run build
 cd examples/gatsbygram
 echo "=== Installing the website dependencies"
 yarn
-ls ./node_modules | grep stable
 echo "=== Overwrite website node_modules with Gatsby's."
 cp -r ../node_modules/* ./node_modules/
-ls ./node_modules | grep stable
 echo "=== Copying built Gatsby to website."
 cp -r ../packages/gatsby/dist ./node_modules/gatsby/dist
 echo "=== Building website"
