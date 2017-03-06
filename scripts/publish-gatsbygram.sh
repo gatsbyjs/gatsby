5e4c5021e6889dfb353529d78b733f2158876eb6
Kyle Mathews <mathews.kyle@gmail.com>
2017-03-06 12:41:56 -0800

    Make website + example sites packages so they get setup with bootstrap w/ latest versions of packages

--- scripts/publish-gatsbygram.sh
+++ scripts/publish-gatsbygram.sh
@@ -2,8 +2,8 @@ echo "=== Building ES5 version of Gatsby"
 ./node_modules/.bin/lerna bootstrap
 ./node_modules/.bin/lerna run build
 cd examples/gatsbygram
-echo "=== Installing the website dependencies"
-yarn
+#echo "=== Installing the website dependencies"
+#yarn
 #echo "=== Copying built Gatsby to website."
 #cp -r ../../packages/gatsby/dist ./node_modules/gatsby/dist
 echo "=== Building website"
