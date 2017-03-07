d70e20f3e4618699f1c250aacb1cee55605df901
Kyle Mathews <mathews.kyle@gmail.com>
2017-03-07 12:00:13 -0800

    Fix another require

--- packages/gatsby-typegen-remark-prismjs/src/index.js
+++ packages/gatsby-typegen-remark-prismjs/src/index.js
@@ -11,7 +11,7 @@ module.exports = ({ markdownAST }) => {
     // (Try to) load languages on demand.
     if (!Prism.languages[node.lang]) {
       try {
-        require("prismjs/components/prism-${node.lang}.js")
+        require(`prismjs/components/prism-${node.lang}.js`)
       } catch (e) {
         // Language wasn't loaded so let's bail.
         return
