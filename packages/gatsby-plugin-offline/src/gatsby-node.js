3f98b323781a7450569c4e4fb10cfc77ddbebc52
Kyle Mathews <mathews.kyle@gmail.com>
2017-03-07 19:19:46 -0800

    Don't use SW for navigation for normal files

--- packages/gatsby-plugin-offline/src/gatsby-node.js
+++ packages/gatsby-plugin-offline/src/gatsby-node.js
@@ -20,6 +20,12 @@ exports.postBuild = () => {
     ],
     stripPrefix: rootDir,
     navigateFallback: `/offline-plugin-app-shell-fallback/index.html`,
+    // Only match URLs without extensions.
+    // So example.com/about/ will pass but
+    // example.com/cheeseburger.jpg will not.
+    // We only want the service worker to handle our "clean"
+    // URLs and not any files hosted on the site.
+    navigateFallbackWhitelist: [/^.*(?!\.\w?$)/],
     cacheId: `gatsby-plugin-offline`,
     // Do cache bust JS URLs until can figure out how to make Webpack's
     // URLs truely content-addressed.
