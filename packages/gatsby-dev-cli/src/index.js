5854cb79043ca7e8f09a202e85c0418951cfb382
Fabien BERNARD <fabien0102@hotmail.com>
2017-03-29 13:04:54 +0000

    [1.0] improve gatsby-dev-cli (#766)
    * Add a fallback on package.json gatsby packages
    
    * Fix eslint, remove debug logs, and deal with errors
    
    * Add a README.md for gatsby-dev-cli

--- packages/gatsby-dev-cli/src/index.js
+++ packages/gatsby-dev-cli/src/index.js
@@ -1,39 +1,37 @@
-const chokidar = require("chokidar")
-const _ = require("lodash")
-const fs = require("fs-extra")
-const syspath = require("path")
+const chokidar = require("chokidar");
+const _ = require("lodash");
+const fs = require("fs-extra");
+const syspath = require("path");
 
 const ignoreRegs = [
   new RegExp(`\/node_modules\/`, "i"),
   new RegExp(`\.git`, "i"),
   new RegExp(`\/src\/`, "i"),
-]
+];
 
 module.exports = (root, packages) => {
   packages.forEach(p => {
-    const prefix = `${root}/packages/${p}`
+    const prefix = `${root}/packages/${p}`;
     chokidar
       .watch(prefix, {
         ignored: [
           (path, stats) => {
-            // console.log("path", path, stats);
-            return _.some(ignoreRegs, reg => reg.test(path))
+            return _.some(ignoreRegs, reg => reg.test(path));
           },
         ],
       })
       .on("all", (event, path) => {
         if (event === "change" || event === "add") {
           // Copy it over local version.
-          // console.log(syspath.relative(prefix, path));
           const newPath = syspath.join(
             `./node_modules/${p}`,
             syspath.relative(prefix, path)
-          )
-          // console.log("new path", newPath);
-          fs.copySync(path, newPath)
-          console.log(`copied ${path} to ${newPath}`)
+          );
+          fs.copy(path, newPath, err => {
+            if (err) console.error(err);
+            console.log(`copied ${path} to ${newPath}`);
+          });
         }
-        // console.log(event, path);
-      })
-  })
-}
+      });
+  });
+};
