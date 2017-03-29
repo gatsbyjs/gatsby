f10fd8c115a861b143fa9854d246b399cd9ab1ae
fabien0102 <fabien0102@planee.fr>
2017-03-29 16:06:43 +0200

    Update build

--- packages/gatsby-dev-cli/index.js
+++ packages/gatsby-dev-cli/index.js
@@ -5,14 +5,13 @@ var _ = require("lodash");
 var fs = require("fs-extra");
 var syspath = require("path");
 
-var ignoreRegs = [new RegExp(`\/node_modules\/`, "i"), new RegExp(`\.git`, "i"), new RegExp(`\/src\/`, "i")];
+var ignoreRegs = [/[\/\\]node_modules[\/\\]/i, /\.git/i, /[\/\\]src[\/\\]/i];
 
 module.exports = function (root, packages) {
   packages.forEach(function (p) {
     var prefix = `${root}/packages/${p}`;
     chokidar.watch(prefix, {
       ignored: [function (path, stats) {
-        // console.log("path", path, stats);
         return _.some(ignoreRegs, function (reg) {
           return reg.test(path);
         });
@@ -20,13 +19,12 @@ module.exports = function (root, packages) {
     }).on("all", function (event, path) {
       if (event === "change" || event === "add") {
         // Copy it over local version.
-        // console.log(syspath.relative(prefix, path));
         var newPath = syspath.join(`./node_modules/${p}`, syspath.relative(prefix, path));
-        // console.log("new path", newPath);
-        fs.copySync(path, newPath);
-        console.log(`copied ${path} to ${newPath}`);
+        fs.copy(path, newPath, function (err) {
+          if (err) console.error(err);
+          console.log(`copied ${path} to ${newPath}`);
+        });
       }
-      // console.log(event, path);
     });
   });
 };
