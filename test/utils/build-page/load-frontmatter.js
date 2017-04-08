9a80a5791918f4be9e58b364bfc90f3c44f65799
Tom Bonnike <bonniketom@gmail.com>
2017-04-08 22:53:50 +0200

    Add test for array literal values in page's metadata

--- test/utils/build-page/load-frontmatter.js
+++ test/utils/build-page/load-frontmatter.js
@@ -12,3 +12,10 @@ test('it works for template literal titles', (t) => {
   const data = loadFrontmatter(pagePath)
   t.is(data.title, 'Bar')
 })
+
+test('it works with array literal values', (t) => {
+  const pagePath = '../../fixtures/template-literal-frontmatter/pages/array-literal.js'
+  const data = loadFrontmatter(pagePath)
+  t.is(data.titles[0], 'My title')
+  t.is(data.titles[1], 'My other title')
+})
