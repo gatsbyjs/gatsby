d771a7cd07f3ffe1a3dc3b13825432991643bfd8
fabien0102 <fabien0102@planee.fr>
2017-04-09 11:29:04 +0200

    Fix responsive image on windows

--- packages/gatsby-typegen-remark-responsive-image/src/index.js
+++ packages/gatsby-typegen-remark-responsive-image/src/index.js
@@ -51,7 +51,10 @@ module.exports = (
         new Promise((resolve, reject) => {
           // Ignore gifs as we can't process them.
           if (isRelativeUrl(node.url) && node.url.slice(-3) !== `gif`) {
-            const imagePath = path.join(markdownNode.parent.dir, node.url);
+            const imagePath = path.posix.join(
+              markdownNode.parent.dir,
+              node.url
+            );
             const imageNode = _.find(files, file => {
               if (file && file.id) {
                 return file.id === imagePath;
