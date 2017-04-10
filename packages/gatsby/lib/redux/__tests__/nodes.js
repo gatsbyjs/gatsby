6747d477431742889aa3e830d14f5c0574ed6366
Kyle Mathews <mathews.kyle@gmail.com>
2017-04-10 14:58:22 -0700

    Add nodes tests

---
+++ packages/gatsby/lib/redux/__tests__/nodes.js
@@ -0,0 +1,46 @@
+const { actions, boundActions } = require("../actions");
+const nodeReducer = require("../reducers/nodes");
+
+describe(`Create and update nodes`, () => {
+  // TODO add these back when we stop directly consoleing errors.
+  // Right now makes tests noisy.
+  //
+  // it(`validates created nodes`, () => {
+  // const action = actions.createNode({
+  // type: `Test`,
+  // });
+  // expect(action.type).toEqual(`VALIDATION_ERROR`);
+  // });
+  // it(`validates updated nodes`, () => {
+  // const action = actions.updateNode({
+  // type: `Test`,
+  // });
+  // expect(action.type).toEqual(`VALIDATION_ERROR`);
+  // });
+
+  it(`allows creating nodes`, () => {
+    const action = actions.createNode({
+      id: `hi`,
+      type: `Test`,
+      pickle: true,
+    });
+    expect(action).toMatchSnapshot();
+    expect(nodeReducer(undefined, action)).toMatchSnapshot();
+  });
+
+  it(`allows updating nodes`, () => {
+    const action = actions.createNode({
+      id: `hi`,
+      type: `Test`,
+      pickle: true,
+    });
+    const updateAction = actions.createNode({
+      id: `hi`,
+      type: `Test`,
+      pickle: false,
+    });
+    let state = nodeReducer(undefined, action);
+    state = nodeReducer(state, updateAction);
+    expect(state["hi"].pickle).toEqual(false);
+  });
+});
