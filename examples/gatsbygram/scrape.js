939fc0fbde1efbb25e4c24c8022365167dfb7dce
fabien0102 <fabien0102@planee.fr>
2017-04-11 19:18:54 +0200

    Deal with pagination

--- examples/gatsbygram/scrape.js
+++ examples/gatsbygram/scrape.js
@@ -34,38 +34,63 @@ const bar = new ProgressBar(
 // Create the images directory
 mkdirp.sync(`./data/images`)
 
-request(
-  `https://www.instagram.com/${username}/media`,
-  { encoding: `utf8` },
-  (err, res, body) => {
-    if (err) console.log(`error: ${err}`)
+let posts = [];
 
-    // Parse posts
-    const posts = JSON.parse(body)
-      .items.filter(item => item.type === `image`)
-      .map(item => {
-        const imgUrl = get(item, `images.standard_resolution.url`, "")
 
-        // Download image locally
-        bar.total++
-        download(imgUrl, `./data/images/${item.code}.jpg`, () => bar.tick())
+// Write json
+const saveJSON = _ => fs.writeFileSync(`./data/posts.json`, JSON.stringify(posts, "", 2))
 
-        // Return a simplify object
-        return {
-          id: item.code,
-          username: item.user.username,
-          avatar: item.user.profile_picture,
-          time: toISO8601(item[`created_time`]),
-          type: item.type,
-          likes: item.likes.count,
-          comment: item.comments.count,
-          text: item.caption.text,
-          media: imgUrl,
-          image: `images/${item.code}.jpg`,
-        }
-      })
+const getPosts = (maxId) => {
+  let url = `https://www.instagram.com/${username}/media`
+  if (maxId) url += `?max_id=${maxId}`
 
-    // Write json
-    fs.writeFileSync(`./data/posts.json`, JSON.stringify(posts, "", 2))
-  }
-)
+  request(
+    url,
+    { encoding: `utf8` },
+    (err, res, body) => {
+      if (err) console.log(`error: ${err}`)
+
+      // Parse posts
+      let lastId
+      JSON.parse(body)
+        .items
+        .filter(item => item.type === `image`)
+        .map(item => {
+          // Parse item to a simple object
+          return {
+            id: get(item, `id`),
+            code: get(item, `code`),
+            username: get(item, `user.username`),
+            avatar: get(item, `user.profile_picture`),
+            time: toISO8601(get(item, `created_time`)),
+            type: get(item, `type`),
+            likes: get(item, `likes.count`),
+            comment: get(item, `comments.count`),
+            text: get(item, `caption.text`),
+            media: get(item, `images.standard_resolution.url`),
+            image: `images/${item.code}.jpg`,
+          }
+        })
+        .forEach(item => {
+          if (posts.length >= 100) return
+
+          // Download image locally
+          bar.total++
+          download(item.media, `./data/images/${item.code}.jpg`, () => bar.tick())
+
+          // Add item to posts
+          posts.push(item)
+
+          // Save lastId for next request
+          lastId = item.id
+        })
+
+      if (posts.length < 100)
+        getPosts(lastId)
+      else
+        saveJSON()
+    }
+  )
+}
+
+getPosts()
