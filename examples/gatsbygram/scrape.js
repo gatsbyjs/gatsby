7547efa094bc9216d1f654a82fb7e6bf126d81a1
fabien0102 <fabien0102@planee.fr>
2017-04-11 15:46:15 +0200

    Update scraper to deal with avatar picture

--- examples/gatsbygram/scrape.js
+++ examples/gatsbygram/scrape.js
@@ -1,8 +1,9 @@
-const { InstagramPosts } = require("instagram-screen-scrape")
-const url = require("url")
 const fs = require("fs")
-const ProgressBar = require("progress")
+const request = require("request")
 const mkdirp = require("mkdirp")
+const ProgressBar = require("progress")
+const { get } = require("lodash")
+const download = require("./utils/download-file")
 
 const username = process.argv[2]
 
@@ -18,13 +19,8 @@ node scrape.js INSTAGRAM_USERNAME
   process.exit()
 }
 
-const download = require("./utils/download-file")
-
-// Create the images directory
-mkdirp.sync(`./data/images`)
-
-// Create the stream
-const streamOfPosts = new InstagramPosts({ username })
+// Convert timestamp to ISO 8601.
+const toISO8601 = (timestamp) => new Date(timestamp * 1000).toJSON()
 
 // Create the progress bar
 const bar = new ProgressBar(
@@ -35,46 +31,37 @@ const bar = new ProgressBar(
   }
 )
 
-const posts = []
-streamOfPosts.on(`data`, post => {
-  // Ignore video files.
-  if (post && post.media && post.media.slice(-3) === `mp4`) {
-    return
-  }
+// Create the images directory
+mkdirp.sync(`./data/images`)
 
-  // Only download the first 100 posts.
-  if (posts.length >= 100) {
-    return
-  }
+request(`https://www.instagram.com/${username}/media`, { encoding: `utf8` }, (err, res, body) => {
+  if (err) console.log(`error: ${err}`)
 
-  // Up total
-  bar.total += 1
+  // Parse posts
+  const posts = JSON.parse(body).items
+    .filter(item => item.type === `image`)
+    .map(item => {
+      const imgUrl = get(item, `images.standard_resolution.url`, "")
 
-  // Convert timestamp to ISO 8601.
-  post.time = new Date(post.time * 1000).toJSON()
+      // Download image locally
+      bar.total++
+      download(imgUrl, `./data/images/${item.code}.jpg`, () => bar.tick())
 
-  const parsedUrl = url.parse(post.media)
-  const splitPathname = parsedUrl.pathname.split(`/`)
-  const newPathname = []
-    .concat(splitPathname[0], splitPathname[1], splitPathname.slice(-1))
-    .join(`/`)
-  const newUrl = `${parsedUrl.protocol}//${parsedUrl.host}${newPathname}`
+      // Return a simplify object
+      return {
+        id: item.code,
+        username: item.user.username,
+        avatar: item.user.profile_picture,
+        time: toISO8601(item[`created_time`]),
+        type: item.type,
+        likes: item.likes.count,
+        comment: item.comments.count,
+        text: item.caption.text,
+        media: imgUrl,
+        image: `images/${item.code}.jpg`
+      }
+    })
 
-  // Delay downloading a bit so our scrapping can catchup so our progressbar
-  // proceeds smoothly.
-  setTimeout(
-    () => {
-      download(newUrl, `./data/images/${post.id}.jpg`, () => bar.tick())
-    },
-    1000
-  )
-
-  // In our json representation of the post, link to the downloaded image.
-  // This will be used in our Gatsby code to access the image.
-  post.image = `images/${post.id}.jpg`
-  posts.push(post)
-})
-
-streamOfPosts.on(`end`, () => {
-  fs.writeFileSync(`./data/posts.json`, JSON.stringify(posts))
+  // Write json
+  fs.writeFileSync(`./data/posts.json`, JSON.stringify(posts, "", 2))
 })
