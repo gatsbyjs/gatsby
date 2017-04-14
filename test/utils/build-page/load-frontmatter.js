028790a42f101964a8539aec80883c262977cc8d
Kyle Mathews <mathews.kyle@gmail.com>
2017-04-14 14:12:08 -0700

    Change output filename for extracted styles on the HTML/JS build steps
    Fixes #798
    
    We leave CSS Modules running in the html/js build steps so that
    JavaScript has access to the generated classnames. But as a side effect,
    we're still extracting styles during these steps and (before this PR)
    were overwritting the styles.css file generated in the build-css step.
    
    This PR changes the output filename of the extracted styles in
    build-html and build-js to .styles.css to hide them away.

--- test/utils/build-page/load-frontmatter.js
+++ test/utils/build-page/load-frontmatter.js
@@ -1,27 +1,31 @@
-import loadFrontmatter from '../../../lib/utils/build-page/load-frontmatter'
 import test from 'ava'
+import loadFrontmatter from '../../../lib/utils/build-page/load-frontmatter'
 
 test('it works for string literal titles', (t) => {
-  const pagePath = '../../fixtures/javascript-pages-frontmatter/pages/string-literal.js'
+  const pagePath =
+    '../../fixtures/javascript-pages-frontmatter/pages/string-literal.js'
   const data = loadFrontmatter(pagePath)
   t.is(data.title, 'Foo')
 })
 
 test('it works for template literal titles', (t) => {
-  const pagePath = '../../fixtures/javascript-pages-frontmatter/pages/template-literal.js'
+  const pagePath =
+    '../../fixtures/javascript-pages-frontmatter/pages/template-literal.js'
   const data = loadFrontmatter(pagePath)
   t.is(data.title, 'Bar')
 })
 
 test('it works with array literal values', (t) => {
-  const pagePath = '../../fixtures/javascript-pages-frontmatter/pages/array-literal.js'
+  const pagePath =
+    '../../fixtures/javascript-pages-frontmatter/pages/array-literal.js'
   const data = loadFrontmatter(pagePath)
   t.is(data.titles[0], 'My title')
   t.is(data.titles[1], 'My other title')
 })
 
 test('it works with object literal values', (t) => {
-  const pagePath = '../../fixtures/javascript-pages-frontmatter/pages/object-literal.js'
+  const pagePath =
+    '../../fixtures/javascript-pages-frontmatter/pages/object-literal.js'
   const data = loadFrontmatter(pagePath)
   t.is(data.titles.main, 'My title')
   t.is(data.titles.sub, 'My other title')
