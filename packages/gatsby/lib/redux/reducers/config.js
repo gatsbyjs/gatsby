d26e24f3adf4f3d38972fafa37af217af1df2897
Kyle Mathews <mathews.kyle@gmail.com>
2017-04-14 18:17:19 -0700

    fix typo in error message

--- packages/gatsby/lib/redux/reducers/config.js
+++ packages/gatsby/lib/redux/reducers/config.js
@@ -1,29 +1,29 @@
-const Joi = require("joi");
-const chalk = require("chalk");
+const Joi = require("joi")
+const chalk = require("chalk")
 
-const { gatsbyConfigSchema } = require("../../joi-schemas/joi");
+const { gatsbyConfigSchema } = require("../../joi-schemas/joi")
 
 module.exports = (state = {}, action) => {
   switch (action.type) {
     case "SET_SITE_CONFIG":
       // Validate the config.
-      const result = Joi.validate(action.payload, gatsbyConfigSchema);
+      const result = Joi.validate(action.payload, gatsbyConfigSchema)
       // TODO use Redux for capturing errors from different
       // parts of Gatsby so a) can capture richer errors and b) be
       // more flexible how to display them.
       if (result.error) {
         console.log(
-          chalk.blue.bgYellow(`The site's gatsby.config.js failed validation`)
-        );
-        console.log(chalk.bold.red(result.error));
-        console.log(action.payload);
-        throw new Error(`The site's gatsby.config.js failed validation`);
-        return;
+          chalk.blue.bgYellow(`The site's gatsby-config.js failed validation`)
+        )
+        console.log(chalk.bold.red(result.error))
+        console.log(action.payload)
+        throw new Error(`The site's gatsby-config.js failed validation`)
+        return
       }
       return {
         ...action.payload,
-      };
+      }
     default:
-      return state;
+      return state
   }
-};
+}
