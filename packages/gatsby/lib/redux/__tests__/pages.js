1206e21ba5c00e514fc5f88f77c15eeef59712f9
Kyle Mathews <mathews.kyle@gmail.com>
2017-04-17 11:36:22 -0700

    Update tests (#813)

--- packages/gatsby/lib/redux/__tests__/pages.js
+++ packages/gatsby/lib/redux/__tests__/pages.js
@@ -1,16 +1,16 @@
-const { store, reducer } = require("../index");
-const { actions, boundActions } = require("../actions");
+const reducer = require("../reducers/pages")
+const { actions } = require("../actions")
 
 describe(`Add pages`, () => {
   it(`allows you to add pages`, () => {
     const action = actions.upsertPage({
       path: `/hi/`,
       component: `/whatever/index.js`,
-    });
-    const state = reducer({ pages: [] }, action);
-    expect(action).toMatchSnapshot();
-    expect(state).toMatchSnapshot();
-  });
+    })
+    const state = reducer(undefined, action)
+    expect(action).toMatchSnapshot()
+    expect(state).toMatchSnapshot()
+  })
 
   it(`allows you to add pages with context`, () => {
     const action = actions.upsertPage({
@@ -19,42 +19,42 @@ describe(`Add pages`, () => {
       context: {
         id: 123,
       },
-    });
-    const state = reducer({ pages: [] }, action);
-    expect(action).toMatchSnapshot();
-    expect(state).toMatchSnapshot();
-  });
+    })
+    const state = reducer(undefined, action)
+    expect(action).toMatchSnapshot()
+    expect(state).toMatchSnapshot()
+  })
 
   it(`allows you to add multiple pages`, () => {
     const action = actions.upsertPage({
       path: `/hi/`,
       component: `/whatever/index.js`,
-    });
+    })
     const action2 = actions.upsertPage({
       path: `/hi/pizza/`,
       component: `/whatever/index.js`,
-    });
-    let state = reducer({ pages: [] }, action);
-    state = reducer(state, action2);
-    expect(state).toMatchSnapshot();
-    expect(state.pages.length).toEqual(2);
-  });
+    })
+    let state = reducer(undefined, action)
+    state = reducer(state, action2)
+    expect(state).toMatchSnapshot()
+    expect(state.length).toEqual(2)
+  })
 
   it(`allows you to update existing pages (based on path)`, () => {
     const action = actions.upsertPage({
       path: `/hi/`,
       component: `/whatever/index.js`,
-    });
+    })
 
     // Change the component
     const action2 = actions.upsertPage({
       path: `/hi/`,
       component: `/whatever2/index.js`,
-    });
+    })
 
-    let state = reducer({ pages: [] }, action);
-    state = reducer(state, action2);
-    expect(state).toMatchSnapshot();
-    expect(state.pages.length).toEqual(1);
-  });
-});
+    let state = reducer(undefined, action)
+    state = reducer(state, action2)
+    expect(state).toMatchSnapshot()
+    expect(state.length).toEqual(1)
+  })
+})
