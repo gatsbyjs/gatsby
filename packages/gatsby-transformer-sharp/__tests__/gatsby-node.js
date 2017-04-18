cf48581c1edf214e4892c3822fd587467c4d3ebd
Kyle Mathews <mathews.kyle@gmail.com>
2017-04-18 16:35:50 -0700

    Add a test for transformer-sharp

---
+++ packages/gatsby-transformer-sharp/__tests__/gatsby-node.js
@@ -0,0 +1,27 @@
+const Promise = require("bluebird")
+
+const { onNodeCreate } = require("../src/gatsby-node")
+
+describe(`Process image nodes correctly`, () => {
+  it(`correctly creates an ImageSharp node from a file image node`, async () => {
+    const node = {
+      id: "whatever",
+      contentDigest: "whatever",
+      extension: `png`,
+      children: [],
+    }
+    const createNode = jest.fn()
+    const updateNode = jest.fn()
+    const boundActionCreators = { createNode, updateNode }
+
+    await onNodeCreate({
+      node,
+      boundActionCreators,
+    }).then(() => {
+      expect(createNode.mock.calls).toMatchSnapshot()
+      expect(updateNode.mock.calls).toMatchSnapshot()
+      expect(createNode).toHaveBeenCalledTimes(1)
+      expect(updateNode).toHaveBeenCalledTimes(1)
+    })
+  })
+})
--- packages/gatsby-transformer-sharp/src/gatsby-node.js
+++ packages/gatsby-transformer-sharp/src/gatsby-node.js
@@ -2,6 +2,7 @@ const _ = require("lodash")
 
 async function onNodeCreate({ node, boundActionCreators }) {
   const { createNode, updateNode } = boundActionCreators
+
   const extensions = ["jpeg", "jpg", "png", "webp", "tif", "tiff", "svg"]
   if (!_.includes(extensions, node.extension)) {
     return
@@ -20,6 +21,8 @@ async function onNodeCreate({ node, boundActionCreators }) {
   node.children = node.children.concat([imageNode.id])
   updateNode(node)
   createNode(imageNode)
+
+  return
 }
 
 exports.onNodeCreate = onNodeCreate
