163dca9b474926af9377e503fb4747a969a5ece0
Kyle Mathews <mathews.kyle@gmail.com>
2017-04-21 12:19:48 -0700

    This was leftover code which started causing trouble with caching

--- packages/gatsby/lib/schema/build-node-types.js
+++ packages/gatsby/lib/schema/build-node-types.js
@@ -131,7 +131,6 @@ module.exports = async () =>
                 name: _.camelCase(`${typeName} field`),
                 type: nodeType.nodeObjectType,
                 resolve: (node, a, context, { fieldName }) => {
-                  const fieldSelector = `${node.___path}.${fieldName}`
                   let fieldValue = node[fieldName]
                   const sourceFileNode = _.find(
                     getNodes(),
