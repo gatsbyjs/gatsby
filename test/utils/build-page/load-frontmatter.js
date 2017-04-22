d1c2d411b8ee94b9f106f6aa2f66aee7a7059250
Minh Nguyen <minhnguyenxx@gmail.com>
2017-04-22 22:43:19 +0100

    Support page data declared as named exports (#838)

--- test/utils/build-page/load-frontmatter.js
+++ test/utils/build-page/load-frontmatter.js
@@ -8,9 +8,23 @@ test('it works for string literal titles', t => {
   t.is(data.title, 'Foo')
 })
 
+test('it works for string literal titles declared as a named export', t => {
+  const pagePath =
+    '../../fixtures/javascript-pages-frontmatter/pages/string-literal-named-export.js'
+  const data = loadFrontmatter(pagePath)
+  t.is(data.title, 'Foo')
+})
+
 test('it works for template literal titles', t => {
   const pagePath =
     '../../fixtures/javascript-pages-frontmatter/pages/template-literal.js'
+  const data = loadFrontmatter(pagePath)
+  t.is(data.title, 'Bar')
+})
+
+test('it works for template literal titles declared as a named export', t => {
+  const pagePath =
+    '../../fixtures/javascript-pages-frontmatter/pages/template-literal-named-export.js'
   const data = loadFrontmatter(pagePath)
   t.is(data.title, 'Bar')
 })
@@ -23,9 +37,25 @@ test('it works with array literal values', t => {
   t.is(data.titles[1], 'My other title')
 })
 
+test('it works with array literal values declared as a named export', t => {
+  const pagePath =
+    '../../fixtures/javascript-pages-frontmatter/pages/array-literal-named-export.js'
+  const data = loadFrontmatter(pagePath)
+  t.is(data.titles[0], 'My title')
+  t.is(data.titles[1], 'My other title')
+})
+
 test('it works with object literal values', t => {
   const pagePath =
     '../../fixtures/javascript-pages-frontmatter/pages/object-literal.js'
+  const data = loadFrontmatter(pagePath)
+  t.is(data.titles.main, 'My title')
+  t.is(data.titles.sub, 'My other title')
+})
+
+test('it works with object literal values declared as a named export', t => {
+  const pagePath =
+    '../../fixtures/javascript-pages-frontmatter/pages/object-literal-named-export.js'
   const data = loadFrontmatter(pagePath)
   t.is(data.titles.main, 'My title')
   t.is(data.titles.sub, 'My other title')
