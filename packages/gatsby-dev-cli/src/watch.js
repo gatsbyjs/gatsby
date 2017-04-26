9f5602f96c0f3a909a7acb020481661847b4f355
Thijs Koerselman <thijskoerselman@gmail.com>
2017-04-26 09:17:41 +0200

    Refactor gatsby-dev-cli structure and improve console output

---
+++ packages/gatsby-dev-cli/src/watch.js
@@ -0,0 +1,41 @@
+const chokidar = require("chokidar")
+const _ = require("lodash")
+const fs = require("fs-extra")
+const syspath = require("path")
+const report = require("yurnalist")
+
+const ignoreRegs = [/[\/\\]node_modules[\/\\]/i, /\.git/i, /[\/\\]src[\/\\]/i]
+
+const debouncedQuit = _.debounce(() => {
+  process.exit()
+}, 500)
+
+function watch(root, packages, scanOnce) {
+  packages.forEach(p => {
+    const prefix = `${root}/packages/${p}`
+    chokidar
+      .watch(prefix, {
+        ignored: [
+          (path, stats) => {
+            return _.some(ignoreRegs, reg => reg.test(path))
+          },
+        ],
+      })
+      .on("all", (event, path) => {
+        if (event === "change" || event === "add") {
+          // Copy it over local version.
+          const newPath = syspath.join(`./node_modules/${p}`, syspath.relative(prefix, path))
+          fs.copy(path, newPath, err => {
+            if (err) report.error(err)
+            report.info(`copied ${path} to ${newPath}`)
+          })
+
+          if (scanOnce) {
+            debouncedQuit()
+          }
+        }
+      })
+  })
+}
+
+module.exports = watch
