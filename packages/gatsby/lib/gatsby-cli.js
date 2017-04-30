795b2001134725349c22559588ec09e7d82a0440
Thijs Koerselman <thijskoerselman@gmail.com>
2017-04-30 13:23:52 +0200

    Format gatsby cli source

--- packages/gatsby/lib/gatsby-cli.js
+++ packages/gatsby/lib/gatsby-cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-const path = require("path")
-const fs = require("fs")
+const path = require(`path`)
+const fs = require(`fs`)
 
 const version = process.version
 const verDigit = Number(version.match(/\d+/)[0])
@@ -8,28 +8,27 @@ const verDigit = Number(version.match(/\d+/)[0])
 if (verDigit < 4) {
   // yurnalist also requires >= 4 so we use console.error here
   console.error(
-    `Gatsby 1.0+ requires node.js v4 or higher (you have ${version}).\nUpgrade node to the latest stable release.`
+    `Gatsby 1.0+ requires node.js v4 or higher (you have ${version}).
+    Upgrade node to the latest stable release.`
   )
   process.exit()
 }
 
-// yurnalist also requires >= node 4 so move it below the check
-const report = require("yurnalist")
-report.info(`bin/gatsby: time since started: ${process.uptime()}`)
+console.log(`bin/gatsby: time since started: ${process.uptime()}`)
 
-console.time("initial loading")
+console.time(`initial loading`)
 global.appStartTime = Date.now()
 
 /*
   Get the locally installed version of gatsby/lib/bin/cli.js from the place where
   this program was executed.
 */
-const cliFile = "dist/bin/cli.js"
-const localPath = path.resolve("node_modules/gatsby", cliFile)
+const cliFile = `dist/bin/cli.js`
+const localPath = path.resolve(`node_modules/gatsby`, cliFile)
 
 const useGlobalGatsby = function() {
   // Never use global install for new and help commands
-  if (["new", "--help"].includes(process.argv[2])) {
+  if ([`new`, `--help`].includes(process.argv[2])) {
     report.error(
       `A local install of Gatsby was not found.
       You should save Gatsby as a site dependency e.g. npm install --save gatsby`
@@ -37,15 +36,15 @@ const useGlobalGatsby = function() {
     process.exit()
   }
 
-  require("./bin/cli")
+  require(`./bin/cli`)
 }
 
 if (fs.existsSync(localPath)) {
   try {
     require(localPath)
-    console.timeEnd("initial loading")
+    console.timeEnd(`initial loading`)
   } catch (error) {
-    report.error("A local install of Gatsby exists but failed to load.")
+    report.error(`A local install of Gatsby exists but failed to load.`)
     report.error(error)
   }
 } else {
