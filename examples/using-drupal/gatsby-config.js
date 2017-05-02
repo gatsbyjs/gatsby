7aa4cd26c2346f98bab6bda6d776dfacab983d82
Kyle Mathews <mathews.kyle@gmail.com>
2017-05-01 18:00:32 -0700

    Add Google Analytics to using-drupal website

--- examples/using-drupal/gatsby-config.js
+++ examples/using-drupal/gatsby-config.js
@@ -13,5 +13,11 @@ module.exports = {
       resolve: `gatsby-source-drupal`,
       options: { baseUrl: `http://dev-gatsbyjs-d8.pantheonsite.io` },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-93349937-2`,
+      },
+    },
   ],
 }
