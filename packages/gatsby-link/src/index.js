25dc2f2d0cd6b74d047cc1e05656432ef2cd133e
Thijs Koerselman <thijskoerselman@gmail.com>
2017-05-03 21:46:17 +0200

    [1.0] Fix gatsby-link for undefined nextState (#907)
    * Fix gatsby-link for undefined nextState
    
    * Solves an error if `/route/child/` is available but `/route/` is not.
    
    * Strip debug code

--- packages/gatsby-link/src/index.js
+++ packages/gatsby-link/src/index.js
@@ -16,8 +16,8 @@ const GatsbyLink = createClass({
     to: PropTypes.string.isRequired,
   },
   componentDidMount() {
-    // Only enable prefetching of Link resources in production and for browsers that
-    // don't support service workers *cough* Safari/IE *cough*.
+    // Only enable prefetching of Link resources in production and for browsers
+    // that don't support service workers *cough* Safari/IE *cough*.
     if (
       (process.env.NODE_ENV === `production` &&
         !(`serviceWorker` in navigator)) ||
@@ -35,9 +35,13 @@ const GatsbyLink = createClass({
           [routes],
           createLocation(this.props.to),
           (error, nextState) => {
-            getComponents(nextState, () =>
-              console.log(`loaded assets for ${this.props.to}`)
-            )
+            if (error) {
+              return console.error(error)
+            }
+
+            if (nextState) {
+              getComponents(nextState)
+            }
           }
         )
       }
