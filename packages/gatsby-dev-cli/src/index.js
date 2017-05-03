ddf945086c147fc4d29d37e3b66bd35acfc6e17f
Thijs Koerselman <thijskoerselman@gmail.com>
2017-05-03 21:52:55 +0200

    [1.0] Clean up CLI code (#906)
    * Refactor gatsby-dev-cli structure and improve console output
    
    * Build to dist and with source maps
    
    * Move bin/gatsby to source folder
    
    * Set correct bin path and rename to gatsby-cli
    
    * Clean up gatsby cli code with modern js
    
    * Also compile gatsby-source-filesystem to dist
    
    * Compile all packages to root again
    
    * Generate inline source maps in development
    
    * Some cleanup
    
    * Format gatsby cli source
    
    * Fix gatsby-dev-cli source mixup
    
    * Delete gitignore
    
    * Make source maps work
    
    * Clean up npmignore and gitignore
    
    * update gatsby-dev cli
    
    * Fix some indentation issues
    
    * Update package build scripts
    
    * Add alias option handling and help to gatsby-dev cli
    
    * Set graphql-skip-limit build output back to where it was
    
    * Enable source maps also for production
    
    * Remove unused dependency
    
    * Run format
    
    * Remove old comment

--- packages/gatsby-dev-cli/src/index.js
+++ packages/gatsby-dev-cli/src/index.js
@@ -1,48 +1,81 @@
-const chokidar = require(`chokidar`)
+#!/usr/bin/env node
+
+const Configstore = require(`configstore`)
+const pkg = require(`../package.json`)
 const _ = require(`lodash`)
+const path = require(`path`)
+const watch = require(`./watch`)
+
+const argv = require(`yargs`)
+  .usage(`Usage: gatsby-dev [options]`)
+  .alias(`q`, `quiet`)
+  .nargs(`q`, 0)
+  .describe(`q`, `Do not output copy file information`)
+  .alias(`s`, `scan-once`)
+  .nargs(`s`, 0)
+  .describe(`s`, `Scan once. Do not start file watch`)
+  .alias(`p`, `set-path-to-repo`)
+  .nargs(`p`, 0)
+  .describe(
+    `p`,
+    `Set path to Gatsby repository.
+You typically only need to configure this once.`
+  )
+  .help(`h`)
+  .alias(`h`, `help`).array(`packages`).argv
+
+const conf = new Configstore(pkg.name)
+
 const fs = require(`fs-extra`)
-const syspath = require(`path`)
+const havePackageJsonFile = fs.existsSync(`package.json`)
 
-const ignoreRegs = [/[\/\\]node_modules[\/\\]/i, /\.git/i, /[\/\\]src[\/\\]/i]
-
-let copied = 0
-const debouncedQuit = _.debounce(() => {
-  console.log(`gatsby-dev copied ${copied} files`)
+if (!havePackageJsonFile) {
+  console.error(`Current folder must have a package.json file!`)
   process.exit()
-}, 500)
-
-module.exports = (root, packages, { scanOnce, quiet }) => {
-  packages.forEach(p => {
-    const prefix = `${root}/packages/${p}`
-    chokidar
-      .watch(prefix, {
-        ignored: [
-          (path, stats) => {
-            return _.some(ignoreRegs, reg => reg.test(path))
-          },
-        ],
-      })
-      .on(`all`, (event, path) => {
-        if (event === `change` || event === `add`) {
-          // Copy it over local version.
-          const newPath = syspath.join(
-            `./node_modules/${p}`,
-            syspath.relative(prefix, path)
-          )
-          // Make any directories that are needed.
-          fs.ensureFileSync(newPath)
-          fs.copy(path, newPath, err => {
-            if (err) console.error(err)
-            copied += 1
-            if (!quiet) {
-              console.log(`copied ${path} to ${newPath}`)
-            }
-          })
-
-          if (scanOnce) {
-            debouncedQuit()
-          }
-        }
-      })
-  })
 }
+
+const localPkg = JSON.parse(fs.readFileSync(`package.json`))
+const packages = Object.keys(
+  Object.assign({}, localPkg.dependencies, localPkg.devDependencies)
+)
+
+const gatsbyPackages = packages.filter(p => p.startsWith(`gatsby`))
+
+const pathToRepo = argv[`set-path-to-repo`]
+if (pathToRepo) {
+  console.log(`Saving path to your Gatsby repo`)
+  conf.set(`gatsby-location`, path.resolve(pathToRepo))
+  process.exit()
+}
+
+const gatsbyLocation = conf.get(`gatsby-location`)
+
+if (!gatsbyLocation) {
+  console.error(`
+You haven't set the path yet to your cloned
+version of Gatsby. Do so now by running:
+
+gatsby-dev --set-path-to-repo /path/to/my/cloned/version/gatsby
+`)
+  process.exit()
+}
+
+if (!argv.packages && _.isEmpty(gatsbyPackages)) {
+  console.error(`
+You haven't got any gatsby dependencies into your current package.json
+
+You probably want to pass in a list of packages to start
+developing on! For example:
+
+gatsby-dev --packages gatsby gatsby-typegen-remark
+
+If you prefer to place them in your package.json dependencies instead,
+gatsby-dev will pick them up.
+`)
+  process.exit()
+}
+
+watch(gatsbyLocation, argv.packages || gatsbyPackages, {
+  quiet: argv.quiet,
+  scanOnce: argv[`scan-once`],
+})
