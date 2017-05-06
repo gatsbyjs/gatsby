83671102fdd490ebf647f687c822624f12210ee1
Kyle Mathews <mathews.kyle@gmail.com>
2017-05-05 18:07:49 -0700

    Fix source-drupal plugin

--- packages/gatsby-source-drupal/src/gatsby-node.js
+++ packages/gatsby-source-drupal/src/gatsby-node.js
@@ -58,8 +58,9 @@ exports.sourceNodes = async (
   console.time(`fetch Drupal data`)
   console.log(`Starting to fetch data from Drupal`)
 
-  const lastFetched = store.getState().status[`gatsby-source-drupal`]
-    .lastFetched
+  const lastFetched = store.getState().status.sourcePlugins[
+    `gatsby-source-drupal`
+  ].lastFetched
 
   let url
   if (lastFetched) {
