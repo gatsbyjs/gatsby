3028b1df922a20b0c5fb4c2748b4c9dc63e663a5
Jason Quense <monastic.panic@gmail.com>
2017-05-10 11:54:50 -0400

    Switch to jest config, clean up paths

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const path = require(`path`)
+const glob = require(`glob`)
+
+const pkgs = glob.sync(`./packages/*`).map(p => p.replace(/^\./, `<rootDir>`))
+
+const distDirs = pkgs.map(p => path.join(p, `dist`))
+
+module.exports = {
+  notify: true,
+  verbose: true,
+  roots: pkgs,
+  modulePathIgnorePatterns: distDirs,
+  coveragePathIgnorePatterns: distDirs,
+  testPathIgnorePatterns: [`/dist/`, `/node_modules/`, `__tests__/fixtures`],
+}
