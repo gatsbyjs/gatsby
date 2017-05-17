166296ff37b109331a02c8a121ffafe8aceccebe
Kyle Mathews <mathews.kyle@gmail.com>
2017-05-17 10:43:55 +0200

    Add some code showing usage of Lost plugin

--- examples/using-postcss-sass/src/pages/index.js
+++ examples/using-postcss-sass/src/pages/index.js
@@ -22,6 +22,14 @@ class Index extends React.Component {
             </li>
           </ul>
         </div>
+        <div className="ColumnSection__grid">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+          <div>5</div>
+          <div>6</div>
+        </div>
       </div>
     )
   }
