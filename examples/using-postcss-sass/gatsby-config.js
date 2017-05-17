341c774d36e2d7829c373e422621de339e2cff06
Kyle Mathews <mathews.kyle@gmail.com>
2017-05-17 10:50:11 +0200

    Add google analytics and offline plugins to site

--- examples/using-postcss-sass/gatsby-config.js
+++ examples/using-postcss-sass/gatsby-config.js
@@ -8,5 +8,12 @@ module.exports = {
         postCssPlugins: [lost()],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-93349937-2`,
+      },
+    },
+    `gatsby-plugin-offline`,
   ],
 }
