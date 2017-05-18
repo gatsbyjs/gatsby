f4a34bb051b01a501bad2bfce875a90e664ea8e8
Scotty Eckenthal <scotty@meetfabric.com>
2017-05-18 16:02:42 -0400

    Adds example boilerplate

---
+++ examples/no-trailing-slashes/gatsby-node.js
@@ -0,0 +1,7 @@
+const _ = require(`lodash`)
+const Promise = require(`bluebird`)
+const path = require(`path`)
+const slash = require(`slash`)
+
+// TODO
+// exports.onUpsertPage = ({ page, boundActionCreators }) => {}
