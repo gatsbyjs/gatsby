85a5ec30f9088a5cdf51676d0c3b5ed09a449ba6
Scotty Eckenthal <scotty@meetfabric.com>
2017-05-18 16:09:14 -0400

    Adds example

--- examples/no-trailing-slashes/gatsby-node.js
+++ examples/no-trailing-slashes/gatsby-node.js
@@ -1,7 +1,25 @@
 const _ = require(`lodash`)
 const Promise = require(`bluebird`)
-const path = require(`path`)
-const slash = require(`slash`)
 
-// TODO
-// exports.onUpsertPage = ({ page, boundActionCreators }) => {}
+// Replacing '/' would result in empty string which is invalid
+const replacePath = path => (path === `/`) ? path : path.replace(/\/$/, ``)
+
+exports.onUpsertPage = ({ page, boundActionCreators }) => {
+  const { upsertPage, deletePageByPath } = boundActionCreators
+
+  return new Promise((resolve, reject) => {
+    // Remove trailing slash
+    const oldPath = page.path
+    page.path = replacePath(page.path)
+    if (page.path !== oldPath) {
+
+      // Remove the old page
+      deletePageByPath(oldPath)
+
+      // Add the new page
+      upsertPage(page)
+    }
+
+    resolve()
+  })
+}
