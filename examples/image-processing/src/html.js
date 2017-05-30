c43c33a80d3223959391677a845cdbd24e61ba82
Florian Kissling <fk@users.noreply.github.com>
2017-05-30 16:41:14 +0200

    Fix typography styles for "image-processing" example build (#1061)

--- examples/image-processing/src/html.js
+++ examples/image-processing/src/html.js
@@ -1,4 +1,7 @@
 import React from "react"
+import { TypographyStyle } from "react-typography"
+
+import typography from "./utils/typography"
 
 module.exports = React.createClass({
   render() {
@@ -19,6 +22,7 @@ module.exports = React.createClass({
             content="width=device-width, initial-scale=1.0"
           />
           <title>Gatsby - Image Processing</title>
+          <TypographyStyle typography={typography} />
         </head>
         <body>
           <div
