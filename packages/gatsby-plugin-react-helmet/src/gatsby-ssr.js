d67253fa7bf66115b97c34db7f85ff84ca2efb4e
Kyle Mathews <mathews.kyle@gmail.com>
2017-06-02 09:10:37 -0700

    Add other helmet entities as components

--- packages/gatsby-plugin-react-helmet/src/gatsby-ssr.js
+++ packages/gatsby-plugin-react-helmet/src/gatsby-ssr.js
@@ -3,7 +3,10 @@ import helmet from "react-helmet"
 exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
   setHeadComponents([
     helmet.title.toComponent(),
-    helmet.meta.toComponent(),
     helmet.link.toComponent(),
+    helmet.meta.toComponent(),
+    helmet.noscript.toComponent(),
+    helmet.script.toComponent(),
+    helmet.style.toComponent(),
   ])
 }
