beddbe515afb404389f0b2ae7ceead5cc79db74b
Kyle Mathews <mathews.kyle@gmail.com>
2017-06-02 10:27:42 -0700

    Add glamor example site

---
+++ examples/using-glamor/src/pages/index.js
@@ -0,0 +1,35 @@
+import React from "react"
+import Link from "gatsby-link"
+
+class IndexPage extends React.Component {
+  render() {
+    return (
+      <div
+        css={{
+          margin: `0 auto`,
+          marginTop: `3rem`,
+          padding: `1.5rem`,
+          maxWidth: 800,
+          color: `red`,
+        }}
+      >
+        <h1>hello world</h1>
+        <p css={{ color: `blue` }}>This is some cool stuff</p>
+        <p
+          css={{
+            color: `pink`,
+            "@media (min-width: 750px)": { color: `blue` },
+          }}
+        >
+          Make the screen narrower, on smaller screens I turn pink!
+        </p>
+        <p css={{ fontStyle: `italic` }}>
+          Edit me in your text editor, changes to styles hot reload!
+        </p>
+        <Link to="/other-page/">Go exploring</Link>
+      </div>
+    )
+  }
+}
+
+export default IndexPage
