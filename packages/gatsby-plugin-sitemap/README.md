c164f8d40746c7e4aa097ac37d2623385dd8bf76
Nicholas Young <nyoung@uptime.ventures>
2017-06-06 16:33:17 -0600

    First draft of gatsby-plugin-sitemap

---
+++ packages/gatsby-plugin-sitemap/README.md
@@ -0,0 +1,3 @@
+# gatsby-plugin-sitemap
+
+Stub README
---
+++ packages/gatsby-plugin-sitemap/src/gatsby-node.js
@@ -0,0 +1,21 @@
+import path from 'path'
+import sitemap from 'sitemap'
+import { defaultOptions, runQuery, writeFile } from './internals'
+
+const publicPath = './public'
+
+exports.onPostBuild = async ({ graphql }, pluginOptions) => {
+  delete pluginOptions.plugins
+
+  const { query, serialize, output, ...rest } = {
+    ...defaultOptions,
+    ...pluginOptions,
+  }
+
+  const map = sitemap.createSitemap(rest)
+  const records = await runQuery(graphql, query)
+  const saved = path.join(publicPath, output)
+   
+  serialize(records).forEach(u => map.add(u))
+  return await writeFile(saved, map.toString())
+}
