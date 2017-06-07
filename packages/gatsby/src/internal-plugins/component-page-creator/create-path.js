bb989a9897c68d6071901f797757740714ae8999
Kyle Mathews <mathews.kyle@gmail.com>
2017-06-06 19:20:19 -0700

    Get relative paths with path.posix

--- packages/gatsby/src/internal-plugins/component-page-creator/create-path.js
+++ packages/gatsby/src/internal-plugins/component-page-creator/create-path.js
@@ -3,7 +3,7 @@ import parsePath from "parse-filepath"
 import path from "path"
 
 module.exports = (basePath: string, filePath: string): string => {
-  const relativePath = path.relative(basePath, filePath)
+  const relativePath = path.posix.relative(basePath, filePath)
   const parsedPath = parsePath(relativePath)
   const { dirname } = parsedPath
   let { name } = parsedPath
