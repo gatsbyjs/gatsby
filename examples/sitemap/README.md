8f1724d5ead3aad22a60c75e25fea089a51dcd29
Nicholas Young <nyoung@uptime.ventures>
2017-06-07 09:47:05 -0600

    [1.0] Sitemap Generator Plugin (#1115)
    * First draft of gatsby-plugin-sitemap
    
    * Sitemap plugin example
    
    * Add README
    
    * Fix example README

---
+++ examples/sitemap/README.md
@@ -0,0 +1,5 @@
+# Sitemap
+
+https://sitemap.gatsbyjs.org
+
+Gatsby example site explaining how to use the sitemap generator plugin.
---
+++ examples/sitemap/src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const IndexRoute = () => (
+  <div>
+    <p>
+      Welcome to the GatsbyJS Sitemap Demo.
+      Visit <a href="/sitemap.xml">to see the generated sitemap.</a>
+    </p>
+  </div>
+)
+
+
+export default IndexRoute
