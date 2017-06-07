4ddb424a305af0c851eb494dc711767cfefdf0cd
Nicholas Young <nyoung@uptime.ventures>
2017-06-07 08:19:01 -0600

    Sitemap plugin example

---
+++ examples/sitemap/gatsby-node.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+exports.onCreateNode = ({ node, boundActionCreators, getNode }) => {
+  const { createNodeField } = boundActionCreators
+
+  if (node.internal.type === 'MarkdownRemark') {
+    const fileNode = getNode(node.parent)
+    let nodeSlug 
+    nodeSlug = ensureSlashes(path.basename(fileNode.relativePath, path.extname(fileNode.relativePath)))
+    if (nodeSlug) {
+      createNodeField({ node, fieldName: 'slug', fieldValue: nodeSlug })
+    }
+  }
+}
+
+function ensureSlashes(slug) {
+  if (slug.charAt(0) !== '/') {
+    slug = '/' + slug
+  }
+
+  if (slug.charAt(slug.length -1) !== '/') {
+    slug = slug + '/'
+  }
+
+  return slug
+}
