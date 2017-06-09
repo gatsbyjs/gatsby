4bca4a035d9dff86a78439c566f8f58d3c2baa63
fabien0102 <fabien0102@planee.fr>
2017-06-09 17:26:40 +0200

    Fix create-path on windows

--- packages/gatsby/src/internal-plugins/component-page-creator/create-path.js
+++ packages/gatsby/src/internal-plugins/component-page-creator/create-path.js
@@ -5,7 +5,6 @@ import path from "path"
 module.exports = (basePath: string, filePath: string): string => {
   const relativePath = path.posix.relative(basePath, filePath)
   const parsedPath = parsePath(relativePath)
-  const { dirname } = parsedPath
   let { name } = parsedPath
   if (name === `index`) {
     name = ``
@@ -13,7 +12,7 @@ module.exports = (basePath: string, filePath: string): string => {
 
   // Use path.posix to force forward slash paths. Otherwise on windows
   // node.js will add backward slashes.
-  const pagePath = path.posix.join(`/`, dirname, name, `/`)
+  const pagePath = path.posix.join(`/`, name, `/`)
 
   return pagePath
 }
