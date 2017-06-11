7f6229a0a2884c095cb1f3780d1a20fc67809283
fabien0102 <fabien0102@planee.fr>
2017-06-11 15:38:05 +0200

    Add ts and tsx into query-watcher

--- packages/gatsby/src/internal-plugins/query-runner/query-watcher.js
+++ packages/gatsby/src/internal-plugins/query-runner/query-watcher.js
@@ -69,7 +69,7 @@ exports.watch = rootDir => {
   }, 100)
 
   watcher = chokidar
-    .watch(`${rootDir}/src/**/*.{js,jsx}`)
+    .watch(`${rootDir}/src/**/*.{js,jsx,ts,tsx}`)
     .on(`change`, path => {
       debounceCompile()
     })
