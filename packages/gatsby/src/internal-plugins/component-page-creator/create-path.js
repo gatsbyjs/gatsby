32e7508b54bbe264b90e1a0714efb56f62e3b85e
fabien0102 <fabien0102@planee.fr>
2017-06-11 11:21:48 +0200

    Add tests and refactor create-path

--- packages/gatsby/src/internal-plugins/component-page-creator/create-path.js
+++ packages/gatsby/src/internal-plugins/component-page-creator/create-path.js
@@ -1,18 +1,12 @@
 // @flow
 import parsePath from "parse-filepath"
 import path from "path"
+import slash from "slash"
 
 module.exports = (basePath: string, filePath: string): string => {
-  const relativePath = path.posix.relative(basePath, filePath)
-  const parsedPath = parsePath(relativePath)
-  let { name } = parsedPath
-  if (name === `index`) {
-    name = ``
-  }
+  const relativePath = path.posix.relative(slash(basePath), slash(filePath))
+  const { dirname, name } = parsePath(relativePath)
+  const parsedName = name === `index` ? `` : name
 
-  // Use path.posix to force forward slash paths. Otherwise on windows
-  // node.js will add backward slashes.
-  const pagePath = path.posix.join(`/`, name, `/`)
-
-  return pagePath
+  return path.posix.join(`/`, dirname, parsedName, `/`)
 }
