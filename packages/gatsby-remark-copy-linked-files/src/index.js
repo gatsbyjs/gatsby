b7aeb66763af8c045db5d92c5852e1a38fa073bb
Kyle Mathews <mathews.kyle@gmail.com>
2017-06-12 21:45:21 -0700

    Add check that we can actually find a linked image file node

--- packages/gatsby-remark-copy-linked-files/src/index.js
+++ packages/gatsby-remark-copy-linked-files/src/index.js
@@ -44,8 +44,8 @@ module.exports = ({ files, markdownNode, markdownAST, getNode }) => {
     visitor(link)
   })
 
-  // Also copy gifs since Sharp can't process them,
-  // and svgs since we exclude them from the image processing pipeline in
+  // Also copy gifs since Sharp can't process them as well as svgs since we
+  // exclude them from the image processing pipeline in
   // gatsby-remark-responsive-image
   visit(markdownAST, `image`, image => {
     const imagePath = path.join(getNode(markdownNode.parent).dir, image.url)
@@ -53,9 +53,12 @@ module.exports = ({ files, markdownNode, markdownAST, getNode }) => {
       if (file && file.absolutePath) {
         return file.absolutePath === imagePath
       }
-      return null
+      return false
     })
-    if (imageNode.extension === `gif` || imageNode.extension === `svg`) {
+    if (
+      imageNode &&
+      (imageNode.extension === `gif` || imageNode.extension === `svg`)
+    ) {
       visitor(image)
     }
   })
