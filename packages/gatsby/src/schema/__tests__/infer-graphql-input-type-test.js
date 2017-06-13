eab7d284f99903644363ad583312abc1f26a20a2
Kyle Mathews <mathews.kyle@gmail.com>
2017-06-13 11:53:45 -0700

    Use custom delimiter when flattening example values for enum fields so easy to convert back

--- packages/gatsby/src/schema/__tests__/infer-graphql-input-type-test.js
+++ packages/gatsby/src/schema/__tests__/infer-graphql-input-type-test.js
@@ -214,7 +214,7 @@ describe(`GraphQL Input args`, () => {
       ],
     })
 
-    expect(Object.keys(fields.foo.type.getFields())[2]).toEqual(`foo___moo`)
+    expect(Object.keys(fields.foo.type.getFields())[2]).toEqual(`foo_moo`)
   })
   it(`Removes specific root fields`, () => {
     let fields = inferInputObjectStructureFromNodes({
