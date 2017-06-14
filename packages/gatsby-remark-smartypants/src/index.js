fffbd8a395ae4593df9c23c8b9ed6df05bd50224
Jason Mitchell <jason@nullog.net>
2017-06-14 12:52:34 -0400

    make plugin options the second argument

--- packages/gatsby-remark-smartypants/src/index.js
+++ packages/gatsby-remark-smartypants/src/index.js
@@ -3,7 +3,7 @@ const visit = require(`unist-util-visit`)
 const smartypants = require(`retext-smartypants`)
 const _ = require('lodash')
 
-module.exports = ({ markdownAST pluginOptions }) => {
+module.exports = ({ markdownAST }, pluginOptions = {}) =>
   visit(markdownAST, `text`, node => {
     const defaultOptions = {}
     const options = _.defaults(pluginOptions, defaultOptions)
