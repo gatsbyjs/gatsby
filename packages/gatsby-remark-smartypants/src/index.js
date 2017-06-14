fe22b515f21b8f79a9d4c53cccb40060913d5c27
Jason Mitchell <jason@nullog.net>
2017-06-14 02:47:41 -0400

    take two, don't commit compiled code ;)

--- packages/gatsby-remark-smartypants/src/index.js
+++ packages/gatsby-remark-smartypants/src/index.js
@@ -1,18 +1,13 @@
 const retext = require(`retext`)
 const visit = require(`unist-util-visit`)
 const smartypants = require(`retext-smartypants`)
-var defaults = require('lodash.defaults');
+const _ = require('lodash')
 
-module.exports = function (_ref) {
-  var markdownAST = _ref.markdownAST;
-  var pluginOptions = _ref.pluginOptions;
-  var defaultOptions = {
-  }
-  
-  var options = defaults(pluginOptions, defaultOptions)
-
-  visit(markdownAST, `text`, function (node) {
-    var processedText = String(retext().use(smartypants, options).process(node.value));
-    node.value = processedText;
-  });
-};
+module.exports = ({ markdownAST pluginOptions }) => {
+  visit(markdownAST, `text`, node => {
+    const defaultOptions = { }
+    const options = _.defaults(pluginOptions, defaultOptions)
+    const processedText = String(retext().use(smartypants, options).process(node.value))
+    node.value = processedText
+  })
+}
