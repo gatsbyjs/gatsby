0a4607511e7ced45c13938a0562a0e3b723564d1
Jason Mitchell <jason@nullog.net>
2017-06-14 02:52:05 -0400

    remove hidden null char at file end

--- packages/gatsby-remark-smartypants/src/index.js
+++ packages/gatsby-remark-smartypants/src/index.js
@@ -5,7 +5,7 @@ const _ = require('lodash')
 
 module.exports = ({ markdownAST pluginOptions }) => {
   visit(markdownAST, `text`, node => {
-    const defaultOptions = { }
+    const defaultOptions = {}
     const options = _.defaults(pluginOptions, defaultOptions)
     const processedText = String(retext().use(smartypants, options).process(node.value))
     node.value = processedText
