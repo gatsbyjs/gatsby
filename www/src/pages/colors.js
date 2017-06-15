3f84a5b9e5e8a8c189c61e494cecb90e68b20db8
Kyle Mathews <mathews.kyle@gmail.com>
2017-06-14 22:27:38 -0700

    Add index label

--- www/src/pages/colors.js
+++ www/src/pages/colors.js
@@ -1,12 +1,32 @@
 import React from "react"
 import colors from "../utils/colors"
-import { rhythm } from "../utils/typography"
+import { rhythm, options } from "../utils/typography"
 import Container from "../components/container"
+import range from "lodash/range"
 
 export default () =>
   <Container>
     <h1 css={{ margin: 0 }}>Colors</h1>
     <div css={{ overflow: `hidden` }}>
+      <div css={{ float: `left`, marginRight: 3, textAlign: `center` }}>
+        <br />
+        <br />
+        <br />
+        <div css={{ marginBottom: 7.5 }} />
+        {range(0, 16).map(a =>
+          <div
+            css={{
+              height: 30,
+              width: 30,
+              textAlign: `center`,
+              fontWeight: `bold`,
+              fontFamily: options.headerFontFamily.join(`,`),
+            }}
+          >
+            {a}
+          </div>
+        )}
+      </div>
       <div css={{ float: `left`, marginRight: 3, textAlign: `center` }}>
         <h4>a</h4>
         {colors.a.map(a =>
