a4d420f586ca1ad0f27a84fb3750042b745ebcef
Kyle Mathews <mathews.kyle@gmail.com>
2017-06-20 14:35:18 -0700

    If loading from service worker and can't find page, probably on old service worker so force refresh

--- packages/gatsby-plugin-offline/src/app-shell.js
+++ packages/gatsby-plugin-offline/src/app-shell.js
@@ -1,12 +1,6 @@
 import React from "react"
 
 class AppShell extends React.Component {
-  componentDidMount() {
-    // TODO check if page exists and if not,
-    // force a hard reload.
-    window.___navigateTo(this.props.location.pathname)
-  }
-
   render() {
     return <div />
   }
