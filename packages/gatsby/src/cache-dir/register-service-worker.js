bbe6a5c7fd28324bf5ce8df345ac97e2182db570
Kyle Mathews <mathews.kyle@gmail.com>
2017-06-20 17:11:45 -0700

    Support path prefixes for service workers

--- packages/gatsby/src/cache-dir/register-service-worker.js
+++ packages/gatsby/src/cache-dir/register-service-worker.js
@@ -1,8 +1,13 @@
 import emitter from "./emitter"
 
+let pathPrefix = `/`
+if (__PREFIX_PATHS__) {
+  pathPrefix = __PATH_PREFIX__
+}
+
 if (`serviceWorker` in navigator) {
   navigator.serviceWorker
-    .register(`sw.js`)
+    .register(`${pathPrefix}sw.js`)
     .then(function(reg) {
       reg.addEventListener(`updatefound`, () => {
         // The updatefound event implies that reg.installing is set; see
