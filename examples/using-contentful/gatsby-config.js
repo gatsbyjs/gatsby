1ef49dd7b4983a3aa09a4a6b42f010b96f3f914e
Khaled Garbaya <khaledgarbaya@gmail.com>
2017-06-27 11:58:17 +0200

    Use localized space

--- examples/using-contentful/gatsby-config.js
+++ examples/using-contentful/gatsby-config.js
@@ -6,8 +6,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `ubriaw6jfhm1`,
-        accessToken: `e481b0f7c5572374474b29f81a91e8ac487bb27d70a6f14dd12142837d8e980a`,
+        spaceId: `rocybtov1ozk`,
+        accessToken: `6f35edf0db39085e9b9c19bd92943e4519c77e72c852d961968665f1324bfc94`,
       },
     },
     `gatsby-transformer-remark`,
