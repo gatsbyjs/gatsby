0ce94328dbc230cc6a5d75c705149495c40ac105
Florian Kissling <fk@users.noreply.github.com>
2017-06-28 00:52:24 +0200

    [1.0] using-remark: preload fonts used on the homepage (#1256)
    * Preload fonts used on the homepage
    
    * Prefetch fonts used on non-index routes

--- examples/using-remark/src/html.js
+++ examples/using-remark/src/html.js
@@ -27,6 +27,38 @@ class HTML extends React.Component {
     return (
       <html lang="en">
         <head>
+          <link
+            rel="preload"
+            href={`/static/spectral-latin-400.bc2de9de.woff2`}
+            as="font"
+            crossOrigin
+          />
+          <link
+            rel="preload"
+            href={`/static/spectral-latin-800.53eca5bf.woff2`}
+            as="font"
+            crossOrigin
+          />
+          <link
+            rel="prefetch"
+            href={`/static/spectral-latin-400italic.b0c97eb5.woff2`}
+          />
+          <link
+            rel="prefetch"
+            href={`/static/spectral-latin-700.601f0e2d.woff2`}
+          />
+          <link
+            rel="prefetch"
+            href={`/static/spectral-latin-700italic.64a7de98.woff2`}
+          />
+          <link
+            rel="prefetch"
+            href={`/static/space-mono-latin-400.a8338881.woff2`}
+          />
+          <link
+            rel="prefetch"
+            href={`/static/space-mono-latin-700.eadcd2d5.woff2`}
+          />
           {this.props.headComponents}
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
