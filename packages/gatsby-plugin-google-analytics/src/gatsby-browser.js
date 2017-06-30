eadbfb51c744b284381bfd381e06f563155a4274
Cameron Manavian <cameron@libretto.io>
2017-06-30 14:49:16 -0700

    remove lastpath backup

--- packages/gatsby-plugin-google-analytics/src/gatsby-browser.js
+++ packages/gatsby-plugin-google-analytics/src/gatsby-browser.js
@@ -1,12 +1,7 @@
-var lastPath = null
 exports.onRouteUpdate = function(newRoute) {
   // Don't track while developing.
   if (process.env.NODE_ENV === `production`) {
-    var newPath = newRoute.pathname || newRoute.location.pathname
-    if (lastPath !== newPath) {
-      lastPath = newPath
-      ga(`set`, `page`, newPath)
-      ga(`send`, `pageview`)
-    }
+    ga(`set`, `page`, (newRoute.location || {}).pathname)
+    ga(`send`, `pageview`)
   }
 }
