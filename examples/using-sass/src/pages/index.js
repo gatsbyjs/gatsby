83c2146fb81d6f77a485595437a7ba8ba3086cc1
Kyle Mathews <mathews.kyle@gmail.com>
2017-06-30 09:50:20 -0700

    small updates to using-sass example site

--- examples/using-sass/src/pages/index.js
+++ examples/using-sass/src/pages/index.js
@@ -19,14 +19,6 @@ class Index extends React.Component {
             </li>
           </ul>
         </div>
-        <div className="ColumnSection__grid">
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
-          <div>4</div>
-          <div>5</div>
-          <div>6</div>
-        </div>
       </div>
     )
   }
