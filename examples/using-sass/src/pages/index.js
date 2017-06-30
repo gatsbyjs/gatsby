47003ea0ef60a303c91ac77028c7726e320fa576
Daniel Farrell <daniel.farrell@mobelux.com>
2017-06-30 12:09:27 -0400

    Add an example for using the sass plugin (#1312)

---
+++ examples/using-sass/src/pages/index.js
@@ -0,0 +1,35 @@
+import React from "react"
+
+class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className="tu">Hi sassy friends</h1>
+        <div className="sass-nav-example">
+          <h2>Nav example</h2>
+          <ul className="pa0 ma0 list">
+            <li>
+              <a href="#">Store</a>
+            </li>
+            <li>
+              <a href="#">Help</a>
+            </li>
+            <li>
+              <a href="#">Logout</a>
+            </li>
+          </ul>
+        </div>
+        <div className="ColumnSection__grid">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+          <div>5</div>
+          <div>6</div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Index
