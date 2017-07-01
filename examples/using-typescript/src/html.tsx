e99809193c0e939a8b86f2e2c0cbaa8d3d4282f3
Kyle Mathews <mathews.kyle@gmail.com>
2017-07-01 10:04:59 -0700

    Fix check if there's a sw plugin added

--- examples/using-typescript/src/html.tsx
+++ examples/using-typescript/src/html.tsx
@@ -2,7 +2,6 @@
 /* tslint:disable no-console */
 
 import * as React from "react";
-import Helmet from "react-helmet";
 
 // Load production style
 let styles: string;
@@ -23,8 +22,6 @@ interface HtmlProps {
 // Use `module.exports` to be compliante with `webpack-require` import method
 module.exports = React.createClass<HtmlProps, void>({
   render() {
-    const head = Helmet.rewind();
-
     const css = (process.env.NODE_ENV === `production`) ?
       <style
         id="gatsby-inlined-css"
@@ -43,9 +40,6 @@ module.exports = React.createClass<HtmlProps, void>({
             name="viewport"
             content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
           />
-          {head.title.toComponent()}
-          {head.meta.toComponent()}
-          {head.link.toComponent()}
           {css}
         </head>
         <body>
