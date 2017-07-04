a10aed50a7d6ec3d7ab00ebde9a06337ad4a0ff5
Kyle Mathews <mathews.kyle@gmail.com>
2017-07-04 12:10:19 -0700

    Guard against calling ga function if it doesn't (yet) exist

--- packages/gatsby-plugin-google-analytics/src/gatsby-browser.js
+++ packages/gatsby-plugin-google-analytics/src/gatsby-browser.js
@@ -1,6 +1,6 @@
 exports.onRouteUpdate = function({ location }) {
   // Don't track while developing.
-  if (process.env.NODE_ENV === `production`) {
+  if (process.env.NODE_ENV === `production` && typeof ga === `function`) {
     ga(`set`, `page`, (location || {}).pathname)
     ga(`send`, `pageview`)
   }
