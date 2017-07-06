6b476266a473f28c2f00060482a4eea91e010400
Scotty Eckenthal <scotty@meetfabric.com>
2017-07-06 08:38:03 -0400

    Retooling gatsby-redirect

--- packages/gatsby-plugin-redirects/src/components/GatsbyRedirect.js
+++ packages/gatsby-plugin-redirects/src/components/GatsbyRedirect.js
@@ -1,31 +1,51 @@
-/* global window */
+import React from "react"
+import PropTypes from "prop-types"
+import { Route, Redirect } from "react-router"
+import { navigateTo } from "gatsby-link"
 
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Route, Redirect } from 'react-router'
+let pathPrefix = ``
+if (__PREFIX_PATHS__) {
+  pathPrefix = __PATH_PREFIX__
+}
 
 class GatsbyRedirect extends React.Component {
 
-    componentDidMount() {
-        if (process.env.NODE_ENV === `production`) {
-            window.___navigateTo(this.props.pathContext.to)
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      to: pathPrefix + props.pathContext.to,
     }
+  }
 
-    render() {
-        return (
-            <Route
-                path="*"
-                render={() => <Redirect to={this.props.pathContext.to} />}
-            />
-        )
+  componentDidMount() {
+    if (process.env.NODE_ENV === `production`) {
+      navigateTo(this.state.to)
     }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.pathContext.to !== nextProps.pathContext.to) {
+      this.setState({
+        to: pathPrefix + nextProps.pathContext.to,
+      })
+    }
+    navigateTo(this.state.to)
+  }
+
+  render() {
+    return (
+      <Route
+        path="*"
+        render={() => <Redirect to={this.state.to} />}
+      />
+    )
+  }
 }
 
 GatsbyRedirect.propTypes = {
-    pathContext: PropTypes.shape({
-        to: PropTypes.string.isRequired,
-    }),
+  pathContext: PropTypes.shape({
+    to: PropTypes.string.isRequired,
+  }).isRequired,
 }
 
 export default GatsbyRedirect
