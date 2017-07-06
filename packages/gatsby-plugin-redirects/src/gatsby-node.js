605f7020945332311c8c9151863b4cdc10d53091
Scotty Eckenthal <scotty@meetfabric.com>
2017-07-06 09:05:12 -0400

    Allows pass of custom component

--- packages/gatsby-plugin-redirects/src/gatsby-node.js
+++ packages/gatsby-plugin-redirects/src/gatsby-node.js
@@ -1,18 +1,23 @@
 import path from 'path'
 
-const component = path.resolve(__dirname, `components/GatsbyRedirect.js`)
+const defaultComponent = path.resolve(__dirname, `components/GatsbyRedirect.js`)
 
-const createPageOps = ({ from: path, to }) => ({
+const createPageOps = ({
+    from: path,
+    to,
+    component = defaultComponent,
+}) => ({
     path,
     component,
     context: { to },
 })
 
-export const createPages = ({ boundActionCreators }, { redirects }) => {
+export const createPages = ({ boundActionCreators }, { redirects, component }) => {
     const { createPage } = boundActionCreators
 
     return new Promise((resolve, reject) => {
         redirects.forEach(entry => {
+            entry.component = entry.component || component
             createPage(createPageOps(entry))
         })
         return resolve()
