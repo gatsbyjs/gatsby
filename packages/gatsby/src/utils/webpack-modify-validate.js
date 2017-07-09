69e73cfbb2602995dba04b5e930cf2bb0315a672
Ian Sinnott <ian@iansinnott.com>
2017-07-09 01:48:05 -0700

    Add a validation whitelist to allow the "stylus" key in webpack config
    This is a special case but we may run in to other cases before upgrading to
    webpack v3. In webpack v1 many plugins and loaders used top-level configuration
    keys to handle custom configuration. webpack-validator does not like this, but
    it does provide the option to whitelist keys, which is exactly what I did here.
    
    I also added a (hopefully) helpful error message to clarify what went wrong in
    the case of an invalid webpack config.

--- packages/gatsby/src/utils/webpack-modify-validate.js
+++ packages/gatsby/src/utils/webpack-modify-validate.js
@@ -1,8 +1,20 @@
 import _ from "lodash"
 import invariant from "invariant"
 import path from "path"
-import validate from "webpack-validator"
+import validate, { Joi } from "webpack-validator"
 import apiRunnerNode from "./api-runner-node"
+
+// We whitelist special config keys that are not part of a standard Webpack v1
+// config but are in common usage. We should be able to completely remove this
+// once we're on Webpack v3.
+//
+// For info on whitelisting with webpack-validator see:
+// https://github.com/js-dxtools/webpack-validator#customizing
+const validationWhitelist = Joi.object({
+  stylus: Joi.object({
+    use: Joi.any(),
+  }),
+})
 
 export default (async function ValidateWebpackConfig(config, stage) {
   // We don't care about the return as plugins just mutate the config directly.
@@ -21,6 +33,7 @@ export default (async function ValidateWebpackConfig(config, stage) {
 
   const validationState = validate(config.resolve(), {
     returnValidation: true,
+    schemaExtension: validationWhitelist,
   })
 
   if (!validationState.error) {
@@ -34,6 +47,13 @@ export default (async function ValidateWebpackConfig(config, stage) {
     console.log(err.type, `,`, err.message)
     console.log(`\n`)
   })
+
+  console.log(
+    `Your Webpack config does not appear to be valid. This could be because of
+something you added or a plugin. If you don't recognize the invalid keys listed
+above try removing plugins and rebuilding to identify the culprit.
+`
+  )
 
   return process.exit(1)
 })
