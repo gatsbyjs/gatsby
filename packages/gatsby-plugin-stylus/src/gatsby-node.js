99911a065559a483aeb9e54e6912171680be257e
Ian Sinnott <ian@iansinnott.com>
2017-07-09 01:47:30 -0700

    Add support for using stylus modules with the plugin

--- packages/gatsby-plugin-stylus/src/gatsby-node.js
+++ packages/gatsby-plugin-stylus/src/gatsby-node.js
@@ -29,14 +29,12 @@ exports.modifyWebpackConfig = ({ config, stage }, options = {}) => {
 
   // Pass in stylus plugins regardless of stage.
   if (Array.isArray(options.use)) {
-    console.log(`WARNING: gatsby-plugin-stylus \`use\` option is not yet supported. See https://github.com/gatsbyjs/gatsby/issues/1432`)
-    console.log()
-    // config.merge(current => {
-    //   current.stylus = {
-    //     use: options.use,
-    //   }
-    //   return current
-    // })
+    config.merge(current => {
+      current.stylus = {
+        use: options.use,
+      }
+      return current
+    })
   } else if (options.use) {
     throw new Error(`gatsby-plugin-stylus "use" option passed with ${options.use}. Pass an array of stylus plugins instead`)
   }
