cd0a5008a681f647a31573d6ca0778c52a83b552
Tegan Churchill <rawrmonstar@users.noreply.github.com>
2017-07-11 15:30:43 -0700

    Add new plugin `gatsby-plugin-emotion` (#1447)
    * Add add new plugin `gatsby-plugin-emotion` to add `emotion` support
    
    * Add emotion example site

---
+++ examples/using-emotion/src/pages/index.js
@@ -0,0 +1,60 @@
+import React from "react"
+import { fontFace, injectGlobal } from "emotion"
+import styled from "emotion/react"
+
+injectGlobal`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`
+
+fontFace`
+  font-family: system-ui;
+  font-style: normal;
+  font-weight: 400;
+  src: local(".SFNSText-Regular"), local(".HelveticaNeueDeskInterface-Regular"), local(".LucidaGrandeUI"), local("Segoe UI"), local("Ubuntu"), local("Roboto-Regular"), local("DroidSans"), local("Tahoma");
+`
+
+const Wrapper = styled.section`
+  align-items: center;
+  background: #282a36;
+  display: flex;
+  flex-direction: column;
+  font-family: system-ui;
+  height: 100vh;
+  justify-content: center;
+  width: 100vw;
+`
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  color: #ff79c6;
+  margin-bottom: .5em;
+  & a {
+    color: #8be9fd;
+  }
+`
+
+const Subtitle = styled.p`
+  color: #bd93f9;
+  & a {
+    color: inherit;
+  }
+`
+
+const IndexPage = () =>
+  <Wrapper>
+    <Title>
+      Hello World, this is my first component styled with{` `}
+      <a href="https://emotion.sh/">emotion</a>!
+    </Title>
+    <Subtitle>
+      <a href="https://www.gatsbyjs.org/docs/packages/gatsby-plugin-emotion/">
+        gatsby-plugin-emotion docs
+      </a>
+    </Subtitle>
+  </Wrapper>
+
+export default IndexPage
---
+++ packages/gatsby-plugin-emotion/README.md
@@ -0,0 +1,17 @@
+# gatsby-plugin-emotion
+Provide support for using the css-in-js library [Emotion](https://github.com/tkh44/emotion) including server side rendering.
+
+## Install
+
+```
+npm install --save gatsby-plugin-emotion
+```
+
+## How to use
+Add the plugin to your `gatsby-config.js`.
+
+```js
+plugins: [
+  'gatbsy-plugin-emotion'
+]
+```
