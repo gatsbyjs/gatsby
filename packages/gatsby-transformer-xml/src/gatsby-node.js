76946a1f319f5ef6459741f9d712ab1b135a436d
Khaled Garbaya <khaled@contentful.com>
2017-07-11 10:21:20 +0200

    add gatsby-transformer-xml

---
+++ packages/gatsby-transformer-xml/src/gatsby-node.js
@@ -0,0 +1,40 @@
+const parseXml = require(`xml-parser`)
+const crypto = require(`crypto`)
+const _ = require(`lodash`)
+
+async function onCreateNode({ node, boundActionCreators, loadNodeContent }) {
+  const { createNode, createParentChildLink } = boundActionCreators
+  
+  // We only care about XML content.
+  if (![`application/xml`, `text/xml`].includes(node.internal.mediaType)) {
+    return
+  }
+
+  const rawXml = loadNodeContent(node)
+  const parsedXml = parseXml(rawXml)
+  const nodeArray = parsedXml.root.children.map((obj, i) => {
+    const objStr = JSON.stringify(obj)
+    const contentDigest = crypto
+      .createHash(`md5`)
+      .update(objStr)
+      .digest(`hex`)
+    return {
+        ...obj,
+        id: `${node.id} [${i}] >>> XML`,
+        parent: node.id,
+        internal: {
+          contentDigest,
+          type: _.upperFirst(_.camelCase(`${node.name} xml`)),
+        },
+      }
+  })
+
+  _.each(nodeArray, j => {
+    createNode(j)
+    createParentChildLink({ parent: node, child: j  })
+  })
+  return
+}
+
+exports.onCreateNode = onCreateNode
+
