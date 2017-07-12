292134b9ab09fca9bf3c079143fc572b8ca53df6
Felix Jung <felix@jung.fm>
2017-07-13 00:01:17 +0200

    Fix and extend tests for path module.

--- packages/gatsby/src/utils/__tests__/path.js
+++ packages/gatsby/src/utils/__tests__/path.js
@@ -1,23 +1,55 @@
 const { joinPath, withBasePath } = require(`../path`)
+const os = require(`os`)
 
 describe(`paths`, () => {
   describe(`joinPath`, () => {
-    it(`joins paths like path.join on Unix-type platforms`, () => {
-      const paths = [`/foo`, `bar`, `baz`]
-      const expected = paths.join(`/`)
-      const actual = joinPath(...paths)
-      expect(actual).toBe(expected)
-    })
+    if (os.platform() !== `win32`) {
+      it(`joins paths like path.join on Unix-type platforms.`, () => {
+        const paths = [`/foo`, `bar`, `baz`]
+        const expected = paths.join(`/`)
+        const actual = joinPath(...paths)
+        expect(actual).toBe(expected)
+      })
+    }
+
+    if (os.platform() === `win32`) {
+      it(`replaces '\\' with '\\\\' on Windows.`, () => {
+        const paths = [`foo`, `bar`, `baz`]
+        const expected = paths.join(`\\\\`)
+        const actual = joinPath(...paths)
+        expect(actual).toBe(expected)
+      })
+    }
   })
 
   describe(`withBasePath`, () => {
-    it(`returns a function that behaves like joinPath, but always prepends a base path`, () => {
-      const basePath = `/foo`
-      const subPath = `bar`
-      const withFooPath = withBasePath(basePath)
-      const expected = `${basePath}/${subPath}`
-      const actual = withFooPath(subPath)
+    it(`returns a function.`, () => {
+      const withEmptyBasePath = withBasePath(``)
+      const expected = `function`
+      const actual = typeof withEmptyBasePath
       expect(actual).toBe(expected)
     })
+
+    if (os.platform() !== `win32`) {
+      it(`behaves like joinPath() on Unix-type platforms, but prepends a basePath`, () => {
+        const basePath = `/foo`
+        const subPath = `bar`
+        const withFooPath = withBasePath(basePath)
+        const expected = `${basePath}/${subPath}`
+        const actual = withFooPath(subPath)
+        expect(actual).toBe(expected)
+      })
+    }
+
+    if (os.platform() === `win32`) {
+      it(`behaves like joinPath() on Windows, but prepends a basePath`, () => {
+        const basePath = `foo`
+        const subPath = `bar`
+        const withFooPath = withBasePath(basePath)
+        const expected = `${basePath}\\\\${subPath}`
+        const actual = withFooPath(subPath)
+        expect(actual).toBe(expected)
+      })
+    }
   })
 })
