0d3783cea62ab46fab4c1e2a71fd7e6c538e860b
Ian Sinnott <ian@iansinnott.com>
2017-07-11 18:01:26 -0700

    Add stylus support (#1437)
    * Add jest-cli to dependencies
    
    I was getting a module resolution error after installing. Test command was
    throwing an error saying it couldn't find jest-cli so now its in deps.
    
    * Add stylus plugin
    
    * Add support for using stylus modules with the plugin
    
    * Add a validation whitelist to allow the "stylus" key in webpack config
    
    This is a special case but we may run in to other cases before upgrading to
    webpack v3. In webpack v1 many plugins and loaders used top-level configuration
    keys to handle custom configuration. webpack-validator does not like this, but
    it does provide the option to whitelist keys, which is exactly what I did here.
    
    I also added a (hopefully) helpful error message to clarify what went wrong in
    the case of an invalid webpack config.
    
    * Add example of stylus plugin usage to gatsby-plugin-stylus
    
    * Do not validate stylus options object shape
    
    Validating the shape felt like overkill, and it might prevent valid options form
    being used if I missed something.
    
    * Configure stylus to use module extension for overall gatsby consistency
    
    * Update readme to reflect css module configuration change
    
    * Add common-tags helper to improve long string formatting
    
    * Remove debug code

---
+++ packages/gatsby-plugin-stylus/src/gatsby-node.js
@@ -0,0 +1,120 @@
+/**
+ * Usage:
+ *
+ * // gatsby-config.js
+ * plugins: [
+ *  `gatsby-plugin-stylus`,
+ * ],
+ *
+ * // Usage with options:
+ *
+ * // gatsby-config.js
+ * plugins: [
+ *   {
+ *     resolve: `gatsby-plugin-stylus`,
+ *     options: {
+ *       use: [],
+ *     },
+ *   },
+ * ],
+ */
+const ExtractTextPlugin = require(`extract-text-webpack-plugin`)
+
+exports.modifyWebpackConfig = ({ config, stage }, options = {}) => {
+  const cssModulesConfProd = `css?modules&minimize&importLoaders=1`
+  const cssModulesConfDev =
+    `css?modules&importLoaders=1&localIdentName=[name]---[local]---[hash:base64:5]`
+
+  // Pass in stylus plugins regardless of stage.
+  if (Array.isArray(options.use)) {
+    config.merge(current => {
+      current.stylus = {
+        use: options.use,
+      }
+      return current
+    })
+  } else if (options.use) {
+    throw new Error(`gatsby-plugin-stylus "use" option passed with ${options.use}. Pass an array of stylus plugins instead`)
+  }
+
+  const stylusFiles = /\.styl$/
+  const stylusModulesFiles = /\.module\.styl$/
+
+  switch (stage) {
+    case `develop`: {
+      config.loader(`stylus`, {
+        test: stylusFiles,
+        exclude: stylusModulesFiles,
+        loaders: [`style`, `css`, `postcss`, `stylus`],
+      })
+      config.loader(`stylusModules`, {
+        test: stylusModulesFiles,
+        loaders: [`style`, cssModulesConfDev, `postcss`, `stylus`],
+      })
+      return config
+    }
+
+    case `build-css`: {
+      config.loader(`stylus`, {
+        test: stylusFiles,
+        exclude: stylusModulesFiles,
+        loader: ExtractTextPlugin.extract(`style`, [
+          `css?minimize`,
+          `postcss`,
+          `stylus`,
+        ]),
+      })
+      config.loader(`stylusModules`, {
+        test: stylusModulesFiles,
+        loader: ExtractTextPlugin.extract(`style`, [
+          cssModulesConfProd,
+          `postcss`,
+          `stylus`,
+        ]),
+      })
+      return config
+    }
+
+    case `develop-html`:
+    case `build-html`: {
+      const moduleLoader = ExtractTextPlugin.extract(`style`, [
+        cssModulesConfProd,
+        `postcss`,
+        `stylus`,
+      ])
+
+      config.loader(`stylus`, {
+        test: stylusFiles,
+        exclude: stylusModulesFiles,
+        loader: `null`,
+      })
+      config.loader(`stylusModules`, {
+        test: stylusModulesFiles,
+        loader: moduleLoader,
+      })
+      return config
+    }
+
+    case `build-javascript`: {
+      const moduleLoader = ExtractTextPlugin.extract(`style`, [
+        cssModulesConfProd,
+        `stylus`,
+      ])
+      config.loader(`stylus`, {
+        test: stylusFiles,
+        exclude: stylusModulesFiles,
+        loader: `null`,
+      })
+      config.loader(`stylusModules`, {
+        test: stylusModulesFiles,
+        loader: moduleLoader,
+      })
+
+      return config
+    }
+
+    default: {
+      return config
+    }
+  }
+}
