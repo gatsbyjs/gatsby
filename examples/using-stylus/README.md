42f3b62e0dfd6a9a096f2884283a00b0b84831d2
Ian Sinnott <ian@iansinnott.com>
2017-07-11 23:57:54 -0700

    Add stylus example

---
+++ examples/using-stylus/README.md
@@ -0,0 +1,11 @@
+# Using gatsby-plugin-stylus
+
+https://using-postcss-sass.gatsbyjs.org
+
+Example site that demostrates how to build Gatsby sites
+using the Gatsby plugin `gatsby-plugin-stylus`
+which sets up webpack to run [Stylus][] code.
+
+The example includes both "plain" Stylus and usage of CSS modules.
+
+[Stylus]: https://github.com/stylus/stylus
---
+++ examples/using-stylus/src/pages/index.js
@@ -0,0 +1,36 @@
+import React from "react"
+import "./index.styl"
+
+class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className="the-stylus-class">Hi stylish friends</h1>
+        <div className="stylus-nav-example">
+          <h2>Nav example</h2>
+          <ul>
+            <li>
+              <a href="#">Store</a>
+            </li>
+            <li>
+              <a href="#">Help</a>
+            </li>
+            <li>
+              <a href="#">Logout</a>
+            </li>
+          </ul>
+        </div>
+        <div className="ColumnSection__grid">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+          <div>5</div>
+          <div>6</div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Index
