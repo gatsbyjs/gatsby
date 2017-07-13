d1b72dfa3ea53650c57b752deb9c5d4450915776
Brian Vaughn <brian.david.vaughn@gmail.com>
2017-07-13 14:07:15 -0700

    Prism plugin bugfix alt (#1491)
    * Replace class=language-* with data-language=* to avoid potential Prism bug
    
    * Remark Prism plugin now accepts useDataAttribute option
    Default behavior remains as before, to use a class='language-*' prefix. New behavior allows for a data-language=* alternate.
    Snapshot tests added.
    
    * Alternate, custom-class-prefix approach to fixing Prism+ReactLive bug
    
    * Added README note about new plugin option
    
    * Provided more context for the remark prism docs comment

--- packages/gatsby-remark-prismjs/src/index.js
+++ packages/gatsby-remark-prismjs/src/index.js
@@ -3,7 +3,7 @@ const visit = require(`unist-util-visit`)
 const parseLineNumberRange = require(`./parse-line-number-range`)
 const highlightCode = require(`./highlight-code`)
 
-module.exports = ({ markdownAST }) => {
+module.exports = ({ markdownAST }, { classPrefix = 'language-' } = {}) => {
   visit(markdownAST, `code`, node => {
     let language = node.lang
     let { splitLanguage, highlightLines } = parseLineNumberRange(language)
@@ -15,17 +15,24 @@ module.exports = ({ markdownAST }) => {
     // outcome without any additional CSS.
     //
     // @see https://github.com/PrismJS/prism/blob/1d5047df37aacc900f8270b1c6215028f6988eb1/themes/prism.css#L49-L54
-    let preCssClassLanguage = `none`
+    let languageName = `none`
     if (language) {
       language = language.toLowerCase()
-      preCssClassLanguage = language
+      languageName = language
     }
+
+    // Allow users to specify a custom class prefix to avoid breaking
+    // line highlights if Prism is required by any other code.
+    // This supports custom user styling without causing Prism to
+    // re-process our already-highlighted markup.
+    // @see https://github.com/gatsbyjs/gatsby/issues/1486
+    const className = `${classPrefix}${languageName}`
 
     // Replace the node with the markup we need to make
     // 100% width highlighted code lines work
     node.type = `html`
     node.value = `<div class="gatsby-highlight">
-      <pre class="language-${preCssClassLanguage}"><code>${highlightCode(
+      <pre class="${className}"><code>${highlightCode(
       language,
       node.value,
       highlightLines
