1fb1fb6ec3b21a0327b009103a51444c80ae32b0
Jake Deichert <git@jakedeichert.com>
2017-07-13 20:30:21 -0400

    Prevent onClick logic sooner

--- packages/gatsby-link/src/index.js
+++ packages/gatsby-link/src/index.js
@@ -46,25 +46,7 @@ class GatsbyLink extends React.Component {
       <El
         onClick={e => {
           onClick && onClick(e)
-          // Is this link pointing to a hash on the same page? If so,
-          // just scroll there.
-          let pathname = this.state.to
-          if (pathname.split(`#`).length > 1) {
-            pathname = pathname.split(`#`).slice(0, -1).join(``)
-          }
-          if (pathname === window.location.pathname) {
-            const hashFragment = this.state.to.split(`#`).slice(1).join(`#`)
-            const element = document.getElementById(hashFragment)
-            if (element !== null) {
-              element.scrollIntoView()
-              return true
-            }
-          }
-
-          // In production, make sure the necessary scripts are
-          // loaded before continuing.
           if (
-            process.env.NODE_ENV === `production` &&
             e.button === 0 && // ignore right clicks
             !this.props.target && // let browser handle "target=_blank"
             !e.defaultPrevented && // onClick prevented default
@@ -73,8 +55,27 @@ class GatsbyLink extends React.Component {
             !e.ctrlKey &&
             !e.shiftKey
           ) {
-            e.preventDefault()
-            window.___navigateTo(this.state.to)
+            // Is this link pointing to a hash on the same page? If so,
+            // just scroll there.
+            let pathname = this.state.to
+            if (pathname.split(`#`).length > 1) {
+              pathname = pathname.split(`#`).slice(0, -1).join(``)
+            }
+            if (pathname === window.location.pathname) {
+              const hashFragment = this.state.to.split(`#`).slice(1).join(`#`)
+              const element = document.getElementById(hashFragment)
+              if (element !== null) {
+                element.scrollIntoView()
+                return true
+              }
+            }
+
+            // In production, make sure the necessary scripts are
+            // loaded before continuing.
+            if (process.env.NODE_ENV === `production`) {
+              e.preventDefault()
+              window.___navigateTo(this.state.to)
+            }
           }
         }}
         {...rest}
