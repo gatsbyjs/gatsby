a16706626b397fffe47b192b835104e01b2977a7
Chiedo <chiedo@chie.do>
2017-07-19 08:26:54 -0400

    Added the option to use the original image

--- packages/gatsby-transformer-sharp/src/extend-node-type.js
+++ packages/gatsby-transformer-sharp/src/extend-node-type.js
@@ -16,6 +16,11 @@ const {
 } = require(`gatsby-plugin-sharp`)
 
 const sharp = require(`sharp`)
+const {promisify} = require('util');
+const ncp = require('ncp');
+const ncpAsync = promisify(ncp);
+const sizeOf = require('image-size');
+const path = require('path');
 
 const ImageFormatType = new GraphQLEnumType({
   name: `ImageFormat`,
@@ -60,6 +65,36 @@ module.exports = ({ type, pathPrefix, getNodeAndSavePathDependency }) => {
   }
 
   return {
+    original: {
+      type: new GraphQLObjectType({
+        name: `ImageSharpOriginal`,
+        fields: {
+          width: { type: GraphQLFloat },
+          height: { type: GraphQLFloat },
+          src: { type: GraphQLString },
+        },
+      }),
+      args: {
+      },
+      async resolve(image, fieldArgs, context) {
+        const details = getNodeAndSavePathDependency(image.parent, context.path);
+        const dimensions = sizeOf(details.absolutePath);
+        const imageName = `${image.internal.contentDigest}${details.ext}`;
+        const publicPath = path.join(
+          process.cwd(),
+          `public`,
+          `static/${imageName}`
+        );
+
+        await ncpAsync(details.absolutePath, publicPath);
+
+        return {
+          width: dimensions.width,
+          height: dimensions.height,
+          src: '/static/' + imageName,
+        }
+      },
+    },
     responsiveResolution: {
       type: new GraphQLObjectType({
         name: `ImageSharpResponsiveResolution`,
