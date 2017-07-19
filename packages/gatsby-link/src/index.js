1a71f2662306f3e008378820500cbb967cf655b7
Jake Deichert <git@jakedeichert.com>
2017-07-18 20:10:24 -0400

    Fix the onClick override logic (#1489)
    * Fix the onClick override logic
    
    Solution is almost a direct copy/paste from react-router's Link
    component (as suggested):
    https://github.com/ReactTraining/react-router/blob/676e8ac14da780e9fbd32
    7576971fe549f3d0b8c/packages/react-router-dom/modules/Link.js#L40-L43
    
    Now, cmd/ctrl-clicking opens links in a new tab, as expected! Also,
    target="_blank" now works.
    
    * Adjust if statement
    
    * Simplify further
    
    * Prevent onClick logic sooner

--- packages/gatsby-link/src/index.js
+++ packages/gatsby-link/src/index.js
@@ -46,26 +46,36 @@ class GatsbyLink extends React.Component {
       <El
         onClick={e => {
           onClick && onClick(e)
-          // Is this link pointing to a hash on the same page? If so,
-          // just scroll there.
-          let pathname = this.state.to
-          if (pathname.split(`#`).length > 1) {
-            pathname = pathname.split(`#`).slice(0, -1).join(``)
-          }
-          if (pathname === window.location.pathname) {
-            const hashFragment = this.state.to.split(`#`).slice(1).join(`#`)
-            const element = document.getElementById(hashFragment)
-            if (element !== null) {
-              element.scrollIntoView()
-              return true
+          if (
+            e.button === 0 && // ignore right clicks
+            !this.props.target && // let browser handle "target=_blank"
+            !e.defaultPrevented && // onClick prevented default
+            !e.metaKey && // ignore clicks with modifier keys...
+            !e.altKey &&
+            !e.ctrlKey &&
+            !e.shiftKey
+          ) {
+            // Is this link pointing to a hash on the same page? If so,
+            // just scroll there.
+            let pathname = this.state.to
+            if (pathname.split(`#`).length > 1) {
+              pathname = pathname.split(`#`).slice(0, -1).join(``)
             }
-          }
+            if (pathname === window.location.pathname) {
+              const hashFragment = this.state.to.split(`#`).slice(1).join(`#`)
+              const element = document.getElementById(hashFragment)
+              if (element !== null) {
+                element.scrollIntoView()
+                return true
+              }
+            }
 
-          // In production, make sure the necessary scripts are
-          // loaded before continuing.
-          if (process.env.NODE_ENV === `production`) {
-            e.preventDefault()
-            window.___navigateTo(this.state.to)
+            // In production, make sure the necessary scripts are
+            // loaded before continuing.
+            if (process.env.NODE_ENV === `production`) {
+              e.preventDefault()
+              window.___navigateTo(this.state.to)
+            }
           }
         }}
         {...rest}
