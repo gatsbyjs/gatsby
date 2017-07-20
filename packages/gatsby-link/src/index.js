gatsbyjs/gatsby

commit 217234398e3082ad73ed82acd48b13624ed5c03b

Fix prefixes in gatsby-link + navigateTo (#1550)

packages/gatsby-link/src/index.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types"
 
 let pathPrefix = ``
 if (__PREFIX_PATHS__) {
-  pathPrefix = __PATH_PREFIX__
+  pathPrefix = `${__PATH_PREFIX__}/`
 }
 
 class GatsbyLink extends React.Component {
@@ -92,5 +92,5 @@ GatsbyLink.contextTypes = {
 export default GatsbyLink
 
 export const navigateTo = pathname => {
-  window.___navigateTo(pathname)
+  window.___navigateTo(pathPrefix + pathname)
 }

commit 41491b02f340da3c0a4920e12434f1d904cf66a6

Removes the slash between the `pathPrefix` and `pathname` when navigating - this bug was introduced by (#1550)

packages/gatsby-link/src/index.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types"
 
 let pathPrefix = ``
 if (__PREFIX_PATHS__) {
-  pathPrefix = `${__PATH_PREFIX__}/`
+  pathPrefix = __PATH_PREFIX__.replace(/\/?$/, '');
 }
 
 class GatsbyLink extends React.Component {