e04ce20c1000f0f9fd856d096a7e0cdb20cac088
joris hermans <JHermans85@hotmail.com>
2017-07-25 20:55:20 +0200

    connect to a mongodb cloud instance

--- examples/using-mongodb/gatsby-config.js
+++ examples/using-mongodb/gatsby-config.js
@@ -5,7 +5,10 @@ module.exports = {
   plugins: [
     {
       resolve: `gatsby-source-mongodb`,
-      options: { dbName: `cloud`, collection: `documents` },
+      options: { dbName: `cloud`, collection: `documents`,
+                 server: { address: `ds143532.mlab.com`, port: 43532 },
+                 auth: { user: `admin`, password: `12345` } 
+      },
     },
     `gatsby-plugin-react-helmet`,
   ],
