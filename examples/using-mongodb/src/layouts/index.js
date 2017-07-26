c30a4b100b3358813cd48d9dead78370cb386e2d
joris hermans <JHermans85@hotmail.com>
2017-07-26 22:42:58 +0200

    adding react helmet

--- examples/using-mongodb/src/layouts/index.js
+++ examples/using-mongodb/src/layouts/index.js
@@ -1,10 +1,20 @@
 import React from "react"
 import Link from "gatsby-link"
+import Helmet from "react-helmet"
 
 class DefaultLayout extends React.Component {
   render() {
+    let siteMetadata = {title: 'mongoDB within Gatsby.JS'}
+
     return (
-      <div>
+      <div class="websites">
+        <Helmet
+          defaultTitle={siteMetadata.title}
+          meta={[
+            { name: `description`, content: `An example site to show how to use mongoDB with Gatsby.JS` },
+            { name: `keywords`, content: `websites listings` },
+          ]}
+        />
         <Link style={{ textDecoration: `none` }} to="/">
           <h3 style={{ color: `tomato` }}>
             Example of using mongoDB as a data source for a Gatsby site
