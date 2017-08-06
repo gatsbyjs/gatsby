3e0a98edb23b480e1d64709927f20fc1803eb3dc
Jacob Bolda <jbolda@gmail.com>
2017-08-06 00:25:16 -0500

    dig deeper into mermaid internals to avoid expectation of DOM

--- packages/gatsby-remark-flowchart/src/index.js
+++ packages/gatsby-remark-flowchart/src/index.js
@@ -1,13 +1,13 @@
 const visit = require(`unist-util-visit`)
-// const mermaid = require(`mermaid`)
+const flowchart = require(`mermaid/src/diagrams/flowchart/flowRenderer`)
 const _ = require(`lodash`)
 
 module.exports = ({ markdownAST }, pluginOptions = {}) =>
   visit(markdownAST, `code`, node => {
     if (node.lang === `flowchart`) {
-      console.log('flowchart', node)
-      // let diagram = mermaidAPI.render('graphDiv', node.value, (svgCode, bindFunctions) => {
-      //   node.value = svgCode
-      // })
+      // this doesn't work as it is expecting "document" and running within the context of the DOM
+      let diagram = flowchart.draw(node.value, 'flowchart', true)
+      console.log(diagram)
+      // node.value = diagram
     }
   })
