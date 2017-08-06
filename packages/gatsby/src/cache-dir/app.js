7ef3fe6999d7ab0ac04a0fbb23cca3a2cc68097f
Alexander Trauzzi <atrauzzi@gmail.com>
2017-08-05 20:47:08 -0500

    Here's what I'm thinking...

--- packages/gatsby/src/cache-dir/app.js
+++ packages/gatsby/src/cache-dir/app.js
@@ -1,65 +1,69 @@
-const apiRunner = require(`./api-runner-browser`)
-
 import React from "react"
 import ReactDOM from "react-dom"
 import { AppContainer as HotContainer } from "react-hot-loader"
 import socketIo from "./socketIo"
+import apiRunner from "./api-runner-browser"
+import emitter from "./emitter"
+import Root from "./root"
 
-window.___emitter = require(`./emitter`)
 
-// Let the site/plugins run code very early.
-apiRunner(`onClientEntry`)
-
-// Hook up the client to socket.io on server
-socketIo()
-
-/**
- * Service Workers are persistent by nature. They stick around,
- * serving a cached version of the site if they aren't removed.
- * This is especially frustrating when you need to test the
- * production build on your local machine.
- *
- * Let's unregister the service workers in development, and tidy up a few errors.
- */
-if (`serviceWorker` in navigator) {
-  navigator.serviceWorker.getRegistrations().then(registrations => {
-    for (let registration of registrations) {
-      registration.unregister()
-    }
-  })
-}
-
-const rootElement = document.getElementById(`___gatsby`)
-
-let Root = require(`./root`)
+// Troubles with this import in the past?
 if (Root.default) {
   Root = Root.default
 }
 
-ReactDOM.render(
-  <HotContainer>
-    <Root />
-  </HotContainer>,
-  rootElement,
-  () => {
-    apiRunner(`onInitialClientRender`)
-  }
-)
+window.___emitter = emitter
 
-if (module.hot) {
-  module.hot.accept(`./root`, () => {
-    let NextRoot = require(`./root`)
-    if (NextRoot.default) {
-      NextRoot = NextRoot.default
+Promise
+  .all(apiRunner(`onClientEntry`))
+  .then(() => {
+
+    const rootElement = document.getElementById(`___gatsby`)
+
+    socketIo()
+
+    /**
+     * Service Workers are persistent by nature. They stick around,
+     * serving a cached version of the site if they aren't removed.
+     * This is especially frustrating when you need to test the
+     * production build on your local machine.
+     *
+     * Let's unregister the service workers in development, and tidy up a few errors.
+     */
+    if (`serviceWorker` in navigator) {
+
+      navigator.serviceWorker.getRegistrations().then(registrations => {
+
+        for (let registration of registrations) {
+
+          registration.unregister()
+        }
+      })
     }
+   
     ReactDOM.render(
-      <HotContainer>
-        <NextRoot />
-      </HotContainer>,
+      <HotContainer><Root /></HotContainer>,
       rootElement,
-      () => {
-        apiRunner(`onInitialClientRender`)
-      }
+      () => apiRunner(`onInitialClientRender`)
     )
+
+    if (module.hot) {
+
+      module.hot.accept(`./root`, () => {
+
+        let NextRoot = require(`./root`)
+
+        if (NextRoot.default) {
+
+          NextRoot = NextRoot.default
+        }
+        ReactDOM.render(
+          <HotContainer>
+            <NextRoot />
+          </HotContainer>,
+          rootElement,
+          () => apiRunner(`onInitialClientRender`)
+        )
+      })
+    }
   })
-}
