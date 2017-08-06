4d019392a68dabd16b77ea1bd0c63e962548d5e7
Jacob Bolda <jbolda@gmail.com>
2017-08-05 21:14:22 -0500

    switch out flowchart.js for mermaid

--- packages/gatsby-remark-flowchart/src/index.js
+++ packages/gatsby-remark-flowchart/src/index.js
@@ -1,13 +1,13 @@
 const visit = require(`unist-util-visit`)
-const raphael = require(`raphael`)
-const flowchart = require(`./flowchart.js`)
+// const mermaid = require(`mermaid`)
 const _ = require(`lodash`)
 
 module.exports = ({ markdownAST }, pluginOptions = {}) =>
   visit(markdownAST, `code`, node => {
     if (node.lang === `flowchart`) {
       console.log('flowchart', node)
-      let diagram = flowchart.parse(node.value)
-      node.value = diagram.drawSVG('diagram')
+      // let diagram = mermaidAPI.render('graphDiv', node.value, (svgCode, bindFunctions) => {
+      //   node.value = svgCode
+      // })
     }
   })
