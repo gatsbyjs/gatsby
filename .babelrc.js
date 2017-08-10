635d13982c17f2d889291bfa566495d1282fbd21
Kyle Mathews <mathews.kyle@gmail.com>
2017-08-10 18:36:31 -0400

    Format

--- .babelrc.js
+++ .babelrc.js
@@ -1,31 +1,37 @@
-
 const r = m => require.resolve(m)
 
 function preset(_, options = {}) {
-  const PRODUCTION = process.env.NODE_ENV === 'production';
+  const PRODUCTION = process.env.NODE_ENV === "production"
 
   return {
     presets: [
-      [r('babel-preset-env'), {
-        loose: true,
-        debug: !!options.debug,
-        modules: 'commonjs',
-        targets: {
-          node: PRODUCTION ? 4.0 : 'current'
-        }
-      }],
-      r('babel-preset-react'),
-      r('babel-preset-flow'),
+      [
+        r("babel-preset-env"),
+        {
+          loose: true,
+          debug: !!options.debug,
+          modules: "commonjs",
+          targets: {
+            node: PRODUCTION ? 4.0 : "current",
+            uglify: PRODUCTION ? true : false,
+          },
+        },
+      ],
+      r("babel-preset-react"),
+      r("babel-preset-flow"),
     ],
     plugins: [
-      r('babel-plugin-transform-object-rest-spread'),
-      [r('babel-plugin-transform-runtime'), {
-        polyfill: false,
-        regenerator: false,
-      }],
+      r("babel-plugin-transform-object-rest-spread"),
+      [
+        r("babel-plugin-transform-runtime"),
+        {
+          polyfill: false,
+          regenerator: false,
+        },
+      ],
       r(`babel-plugin-transform-flow-strip-types`),
-    ]
+    ],
   }
 }
 
-module.exports = preset;
+module.exports = preset
