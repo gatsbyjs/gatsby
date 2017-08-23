c414e0257de17da57d8499204564bb6a44479b99
Daniel Farrell <daniel@mobelux.com>
2017-08-23 14:53:29 -0400

    Compile the gatsby-module-loader with babel

--- packages/gatsby/src/loaders/gatsby-module-loader/plugin.js
+++ packages/gatsby/src/loaders/gatsby-module-loader/plugin.js
@@ -1,23 +1,21 @@
-module.exports = function() {}
-module.exports.prototype.apply = function(compiler) {
-  compiler.plugin(`compilation`, function(compilation) {
-    compilation.mainTemplate.plugin(`require-extensions`, function(
-      source,
-      chunk,
-      hash
-    ) {
+"use strict";
+
+module.exports = function () {};
+module.exports.prototype.apply = function (compiler) {
+  compiler.plugin("compilation", function (compilation) {
+    compilation.mainTemplate.plugin("require-extensions", function (source, chunk, hash) {
       if (chunk.chunks.length > 0) {
-        var buf = []
+        var buf = [];
 
-        buf.push(``)
-        buf.push(``)
-        buf.push(`// expose the chunks object`)
-        buf.push(this.requireFn + `.s = installedChunks;`)
+        buf.push("");
+        buf.push("");
+        buf.push("// expose the chunks object");
+        buf.push(this.requireFn + ".s = installedChunks;");
 
-        return source + this.asString(buf)
+        return source + this.asString(buf);
       }
 
-      return source
-    })
-  })
-}
+      return source;
+    });
+  });
+};
