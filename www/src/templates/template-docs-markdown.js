630f7be7a61ba2822a759abcd71a183a992bd459
Mark Michon <michon@gmail.com>
2017-08-28 15:18:11 -0400

    Update react-helmet syntax to new simplified API (#1919)

--- www/src/templates/template-docs-markdown.js
+++ www/src/templates/template-docs-markdown.js
@@ -9,39 +9,16 @@ const DocsTemplate = React.createClass({
     const page = this.props.data.markdownRemark
     return (
       <Container>
-        <Helmet
-          title={page.frontmatter.title}
-          meta={[
-            {
-              name: `description`,
-              content: page.excerpt,
-            },
-            {
-              name: `og:description`,
-              content: page.excerpt,
-            },
-            {
-              name: `twitter:description`,
-              content: page.excerpt,
-            },
-            {
-              name: `og:title`,
-              content: page.frontmatter.title,
-            },
-            {
-              name: `og:type`,
-              content: `article`,
-            },
-            {
-              name: `twitter:label1`,
-              content: `Reading time`,
-            },
-            {
-              name: `twitter:data1`,
-              content: `${page.timeToRead} min read`,
-            },
-          ]}
-        />
+        <Helmet>
+          <title>{page.frontmatter.title}</title>
+          <meta name="description" content={page.excerpt} />
+          <meta name="og:description" content={page.excerpt} />
+          <meta name="twitter:description" content={page.excerpt} />
+          <meta name="og:title" content={page.frontmatter.title} />
+          <meta name="og:type" content="article" />
+          <meta name="twitter.label1" content="Reading time" />
+          <meta name="twitter:data1" content={`${page.timeToRead} min read`} />
+        </Helmet>
         <h1 css={{ marginTop: 0 }}>
           {page.frontmatter.title}
         </h1>
