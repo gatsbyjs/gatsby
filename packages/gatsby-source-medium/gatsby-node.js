26d4f268db0b98548bd913b6ffbca0cccc1197de
Robert Vogt <robert@smartive.ch>
2017-09-04 11:09:05 +0200

    Add User <-> Post GraphQL Links

--- packages/gatsby-source-medium/gatsby-node.js
+++ packages/gatsby-source-medium/gatsby-node.js
@@ -22,24 +22,39 @@ exports.sourceNodes = async ({ boundActionCreators }, { username }) => {
     const userKeys = Object.keys(json.payload.references.User)
 
     const importableResources = [
+      userKeys.map(key => json.payload.references.User[key]),
       posts,
       collectionKeys.map(key => json.payload.references.Collection[key]),
-      userKeys.map(key => json.payload.references.User[key]),
     ]
 
     const resources = Array.prototype.concat(...importableResources)
     resources.map(resource => {
       const digest = crypto.createHash(`md5`).update(JSON.stringify(resource)).digest(`hex`)
 
-      const node = Object.assign(resource, {
-        id: resource.id ? resource.id : resource.userId,
-        parent: `__SOURCE__`,
-        children: [],
-        internal: {
-          type: `Medium${resource.type}`,
-          contentDigest: digest,
+      const links =
+        resource.type === `Post`
+          ? {
+              author___NODE: resource.creatorId,
+            }
+          : resource.type === `User`
+            ? {
+                posts___NODE: posts.filter(post => post.creatorId === resource.userId).map(post => post.id),
+              }
+            : {}
+
+      const node = Object.assign(
+        resource,
+        {
+          id: resource.id ? resource.id : resource.userId,
+          parent: `__SOURCE__`,
+          children: [],
+          internal: {
+            type: `Medium${resource.type}`,
+            contentDigest: digest,
+          },
         },
-      })
+        links
+      )
 
       createNode(node)
     })
