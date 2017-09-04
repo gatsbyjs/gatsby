4e89e09bbdedb066182af81ac735bc53c05f9f05
Robert <deniaz@users.noreply.github.com>
2017-09-04 23:59:41 +0200

    [gatsby-source-medium] Add Medium Source (#1907)
    * Add gatsby-source-medium Plugin
    
    * Fix package name in README
    
    * Improve README
    
    * Remove unnecessary attributes
    
    * Add User <-> Post GraphQL Links
    
    * Add Medium Example Page
    
    * Fix typo

---
+++ examples/using-medium/src/pages/index.js
@@ -0,0 +1,48 @@
+import React from "react"
+
+const mediumCDNUrl = `https://cdn-images-1.medium.com/max/150/`
+
+const IndexPage = ({ data }) => {
+  const posts = data.allMediumPost.edges
+
+  return (
+    <main>
+      {posts.map(post =>
+        <article key={post.node.id}>
+          <h2>
+            {post.node.title}
+          </h2>
+          <h3>
+            by {post.node.author.name}
+          </h3>
+          <img src={`${mediumCDNUrl}/${post.node.virtuals.previewImage.imageId}`} alt={post.node.title} width="150" />
+        </article>
+      )}
+    </main>
+  )
+}
+
+IndexPage.propTypes
+
+export default IndexPage
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allMediumPost(limit: 5, sort: { fields: [createdAt], order: DESC }) {
+      edges {
+        node {
+          id
+          title
+          author {
+            name
+          }
+          virtuals {
+            previewImage {
+              imageId
+            }
+          }
+        }
+      }
+    }
+  }
+`
