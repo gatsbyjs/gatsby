708705d02ac33a494131ad9f311c89746368cbd2
Tyler Pham <tyler@disruptiveart.com>
2017-09-05 12:20:38 -0400

    add checks for includePaths for sass-loader (#1970)
    * gatsbyjs/gatsby#1968: add checks for includePaths for sass-loader
    
    * added missing gatsby-1-config-css-modules module
    
    * remove gatsby-1-config-css-modules
    
    * #1968: use json string for sass-loader

--- packages/gatsby-plugin-sass/src/__tests__/gatsby-node.js
+++ packages/gatsby-plugin-sass/src/__tests__/gatsby-node.js
@@ -49,8 +49,11 @@ describe(`gatsby-plugin-sass`, () => {
     stages.forEach(stage => {
       describe(`stage: ${stage}`, () => {
         ;[
-          { options: {}, sassLoader: `sass` },
-          { options: { precision: 8 }, sassLoader: `sass?precision=8` },
+          { options: {}, sassLoader: `sass?{}` },
+          { options: { precision: 8 }, sassLoader: `sass?${JSON.stringify({ precision: 8 }).toString()}` },
+          { options: { includePaths: [] }, sassLoader: `sass?${JSON.stringify({ includePaths: [] }).toString()}` },
+          { options: { precision: 8, includePaths: [] }, sassLoader: `sass?${JSON.stringify({ precision: 8, includePaths: [] }).toString()}` },
+          { options: { includePaths: [`./node_modules`, `./path`] }, sassLoader: `sass?${JSON.stringify({ includePaths: [`./node_modules`, `./path`] }).toString()}` },
         ].forEach(({ options, sassLoader }) => {
           const stringified = JSON.stringify(options)
 
