cd8225cda55626588b9b6d0f68374a51dbe9e3d4
Kyle Mathews <mathews.kyle@gmail.com>
2017-09-10 07:00:18 +0100

    Show blog header on < Tablet (#2070)

--- www/src/pages/blog/index.js
+++ www/src/pages/blog/index.js
@@ -35,14 +35,17 @@ class BlogPostsIndex extends React.Component {
           <h1
             css={{
               marginTop: 0,
-              position: `absolute`,
-              width: 1,
-              height: 1,
-              padding: 0,
-              overflow: `hidden`,
-              clip: `rect(0,0,0,0)`,
-              whiteSpace: `nowrap`,
-              clipPath: `inset(50%)`,
+              [presets.Tablet]: {
+                marginTop: 0,
+                position: `absolute`,
+                width: 1,
+                height: 1,
+                padding: 0,
+                overflow: `hidden`,
+                clip: `rect(0,0,0,0)`,
+                whiteSpace: `nowrap`,
+                clipPath: `inset(50%)`,
+              },
             }}
           >
             Blog
