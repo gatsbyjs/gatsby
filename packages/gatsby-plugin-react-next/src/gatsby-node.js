d8331201db40e2d16dd2e1ede62ef229b591aeee
Kyle Mathews <mathews.kyle@gmail.com>
2017-09-16 14:40:41 +0200

    Only set alias for React 16 if new React not at top-level

--- packages/gatsby-plugin-react-next/src/gatsby-node.js
+++ packages/gatsby-plugin-react-next/src/gatsby-node.js
@@ -1,7 +1,11 @@
+const React = require(`react`)
+
 exports.modifyWebpackConfig = ({ config, stage }) => {
-  config._config.resolve.alias = {
-    react: `gatsby-plugin-react-next/node_modules/react`,
-    "react-dom": `gatsby-plugin-react-next/node_modules/react-dom`,
+  if (React.version.slice(0, 2) !== `16`) {
+    config._config.resolve.alias = {
+      react: `gatsby-plugin-react-next/node_modules/react`,
+      "react-dom": `gatsby-plugin-react-next/node_modules/react-dom`,
+    }
   }
 
   return config
