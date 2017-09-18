1bc6c3ec2d510c2dc60cf4397cfeff4f6a934729
Kyle Mathews <mathews.kyle@gmail.com>
2017-09-18 16:25:26 -0700

    Serialize date objects in markdown frontmatter to avoid type mismatches when inferring GraphQL types fixes #2096

--- packages/gatsby-transformer-remark/src/on-node-create.js
+++ packages/gatsby-transformer-remark/src/on-node-create.js
@@ -1,5 +1,6 @@
 const grayMatter = require(`gray-matter`)
 const crypto = require(`crypto`)
+const _ = require(`lodash`)
 
 module.exports = async function onCreateNode({
   node,
@@ -18,7 +19,20 @@ module.exports = async function onCreateNode({
   }
 
   const content = await loadNodeContent(node)
-  const data = grayMatter(content)
+  let data = grayMatter(content)
+
+  // Convert date objects to string. Otherwise there's type mismatches
+  // during inference as some dates are strings and others date objects.
+  if (data.data) {
+    data.data = _.mapValues(data.data, v => {
+      if (_.isDate(v)) {
+        return v.toString()
+      } else {
+        return v
+      }
+    })
+  }
+
   const contentDigest = crypto
     .createHash(`md5`)
     .update(JSON.stringify(data))
