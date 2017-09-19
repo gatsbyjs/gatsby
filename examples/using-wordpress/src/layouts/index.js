ff1fc3c6e7aade4084cd248a670ccaeffebd273c
Kyle Mathews <mathews.kyle@gmail.com>
2017-09-19 16:45:56 -0700

    Improve responsive styles for using-wordpress

--- examples/using-wordpress/src/layouts/index.js
+++ examples/using-wordpress/src/layouts/index.js
@@ -4,28 +4,49 @@ import Link from "gatsby-link"
 
 import { rhythm, scale } from "../utils/typography"
 
-const containerStyle = { maxWidth: 700, margin: `0 auto`, padding: 10 }
+const containerStyle = {
+  maxWidth: 700,
+  margin: `0 auto`,
+  padding: rhythm(3 / 4),
+}
 
 class DefaultLayout extends React.Component {
   render() {
     return (
       <div>
         <div
-          style={{
+          css={{
             background: `rgb(207, 58, 62)`,
             padding: `${rhythm(2)} ${rhythm(3 / 4)}`,
             marginBottom: rhythm(1),
           }}
         >
-          <div style={containerStyle}>
-            <h1 style={{ margin: 0, ...scale(1.9) }}>
-              <Link style={{ color: `rgb(224,203,144)` }} to="/">
+          <div css={containerStyle}>
+            <h1
+              css={{
+                margin: 0,
+                ...scale(1.5),
+                "@media screen and (min-width: 500px)": {
+                  ...scale(1.9),
+                },
+              }}
+            >
+              <Link
+                css={{
+                  color: `rgb(224,203,144)`,
+                  ":hover": {
+                    color: `rgb(224,203,144)`,
+                    textDecoration: `none`,
+                  },
+                }}
+                to="/"
+              >
                 Gatsby + Wordpress!!
               </Link>
             </h1>
           </div>
         </div>
-        <div style={containerStyle}>{this.props.children()}</div>
+        <div css={containerStyle}>{this.props.children()}</div>
       </div>
     )
   }
