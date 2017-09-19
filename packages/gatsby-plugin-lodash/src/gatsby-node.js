b5d1f0801af9415b111f7900addfc53daf0952bc
Kyle Mathews <mathews.kyle@gmail.com>
2017-09-19 12:32:27 -0700

    Enable all feature sets by default and allow opting out

--- packages/gatsby-plugin-lodash/src/gatsby-node.js
+++ packages/gatsby-plugin-lodash/src/gatsby-node.js
@@ -1,9 +1,31 @@
 const webpackLodashPlugin = require(`lodash-webpack-plugin`)
 
 // Add Lodash webpack plugin
-exports.modifyWebpackConfig = ({ config, stage }) => {
+exports.modifyWebpackConfig = ({ config, stage }, { disabledFeatures }) => {
   if (stage === `build-javascript`) {
-    config.plugin(`Lodash`, webpackLodashPlugin, null)
+    const features = {
+      shorthands: true,
+      cloning: true,
+      currying: true,
+      caching: true,
+      collections: true,
+      exotics: true,
+      guards: true,
+      metadata: true,
+      deburring: true,
+      unicode: true,
+      chaining: true,
+      memoizing: true,
+      coercions: true,
+      flattening: true,
+      paths: true,
+      placeholders: true,
+    }
+
+    disabledFeatures.forEach(feature => {
+      delete features[feature]
+    })
+    config.plugin(`Lodash`, webpackLodashPlugin, features)
   }
 
   return
