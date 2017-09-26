2cefaeb633ca72d9e5c8ad2cf0c70c8a1f068c1e
Jacob Bolda <jbolda@gmail.com>
2017-09-26 06:58:07 -0500

    add redux page multi-component test --build-all

--- packages/gatsby/src/redux/__tests__/pages.js
+++ packages/gatsby/src/redux/__tests__/pages.js
@@ -98,4 +98,94 @@ describe(`Add pages`, () => {
     expect(state).toMatchSnapshot()
     expect(state.length).toEqual(0)
   })
+
+  it(`allows you to add pages with multiple components`, () => {
+    const action = actions.createPage(
+      {
+        path: `/hi/`,
+        component: [`/whatever/index.js`, `/morewhatever/index.js`],
+      },
+      { id: `test`, name: `test` }
+    )
+    const state = reducer(undefined, action)
+    expect(action).toMatchSnapshot()
+    expect(state).toMatchSnapshot()
+  })
+
+  it(`allows you to add pages with context when using multiple components`, () => {
+    const action = actions.createPage(
+      {
+        path: `/hi/`,
+        component: [`/whatever/index.js`, `/differentwhatever/index.js`],
+        context: {
+          id: 123,
+        },
+      },
+      { id: `test`, name: `test` }
+    )
+    const state = reducer(undefined, action)
+    expect(action).toMatchSnapshot()
+    expect(state).toMatchSnapshot()
+  })
+
+  it(`allows you to add multiple pages each with multiple components`, () => {
+    const action = actions.createPage(
+      {
+        path: `/hi/`,
+        component: [`/whatever/index.js`, `/morewhatever/index.js`],
+      },
+      { id: `test`, name: `test` }
+    )
+    const action2 = actions.createPage(
+      {
+        path: `/hi/hotpizza/`,
+        component: [`/whatever/index.js`, `/pizza/index.js`],
+      },
+      { id: `test`, name: `test` }
+    )
+    let state = reducer(undefined, action)
+    state = reducer(state, action2)
+    expect(state).toMatchSnapshot()
+    expect(state.length).toEqual(2)
+  })
+
+  it(`allows you to update existing pages with multiple components (based on path)`, () => {
+    const action = actions.createPage(
+      {
+        path: `/hi/`,
+        component: [`/something/index.js`, `/morewhatever/index.js`],
+      },
+      { id: `test`, name: `test` }
+    )
+
+    // Change the component
+    const action2 = actions.createPage(
+      {
+        path: `/hi/`,
+        component: [`/whateven/index.js`, `/supercereal/index.js`],
+      },
+      { id: `test`, name: `test` }
+    )
+
+    let state = reducer(undefined, action)
+    state = reducer(state, action2)
+    expect(state).toMatchSnapshot()
+    expect(state.length).toEqual(1)
+  })
+
+  it(`allows you to delete paths that have multiple components`, () => {
+    const action = actions.createPage(
+      {
+        path: `/hi/`,
+        component: [`/ohhey/index.js`, `/goodbye/index.js`],
+      },
+      { name: `test` }
+    )
+    const action2 = actions.deletePage({ path: `/hi/` })
+
+    let state = reducer(undefined, action)
+    state = reducer(state, action2)
+    expect(state).toMatchSnapshot()
+    expect(state.length).toEqual(0)
+  })
 })
