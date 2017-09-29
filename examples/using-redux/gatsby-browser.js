45783c5e057fa7134fb8889fc4ec1fb24153691b
Fabien BERNARD <fabien0102@hotmail.com>
2017-09-29 21:38:46 +0200

    Fix redux example (#2274)
    BrowserRouter doesn't provide any `history` props. We need to use Browser instead.
    
    ref: https://github.com/ReactTraining/react-router/blob/master/packages/react-router-dom/modules/BrowserRouter.js#L24

--- examples/using-redux/gatsby-browser.js
+++ examples/using-redux/gatsby-browser.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
 import createStore from './src/state/createStore'
