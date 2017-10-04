eabc9e14f41eff280ab7bc2f84e783457e4b87fc
M4rrc0 <marcoet@gmail.com>
2017-10-04 22:41:21 +0200

    fade out base64 on full image load (#2338)
    * fade out base64 on full image load
    
    * format

--- packages/gatsby-image/src/index.js
+++ packages/gatsby-image/src/index.js
@@ -111,7 +111,12 @@ class Image extends React.Component {
 
           {/* Show the blury base64 image. */}
           {image.base64 && (
-            <Img alt={alt} title={title} src={image.base64} opacity={1} />
+            <Img
+              alt={alt}
+              title={title}
+              src={image.base64}
+              opacity={!this.state.imgLoaded ? 1 : 0}
+            />
           )}
 
           {/* Show a solid background color. */}
@@ -164,7 +169,12 @@ class Image extends React.Component {
         >
           {/* Show the blury base64 image. */}
           {image.base64 && (
-            <Img alt={alt} title={title} src={image.base64} opacity={1} />
+            <Img
+              alt={alt}
+              title={title}
+              src={image.base64}
+              opacity={!this.state.imgLoaded ? 1 : 0}
+            />
           )}
 
           {/* Show a solid background color. */}
