45a19f4516f7274c4cf5ba1819fad870bda3a2a6
Nadiia <nadiia.dmytrenko@gmail.com>
2017-10-07 17:17:55 -0700

    Adding gatsby-plugin-styletron and an example site for it. (#2365)

---
+++ packages/gatsby-plugin-styletron/src/gatsby-ssr.js
@@ -0,0 +1,25 @@
+const React = require(`react`)
+const Styletron = require(`styletron-server`)
+const { StyletronProvider } = require(`styletron-react`)
+const { renderToString } = require(`react-dom/server`)
+
+exports.replaceRenderer = ({ bodyComponent, setHeadComponents, replaceBodyHTMLString }) => {
+  const styletron = new Styletron()
+
+  const app = (
+    <StyletronProvider styletron={styletron}>
+      {bodyComponent}
+    </StyletronProvider>
+  )
+
+  replaceBodyHTMLString(renderToString(app))
+
+  const stylesheets = styletron.getStylesheets()
+  const headComponents = stylesheets.map((sheet, index) => (
+      <style media={sheet.media || `all`} key={index}>
+        {sheet.css}
+      </style>
+    ))
+
+  setHeadComponents(headComponents)
+}
