ce10ef9eff9d4b5205f92727a04ea21ef973c807
Derek Lindahl <derek.lindahl@gmail.com>
2017-10-07 17:18:49 -0700

    [gatsby-plugins-canonical-urls] Add default pathname (#2363)
    Fixes #2362 by preventing generated canonial urls from appending
    `undefined` in development mode

--- packages/gatsby-plugin-canonical-urls/src/gatsby-ssr.js
+++ packages/gatsby-plugin-canonical-urls/src/gatsby-ssr.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-exports.onRenderBody = ({ setHeadComponents, pathname }, pluginOptions) => {
+exports.onRenderBody = ({ setHeadComponents, pathname = `/` }, pluginOptions) => {
   const url = `${pluginOptions.siteUrl}${pathname}`
   setHeadComponents([<link rel="canonical" key={url} href={url} />])
 }
