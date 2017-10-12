480d9478e74d2eda4b89716192cf0e2833912a9a
Kyle Mathews <mathews.kyle@gmail.com>
2017-10-12 14:05:52 -0700

    Format

--- packages/gatsby-remark-copy-linked-files/src/index.js
+++ packages/gatsby-remark-copy-linked-files/src/index.js
@@ -4,16 +4,18 @@ const fsExtra = require(`fs-extra`)
 const path = require(`path`)
 const _ = require(`lodash`)
 const cheerio = require(`cheerio`)
-const sizeOf = require('image-size');
+const sizeOf = require(`image-size`)
 
-module.exports = ({ files, markdownNode, markdownAST, getNode }, pluginOptions) => {
+module.exports = (
+  { files, markdownNode, markdownAST, getNode },
+  pluginOptions
+) => {
   const defaults = {
-    ignoreFileExtensions: ['png', 'jpg', 'jpeg', 'bmp', 'tiff'],
+    ignoreFileExtensions: [`png`, `jpg`, `jpeg`, `bmp`, `tiff`],
   }
 
   const options = _.defaults(pluginOptions, defaults)
 
-module.exports = ({ files, markdownNode, markdownAST, getNode }) => {
   const filesToCopy = new Map()
   // Copy linked files to the public directory and modify the AST to point to
   // new location of the files.
@@ -35,10 +37,10 @@ module.exports = ({ files, markdownNode, markdownAST, getNode }) => {
           `public`,
           `${linkNode.internal.contentDigest}.${linkNode.extension}`
         )
-        
+
         // Prevent uneeded copying
-        if(linkPath === newPath) {
-          return;
+        if (linkPath === newPath) {
+          return
         }
 
         const relativePath = path.join(
@@ -56,46 +58,52 @@ module.exports = ({ files, markdownNode, markdownAST, getNode }) => {
   const generateImagesAndUpdateNode = async function(image) {
     const imagePath = path.posix.join(
       getNode(markdownNode.parent).dir,
-      image.attr('src'),
-    );
+      image.attr(`src`)
+    )
     const imageNode = _.find(files, file => {
       if (file && file.absolutePath) {
-        return file.absolutePath === imagePath;
+        return file.absolutePath === imagePath
       }
-      return null;
-    });
+      return null
+    })
     if (!imageNode || !imageNode.absolutePath) {
-      return;
+      return
     }
 
-    const initialImageSrc = image.attr('src');
+    const initialImageSrc = image.attr(`src`)
     // The link object will be modified to the new location so we'll
     // use that data to update our ref
-    const link = {url: image.attr('src')};
-    await visitor(link);
-    image.attr('src', link.url)
-    
-    let dimensions;
+    const link = { url: image.attr(`src`) }
+    await visitor(link)
+    image.attr(`src`, link.url)
 
-    if(!image.attr('width') || !image.attr('height')) {
-      dimensions = sizeOf(imageNode.absolutePath);
+    let dimensions
+
+    if (!image.attr(`width`) || !image.attr(`height`)) {
+      dimensions = sizeOf(imageNode.absolutePath)
     }
 
     // Generate default alt tag
-    const srcSplit = initialImageSrc.split('/');
-    const fileName = srcSplit[srcSplit.length - 1];
-    const fileNameNoExt = fileName.replace(/\.[^/.]+$/, '');
-    const defaultAlt = fileNameNoExt.replace(/[^A-Z0-9]/gi, ' ');
+    const srcSplit = initialImageSrc.split(`/`)
+    const fileName = srcSplit[srcSplit.length - 1]
+    const fileNameNoExt = fileName.replace(/\.[^/.]+$/, ``)
+    const defaultAlt = fileNameNoExt.replace(/[^A-Z0-9]/gi, ` `)
 
-    image.attr('alt', image.attr('alt') ? image.attr('alt') : defaultAlt)
-    image.attr('width', image.attr('width') ? image.attr('width') : dimensions.width)
-    image.attr('height', image.attr('height') ? image.attr('height') : dimensions.height)
-  };
+    image.attr(`alt`, image.attr(`alt`) ? image.attr(`alt`) : defaultAlt)
+    image.attr(
+      `width`,
+      image.attr(`width`) ? image.attr(`width`) : dimensions.width
+    )
+    image.attr(
+      `height`,
+      image.attr(`height`) ? image.attr(`height`) : dimensions.height
+    )
+  }
 
   visit(markdownAST, `link`, link => {
-    const ext = link.url.split('.').pop()
-    if(options.ignoreFileExtensions.includes(ext)) {
-      return;
+    const ext = link.url.split(`.`).pop()
+    if (options.ignoreFileExtensions.includes(ext)) {
+      return
     }
 
     visitor(link)
@@ -103,9 +111,9 @@ module.exports = ({ files, markdownNode, markdownAST, getNode }) => {
 
   // This will only work for markdown img tags
   visit(markdownAST, `image`, image => {
-    const ext = image.url.split('.').pop()
-    if(options.ignoreFileExtensions.includes(ext)) {
-      return;
+    const ext = image.url.split(`.`).pop()
+    if (options.ignoreFileExtensions.includes(ext)) {
+      return
     }
 
     const imagePath = path.join(getNode(markdownNode.parent).dir, image.url)
@@ -116,98 +124,98 @@ module.exports = ({ files, markdownNode, markdownAST, getNode }) => {
       return false
     })
 
-    if(imageNode) {
+    if (imageNode) {
       visitor(image)
     }
   })
 
   // For each HTML Node
-  visit(markdownAST, `html`, async (node) => {
-    const $ = cheerio.load(node.value);
+  visit(markdownAST, `html`, async node => {
+    const $ = cheerio.load(node.value)
     // Handle Images
-    const imageRefs = [];
-      $('img').each(function() {
-        try {
-          if (isRelativeUrl($(this).attr('src'))) {
-            imageRefs.push($(this));
-          }
-        } catch(err) {
-
+    const imageRefs = []
+    $(`img`).each(function() {
+      try {
+        if (isRelativeUrl($(this).attr(`src`))) {
+          imageRefs.push($(this))
         }
-      });
+      } catch (err) {}
+    })
 
     for (let thisImg of imageRefs) {
       try {
-        const ext = thisImg.attr('src').split('.').pop()
-        if(options.ignoreFileExtensions.includes(ext)) {
-          return;
+        const ext = thisImg
+          .attr(`src`)
+          .split(`.`)
+          .pop()
+        if (options.ignoreFileExtensions.includes(ext)) {
+          return
         }
 
-        await generateImagesAndUpdateNode(thisImg);
-      } catch (err) {
-      }
-    };
-    
-    const videoRefs = [];
-    // Handle video tags. 
-    $('video source').each(function() {
+        await generateImagesAndUpdateNode(thisImg)
+      } catch (err) {}
+    }
+
+    const videoRefs = []
+    // Handle video tags.
+    $(`video source`).each(function() {
       try {
-        if (isRelativeUrl($(this).attr('src'))) {
-          videoRefs.push($(this));
+        if (isRelativeUrl($(this).attr(`src`))) {
+          videoRefs.push($(this))
         }
-      } catch(err) {
-
-      }
-    });
+      } catch (err) {}
+    })
 
     for (let thisVideo of videoRefs) {
       try {
-        const ext = thisVideo.attr('src').split('.').pop()
-        if(options.ignoreFileExtensions.includes(ext)) {
-          return;
+        const ext = thisVideo
+          .attr(`src`)
+          .split(`.`)
+          .pop()
+        if (options.ignoreFileExtensions.includes(ext)) {
+          return
         }
-        
+
         // The link object will be modified to the new location so we'll
         // use that data to update our ref
-        const link = {url: thisVideo.attr('src')};
-        await visitor(link);
-        thisVideo.attr('src', link.url)
-      } catch (err) {
-      }
-    };
+        const link = { url: thisVideo.attr(`src`) }
+        await visitor(link)
+        thisVideo.attr(`src`, link.url)
+      } catch (err) {}
+    }
 
-    // Handle a tags. 
-    const aRefs = [];
-    $('a').each(function() {
+    // Handle a tags.
+    const aRefs = []
+    $(`a`).each(function() {
       try {
-        if (isRelativeUrl($(this).attr('href'))) {
-          aRefs.push($(this));
+        if (isRelativeUrl($(this).attr(`href`))) {
+          aRefs.push($(this))
         }
-      } catch(err) {
-
-      }
-    });
+      } catch (err) {}
+    })
 
     for (let thisATag of aRefs) {
       try {
-        const ext = thisATag.attr('href').split('.').pop()
-        if(options.ignoreFileExtensions.includes(ext)) {
-          return;
+        const ext = thisATag
+          .attr(`href`)
+          .split(`.`)
+          .pop()
+        if (options.ignoreFileExtensions.includes(ext)) {
+          return
         }
 
         // The link object will be modified to the new location so we'll
         // use that data to update our ref
-        const link = {url: thisATag.attr('href')};
-        await visitor(link);
-        thisATag.attr('href', link.url)
-      } catch (err) {
-      }
-    };
+        const link = { url: thisATag.attr(`href`) }
+        await visitor(link)
+        thisATag.attr(`href`, link.url)
+      } catch (err) {}
+    }
 
     // Replace the image node with an inline HTML node.
-    node.type = 'html';
-    node.value = $.html();
-    return;
+    node.type = `html`
+    node.value = $.html()
+    return
   })
 
   return Promise.all(
