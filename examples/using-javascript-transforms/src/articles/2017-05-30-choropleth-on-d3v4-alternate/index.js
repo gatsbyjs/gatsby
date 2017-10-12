fa9269bdff0e81debb8554d13b8eda18f2f32396
Jacob Bolda <jbolda@gmail.com>
2017-10-12 14:48:46 -0500

    Finalize Using-javascript-transforms (#2283)
    * shouldn't have put this in here
    
    * make this general
    
    * Delete future-markdown.js
    
    * Update and rename markdown.js to mdBlogPost.js
    
    * Create markdown.js
    
    * Create about.md
    
    * Delete index.js
    
    * Create master.js
    
    * Update and rename blog-post.js to blogPost.js
    
    * Update and rename inset-page.js to insetPage.js
    
    * Update package.json
    
    * update Gatsby-node.js for new layout structure
    
    * markdown only specifically for page
    
    * clean up existing graphql and moved
    
    * update to fix /contact and /about
    
    * add BlogPostChrome higher order component
    
    * update blog posts to graphql fragments
    
    * Update README.md
    
    * Run format

--- examples/using-javascript-transforms/src/articles/2017-05-30-choropleth-on-d3v4-alternate/index.js
+++ examples/using-javascript-transforms/src/articles/2017-05-30-choropleth-on-d3v4-alternate/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import BlogPostChrome from "../../components/BlogPostChrome"
 import { findDOMNode } from "react-dom"
 var d3 = require(`d3`)
 
@@ -48,10 +49,9 @@ class choroplethAltBase extends React.Component {
   render() {
     let data = this.props.data.markdownRemark
     let html = data.html
-    let frontmatter = this.props.data.jsFrontmatter.data
 
     return (
-      <div className="">
+      <BlogPostChrome {...this.props.data.jsFrontmatter.data}>
         <div className="section">
           <div className="container">
             <div id="states" />
@@ -63,7 +63,7 @@ class choroplethAltBase extends React.Component {
             <div dangerouslySetInnerHTML={{ __html: html }} />
           </div>
         </div>
-      </div>
+      </BlogPostChrome>
     )
   }
 }
@@ -209,16 +209,7 @@ export const pageQuery = graphql`
       html
     }
     jsFrontmatter(fields: { slug: { eq: $slug } }) {
-      data {
-        error
-        layoutType
-        path
-        title
-        written
-        category
-        description
-        updated
-      }
+      ...JSBlogPost_data
     }
   }
 `
