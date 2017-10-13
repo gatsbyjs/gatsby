95eb8dc8bccb0962a868d94a9ba2f9f55303a8f2
Rafael Cordones <rafael@cordones.me>
2017-10-13 20:12:29 +0200

    gatsby-source-graphcms: - removed unused code - added logic to fail processing if GraphQL query parameter is not passed in configuration

--- packages/gatsby-source-graphcms/src/gatsby-node.js
+++ packages/gatsby-source-graphcms/src/gatsby-node.js
@@ -1,12 +1,14 @@
 import { GraphQLClient } from "graphql-request"
 import R from "ramda"
 import crypto from "crypto"
-import { assembleQueries, extractTypeName } from "./util"
+import { extractTypeName } from "./util"
 
 const SOURCE_NAME = `GraphCMS`
 
+const DEBUG_MODE = false
+
 exports.sourceNodes = async (
-  { boundActionCreators, getNode, hasNodeChanged },
+  { boundActionCreators, reporter },
   { endpoint, token, query }
 ) => {
   const { createNode } = boundActionCreators
@@ -21,69 +23,41 @@ exports.sourceNodes = async (
 
   if (query) {
     const userQueryResult = await client.request(query)
-    console.log(userQueryResult)
-    R.forEachObjIndexed(createNodes(createNode), userQueryResult)
-  }
-
-  // TODO: Use introspection to construct a query to fetch all data and add them as nodes.
-  // const typeData = await client.request(metaQuery)
-  //
-  // const {
-  //   __type: {
-  //     possibleTypes
-  //   },
-  // } = typeData
-  //
-  // R.map(type => {
-  //   console.log('type ', type.name, 'has fields: \n', type.fields);
-  // })(possibleTypes)
-
-  // const fullData = await client.request(assembleQueries(typeData))
-}
-
-const findEmbeddedFields = R.map(field => {
-  console.log(`field: `, field.name, `is type: `, field.type.name)
-  //is a node
-
-  const embeddedField = R.find(R.propEq(`name`, field.type.name))(possibleTypes)
-  console.log(`embedded field is `, embeddedField)
-})
-
-// Query for fetching all node times from graphql endpoint.
-const metaQuery = `
-query getTypeFields {
-  __type(name: "Node") {
-    possibleTypes {
-      name
-      fields {
-        name
-        type {
-          name
-        }
-      }
+    if (DEBUG_MODE) {
+      const jsonUserQueryResult = JSON.stringify(userQueryResult, undefined, 2)
+      console.log(`\ngatsby-source-graphcms: GraphQL query results: ${jsonUserQueryResult}`)
     }
+    R.forEachObjIndexed(createNodes(createNode, reporter), userQueryResult)
+  } else {
+    reporter.panic(`gatsby-source-graphcms: you need to provide a GraphQL query in the plugin 'query' parameter`)
   }
 }
-`
 
-const createNodes = createNode => (value, key) => {
-  R.forEach(node => {
-    const { id, ...fields } = node
+const createNodes = (createNode, reporter) => (value, key) => {
+  R.forEach(queryResultNode => {
+    const { id, ...fields } = queryResultNode
+    const jsonNode = JSON.stringify(queryResultNode)
+    const gatsbyNode = {
+        id,
+        ...fields,
+        parent: `${SOURCE_NAME}_${key}`,
+        children: [],
+        internal: {
+          type: extractTypeName(key),
+          content: jsonNode,
+          contentDigest: crypto.createHash(`md5`).update(jsonNode).digest(`hex`),
+        },
+      }
 
-    const jsonNode = JSON.stringify(node)
+    if (DEBUG_MODE) {
+      const jsonFields = JSON.stringify(fields)
+      const jsonGatsbyNode = JSON.stringify(gatsbyNode)
+      reporter.info(`  processing node: ${jsonNode}`)
+      reporter.info(`    node id ${id}`)
+      reporter.info(`    node fields: ${jsonFields}`)
+      reporter.info(`    gatsby node: ${jsonGatsbyNode}`)
+    }
 
-    console.log(`Making node with id `, id, ` and fields: `, fields)
-
-    createNode({
-      id,
-      ...fields,
-      parent: `${SOURCE_NAME}_${key}`,
-      children: [],
-      internal: {
-        type: extractTypeName(key),
-        content: jsonNode,
-        contentDigest: crypto.createHash(`md5`).update(jsonNode).digest(`hex`),
-      },
-    })
+    createNode(gatsbyNode)
   }, value)
 }
