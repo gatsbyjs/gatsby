52f2efa518ed7f1e028bd2ccf7cee15e27d2cb5b
Kyle Mathews <mathews.kyle@gmail.com>
2017-10-14 09:43:38 -0700

    Run format

--- packages/gatsby-image/src/index.js
+++ packages/gatsby-image/src/index.js
@@ -159,7 +159,9 @@ class Image extends React.Component {
       // The outer div is necessary to reset the z-index to 0.
       return (
         <div
-          className={`${outerWrapperClassName ? outerWrapperClassName : ``} gatsby-image-outer-wrapper`}
+          className={`${outerWrapperClassName
+            ? outerWrapperClassName
+            : ``} gatsby-image-outer-wrapper`}
           style={{
             zIndex: 0,
             // Let users set component to be absolutely positioned.
@@ -251,7 +253,9 @@ class Image extends React.Component {
       // The outer div is necessary to reset the z-index to 0.
       return (
         <div
-          className={`${outerWrapperClassName ? outerWrapperClassName : ``} gatsby-image-outer-wrapper`}
+          className={`${outerWrapperClassName
+            ? outerWrapperClassName
+            : ``} gatsby-image-outer-wrapper`}
           style={{
             zIndex: 0,
             // Let users set component to be absolutely positioned.
@@ -326,7 +330,10 @@ Image.propTypes = {
   title: PropTypes.string,
   alt: PropTypes.string,
   className: PropTypes.oneOfType([PropTypes.string, PropTypes.object]), // Support Glamor's css prop.
-  outerWrapperClassName: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+  outerWrapperClassName: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.object,
+  ]),
   style: PropTypes.object,
   position: PropTypes.string,
   backgroundColor: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
