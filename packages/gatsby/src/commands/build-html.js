1a0239692418f0fcf1ebc8376c6ab6b9e175c22c
Markus Chmelar <mexx@innovaptor.com>
2017-10-16 21:19:16 +0200

    Handle undefined webpack build error (#2360)
    In some cases, `stats.compilation.errors` contains an `undefined`. In these cases, `toJson` crashes when accessing a method on this undefined object. 
    This change now filters out `undefined` errors

--- packages/gatsby/src/commands/build-html.js
+++ packages/gatsby/src/commands/build-html.js
@@ -30,7 +30,7 @@ module.exports = async (program: any) => {
       }
       const outputFile = `${directory}/public/render-page.js`
       if (stats.hasErrors()) {
-        let webpackErrors = stats.toJson().errors
+        let webpackErrors = stats.compilation.errors.filter((e) => e)
         return reject(
           createErrorFromString(webpackErrors[0], `${outputFile}.map`)
         )
