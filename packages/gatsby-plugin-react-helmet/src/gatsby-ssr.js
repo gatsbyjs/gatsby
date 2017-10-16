4b31804f933edf075584c3f0a31d078f988f27db
Kyle Mathews <mathews.kyle@gmail.com>
2017-10-16 13:06:57 -0700

    Check if set(Body|Html)Attributes functions exist before using (#2476)
    Since older versions of Gatsby won't have them.

--- packages/gatsby-plugin-react-helmet/src/gatsby-ssr.js
+++ packages/gatsby-plugin-react-helmet/src/gatsby-ssr.js
@@ -5,8 +5,13 @@ exports.onRenderBody = (
   pluginOptions
 ) => {
   const helmet = Helmet.renderStatic()
-  setHtmlAttributes(helmet.htmlAttributes.toComponent())
-  setBodyAttributes(helmet.bodyAttributes.toComponent())
+  // These action functions were added partway through the Gatsby 1.x cycle.
+  if (setHtmlAttributes) {
+    setHtmlAttributes(helmet.htmlAttributes.toComponent())
+  }
+  if (setBodyAttributes) {
+    setBodyAttributes(helmet.bodyAttributes.toComponent())
+  }
   setHeadComponents([
     helmet.title.toComponent(),
     helmet.link.toComponent(),
