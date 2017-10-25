2f398f97adcf959216ab7a679df3b1c7bbf9262b
Emmanuel Fallancy <emmanuel.fallancy@gmail.com>
2017-10-25 23:25:37 +1100

    Add console warning as prevention on drop off of UI from being displayed

--- packages/gatsby-react-router-scroll/src/StateStorage.js
+++ packages/gatsby-react-router-scroll/src/StateStorage.js
@@ -2,20 +2,38 @@ const STATE_KEY_PREFIX = `@@scroll|`
 
 export default class SessionStorage {
   read(location, key) {
-    const stateKey = this.getStateKey(location, key)
-    const value = sessionStorage.getItem(stateKey)
-    return JSON.parse(value)
+    try {
+      const stateKey = this.getStateKey(location, key)
+      const value = sessionStorage.getItem(stateKey)
+      return JSON.parse(value)
+    } catch (e) {
+      console.warn('[gatsby-react-router-scroll] Unable to access sessionStorage; sessionStorage is not available.');
+      return;
+    }
+
   }
 
   save(location, key, value) {
-    const stateKey = this.getStateKey(location, key)
-    const storedValue = JSON.stringify(value)
-    sessionStorage.setItem(stateKey, storedValue)
+    try {
+      const stateKey = this.getStateKey(location, key)
+      const storedValue = JSON.stringify(value)
+      sessionStorage.setItem(stateKey, storedValue)
+    } catch (e) {
+      console.warn('[gatsby-react-router-scroll] Unable to save state in sessionStorage; sessionStorage is not available.');
+      return;
+    }
+
   }
 
   getStateKey(location, key) {
-    const locationKey = location.key
-    const stateKeyBase = `${STATE_KEY_PREFIX}${locationKey}`
-    return key == null ? stateKeyBase : `${stateKeyBase}|${key}`
+    try {
+      const locationKey = location.key
+      const stateKeyBase = `${STATE_KEY_PREFIX}${locationKey}`
+      return key == null ? stateKeyBase : `${stateKeyBase}|${key}`
+    } catch (e) {
+      console.warn('Please enable cookies');
+      return;
+    }
+
   }
 }
