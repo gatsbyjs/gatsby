c211bc65ad6f9ee67c2790de77a528f2e2523078
Emmanuel Fallancy <emmanuel.fallancy@gmail.com>
2017-10-26 16:42:42 +1100

    Preserve scroll position in window global when sessionStorage is not available

--- packages/gatsby-react-router-scroll/src/StateStorage.js
+++ packages/gatsby-react-router-scroll/src/StateStorage.js
@@ -1,23 +1,38 @@
 const STATE_KEY_PREFIX = `@@scroll|`
+const GATSBY_ROUTER_SCROLL_STATE = `___GATSBY_REACT_ROUTER_SCROLL`
 
 export default class SessionStorage {
   read(location, key) {
+    const stateKey = this.getStateKey(location, key)
+
     try {
-      const stateKey = this.getStateKey(location, key)
       const value = sessionStorage.getItem(stateKey)
       return JSON.parse(value)
     } catch (e) {
       console.warn(`[gatsby-react-router-scroll] Unable to access sessionStorage; sessionStorage is not available.`)
+
+      if (window && window[GATSBY_ROUTER_SCROLL_STATE] && window[GATSBY_ROUTER_SCROLL_STATE][stateKey]) {
+        return window[GATSBY_ROUTER_SCROLL_STATE][stateKey]
+      }
+
       return ({})
     }
   }
 
   save(location, key, value) {
+    const stateKey = this.getStateKey(location, key)
+    const storedValue = JSON.stringify(value)
+
     try {
-      const stateKey = this.getStateKey(location, key)
-      const storedValue = JSON.stringify(value)
       sessionStorage.setItem(stateKey, storedValue)
     } catch (e) {
+      if (window && window[GATSBY_ROUTER_SCROLL_STATE]) {
+        window[GATSBY_ROUTER_SCROLL_STATE][stateKey] = JSON.parse(storedValue)
+      } else {
+        window[GATSBY_ROUTER_SCROLL_STATE] = {}
+        window[GATSBY_ROUTER_SCROLL_STATE][stateKey] = JSON.parse(storedValue)
+      }
+
       console.warn(`[gatsby-react-router-scroll] Unable to save state in sessionStorage; sessionStorage is not available.`)
     }
 
