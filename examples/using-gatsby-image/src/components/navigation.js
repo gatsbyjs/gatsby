dd1ae12f7beaa6fa9ad2e8fdd2259ec099ecd9a1
Florian Kissling <fk@users.noreply.github.com>
2017-10-26 03:25:05 +0200

    [using-gatsby-image] Improve default/active nav item contrast, fix SVG size labels (#2630)
    * Fix active/default state
    
    * Fix SVG size labels

--- examples/using-gatsby-image/src/components/navigation.js
+++ examples/using-gatsby-image/src/components/navigation.js
@@ -5,7 +5,11 @@ import { rhythm, scale, options } from "../utils/typography"
 
 const Navigation = () => (
   <div>
-    <h1 css={{ marginBottom: options.blockMarginBottom }}>
+    <h1
+      css={{
+        marginBottom: 0,
+      }}
+    >
       <Link
         to="/"
         css={{
@@ -17,46 +21,63 @@ const Navigation = () => (
         Gatsby Image
       </Link>
     </h1>
-    <nav
+    <ul
       css={{
         fontFamily: options.headerFontFamily.join(`,`),
-        "& a": {
-          color: options.bodyColor,
-          opacity: 0.7,
-          textTransform: `uppercase`,
-          backgroundImage: `none`,
+        margin: 0,
+        listStyle: `none`,
+        "& li": {
+          display: `inline`,
           fontWeight: `300`,
-          transition: `all 200ms ease-out`,
-          letterSpacing: `.005em`,
         },
-        "& a:after": {
+        "& li:after": {
           content: ` / `,
           color: options.bodyColor,
           opacity: 0.2,
           padding: `0 ${rhythm(options.blockMarginBottom / 4)}`,
         },
-        "& a:last-child:after": {
+        "& li:last-child:after": {
           display: `none`,
+        },
+        "& a": {
+          color: options.bodyColor,
+          opacity: 0.57,
+          textTransform: `uppercase`,
+          backgroundImage: `none`,
+          transition: `all 200ms ease-out`,
+          letterSpacing: `.005em`,
         },
         "& .nav-link-active, & a:hover": {
           color: options.accentColor,
           opacity: 1,
+          backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0) 1px, ${options.accentColor} 1px, ${options.accentColor} 2px, rgba(0, 0, 0, 0) 2px);`,
+        },
+        "& .nav-link-active": {
+          // fontWeight: `700`,
         },
       }}
     >
-      <Link to="/blur-up/" activeClassName="nav-link-active">
-        Blur Up
-      </Link>
-      <Link to="/background-color/" activeClassName="nav-link-active">
-        Background Color
-      </Link>
-      <Link to="/traced-svg/" activeClassName="nav-link-active">
-        Traced SVG
-      </Link>
-      <Link to="/prefer-webp/" activeClassName="nav-link-active">
-        Prefer WebP
-      </Link>
-    </nav>
+      <li>
+        <Link to="/blur-up/" activeClassName="nav-link-active">
+          Blur Up
+        </Link>
+      </li>
+      <li>
+        <Link to="/background-color/" activeClassName="nav-link-active">
+          Background Color
+        </Link>
+      </li>
+      <li>
+        <Link to="/traced-svg/" activeClassName="nav-link-active">
+          Traced SVG
+        </Link>
+      </li>
+      <li>
+        <Link to="/prefer-webp/" activeClassName="nav-link-active">
+          Prefer WebP
+        </Link>
+      </li>
+    </ul>
   </div>
 )
 
