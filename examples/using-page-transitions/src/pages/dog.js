d9ea4fc9c6c9880a9c2c1e1fae20b33aef1dd2da
Andy Kenward <hello@andykenward.com>
2017-10-30 00:10:13 +0000

    [using-page-transitions] fixes #2669 example missing transition
    There should probably be a default style instead. Which will cover SSR and javascript turned off.

--- examples/using-page-transitions/src/pages/dog.js
+++ examples/using-page-transitions/src/pages/dog.js
@@ -3,7 +3,7 @@ import React from "react"
 import Link from "gatsby-link"
 
 const Dog = ({ transition }) => (
-  <div style={transition.style}>
+  <div style={transition && transition.style}>
     <h1>ruff</h1>
     <div>
       <Link to="/">Go to home</Link>
