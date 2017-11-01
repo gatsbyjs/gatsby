0ca61dfb6c67dca60b7468ccc3d9faca0628932c
Jason Quense <monastic.panic@gmail.com>
2017-11-01 14:14:59 -0400

    Add missing dips and www to workspace

--- packages/gatsby-plugin-react-next/src/gatsby-node.js
+++ packages/gatsby-plugin-react-next/src/gatsby-node.js
@@ -1,11 +1,17 @@
-const path = require(`path`)
-const React = require(path.resolve(process.cwd(), `node_modules/react`))
+const resolve = require(`resolve`)
+
+const rootReact = resolve.sync(`react`, {
+  basedir: process.cwd(),
+  preserveSymlinks: true,
+})
+console.log(rootReact)
+const React = require(rootReact)
 
 exports.modifyWebpackConfig = ({ config, stage }) => {
   if (React.version.slice(0, 2) !== `16`) {
     config._config.resolve.alias = {
       react: `gatsby-plugin-react-next/node_modules/react`,
-      "react-dom": `gatsby-plugin-react-next/node_modules/react-dom`,
+      'react-dom': `gatsby-plugin-react-next/node_modules/react-dom`,
     }
   }
 
