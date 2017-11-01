e4527f91b5f4fcf930606adc09ad023896bdc9e3
Steven Surgnier <stevensurgnier@gmail.com>
2017-11-01 12:41:03 -0400

    add new browser api runner, replaceHistory (#2695)
    * add replaceHistory browser api runner
    
    * add replaceHistory docs
    
    * use history::getUserConfirmation and CustomEvent to choreograph exit
    
    * use double quotes
    
    * Add a long page example
    
    * Run format

--- examples/using-page-transitions/src/pages/index.js
+++ examples/using-page-transitions/src/pages/index.js
@@ -11,6 +11,9 @@ const Index = ({ transition }) => (
     <div>
       <Link to="/dog/">Go to dog</Link>
     </div>
+    <div>
+      <Link to="/long-page/">Go to long page</Link>
+    </div>
   </div>
 )
 
--- packages/gatsby/cache-dir/dev-history.js
+++
@@ -1,2 +0,0 @@
-import createHistory from "history/createBrowserHistory"
-module.exports = createHistory()
