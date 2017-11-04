216ea80fd7763139035e638438a8601556b51e55
Kyle Mathews <mathews.kyle@gmail.com>
2017-11-04 11:40:15 -0700

    Only call onPostBootstrap when it's actually done (#2789)

--- packages/gatsby/src/bootstrap/index.js
+++ packages/gatsby/src/bootstrap/index.js
@@ -372,7 +372,14 @@ module.exports = async (args: BootstrapArgs) => {
       report.log(``)
       report.info(`bootstrap finished - ${process.uptime()} s`)
       report.log(``)
-      resolve({ graphqlRunner })
+
+      // onPostBootstrap
+      activity = report.activityTimer(`onPostBootstrap`)
+      activity.start()
+      apiRunnerNode(`onPostBootstrap`).then(() => {
+        activity.end()
+        resolve({ graphqlRunner })
+      })
     }
   }, 100)
 
@@ -390,16 +397,7 @@ module.exports = async (args: BootstrapArgs) => {
   } else {
     return new Promise(resolve => {
       // Wait until all side effect jobs are finished.
-      emitter.on(`END_JOB`, () => {
-        // onPostBootstrap
-        activity = report.activityTimer(`onPostBootstrap`)
-        activity.start()
-        apiRunnerNode(`onPostBootstrap`).then(() => {
-          activity.end()
-
-          return checkJobsDone(resolve)
-        })
-      })
+      emitter.on(`END_JOB`, () => checkJobsDone(resolve))
     })
   }
 }
