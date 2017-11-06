4acfa571f95b2eb9a4f572aa4e72d03bb4977688
Jacob Blomgren <jacob.blomgren@hotmail.com>
2017-11-07 00:14:02 +0100

    Add onLoad prop to gatsby-image (#2818)

--- packages/gatsby-image/src/index.js
+++ packages/gatsby-image/src/index.js
@@ -260,8 +260,10 @@ class Image extends React.Component {
                 opacity={
                   this.state.imgLoaded || this.props.fadeIn === false ? 1 : 0
                 }
-                onLoad={() =>
-                  this.state.IOSupported && this.setState({ imgLoaded: true })}
+                onLoad={() => {
+                  this.state.IOSupported && this.setState({ imgLoaded: true })
+                  this.props.onLoad && this.props.onLoad()
+                }}
               />
             )}
           </div>
@@ -358,7 +360,10 @@ class Image extends React.Component {
                 opacity={
                   this.state.imgLoaded || this.props.fadeIn === false ? 1 : 0
                 }
-                onLoad={() => this.setState({ imgLoaded: true })}
+                onLoad={() => {
+                  this.setState({ imgLoaded: true })
+                  this.props.onLoad && this.props.onLoad()
+                }}
               />
             )}
           </div>
@@ -391,6 +396,7 @@ Image.propTypes = {
   style: PropTypes.object,
   position: PropTypes.string,
   backgroundColor: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
+  onLoad: PropTypes.func,
 }
 
 export default Image
