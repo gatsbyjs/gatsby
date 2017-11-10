9e75ca839dac00e94faaa5e2b0b2eec349120a63
Florian Kissling <flo@6230.org>
2017-11-10 03:55:05 +0100

    Don’t use remark-images for examples/using-remark

--- examples/using-remark/gatsby-config.js
+++ examples/using-remark/gatsby-config.js
@@ -22,18 +22,26 @@ module.exports = {
       options: {
         plugins: [
           {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 740,
-            },
-          },
-          {
             resolve: `gatsby-remark-responsive-iframe`,
             options: {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
-          `gatsby-remark-copy-linked-files`,
+          {
+            resolve: 'gatsby-remark-copy-linked-files',
+            options: {
+              // `ignoreFileExtensions` defaults to [`png`, `jpg`, `jpeg`, `bmp`, `tiff`]
+              // as we assume you'll use gatsby-remark-images to handle
+              // images in markdown as it automatically creates responsive
+              // versions of images.
+              //
+              // If you'd like to not use gatsby-remark-images and just copy your
+              // original images to the public directory, set
+              // `ignoreFileExtensions` to an empty array.
+              ignoreFileExtensions: [],
+            },
+          },
+
           {
             resolve: `gatsby-remark-smartypants`,
             options: {
