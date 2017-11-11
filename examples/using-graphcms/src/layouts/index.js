831b8aca04c2594be252a3d4c3794355ea184423
Ricky de Laveaga <git@rdela.com>
2017-11-10 19:33:33 -0800

    run `yarn format-examples`

--- examples/using-graphcms/src/layouts/index.js
+++ examples/using-graphcms/src/layouts/index.js
@@ -1,30 +1,30 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
+import React from "react"
+import PropTypes from "prop-types"
+import Link from "gatsby-link"
+import Helmet from "react-helmet"
 
-import './index.css'
+import "./index.css"
 
 const Header = () => (
   <div
     style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
+      background: `rebeccapurple`,
+      marginBottom: `1.45rem`,
     }}
   >
     <div
       style={{
-        margin: '0 auto',
+        margin: `0 auto`,
         maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
+        padding: `1.45rem 1.0875rem`,
       }}
     >
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
-            color: 'white',
-            textDecoration: 'none',
+            color: `white`,
+            textDecoration: `none`,
           }}
         >
           Gatsby GraphCMS Example
@@ -39,16 +39,16 @@ const TemplateWrapper = ({ children }) => (
     <Helmet
       title="Gatsby GraphCMS Example"
       meta={[
-        { name: 'description', content: 'Gatsby GraphCMS Example' },
-        { name: 'keywords', content: 'gatsby, graphcms, example' },
+        { name: `description`, content: `Gatsby GraphCMS Example` },
+        { name: `keywords`, content: `gatsby, graphcms, example` },
       ]}
     />
     <Header />
     <div
       style={{
-        margin: '0 auto',
+        margin: `0 auto`,
         maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
+        padding: `0px 1.0875rem 1.45rem`,
         paddingTop: 0,
       }}
     >
--- examples/using-graphcms/src/pages/index.js
+++ examples/using-graphcms/src/pages/index.js
@@ -1,30 +1,30 @@
-import React, { Component } from 'react';
-import StarRatingComponent from 'react-star-rating-component';
-import Link from 'gatsby-link';
+import React, { Component } from "react"
+import StarRatingComponent from "react-star-rating-component"
+import Link from "gatsby-link"
 
 class IndexPage extends Component {
   render() {
-    const artists = this.props.data.artists.edges;
-    const records = this.props.data.records.edges;
-    const reviews = this.props.data.reviews.edges;
+    const artists = this.props.data.artists.edges
+    const records = this.props.data.records.edges
+    const reviews = this.props.data.reviews.edges
 
-    console.log(`artists: `, artists);
-    console.log(`records: `, records);
-    console.log(`reviews: `, reviews);
+    console.log(`artists: `, artists)
+    console.log(`records: `, records)
+    console.log(`reviews: `, reviews)
 
     return (
       <div>
         <section className="artists">
           <p>
-            Welcome to your new Gatsby example site using the
-            GraphCMS source plugin.
+            Welcome to your new Gatsby example site using the GraphCMS source
+            plugin.
           </p>
           <h2>Artists</h2>
           <nav>
             <ul
               style={{
-                listStyle: 'none',
-                margin: '0 0 2rem',
+                listStyle: `none`,
+                margin: `0 0 2rem`,
               }}
             >
               {artists.map(({ node }, i) => (
@@ -42,31 +42,42 @@ class IndexPage extends Component {
               <h3 id={node.slug}>{node.name}</h3>
               <figure>
                 <img
-                src={`https://media.graphcms.com/resize=w:512,h:512,a:top,fit:crop/${node.picture.handle}`}
-                alt={node.name}
-                title={node.name}
-                width="256"
+                  src={`https://media.graphcms.com/resize=w:512,h:512,a:top,fit:crop/${
+                    node.picture.handle
+                  }`}
+                  alt={node.name}
+                  title={node.name}
+                  width="256"
                 />
                 <figcaption>
-                  <small style={{
-                    fontFamily: '-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif',
-                  }}>
-                    <a href={`https://media.graphcms.com/${node.picture.handle}`}>
-                      full-size, hi-res photo:
-                      ({node.picture.width} W &times; {node.picture.height} H)
-                      </a>
+                  <small
+                    style={{
+                      fontFamily:
+                        `-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif`,
+                    }}
+                  >
+                    <a
+                      href={`https://media.graphcms.com/${node.picture.handle}`}
+                    >
+                      full-size, hi-res photo: ({node.picture.width} W &times;{` `}
+                      {node.picture.height} H)
+                    </a>
                   </small>
                 </figcaption>
               </figure>
               <ul
                 style={{
-                  listStyle: 'none',
-                  margin: '0 0 3rem',
+                  listStyle: `none`,
+                  margin: `0 0 3rem`,
                 }}
               >
                 {node.records.map((record, i) => (
                   <li key={record.id}>
-                    <h4><p><Link to={`#${record.slug}`}>{record.title}</Link></p></h4>
+                    <h4>
+                      <p>
+                        <Link to={`#${record.slug}`}>{record.title}</Link>
+                      </p>
+                    </h4>
                   </li>
                 ))}
               </ul>
@@ -76,29 +87,45 @@ class IndexPage extends Component {
         <section className="records">
           <h2>Records</h2>
           {records.map(({ node }, i) => (
-            <article id={node.slug} key={node.id} style={{
-              marginBottom: '3rem',
-            }}>
+            <article
+              id={node.slug}
+              key={node.id}
+              style={{
+                marginBottom: `3rem`,
+              }}
+            >
               <figure>
                 <img
-                src={`https://media.graphcms.com/resize=w:512,h:512,a:top,fit:crop/${node.cover.handle}`}
-                alt={node.title}
-                title={node.title}
-                width="256"
+                  src={`https://media.graphcms.com/resize=w:512,h:512,a:top,fit:crop/${
+                    node.cover.handle
+                  }`}
+                  alt={node.title}
+                  title={node.title}
+                  width="256"
                 />
-              <figcaption>
-                <h3>{node.title}</h3>
-              </figcaption>
-              { node.artist ?
-                <p><Link to={`#${node.artist.slug}`}>{node.artist.name}</Link></p>
-                : <p>(Compilation album, various artists)</p>
-              }
-              {node.tracks.map((track, i) => (
-                <h6 key={track.id}>{track.title} {new Date(1000 * track.aliasedLength).toISOString().substr(14, 5)}</h6>
-              ))}
+                <figcaption>
+                  <h3>{node.title}</h3>
+                </figcaption>
+                {node.artist ? (
+                  <p>
+                    <Link to={`#${node.artist.slug}`}>{node.artist.name}</Link>
+                  </p>
+                ) : (
+                  <p>(Compilation album, various artists)</p>
+                )}
+                {node.tracks.map((track, i) => (
+                  <h6 key={track.id}>
+                    {track.title}{` `}
+                    {new Date(1000 * track.aliasedLength)
+                      .toISOString()
+                      .substr(14, 5)}
+                  </h6>
+                ))}
               </figure>
               {node.reviews.map((review, i) => (
-                <p key={review.id}><Link to={`#${review.slug}`}>{review.title}</Link></p>
+                <p key={review.id}>
+                  <Link to={`#${review.slug}`}>{review.title}</Link>
+                </p>
               ))}
             </article>
           ))}
@@ -106,48 +133,59 @@ class IndexPage extends Component {
         <section className="reviews">
           <h2>Reviews</h2>
           {reviews.map(({ node }, i) => (
-            <article key={node.id} style={{
-              marginBottom: '2rem',
-            }}>
+            <article
+              key={node.id}
+              style={{
+                marginBottom: `2rem`,
+              }}
+            >
               <h3 id={node.slug}>{node.title}</h3>
-              <p>for <Link to={`#${node.record.slug}`}>{node.record.title}</Link> by <Link to={`#${node.record.artist.slug}`}>{node.record.artist.name}</Link></p>
-              { node.rating &&
-                <div className='star-wrapper' style={{
-                    fontSize: '1.5rem',
-                    marginBottom: '1rem',
-                  }}>
+              <p>
+                for <Link to={`#${node.record.slug}`}>{node.record.title}</Link>{` `}
+                by{` `}
+                <Link to={`#${node.record.artist.slug}`}>
+                  {node.record.artist.name}
+                </Link>
+              </p>
+              {node.rating && (
+                <div
+                  className="star-wrapper"
+                  style={{
+                    fontSize: `1.5rem`,
+                    marginBottom: `1rem`,
+                  }}
+                >
                   <StarRatingComponent
-                    name='Rating'
-                    className='rating'
+                    name="Rating"
+                    className="rating"
                     starCount={5}
                     value={node.rating}
                     editing={false}
                   />
                 </div>
-              }
-              { node.review &&
+              )}
+              {node.review && (
                 <div>
                   <p>{node.review}</p>
                 </div>
-              }
-              { node.comments.length ?
+              )}
+              {node.comments.length ? (
                 <div>
-                <h6>Comments</h6>
-                { node.comments.map((comment, i) => (
-                  <p key={comment.body}>{comment.body}</p>
-                ))}
+                  <h6>Comments</h6>
+                  {node.comments.map((comment, i) => (
+                    <p key={comment.body}>{comment.body}</p>
+                  ))}
                 </div>
-                : null
-              }
+              ) : null}
             </article>
           ))}
         </section>
       </div>
-    );
+    )
   }
 }
 
-export default IndexPage;
+export default IndexPage
 
 export const pageQuery = graphql`
   query getAllArtistsRecordsReviews {
@@ -221,4 +259,5 @@ export const pageQuery = graphql`
         }
       }
     }
-  }`;
+  }
+`
