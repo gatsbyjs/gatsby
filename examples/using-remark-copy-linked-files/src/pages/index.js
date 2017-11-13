aeb5a2568bf16726dc482a53e4d4513aafb43ca9
Florian Kissling <flo@6230.org>
2017-11-13 04:55:01 +0100

    Add examples/using-remark-copy-linked-files
    Not making real tests obsolete at all, but all I could come up with for now.

---
+++ examples/using-remark-copy-linked-files/src/pages/index.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import get from 'lodash/get'
+import Helmet from 'react-helmet'
+
+import Bio from '../components/Bio'
+import { rhythm } from '../utils/typography'
+
+class BlogIndex extends React.Component {
+  render() {
+    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
+    const posts = get(this, 'props.data.allMarkdownRemark.edges')
+
+    return (
+      <div>
+        <Helmet title={siteTitle} />
+        <Bio />
+        {posts.map(post => {
+          if (post.node.frontmatter.path !== '/404/') {
+            const title = get(post, 'node.frontmatter.title') || post.node.path
+            return (
+              <div key={post.node.frontmatter.path}>
+                <h3
+                  style={{
+                    marginBottom: rhythm(1 / 4),
+                  }}
+                >
+                  <Link
+                    style={{ boxShadow: 'none' }}
+                    to={post.node.frontmatter.path}
+                  >
+                    {title}
+                  </Link>
+                </h3>
+                <small>{post.node.frontmatter.date}</small>
+                <p dangerouslySetInnerHTML={{ __html: post.node.excerpt }} />
+              </div>
+            )
+          }
+        })}
+      </div>
+    )
+  }
+}
+
+export default BlogIndex
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          frontmatter {
+            path
+            date(formatString: "DD MMMM, YYYY")
+          }
+          frontmatter {
+            title
+          }
+        }
+      }
+    }
+  }
+`
