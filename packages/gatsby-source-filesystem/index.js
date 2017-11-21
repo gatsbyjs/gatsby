2b0036a4313784c913c3599262d80c641e0ac8af
Matt Uttridge <uttrasey@gmail.com>
2017-11-21 17:13:43 +0000

    Found a hook to start from

--- packages/gatsby-source-filesystem/index.js
+++ packages/gatsby-source-filesystem/index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var fs = require(`fs-extra`);
+const fs = require(`fs-extra`);
 
 function loadNodeContent(fileNode) {
   return fs.readFile(fileNode.absolutePath, `utf-8`);
--- packages/gatsby/src/schema/index.js
+++ packages/gatsby/src/schema/index.js
@@ -1,11 +1,26 @@
 /* @flow */
 const _ = require(`lodash`)
-const { GraphQLSchema, GraphQLObjectType } = require(`graphql`)
+const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLNonNull } = require(`graphql`)
 
 const buildNodeTypes = require(`./build-node-types`)
 const buildNodeConnections = require(`./build-node-connections`)
 const { store } = require(`../redux`)
 const invariant = require(`invariant`)
+
+const genExtra = () => {
+  console.log(`in the mixer`)
+  return {
+    search: {
+      type: GraphQLString,
+      args: {
+        text: { type : new GraphQLNonNull(GraphQLString) },
+      },
+      resolve(root, args) {
+        return args.text
+      },
+    },
+  }
+}
 
 module.exports = async () => {
   const typesGQL = await buildNodeTypes()
@@ -17,10 +32,12 @@ module.exports = async () => {
   invariant(!_.isEmpty(nodes), `There are no available GQL nodes`)
   invariant(!_.isEmpty(connections), `There are no available GQL connections`)
 
+  const extra = genExtra()
+
   const schema = new GraphQLSchema({
     query: new GraphQLObjectType({
       name: `RootQueryType`,
-      fields: { ...connections, ...nodes },
+      fields: { ...connections, ...nodes, ...extra },
     }),
   })
 
