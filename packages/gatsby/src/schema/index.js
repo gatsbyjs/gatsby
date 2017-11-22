675afcb3a489482d2e477e8417c825eb09706235
Matt Uttridge <uttrasey@gmail.com>
2017-11-22 12:25:12 +0000

    Acheived using new API

--- packages/gatsby/src/schema/index.js
+++ packages/gatsby/src/schema/index.js
@@ -1,60 +1,37 @@
 /* @flow */
 const _ = require(`lodash`)
-const { GraphQLSchema, GraphQLObjectType, GraphQLUnionType, GraphQLList } = require(`graphql`)
+const { GraphQLSchema, GraphQLObjectType } = require(`graphql`)
 
+const apiRunner = require(`../utils/api-runner-node`)
 const buildNodeTypes = require(`./build-node-types`)
 const buildNodeConnections = require(`./build-node-connections`)
 const { store, getNodes } = require(`../redux`)
 const invariant = require(`invariant`)
 
-/**
- * The GraphQL definition of our shape union type
- */
-const defineShapesType = (CircleGql, SquareGql) => (
-  new GraphQLUnionType({
-    name: `AllShapesType`,
-    description: `All shapes under one roof`,
-    types: [ CircleGql, SquareGql ],
-    resolveType: (data) => {
-      const { type } = data.internal
-      switch (type) {
-        case `ContentfulCircle`:
-          return CircleGql
-        case `ContentfulSquare`:
-          return SquareGql
-        default:
-          return null
-      }
-    },
+async function getEnhancedGqlFields(typesGQL) {
+  const enhancedFields = await apiRunner(`enhanceSchema`, {
+    types: typesGQL,
+    allNodes: getNodes(),
+    traceId: `initial-enhanceSchema`,
   })
-)
-
-/**
- * TODO:
- * TODO: resolve
- * TODO: args
- */
-const genExtra = (CircleGql, SquareGql) => {
-  return {
-    allShapes: {
-      type: new GraphQLList(defineShapesType(CircleGql, SquareGql)),
-      args: {},
-      resolve(root, args) {
-        const latestNodes = _.filter(
-          getNodes(),
-          n => (n.internal.type === `ContentfulCircle`) ||
-                (n.internal.type === `ContentfulSquare`)
-        )
-        return latestNodes
-      },
-    },
-  }
+  return enhancedFields
 }
 
 module.exports = async () => {
   const typesGQL = await buildNodeTypes()
-  const connections = buildNodeConnections(_.values(typesGQL))
+  let enhancedFields = {}
+  const enhancedFieldResults = await getEnhancedGqlFields(typesGQL)
+  Object.keys(enhancedFieldResults).forEach(key => {
+    enhancedFields = {
+      ...enhancedFields,
+      ...enhancedFieldResults[key],
+    }
+  })
 
+  // console.log(`got enhanced fields`, Object.keys(enhancedFields))
+  // console.log(`delme`, Object.keys(enhancedFields[0]))
+
+  const connections = buildNodeConnections(_.values(typesGQL))
 
   // Pull off just the graphql node from each type object.
   const nodes = _.mapValues(typesGQL, `node`)
@@ -62,14 +39,10 @@ module.exports = async () => {
   invariant(!_.isEmpty(nodes), `There are no available GQL nodes`)
   invariant(!_.isEmpty(connections), `There are no available GQL connections`)
 
-  const CircleType = typesGQL[`contentfulCircle`].nodeObjectType
-  const SqaureType = typesGQL[`contentfulSquare`].nodeObjectType
-  const extra = genExtra(CircleType, SqaureType)
-
   const schema = new GraphQLSchema({
     query: new GraphQLObjectType({
       name: `RootQueryType`,
-      fields: { ...connections, ...nodes, ...extra },
+      fields: { ...connections, ...nodes, ...enhancedFields },
     }),
   })
 
