3ae567a0380047637a93de294c276faed33a79d4
Brian Vaughn <brian.david.vaughn@gmail.com>
2017-11-23 14:45:16 -0800

    Added gatsby-remark-embed-snippet

---
+++ packages/gatsby-remark-embed-snippet/source/__tests__/index.js
@@ -0,0 +1,225 @@
+jest.mock(`fs`, () => {
+  return {
+    existsSync: jest.fn(),
+    readFileSync: jest.fn(),
+  }
+})
+
+const fs = require(`fs`)
+const Remark = require(`remark`)
+const plugin = require(`../index`)
+
+const remark = new Remark()
+
+describe(`gatsby-remark-embed-snippet`, () => {
+  beforeEach(() => {
+    fs.existsSync.mockReset()
+    fs.existsSync.mockReturnValue(true)
+
+    fs.readFileSync.mockReset()
+    fs.readFileSync.mockReturnValue(`const foo = "bar";`)
+  })
+
+  it('should error if missing required config options', () => {
+    const markdownAST = remark.parse('```embed:hello-world.js```')
+
+    expect(
+      () => plugin({ markdownAST })
+    ).toThrow(
+      `Required option "directory" not specified`
+    )
+  })
+
+  it('should error if the specified directory does not exist', () => {
+    fs.existsSync.mockReturnValue(false)
+
+    const markdownAST = remark.parse('```embed:hello-world.js```')
+
+    expect(
+      () => plugin({ markdownAST }, { directory: `invalid` })
+    ).toThrow(
+      `Invalid directory specified "invalid"`
+    )
+  })
+
+  it('should error if an invalid file path is specified', () => {
+    fs.existsSync.mockImplementation(path => path !== `examples/hello-world.js`)
+
+    const markdownAST = remark.parse('```embed:hello-world.js```')
+
+    expect(() =>
+      plugin({ markdownAST }, { directory: `examples` })
+    ).toThrow(`Invalid snippet specified; no such file "examples/hello-world.js"`)
+  })
+
+  it('should not modify non-embed inlineCode nodes', () => {
+    const markdownAST = remark.parse('```console.log("hi")```')
+    const transformed = plugin({ markdownAST }, { directory: `examples` })
+
+    expect(transformed).toMatchSnapshot()
+  })
+
+  it('should convert embed inlineCode nodes to Prism code blocks', () => {
+    const markdownAST = remark.parse('```embed:hello-world.js```')
+    const transformed = plugin({ markdownAST }, { directory: `examples` })
+
+    expect(transformed).toMatchSnapshot()
+  })
+
+  describe('CSS files', () => {
+    beforeEach(() => {
+      fs.readFileSync.mockReturnValue(`html { height: 100%; }`)
+    })
+
+    it('should extract the correct Prism language', () => {
+      const markdownAST = remark.parse('```embed:hello-world.css```')
+      const transformed = plugin({ markdownAST }, {
+        directory: `examples`,
+      })
+
+      expect(transformed).toMatchSnapshot()
+    })
+
+    it('should support highlight-line and highlight-next-line markers', () => {
+      fs.readFileSync.mockReturnValue(`
+        html {
+          /* highlight-next-line */
+          height: 100%;
+        }
+
+        * {
+          box-sizing: border-box; /* highlight-line */
+        }
+      `.replace(/^ +/gm, '').trim())
+
+      const markdownAST = remark.parse('```embed:hello-world.css```')
+      const transformed = plugin({ markdownAST }, {
+        directory: `examples`,
+      })
+
+      expect(transformed).toMatchSnapshot()
+    })
+  })
+
+  describe('HTML files', () => {
+    it('should extract the correct Prism language', () => {
+      fs.readFileSync.mockReturnValue(`<html></html>`)
+
+      const markdownAST = remark.parse('```embed:hello-world.html```')
+      const transformed = plugin({ markdownAST }, {
+        directory: `examples`,
+      })
+
+      expect(transformed).toMatchSnapshot()
+    })
+
+    it('should support highlight-line and highlight-next-line markers', () => {
+      fs.readFileSync.mockReturnValue(`
+        <html>
+          <body>
+            <h1>highlight me</h1> <!-- highlight-line -->
+            <p>
+              <!-- highlight-next-line -->
+              And me
+            </p>
+          </body>
+        </html>
+      `.replace(/^ +/gm, '').trim())
+
+      const markdownAST = remark.parse('```embed:hello-world.html```')
+      const transformed = plugin({ markdownAST }, {
+        directory: `examples`,
+      })
+
+      expect(transformed).toMatchSnapshot()
+    })
+  })
+
+  describe('JavaScript files', () => {
+    it('should extract the correct Prism language', () => {
+      fs.readFileSync.mockReturnValue(`const foo = "bar";`)
+
+      const markdownAST = remark.parse('```embed:hello-world.js```')
+      const transformed = plugin({ markdownAST }, {
+        directory: `examples`,
+      })
+
+      expect(transformed).toMatchSnapshot()
+    })
+
+    it('should support highlight-line and highlight-next-line markers', () => {
+      fs.readFileSync.mockReturnValue(`
+        import React from 'react';
+        import ReactDOM from 'react-dom';
+
+        // highlight-next-line
+        ReactDOM.render(
+          <h1>Hello, world!</h1>,
+          document.getElementById('root')
+        ); // highlight-line
+      `.replace(/^ +/gm, '').trim())
+
+      const markdownAST = remark.parse('```embed:hello-world.js```')
+      const transformed = plugin({ markdownAST }, {
+        directory: `examples`,
+      })
+
+      expect(transformed).toMatchSnapshot()
+    })
+  })
+
+  describe('Markdown files', () => {
+    it('should extract the correct Prism language', () => {
+      fs.readFileSync.mockReturnValue(`# Hi`)
+
+      const markdownAST = remark.parse('```embed:hello-world.md```')
+      const transformed = plugin({ markdownAST }, {
+        directory: `examples`,
+      })
+
+      expect(transformed).toMatchSnapshot()
+    })
+  })
+
+  describe('YAML files', () => {
+    beforeEach(() => {
+      fs.readFileSync.mockReturnValue(`name: Brian Vaughn`)
+    })
+
+    it('should extract the correct Prism language', () => {
+      const markdownAST = remark.parse('```embed:hello-world.yaml```')
+      const transformed = plugin({ markdownAST }, {
+        directory: `examples`,
+      })
+
+      expect(transformed).toMatchSnapshot()
+    })
+
+    it('should support highlight-line and highlight-next-line markers', () => {
+      fs.readFileSync.mockReturnValue(`
+        foo: 1 # highlight-line
+        bar: 2
+        # highlight-next-line 
+        baz: 3
+      `.replace(/^ +/gm, '').trim())
+
+      const markdownAST = remark.parse('```embed:hello-world.yaml```')
+      const transformed = plugin({ markdownAST }, {
+        directory: `examples`,
+      })
+
+      expect(transformed).toMatchSnapshot()
+    })
+  })
+
+  describe('unknown file extensions', () => {
+    it('should set the correct default Prism language', () => {
+      const markdownAST = remark.parse('```embed:hello-world```')
+      const transformed = plugin({ markdownAST }, {
+        directory: `examples`,
+      })
+
+      expect(transformed).toMatchSnapshot()
+    })
+  })
+})
---
+++ packages/gatsby-remark-embed-snippet/source/index.js
@@ -0,0 +1,120 @@
+"use strict"
+
+const fs = require(`fs`)
+const normalizePath = require(`normalize-path`)
+const visit = require(`unist-util-visit`)
+
+// TODO: This is a terrible hack!
+// Let's find a real way to share this utility code between these 2 plug-ins?
+const highlightCode = require(`../../gatsby-remark-prismjs/src/highlight-code`)
+
+// Language defaults to extension.toLowerCase();
+// This map tracks languages that don't match their extension.
+const FILE_EXTENSION_TO_LANGUAGE_MAP = {
+  js: `jsx`,
+  md: `markup`,
+}
+
+const getLanguage = file => {
+  if (!file.includes('.')) {
+    return `none`
+  }
+
+  const extension = file.split(`.`).pop()
+
+  return FILE_EXTENSION_TO_LANGUAGE_MAP.hasOwnProperty(extension)
+    ? FILE_EXTENSION_TO_LANGUAGE_MAP[extension]
+    : extension.toLowerCase()
+}
+
+module.exports = (
+  { markdownAST },
+  {
+    classPrefix = `language-`,
+    directory,
+  } = {}
+) => {
+  if (!directory) {
+    throw Error(`Required option "directory" not specified`)
+  } else if (!fs.existsSync(directory)) {
+    throw Error(`Invalid directory specified "${directory}"`)
+  } else if (!directory.endsWith(`/`)) {
+    directory += `/`
+  }
+
+  visit(markdownAST, `inlineCode`, node => {
+    const {value} = node
+
+    if (value.startsWith(`embed:`)) {
+      const file = value.substr(6)
+      const path = normalizePath(`${directory}${file}`)
+
+      if (!fs.existsSync(path)) {
+        throw Error(`Invalid snippet specified; no such file "${path}"`)
+      }
+
+      // Parse file contents and extract comments: highlight-line, highlight-next-line
+      // We support JS, HTML, CSS, and YAML style comments
+      // Turn them into an Array<number> format as expected by highlightCode().
+      // The order if these operations is important!
+      // Filtering next-line comments impacts line-numbers for same-line comments.
+      const highlightLines = []
+      const code = fs
+        .readFileSync(path, `utf8`)
+        .split('\n')
+        .filter((line, index) => {
+          if (line.includes('highlight-next-line')) {
+            // Although we're highlighting the next line,
+            // We can use the current index since we also filter this lines.
+            // (Highlight line numbers are 1-based).
+            highlightLines.push(index + 1)
+
+            // Strip lines that contain the 'next-line comments' token.
+            return false
+          }
+
+          return true
+        })
+        .map((line, index) => {
+          if (line.includes('highlight-line')) {
+            // Mark this line for highlighting.
+            // (Highlight line numbers are 1-based).
+            highlightLines.push(index + 1)
+
+            // Strip the highlight comment itself.
+            return line.replace(/\s+(\/\*|\/\/|<\!--|#)\s(highlight-line)\s*(\*\/|-->)*/, '')
+          }
+
+          return line
+        })
+        .join('\n')
+
+      // PrismJS's theme styles are targeting pre[class*="language-"]
+      // to apply its styles. We do the same here so that users
+      // can apply a PrismJS theme and get the expected, ready-to-use
+      // outcome without any additional CSS.
+      //
+      // @see https://github.com/PrismJS/prism/blob/1d5047df37aacc900f8270b1c6215028f6988eb1/themes/prism.css#L49-L54
+      const language = getLanguage(file)
+
+      // Allow users to specify a custom class prefix to avoid breaking
+      // line highlights if Prism is required by any other code.
+      // This supports custom user styling without causing Prism to
+      // re-process our already-highlighted markup.
+      // @see https://github.com/gatsbyjs/gatsby/issues/1486
+      const className = language.split(' ').map(token => `${classPrefix}${token}`).join(' ')
+
+      // Replace the node with the markup we need to make 100% width highlighted code lines work
+      node.type = `html`
+      node.value = `<div class="gatsby-highlight">
+        <pre class="${className}"><code>${highlightCode(
+        language,
+        code,
+        highlightLines
+      )}</code></pre>
+        </div>`
+    }
+  })
+
+  return markdownAST
+}
