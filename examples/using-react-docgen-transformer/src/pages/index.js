6e15197aa8f5274ea9cd73ca155238e2cf8780bc
Scotty Eckenthal <scott.eckenthal@gmail.com>
2017-12-15 12:41:39 -0500

    Adds base stuff

--- examples/using-react-docgen-transformer/src/pages/index.js
+++ examples/using-react-docgen-transformer/src/pages/index.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from "react"
+import Button from "../components/Button"
 
 class IndexComponent extends React.Component {
-  render () {
-    return <div>Hello world</div>
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <Button>Click me</Button>
+      </div>
+    )
   }
 }
 
