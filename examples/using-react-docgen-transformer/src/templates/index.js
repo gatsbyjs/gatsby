e7f996f8045a69ed64ce9c0ee6ab0d734431f259
Scotty Eckenthal <scott.eckenthal@gmail.com>
2017-12-15 17:47:47 -0500

    Adds components and index

---
+++ examples/using-react-docgen-transformer/src/templates/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+class ComponentIndex extends React.Component {
+  render() {
+    console.log(this.props)
+    return <div>component index</div>
+  }
+}
+
+export default ComponentIndex
