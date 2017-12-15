5399bbab10915b583157e4a95f686fe785ddcce0
Scotty Eckenthal <scott.eckenthal@gmail.com>
2017-12-14 19:24:31 -0500

    Adds plugin stuff

--- packages/gatsby-plugin-remove-trailing-slashes/README.md
+++ packages/gatsby-plugin-remove-trailing-slashes/README.md
@@ -1,3 +1,26 @@
 # gatsby-plugin-remove-trailing-slashes
 
-Stub README
+This plugin simply removes your project's paths. For example,
+`yoursite.com/about/` becomes `yoursite.com/about`.
+
+## Usage
+
+Install:
+
+```
+yarn add --dev gatsby-plugin-remove-trailing-slashes
+```
+
+Then configure via `gatsby-config.js`.
+
+```js
+{
+  ...
+  plugins: [
+    ...,
+    `gatsby-plugin-remove-trailing-slashes`,
+  ]
+}
+```
+
+That's it.
---
+++ packages/gatsby-plugin-remove-trailing-slashes/gatsby-node.js
@@ -0,0 +1,16 @@
+// Replacing '/' would result in empty string which is invalid
+const replacePath = _path => (_path === `/` ? _path : _path.replace(/\/$/, ``))
+
+exports.onCreatePage = ({ page, store, boundActionCreators }) => {
+  const { createPage, deletePage } = boundActionCreators
+
+  return new Promise(resolve => {
+    const oldPage = Object.assign({}, page)
+    page.path = replacePath(page.path)
+    if (page.path !== oldPage.path) {
+      deletePage(oldPage)
+      createPage(page)
+    }
+    resolve()
+  })
+}
