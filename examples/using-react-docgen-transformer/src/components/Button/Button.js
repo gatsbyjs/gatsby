b6ca427bbb476ef2d6cf599fb6e11cee85635de5
Scotty Eckenthal <scott.eckenthal@gmail.com>
2017-12-15 13:44:34 -0500

    Configurable background colors

--- examples/using-react-docgen-transformer/src/components/Button/Button.js
+++ examples/using-react-docgen-transformer/src/components/Button/Button.js
@@ -1,36 +1,54 @@
 import React from "react"
+import PropTypes from "prop-types"
 import { css } from "glamor"
 
-const blue = {
-  primary: `#A0CED9`,
-  hover: `#92BCC6`,
-}
+const blue = `blue`
+const orange = `orange`
+const green = `green`
 
-const orange = {
-  primary: `#EAB69B`,
-  hover: `#E8AF91`,
-}
-
-const green = {
-  primary: `#ADF7B6`,
-  hover: `#9EE1A6`,
-}
-
-const styles = css({
-  backgroundColor: blue.primary,
-  color: `rgba(36, 47, 60, 0.66)`,
-  display: `inline-block`,
-  fontSize: `18px`,
-  padding: `16px 24px`,
-  minWidth: `278px`,
-  borderRadius: `3px`,
-  border: 0,
-  cursor: `pointer`,
-  "&:hover": {
-    backgroundColor: blue.hover,
+const colors = {
+  [blue]: {
+    primary: `#A0CED9`,
+    hover: `#92BCC6`,
   },
-})
 
-const Button = props => <button className={styles} {...props} />
+  [orange]: {
+    primary: `#EAB69B`,
+    hover: `#E8AF91`,
+  },
+
+  [green]: {
+    primary: `#ADF7B6`,
+    hover: `#9EE1A6`,
+  },
+}
+
+const styles = ({ color } = {}) =>
+  css({
+    backgroundColor: colors[color].primary,
+    color: `rgba(36, 47, 60, 0.66)`,
+    display: `inline-block`,
+    fontSize: `18px`,
+    padding: `16px 24px`,
+    minWidth: `278px`,
+    borderRadius: `3px`,
+    border: 0,
+    cursor: `pointer`,
+    "&:hover": {
+      backgroundColor: colors[color].hover,
+    },
+  })
+
+const Button = ({ color, ...rest }) => (
+  <button className={styles({ color })} {...rest} />
+)
+
+Button.propTypes = {
+  color: PropTypes.oneOf(Object.keys(colors)),
+}
+
+Button.defaultProps = {
+  color: blue,
+}
 
 export default Button
