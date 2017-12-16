f2a8d647336ababd32ecc94523e6839ea1558c69
Scotty Eckenthal <scott.eckenthal@gmail.com>
2017-12-16 12:04:27 -0500

    Parsing / rendering HTML

---
+++ examples/using-react-docgen-transformer/src/templates/components/ComponentPreview/ComponentPreview.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live"
+
+class ComponentPreview extends React.Component {
+  render() {
+    return
+  }
+}
+
+export default ComponentPreview
