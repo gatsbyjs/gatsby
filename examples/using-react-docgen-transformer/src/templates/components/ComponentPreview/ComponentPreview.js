a1366036e8f7d803860e9bcc622a680310f7e55c
Scotty Eckenthal <scott.eckenthal@gmail.com>
2017-12-16 14:40:34 -0500

    It works

--- examples/using-react-docgen-transformer/src/templates/components/ComponentPreview/ComponentPreview.js
+++ examples/using-react-docgen-transformer/src/templates/components/ComponentPreview/ComponentPreview.js
@@ -1,10 +1,23 @@
 import React from "react"
+import PropTypes from "prop-types"
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live"
+
+import Button from "../../../components/Button"
 
 class ComponentPreview extends React.Component {
   render() {
-    return
+    return (
+      <LiveProvider scope={{ Button }} code={this.props.code}>
+        <LiveEditor />
+        <LiveError />
+        <LivePreview />
+      </LiveProvider>
+    )
   }
+}
+
+ComponentPreview.propTypes = {
+  code: PropTypes.string.isRequired,
 }
 
 export default ComponentPreview
