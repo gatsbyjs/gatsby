40035e14e71d9666040f0d7cd70405d88873b367
Scotty Eckenthal <scott.eckenthal@gmail.com>
2017-12-16 12:25:30 -0500

    Starts parsing nodes

--- examples/using-react-docgen-transformer/src/templates/components/Example/Example.js
+++ examples/using-react-docgen-transformer/src/templates/components/Example/Example.js
@@ -1,13 +1,35 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { Parser } from "html-to-react"
-import _ from "lodash"
+import { Parser, ProcessNodeDefinitions } from "html-to-react"
+
+const isValidNode = () => true
+const isCodeExample = ({ name = `` } = {}) => name === `pre`
 
 const parser = new Parser()
+const processNodeDefinitions = new ProcessNodeDefinitions(React)
+
+const processingInstructions = [
+  {
+    shouldProcessNode: isCodeExample,
+    processNode: (node, children) => {
+      console.log(node)
+      return processNodeDefinitions.processDefaultNode(node, children)
+    },
+  },
+  {
+    shouldProcessNode: isValidNode,
+    processNode: processNodeDefinitions.processDefaultNode,
+  },
+]
 
 class Example extends React.Component {
   render() {
-    const html = _.filter(parser.parse(this.props.html), _.isObject)
+    const html = parser.parseWithInstructions(
+      this.props.html,
+      isValidNode,
+      processingInstructions
+    )
+    console.log(html)
     return <div>{html}</div>
   }
 }
