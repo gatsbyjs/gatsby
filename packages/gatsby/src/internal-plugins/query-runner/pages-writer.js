18106e8b451c9f9912e290442d12dd8505e7b8e2
BuildTools <unconfigured@null.spigotmc.org>
2017-12-18 23:39:10 +0100

    Load layouts before pages

--- packages/gatsby/src/internal-plugins/query-runner/pages-writer.js
+++ packages/gatsby/src/internal-plugins/query-runner/pages-writer.js
@@ -58,6 +58,15 @@ const writePages = async () => {
   let syncRequires = `// prefer default export if available
 const preferDefault = m => m && m.default || m
 \n\n`
+syncRequires += `exports.layouts = {\n${pageLayouts
+  .map(
+    l =>
+      `  "${l.machineId}": preferDefault(require("${
+        l.componentWrapperPath
+      }"))`
+  )
+  .join(`,\n`)}
+}\n\n`
   syncRequires += `exports.components = {\n${components
     .map(
       c =>
@@ -77,16 +86,8 @@ const preferDefault = m => m && m.default || m
         )}")`
     )
     .join(`,\n`)}
-}\n\n`
-  syncRequires += `exports.layouts = {\n${pageLayouts
-    .map(
-      l =>
-        `  "${l.machineId}": preferDefault(require("${
-          l.componentWrapperPath
-        }"))`
-    )
-    .join(`,\n`)}
 }`
+
 
   // Create file with async requires of layouts/components/json files.
   let asyncRequires = `// prefer default export if available
