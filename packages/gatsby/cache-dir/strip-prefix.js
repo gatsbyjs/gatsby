536482ebbfb2f48bb12678f1350da893570b74d9
Kyle Mathews <mathews.kyle@gmail.com>
2017-12-21 16:20:32 -0800

    polyfill startsWith

--- packages/gatsby/cache-dir/strip-prefix.js
+++ packages/gatsby/cache-dir/strip-prefix.js
@@ -2,6 +2,14 @@
  * Remove a prefix from a string. Return the input string if the given prefix
  * isn't found.
  */
+
+// Polyfill startsWith
+if (!String.prototype.startsWith) {
+  String.prototype.startsWith = function(search, pos) {
+    return this.substr(!pos || pos < 0 ? 0 : +pos, search.length) === search
+  }
+}
+
 export default (str, prefix = ``) => {
   if (str.startsWith(prefix)) return str.slice(prefix.length)
   return str
