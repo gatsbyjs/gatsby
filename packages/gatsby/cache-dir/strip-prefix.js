f352bbd43c729f009ae31a0fb9dbb00282eebeaa
Kyle Mathews <mathews.kyle@gmail.com>
2017-12-21 16:24:34 -0800

    Implement startsWith

--- packages/gatsby/cache-dir/strip-prefix.js
+++ packages/gatsby/cache-dir/strip-prefix.js
@@ -3,14 +3,7 @@
  * isn't found.
  */
 
-// Polyfill startsWith
-if (!String.prototype.startsWith) {
-  String.prototype.startsWith = function(search, pos) {
-    return this.substr(!pos || pos < 0 ? 0 : +pos, search.length) === search
-  }
-}
-
 export default (str, prefix = ``) => {
-  if (str.startsWith(prefix)) return str.slice(prefix.length)
+  if (str.substr(0, prefix.length) === prefix) return str.slice(prefix.length)
   return str
 }
