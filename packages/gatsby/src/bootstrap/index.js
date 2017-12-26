b06ab23454ec5c91c275749c39c3f564f3562734
pwener <phwener@gmail.com>
2017-12-26 00:18:37 -0200

    Testing v8-compile-cache

--- packages/gatsby/src/bootstrap/index.js
+++ packages/gatsby/src/bootstrap/index.js
@@ -393,6 +393,7 @@ module.exports = async (args: BootstrapArgs) => {
     report.log(``)
     report.info(`bootstrap finished - ${process.uptime()} s`)
     report.log(``)
+    process.exit()
     return { graphqlRunner }
   } else {
     return new Promise(resolve => {
