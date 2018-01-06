dd2e4f431894dc6337f72962eb805a88395166bd
Sebastian Stüber <sebastian.stueber@rwth-aachen.de>
2018-01-06 17:36:04 +0100

    window scrollToTop on click to same site

--- packages/gatsby-link/src/index.js
+++ packages/gatsby-link/src/index.js
@@ -132,6 +132,9 @@ class GatsbyLink extends React.Component {
               if (element !== null) {
                 element.scrollIntoView()
                 return true
+              }else{
+                window.scrollTo(0,0)
+                return true
               }
             }
 
