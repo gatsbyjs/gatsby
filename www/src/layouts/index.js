8486bf022f176aa09ff0df9225db9eb1201b3d94
James Stack <james.a.stack@gmail.com>
2018-01-09 15:16:10 -0800

    Remove unneccesary css

--- www/src/layouts/index.js
+++ www/src/layouts/index.js
@@ -209,7 +209,7 @@ class DefaultLayout extends React.Component {
           </div>
 
           {/* This div displays the contents of any plugin page*/}
-
+          
           <div
             css={{
               [presets.Tablet]: {
