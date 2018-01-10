3ad469b1be0ae0a4144c57ad46c69da59348cf0c
denster <mindsdreamer@gmail.com>
2018-01-10 16:34:40 +0300

    Typescript definition update (#3320)
    Updates to pass typescript compiler(tsc) type checks, given the Gatsby's default layout in src/layouts/index.js.

--- packages/gatsby-link/index.d.ts
+++ packages/gatsby-link/index.d.ts
@@ -4,10 +4,11 @@ import { NavLinkProps } from "react-router-dom";
 export interface GatsbyLinkProps extends NavLinkProps {
   onClick?: (event: any) => void
   className?: string
+  style?:any;
 }
 
 export const navigateTo: (path: string) => void;
 
 export const withPrefix: (path: string) => string;
 
-export default class GatsbyLink extends React.Component<GatsbyLinkProps> { }
+export default class GatsbyLink extends React.Component<GatsbyLinkProps, any> { }
