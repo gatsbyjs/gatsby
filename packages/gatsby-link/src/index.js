e45de4c2fd2bb0d3fff0c48ad14cda7a95da8918
Kyle Mathews <mathews.kyle@gmail.com>
2018-01-10 04:37:49 -0800

    Update index.js

--- packages/gatsby-link/src/index.js
+++ packages/gatsby-link/src/index.js
@@ -146,6 +146,8 @@ class GatsbyLink extends React.Component {
                 element.scrollIntoView()
                 return true
               } else {
+                // This is just a normal link to the current page so let's emulate default
+                // browser behavior by scrolling now to the top of the page.
                 window.scrollTo(0, 0)
                 return true
               }
