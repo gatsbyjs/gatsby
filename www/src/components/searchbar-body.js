9d33f446d5d04f07d377f352a1bf66612b19d154
James Stack <james.a.stack@gmail.com>
2018-01-11 16:39:33 -0800

    Add algolia-npm search and url syncing

--- www/src/components/searchbar-body.js
+++ www/src/components/searchbar-body.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-import { InstantSearch, Hits, SearchBox, Stats } from 'react-instantsearch/dom'
+import { InstantSearch, Hits, SearchBox, Stats, RefinementList } from 'react-instantsearch/dom'
+import distanceInWords from 'date-fns/distance_in_words'
 import presets from "../utils/presets"
 import Link from 'gatsby-link'
 
-import typography from "../utils/typography"
+import typography, { rhythm } from "../utils/typography"
 
 const Search = () => {
   return (
@@ -16,8 +17,15 @@ const Search = () => {
         translations={{placeholder: 'Search Gatsby Library by keyword'}}
       />
       </div>
+      <div css={{
+        display: `none`
+      }}>
+        <RefinementList attributeName="keywords"
+          defaultRefinement={["gatsby-plugin"]}
+        />
+      </div>
      <div css={{
-       paddingBottom: `10px`,
+       paddingBottom: rhythm(1),
      }}>
        <Stats
          translations={{
@@ -29,7 +37,7 @@ const Search = () => {
      </div>
       <div css={{
         backgroundColor: `white`,
-        height: `calc(100vh - 250px)`,
+        height: `calc(100vh - 275px)`,
         overflowY: `scroll`,
         WebkitOverflowScrolling: `touch`,
         "::-webkit-scrollbar": {
@@ -45,53 +53,196 @@ const Search = () => {
       }}>
         <Hits hitComponent={Result} />
       </div>
-    </div>
-  )
-}
-
-const Result = ({ hit }) => {
-  return (
-    <div css={{
-      borderBottom: `1px solid grey`,
-    }}>
-      <div css={{
-        padding: `10px`,
-      }}>
-        <Link to={hit.node.fields.slug}
-          style={{
-            color: `white`,
-            border: `0`,
-            boxShadow: `0 0 0 0`,
-            backgroundColor: `#696969`,
-            padding: `3px 6px 3px 6px`,
-            fontFamily: typography.options.headerFontFamily.join(`,`),
-          }}
-          >
-            {hit.node.fields.title}
-        </Link>
-        <div css={{
-          fontFamily: typography.options.bodyFontFamily.join(`,`),
-          fontSize: `${13 / 16 * 100}%`,
-          lineHeight: `1.1`,
-          paddingTop: `7px`,
-        }}>
-          {hit.node.excerpt.split(hit.node.fields.title)[1]}
-        </div>
+      <div>
+        <h3 css={{
+          fontSize: rhythm(.75),
+          textAlign: `center`,
+        }}>Search by <Link to={`https://www.algolia.com/`} style={{color: `#5c2965`, border: `none`, boxShadow: `none`}}>Algolia</Link></h3>
       </div>
     </div>
   )
 }
 
+// class ResultClass extends Component {
+//   constructor(props){
+//     super(props)
+//     this.state = { selected: false}
+//   }
+//
+//   componentWillReceivedProps(nextProps){
+//     if (window.location.pathname === `/packages/${this.props.hit.name}`){
+//       this.setState({ selected: true})
+//     }
+//   }
+//
+//   render(){
+//     console.log(this.props);
+//     const lastUpdated = `${distanceInWords(new Date(this.props.hit.modified), new Date())} ago`;
+//     const selected = window.location.pathname === `/packages/${this.props.hit.name}`;
+//     return (
+//       <Link
+//         to={`/packages/${this.props.hit.name}`}
+//         style={{
+//         display: `block`,
+//         fontFamily: typography.options.bodyFontFamily.join(`,`),
+//         fontWeight: `400`,
+//         backgroundColor: selected ? `#743272` : `#fafafa`,
+//         padding: rhythm(.5),
+//       }}>
+//         <div
+//           css={{
+//             fontFamily: typography.options.headerFontFamily.join(`,`),
+//             fontWeight: `bold`,
+//             display: `inline-block`,
+//             color: `white`,
+//             backgroundColor: `#696969`,
+//             padding: `3px 6px 3px 6px`,
+//           }}
+//           >
+//             {this.props.hit.name}
+//         </div>
+//
+//         <span
+//           css={{
+//             paddingLeft: rhythm(1),
+//             fontSize: rhythm(.5),
+//           }}
+//           >
+//           {this.props.hit.humanDownloadsLast30Days}
+//         </span>
+//
+//         <div css={{
+//           fontSize: rhythm(.6),
+//           lineHeight: rhythm(3/4),
+//           paddingTop: rhythm(3/4),
+//         }}>
+//           {this.props.hit.description}
+//         </div>
+//
+//         <div css={{
+//           fontSize: rhythm(.5),
+//           color: `#D3D3D3`
+//         }}>
+//           {this.props.hit.keywords.join(", ")}
+//         </div>
+//
+//         <div
+//           css={{
+//             display: `flex`,
+//             borderBottom: `1 px solid grey`,
+//           }}
+//           >
+//           <img
+//             width="20"
+//             height="20"
+//             src={this.props.hit.lastPublisher.avatar} />
+//             {this.props.hit.lastPublisher.name} {lastUpdated}
+//         </div>
+//       </Link>
+//     )
+//   }
+// }
+
+const Result = ({ hit }) => {
+  const lastUpdated = `${distanceInWords(new Date(hit.modified), new Date())} ago`;
+  return (
+    <Link
+      to={`/packages/${hit.name}`}
+      style={{
+      display: `block`,
+      fontFamily: typography.options.bodyFontFamily.join(`,`),
+      fontWeight: `400`,
+      padding: rhythm(.5),
+    }}>
+      <div
+        css={{
+          fontFamily: typography.options.headerFontFamily.join(`,`),
+          fontWeight: `bold`,
+          display: `inline-block`,
+          color: `white`,
+          backgroundColor: `#696969`,
+          padding: `3px 6px 3px 6px`,
+        }}
+        >
+          {hit.name}
+      </div>
+
+      <span
+        css={{
+          paddingLeft: rhythm(1),
+          fontSize: rhythm(.5),
+        }}
+        >
+        {hit.humanDownloadsLast30Days}
+      </span>
+
+      <div css={{
+        fontSize: rhythm(.6),
+        paddingTop: rhythm(1/2),
+      }}>
+        {hit.description}
+      </div>
+
+      <div css={{
+        fontSize: rhythm(.5),
+        color: `#D3D3D3`,
+      }}>
+        {hit.keywords.join(", ")}
+      </div>
+
+      <div
+        css={{
+          display: `flex`,
+          paddingTop: rhythm(.25),
+        }}
+        >
+        <img
+          width="20"
+          height="20"
+          src={hit.lastPublisher.avatar}
+         />
+          <span css={{paddingLeft: rhythm(.25), fontSize: rhythm(.5), textTransform: `uppercase`}}>{hit.lastPublisher.name}</span>
+          <span css={{paddingLeft: rhythm(.25), fontSize: rhythm(.5)}}>{lastUpdated}</span>
+      </div>
+    </Link>
+  )
+}
+
+// This is experimental
+const updateAfter = 700
+//
+//
+//
+//
 
 class SearchBar extends Component {
+  constructor(props){
+    super(props)
+    this.state = {searchState: {query: this.urlToSearch(), page: 1} }
+  }
+
+  urlToSearch = () => {
+    return (this.props.history.location.search).slice(2);
+  }
+
+  onSearchStateChange = searchState => {
+    clearTimeout(this.debouncedSetState);
+    this.debouncedSetState = setTimeout(() => {
+      this.props.history.replace(`/packages?=${searchState.query}`)
+    }, updateAfter);
+    this.setState({ searchState })
+  }
+
   render(){
     return(
       <div>
           <div>
             <InstantSearch
-              apiKey="46abe2288ee056d552a0a0e938be4938"
-              appId="I44D3HTHDS"
-              indexName="plugins"
+              apiKey="f54e21fa3a2a0160595bb058179bfb1e"
+              appId="OFCNCOG2CU"
+              indexName="npm-search"
+              searchState={this.state.searchState}
+              onSearchStateChange={this.onSearchStateChange}
               >
               <Search />
             </InstantSearch>
@@ -100,5 +251,6 @@ class SearchBar extends Component {
     )
   }
 }
+
 
 export default SearchBar
--- www/src/layouts/index.js
+++ www/src/layouts/index.js
@@ -28,10 +28,9 @@ class DefaultLayout extends React.Component {
     const isHomepage = this.props.location.pathname == `/`
     const hasSidebar =
       this.props.location.pathname.slice(0, 6) === `/docs/` ||
-      this.props.location.pathname.slice(0, 10) === `/packages/` ||
+      this.props.location.pathname.slice(0, 9) === `/packages` ||
       this.props.location.pathname.slice(0, 10) === `/tutorial/` ||
-      this.props.location.pathname.slice(0, 9) === `/features` ||
-      this.props.location.pathname.slice(0, 8) === `/plugins`
+      this.props.location.pathname.slice(0, 9) === `/features`
 
     const sidebarStyles = {
       borderRight: `1px solid ${colors.b[0]}`,
@@ -142,14 +141,13 @@ class DefaultLayout extends React.Component {
               ...searchbarStyles,
               [presets.Tablet]: {
                 display:
-                  this.props.location.pathname.slice(0, 8) === `/plugins` ||
-                  this.props.location.pathname.slice(0, 10) === `/packages/`
+                  this.props.location.pathname.slice(0, 9) === `/packages`
                     ? `block`
                     : `none`,
               },
             }}
           >
-            <SearchBar />
+            <SearchBar history={this.props.history} />
           </div>
 
           {/* TODO Move this under docs/tutorial/index.js once Gatsby supports multiple levels
@@ -186,8 +184,7 @@ class DefaultLayout extends React.Component {
               [presets.Tablet]: {
                 paddingLeft: hasSidebar ? rhythm(10) : 0,
                 display:
-                  this.props.location.pathname.slice(0, 9) !== `/packages` &&
-                  this.props.location.pathname.slice(0, 8) !== `/plugins`
+                  this.props.location.pathname.slice(0, 9) !== `/packages`
 
                    ? `block`
                    : `none`,
@@ -201,14 +198,13 @@ class DefaultLayout extends React.Component {
           </div>
 
           {/* This div displays the contents of any plugin page*/}
-          
+
           <div
             css={{
               [presets.Tablet]: {
                 paddingLeft: hasSidebar ? rhythm(18) : 0,
                 display:
-                  this.props.location.pathname.slice(0, 9) === `/packages` ||
-                  this.props.location.pathname.slice(0, 8) === `/plugins`
+                  this.props.location.pathname.slice(0, 9) === `/packages`
                    ? `block`
                    : `none`,
               },
