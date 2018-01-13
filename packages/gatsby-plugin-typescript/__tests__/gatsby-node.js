bdc1d6c3d9702cb1ab7617b4bc512907c48ba16e
Sam Bhagwat <calcsam@gmail.com>
2018-01-12 16:19:45 -0800

    remove merge artifact

--- packages/gatsby-plugin-typescript/__tests__/gatsby-node.js
+++ packages/gatsby-plugin-typescript/__tests__/gatsby-node.js
@@ -2,7 +2,6 @@
 
 jest.mock(`../resolve`, () => module => `/resolved/path/${module}`);
 
-const babelPluginRemoveQueries = require(`babel-plugin-remove-graphql-queries`);
 const {
   resolvableExtensions,
   modifyWebpackConfig,
@@ -10,89 +9,21 @@ const {
 } = require(`../gatsby-node`);
 
 describe(`gatsby-plugin-typescript`, () => {
-  let args;
-
-  function getLoader() {
-    const call = args.actions.setWebpackConfig.mock.calls[0];
-    return call[0].module.rules[0];
-  }
-
-  beforeEach(() => {
-    const actions = {
-      setWebpackConfig: jest.fn()
-    };
-    const loaders = { js: jest.fn(() => `babel-loader`) };
-    args = { actions, loaders };
-  });
-
   it(`returns correct extensions`, () => {
     expect(resolvableExtensions()).toMatchSnapshot();
   });
 
   it(`modifies webpack config`, () => {
-    modifyWebpackConfig(args, { compilerOptions: {} });
+    const boundActionCreators = {
+      setWebpackConfig: jest.fn()
+    };
+    const loaders = { js: () => `babel-loader` };
 
-    expect(args.actions.setWebpackConfig).toHaveBeenCalledTimes(1);
-    const lastCall = args.actions.setWebpackConfig.mock.calls.pop();
+    modifyWebpackConfig({ boundActionCreators, loaders }, { compilerOptions: {} });
+
+    expect(boundActionCreators.setWebpackConfig).toHaveBeenCalledTimes(1);
+    const lastCall = boundActionCreators.setWebpackConfig.mock.calls.pop();
     expect(lastCall).toMatchSnapshot();
-  });
-
-  it(`adds the remove graphql queries plugin`, () => {
-    modifyWebpackConfig(args, { compilerOptions: {} });
-
-    expect(args.loaders.js).toHaveBeenCalledTimes(1);
-    const lastCall = args.loaders.js.mock.calls.pop();
-
-    expect(lastCall[0]).toEqual({
-      plugins: [babelPluginRemoveQueries]
-    });
-  });
-
-  it(`passes the configuration to the ts-loader plugin`, () => {
-    const options = { compilerOptions: { foo: `bar` }, transpileOnly: false };
-
-    modifyWebpackConfig(args, options);
-
-    const expectedOptions = {
-      compilerOptions: {
-        target: `esnext`,
-        experimentalDecorators: true,
-        jsx: `react`,
-        foo: `bar`,
-        module: `es6`
-      },
-      transpileOnly: false
-    };
-
-    expect(getLoader()).toEqual({
-      test: /\.tsx?$/,
-      use: [`babel-loader`, {
-        loader: `/resolved/path/ts-loader`,
-        options: expectedOptions
-      }]
-    });
-  });
-
-  it(`uses default configuration for the ts-loader plugin when no config is provided`, () => {
-    modifyWebpackConfig(args, { compilerOptions: {} });
-
-    const expectedOptions = {
-      compilerOptions: {
-        target: `esnext`,
-        experimentalDecorators: true,
-        jsx: `react`,
-        module: `es6`
-      },
-      transpileOnly: true
-    };
-
-    expect(getLoader()).toEqual({
-      test: /\.tsx?$/,
-      use: [`babel-loader`, {
-        loader: `/resolved/path/ts-loader`,
-        options: expectedOptions
-      }]
-    });
   });
 
   describe(`pre-processing`, () => {
