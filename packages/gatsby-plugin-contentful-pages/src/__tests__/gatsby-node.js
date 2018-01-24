148e96dcdf8d8c090d83a2fd732db73b4c6010b1
Felix Jung <felix@jung.fm>
2018-01-24 08:08:06 +0100

    Only ignore root gatsby-node and util

---
+++ packages/gatsby-plugin-contentful-pages/src/__tests__/gatsby-node.js
@@ -0,0 +1,7 @@
+// const { createPages } = require(`../gatsby-node`)
+
+describe(`gatsby-plugin-contentful-pages`, () => {
+  it(`should do something`, () => {
+    expect(true).toBe(false)
+  })
+})
