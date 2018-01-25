e8241615f9eb59308b2fde2ae86a247d0a8ddcf0
Michal Piechowiak <misiek.piechowiak@gmail.com>
2018-01-25 01:47:16 +0100

    don't lose field args and resolve function when infering array (fixes date format options not available in arrays of dates)

--- packages/gatsby/src/schema/infer-graphql-type.js
+++ packages/gatsby/src/schema/infer-graphql-type.js
@@ -85,7 +85,27 @@ function inferGraphQLType({
       `Could not infer graphQL type for value: ${exampleValue}`
     )
 
-    return { type: new GraphQLList(inferredType.type) }
+    const { type, args = null, resolve = null } = inferredType
+
+    const listType = { type: new GraphQLList(type), args }
+
+    if (resolve) {
+      // If inferredType has resolve function wrap it with Array.map
+      listType.resolve = (object, args, context, resolveInfo) => {
+        const fieldValue = object[fieldName]
+        if (!fieldValue) {
+          return null
+        }
+
+        // Field resolver expects first parameter to be plain object
+        // containing key with name of field we want to resolve.
+        return fieldValue.map(value =>
+          resolve({ [fieldName]: value }, args, context, resolveInfo)
+        )
+      }
+    }
+
+    return listType
   }
 
   // Check if this is a date.
