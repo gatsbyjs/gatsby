d6d3e6cec117c8cf7e232b1ff419482a26f4c6d7
Mike Allanson <michael.allanson@gmail.com>
2018-01-26 00:25:16 +0000

    Remove bootstrap step from this command so it's easier to run on Heroku (#3676)
    Heroku has a 60 second limit on app startup time, so this command
    shouldn't attempt to do any bootstrapping.

--- packages/gatsby/src/commands/data-explorer.js
+++ packages/gatsby/src/commands/data-explorer.js
@@ -3,14 +3,11 @@
 const express = require(`express`)
 const graphqlHTTP = require(`express-graphql`)
 const { store } = require(`../redux`)
-const bootstrap = require(`../bootstrap`)
 
+// Note: the store must exist already. Create it with `gatsby build`.
 module.exports = async (program: any) => {
   let { port, host } = program
   port = typeof port === `string` ? parseInt(port, 10) : port
-
-  // bootstrap to ensure schema is in the store
-  await bootstrap(program)
 
   const app = express()
   app.use(
