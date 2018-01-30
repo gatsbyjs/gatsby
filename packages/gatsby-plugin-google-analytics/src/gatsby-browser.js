d025ae117b9b323ca1f12891aa713b27b724bd1b
Kyle Mathews <mathews.kyle@gmail.com>
2018-01-29 16:52:25 -0800

    Add setTimeout before calling onRouteUpdate to ensure React has written to DOM before plugins run code (#3772)

--- packages/gatsby-plugin-google-analytics/src/gatsby-browser.js
+++ packages/gatsby-plugin-google-analytics/src/gatsby-browser.js
@@ -1,10 +1,7 @@
 exports.onRouteUpdate = function({ location }) {
   // Don't track while developing.
   if (process.env.NODE_ENV === `production` && typeof ga === `function`) {
-    // Wait for the title update (see #2478)
-    setTimeout(() => {
-      window.ga(`set`, `page`, (location || {}).pathname)
-      window.ga(`send`, `pageview`)
-    }, 0)
+    window.ga(`set`, `page`, (location || {}).pathname)
+    window.ga(`send`, `pageview`)
   }
 }
