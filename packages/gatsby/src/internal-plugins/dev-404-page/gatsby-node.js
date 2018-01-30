f65dae35537b43e393296cc61c4a15687fb9c712
Steven Surgnier <stevensurgnier@gmail.com>
2018-01-30 11:19:20 -0500

    change dev-404-page to implement createPagesStatefully

--- packages/gatsby/src/internal-plugins/dev-404-page/gatsby-node.js
+++ packages/gatsby/src/internal-plugins/dev-404-page/gatsby-node.js
@@ -1,24 +1,21 @@
 const path = require(`path`)
 const fs = require(`fs-extra`)
+const chokidar = require(`chokidar`)
 
-let createdPage = false
-
-exports.createPages = async ({ store, boundActionCreators }) => {
+exports.createPagesStatefully = async ({ store, boundActionCreators }, options, done) => {
   if (process.env.NODE_ENV !== `production`) {
     const { program } = store.getState()
     const { createPage } = boundActionCreators
-    const currentPath = path.join(__dirname, `./raw_dev-404-page.js`)
-    const newPath = path.join(program.directory, `.cache`, `dev-404-page.js`)
-
-    if (!createdPage) {
-      createdPage = true
-
-      await fs.copy(currentPath, newPath)
-
-      createPage({
-        component: newPath,
-        path: `/dev-404-page/`,
-      })
-    }
+    const source = path.join(__dirname, `./raw_dev-404-page.js`)
+    const destination = path.join(program.directory, `.cache`, `dev-404-page.js`)
+    const copy = () => fs.copy(source, destination)
+    await copy()
+    createPage({
+      component: destination,
+      path: `/dev-404-page/`,
+    })
+    chokidar.watch(source)
+      .on(`change`, () => copy())
+      .on(`ready`, () => done())
   }
 }
