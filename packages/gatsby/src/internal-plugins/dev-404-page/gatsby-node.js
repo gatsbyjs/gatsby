6bbc0d17a84a532c4d58ba5cba852be232114b51
Steven Surgnier <stevensurgnier@gmail.com>
2018-02-01 15:37:29 -0500

    More hot reloading fixes (#3769)
    * use createPage singleton in dev-404-page
    
    closes #3551
    
    * write to a temp file and rename
    
    decrease the chance of reading from an incomplete file
    
    cc #3094 #3201
    
    * change dev-404-page to implement createPagesStatefully
    
    * update the jest snapshot

--- packages/gatsby/src/internal-plugins/dev-404-page/gatsby-node.js
+++ packages/gatsby/src/internal-plugins/dev-404-page/gatsby-node.js
@@ -1,18 +1,21 @@
 const path = require(`path`)
 const fs = require(`fs-extra`)
+const chokidar = require(`chokidar`)
 
-exports.createPages = async ({ store, boundActionCreators }) => {
+exports.createPagesStatefully = async ({ store, boundActionCreators }, options, done) => {
   if (process.env.NODE_ENV !== `production`) {
     const { program } = store.getState()
     const { createPage } = boundActionCreators
-    const currentPath = path.join(__dirname, `./raw_dev-404-page.js`)
-    const newPath = path.join(program.directory, `.cache`, `dev-404-page.js`)
-
-    await fs.copy(currentPath, newPath)
-
+    const source = path.join(__dirname, `./raw_dev-404-page.js`)
+    const destination = path.join(program.directory, `.cache`, `dev-404-page.js`)
+    const copy = () => fs.copy(source, destination)
+    await copy()
     createPage({
-      component: newPath,
+      component: destination,
       path: `/dev-404-page/`,
     })
+    chokidar.watch(source)
+      .on(`change`, () => copy())
+      .on(`ready`, () => done())
   }
 }
