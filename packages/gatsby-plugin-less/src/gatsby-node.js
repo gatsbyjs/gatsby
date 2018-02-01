67fc545acfba35d9ed3201ce5a70de4080ea2273
Ole Martin Ruud <barskern@outlook.com>
2018-02-01 22:45:35 +0100

    [gatsby-plugin-less] Extend less-plugin with support for `modifyVars` (#3801)
    * [gatsby-plugin-less] Extend less-plugin with support for `modifyVars`
    
    The less plugin did its job, but when I was going to use it with a
    library I experienced that it was lacking the ability to modify
    less-variables. This made it hard to customize less libraries.
    
    By letting the user provide a `options` in `gatsby-config.js`, the
    plugin can overwrite variables defined in the less stylesheet and hence
    makes it a breeze to customize libraries.
    
    The user has two options to include vars: either a object defined
    directly in the `gatsby-config.js` or define a file which exports an
    object that will be used as the options.
    
    * Update README.md

--- packages/gatsby-plugin-less/src/gatsby-node.js
+++ packages/gatsby-plugin-less/src/gatsby-node.js
@@ -1,21 +1,50 @@
-import ExtractTextPlugin from "extract-text-webpack-plugin"
-import { cssModulesConfig } from "gatsby-1-config-css-modules"
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
+import { cssModulesConfig } from 'gatsby-1-config-css-modules'
+import path from 'path'
 
-exports.modifyWebpackConfig = ({ config, stage }) => {
+exports.modifyWebpackConfig = ({ config, stage }, { theme }) => {
   const lessFiles = /\.less$/
   const lessModulesFiles = /\.module\.less$/
+
+  let themeJson = ``
+
+  if (typeof theme === `string` && theme !== ``) {
+    try {
+      const themeFile = require(path.resolve(theme))
+      themeJson = JSON.stringify(themeFile)
+    } catch (err) {
+      throw new Error(`Couldn't convert js to json object at path: '${theme}'\n${err}`)
+    }
+  } else if (typeof theme === `object`) {
+    try {
+      themeJson = JSON.stringify(theme)
+    } catch (err) {
+      throw new Error(`Couldn't convert javascript object to json object.\n${err}`)
+    }
+  }
+
+  let lessLoaderDev = ``
+  let lessLoaderProd = ``
+
+  if (themeJson) {
+    lessLoaderDev = `less?{"sourceMap":true,"modifyVars":${themeJson}}`
+    lessLoaderProd = `less?{"modifyVars":${themeJson}}`
+  } else {
+    lessLoaderDev = `less?{"sourceMap":true}`
+    lessLoaderProd = `less`
+  }
 
   switch (stage) {
     case `develop`: {
       config.loader(`less`, {
         test: lessFiles,
         exclude: lessModulesFiles,
-        loaders: [`style`, `css`, `less`],
+        loaders: [`style`, `css`, lessLoaderDev],
       })
 
       config.loader(`lessModules`, {
         test: lessModulesFiles,
-        loaders: [`style`, cssModulesConfig(stage), `less`],
+        loaders: [`style`, cssModulesConfig(stage), lessLoaderDev],
       })
       return config
     }
@@ -23,14 +52,14 @@ exports.modifyWebpackConfig = ({ config, stage }) => {
       config.loader(`less`, {
         test: lessFiles,
         exclude: lessModulesFiles,
-        loader: ExtractTextPlugin.extract([`css?minimize`, `less`]),
+        loader: ExtractTextPlugin.extract([`css?minimize`, lessLoaderProd]),
       })
 
       config.loader(`lessModules`, {
         test: lessModulesFiles,
         loader: ExtractTextPlugin.extract(`style`, [
           cssModulesConfig(stage),
-          `less`,
+          lessLoaderProd,
         ]),
       })
       return config
@@ -48,7 +77,7 @@ exports.modifyWebpackConfig = ({ config, stage }) => {
         test: lessModulesFiles,
         loader: ExtractTextPlugin.extract(`style`, [
           cssModulesConfig(stage),
-          `less`,
+          lessLoaderProd,
         ]),
       })
       return config
