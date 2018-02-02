00156e85b6f9966989686cf96fea6ca1d9de6579
David Beitey <david@davidjb.com>
2018-02-02 06:43:37 +0000

    Use HTTPS over git:// when cloning starters
    `git://` (which is what `hostInfo.git(...)` gets you) uses unencrypted transport isn't recommended as a way to clone GitHub repositories (https://help.github.com/articles/which-remote-url-should-i-use/).  This change uses HTTPS instead.

--- packages/gatsby-cli/src/init-starter.js
+++ packages/gatsby-cli/src/init-starter.js
@@ -82,7 +82,7 @@ const clone = async (hostInfo: any, rootPath: string) => {
     url = hostInfo.ssh({ noCommittish: true })
     // Otherwise default to normal git syntax.
   } else {
-    url = hostInfo.git({ noCommittish: true })
+    url = hostInfo.https({ noCommittish: true })
   }
 
   const branch = hostInfo.committish ? `-b ${hostInfo.committish}` : ``
