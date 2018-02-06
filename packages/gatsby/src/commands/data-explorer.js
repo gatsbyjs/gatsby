014bda531c26336eb19100c6a931ca48841945ed
Mike Allanson <michael.allanson@gmail.com>
2018-02-06 03:37:04 +0000

    Experimenting (#3843)

--- packages/gatsby/src/commands/data-explorer.js
+++ packages/gatsby/src/commands/data-explorer.js
@@ -3,17 +3,28 @@
 const express = require(`express`)
 const graphqlHTTP = require(`express-graphql`)
 const { store } = require(`../redux`)
+const bootstrap = require(`../bootstrap`)
+const { GraphQLSchema } = require(`graphql`)
 
-// Note: the store must exist already. Create it with `gatsby build`.
 module.exports = async (program: any) => {
   let { port, host } = program
   port = typeof port === `string` ? parseInt(port, 10) : port
+
+  // bootstrap to ensure schema is in the store
+  await bootstrap(program)
+
+  const schema = store.getState().schema
+
+  console.log(
+    `Schema is instance of GraphQLSchema?`,
+    schema instanceof GraphQLSchema
+  )
 
   const app = express()
   app.use(
     `/`,
     graphqlHTTP({
-      schema: store.getState().schema,
+      schema,
       graphiql: true,
     })
   )
