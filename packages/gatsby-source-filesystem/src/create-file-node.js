e6aa7eb665096f2f817d09c48ecfecfa5357c801
Michal Piechowiak <misiek.piechowiak@gmail.com>
2018-02-09 12:40:55 +0100

    add nodeDescription field to node internal object
    Signed-off-by: Michal Piechowiak <misiek.piechowiak@gmail.com>

--- packages/gatsby-source-filesystem/src/create-file-node.js
+++ packages/gatsby-source-filesystem/src/create-file-node.js
@@ -39,6 +39,7 @@ exports.createFileNode = async (pathToFile, pluginOptions = {}) => {
     internal = {
       contentDigest,
       type: `Directory`,
+      nodeDescription: `Directory "${path.relative(process.cwd(), slashed)}"`,
     }
   } else {
     const contentDigest = await md5File(slashedFile.absolutePath)
@@ -46,6 +47,7 @@ exports.createFileNode = async (pathToFile, pluginOptions = {}) => {
       contentDigest,
       mediaType: mime.lookup(slashedFile.ext),
       type: `File`,
+      nodeDescription: `File "${path.relative(process.cwd(), slashed)}"`,
     }
   }
 
