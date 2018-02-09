8c94717c33384db0ab537db56f056a72889caaf8
Jacob Bolda <jbolda@gmail.com>
2018-02-09 07:05:42 -0600

    clear out unused deps

--- examples/using-javascript-transforms/gatsby-config.js
+++ examples/using-javascript-transforms/gatsby-config.js
@@ -25,29 +25,14 @@ module.exports = {
       },
     },
     `gatsby-transformer-javascript-frontmatter`,
-    `gatsby-transformer-yaml`,
     {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 690,
-            },
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
-            options: {},
-          },
           `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
         ],
       },
     },
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-postcss-sass`,
-    `gatsby-plugin-offline`,
+    `gatsby-plugin-sass`,
   ],
 }
