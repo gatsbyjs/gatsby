21040f05403eee865aafc51a5864c9fbea91fd02
Dustin Schau <dustinschau@gmail.com>
2018-02-12 20:58:29 -0800

    chore: update comment

--- packages/gatsby/src/schema/create-key.js
+++ packages/gatsby/src/schema/create-key.js
@@ -16,7 +16,7 @@ module.exports = (key: string): string => {
 
   const replaced = key.replace(nonAlphaNumericExpr, `_`)
 
-  // key is invalid; normalize with a leading underscore and dasherize rest
+  // key is invalid; normalize with leading underscore and rest with x
   if (replaced.match(/^__/)) {
     return replaced.replace(/_/g, (char, index) => (index === 0 ? `_` : `x`))
   }
