a3f4fb5f08b1abbae98e3baba0ca4e00c865432b
Sriram Thiagarajan <sri.sjc@gmail.com>
2018-02-17 16:53:23 +0530

    fix noscript img style

--- packages/gatsby-image/src/index.js
+++ packages/gatsby-image/src/index.js
@@ -99,7 +99,7 @@ const noscriptImg = props => {
     height = ``,
     transitionDelay = ``,
   } = props
-  return `<img width=${width} height=${height} src="${src}" srcset="${srcSet}" alt="${alt}" title="${title}" sizes="${sizes}" style="position:absolute;top:0;left:0;transition:opacity 0.5s;transition-delay:${transitionDelay};opacity:${opacity};width:100%;height:100%;object-fit:cover;objectPosition:center"/>`
+  return `<img width=${width} height=${height} src="${src}" srcset="${srcSet}" alt="${alt}" title="${title}" sizes="${sizes}" style="position:absolute;top:0;left:0;transition:opacity 0.5s;transition-delay:${transitionDelay};opacity:${opacity};width:100%;height:100%;object-fit:cover;object-position:center"/>`
 }
 
 const Img = props => {
