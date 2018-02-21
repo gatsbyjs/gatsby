2a43aff18ca9f9e2574bd50c81addad1c30b0707
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2018-02-21 14:09:57 +1100

    Fix crash by rounding auto-calculated height in resize

--- packages/gatsby-plugin-sharp/src/index.js
+++ packages/gatsby-plugin-sharp/src/index.js
@@ -299,7 +299,7 @@ function queueImageResizing({ file, args = {} }) {
     // Use the aspect ratio of the image to calculate what will be the resulting
     // height.
     width = options.width
-    height = options.width / aspectRatio
+    height = Math.round(options.width / aspectRatio)
   }
 
   // Create job and process.
