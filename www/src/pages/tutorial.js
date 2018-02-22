d509796db5c87495ee86cec1cda01bd13d8bb89c
shannonbux <32467162+shannonbux@users.noreply.github.com>
2018-02-21 17:19:58 -0700

    Edits

--- www/src/pages/tutorial.js
+++ www/src/pages/tutorial.js
@@ -11,9 +11,7 @@ export default () => (
     </Helmet>
     <h1 css={{ marginTop: 0 }}>Getting Started</h1>
     <p>
-      Hi! 
-      
-      Depending how new you are to Gatsby, you can try one of the following:
+      Welcome. There are several options you may want to choose when building with Gatsby:
       * [tutorial](/docs/tutorial/index.md) if you want step-by-step instructions
       * [Quick start](/pages/docs/index.js) to install Gatsby and/or dive into the docs
 
