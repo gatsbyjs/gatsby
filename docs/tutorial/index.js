c6769ed5abb68043fff4c058966d3e4f549984ec
shannonbux <32467162+shannonbux@users.noreply.github.com>
2018-02-21 17:16:29 -0700

    go here active construction

--- docs/tutorial/index.js
+++ docs/tutorial/index.js
@@ -6,7 +6,7 @@ We’re so happy you decided to try using Gatsby. This tutorial has five parts t
      
 This tutorial is for *everyone*! You do not need to be a programmer or React.js expert. We'll walk you through things.
 
-0. [Programming basics (Part Zero)](/tutorial/part-zero/): If you are new to programming, here's an introduction to some basics like terminal,  code editors, and browser consoles.
+0. [Programming basics (Part Zero)](/tutorial/part-zero/): If you are new to programming, go here for an introduction to some basics like terminal,  code editors, and browser consoles.
 1. [Introduction to basics of Gatsby (Part One)](/tutorial/part-one/) - Starting new projects, developing, and deploying sites.
 2. [Introduction to using CSS in Gatsby (Part Two)](/tutorial/part-two/): Explore libraries like Typography.js and CSS Modules.
 3. [Explore building nested layouts in Gatsby (Part Three)](/tutorial/part-three/). Layouts are sections of your site that are reused across multiple pages like headers and footers.
