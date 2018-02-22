e0a3681c0a6f0a0f8da1543aefe4124de17f2246
shannonbux <32467162+shannonbux@users.noreply.github.com>
2018-02-21 17:09:28 -0700

    Changing this to .md
    Man, hope I got the links right. I have read the Github docs on this dozens of times and still get confused. Will need to update this doc again as we add more parts to the tutorials.

--- docs/tutorial/index.js
+++ docs/tutorial/index.js
@@ -1,56 +1,13 @@
-import React from "react"
-import Helmet from "react-helmet"
-import Link from "gatsby-link"
+---
+title: Gatsby.js Tutorial
+---
 
-import Container from "../components/container"
+We’re so happy you decided to try using Gatsby. This tutorial has five parts that'll walk you from getting started developing and building Gatsby sites to deploying a finished and polished high performance static PWA.
+     
+This tutorial is for *everyone*! You do not need to be a programmer or React.js expert. We'll walk you through things.
 
-export default () => (
-  <Container>
-    <Helmet>
-      <title>Tutorial</title>
-    </Helmet>
-    <h1 css={{ marginTop: 0 }}>Gatsby.js Tutorial</h1>
-   <p>
-      We’re so happy you decided to try using Gatsby. This tutorial has seven
-      parts that'll walk you from getting started developing and building Gatsby
-      sites to deploying a finished and polished high performance static PWA.
-    </p>
-    <p>
-      This tutorial is for <em>everyone</em>! You do not need to be a programmer
-      or React.js expert. We'll walk you through things.
-    </p>
-
-    <ol>
-      <li>
-        <Link to="/tutorial/part-zero/">Programming basics (Part Zero)</Link>. 
-        If you are new to programming, here's an introduction to some basics like terminal, 
-        code editors, and browser consoles.
-      </li>
-      <li>
-        <Link to="/tutorial/part-one/">Introduction to basics of Gatsby</Link>
-        {` `}— Starting new projects, developing, and deploying sites.
-      </li>
-      <li>
-        <Link to="/tutorial/part-two/">
-          Introduction to using CSS in Gatsby
-        </Link>. Explore libraries like Typography.js and CSS Modules.
-      </li>
-      <li>
-        <Link to="/tutorial/part-three/">
-          Explore building nested layouts in Gatsby
-        </Link>. Layouts are sections of your site that are reused across
-        multiple pages like headers and footers.
-      </li>
-      <li>
-        <Link to="/tutorial/part-four/">
-          Learn how to work with Gatsby's data layer.
-        </Link>
-        {` `}
-        Explore source & transformer plugins. Get introduced to programmatic
-        pages and how to write GraphQL queries. In this part of the tutorial
-        we'll build a bare-bones markdown blog.
-      </li>
-    </ol>
-  </Container>
-)
- 
+0. [Programming basics (Part Zero)](/tutorial/part-zero/): If you are new to programming, here's an introduction to some basics like terminal,  code editors, and browser consoles.
+1. [Introduction to basics of Gatsby (Part One)](/tutorial/part-one/) - Starting new projects, developing, and deploying sites.
+2. [Introduction to using CSS in Gatsby (Part Two)](/tutorial/part-two/): Explore libraries like Typography.js and CSS Modules.
+3. [Explore building nested layouts in Gatsby (Part Three)](/tutorial/part-three/). Layouts are sections of your site that are reused across multiple pages like headers and footers.
+4. [Learn how to work with Gatsby]((/tutorial/part-four/))'s data layer in Part Four. Explore source & transformer plugins. Get introduced to programmatic pages and how to write GraphQL queries. In this part of the tutorial we'll build a bare-bones markdown blog.
