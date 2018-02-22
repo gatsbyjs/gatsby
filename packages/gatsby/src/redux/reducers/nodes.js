bfbd056498c969d9e14d0402ac21e1d03d2d1cea
Kyle Mathews <mathews.kyle@gmail.com>
2018-02-22 15:31:27 -0800

    Delete transformed descendant nodes when the parent node is changed to avoid leaving stale leaves fixes #4176 (#4195)
    * Delete transformed descendant nodes when the parent node is changed to avoid leaving stale leaves fixes #4176
    
    * Fix lint error

--- packages/gatsby/src/redux/reducers/nodes.js
+++ packages/gatsby/src/redux/reducers/nodes.js
@@ -1,13 +1,47 @@
 const _ = require(`lodash`)
 
+const parentChildrenMap = new Map()
+
 module.exports = (state = {}, action) => {
   let newState
   switch (action.type) {
     case `DELETE_CACHE`:
       return {}
-    case `CREATE_NODE`:
+    case `CREATE_NODE`: {
+      // Remove any previously created descendant nodes as they're all due to be
+      // recreated.
+      const findChildrenRecursively = (children = []) => {
+        children = children.concat(
+          ...children.map(child => {
+            const newChildren = parentChildrenMap.get(child)
+            parentChildrenMap.delete(child)
+            if (newChildren) {
+              return findChildrenRecursively(newChildren)
+            } else {
+              return []
+            }
+          })
+        )
+
+        return children
+      }
+
+      const descendantNodes = findChildrenRecursively(
+        parentChildrenMap.get(action.payload.id)
+      )
+      parentChildrenMap.delete(action.payload.id)
+      newState = _.omit(state, descendantNodes)
+
+      newState = {
+        ...newState,
+        [action.payload.id]: action.payload,
+      }
+      return newState
+    }
+
     case `ADD_FIELD_TO_NODE`:
     case `ADD_CHILD_NODE_TO_PARENT_NODE`:
+      parentChildrenMap.set(action.payload.id, action.payload.children)
       newState = {
         ...state,
         [action.payload.id]: action.payload,
