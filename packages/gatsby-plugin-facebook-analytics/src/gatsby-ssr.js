0deeeff2c9c043114bc0e71c75c0e41a47cc7ad2
Yeison Daza <jahir045@gmail.com>
2018-02-24 09:40:12 -0500

    '#add: facebook-analitycs'

---
+++ packages/gatsby-plugin-facebook-analytics/src/gatsby-ssr.js
@@ -0,0 +1,36 @@
+import React from "react"
+import { stripIndent } from "common-tags"
+
+exports.onRenderBody = ({ setPostBodyComponents }, pluginOptions) => {
+  if (
+    process.env.NODE_ENV === `production` ||
+    pluginOptions.includeInDevelopment
+  ) {
+    setPostBodyComponents([
+      <script
+        key="plugin-facebook-analitycs"
+        dangerouslySetInnerHTML={{
+          __html: stripIndent`
+          window.fbAsyncInit = function() {
+            FB.init({
+              appId      : ${pluginOptions.appId},
+              xfbml      : true,
+              version    : 'v2.12'
+            });
+
+            FB.AppEvents.logPageView();
+
+          };
+
+          (function(d, s, id){
+             var js, fjs = d.getElementsByTagName(s)[0];
+             if (d.getElementById(id)) {return;}
+             js = d.createElement(s); js.id = id;
+             js.src = "https://connect.facebook.net/en_US/sdk.js";
+             fjs.parentNode.insertBefore(js, fjs);
+           }(document, 'script', 'facebook-jssdk'));`,
+        }}
+      />,
+    ])
+  }
+}
