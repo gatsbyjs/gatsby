2ef8c4c9397cc8e8855af99b6c8e5bd20c066626
Kyle Mathews <mathews.kyle@gmail.com>
2018-03-02 14:37:38 -0800

    Update index.js

--- www/src/pages/docs/index.js
+++ www/src/pages/docs/index.js
@@ -17,7 +17,8 @@ class IndexRoute extends React.Component {
         <h1 id="get-started" css={{ marginTop: 0 }}>
           Get started
         </h1>
-        <p>Gatsby is a blazing-fast static site generator for React. For step-by-step instructions, see the <Link to="/tutorial/">tutorial</Link>.</p>
+        <p>Gatsby is a blazing-fast static site generator for React.</p>
+        <p>For more detailed step-by-step instructions on getting started, see the <Link to="/tutorial/">tutorial</Link>.</p>
         <h2>Install Gatsby{`'`}s command line tool</h2>
         <p>
           <code>npm install --global gatsby-cli</code>
