3aff5f0e7a063df2f79c6c536fc27888f0378739
Fabian Schultz <desk@fabianschultz.com>
2018-03-14 01:10:56 +0100

    Use \n instead of new log.

--- packages/gatsby/src/utils/get-ssl-cert.js
+++ packages/gatsby/src/utils/get-ssl-cert.js
@@ -2,16 +2,14 @@ const getDevelopmentCertificate = require(`devcert-san`).default
 const report = require(`gatsby-cli/lib/reporter`)
 
 module.exports = async name => {
-  report.info(`setting up SSL certificate (may require sudo)`)
-  report.log(``)
+  report.info(`setting up SSL certificate (may require sudo)\n`)
 
   try {
     return await getDevelopmentCertificate(name, {
       installCertutil: true,
     })
   } catch (err) {
-    report.log(``)
-    report.panic(`Failed to generate dev SSL certificate`, err)
+    report.panic(`\nFailed to generate dev SSL certificate`, err)
   }
 
   return false
