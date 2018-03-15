ea343a2e9de2b3e19f8d38164ed3c75fd93667dc
Chan Long Sang <longsangstan@gmail.com>
2018-03-16 02:39:35 +0800

    Update gatsby-browser.js

--- packages/gatsby-plugin-google-analytics/src/gatsby-browser.js
+++ packages/gatsby-plugin-google-analytics/src/gatsby-browser.js
@@ -2,10 +2,10 @@ exports.onRouteUpdate = function({ location }) {
   // Don't track while developing.
   if (process.env.NODE_ENV === `production` && typeof ga === `function`) {
     window.ga(
-        "set",
-        "page",
+        `set`,
+        `page`,
         location ? location.pathname + location.search : undefined
-    );
+    )
     window.ga(`send`, `pageview`)
   }
 }
