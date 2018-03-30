933f2a36a731f4514faae421b2c73ee8bf859bd4
Giacomo Zinetti <giacomo.zinetti@giko.it>
2018-03-30 12:28:30 +0200

    add config avoid_meta for gatsby-plugin-manifest

--- packages/gatsby-plugin-manifest/src/gatsby-node.js
+++ packages/gatsby-plugin-manifest/src/gatsby-node.js
@@ -5,6 +5,7 @@ exports.onPostBuild = (args, pluginOptions) =>
   new Promise(resolve => {
     const manifest = { ...pluginOptions }
     delete manifest.plugins
+    delete manifest.avoid_meta
     fs.writeFileSync(`./public/manifest.json`, JSON.stringify(manifest))
     resolve()
   })
