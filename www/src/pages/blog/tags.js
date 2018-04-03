e3d02266cdb46bcd08aa4e00d219e044d6b411d6
Sam Bhagwat <calcsam@gmail.com>
2018-04-03 14:07:23 -0700

    fix link from tags page

--- www/src/pages/blog/tags.js
+++ www/src/pages/blog/tags.js
@@ -20,7 +20,7 @@ const TagsPage = ({
         <ul>
           {group.map(tag => (
             <li key={tag.fieldValue}>
-              <Link to={`/tags/${kebabCase(tag.fieldValue)}/`}>
+              <Link to={`/blog/tags/${kebabCase(tag.fieldValue)}/`}>
                 {tag.fieldValue} ({tag.totalCount})
               </Link>
             </li>
