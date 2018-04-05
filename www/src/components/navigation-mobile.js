677eb92e0bc21e30847d0681b935dbfc93977c18
Robin Malhotra <5009041+codeOfRobin@users.noreply.github.com>
2018-04-05 23:42:37 +0530

    iPhone X support (#4835)
    Adds support for the iPhone X safe area insets.
    
    Link: https://webkit.org/blog/7929/designing-websites-for-iphone-x/
    
    Here’s a preview of what it looks like: https://twitter.com/codeOfRobin/status/981473964223430658
    
    Signed-off-by: Robin Malhotra <me@rmalhotra.com>

--- www/src/components/navigation-mobile.js
+++ www/src/components/navigation-mobile.js
@@ -45,6 +45,7 @@ const MobileNavigation = () => (
       borderTop: `1px solid ${colors.ui.light}`,
       background: colors.ui.whisper,
       fontFamily: typography.options.headerFontFamily.join(`,`),
+      paddingBottom: `env(safe-area-inset-bottom)`,
       [presets.Tablet]: {
         display: `none`,
       },
