8db6089baf2b737ec43b99b0b7438e315b6e750a
slorber <lorber.sebastien@gmail.com>
2018-04-07 20:25:02 +0200

    Add GA to RNW example

--- examples/using-react-native-web/gatsby-config.js
+++ examples/using-react-native-web/gatsby-config.js
@@ -5,5 +5,11 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-next`,
     `gatsby-plugin-react-native-web`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-93349937-2`,
+      },
+    },
   ],
 };
