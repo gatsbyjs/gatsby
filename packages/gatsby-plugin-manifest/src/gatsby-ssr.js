7bfd7421870f9d76057ff1ad5ad702f30da1633f
Aaron Lampros <alampros@gmail.com>
2018-04-07 15:28:21 -0400

    Move default icons to common file (#4867)
    Fixes #4865

--- packages/gatsby-plugin-manifest/src/gatsby-ssr.js
+++ packages/gatsby-plugin-manifest/src/gatsby-ssr.js
@@ -1,9 +1,11 @@
 import React from "react"
 import { withPrefix } from "gatsby-link"
+import { defaultIcons } from "./common.js"
+
 
 exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
 
-  const { icons } = pluginOptions
+  const icons = pluginOptions.icons || defaultIcons
   const iconPath = icons[0].src.substring(0, icons[0].src.lastIndexOf(`/`))
 
   setHeadComponents([
