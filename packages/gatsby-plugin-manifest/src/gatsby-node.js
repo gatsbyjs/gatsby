8ea2be6402bfe0378eda87e929b534af39e4505b
dojineko <dojineko.spiral@gmail.com>
2018-04-09 10:16:11 +0900

    Use path.join

--- packages/gatsby-plugin-manifest/src/gatsby-node.js
+++ packages/gatsby-plugin-manifest/src/gatsby-node.js
@@ -1,4 +1,5 @@
 const fs = require(`fs`)
+const path = require(`path`)
 const Promise = require(`bluebird`)
 const sharp = require(`sharp`)
 const defaultIcons = require(`./common.js`).defaultIcons
@@ -8,7 +9,7 @@ sharp.simd(true)
 function generateIcons(icons, srcIcon) {
   return Promise.map(icons, icon => {
     const size = parseInt(icon.sizes.substring(0, icon.sizes.lastIndexOf(`x`)))
-    const imgPath = `./public/` + icon.src
+    const imgPath = path.join(`public`, icon.src)
 
     return sharp(srcIcon)
       .resize(size)
@@ -33,14 +34,14 @@ exports.onPostBuild = (args, pluginOptions) =>
     }
 
     // Determine destination path for icons.
-    const iconPath = `./public/` + manifest.icons[0].src.substring(0, manifest.icons[0].src.lastIndexOf(`/`))
+    const iconPath = path.join(`public`, manifest.icons[0].src.substring(0, manifest.icons[0].src.lastIndexOf(`/`)))
 
     //create destination directory if it doesn't exist
     if (!fs.existsSync(iconPath)){
       fs.mkdirSync(iconPath)
     }
 
-    fs.writeFileSync(`./public/manifest.json`, JSON.stringify(manifest))
+    fs.writeFileSync(path.join(`public`, `manifest.json`), JSON.stringify(manifest))
 
     // Only auto-generate icons if a src icon is defined.
     if (icon !== undefined) {
