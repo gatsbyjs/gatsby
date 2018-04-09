193d2a90a3e1c682463debc2348c259d0882d4a2
dojineko <dojineko.spiral@gmail.com>
2018-04-10 04:24:06 +0900

    Relocate manifest.json to ./public/ (#4890)
    * Relocate manifest.json to ./public/
    
    * Remove unused variables
    
    * Use path.join
    
    Signed-off-by: benjamin hoffman <6520022+benjaminhoffman@users.noreply.github.com>

--- packages/gatsby-plugin-manifest/src/gatsby-ssr.js
+++ packages/gatsby-plugin-manifest/src/gatsby-ssr.js
@@ -1,18 +1,12 @@
 import React from "react"
 import { withPrefix } from "gatsby-link"
-import { defaultIcons } from "./common.js"
-
 
 exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
-
-  const icons = pluginOptions.icons || defaultIcons
-  const iconPath = icons[0].src.substring(0, icons[0].src.lastIndexOf(`/`))
-
   setHeadComponents([
     <link
       key={`gatsby-plugin-manifest-link`}
       rel="manifest"
-      href={withPrefix(`${iconPath}/manifest.json`)}
+      href={withPrefix(`manifest.json`)}
     />,
     <meta
       key={`gatsby-plugin-manifest-meta`}
