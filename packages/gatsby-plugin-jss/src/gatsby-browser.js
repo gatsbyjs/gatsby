fed5aaf7c99ff62a7f4239b4bfa2219553881621
3axap4eHko <3axap4eHko@gmail.com>
2018-04-12 17:02:05 -0400

    adds jss theme provider

--- packages/gatsby-plugin-jss/src/gatsby-browser.js
+++ packages/gatsby-plugin-jss/src/gatsby-browser.js
@@ -1,6 +1,16 @@
+import { ThemeProvider } from "react-jss";
+
 // remove the JSS style tag generated on the server to avoid conflicts with the one added on the client
 exports.onInitialClientRender = () => {
   // eslint-disable-next-line no-undef
   const ssStyles = window.document.getElementById(`server-side-jss`)
   ssStyles && ssStyles.parentNode.removeChild(ssStyles)
+}
+
+exports.wrapRootComponent = ({ Root }, { theme = {} }) => () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Root />
+    </ThemeProvider>
+  )
 }
