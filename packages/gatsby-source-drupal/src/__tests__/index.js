6d8e5bef22218cfeaeeb5e3f5a59375d28cb6f6a
Michal Piechowiak <misiek.piechowiak@gmail.com>
2018-04-28 00:09:33 +0200

    [v2] drupal test fix + drupal/wordpress dependency change (#5170)
    * update drupal test
    
    * drupal/wordpress change filesystem version to next to avoid problematic resolutions with alpha versions

--- packages/gatsby-source-drupal/src/__tests__/index.js
+++ packages/gatsby-source-drupal/src/__tests__/index.js
@@ -2,9 +2,8 @@ const { nodeFromData } = require(`../normalize`)
 const { idOverlayExample } = require(`./data.json`)
 
 describe(`node completion`, () => {
-  it(`should preserve the node ID from jsonapi`, () => {
-    const node = nodeFromData(idOverlayExample)
-    expect(node.id).toEqual(idOverlayExample.id)
+  it(`should correctly generate node from jsonapi`, () => {
+    const node = nodeFromData(idOverlayExample, id => id)
     expect(node.bundle).toEqual(idOverlayExample.attributes.bundle)
     expect(node._attributes_id).toEqual(idOverlayExample.attributes.id)
   })
