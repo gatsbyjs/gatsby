67e3826c98ffd39e8de3fc0e01f01ec0b378b03a
Michal Piechowiak <misiek.piechowiak@gmail.com>
2018-04-27 13:42:39 +0200

    cleanup debug console logs

--- packages/gatsby-source-contentful/src/fetch.js
+++ packages/gatsby-source-contentful/src/fetch.js
@@ -23,12 +23,10 @@ module.exports = async ({ spaceId, accessToken, host, syncToken, environment }) 
   let defaultLocale = `en-US`
   try {
     console.log(`Fetching default locale`)
-    console.log(client)
     locales = await client.getLocales().then(response => response.items)
     defaultLocale = _.find(locales, { default: true }).code
     console.log(`default local is : ${defaultLocale}`)
   } catch (e) {
-    console.log(e)
     console.log(
       `Accessing your Contentful space failed. Perhaps you're offline or the spaceId/accessToken is incorrect.`
     )
