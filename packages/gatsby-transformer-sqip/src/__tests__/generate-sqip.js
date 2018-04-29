48006eb509feb948e59c258f7f0a2b5e87693965
Benedikt Rötsch <info@benedikt-roetsch.de>
2018-04-29 22:02:17 +0200

    finalize unit tests for actual sqip implementation

--- packages/gatsby-transformer-sqip/src/__tests__/generate-sqip.js
+++ packages/gatsby-transformer-sqip/src/__tests__/generate-sqip.js
@@ -1,8 +1,32 @@
 const { resolve } = require(`path`)
 
-const { remove } = require(`fs-extra`)
+const { exists, readFile, writeFile } = require(`fs-extra`)
+const sqip = require(`sqip`)
 
 const generateSqip = require(`../generate-sqip.js`)
+
+jest.mock(`sqip`, () =>
+  jest.fn(() => {
+    return {
+      final_svg: `<svg><!-- Mocked SQIP SVG --></svg>`,
+    }
+  })
+)
+
+jest.mock(`fs-extra`, () => {
+  return {
+    exists: jest.fn(() => false),
+    readFile: jest.fn(() => `<svg><!-- Cached SQIP SVG --></svg>`),
+    writeFile: jest.fn(),
+  }
+})
+
+afterEach(() => {
+  sqip.mockClear()
+  exists.mockClear()
+  readFile.mockClear()
+  writeFile.mockClear()
+})
 
 describe(`gatsby-transformer-sqip`, async () => {
   const absolutePath = resolve(
@@ -12,10 +36,8 @@ describe(`gatsby-transformer-sqip`, async () => {
   )
   const cacheDir = __dirname
 
-  // const file = getFileObject(absolutePath)
-
   describe(`generateSqip`, () => {
-    it(`generates sqip`, async () => {
+    it(`not cached`, async () => {
       const cache = {
         get: jest.fn(),
         set: jest.fn(),
@@ -31,10 +53,43 @@ describe(`gatsby-transformer-sqip`, async () => {
         blur,
         mode,
       })
-      // @todo snapshots won't help here since primitive changes it's output every time. Consider counting primitives and checking blur setting via cheerio
       expect(result).toMatchSnapshot()
-      // @todo delete properly
-      await remove(resolve(cacheDir, `alisa-anton-166247-unsplash-400px.svg`))
+
+      expect(sqip).toHaveBeenCalledTimes(1)
+      const sqipArgs = sqip.mock.calls[0][0]
+      expect(sqipArgs.filename).toMatch(absolutePath)
+      delete sqipArgs.filename
+      expect(sqipArgs).toMatchSnapshot()
+
+      expect(exists).toHaveBeenCalledTimes(1)
+      expect(writeFile).toHaveBeenCalledTimes(1)
+      expect(readFile).toHaveBeenCalledTimes(0)
+    })
+    it(`cached`, async () => {
+      exists.mockImplementationOnce(() => true)
+      const cache = {
+        get: jest.fn(),
+        set: jest.fn(),
+      }
+      const numberOfPrimitives = 5
+      const blur = 0
+      const mode = 3
+      const result = await generateSqip({
+        cache,
+        cacheDir,
+        absolutePath,
+        numberOfPrimitives,
+        blur,
+        mode,
+      })
+
+      expect(result).toMatchSnapshot()
+
+      expect(sqip).toHaveBeenCalledTimes(0)
+
+      expect(exists).toHaveBeenCalledTimes(1)
+      expect(writeFile).toHaveBeenCalledTimes(0)
+      expect(readFile).toHaveBeenCalledTimes(1)
     })
   })
 })
