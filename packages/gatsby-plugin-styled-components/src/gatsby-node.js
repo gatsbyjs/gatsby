f6afa451c131950f09b6187207e6020d43ccafd7
Neo <nihgwu@live.com>
2018-05-01 22:54:55 +0800

    throw instead of ignore the plugin

--- packages/gatsby-plugin-styled-components/src/gatsby-node.js
+++ packages/gatsby-plugin-styled-components/src/gatsby-node.js
@@ -1,15 +1,13 @@
 // Add Babel plugin
-let babelPluginExists = false
 try {
   require.resolve(`babel-plugin-styled-components`)
-  babelPluginExists = true
 } catch (e) {
-  throw new Error(`'babel-plugin-styled-components' is not installed`)
+  throw new Error(
+    `'babel-plugin-styled-components' is not installed which is needed by plugin 'gatsyb-plugin-styled-components'`
+  )
 }
 
 exports.onCreateBabelConfig = ({ stage, actions }) => {
-  if (!babelPluginExists) return
-
   actions.setBabelPlugin({
     name: `babel-plugin-styled-components`,
     stage,
