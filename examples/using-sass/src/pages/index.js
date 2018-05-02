167a13e209ba68432c7aeed28f2894eeb06bb980
Michal Piechowiak <misiek.piechowiak@gmail.com>
2018-05-02 16:18:52 +0200

    update "using-sass" example

--- examples/using-sass/src/pages/index.js
+++ examples/using-sass/src/pages/index.js
@@ -1,20 +1,25 @@
 import React from "react"
 
+import Layout from "../layouts/index"
+
 class Index extends React.Component {
   render() {
     return (
-      <div>
+      <Layout>
         <h1 className="tu">Hi sassy friends</h1>
         <div className="sass-nav-example">
           <h2>Nav example</h2>
           <ul className="pa0 ma0 list">
             <li>
+              {/* eslint-disable-next-line */}
               <a href="#">Store</a>
             </li>
             <li>
+              {/* eslint-disable-next-line */}
               <a href="#">Help</a>
             </li>
             <li>
+              {/* eslint-disable-next-line */}
               <a href="#">Logout</a>
             </li>
             <li>
@@ -24,7 +29,7 @@ class Index extends React.Component {
             </li>
           </ul>
         </div>
-      </div>
+      </Layout>
     )
   }
 }
