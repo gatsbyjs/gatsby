7a2ce34ecaea792842e08d6c694a01b68f447e55
Kyle Mathews <mathews.kyle@gmail.com>
2018-05-11 08:15:17 -0700

    [v2] Make default browserslist more futureproof (#5114)
    * [v2] Make default browserslist more futureproof
    
    This is inspired by Jamie Kyle's blog post/issue in babel yesterday
    on the problems of using "last 2 versions" with @babel/preset-env which
    includes browsers which aren't receiving updates anymore so "last 2
    versions" will always be building for them.
    
    To quote his post:
    
    >Using market share percentages as your baseline is much better than some arbitrary number of previous versions. If a browser is used by 0 people, then market percentages will reflect that.
    
    * https://jamie.build/last-2-versions
    * https://github.com/babel/babel/issues/7789
    
    * Use recommended browser defaults

--- packages/gatsby-cli/src/create-cli.js
+++ packages/gatsby-cli/src/create-cli.js
@@ -4,7 +4,7 @@ const yargs = require(`yargs`)
 const report = require(`./reporter`)
 const fs = require(`fs`)
 
-const DEFAULT_BROWSERS = [`> 1%`, `last 2 versions`, `IE >= 9`]
+const DEFAULT_BROWSERS = [`>0.25%`, `not dead`]
 
 const handlerP = fn => (...args) => {
   Promise.resolve(fn(...args)).then(
