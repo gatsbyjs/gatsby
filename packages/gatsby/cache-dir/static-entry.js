cc5dac3cd33ed28aecc950b233893b41fd4c5478
Fredrik Søgaard <fredrik-sogaard@users.noreply.github.com>
2018-05-14 15:15:26 +0200

    Fix for IE10 not loading scripts (#5404)
    * Fix for IE10 not loading scripts
    
    * Fixed duplicate declaration.

--- packages/gatsby/cache-dir/static-entry.js
+++ packages/gatsby/cache-dir/static-entry.js
@@ -202,7 +202,7 @@ module.exports = (locals, callback) => {
     <script
       key={`script-loader`}
       dangerouslySetInnerHTML={{
-        __html: `/*<![CDATA[*/!function(e,t,r){function n(){for(;d[0]&&"loaded"==d[0][f];)c=d.shift(),c[o]=!i.parentNode.insertBefore(c,i)}for(var s,a,c,d=[],i=e.scripts[0],o="onreadystatechange",f="readyState";s=r.shift();)a=e.createElement(t),"async"in i?(a.async=!1,e.head.appendChild(a)):i[f]?(d.push(a),a[o]=n):e.write("<"+t+' src="'+s+'" defer></'+t+">"),a.src=s}(document,"script",[${scriptsString}])/*]]>*/`,
+        __html: `/*<![CDATA[*/[${scriptsString}].forEach(function(s){document.write('<script src="'+s+'" defer></'+'script>')})/*]]>*/`,
       }}
     />
   )
