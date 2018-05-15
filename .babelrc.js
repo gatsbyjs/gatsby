0a866289041f04c477d9886adcd6b4841c049483
Mike Allanson <michael.allanson@gmail.com>
2018-05-15 12:03:30 +0100

    [v2] Update Babel to 7.0.0-beta.47 (#5399)
    * Update babel to 7.0.0-beta.46
    
    * Update yarn.lock
    
    * Fix ignore paths
    
    * Add missing .babelrc files
    
    * Add config for Gatsby package
    
    * Fix Jest source compilation
    
    * Update to beta 47
    
    * Update yarn.lock

--- .babelrc.js
+++ .babelrc.js
@@ -1,13 +1,15 @@
-let ignore = [`**/dist/**`]
+let ignore = [`**/dist`]
 
 // Jest needs to compile this code, but generally we don't want this copied
 // to output folders
 if (process.env.NODE_ENV !== `test`) {
-  ignore.push(`**/__tests__/**`)
+  ignore.push(`**/__tests__`)
 }
+
+const presetAbsPath = require(`path`).join(__dirname, '.babel-preset.js')
 
 module.exports = {
   sourceMaps: true,
-  presets: [`./.babel-preset.js`],
+  presets: [presetAbsPath],
   ignore,
 }
