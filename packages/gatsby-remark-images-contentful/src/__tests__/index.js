e9f8acd13134a82abb2d9465482d78856a24fa3a
Khaled Garbaya <khaled@contentful.com>
2018-05-24 15:13:33 +0200

    Initial commit

---
+++ packages/gatsby-remark-images-contentful/src/__tests__/index.js
@@ -0,0 +1,144 @@
+const Remark = require(`remark`)
+
+const plugin = require(`../`)
+
+const remark = new Remark().data(`settings`, {
+  commonmark: true,
+  footnotes: true,
+  pedantic: true,
+})
+
+const createNode = content => {
+  const node = {
+    id: 1234,
+  }
+
+  const markdownNode = {
+    id: `${node.id} >>> MarkdownRemark`,
+    children: [],
+    parent: node.id,
+    internal: {
+      content,
+      contentDigest: `some-hash`,
+      type: `MarkdownRemark`,
+    },
+  }
+
+  markdownNode.frontmatter = {
+    title: ``, // always include a title
+    parent: node.id,
+  }
+
+  return markdownNode
+}
+
+const createPluginOptions = (content, imagePaths = `/`) => {
+  const dirName = `not-a-real-dir`
+  return {
+    files: [].concat(imagePaths).map(imagePath => {
+      return {
+        absolutePath: `${dirName}/${imagePath}`,
+      }
+    }),
+    markdownNode: createNode(content),
+    markdownAST: remark.parse(content),
+    getNode: () => {
+      return {
+        dir: dirName,
+      }
+    },
+  }
+}
+
+test(`it returns empty array when 0 images`, async () => {
+  const content = `
+# hello world
+Look ma, no images
+  `.trim()
+
+  const result = await plugin(createPluginOptions(content))
+
+  expect(result).toEqual([])
+})
+
+test(`it leaves relative images alone`, async () => {
+  const imagePath = `images/an-image.jpeg`
+  const content = `
+![asdf](${imagePath})
+  `.trim()
+
+  const result = await plugin(createPluginOptions(content, imagePath))
+
+  expect(result).toEqual([])
+})
+
+test(`it leaves non-contentful images alone`, async () => {
+  const imagePath = `https://google.com/images/an-image.jpeg`
+  const content = `
+![asdf](${imagePath})
+  `.trim()
+
+  const result = await plugin(createPluginOptions(content, imagePath))
+
+  expect(result).toEqual([])
+})
+
+test(`it transforms images in markdown`, async () => {
+  const imagePath = `https://images.ctfassets.net/rocybtov1ozk/wtrHxeu3zEoEce2MokCSi/73dce36715f16e27cf5ff0d2d97d7dff/quwowooybuqbl6ntboz3.jpg`
+  const content = `
+![image](${imagePath})
+  `.trim()
+
+  const nodes = await plugin(createPluginOptions(content, imagePath))
+
+  expect(nodes.length).toBe(1)
+
+  const node = nodes.pop()
+  expect(node.type).toBe(`html`)
+  expect(node.value).toMatchSnapshot()
+  expect(node.value).not.toMatch(`<html>`)
+})
+
+test(`it transforms multiple images in markdown`, async () => {
+  const imagePaths = [
+    `https://images.ctfassets.net/rocybtov1ozk/wtrHxeu3zEoEce2MokCSi/73dce36715f16e27cf5ff0d2d97d7dff/quwowooybuqbl6ntboz3.jpg`,
+    `https://images.ctfassets.net/rocybtov1ozk/wtrHxeu3zEoEce2MokCSi/73dce36715f16e27cf5ff0d2d97d7dff/quwowooybuqbl6ntboz3.jpg`,
+  ]
+
+  const content = `
+![image 1](${imagePaths[0]})
+![image 2](${imagePaths[1]})
+  `.trim()
+
+  const nodes = await plugin(createPluginOptions(content, imagePaths))
+
+  expect(nodes.length).toBe(imagePaths.length)
+})
+
+test(`it transforms HTML img tags`, async () => {
+  const imagePath = `https://images.ctfassets.net/rocybtov1ozk/wtrHxeu3zEoEce2MokCSi/73dce36715f16e27cf5ff0d2d97d7dff/quwowooybuqbl6ntboz3.jpg`
+
+  const content = `
+<img src="${imagePath}">
+  `.trim()
+
+  const nodes = await plugin(createPluginOptions(content, imagePath))
+
+  expect(nodes.length).toBe(1)
+
+  const node = nodes.pop()
+  expect(node.type).toBe(`html`)
+  expect(node.value).toMatchSnapshot()
+  expect(node.value).not.toMatch(`<html>`)
+})
+
+test(`it leaves relative HTML img tags alone`, async () => {
+  const imagePath = `images/this-was-an-image.jpeg`
+
+  const content = `
+<img src="${imagePath}">
+  `.trim()
+
+  const nodes = await plugin(createPluginOptions(content, imagePath))
+  expect(nodes[0].value).toBe(content)
+})
