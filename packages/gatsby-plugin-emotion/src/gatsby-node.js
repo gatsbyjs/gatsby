0798c4e5379a75d734cc1de8b3900479f89b3c42
Gabe Barrientos <gabe@skipio.com>
2018-05-28 08:32:18 -0600

    Extend emotion plugin options

--- packages/gatsby-plugin-emotion/src/gatsby-node.js
+++ packages/gatsby-plugin-emotion/src/gatsby-node.js
@@ -1,8 +1,8 @@
-exports.modifyBabelrc = ({ babelrc }) => {
+exports.modifyBabelrc = ({ babelrc },pluginOptions) => {
   if (process.env.NODE_ENV !== `production`) {
     return {
       plugins: [
-        [require.resolve(`babel-plugin-emotion`), { sourceMap: true }],
+        [require.resolve(`babel-plugin-emotion`), pluginOptions ? pluginOptions : { sourceMap: true,  autoLabel:true}],
       ].concat(babelrc.plugins),
     }
   }
