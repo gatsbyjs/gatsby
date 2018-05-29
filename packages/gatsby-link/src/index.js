648ce928478114e3c386a4075a548305f9e7d79b
Jason Lengstorf <jason@lengstorf.com>
2018-05-29 13:12:36 -0700

    fix: focus on the root div after navigation
    This fix applies focus to the main app div after page navigation,
    which triggers screen readers and other assistive tech to announce
    the new page. Thanks to @nickcolley for initially pointing this
    out, and to @LJWatson for jumping in with a fix!
    
    fix #5581

--- packages/gatsby-link/src/index.js
+++ packages/gatsby-link/src/index.js
@@ -149,6 +149,7 @@ class GatsbyLink extends React.Component {
                 // This is just a normal link to the current page so let's emulate default
                 // browser behavior by scrolling now to the top of the page.
                 window.scrollTo(0, 0)
+                document.querySelector(`#___gatsby`).focus()
                 return true
               }
             }
