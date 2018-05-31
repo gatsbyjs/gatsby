a623ba8708ac7947c449aeeaff037bf41aafeef5
Ryandi Tjia <ryandi.tjia@gmail.com>
2018-05-31 21:52:53 +0700

    [v2][www] Fix missing search sidebar for plugins/packages pages (#5550)
    * Add react-instantsearch package required by plugins searchbar
    
    * Add package searchbar to pages that require them
    
    * Rename searchbar to plugin-searchbar

--- www/src/templates/template-docs-remote-packages.js
+++ www/src/templates/template-docs-remote-packages.js
@@ -1,6 +1,7 @@
 import React from "react"
 
 import Layout from "../components/layout"
+import PageWithPluginSearchBar from "../components/page-with-plugin-searchbar"
 import PackageReadme from "../components/package-readme"
 
 class DocsRemotePackagesTemplate extends React.Component {
@@ -10,21 +11,23 @@ class DocsRemotePackagesTemplate extends React.Component {
     } = this.props
     return (
       <Layout location={this.props.location}>
-        <PackageReadme
-          page={markdownRemark}
-          packageName={npmPackage.name}
-          excerpt={npmPackage.readme.childMarkdownRemark.excerpt}
-          html={npmPackage.readme.childMarkdownRemark.html}
-          githubUrl={
-            npmPackage.repository !== null
-              ? npmPackage.repository.url
-              : `https://github.com/search?q=${npmPackage.name}`
-          }
-          modified={npmPackage.modified}
-          timeToRead={npmPackage.readme.childMarkdownRemark.timeToRead}
-          keywords={npmPackage.keywords}
-          lastPublisher={npmPackage.lastPublisher}
-        />
+        <PageWithPluginSearchBar history={this.props.history}>
+          <PackageReadme
+            page={markdownRemark}
+            packageName={npmPackage.name}
+            excerpt={npmPackage.readme.childMarkdownRemark.excerpt}
+            html={npmPackage.readme.childMarkdownRemark.html}
+            githubUrl={
+              npmPackage.repository !== null
+                ? npmPackage.repository.url
+                : `https://github.com/search?q=${npmPackage.name}`
+            }
+            modified={npmPackage.modified}
+            timeToRead={npmPackage.readme.childMarkdownRemark.timeToRead}
+            keywords={npmPackage.keywords}
+            lastPublisher={npmPackage.lastPublisher}
+          />
+        </PageWithPluginSearchBar>
       </Layout>
     )
   }
