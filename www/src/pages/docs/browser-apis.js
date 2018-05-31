bea6ee57e019b7010585717e3b953cf7420cb63d
Ryandi Tjia <ryandi.tjia@gmail.com>
2018-05-31 20:17:35 +0700

    [v2][www] Abstracted sidebar to layout component (#5537)
    * Abstracted sidebar to layout component
    
    * Hide sidebar on mobile

--- www/src/pages/docs/browser-apis.js
+++ www/src/pages/docs/browser-apis.js
@@ -6,6 +6,7 @@ import Functions from "../../components/function-list"
 import { rhythm, scale } from "../../utils/typography"
 import Layout from "../../components/layout"
 import Container from "../../components/container"
+import docsSidebar from "../../pages/docs/doc-links.yaml"
 
 class BrowserAPIDocs extends React.Component {
   render() {
@@ -15,7 +16,7 @@ class BrowserAPIDocs extends React.Component {
     )
 
     return (
-      <Layout location={this.props.location}>
+      <Layout location={this.props.location} sidebarYaml={docsSidebar}>
         <Container>
           <Helmet>
             <title>Browser APIs</title>
