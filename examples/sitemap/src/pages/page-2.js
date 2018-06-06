bdacdf3776a87047505326ce4685f3c5338e8b3a
Michal Piechowiak <misiek.piechowiak@gmail.com>
2018-06-06 17:17:05 +0200

    this page isn't secret

--- examples/sitemap/src/pages/page-2.js
+++ examples/sitemap/src/pages/page-2.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Page = () => (
   <div>
-    <h1>Secret page</h1>
+    <h1>Not so secret page</h1>
     <p>This page should be included in sitemap.xml</p>
   </div>
 )
