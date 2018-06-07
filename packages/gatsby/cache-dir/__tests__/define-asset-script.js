67fa861c48195b40b9ffae2636bd71ee716d79b2
Kawin Glomjai <kawin.glomjai@axia.life>
2018-06-07 13:52:03 +0700

    add test

---
+++ packages/gatsby/cache-dir/__tests__/define-asset-script.js
@@ -0,0 +1,26 @@
+import defineAssetScript from '../define-asset-script'
+import get from 'lodash/get'
+import isArray from 'lodash/isArray'
+
+const mockStats = require(`../json/mock-stats-object`)
+
+describe(`define-asset-script`, () => {
+  it(`should mutate "ChartBar.js" rel item to "prefetch"`, () => {
+    const linkComponent = [`About`,`ChartBar`, `foo`, `main`].map(s => { 
+      const fetchKey = `assetsByChunkName[${s}]`
+      const fetchedEntryPoints = get(mockStats, `entrypoints`)
+
+      let fetchedScript = get(mockStats, fetchKey)
+      fetchedScript = isArray(fetchedScript) ? fetchedScript[0] : fetchedScript
+      return defineAssetScript(fetchedEntryPoints, fetchedScript, { rel: `preload`, prefixedScript: fetchedScript })
+    })
+    // expect(linkComponent).toEqual()
+    linkComponent.forEach(({ rel, prefixedScript }) => {
+      if(prefixedScript === `ChartBar.js`) {
+        expect(rel).toBe(`prefetch`)
+      } else {
+        expect(rel).toBe(`preload`)
+      }
+    })
+  })
+})
