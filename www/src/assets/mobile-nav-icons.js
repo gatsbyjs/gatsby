4068c286dd8e1e9a7cece6e74b7f7dc206992b7f
tsiq-swyx <shawn.wang@twosigmaiq.com>
2018-06-07 22:50:22 -0400

    add showcase magic icon

--- www/src/assets/mobile-nav-icons.js
+++ www/src/assets/mobile-nav-icons.js
@@ -2,5 +2,6 @@ import BlogIcon from "./blog.svg"
 import DocsIcon from "./docs.svg"
 import TutorialIcon from "./tutorial.svg"
 import PluginsIcon from "./plugins.svg"
+import ShowcaseIcon from "./showcase-magic.svg"
 
-export { BlogIcon, DocsIcon, TutorialIcon, PluginsIcon }
+export { BlogIcon, DocsIcon, TutorialIcon, PluginsIcon, ShowcaseIcon }
