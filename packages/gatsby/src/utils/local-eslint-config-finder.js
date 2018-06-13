1b5bd56dbc7118b40f4088d1e73c27983b7714df
Kurtis Kemple <kurtiskemple@gmail.com>
2018-06-13 09:25:47 -0400

    add: disable gatsby eslint if local found

---
+++ packages/gatsby/src/utils/local-eslint-config-finder.js
@@ -0,0 +1,23 @@
+const path = require(`path`)
+const glob = require(`glob`)
+
+module.exports = directory => {
+  let hasEslintConfig = false
+
+  try {
+    const pkg = require(path.resolve(directory, `package.json`))
+    if (pkg.eslintConfig) {
+      hasEslintConfig = true
+    }
+  } catch (error) {
+    // not sure what we should do here...
+  }
+
+  const eslintFiles = glob.sync(`.eslintrc*`, { cwd: directory })
+
+  if (eslintFiles.length) {
+    hasEslintConfig = true
+  }
+
+  return hasEslintConfig
+}
