4c772fced502603ec2db8e3ffaeb0b9dec04804b
Kurtis Kemple <kurtiskemple@gmail.com>
2018-06-15 14:06:10 -0400

    fix: local-eslint-config-finder

--- packages/gatsby/src/utils/local-eslint-config-finder.js
+++ packages/gatsby/src/utils/local-eslint-config-finder.js
@@ -1,25 +1,28 @@
 const path = require(`path`)
 const glob = require(`glob`)
+const debug = require(`debug`)(`gatsby:webpack-eslint-config`)
+const report = require(`gatsby-cli/lib/reporter`)
 
 module.exports = directory => {
-  let hasEslintConfig = false
-
   try {
+    debug(`Attempting to load package.json for eslint config check`)
+
     const pkg = require(path.resolve(directory, `package.json`))
     if (pkg.eslintConfig) {
-      hasEslintConfig = true
+      return true
     }
-  } catch (error) {
-    // not sure what we should do here...
+  } catch (err) {
+    report.error(`There was a problem processing the package.json file`, err)
   }
 
+  debug(`Checking for eslint config file`)
   const eslintFiles = glob.sync(`.eslintrc?(.js|.json|.yaml|.yml)`, {
     cwd: directory,
   })
 
   if (eslintFiles.length) {
-    hasEslintConfig = true
+    return true
   }
 
-  return hasEslintConfig
+  return false
 }
