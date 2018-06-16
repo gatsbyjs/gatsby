8780c1cacaf643b60d307337571d1e04ca6c4550
Mike Allanson <michael.allanson@gmail.com>
2018-06-16 19:38:20 +0100

    [v2] Fix v1 / v2 version conflicts (#5899)
    * Remove old / v1-specific plugins
    
    * Fix conflicting versions between v1 and v2, update gatsby peerdep to v2
    
    * Update dependencies for version check
    
    * Update snapshot
    
    * Update peerDep to match new version

--- packages/gatsby-1-config-extract-plugin/__mocks__/extract-text-webpack-plugin.js
+++
@@ -1,8 +0,0 @@
-// extract-text-webpack-plugin depends on webpack 1 which is no longer used in
-// Gatsby v2. Mock its implementation here.
-
-const mock = jest.fn().mockImplementation(filename => {
-  return { filename }
-})
-
-module.exports = mock
