a6f4881107a2478e913c8abf1fde7e879ebc9103
Sam Bhagwat <calcsam@gmail.com>
2018-06-16 13:14:12 -0700

    Topics/showcase rebase v2 (#5941)
    * Current work on showcase against v2
    
    * Update outdated gatsby-transformer-screenshot
    
    * Update gatsby-config to remove vestigial folder
    
    * Clean up after merge
    
    * make css fixes to showcase: add featured sites at top, make searchbox nicer, cta buttons. put prev and next for permalink pages
    
    * Use next versions of sharp plugins
    
    * details page is now responsive
    
    * main page responsive
    
    * add showcase magic icon
    
    * Fix production builds
    
    * Make mobile sizing work correctly
    
    * Fix warning (UNSAFE_componentWillMount)
    
    * Sloppy fix for missing node.fields.slug
    
    * (WiP) Various „Featured Sites“ fixes
    
    * stop Prettier from inserting stray backticks around „View all“ (React.Fragment helped)
    * add functionality to „View all“ link
    * typography
    * add background image to container
    * fix main layout container horizontal whitespace
    
    * Switch from „resolutions“ to „sizes“ for „Featured Sites“ images
    
    … which allows us to define different widths per breakpoint for „Featured Sites“ cards. Along the way:
    
    * (WiP) add „View all Featured Sites“ card design
    * „built by“, category typography
    * fix showcase-magic.svg viewbox
    
    * Abandon window.width in favor of CSS media queries
    
    * Fix „All featured sites“ card for smaller screens…’n MOAR
    
    * add basic styles for search form
    * add „Featured Sites“ card hover styles
    * add sticky sidebar/„x Showcase Sites/Search“ header
    * consolidate button styles for „Submit Site“ and „Load more“
    * sidebar UI styles first rinse touching
      * „collapsible“ header styles
      * filter item styles
    * add webkit-scrollbar styles for both `overflow: scroll` containers
    
    * Simplify markup, semantics
    
    * Showcase card hover, whitespace
    
    * Filter item vertical padding, increase gutter for featured cards on Desktop
    
    * Allow clicking through the gradient overlay
    
    * Don’t let gradient overlay cover scrollbar
    
    * Consolidate (not all) card hover styles
    
    * Define list card width
    
    * UI fixes
    
    * Increase modal background opacity
    
    * Set modal content width, various modal UI fixes…and Prettier
    
    * Add modal close button
    
    * Fix modal/close button border radius
    
    * Previous/next fontSize, fix source link appearance
    
    * Checkboxes for sidebar filter items, „Clear filters“ button…MOAR
    
    * support multi-line sidebar filter item titles
    * add initial design for „Clear filters“ button
    * align „caret“ icon in <Collapsible /> header
    * add arrow forward, downward icons
    
    * Adjust modal description, categories appearance
    
    * Fix permalink view basic layout
    
    * Detail view: Add „Visit Site“ button
    
    * Revisit „Clear all Filters“ button UI
    
    * Last bits of detail view header design
    
    Along the way: color variables, consolidate styles.
    
    * Add gutterDesktop
    
    * Remove units
    
    * Remove duplicate key, order
    
    * (WIP) Big prev/next images on permalink pages
    
    * „Featured Sites“ „view all“ links: Scroll to filtered showcase list
    
    scroll-to-anchor.js by @jlengstorf
    
    * Fix „View all Featured Sites“ right margin
    
    Ref. https://stackoverflow.com/a/28682628
    
    * Prettier
    
    * Underline title when hovering card
    
    * Fix #showcase position
    
    * Add OpenGraph/Twitter image meta tags
    
    Analog to what we do in `blog-post-template.js`.
    
    * Add first shot at the „social share“ menu
    
    * Mobile improvements
    
    * Fix dupe keys
    
    * Fix horizontal margin
    
    * Link „permalink“ page prev/next images
    
    * Whitespace
    
    * Square „prev/next“ images for permalink pages
    
    * Mobile navigation shouldn’t cover images
    
    * Remove image marginBottom
    
    * Hide „Featured Sites“ for less than Desktop for now
    
    * Implement reduced version of permalink page prev/next link design
    
    * Update „Submit site“ href
    
    * Update metadata urls
    
    * fix package.json
    
    * showcase fix rebase & modified hack
    
    * more rebase edits

--- www/src/components/layout.js
+++ www/src/components/layout.js
@@ -1,11 +1,17 @@
 import React from "react"
+import Modal from "react-modal"
 import Helmet from "react-helmet"
+import MdClose from "react-icons/lib/md/close"
 
-import presets from "../utils/presets"
+import { navigateTo, PageRenderer } from "gatsby"
+
+import presets, { colors } from "../utils/presets"
 import Navigation from "../components/navigation"
 import MobileNavigation from "../components/navigation-mobile"
 import PageWithSidebar from "../components/page-with-sidebar"
 import "../css/prism-coy.css"
+
+import mousetrap from "mousetrap"
 
 // Import Futura PT typeface
 import "../fonts/Webfonts/futurapt_book_macroman/stylesheet.css"
@@ -17,12 +23,128 @@ import "../fonts/Webfonts/futurapt_demiitalic_macroman/stylesheet.css"
 import "typeface-spectral"
 import "typeface-space-mono"
 
+let windowWidth
+
 class DefaultLayout extends React.Component {
+  constructor() {
+    super()
+    this.handleCloseModal = this.handleCloseModal.bind(this)
+  }
+
+  handleCloseModal() {
+    navigateTo(this.props.modalBackgroundPath)
+  }
+
+  componentDidMount() {
+    Modal.setAppElement(`#___gatsby`)
+
+    if (this.props.isModal && window.innerWidth > 750) {
+      mousetrap.bind(`left`, this.props.modalPrevious)
+      mousetrap.bind(`right`, this.props.modalNext)
+      mousetrap.bind(`spacebar`, this.props.modalNext)
+
+      document.querySelector(`html`).style.overflowY = `hidden`
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.props.isModal && window.innerWidth > 750) {
+      mousetrap.unbind(`left`)
+      mousetrap.unbind(`right`)
+      mousetrap.unbind(`spacebar`)
+
+      document.querySelector(`html`).style.overflowY = `auto`
+    }
+  }
+
   render() {
     const isHomepage = this.props.location.pathname === `/`
 
     // SEE: template-docs-markdown for why this.props.isSidebarDisabled is here
     const isSidebarDisabled = this.props.isSidebarDisabled || !this.props.sidebarYaml
+    let isModal = false
+    if (!windowWidth && typeof window !== `undefined`) {
+      windowWidth = window.innerWidth
+    }
+    if (this.props.isModal && windowWidth > 750) {
+      isModal = true
+    }
+
+    if (isModal && window.innerWidth > 750) {
+      return (
+        <React.Fragment>
+          <PageRenderer
+            location={{ pathname: this.props.modalBackgroundPath }}
+          />
+          <Modal
+            isOpen={true}
+            style={{
+              content: {
+                top: `inherit`,
+                left: `inherit`,
+                right: `inherit`,
+                bottom: `inherit`,
+                margin: `0 auto`,
+                width: `750px`,
+                background: `none`,
+                border: `none`,
+                padding: `40px 0`,
+                overflow: `visible`,
+              },
+              overlay: {
+                position: `absolute`,
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: `unset`,
+                minHeight: `100%`,
+                minWidth: `100%`,
+                zIndex: 10,
+                overflowY: `auto`,
+                backgroundColor: `rgba(255, 255, 255, 0.95)`,
+              },
+            }}
+            onRequestClose={() => navigateTo(this.props.modalBackgroundPath)}
+            contentLabel="Site Details Modal"
+          >
+            <div
+              css={{
+                backgroundColor: `#ffffff`,
+                borderRadius: presets.radius,
+                boxShadow: `0 0 90px -24px ${colors.gatsby}`,
+                position: `relative`,
+              }}
+            >
+              <button
+                onClick={this.handleCloseModal}
+                css={{
+                  background: colors.ui.bright,
+                  border: 0,
+                  borderBottomLeftRadius: presets.radius,
+                  borderTopRightRadius: presets.radius,
+                  color: colors.gatsby,
+                  cursor: `pointer`,
+                  position: `absolute`,
+                  left: `auto`,
+                  right: 0,
+                  height: 40,
+                  width: 40,
+                  "&:hover": {
+                    background: colors.gatsby,
+                    color: `#fff`,
+                  },
+                }}
+              >
+                <MdClose />
+              </button>
+              {this.props.children}
+              {this.props.modalPreviousLink}
+              {this.props.modalNextLink}
+            </div>
+          </Modal>
+        </React.Fragment>
+      )
+    }
 
     return (
       <div className={isHomepage ? `is-homepage` : ``}>
