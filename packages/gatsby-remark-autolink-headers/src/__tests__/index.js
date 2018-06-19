a26fcbfa562a774d6d865dda5f2aa317f4a7392a
Lennart <lekoarts@gmail.com>
2018-06-19 18:17:01 +0200

    [gatsby-remark-autolink-headers] Add option to choose a SVG icon (#5961)
    * Add option to choose a SVG icon
    
    * First step with tests
    
    * Add test for "icon" usage
    
    * Update README

--- packages/gatsby-remark-autolink-headers/src/__tests__/index.js
+++ packages/gatsby-remark-autolink-headers/src/__tests__/index.js
@@ -13,7 +13,7 @@ describe(`gatsby-remark-autolink-headers`, () => {
   it(`adds id to a markdown header`, () => {
     const markdownAST = remark.parse(`# Heading Uno`)
 
-    const transformed = plugin({ markdownAST })
+    const transformed = plugin({ markdownAST }, {})
 
     visit(transformed, `heading`, node => {
       expect(node.data.id).toBeDefined()
@@ -31,7 +31,36 @@ describe(`gatsby-remark-autolink-headers`, () => {
 ### Heading Three
     `)
 
-    const transformed = plugin({ markdownAST })
+    const transformed = plugin({ markdownAST }, {})
+
+    visit(transformed, `heading`, node => {
+      expect(node.data.id).toBeDefined()
+    })
+  })
+  it(`adds id to a markdown header with custom svg icon`, () => {
+    const markdownAST = remark.parse(`# Heading Uno`)
+    const icon = `<svg width="400" height="110"><rect width="300" height="100" /></svg>`
+
+    const transformed = plugin({ markdownAST }, { icon })
+
+    visit(transformed, `heading`, node => {
+      expect(node.data.id).toBeDefined()
+
+      expect(node).toMatchSnapshot()
+    })
+  })
+
+  it(`adds ids to each markdown header with custom svg icon`, () => {
+    const markdownAST = remark.parse(`
+# Heading One
+
+## Heading Two
+
+### Heading Three
+    `)
+    const icon = `<svg width="400" height="110"><rect width="300" height="100" /></svg>`
+
+    const transformed = plugin({ markdownAST }, { icon })
 
     visit(transformed, `heading`, node => {
       expect(node.data.id).toBeDefined()
