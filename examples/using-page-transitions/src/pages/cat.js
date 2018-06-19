569c73a46f024538afc807cbdd7f9b5b683f2877
Michal Piechowiak <misiek.piechowiak@gmail.com>
2018-06-19 17:13:19 +0200

    [V2] Fix and rework using page transitions (target correct branch) (#6022)
    * Update using-page-transitions example for gatsby V2
    
    Signed-off-by: Sam Hellawell <sshellawell@gmail.com>
    
    * fix Browser history needs a DOM error on build
    
    Signed-off-by: Sam Hellawell <sshellawell@gmail.com>
    
    * Rework and fix transitions in build for v2
    
    Signed-off-by: Sam Hellawell <sshellawell@gmail.com>
    
    * fix warnings in transition.js
    
    Signed-off-by: Sam Hellawell <sshellawell@gmail.com>
    
    * fix eslint warnings
    
    * replace UNSAFE_componentWillReceiveProps with getDerivedStateFromProps

--- examples/using-page-transitions/src/pages/cat.js
+++ examples/using-page-transitions/src/pages/cat.js
@@ -1,20 +1,21 @@
 /* eslint-disable react/prop-types */
 import React from "react"
 import { Link } from "gatsby"
+import TemplateWrapper from "../components/layout"
 
-const Cat = ({ transition }) => (
-  <div style={transition && transition.style}>
-    <h1>meow</h1>
-    <div>
-      <Link to="/">Go to home</Link>
-    </div>
-    <div>
-      <Link to="/dog/">Go to dog</Link>
-    </div>
-    <div>
-      <Link to="/long-page/">Go to long page</Link>
-    </div>
-  </div>
+const Cat = () => (
+  <TemplateWrapper>
+      <h1>meow</h1>
+      <div>
+        <Link to="/">Go to home</Link>
+      </div>
+      <div>
+        <Link to="/dog/">Go to dog</Link>
+      </div>
+      <div>
+        <Link to="/long-page/">Go to long page</Link>
+      </div>
+  </TemplateWrapper>
 )
 
 export default Cat
--- examples/using-page-transitions/src/pages/dog.js
+++ examples/using-page-transitions/src/pages/dog.js
@@ -1,20 +1,23 @@
 /* eslint-disable react/prop-types */
 import React from "react"
 import { Link } from "gatsby"
+import TemplateWrapper from "../components/layout"
 
 const Dog = ({ transition }) => (
-  <div style={transition && transition.style}>
-    <h1>ruff</h1>
-    <div>
-      <Link to="/">Go to home</Link>
+  <TemplateWrapper>
+    <div style={transition && transition.style}>
+      <h1>ruff</h1>
+      <div>
+        <Link to="/">Go to home</Link>
+      </div>
+      <div>
+        <Link to="/cat/">Go to cat</Link>
+      </div>
+      <div>
+        <Link to="/long-page/">Go to long page</Link>
+      </div>
     </div>
-    <div>
-      <Link to="/cat/">Go to cat</Link>
-    </div>
-    <div>
-      <Link to="/long-page/">Go to long page</Link>
-    </div>
-  </div>
+  </TemplateWrapper>
 )
 
 export default Dog
