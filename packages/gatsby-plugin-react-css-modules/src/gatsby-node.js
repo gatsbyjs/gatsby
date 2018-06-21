4c97c67ff4073d870f8fee3ba411eb84c69790e9
Sean King <sean@seanking.org>
2018-06-21 20:01:50 +1000

    Pull in correct dep for plugin-react-css-modules

--- packages/gatsby-plugin-react-css-modules/src/gatsby-node.js
+++ packages/gatsby-plugin-react-css-modules/src/gatsby-node.js
@@ -1,6 +1,6 @@
 exports.onCreateBabelConfig = ({ actions }) => {
   actions.setBabelPlugin({
-    name: `react-css-modules`,
+    name: `babel-plugin-react-css-modules`,
     options: {
       generateScopedName: `[name]--[local]--[hash:base64:5]`,
       webpackHotModuleReloading: process.env.NODE_ENV !== `production`,
