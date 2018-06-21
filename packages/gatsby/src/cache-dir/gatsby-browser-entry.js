233609572ad0855859c34fd99efc5057bda4a717
Jason Quense <monastic.panic@gmail.com>
2018-06-21 13:52:53 -0400

    Import graphql -> master (#6071)
    * require graphql import
    
    * rollback breaking change
    
    * better runtime error
    
    * MOAR EDGE CASES!

--- packages/gatsby/src/cache-dir/gatsby-browser-entry.js
+++ packages/gatsby/src/cache-dir/gatsby-browser-entry.js
@@ -46,9 +46,19 @@ StaticQuery.propTypes = {
   children: PropTypes.func,
 }
 
+function graphql() {
+  throw new Error(
+    `It appears like Gatsby is misconfigured. Gatsby related \`graphql\` calls ` +
+      `are supposed to only be evaluated at compile time, and then compiled away,. ` +
+      `Unfortunately, something went wrong and the query was left in the compiled code.\n\n.` +
+      `Unless your site has a complex or custom babel/Gatsby configuration this is likely a bug in Gatsby.`
+  )
+}
+
 export {
   Link,
   withPrefix,
+  graphql,
   push,
   replace,
   navigateTo, // TODO: remove navigateTo for v3
