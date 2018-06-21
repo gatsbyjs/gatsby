95eac53d71c7def8c3f75408334ccea8692d9775
Khaled Garbaya <khaledgarbaya@gmail.com>
2018-06-21 14:58:25 +0200

    [source-contentful] offline mode (#5869)
    * Add fallback to offline
    
    * Fix lint
    
    * Cleanup
    
    * small fixes
    
    * format

--- packages/gatsby-source-contentful/src/fetch.js
+++ packages/gatsby-source-contentful/src/fetch.js
@@ -1,6 +1,5 @@
 const contentful = require(`contentful`)
 const _ = require(`lodash`)
-
 const normalize = require(`./normalize`)
 
 module.exports = async ({
@@ -10,8 +9,9 @@ module.exports = async ({
   syncToken,
   environment,
 }) => {
-  // Fetch articles.
+  // Fetch entries.
   console.time(`Fetch Contentful data`)
+
   console.log(`Starting to fetch data from Contentful`)
 
   const client = contentful.createClient({
@@ -36,9 +36,10 @@ module.exports = async ({
     console.log(
       `Accessing your Contentful space failed. Perhaps you're offline or the spaceId/accessToken is incorrect.`
     )
-    // TODO perhaps continue if there's cached data? That would let
-    // someone develop a contentful site even if not connected to the internet.
-    // For prod builds though always fail if we can't get the latest data.
+    console.log(
+      `Try running setting GATSBY_CONTENTFUL_OFFLINE=true to see if we can serve from cache.`
+    )
+
     process.exit(1)
   }
 
@@ -91,12 +92,14 @@ module.exports = async ({
     return null
   })
 
-  return {
+  const result = {
     currentSyncData,
     contentTypeItems,
     defaultLocale,
     locales,
   }
+
+  return result
 }
 
 /**
