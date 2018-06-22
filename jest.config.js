e01d0c18a5f07bc4e996c70efe039a45bca8e02c
Michal Piechowiak <misiek.piechowiak@gmail.com>
2018-06-22 12:24:19 +0200

    jest: ignore built __tests__ (#6089)
    * jest: ignore built __tests__
    
    currently used version of @babel/cli seems to not honor `--ignore` on windows, so __tests__ dirs are getting built, this should temporary hack, should fix some of the tests failing on appveyor
    
    * jest: ignore gatsby built dist and cache-dir dirs

--- jest.config.js
+++ jest.config.js
@@ -3,14 +3,19 @@ const glob = require(`glob`)
 
 const pkgs = glob.sync(`./packages/*`).map(p => p.replace(/^\./, `<rootDir>`))
 
-const distDirs = pkgs.map(p => path.join(p, `dist`))
+const reGatsby = /gatsby$/
+const gatsbyDir = pkgs.find(p => reGatsby.exec(p))
+const gatsbyBuildDirs = [`dist`, `cache-dir`].map(dir => path.join(gatsbyDir, dir))
+const builtTestsDirs = pkgs.map(p => path.join(p, `__tests__`))
+
+const ignoreDirs = [].concat(gatsbyBuildDirs, builtTestsDirs)
 
 module.exports = {
   notify: true,
   verbose: true,
   roots: pkgs,
-  modulePathIgnorePatterns: distDirs,
-  coveragePathIgnorePatterns: distDirs,
+  modulePathIgnorePatterns: ignoreDirs,
+  coveragePathIgnorePatterns: ignoreDirs,
   testPathIgnorePatterns: [
     `/examples/`,
     `/www/`,
