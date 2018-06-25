84d18e5c609a1ec9daecf4df01a026d3c68193c9
Ryandi Tjia <ryandi.tjia@gmail.com>
2018-06-25 21:17:03 +0700

    Remove redundant left padding

--- www/src/templates/template-docs-markdown.js
+++ www/src/templates/template-docs-markdown.js
@@ -28,34 +28,28 @@ class DocsTemplate extends React.Component {
       >
         <DocSearchContent>
           <Container>
-            <div
-              css={{
-                paddingLeft: rhythm(2),
-              }}
-            >
-              <Helmet>
-                <title>{page.frontmatter.title}</title>
-                <meta name="description" content={page.excerpt} />
-                <meta name="og:description" content={page.excerpt} />
-                <meta name="twitter:description" content={page.excerpt} />
-                <meta name="og:title" content={page.frontmatter.title} />
-                <meta name="og:type" content="article" />
-                <meta name="twitter.label1" content="Reading time" />
-                <meta
-                  name="twitter:data1"
-                  content={`${page.timeToRead} min read`}
-                />
-              </Helmet>
-              <h1 id={page.fields.anchor} css={{ marginTop: 0 }}>
-                {page.frontmatter.title}
-              </h1>
-              <div
-                dangerouslySetInnerHTML={{
-                  __html: page.html,
-                }}
+            <Helmet>
+              <title>{page.frontmatter.title}</title>
+              <meta name="description" content={page.excerpt} />
+              <meta name="og:description" content={page.excerpt} />
+              <meta name="twitter:description" content={page.excerpt} />
+              <meta name="og:title" content={page.frontmatter.title} />
+              <meta name="og:type" content="article" />
+              <meta name="twitter.label1" content="Reading time" />
+              <meta
+                name="twitter:data1"
+                content={`${page.timeToRead} min read`}
               />
-              <MarkdownPageFooter page={page} />
-            </div>
+            </Helmet>
+            <h1 id={page.fields.anchor} css={{ marginTop: 0 }}>
+              {page.frontmatter.title}
+            </h1>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: page.html,
+              }}
+            />
+            <MarkdownPageFooter page={page} />
           </Container>
         </DocSearchContent>
       </Layout>
