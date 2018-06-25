bd30efaa8f1f8793bd4854819b6b6690ffc540a6
Ryandi Tjia <ryandi.tjia@gmail.com>
2018-06-26 04:02:54 +0700

    [v2] Fix doc not centered (#6146)
    * Remove redundant left padding
    
    * Prettify code

--- www/src/templates/template-docs-markdown.js
+++ www/src/templates/template-docs-markdown.js
@@ -8,44 +8,36 @@ import MarkdownPageFooter from "../components/markdown-page-footer"
 import DocSearchContent from "../components/docsearch-content"
 
 import Container from "../components/container"
-import { rhythm } from "../utils/typography"
 
 class DocsTemplate extends React.Component {
   render() {
     const page = this.props.data.markdownRemark
     return (
-      <Layout
-        location={this.props.location}
-        isSidebarDisabled={
-          this.props.location.pathname === `/community/` ||
-          this.props.location.pathname === `/code-of-conduct/`
-        }
-        sidebarYaml={
-          this.props.location.pathname.slice(0, 5) === `/docs`
-            ? docsSidebar
-            : tutorialSidebar
-        }
-      >
-        <DocSearchContent>
-          <Container>
-            <div
-              css={{
-                paddingLeft: rhythm(2),
-              }}
-            >
-              <Helmet>
-                <title>{page.frontmatter.title}</title>
-                <meta name="description" content={page.excerpt} />
-                <meta name="og:description" content={page.excerpt} />
-                <meta name="twitter:description" content={page.excerpt} />
-                <meta name="og:title" content={page.frontmatter.title} />
-                <meta name="og:type" content="article" />
-                <meta name="twitter.label1" content="Reading time" />
-                <meta
-                  name="twitter:data1"
-                  content={`${page.timeToRead} min read`}
-                />
-              </Helmet>
+      <>
+        <Helmet>
+          <title>{page.frontmatter.title}</title>
+          <meta name="description" content={page.excerpt} />
+          <meta name="og:description" content={page.excerpt} />
+          <meta name="twitter:description" content={page.excerpt} />
+          <meta name="og:title" content={page.frontmatter.title} />
+          <meta name="og:type" content="article" />
+          <meta name="twitter.label1" content="Reading time" />
+          <meta name="twitter:data1" content={`${page.timeToRead} min read`} />
+        </Helmet>
+        <Layout
+          location={this.props.location}
+          isSidebarDisabled={
+            this.props.location.pathname === `/community/` ||
+            this.props.location.pathname === `/code-of-conduct/`
+          }
+          sidebarYaml={
+            this.props.location.pathname.slice(0, 5) === `/docs`
+              ? docsSidebar
+              : tutorialSidebar
+          }
+        >
+          <DocSearchContent>
+            <Container>
               <h1 id={page.fields.anchor} css={{ marginTop: 0 }}>
                 {page.frontmatter.title}
               </h1>
@@ -55,10 +47,10 @@ class DocsTemplate extends React.Component {
                 }}
               />
               <MarkdownPageFooter page={page} />
-            </div>
-          </Container>
-        </DocSearchContent>
-      </Layout>
+            </Container>
+          </DocSearchContent>
+        </Layout>
+      </>
     )
   }
 }
