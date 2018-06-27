32bdb3668ecce8e6e31a167f4107537f5ee7bfef
Michal Piechowiak <misiek.piechowiak@gmail.com>
2018-06-27 13:26:27 +0200

    format

--- packages/gatsby/src/cache-dir/json-store.js
+++ packages/gatsby/src/cache-dir/json-store.js
@@ -4,7 +4,10 @@ import PageRenderer from "./page-renderer"
 import { StaticQueryContext } from "gatsby"
 import socketIo, { getStaticQueryData, getPageQueryData } from "./socketIo"
 
-const getPathFromProps = props => props.pageResources && props.pageResources.page ? props.pageResources.page.path : null
+const getPathFromProps = props =>
+  props.pageResources && props.pageResources.page
+    ? props.pageResources.page.path
+    : null
 
 class JSONStore extends React.Component {
   constructor(props) {
