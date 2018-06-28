eeef674e03c6e9fbdda0fea4821914a1861584fa
Mike Allanson <michael.allanson@gmail.com>
2018-06-28 17:16:30 +0100

    Update gatsby-source-hacker-news and the hn example to Gatsby v2 (#6214)

--- examples/hn/src/pages/index.js
+++ examples/hn/src/pages/index.js
@@ -1,24 +1,27 @@
 import React from "react"
-
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
 import StoryItem from "../components/story-item"
 
 class Index extends React.Component {
   render() {
     const { allHnStory } = this.props.data
-
-    console.log(this.props)
     return (
-      <table border="0" className="itemlist" cellPadding={0} cellSpacing={0}>
-        {allHnStory.edges.map(({ node }) => <StoryItem story={node} />)}
-        <tr className="morespace" style={{ height: `10px` }} />
-        <tr>
-          <td style={{ paddingLeft: 36 }} className="title">
-            <a href="/page/2/" className="morelink" rel="nofollow">
-              More
-            </a>
-          </td>
-        </tr>
-      </table>
+      <Layout>
+        <table border="0" className="itemlist" cellPadding={0} cellSpacing={0}>
+          <tbody>
+            {allHnStory.edges.map(({ node }) => <StoryItem key={node.id} story={node} />)}
+            <tr className="morespace" style={{ height: `10px` }} />
+            <tr>
+              <td style={{ paddingLeft: 36 }} className="title">
+                <a href="/page/2/" className="morelink" rel="nofollow">
+                  More
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </Layout>
     )
   }
 }
