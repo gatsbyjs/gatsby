d6ac73072003dd3436ed8813f860e457bea59d94
Emanuele Peruffo <info@emanueleperuffo.com>
2018-07-04 13:03:01 +0200

    Made gatsby-plugin-netlify-cms work with gatsby v2

--- packages/gatsby-plugin-netlify-cms/src/gatsby-node.js
+++ packages/gatsby-plugin-netlify-cms/src/gatsby-node.js
@@ -13,14 +13,13 @@ function plugins(stage) {
     }),
   ]
 
-  switch (stage) {
-    case `develop`:
-      return commonPlugins
-    case `build-javascript`:
-      return [...commonPlugins, extractCmsCss]
-    default:
-      return []
+  if (stage === `develop`) {
+    return commonPlugins
+  } else if (stage === `build-javascript`) {
+    return [...commonPlugins, extractCmsCss]
   }
+
+  return []
 }
 
 /**
@@ -28,47 +27,116 @@ function plugins(stage) {
  * using webpack-configurator for webpack config extension, and also on the
  * target loader key being named "css" in Gatsby's webpack config.
  */
-function excludeFromLoader(key, config) {
-  config.loader(key, ({ exclude, ...configRest }) => {
-    const regex = /\/node_modules\/netlify-cms\//
-    if (!exclude) {
-      return { ...configRest, exclude: regex }
-    }
-    if (Array.isArray(exclude)) {
-      return { ...configRest, exclude: [...exclude, regex] }
-    }
-    return { ...configRest, exclude: [exclude, regex] }
+function excludeFromLoader({ actions, rules, getConfig }) {
+  const regex = /\/node_modules\/netlify-cms\//
+
+  const cssRule = { ...rules.css(), exclude: regex }
+
+  const prevConfig = getConfig()
+
+  actions.replaceWebpackConfig({
+    ...prevConfig,
+
+    module: {
+      ...prevConfig.module,
+
+      rules: prevConfig.module.rules.map((rule) => {
+        const isCssRule = rule.oneOf && (
+          rule.oneOf.some(({ test }) => String(test) === String(cssRule.test))
+        )
+
+        if (isCssRule) {
+          const newRule = {
+            ...rule,
+
+            oneOf: rule.oneOf.map((rule) => {
+              const { exclude: prevExclude } = rule
+
+              if (!prevExclude) {
+                return {
+                  ...rule,
+                  exclude: regex,
+                }
+              } else if (Array.isArray(prevExclude)) {
+                return {
+                  ...rule,
+                  exclude: [
+                    ...prevExclude,
+                    regex,
+                  ],
+                }
+              }
+
+              return {
+                ...rule,
+                exclude: [ prevExclude, regex ],
+              }
+            }),
+          }
+
+          return newRule
+        }
+
+        return rule
+      }),
+    },
   })
 }
 
-function module(config, stage) {
-  switch (stage) {
-    case `build-css`:
-      excludeFromLoader(`css`, config)
-      return config
-    case `build-javascript`:
-      excludeFromLoader(`css`, config)
+function module(args) {
+  const { stage, getConfig, actions } = args
 
-      // Exclusively extract Netlify CMS styles to /cms.css (filename configured
-      // above with plugin instantiation).
-      config.loader(`cms-css`, {
-        test: /\.css$/,
-        include: [/\/node_modules\/netlify-cms\//],
-        loader: extractCmsCss.extract([`css`]),
-      })
-      return config
-    default:
-      return config
+  if (stage === `build-css`) {
+    excludeFromLoader(args)
+  } else if (stage === `build-javascript`) {
+    excludeFromLoader(args)
+
+    // Exclusively extract Netlify CMS styles to /cms.css (filename configured
+    // above with plugin instantiation).
+
+    const prevConfig = getConfig()
+
+    actions.replaceWebpackConfig({
+      ...prevConfig,
+
+      module: {
+        ...prevConfig.module,
+
+        rules: [
+          ...prevConfig.module.rules,
+
+          {
+            test: /\.css$/,
+            include: [/\/node_modules\/netlify-cms\//],
+            loader: extractCmsCss.extract([`css`]),
+          },
+        ],
+      },
+    })
   }
 }
 
-exports.onCreateWebpackConfig = ({ actions, stage }, { modulePath }) => {
-  const config = actions.setWebpackConfig({
-    entry: {
-      cms: [`${__dirname}/cms.js`, modulePath].filter(p => p),
-    },
-    plugins: plugins(stage),
-  })
+exports.onCreateWebpackConfig = (args, { modulePath }) => {
+  const { actions, stage, getConfig } = args
 
-  module(config, stage)
+  if (stage === `develop` || stage === `build-javascript`) {
+    const prevConfig = getConfig()
+
+    actions.replaceWebpackConfig({
+      ...prevConfig,
+
+      entry: {
+        ...prevConfig.entry,
+
+        cms: [`${__dirname}/cms.js`, modulePath].filter(p => p),
+      },
+      plugins: [
+        ...prevConfig.plugins,
+
+        ...plugins(stage),
+      ],
+    })
+  }
+
+  module(args)
 }
