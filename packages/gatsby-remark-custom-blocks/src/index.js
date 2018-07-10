8ed4684845c1c55e8f934c1373f294618f1592b9
Michael McQuade <giraffesyo@gmail.com>
2018-07-10 11:14:09 -0400

    Use correctly formatted input to remark-custom-blocks

--- packages/gatsby-remark-custom-blocks/src/index.js
+++ packages/gatsby-remark-custom-blocks/src/index.js
@@ -6,5 +6,14 @@ module.exports.setParserPlugins = options => {
   if (isNil(options) || !has(options, `blocks`)) {
     throw Error(`missing required "blocks" option`)
   }
-  return [[remarkCustomBlocks, options.blocks]]
+  const remarkCustomFormattedBlocks = Object.entries(options.blocks).reduce(
+    (blocks, currentBlock) => {
+      return {
+        ...blocks,
+        [currentBlock[0]]: { classes: currentBlock[1] },
+      }
+    },
+    {}
+  )
+  return [[remarkCustomBlocks, remarkCustomFormattedBlocks]]
 }
