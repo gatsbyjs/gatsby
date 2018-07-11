34db11f0aecde47994cd31ddae6eb42b31eab15a
Kyle Mathews <mathews.kyle@gmail.com>
2018-07-10 17:15:57 -0700

    Remove another profile

--- packages/gatsby/src/redux/reducers/pages.js
+++ packages/gatsby/src/redux/reducers/pages.js
@@ -1,12 +1,10 @@
 const normalize = require(`normalize-path`)
-const profile = require(`../../utils/profile`)
 
 module.exports = (state = new Map(), action) => {
   switch (action.type) {
     case `DELETE_CACHE`:
       return new Map()
     case `CREATE_PAGE`: {
-      const start = process.hrtime()
       action.payload.component = normalize(action.payload.component)
       if (!action.plugin && !action.plugin.name) {
         console.log(``)
@@ -22,11 +20,6 @@ module.exports = (state = new Map(), action) => {
       action.payload.pluginCreatorId = action.plugin.id
 
       state.set(action.payload.path, action.payload)
-      profile({
-        start,
-        name: `reducers/pages`,
-        parent: `site createPages`,
-      })
       return state
     }
     case `DELETE_PAGE`: {
