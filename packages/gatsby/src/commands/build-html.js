64eb51173728b4e9dd324d8627ce5ddc3b17b76d
Kyle Mathews <mathews.kyle@gmail.com>
2018-07-12 15:16:07 -0700

    Add multi-process HTML rendering support to Gatsby

--- packages/gatsby/src/commands/build-html.js
+++ packages/gatsby/src/commands/build-html.js
@@ -6,7 +6,7 @@ const debug = require(`debug`)(`gatsby:html`)
 const webpackConfig = require(`../utils/webpack.config`)
 const { store } = require(`../redux`)
 const { createErrorFromString } = require(`gatsby-cli/lib/reporter/errors`)
-const renderHTML = require(`../utils/html-renderer`)
+const renderHTMLQueue = require(`../utils/html-renderer-queue`)
 
 module.exports = async (program: any, activity: any) => {
   const { directory } = program
@@ -41,7 +41,7 @@ module.exports = async (program: any, activity: any) => {
         )
       }
 
-      return renderHTML(require(outputFile), pages, activity)
+      return renderHTMLQueue(outputFile, pages, activity)
         .then(() => {
           // Remove the temp JS bundle file built for the static-site-generator-plugin
           try {
