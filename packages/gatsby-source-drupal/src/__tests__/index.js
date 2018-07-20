51ff80954568a874a284e9b854134666ceea077b
Michal Piechowiak <misiek.piechowiak@gmail.com>
2018-07-20 14:34:50 +0200

    [gatsby-source-drupal] relationship handling improvments (#5020)
    * [gatsby-source-drupal] add support for relationships with many values (arrays), fix back refs not showing for entities without any relationships, change backrefs to arrays as we can't ensure that backref will have single value
    
    Signed-off-by: Michal Piechowiak <misiek.piechowiak@gmail.com>
    
    * wip
    
    * finish tests
    
    * remove fixture that's not used anymore

--- packages/gatsby-source-drupal/src/__tests__/index.js
+++ packages/gatsby-source-drupal/src/__tests__/index.js
@@ -1,10 +1,101 @@
-const { nodeFromData } = require(`../normalize`)
-const { idOverlayExample } = require(`./data.json`)
+jest.mock(`axios`, () => {
+  return {
+    get: path => {
+      const last = path.split(`/`).pop()
+      try {
+        return { data: require(`./fixtures/${last}.json`) }
+      } catch (e) {
+        console.log(`Error`, e)
+        return null
+      }
+    },
+  }
+})
 
-describe(`node completion`, () => {
-  it(`should correctly generate node from jsonapi`, () => {
-    const node = nodeFromData(idOverlayExample, id => id)
-    expect(node.bundle).toEqual(idOverlayExample.attributes.bundle)
-    expect(node._attributes_id).toEqual(idOverlayExample.attributes.id)
+const { sourceNodes } = require(`../gatsby-node`)
+
+describe(`gatsby-source-drupal`, () => {
+  const nodes = {}
+  const createNodeId = id => `generated-id-${id}`
+
+  beforeAll(async () => {
+    const args = {
+      createNodeId,
+      actions: {
+        createNode: jest.fn(node => (nodes[node.id] = node)),
+      },
+    }
+
+    await sourceNodes(args, { baseUrl: `http://fixture` })
   })
+
+  it(`Generates nodes`, () => {
+    expect(Object.keys(nodes).length).not.toEqual(0)
+    expect(nodes[createNodeId(`file-1`)]).toBeDefined()
+    expect(nodes[createNodeId(`tag-1`)]).toBeDefined()
+    expect(nodes[createNodeId(`tag-2`)]).toBeDefined()
+    expect(nodes[createNodeId(`article-1`)]).toBeDefined()
+    expect(nodes[createNodeId(`article-2`)]).toBeDefined()
+    expect(nodes[createNodeId(`article-3`)]).toBeDefined()
+  })
+
+  it(`Nodes contain attributes data`, () => {
+    expect(nodes[createNodeId(`file-1`)].filename).toEqual(`main-image.png`)
+    expect(nodes[createNodeId(`article-2`)].title).toEqual(`Article #2`)
+    expect(nodes[createNodeId(`tag-1`)].langcode).toEqual(`en`)
+  })
+
+  it(`Preserves attributes.id`, () => {
+    expect(nodes[createNodeId(`article-2`)]._attributes_id).toEqual(22)
+  })
+
+  it(`Handles 1:1 relationship`, () => {
+    expect(
+      nodes[createNodeId(`article-1`)].relationships.field_main_image___NODE
+    ).not.toBeDefined()
+    expect(
+      nodes[createNodeId(`article-2`)].relationships.field_main_image___NODE
+    ).toEqual(createNodeId(`file-1`))
+    expect(
+      nodes[createNodeId(`article-3`)].relationships.field_main_image___NODE
+    ).toEqual(createNodeId(`file-1`))
+  })
+
+  it(`Handles 1:N relationship`, () => {
+    expect(
+      nodes[createNodeId(`article-1`)].relationships.field_tags___NODE
+    ).toEqual(
+      expect.arrayContaining([createNodeId(`tag-1`), createNodeId(`tag-2`)])
+    )
+    expect(
+      nodes[createNodeId(`article-2`)].relationships.field_tags___NODE
+    ).not.toBeDefined()
+    expect(
+      nodes[createNodeId(`article-3`)].relationships.field_tags___NODE
+    ).toEqual(expect.arrayContaining([createNodeId(`tag-1`)]))
+  })
+
+  it(`Creates back references`, () => {
+    expect(
+      nodes[createNodeId(`file-1`)].relationships[`node--article___NODE`]
+    ).toEqual(
+      expect.arrayContaining([
+        createNodeId(`article-2`),
+        createNodeId(`article-3`),
+      ])
+    )
+    expect(
+      nodes[createNodeId(`tag-1`)].relationships[`node--article___NODE`]
+    ).toEqual(
+      expect.arrayContaining([
+        createNodeId(`article-1`),
+        createNodeId(`article-3`),
+      ])
+    )
+    expect(
+      nodes[createNodeId(`tag-2`)].relationships[`node--article___NODE`]
+    ).toEqual(expect.arrayContaining([createNodeId(`article-1`)]))
+  })
+
+  // TO-DO: add tests for fetching files
 })
