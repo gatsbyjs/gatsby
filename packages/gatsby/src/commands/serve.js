ce9c3a8706fd729abee28d6762ff5da21872bb3a
trvsysadmin <32056612+trvsysadmin@users.noreply.github.com>
2018-07-20 14:58:41 +0800

    fix: Bumping Serve Up to 9.2.0 Broke Calls to Serve's API (#6598)
    * fix: Bumping Serve Up to 9.2.0 Broke Calls to Serve's API
    
    Follow Up on 69125ccb5604830f6b11996ff3f6dddbe6fe0260
    
    * fix: Linting Errors
    
    * fix: Linting Errors
    
    * feat: Add Back OpenURL for gatsby serve --open option

--- packages/gatsby/src/commands/serve.js
+++ packages/gatsby/src/commands/serve.js
@@ -1,14 +1,29 @@
 /* @flow weak */
-const serve = require(`serve`)
+const handler = require(`serve-handler`)
+const openurl = require(`opn`)
+const http = require(`http`)
 const signalExit = require(`signal-exit`)
 
 module.exports = program => {
-  let { port, open, directory } = program
+  let { port, open } = program
   port = typeof port === `string` ? parseInt(port, 10) : port
 
-  let server = serve(`${directory}/public`, { port, open })
+  let server = http.createServer((request, response) => handler(request, response, {
+      "public": `public`,
+    }),
+  )
+
+  server.listen(port, () => {
+    let openUrlString = `http://localhost:` + port
+    console.log(`gatsby serve running at:`, openUrlString)
+    if (open) {
+      let openUrlString = `http://localhost:` + port
+      console.log(`Opening browser...`)
+      openurl(openUrlString)
+    }
+  })
 
   signalExit((code, signal) => {
-    server.stop()
+    server.close()
   })
 }
