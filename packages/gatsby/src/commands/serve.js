5c88c9b115998abc13468e008d6cc54eb1f0c15b
thetlinthu <thet@traverse.ai>
2018-07-20 13:20:35 +0800

    feat: Add Back OpenURL for gatsby serve --open option

--- packages/gatsby/src/commands/serve.js
+++ packages/gatsby/src/commands/serve.js
@@ -1,10 +1,11 @@
 /* @flow weak */
 const handler = require(`serve-handler`)
+const openurl = require(`opn`)
 const http = require(`http`)
 const signalExit = require(`signal-exit`)
 
 module.exports = program => {
-  let { port } = program
+  let { port, open } = program
   port = typeof port === `string` ? parseInt(port, 10) : port
 
   let server = http.createServer((request, response) => handler(request, response, {
@@ -13,7 +14,13 @@ module.exports = program => {
   )
 
   server.listen(port, () => {
-    console.log(`gatsby serve running at port: `, port)
+    let openUrlString = `http://localhost:` + port
+    console.log(`gatsby serve running at:`, openUrlString)
+    if (open) {
+      let openUrlString = `http://localhost:` + port
+      console.log(`Opening browser...`)
+      openurl(openUrlString)
+    }
   })
 
   signalExit((code, signal) => {
