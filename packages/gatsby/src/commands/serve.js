5f54f1e6775b4cefe8392384a2e71db29c2fe09b
thetlinthu <thet@traverse.ai>
2018-07-20 12:43:47 +0800

    fix: Linting Errors

--- packages/gatsby/src/commands/serve.js
+++ packages/gatsby/src/commands/serve.js
@@ -1,6 +1,6 @@
 /* @flow weak */
-const handler = require('serve-handler');
-const http = require('http');
+const handler = require(`serve-handler`)
+const http = require(`http`)
 const signalExit = require(`signal-exit`)
 
 module.exports = program => {
@@ -9,15 +9,15 @@ module.exports = program => {
 
   let server = http.createServer((request, response) => {
     return handler(request, response, {
-      "public": "public"
-    });
-  });
+      "public": `public`,
+    })
+  })
 
   server.listen(port, () => {
-    console.log("gatsby serve running at port: ", port)
-  });
+    console.log(`gatsby serve running at port: `, port)
+  })
 
   signalExit((code, signal) => {
-    server.close();
+    server.close()
   })
 }
