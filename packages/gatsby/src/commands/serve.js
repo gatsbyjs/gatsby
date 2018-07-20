f83d18f08db5ad12311e8560e042e56093efa2b1
thetlinthu <thet@traverse.ai>
2018-07-20 12:21:00 +0800

    fix: Bumping Serve Up to 9.2.0 Broke Calls to Serve's API
    Follow Up on 69125ccb5604830f6b11996ff3f6dddbe6fe0260

--- packages/gatsby/src/commands/serve.js
+++ packages/gatsby/src/commands/serve.js
@@ -1,14 +1,23 @@
 /* @flow weak */
-const serve = require(`serve`)
+const handler = require('serve-handler');
+const http = require('http');
 const signalExit = require(`signal-exit`)
 
 module.exports = program => {
-  let { port, open, directory } = program
+  let { port } = program
   port = typeof port === `string` ? parseInt(port, 10) : port
 
-  let server = serve(`${directory}/public`, { port, open })
+  let server = http.createServer((request, response) => {
+    return handler(request, response, {
+      "public": "public"
+    });
+  });
+
+  server.listen(port, () => {
+    console.log("gatsby serve running at port: ", port)
+  });
 
   signalExit((code, signal) => {
-    server.stop()
+    server.close();
   })
 }
