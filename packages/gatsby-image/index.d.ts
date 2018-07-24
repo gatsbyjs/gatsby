3de2bd50508ebfd8a4f52ceeab857cfa4b493eef
Kalle Ott <kalle.ott@cap3.de>
2018-07-24 20:56:56 +0200

    update to typescript declarations of gatsby-image

--- packages/gatsby-image/index.d.ts
+++ packages/gatsby-image/index.d.ts
@@ -1,21 +1,25 @@
-import * as React from "react";
+import * as React from "react"
 
 interface GatsbyImageProps {
-  resolutions?: object;
-  sizes?: object;
-  fixed?: object;
-  fluid?: object;
-  fadeIn?: boolean;
-  title?: string;
-  alt?: string;
-  className?: string | object;
-  outerWrapperClassName?: string | object;
-  style?: object;
-  imgStyle?: object;
-  position?: string;
-  backgroundColor?: string | boolean;
-  onLoad?: (event: any) => void;
-  Tag?: string;
+  resolutions?: object
+  sizes?: object
+  fixed?: object
+  fluid?: object
+  fadeIn?: boolean
+  title?: string
+  alt?: string
+  className?: string | object
+  outerWrapperClassName?: string | object
+  style?: object
+  imgStyle?: object
+  position?: string
+  backgroundColor?: string | boolean
+  onLoad?: (event: any) => void
+  onStartLoad?: () => void
+  Tag?: string
 }
 
-export default class GatsbyImage extends React.Component<GatsbyImageProps, any> {}
+export default class GatsbyImage extends React.Component<
+  GatsbyImageProps,
+  any
+> {}
