af31085d608badad94b37347b38d866c85989efa
Avi Goldman <avrahamymgoldman@gmail.com>
2018-07-27 18:03:27 -0400

    added exerpt field

--- packages/gatsby-plugin-mdx/extend-node-type.js
+++ packages/gatsby-plugin-mdx/extend-node-type.js
@@ -5,8 +5,15 @@ const {
   GraphQLInt,
   GraphQLEnumType,
   GraphQLJSON
-} = require(`gatsby/graphql`);
+} = require("gatsby/graphql");
+const visit = require("unist-util-visit");
+const grayMatter = require("gray-matter");
+const { createMdxAstCompiler } = require("@mdx-js/mdx");
+const prune = require("underscore.string/prune");
 const mdx = require("./utils/mdx");
+const defaultOptions = require("./utils/default-options");
+
+const stripFrontmatter = source => grayMatter(source).content;
 
 module.exports = (
   { type, store, pathPrefix, getNode, getNodes, cache, reporter },
@@ -16,12 +23,16 @@ module.exports = (
     return {};
   }
 
-  pluginsCacheStr = pluginOptions.plugins.map(p => p.name).join(``);
-  pathPrefixCacheStr = pathPrefix || ``;
+  const options = defaultOptions(pluginOptions);
+  const compiler = createMdxAstCompiler(options);
 
   return new Promise((resolve, reject) => {
-    async function getCode(markdownNode) {
-      const code = await mdx(markdownNode.rawBody);
+    async function getAST(mdxNode) {
+      return compiler.parse(stripFrontmatter(mdxNode.rawBody));
+    }
+
+    async function getCode(mdxNode) {
+      const code = await mdx(mdxNode.rawBody);
       return `import React from 'react'
 import { MDXTag } from '@mdx-js/tag'
 
@@ -33,6 +44,31 @@ ${code}`;
         type: GraphQLString,
         resolve(markdownNode) {
           return getCode(markdownNode);
+        }
+      },
+      excerpt: {
+        type: GraphQLString,
+        args: {
+          pruneLength: {
+            type: GraphQLInt,
+            defaultValue: 140
+          }
+        },
+        async resolve(mdxNode, { pruneLength }) {
+          if (mdxNode.excerpt) {
+            return Promise.resolve(mdxNode.excerpt);
+          }
+          const ast = await getAST(mdxNode);
+
+          const excerptNodes = [];
+          visit(ast, node => {
+            if (node.type === "text" || node.type === "inlineCode") {
+              excerptNodes.push(node.value);
+            }
+            return;
+          });
+
+          return prune(excerptNodes.join(" "), pruneLength, "…");
         }
       }
     });
