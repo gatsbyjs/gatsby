ab58ef26e09f25c67a719117e3229df0b813d489
Avi Goldman <avrahamymgoldman@gmail.com>
2018-07-27 16:44:24 -0400

    broke out onCreateNode into its own file

--- packages/gatsby-plugin-mdx/gatsby-node.js
+++ packages/gatsby-plugin-mdx/gatsby-node.js
@@ -1,85 +1,26 @@
-const crypto = require("crypto");
 const path = require("path");
-const merge = require("lodash.merge");
 const escapeStringRegexp = require("escape-string-regexp");
+const defaultOptions = require("./utils/default-options");
 const mdx = require("./utils/mdx");
-const extractExports = require("./utils/extract-exports");
-
-const defaultExtensions = [".mdx"];
 
 /**
  * Create Mdx nodes from MDX files.
  */
-exports.onCreateNode = async function onCreateNode(
-  { node, getNode, loadNodeContent, actions, createNodeId },
-  pluginOptions
-) {
-  const extensions = pluginOptions.extensions || defaultExtensions;
-  const { createNode, createParentChildLink } = actions;
+exports.onCreateNode = require("./on-create-node");
 
-  // We only care about markdown content.
-  // replace with mediaType when mime-db is merged
-  //    node.internal.mediaType !== `text/mdx`
-  if (!extensions.includes(node.ext)) {
-    return;
-  }
-
-  const nodeContent = await loadNodeContent(node);
-  const code = await mdx(nodeContent);
-
-  // extract all the exports
-  const nodeExports = extractExports(code);
-
-  // grab the frontmatter
-  const classicFrontmatter = nodeExports._frontmatter || {};
-  const exportFrontmatter = nodeExports.frontmatter || {};
-
-  // // delete the frontmatter from the exports
-  delete nodeExports._frontmatter;
-  delete nodeExports.frontmatter;
-
-  const frontmatter = merge(classicFrontmatter, exportFrontmatter);
-
-  const mdxNode = {
-    id: createNodeId(`${node.id} >>> Mdx`),
-    children: [],
-    parent: node.id,
-    internal: {
-      content: nodeContent,
-      type: `Mdx`
-    },
-    frontmatter: {
-      title: ``, // always include a title
-      ...frontmatter,
-      _PARENT: node.id
-    },
-    exports: nodeExports,
-    rawBody: nodeContent
-  };
-
-  // Add path to the markdown file path
-  if (node.internal.type === `File`) {
-    mdxNode.fileAbsolutePath = node.absolutePath;
-    mdxNode.relativePath = node.relativePath;
-    mdxNode.fileNode = node;
-  }
-
-  mdxNode.internal.contentDigest = crypto
-    .createHash(`md5`)
-    .update(JSON.stringify(mdxNode))
-    .digest(`hex`);
-
-  createNode(mdxNode);
-  createParentChildLink({ parent: node, child: mdxNode });
-};
-
+/**
+ * Add additional fields to MDX nodes
+ */
 exports.setFieldsOnGraphQLNodeType = require("./extend-node-type");
 
+/**
+ * Add the webpack config for loading MDX files
+ */
 exports.onCreateWebpackConfig = (
   { stage, rules, loaders, plugins, actions },
   pluginOptions
 ) => {
-  const extensions = pluginOptions.extensions || defaultExtensions;
+  const { extensions } = defaultOptions(pluginOptions);
   const testPattern = new RegExp(
     extensions.map(ext => `${escapeStringRegexp(ext)}$`).join("|")
   );
@@ -112,7 +53,7 @@ exports.onCreateWebpackConfig = (
  * determines which files in the pages/ directory get built as pages.
  */
 exports.resolvableExtensions = (data, pluginOptions) =>
-  pluginOptions.extensions || defaultExtensions;
+  defaultOptions(pluginOptions).extensions;
 
 /**
  * Convert MDX to JSX so that Gatsby can extract the GraphQL queries.
@@ -121,11 +62,11 @@ exports.preprocessSource = async function preprocessSource(
   { filename, contents },
   pluginOptions
 ) {
-  const extensions = pluginOptions.extensions || defaultExtensions;
+  const { extensions, ...options } = defaultOptions(pluginOptions);
   const ext = path.extname(filename);
 
   if (extensions.includes(ext)) {
-    const code = await mdx(contents, pluginOptions);
+    const code = await mdx(contents, options);
     return code;
   }
   return null;
