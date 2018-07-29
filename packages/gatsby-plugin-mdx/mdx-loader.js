e23b413cd90042b4101324baa8b063c355a49d47
Christopher Biscardi <biscarch@sketcht.com>
2018-07-28 23:01:13 -0700

    begin debug instrumentation \n\n fix defaultLayout bug

--- packages/gatsby-plugin-mdx/mdx-loader.js
+++ packages/gatsby-plugin-mdx/mdx-loader.js
@@ -1,22 +1,27 @@
 const { getOptions } = require("loader-utils");
-const mdx = require('./utils/mdx')
+const mdx = require("./utils/mdx");
+const debug = require("debug")("gatsby-mdx:mdx-loader");
 
 const hasDefaultExport = str => /\nexport default/.test(str);
 
-module.exports = async function(source) {
+module.exports = async function(content) {
   const callback = this.async();
   const options = getOptions(this);
 
-  let code = await mdx(source, options);
-
-  if (!hasDefaultExport(code) && !!options.defaultLayout) {
+  let code = content;
+  // after running mdx, the code *always* has a default export, so this
+  // check needs to happen first.
+  if (!hasDefaultExport(content) && !!options.defaultLayout) {
+    debug("inserting default layout", options.defaultLayout);
     code = `import DefaultLayout from "${options.defaultLayout}"
 
 
 export default DefaultLayout
 
-${code}`;
+${content}`;
   }
+
+  code = await mdx(code, options);
 
   return callback(
     null,
