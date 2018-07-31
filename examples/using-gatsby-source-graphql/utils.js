023cb1e0175b24611f2daaace46a8edff0310ffb
Mikhail Novikov <freiksenet@gmail.com>
2018-07-31 14:06:16 +0300

    Fixed pre-release issues

--- examples/using-gatsby-source-graphql/utils.js
+++
@@ -1,7 +0,0 @@
-const dateformat = require(`dateformat`)
-
-exports.makeBlogPath = ({ id, createdAt, slug }) => {
-  const date = new Date(createdAt)
-  const formattedDate = dateformat(date, `yyyy-mm-dd`)
-  return `/${formattedDate}-${slug}`
-}
