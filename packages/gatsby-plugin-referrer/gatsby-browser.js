7a2ad51d63d57d1731adbc23e3ac7dc1ded1bab0
Michal Piechowiak <misiek.piechowiak@gmail.com>
2018-07-31 17:07:27 +0200

    update .gitignore to not track built code

--- packages/gatsby-plugin-referrer/gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-exports.onClientEntry = function () {
-  if (document.cookie.indexOf(`gatsbyOriginalReferrer`) < 0) {
-    // create cookie
-    document.cookie = `gatsbyOriginalReferrer=${document.referrer};path=/;max-age=31536000`;
-  }
-};
