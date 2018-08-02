54800eab5373e92ca6e4da3d9df8724d2ba431c8
Sriram Thiagarajan <sri.sjc@gmail.com>
2018-08-02 21:38:28 +0530

    combine imports from gatsby package

--- examples/gatsbygram/src/components/post.js
+++ examples/gatsbygram/src/components/post.js
@@ -1,9 +1,8 @@
 import * as PropTypes from "prop-types"
 import React from "react"
 import HeartIcon from "react-icons/lib/fa/heart"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 import Img from "gatsby-image"
-import { graphql } from "gatsby"
 
 import { rhythm, scale } from "../utils/typography"
 import presets from "../utils/presets"
