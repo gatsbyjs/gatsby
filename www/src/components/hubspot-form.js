93581e66942bc87b5bfba5861834ca103aed2c2e
Sam Bhagwat <calcsam@gmail.com>
2018-08-02 09:54:53 -0700

    Update hubspot-form.js

--- www/src/components/hubspot-form.js
+++ www/src/components/hubspot-form.js
@@ -3,12 +3,12 @@ import HubspotForm from "react-hubspot-form"
 
 export default class GatsbyHubspotForm extends Component {
   render() {
-    const { portalId, formId } = this.props
+    const { portalId, formId, sfdcCampaignId } = this.props
 
     // See https://designers.hubspot.com/docs/cos/hubspot-form-markup#styling-forms for information on how to style the form
     // Set css="" to disable HubSpot's default styles
     return (
-      <HubspotForm portalId={portalId} formId={formId} loading="Loading..." />
+      <HubspotForm portalId={portalId} formId={formId} sfdcCampaignId={sfdcCampaignId} loading="Loading..." />
     )
   }
 }
