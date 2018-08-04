cee482fbc271bea137cd8778a211b073f8ed412f
Kyle Mathews <mathews.kyle@gmail.com>
2018-08-04 10:23:07 -0700

    Upgrade client-only-paths example site

--- examples/client-only-paths/gatsby-node.js
+++ examples/client-only-paths/gatsby-node.js
@@ -1,17 +1,11 @@
-const Promise = require(`bluebird`)
-const path = require(`path`)
-
 // Implement the Gatsby API “onCreatePage”. This is
 // called after every page is created.
 exports.onCreatePage = ({ page, actions }) => {
   const { createPage } = actions
-  return new Promise((resolve, reject) => {
-    // Make the front page match everything client side.
-    // Normally your paths should be a bit more judicious.
-    if (page.path === `/`) {
-      page.matchPath = `/:path`
-      createPage(page)
-    }
-    resolve()
-  })
+  // Make the front page match everything client side.
+  // Normally your paths should be a bit more judicious.
+  if (page.path === `/`) {
+    page.matchPath = `/*`
+    createPage(page)
+  }
 }
