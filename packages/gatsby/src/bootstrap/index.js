6ce9b54d41f501b4b3860d353643d595eef7c1c6
Anton Harniakou <anton.harniakou@gmail.com>
2018-08-06 11:39:40 +0300

    Fix a bug with gatsby-browser.js not loading in production (#7011)
    * Fix a bug with gatsby-browser.js not loading in production
    
    * Update index.js

--- packages/gatsby/src/bootstrap/index.js
+++ packages/gatsby/src/bootstrap/index.js
@@ -216,6 +216,13 @@ module.exports = async (args: BootstrapArgs) => {
     if (env === `ssr` && plugin.skipSSR === true) return undefined
 
     const envAPIs = plugin[`${env}APIs`]
+
+    // Always include the site's gatsby-browser.js if it exists as it's
+    // a handy place to include global styles and other global imports.
+    if (env === `browser` && plugin.name === `default-site-plugin`) {
+      return slash(path.join(plugin.resolve, `gatsby-${env}`))
+    }
+
     if (envAPIs && Array.isArray(envAPIs) && envAPIs.length > 0) {
       return slash(path.join(plugin.resolve, `gatsby-${env}`))
     }
@@ -231,6 +238,7 @@ module.exports = async (args: BootstrapArgs) => {
     }),
     plugin => plugin.resolve
   )
+
   const browserPlugins = _.filter(
     flattenedPlugins.map(plugin => {
       return {
