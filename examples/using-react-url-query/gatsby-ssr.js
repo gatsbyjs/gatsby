2e06a4736065e98b999a6f79352ac9ba78b3316c
Mike Allanson <michael.allanson@gmail.com>
2018-08-06 21:57:33 +0100

    Remove using-react-url-query example

--- examples/using-react-url-query/gatsby-ssr.js
+++
@@ -1,8 +0,0 @@
-import createHistory from 'history/createMemoryHistory'
-import { configureUrlQuery } from 'react-url-query'
-
-const history = createHistory()
-
-configureUrlQuery({ history })
-
-exports.replaceHistory = () => history
