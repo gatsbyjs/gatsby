aaffdfdf22ef3999cba5bf3ac0e366aca9a2fc0e
Kyle Mathews <mathews.kyle@gmail.com>
2018-08-08 14:00:10 +0100

    Revert "fix reach router feeling slow (actually it's too fast and it cause problems!) (#7120)"
    This reverts commit 96cdb47706e6dcf47d376949e11bc5b1013a820d.

--- packages/gatsby/cache-dir/socketIo.js
+++ packages/gatsby/cache-dir/socketIo.js
@@ -29,7 +29,6 @@ export default function socketIo() {
                 [msg.payload.id]: msg.payload.result,
               }
             }
-            ___emitter.emit(msg.type, msg.payload)
           }
           if (msg.type === `pageQueryResult`) {
             if (didDataChange(msg, pageQueryData)) {
@@ -37,9 +36,9 @@ export default function socketIo() {
                 ...pageQueryData,
                 [msg.payload.id]: msg.payload.result,
               }
-              ___emitter.emit(msg.type, msg.payload)
             }
-          } else if (msg.type && msg.payload) {
+          }
+          if (msg.type && msg.payload) {
             ___emitter.emit(msg.type, msg.payload)
           }
         })
