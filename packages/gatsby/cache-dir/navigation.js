3f14787583b4d088cfff10f632a0ec162a85d7e3
Kyle Mathews <mathews.kyle@gmail.com>
2018-08-08 10:31:30 +0100

    Fix showing dev 404 page when there isn't one in development

--- packages/gatsby/cache-dir/navigation.js
+++ packages/gatsby/cache-dir/navigation.js
@@ -87,7 +87,15 @@ const navigate = (to, replace) => {
       // We fetch resources for 404 page in page-renderer.js. Calling it
       // here is to ensure that we have needed resouces to render page
       // before navigating to it
-      loader.getResourcesForPathname(`/404.html`, loaderCallback)
+      if (process.env.NODE_ENV !== `production`) {
+        // In development, the loading happens immediately so we
+        // can then navigate
+        loader.getResourcesForPathname(`/404.html`)
+        clearTimeout(timeoutId)
+        reachNavigate(to, { replace })
+      } else {
+        loader.getResourcesForPathname(`/404.html`, loaderCallback)
+      }
     } else {
       clearTimeout(timeoutId)
       reachNavigate(to, { replace })
