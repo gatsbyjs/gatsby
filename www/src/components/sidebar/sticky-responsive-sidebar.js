ab1f840815d84f86084a8f5d373452ae21199245
Florian Kissling <florian.kissling@gmail.com>
2018-08-09 21:11:50 +0200

    No need for React.Fragment anymore

--- www/src/components/sidebar/sticky-responsive-sidebar.js
+++ www/src/components/sidebar/sticky-responsive-sidebar.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react"
+import React, { Component } from "react"
 
 import Sidebar from "./sidebar"
 import ScrollSyncSidebar from "./scroll-sync-sidebar"
