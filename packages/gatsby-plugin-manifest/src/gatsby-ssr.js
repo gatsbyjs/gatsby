bd6164dae2e6f81768b456aac16f2caee6fc5aff
Asko Soukka <asko.soukka@iki.fi>
2018-08-12 07:50:30 +0300

    feat: generate apple-touch-icons for all generated icon sizes

--- packages/gatsby-plugin-manifest/src/gatsby-ssr.js
+++ packages/gatsby-plugin-manifest/src/gatsby-ssr.js
@@ -1,8 +1,16 @@
 import React from "react"
 import { withPrefix } from "gatsby"
+import { defaultIcons, doesIconExist } from "./common.js"
 
 exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
-  // If icons were generated, also add a favicon link.
+  const manifest = { ...pluginOptions }
+
+  // If icons are not manually defined, use the default icon set.
+  if (!manifest.icons) {
+    manifest.icons = defaultIcons
+  }
+
+  // If icons were generated, also add a favicon link and apple-touch-icons.
   if (pluginOptions.icon) {
     setHeadComponents([
       <link
@@ -11,6 +19,14 @@ exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
         href={withPrefix(`/icons/icon-48x48.png`)}
       />,
     ])
+    setHeadComponents(manifest.icons.map(icon => (
+      <link
+        key={`gatsby-plugin-manifest-apple-touch-icon-${icon.sizes}`}
+        rel="apple-touch-icon"
+        sizes={icon.sizes}
+        href={withPrefix(`/icons/icon-${icon.sizes}.png`)}
+      />
+    )))
   }
   setHeadComponents([
     <link
