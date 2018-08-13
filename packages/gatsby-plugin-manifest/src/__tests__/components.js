06e87adda5ce579f0a68c2a5dab3ae0dd52f4580
Asko Soukka <asko.soukka@iki.fi>
2018-08-13 20:00:40 +0300

    fix: favicon and apple-touch-icon generation to support hybrid mode configuration

--- packages/gatsby-plugin-manifest/src/__tests__/components.js
+++ packages/gatsby-plugin-manifest/src/__tests__/components.js
@@ -2,28 +2,46 @@ const { generateHeadComponents } = require(`../components`)
 
 describe(`gatsby-plugin-manifest`, () => {
   describe(`generateHeadComponents`, () => {
-    it(`creates head components`, () => {
-      const headComponents = [];
-      for (const component of generateHeadComponents(
-        path => path,
-        {
-          icon: true,
-          theme_color: `#000000`,
-          manifest: {}
-        }
-      )) { headComponents.push(component) }
+    it(`creates automatic mode head components`, () => {
+      const headComponents = []
+      for (const component of generateHeadComponents(path => path, {
+        icon: true,
+        theme_color: `#000000`,
+      })) {
+        headComponents.push(component)
+      }
       expect(headComponents).toMatchSnapshot()
     })
-    it(`creates prefixed head components`, () => {
-      const headComponents = [];
-      for (const component of generateHeadComponents(
-        path => `/prefix${path}`,
-        {
-          icon: true,
-          theme_color: `#000000`,
-          manifest: {}
-        }
-      )) { headComponents.push(component) }
+    it(`creates automatic mode head components prefixed`, () => {
+      const headComponents = []
+      for (const component of generateHeadComponents(path => `/prefix${path}`, {
+        icon: true,
+        theme_color: `#000000`,
+      })) {
+        headComponents.push(component)
+      }
+      expect(headComponents).toMatchSnapshot()
+    })
+    it(`creates hybrid mode head components`, () => {
+      const headComponents = []
+      for (const component of generateHeadComponents(path => path, {
+        icon: true,
+        theme_color: `#000000`,
+        icons: [
+          {
+            src: `/favicons/android-chrome-48x48.png`,
+            sizes: `48x48`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      })) {
+        headComponents.push(component)
+      }
       expect(headComponents).toMatchSnapshot()
     })
   })
--- packages/gatsby-plugin-manifest/src/components.js
+++ packages/gatsby-plugin-manifest/src/components.js
@@ -1,5 +1,7 @@
 import React from "react"
-import { defaultIcons, doesIconExist } from "./common.js"
+import { defaultIcons } from "./common.js"
+
+const normalize = s => `/${s.replace(/^\/+/g, "")}`
 
 exports.generateHeadComponents = function*(withPrefix, pluginOptions) {
   const manifest = { ...pluginOptions }
@@ -11,20 +13,22 @@ exports.generateHeadComponents = function*(withPrefix, pluginOptions) {
 
   // If icons were generated, also add a favicon link and apple-touch-icons.
   if (pluginOptions.icon) {
-    yield (
-      <link
-        key={`gatsby-plugin-manifest-icon-link`}
-        rel="shortcut icon"
-        href={withPrefix(`/icons/icon-48x48.png`)}
-      />
-    )
+    if (manifest.icons && manifest.icons.length) {
+      yield (
+        <link
+          key={`gatsby-plugin-manifest-icon-link`}
+          rel="shortcut icon"
+          href={withPrefix(`${normalize(manifest.icons[0].src)}`)}
+        />
+      )
+    }
     for (const icon of manifest.icons) {
       yield (
         <link
           key={`gatsby-plugin-manifest-apple-touch-icon-${icon.sizes}`}
           rel="apple-touch-icon"
           sizes={icon.sizes}
-          href={withPrefix(`/icons/icon-${icon.sizes}.png`)}
+          href={withPrefix(`${normalize(icon.src)}`)}
         />
       )
     }
