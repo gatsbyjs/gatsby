0a39eb7e48c091ae6dd17890946dc3e18c5390e8
Nicolas Goutay <phacks@users.noreply.github.com>
2018-08-14 15:53:29 +0000

    Add line numbers to prismjs plugin (#5821)
    * Add line numbers to prismjs plugin
    
    * Fix line numbers with line highlighting layout problems
    
    * Scope line numbering by code block, add start index
    
    * Fix line numbering problem in using-remark example
    
    * Add <gatsby-remark-prismjs>/add-line-numbers.js to gitignore
    
    * Fix oversized padding around code blocks
    
    * Run missed tests and add a few more
    
    * Make it easier to read generated code string
    
    * Use plain old string concatenation instead
    
    * Add one more snapshot test
    
    * Add another example
    
    * Skip formatting for this block

--- packages/gatsby-remark-prismjs/src/__tests__/parse-line-number-range.js
+++ packages/gatsby-remark-prismjs/src/__tests__/parse-line-number-range.js
@@ -39,6 +39,67 @@ describe(`parses numeric ranges from the languages markdown code directive`, () 
     ])
   })
 
+  describe(`parses line numbering options from the languages markdown code directive`, () => {
+    it(`parses the right line number start index from the languages variable`, () => {
+      expect(parseLineNumberRange(`jsx{numberLines: true}`).numberLines).toEqual(true)
+      expect(parseLineNumberRange(`jsx{numberLines: true}`).numberLinesStartAt).toEqual(1)
+      expect(parseLineNumberRange(`jsx{numberLines: 3}`).numberLines).toEqual(true)
+      expect(parseLineNumberRange(`jsx{numberLines: 3}`).numberLinesStartAt).toEqual(3)
+    })
+
+    it(`parses the right line number start index without a specified language`, () => {
+      expect(parseLineNumberRange(`{numberLines: true}`).numberLines).toEqual(true)
+      expect(parseLineNumberRange(`{numberLines: true}`).numberLinesStartAt).toEqual(1)
+      expect(parseLineNumberRange(`{numberLines: 3}`).numberLines).toEqual(true)
+      expect(parseLineNumberRange(`{numberLines: 3}`).numberLinesStartAt).toEqual(3)
+    })
+
+    it(`ignores non-true or non-number values`, () => {
+      expect(parseLineNumberRange(`jsx{numberLines: false}`).numberLines).toEqual(false)
+      expect(parseLineNumberRange(`jsx{numberLines: NaN}`).numberLines).toEqual(false)
+    })
+
+    it(`casts decimals line number start into the nearest lower integer`, () => {
+    expect(parseLineNumberRange(`jsx{numberLines: 1.2}`).numberLinesStartAt).toEqual(1)
+    expect(parseLineNumberRange(`jsx{numberLines: 1.8}`).numberLinesStartAt).toEqual(1)
+    })
+  })
+
+  describe(`parses both line numbering and line highlighting options`, () => {
+    it(`one line highlighted`, () => {
+      expect(parseLineNumberRange(`jsx{1}{numberLines: 3}`)).toEqual({
+        splitLanguage: `jsx`,
+        highlightLines: [1],
+        numberLines: true,
+        numberLinesStartAt: 3,
+      })
+    })
+    it(`multiple lines highlighted`, () => {
+      expect(parseLineNumberRange(`jsx{1,5,7-8}{numberLines: 3}`)).toEqual({
+        splitLanguage: `jsx`,
+        highlightLines: [1,5,7,8],
+        numberLines: true,
+        numberLinesStartAt: 3,
+      })
+    })
+    it(`numberLines: true`, () => {
+      expect(parseLineNumberRange(`jsx{1,5,7-8}{numberLines: true}`)).toEqual({
+        splitLanguage: `jsx`,
+        highlightLines: [1,5,7,8],
+        numberLines: true,
+        numberLinesStartAt: 1,
+      })
+    })
+    it(`reverse ordering`, () => {
+      expect(parseLineNumberRange(`jsx{numberLines: 4}{2}`)).toEqual({
+        splitLanguage: `jsx`,
+        highlightLines: [2],
+        numberLines: true,
+        numberLinesStartAt: 4,
+      })
+    })
+  })
+
   it(`handles bad inputs`, () => {
     expect(parseLineNumberRange(`jsx{-1`).highlightLines).toEqual([])
     expect(parseLineNumberRange(`jsx{-1....`).highlightLines).toEqual([])
--- packages/gatsby-remark-prismjs/src/index.js
+++ packages/gatsby-remark-prismjs/src/index.js
@@ -2,6 +2,7 @@ const visit = require(`unist-util-visit`)
 
 const parseLineNumberRange = require(`./parse-line-number-range`)
 const highlightCode = require(`./highlight-code`)
+const addLineNumbers = require(`./add-line-numbers`)
 
 module.exports = (
   { markdownAST },
@@ -14,7 +15,12 @@ module.exports = (
 
   visit(markdownAST, `code`, node => {
     let language = node.lang
-    let { splitLanguage, highlightLines } = parseLineNumberRange(language)
+    let {
+      splitLanguage,
+      highlightLines,
+      numberLines,
+      numberLinesStartAt,
+    } = parseLineNumberRange(language)
     language = splitLanguage
 
     // PrismJS's theme styles are targeting pre[class*="language-"]
@@ -24,9 +30,7 @@ module.exports = (
     //
     // @see https://github.com/PrismJS/prism/blob/1d5047df37aacc900f8270b1c6215028f6988eb1/themes/prism.css#L49-L54
     let languageName = `text`
-    if (language) {
-      languageName = normalizeLanguage(language)
-    }
+    if (language) { languageName = normalizeLanguage(language) }
 
     // Allow users to specify a custom class prefix to avoid breaking
     // line highlights if Prism is required by any other code.
@@ -35,17 +39,28 @@ module.exports = (
     // @see https://github.com/gatsbyjs/gatsby/issues/1486
     const className = `${classPrefix}${languageName}`
 
+    let numLinesStyle, numLinesClass, numLinesNumber
+    numLinesStyle = numLinesClass = numLinesNumber = ``
+    if (numberLines) {
+      numLinesStyle = ` style="counter-reset: linenumber ${numberLinesStartAt - 1}"`
+      numLinesClass = ` line-numbers`
+      numLinesNumber = addLineNumbers(node.value)
+    }
+
     // Replace the node with the markup we need to make
     // 100% width highlighted code lines work
     node.type = `html`
-    node.value = `<div class="gatsby-highlight" data-language="${languageName}">
-      <pre class="${className}"><code class="${className}">${highlightCode(
-      language,
-      node.value,
-      highlightLines
-    )}</code></pre>
-      </div>`
-  })
+    // prettier-ignore
+    node.value = ``
+    + `<div class="gatsby-highlight" data-language="${languageName}">`
+    +   `<pre${numLinesStyle} class="${className}${numLinesClass}">`
+    +     `<code class="${className}">`
+    +       `${highlightCode(language, node.value, highlightLines)}`
+    +     `</code>`
+    +     `${numLinesNumber}`
+    +   `</pre>`
+    + `</div>`
+    })
 
   visit(markdownAST, `inlineCode`, node => {
     let languageName = `text`
@@ -61,9 +76,6 @@ module.exports = (
     const className = `${classPrefix}${languageName}`
 
     node.type = `html`
-    node.value = `<code class="${className}">${highlightCode(
-      languageName,
-      node.value
-    )}</code>`
+    node.value = `<code class="${className}">${highlightCode(languageName, node.value)}</code>`
   })
 }
