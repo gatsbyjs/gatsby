15ee19294647dbcbb7861507d5d7ccb80211645f
David Bailey <david.j.b@vivaldi.net>
2018-08-15 17:44:54 +0100

    Bump gatsby-plugin-offline version and update docs

--- packages/gatsby/cache-dir/page-renderer.js
+++ packages/gatsby/cache-dir/page-renderer.js
@@ -120,6 +120,13 @@ class PageRenderer extends React.Component {
 
   render() {
     if (!(this.state.pageResources && this.state.pageResources.json)) {
+      // Try to load the page directly.
+      //
+      // Usually the page either doesn't exist, resulting in a 404 error, or
+      // isn't available offline. Appending the query ensures the correct error
+      // message is displayed, or if the page is available online, it will
+      // load properly.
+
       if (window.location.search) {
         window.location.search += `&no-cache=1`
       } else {
