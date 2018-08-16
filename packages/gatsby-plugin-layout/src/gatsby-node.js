77cba0c865cc165638febded8feeafd1f14acf84
Michal Piechowiak <misiek.piechowiak@gmail.com>
2018-08-16 02:12:38 +0200

    [gatsby-plugin-layout] default to `src/layouts/index.js`

--- packages/gatsby-plugin-layout/src/gatsby-node.js
+++ packages/gatsby-plugin-layout/src/gatsby-node.js
@@ -1,26 +1,47 @@
 const fs = require(`fs`)
+const path = require(`path`)
 
 let didRunAlready = false
+let absoluteComponentPath
 
-exports.onPreInit = (_, { component }) => {
-  if (didRunAlready) {
-    throw new Error(`You can only have single instance of gatsby-plugin-layout`)
-  } else if (!component) {
-    throw new Error(`Component option is required`)
-  } else if (!fs.existsSync(component)) {
-    throw new Error(
-      `Path to component isn't valid. Please use require.resolve with path relative to gatsby-config`
+exports.onPreInit = ({ store }, { component }) => {
+  let userSpecifiedPath = true
+  const defaultLayoutComponentPath = `src/layouts/index.js`
+  if (!component) {
+    // Default to `src/layouts/index.js` for drop-in replacement of v1 layouts
+    component = path.join(
+      store.getState().program.directory,
+      defaultLayoutComponentPath
     )
+    userSpecifiedPath = false
+  }
+
+  if (didRunAlready) {
+    throw new Error(
+      `You can only have single instance of gatsby-plugin-layout in your gatsby-config.js`
+    )
+  } else if (!fs.existsSync(component)) {
+    if (userSpecifiedPath) {
+      throw new Error(
+        `Couldn't find layout component at specified path: "${component}".`
+      )
+    } else {
+      throw new Error(
+        `Couldn't find layout component at "${defaultLayoutComponentPath}.\n\n` +
+          `Please create layout component in that location or specify path to layout component in gatsby-config.js`
+      )
+    }
   }
 
   didRunAlready = true
+  absoluteComponentPath = component
 }
 
-exports.onCreateWebpackConfig = ({ actions, plugins }, { component }) => {
+exports.onCreateWebpackConfig = ({ actions, plugins }) => {
   actions.setWebpackConfig({
     plugins: [
       plugins.define({
-        GATSBY_LAYOUT_COMPONENT_PATH: JSON.stringify(component),
+        GATSBY_LAYOUT_COMPONENT_PATH: JSON.stringify(absoluteComponentPath),
       }),
     ],
   })
