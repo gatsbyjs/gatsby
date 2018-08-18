e982c83b0e5427931438bf6bb07046f384c4d086
Avi Goldman <avrahamymgoldman@gmail.com>
2018-08-18 15:46:49 -0700

    Frontmatter as Page Context (#56)
    Use frontmatter in page-plugin context so people can make queries based on it

--- packages/gatsby-plugin-mdx/gatsby-node.js
+++ packages/gatsby-plugin-mdx/gatsby-node.js
@@ -1,6 +1,9 @@
 const path = require("path");
+const fs = require("fs-extra");
+const merge = require("lodash/merge");
 const escapeStringRegexp = require("escape-string-regexp");
 const defaultOptions = require("./utils/default-options");
+const extractExports = require("./utils/extract-exports");
 const mdx = require("./utils/mdx");
 
 /**
@@ -12,6 +15,37 @@ exports.onCreateNode = require("./on-create-node");
  * Add additional fields to MDX nodes
  */
 exports.setFieldsOnGraphQLNodeType = require("./extend-node-type");
+
+/**
+ * Add frontmatter as page context for MDX pages
+ */
+exports.onCreatePage = async ({ page, actions }, pluginOptions) => {
+  const { createPage, deletePage } = actions;
+  const { extensions, ...options } = defaultOptions(pluginOptions);
+  const ext = path.extname(page.component);
+
+  if (extensions.includes(ext)) {
+    const content = await fs.readFile(page.component, "utf8");
+    const code = await mdx(content, options);
+
+    // grab the exported frontmatter
+    const { frontmatter } = extractExports(code);
+
+    deletePage(page);
+    createPage(
+      merge(
+        {
+          context: {
+            frontmatter: {
+              ...frontmatter
+            }
+          }
+        },
+        page
+      )
+    );
+  }
+};
 
 /**
  * Add the webpack config for loading MDX files
