5a633fff2abaa36dd6300c5a6fe88cafccfb4cdf
Resi Respati <resir014@gmail.com>
2018-08-21 02:39:47 +0700

    [gatsby-plugin-typescript] load TS preset via onCreateBabelConfig (#7417)
    * [gatsby-plugin-typescript] load TS preset via onCreateBabelConfig
    
    * fixes

--- packages/gatsby-plugin-typescript/src/gatsby-node.js
+++ packages/gatsby-plugin-typescript/src/gatsby-node.js
@@ -1,37 +1,10 @@
-const { tsPresetsFromJsPresets } = require(`./`)
-
 const resolvableExtensions = () => [`.ts`, `.tsx`]
 
-function onCreateWebpackConfig({ actions, loaders, stage }) {
-  const jsLoader = loaders.js()
-  if (
-    !(
-      jsLoader &&
-      jsLoader.loader &&
-      jsLoader.options &&
-      jsLoader.options.presets
-    )
-  ) {
-    return
-  }
-  actions.setWebpackConfig({
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: [
-            {
-              loader: jsLoader.loader,
-              options: {
-                ...jsLoader.options,
-                presets: tsPresetsFromJsPresets(jsLoader.options.presets),
-              },
-            },
-          ],
-        },
-      ],
-    },
+function onCreateBabelConfig({ actions }, pluginOptions) {
+  actions.setBabelPreset({
+    name: `@babel/preset-typescript`,
   })
 }
-exports.onCreateWebpackConfig = onCreateWebpackConfig
+
 exports.resolvableExtensions = resolvableExtensions
+exports.onCreateBabelConfig = onCreateBabelConfig
