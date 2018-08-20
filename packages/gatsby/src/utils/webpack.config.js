e0a2442a7b7bb9ed12b7725b16275c8065ac3250
Michal Piechowiak <misiek.piechowiak@gmail.com>
2018-08-20 12:35:56 -0700

    add relative path to resolve.modules

--- packages/gatsby/src/utils/webpack.config.js
+++ packages/gatsby/src/utils/webpack.config.js
@@ -195,15 +195,17 @@ module.exports = async (
         break
       case `build-javascript`: {
         // Minify Javascript only if needed.
-        configPlugins = program.noUglify ? configPlugins : configPlugins.concat([
-          plugins.uglify({
-            uglifyOptions: {
-              compress: {
-                drop_console: false,
-              },
-            },
-          }),
-        ])
+        configPlugins = program.noUglify
+          ? configPlugins
+          : configPlugins.concat([
+              plugins.uglify({
+                uglifyOptions: {
+                  compress: {
+                    drop_console: false,
+                  },
+                },
+              }),
+            ])
         configPlugins = configPlugins.concat([
           plugins.extractText(),
           // Write out stats object mapping named dynamic imports (aka page
@@ -357,6 +359,9 @@ module.exports = async (
       modules: [
         directoryPath(path.join(`src`, `node_modules`)),
         `node_modules`,
+        // This is head scratching - without it css modules in production will fail
+        // to find module with relative path
+        `./`,
       ],
       alias: {
         gatsby$: directoryPath(path.join(`.cache`, `gatsby-browser-entry.js`)),
