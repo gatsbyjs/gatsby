5d29186847b633e488806511722f35a1d7d62971
thebigredgeek <rhyneandrew@gmail.com>
2018-08-20 11:23:29 -0700

    fixes

--- packages/gatsby-plugin-typescript/src/gatsby-node.js
+++ packages/gatsby-plugin-typescript/src/gatsby-node.js
@@ -1,5 +1,3 @@
-// const { tsPresetsFromJsPresets } = require(`./`)
-
 const resolvableExtensions = () => [`.ts`, `.tsx`]
 
 function onCreateBabelConfig({ actions }, pluginOptions) {
@@ -8,32 +6,5 @@ function onCreateBabelConfig({ actions }, pluginOptions) {
   })
 }
 
-function onCreateWebpackConfig({ actions, loaders, stage }) {
-  const jsLoader = loaders.js()
-  if (
-    !(
-      jsLoader &&
-      jsLoader.loader
-    )
-  ) {
-    return
-  }
-  actions.setWebpackConfig({
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: [
-            {
-              loader: jsLoader.loader,
-            },
-          ],
-        },
-      ],
-    },
-  })
-}
-
-exports.onCreateWebpackConfig = onCreateWebpackConfig
 exports.resolvableExtensions = resolvableExtensions
 exports.onCreateBabelConfig = onCreateBabelConfig
