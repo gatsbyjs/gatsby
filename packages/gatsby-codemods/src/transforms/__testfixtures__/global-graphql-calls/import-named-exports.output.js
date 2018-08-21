b280ffe0e45f40e621b7d6b4134c9dc6ca0a8d49
Kyle Mathews <mathews.kyle@gmail.com>
2018-08-20 17:36:42 -0700

    Fix test broken earlier

--- packages/gatsby-codemods/src/transforms/__testfixtures__/global-graphql-calls/import-named-exports.output.js
+++ packages/gatsby-codemods/src/transforms/__testfixtures__/global-graphql-calls/import-named-exports.output.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import { Link, graphql } from "gatsby"
+import { Link, graphql } from "gatsby";
 
 export const query = graphql`
   query {
@@ -7,4 +7,4 @@ export const query = graphql`
       prefix
     }
   }
-`
+`;
