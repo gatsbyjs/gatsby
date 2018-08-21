e6944bc6de2b3a6e91e7a4456a629870ef573305
Resi Respati <resir014@gmail.com>
2018-08-22 01:51:08 +0700

    [gatsby-plugin-typescript] bring back onCreateWebpackConfig (#7525)
    Turns out we still need onCreateWebpackConfig for Webpack to *actually*
    parse files other than JavaScript, so... yeah.

--- packages/gatsby-plugin-typescript/src/gatsby-node.js
+++ packages/gatsby-plugin-typescript/src/gatsby-node.js
@@ -6,5 +6,25 @@ function onCreateBabelConfig({ actions }, pluginOptions) {
   })
 }
 
+function onCreateWebpackConfig({ actions, loaders }) {
+  const jsLoader = loaders.js()
+
+  if (!jsLoader) {
+    return
+  }
+
+  actions.setWebpackConfig({
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: jsLoader,
+        },
+      ],
+    },
+  })
+}
+
 exports.resolvableExtensions = resolvableExtensions
 exports.onCreateBabelConfig = onCreateBabelConfig
+exports.onCreateWebpackConfig = onCreateWebpackConfig
