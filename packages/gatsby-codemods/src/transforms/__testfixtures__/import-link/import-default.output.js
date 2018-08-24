c3d76c8f839494d898e2991dde4656e4605609c8
Sriram Thiagarajan <sri.sjc@gmail.com>
2018-08-24 22:13:08 +0530

    codemod for gatsby-link import

---
+++ packages/gatsby-codemods/src/transforms/__testfixtures__/import-link/import-default.output.js
@@ -0,0 +1,1 @@
+import { Link } from "gatsby";
---
+++ packages/gatsby-codemods/src/transforms/import-link.js
@@ -0,0 +1,154 @@
+const MODULE_NAME = `gatsby`
+const IMPORT_NAME = `Link`
+
+function findGatsbyRequire(root, j) {
+  const requires = root.find(j.VariableDeclarator, {
+    init: {
+      callee: { name: `require` },
+    },
+  })
+
+  let string = requires.find(j.VariableDeclarator, {
+    init: { arguments: [{ value: MODULE_NAME }] },
+  })
+
+  if (string.length) return string
+  // require(`gatsby`)
+  return requires.filter(path => {
+    let template = path.get(`init`, `arguments`, 0, `quasis`, 0).node
+    return !!template && template.value.raw === MODULE_NAME
+  })
+}
+
+function addEsmImport(j, root) {
+  let existingImport = root.find(j.ImportDeclaration, {
+    source: { value: `gatsby` },
+  })
+
+  if (
+    existingImport.find(j.ImportSpecifier, { imported: { name: IMPORT_NAME } })
+      .length
+  )
+    return // already exists
+
+  if (!existingImport.length) {
+    root
+      .find(j.Program)
+      .get(`body`, 0)
+      .insertBefore(
+        j.importDeclaration(
+          [j.importSpecifier(j.identifier(IMPORT_NAME))],
+          j.literal(MODULE_NAME)
+        )
+      )
+    return
+  }
+
+  const namespace = existingImport.find(j.ImportNamespaceSpecifier)
+
+  if (namespace.length) {
+    let { name } = namespace.get(0).node.local
+    root
+      .find(j.JSXIdentifier, { name: IMPORT_NAME })
+      .replaceWith(({ node }) => {
+        node.name = `${name}.${IMPORT_NAME}`
+        return node
+      })
+    return
+  }
+  let { specifiers } = existingImport.get(0).node
+  existingImport
+    .get(`specifiers`, specifiers.length - 1)
+    .insertAfter(j.importSpecifier(j.identifier(IMPORT_NAME)))
+}
+
+function removeGatsbyLinkEsmImport(j, root) {
+  root.find(j.ImportDeclaration).forEach(path => {
+    if (path.value.source.value === `gatsby-link`) {
+      j(path).remove()
+    }
+  })
+}
+
+function removeGatsbyLinkRequire(j, root) {
+  root
+    .find(j.VariableDeclarator, {
+      init: {
+        callee: { name: `require` },
+      },
+    })
+    .forEach(path => {
+      let template = path.get(`init`, `arguments`, 0, `value`)
+      if (template.value === `gatsby-link`) {
+        j(path).remove()
+        return
+      }
+
+      template = path.get(`init`, `arguments`, 0, `quasis`, 0).node
+      if (template.value.raw === `gatsby-link`) {
+        j(path).remove()
+      }
+    })
+}
+
+function addRequire(j, root) {
+  let existingImport = findGatsbyRequire(root, j)
+
+  if (existingImport.find(j.Identifier, { value: IMPORT_NAME }).length) return // already exists
+
+  if (!existingImport.length) {
+    root
+      .find(j.Program)
+      .get(`body`, 0)
+      .insertBefore(
+        j.template.statement([
+          `const { ${IMPORT_NAME} } = require('${MODULE_NAME}');\n`,
+        ])
+      )
+    return
+  }
+
+  const pattern = existingImport.find(j.ObjectPattern)
+
+  if (!pattern.length) {
+    let { name } = existingImport.get(`id`).node
+    root
+      .find(j.JSXIdentifier, { name: IMPORT_NAME })
+      .replaceWith(({ node }) => {
+        node.name = `${name}.${IMPORT_NAME}`
+        return node
+      })
+    return
+  }
+  let { properties } = pattern.get(0).node
+  let property = j.objectProperty(
+    j.identifier(IMPORT_NAME),
+    j.identifier(IMPORT_NAME)
+  )
+  property.shorthand = true
+  pattern.get(`properties`, properties.length - 1).insertAfter(property)
+}
+
+module.exports = (file, api, options) => {
+  const j = api.jscodeshift
+  const root = j(file.source)
+
+  const links = root.find(j.Identifier, {
+    name: IMPORT_NAME,
+  })
+
+  if (!links.length) return false
+
+  const useImportSyntax =
+    root.find(j.ImportDeclaration, { importKind: `value` }).length > 0
+
+  if (useImportSyntax) {
+    addEsmImport(j, root)
+    removeGatsbyLinkEsmImport(j, root)
+  } else {
+    addRequire(j, root)
+    removeGatsbyLinkRequire(j, root)
+  }
+
+  return root.toSource()
+}
