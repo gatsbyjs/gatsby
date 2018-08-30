0be1844fb1e1f79d80a83e66a5c2988e29fef7fb
Jonatas Miguel <jmiguel@chegg.com>
2018-08-30 15:36:56 -0400

    Extract `a` tag selection for easier testing. anchor -> targetAnchor

--- packages/gatsby-plugin-catch-links/src/catch-links.js
+++ packages/gatsby-plugin-catch-links/src/catch-links.js
@@ -9,48 +9,56 @@ export const userIsForcingNavigation = event => (
   event.defaultPrevented
 )
 
+export const findClosestAnchor = event => {
+  for (
+    var node = event.target; 
+    node.parentNode; 
+    node = node.parentNode
+  ) {
+    if (node.nodeName.toLowerCase() === `a`) {
+      return node
+    }
+  }
+
+  return null
+}
+
 module.exports = function(root, cb) {
   root.addEventListener(`click`, function(ev) {
     if ( userIsForcingNavigation(ev) ) return true
 
-    var anchor = null
-    for (var n = ev.target; n.parentNode; n = n.parentNode) {
-      if (n.nodeName === `A`) {
-        anchor = n
-        break
-      }
-    }
-    if (!anchor) return true
+    const targetAnchor = findClosestAnchor(ev)
+    if (targetAnchor == null) return true
 
     // Don't catch links where a target (other than self) is set
     // e.g. _blank.
-    if (anchor.target && anchor.target.toLowerCase() !== `_self`) return true
+    if (targetAnchor.target && targetAnchor.target.toLowerCase() !== `_self`) return true
 
     // Don't catch links pointed to the same page but with a hash.
-    if (anchor.pathname === window.location.pathname && anchor.hash !== ``) {
+    if (targetAnchor.pathname === window.location.pathname && targetAnchor.hash !== ``) {
       return true
     }
 
     // Dynamically created anchor links (href="#my-anchor") do not always have pathname on IE
-    if (anchor.pathname === ``) {
+    if (targetAnchor.pathname === ``) {
       return true
     }
 
     // Don't catch links pointed at what look like file extensions (other than
     // .htm/html extensions).
-    if (anchor.pathname.search(/^.*\.((?!htm)[a-z0-9]{1,5})$/i) !== -1) {
+    if (targetAnchor.pathname.search(/^.*\.((?!htm)[a-z0-9]{1,5})$/i) !== -1) {
       return true
     }
 
     // IE clears the host value if the anchor href changed after creation, e.g.
     // in React. Creating a new anchor element to ensure host value is present
     var a1 = document.createElement(`a`)
-    a1.href = anchor.href
+    a1.href = targetAnchor.href
 
     // In IE, the default port is included in the anchor host but excluded from
     // the location host.  This affects the ability to directly compare
     // location host to anchor host.  For example: http://example.com would
-    // have a location.host of 'example.com' and an anchor.host of
+    // have a location.host of 'example.com' and an targetAnchor.host of
     // 'example.com:80' Creating anchor from the location.href to normalize the
     // host value.
     var a2 = document.createElement(`a`)
@@ -75,7 +83,7 @@ module.exports = function(root, cb) {
 
     ev.preventDefault()
 
-    cb(anchor.getAttribute(`href`))
+    cb(targetAnchor.getAttribute(`href`))
     return false
   })
 }
