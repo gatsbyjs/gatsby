eb1c1359c6b8cfa518438e07794c8c3b72f77654
Dustin Schau <DSchau@users.noreply.github.com>
2018-09-05 09:33:34 -0500

    docs: add a title to code snippets in the first part of the tutorial (#7867)
    * docs: add a title to code snippets in the first part of the tutorial
    
    * style: run prettier
    
    * refactor: use established constants for some values instead of hard
    coding
    
    * style: use scale for font size
    
    * refactor: co-locate gatsby code highlight style with other code style

--- www/gatsby-config.js
+++ www/gatsby-config.js
@@ -55,6 +55,7 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
+          `gatsby-remark-code-titles`,
           {
             resolve: `gatsby-remark-images`,
             options: {
