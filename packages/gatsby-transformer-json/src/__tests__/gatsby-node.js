51c4ddd0bc785c8ae767ace039e5c8b3de63bcae
PoTa <pota@mosfet.hu>
2018-09-06 07:46:13 +0200

    add typeName option to gatsby-transformer-json

--- packages/gatsby-transformer-json/src/__tests__/gatsby-node.js
+++ packages/gatsby-transformer-json/src/__tests__/gatsby-node.js
@@ -67,4 +67,65 @@ describe(`Process JSON nodes correctly`, () => {
       expect(createParentChildLink).toHaveBeenCalledTimes(1)
     })
   })
+
+  it(`correctly sets node type for array of objects`, async () => {
+    ;[
+      [null, [`NodeNameJson`, `NodeNameJson`]],
+      [`fixed`, [`fixed`, `fixed`]],
+      [((node, obj) => obj.funny), [`yup`, `nope`]],
+    ].forEach(async ([typeName, [expectedOne, expectedTwo]]) => {
+      const data = [
+        { id: `foo`, blue: true, funny: `yup` },
+        { blue: false, funny: `nope` },
+      ]
+      node.content = JSON.stringify(data)
+
+      const createNode = jest.fn()
+      const createParentChildLink = jest.fn()
+      const actions = { createNode, createParentChildLink }
+      const createNodeId = jest.fn()
+      createNodeId.mockReturnValue(`uuid-from-gatsby`)
+
+      await onCreateNode({
+        node,
+        loadNodeContent,
+        actions,
+        createNodeId,
+      }, {
+        typeName,
+      }).then(() => {
+        expect(createNode.mock.calls[0][0].internal.type).toEqual(expectedOne)
+        expect(createNode.mock.calls[1][0].internal.type).toEqual(expectedTwo)
+      })
+    })
+  })
+
+  it(`correctly sets node type for single object`, async () => {
+    ;[
+      [null, `FooJson`],
+      [`fixed`, `fixed`],
+      [((node, obj) => obj.funny), `yup`],
+    ].forEach(async ([typeName, expected]) => {
+      const data = { id: `foo`, blue: true, funny: `yup` }
+      node.content = JSON.stringify(data)
+      node.dir = `/tmp/foo/`
+
+      const createNode = jest.fn()
+      const createParentChildLink = jest.fn()
+      const actions = { createNode, createParentChildLink }
+      const createNodeId = jest.fn()
+      createNodeId.mockReturnValue(`uuid-from-gatsby`)
+
+      await onCreateNode({
+        node,
+        loadNodeContent,
+        actions,
+        createNodeId,
+      }, {
+        typeName,
+      }).then(() => {
+        expect(createNode.mock.calls[0][0].internal.type).toEqual(expected)
+      })
+    })
+  })
 })
