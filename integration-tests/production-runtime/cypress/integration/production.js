a537a36700a972cb2ec368407d928d2471b1fbda
David Bailey <david.j.b@vivaldi.net>
2018-09-06 18:01:16 +0100

    Add Cypress production test

--- integration-tests/production-runtime/cypress/integration/production.js
+++ integration-tests/production-runtime/cypress/integration/production.js
@@ -3,6 +3,23 @@
 describe(`Production build tests`, () => {
   it(`should render properly`, () => {
     cy.visit(`/`)
-    
+  })
+
+  it(`should navigate back after a reload`, () => {
+    cy.getTestElement(`page2`)
+      .first()
+      .click()
+
+    cy.waitForRouteChange()
+      .url()
+      .should(`contain`, `page-2`)
+
+    cy.reload()
+      .waitForRouteChange()
+      .go(`back`)
+
+    cy.waitForRouteChange()
+      .getTestElement(`page2`)
+      .should(`exist`)
   })
 })
--- integration-tests/production-runtime/src/pages/index.js
+++ integration-tests/production-runtime/src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/"><span data-testid="page2">Go to page 2</span></Link>
   </Layout>
 )
 
