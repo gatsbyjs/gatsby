74f3e4f02fb14aa45746fb2737631b987dce01e7
Michal Piechowiak <misiek.piechowiak@gmail.com>
2018-09-06 10:36:56 +0200

    [gatsby-plugin-layout] check if layout component file is resolved in ssr/webpack, not in node (#7551)

--- packages/gatsby-plugin-layout/src/wrap-page.js
+++ packages/gatsby-plugin-layout/src/wrap-page.js
@@ -1,7 +1,19 @@
 const React = require(`react`)
 
 const preferDefault = m => (m && m.default) || m
-const Layout = preferDefault(require(GATSBY_LAYOUT_COMPONENT_PATH))
+let Layout
+try {
+  Layout = preferDefault(require(GATSBY_LAYOUT_COMPONENT_PATH))
+} catch (e) {
+  if (e.toString().indexOf(`Error: Cannot find module`) !== -1) {
+    throw new Error(
+      `Couldn't find layout component at "${GATSBY_LAYOUT_COMPONENT_PATH}.\n\n` +
+        `Please create layout component in that location or specify path to layout component in gatsby-config.js`
+    )
+  } else {
+    throw e
+  }
+}
 
 // eslint-disable-next-line react/prop-types,react/display-name
 module.exports = ({ element, props }) => <Layout {...props}>{element}</Layout>
