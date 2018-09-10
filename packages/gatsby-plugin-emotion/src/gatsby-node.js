049ee426994b0c42084639b4a649c2b675ef275b
Mitchell Hamilton <mitchell@mitchellhamilton.me>
2018-09-11 09:59:36 +1000

    Change default options for babel-plugin-emotion in gatsby-plugin-emotion (#7651)

--- packages/gatsby-plugin-emotion/src/gatsby-node.js
+++ packages/gatsby-plugin-emotion/src/gatsby-node.js
@@ -2,7 +2,9 @@ exports.onCreateBabelConfig = ({ actions }, pluginOptions) => {
   actions.setBabelPlugin({
     name: `babel-plugin-emotion`,
     options: {
-      sourceMap: process.env.NODE_ENV === `production` ? false : true,
+      sourceMap: process.env.NODE_ENV !== `production`,
+      autoLabel: process.env.NODE_ENV !== `production`,
+      hoist: process.env.NODE_ENV === `production`,
       ...(pluginOptions ? pluginOptions : {}),
     },
   })
