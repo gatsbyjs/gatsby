92773fa21cc6b9f5ae7ad4eb6e64e7ab5d4a2dab
Florian Kissling <21834+fk@users.noreply.github.com>
2018-09-10 22:23:01 +0200

    [www] Clean up src/utils/colors.js, consolidate PrismJS syntax highlighting styles (#8007)
    * Bye bye .org/colors
    
    * Clean up src/utils/colors.js, consolidate PrismJS styles
    
    * move the prism-coy.css contents to typography.js
    * clean up `www/src/utils/colors.js` — bye-bye cubehelix colors; it’s us, not you; you’ll always be in our hearts <3

--- www/src/utils/colors.js
+++ www/src/utils/colors.js
@@ -1,60 +1,6 @@
 import gray from "gray-percentage"
 
-const colorStr = `
-#281505
-#042b1b
-#2e1740
-#3a2407
-#073e2e
-#452054
-#48340a
-#0d4f43
-#5c2965
-#53450e
-#165e5a
-#743272
-#5c5815
-#216c72
-#8a3d7d
-#636a1e
-#2f798a
-#9f4984
-#697d2a
-#4084a1
-#b3568b
-#6f8f39
-#538eb6
-#c36490
-#75a14b
-#6998c9
-#d17494
-#7db15f
-#7fa1d9
-#dd859a
-#86c076
-#96abe6
-#e598a1
-#91ce8e
-#adb6f0
-#ecabaa
-#a0daa6
-#c2c2f7
-#f1beb6
-#b1e5be
-#d6cffb
-#f4d1c6
-#c6eed5
-#e7defe
-#f8e4d9
-#def6ea
-#f5eefe
-#fcf6f0
-`
-
 const colors = {
-  a: [],
-  b: [],
-  c: [],
   // original palette by @SachaG
   // @see https://www.figma.com/file/J6IYJEtdRmwJQOrcZ2DfvxDD/Gatsby
   gatsby: `#663399`, // was #744c9e
@@ -78,34 +24,22 @@ const colors = {
     light: gray(80, 270),
   },
   code: {
-    bg: `#fdfaf6`,
+    bg: `#fdfaf6`, // colors.a[0] #fcf6f0
     border: `#faede5`,
     text: `#866c5b`,
     remove: `#e45c5c`,
     add: `#4a9c59`,
+    selector: `#b3568b`,
+    tag: `#4084a1`,
+    keyword: `#538eb6`,
+    comment: `#6f8f39`,
+    punctuation: `#53450e`,
+    regex: `#d88489`,
+    cssString: `#a2466c`,
+    invisibles: `#e0d7d1`,
+    scrollbarThumb: `#f4d1c6`,
+    lineHighlightBorder: `#f1beb6`,
   },
 }
-
-let pointer = `a`
-colorStr
-  .split(`\n`)
-  .reverse()
-  .forEach(c => {
-    if (c === ``) return
-    colors[pointer].push(c)
-    switch (pointer) {
-      case `a`:
-        pointer = `b`
-        break
-      case `b`:
-        pointer = `c`
-        break
-      case `c`:
-        pointer = `a`
-        break
-      default:
-        break
-    }
-  })
 
 export default colors
