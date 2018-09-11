38a4cfcdf7ffd2c973a762272eb1c315f629c241
Dustin Schau <dustinschau@gmail.com>
2018-09-11 14:21:28 -0500

    ci: run script in correct lifecycle (after yarn in child directory already ran)

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 INTEGRATION_TEST=$1
-SRC_PATH=$2
-RELATIVE_PATH="${3:-../../}" # set to third arg if defined, otherwise use ../../
+RELATIVE_PATH="${2:-../../}" # set to second arg if defined, otherwise use ../../
 
 if [[ "$INTEGRATION_TEST" = true ]]; then
   npm install -g gatsby-dev-cli
 
+  EXISTING_DIR=$(pwd)
+
   # bootstrapping all packages so we test _this_ PR's changes
   echo "=== bootstrapping"
+  cd $RELATIVE_PATH
   yarn bootstrap
 
   # setting up child integration test link to gatsby packages
+  cd $EXISTING_DIR
   echo "=== setting up link to current changes with gatsby-dev"
-  cd $SRC_PATH
   gatsby-dev --set-path-to-repo $RELATIVE_PATH
   gatsby-dev --scan-once --quiet
 fi
