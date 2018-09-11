1854a790eee24e077e600e6d6a59fb679fce308b
Dustin Schau <dustinschau@gmail.com>
2018-09-11 13:40:57 -0500

    fix: use bash header

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 INTEGRATION_TEST=$1
 SRC_PATH=$2
 
@@ -6,9 +6,11 @@ if [[ "$INTEGRATION_TEST" = true ]]; then
   npm install -g gatsby-dev-cli
 
   # bootstrapping all packages so we test _this_ PR's changes
+  echo "=== bootstrapping"
   yarn bootstrap
 
   # setting up child integration test link to gatsby packages
+  echo "=== setting up link to current changes with gatsby-dev"
   cd $SRC_PATH
   gatsby-dev --set-path-to-repo ../
   gatsby-dev --scan-once --quiet
