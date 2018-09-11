70bf52042278c0387566a964009d40cd834b9388
Dustin Schau <dustinschau@gmail.com>
2018-09-11 14:52:21 -0500

    ci: iron out working path stuff

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 INTEGRATION_TEST=$1
-RELATIVE_PATH="${2:-../../}" # set to second arg if defined, otherwise use ../../
-
-echo "current working directory: $(pwd)"
+SRC_PATH=$2
+RELATIVE_PATH="${3:-../../}" # set to third arg if defined, otherwise use ../../
 
 if [[ "$INTEGRATION_TEST" = true ]]; then
   npm install -g gatsby-dev-cli
 
-  EXISTING_DIR=$(pwd)
-
   # bootstrapping all packages so we test _this_ PR's changes
-  cd $RELATIVE_PATH
   echo "=== bootstrapping $(pwd)"
   yarn bootstrap
 
   # setting up child integration test link to gatsby packages
-  cd $EXISTING_DIR
+  cd $SRC_PATH
   echo "=== setting up link to current changes with gatsby-dev in $(pwd)"
   gatsby-dev --set-path-to-repo $RELATIVE_PATH
   gatsby-dev --scan-once --quiet
