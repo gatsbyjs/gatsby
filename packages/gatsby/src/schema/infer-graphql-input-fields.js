8082fa017be8d950b12fe706657a688866a7abe4
Michal Piechowiak <misiek.piechowiak@gmail.com>
2018-09-12 13:58:25 +0200

    create proper input filters for ___NODE arrays

--- packages/gatsby/src/schema/infer-graphql-input-fields.js
+++ packages/gatsby/src/schema/infer-graphql-input-fields.js
@@ -268,7 +268,8 @@ export function inferInputObjectStructureFromNodes({
 
     if (_.includes(key, `___NODE`)) {
       // TODO: Union the objects in array
-      const nodeToFind = _.isArray(value) ? value[0] : value
+      const isArray = _.isArray(value)
+      const nodeToFind = isArray ? value[0] : value
       const linkedNode = findLinkedNode(nodeToFind)
 
       // Get from cache if found, else store into it
@@ -286,7 +287,7 @@ export function inferInputObjectStructureFromNodes({
         linkedNodeCache[linkedNode.internal.type] = value
       }
 
-      if (_.isArray(value)) {
+      if (isArray) {
         value = [value]
       }
 
@@ -299,7 +300,7 @@ export function inferInputObjectStructureFromNodes({
       prefix: `${prefix}${_.upperFirst(key)}`,
     })
 
-    if (field == null) return
+    if (field === null) return
     inferredFields[createKey(key)] = field
   })
 
