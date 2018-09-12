fb00028dea5b9187a07a5e6b31118cbf8cd305ed
Dustin Schau <dustinschau@gmail.com>
2018-09-12 14:32:59 -0500

    test: add gatsby-image e2e tests validating base functionality

---
+++ integration-tests/gatsby-image/cypress/integration/fixed.js
@@ -0,0 +1,24 @@
+const fixedTestId = 'image-fixed';
+
+describe('fixed', () => {
+  beforeEach(() => {
+    cy.visit(`/`)
+  })
+
+  it('does not render a spacer div', () => {
+    cy.getTestElement(fixedTestId, '.gatsby-image-outer-wrapper > .gatsby-image-wrapper > div')
+      .should('not.exist')
+  })
+
+  it('renders an image with fixed height', () => {
+    cy.getTestElement(fixedTestId, '.gatsby-image-wrapper')
+      .should('have.attr', 'style')
+      .and('match', /height:\d+/)
+  })
+
+  it('renders an image with fixed width', () => {
+    cy.getTestElement(fixedTestId, '.gatsby-image-wrapper')
+      .should('have.attr', 'style')
+      .and('match', /width:\d+/)
+  })
+})
---
+++ integration-tests/gatsby-image/src/pages/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import Image from 'gatsby-image'
+
+import Layout from '../components/layout'
+
+const IndexPage = ({ data }) => (
+  <Layout>
+    <h1>Fluid</h1>
+    <span data-testid="image-fluid"><Image fluid={data.fruitsFluid.childImageSharp.fluid} /></span>
+    <h1>Fixed</h1>
+    <span data-testid="image-fixed"><Image fixed={data.fruitsFixed.childImageSharp.fixed} /></span>
+  </Layout>
+)
+
+export default IndexPage
+
+export const pageQuery = graphql`
+  query {
+    fruitsFluid:file(relativePath: {eq: "citrus-fruits.jpg"}) {
+      childImageSharp {
+        fluid(maxWidth: 500) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    fruitsFixed:file(relativePath: {eq: "citrus-fruits.jpg"}) {
+      childImageSharp {
+        fixed(width: 500) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`;
