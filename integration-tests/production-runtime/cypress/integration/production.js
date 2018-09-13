bf5c7bf7a041020dc9155b3b5875332788167fdb
David Bailey <4248177+davidbailey00@users.noreply.github.com>
2018-09-13 16:11:45 +0100

    [v2] [cache-dir] Fix navigation after reload (#7812)
    * Fix navigation after reloading
    
    * Add commented apiRunner calls
    
    * Add new test for back after reload and upgrade packages
    
    * fix fetching resources in page-renderer
    
    * revert changes to production-app - this is handled by fix in page-renderer
    
    * remove commented out routeUpdate api calls - it will be fixed in separate PR
    
    * revert changes to app.js
    
    * split page renderer into 2 componets
    
    * update cypress test to go back to home page, so next test start fresh
    
    * Add Cypress production test
    
    * this isn't doing anything - json is no longer in pageResources in dev
    
    * we no longer transition to page when it takes long time to load - this can be removed
    
    * keep rendering old page while waiting for resources for new page
    
    this is mostly covered by navigation code, but in case of using history.back / forward
    functions, browser will change URL immediately and it's not handled by navigation code then.
    
    * move firing on(Pre)RouteUpdate APIs to pageRenderer - it will guarantee state of DOM
    
    * no need to destructure these
    
    * only fire on(Pre)RouteUpdate APIs for main PageRenderer - don't do that for publicly exported PageRenderer
    
    * reset route change promise when going back or forward in history
    
    * Use Chrome for Cypress tests
    
    * Remove waitForRouteChange after reloading, and test the URL
    
    * Undo using Chrome on CI
    
    * wait for full load in cypress
    
    * handle loading 404 resources in loader.js
    
    * update location changes to page even pathname didn't change, remove some dead code
    
    * Revert "Undo using Chrome on CI"
    
    This reverts commit 6d7cbf52362fe99379568ceecf108d814cd38263.
    
    * Import Cypress commands from package
    
    * revert gatsbygram package dependency version changes

--- integration-tests/production-runtime/cypress/integration/production.js
+++ integration-tests/production-runtime/cypress/integration/production.js
@@ -1,8 +1,26 @@
-/* eslint-disable no-undef */
+/* global cy */
 
 describe(`Production build tests`, () => {
   it(`should render properly`, () => {
-    cy.visit(`/`)
-    
+    cy.visit(`/`).waitForRouteChange()
+  })
+
+  it(`should navigate back after a reload`, () => {
+    cy.getTestElement(`page2`)
+      .first()
+      .click()
+
+    cy.waitForRouteChange()
+      .location(`pathname`)
+      .should(`equal`, `/page-2/`)
+
+    cy.reload()
+      .go(`back`)
+
+    cy.waitForRouteChange()
+      .getTestElement(`page2`)
+      .should(`exist`)
+      .location(`pathname`)
+      .should(`equal`, `/`)
   })
 })
--- packages/gatsby/cache-dir/page-renderer.js
+++ packages/gatsby/cache-dir/page-renderer.js
@@ -1,161 +1,56 @@
 import React, { createElement } from "react"
 import PropTypes from "prop-types"
-import loader, { publicLoader } from "./loader"
-import emitter from "./emitter"
-import shallowCompare from "shallow-compare"
+import { publicLoader } from "./loader"
 import { apiRunner } from "./api-runner-browser"
+import { onRouteUpdate, onPreRouteUpdate } from "./navigation"
 
-// Pass pathname in as prop.
-// component will try fetching resources. If they exist,
-// will just render, else will render null.
+// Renders page and fire on(Pre)RouteUpdate APIs
 class PageRenderer extends React.Component {
   constructor(props) {
-    super()
-    let location = props.location
-
-    // Set the pathname for 404 pages.
-    const pathname = this.getPathName(location)
-
-    this.state = {
-      lastPathname: location.pathname,
-      pageResources: loader.getResourcesForPathnameSync(pathname),
+    super(props)
+    if (props.isMain) {
+      onPreRouteUpdate(props.location)
     }
-  }
-
-  static getDerivedStateFromProps({ pageResources, location }, prevState) {
-    let nextState = { lastPathname: location.pathname }
-
-    if (
-      process.env.NODE_ENV !== `production` &&
-      pageResources &&
-      pageResources.json
-    ) {
-      nextState.pageResources = pageResources
-    }
-
-    if (prevState.lastPathname !== location.pathname) {
-      const pageResources = loader.getResourcesForPathnameSync(
-        location.pathname
-      )
-
-      if (pageResources) {
-        nextState.pageResources = pageResources
-      }
-    }
-
-    return nextState
   }
 
   componentDidMount() {
-    // Listen to events so when our page gets updated, we can transition.
-    // This is only useful on delayed transitions as the page will get rendered
-    // without the necessary page resources and then re-render once those come in.
-    emitter.on(`onPostLoadPageResources`, e => {
-      const page = loader.getPage(this.props.location.pathname)
+    if (this.props.isMain) {
+      onRouteUpdate(this.props.location)
+    }
+  }
 
-      if (page && e.page.path === page.path) {
-        this.setState({ pageResources: e.pageResources })
+  componentDidUpdate(prevProps, prevState, shouldFireRouteUpdate) {
+    if (this.props.isMain && shouldFireRouteUpdate) {
+      onRouteUpdate(this.props.location)
+    }
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    if (this.props.isMain) {
+      if (this.props.location.pathname !== prevProps.location.pathname) {
+        onPreRouteUpdate(this.props.location)
+        return true
       }
-    })
-  }
 
-  componentDidUpdate(prevProps) {
-    if (prevProps === this.props) return
-
-    const { location } = this.props
-    const pathName = this.getPathName(location)
-
-    if (!loader.getResourcesForPathnameSync(pathName))
-      // Page resources won't be set in cases where the browser back button
-      // or forward button is pushed as we can't wait as normal for resources
-      // to load before changing the page.
-      loader.getResourcesForPathnameSync(pathName, pageResources => {
-        // The page may have changed since we started this, in which case doesn't update
-        if (this.props.location.pathname !== location.pathname) {
-          return
-        }
-
-        this.setState({
-          pageResources,
-        })
-      })
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    // 404
-    if (!nextState.pageResources) {
-      return true
+      return false
     }
-    // Check if the component or json have changed.
-    if (!this.state.pageResources && nextState.pageResources) {
-      return true
-    }
-    if (
-      this.state.pageResources.component !== nextState.pageResources.component
-    ) {
-      return true
-    }
-
-    if (this.state.pageResources.json !== nextState.pageResources.json) {
-      return true
-    }
-
-    // Check if location has changed on a page using internal routing
-    // via matchPath configuration.
-    if (
-      this.props.location.key !== nextProps.location.key &&
-      nextState.pageResources.page &&
-      (nextState.pageResources.page.matchPath ||
-        nextState.pageResources.page.path)
-    ) {
-      return true
-    }
-
-    return shallowCompare(this, nextProps, nextState)
-  }
-
-  getPathName(location) {
-    return !loader.getPage(location.pathname) ? `/404.html` : location.pathname
+    return null
   }
 
   render() {
-    if (
-      !(this.state.pageResources && this.state.pageResources.json) &&
-      process.env.NODE_ENV === `production`
-    ) {
-      // Try to load the page directly - this should result in a 404 or
-      // network offline error.
-
-      const url = new URL(window.location)
-      if (url.search) {
-        url.search += `&no-cache=1`
-      } else {
-        url.search = `?no-cache=1`
-      }
-      window.location.replace(url)
-
-      return null
-    }
-
-    const pathContext =
-      process.env.NODE_ENV !== `production`
-        ? this.props.pageContext
-        : this.state.pageResources.json.pageContext
-
     const props = {
       ...this.props,
-      ...this.state.pageResources.json,
-      pathContext,
+      pathContext: this.props.pageContext,
     }
 
     const [replacementElement] = apiRunner(`replaceComponentRenderer`, {
-      props: { ...this.props, pageResources: this.state.pageResources },
+      props: this.props,
       loader: publicLoader,
     })
 
     const pageElement =
       replacementElement ||
-      createElement(this.state.pageResources.component, props)
+      createElement(this.props.pageResources.component, props)
 
     const wrappedPage = apiRunner(
       `wrapPageElement`,
@@ -171,9 +66,11 @@ class PageRenderer extends React.Component {
 }
 
 PageRenderer.propTypes = {
-  location: PropTypes.object,
-  pageResources: PropTypes.object,
-  pageContext: PropTypes.object,
+  location: PropTypes.object.isRequired,
+  pageResources: PropTypes.object.isRequired,
+  data: PropTypes.object,
+  pageContext: PropTypes.object.isRequired,
+  isMain: PropTypes.bool,
 }
 
 export default PageRenderer
