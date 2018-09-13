2f17090bbb7463913e4a3af2b624774a9e42331b
David Bailey <david.j.b@vivaldi.net>
2018-09-13 19:54:33 +0100

    Create build-examples script

---
+++ scripts/build-examples.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+status=""
+
+cd examples
+for example in *; do
+  if [ -d "$example" ]; then
+    cd $example
+    yarn &&
+    gatsby-dev -s &&
+    yarn build &&
+    status="$status[success] building $example"$'\n' ||
+    status="$status[failure] building $example"$'\n'
+    cd ..
+  fi
+done
+
+echo
+echo -n "${status}"
