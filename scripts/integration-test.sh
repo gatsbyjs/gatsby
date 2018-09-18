0aea56b22b702573af0f85d0d1c5b5aa4160af04
Dustin Schau <dustinschau@gmail.com>
2018-09-18 15:50:28 -0500

    ci: attempt to trigger a re-buidl

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -5,7 +5,7 @@ GATSBY_PATH="../../"
 sudo npm install -g gatsby-dev-cli
 
 # setting up child integration test link to gatsby packages
-cd integration-tests/$SRC_PATH
+cd $SRC_PATH
 yarn
 echo "=== setting up link to current changes with gatsby-dev in $(pwd)"
 gatsby-dev --set-path-to-repo $GATSBY_PATH
