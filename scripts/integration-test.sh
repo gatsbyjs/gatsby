22bb1b1d767eae8e8d5c236f0b611b5a087e37e3
Dustin Schau <dustinschau@gmail.com>
2018-09-18 16:21:12 -0500

    ci: make a few changes/fixes

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -7,7 +7,6 @@ sudo npm install -g gatsby-dev-cli
 # setting up child integration test link to gatsby packages
 cd $SRC_PATH
 yarn
-echo "=== setting up link to current changes with gatsby-dev in $(pwd)"
 gatsby-dev --set-path-to-repo $GATSBY_PATH
-gatsby-dev --scan-once --quiet --copy-all # copies _all_ files in gatsby/packages
+gatsby-dev --scan-once --copy-all # copies _all_ files in gatsby/packages
 yarn test
