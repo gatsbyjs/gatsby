7c2bd2904caf4e4a5fd2256bb7143efa26272630
Jonathan Suh <hello@jonsuh.com>
2018-09-18 20:58:58 -0500

    refactor: Improve defaults and options.

--- packages/gatsby-remark-autolink-headers/src/gatsby-ssr.js
+++ packages/gatsby-remark-autolink-headers/src/gatsby-ssr.js
@@ -1,20 +1,17 @@
 import React from "react"
 
+const pluginDefaults = {
+  className: `anchor`,
+  icon: `<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>`,
+  offsetY: 0,
+}
+
 exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
-  let className = `anchor`
-  if (pluginOptions.className) {
-    className = pluginOptions.className
-  }
-
-  let offsetY = 0
-  if (pluginOptions.offsetY) {
-    offsetY = pluginOptions.offsetY
-  }
-
-  let showIcon = true
-  if (`icon` in pluginOptions) {
-    showIcon = pluginOptions.icon
-  }
+  const {
+    className,
+    icon,
+    offsetY,
+  } = Object.assign(pluginDefaults, pluginOptions)
 
   const styles = `
     .${className} {
@@ -62,7 +59,7 @@ exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
     })
   `
 
-  const style = showIcon
+  const style = icon
     ?  <style key={`gatsby-remark-autolink-headers-style`} type="text/css">
         {styles}
       </style>
