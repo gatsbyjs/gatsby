2e477789ce4f3fa623d786c342bc42129b5e790c
Matthias Zaunseder <matthias.zaunseder@hotmail.de>
2018-09-19 13:16:57 +0200

    [gatsby-plugin-manifest] Favicon path respects a different folder in hybrid mode

--- packages/gatsby-plugin-manifest/src/gatsby-ssr.js
+++ packages/gatsby-plugin-manifest/src/gatsby-ssr.js
@@ -4,11 +4,18 @@ import { withPrefix } from "gatsby"
 exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
   // If icons were generated, also add a favicon link.
   if (pluginOptions.icon) {
+    let iconPath = '/icons';
+
+    // The icon path could be different in hybrid mode
+    if (pluginOptions.icons && pluginOptions.icons.length) {
+      iconPath = pluginOptions.icons[0].src.substring(0, pluginOptions.icons[0].src.lastIndexOf("/"))
+    }
+
     setHeadComponents([
       <link
         key={`gatsby-plugin-manifest-icon-link`}
         rel="shortcut icon"
-        href={withPrefix(`/icons/icon-48x48.png`)}
+        href={withPrefix(`${iconPath}/icon-48x48.png`)}
       />,
     ])
   }
