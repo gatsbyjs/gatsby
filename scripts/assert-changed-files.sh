8dfae4669a1ef31c444dfc2f44d79e8553d20e93
Dustin Schau <dustinschau@gmail.com>
2018-09-19 16:14:42 -0500

    ci: fix files_count

--- scripts/assert-changed-files.sh
+++ scripts/assert-changed-files.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 GREP_PATTERN=$1
 
-CHANGED_FILES="$(git diff-tree --no-commit-id --name-only -r $CIRCLE_SHA1 | grep -E "$GREP_PATTERN")"
-FILES_COUNT=$(echo $CHANGED_FILES | wc -l)
+FILES_COUNT="$(git diff-tree --no-commit-id --name-only -r $CIRCLE_SHA1 | grep -E "$GREP_PATTERN" | wc -l)"
 
 if [ $FILES_COUNT -eq 0 ]; then
-  echo "$CHANGED_FILES"
   echo "0 files matching '$GREP_PATTERN'; exiting and marking successful."
   circleci step halt || exit 1
 else
-  echo "$CHANGED_FILES"
   echo "$FILES_COUNT file(s) matching '$GREP_PATTERN'; continuing."
 fi
