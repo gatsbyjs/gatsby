3f0e09099361e077d81490588203e7921631c0b9
Orestis Ioannou <oorestisime@gmail.com>
2018-09-20 22:16:06 +0200

    Better configure coverage sources and remoce ratchet

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,7 @@ const gatsbyBuildDirs = [`dist`].map(dir => path.join(gatsbyDir, dir))
 const builtTestsDirs = pkgs.map(p => path.join(p, `__tests__`))
 const distTestDirs = pkgs.map(p => path.join(p, `dist`))
 const ignoreDirs = [].concat(gatsbyBuildDirs, builtTestsDirs, distTestDirs)
+const coverageDirs = pkgs.map(p => path.join(p, `src/**/*.js`))
 
 module.exports = {
   notify: true,
@@ -28,13 +29,14 @@ module.exports = {
     "^highlight.js$": `<rootDir>/node_modules/highlight.js/lib/index.js`,
   },
   collectCoverage: true,
-  coverageReporters: [`json-summary`, `text`],
+  coverageReporters: [`json-summary`, `text`, `html`],
   coverageThreshold: {
     global: {
-      lines: 70,
-      statements: 70,
-      functions: 66,
-      branches: 57,
+      lines: 42,
+      statements: 43,
+      functions: 40,
+      branches: 42,
     },
   },
+  collectCoverageFrom: coverageDirs,
 }
