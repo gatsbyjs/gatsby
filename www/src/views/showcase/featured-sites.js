0928931a9279e1bba857b25c26ee511cf1817244
Florian Kissling <florian.kissling@gmail.com>
2018-09-22 16:13:42 +0200

    [www] Add active state to mobile navigation
    * switch mobile nav icons to inline SVGs
      * add react-svg-inline, which might be a little too much for what we actually need here
    * fix „Showcase“ icon
      * rename from `showcase-magic.svg` to `showcase.svg`
    * tidy all mobile nav SVGs — control their appearance via CSS
    * add a `svg-defs.svg` containing all SVG gradient definitions
    * -> now using only colors from `utils/colors` to define mobile nav icon colors
    * fix nav item width to 64px (5*64=320), allows us to try `font-weight: bold` for the active nav item
    * note to self: that „Plugins“ icon should be better

--- www/src/views/showcase/featured-sites.js
+++ www/src/views/showcase/featured-sites.js
@@ -2,16 +2,19 @@ import React, { Component } from "react"
 import { Link } from "gatsby"
 import Img from "gatsby-image"
 import hex2rgba from "hex2rgba"
+import SVGInline from "react-svg-inline"
 
 import styles from "../shared/styles"
 import MdArrowForward from "react-icons/lib/md/arrow-forward"
 import ShowcaseItemCategories from "./showcase-item-categories"
 import FeaturedSitesIcon from "../../assets/featured-sites-icons.svg"
 import { ShowcaseIcon } from "../../assets/mobile-nav-icons"
+import SvgDefs from "../../assets/svg-defs"
 import URLQuery from "../../components/url-query"
 import { options, rhythm, scale } from "../../utils/typography"
 import presets, { colors } from "../../utils/presets"
 import scrollToAnchor from "../../utils/scroll-to-anchor"
+import { svgStyles } from "../../utils/styles"
 import Button from "../../components/button"
 import ArrowForwardIcon from "react-icons/lib/md/arrow-forward"
 
@@ -234,17 +237,17 @@ class FeaturedSites extends Component {
                     {...styles.featuredSitesCard}
                     css={{
                       marginRight: `${rhythm(3 / 4)} !important`,
-                      // border: `1px solid ${hex2rgba(colors.lilac, 0.2)}`,
+                      backgroundColor: hex2rgba(colors.ui.light, 0.25),
                       borderRadius: presets.radius,
                       textAlign: `center`,
                       "&&": {
-                        border: 0,
+                        border: `1px solid ${colors.ui.light}`,
                         boxShadow: `none`,
                         transition: `all ${presets.animation.speedDefault} ${
                           presets.animation.curveDefault
                         }`,
                         "&:hover": {
-                          backgroundColor: hex2rgba(colors.ui.light, 0.25),
+                          background: `#fff`,
                           transform: `translateY(-3px)`,
                           boxShadow: `0 8px 20px ${hex2rgba(
                             colors.lilac,
@@ -261,33 +264,20 @@ class FeaturedSites extends Component {
                   >
                     <div
                       css={{
-                        background: colors.ui.whisper,
+                        borderRadius: presets.radius,
                         display: `flex`,
                         alignItems: `center`,
                         position: `relative`,
                         flexBasis: `100%`,
                       }}
                     >
-                      <img
-                        src={ShowcaseIcon}
-                        css={{
-                          position: `absolute`,
-                          height: `100%`,
-                          width: `auto`,
-                          display: `block`,
-                          margin: `0`,
-                          opacity: 0.04,
-                        }}
-                        alt=""
-                      />
                       <span
                         css={{
                           margin: `0 auto`,
                           color: colors.gatsby,
                         }}
                       >
-                        <img
-                          src={ShowcaseIcon}
+                        <span
                           css={{
                             height: 44,
                             width: `auto`,
@@ -301,9 +291,15 @@ class FeaturedSites extends Component {
                             [presets.Hd]: {
                               height: 72,
                             },
+
+                            "& svg": {
+                              height: `100%`,
+                              ...svgStyles.active,
+                            },
                           }}
-                          alt=""
-                        />
+                        >
+                          <SVGInline svg={ShowcaseIcon} />
+                        </span>
                         View all Featured Sites
                       </span>
                     </div>
