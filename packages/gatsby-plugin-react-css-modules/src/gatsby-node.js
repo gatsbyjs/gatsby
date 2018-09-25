9ef586adaf6595e82cd2faec98d2332b48609825
Darpan Kakadia <kakadiadarpan@gmail.com>
2018-09-25 15:50:51 +0530

    Added option to minify css class names generated in production build for react-css-modules. (#8496)
    * Added option to minify css class names generated in production build for react-css-modules.
    
    * Removed minifyClassNames options.

--- packages/gatsby-plugin-react-css-modules/src/gatsby-node.js
+++ packages/gatsby-plugin-react-css-modules/src/gatsby-node.js
@@ -1,8 +1,10 @@
 exports.onCreateBabelConfig = ({ actions }, pluginOptions) => {
+  const isDevelopment = process.env.NODE_ENV !== `production`
+
   actions.setBabelPlugin({
     name: `babel-plugin-react-css-modules`,
     options: {
-      generateScopedName: `[name]--[local]--[hash:base64:5]`,
+      generateScopedName: isDevelopment ? `[name]--[local]--[hash:base64:5]` : `[hash:base64:5]`,
       webpackHotModuleReloading: process.env.NODE_ENV !== `production`,
       ...pluginOptions,
     },
