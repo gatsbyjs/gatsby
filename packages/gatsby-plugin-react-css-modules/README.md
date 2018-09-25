88ffb566ac5e6e8a3bd79848b5708b6114bf9410
Darpan Kakadia <me@kakadiadarpan.com>
2018-09-25 09:27:18 +0530

    Removed minifyClassNames options.

--- packages/gatsby-plugin-react-css-modules/README.md
+++ packages/gatsby-plugin-react-css-modules/README.md
@@ -21,11 +21,6 @@ plugins: [
   {
     resolve: `gatsby-plugin-react-css-modules`,
     options: {
-      // To minify css-module class names
-      // generated in a production build
-      // pass the below flag as true.
-      // default: false
-      minifyClassNames: true,
       // *.css files are included by default.
       // To support another syntax (e.g. SCSS),
       // add `postcss-scss` to your project's devDependencies
@@ -36,6 +31,7 @@ plugins: [
 
       // Exclude global styles from the plugin using a RegExp:
       exclude: `\/global\/`,
+      // For all the options check babel-plugin-react-css-modules README link provided above
     },
   },
 ]
--- packages/gatsby-plugin-react-css-modules/src/gatsby-node.js
+++ packages/gatsby-plugin-react-css-modules/src/gatsby-node.js
@@ -1,16 +1,10 @@
 exports.onCreateBabelConfig = ({ actions }, pluginOptions) => {
   const isDevelopment = process.env.NODE_ENV !== `production`
-  const longNames = `[name]--[local]--[hash:base64:5]`
-  const shortNames = `[hash:base64:5]`
-  const minifyClassNames = pluginOptions.minifyClassNames || false
-
-  delete pluginOptions.minifyClassNames
-  //Let's delete minifyClassNames option as it is not needed by babel-plugin-react-css-modules
 
   actions.setBabelPlugin({
     name: `babel-plugin-react-css-modules`,
     options: {
-      generateScopedName: isDevelopment ? longNames : (minifyClassNames ? shortNames : longNames) ,
+      generateScopedName: isDevelopment ? `[name]--[local]--[hash:base64:5]` : `[hash:base64:5]`,
       webpackHotModuleReloading: process.env.NODE_ENV !== `production`,
       ...pluginOptions,
     },
