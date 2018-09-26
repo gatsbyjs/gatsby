8b95fb8299ce399255ec47843c9db108bf6faf1c
Dustin Schau <dustinschau@gmail.com>
2018-09-26 10:06:28 -0500

    fix: change strategy for watching; watch an array, and listen to ready

--- packages/gatsby-dev-cli/src/watch.js
+++ packages/gatsby-dev-cli/src/watch.js
@@ -5,10 +5,10 @@ const syspath = require(`path`)
 
 let numCopied = 0
 
-const debouncedQuit = _.debounce(() => {
+const quit = () => {
   console.log(`Copied ${numCopied} files`)
   process.exit()
-}, process.env.CI ? 2500 : 500)
+}
 
 const copyPath = (oldPath, newPath, quiet) =>
   new Promise((resolve, reject) => {
@@ -27,53 +27,57 @@ const copyPath = (oldPath, newPath, quiet) =>
   })
 
 function watch(root, packages, { scanOnce, quiet }) {
-  packages.forEach(p => {
-    const prefix = syspath.join(root, `/packages/`, p)
+  const ignored = [
+    /[/\\]node_modules[/\\]/i,
+    /\.git/i,
+    /\.DS_Store/,
+  ].concat(
+    packages.map(p => new RegExp(`${p}[\\/\\\\]src[\\/\\\\]`, `i`))
+  )
+  const watchers = packages.map(p => syspath.join(root, `/packages/`, p))
 
-    const ignoreRegs = [
-      /[/\\]node_modules[/\\]/i,
-      /\.git/i,
-      /\.DS_Store/,
-      new RegExp(`${p}[\\/\\\\]src[\\/\\\\]`, `i`),
-    ]
+  let allCopies = []
 
-    chokidar
-      .watch(prefix, {
-        ignored: [(path, stats) => _.some(ignoreRegs, reg => reg.test(path))],
-      })
-      .on(`all`, (event, path) => {
-        if (event === `change` || event === `add`) {
-          // Copy it over local version.
+  chokidar.watch(watchers, {
+    ignored: [(path, stats) => _.some(ignored, reg => reg.test(path))],
+  })
+    .on(`all`, (event, path) => {
+      if (event === `change` || event === `add`) {
+        const name = syspath.basename(syspath.dirname(path.split(`packages/`).pop()))
+        const prefix = syspath.join(root, `/packages/`, name)
 
-          // Don't copy over the Gatsby bin file as that breaks the NPM symlink.
-          if (_.includes(path, `dist/gatsby-cli.js`)) {
-            return
-          }
+        // Copy it over local version.
+        // Don't copy over the Gatsby bin file as that breaks the NPM symlink.
+        if (_.includes(path, `dist/gatsby-cli.js`)) {
+          return
+        }
 
-          const newPath = syspath.join(
-            `./node_modules/${p}`,
-            syspath.relative(prefix, path)
+        const newPath = syspath.join(
+          `./node_modules/${name}`,
+          syspath.relative(prefix, path)
+        )
+
+        let localCopies = [copyPath(path, newPath, quiet)]
+
+        // If this is from "cache-dir" also copy it into the site's .cache
+        if (_.includes(path, `cache-dir`)) {
+          const newCachePath = syspath.join(
+            `.cache/`,
+            syspath.relative(syspath.join(prefix, `cache-dir`), path)
           )
+          localCopies.push(copyPath(path, newCachePath, quiet))
+        }
 
-          let copies = [copyPath(path, newPath, quiet)]
-
-          // If this is from "cache-dir" also copy it into the site's .cache
-          if (_.includes(path, `cache-dir`)) {
-            const newCachePath = syspath.join(
-              `.cache/`,
-              syspath.relative(syspath.join(prefix, `cache-dir`), path)
-            )
-            copies.push(copyPath(path, newCachePath, quiet))
-          }
-
-          Promise.all(copies).then(() => {
-            if (scanOnce) {
-              debouncedQuit()
-            }
-          })
+        allCopies = allCopies.concat(localCopies)
+      }
+    })
+    .on(`ready`, () => {
+      Promise.all(allCopies).then(() => {
+        if (scanOnce) {
+          quit()
         }
       })
-  })
+    })
 }
 
 module.exports = watch
