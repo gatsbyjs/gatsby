a4cd35cdcd268c3a5c8c9d0cf320269fb46da00c
Melanie Seltzer <melleh11@gmail.com>
2018-09-27 14:05:10 -0700

    Run a test for each protocol using forEach

--- packages/gatsby/src/redux/reducers/__tests__/redirects.js
+++ packages/gatsby/src/redux/reducers/__tests__/redirects.js
@@ -43,84 +43,31 @@ describe(`redirects`, () => {
     )
   })
 
-  it(`lets you redirect using any protocol`, () => {
-    const action = {
-      type: `CREATE_REDIRECT`,
-      payload: {
-        fromPath: `/page1`,
-        toPath: `https://example.com`,
-      },
-    }
-    const action2 = {
-      type: `CREATE_REDIRECT`,
-      payload: {
-        fromPath: `/page2`,
-        toPath: `http://example.com`,
-      },
-    }
-    const action3 = {
-      type: `CREATE_REDIRECT`,
-      payload: {
-        fromPath: `/page3`,
-        toPath: `//example.com`,
-      },
-    }
-    const action4 = {
-      type: `CREATE_REDIRECT`,
-      payload: {
-        fromPath: `/page4`,
-        toPath: `ftp://example.com`,
-      },
-    }
-    const action5 = {
-      type: `CREATE_REDIRECT`,
-      payload: {
-        fromPath: `/page5`,
-        toPath: `mailto:example@email.com`,
-      },
-    }
-    const action6 = {
-      type: `CREATE_REDIRECT`,
-      payload: {
-        fromPath: `/page6`,
-        toPath: `/page6/`,
-      },
-    }
+  const protocolArr = [
+    [`https`, `https://example.com`],
+    [`http`, `http://example.com`],
+    [`//`, `//example.com`],
+    [`ftp`, `ftp://example.com`],
+    [`mailto`, `mailto:example@email.com`],
+  ]
+  
+  protocolArr.forEach(([protocol, toPath], index) => {
+    it(`lets you redirect using ${protocol}`, () => {
+      const fromPath = `/page${index}`
+      const action = {
+        type: `CREATE_REDIRECT`,
+        payload: {
+          fromPath,
+          toPath,
+        },
+      }
 
-    let state = reducer(undefined, action)
-    state = reducer(state, action2)
-    state = reducer(state, action3)
-    state = reducer(state, action4)
-    state = reducer(state, action5)
-    state = reducer(state, action6)
-
-    expect(state).toEqual(
-      [
+      expect(reducer(undefined, action)).toEqual([
         {
-          fromPath: `/page1`,
-          toPath: `https://example.com`,
+          fromPath,
+          toPath,
         },
-        {
-          fromPath: `/page2`,
-          toPath: `http://example.com`,
-        },
-        {
-          fromPath: `/page3`,
-          toPath: `//example.com`,
-        },
-        {
-          fromPath: `/page4`,
-          toPath: `ftp://example.com`,
-        },
-        {
-          fromPath: `/page5`,
-          toPath: `mailto:example@email.com`,
-        },
-        {
-          fromPath: `/page6`,
-          toPath: `/page6/`,
-        },
-      ]
-    )
+      ])
+    })
   })
 })
