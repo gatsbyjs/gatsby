f497b7457dbdbb1eec5290f3af3a01bb273ee1bc
Bogdan Luca <luca.bogdan@gmail.com>
2018-09-27 14:07:29 +0300

    fix: fix unhandled rejection when no browser found when running with --open flag (#8507)
    * [cli] Fix unhandled rejection when no browser found
    
    * [cli] Beautify serve output
    
    * [cli] Change warning message when no browser is available

--- packages/gatsby/src/commands/serve.js
+++ packages/gatsby/src/commands/serve.js
@@ -5,6 +5,7 @@ const compression = require(`compression`)
 const express = require(`express`)
 const getConfigFile = require(`../bootstrap/get-config-file`)
 const preferDefault = require(`../bootstrap/prefer-default`)
+const chalk = require(`chalk`)
 
 module.exports = async program => {
   let { prefixPaths, port, open } = program
@@ -32,10 +33,11 @@ module.exports = async program => {
 
   const server = app.listen(port, () => {
     let openUrlString = `http://localhost:${port}${pathPrefix}`
-    console.log(`gatsby serve running at:`, openUrlString)
+    console.log(`${chalk.blue(`info`)} gatsby serve running at: ${chalk.bold(openUrlString)}`)
     if (open) {
-      console.log(`Opening browser...`)
+      console.log(`${chalk.blue(`info`)} Opening browser...`)
       openurl(openUrlString)
+        .catch(err => console.log(`${chalk.yellow(`warn`)} Browser not opened because no browser was found`))
     }
   })
 
