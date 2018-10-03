5386e1dfa98917320038f22b0e5059c0a9946f75
Dustin Schau <DSchau@users.noreply.github.com>
2018-10-03 12:55:53 -0500

    feat(gatsby): switch to different caching mechanism backed by fs (#8435)
    * feat: switch to different caching mechanism backed by fs
    
    * chore: remove unneeded log statement
    
    * feat: add memory cache as backing instance
    
    * chore: remove async requirement (not async)
    
    * chore: minor tweaks
    
    * fix: ensure directory exists before instantiating fs cache
    
    * chore: use an hour for ttl instead of 60 seconds
    
    Note: this _really_ speeds up subsequent builds!
    
    * chore: add a page template and more representative data to md benchmark
    
    * chore: parse int on possible strings
    
    * chore: update versions in md benchmarck
    
    * fix: use callback (which works?) instead of assuming promise
    
    * chore: remove accidental buffer change
    
    * test: fix failing unit tests and slightly refactor API
    
    * perf: use cache lookup to not make wasteful fs calls
    
    * chore: bump version to get node 6 dep
    
    * fix: do not expire cache files (this matches old behavior)
    
    * chore: use correct name
    
    * test: add a test for ttl
    
    * style: fix lint errors

---
+++ benchmarks/markdown/page-template.js
@@ -0,0 +1,25 @@
+const faker = require(`faker`)
+const matter = require(`gray-matter`)
+
+const NUM_ROWS = parseInt(process.env.NUM_ROWS || 25, 10);
+
+module.exports = index => `
+${matter.stringify(``, {
+  title: faker.lorem.sentence(),
+  slug: `/${faker.helpers.slugify(faker.lorem.sentence())}`
+}).trim()}
+
+## Page #${index}
+
+### API
+
+|Name|Description|Required|
+|:--:|-----------|--------|
+${new Array(faker.random.number(NUM_ROWS)).fill(undefined).map(() => `
+|${faker.lorem.word()}|${faker.lorem.sentence()}|${faker.random.boolean()}|
+`.trim()).join('\n')}
+
+### More Detail
+
+${faker.lorem.paragraphs()}
+`
