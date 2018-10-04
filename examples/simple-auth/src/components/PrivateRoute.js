b1d2492db6262eb9000cfa2df67342f6a8b882eb
Simon Mansfield <siyfion@gmail.com>
2018-10-04 17:21:34 +0100

    Switch redirectTo to navigate

--- examples/simple-auth/src/components/PrivateRoute.js
+++ examples/simple-auth/src/components/PrivateRoute.js
@@ -1,12 +1,12 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { redirectTo } from "@reach/router"
+import { navigate } from "gatsby"
 import { isLoggedIn } from "../utils/auth"
 
 const PrivateRoute = ({ component: Component, location, ...rest }) => {
   if (!isLoggedIn() && location.pathname !== `/app/login`) {
     // If we’re not logged in, redirect to the home page.
-    redirectTo(`/app/login`)
+    navigate(`/app/login`)
     return null
   }
 
