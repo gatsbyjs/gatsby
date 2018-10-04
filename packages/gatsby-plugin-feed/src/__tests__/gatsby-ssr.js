e4e64362e5e030a0b4a875091817796e5a36e9f3
Edward Yong Gyun Kim <edward@edykim.com>
2018-10-04 08:00:11 -0700

    Add tests with path prefix

--- packages/gatsby-plugin-feed/src/__tests__/gatsby-ssr.js
+++ packages/gatsby-plugin-feed/src/__tests__/gatsby-ssr.js
@@ -1,10 +1,16 @@
 const { onRenderBody } = require(`../gatsby-ssr`)
 
-beforeEach(() => {
-  global.__PATH_PREFIX__ = ``
-})
+const defaultPathPrefix = global.__PATH_PREFIX__
 
 describe(`Adds <Link> for feed to head`, () => {
+  beforeEach(() => {
+    global.__PATH_PREFIX__ = ``
+  })
+
+  afterEach(() => {
+    global.__PATH_PREFIX__ = defaultPathPrefix
+  })
+
   it(`creates Link if feeds does exist`, async () => {
     const pluginOptions = {
       feeds: [
@@ -26,6 +32,31 @@ describe(`Adds <Link> for feed to head`, () => {
     expect(setHeadComponents).toHaveBeenCalledTimes(1)
   })
   it(`creates multiple Link if feeds are several`, async () => {
+    const pluginOptions = {
+      feeds: [
+        {
+          output: `gryffindor/feed.xml`,
+        },
+        {
+          output: `ravenclaw/feed.xml`,
+        },
+      ],
+    }
+    const setHeadComponents = jest.fn()
+
+    await onRenderBody(
+      {
+        setHeadComponents,
+      },
+      pluginOptions
+    )
+
+    expect(setHeadComponents).toMatchSnapshot()
+    expect(setHeadComponents).toHaveBeenCalledTimes(1)
+  })
+  it(`creates Link href with path prefix when __PATH_PREFIX__ sets`, async () => {
+    global.__PATH_PREFIX__ = `/hogwarts`
+
     const pluginOptions = {
       feeds: [
         {
