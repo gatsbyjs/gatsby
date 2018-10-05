a72a7b4e1e8a1e67cfcbf8e635e3866ca1a5ca96
Edward Kim <33057457+edykim@users.noreply.github.com>
2018-10-05 04:37:28 -0700

    test: add tests for gatsby-plugin-feed (#8778)
    * Add tests for gatsby-plugin-feed of ssr
    
    Signed-off-by: Edward Yong Gyun Kim <edward@edykim.com>
    
    * Update snapshots
    
    * Add tests with path prefix

---
+++ packages/gatsby-plugin-feed/src/__tests__/gatsby-ssr.js
@@ -0,0 +1,82 @@
+const { onRenderBody } = require(`../gatsby-ssr`)
+
+const defaultPathPrefix = global.__PATH_PREFIX__
+
+describe(`Adds <Link> for feed to head`, () => {
+  beforeEach(() => {
+    global.__PATH_PREFIX__ = ``
+  })
+
+  afterEach(() => {
+    global.__PATH_PREFIX__ = defaultPathPrefix
+  })
+
+  it(`creates Link if feeds does exist`, async () => {
+    const pluginOptions = {
+      feeds: [
+        {
+          output: `feed.xml`,
+        },
+      ],
+    }
+    const setHeadComponents = jest.fn()
+
+    await onRenderBody(
+      {
+        setHeadComponents,
+      },
+      pluginOptions
+    )
+
+    expect(setHeadComponents).toMatchSnapshot()
+    expect(setHeadComponents).toHaveBeenCalledTimes(1)
+  })
+  it(`creates multiple Link if feeds are several`, async () => {
+    const pluginOptions = {
+      feeds: [
+        {
+          output: `gryffindor/feed.xml`,
+        },
+        {
+          output: `ravenclaw/feed.xml`,
+        },
+      ],
+    }
+    const setHeadComponents = jest.fn()
+
+    await onRenderBody(
+      {
+        setHeadComponents,
+      },
+      pluginOptions
+    )
+
+    expect(setHeadComponents).toMatchSnapshot()
+    expect(setHeadComponents).toHaveBeenCalledTimes(1)
+  })
+  it(`creates Link href with path prefix when __PATH_PREFIX__ sets`, async () => {
+    global.__PATH_PREFIX__ = `/hogwarts`
+
+    const pluginOptions = {
+      feeds: [
+        {
+          output: `gryffindor/feed.xml`,
+        },
+        {
+          output: `ravenclaw/feed.xml`,
+        },
+      ],
+    }
+    const setHeadComponents = jest.fn()
+
+    await onRenderBody(
+      {
+        setHeadComponents,
+      },
+      pluginOptions
+    )
+
+    expect(setHeadComponents).toMatchSnapshot()
+    expect(setHeadComponents).toHaveBeenCalledTimes(1)
+  })
+})
