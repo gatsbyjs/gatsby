4fd36e50d36fab7c8dcfc9b2f4d2d2c2e8bf41d2
Philipp Spiess <hello@philippspiess.com>
2018-10-07 13:36:42 +0200

    Remove integration tests

--- packages/babel-preset-gatsby-package/src/__tests__/index.js
+++ packages/babel-preset-gatsby-package/src/__tests__/index.js
@@ -1,19 +1,109 @@
-const babel = require(`babel-core`)
 const preset = require(`../`)
 
-function matchesSnapshot(query, options = {}) {
-  const { code } = babel.transform(query, {
-    presets: [[preset, options]],
-  })
-  expect(code).toMatchSnapshot()
-}
+it(`Specifies proper presets and plugins in Node mode`, () => {
+  const { presets, plugins } = preset()
 
-it(`Applies @babel/preset-env for target browsers`, () => {
-  matchesSnapshot(`const p = () => "s";`, { browser: true })
+  expect(presets).toEqual([
+    [
+      expect.stringContaining(`@babel/preset-env`),
+      {
+        debug: false,
+        loose: true,
+        modules: `commonjs`,
+        shippedProposals: true,
+        targets: {
+          node: `current`,
+        },
+        useBuiltIns: `entry`,
+      },
+    ],
+    [expect.stringContaining(`@babel/preset-react`), { development: true }],
+    expect.stringContaining(`@babel/preset-flow`),
+  ])
+  expect(plugins).toEqual([
+    expect.stringContaining(`@babel/plugin-proposal-class-properties`),
+    expect.stringContaining(`@babel/plugin-proposal-optional-chaining`),
+    expect.stringContaining(`@babel/plugin-transform-runtime`),
+  ])
 })
 
-it(`Allows custom targets`, () => {
-  matchesSnapshot(`const p = () => "s";`, {
-    targets: { browsers: `last 1 Chrome versions` },
-  })
+it(`Specifies proper presets and plugins in debug Node mode`, () => {
+  const { presets, plugins } = preset(null, { debug: true })
+
+  expect(presets).toEqual([
+    [
+      expect.stringContaining(`@babel/preset-env`),
+      {
+        debug: true,
+        loose: true,
+        modules: `commonjs`,
+        shippedProposals: true,
+        targets: {
+          node: `current`,
+        },
+        useBuiltIns: `entry`,
+      },
+    ],
+    [expect.stringContaining(`@babel/preset-react`), { development: true }],
+    expect.stringContaining(`@babel/preset-flow`),
+  ])
+  expect(plugins).toEqual([
+    expect.stringContaining(`@babel/plugin-proposal-class-properties`),
+    expect.stringContaining(`@babel/plugin-proposal-optional-chaining`),
+    expect.stringContaining(`@babel/plugin-transform-runtime`),
+  ])
+})
+
+it(`Specifies proper presets and plugins in browser mode`, () => {
+  const { presets, plugins } = preset(null, { browser: true })
+
+  expect(presets).toEqual([
+    [
+      expect.stringContaining(`@babel/preset-env`),
+      {
+        debug: false,
+        loose: true,
+        modules: `commonjs`,
+        shippedProposals: true,
+        targets: {
+          browsers: [`last 2 versions`, `not ie <= 11`, `not android 4.4.3`],
+        },
+        useBuiltIns: false,
+      },
+    ],
+    [expect.stringContaining(`@babel/preset-react`), { development: true }],
+    expect.stringContaining(`@babel/preset-flow`),
+  ])
+  expect(plugins).toEqual([
+    expect.stringContaining(`@babel/plugin-proposal-class-properties`),
+    expect.stringContaining(`@babel/plugin-proposal-optional-chaining`),
+    expect.stringContaining(`@babel/plugin-transform-runtime`),
+  ])
+})
+
+it(`Specifies proper presets and plugins in debug browser mode`, () => {
+  const { presets, plugins } = preset(null, { browser: true, debug: true })
+
+  expect(presets).toEqual([
+    [
+      expect.stringContaining(`@babel/preset-env`),
+      {
+        debug: true,
+        loose: true,
+        modules: `commonjs`,
+        shippedProposals: true,
+        targets: {
+          browsers: [`last 2 versions`, `not ie <= 11`, `not android 4.4.3`],
+        },
+        useBuiltIns: false,
+      },
+    ],
+    [expect.stringContaining(`@babel/preset-react`), { development: true }],
+    expect.stringContaining(`@babel/preset-flow`),
+  ])
+  expect(plugins).toEqual([
+    expect.stringContaining(`@babel/plugin-proposal-class-properties`),
+    expect.stringContaining(`@babel/plugin-proposal-optional-chaining`),
+    expect.stringContaining(`@babel/plugin-transform-runtime`),
+  ])
 })
