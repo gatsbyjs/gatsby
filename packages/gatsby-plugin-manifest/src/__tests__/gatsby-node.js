f273c82f9b783ddfe7164dd659732be3e8fcc977
Orestis Ioannou <oorestisime@gmail.com>
2018-10-08 22:28:09 +0200

    test: add gatsby-node tests for plugin manifest (#8598)
    * Add gatsby-node tests for plugin manifest
    
    * Fixes after review
    
    * Better fix the jest mocks
    
    * Fix test to not fail for windows

---
+++ packages/gatsby-plugin-manifest/src/__tests__/gatsby-node.js
@@ -0,0 +1,47 @@
+jest.mock(`fs`, () => {
+  return {
+    existsSync: jest.fn().mockImplementation(() => true),
+    writeFileSync: jest.fn(),
+    statSync: jest.fn(),
+  }
+})
+const fs = require(`fs`)
+const path = require(`path`)
+const { onPostBootstrap } = require(`../gatsby-node`)
+
+describe(`Test plugin manifest options`, () => {
+  it(`correctly works with default parameters`, async () => {
+    await onPostBootstrap([], {
+      name: `GatsbyJS`,
+      short_name: `GatsbyJS`,
+      start_url: `/`,
+      background_color: `#f7f0eb`,
+      theme_color: `#a2466c`,
+      display: `minimal-ui`,
+    })
+    const [filePath, contents] = fs.writeFileSync.mock.calls[0]
+    expect(filePath).toEqual(path.join(`public`, `manifest.webmanifest`))
+    expect(contents).toMatchSnapshot()
+  })
+  it(`fails on non existing icon`, (done) => {
+    fs.statSync.mockReturnValueOnce({ isFile: () => false })
+    onPostBootstrap([], {
+      name: `GatsbyJS`,
+      short_name: `GatsbyJS`,
+      start_url: `/`,
+      background_color: `#f7f0eb`,
+      theme_color: `#a2466c`,
+      display: `minimal-ui`,
+      icon: `non/existing/path`,
+      icons: [{
+        src: `icons/icon-48x48.png`,
+        sizes: `48x48`,
+        type: `image/png`,
+      }],
+    })
+    .catch((err) => {
+      expect(err).toMatchSnapshot()
+      done()
+    })
+  })
+})
