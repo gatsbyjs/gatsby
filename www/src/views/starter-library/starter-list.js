a5963bb2362c9a6ab5fe0ff7207ac5aa6a782e12
Michal Piechowiak <misiek.piechowiak@gmail.com>
2018-10-08 14:33:01 +0200

    use gatsby-link

--- www/src/views/starter-library/starter-list.js
+++ www/src/views/starter-library/starter-list.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 import LaunchDemoIcon from "react-icons/lib/md/launch"
 import GithubIcon from "react-icons/lib/go/mark-github"
 import CopyToClipboardIcon from "react-icons/lib/go/clippy"
@@ -36,10 +37,7 @@ const StartersList = ({ urlState, starters, count, sortRecent }) => {
             <small>
               Maybe you should write one and
               {` `}
-              <a href="https://www.gatsbyjs.org/docs/submit-to-starter-library/">
-                submit it
-              </a>
-              ?
+              <Link to="/docs/submit-to-starter-library/">submit it</Link>?
             </small>
           </div>
         </h1>
