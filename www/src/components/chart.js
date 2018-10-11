e83809501165cb3087faf83f9ad424b6415c9626
Sam Bhagwat <calcsam@gmail.com>
2018-10-10 17:54:52 -0700

    [Hold until 10-11] Content mesh part 3 (#8977)
    * content mesh part 3
    
    * new chart stuff
    
    * link updates
    
    * Update index.md
    
    * Update index.md
    
    * Update index.md
    
    * Update index.md

---
+++ www/src/components/chart.js
@@ -0,0 +1,105 @@
+import React from "react"
+import ReactHighcharts from "react-highcharts"
+
+const dateToUTC = date => {
+  const d = String(date)
+  const year = Number(d.slice(0, 4))
+  const monthNum = Number(d.slice(5))
+
+  return Date.UTC(year, monthNum - 1, 0)
+}
+
+const highchartsOptions = {
+  colors: ["#fd9800", "#008FD5", "#77AB43", "#663399", "#C4C4C4"],
+  chart: {
+    backgroundColor: null,
+    style: {
+      fontFamily: "Overpass, sans-serif",
+    },
+  },
+  title: {
+    style: {
+      color: "#241236",
+    },
+    align: "left",
+  },
+  legend: {
+    align: "left",
+    verticalAlign: "bottom",
+  },
+  tooltip: {
+    backgroundColor: "#FFFFFF",
+    borderRadius: 2,
+  },
+  credits: {
+    enabled: false,
+  },
+  xAxis: {
+    gridLineWidth: 1,
+    gridLineColor: "#F3F3F3",
+    lineColor: "#F3F3F3",
+    minorGridLineColor: "#F3F3F3",
+    tickColor: "#F3F3F3",
+    tickWidth: 1,
+  },
+  yAxis: {
+    gridLineColor: "#F3F3F3",
+    lineColor: "#F3F3F3",
+    minorGridLineColor: "#F3F3F3",
+    tickColor: "#F3F3F3",
+    tickWidth: 1,
+  },
+  plotOptions: {
+    areaspline: {
+      fillOpacity: 0.65,
+    },
+  },
+}
+
+const DateChart = props => {
+  const seriesData = JSON.parse(props.seriesData || props[`series-data`])
+  const yAxisLabel = props.yAxisLabel || props[`y-axis-label`]
+  console.log(props)
+  const config = {
+    chart: {
+      type: "spline",
+      zoomType: "x",
+    },
+    tooltip: {
+      //pointFormat: '{series.name}: <b>{point.percentage:.2f}%</b>'
+    },
+    title: {
+      text: props.title,
+    },
+    xAxis: {
+      type: "datetime",
+      title: {
+        text: "Date",
+      },
+    },
+    yAxis: {
+      title: {
+        text: yAxisLabel,
+      },
+    },
+    series: seriesData.map(series => ({
+      name: series.name,
+      data: series.data.map(edge => [
+        dateToUTC(edge.date),
+        parseFloat(edge.value),
+      ]),
+    })),
+  }
+  return (
+    <div className="gatsby-highcharts">
+      <ReactHighcharts
+        config={{
+          ...highchartsOptions,
+          ...config,
+        }}
+      />
+    </div>
+  )
+}
+
+export default DateChart
