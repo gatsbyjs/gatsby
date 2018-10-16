5fae19820385444dfc35cb1bf03fdbed98b53ced
Harold Angenent <harold@trendwerk.nl>
2018-10-16 10:05:38 +0200

    Clear union types when updating schema

--- packages/gatsby/src/schema/infer-graphql-type.js
+++ packages/gatsby/src/schema/infer-graphql-type.js
@@ -24,7 +24,7 @@ const {
 const DateType = require(`./types/type-date`)
 const FileType = require(`./types/type-file`)
 const is32BitInteger = require(`../utils/is-32-bit-integer`)
-const unionTypes = {}
+const unionTypes = new Map()
 
 import type { GraphQLOutputType } from "graphql"
 import type {
@@ -257,17 +257,15 @@ function inferFromFieldName(value, selector, types): GraphQLFieldConfig<*, *> {
     let type
     // If there's more than one type, we'll create a union type.
     if (fields.length > 1) {
-      const typeName = `Union_${key}`
+      const typeName = `Union_${key}_${fields.map(f => f.name).sort().join(`__`)}`
 
-      if (unionTypes[typeName]) {
-        type = unionTypes[typeName].find(type =>
-          _.isEqual(type.getTypes(), fields.map(f => f.nodeObjectType))
-        )
+      if (unionTypes.has(typeName)) {
+        type = unionTypes.get(typeName)
       }
 
       if (!type) {
         type = new GraphQLUnionType({
-          name: createTypeName(typeName),
+          name: createTypeName(`Union_${key}`),
           description: `Union interface for the field "${key}" for types [${fields
             .map(f => f.name)
             .sort()
@@ -276,7 +274,7 @@ function inferFromFieldName(value, selector, types): GraphQLFieldConfig<*, *> {
           resolveType: data =>
             fields.find(f => f.name == data.internal.type).nodeObjectType,
         })
-        ;(unionTypes[typeName] = unionTypes[typeName] || []).push(type)
+        unionTypes.set(typeName, type)
       }
     } else {
       type = fields[0].nodeObjectType
@@ -427,4 +425,8 @@ function _inferObjectStructureFromNodes(
 
 export function inferObjectStructureFromNodes(options: inferTypeOptions) {
   return _inferObjectStructureFromNodes(options, null)
+}
+
+export function clearUnionTypes() {
+  unionTypes.clear()
 }
