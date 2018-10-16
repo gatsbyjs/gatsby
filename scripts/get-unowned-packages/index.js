cdb9f378bdc4140dbd445c067defad43ded3f14f
Dustin Schau <dustinschau@gmail.com>
2018-10-16 18:33:48 -0500

    refactor: tweak some logic for clarity (thanks @pieh)

--- scripts/get-unowned-packages/index.js
+++ scripts/get-unowned-packages/index.js
@@ -3,13 +3,11 @@ const PackageGraph = require(`@lerna/package-graph`)
 const filterPackages = require(`@lerna/filter-packages`)
 const util = require(`util`)
 const path = require(`path`)
-const { exec, execSync } = require(`child_process`)
-
-const execP = util.promisify(exec)
+const exec = util.promisify(require(`child_process`).exec)
 
 const getPackagesWithReadWriteAccess = async user => {
   const cmd = `npm access ls-packages ${user}`
-  const { stdout } = await execP(cmd)
+  const { stdout } = await exec(cmd)
   const permissions = JSON.parse(stdout)
   return Object.entries(permissions).reduce((lookup, [pkgName, access]) => {
     if (access === `read-write`) {
@@ -38,7 +36,7 @@ module.exports = function getUnownedPackages({
     // infer user from npm whoami
     // set registry because yarn run hijacks registry
     if (!user) {
-      user = await execP(`npm whoami --registry https://registry.npmjs.org`)
+      user = await exec(`npm whoami --registry https://registry.npmjs.org`)
         .then(({ stdout }) => stdout.trim())
         .catch(() => process.exit(1))
     }
@@ -46,9 +44,18 @@ module.exports = function getUnownedPackages({
     const alreadyOwnedPackages = await getPackagesWithReadWriteAccess(user)
 
     const publicGatsbyPackagesWithoutAccess = publicGatsbyPackages.filter(
-      pkg =>
-        !alreadyOwnedPackages[pkg.name] &&
-        !execSync(`npm view ${pkg.name} version`).stderr
+      async pkg => {
+        const isOwned = alreadyOwnedPackages[pkg.name]
+        if (isOwned) {
+          return false
+        }
+
+        const isPublished = await exec(`npm view ${pkg.name} version`)
+          .then(() => true)
+          .catch(() => false)
+
+        return !isPublished
+      }
     )
 
     return {
