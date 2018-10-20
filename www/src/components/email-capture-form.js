247d5c4526ca9bff82408937b21e47a6eeea38b4
Kirankumar Ambati <kiran.chinna12520@gmail.com>
2018-10-20 18:47:44 +0530

    email-capture-form.js: converted stateful component to stateless

--- www/src/components/email-capture-form.js
+++ www/src/components/email-capture-form.js
@@ -3,31 +3,25 @@ import { rhythm } from "../utils/typography"
 import { colors } from "../utils/presets"
 import HubspotForm from "./hubspot-form"
 
-class EmailCaptureForm extends React.Component {
-  render() {
-    const { signupMessage, overrideCSS } = this.props
-
-    return (
-      <div
-        css={{
-          borderTop: `2px solid ${colors.lilac}`,
-          marginTop: rhythm(3),
-          paddingTop: `${rhythm(1)}`,
-          ...overrideCSS,
-        }}
-      >
-        <div>
-          <p>{signupMessage}</p>
-          <HubspotForm
-            portalId="4731712"
-            formId="089352d8-a617-4cba-ba46-6e52de5b6a1d"
-            sfdcCampaignId="701f4000000Us7pAAC"
-          />
-        </div>
-      </div>
-    )
-  }
-}
+const EmailCaptureForm = ({ signupMessage, overrideCSS }) => (
+  <div
+    css={{
+      borderTop: `2px solid ${colors.lilac}`,
+      marginTop: rhythm(3),
+      paddingTop: `${rhythm(1)}`,
+      ...overrideCSS,
+    }}
+  >
+    <div>
+      <p>{signupMessage}</p>
+      <HubspotForm
+        portalId="4731712"
+        formId="089352d8-a617-4cba-ba46-6e52de5b6a1d"
+        sfdcCampaignId="701f4000000Us7pAAC"
+      />
+    </div>
+  </div>
+)
 
 EmailCaptureForm.defaultProps = {
   signupMessage: `Enjoyed this post? Receive the next one in your inbox!`,
