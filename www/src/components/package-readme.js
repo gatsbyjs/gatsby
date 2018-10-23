3115619b0091b5dc48820fb5bf9d9bdc122892db
Aleksandr Filatov <greybax@gmail.com>
2018-10-23 15:04:44 -0700

    update open graph meta tags attributes from name to property (#9302)
    Fix for issue #9288 
    Replaced "name=og:<some-name>" -> "property=og<some-name>."

--- www/src/components/package-readme.js
+++ www/src/components/package-readme.js
@@ -24,10 +24,10 @@ class PackageReadMe extends React.Component {
         <Helmet>
           <title>{packageName}</title>
           <meta name="description" content={excerpt} />
-          <meta name="og:description" content={excerpt} />
+          <meta property="og:description" content={excerpt} />
           <meta name="twitter:description" content={excerpt} />
-          <meta name="og:title" content={packageName} />
-          <meta name="og:type" content="article" />
+          <meta property="og:title" content={packageName} />
+          <meta property="og:type" content="article" />
           <meta name="twitter.label1" content="Reading time" />
           <meta name="twitter:data1" content={`${timeToRead} min read`} />
         </Helmet>
