163d7868749066d67e9cdecf0d3d5fa43881b320
Florian Kissling <florian.kissling@gmail.com>
2018-10-26 03:55:54 +0200

    Use `styled`

--- examples/using-gatsby-image/src/components/floating-image.js
+++ examples/using-gatsby-image/src/components/floating-image.js
@@ -1,40 +1,31 @@
 import React from "react"
 import Img from "gatsby-image"
+import styled from "react-emotion"
 
 import { rhythm, options } from "../utils/typography"
 import presets from "../utils/presets"
 
+const Image = styled(Img)({
+  marginBottom: rhythm(options.blockMarginBottom * 2),
+  marginLeft: rhythm(options.blockMarginBottom * 2),
+  marginRight: -20,
+  float: `right`,
+  [presets.Phablet]: {
+    display: `none`,
+  },
+})
+
+const ImageDesktop = styled(Image)({
+  display: `none`,
+  [presets.Phablet]: {
+    display: `inline-block`,
+  },
+})
+
 const FloatingImage = ({ imageMobile, imageDesktop }) => (
   <React.Fragment>
-    <Img
-      style={{ display: `inherit` }}
-      css={{
-        marginBottom: rhythm(options.blockMarginBottom * 2),
-        marginLeft: rhythm(options.blockMarginBottom * 2),
-        marginRight: -20,
-        float: `right`,
-        "&": {
-          [presets.Phablet]: {
-            display: `none`,
-          },
-        },
-      }}
-      fixed={imageMobile}
-    />
-    <Img
-      style={{ display: `inherit` }}
-      css={{
-        marginBottom: rhythm(options.blockMarginBottom * 2),
-        marginLeft: rhythm(options.blockMarginBottom * 2),
-        float: `right`,
-        display: `none`,
-        [presets.Phablet]: {
-          display: `inline-block`,
-          marginRight: -20,
-        },
-      }}
-      fixed={imageDesktop}
-    />
+    <Image style={{ display: `inherit` }} fixed={imageMobile} />
+    <ImageDesktop style={{ display: `inherit` }} fixed={imageDesktop} />
   </React.Fragment>
 )
 
