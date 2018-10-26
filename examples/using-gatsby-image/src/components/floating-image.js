80fc3232019797a4bef589872ffcea2e9a6099bb
Florian Kissling <florian.kissling@gmail.com>
2018-10-26 04:04:15 +0200

    Doc, order, be explicit

--- examples/using-gatsby-image/src/components/floating-image.js
+++ examples/using-gatsby-image/src/components/floating-image.js
@@ -6,10 +6,11 @@ import { rhythm, options } from "../utils/typography"
 import presets from "../utils/presets"
 
 const Image = styled(Img)({
+  display: `inline-block`,
+  float: `right`,
   marginBottom: rhythm(options.blockMarginBottom * 2),
   marginLeft: rhythm(options.blockMarginBottom * 2),
   marginRight: -20,
-  float: `right`,
   [presets.Phablet]: {
     display: `none`,
   },
@@ -24,6 +25,18 @@ const ImageDesktop = styled(Image)({
 
 const FloatingImage = ({ imageMobile, imageDesktop }) => (
   <React.Fragment>
+    {/*
+        gatsby-image sets a couple of inline styles on its outer
+        container and allows overriding via its `style` prop. One
+        of these inline styles is `display: inline-block´.
+
+        In this case we have two images, one for mobile and one
+        for desktop, and toggle their visibility with `display`,
+        so we need to override the gatsby-image default to make
+        our own styles work.
+
+        https://www.gatsbyjs.org/packages/gatsby-image/#gatsby-image-props
+    */}
     <Image style={{ display: `inherit` }} fixed={imageMobile} />
     <ImageDesktop style={{ display: `inherit` }} fixed={imageDesktop} />
   </React.Fragment>
