9bfdd8cd7579b266f49247cb98fa6a47f86d6bcc
Tanvi Bhakta <tanvibhakta@gmail.com>
2018-10-26 18:55:01 +0530

    Update constant.js to include workbox files
    It is important to cache workbox as they are the files that create the service workers.

--- packages/gatsby-plugin-netlify/src/constants.js
+++ packages/gatsby-plugin-netlify/src/constants.js
@@ -27,6 +27,7 @@ export const SECURITY_HEADERS = {
 export const CACHING_HEADERS = {
   "/static/*": [`Cache-Control: public, max-age=31536000, immutable`],
   "/*\.js": [`Cache-Control: public, max-age=31536000, immutable`],
+  "/workbox*/*\.js": [`Cache-Control: public, max-age=31536000, immutable`],
 }
 
 export const LINK_REGEX = /^(Link: <\/)(.+)(>;.+)/
