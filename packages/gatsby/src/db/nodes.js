9e677244229d5881d4c4cac1f79bbc16e540da49
Anthony Marcar <anthony@moocar.me>
2018-10-26 16:42:26 +1100

    switch for db nodes backend

--- packages/gatsby/src/db/nodes.js
+++ packages/gatsby/src/db/nodes.js
@@ -1,4 +1,16 @@
-const loki = process.env.GATSBY_USE_LOKI_DB
-const nodesDb = loki ? require(`./loki/nodes`) : require(`../redux/nodes`)
+const backend = process.env.GATSBY_DB_NODES || `redux`
+let nodesDb
+switch (backend) {
+  case `redux`:
+    nodesDb = require(`../redux/nodes`)
+    break
+  case `loki`:
+    nodesDb = require(`./loki/nodes`)
+    break
+  default:
+    throw new Error(
+      `Unsupported DB nodes backend (value of env var GATSBY_DB_NODES)`
+    )
+}
 
 module.exports = nodesDb
