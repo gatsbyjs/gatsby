25de3da71fa7105420bfac394a0e7b417c02ff3b
Florian Kissling <florian.kissling@gmail.com>
2018-10-26 22:31:58 +0200

    Don’t link away from example site

--- examples/using-gatsby-image/src/components/lorem.js
+++ examples/using-gatsby-image/src/components/lorem.js
@@ -4,18 +4,17 @@ const Lorem = () => (
   <div>
     <p>
       Ah, I see you have the machine that goes ping. This is my favorite. You
-      see we <a href="http://example.com/">lease it back</a> from the company we
-      sold it to and that way it comes under the monthly current budget and not
-      the capital account.
+      see we lease it back from the company we sold it to and that way it comes
+      under the monthly current budget and not the capital account.
     </p>
     <h2>Lorem ipsum dolor sit amet, consetetur sadipscing elitr</h2>
     <p>
       Proteus ut dis nec exsecrantia data: agrestes, truculenta Peleus. Et
       diffidunt, talia intravit Thaumantias; figere et <em>et</em> qui socio
-      qui, <a href="http://example.com/">tuo servet unda</a> hoc
+      qui, tuo servet unda hoc <strong>classi</strong>? Causam <em>quemque</em>?
+      Subigebant cornibus fibras ut per nare nati, cunctis et
       {` `}
-      <strong>classi</strong>? Causam <em>quemque</em>? Subigebant cornibus
-      fibras ut per nare nati, cunctis et <strong>illa verba</strong> inrita.
+      <strong>illa verba</strong> inrita.
     </p>
     <ol>
       <li>Furori adacto</li>
