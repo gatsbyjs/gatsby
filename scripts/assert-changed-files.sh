26e9f5d09453f277ae1cf826475d3fef048fef06
phacks <nicolas.goutay@gmail.com>
2018-10-27 15:41:35 +0200

    Fix unit test trigger — compare branch instead of last commit

--- scripts/assert-changed-files.sh
+++ scripts/assert-changed-files.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 GREP_PATTERN=$1
 
-FILES_COUNT="$(git diff-tree --no-commit-id --name-only -r $CIRCLE_SHA1 | grep -E "$GREP_PATTERN" | wc -l)"
+FILES_COUNT="$(git diff-tree --no-commit-id --name-only -r $CIRCLE_BRANCH origin/master | grep -E "$GREP_PATTERN" | wc -l)"
+echo "$CIRCLE_BRANCH '$CIRCLE_BRANCH'"
+echo "diffs: '$(git diff-tree --no-commit-id --name-only -r $CIRCLE_BRANCH origin/master)'"
 
 if [ $FILES_COUNT -eq 0 ]; then
   echo "0 files matching '$GREP_PATTERN'; exiting and marking successful."
