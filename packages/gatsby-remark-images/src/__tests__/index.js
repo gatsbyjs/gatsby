6ecf5dfeb64641db37b78b75637e317f75c99d90
Thomas Lent <tcl293@gmail.com>
2018-10-27 23:42:13 -0400

    Add test for tracedSVG option

--- packages/gatsby-remark-images/src/__tests__/index.js
+++ packages/gatsby-remark-images/src/__tests__/index.js
@@ -1,3 +1,7 @@
+const mockTraceSVG = jest.fn(
+  async () => `data:image/svg+xml,%3csvg 'MOCK SVG'%3c/svg%3e`
+)
+
 jest.mock(`gatsby-plugin-sharp`, () => {
   return {
     fluid({ file, args }) {
@@ -11,10 +15,12 @@ jest.mock(`gatsby-plugin-sharp`, () => {
         base64: `data:image/png;base64,iVBORw`,
       })
     },
+    traceSVG: mockTraceSVG,
   }
 })
 
 const Remark = require(`remark`)
+const { Potrace } = require(`potrace`)
 
 const plugin = require(`../`)
 
@@ -206,4 +212,33 @@ test(`it handles goofy nesting properly`, async () => {
   expect(node.type).toBe(`html`)
   expect(node.value).toMatchSnapshot()
   expect(node.value).not.toMatch(`<html>`)
+})
+
+test(`it uses tracedSVG placeholder when enabled`, async () => {
+  const imagePath = `images/my-image.jpeg`
+  const content = `
+
+![image](./${imagePath})
+  `.trim()
+
+  const nodes = await plugin(createPluginOptions(content, imagePath), {
+    tracedSVG: { color: `COLOR_AUTO`, turnPolicy: `TURNPOLICY_LEFT` },
+  })
+
+  expect(nodes.length).toBe(1)
+
+  const node = nodes.pop()
+  expect(node.type).toBe(`html`)
+  expect(node.value).toMatchSnapshot()
+  expect(node.value).not.toMatch(`<html>`)
+  expect(mockTraceSVG).toBeCalledTimes(1)
+
+  expect(mockTraceSVG).toBeCalledWith(
+    expect.objectContaining({
+      // fileArgs cannot be left undefined or traceSVG errors
+      fileArgs: expect.any(Object),
+      // args containing Potrace constants should be translated to their values
+      args: { color: Potrace.COLOR_AUTO, turnPolicy: Potrace.TURNPOLICY_LEFT },
+    })
+  )
 })
