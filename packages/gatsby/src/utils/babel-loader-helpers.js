8fc33c02a1692c04ace1d1cc58608820f35e6a5a
Dustin Schau <DSchau@users.noreply.github.com>
2018-10-29 08:06:57 -0700

    chore: implement babel-preset-gatsby into packages that need it (#9323)
    * Revert "chore: revert extra package changes to publish babel-preset-gatsby first (#9322)"
    
    This reverts commit a392fb44b194883d84471719bba65b651d602b2c.
    
    * chore: implement little fix and add to gtag
    
    * chore: add babel-preset-gatsby-package dep where it's needed
    
    * chore: bump versionin gatsby
    
    * chore: update plop file
    
    * chore: remove src/ for babel-preset-gatsby-package
    
    * test: get tests running for packages without src/ directories
    
    * chore: add babel-preset-gatsby-package for correctness @pieh
    
    * chore: add missing comma

--- packages/gatsby/src/utils/babel-loader-helpers.js
+++ packages/gatsby/src/utils/babel-loader-helpers.js
@@ -48,95 +48,14 @@ const prepareOptions = (babel, resolve = require.resolve) => {
     )
   }
 
-  // Fallback presets/plugins
+  // Fallback preset
   const fallbackPresets = []
-  const fallbackPlugins = []
-
-  let targets
-  if (stage === `build-html`) {
-    targets = {
-      node: `current`,
-    }
-  } else {
-    targets = {
-      browsers: pluginBabelConfig.browserslist,
-    }
-  }
 
   fallbackPresets.push(
-    babel.createConfigItem(
-      [
-        resolve(`@babel/preset-env`),
-        {
-          loose: true,
-          modules: false,
-          useBuiltIns: `usage`,
-          targets,
-        },
-      ],
-      {
-        type: `preset`,
-      }
-    )
-  )
-
-  fallbackPresets.push(
-    babel.createConfigItem(
-      [
-        resolve(`@babel/preset-react`),
-        {
-          useBuiltIns: true,
-          pragma: `React.createElement`,
-          development: stage === `develop`,
-        },
-      ],
-      {
-        type: `preset`,
-      }
-    )
-  )
-
-  fallbackPlugins.push(
-    babel.createConfigItem(
-      [
-        resolve(`@babel/plugin-proposal-class-properties`),
-        {
-          loose: true,
-        },
-      ],
-      {
-        type: `plugin`,
-      }
-    )
-  )
-
-  fallbackPlugins.push(
-    babel.createConfigItem([resolve(`babel-plugin-macros`)], {
-      type: `plugin`,
+    babel.createConfigItem([resolve(`babel-preset-gatsby`)], {
+      type: `preset`,
     })
   )
-
-  fallbackPlugins.push(
-    babel.createConfigItem([resolve(`@babel/plugin-syntax-dynamic-import`)], {
-      type: `plugin`,
-    })
-  )
-
-  fallbackPlugins.push(
-    babel.createConfigItem(
-      [
-        resolve(`@babel/plugin-transform-runtime`),
-        {
-          helpers: true,
-          regenerator: true,
-        },
-      ],
-      {
-        type: `plugin`,
-      }
-    )
-  )
-
   // Go through babel state and create config items for presets/plugins from.
   const reduxPlugins = []
   const reduxPresets = []
@@ -160,7 +79,6 @@ const prepareOptions = (babel, resolve = require.resolve) => {
     reduxPlugins,
     requiredPresets,
     requiredPlugins,
-    fallbackPlugins,
     fallbackPresets,
   ]
 }
