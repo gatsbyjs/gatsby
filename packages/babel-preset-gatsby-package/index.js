98a439b8d15cdb4e31b32630abbc939c81c5bbd1
Alexander Nanberg <alexander.nanberg@strateg.se>
2018-10-30 16:42:14 +0100

    fix(gatsby-plugin-netlify-cms): dynamically import netlify-identity-widget

--- packages/babel-preset-gatsby-package/index.js
+++ packages/babel-preset-gatsby-package/index.js
@@ -43,6 +43,7 @@ function preset(context, options = {}) {
       r(`@babel/plugin-proposal-class-properties`),
       r(`@babel/plugin-proposal-optional-chaining`),
       r(`@babel/plugin-transform-runtime`),
+      r(`@babel/plugin-syntax-dynamic-import`),
     ],
   }
 }
