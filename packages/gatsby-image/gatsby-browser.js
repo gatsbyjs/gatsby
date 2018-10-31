b3e77247be4e33153bff6379abfebb76ddc631e0
Michael Uloth <hello@michaeluloth.com>
2018-10-31 16:58:09 -0400

    Add loadPolyfills condition to gatsby-browser (currently evaluates as undefined, so polyfills never load)

--- packages/gatsby-image/gatsby-browser.js
+++ packages/gatsby-image/gatsby-browser.js
@@ -1,17 +1,22 @@
 "use strict"
 
 exports.onClientEntry = () => {
-  // IntersectionObserver polyfill for gatsby-image (Safari, IE)
-  if (typeof window.IntersectionObserver === `undefined`) {
-    import(`intersection-observer`)
-  }
+  // TODO: remove after successfully accessing this value
+  console.log(`process.env.GATSBY_IMAGE_LOAD_POLYFILLS`, process.env.GATSBY_IMAGE_LOAD_POLYFILLS)
 
-  // Object-fit/Object-position polyfill for gatsby-image (IE)
-  const testImg = document.createElement(`img`)
-  if (
-    typeof testImg.style.objectFit === `undefined` ||
-    typeof testImg.style.objectPosition === `undefined`
-  ) {
-    import(`object-fit-images`).then(ObjectFitImages => ObjectFitImages())
+  if (process.env.GATSBY_IMAGE_LOAD_POLYFILLS) {
+    // IntersectionObserver polyfill for gatsby-image (Safari, IE)
+    if (typeof window.IntersectionObserver === `undefined`) {
+      import(`intersection-observer`)
+    }
+
+    // Object-fit/Object-position polyfill for gatsby-image (IE)
+    const testImg = document.createElement(`img`)
+    if (
+      typeof testImg.style.objectFit === `undefined` ||
+      typeof testImg.style.objectPosition === `undefined`
+    ) {
+      import(`object-fit-images`).then(ObjectFitImages => ObjectFitImages())
+    }
   }
 }
