c5f98430c7e0b202ab6a0ee6c4fa9b2eb0a9a6fd
Dustin Schau <dustinschau@gmail.com>
2018-11-01 16:42:39 -0500

    chore: make extensible (not just examples)

--- scripts/sync-repos.sh
+++ scripts/sync-repos.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-FOLDER_PATTERN="${1:-examples}" # grab first argument, otherwise use examples folder
+BASE_FOLDER="${1:-examples}" # grab first argument, otherwise use examples folder
 
 CURRENT_COMMIT="$(git log --format="%H" -n 1)"
 CURRENT_COMMIT_MESSAGE="$(git log --format="%s" -n 1)"
 LAST_COMMIT="$(git log --format="%H" -n 2 | tail -1)"
 
-CHANGED_FILES="$(git diff-tree --no-commit-id --name-only -r $CURRENT_COMMIT $LAST_COMMIT | grep -E "$FOLDER_PATTERN/*")"
+CHANGED_FILES="$(git diff-tree --no-commit-id --name-only -r $CURRENT_COMMIT $LAST_COMMIT | grep -E "$BASE_FOLDER/*")"
 
 # TODO: figure out secure way to push to github
 for file in $CHANGED_FILES
 do
   FOLDER="$(echo $file | cut -d'/' -f2)"
-  cd examples/$FOLDER
+  cd $BASE_FOLDER/$FOLDER
 
   git init
   git remote add origin git@github.com:gatsbyjs/$FOLDER
