c632a86ebfda60ef5c564bd18b407871e3f95233
Kirankumar Ambati <kiran.chinna12520@gmail.com>
2018-11-01 20:39:52 +0530

    chore: removed unused imports and changes related to code formatting (#9610)
    <!--
      Q. Which branch should I use for my pull request?
      A. Use `master` branch (probably).
    
      Q. Which branch if my change is a bug fix for Gatsby v1?
      A. In this case, you should use the `v1` branch
    
      Q. Which branch if I'm still not sure?
      A. Use `master` branch. Ask in the PR if you're not sure and a Gatsby maintainer will be happy to help :)
    
      Note: We will only accept bug fixes for Gatsby v1. New features should be added to Gatsby v2.
    
      Learn more about contributing: https://www.gatsbyjs.org/docs/how-to-contribute/
    -->

--- www/src/components/ecosystem/ecosystem-board.js
+++ www/src/components/ecosystem/ecosystem-board.js
@@ -23,46 +23,43 @@ const EcosystemBoardRoot = styled("div")`
 const EcosystemBoard = ({
   icons: { plugins: PluginsIcon, starters: StartersIcon },
   starters,
-  plugins,
-  background,
-}) => {
-  return (
-    <EcosystemBoardRoot>
-      <EcosystemSection
-        title="Plugins"
-        description="With 429 plugins, lorem ipsum sunt we need some proper copy here pulling in data from your favorite source is only a few mouseclicks away."
-        subTitle="Featured Plugins"
-        icon={PluginsIcon}
-        links={[
-          { label: "Browse Plugins", to: "/plugins/" },
-          {
-            label: "Plugin Authoring",
-            to: "/docs/plugin-authoring/",
-            secondary: true,
-          },
-          { label: "Plugin Docs", to: "/docs/plugins/", secondary: true },
-        ]}
-        featuredItems={plugins}
-      />
-      <EcosystemSection
-        title="Starters"
-        description="Starters are boilerplate Gatsby sites maintained by the community which give you a headstart for your Gatsby project."
-        subTitle="Featured Starters"
-        icon={StartersIcon}
-        links={[
-          { label: "Browse Starters", to: "/starters/" },
-          { label: "Starter Docs", to: "/docs/starters/", secondary: true },
-        ]}
-        featuredItems={starters}
-      />
-      <EcosystemSection
-        title="External Resources"
-        description="More awesome Gatsby content, created by the community."
-        links={[{ label: "Browse Resources", to: "/docs/awesome-gatsby/" }]}
-      />
-    </EcosystemBoardRoot>
-  )
-}
+  plugins
+}) => (
+  <EcosystemBoardRoot>
+    <EcosystemSection
+      title="Plugins"
+      description="With 429 plugins, lorem ipsum sunt we need some proper copy here pulling in data from your favorite source is only a few mouseclicks away."
+      subTitle="Featured Plugins"
+      icon={PluginsIcon}
+      links={[
+        { label: "Browse Plugins", to: "/plugins/" },
+        {
+          label: "Plugin Authoring",
+          to: "/docs/plugin-authoring/",
+          secondary: true,
+        },
+        { label: "Plugin Docs", to: "/docs/plugins/", secondary: true },
+      ]}
+      featuredItems={plugins}
+    />
+    <EcosystemSection
+      title="Starters"
+      description="Starters are boilerplate Gatsby sites maintained by the community which give you a headstart for your Gatsby project."
+      subTitle="Featured Starters"
+      icon={StartersIcon}
+      links={[
+        { label: "Browse Starters", to: "/starters/" },
+        { label: "Starter Docs", to: "/docs/starters/", secondary: true },
+      ]}
+      featuredItems={starters}
+    />
+    <EcosystemSection
+      title="External Resources"
+      description="More awesome Gatsby content, created by the community."
+      links={[{ label: "Browse Resources", to: "/docs/awesome-gatsby/" }]}
+    />
+  </EcosystemBoardRoot>
+)
 
 EcosystemBoard.propTypes = {
   icons: PropTypes.object,
