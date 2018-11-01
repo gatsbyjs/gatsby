390de8d7c75ad365e90aa79b9b8373564891ab40
Dustin Schau <dustinschau@gmail.com>
2018-11-01 16:32:12 -0500

    feat: add script and circleci job to sync examples to read-only repos
    WIP WIP WIP WIP
    
    This simple script will check the most recent commit's file changes
    (i.e. on master push) vs. the previous state of the repo. It then
    iterates over those files (if they match a given pattern, e.g.
    examples/*) and will push to those repo clones.
    
    This will give us some nice benefits, e.g. tracking on forks/stars/etc.
    for our examples, starters, etc. while keeping the code in our monorepo,
    but also providing a means for others to access those repos outside of
    the monorepo

---
+++ scripts/sync-repos.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+FOLDER_PATTERN="${1:-examples}" # grab first argument, otherwise use examples folder
+
+CURRENT_COMMIT="$(git log --format="%H" -n 1)"
+CURRENT_COMMIT_MESSAGE="$(git log --format="%s" -n 1)"
+LAST_COMMIT="$(git log --format="%H" -n 2 | tail -1)"
+
+CHANGED_FILES="$(git diff-tree --no-commit-id --name-only -r $CURRENT_COMMIT $LAST_COMMIT | grep -E "$FOLDER_PATTERN/*")"
+
+# TODO: figure out secure way to push to github
+for file in $CHANGED_FILES
+do
+  FOLDER="$(echo $file | cut -d'/' -f2)"
+  cd examples/$FOLDER
+
+  git init
+  git remote add origin git@github.com:gatsbyjs/$FOLDER
+  git fetch origin/master
+  git add .
+  git commit -m "$CURRENT_COMMIT_MESSAGE"
+  git push origin master
+done
