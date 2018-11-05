05426d767dbefeec8fc2dac3208f0a701de24d46
Julian <julian@pizzapizza.io>
2018-11-05 19:14:31 +0100

    order pages according to the number of url segments in matchPath in order to make nested matchPaths work

--- packages/gatsby/src/internal-plugins/query-runner/pages-writer.js
+++ packages/gatsby/src/internal-plugins/query-runner/pages-writer.js
@@ -39,7 +39,7 @@ const writePages = async () => {
     // Ensure pages keep the same sorting through builds
     // and sort pages with matchPath to end so explicit routes
     // will match before general.
-    .sortBy(p => `${p.matchPath ? 1 : 0}${p.path}`)
+    .sortBy(p => `${p.matchPath ? 9999 - p.matchPath.split(`/`).length : `0000`}${p.path}`)
     .value()
   const newHash = crypto
     .createHash(`md5`)
