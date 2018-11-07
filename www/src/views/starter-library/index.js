56fe6d71834727693e6d7f1a2e156c1b26271064
Alexander Nanberg <alexander.nanberg@strateg.se>
2018-11-07 08:11:20 +0100

    Update formatting

--- www/src/views/starter-library/index.js
+++ www/src/views/starter-library/index.js
@@ -16,8 +16,8 @@ class StarterLibraryPage extends Component {
       urlState.s !== ``
         ? urlState.s // if theres a search term
         : urlState.d && !Array.isArray(urlState.d)
-          ? urlState.d // if theres a single dependency
-          : `Showcase` // if no search term or single dependency
+        ? urlState.d // if theres a single dependency
+        : `Showcase` // if no search term or single dependency
     return (
       <Layout location={location}>
         <Helmet>
