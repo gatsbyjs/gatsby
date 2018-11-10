117b3cd010e0503b94f18f183f251190ab0ba268
Asko Soukka <asko.soukka@iki.fi>
2018-11-10 14:53:25 +0200

    feat(gatsby-plugin-manifest): add option to generate apple-touch-icons links (#7256)
    Fixes #5887 /cc  @seaneking @moonmeister 
    
    I'd like to have gatsby-plugin-manifest to generate apple-touch-icon meta tags (links) for me.
    
    Would it be simple enough to generate tags under the same rules as for favicon (only when the icon is being generated by the plugin)?
    
    This has been required to be a new major release, but is that v2 (still in beta) or v3?
    
    The initial commit https://github.com/datakurre/gatsby/commit/4cf3ba4e3f76d6fd6fef888f1e92895ba999f006  should be the smallest possible change, but I'd probably like to move the code in a separate module where `withPrefix` is passed as a function argument and could be mocked in tests and make them simpler.
    
    * [x] discussion
    * [x] tests
    * [x] docs update?

---
+++ packages/gatsby-plugin-manifest/src/__tests__/gatsby-ssr.js
@@ -0,0 +1,71 @@
+// const { shallow } = require(`enzyme`)
+const { onRenderBody } = require(`../gatsby-ssr`)
+
+let headComponents
+const setHeadComponents = args => (headComponents = headComponents.concat(args))
+
+const ssrArgs = {
+  setHeadComponents,
+}
+
+describe(`gatsby-plugin-manifest`, () => {
+  beforeEach(() => {
+    global.__PATH_PREFIX__ = ``
+    headComponents = []
+  })
+
+  it(`Adds "shortcut icon" and "manifest" links and "theme_color" meta tag to head`, () => {
+    onRenderBody(ssrArgs, { icon: true, theme_color: `#000000` })
+    expect(headComponents).toMatchSnapshot()
+  })
+
+  describe(`Creates legacy apple touch links if opted in`, () => {
+    it(`Using default set of icons`, () => {
+      onRenderBody(ssrArgs, {
+        icon: true,
+        theme_color: `#000000`,
+        legacy: true,
+      })
+      expect(headComponents).toMatchSnapshot()
+    })
+
+    it(`Using user specified list of icons`, () => {
+      onRenderBody(ssrArgs, {
+        icon: true,
+        theme_color: `#000000`,
+        legacy: true,
+        icons: [
+          {
+            src: `/favicons/android-chrome-48x48.png`,
+            sizes: `48x48`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      })
+      expect(headComponents).toMatchSnapshot()
+    })
+  })
+
+  it(`Creates href attributes using pathPrefix`, () => {
+    global.__PATH_PREFIX__ = `/path-prefix`
+
+    onRenderBody(ssrArgs, {
+      icon: true,
+      theme_color: `#000000`,
+      legacy: true,
+    })
+
+    headComponents
+      .filter(component => component.type === `link`)
+      .forEach(component => {
+        expect(component.props.href).toEqual(
+          expect.stringMatching(/^\/path-prefix\//)
+        )
+      })
+  })
+})
