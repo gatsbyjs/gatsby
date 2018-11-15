627a9800fcc4a55e3616f9ea6ea58ada933ab779
Michal Piechowiak <misiek.piechowiak@gmail.com>
2018-11-15 13:57:50 +0100

    use Map instead of object and delete sheet when no longer needed

--- packages/gatsby-plugin-styled-components/src/gatsby-ssr.js
+++ packages/gatsby-plugin-styled-components/src/gatsby-ssr.js
@@ -1,18 +1,19 @@
 import React from "react"
 import { ServerStyleSheet, StyleSheetManager } from "styled-components"
 
-const sheetByPathname = {}
+const sheetByPathname = new Map()
 
 // eslint-disable-next-line react/prop-types,react/display-name
 exports.wrapRootElement = ({ element, pathname }) => {
-  const sheet = (sheetByPathname[pathname] = new ServerStyleSheet())
+  const sheet = new ServerStyleSheet()
+  sheetByPathname.set(pathname, sheet)
   return <StyleSheetManager sheet={sheet.instance}>{element}</StyleSheetManager>
 }
 
 exports.onRenderBody = ({ setHeadComponents, pathname }) => {
-  if (!sheetByPathname[pathname]) {
-    sheetByPathname[pathname] = new ServerStyleSheet()
+  const sheet = sheetByPathname.get(pathname)
+  if (sheet) {
+    setHeadComponents([sheet.getStyleElement()])
+    sheetByPathname.delete(pathname)
   }
-
-  setHeadComponents([sheetByPathname[pathname].getStyleElement()])
 }
