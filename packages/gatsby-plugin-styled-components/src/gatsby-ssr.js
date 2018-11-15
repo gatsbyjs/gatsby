a9f4b233f09980462ef5f02765d51c9a535091ae
Loïc Mahieu <mahieuloic@gmail.com>
2018-11-15 11:55:48 +0100

    fix(gatsby-plugin-styled-components): fix global styles pollution (#9922)
    Fix #9922
    
    `ServerStyleSheet` should be specific to each pages in order to not pollutes the other. Theses changes removes the "global" stylesheet and use the `pathname` for identify each pages.

--- packages/gatsby-plugin-styled-components/src/gatsby-ssr.js
+++ packages/gatsby-plugin-styled-components/src/gatsby-ssr.js
@@ -1,13 +1,18 @@
 import React from "react"
 import { ServerStyleSheet, StyleSheetManager } from "styled-components"
 
-const sheet = new ServerStyleSheet()
+const sheetByPathname = {}
 
 // eslint-disable-next-line react/prop-types,react/display-name
-exports.wrapRootElement = ({ element }) => (
-  <StyleSheetManager sheet={sheet.instance}>{element}</StyleSheetManager>
-)
+exports.wrapRootElement = ({ element, pathname }) => {
+  const sheet = (sheetByPathname[pathname] = new ServerStyleSheet())
+  return <StyleSheetManager sheet={sheet.instance}>{element}</StyleSheetManager>
+}
 
-exports.onRenderBody = ({ setHeadComponents }) => {
-  setHeadComponents([sheet.getStyleElement()])
+exports.onRenderBody = ({ setHeadComponents, pathname }) => {
+  if (!sheetByPathname[pathname]) {
+    sheetByPathname[pathname] = new ServerStyleSheet()
+  }
+
+  setHeadComponents([sheetByPathname[pathname].getStyleElement()])
 }
