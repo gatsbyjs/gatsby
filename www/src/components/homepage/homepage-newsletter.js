5ee473bb8844fb218c5233376d3daf222edb954e
greglobinski <greglobinski@gmail.com>
2018-11-18 22:05:18 +0100

    feat(www): mobile style for Homepage Newsletter section

--- www/src/components/homepage/homepage-newsletter.js
+++ www/src/components/homepage/homepage-newsletter.js
@@ -1,22 +1,84 @@
 import React from "react"
 import styled from "react-emotion"
 
+import HomepageSection, { Name, Title } from "./homepage-section"
 import EmailCaptureForm from "../../components/email-capture-form"
 
-import presets from "../../utils/presets"
-import { rhythm } from "../../utils/typography"
+import { NewsletterFormOrnament } from "../../assets/ornaments"
 
-const HomepageNewsletterRoot = styled(`section`)`
-  background: #fff;
-  margin: 0 -${rhythm(presets.gutters.default / 2)};
-  padding: ${rhythm(presets.gutters.default / 2)};
-  width: calc(100% + ${rhythm(presets.gutters.default)});
+import { rhythm, options } from "../../utils/typography"
+import presets, { colors } from "../../utils/presets"
+
+const HomepageNewsletterRoot = styled(HomepageSection)`
+  padding: calc(${rhythm(presets.gutters.default)} * 2);
+  padding-bottom: calc(${rhythm(presets.gutters.default)} * 3);
+  position: relative;
+  z-index: 0;
+
+  ${Name} {
+    font-fa1mily: ${options.headerFontFamily.join(`,`)};
+    font-size: 0.875rem;
+    margin-left: 0;
+    margin-bottom: 0.2rem;
+    text-transform: uppercase;
+  }
+
+  ${Title} {
+    font-size: 1.25rem;
+    line-height: 1.3;
+    margin-bottom: 1.25rem;
+  }
+
+  :before {
+    border: 1px solid ${colors.ui.light};
+    border-radius: ${presets.radiusLg}px;
+    bottom: ${rhythm(presets.gutters.default * 1.5)};
+    content: "";
+    left: ${rhythm(presets.gutters.default / 2)};
+    position: absolute;
+    right: ${rhythm(presets.gutters.default / 2)};
+    top: ${rhythm(presets.gutters.default / 2)};
+    z-index: -1;
+  }
+
+  :after {
+    border-radius: 0 0 ${presets.radiusLg}px ${presets.radiusLg}px;
+    background: ${colors.ui.whisper}
+      repeating-linear-gradient(
+        135deg,
+        ${colors.lemon},
+        ${colors.lemon} 20px,
+        transparent 20px,
+        transparent 40px,
+        ${colors.mint} 40px,
+        ${colors.mint} 60px,
+        transparent 60px,
+        transparent 80px
+      );
+    bottom: ${rhythm(presets.gutters.default * 1.5)};
+    content: "";
+    height: 8px;
+    left: ${rhythm(presets.gutters.default / 2)};
+    right: ${rhythm(presets.gutters.default / 2)};
+    position: absolute;
+    z-index: -2;
+  }
 `
 
-const HomepageNewsletter = ({ posts }) => (
-  <HomepageNewsletterRoot>
+const Ornament = styled(`span`)`
+  left: calc(${rhythm(presets.gutters.default / 2)} - 4px);
+  position: absolute;
+  top: calc(${rhythm(presets.gutters.default / 2)} - 8px);
+`
+
+const HomepageNewsletter = () => (
+  <HomepageNewsletterRoot
+    sectionName="The Gatsby Newsletter"
+    title="Keep up with the latest things Gatsby!"
+  >
+    <Ornament dangerouslySetInnerHTML={{ __html: NewsletterFormOrnament }} />
     <EmailCaptureForm
-      newStyle={true}
+      isHomepage={true}
       confirmMessage="Success! You have been subscribed to the Gatsby newsletter. Expect to see a newsletter in your inbox each Wednesday (or the equivalent of US Wednesday in your time zone)!"
     />
   </HomepageNewsletterRoot>
--- www/src/components/homepage/homepage-section.js
+++ www/src/components/homepage/homepage-section.js
@@ -14,7 +14,7 @@ const HomepageSectionRoot = styled(`section`)`
   background: ${props => (props.inverse ? colors.gatsby : `#fff`)};
   color: ${props => (props.inverse ? colors.ui.light : colors.gatsbyDark)};
   margin: 0 -${rhythm(presets.gutters.default / 2)};
-  padding: ${rhythm(2)} ${rhythm(presets.gutters.default / 2)};
+  padding: ${rhythm(1)} ${rhythm(presets.gutters.default / 2)};
   width: calc(100% + ${rhythm(presets.gutters.default)});
 
   ${presets.Hd} {
@@ -37,7 +37,7 @@ const Header = styled(`header`)`
   }
 `
 
-const Name = styled(`h3`)`
+export const Name = styled(`h3`)`
   align-items: center;
   color: ${props => (props.inverse ? colors.ui.light : colors.lilac)};
   display: flex;
@@ -67,7 +67,7 @@ const Icon = styled(`span`)`
   }
 `
 
-const Title = styled(`h1`)`
+export const Title = styled(`h1`)`
   color: ${props => (props.inverse ? colors.lemon : colors.gatsby)};
   font-size: 1.75rem;
   margin: 0;
@@ -103,10 +103,11 @@ const HomepageSection = ({
   introduction,
   inverseStyle,
   links,
+  className,
 }) => (
-  <HomepageSectionRoot inverse={inverseStyle}>
-    <Header>
-      {sectionName && (
+  <HomepageSectionRoot inverse={inverseStyle} className={className}>
+    {sectionName && (
+      <Header>
         <Name inverse={inverseStyle}>
           {sectionIcon && (
             <Icon
@@ -116,29 +117,31 @@ const HomepageSection = ({
           )}
           {sectionName}
         </Name>
-      )}
-      {title && <Title inverse={inverseStyle}>{title}</Title>}
-      {introduction && (
-        <Introduction inverse={inverseStyle}>{introduction}</Introduction>
-      )}
-      <Actions>
-        {links.map((item, idx) => {
-          const { to, label, icon: Icon, secondary } = item
+        {title && <Title inverse={inverseStyle}>{title}</Title>}
+        {introduction && (
+          <Introduction inverse={inverseStyle}>{introduction}</Introduction>
+        )}
+        {links && (
+          <Actions>
+            {links.map((item, idx) => {
+              const { to, label, icon: Icon, secondary } = item
 
-          return (
-            <Button
-              key={label}
-              to={to}
-              small
-              ondark={inverseStyle ? true : false}
-              secondary={secondary}
-            >
-              {label} {Icon && <Icon />}
-            </Button>
-          )
-        })}
-      </Actions>
-    </Header>
+              return (
+                <Button
+                  key={label}
+                  to={to}
+                  small
+                  ondark={inverseStyle ? true : false}
+                  secondary={secondary}
+                >
+                  {label} {Icon && <Icon />}
+                </Button>
+              )
+            })}
+          </Actions>
+        )}
+      </Header>
+    )}
     {children}
   </HomepageSectionRoot>
 )
