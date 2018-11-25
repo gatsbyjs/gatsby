d8a597f99b7bd44b52ea9106f83c6f4ef115b1d7
Christopher Biscardi <biscarch@sketcht.com>
2018-11-24 23:59:36 -0800

    fix if
    update snapshots

--- packages/gatsby/src/internal-plugins/webpack-theme-component-shadowing/gatsby-node.js
+++ packages/gatsby/src/internal-plugins/webpack-theme-component-shadowing/gatsby-node.js
@@ -1,22 +1,17 @@
 const GatsbyThemeComponentShadowingResolverPlugin = require(`.`)
 
-exports.onCreateWebpackConfig = ({
-  store,
-  stage,
-  getConfig,
-  rules,
-  loaders,
-  actions,
-}) => {
+exports.onCreateWebpackConfig = (
+  { store, stage, getConfig, rules, loaders, actions },
+  pluginOptions
+) => {
   const { config, program } = store.getState()
-  const themes = config.__experimentalThemes.map(({ resolve }) => resolve)
 
-  if (themes) {
+  if (config.__experimentalThemes) {
     actions.setWebpackConfig({
       resolve: {
         plugins: [
           new GatsbyThemeComponentShadowingResolverPlugin({
-            themes,
+            themes: config.__experimentalThemes.map(({ resolve }) => resolve),
             projectRoot: program.directory,
           }),
         ],
