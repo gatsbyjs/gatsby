3b9598a155209ff9e3d459907100fb2b98039c54
Xiaoru Li <dev@xiaoru.li>
2018-11-27 17:53:14 +0100

    Catch error and panic only on build

--- packages/gatsby-remark-katex/src/index.js
+++ packages/gatsby-remark-katex/src/index.js
@@ -2,21 +2,29 @@ const visit = require(`unist-util-visit`)
 const katex = require(`katex`)
 const remarkMath = require(`remark-math`)
 
-module.exports = ({ markdownAST }) => {
+module.exports = ({ markdownAST, reporter }) => {
   visit(markdownAST, `inlineMath`, node => {
     node.type = `html`
-    node.value = katex.renderToString(node.value, {
-      displayMode: false,
-      throwOnError: false,
-    })
+
+    try {
+      node.value = katex.renderToString(node.value, {
+        displayMode: false,
+      })
+    } catch (err) {
+      reporter.panicOnBuild(`KaTeX rendering error: ${err.message}`)
+    }
   })
 
   visit(markdownAST, `math`, node => {
     node.type = `html`
-    node.value = katex.renderToString(node.value, {
-      displayMode: true,
-      throwOnError: false,
-    })
+
+    try {
+      node.value = katex.renderToString(node.value, {
+        displayMode: true,
+      })
+    } catch (err) {
+      reporter.panicOnBuild(`KaTeX rendering error: ${err.message}`)
+    }
   })
 }
 
