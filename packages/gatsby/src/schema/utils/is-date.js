143a1ac11e8465369dc2adfb07fbd014ade8bd25
Stefan Probst <stefanprobst@kolabnow.com>
2018-11-29 00:26:36 +0100

    Document Date constructor weirdness

--- packages/gatsby/src/schema/utils/is-date.js
+++ packages/gatsby/src/schema/utils/is-date.js
@@ -1,6 +1,8 @@
-// Maybe FIXME: Currently, number strings like "404" are interpreted
-// as a year, i.e. a Date. This is problematic for File.name - but,
-// `gatsby-source-filesystem` should provide `typeDefs` anyway.
+// FIXME: Currently, number strings like "404" are interpreted
+// as a year, i.e. a Date.
+// FIXME: Interesting weirdness: "13"-"31" are invalid Dates.
+// Not entirely sure what the [spec](http://www.ecma-international.org/ecma-262/6.0/#sec-date-time-string-format)
+// says here.
 const isDate = string => isFinite(new Date(string).getTime())
 
 module.exports = isDate
