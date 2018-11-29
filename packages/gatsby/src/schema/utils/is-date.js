6dcc7da5f6c438ef1d41c888f1f7bc2da1b5fef4
Stefan Probst <stefanprobst@kolabnow.com>
2018-11-29 01:28:39 +0100

    Fix isDate

--- packages/gatsby/src/schema/utils/is-date.js
+++ packages/gatsby/src/schema/utils/is-date.js
@@ -1,8 +1,13 @@
-// FIXME: Currently, number strings like "404" are interpreted
-// as a year, i.e. a Date.
-// FIXME: Interesting weirdness: "13"-"31" are invalid Dates.
-// Not entirely sure what the [spec](http://www.ecma-international.org/ecma-262/6.0/#sec-date-time-string-format)
-// says here.
-const isDate = string => isFinite(new Date(string).getTime())
+const isDate = string => {
+  // When encountering a numberstring like "404" or "13", the
+  // [ES spec](http://www.ecma-international.org/ecma-262/6.0/#sec-date-time-string-format)
+  // only specifies behavior for 4-digit YYYY years. Numberstrings in the
+  // formats YYY, YY, Y are out-of-spec. V8 interprets most as valid dates,
+  // except "13"-"31" (Spidermonkey is stricter and disallows all).
+  const num = parseInt(string, 10)
+  if (isFinite(num) && num < 1000) return false
+
+  return isFinite(new Date(string).getTime())
+}
 
 module.exports = isDate
