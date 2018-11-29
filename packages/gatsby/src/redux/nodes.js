4bcca2a529aae89707c64e09b5aa0abaa19cf35a
Anthony Marcar <Anthony.Marcar@gmail.com>
2018-11-29 23:57:08 +1100

    feat(gatsby): add lokijs nodes db implementation (#9919)
    This PR adds a feature flag `GATSBY_DB_NODES` that can be used to change the storage engine for the gatsby data layer (`nodes`). 
    
    - `redux` (default) which uses the existing redux implementation, and `sift` for querying. Or, you can use 
    - `loki` which uses the loki in-memory database to store and query.
    
    This PR re-implements functionality in #9338, but with all tests passing and addressing previous feedback. It should also be easier to review since it builds on several refactorings. 
    
    Some things to note:
    
    1. I [submitted a PR to lokijs](https://github.com/techfort/LokiJS/pull/718) which still hasn't been merged, though the author says he'll start working on it soon. Therefore, in in interim, I've published [@moocar/lokijs](https://www.npmjs.com/package/@moocar/lokijs). 
    1. I haven't implemented auto indexing of query fields yet. I'll attack that next.
    1. I suggest we ask the community to try out the feature flag once it's merged to get feedback. All the tests pass, but this is such a big change that we'll want to test it gradually
    1. While loki uses the same mongo query language as sift, they do have different behavior. Most of my time on this PR was spent ensuring that loki behaves **exactly** like sift. See [db/loki/nodes-query.js](https://github.com/gatsbyjs/gatsby/blob/cddbe893a4ce638babb1cbe5e5da4c13b6f5e57d/packages/gatsby/src/db/loki/nodes-query.js). But there's a chance a few edge cases have slipped through the cracks.
    1. the feature flag works with the tests too `GATSBY_DB_NODES=loki yarn test`. We should perhaps look into running this on all PRs

--- packages/gatsby/src/redux/nodes.js
+++ packages/gatsby/src/redux/nodes.js
@@ -1,5 +1,3 @@
-const _ = require(`lodash`)
-const Promise = require(`bluebird`)
 const { store } = require(`./index`)
 
 /**
@@ -43,36 +41,6 @@ exports.hasNodeChanged = (id, digest) => {
     return true
   } else {
     return node.internal.contentDigest !== digest
-  }
-}
-
-/**
- * Get content for a node from the plugin that created it.
- *
- * @param {Object} node
- * @returns {promise}
- */
-exports.loadNodeContent = node => {
-  if (_.isString(node.internal.content)) {
-    return Promise.resolve(node.internal.content)
-  } else {
-    return new Promise(resolve => {
-      // Load plugin's loader function
-      const plugin = store
-        .getState()
-        .flattenedPlugins.find(plug => plug.name === node.internal.owner)
-      const { loadNodeContent } = require(plugin.resolve)
-      if (!loadNodeContent) {
-        throw new Error(
-          `Could not find function loadNodeContent for plugin ${plugin.name}`
-        )
-      }
-
-      return loadNodeContent(node).then(content => {
-        // TODO update node's content field here.
-        resolve(content)
-      })
-    })
   }
 }
 
