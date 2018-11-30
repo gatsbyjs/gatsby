28cbda70bcf8fe36e621677ff3d24f8b7dc2d8cc
Justin Hall <justin.r.hall@gmail.com>
2018-11-30 12:14:15 -0700

    feat(gatsby-plugin-emotion): update plugin for emotion v10

--- packages/gatsby-plugin-emotion/src/gatsby-node.js
+++ packages/gatsby-plugin-emotion/src/gatsby-node.js
@@ -1,10 +1,31 @@
-exports.onCreateBabelConfig = ({ actions }, pluginOptions) => {
+exports.onCreateBabelConfig = ({ actions, stage }, pluginOptions) => {
+  actions.setBabelPreset({
+    name: `@babel/preset-react`,
+    stage,
+    options: {
+      useBuiltIns: true,
+      pragma: `___EmotionJSX`,
+      development: stage === `develop`,
+    },
+  })
+
+  actions.setBabelPlugin({
+    name: `babel-plugin-jsx-pragmatic`,
+    stage,
+    options: {
+      export: `jsx`,
+      module: `@emotion/core`,
+      import: `___EmotionJSX`,
+    },
+  })
+
   actions.setBabelPlugin({
     name: `babel-plugin-emotion`,
+    stage,
     options: {
+      cssPropOptimization: true,
       sourceMap: process.env.NODE_ENV !== `production`,
       autoLabel: process.env.NODE_ENV !== `production`,
-      hoist: process.env.NODE_ENV === `production`,
       ...(pluginOptions ? pluginOptions : {}),
     },
   })
