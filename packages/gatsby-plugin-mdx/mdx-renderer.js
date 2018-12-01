42d26bfdd56e821cdf8b4ba43ea7d273baa9d691
Christopher Biscardi <chris@christopherbiscardi.com>
2018-12-01 14:21:37 -0800

    misc upgrades and stuff for mdx-js/mdx v16 (#194)

--- packages/gatsby-plugin-mdx/mdx-renderer.js
+++ packages/gatsby-plugin-mdx/mdx-renderer.js
@@ -1,11 +1,18 @@
+const React = require("react");
+const { MDXTag } = require("@mdx-js/tag");
 const { withMDXComponents } = require("@mdx-js/tag/dist/mdx-provider");
 const { withMDXScope } = require("./context");
 
 module.exports = withMDXScope(
   withMDXComponents(({ scope = {}, components = {}, children, ...props }) => {
+    if (!children) {
+      return null;
+    }
     const fullScope = {
-      components,
-      props,
+      // React and MDXTag are here just in case the user doesn't pass them in
+      // in a manual usage of the renderer
+      React,
+      MDXTag,
       ...scope
     };
 
@@ -14,7 +21,7 @@ module.exports = withMDXScope(
     const values = keys.map(key => fullScope[key]);
     const fn = new Function("_fn", ...keys, `${children}`);
 
-    const end = fn({}, ...values)({ components, ...props });
-    return end;
+    const End = fn({}, ...values);
+    return React.createElement(End, { components, ...props });
   })
 );
