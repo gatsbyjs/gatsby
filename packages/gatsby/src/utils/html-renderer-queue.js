0ed6edc18b6bdda6dfd827e2dd2dcb20b555e2ab
Dominic Fallows <dominic.fallows@ii.co.uk>
2018-12-03 19:10:46 +0000

    Create utilty function for cpuCount, align sharp's concurrency with Gatsb'y cpuCount handling

--- packages/gatsby/src/utils/html-renderer-queue.js
+++ packages/gatsby/src/utils/html-renderer-queue.js
@@ -2,41 +2,14 @@ const Promise = require(`bluebird`)
 const convertHrtime = require(`convert-hrtime`)
 const Worker = require(`jest-worker`).default
 const { chunk } = require(`lodash`)
+const cpuCount = require(`./cpu-count`)()
 
-const workerPool = () => {
-  // Default number of workers === physical CPU count,
-  // or default to 1 if we can't detect
-  let numOfWorkers = require(`physical-cpu-count`) || 1
-
-  if (process.env.GATSBY_CPU_COUNT) {
-    switch (typeof process.env.GATSBY_CPU_COUNT) {
-      case `string`:
-        // Leave at Default number of workers
-        // if process.env.GATSBY_CPU_COUNT === `physical_cores`)
-
-        // Number of workers === logical CPU count or default
-        if (process.env.GATSBY_CPU_COUNT === `logical_cores`) {
-          numOfWorkers = require(`os`).cpus().length || numOfWorkers
-        }
-        break
-
-      case `number`:
-        // Number of workers === passed in count,
-        numOfWorkers = parseInt(process.env.GATSBY_CPU_COUNT, 10)
-        break
-
-      default:
-        break
-    }
-  }
-
-  return new Worker(require.resolve(`./worker`), {
-    numOfWorkers,
-    forkOptions: {
-      silent: false,
-    },
-  })
-}
+const workerPool = new Worker(require.resolve(`./worker`), {
+  cpuCount,
+  forkOptions: {
+    silent: false,
+  },
+})
 
 module.exports = (htmlComponentRendererPath, pages, activity) =>
   new Promise((resolve, reject) => {
