8c88814ded62ec92f6495a6d19259d6738481ff7
Dustin Schau <dustinschau@gmail.com>
2018-12-03 15:47:22 -0600

    chore: Add clone folders script

---
+++ scripts/clone-folder.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+FOLDER=$1
+BASE=$(pwd)
+
+for folder in $FOLDER/*; do
+  [ -d "$folder" ] || continue # only directories
+  cd $BASE\
+
+  NAME=$(cat $folder/package.json | jq -r '.name')
+  CLONE_DIR="__${NAME}__clone__"
+
+  git clone https://$GH_TOKEN@github.com/dschau/$NAME.git $CLONE_DIR
+  cp -r $folder/. $CLONE_DIR
+  cd $CLONE_DIR
+  git add .
+  git commit --message "chore: syncing with gatsbyjs/starters monorepo"
+  git push origin master
+done
