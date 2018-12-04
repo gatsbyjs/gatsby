5754c2cff88fbe852d9bd699cac4772ac9cc4bb3
Justin Hall <justin.r.hall@gmail.com>
2018-12-04 12:18:20 -0700

    feat(gatsby-plugin-emotion): update plugin for emotion v10 (#10226)
    # :tada: Emotion v10 is here! :tada: 
    
    - [X] Update dependencies
    - [X] Update critical style handling
    - [X] Add Babel config for `jsx` pragma
    - [X] Update plugin install instructions
    - [X] Update Gatsby tutorial (part 4)
    - [X] Update the "using emotion" example

--- packages/gatsby-plugin-emotion/src/gatsby-node.js
+++ packages/gatsby-plugin-emotion/src/gatsby-node.js
@@ -1,10 +1,28 @@
-exports.onCreateBabelConfig = ({ actions }, pluginOptions) => {
+export const onCreateBabelConfig = ({ actions }, pluginOptions) => {
+  const pragmaName = `___EmotionJSX`
+
+  actions.setBabelPlugin({
+    name: `babel-plugin-jsx-pragmatic`,
+    options: {
+      export: `jsx`,
+      module: `@emotion/core`,
+      import: pragmaName,
+    },
+  })
+
+  actions.setBabelPlugin({
+    name: `@babel/plugin-transform-react-jsx`,
+    options: {
+      pragma: pragmaName,
+    },
+  })
+
   actions.setBabelPlugin({
     name: `babel-plugin-emotion`,
     options: {
+      cssPropOptimization: true,
       sourceMap: process.env.NODE_ENV !== `production`,
       autoLabel: process.env.NODE_ENV !== `production`,
-      hoist: process.env.NODE_ENV === `production`,
       ...(pluginOptions ? pluginOptions : {}),
     },
   })
