4cc4fa7102b99723c8c92a44a51f30666887efc9
Michal Piechowiak <misiek.piechowiak@gmail.com>
2018-12-05 00:02:26 +0100

    switch from snapshot testing to more descriptive assert

--- e2e-tests/production-runtime/cypress/integration/lifecycle-methods.js
+++ e2e-tests/production-runtime/cypress/integration/lifecycle-methods.js
@@ -6,8 +6,13 @@ describe(`Production build tests`, () => {
       .click()
       .waitForAPI(`onRouteUpdate`)
 
+    // we expect 2 `componentDidMount` calls - 1 for initial page and 1 for second page
     cy.window().then(win => {
-      cy.wrap(win.___PageComponentLifecycleCallsLog).snapshot()
+      expect(
+        win.___PageComponentLifecycleCallsLog.filter(
+          entry => entry.action === `componentDidMount`
+        ).length
+      ).to.equal(2)
     })
   })
 
@@ -18,8 +23,13 @@ describe(`Production build tests`, () => {
       .click()
       .waitForAPI(`onRouteUpdate`)
 
+    // we expect 2 `componentDidMount` calls - 1 for initial page and 1 for duplicated page
     cy.window().then(win => {
-      cy.wrap(win.___PageComponentLifecycleCallsLog).snapshot()
+      expect(
+        win.___PageComponentLifecycleCallsLog.filter(
+          entry => entry.action === `componentDidMount`
+        ).length
+      ).to.equal(2)
     })
   })
 
@@ -34,8 +44,13 @@ describe(`Production build tests`, () => {
       .click()
       .waitForAPI(`onRouteUpdate`)
 
+    // we expect just 1 `componentDidMount` call, when navigating inside matchPath
     cy.window().then(win => {
-      cy.wrap(win.___PageComponentLifecycleCallsLog).snapshot()
+      expect(
+        win.___PageComponentLifecycleCallsLog.filter(
+          entry => entry.action === `componentDidMount`
+        ).length
+      ).to.equal(1)
     })
   })
 })
