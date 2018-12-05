98c4520a11550fee6005989e96c0a138ac39b317
Dustin Schau <dustinschau@gmail.com>
2018-12-05 13:09:00 -0600

    chore: add a comment

--- starters/blog/gatsby-config.js
+++ starters/blog/gatsby-config.js
@@ -41,7 +41,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        // trackingId: `ADD YOUR TRACKING ID HERE`,
       },
     },
     `gatsby-plugin-feed`,
