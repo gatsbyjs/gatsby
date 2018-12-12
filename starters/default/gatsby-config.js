e9bfc5d7e79fcc30a419a898e1431fb2a75aab2b
Dustin Schau <dustinschau@gmail.com>
2018-12-12 09:50:21 -0600

    chore: sync with upstream

--- starters/default/gatsby-config.js
+++ starters/default/gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- starters/default/src/pages/index.js
+++ starters/default/src/pages/index.js
@@ -3,9 +3,11 @@ import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 import Image from '../components/image'
+import SEO from '../components/seo'
 
 const IndexPage = () => (
   <Layout>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
