77df044e480854d73a7f1ebfcd8bb814c2c06529
Stefan Probst <stefanprobst@kolabnow.com>
2018-12-18 10:27:39 +0100

    Remove unused files

--- packages/gatsby/src/schema/query/sift/filter.js
+++
@@ -1,5 +0,0 @@
-const sift = require(`sift`)
-
-const filter = (filters, nodes) => sift({ $and: filters }, nodes)
-
-module.exports = filter
--- packages/gatsby/src/schema/query/sift/find.js
+++
@@ -1,8 +0,0 @@
-const sift = require(`sift`)
-
-const find = (filters, nodes = []) => {
-  const index = sift.indexOf({ $and: filters }, nodes)
-  return index !== -1 ? nodes[index] : null
-}
-
-module.exports = find
