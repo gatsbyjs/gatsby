dee0a253a86b17609b1f6309edb776057317cbe6
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2018-12-19 20:08:18 +0530

    Update comment

--- packages/gatsby/src/schema/data-tree-utils.js
+++ packages/gatsby/src/schema/data-tree-utils.js
@@ -245,7 +245,7 @@ const extractFieldExamples = (
   for (let key of allKeys) {
     if (ignoreFields.includes(key)) continue
     // Unfortunately an empty string as an Object key is perfectly valid JavaScript
-    // We ignore these because our schema generation requires keys to have names
+    // We ignore these because GraphQL requires keys to have names
     if (key === ``) continue
     const nextSelector = selector ? `${selector}.${key}` : key
 
