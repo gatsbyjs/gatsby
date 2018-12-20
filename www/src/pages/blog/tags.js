157fbb359b60842450f8f61c9587941a69581952
Elliot Hesp <elliot.hesp@gmail.com>
2018-12-20 19:08:57 +0000

    Fix gatsby-plugin-react-helmet for v6.0.0 (#10578)
    ## Description
    
    In version 6.0.0 of react-helmet, the default export has been removed. There is now only a named export, e.g.
    
    ```
    import { Helmet } from 'react-helmet';
    ```
    
    In v5.x.x, both a default export and named are exported. Change is here:
    
    https://github.com/nfl/react-helmet/commit/20ea3858f503ec521ad5b1facdac5816a0019317#diff-cc54072daf5278847980b841520c8fffL287
    
    This is a backwards compatible change. v5 has an annoying bug (https://github.com/nfl/react-helmet/issues/373) which v6 seems to fix.

--- www/src/pages/blog/tags.js
+++ www/src/pages/blog/tags.js
@@ -6,7 +6,7 @@ import { graphql } from "gatsby"
 import kebabCase from "lodash/kebabCase"
 
 // Components
-import Helmet from "react-helmet"
+import { Helmet } from "react-helmet"
 import { Link } from "gatsby"
 import Layout from "../../components/layout"
 import Container from "../../components/container"
