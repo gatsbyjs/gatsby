ee723b9e048725b3504a5cb28008b178d6fc1548
Dustin Schau <dustinschau@gmail.com>
2018-12-27 14:47:30 -0600

    chore(starters): standardize on lower case naming convention

--- starters/blog/src/pages/404.js
+++ starters/blog/src/pages/404.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import Layout from '../components/Layout'
+import Layout from '../components/layout'
 import SEO from '../components/seo'
 
 class NotFoundPage extends React.Component {
