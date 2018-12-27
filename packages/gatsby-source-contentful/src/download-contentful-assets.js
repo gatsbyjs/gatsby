c24051d67af18efbcf84325f45b49f13f17dbdf5
polarathene <brennan.kinney.nz@gmail.com>
2018-12-28 01:51:34 +0800

    Appease CI lint errors

--- packages/gatsby-source-contentful/src/download-contentful-assets.js
+++ packages/gatsby-source-contentful/src/download-contentful-assets.js
@@ -1,10 +1,13 @@
-const ProgressBar = require('progress')
-const { createRemoteFileNode } = require('gatsby-source-filesystem')
+const ProgressBar = require(`progress`)
+const { createRemoteFileNode } = require(`gatsby-source-filesystem`)
 
-const bar = new ProgressBar(`Downloading Contentful Assets [:bar] :current/:total :elapsed secs :percent`, {
-  total: 0,
-  width: 30,
-})
+const bar = new ProgressBar(
+  `Downloading Contentful Assets [:bar] :current/:total :elapsed secs :percent`,
+  {
+    total: 0,
+    width: 30,
+  }
+)
 
 let totalJobs = 0
 
@@ -15,7 +18,7 @@ let totalJobs = 0
  * @param gatsbyFunctions - Gatsby's internal helper functions
  */
 
-const downloadContentfulAssets = async (gatsbyFunctions) => {
+const downloadContentfulAssets = async gatsbyFunctions => {
   const {
     actions: { createNode, touchNode },
     createNodeId,
@@ -26,57 +29,58 @@ const downloadContentfulAssets = async (gatsbyFunctions) => {
 
   // Any ContentfulAsset nodes will be downloaded, cached and copied to public/static
   // regardless of if you use `localFile` to link an asset or not.
-  const contentfulAssetNodes = getNodes()
-    .filter(n =>
-      (n.internal.owner === `gatsby-source-contentful`) &&
-      (n.internal.type === `ContentfulAsset`)
-    )
+  const contentfulAssetNodes = getNodes().filter(
+    n =>
+      n.internal.owner === `gatsby-source-contentful` &&
+      n.internal.type === `ContentfulAsset`
+  )
 
-  await Promise.all(contentfulAssetNodes.map(async node => {
+  await Promise.all(
+    contentfulAssetNodes.map(async node => {
+      totalJobs += 1
+      bar.total = totalJobs
 
-  totalJobs += 1
-  bar.total = totalJobs
+      let fileNodeID
+      const remoteDataCacheKey = `contentful-asset-${node.contentful_id}`
+      const cacheRemoteData = await cache.get(remoteDataCacheKey)
+      const url = `http://${node.file.url.slice(2)}`
 
-  let fileNodeID
-  const remoteDataCacheKey = `contentful-asset-${node.contentful_id}`
-  const cacheRemoteData = await cache.get(remoteDataCacheKey)
-  const url = "http://" + node.file.url.slice(2)
-
-  // Avoid downloading the asset again if it's been cached
-  // Note: Contentful Assets do not provide useful metadata
-  // to compare a modified asset to a cached version?
-  if (cacheRemoteData) {
-    fileNodeID = cacheRemoteData.fileNodeID // eslint-disable-line prefer-destructuring
-    touchNode({ nodeId: cacheRemoteData.fileNodeID })
-  }
-
-  // If we don't have cached data, download the file
-  if (!fileNodeID) {
-    try {
-      const fileNode = await createRemoteFileNode({
-        url,
-        store,
-        cache,
-        createNode,
-        createNodeId,
-      })
-
-      if (fileNode) {
-        bar.tick()
-        fileNodeID = fileNode.id
-
-        await cache.set(remoteDataCacheKey, { fileNodeID })
+      // Avoid downloading the asset again if it's been cached
+      // Note: Contentful Assets do not provide useful metadata
+      // to compare a modified asset to a cached version?
+      if (cacheRemoteData) {
+        fileNodeID = cacheRemoteData.fileNodeID // eslint-disable-line prefer-destructuring
+        touchNode({ nodeId: cacheRemoteData.fileNodeID })
       }
-    }  catch (err) {
-      // Ignore
-    }
-  }
 
-  if (fileNodeID) {
-    node.localFile___NODE = fileNodeID
-  }
+      // If we don't have cached data, download the file
+      if (!fileNodeID) {
+        try {
+          const fileNode = await createRemoteFileNode({
+            url,
+            store,
+            cache,
+            createNode,
+            createNodeId,
+          })
 
-  return node
-  }))
+          if (fileNode) {
+            bar.tick()
+            fileNodeID = fileNode.id
+
+            await cache.set(remoteDataCacheKey, { fileNodeID })
+          }
+        } catch (err) {
+          // Ignore
+        }
+      }
+
+      if (fileNodeID) {
+        node.localFile___NODE = fileNodeID
+      }
+
+      return node
+    })
+  )
 }
 exports.downloadContentfulAssets = downloadContentfulAssets
