0bef2d38fb28180388d325663adadec0cc0e4a99
Dustin Schau <dustinschau@gmail.com>
2018-12-27 14:50:53 -0600

    chore: update in api-ssr as well

--- packages/gatsby/cache-dir/api-ssr-docs.js
+++ packages/gatsby/cache-dir/api-ssr-docs.js
@@ -145,7 +145,7 @@ exports.onPreRenderHTML = true
  * @param {string} $0.pathname Path of page.
  * @example
  * import React from "react"
- * import Layout from "./src/components/Layout"
+ * import Layout from "./src/components/layout"
  *
  * export const wrapPageElement = ({ element, props }) => {
  *   // props provide same data to Layout as Page element will get
