f0a9c098e28bb1238cc15c0b51d499981a6c212e
Stefan Probst <stefanprobst@kolabnow.com>
2018-12-28 13:05:06 +0100

    Only cache in production

--- packages/gatsby/src/schema/db/redux/index.js
+++ packages/gatsby/src/schema/db/redux/index.js
@@ -1,13 +1,16 @@
 const { getNodes, getNode } = require(`../../../redux/nodes`)
+const { isProductionBuild } = require(`../../utils`)
 
 const cache = new Map()
 
 const getNodesByType = type => {
-  if (cache.has(type)) {
+  if (isProductionBuild && cache.has(type)) {
     return cache.get(type)
   }
   const nodes = getNodes().filter(node => node.internal.type === type)
-  cache.set(type, nodes)
+  if (isProductionBuild) {
+    cache.set(type, nodes)
+  }
   return nodes
 }
 
