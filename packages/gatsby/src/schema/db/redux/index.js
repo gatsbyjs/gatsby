dd5c9f0ea732e2357cabfb46620b275b1f5f1c6b
Stefan Probst <stefanprobst@kolabnow.com>
2018-12-28 15:11:47 +0100

    Fix import, add comments

--- packages/gatsby/src/schema/db/redux/index.js
+++ packages/gatsby/src/schema/db/redux/index.js
@@ -1,8 +1,14 @@
 const { getNodes, getNode } = require(`../../../redux/nodes`)
-const { isProductionBuild } = require(`../../utils`)
+const isProductionBuild = require(`../../utils/is-production-build`)
+
+// const { emitter } = require(`../../../redux`)
+// let isBootstrapFinished = false
+// emitter.on(`BOOTSTRAP_FINISHED`, () => (isBootstrapFinished = true))
 
 const cache = new Map()
 
+// TODO: We need a proper type mapping in the store
+// new Map([type: new Map([id, node])])
 const getNodesByType = type => {
   if (isProductionBuild && cache.has(type)) {
     return cache.get(type)
