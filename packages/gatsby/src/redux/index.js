74bcb3e347888d7c6953cb7209caf783abf93c2a
Stefan Probst <stefanprobst@kolabnow.com>
2018-12-30 21:53:30 +0100

    Add serialization with v8

--- packages/gatsby/src/redux/index.js
+++ packages/gatsby/src/redux/index.js
@@ -2,7 +2,7 @@ const Redux = require(`redux`)
 const _ = require(`lodash`)
 const fs = require(`fs`)
 const mitt = require(`mitt`)
-const stringify = require(`json-stringify-safe`)
+const v8 = require(`v8`)
 
 // Create event emitter for actions
 const emitter = mitt()
@@ -10,43 +10,17 @@ const emitter = mitt()
 // Reducers
 const reducers = require(`./reducers`)
 
-const objectToMap = obj => {
-  let map = new Map()
-  Object.keys(obj).forEach(key => {
-    map.set(key, obj[key])
-  })
-  return map
-}
+const readFileSync = file => v8.deserialize(fs.readFileSync(file))
 
-const mapToObject = map => {
-  const obj = {}
-  for (let [key, value] of map) {
-    obj[key] = value
-  }
-  return obj
-}
+const writeFileSync = (file, contents) =>
+  fs.writeFileSync(file, v8.serialize(contents))
 
-// Read from cache the old node data.
+const file = `${process.cwd()}/.cache/redux-state.json`
+
+// Read old node data from cache.
 let initialState = {}
 try {
-  const file = fs.readFileSync(`${process.cwd()}/.cache/redux-state.json`)
-  // Apparently the file mocking in node-tracking-test.js
-  // can override the file reading replacing the mocked string with
-  // an already parsed object.
-  if (Buffer.isBuffer(file) || typeof file === `string`) {
-    initialState = JSON.parse(file)
-  }
-  if (initialState.staticQueryComponents) {
-    initialState.staticQueryComponents = objectToMap(
-      initialState.staticQueryComponents
-    )
-  }
-  if (initialState.components) {
-    initialState.components = objectToMap(initialState.components)
-  }
-  if (initialState.nodes) {
-    initialState.nodes = objectToMap(initialState.nodes)
-  }
+  initialState = readFileSync(file)
 } catch (e) {
   // ignore errors.
 }
@@ -74,17 +48,7 @@ function saveState() {
     `staticQueryComponents`,
   ])
 
-  pickedState.staticQueryComponents = mapToObject(
-    pickedState.staticQueryComponents
-  )
-  pickedState.components = mapToObject(pickedState.components)
-  pickedState.nodes = pickedState.nodes ? mapToObject(pickedState.nodes) : []
-  const stringified = stringify(pickedState, null, 2)
-  fs.writeFile(
-    `${process.cwd()}/.cache/redux-state.json`,
-    stringified,
-    () => {}
-  )
+  writeFileSync(file, pickedState)
 }
 
 exports.saveState = saveState
