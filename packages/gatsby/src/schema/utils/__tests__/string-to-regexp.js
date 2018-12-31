effce549f77328db3a7bb0fbe5afcdad8b5fcaec
Stefan Probst <stefanprobst@kolabnow.com>
2018-12-31 14:05:01 +0100

    Add invariant

--- packages/gatsby/src/schema/utils/__tests__/string-to-regexp.js
+++ packages/gatsby/src/schema/utils/__tests__/string-to-regexp.js
@@ -7,4 +7,19 @@ describe(`Convert string to RegExp`, () => {
     expect(regex.source).toBe(`\\w+`)
     expect(regex.flags).toBe(`gimuy`)
   })
+
+  it(`handles slashes`, () => {
+    const regex = stringToRegExp(`/\\w+/\\w+/`)
+    expect(regex).toBeInstanceOf(RegExp)
+    expect(regex.source).toBe(`\\w+\\/\\w+`)
+    expect(regex.flags).toBe(``)
+  })
+
+  it(`throws on invalid regex string`, () => {
+    expect(() => stringToRegExp(`/\\w+`)).toThrow()
+    expect(() => stringToRegExp(`\\w+/`)).toThrow()
+    expect(() => stringToRegExp(`\\w+`)).toThrow()
+    expect(() => stringToRegExp(`i/\\w+/`)).toThrow()
+    expect(() => stringToRegExp(`/\\w+/z`)).toThrow()
+  })
 })
--- packages/gatsby/src/schema/utils/string-to-regexp.js
+++ packages/gatsby/src/schema/utils/string-to-regexp.js
@@ -1,5 +1,11 @@
+const invariant = require(`invariant`)
+
 const stringToRegExp = str => {
   const lastSlash = str.lastIndexOf(`/`)
+  invariant(
+    lastSlash > 0 && str.startsWith(`/`),
+    `Regular expressions have to be surrounded by slashes, e.g. /\\.txt$/i`
+  )
   return new RegExp(str.slice(1, lastSlash), str.slice(lastSlash + 1))
 }
 
