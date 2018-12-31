e9c8ec972b583b89dc27e193fdb198078d53ce4a
Stefan Probst <stefanprobst@kolabnow.com>
2018-12-31 10:43:03 +0100

    Remove comments

--- packages/gatsby/src/schema/index.js
+++ packages/gatsby/src/schema/index.js
@@ -8,16 +8,9 @@ module.exports = async ({ parentSpan, update }) => {
   // const spanArgs = parentSpan ? { childOf: parentSpan } : {}
   // const span = tracer.startSpan(`build schema`, spanArgs)
 
-  // const thirdPartySchemas = store.getState().thirdPartySchemas || []
   const schema = update ? await updateSchema() : await buildSchema()
 
   // span.finish()
-
-  // FIXME: Disable schema stitching for now, because `mergeSchemas` is all over
-  // the place -- and unfortunately breaks everything. Explain in more detail soon.
-  // const schema = mergeSchemas({
-  //   schemas: [gatsbySchema, ...thirdPartySchemas],
-  // })
 
   store.dispatch({
     type: `SET_SCHEMA`,
