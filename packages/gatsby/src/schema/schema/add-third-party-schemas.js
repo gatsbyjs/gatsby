5c76114eeccbb9a5cada10b4846a12a630c6b397
Stefan Probst <stefanprobst@kolabnow.com>
2019-01-02 11:43:11 +0100

    Simplify further

--- packages/gatsby/src/schema/schema/add-third-party-schemas.js
+++ packages/gatsby/src/schema/schema/add-third-party-schemas.js
@@ -1,19 +1,12 @@
-const { schemaComposer } = require(`graphql-compose`)
+const { schemaComposer, TypeComposer } = require(`graphql-compose`)
 
 const { store } = require(`../../redux`)
 
 const addThirdPartySchemas = () => {
   const schemas = store.getState().thirdPartySchemas
   schemas.forEach(schema => {
-    const fields = schema.getQueryType().getFields()
-    Object.entries(fields).forEach(([fieldName, fieldConfig]) => {
-      // We need to recreate the field, because the result of `getFields`
-      // is a runtime representation that cannot be used to build new schemas.
-      const { type, args, resolve, description } = fieldConfig
-      schemaComposer.Query.addFields({
-        [fieldName]: { type, args, resolve, description },
-      })
-    })
+    const QueryTC = TypeComposer.create(schema.getQueryType())
+    schemaComposer.Query.addFields(QueryTC.getFields())
   })
 }
 
