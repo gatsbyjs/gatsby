d98816286f3daff91dc630ae21cad85880df5e9f
mottox2 <y.mottox2@gmail.com>
2019-01-03 05:53:17 +0900

    Format

--- starters/blog/gatsby-node.js
+++ starters/blog/gatsby-node.js
@@ -34,8 +34,7 @@ exports.createPages = ({ graphql, actions }) => {
     const posts = result.data.allMarkdownRemark.edges
 
     posts.forEach((post, index) => {
-      const previous =
-        index === posts.length - 1 ? null : posts[index + 1].node
+      const previous = index === posts.length - 1 ? null : posts[index + 1].node
       const next = index === 0 ? null : posts[index - 1].node
 
       createPage({
