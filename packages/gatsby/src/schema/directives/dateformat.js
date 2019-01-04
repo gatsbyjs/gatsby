fb99bbd5c521d9a0691d8bc75fc15dd96f1e9205
Stefan Probst <stefanprobst@kolabnow.com>
2019-01-04 16:26:58 +0100

    Update dateformat

--- packages/gatsby/src/schema/directives/dateformat.js
+++ packages/gatsby/src/schema/directives/dateformat.js
@@ -8,19 +8,7 @@ const {
 const { SchemaDirectiveVisitor } = require(`graphql-tools`)
 const { format, distanceInWordsToNow } = require(`date-fns`)
 
-// `date-fns-timezone` (compatible with v1): `formatToTimeZone(date, pattern. { locale, timeZone })`
-// `date-fns-tz` (compatible with v2): `format(date, pattern. { locale, timeZone })`
-// When updating to `date-fns` v2, don't forget that locale
-// names have changed!
-// Also: distanceInWordsFromNow => formatDistance/formatRelative(date, baseDate, { locale })
-
-// Keep an eye on Intl.DateTimeFormat and Intl.RelativeTimeFormat
-
-// full-icu support can be enabled in Node with `yarn add full-icu` and
-// setting NODE_ICU_DATA=./node_modules/full-icu
-
 const formatDate = (date, pattern, lang, timeZone, distanceToNow) => {
-  // return new Date(date).toLocaleString(locale, { timeZone: timeZone })
   const locale = lang && require(`date-fns/locale/${lang}`)
   return distanceToNow
     ? distanceInWordsToNow(date, { locale })
@@ -33,51 +21,31 @@ const formatDate = (date, pattern, lang, timeZone, distanceToNow) => {
 const DateFormatDirective = new GraphQLDirective({
   name: `dateformat`,
   locations: [DirectiveLocation.FIELD_DEFINITION],
-  args: {
-    defaultFormat: { type: GraphQLString, defaultValue: `YYYY-MM-DD` },
-    defaultLocale: { type: GraphQLString, defaultValue: `en` },
-    defaultTimeZone: { type: GraphQLString, defaultValue: `UTC` },
-    defaultDistanceToNow: { type: GraphQLBoolean, defaultValue: false },
-  },
 })
 
 // @see https://www.apollographql.com/docs/graphql-tools/schema-directives.html#Formatting-date-strings
 class DateFormatDirectiveVisitor extends SchemaDirectiveVisitor {
   visitFieldDefinition(field) {
     const { resolve = defaultFieldResolver } = field
-    const {
-      defaultFormat,
-      defaultLocale,
-      defaultTimeZone,
-      defaultDistanceToNow,
-    } = this.args
 
-    // field.args.push(
-    //   { name: `format`, type: GraphQLString },
-    //   { name: `locale`, type: GraphQLString },
-    //   { name: `timeZone`, type: GraphQLString },
-    //   { name: `distanceToNow`, type: GraphQLBoolean }
-    // )
-    field.args = [
+    field.args.push(
       { name: `format`, type: GraphQLString },
       { name: `locale`, type: GraphQLString },
       { name: `timeZone`, type: GraphQLString },
-      { name: `distanceToNow`, type: GraphQLBoolean },
-    ]
+      { name: `distanceToNow`, type: GraphQLBoolean }
+    )
 
     field.resolve = async (source, args, context, info) => {
       const { format, locale, timeZone, distanceToNow, ...rest } = args
       const date = await resolve(source, rest, context, info)
-      return formatDate(
-        date,
-        format || defaultFormat,
-        locale || defaultLocale,
-        timeZone || defaultTimeZone,
-        distanceToNow || defaultDistanceToNow
-      )
+      const shouldReturnFormattedString =
+        format || locale || timeZone || distanceToNow
+      if (shouldReturnFormattedString) {
+        info.returnType = GraphQLString
+        return formatDate(date, format, locale, timeZone, distanceToNow)
+      }
+      return date
     }
-
-    field.type = GraphQLString
   }
 }
 
