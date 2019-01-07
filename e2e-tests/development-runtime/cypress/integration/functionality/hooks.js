fd7462aaf9dd3dd45dcd1cc3f2808e4a7d522113
Dustin Schau <dustinschau@gmail.com>
2019-01-07 10:38:01 -0600

    chore: reset

---
+++ e2e-tests/development-runtime/cypress/integration/functionality/hooks.js
@@ -0,0 +1,27 @@
+const COUNT_ID = `count`
+
+describe(`hooks`, () => {
+  beforeEach(() => {
+    cy.visit(`/hooks`).waitForAPI(`onRouteUpdate`)
+  })
+
+  it(`displays initial state`, () => {
+    cy.getTestElement(COUNT_ID)
+      .invoke(`text`)
+      .should(`eq`, `0`)
+  })
+
+  it(`can update local state`, () => {
+    cy.getTestElement(`increment`).click()
+
+    cy.getTestElement(COUNT_ID)
+      .invoke(`text`)
+      .should(`eq`, `1`)
+
+    cy.getTestElement(`decrement`).click()
+
+    cy.getTestElement(COUNT_ID)
+      .invoke(`text`)
+      .should(`eq`, `0`)
+  })
+})
