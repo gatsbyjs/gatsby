76cf4fcfac6bd38a3de0a167579eff8b60b6bab6
Dustin Schau <dustinschau@gmail.com>
2019-01-08 12:24:09 -0600

    chore: tweak language again

--- e2e-tests/production-runtime/src/pages/global-style.js
+++ e2e-tests/production-runtime/src/pages/global-style.js
@@ -5,10 +5,10 @@ import Layout from '../components/layout'
 const GlobalStyle = () => (
   <Layout>
     <h1 data-testid="global-plugin-style">
-      This text should be red (via `gatsby-plugin-global-style`)
+      This text should have a large z-index (via `gatsby-plugin-global-style`)
     </h1>
     <h2 data-testid="global-style">
-      This text should be blue (via root-level `gatsby-browser.js`)
+      This text should have a large z-index (via root-level `gatsby-browser.js`)
     </h2>
   </Layout>
 )
