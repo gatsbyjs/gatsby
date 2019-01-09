651ea8847a1791384f3fd0e517b53807ab1db1f3
Stefan Probst <stefanprobst@kolabnow.com>
2019-01-09 10:07:54 +0100

    Fix

--- packages/gatsby/src/schema/resolvers/resolvers.js
+++ packages/gatsby/src/schema/resolvers/resolvers.js
@@ -58,7 +58,7 @@ const find = type => async (rp, firstResultOnly) => {
   const queryArgs = withSpecialCases({ type, ...rp })
   // Don't create page dependencies in getNodesForQuery
   /* eslint-disable-next-line no-unused-vars */
-  const { path, ...context } = rp.context
+  const { path, ...context } = rp.context || {}
   return query(
     await getNodesForQuery(type, queryArgs.filter, context),
     queryArgs,
