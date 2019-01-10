634c8b41309dd68f08110987696f128f3d0af060
Ward Peeters <ward@coding-tech.com>
2019-01-10 12:25:48 +0100

    fix queue caching

--- packages/gatsby-plugin-sharp/src/gatsby-node.js
+++ packages/gatsby-plugin-sharp/src/gatsby-node.js
@@ -6,49 +6,64 @@ const {
 } = require(`./index`)
 const processFile = require(`./processFile`)
 const { scheduleJob } = require(`./scheduler`)
+const fs = require(`fs-extra`)
 
-exports.onPreInit = ({ actions, cache }, pluginOptions) => {
-  cache.init()
+const getQueueFromCache = async cache => {
+  const rawQueue = await cache.get(`queue`)
 
+  if (!rawQueue) {
+    return new Map()
+  }
+
+  return new Map(rawQueue)
+}
+
+exports.onPreInit = async ({ actions }, pluginOptions) => {
   setBoundActionCreators(actions)
   setPluginOptions(pluginOptions)
 }
 
 // JSON.stringify doesn't work on an Map so we need to convert it to an array
-exports.onPostBootstrap = async ({ cache }) =>
-  cache.set(`queue`, Array.from(queue))
+exports.onPostBootstrap = async ({ cache }) => {
+  const cachedQueue = await getQueueFromCache(cache)
+
+  // merge both queues
+  for (const [key, job] of cachedQueue) {
+    queue.set(key, job)
+  }
+
+  return cache.set(`queue`, Array.from(queue))
+}
 
 /**
  * Execute all unprocessed images on gatsby build
  */
-exports.onPostBuild = async (
-  { cache, boundActionCreators, reporter },
-  pluginOptions
-) => {
-  const rawQueue = await cache.get(`queue`)
-  const queue = new Map(rawQueue)
+let promises = []
+exports.onPreBuild = async ({ cache, boundActionCreators }, pluginOptions) => {
+  const cachedQueue = await getQueueFromCache(cache)
 
   let promises = []
-  for (const [, job] of queue) {
+  for (const [, job] of cachedQueue) {
     promises.push(scheduleJob(job, boundActionCreators, pluginOptions))
   }
-
-  return Promise.all(promises).catch(({ err, message }) => {
-    reportError(message || err.message, err, reporter)
-  })
 }
+
+/**
+ * wait for all images to be processed
+ */
+exports.onPostBuild = ({ cache, reporter }) =>
+  Promise.all(promises)
+    .then(() => cache.set(`queue`, []))
+    .catch(({ err, message }) => {
+      reportError(message || err.message, err, reporter)
+    })
 
 /**
  * Build images on the fly when they are requested by the browser
  */
 exports.onCreateDevServer = ({ app, cache }, pluginOptions) => {
   app.use(async (req, res, next) => {
-    const rawQueue = await cache.get(`queue`)
-    if (!rawQueue) {
-      return next()
-    }
-
-    const queue = new Map(rawQueue)
+    const queue = await getQueueFromCache(cache)
     if (!queue.has(req.originalUrl)) {
       return next()
     }
