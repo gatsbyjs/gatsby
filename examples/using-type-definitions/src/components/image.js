af1c76e30d3611401b56d062e092425b33d9b42f
Stefan Probst <stefanprobst@kolabnow.com>
2019-01-11 01:48:18 +0100

    Fix

--- examples/using-type-definitions/src/components/image.js
+++ examples/using-type-definitions/src/components/image.js
@@ -17,9 +17,7 @@ const Image = () => (
   <StaticQuery
     query={graphql`
       query {
-        placeholderImage: file(
-          filter: { relativePath: { eq: "gatsby-astronaut.png" } }
-        ) {
+        placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
           childImageSharp {
             fluid(maxWidth: 300) {
               ...GatsbyImageSharpFluid
--- packages/gatsby-source-filesystem/src/add-type-defs.js
+++ packages/gatsby-source-filesystem/src/add-type-defs.js
@@ -1,39 +1,41 @@
 // TODO: What is actually needed?
 module.exports = ({ addTypeDefs }) => {
   const typeDefs = `
-    absolutePath: String
-    accessTime: Date
-    atime: Date
-    atimeMs: Float
-    base: String
-    birthtime: Date
-    birthTime: Date
-    birthtimeMs: Float
-    # blksize: Int (Linux: yes, Windows: no, Mac: ???)
-    # blocks: Int (Linux: yes, Windows: no, Mac: ???)
-    changeTime: Date
-    ctime: Date
-    ctimeMs: Float
-    dev: Int
-    dir: String
-    ext: String
-    extension: String
-    gid: Int
-    ino: Int
-    mode: Int
-    modifiedTime: Date
-    mtime: Date
-    mtimeMs: Float
-    name: String
-    nlink: Int
-    prettySize: String
-    rdev: Int
-    relativeDirectory: String
-    relativePath: String
-    root: String
-    size: Int
-    sourceInstanceName: String
-    uid: Int
+    type File implements Node {
+      absolutePath: String
+      accessTime: Date
+      atime: Date
+      atimeMs: Float
+      base: String
+      birthtime: Date
+      birthTime: Date
+      birthtimeMs: Float
+      # blksize: Int (Linux: yes, Windows: no, Mac: ???)
+      # blocks: Int (Linux: yes, Windows: no, Mac: ???)
+      changeTime: Date
+      ctime: Date
+      ctimeMs: Float
+      dev: Int
+      dir: String
+      ext: String
+      extension: String
+      gid: Int
+      ino: Int
+      mode: Int
+      modifiedTime: Date
+      mtime: Date
+      mtimeMs: Float
+      name: String
+      nlink: Int
+      prettySize: String
+      rdev: Int
+      relativeDirectory: String
+      relativePath: String
+      root: String
+      size: Int
+      sourceInstanceName: String
+      uid: Int
+    }
   `
   addTypeDefs(typeDefs)
 }
