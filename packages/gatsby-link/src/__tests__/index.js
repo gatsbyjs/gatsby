4852074f7aa455855bd774748f8acdaa50881a8e
Orestis Ioannou <oorestisime@gmail.com>
2019-01-12 22:27:51 +0000

    [gatsby-link] warn when <link> is used for external links (#10981)
    Fix: #10978
    
    <!--
      Have any questions? Check out the contributing docs at https://gatsby.app/contribute, or
      ask in this Pull Request and a Gatsby maintainer will be happy to help :)
    -->

--- packages/gatsby-link/src/__tests__/index.js
+++ packages/gatsby-link/src/__tests__/index.js
@@ -88,7 +88,6 @@ describe(`<Link />`, () => {
     it(`accepts to as a string`, () => {
       const location = `/courses?sort=name`
       const { link } = setup({ linkProps: { to: location } })
-
       expect(link.getAttribute(`href`)).toEqual(location)
     })
 
@@ -97,6 +96,20 @@ describe(`<Link />`, () => {
       const location = `/courses?sort=name`
       const { link } = setup({ linkProps: { to: location }, pathPrefix })
       expect(link.getAttribute(`href`)).toEqual(`${pathPrefix}${location}`)
+    })
+
+    it(`does not warn when internal`, () => {
+      jest.spyOn(global.console, `warn`)
+      const to = `/courses?sort=name`
+      setup({ linkProps: { to } })
+      expect(console.warn).not.toBeCalled()
+    })
+
+    it(`warns when not internal`, () => {
+      jest.spyOn(global.console, `warn`)
+      const to = `https://gatsby.org`
+      setup({ linkProps: { to } })
+      expect(console.warn).toBeCalled()
     })
   })
 
