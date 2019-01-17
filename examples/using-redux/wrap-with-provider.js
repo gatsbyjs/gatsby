e9b21ede5fc4a254427eaf5be345ecbabdc19a18
Christopher Boyd <xpboyd@gmail.com>
2019-01-17 17:10:04 -0500

    fix(using-redux): create a fresh store for each SSR page

--- examples/using-redux/wrap-with-provider.js
+++ examples/using-redux/wrap-with-provider.js
@@ -3,7 +3,9 @@ import { Provider } from "react-redux"
 
 import createStore from "./src/state/createStore"
 
-const store = createStore()
-
 // eslint-disable-next-line react/display-name,react/prop-types
-export default ({ element }) => <Provider store={store}>{element}</Provider>
+export default ({ element }) => {
+  // Create a fresh store for each SSR page:
+  const store = createStore()
+  return (<Provider store={store}>{element}</Provider>)
+}
