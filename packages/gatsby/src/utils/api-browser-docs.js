b8422553a6cc2b0b1195ba1c788eea4727ec13ca
Jordan Overbye <jordan@outfit.io>
2019-01-18 19:00:49 +1000

    feat(browser-api): add previous location argument to navigation updates (#11095)
    * Pass in prevLocation from prevProps to apiRunner
    
    * Updated browser API documentation
    
    * Removed action from docs for onRouteUpdate and exports.onPreRouteUpdate

--- packages/gatsby/src/utils/api-browser-docs.js
+++ packages/gatsby/src/utils/api-browser-docs.js
@@ -21,10 +21,11 @@ exports.onInitialClientRender = true
  * Called when changing location is started.
  * @param {object} $0
  * @param {object} $0.location A location object
- * @param {object} $0.action The "action" that caused the route change
+ * @param {object|null} $0.prevLocation The previous location object
  * @example
- * exports.onPreRouteUpdate = ({ location }) => {
- *   console.log("Gatsby started to change location", location.pathname)
+ * exports.onPreRouteUpdate = ({ location, prevLocation }) => {
+ *   console.log("Gatsby started to change location to", location.pathname)
+ *   console.log("Gatsby started to change location from", prevLocation ? prevLocation.pathname : null)
  * }
  */
 exports.onPreRouteUpdate = true
@@ -45,10 +46,11 @@ exports.onRouteUpdateDelayed = true
  * Called when the user changes routes
  * @param {object} $0
  * @param {object} $0.location A location object
- * @param {object} $0.action The "action" that caused the route change
+ * @param {object|null} $0.prevLocation The previous location object
  * @example
- * exports.onRouteUpdate = ({ location }) => {
+ * exports.onRouteUpdate = ({ location, prevLocation }) => {
  *   console.log('new pathname', location.pathname)
+ *   console.log('old pathname', prevLocation ? prevLocation.pathname : null)
  *
  *   // Track pageview with google analytics
  *   window.ga(
