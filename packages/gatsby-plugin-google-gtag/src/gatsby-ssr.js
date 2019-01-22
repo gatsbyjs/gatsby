89361ed1ebf15e137d22a22bf19a5bd198806e27
Jacob Blomgren <blomgren@protonmail.com>
2019-01-22 21:52:38 +0100

    fix(plugin-google-gtag): make respectDNT config option use the right path

--- packages/gatsby-plugin-google-gtag/src/gatsby-ssr.js
+++ packages/gatsby-plugin-google-gtag/src/gatsby-ssr.js
@@ -31,6 +31,11 @@ exports.onRenderBody = (
     ? setHeadComponents
     : setPostBodyComponents
 
+  // TODO: remove pluginOptions.respectDNT in the next major release of this plugin.
+  // See issue https://github.com/gatsbyjs/gatsby/issues/11159 for the discussion.
+  const respectDNT =
+    pluginOptions.pluginConfig.respectDNT || pluginOptions.respectDNT
+
   const renderHtml = () => `
       ${
         excludeGtagPaths.length
@@ -44,8 +49,7 @@ exports.onRenderBody = (
           : ``
       }
       if(${
-        typeof pluginOptions.respectDNT !== `undefined` &&
-        pluginOptions.respectDNT === true
+        respectDNT
           ? `!(navigator.doNotTrack == "1" || window.doNotTrack == "1")`
           : `true`
       }) {
