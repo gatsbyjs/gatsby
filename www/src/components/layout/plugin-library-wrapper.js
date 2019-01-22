a5203d6111279265e84c35d6dd044f151edca5b3
Amberley Romo <amberleyjohanna@gmail.com>
2019-01-22 12:20:58 -0600

    fix: linting

--- www/src/components/layout/plugin-library-wrapper.js
+++ www/src/components/layout/plugin-library-wrapper.js
@@ -10,7 +10,7 @@ const PluginLibraryWrappedLayout = props => {
   return (
     <Layout location={location}>
       <PageWithPluginSearchBar
-        isPluginsIndex={location.pathname === "/plugins/"}
+        isPluginsIndex={location.pathname === `/plugins/`}
         location={location}
       >
         {children}
