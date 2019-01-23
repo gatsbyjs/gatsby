19fb90287d130552b1072ceea88bfd84268e0c8e
Amberley <amberleyromo@users.noreply.github.com>
2019-01-23 10:01:36 -0600

    fix: env var name change for build (#11228)

--- www/src/templates/template-docs-local-packages.js
+++ www/src/templates/template-docs-local-packages.js
@@ -70,7 +70,7 @@ class DocsLocalPackagesTemplate extends React.Component {
         />
         <Unbird
           dataSetId="5c1ac24b4a828a169b6c235c"
-          publicKey={process.env.UNBIRD_FEEDBACK_KEY_PLUGINLIB}
+          publicKey={process.env.GATSBY_FEEDBACK_KEY_PLUGINLIB}
           feedbackPrompt="Have feedback on the Plugin Library?"
         />
       </>
