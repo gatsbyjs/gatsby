b8567d19ec68e408787d846d1d36fb6772dd2ac3
Anthony Marcar <anthony@moocar.me>
2019-01-23 12:32:45 +1100

    add GATSBY_DB_NODES=loki to various CI jobs

--- scripts/e2e-test.sh
+++ scripts/e2e-test.sh
@@ -11,5 +11,8 @@ yarn &&
 gatsby-dev --set-path-to-repo $GATSBY_PATH &&
 gatsby-dev --scan-once --copy-all --quiet && # copies _all_ files in gatsby/packages
 chmod +x ./node_modules/.bin/gatsby && # this is sometimes necessary to ensure executable
+echo "running test with default GATSBY_DB_NODES env var (redux)" &&
 yarn $CUSTOM_COMMAND &&
+echo "running test with GATSBY_DB_NODES=loki" &&
+GATSBY_DB_NODES=loki yarn $CUSTOM_COMMAND &&
 echo "e2e test run succeeded"
