f556f7eb13078b70b611ca46f644b2bbf370f6e8
Justin Chi <jchi@memsql.com>
2019-01-24 10:14:46 -0800

    For gatsby-plugin-netlify-cms, allow imported components to resolve relative to project src/, allow plugins webpack instance to not choke on Gatsby modules

--- packages/gatsby-plugin-netlify-cms/src/gatsby-node.js
+++ packages/gatsby-plugin-netlify-cms/src/gatsby-node.js
@@ -59,6 +59,9 @@ exports.onCreateWebpackConfig = (
       output: {
         path: path.join(program.directory, `public`, publicPathClean),
       },
+      resolve: {
+        modules: [path.resolve(__dirname, `../../src`), `node_modules`],
+      },
       module: {
         /**
          * Manually swap `style-loader` for `MiniCssExtractPlugin.loader`.
@@ -144,6 +147,30 @@ exports.onCreateWebpackConfig = (
       optimization: {},
       devtool: stage === `develop` ? `cheap-module-source-map` : `source-map`,
     }
+
+    config.module.rules.push({
+      test: /gatsby\/cache-dir.*\.js$/,
+      loader: require.resolve(`babel-loader`),
+      options: {
+        presets: [
+          require.resolve(`@babel/preset-react`),
+          [
+            require.resolve(`@babel/preset-env`),
+            {
+              shippedProposals: true,
+              useBuiltIns: `entry`,
+            },
+          ],
+        ],
+        plugins: [require.resolve(`@babel/plugin-proposal-class-properties`)],
+      },
+    })
+
+    // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
+    config.module.rules.exclude = [/node_modules\/(?!(gatsby)\/)/]
+
+    // Prefer Gatsby ES6 entrypoint (module) over commonjs (main) entrypoint
+    config.resolve.mainFields = [`browser`, `module`, `main`]
 
     if (stage === `develop`) {
       webpack(config).watch({}, () => {})
