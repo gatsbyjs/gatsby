d777462525f32da0ef8c1138a4ef5f4425263d0e
Flying Katsu <Warped2713@users.noreply.github.com>
2019-01-25 17:23:23 -0500

    test(gatsby-plugin-page-creator): filters files matching ignore patterns

--- packages/gatsby-plugin-page-creator/src/__tests__/gatsby-node.js
+++ packages/gatsby-plugin-page-creator/src/__tests__/gatsby-node.js
@@ -1,5 +1,6 @@
 const validatePath = require(`../validate-path`)
 const createPath = require(`../create-path`)
+const ignorePath = require(`../ignore-path`)
 
 describe(`JavaScript page creator`, () => {
   it(`includes the correct file types`, () => {
@@ -126,6 +127,125 @@ describe(`JavaScript page creator`, () => {
       ]
 
       expect(paths.map(p => createPath(basePath, p))).toMatchSnapshot()
+    })
+  })
+
+  describe(`ignore-path`, () => {
+    it(`does not filter files without an ignore object`, () => {
+      const ignoreFiles = [
+        { path: `blog.js` },
+        { path: `post.tsx` },
+        { path: `file.example.jsx` },
+        { path: `file.example.tsx` },
+        { path: `dir/file.example.js` },
+      ]
+
+      const testFiles = ignoreFiles.concat([
+        // blog or post in sub directory, other pages in top directory
+        { path: `dir/blog.jsx` },
+        { path: `dir/post.ts` },
+        { path: `other.js` },
+        // example page or directory without the preprended extension
+        { path: `example.js` },
+        { path: `example/file.tsx` },
+        { path: `dir/example.jsx` },
+      ])
+
+      expect(testFiles.filter(file => ignorePath(file.path, null))).toEqual(
+        testFiles
+      )
+    })
+
+    it(`does not filter files without any ignore patterns`, () => {
+      const ignoreFiles = [
+        { path: `blog.js` },
+        { path: `post.tsx` },
+        { path: `file.example.jsx` },
+        { path: `file.example.tsx` },
+        { path: `dir/file.example.js` },
+      ]
+
+      const testFiles = ignoreFiles.concat([
+        // blog or post in sub directory, other pages in top directory
+        { path: `dir/blog.jsx` },
+        { path: `dir/post.ts` },
+        { path: `other.js` },
+        // example page or directory without the preprended extension
+        { path: `example.js` },
+        { path: `example/file.tsx` },
+        { path: `dir/example.jsx` },
+      ])
+
+      expect(testFiles.filter(file => ignorePath(file.path, {}))).toEqual(
+        testFiles
+      )
+    })
+
+    it(`filters out files matching ignore patterns`, () => {
+      const patterns = [
+        // files named 'blog' or 'post' in top of pages directory
+        `(blog|post).(js|ts)?(x)*`,
+        // any files with .example prepended to the js(x)/ts(x) extension
+        `**/*.example.(js|ts)?(x)`,
+      ]
+
+      const ignoreFiles = [
+        { path: `blog.js` },
+        { path: `post.tsx` },
+        { path: `file.example.jsx` },
+        { path: `file.example.tsx` },
+        { path: `dir/file.example.js` },
+      ]
+
+      const testFiles = ignoreFiles.concat([
+        // blog or post in sub directory, other pages in top directory
+        { path: `dir/blog.jsx` },
+        { path: `dir/post.ts` },
+        { path: `other.js` },
+        // example page or directory without the preprended extension
+        { path: `example.js` },
+        { path: `example/file.tsx` },
+        { path: `dir/example.jsx` },
+      ])
+
+      expect(
+        testFiles.filter(file => ignorePath(file.path, { patterns }))
+      ).toEqual(ignoreFiles)
+    })
+
+    it(`filters out files matching ignore patterns w/ options.nocase`, () => {
+      const patterns = [
+        // files named 'blog' or 'post' in top of pages directory
+        `(blog|post).(js|ts)?(x)*`,
+        // any files with .example prepended to the js(x)/ts(x) extension
+        `**/*.example.(js|ts)?(x)`,
+      ]
+
+      // https://www.npmjs.com/package/micromatch#optionsnocase
+      const options = { nocase: true }
+
+      const ignoreFiles = [
+        { path: `blog.js` },
+        { path: `post.tsx` },
+        { path: `file.example.jsx` },
+        { path: `file.example.tsx` },
+        { path: `dir/file.example.js` },
+      ]
+
+      const testFiles = ignoreFiles.concat([
+        // blog or post in sub directory, other pages in top directory
+        { path: `dir/blog.jsx` },
+        { path: `dir/post.ts` },
+        { path: `other.js` },
+        // example page or directory without the preprended extension
+        { path: `example.js` },
+        { path: `example/file.tsx` },
+        { path: `dir/example.jsx` },
+      ])
+
+      expect(
+        testFiles.filter(file => ignorePath(file.path, { patterns, options }))
+      ).toEqual(ignoreFiles)
     })
   })
 })
