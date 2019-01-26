98c5c57d6b0d6f0c0ad7d38f3f991f865f1e3950
Natalie Diebold <Warped2713@users.noreply.github.com>
2019-01-25 19:18:16 -0500

    chore(lint): remove semicolon

--- packages/gatsby-plugin-page-creator/src/ignore-path.js
+++ packages/gatsby-plugin-page-creator/src/ignore-path.js
@@ -2,7 +2,7 @@ const mm = require(`micromatch`)
 
 module.exports = (path, ignore) => {
   // Don't do anything if no ignore patterns
-  if (!ignore || !ignore.patterns) return false;
+  if (!ignore || !ignore.patterns) return false
   // Return true if the path should be ignored (matches any given ignore patterns)
   return mm.any(path, ignore.patterns, ignore.options)
 }
