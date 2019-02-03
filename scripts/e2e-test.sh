3e2d0d57f25bf3ef0006ca1e2540eb121c2c1fbe
Michal Piechowiak <misiek.piechowiak@gmail.com>
2019-02-03 17:52:07 +0100

    yuck - don't look

--- scripts/e2e-test.sh
+++ scripts/e2e-test.sh
@@ -3,13 +3,15 @@ SRC_PATH=$1
 CUSTOM_COMMAND="${2:-yarn test}"
 GATSBY_PATH="${CIRCLE_WORKING_DIRECTORY:-../../}"
 
-npm install -g gatsby-dev-cli &&
+# npm install -g gatsby-dev-cli &&
 
 # setting up child integration test link to gatsby packages
 cd $SRC_PATH &&
 yarn &&
-gatsby-dev --set-path-to-repo $GATSBY_PATH &&
-gatsby-dev --scan-once --copy-all --quiet && # copies _all_ files in gatsby/packages
+#gatsby-dev --set-path-to-repo $GATSBY_PATH &&
+#gatsby-dev --scan-once --copy-all --quiet && # copies _all_ files in gatsby/packages
+node ../../packages/gatsby-dev-cli/dist/index.js --set-path-to-repo $GATSBY_PATH &&
+node ../../packages/gatsby-dev-cli/dist/index.js --scan-once --copy-all --quiet && # copies _all_ files in gatsby/packages
 chmod +x ./node_modules/.bin/gatsby && # this is sometimes necessary to ensure executable
 sh -c "$CUSTOM_COMMAND" &&
 echo "e2e test run succeeded"
