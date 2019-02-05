3b2b28e9eff9eb6ef7519ed369fe2cc6e24fcaf4
Michal Piechowiak <misiek.piechowiak@gmail.com>
2019-02-05 13:32:31 -0500

    Update examples/using-redux/wrap-with-provider.js
    From pieh's suggestion
    
    Co-Authored-By: cpboyd <xpboyd@gmail.com>

--- examples/using-redux/wrap-with-provider.js
+++ examples/using-redux/wrap-with-provider.js
@@ -5,7 +5,9 @@ import createStore from "./src/state/createStore"
 
 // eslint-disable-next-line react/display-name,react/prop-types
 export default ({ element }) => {
-  // Create a fresh store for each SSR page:
+  // Instantiating store in `wrapRootElement` handler ensures:
+  //  - there is fresh store for each SSR page
+  //  - it will be called only once in browser, when React mounts
   const store = createStore()
   return <Provider store={store}>{element}</Provider>
 }
