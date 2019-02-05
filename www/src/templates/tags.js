368cb72079cc8dafee5d6d99c4cd79125354c7e3
Lee Mulvey <lmulvey@me.com>
2019-02-05 08:03:59 -0700

    feat(www): update tags design, add 'View All Tags' buttons (#10936)

--- www/src/templates/tags.js
+++ www/src/templates/tags.js
@@ -1,7 +1,9 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { graphql } from "gatsby"
+import TagsIcon from "react-icons/lib/ti/tags"
 
 import BlogPostPreviewItem from "../components/blog-post-preview-item"
+import Button from "../components/button"
 import Container from "../components/container"
 import Layout from "../components/layout"
 import { rhythm } from "../utils/typography"
@@ -17,6 +19,9 @@ const Tags = ({ pageContext, data, location }) => {
     <Layout location={location}>
       <Container>
         <h1>{tagHeader}</h1>
+        <Button tiny key="blog-post-view-all-tags-button" to="/blog/tags">
+          View All Tags <TagsIcon />
+        </Button>
         {edges.map(({ node }) => (
           <BlogPostPreviewItem
             post={node}
@@ -24,7 +29,6 @@ const Tags = ({ pageContext, data, location }) => {
             css={{ marginBottom: rhythm(2) }}
           />
         ))}
-        <Link to="/blog/tags">All tags</Link>
       </Container>
     </Layout>
   )
