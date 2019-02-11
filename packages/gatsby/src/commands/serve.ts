6605be3ea4464e8154bf27872c2494194c3104a0
Dustin Schau <DSchau@users.noreply.github.com>
2019-02-11 14:48:39 -0600

    fix(gatsby): add history fallback for client-only routes (#11610)
    * Revert "fix(gatsby): use history fallback to display client-only routes in serve (#11581)"
    
    This reverts commit 75f6118a5a850de9755d5fdd6fdae4523e7c720e.
    
    * Revert "fix(gatsby): use history fallback to display client-only routes in serve (#11581)"
    
    This reverts commit 75f6118a5a850de9755d5fdd6fdae4523e7c720e.
    
    * fix: use smart history fallback that understands gatsby matchPath
    
    * chore: fix
    
    * refactor: swap to reach router instead of micromatch
    
    * chore: move check up
    
    * chore: tweak name

--- packages/gatsby/src/commands/serve.ts
+++ packages/gatsby/src/commands/serve.ts
@@ -1,13 +1,39 @@
 /* @flow weak */
 const path = require(`path`)
 const openurl = require(`better-opn`)
+const fs = require(`fs-extra`)
 const signalExit = require(`signal-exit`)
 const compression = require(`compression`)
 const express = require(`express`)
-const historyFallback = require(`express-history-api-fallback`)
 const getConfigFile = require(`../bootstrap/get-config-file`)
 const preferDefault = require(`../bootstrap/prefer-default`)
 const chalk = require(`chalk`)
+const { match: reachMatch } = require(`@reach/router/lib/utils`)
+
+const getPages = directory =>
+  fs
+    .readFile(path.join(directory, `.cache`, `pages.json`))
+    .then(contents => JSON.parse(contents))
+    .catch(() => [])
+
+const clientOnlyPathsRouter = (pages, options) => {
+  const clientOnlyRoutes = pages
+    .filter(page => page.matchPath)
+    .map(page => page.matchPath)
+  return (req, res, next) => {
+    const { url } = req
+    if (req.accepts(`html`)) {
+      if (clientOnlyRoutes.some(route => reachMatch(route, url) !== null)) {
+        return res.sendFile(`index.html`, options, err => {
+          if (err) {
+            next()
+          }
+        })
+      }
+    }
+    return next()
+  }
+}
 
 module.exports = async program => {
   let { prefixPaths, port, open, host } = program
@@ -21,18 +47,18 @@ module.exports = async program => {
   pathPrefix = prefixPaths && pathPrefix ? pathPrefix : `/`
 
   const root = path.join(program.directory, `public`)
+  const pages = await getPages(program.directory)
 
   const app = express()
   const router = express.Router()
   router.use(compression())
   router.use(express.static(`public`))
-  router.use(historyFallback(`index.html`, { root }))
+  router.use(clientOnlyPathsRouter(pages, { root }))
   router.use((req, res, next) => {
     if (req.accepts(`html`)) {
-      res.status(404).sendFile(`404.html`, { root })
-    } else {
-      next()
+      return res.status(404).sendFile(`404.html`, { root })
     }
+    return next()
   })
   app.use(pathPrefix, router)
 
@@ -55,7 +81,5 @@ module.exports = async program => {
     }
   })
 
-  signalExit((code, signal) => {
-    server.close()
-  })
+  signalExit(() => server.close())
 }
