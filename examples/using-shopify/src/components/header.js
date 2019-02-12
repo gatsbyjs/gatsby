ba250eaf9abe06d974b7bcd64985df642a12bd46
Dustin Schau <dustinschau@gmail.com>
2019-02-12 17:53:12 -0600

    chore: little fixes

--- examples/using-shopify/src/components/header.js
+++ examples/using-shopify/src/components/header.js
@@ -16,7 +16,7 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <h1 style={{ fontSize: 24, margin: 0 }}>
         <Link
           to="/"
           style={{
