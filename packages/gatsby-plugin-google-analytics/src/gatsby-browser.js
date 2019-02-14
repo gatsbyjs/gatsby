e20beee5eac0e58acdfbbbe6af6b91ba1dc4bed8
Ward Peeters <ward@coding-tech.com>
2019-02-14 05:08:48 -0800

    use better timing than 0

--- packages/gatsby-plugin-google-analytics/src/gatsby-browser.js
+++ packages/gatsby-plugin-google-analytics/src/gatsby-browser.js
@@ -8,14 +8,27 @@ exports.onRouteUpdate = function({ location }) {
     ) {
       return
     }
-    
-    setTimeout(() => {
+
+    // wrap inside a timeout to make sure react-helmet is done with it's changes #9139
+    // reactHelmet is using requestAnimationFrame so we should use it too: https://github.com/nfl/react-helmet/blob/5.2.0/src/HelmetUtils.js#L296-L299
+    const sendPageView = () => {
       window.ga(
         `set`,
         `page`,
-        location ? location.pathname + location.search + location.hash : undefined
+        location
+          ? location.pathname + location.search + location.hash
+          : undefined
       )
       window.ga(`send`, `pageview`)
-    }, 0)
+    }
+
+    if (`requestAnimationFrame` in window) {
+      requestAnimationFrame(() => {
+        requestAnimationFrame(sendPageView)
+      })
+    } else {
+      // simulate 2 rAF calls
+      setTimeout(sendPageView, 32)
+    }
   }
 }
