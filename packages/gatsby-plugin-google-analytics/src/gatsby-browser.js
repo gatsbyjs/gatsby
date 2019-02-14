ccfc8c4b82a508777df77fbb1789210bc43d8a78
Ward Peeters <ward@coding-tech.com>
2019-02-14 05:10:10 -0800

    fix comment

--- packages/gatsby-plugin-google-analytics/src/gatsby-browser.js
+++ packages/gatsby-plugin-google-analytics/src/gatsby-browser.js
@@ -9,7 +9,7 @@ exports.onRouteUpdate = function({ location }) {
       return
     }
 
-    // wrap inside a timeout to make sure react-helmet is done with it's changes #9139
+    // wrap inside a timeout to make sure react-helmet is done with it's changes (https://github.com/gatsbyjs/gatsby/issues/9139)
     // reactHelmet is using requestAnimationFrame so we should use it too: https://github.com/nfl/react-helmet/blob/5.2.0/src/HelmetUtils.js#L296-L299
     const sendPageView = () => {
       window.ga(
