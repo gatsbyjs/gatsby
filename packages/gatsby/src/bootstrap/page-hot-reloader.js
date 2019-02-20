516b42c2a5938e1bb62193fc2ddf8adffa2df409
Kyle Mathews <mathews.kyle@gmail.com>
2019-02-20 02:58:06 -0800

    Document FAKE_API_CALL

--- packages/gatsby/src/bootstrap/page-hot-reloader.js
+++ packages/gatsby/src/bootstrap/page-hot-reloader.js
@@ -16,6 +16,10 @@ emitter.on(`CREATE_NODE`, action => {
 emitter.on(`DELETE_NODE`, action => {
   if (action.payload.internal.type !== `SitePage`) {
     pagesDirty = true
+    // Make a fake API call to trigger `API_RUNNING_QUEUE_EMPTY` being called.
+    // We don't want to call runCreatePages here as there might be work in
+    // progress. So this is a safe way to make sure runCreatePages gets called
+    // at a safe time.
     apiRunnerNode(`FAKE_API_CALL`)
   }
 })
