5e66f87aff443bda5553ba2ba6a6c90cf0b7dd6e
Michal Piechowiak <misiek.piechowiak@gmail.com>
2019-02-21 00:12:26 +0100

    fix(gatsby): don't show false positive async createPage warning (#11929)

--- packages/gatsby/src/utils/api-runner-node.js
+++ packages/gatsby/src/utils/api-runner-node.js
@@ -99,39 +99,47 @@ const runAPI = (plugin, api, args) => {
 
     // Ideally this would be more abstracted and applied to more situations, but right now
     // this can be potentially breaking so targeting `createPages` API and `createPage` action
+    let actions = doubleBoundActionCreators
     let apiFinished = false
     if (api === `createPages`) {
       let alreadyDisplayed = false
-      const createPageAction = doubleBoundActionCreators.createPage
-      doubleBoundActionCreators.createPage = (...args) => {
-        createPageAction(...args)
-        if (apiFinished && !alreadyDisplayed) {
-          const warning = [
-            reporter.stripIndent(`
+      const createPageAction = actions.createPage
+      // create new actions object with wrapped createPage action
+      // doubleBoundActionCreators is memoized, so we can't just
+      // reassign createPage field as this would cause this extra logic
+      // to be used in subsequent APIs and we only want to target this `createPages` call.
+      actions = {
+        ...actions,
+        createPage: (...args) => {
+          createPageAction(...args)
+          if (apiFinished && !alreadyDisplayed) {
+            const warning = [
+              reporter.stripIndent(`
               Action ${chalk.bold(
                 `createPage`
               )} was called outside of its expected asynchronous lifecycle ${chalk.bold(
-              `createPages`
-            )} in ${chalk.bold(plugin.name)}.
+                `createPages`
+              )} in ${chalk.bold(plugin.name)}.
               Ensure that you return a Promise from ${chalk.bold(
                 `createPages`
               )} and are awaiting any asynchronous method invocations (like ${chalk.bold(
-              `graphql`
-            )} or http requests).
+                `graphql`
+              )} or http requests).
               For more info and debugging tips: see ${chalk.bold(
                 `https://gatsby.app/sync-actions`
               )}
             `),
-          ]
+            ]
 
-          const possiblyCodeFrame = getNonGatsbyCodeFrame()
-          if (possiblyCodeFrame) {
-            warning.push(possiblyCodeFrame)
+            const possiblyCodeFrame = getNonGatsbyCodeFrame()
+            if (possiblyCodeFrame) {
+              warning.push(possiblyCodeFrame)
+            }
+
+            reporter.warn(warning.join(`\n\n`))
+            alreadyDisplayed = true
           }
-
-          reporter.warn(warning.join(`\n\n`))
-          alreadyDisplayed = true
-        }
+        },
       }
     }
 
@@ -139,8 +147,8 @@ const runAPI = (plugin, api, args) => {
       {
         ...args,
         pathPrefix,
-        boundActionCreators: doubleBoundActionCreators,
-        actions: doubleBoundActionCreators,
+        boundActionCreators: actions,
+        actions,
         loadNodeContent,
         store,
         emitter,
