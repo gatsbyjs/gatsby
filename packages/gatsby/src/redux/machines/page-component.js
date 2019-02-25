647b2ddca15fde3e31b26cd0ff28feca15bb40aa
Kyle Mathews <mathews.kyle@gmail.com>
2019-02-25 10:24:48 -0800

    Extract page component machine into new 'machines' directory

---
+++ packages/gatsby/src/redux/machines/page-component.js
@@ -0,0 +1,143 @@
+const {
+  Machine,
+  actions: { assign },
+} = require(`xstate`)
+
+module.exports = Machine(
+  {
+    id: `pageComponents`,
+    initial: `inactive`,
+    context: {
+      isInBootstrap: true,
+      componentPath: ``,
+      query: ``,
+    },
+    states: {
+      inactive: {
+        on: {
+          // Transient transition
+          // Will transition to either 'inactiveWhileBootstrapping' or 'extractingQueries'
+          // immediately upon entering 'inactive' state if the condition is met.
+          "": [
+            { target: `inactiveWhileBootstrapping`, cond: `isBootstrapping` },
+            { target: `extractingQueries`, cond: `isNotBootstrapping` },
+          ],
+        },
+      },
+      inactiveWhileBootstrapping: {
+        on: {
+          BOOTSTRAP_FINISHED: {
+            target: `extractingQueries`,
+            actions: `setBootstrapFinished`,
+          },
+          QUERY_EXTRACTED: `runningPageQueries`,
+          QUERY_EXTRACTION_GRAPHQL_ERROR: `queryExtractionGraphQLError`,
+          QUERY_EXTRACTION_BABEL_ERROR: `queryExtractionBabelError`,
+          NEW_PAGE_CREATED: {
+            actions: `setPage`,
+          },
+        },
+      },
+      extractingQueries: {
+        onEntry: [`extractQueries`],
+        on: {
+          BOOTSTRAP_FINISHED: {
+            actions: `setBootstrapFinished`,
+          },
+          QUERY_CHANGED: `runningPageQueries`,
+          QUERY_DID_NOT_CHANGE: `idle`,
+          QUERY_EXTRACTION_GRAPHQL_ERROR: `queryExtractionGraphQLError`,
+          QUERY_EXTRACTION_BABEL_ERROR: `queryExtractionBabelError`,
+          NEW_PAGE_CREATED: {
+            actions: `setPage`,
+          },
+        },
+      },
+      queryExtractionGraphQLError: {
+        on: {
+          PAGE_COMPONENT_CHANGED: `extractingQueries`,
+          BOOTSTRAP_FINISHED: {
+            actions: `setBootstrapFinished`,
+          },
+        },
+      },
+      queryExtractionBabelError: {
+        on: {
+          PAGE_COMPONENT_CHANGED: `extractingQueries`,
+          BOOTSTRAP_FINISHED: {
+            actions: `setBootstrapFinished`,
+          },
+        },
+      },
+      runningPageQueries: {
+        onEntry: [`setQuery`, `runPageComponentQueries`],
+        on: {
+          BOOTSTRAP_FINISHED: {
+            actions: `setBootstrapFinished`,
+          },
+          QUERIES_COMPLETE: `idle`,
+          NEW_PAGE_CREATED: {
+            actions: `setPage`,
+          },
+        },
+      },
+      idle: {
+        on: {
+          PAGE_COMPONENT_CHANGED: `extractingQueries`,
+          BOOTSTRAP_FINISHED: {
+            actions: `setBootstrapFinished`,
+          },
+          NEW_PAGE_CREATED: {
+            actions: `setPage`,
+          },
+        },
+      },
+    },
+  },
+  {
+    guards: {
+      isBootstrapping: context => context.isInBootstrap,
+      isNotBootstrapping: context => !context.isInBootstrap,
+    },
+    actions: {
+      extractQueries: () => {
+        const {
+          debounceCompile,
+        } = require(`../../internal-plugins/query-runner/query-watcher`)
+
+        debounceCompile()
+      },
+      runPageComponentQueries: context => {
+        const {
+          queueQueriesForPageComponent,
+        } = require(`../../internal-plugins/query-runner/query-watcher`)
+        // Wait a bit as calling this function immediately triggers
+        // an Action call which Redux squawks about.
+        setTimeout(() => {
+          queueQueriesForPageComponent(context.componentPath)
+        }, 0)
+      },
+      setQuery: assign({
+        query: (ctx, event) => {
+          if (typeof event.query !== `undefined` || event.query !== null) {
+            return event.query
+          } else {
+            return ctx.query
+          }
+        },
+      }),
+      setPage: assign({
+        pages: (ctx, event) => {
+          if (event.path) {
+            return ctx.pages.concat(event.path)
+          } else {
+            return ctx.pages
+          }
+        },
+      }),
+      setBootstrapFinished: assign({
+        isInBootstrap: false,
+      }),
+    },
+  }
+)
