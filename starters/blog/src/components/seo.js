3bb34de30abaf71014b670fc1ff4613ccd46ee41
Dylan Tackoor <dylan@worldmedia.net>
2019-02-25 13:34:06 -0500

    Added comments explaining StaticQuery vs useStaticQuery examples

--- starters/blog/src/components/seo.js
+++ starters/blog/src/components/seo.js
@@ -1,5 +1,6 @@
 /**
- * SEO component using the Gatsby useStaticQuery React hook
+ * SEO component that queries for data with
+ *  Gatsby's useStaticQuery React hook
  *
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
