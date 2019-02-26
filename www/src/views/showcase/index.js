25786720ee6ff550f03f96d738fa245adcba062a
Ward Peeters <ward@coding-tech.com>
2019-02-26 21:57:21 +0100

    fix(www): fix scroll when filtering showcase (#12105)
    <!--
      Have any questions? Check out the contributing docs at https://gatsby.app/contribute, or
      ask in this Pull Request and a Gatsby maintainer will be happy to help :)
    -->
    
    ## Description
    Fixes scrolling when clicking on filters in the showcase. All filters do exactly the same now. I moved state up.
    
    <!-- Write a brief description of the changes introduced by this PR -->
    
    ## Related Issues
    #11001 
    #12003
    <!--
      Link to the issue that is fixed by this PR (if there is one)
      e.g. Fixes #1234, Addresses #1234, Related to #1234, etc.
    -->

--- www/src/views/showcase/index.js
+++ www/src/views/showcase/index.js
@@ -1,15 +1,52 @@
 import React, { Component } from "react"
 import { Helmet } from "react-helmet"
-
+import qs from "qs"
+import { navigate } from "gatsby"
+import scrollToAnchor from "../../utils/scroll-to-anchor"
 import FeaturedSites from "./featured-sites"
 import FilteredShowcase from "./filtered-showcase"
 import Layout from "../../components/layout"
 
 class ShowcaseView extends Component {
   showcase = React.createRef()
+  state = {
+    filters: [],
+  }
 
-  render = () => {
+  componentDidMount() {
+    const {
+      location: { search },
+    } = this.props
+
+    const { filters } = qs.parse(search.replace(`?`, ``))
+
+    if (filters && filters.length) {
+      this.setFilters(filters)
+    }
+  }
+
+  componentDidUpdate() {
+    const {
+      location: { pathname, search },
+    } = this.props
+    const queryString = qs.stringify(this.state)
+
+    if (search !== `?${queryString}`) {
+      navigate(`${pathname}?${queryString}`, { replace: true })
+    }
+  }
+
+  setFilters = filters => {
+    this.setState({
+      filters: [].concat(filters),
+    })
+
+    scrollToAnchor(this.showcase.current, () => {})()
+  }
+
+  render() {
     const { location, data } = this.props
+    const { filters } = this.state
 
     return (
       <Layout location={location}>
@@ -17,11 +54,16 @@ class ShowcaseView extends Component {
           <title>Showcase</title>
         </Helmet>
         <FeaturedSites
+          setFilters={this.setFilters}
           featured={data.featured.edges}
           showcase={this.showcase}
         />
         <div id="showcase" css={{ height: 0 }} ref={this.showcase} />
-        <FilteredShowcase data={data} />
+        <FilteredShowcase
+          filters={filters}
+          setFilters={this.setFilters}
+          data={data}
+        />
       </Layout>
     )
   }
