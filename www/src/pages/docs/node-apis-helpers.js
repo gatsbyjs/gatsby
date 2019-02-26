acdb76921c106054691b169fd34820a972d93f4e
Mike Allanson <michael.allanson@gmail.com>
2019-02-26 16:01:48 +0100

    Update www/src/pages/docs/node-apis-helpers.js
    Co-Authored-By: pieh <misiek.piechowiak@gmail.com>

--- www/src/pages/docs/node-apis-helpers.js
+++ www/src/pages/docs/node-apis-helpers.js
@@ -26,7 +26,7 @@ class NodeAPIHelperDocs extends React.Component {
           </h1>
           <p>
             The first argument passed to each of{` `}
-            <Link to="/docs/node-apis/">Gatsby Node APIs</Link> is object
+            <Link to="/docs/node-apis/">Gatsby's Node APIs</Link> is an object
             containing set of utilities.
           </p>
           <h2 css={{ marginBottom: rhythm(1 / 2) }}>Helpers</h2>
