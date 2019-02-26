6cba4b455f276e3577d1c572834f97b453fb3724
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2019-02-26 07:03:47 +0530

    Improve error messaging when sharp isn't available

--- packages/gatsby-plugin-sharp/src/gatsby-node.js
+++ packages/gatsby-plugin-sharp/src/gatsby-node.js
@@ -2,11 +2,15 @@ exports.onPreInit = ({ actions, reporter }, pluginOptions) => {
   try {
     require(`sharp`)
   } catch (error) {
-    // Bail early if sharp isn't working
-    // TODO: Add link to docs
+    // Bail early if sharp isn't available
     reporter.panic(
-      `
-      "sharp" doesn't seem to have been built or installed correctly
+      reporter.stripIndent`
+        The dependency "sharp" does not seem to have been built or installed correctly.
+
+        - Try to reinstall packages and look for errors during installation
+        - Consult "sharp" installation page at http://sharp.pixelplumbing.com/en/stable/install/
+        
+        If neither of the above work, please open an issue in https://github.com/gatsbyjs/gatsby/issues
       `
     )
   }
