457dd8842208b456a62e86c7e10ee20964b8a65d
Olle Lauri Boström <ollebostr@gmail.com>
2019-03-01 14:00:19 +0100

    test(gatsby-plugin-nprogress): add tests for nprogress plugin

---
+++ packages/gatsby-plugin-nprogress/src/__tests__/gatsby-browser.js
@@ -0,0 +1,40 @@
+import NProgress from "nprogress"
+import {
+  onClientEntry,
+  onRouteUpdateDelayed,
+  onRouteUpdate,
+} from "../gatsby-browser"
+
+jest.mock(`nprogress`)
+
+describe(`gatsby-plugin-nprogress`, () => {
+  describe(`onClientEntry`, () => {
+    it(`calls NProgress.configure with the correct styles`, () => {
+      const element = {}
+      const createElement = jest.spyOn(document, `createElement`)
+      const appendChild = jest.spyOn(document.head, `appendChild`)
+      createElement.mockReturnValue(element)
+      appendChild.mockReturnValue(true)
+      NProgress.configure = jest.fn()
+      onClientEntry(null, { showSpinner: false })
+      expect(NProgress.configure.mock.calls).toMatchSnapshot()
+      expect(element).toMatchSnapshot()
+    })
+  })
+
+  describe(`onRouteUpdateDelayed`, () => {
+    it(`calls NProgress.start`, () => {
+      NProgress.start = jest.fn()
+      onRouteUpdateDelayed()
+      expect(NProgress.start).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  describe(`onRouteUpdate`, () => {
+    it(`calls NProgress.done`, () => {
+      NProgress.done = jest.fn()
+      onRouteUpdate()
+      expect(NProgress.done).toHaveBeenCalledTimes(1)
+    })
+  })
+})
