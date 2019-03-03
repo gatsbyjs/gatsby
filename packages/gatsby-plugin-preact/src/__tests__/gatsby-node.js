7ca65a90b300e6f4ecfbcfd3a514a805ca6a06b1
Olle Lauri Boström <ollebostr@gmail.com>
2019-03-03 12:44:53 +0100

    refactor: refactor tests

--- packages/gatsby-plugin-preact/src/__tests__/gatsby-node.js
+++ packages/gatsby-plugin-preact/src/__tests__/gatsby-node.js
@@ -3,9 +3,26 @@ const { onCreateWebpackConfig } = require(`../gatsby-node`)
 describe(`gatsby-plugin-preact`, () => {
   it(`sets the correct webpack config`, () => {
     const actions = { setWebpackConfig: jest.fn() }
-    onCreateWebpackConfig({ stage: ``, actions })
-    onCreateWebpackConfig({ stage: `develop-html`, actions })
+
+    onCreateWebpackConfig({ stage: `develop`, actions })
+
     expect(actions.setWebpackConfig).toHaveBeenCalledTimes(1)
-    expect(actions.setWebpackConfig.mock.calls).toMatchSnapshot()
+    expect(actions.setWebpackConfig).toHaveBeenCalledWith({
+      resolve: {
+        alias: {
+          react: `preact-compat`,
+          "react-dom": `preact-compat`,
+          "create-react-class": `preact-compat/lib/create-react-class`,
+        },
+      },
+    })
+  })
+
+  it(`does not invoke setWebpackConfig when stage is develop-html`, () => {
+    const actions = { setWebpackConfig: jest.fn() }
+
+    onCreateWebpackConfig({ stage: `develop-html`, actions })
+
+    expect(actions.setWebpackConfig).not.toHaveBeenCalled()
   })
 })
