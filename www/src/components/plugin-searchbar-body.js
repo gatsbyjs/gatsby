6434165b7d71487ea6e7df0e0d533ecf6a20e775
Benjamin Lannon <benjamin@lannonbr.com>
2019-03-04 10:45:13 -0500

    feat(www): Marking official plugins in Plugin Library (#11753)
    * Marking official plugins in Plugin Library
    
    * Cleaning up styling for components
    
    * Swapped startsWith with indexOf
    
    * Update www/src/components/plugin-searchbar-body.js
    
    Co-Authored-By: lannonbr <benjamin@lannonbr.com>
    
    * Fixed typography & false positives
    
    * Rm import, fix defined but never used
    
    * Move „Official Plugin“ label before GitHub source link
    
    Makes „Plugin header items“ („Official Plugin“ label, GitHub source link, „Starters using this“) responsive.
    
    * Reduce „Official Plugin“ Gatsby icon size in plugin list
    
    Set width for download count to keep icon position consistent.

--- www/src/components/plugin-searchbar-body.js
+++ www/src/components/plugin-searchbar-body.js
@@ -13,6 +13,7 @@ import { colors } from "../utils/presets"
 import { Link } from "gatsby"
 import DownloadArrow from "react-icons/lib/md/file-download"
 import AlgoliaLogo from "../assets/algolia.svg"
+import GatsbyIcon from "../monogram.svg"
 import debounce from "lodash/debounce"
 import unescape from "lodash/unescape"
 
@@ -398,6 +399,8 @@ const Result = ({ hit, pathname, query }) => {
             fontFamily: typography.options.headerFontFamily.join(`,`),
             fontWeight: `bold`,
             lineHeight: 1.2,
+            display: `flex`,
+            alignItems: `center`,
             marginBottom: 0,
             marginTop: 0,
             letterSpacing: 0,
@@ -420,15 +423,38 @@ const Result = ({ hit, pathname, query }) => {
             fontSize: rhythm(4 / 7),
           }}
         >
-          {hit.humanDownloadsLast30Days}
-          {` `}
+          {hit.repository &&
+            hit.name[0] !== `@` &&
+            hit.repository.url.indexOf(`https://github.com/gatsbyjs/gatsby`) ===
+              0 && (
+              <img
+                src={GatsbyIcon}
+                css={{
+                  height: 12,
+                  marginBottom: 0,
+                  marginRight: 5,
+                  filter: selected ? false : `grayscale(100%)`,
+                  opacity: selected ? false : `0.2`,
+                }}
+                alt={`Official Gatsby Plugin`}
+              />
+            )}
           <span
             css={{
-              color: selected ? colors.lilac : colors.gray.bright,
-              marginLeft: rhythm(1 / 6),
+              width: `4.5em`,
+              textAlign: `right`,
             }}
           >
-            <DownloadArrow />
+            {hit.humanDownloadsLast30Days}
+            {` `}
+            <span
+              css={{
+                color: selected ? colors.lilac : colors.gray.bright,
+                marginLeft: rhythm(1 / 6),
+              }}
+            >
+              <DownloadArrow />
+            </span>
           </span>
         </div>
       </div>
