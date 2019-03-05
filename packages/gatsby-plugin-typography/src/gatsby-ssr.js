c8c396440dfe4ec7fd45625e9cb3c4fe1a237c74
Dustin Schau <dustinschau@gmail.com>
2019-03-04 19:06:21 -0600

    test(gatsby-plugin-typography): add some tests for gatsby-ssr

--- packages/gatsby-plugin-typography/src/gatsby-ssr.js
+++ packages/gatsby-plugin-typography/src/gatsby-ssr.js
@@ -3,12 +3,14 @@ import { TypographyStyle, GoogleFont } from "react-typography"
 import typography from "./.cache/typography"
 
 exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
-  const googleFont = pluginOptions.omitGoogleFont ? (
-    []
-  ) : (
-    <GoogleFont key={`GoogleFont`} typography={typography} />
-  )
   if (process.env.BUILD_STAGE === `build-html`) {
+    const googleFont = [].concat(
+      pluginOptions.omitGoogleFont ? (
+        []
+      ) : (
+        <GoogleFont key={`GoogleFont`} typography={typography} />
+      )
+    )
     setHeadComponents([
       <TypographyStyle key={`TypographyStyle`} typography={typography} />,
       ...googleFont,
