640d7c58c4154b088f8cf9719699569e1aed0e52
tomoyukikashiro <ksrtmyk@gmail.com>
2019-03-05 21:30:24 +0900

    fix(gatsby-plugin-sitemap): return statement ASAP.

--- packages/gatsby-plugin-sitemap/src/gatsby-node.js
+++ packages/gatsby-plugin-sitemap/src/gatsby-node.js
@@ -44,32 +44,31 @@ exports.onPostBuild = async ({ graphql, pathPrefix }, pluginOptions) => {
 
   if (!rest.sitemapSize || urls.length <= rest.sitemapSize) {
     const map = sitemap.createSitemap(rest)
-    serialize(queryRecords).forEach(u => map.add(u))
+    urls.forEach(u => map.add(u))
     return writeFile(saved, map.toString())
-  } else {
-    const {
-      site: {
-        siteMetadata: { siteUrl },
-      },
-    } = queryRecords
-    return new Promise((resolve, reject) => {
-      // sitemapv-index.xml is default file name. (https://git.io/fhNgG)
-      const indexFilePath = path.join(
-        distDir,
-        `${rest.sitemapName || `sitemap`}-index.xml`
-      )
-      const sitemapIndexOptions = {
-        ...rest,
-          hostname: hostname || withoutTrailingSlash(siteUrl),
-          targetFolder: distDir,
-          urls,
-          callback: error => {
-            if (error) throw new Error(error)
-            renameFile(indexFilePath, saved).then(resolve)
-          },
-        },
-      }
-      sitemap.createSitemapIndex(sitemapIndexOptions)
-    })
   }
+
+  const {
+    site: {
+      siteMetadata: { siteUrl },
+    },
+  } = queryRecords
+  return new Promise(resolve => {
+    // sitemapv-index.xml is default file name. (https://git.io/fhNgG)
+    const indexFilePath = path.join(
+      distDir,
+      `${rest.sitemapName || `sitemap`}-index.xml`
+    )
+    const sitemapIndexOptions = {
+      ...rest,
+      hostname: hostname || withoutTrailingSlash(siteUrl),
+      targetFolder: distDir,
+      urls,
+      callback: error => {
+        if (error) throw new Error(error)
+        renameFile(indexFilePath, saved).then(resolve)
+      },
+    }
+    sitemap.createSitemapIndex(sitemapIndexOptions)
+  })
 }
