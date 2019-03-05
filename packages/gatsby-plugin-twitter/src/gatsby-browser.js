b939b8b998082e945a107c46e41304197fbc3b19
Olle Lauri Boström <ollebostr@gmail.com>
2019-03-05 02:28:23 +0100

    fix(gatsby-plugin-twitter): add back semi-colons to injected script (#12296)
    ## Description
    Adds back the semi-colors to the injected script that was removed in #12193. As described [here](https://github.com/gatsbyjs/gatsby/pull/12193#issuecomment-469418343) this was causing the twitter embeds not to render.

--- packages/gatsby-plugin-twitter/src/gatsby-browser.js
+++ packages/gatsby-plugin-twitter/src/gatsby-browser.js
@@ -10,18 +10,18 @@ const injectTwitterScript = () => {
     window.twttr = (function(d, s, id) {
       var js,
         fjs = d.getElementsByTagName(s)[0],
-        t = window.twttr || {}
-      if (d.getElementById(id)) return t
-      js = d.createElement(s)
-      js.id = id
-      js.src = "https://platform.twitter.com/widgets.js"
-      fjs.parentNode.insertBefore(js, fjs)
-      t._e = []
+        t = window.twttr || {};
+      if (d.getElementById(id)) return t;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://platform.twitter.com/widgets.js";
+      fjs.parentNode.insertBefore(js, fjs);
+      t._e = [];
       t.ready = function(f) {
-        t._e.push(f)
-      }
-      return t
-    })(document, "script", "twitter-wjs")
+        t._e.push(f);
+      };
+      return t;
+    })(document, "script", "twitter-wjs");
   `)
 }
 
