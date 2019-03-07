f2aa44d656f125bbeedbf546d53ad3d43ee41ae9
Florian Kissling <florian.kissling@gmail.com>
2019-03-07 22:37:25 +0100

    Improve <Container> vertical padding

--- www/src/components/container.js
+++ www/src/components/container.js
@@ -1,5 +1,6 @@
 import React from "react"
 
+import presets from "../utils/presets"
 import { rhythm, options } from "../utils/typography"
 
 const Container = ({ children, hasSideBar = true, overrideCSS }) => (
@@ -7,10 +8,14 @@ const Container = ({ children, hasSideBar = true, overrideCSS }) => (
     css={{
       maxWidth: hasSideBar ? rhythm(28) : rhythm(36),
       margin: `0 auto`,
-      padding: `${rhythm(options.blockMarginBottom * 2)} ${rhythm(
+      padding: `${rhythm(options.blockMarginBottom)} ${rhythm(
         options.blockMarginBottom
       )}`,
       position: `relative`,
+      [presets.Lg]: {
+        paddingTop: rhythm(options.blockMarginBottom * 2),
+        paddingBottom: rhythm(options.blockMarginBottom * 2),
+      },
       ...overrideCSS,
     }}
   >
