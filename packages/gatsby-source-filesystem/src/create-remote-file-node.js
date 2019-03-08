58a5ef1eebe615606c7b5d29db8b882ab44bf3d8
Samuel Cha <sunchipssam@gmail.com>
2019-03-07 20:57:35 -0600

    rereplace createContentDigest with crypto in gatsby-source-filesystem

--- packages/gatsby-source-filesystem/src/create-remote-file-node.js
+++ packages/gatsby-source-filesystem/src/create-remote-file-node.js
@@ -1,6 +1,6 @@
 const fs = require(`fs-extra`)
 const got = require(`got`)
-const createContentDigest = require(`../../gatsby/src/utils/create-content-digest`)
+const crypto = require(`crypto`)
 const path = require(`path`)
 const { isWebUri } = require(`valid-url`)
 const Queue = require(`better-queue`)
@@ -56,6 +56,20 @@ const bar = new ProgressBar(
 /*********
  * utils *
  *********/
+
+/**
+ * createHash
+ * --
+ *
+ * Create an md5 hash of the given str
+ * @param  {Stringq} str
+ * @return {String}
+ */
+const createHash = str =>
+  crypto
+    .createHash(`md5`)
+    .update(str)
+    .digest(`hex`)
 
 const CACHE_DIR = `.cache`
 const FS_PLUGIN_DIR = `gatsby-source-filesystem`
@@ -199,7 +213,7 @@ async function processRemoteNode({
   }
 
   // Create the temp and permanent file names for the url.
-  const digest = createContentDigest(url)
+  const digest = createHash(url)
   if (!name) {
     name = getRemoteFileName(url)
   }
