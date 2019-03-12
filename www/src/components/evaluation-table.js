9f2af34c852a90b7c0dbeb2125e0b95e664d38fc
Florian Kissling <florian.kissling@gmail.com>
2019-03-12 16:11:46 +0100

    Fix a couple `key`s

--- www/src/components/evaluation-table.js
+++ www/src/components/evaluation-table.js
@@ -116,9 +116,9 @@ class EvaluationTable extends Component {
                       <SectionHeaderBottom
                         display={row.node.Subcategory}
                         category={row.node.Subcategory}
-                        key={`section-header-bottom-${i}`}
+                        key={`section-header-${s}-bottom-${i}`}
                       />,
-                      <tr key={`first-row-${i}`}>
+                      <tr key={`section-${s}-first-row-${i}`}>
                         {headers.map((header, j) => (
                           <td
                             key={j}
@@ -159,7 +159,7 @@ class EvaluationTable extends Component {
                         style={{
                           display: showTooltip(s, i) ? `table-row` : `none`,
                         }}
-                        key={`second-row-${i}`}
+                        key={`section-${s}-second-row-${i}`}
                       >
                         <td
                           css={{
