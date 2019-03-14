ef8d0674aad452cc5a0c28cfc22602b48f3cea11
Michal Piechowiak <misiek.piechowiak@gmail.com>
2019-03-14 23:52:44 +0100

    clarify that FORCE_COLOR=0 is respected in chalk and ansi-colors

--- packages/gatsby-cli/src/create-cli.js
+++ packages/gatsby-cli/src/create-cli.js
@@ -84,8 +84,9 @@ function buildLocalCommands(cli, isLocalSite) {
     return argv => {
       report.setVerbose(!!argv.verbose)
       if (argv.noColor) {
-        // disables colors in chalk
-        // see: https://www.npmjs.com/package/chalk#chalksupportscolor
+        // disables colors in popular terminal output coloring packages
+        //  - chalk: see https://www.npmjs.com/package/chalk#chalksupportscolor
+        //  - ansi-colors: see https://github.com/doowb/ansi-colors/blob/8024126c7115a0efb25a9a0e87bc5e29fd66831f/index.js#L5-L7
         process.env.FORCE_COLOR = `0`
       }
 
