7ef4c3cd51da1494bc6672b53afe5b548f511d1a
Wesley L. Handy <wesleylhandy@gmail.com>
2019-03-15 17:24:12 -0400

    add another optional parameter to the plugin to allow passing client options

--- packages/gatsby-source-mongodb/README.md
+++ packages/gatsby-source-mongodb/README.md
@@ -47,7 +47,8 @@ module.exports = {
 - **auth**: the authentication data to login a Mongodb collection, with sub
   properties user and password. ex. auth: { user: `admin`, password: `12345` }
 - **extraParams**: useful to set additional parameters for the connection, like authSource, ssl or replicaSet
-  (needed for connecting to MongoDB Atlas db as a service), ex: extraParams: { replicaSet: `test-shard-0`, ssl: `true`, authSource: `admin` }
+  (needed for connecting to MongoDB Atlas db as a service), ex: extraParams: { replicaSet: `test-shard-0`, ssl: `true`, authSource: `admin` }. These are the types of options that can be appended as query parameters to the connection URI: https://docs.mongodb.com/manual/reference/connection-string/#connections-connection-options
+- **clientOptions**: for setting options on the creation of a `new MongoClient`. By default, to handle the various connection URI's necessary for newer versions of MongoDB Atlas, for instance, we pass { `useNewUrlParser`: `true` }. You can override the default by passing either an empty object literal or filled with other valid connection options These are the subset of connection options that have to be passed as part of an object to the creation of a new `MongoClient` instance: http://mongodb.github.io/node-mongodb-native/3.1/reference/connecting/connection-settings/
 
 ### Mapping mediatype feature
 
---
+++ packages/gatsby-source-mongodb/gatsby-node.js
@@ -0,0 +1,122 @@
+"use strict";
+
+const MongoClient = require(`mongodb`).MongoClient;
+
+const crypto = require(`crypto`);
+
+const prepareMappingChildNode = require(`./mapping`);
+
+const queryString = require(`query-string`);
+
+exports.sourceNodes = ({
+  actions,
+  getNode,
+  createNodeId,
+  hasNodeChanged
+}, pluginOptions) => {
+  const {
+    createNode
+  } = actions;
+  let serverOptions = pluginOptions.server || {
+    address: `localhost`,
+    port: 27017
+  };
+  let dbName = pluginOptions.dbName || `local`,
+      authUrlPart = ``;
+  if (pluginOptions.auth) authUrlPart = `${pluginOptions.auth.user}:${pluginOptions.auth.password}@`;
+  let connectionExtraParams = getConnectionExtraParams(pluginOptions.extraParams);
+  const clientOptions = pluginOptions.clientOptions || { useNewUrlParser: true }
+  const connectionURL = pluginOptions.connectionString ? `${pluginOptions.connectionString}/${dbName}${connectionExtraParams}` : `mongodb://${authUrlPart}${serverOptions.address}:${serverOptions.port}/${dbName}${connectionExtraParams}`;
+  const mongoClient = new MongoClient(connectionURL, clientOptions);
+  return mongoClient.connect().then(client => {
+    const db = client.db(dbName);
+    let collection = pluginOptions.collection || [`documents`];
+
+    if (!Array.isArray(collection)) {
+      collection = [collection];
+    }
+
+    return Promise.all(collection.map(col => createNodes(db, pluginOptions, dbName, createNode, createNodeId, col))).then(() => {
+      mongoClient.close();
+    }).catch(err => {
+      console.warn(err);
+      mongoClient.close();
+      return err;
+    });
+  }).catch(err => {
+    console.warn(err);
+    return err;
+  });
+};
+
+function createNodes(db, pluginOptions, dbName, createNode, createNodeId, collectionName) {
+  return new Promise((resolve, reject) => {
+    let collection = db.collection(collectionName);
+    let cursor = collection.find(); // Execute the each command, triggers for each document
+
+    cursor.toArray((err, documents) => {
+      if (err) {
+        reject(err);
+      }
+
+      documents.forEach(item => {
+        var id = item._id.toString();
+
+        delete item._id;
+        var node = { // Data for the node.
+          ...item,
+          id: createNodeId(`${id}`),
+          mongodb_id: id,
+          parent: `__${collectionName}__`,
+          children: [],
+          internal: {
+            type: `mongodb${sanitizeName(dbName)}${sanitizeName(collectionName)}`,
+            content: JSON.stringify(item),
+            contentDigest: crypto.createHash(`md5`).update(JSON.stringify(item)).digest(`hex`)
+          }
+        };
+        const childrenNodes = [];
+
+        if (pluginOptions.map) {
+          let mapObj = pluginOptions.map;
+
+          if (pluginOptions.map[collectionName]) {
+            mapObj = pluginOptions.map[collectionName];
+          } // We need to map certain fields to a contenttype.
+
+
+          Object.keys(mapObj).forEach(mediaItemFieldKey => {
+            if (node[mediaItemFieldKey] && (typeof mapObj[mediaItemFieldKey] === `string` || mapObj[mediaItemFieldKey] instanceof String)) {
+              const mappingChildNode = prepareMappingChildNode(node, mediaItemFieldKey, node[mediaItemFieldKey], mapObj[mediaItemFieldKey], createNode);
+              node[`${mediaItemFieldKey}___NODE`] = mappingChildNode.id;
+              childrenNodes.push(mappingChildNode);
+              delete node[mediaItemFieldKey];
+            }
+          });
+        }
+
+        createNode(node);
+        childrenNodes.forEach(node => {
+          createNode(node);
+        });
+      });
+      resolve();
+    });
+  });
+}
+
+function sanitizeName(s) {
+  return s.replace(/[^_a-zA-Z0-9]/, ``).replace(/\b\w/g, l => l.toUpperCase());
+}
+
+function getConnectionExtraParams(extraParams) {
+  let connectionSuffix;
+
+  if (extraParams) {
+    connectionSuffix = queryString.stringify(extraParams, {
+      sort: false
+    });
+  }
+
+  return connectionSuffix ? `?` + connectionSuffix : ``;
+}
