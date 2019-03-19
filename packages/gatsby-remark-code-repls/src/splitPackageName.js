e87a71f540c7935a254f153251c2dac979415adb
Krzysztof Pniak <krzysztof.pniak@cardinality.co.uk>
2019-03-19 16:31:15 +0100

    feat(gatsby-remark-code-repls): adds splitPackageName implementation

--- packages/gatsby-remark-code-repls/src/splitPackageName.js
+++ packages/gatsby-remark-code-repls/src/splitPackageName.js
@@ -1,3 +1,12 @@
-const splitPackageName = dependency => {}
+const splitPackageName = dependency => {
+  if (dependency.substr(1).includes(`@`)) {
+    const splitIndex = dependency.lastIndexOf(`@`)
+    const name = dependency.substr(0, splitIndex)
+    const version = dependency.substr(splitIndex + 1)
+    return [name, version]
+  } else {
+    return [dependency, null]
+  }
+}
 
 module.exports = { splitPackageName }
