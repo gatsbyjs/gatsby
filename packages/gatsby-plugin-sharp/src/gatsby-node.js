a71f3c714db89f05791c3cd893335fbf824dd5df
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2019-03-20 18:13:44 +0530

    Check for sharp in entrypoint

--- packages/gatsby-plugin-sharp/src/gatsby-node.js
+++ packages/gatsby-plugin-sharp/src/gatsby-node.js
@@ -1,20 +1,4 @@
 exports.onPreInit = ({ actions, reporter }, pluginOptions) => {
-  try {
-    require(`sharp`)
-  } catch (error) {
-    // Bail early if sharp isn't available
-    reporter.panic(
-      reporter.stripIndent`
-        The dependency "sharp" does not seem to have been built or installed correctly.
-
-        - Try to reinstall packages and look for errors during installation
-        - Consult "sharp" installation page at http://sharp.pixelplumbing.com/en/stable/install/
-        
-        If neither of the above work, please open an issue in https://github.com/gatsbyjs/gatsby/issues
-      `
-    )
-  }
-
   const { setBoundActionCreators, setPluginOptions } = require(`./index`)
 
   setBoundActionCreators(actions)
