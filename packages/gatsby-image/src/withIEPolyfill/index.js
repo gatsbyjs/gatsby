2fae59e76d155d6468b0421e1fe7307d9ab187cc
Michael Uloth <hello@michaeluloth.com>
2019-03-20 22:09:50 -0400

    Apply polyfill to current image only

--- packages/gatsby-image/src/withIEPolyfill/index.js
+++ packages/gatsby-image/src/withIEPolyfill/index.js
@@ -1,30 +1,42 @@
-import React from "react"
+import React, { Component, createRef } from "react"
 import PropTypes from "prop-types"
 import Image from "../index"
 
-// Dynamically load polyfill for object-fit/object-position if needed (e.g. in IE)
-const testImg = document.createElement(`img`)
-if (
-  typeof testImg.style.objectFit === `undefined` ||
-  typeof testImg.style.objectPosition === `undefined`
-) {
-  import(`object-fit-images`).then(({ default: ObjectFitImages }) =>
-    ObjectFitImages()
-  )
-}
+class ImageWithIEPolyfill extends Component {
+  constructor(props) {
+    super(props)
+    this.imageRef = createRef()
+  }
 
-function ImageWithIEPolyfill({ objectFit, objectPosition, ...props }) {
-  return (
-    <Image
-      {...props}
-      imgStyle={{
-        ...props.imgStyle,
-        objectFit: objectFit,
-        objectPosition: objectPosition,
-        fontFamily: `"object-fit: ${objectFit}; object-position: ${objectPosition}"`,
-      }}
-    />
-  )
+  // Load object-fit/position polyfill if required (e.g. in IE)
+  componentDidMount() {
+    const testImg = document.createElement(`img`)
+    if (
+      typeof testImg.style.objectFit === `undefined` ||
+      typeof testImg.style.objectPosition === `undefined`
+    ) {
+      import(`object-fit-images`).then(({ default: ObjectFitImages }) =>
+        ObjectFitImages(this.imageRef.current.querySelector(`img`))
+      )
+    }
+  }
+
+  render() {
+    const { objectFit, objectPosition, ...props } = this.props
+
+    return (
+      <Image
+        ref={this.imageRef}
+        {...props}
+        imgStyle={{
+          ...props.imgStyle,
+          objectFit: objectFit,
+          objectPosition: objectPosition,
+          fontFamily: `"object-fit: ${objectFit}; object-position: ${objectPosition}"`,
+        }}
+      />
+    )
+  }
 }
 
 ImageWithIEPolyfill.propTypes = {
