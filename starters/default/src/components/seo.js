6e530b4651c12bbf352840134358146c8bce02cf
Thomas Wang <iothomaswang@gmail.com>
2019-03-22 07:42:36 +0900

    usePrefix with other imports in gatsby

--- starters/default/src/components/seo.js
+++ starters/default/src/components/seo.js
@@ -8,8 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
-import { useStaticQuery, graphql } from "gatsby"
-import { withPrefix } from "gatsby"
+import { useStaticQuery, graphql, withPrefix } from "gatsby"
 
 function SEO({ lang, meta, title, description, author, keywords, url, image }) {
   const { site } = useStaticQuery(
