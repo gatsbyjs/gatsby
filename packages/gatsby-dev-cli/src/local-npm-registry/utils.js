685213c00f6e1f7d214c9c81ab714fdcc9d5260a
Michal Piechowiak <misiek.piechowiak@gmail.com>
2019-03-22 21:00:13 +0100

    less console output

--- packages/gatsby-dev-cli/src/local-npm-registry/utils.js
+++ packages/gatsby-dev-cli/src/local-npm-registry/utils.js
@@ -6,11 +6,13 @@ exports.promisifiedSpawn = ([cmd, args = [], spawnArgs = {}]) =>
     const proc = child_process.spawn(cmd, args, spawnArgs)
 
     // piping output is probably just temporary for dev - it adds a lot of noise
-    proc.stdout.setEncoding(`utf8`)
-    proc.stdout.on(`data`, console.log)
+    if (process.env.DEBUG) {
+      proc.stdout.setEncoding(`utf8`)
+      proc.stdout.on(`data`, console.log)
 
-    proc.stderr.setEncoding(`utf8`)
-    proc.stderr.on(`data`, console.error)
+      proc.stderr.setEncoding(`utf8`)
+      proc.stderr.on(`data`, console.error)
+    }
 
     proc.on(`close`, code => {
       if (code) {
