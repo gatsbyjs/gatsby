39403b8ac211d9c70b7fc028772aed37a1970807
Dustin Schau <dustinschau@gmail.com>
2019-03-28 11:19:35 -0500

    test: add a test for assetPrefix with nesting

--- packages/gatsby/src/joi-schemas/__tests__/joi.js
+++ packages/gatsby/src/joi-schemas/__tests__/joi.js
@@ -1,33 +1,41 @@
 const { gatsbyConfigSchema } = require(`../joi`)
 
 describe(`gatsby config`, () => {
-  it(`strips trailing slashes from url fields`, async () => {
+  it(`strips trailing slashes from url fields`, () => {
     const config = {
       pathPrefix: `/blog///`,
       assetPrefix: `https://cdn.example.com/`,
     }
 
-    expect(await gatsbyConfigSchema.validate(config)).toEqual({
+    expect(gatsbyConfigSchema.validate(config)).resolves.toEqual({
       pathPrefix: `/blog`,
       assetPrefix: `https://cdn.example.com`,
     })
   })
 
-  it(`allows assetPrefix to be full URL`, async () => {
+  it(`allows assetPrefix to be full URL`, () => {
     const config = {
       assetPrefix: `https://cdn.example.com`,
     }
 
-    expect(await gatsbyConfigSchema.validate(config)).toEqual(config)
+    expect(gatsbyConfigSchema.validate(config)).resolves.toEqual(config)
   })
 
-  it(`allows relative paths for url fields`, async () => {
+  it(`allows assetPrefix to be a URL with nested paths`, () => {
+    const config = {
+      assetPrefix: `https://cdn.example.com/some/nested/path`,
+    }
+
+    expect(gatsbyConfigSchema.validate(config)).resolves.toEqual(config)
+  })
+
+  it(`allows relative paths for url fields`, () => {
     const config = {
       pathPrefix: `/blog`,
       assetPrefix: `/assets`,
     }
 
-    expect(await gatsbyConfigSchema.validate(config)).toEqual(config)
+    expect(gatsbyConfigSchema.validate(config)).resolves.toEqual(config)
   })
 
   it(`does not allow pathPrefix to be full URL`, () => {
