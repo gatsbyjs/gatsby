bbce98bd64405f2b1ec3dfff7d7d510ea5e41fe5
Michal Piechowiak <misiek.piechowiak@gmail.com>
2019-03-28 14:54:03 +0100

    document traversePackageDocs fn

--- packages/gatsby-dev-cli/src/watch.js
+++ packages/gatsby-dev-cli/src/watch.js
@@ -20,6 +20,31 @@ const quit = () => {
   process.exit()
 }
 
+/**
+ * Compile final list of packages to watch
+ * This will include packages explictely defined packages and all their dependencies
+ * Also creates dependency graph that is used later to determine which packages
+ * would need to be published when their dependencies change
+ * @param {Object} $0
+ * @param {String} $0.root Path to root of Gatsby monorepo repository
+ * @param {String[]} $0.packages Initial array of packages to watch
+ * This can be extracted from project dependencies or explictely set by `--packages` flag
+ * @param {String[]} $0.monoRepoPackages Array of packages in Gatsby monorepo
+ * @param {String[]} $0.seenPackages Array of packages that were already traversed.
+ * This makes sure dependencies are extracted one time for each package and avoid any
+ * infinite loops.
+ * @param {Object} $0.depTree Lookup table to check dependants for given package.
+ * Used to determine which packages need to be published. Example shape:
+ * ```
+ * {
+ *   "gatsby-cli": Set(["gatsby"]),
+ *   "gatsby-telemtry": Set(["gatsby", "gatsby-cli"]),
+ *   "gatsby-source-filesystem": Set(["gatsby-source-contentful", "gatsby-source-drupal", "gatsby-source-wordpress", etc])
+ *   // no package have remark plugin in dependencies - so dependant list is empty
+ *   "gatsby-transformer-remark": Set([])
+ * }
+ * ```
+ */
 const traversePackagesDeps = ({
   root,
   packages,
