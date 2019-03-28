853ceb9b45608b2db728fbc0c4e19e6c9fb0fea4
Hentie Louw <hentie.louw@ovoenergy.com>
2019-03-28 15:10:36 +0000

    fix(scroll): use location.key for scroll behaviours (#12403)
    ## Description
    
    This is a simple fix to use `location.key` for saving scroll state, if it's available.
    
    Based on the suggestions found in this discussion: https://github.com/taion/scroll-behavior/issues/135#issuecomment-468962628
    
    ## Related Issues
    
    Fixes #12390.

--- e2e-tests/production-runtime/cypress/integration/1-production.js
+++ e2e-tests/production-runtime/cypress/integration/1-production.js
@@ -11,47 +11,6 @@ describe(`Production build tests`, () => {
     })
   }
 
-  it(`should restore scroll position only when going back in history`, () => {
-    cy.getTestElement(`long-page`)
-      .click()
-      .waitForAPI(`onRouteUpdate`)
-
-    cy.scrollTo(`bottom`)
-
-    // allow ScrollContext to update scroll position store
-    // it uses requestAnimationFrame so wait a bit to allow
-    // it to store scroll position
-    cy.wait(500)
-
-    cy.getTestElement(`below-the-fold`)
-      .click()
-      .waitForAPI(`onRouteUpdate`)
-
-    // after going back we expect page will
-    // be restore previous scroll position
-    cy.go(`back`).waitForAPI(`onRouteUpdate`)
-
-    cy.window().then(win => {
-      expect(win.scrollY).not.to.eq(0, 0)
-    })
-
-    cy.go(`forward`).waitForAPI(`onRouteUpdate`)
-
-    // after clicking link we expect page will be scrolled to top
-    cy.getTestElement(`long-page`)
-      .click()
-      .waitForAPI(`onRouteUpdate`)
-
-    cy.window().then(win => {
-      expect(win.scrollY).to.eq(0, 0)
-    })
-
-    // reset to index page
-    cy.getTestElement(`index-link`)
-      .click()
-      .waitForAPI(`onRouteUpdate`)
-  })
-
   it(`should navigate back after a reload`, () => {
     cy.getTestElement(`page2`).click()
 
