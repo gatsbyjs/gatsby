gatsbyjs/gatsby

commit 464ed71ce58bef04d66bcce7d0728e0e1e0ea36a

Changed the wording to be more factual, specific, and "you" oriented (#12886)
* Changed the wording to be more factual, specific, and "you" oriented

* Update www/src/pages/docs/index.js

www/src/pages/docs/index.js
@@ -25,18 +25,16 @@ class IndexRoute extends React.Component {
             <p>There are four main ways to get started with Gatsby:</p>
             <ol>
               <li>
-                <Link to="/tutorial/">Tutorial</Link>: The tutorial is written
-                to be as accessible as possible to people without much web
-                development experience.
+                <Link to="/tutorial/">Tutorial</Link>: Step-by-step instructions on how to install Gatsby and start a project: written for people without Gatsby or web development experience, though it has helped developers of all skill levels.
+                development experience, though developers of all skill levels have said they got a lot of value from it.
               </li>
               <li>
-                <Link to="/docs/quick-start">Quick start</Link>: The quick start
-                is intended for intermediate to advanced developers who prefer
-                to dig straight in.
+                <Link to="/docs/quick-start">Quick start</Link>: Go to the quick start
+                for a one page summary of how to install Gatsby and start a new project.
               </li>
               <li>
                 <Link to="/docs/recipes/">Recipes</Link>: A happy medium between
-                the tutorial and the quick start, find some quick answers for
+                the tutorial and the quick start. Find some quick answers for
                 how to accomplish some specific, common tasks with Gatsby.
               </li>
               <li>

commit 03aa1dbe312a14735281ec6daec1693b6efccbba

prettier fix, issue introduced in #12886
Signed-off-by: Alessandro De Blasis <alex@deblasis.net>

www/src/pages/docs/index.js
@@ -25,12 +25,17 @@ class IndexRoute extends React.Component {
             <p>There are four main ways to get started with Gatsby:</p>
             <ol>
               <li>
-                <Link to="/tutorial/">Tutorial</Link>: Step-by-step instructions on how to install Gatsby and start a project: written for people without Gatsby or web development experience, though it has helped developers of all skill levels.
-                development experience, though developers of all skill levels have said they got a lot of value from it.
+                <Link to="/tutorial/">Tutorial</Link>: Step-by-step instructions
+                on how to install Gatsby and start a project: written for people
+                without Gatsby or web development experience, though it has
+                helped developers of all skill levels. development experience,
+                though developers of all skill levels have said they got a lot
+                of value from it.
               </li>
               <li>
-                <Link to="/docs/quick-start">Quick start</Link>: Go to the quick start
-                for a one page summary of how to install Gatsby and start a new project.
+                <Link to="/docs/quick-start">Quick start</Link>: Go to the quick
+                start for a one page summary of how to install Gatsby and start
+                a new project.
               </li>
               <li>
                 <Link to="/docs/recipes/">Recipes</Link>: A happy medium between