7cbdc13556fdbc9584e36cd026395b516c0adb08
Michal Piechowiak <misiek.piechowiak@gmail.com>
2019-03-29 13:39:56 +0100

    chore: format

--- www/src/pages/docs/index.js
+++ www/src/pages/docs/index.js
@@ -25,12 +25,17 @@ class IndexRoute extends React.Component {
             <p>There are four main ways to get started with Gatsby:</p>
             <ol>
               <li>
-                <Link to="/tutorial/">Tutorial</Link>: Step-by-step instructions on how to install Gatsby and start a project: written for people without Gatsby or web development experience, though it has helped developers of all skill levels.
-                development experience, though developers of all skill levels have said they got a lot of value from it.
+                <Link to="/tutorial/">Tutorial</Link>: Step-by-step instructions
+                on how to install Gatsby and start a project: written for people
+                without Gatsby or web development experience, though it has
+                helped developers of all skill levels. development experience,
+                though developers of all skill levels have said they got a lot
+                of value from it.
               </li>
               <li>
-                <Link to="/docs/quick-start">Quick start</Link>: Go to the quick start
-                for a one page summary of how to install Gatsby and start a new project.
+                <Link to="/docs/quick-start">Quick start</Link>: Go to the quick
+                start for a one page summary of how to install Gatsby and start
+                a new project.
               </li>
               <li>
                 <Link to="/docs/recipes/">Recipes</Link>: A happy medium between
