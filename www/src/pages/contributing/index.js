ffcb75c3aa98cc4c89b3fee144d27d0c83754bde
Sam Bhagwat <calcsam@gmail.com>
2019-03-30 20:59:18 -0700

    Update index.js

--- www/src/pages/contributing/index.js
+++ www/src/pages/contributing/index.js
@@ -21,7 +21,10 @@ class IndexRoute extends React.Component {
               Contributing to Gatsby.js
             </h1>
             <p>
-              Find guides on the Gatsby.js community, code of conduct, and how
+              Thanks for being interested in contributing! We're so glad you want to help!
+            </p>
+            <p>
+              Below you'll find guides on the Gatsby.js community, code of conduct, and how
               to get started contributing:
             </p>
             <ul>
