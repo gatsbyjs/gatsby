1383a9f9cc975c82998662909eb2b1f2657ca9ad
mikael-kristiansson <mikael.kristiansson@schibsted.com>
2019-04-02 14:57:17 +0200

    updated packages/gatsby to use @mikaelkristiansson/domready

--- packages/gatsby/cache-dir/app.js
+++ packages/gatsby/cache-dir/app.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import domReady from "domready-loaded"
+import domReady from "@mikaelkristiansson/domready"
 
 import socketIo from "./socketIo"
 import emitter from "./emitter"
