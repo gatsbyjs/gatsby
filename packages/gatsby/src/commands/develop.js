1b5f5f56f07b30ea58316a697aa077d6d6e438b0
Anthony Marcar <anthony@moocar.me>
2019-04-02 16:25:59 +1100

    make query queue constructable and add batch processing

--- packages/gatsby/src/commands/develop.js
+++ packages/gatsby/src/commands/develop.js
@@ -34,6 +34,8 @@ const apiRunnerNode = require(`../utils/api-runner-node`)
 const telemetry = require(`gatsby-telemetry`)
 const detectPortInUseAndPrompt = require(`../utils/detect-port-in-use-and-prompt`)
 const onExit = require(`signal-exit`)
+const pageQueryRunner = require(`../query/page-query-runner`)
+const queryQueue = require(`../query/queue`)
 
 // const isInteractive = process.stdout.isTTY
 
@@ -86,6 +88,8 @@ async function startServer(program) {
 
   // Start bootstrap process.
   await bootstrap(program)
+
+  pageQueryRunner.startListening(queryQueue.makeDevelop())
 
   await createIndexHtml()
 
