14ea6c807f7e99719913967b105f64ce8a7a00d5
Michal Piechowiak <misiek.piechowiak@gmail.com>
2019-04-02 13:26:39 +0200

    restore original e2e-test script

--- scripts/e2e-test.sh
+++ scripts/e2e-test.sh
@@ -4,15 +4,13 @@ CUSTOM_COMMAND="${2:-yarn test}"
 GATSBY_PATH="${CIRCLE_WORKING_DIRECTORY:-../../}"
 
 # cypress docker does not support sudo and do not need it but the default node executor does
-# command -v sudo && sudo npm install -g gatsby-dev-cli || npm install -g gatsby-dev-cli &&
+command -v sudo && sudo npm install -g gatsby-dev-cli || npm install -g gatsby-dev-cli &&
 
 # setting up child integration test link to gatsby packages
 cd $SRC_PATH &&
-#yarn &&
-#gatsby-dev --set-path-to-repo $GATSBY_PATH &&
-#gatsby-dev --scan-once --copy-all --quiet && # copies _all_ files in gatsby/packages
-node ../../packages/gatsby-dev-cli/dist/index.js --set-path-to-repo $GATSBY_PATH &&
-DEBUG=1 node ../../packages/gatsby-dev-cli/dist/index.js --scan-once --quiet && # copies _all_ files in gatsby/packages
+yarn &&
+gatsby-dev --set-path-to-repo $GATSBY_PATH &&
+gatsby-dev --scan-once --copy-all --quiet && # copies _all_ files in gatsby/packages
 chmod +x ./node_modules/.bin/gatsby && # this is sometimes necessary to ensure executable
 sh -c "$CUSTOM_COMMAND" &&
 echo "e2e test run succeeded"
