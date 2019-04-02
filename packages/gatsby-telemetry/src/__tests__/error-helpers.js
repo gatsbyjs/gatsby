bafe633c6bd76230e351ef817bb0482c804a6e61
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2019-04-02 19:11:02 +0530

    Add path segment test for sanitizeError

--- packages/gatsby-telemetry/src/__tests__/error-helpers.js
+++ packages/gatsby-telemetry/src/__tests__/error-helpers.js
@@ -46,7 +46,54 @@ describe(`Errors Helpers`, () => {
       )
       expect(localPathRegex.test(e.stack)).toBeTruthy()
 
-      expect(localPathRegex.test(e)).toBeFalsy()
+      const sanitizedErrorString = sanitizeError(e)
+
+      expect(sanitizedErrorString.includes(errormessage)).toBe(true)
+      expect(localPathRegex.test(sanitizedErrorString)).toBeFalsy()
+    })
+
+    it(`Sanitizes a section of the current path from error stacktraces`, () => {
+      const errormessage = `this is a test`
+
+      const e = {
+        message: errormessage,
+        stack: `
+        Error: this is an error
+          at Object.<anonymous> (/Users/sidharthachatterjee/Code/gatsby/packages/gatsby-telemetry/blah.js:1:69)
+          at Object.<anonymous> (/Users/sidharthachatterjee/Code/gatsby/fake-path/blah.js:1:69)
+          at Object.<anonymous> (/Users/sidharthachatterjee/.fake-path/index.js:1:69)
+          at Module._compile (internal/modules/cjs/loader.js:736:30)
+          at Object.Module._extensions..js (internal/modules/cjs/loader.js:747:10)
+          at Module.load (internal/modules/cjs/loader.js:628:32)
+          at tryModuleLoad (internal/modules/cjs/loader.js:568:12)
+          at Function.Module._load (internal/modules/cjs/loader.js:560:3)
+          at Function.Module.runMain (internal/modules/cjs/loader.js:801:12)
+          at executeUserCode (internal/bootstrap/node.js:526:15)
+          at startMainThreadExecution (internal/bootstrap/node.js:439:3)
+        `,
+      }
+
+      expect(e).toBeDefined()
+      expect(e.message).toEqual(errormessage)
+      expect(e.stack).toBeDefined()
+
+      const mockCwd = jest
+        .spyOn(process, `cwd`)
+        .mockImplementation(
+          () => `/Users/sidharthachatterjee/Code/gatsby/packages`
+        )
+
+      const usernameRegex = /sidharthachatterjee/
+      const fakePathRegex = /fake-path/g
+      expect(usernameRegex.test(e.stack)).toBeTruthy()
+
+      const sanitizedErrorString = sanitizeError(e)
+
+      expect(sanitizedErrorString.includes(errormessage)).toBe(true)
+      expect(usernameRegex.test(sanitizedErrorString)).toBe(false)
+      expect(sanitizedErrorString.match(fakePathRegex).length).toBe(2)
+
+      mockCwd.mockRestore()
     })
   })
   describe(`cleanPaths`, () => {
