3de89539ad112609088b60894f00b79ce33ada1a
Ward Peeters <ward@coding-tech.com>
2019-04-04 12:24:38 +0200

    test ci check

--- packages/gatsby-cli/src/reporter/ink/components/ink-spinner.js
+++ packages/gatsby-cli/src/reporter/ink/components/ink-spinner.js
@@ -1,90 +1,28 @@
-"use strict"
+import React, { Component } from "react"
+import PropTypes from "prop-types"
+import { Box } from "ink"
+import isCi from "is-ci"
+import spinners from "cli-spinners"
 
-Object.defineProperty(exports, `__esModule`, {
-  value: true,
-})
-exports.default = void 0
+const enableAnimation = !!process.stdout.isTTY && !isCi
 
-var _react = _interopRequireWildcard(require(`react`))
-
-var _propTypes = _interopRequireDefault(require(`prop-types`))
-
-var _ink = require(`ink`)
-
-var _cliSpinners = _interopRequireDefault(require(`cli-spinners`))
-
-function _interopRequireDefault(obj) {
-  return obj && obj.__esModule ? obj : { default: obj }
-}
-
-function _interopRequireWildcard(obj) {
-  if (obj && obj.__esModule) {
-    return obj
-  } else {
-    var newObj = {}
-    if (obj != null) {
-      for (var key in obj) {
-        if (Object.prototype.hasOwnProperty.call(obj, key)) {
-          var desc =
-            Object.defineProperty && Object.getOwnPropertyDescriptor
-              ? Object.getOwnPropertyDescriptor(obj, key)
-              : {}
-          if (desc.get || desc.set) {
-            Object.defineProperty(newObj, key, desc)
-          } else {
-            newObj[key] = obj[key]
-          }
-        }
-      }
-    }
-    newObj.default = obj
-    return newObj
+export default class Spinner extends Component {
+  static propTypes = {
+    type: PropTypes.string,
   }
-}
 
-function _defineProperty(obj, key, value) {
-  if (key in obj) {
-    Object.defineProperty(obj, key, {
-      value: value,
-      enumerable: true,
-      configurable: true,
-      writable: true,
-    })
-  } else {
-    obj[key] = value
+  static defaultProps = {
+    type: `dots`,
   }
-  return obj
-}
 
-const enableAnimation = !!process.stdout.isTTY
-console.log(enableAnimation)
-
-class Spinner extends _react.Component {
-  constructor(...args) {
-    super(...args)
-
-    _defineProperty(this, `state`, {
-      frame: 0,
-    })
-
-    _defineProperty(this, `switchFrame`, () => {
-      const { frame } = this.state
-      const spinner = this.getSpinner()
-      const isLastFrame = frame === spinner.frames.length - 1
-      const nextFrame = isLastFrame ? 0 : frame + 1
-      this.setState({
-        frame: nextFrame,
-      })
-    })
+  state = {
+    frame: 0,
   }
 
   render() {
     const spinner = this.getSpinner()
-    return _react.default.createElement(
-      _ink.Box,
-      null,
-      spinner.frames[this.state.frame]
-    )
+
+    return <Box>{spinner.frames[this.state.frame]}</Box>
   }
 
   componentDidMount() {
@@ -105,16 +43,17 @@ class Spinner extends _react.Component {
   }
 
   getSpinner() {
-    return _cliSpinners.default[this.props.type] || _cliSpinners.default.dots
+    return spinners[this.props.type] || spinners.dots
+  }
+
+  switchFrame = () => {
+    const { frame } = this.state
+    const spinner = this.getSpinner()
+    const isLastFrame = frame === spinner.frames.length - 1
+    const nextFrame = isLastFrame ? 0 : frame + 1
+
+    this.setState({
+      frame: nextFrame,
+    })
   }
 }
-
-exports.default = Spinner
-
-_defineProperty(Spinner, `propTypes`, {
-  type: _propTypes.default.string,
-})
-
-_defineProperty(Spinner, `defaultProps`, {
-  type: `dots`,
-})
