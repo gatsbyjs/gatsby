ed606d76c659ddff2d79a2aae0442e4a6aa28aac
Paul Melnikow <email@paulmelnikow.com>
2019-04-04 10:37:23 -0400

    chore: replace parse-filepath polyfill with native path.parse

--- packages/gatsby-plugin-page-creator/src/create-path.js
+++ packages/gatsby-plugin-page-creator/src/create-path.js
@@ -1,9 +1,8 @@
 // @flow
-import parsePath from "parse-filepath"
 import path from "path"
 
 module.exports = (filePath: string): string => {
-  const { dirname, name } = parsePath(filePath)
+  const { dirname, name } = path.parse(filePath)
   const parsedName = name === `index` ? `` : name
 
   return path.posix.join(`/`, dirname, parsedName, `/`)
