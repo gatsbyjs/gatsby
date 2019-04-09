cac34e78ab69231ad7c03893b0bc773eb62502be
Dustin Schau <dustinschau@gmail.com>
2019-04-08 22:53:27 -0700

    Revert "Merge branch 'fix/clipboard-on-windows' into pieh-patch-2"
    This reverts commit 153dfdffd18865df12a39a0786c39c817bcf153a, reversing
    changes made to c546285cf4e5210e265a19aaa17e568553444c7b.

--- packages/gatsby-cli/src/create-cli.js
+++ packages/gatsby-cli/src/create-cli.js
@@ -5,7 +5,6 @@ const report = require(`./reporter`)
 const didYouMean = require(`./did-you-mean`)
 const envinfo = require(`envinfo`)
 const existsSync = require(`fs-exists-cached`).sync
-const clipboardy = require(`clipboardy`)
 const {
   trackCli,
   setDefaultTags,
@@ -241,36 +240,26 @@ function buildLocalCommands(cli, isLocalSite) {
       }),
     handler: args => {
       try {
-        const copyToClipboard =
-          // Clipboard is not accessible when on a linux tty
-          process.platform === `linux` && !process.env.DISPLAY
-            ? false
-            : args.clipboard
-
-        envinfo
-          .run(
-            {
-              System: [`OS`, `CPU`, `Shell`],
-              Binaries: [`Node`, `npm`, `Yarn`],
-              Browsers: [`Chrome`, `Edge`, `Firefox`, `Safari`],
-              Languages: [`Python`],
-              npmPackages: `gatsby*`,
-              npmGlobalPackages: `gatsby*`,
-            },
-            {
-              duplicates: true,
-              showNotFound: true,
-            }
-          )
-          .then(envinfoOutput => {
-            console.log(envinfoOutput)
-
-            if (copyToClipboard) {
-              clipboardy.writeSync(envinfoOutput)
-            }
-          })
+        envinfo.run(
+          {
+            System: [`OS`, `CPU`, `Shell`],
+            Binaries: [`Node`, `npm`, `Yarn`],
+            Browsers: [`Chrome`, `Edge`, `Firefox`, `Safari`],
+            Languages: [`Python`],
+            npmPackages: `gatsby*`,
+            npmGlobalPackages: `gatsby*`,
+          },
+          {
+            console: true,
+            // Clipboard is not accessible when on a linux tty
+            clipboard:
+              process.platform === `linux` && !process.env.DISPLAY
+                ? false
+                : args.clipboard,
+          }
+        )
       } catch (err) {
-        console.log(`Error: Unable to print environment info`)
+        console.log(`Error: unable to print environment info`)
         console.log(err)
       }
     },
