76d56ba1cb86ec6e331874116935feb9d71c5c6e
Anthony Marcar <anthony@moocar.me>
2019-04-10 20:28:17 +1000

    un-revert matchPaths.json

--- packages/gatsby/src/query/pages-writer.js
+++ packages/gatsby/src/query/pages-writer.js
@@ -124,6 +124,7 @@ const preferDefault = m => m && m.default || m
     writeAndMove(`pages.json`, JSON.stringify(pagesData, null, 4)),
     writeAndMove(`sync-requires.js`, syncRequires),
     writeAndMove(`async-requires.js`, asyncRequires),
+    writeAndMove(`match-paths.json`, JSON.stringify(matchPaths, null, 4)),
     writeAndMove(
       `data.json`,
       JSON.stringify({
