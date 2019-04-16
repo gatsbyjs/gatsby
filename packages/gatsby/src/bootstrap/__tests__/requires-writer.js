2fdd63ce29dc6c70c71d81146c2a777b5547f4c8
Anthony Marcar <anthony@moocar.me>
2019-04-16 15:34:38 -0700

    move query/pages-writer to bootstrap/requires-writer
    And delete data.json

---
+++ packages/gatsby/src/bootstrap/__tests__/requires-writer.js
@@ -0,0 +1,47 @@
+const { joinPath } = require(`../../utils/path`)
+const requiresWriter = require(`../requires-writer`)
+
+const now = Date.now()
+
+const newMockState = () => {
+  const pages = new Map()
+  pages.set(`path1`, {
+    component: `component1`,
+    componentChunkName: `chunkName1`,
+    matchPath: `matchPath1`,
+    path: `/path1`,
+  })
+  pages.set(`path2`, {
+    component: `component2`,
+    componentChunkName: `chunkName2`,
+    path: `/path2`,
+  })
+  const program = { directory: `/dir` }
+  return { pages, program }
+}
+
+jest.mock(`fs-extra`, () => {
+  return {
+    writeFile: () => Promise.resolve(),
+    move: () => {},
+  }
+})
+
+const mockFsExtra = require(`fs-extra`)
+
+describe(`requires-writer`, () => {
+  beforeEach(() => {
+    global.Date.now = () => now
+    requiresWriter.resetLastHash()
+  })
+  describe(`writeAll`, () => {
+    it(`writes requires files`, async () => {
+      const spy = jest.spyOn(mockFsExtra, `writeFile`)
+      await requiresWriter.writeAll(newMockState())
+      expect(spy).toBeCalledWith(
+        joinPath(`/dir`, `.cache`, `match-paths.json.${now}`),
+        JSON.stringify([{ path: `/path1`, matchPath: `matchPath1` }], null, 4)
+      )
+    })
+  })
+})
--- packages/gatsby/src/commands/develop.js
+++ packages/gatsby/src/commands/develop.js
@@ -38,6 +38,7 @@ const onExit = require(`signal-exit`)
 const queryUtil = require(`../query`)
 const queryQueue = require(`../query/queue`)
 const queryWatcher = require(`../query/query-watcher`)
+const requiresWriter = require(`../bootstrap/requires-writer`)
 
 // const isInteractive = process.stdout.isTTY
 
@@ -93,6 +94,7 @@ async function startServer(program) {
 
   db.startAutosave()
   queryUtil.startListening(queryQueue.createDevelopQueue())
+  requiresWriter.startListener()
   queryWatcher.startWatchDeletePage()
 
   await createIndexHtml()
