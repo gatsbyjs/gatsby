0c26fdccc82228d66a96a3c95561b9065dded746
Anthony Marcar <anthony@moocar.me>
2019-04-16 11:40:28 -0700

    remove pages.json

--- packages/gatsby/src/query/pages-writer.js
+++ packages/gatsby/src/query/pages-writer.js
@@ -121,7 +121,6 @@ const preferDefault = m => m && m.default || m
   }
 
   const result = await Promise.all([
-    writeAndMove(`pages.json`, JSON.stringify(pagesData, null, 4)),
     writeAndMove(`sync-requires.js`, syncRequires),
     writeAndMove(`async-requires.js`, asyncRequires),
     writeAndMove(`match-paths.json`, JSON.stringify(matchPaths, null, 4)),
