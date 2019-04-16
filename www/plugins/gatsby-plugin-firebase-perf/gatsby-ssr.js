15c4b41838eda4e4d8d10e173b65eb7b7686b07c
Dustin Schau <dustinschau@gmail.com>
2019-04-15 20:30:55 -0500

    chore: fix issue

--- www/plugins/gatsby-plugin-firebase-perf/gatsby-ssr.js
+++ www/plugins/gatsby-plugin-firebase-perf/gatsby-ssr.js
@@ -11,13 +11,15 @@ exports.onRenderBody = function onRenderBody(
 
   setPostBodyComponents([
     React.createElement(`script`, {
-      key: `gatsby-pluginn-firebase-perf`,
+      key: `gatsby-plugin-firebase-perf`,
       dangerouslySetInnerHTML: {
         __html: `
           (function(sa,gai){function load(f,c){var a=document.createElement('script');
           a.async=1;a.src=f;a.onload=c;var s=document.getElementsByTagName('script')[0];
           s.parentNode.insertBefore(a,s);}load(sa);window.onload = function() {firebase.initializeApp({appId:gai}).performance();};
-          })('https://earlymonitoring.firebaseapp.com/index.min.js', ${appId});
+          })('https://earlymonitoring.firebaseapp.com/index.min.js', ${JSON.stringify(
+            appId
+          )});
         `,
       },
     }),
