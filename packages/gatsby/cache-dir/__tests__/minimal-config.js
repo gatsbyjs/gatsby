975670545b0cfa1c7632732ff44e862b8b37ef07
Ward Peeters <ward@coding-tech.com>
2019-04-17 14:04:50 +0200

    azure vs ward 4-0

--- packages/gatsby/cache-dir/__tests__/minimal-config.js
+++ packages/gatsby/cache-dir/__tests__/minimal-config.js
@@ -2,8 +2,13 @@
 // const child = require(`child_process`)
 
 it(`Builds cache-dir with minimal config`, done => {
-  expect(true).toBe(true)
   done()
+  const used = process.memoryUsage()
+  for (let key in used) {
+    console.log(
+      `${key} ${Math.round((used[key] / 1024 / 1024) * 100) / 100} MB`
+    )
+  }
   // const args = [
   //   require.resolve(`@babel/cli/bin/babel.js`),
   //   path.join(__dirname, `..`),
