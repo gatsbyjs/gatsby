efa9c8063769e32f9b8657920a28b37f67c36246
Julien Maury <6878027+jmau111@users.noreply.github.com>
2019-04-30 20:19:08 +0200

    fix nested SEO component not working
    seems that SEO cannot within Layout component, it needs to be outside

--- starters/default/src/pages/index.js
+++ starters/default/src/pages/index.js
@@ -6,16 +6,18 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
+  <>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+    <Layout>
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  </>
 )
 
 export default IndexPage
