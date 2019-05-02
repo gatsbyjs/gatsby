ca18691f22099b7258eb1e84cc3f945298d32780
Ward Peeters <ward@coding-tech.com>
2019-05-02 14:07:54 +0200

    azure 3

--- packages/gatsby-cli/src/reporter/reporters/index.js
+++ packages/gatsby-cli/src/reporter/reporters/index.js
@@ -2,7 +2,7 @@ const semver = require(`semver`)
 
 console.trace(`loading ink =====================`)
 if (semver.satisfies(process.version, `>=8`)) {
-  module.exports = require(`./yurnalist`)
+  module.exports = require(`./ink`).default
 } else {
   module.exports = require(`./yurnalist`)
 }
