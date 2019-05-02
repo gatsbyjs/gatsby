26293abb66516254687e3268c76681b991036657
Michal Piechowiak <misiek.piechowiak@gmail.com>
2019-05-02 14:46:16 +0200

    add more details to error message and instruct how to fix it

--- packages/gatsby-plugin-manifest/src/safe-sharp.js
+++ packages/gatsby-plugin-manifest/src/safe-sharp.js
@@ -1,6 +1,7 @@
 // this is very hacky (don't hate me), but at least it's not in [JSFuck](http://www.jsfuck.com/)
 
 const childProcess = require(`child_process`)
+const semver = require(`semver`)
 
 const originalConsoleError = console.error
 const restoreConsoleError = () => {
@@ -11,9 +12,14 @@ const handleMessage = msg => {
   if (msg.includes(`Incompatible library version: sharp.node requires`)) {
     restoreConsoleError()
 
-    let msg = [`Gatsby specific instructions here\n`]
+    let msg = [
+      `It looks like there are multiple versions of "sharp" module installed.`,
+      `Please update packages that depend on "sharp".`,
+      ``,
+    ]
 
     try {
+      let tmpMsg = []
       // npm list seems to work in yarn installed projects as well
       const { dependencies } = JSON.parse(
         childProcess.execSync(`npm list sharp --json`, {
@@ -36,17 +42,76 @@ const handleMessage = msg => {
         return null
       }
 
-      // list top level dependencies
-      msg = msg.concat([
-        `List of packages that depend on sharp:`,
-        ...Object.keys(dependencies).map(depName => {
+      const { latestVersion, topLevelPackages } = Object.keys(
+        dependencies
+      ).reduce(
+        (acc, depName) => {
           const sharpVersion = findSharpVersion(dependencies[depName])
-          return ` - ${depName}${sharpVersion ? ` (${sharpVersion})` : ``}`
+          if (sharpVersion) {
+            acc.topLevelPackages[depName] = sharpVersion
+
+            if (
+              !acc.latestVersion ||
+              semver.gt(sharpVersion, acc.latestVersion)
+            ) {
+              acc.latestVersion = sharpVersion
+            }
+          }
+
+          return acc
+        },
+        {
+          latestVersion: undefined,
+          topLevelPackages: {},
+        }
+      )
+
+      let packagesToUpdate = []
+      // list top level dependencies
+      tmpMsg = tmpMsg.concat([
+        `List of installed packages that depend on sharp:`,
+        ...Object.keys(topLevelPackages).map(depName => {
+          const sharpVersion = topLevelPackages[depName]
+          if (sharpVersion !== latestVersion) {
+            packagesToUpdate.push(depName)
+          }
+          return ` - ${depName}${
+            sharpVersion
+              ? ` (${sharpVersion})${
+                  sharpVersion !== latestVersion ? ` - needs update` : ``
+                }`
+              : ``
+          }`
         }),
       ])
+
+      if (packagesToUpdate.length > 0) {
+        tmpMsg = tmpMsg.concat([
+          ``,
+          `If you are using npm, run:`,
+          ``,
+          `npm install ${packagesToUpdate.join(` `)}`,
+          ``,
+          `If you are using yarn, run:`,
+          ``,
+          `yarn add ${packagesToUpdate.join(` `)}`,
+        ])
+      }
+
+      msg = msg.concat(tmpMsg)
     } catch {
-      // wat
+      msg = msg.concat([
+        `To get list of installed packages that depend on sharp try running:`,
+        ` - npm list sharp (if you use npm)`,
+        ` - yarn why sharp (if you use yarn)`,
+        ` and update packages that depend on version older than latest listed in output of above command.`,
+      ])
     }
+
+    msg = msg.concat([
+      ``,
+      `If after updating packages, older version of "sharp" is still showing up, please contact package maintainer and request updating "sharp" dependency.`,
+    ])
 
     console.error(msg.join(`\n`))
   }
