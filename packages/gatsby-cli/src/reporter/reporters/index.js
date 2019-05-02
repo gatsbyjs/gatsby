056580d26de74dc57ac9ef336112e1d86112aa20
Ward Peeters <ward@coding-tech.com>
2019-05-02 15:18:39 +0200

    keep yurnalist when on ci

--- packages/gatsby-cli/src/reporter/reporters/index.js
+++ packages/gatsby-cli/src/reporter/reporters/index.js
@@ -1,7 +1,7 @@
 const semver = require(`semver`)
+const { isCI } = require(`ci-info`)
 
-console.trace(`loading ink =====================`)
-if (semver.satisfies(process.version, `>=8`)) {
+if (semver.satisfies(process.version, `>=8`) && !isCI) {
   module.exports = require(`./ink`).default
 } else {
   module.exports = require(`./yurnalist`)
