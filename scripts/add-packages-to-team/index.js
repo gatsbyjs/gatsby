91abe03f44091da5585587af939418404d8ca859
Ward Peeters <ward@coding-tech.com>
2019-05-03 10:50:38 +0200

    fix linting

--- scripts/add-packages-to-team/index.js
+++ scripts/add-packages-to-team/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-const util = require(`util`)
-const exec = util.promisify(require(`child_process`).exec)
+// const util = require(`util`)
+// const exec = util.promisify(require(`child_process`).exec)
 const argv = require(`yargs`)
   .command(`$0`, `Add new owner to gatsby packages`, {
     user: {
