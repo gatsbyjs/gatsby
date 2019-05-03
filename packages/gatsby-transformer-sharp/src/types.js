bb0bb9c7fa4bb214559d3dcbda42945d8f8e9b70
Chuck <chuck@polishedwp.com>
2019-05-03 09:02:29 -0400

    chore(*): bump of sharp for Node v12 compatibility (#13646)

--- packages/gatsby-transformer-sharp/src/types.js
+++ packages/gatsby-transformer-sharp/src/types.js
@@ -6,7 +6,7 @@ const {
   GraphQLFloat,
   GraphQLEnumType,
 } = require(`gatsby/graphql`)
-const sharp = require(`sharp`)
+const sharp = require(`./safe-sharp`)
 const { Potrace } = require(`potrace`)
 
 const ImageFormatType = new GraphQLEnumType({
