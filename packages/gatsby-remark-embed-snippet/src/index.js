80ac3c348c11f903ad556d0de1dbce9405af1c0e
Tim Hagn <mail@timhagn.com>
2019-05-03 15:40:36 +0200

    remove superfluous code, clean up

--- packages/gatsby-remark-embed-snippet/src/index.js
+++ packages/gatsby-remark-embed-snippet/src/index.js
@@ -4,8 +4,6 @@ const fs = require(`fs`)
 const normalizePath = require(`normalize-path`)
 const visit = require(`unist-util-visit`)
 
-// const highlightCode = require(`gatsby-remark-prismjs/highlight-code`)
-
 // Language defaults to extension.toLowerCase();
 // This map tracks languages that don't match their extension.
 const FILE_EXTENSION_TO_LANGUAGE_MAP = {
@@ -52,26 +50,6 @@ module.exports = ({ markdownAST }, { directory } = {}) => {
       if (!fs.existsSync(path)) {
         throw Error(`Invalid snippet specified; no such file "${path}"`)
       }
-
-      const code = fs.readFileSync(path, `utf8`).trim()
-
-      // PrismJS's theme styles are targeting pre[class*="language-"]
-      // to apply its styles. We do the same here so that users
-      // can apply a PrismJS theme and get the expected, ready-to-use
-      // outcome without any additional CSS.
-      //
-      // @see https://github.com/PrismJS/prism/blob/1d5047df37aacc900f8270b1c6215028f6988eb1/themes/prism.css#L49-L54
-      const language = getLanguage(file)
-
-      // Allow users to specify a custom class prefix to avoid breaking
-      // line highlights if Prism is required by any other code.
-      // This supports custom user styling without causing Prism to
-      // re-process our already-highlighted markup.
-      // @see https://github.com/gatsbyjs/gatsby/issues/1486
-      // const className = language
-      //   .split(` `)
-      //   .map(token => `${classPrefix}${token}`)
-      //   .join(` `)
 
       // Change the node type to code, insert our file as value and set language.
       try {
