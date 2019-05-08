10c2a6eae79fed78182169cf38fea139fe982f40
Sean King <sean@seanking.org>
2019-05-08 16:20:25 +1200

    [gatsby-plugin-preact] Disable preact in development

--- packages/gatsby-plugin-preact/src/__tests__/gatsby-node.js
+++ packages/gatsby-plugin-preact/src/__tests__/gatsby-node.js
@@ -4,7 +4,7 @@ describe(`gatsby-plugin-preact`, () => {
   it(`sets the correct webpack config`, () => {
     const actions = { setWebpackConfig: jest.fn() }
 
-    onCreateWebpackConfig({ stage: `develop`, actions })
+    onCreateWebpackConfig({ stage: `build-javascript`, actions })
 
     expect(actions.setWebpackConfig).toHaveBeenCalledTimes(1)
     expect(actions.setWebpackConfig).toHaveBeenCalledWith({
@@ -17,10 +17,10 @@ describe(`gatsby-plugin-preact`, () => {
     })
   })
 
-  it(`does not invoke setWebpackConfig when stage is develop-html`, () => {
+  it(`does not invoke setWebpackConfig when stage is develop`, () => {
     const actions = { setWebpackConfig: jest.fn() }
 
-    onCreateWebpackConfig({ stage: `develop-html`, actions })
+    onCreateWebpackConfig({ stage: `develop`, actions })
 
     expect(actions.setWebpackConfig).not.toHaveBeenCalled()
   })
--- packages/gatsby-plugin-preact/src/gatsby-node.js
+++ packages/gatsby-plugin-preact/src/gatsby-node.js
@@ -2,7 +2,7 @@ exports.onCreateWebpackConfig = ({ stage, actions }) => {
   // Requiring the server version of React-dom is hardcoded right now
   // in the development server. So we'll just avoid loading Preact there
   // for now.
-  if (stage !== `develop-html`) {
+  if (stage !== `develop`) {
     actions.setWebpackConfig({
       resolve: {
         alias: {
