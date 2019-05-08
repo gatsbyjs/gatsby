c34b080c6f7c44f914958fab848ee1e55479ed4d
hupe1980 <hupe@Franks-MBP-2.fritz.box>
2019-05-08 23:19:49 +0200

    fix: Drop preload link for json from offline shell

---
+++ packages/gatsby-plugin-offline/src/gatsby-ssr.js
@@ -0,0 +1,21 @@
+export const onPreRenderHTML = ({
+  getHeadComponents,
+  pathname,
+  replaceHeadComponents,
+}) => {
+  if (pathname !== `/offline-plugin-app-shell-fallback/`) return
+
+  const headComponents = getHeadComponents()
+
+  const filteredHeadComponents = headComponents.filter(
+    ({ type, props }) =>
+      !(
+        type === `link` &&
+        props.as === `fetch` &&
+        props.rel === `preload` &&
+        props.href.startsWith(`/static/d/`)
+      )
+  )
+
+  replaceHeadComponents(filteredHeadComponents)
+}
