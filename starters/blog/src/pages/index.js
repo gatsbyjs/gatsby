2a0821142c0049c67d77753cc9add5cc50c23424
Matt Downing <mattcdowning@gmail.com>
2019-05-10 14:48:23 -0400

    remove unnecessary keywords meta info

--- starters/blog/src/pages/index.js
+++ starters/blog/src/pages/index.js
@@ -14,10 +14,7 @@ class BlogIndex extends React.Component {
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
-        <SEO
-          title="All posts"
-          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
-        />
+        <SEO title="All posts" />
         <Bio />
         {posts.map(({ node }) => {
           const title = node.frontmatter.title || node.fields.slug
