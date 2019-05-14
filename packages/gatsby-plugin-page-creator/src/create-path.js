cc9d1533e8f0f147143a5c25f61145986f1baa04
Ward Peeters <ward@coding-tech.com>
2019-05-14 09:37:29 +0200

    rename dirname => dir

--- packages/gatsby-plugin-page-creator/src/create-path.js
+++ packages/gatsby-plugin-page-creator/src/create-path.js
@@ -2,8 +2,8 @@
 import path from "path"
 
 module.exports = (filePath: string): string => {
-  const { dirname, name } = path.parse(filePath)
+  const { dir, name } = path.parse(filePath)
   const parsedName = name === `index` ? `` : name
 
-  return path.posix.join(`/`, dirname, parsedName, `/`)
+  return path.posix.join(`/`, dir, parsedName, `/`)
 }
