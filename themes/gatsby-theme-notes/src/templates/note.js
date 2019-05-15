2d916e6a49005e4d07bb43656b7d114784efc0d2
John Otander <johnotander@gmail.com>
2019-05-15 12:14:12 -0600

    feat(gatsby-theme-notes): Implement initial version of notes theme (#14066)
    This implements the first pass of gatsby-theme-notes.
    
    It's still a WIP and Chris will be following with GraphQL schema
    updates. Eventually we will want to handle directory listing via
    GraphQL queries as well.
    
    This also bumps dependencies of all existing themes.

---
+++ themes/gatsby-theme-notes/src/templates/note.js
@@ -0,0 +1,19 @@
+import { graphql } from "gatsby"
+
+import Note from "../components/note"
+
+export default Note
+
+export const pageQuery = graphql`
+  query($id: String!, $title: String) {
+    note: mdx(id: { eq: $id }) {
+      id
+      code {
+        body
+      }
+    }
+    image: ogImage {
+      src(text: $title)
+    }
+  }
+`
