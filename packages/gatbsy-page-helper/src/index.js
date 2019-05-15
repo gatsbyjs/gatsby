8af3fb68610dc0a8343d4142e55b4da3bcc2a115
FRIN Yvonnick <frin.yvonnick@gmail.com>
2019-05-15 09:29:38 +0200

    :tada: Create gatsby-page-helper package

---
+++ packages/gatbsy-page-helper/src/index.js
@@ -0,0 +1,9 @@
+import validatePath from "./validate-path"
+import createPath from "create-path"
+import ignorePath from "ignore-path"
+
+module.exports = {
+  validatePath,
+  createPath,
+  ignorePath,
+}
