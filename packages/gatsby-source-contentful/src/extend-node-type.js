691469e41dc78097d45e824689a48e32f5e9a813
Michal Piechowiak <misiek.piechowiak@gmail.com>
2019-05-21 01:01:25 +0200

    remove old RichTextNode type extension

--- packages/gatsby-source-contentful/src/extend-node-type.js
+++ packages/gatsby-source-contentful/src/extend-node-type.js
@@ -459,22 +459,6 @@ const fluidNodeType = ({ name, getTracedSVG }) => {
 }
 
 exports.extendNodeType = ({ type, store }) => {
-  if (type.name.match(/contentful.*RichTextNode/)) {
-    return {
-      nodeType: {
-        type: GraphQLString,
-        deprecationReason: `This field is deprecated, please use 'json' instead.`,
-      },
-      json: {
-        type: GraphQLJSON,
-        resolve: (source, fieldArgs) => {
-          const contentJSON = JSON.parse(source.internal.content)
-          return contentJSON
-        },
-      },
-    }
-  }
-
   if (type.name !== `ContentfulAsset`) {
     return {}
   }
