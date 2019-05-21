465ccf478bde1de4a19f601ac476cddc68fcba34
Ward Peeters <ward@coding-tech.com>
2019-05-22 00:28:07 +0200

    add progressbar to yurnalist

--- packages/gatsby-cli/src/reporter/reporters/yurnalist/index.js
+++ packages/gatsby-cli/src/reporter/reporters/yurnalist/index.js
@@ -2,9 +2,16 @@
 
 const { createReporter } = require(`yurnalist`)
 const convertHrtime = require(`convert-hrtime`)
+const ProgressBar = require(`progress`)
 
 const VERBOSE = process.env.gatsby_log_level === `verbose`
 const reporter = createReporter({ emoji: true, verbose: VERBOSE })
+
+const calcElapsedTime = startTime => {
+  const elapsed = process.hrtime(startTime)
+
+  return convertHrtime(elapsed)[`seconds`].toFixed(3)
+}
 
 /**
  * Reporter module.
@@ -29,36 +36,57 @@ module.exports = {
   info: reporter.info.bind(reporter),
   warn: reporter.warn.bind(reporter),
   log: reporter.log.bind(reporter),
-  /**
-   * Time an activity.
-   * @param {string} name - Name of activity.
-   * @returns {string} The elapsed time of activity.
-   */
-  createActivity(name) {
-    const spinner = reporter.activity()
-    const start = process.hrtime()
-    let status
 
-    const elapsedTime = () => {
-      var elapsed = process.hrtime(start)
-      return `${convertHrtime(elapsed)[`seconds`].toFixed(3)} s`
+  createActivity: activity => {
+    if (activity.type === 'spinner') {
+      const spinner = reporter.activity();
+      let start
+      let status
+
+      return {
+        update: newState => {
+          if (newState.startTime) {
+            start = newState.startTime
+            spinner.tick(activity.id)
+          }
+          if (newState.status) {
+            status = newState.status
+            spinner.tick(`${activity.id} — ${newState.status}`)
+          }
+        },
+        done: () => {
+          const str = status
+            ? `${activity.id} — ${calcElapsedTime(start)} — ${status}`
+            : `${activity.id} — ${calcElapsedTime(start)}`
+          reporter.success(str)
+          spinner.end()
+        },
+      }
     }
 
-    return {
-      start: () => {
-        spinner.tick(name)
-      },
-      setStatus: s => {
-        status = s
-        spinner.tick(`${name} — ${status}`)
-      },
-      end: () => {
-        const str = status
-          ? `${name} — ${elapsedTime()} — ${status}`
-          : `${name} — ${elapsedTime()}`
-        reporter.success(str)
-        spinner.end()
-      },
+    if (activity.type === 'progress') {
+      const bar = new ProgressBar(
+        ` [:bar] :current/:total :elapsed secs :percent ${activity.id}`,
+        {
+          total: 0,
+          width: 30,
+        }
+      )
+      return {
+        update: newState => {
+          if (newState.total) {
+            bar.total = newState.total
+          }
+          if (newState.current) {
+            bar.tick()
+          }
+        },
+        done: () => {
+          reporter.success(`${activity.id} — ${activity.current}/${activity.total} - ${calcElapsedTime(
+            activity.startTime
+          )} s`)
+        },
+      }
     }
   },
 }
