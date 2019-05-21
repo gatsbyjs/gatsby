c3ffc87d46a5e0aa9849b8608eb4795430ab7e7f
Maël Nison <nison.mael@gmail.com>
2019-05-21 13:24:31 +0200

    Adds support for PnP in test-require-error

--- packages/gatsby/src/utils/test-require-error.js
+++ packages/gatsby/src/utils/test-require-error.js
@@ -1,6 +1,12 @@
 // This module is also copied into the .cache directory some modules copied there
 // from cache-dir can also use this module.
 export default (moduleName, err) => {
+  // PnP will return the following code when a require is allowed per the
+  // dependency tree rules but the requested file doesn't exist
+  if (err.code === `QUALIFIED_PATH_RESOLUTION_FAILED`) {
+    return true
+  }
+
   const regex = new RegExp(
     `Error: Cannot find module\\s.${moduleName.replace(
       /[-/\\^$*+?.()|[\]{}]/g,
