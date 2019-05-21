2b9f55df00b16c1cf3621738c9746983ab442fd5
The Coding Aviator <34807532+thecodingaviator@users.noreply.github.com>
2019-05-21 12:21:58 +0530

    Revert changes to gatsby-source-contentful

--- packages/gatsby-source-contentful/src/extend-node-type.js
+++ packages/gatsby-source-contentful/src/extend-node-type.js
@@ -105,6 +105,7 @@ const resolveFixed = (image, options) => {
   fixedSizes.push(options.width)
   fixedSizes.push(options.width * 1.5)
   fixedSizes.push(options.width * 2)
+  fixedSizes.push(options.width * 3)
   fixedSizes = fixedSizes.map(Math.round)
 
   // Filter out sizes larger than the image's width.
@@ -126,6 +127,9 @@ const resolveFixed = (image, options) => {
           break
         case 2:
           resolution = `2x`
+          break
+        case 3:
+          resolution = `3x`
           break
         default:
       }
@@ -190,6 +194,7 @@ const resolveFluid = (image, options) => {
   fluidSizes.push(options.maxWidth)
   fluidSizes.push(options.maxWidth * 1.5)
   fluidSizes.push(options.maxWidth * 2)
+  fluidSizes.push(options.maxWidth * 3)
   fluidSizes = fluidSizes.map(Math.round)
 
   // Filter out sizes larger than the image's maxWidth.
