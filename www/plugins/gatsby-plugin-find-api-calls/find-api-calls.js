88882e9c2e0213edee83c41599b0b463963d80c6
Lennart <lekoarts@gmail.com>
2019-05-22 16:32:27 +0200

    get createNode working

--- www/plugins/gatsby-plugin-find-api-calls/find-api-calls.js
+++ www/plugins/gatsby-plugin-find-api-calls/find-api-calls.js
@@ -1,22 +1,24 @@
 const parser = require(`@babel/parser`)
 const traverse = require(`@babel/traverse`).default
 const fs = require(`fs-extra`)
-const glob = require(`glob`)
+const util = require(`util`)
+const globCB = require(`glob`)
+const glob = util.promisify(globCB)
 
 const buckets = [
   {
     test: /api-runner-node/,
-    group: `Node_API`,
+    group: `NodeAPI`,
     hits: new Map(),
   },
   {
     test: /api-runner-browser/,
-    group: `Browser_API`,
+    group: `BrowserAPI`,
     hits: new Map(),
   },
   {
     test: /api-runner-ssr/,
-    group: `SSR_API`,
+    group: `SSRAPI`,
     hits: new Map(),
   },
 ]
@@ -63,82 +65,76 @@ const PARSER_OPTIONS = {
   ],
 }
 
-module.exports = function findApiCalls(globRegex) {
-  glob(
-    globRegex,
-    {
-      ignore: [
-        `**/commonjs/**`,
-        `**/node_modules/**`,
-        `**/__tests__/**`,
-        `**/dist/**`,
-        `**/__mocks__/**`,
-        `babel.config.js`,
-        `graphql.js`,
-        `**/flow-typed/**`,
-      ],
-    },
-    (er, files) => {
-      files.forEach(file => {
-        const code = fs.readFileSync(file, { encoding: `utf-8` })
+module.exports = async function findApiCalls(globRegex) {
+  const files = await glob(globRegex, {
+    ignore: [
+      `**/commonjs/**`,
+      `**/node_modules/**`,
+      `**/__tests__/**`,
+      `**/dist/**`,
+      `**/__mocks__/**`,
+      `babel.config.js`,
+      `graphql.js`,
+      `**/flow-typed/**`,
+    ],
+  })
 
-        try {
-          const ast = parser.parse(code, PARSER_OPTIONS)
-          traverse(ast, {
-            CallExpression(path) {
-              if (path.node.callee && path.node.callee.name === `require`) {
-                const arg = path.node.arguments[0]
-                let bucket
-                if (
-                  arg.quasis &&
-                  (bucket = buckets.find(a =>
-                    a.test.test(arg.quasis[0].value.raw)
-                  ))
-                ) {
-                  const test = path.findParent(
-                    path => path.type === `VariableDeclarator`
+  files.forEach(file => {
+    const code = fs.readFileSync(file, { encoding: `utf-8` })
+
+    try {
+      const ast = parser.parse(code, PARSER_OPTIONS)
+      traverse(ast, {
+        CallExpression(path) {
+          if (path.node.callee && path.node.callee.name === `require`) {
+            const arg = path.node.arguments[0]
+            let bucket
+            if (
+              arg.quasis &&
+              (bucket = buckets.find(a => a.test.test(arg.quasis[0].value.raw)))
+            ) {
+              const test = path.findParent(
+                path => path.type === `VariableDeclarator`
+              )
+              if (test) {
+                const apiRunnerName = test.node.id.name
+                const references =
+                  test.scope.bindings[apiRunnerName].referencePaths
+
+                references.forEach(ref => {
+                  const call = ref.findParent(
+                    path => path.type === `CallExpression`
                   )
-                  if (test) {
-                    const apiRunnerName = test.node.id.name
-                    const references =
-                      test.scope.bindings[apiRunnerName].referencePaths
+                  if (
+                    call &&
+                    call.node.arguments &&
+                    call.node.arguments[0].quasis
+                  ) {
+                    const apiHookName =
+                      call.node.arguments[0].quasis[0].value.raw
+                    if (!bucket.hits.has(apiHookName)) {
+                      bucket.hits.set(apiHookName, [])
+                    }
 
-                    references.forEach(ref => {
-                      const call = ref.findParent(
-                        path => path.type === `CallExpression`
-                      )
-                      if (
-                        call &&
-                        call.node.arguments &&
-                        call.node.arguments[0].quasis
-                      ) {
-                        const apiHookName =
-                          call.node.arguments[0].quasis[0].value.raw
-                        if (!bucket.hits.has(apiHookName)) {
-                          bucket.hits.set(apiHookName, [])
-                        }
-
-                        const instance = {
-                          file,
-                          location: call.node.loc,
-                        }
-                        bucket.hits.get(apiHookName).push(instance)
-                      }
-                    })
+                    const instance = {
+                      file,
+                      location: call.node.loc,
+                    }
+                    bucket.hits.get(apiHookName).push(instance)
                   }
-                }
+                })
               }
-            },
-          })
-        } catch (e) {
-          console.log(`parsing error`, {
-            file,
-            e,
-          })
-        }
+            }
+          }
+        },
+      })
+    } catch (e) {
+      console.log(`parsing error`, {
+        file,
+        e,
       })
     }
-  )
+  })
 
   return buckets
 }
--- www/plugins/gatsby-plugin-find-api-calls/gatsby-node.js
+++ www/plugins/gatsby-plugin-find-api-calls/gatsby-node.js
@@ -1,22 +1,59 @@
 const path = require(`path`)
 const findApiCalls = require(`./find-api-calls`)
 
-exports.sourceNodes = ({
+exports.sourceNodes = async ({
   actions,
   createNodeId,
   createContentDigest,
-  reporter,
 }) => {
   const { createNode } = actions
 
   const searchPath = path.join(process.cwd(), `../`)
-
   const globRegex = `${searchPath}**/*.js`
 
-  console.log(`path`, searchPath)
-  console.log(`glob`, globRegex)
+  const buckets = await findApiCalls(globRegex)
 
-  const buckets = findApiCalls(globRegex)
+  /*
+  ApiCalls {
+    edges {
+      group // SSR, Browser
+      name
+      hits {
+        location // line
+      }
+    }
+  }
+  */
+
+  buckets.forEach(bucket => {
+    bucket.hits.forEach((hits, api) => {
+      const apiHits = hits.map(
+        hit =>
+          `https://github.com/gatsbyjs/gatsby/blob/master/packages/gatsby/${
+            hit.file
+          }#L${hit.location.start.line}-L${hit.location.end.line}`
+      )
+
+      const node = {
+        id: createNodeId(`findApiCalls-${bucket.group}-${api}`),
+        parent: null,
+        children: [],
+        group: bucket.group,
+        name: api,
+        hits: apiHits,
+        //[api]: apiHits,
+        internal: {
+          type: `GatsbyAPICall`,
+          //content: JSON.stringify(api),
+          //contentDigest: createContentDigest(api),
+        },
+      }
+
+      node.internal.contentDigest = createContentDigest(node)
+
+      createNode(node)
+    })
+  })
 
   console.log(buckets)
 }
