940c3d619e9aee19ea0ea1f01fa3cdd72dce92e0
Florian Kissling <florian.kissling@gmail.com>
2019-05-23 13:00:20 +0200

    Fix „Featured Sites“ gradient overlay for Safari

--- www/src/views/showcase/featured-sites.js
+++ www/src/views/showcase/featured-sites.js
@@ -315,7 +315,7 @@ class FeaturedSites extends Component {
               right: `-${space[6]}`,
               width: 60,
               pointerEvents: `none`,
-              background: `linear-gradient(90deg, rgba(0,0,0,0) 0%, rgba(255,255,255,1) 100%)`,
+              background: `linear-gradient(90deg, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 100%)`,
             }}
           />
         </div>
