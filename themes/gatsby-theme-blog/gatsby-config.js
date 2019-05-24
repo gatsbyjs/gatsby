604cc64f772b142a6a77ae33e1ccc4d074f1d6ba
Brent Jackson <jxnblk@gmail.com>
2019-05-24 14:02:23 -0400

    Disable global styles

--- themes/gatsby-theme-blog/gatsby-config.js
+++ themes/gatsby-theme-blog/gatsby-config.js
@@ -1,5 +1,3 @@
-const pkg = require("./package.json");
-
 module.exports = {
   siteMetadata: {
     title: `My Blog Title`,
@@ -7,8 +5,8 @@ module.exports = {
     description: `coming soon...`,
     siteUrl: `https://github.com/amberleyromo/gatsby-themes/tree/master/packages/gatsby-theme-darklight-blog`,
     social: {
-      twitter: `amber1ey`
-    }
+      twitter: `amber1ey`,
+    },
   },
   plugins: [
     /*
@@ -17,16 +15,16 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: "blog-posts",
-        path: `content/posts`
-      }
+        name: `blog-posts`,
+        path: `content/posts`,
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: "blog-assets",
-        path: `content/assets`
-      }
+        name: `blog-assets`,
+        path: `content/assets`,
+      },
     },
     /*
      * Default/demo posts
@@ -34,16 +32,16 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: "blog-default-posts",
-        path: `${__dirname}/content/posts`
-      }
+        name: `blog-default-posts`,
+        path: `${__dirname}/content/posts`,
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: "blog-default-assets",
-        path: `${__dirname}/content/assets`
-      }
+        name: `blog-default-assets`,
+        path: `${__dirname}/content/assets`,
+      },
     },
     {
       resolve: `gatsby-transformer-remark`,
@@ -52,50 +50,50 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 590
-            }
+              maxWidth: 590,
+            },
           },
           {
             resolve: `gatsby-remark-responsive-iframe`,
             options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`
-            }
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
           },
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
-          `gatsby-remark-autolink-headers`
-        ]
-      }
+          `gatsby-remark-autolink-headers`,
+        ],
+      },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-mdx`,
       options: {
-        extensions: [".mdx", ".md"],
+        extensions: [`.mdx`, `.md`],
         gatsbyRemarkPlugins: [
           {
-            resolve: "gatsby-remark-images",
+            resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 1380,
-              linkImagesToOriginal: false
-            }
+              linkImagesToOriginal: false,
+            },
           },
-          { resolve: "gatsby-remark-responsive-iframe" },
-          { resolve: "gatsby-remark-copy-linked-files" },
-          { resolve: "gatsby-remark-numbered-footnotes" },
-          { resolve: "gatsby-remark-smartypants" },
-          { resolve: "gatsby-remark-code-titles" },
-          { resolve: "gatsby-remark-prismjs" }
-        ]
-      }
+          { resolve: `gatsby-remark-responsive-iframe` },
+          { resolve: `gatsby-remark-copy-linked-files` },
+          { resolve: `gatsby-remark-numbered-footnotes` },
+          { resolve: `gatsby-remark-smartypants` },
+          { resolve: `gatsby-remark-code-titles` },
+          { resolve: `gatsby-remark-prismjs` },
+        ],
+      },
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // trackingId: `ADD YOUR TRACKING ID HERE`,
-      }
+      },
     },
     {
       resolve: `gatsby-plugin-manifest`,
@@ -106,18 +104,18 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: require.resolve(`./content/assets/gatsby-icon.png`)
-      }
+        icon: require.resolve(`./content/assets/gatsby-icon.png`),
+      },
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-twitter`,
     `gatsby-plugin-netlify`,
     {
-      resolve: "gatsby-plugin-page-creator",
+      resolve: `gatsby-plugin-page-creator`,
       options: {
-        path: `${__dirname}/src/pages`
-      }
+        path: `${__dirname}/src/pages`,
+      },
     },
-  ]
-};
+  ],
+}
