2dd76728503993dc59590c9e2d0d1d226237c471
Brent Jackson <jxnblk@gmail.com>
2019-05-24 14:45:34 -0400

    Update styles in blog theme bio component

--- themes/gatsby-theme-blog/src/components/bio.js
+++ themes/gatsby-theme-blog/src/components/bio.js
@@ -8,6 +8,7 @@
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 import Image from "gatsby-image"
+import { Flex, css } from "theme-ui"
 import BioFragment from "../fragments/bio.mdx"
 
 function Bio() {
@@ -17,27 +18,19 @@ function Bio() {
       render={data => {
         const { author } = data.site.siteMetadata
         return (
-          <div
-            style={{
-              display: `flex`,
-              // marginBottom: rhythm(2.5)
-            }}
-          >
+          <Flex mb={2}>
             <Image
               fixed={data.avatar.childImageSharp.fixed}
               alt={author}
-              style={{
-                // marginRight: rhythm(1 / 2),
-                marginBottom: 0,
-                minWidth: 50,
-                borderRadius: `100%`,
-              }}
-              imgStyle={{
-                borderRadius: `50%`,
-              }}
+              css={css({
+                mr: 1,
+                mb: 0,
+                width: 48,
+                borderRadius: 99999,
+              })}
             />
             <BioFragment />
-          </div>
+          </Flex>
         )
       }}
     />
@@ -48,7 +41,7 @@ const bioQuery = graphql`
   query BioQuery {
     avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
       childImageSharp {
-        fixed(width: 50, height: 50) {
+        fixed(width: 48, height: 48) {
           ...GatsbyImageSharpFixed
         }
       }
