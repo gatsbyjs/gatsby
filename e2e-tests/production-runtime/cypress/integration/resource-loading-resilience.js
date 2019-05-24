34e352e847464e1b06d17af9cbec63f72bf10b3d
Anthony Marcar <anthony@moocar.me>
2019-05-24 13:27:52 +1000

    add page-data to production-runtime/resource-loading-resilience test
    Also use cypress tasks for blocking resources instead of npm run chunks

--- e2e-tests/production-runtime/cypress/integration/resource-loading-resilience.js
+++ e2e-tests/production-runtime/cypress/integration/resource-loading-resilience.js
@@ -1,118 +1,110 @@
-// TODO need to update to work with page-data.json
+Cypress.on(`uncaught:exception`, (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  console.log(err)
+  return false
+})
 
-// Cypress.on(`uncaught:exception`, (err, runnable) => {
-//   // returning false here prevents Cypress from
-//   // failing the test
-//   console.log(err)
-//   return false
-// })
+const waitForAPIOptions = {
+  timeout: 3000,
+}
 
-// const waitForAPIOptions = {
-//   timeout: 3000,
-// }
+const runTests = () => {
+  it(`Loads index`, () => {
+    cy.visit(`/`).waitForAPIorTimeout(`onRouteUpdate`, waitForAPIOptions)
+    cy.getTestElement(`dom-marker`).contains(`index`)
+  })
 
-// const runTests = () => {
-//   it(`Loads index`, () => {
-//     cy.visit(`/`).waitForAPIorTimeout(`onRouteUpdate`, waitForAPIOptions)
-//     cy.getTestElement(`dom-marker`).contains(`index`)
-//   })
+  it(`Navigates to second page`, () => {
+    cy.getTestElement(`page2`).click()
+    cy.waitForAPIorTimeout(`onRouteUpdate`, waitForAPIOptions)
+      .location(`pathname`)
+      .should(`equal`, `/page-2/`)
+    cy.getTestElement(`dom-marker`).contains(`page-2`)
+  })
 
-//   it(`Navigates to second page`, () => {
-//     cy.getTestElement(`page2`).click()
-//     cy.waitForAPIorTimeout(`onRouteUpdate`, waitForAPIOptions)
-//       .location(`pathname`)
-//       .should(`equal`, `/page-2/`)
-//     cy.getTestElement(`dom-marker`).contains(`page-2`)
-//   })
+  it(`Navigates to 404 page`, () => {
+    cy.getTestElement(`404`).click()
+    cy.waitForAPIorTimeout(`onRouteUpdate`, waitForAPIOptions)
+      .location(`pathname`)
+      .should(`equal`, `/page-3/`)
+    cy.getTestElement(`dom-marker`).contains(`404`)
+  })
 
-//   it(`Navigates to 404 page`, () => {
-//     cy.getTestElement(`404`).click()
-//     cy.waitForAPIorTimeout(`onRouteUpdate`, waitForAPIOptions)
-//       .location(`pathname`)
-//       .should(`equal`, `/page-3/`)
-//     cy.getTestElement(`dom-marker`).contains(`404`)
-//   })
+  it(`Loads 404`, () => {
+    cy.visit(`/page-3/`, {
+      failOnStatusCode: false,
+    }).waitForAPIorTimeout(`onRouteUpdate`, waitForAPIOptions)
+    cy.getTestElement(`dom-marker`).contains(`404`)
+  })
 
-//   it(`Loads 404`, () => {
-//     cy.visit(`/page-3/`, {
-//       failOnStatusCode: false,
-//     }).waitForAPIorTimeout(`onRouteUpdate`, waitForAPIOptions)
-//     cy.getTestElement(`dom-marker`).contains(`404`)
-//   })
+  it(`Can navigate from 404 to index`, () => {
+    cy.getTestElement(`index`).click()
+    cy.waitForAPIorTimeout(`onRouteUpdate`, waitForAPIOptions)
+      .location(`pathname`)
+      .should(`equal`, `/`)
+    cy.getTestElement(`dom-marker`).contains(`index`)
+  })
+}
 
-//   it(`Can navigate from 404 to index`, () => {
-//     cy.getTestElement(`index`).click()
-//     cy.waitForAPIorTimeout(`onRouteUpdate`, waitForAPIOptions)
-//       .location(`pathname`)
-//       .should(`equal`, `/`)
-//     cy.getTestElement(`dom-marker`).contains(`index`)
-//   })
-// }
+describe(`Every resources available`, () => {
+  it(`Restore resources`, () => {
+    cy.task(`restoreAllBlockedResources`)
+  })
+  runTests()
+})
 
-// describe(`Every resources available`, () => {
-//   it(`Restore resources`, () => {
-//     cy.exec(`npm run chunks -- restore`)
-//   })
-//   runTests()
-// })
+const runBlockedScenario = (scenario, args) => {
+  it(`Block resources`, () => {
+    cy.task(`restoreAllBlockedResources`).then(() => {
+      cy.task(scenario, args).then(() => {
+        runTests()
+      })
+    })
+  })
+}
 
-// describe(`Missing top level resources`, () => {
-//   describe(`Deleted pages manifest`, () => {
-//     it(`Block resources`, () => {
-//       cy.exec(`npm run chunks -- restore`)
-//       cy.exec(`npm run chunks -- block pages-manifest`)
-//     })
-//     runTests()
-//   })
+describe(`Missing top level resources`, () => {
+  describe(`Deleted app chunk assets`, () => {
+    runBlockedScenario(`blockAssetsForChunk`, { chunk: `app` })
+  })
+})
 
-//   describe(`Deleted app chunk assets`, () => {
-//     it(`Block resources`, () => {
-//       cy.exec(`npm run chunks -- restore`)
-//       cy.exec(`npm run chunks -- block app`)
-//     })
-//     runTests()
-//   })
-// })
+const runSuiteForPage = (label, pagePath) => {
+  describe(`Missing "${label}" resources`, () => {
+    describe(`Missing "${label}" page query results`, () => {
+      runBlockedScenario(`blockAssetsForPage`, {
+        pagePath,
+        filter: `page-data`,
+      })
+    })
+    describe(`Missing "${label}" page page-template asset`, () => {
+      runBlockedScenario(`blockAssetsForPage`, {
+        pagePath,
+        filter: `page-template`,
+      })
+    })
+    describe(`Missing "${label}" page extra assets`, () => {
+      runBlockedScenario(`blockAssetsForPage`, {
+        pagePath,
+        filter: `extra`,
+      })
+    })
+    describe(`Missing all "${label}" page assets`, () => {
+      runBlockedScenario(`blockAssetsForPage`, {
+        pagePath,
+        filter: `all`,
+      })
+    })
+  })
+}
 
-// const runSuiteForPage = (label, path) => {
-//   describe(`Missing "${label}" resources`, () => {
-//     describe(`Missing "${label}" page query results`, () => {
-//       it(`Block resources`, () => {
-//         cy.exec(`npm run chunks -- restore`)
-//         cy.exec(`npm run chunks -- block-page ${path} query-result`)
-//       })
-//       runTests()
-//     })
-//     describe(`Missing "${label}" page page-template asset`, () => {
-//       it(`Block resources`, () => {
-//         cy.exec(`npm run chunks -- restore`)
-//         cy.exec(`npm run chunks -- block-page ${path} page-template`)
-//       })
-//       runTests()
-//     })
-//     describe(`Missing "${label}" page extra assets`, () => {
-//       it(`Block resources`, () => {
-//         cy.exec(`npm run chunks -- restore`)
-//         cy.exec(`npm run chunks -- block-page ${path} extra`)
-//       })
-//       runTests()
-//     })
-//     describe(`Missing all "${label}" page assets`, () => {
-//       it(`Block resources`, () => {
-//         cy.exec(`npm run chunks -- restore`)
-//         cy.exec(`npm run chunks -- block-page ${path} all`)
-//       })
-//       runTests()
-//     })
-//   })
-// }
+runSuiteForPage(`Index`, `/`)
+runSuiteForPage(`Page-2`, `/page-2/`)
+runSuiteForPage(`404`, `/404.html`)
 
-// runSuiteForPage(`Index`, `/`)
-// runSuiteForPage(`Page-2`, `/page-2/`)
-// runSuiteForPage(`404`, `/404.html`)
-
-// describe(`Cleanup`, () => {
-//   it(`Restore resources`, () => {
-//     cy.exec(`npm run chunks -- restore`)
-//   })
-// })
+describe(`Cleanup`, () => {
+  it(`Restore resources`, () => {
+    cy.task(`restoreAllBlockedResources`)
+  })
+})
