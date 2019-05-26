gatsbyjs/gatsby

commit 851397f19f0bc12793b7a46e9067726bc3c72c06

refactor(www): Color tokens (#14259)
* refactor(www): Roll out new color palette

Mostly done, WIP irt naming and a couple leftovers too refactor.

- Remove most of the named color shortcuts until we figure out a good naming convention (or not ;-)) — „most“ to keep noise as low as possible:
  - `gatsbyDark` -> `purple[800]`
  - `gatsbyDarker` — not needed anymore
  - `lavender` -> `purple[400]`
  - `wisteria` -> `lavender` -> `purple[200]`
  - `accentLight` -> `orange[200]`
  - `accentDark` -> `orange[900]`
  - `skyLight` -> `teal[100]`
  - `skyDark` -> `blue[700]`
  - `mint` -> `teal[400]`
  - `success` -> `green[500]`
  - `lemon` -> `yellow[400]`
- Remove outdated comments in `src/utils/tokens/colors`.
- Consolidate `className`-basedCSS styles for SVG icons from `src/components/navigation-mobile` and `src/components/layout/page-heading`.
- Merge former colors.gray with new palette.grey, and move most of the former colors.gray to role-based tokens (defining their roles in the UI), laying the foundation for color themes:
  - `colors.gray.superLight` -> `colors.gray.border`
  - `colors.gray.border` -> `colors.ui.border.subtle`
  - `colors.gray.lightCopy` and `colors.gray.bright` -> `colors.gray.calm` -> `colors.text.secondary`
  - `colors.gray.dark` -> `colors.text.header` — plus remove it from headlines where it’s redundant
  - `colors.gray.whisper` -> `colors.ui.background`
  - `colors.gray.light`, `colors.ui.whisper` -> various (`colors.lilac`, `colors.ui.border.form`)
  - `colors.ui.bright` -> `colors.purple[200]`

Along the way:
- Remove Webkit scrollbar styles for `/ecosystem/`.
- Remove a border-radius in `src/components/rotator` that shouldn’t have been there in the first place.
- Fix a few border colors (`ui.light` -> `ui.border.subtle` mostly).
- Fix (as in „comply to WCAG 2.0 AA“) color contrast of
  - `/plugins/` NPM download count (also simplify markup)
  - `/showcase/` and `/starters/` filter count

www/src/components/banner.js
@@ -26,7 +26,7 @@ const InnerContainer = styled(`div`)`
 `
 
 const Content = styled(`div`)`
-  color: ${colors.ui.bright};
+  color: ${colors.purple[20]};
   font-family: ${fonts.header};
   padding-left: ${horizontalPadding};
   padding-right: ${horizontalPadding};

www/src/components/feedback-widget/widget-wrapper.js
@@ -30,7 +30,7 @@ const opacityEntry = keyframes`
 
 const WrapperDiv = styled(`div`)`
   background-color: ${colors.white};
-  border-top: 1px solid ${colors.gray.border};
+  border-top: 1px solid ${colors.ui.border.subtle};
   height: 100%;
   opacity: 0.5;
   padding: ${space[6]} 0;
@@ -72,7 +72,7 @@ const WrapperDiv = styled(`div`)`
 
   ${mediaQueries.lg} {
     box-shadow: ${shadows.overlay};
-    border: 1px solid ${colors.gray.border};
+    border: 0;
     border-radius: ${radii[2]}px;
     height: 100%;
     padding: ${space[7]} ${space[6]};

www/src/components/layout.js
@@ -10,9 +10,10 @@ import {
   radii,
   space,
   shadows,
-  mediaQueries,
   sizes,
+  fontSizes,
 } from "../utils/presets"
+import { breakpointGutter } from "../utils/styles"
 import Banner from "../components/banner"
 import Navigation from "../components/navigation"
 import MobileNavigation from "../components/navigation-mobile"
@@ -121,20 +122,21 @@ class DefaultLayout extends React.Component {
               <button
                 onClick={this.handleCloseModal}
                 css={{
-                  background: colors.ui.bright,
+                  background: colors.white,
                   border: 0,
-                  borderBottomLeftRadius: radii[1],
-                  borderTopRightRadius: radii[1],
-                  color: colors.gatsby,
+                  borderRadius: radii[6],
+                  color: colors.text.secondary,
                   cursor: `pointer`,
                   position: `absolute`,
                   left: `auto`,
-                  right: 0,
+                  right: space[7],
+                  top: space[8],
                   height: 40,
                   width: 40,
+                  fontSize: fontSizes[4],
                   "&:hover": {
-                    background: colors.gatsby,
-                    color: colors.white,
+                    background: colors.ui.hover,
+                    color: colors.gatsby,
                   },
                 }}
               >
@@ -163,7 +165,7 @@ class DefaultLayout extends React.Component {
             paddingTop: sizes.bannerHeight,
             // make room for the mobile navigation
             paddingBottom: sizes.headerHeight,
-            [mediaQueries.md]: {
+            [breakpointGutter]: {
               paddingTop: `calc(${sizes.bannerHeight} + ${sizes.headerHeight})`,
               paddingBottom: 0,
             },

www/src/components/navigation-mobile.js
@@ -73,8 +73,8 @@ const MobileNavigation = () => (
         bottom: 0,
         left: 0,
         right: 0,
-        zIndex: 1,
-        borderTop: `1px solid ${colors.ui.light}`,
+        zIndex: 10,
+        borderTop: `1px solid ${colors.ui.border.subtle}`,
         background: colors.white,
         height: sizes.headerHeight,
         fontFamily: fonts.header,
@@ -99,33 +99,14 @@ const MobileNavigation = () => (
 
 export default MobileNavigation
 
-const svgActive = {
-  ...svgStyles.active,
-}
-
 const styles = {
   svg: {
     default: {
-      "& .svg-stroke": {
-        strokeMiterlimit: 10,
-        strokeWidth: 1.4173,
-      },
-      "& .svg-stroke-accent": { stroke: colors.lavender },
-      "& .svg-stroke-lilac": { stroke: colors.lavender },
-      "& .svg-fill-lilac": { fill: colors.lavender },
-      "& .svg-fill-gatsby": { fill: colors.lavender },
-      "& .svg-fill-brightest": { fill: colors.white },
-      "& .svg-fill-accent": { fill: colors.lavender },
-      "& .svg-stroke-gatsby": { stroke: colors.lavender },
-      "& .svg-fill-gradient-accent-white-top": { fill: `transparent` },
-      "& .svg-fill-gradient-accent-white-45deg": { fill: `transparent` },
-      "& .svg-fill-gradient-accent-white-bottom": { fill: colors.white },
-      "& .svg-fill-gradient-purple": { fill: colors.lavender },
-      "& .svg-stroke-gradient-purple": { stroke: colors.lavender },
-      "& .svg-fill-wisteria": { fill: `transparent` },
-      "&:hover": { ...svgActive },
+      ...svgStyles.stroke,
+      ...svgStyles.default,
+      "&:hover": { ...svgStyles.active },
     },
-    active: svgActive,
+    active: svgStyles.active,
   },
   link: {
     default: {

www/src/components/navigation.js
@@ -15,6 +15,7 @@ import {
   sizes,
   fonts,
 } from "../utils/presets"
+import { breakpointGutter } from "../utils/styles"
 
 // what we need to nudge down the navItems to sit
 // on the baseline of the logo's wordmark
@@ -80,9 +81,9 @@ const Navigation = ({ pathname }) => {
         //   zIndex: 10,
         //   background: `red`,
         // },
-        [mediaQueries.md]: {
+        [breakpointGutter]: {
+          backgroundColor: isBlog ? colors.ui.background : false,
           position: isHomepage || isBlog ? `absolute` : `fixed`,
-          backgroundColor: isBlog ? colors.gray.whisper : false,
         },
         paddingLeft: `env(safe-area-inset-left)`,
         paddingRight: `env(safe-area-inset-right)`,
@@ -99,7 +100,7 @@ const Navigation = ({ pathname }) => {
             right: 0,
             height: 1,
             zIndex: -1,
-            background: isHomepage ? `transparent` : colors.gray.border,
+            background: isHomepage ? `transparent` : colors.ui.border.subtle,
           },
         }}
       >
@@ -204,7 +205,7 @@ const styles = {
   navItem: {
     fontSize: fontSizes[3],
     borderBottom: `2px solid transparent`,
-    color: colors.gray.copy,
+    color: colors.text.primary,
     display: `block`,
     WebkitFontSmoothing: `antialiased`,
     lineHeight: `calc(${sizes.headerHeight} - ${navItemTopOffset})`,
@@ -217,8 +218,8 @@ const styles = {
       color: colors.gatsby,
     },
     active: {
-      borderBottomColor: colors.gatsby,
-      color: colors.gatsby,
+      borderBottomColor: colors.lilac,
+      color: colors.lilac,
     },
   },
   socialIconItem: {

www/src/utils/styles.js
@@ -1,4 +1,3 @@
-import hex2rgba from "hex2rgba"
 import { keyframes } from "@emotion/core"
 
 import {
@@ -17,6 +16,11 @@ const stripeAnimation = keyframes({
   "100%": { backgroundPosition: `${space[7]} ${space[11]}` },
 })
 
+export const focusStyle = {
+  outline: 0,
+  boxShadow: `0 0 0 ${space[1]} ${colors.input.focusBoxShadow}`,
+}
+
 export const srOnly = {
   position: `absolute`,
   width: 1,
@@ -28,28 +32,11 @@ export const srOnly = {
   border: 0,
 }
 
-export const scrollbarStyles = {
-  WebkitOverflowScrolling: `touch`,
-  "&::-webkit-scrollbar": {
-    width: space[2],
-    height: space[2],
-  },
-  "&::-webkit-scrollbar-thumb": {
-    background: colors.ui.bright,
-  },
-  "&::-webkit-scrollbar-thumb:hover": {
-    background: colors.lilac,
-  },
-  "&::-webkit-scrollbar-track": {
-    background: colors.ui.light,
-  },
-}
-
 export const buttonStyles = {
   default: {
     alignItems: `center`,
     backgroundColor: colors.gatsby,
-    borderRadius: radii[1],
+    borderRadius: radii[2],
     borderWidth: 1,
     borderStyle: `solid`,
     borderColor: colors.gatsby,
@@ -74,8 +61,7 @@ export const buttonStyles = {
       borderColor: colors.gatsby,
     },
     ":focus": {
-      outline: 0,
-      boxShadow: `0 0 0 ${space[1]} ${hex2rgba(colors.lilac, 0.25)}`,
+      ...focusStyle,
     },
     ":after": { content: `''`, display: `block` },
     "& svg": { marginLeft: `.2em` },
@@ -97,6 +83,7 @@ export const buttonStyles = {
     },
   },
   tiny: {
+    borderRadius: radii[1],
     fontSize: fontSizes[1],
     padding: `${space[1]} ${space[2]}`,
     [mediaQueries.md]: {
@@ -104,17 +91,34 @@ export const buttonStyles = {
     },
   },
   ondark: {
-    border: `1px solid ${colors.ui.light}`,
-    background: colors.gatsbyDark,
+    border: `1px solid ${colors.purple[10]}`,
+    background: colors.purple[80],
   },
 }
 
 export const svgStyles = {
-  active: {
+  stroke: {
     "& .svg-stroke": {
-      strokeWidth: 1.4173,
       strokeMiterlimit: 10,
+      strokeWidth: 1.4173,
     },
+  },
+  default: {
+    "& .svg-stroke-accent": { stroke: colors.purple[40] },
+    "& .svg-stroke-lilac": { stroke: colors.purple[40] },
+    "& .svg-fill-lilac": { fill: colors.purple[40] },
+    "& .svg-fill-gatsby": { fill: colors.purple[40] },
+    "& .svg-fill-brightest": { fill: colors.white },
+    "& .svg-fill-accent": { fill: colors.purple[40] },
+    "& .svg-stroke-gatsby": { stroke: colors.purple[40] },
+    "& .svg-fill-gradient-accent-white-top": { fill: `transparent` },
+    "& .svg-fill-gradient-accent-white-45deg": { fill: `transparent` },
+    "& .svg-fill-gradient-accent-white-bottom": { fill: colors.white },
+    "& .svg-fill-gradient-purple": { fill: colors.purple[40] },
+    "& .svg-stroke-gradient-purple": { stroke: colors.purple[40] },
+    "& .svg-fill-lavender": { fill: `transparent` },
+  },
+  active: {
     "& .svg-stroke-accent": { stroke: colors.accent },
     "& .svg-stroke-lilac": { stroke: colors.lilac },
     "& .svg-stroke-gatsby": { stroke: colors.gatsby },
@@ -122,7 +126,7 @@ export const svgStyles = {
     "& .svg-fill-lilac": { fill: colors.lilac },
     "& .svg-fill-gatsby": { fill: colors.gatsby },
     "& .svg-fill-accent": { fill: colors.accent },
-    "& .svg-fill-wisteria": { fill: colors.wisteria },
+    "& .svg-fill-lavender": { fill: colors.lavender },
     "& .svg-fill-brightest": { fill: colors.white },
     "& .svg-fill-gradient-accent-white-45deg": {
       fill: `url(#accent-white-45deg)`,
@@ -144,7 +148,7 @@ export const linkStyles = {
   padding: `${space[3]} 0`,
   "&&": {
     border: 0,
-    color: colors.gray.calm,
+    color: colors.text.secondary,
     display: `flex`,
     fontWeight: `normal`,
   },
@@ -155,17 +159,29 @@ export const linkStyles = {
 
 export const formInput = {
   backgroundColor: colors.white,
-  border: `1px solid ${colors.ui.bright}`,
-  borderRadius: radii[1],
-  color: colors.brand,
+  border: `1px solid ${colors.input.border}`,
+  borderRadius: radii[2],
   padding: space[2],
+  fontFamily: fonts.system,
   fontSize: fontSizes[2],
   verticalAlign: `middle`,
   transition: `all ${transition.speed.default} ${transition.curve.default}`,
   "::placeholder": {
-    color: colors.lilac,
+    color: colors.text.placeholder,
     opacity: 1,
   },
+}
+
+export const formInputFocus = {
+  borderColor: colors.input.focusBorder,
+  ...focusStyle,
+}
+
+export const pullIntoGutter = {
+  marginLeft: `-${space[6]}`,
+  marginRight: `-${space[6]}`,
+  paddingLeft: space[6],
+  paddingRight: space[6],
 }
 
 export const skipLink = {
@@ -192,3 +208,5 @@ export const skipLink = {
     clip: `auto`,
   },
 }
+
+export const breakpointGutter = `@media (min-width: 42rem)`

www/src/components/pagination/index.js
@@ -3,7 +3,7 @@ import { navigate } from "gatsby"
 import ArrowForwardIcon from "react-icons/lib/md/arrow-forward"
 import ArrowBackIcon from "react-icons/lib/md/arrow-back"
 import PaginationLink from "./PaginationLink"
-import { colors, space, mediaQueries, fonts } from "../../utils/presets"
+import { colors, space, mediaQueries, fontSizes } from "../../utils/presets"
 
 class Pagination extends React.Component {
   changePage = e => {
@@ -22,8 +22,6 @@ class Pagination extends React.Component {
     const prevNextLinkStyles = {
       "&&": {
         borderBottom: 0,
-        fontFamily: fonts.header,
-        fontWeight: `bold`,
         color: colors.gatsby,
       },
     }
@@ -31,11 +29,18 @@ class Pagination extends React.Component {
     return (
       <div
         css={{
+          background: colors.ui.background,
           display: `flex`,
           justifyContent: `space-between`,
-          margin: `${space[5]} 0`,
+          margin: `0 -${space[6]}`,
+          padding: `${space[6]}`,
           flexDirection: `column`,
+          fontSize: fontSizes[1],
           [mediaQueries.md]: {
+            background: `transparent`,
+            borderTop: 0,
+            marginBottom: 0,
+            paddingTop: 0,
             flexDirection: `row`,
           },
         }}
@@ -68,7 +73,7 @@ class Pagination extends React.Component {
             display: `flex`,
             alignItems: `center`,
             justifyContent: `flex-end`,
-            fontFamily: fonts.header,
+            fontSize: fontSizes[1],
           }}
         >
           <span>Showing page &nbsp;</span>
@@ -79,7 +84,7 @@ class Pagination extends React.Component {
               appearance: `none`,
               border: `none`,
               padding: `0.5ch 2ch 0.5ch 0.5ch`,
-              color: `rebeccapurple`,
+              color: colors.gatsby,
               fontWeight: `bold`,
             }}
           >
@@ -98,8 +103,8 @@ class Pagination extends React.Component {
             viewBox="0 0 10 5"
             css={{
               position: `relative`,
-              right: `1.5ch`,
-              fill: `rebeccapurple`,
+              right: space[4],
+              fill: colors.gatsby,
               pointerEvents: `none`,
             }}
           >

www/src/pages/contributing/index.js
@@ -68,9 +68,8 @@ class IndexRoute extends React.Component {
               </li>
             </ul>
             <EmailCaptureForm signupMessage="Want to keep up with the latest tips &amp; tricks? Subscribe to our newsletter!" />
-
-            <FooterLinks />
           </Container>
+          <FooterLinks />
         </DocSearchContent>
       </Layout>
     )

commit e21af911c730b98fbaff0f8a2421b24c18c5dd07

fix(www): Sidebar z-index; add „zIndices“ tokens
- fix sidebar navigation on mobile being covered by the mobile navigation (regression introduced in  #14259 while trying to fix the feedback widget on smaller screens covering the mobile navigation)
- add `tokens/z-indices` (as per https://system-ui.com/theme)

www/src/components/banner.js
@@ -2,7 +2,7 @@ import React from "react"
 import styled from "@emotion/styled"
 import { OutboundLink } from "gatsby-plugin-google-analytics"
 
-import { colors, space, sizes, fonts } from "../utils/presets"
+import { colors, space, sizes, fonts, zIndices } from "../utils/presets"
 
 const horizontalPadding = space[6]
 const backgroundColor = colors.gatsby
@@ -12,7 +12,7 @@ const BannerContainer = styled(`aside`)`
   height: ${sizes.bannerHeight};
   position: fixed;
   width: 100%;
-  z-index: 3;
+  z-index: ${zIndices.banner};
   padding-left: env(safe-area-inset-left);
   padding-right: env(safe-area-inset-right);
 `

www/src/components/feedback-widget/widget-wrapper.js
@@ -8,6 +8,7 @@ import {
   radii,
   shadows,
   space,
+  zIndices,
 } from "../../utils/presets"
 
 const boldEntry = keyframes`
@@ -35,7 +36,7 @@ const WrapperDiv = styled(`div`)`
   opacity: 0.5;
   padding: ${space[6]} 0;
   width: 100%;
-  z-index: 2;
+  z-index: ${zIndices.feedbackWidget};
 
   [tabindex="-1"]:focus {
     outline: none;

www/src/components/layout.js
@@ -12,6 +12,7 @@ import {
   shadows,
   sizes,
   fontSizes,
+  zIndices,
 } from "../utils/presets"
 import { breakpointGutter } from "../utils/styles"
 import Banner from "../components/banner"
@@ -103,7 +104,7 @@ class DefaultLayout extends React.Component {
                 bottom: `unset`,
                 minHeight: `100%`,
                 minWidth: `100%`,
-                zIndex: 10,
+                zIndex: zIndices.modal,
                 overflowY: `auto`,
                 backgroundColor: `rgba(255, 255, 255, 0.95)`,
               },

www/src/components/navigation-mobile.js
@@ -18,6 +18,7 @@ import {
   fontSizes,
   lineHeights,
   fonts,
+  zIndices,
 } from "../utils/presets"
 import { svgStyles } from "../utils/styles"
 
@@ -73,7 +74,7 @@ const MobileNavigation = () => (
         bottom: 0,
         left: 0,
         right: 0,
-        zIndex: 10,
+        zIndex: zIndices.navigation,
         borderTop: `1px solid ${colors.ui.border.subtle}`,
         background: colors.white,
         height: sizes.headerHeight,

www/src/components/navigation.js
@@ -14,6 +14,7 @@ import {
   mediaQueries,
   sizes,
   fonts,
+  zIndices,
 } from "../utils/presets"
 import { breakpointGutter } from "../utils/styles"
 
@@ -67,7 +68,7 @@ const Navigation = ({ pathname }) => {
         left: 0,
         right: 0,
         top: sizes.bannerHeight,
-        zIndex: 2,
+        zIndex: zIndices.navigation,
         // use this to test if the header items are properly aligned to the logo
         // wordmark
         // "&:before": {

www/src/components/sidebar/sticky-responsive-sidebar.js
@@ -10,6 +10,7 @@ import {
   space,
   mediaQueries,
   sizes,
+  zIndices,
 } from "../../utils/presets"
 import { rhythm } from "../../utils/typography"
 import ScrollPositionProvider, {
@@ -120,7 +121,7 @@ const styles = {
     top: 0,
     transition: `opacity ${transition.speed.slow} ${transition.curve.default}`,
     width: 320,
-    zIndex: 10,
+    zIndex: zIndices.sidebar,
     [mediaQueries.md]: {
       height: `calc(100vh - ${sizes.headerHeight} - ${sizes.bannerHeight})`,
       maxWidth: `none`,
@@ -157,7 +158,7 @@ const styles = {
     right: space[6],
     visibility: `visible`,
     width: space[10],
-    zIndex: 20,
+    zIndex: zIndices.sidebarToggleButton,
     [mediaQueries.md]: { display: `none` },
   },
   sidebarToggleButtonInner: {

www/src/utils/presets.js
@@ -11,6 +11,7 @@ import {
   fontSizes,
   transition,
   fonts as fontTokens,
+  zIndices,
 } from "./tokens"
 import { rhythm } from "./typography"
 
@@ -34,4 +35,5 @@ export {
   space,
   fontSizes,
   fonts,
+  zIndices,
 }

www/src/utils/styles.js
@@ -10,6 +10,7 @@ import {
   lineHeights,
   fonts,
 } from "./presets"
+import { zIndices } from "./tokens"
 
 const stripeAnimation = keyframes({
   "0%": { backgroundPosition: `0 0` },
@@ -194,7 +195,7 @@ export const skipLink = {
   padding: 0,
   overflow: `hidden`,
   position: `absolute`,
-  zIndex: 100,
+  zIndex: zIndices.skipLink,
   fontSize: fontSizes[1],
   ":focus": {
     padding: space[4],

www/src/utils/tokens/index.js
@@ -10,6 +10,7 @@ import space from "./space"
 import transition from "./transition"
 import fontSizes from "./font-sizes"
 import fonts from "./fonts"
+import zIndices from "./z-indices"
 
 export {
   breakpoints,
@@ -24,4 +25,5 @@ export {
   transition,
   fontSizes,
   fonts,
+  zIndices,
 }

www/src/utils/tokens/z-indices.js
@@ -1,0 +1,9 @@
+export default {
+  feedbackWidget: 2,
+  navigation: 5,
+  banner: 10,
+  modal: 10,
+  sidebar: 10,
+  sidebarToggleButton: 20,
+  skipLink: 100,
+}