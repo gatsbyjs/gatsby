d13cbf21df76ce5712aed147f3b6a88c410d52ac
Charlie Wright <34384816+cwgw@users.noreply.github.com>
2019-05-28 06:18:34 -0700

    fix(gatsby-plugin-sharp): Image rotation (#14302)

--- packages/gatsby-plugin-sharp/src/index.js
+++ packages/gatsby-plugin-sharp/src/index.js
@@ -144,7 +144,11 @@ async function generateBase64({ file, args, reporter }) {
   })
   let pipeline
   try {
-    pipeline = sharp(file.absolutePath).rotate()
+    pipeline = sharp(file.absolutePath)
+
+    if (!options.rotate) {
+      pipeline.rotate()
+    }
   } catch (err) {
     reportError(`Failed to process image ${file.absolutePath}`, err, reporter)
     return null
