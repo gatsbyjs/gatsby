b9921dd4e32e212430a9e3d1786f02f06bea53a2
Ryan Drew <ryan.drew14@gmail.com>
2019-05-29 09:15:13 -0400

    don't fail if ink is installed on Node 6

--- packages/gatsby-cli/src/reporter/reporters/index.js
+++ packages/gatsby-cli/src/reporter/reporters/index.js
@@ -1,3 +1,4 @@
+const semver = require(`semver`)
 const { isCI } = require(`ci-info`)
 
 let inkExists = false
@@ -6,7 +7,7 @@ try {
   // eslint-disable-next-line no-empty
 } catch (err) {}
 
-if (inkExists && !isCI) {
+if (inkExists && semver.satisfies(process.version, `>=8`) && !isCI) {
   module.exports = require(`./ink`).default
 } else {
   module.exports = require(`./yurnalist`)
