8caa34a01979dd6ee0482d28d2f682f0ee79b32f
Ryan Drew <ryan.drew14@gmail.com>
2019-05-29 13:48:01 -0400

    make timeout shorter

--- packages/gatsby-plugin-google-tagmanager/src/gatsby-browser.js
+++ packages/gatsby-plugin-google-tagmanager/src/gatsby-browser.js
@@ -2,5 +2,5 @@ exports.onRouteUpdate = ({ location }) => {
   // wrap inside a timeout to ensure the title has properly been changed
   setTimeout(() => {
     window.dataLayer.push({ event: `gatsby-route-change` })
-  }, 2000)
+  }, 50)
 }
