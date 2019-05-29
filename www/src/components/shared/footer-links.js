2692bb692a94c15e808bc2a6f2bdc3341f52d1a5
Florian Kissling <florian.kissling@gmail.com>
2019-05-29 10:28:09 +0200

    Reorder footer links

--- www/src/components/shared/footer-links.js
+++ www/src/components/shared/footer-links.js
@@ -48,10 +48,10 @@ const FooterLinks = props => (
       <Link to="/accessibility-statement">Accessibility Statement</Link>
     </li>
     <li>
-      <a href="https://www.gatsbyjs.com">Gatsbyjs.com</a>
+      <a href="/guidelines/logo/">Logo &amp; Assets</a>
     </li>
     <li>
-      <a href="/guidelines/logo/">Logo &amp; Assets</a>
+      <a href="https://www.gatsbyjs.com">Gatsbyjs.com</a>
     </li>
   </FooterList>
 )
