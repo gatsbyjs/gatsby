24749fde2e5b928a57c4640b948c8f3165924e6f
Florian Kissling <florian.kissling@gmail.com>
2019-06-02 15:43:25 +0200

    Lint

--- www/src/pages/guidelines/design-tokens.js
+++ www/src/pages/guidelines/design-tokens.js
@@ -467,6 +467,7 @@ const DesignTokens = ({ location }) => (
                       />
                     )
                   }
+                  return false
                 })}
               </tbody>
             </table>
