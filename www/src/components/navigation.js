3c47ad1677952d14d9ac23463de3c9ab1c3a18a4
Florian Kissling <florian.kissling@gmail.com>
2019-06-03 23:49:29 +0200

    Navigate to `/guidelines/logo/` when right-clicking Gatsby logo in header
    I smell a11y issues TBH. Paging @marcysutton!
    /cc @DSchau @KyleAMathews

--- www/src/components/navigation.js
+++ www/src/components/navigation.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, navigate } from "gatsby"
 import GithubIcon from "react-icons/lib/go/mark-github"
 import TwitterIcon from "react-icons/lib/fa/twitter"
 import SearchForm from "../components/search-form"
@@ -36,131 +36,147 @@ const NavItem = ({ linkTo, children }) => (
   </li>
 )
 
-const Navigation = ({ pathname }) => {
-  const isHomepage = pathname === `/`
-  const isBlog = pathname === `/blog/` || pathname.indexOf(`/blog/page/`) === 0
-
-  const socialIconsStyles = {
-    ...styles.navItem,
-    ...styles.socialIconItem,
+class Navigation extends React.Component {
+  handleClick(e) {
+    if (e.type === `contextmenu`) {
+      e.preventDefault()
+      navigate(`/guidelines/logo/`)
+    }
   }
 
-  const SocialNavItem = ({ href, title, children, overrideCSS }) => (
-    <a
-      href={href}
-      title={title}
-      css={{
-        ...socialIconsStyles,
-        fontSize: fontSizes[2],
-        ...overrideCSS,
-      }}
-    >
-      {children}
-    </a>
-  )
+  render() {
+    const { pathname } = this.props
+    const isHomepage = pathname === `/`
+    const isBlog =
+      pathname === `/blog/` || pathname.indexOf(`/blog/page/`) === 0
 
-  return (
-    <header
-      css={{
-        backgroundColor: `rgba(255,255,255,0.975)`,
-        position: `relative`,
-        height: sizes.headerHeight,
-        left: 0,
-        right: 0,
-        top: sizes.bannerHeight,
-        zIndex: zIndices.navigation,
-        // use this to test if the header items are properly aligned to the logo
-        // wordmark
-        // "&:before": {
-        //   content: `''`,
-        //   position: `absolute`,
-        //   bottom: `1.25rem`,
-        //   left: 0,
-        //   right: 0,
-        //   width: `100%`,
-        //   height: 1,
-        //   zIndex: 10,
-        //   background: `red`,
-        // },
-        [breakpointGutter]: {
-          backgroundColor: isBlog ? colors.ui.background : false,
-          position: isHomepage || isBlog ? `absolute` : `fixed`,
-        },
-        paddingLeft: `env(safe-area-inset-left)`,
-        paddingRight: `env(safe-area-inset-right)`,
-      }}
-    >
-      <div
+    const socialIconsStyles = {
+      ...styles.navItem,
+      ...styles.socialIconItem,
+    }
+
+    const SocialNavItem = ({ href, title, children, overrideCSS }) => (
+      <a
+        href={href}
+        title={title}
         css={{
-          ...styles.containerInner,
-          "&:after": {
-            content: `''`,
-            position: `absolute`,
-            bottom: 0,
-            left: 0,
-            right: 0,
-            height: 1,
-            zIndex: -1,
-            background: isHomepage ? `transparent` : colors.ui.border.subtle,
-          },
+          ...socialIconsStyles,
+          fontSize: fontSizes[2],
+          ...overrideCSS,
         }}
       >
-        <Link
-          to="/"
-          css={styles.logoLink}
-          aria-label="Gatsby, Back to homepage"
+        {children}
+      </a>
+    )
+
+    return (
+      <header
+        css={{
+          backgroundColor: `rgba(255,255,255,0.975)`,
+          position: `relative`,
+          height: sizes.headerHeight,
+          left: 0,
+          right: 0,
+          top: sizes.bannerHeight,
+          zIndex: zIndices.navigation,
+          // use this to test if the header items are properly aligned to the logo
+          // wordmark
+          // "&:before": {
+          //   content: `''`,
+          //   position: `absolute`,
+          //   bottom: `1.25rem`,
+          //   left: 0,
+          //   right: 0,
+          //   width: `100%`,
+          //   height: 1,
+          //   zIndex: 10,
+          //   background: `red`,
+          // },
+          [breakpointGutter]: {
+            backgroundColor: isBlog ? colors.ui.background : false,
+            position: isHomepage || isBlog ? `absolute` : `fixed`,
+          },
+          paddingLeft: `env(safe-area-inset-left)`,
+          paddingRight: `env(safe-area-inset-right)`,
+        }}
+      >
+        <div
+          css={{
+            ...styles.containerInner,
+            "&:after": {
+              content: `''`,
+              position: `absolute`,
+              bottom: 0,
+              left: 0,
+              right: 0,
+              height: 1,
+              zIndex: -1,
+              background: isHomepage ? `transparent` : colors.ui.border.subtle,
+            },
+          }}
         >
-          <img
-            src={logo}
-            css={styles.logo}
-            alt="Gatsby Logo"
-            aria-hidden="true"
-          />
-        </Link>
-        <nav
-          className="navigation"
-          aria-label="Primary Navigation"
-          css={styles.navContainer}
-        >
-          <ul css={styles.ulContainer}>
-            <NavItem linkTo="/docs/">Docs</NavItem>
-            <NavItem linkTo="/tutorial/">Tutorials</NavItem>
-            <NavItem linkTo="/plugins/">Plugins</NavItem>
-            <NavItem linkTo="/features/">Features</NavItem>
-            <NavItem linkTo="/blog/">Blog</NavItem>
-            <NavItem linkTo="/showcase/">Showcase</NavItem>
-            <NavItem linkTo="/contributing/">Contributing</NavItem>
-          </ul>
-        </nav>
-        <div css={styles.searchAndSocialContainer}>
-          <SearchForm
-            key="SearchForm"
-            iconColor={iconColor}
-            offsetVertical="-0.2175rem"
-          />
-          <SocialNavItem
-            href="https://github.com/gatsbyjs/gatsby"
-            title="GitHub"
+          <Link
+            to="/"
+            css={styles.logoLink}
+            aria-label="Gatsby, Back to homepage"
+            onClick={e => this.handleClick(e)}
+            onContextMenu={e => this.handleClick(e)}
           >
-            <GithubIcon style={{ verticalAlign: `text-top` }} />
-          </SocialNavItem>
-          <div
-            css={{
-              display: `none`,
-              [mediaQueries.lg]: { display: `flex` },
-            }}
+            <img
+              src={logo}
+              css={styles.logo}
+              alt="Gatsby Logo"
+              aria-hidden="true"
+            />
+          </Link>
+          <nav
+            className="navigation"
+            aria-label="Primary Navigation"
+            css={styles.navContainer}
           >
-            <SocialNavItem href="https://gatsby.dev/discord" title="Discord">
-              <DiscordIcon overrideCSS={{ verticalAlign: `text-top` }} />
+            <ul css={styles.ulContainer}>
+              <NavItem linkTo="/docs/">Docs</NavItem>
+              <NavItem linkTo="/tutorial/">Tutorials</NavItem>
+              <NavItem linkTo="/plugins/">Plugins</NavItem>
+              <NavItem linkTo="/features/">Features</NavItem>
+              <NavItem linkTo="/blog/">Blog</NavItem>
+              <NavItem linkTo="/showcase/">Showcase</NavItem>
+              <NavItem linkTo="/contributing/">Contributing</NavItem>
+            </ul>
+          </nav>
+          <div css={styles.searchAndSocialContainer}>
+            <SearchForm
+              key="SearchForm"
+              iconColor={iconColor}
+              offsetVertical="-0.2175rem"
+            />
+            <SocialNavItem
+              href="https://github.com/gatsbyjs/gatsby"
+              title="GitHub"
+            >
+              <GithubIcon style={{ verticalAlign: `text-top` }} />
             </SocialNavItem>
-            <SocialNavItem href="https://twitter.com/gatsbyjs" title="Twitter">
-              <TwitterIcon style={{ verticalAlign: `text-top` }} />
-            </SocialNavItem>
+            <div
+              css={{
+                display: `none`,
+                [mediaQueries.lg]: { display: `flex` },
+              }}
+            >
+              <SocialNavItem href="https://gatsby.dev/discord" title="Discord">
+                <DiscordIcon overrideCSS={{ verticalAlign: `text-top` }} />
+              </SocialNavItem>
+              <SocialNavItem
+                href="https://twitter.com/gatsbyjs"
+                title="Twitter"
+              >
+                <TwitterIcon style={{ verticalAlign: `text-top` }} />
+              </SocialNavItem>
+            </div>
           </div>
         </div>
-      </div>
-    </header>
-  )
+      </header>
+    )
+  }
 }
 
 const styles = {
