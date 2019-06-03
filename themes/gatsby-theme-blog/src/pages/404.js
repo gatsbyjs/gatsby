f534bce5b4d1b44d48d1b260460ff2fcfb933742
Amberley Romo <amberleyjohanna@gmail.com>
2019-06-03 16:59:43 -0500

    404 page consistency

--- themes/gatsby-theme-blog/src/pages/404.js
+++ themes/gatsby-theme-blog/src/pages/404.js
@@ -5,23 +5,25 @@ import { Styled } from "theme-ui"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-class NotFoundPage extends React.Component {
-  render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
-
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="404: Not Found" />
-        <Styled.h1>Not Found</Styled.h1>
-        <Styled.p>
-          The page you’re trying to visit has moved or doesn’t exist. Please
-          check the URL or use the site navigation to find the content you’re
-          looking for.
-        </Styled.p>
-      </Layout>
-    )
-  }
+const NotFoundPage = ({
+  location,
+  data: {
+    site: {
+      siteMetadata: { title: siteTitle },
+    },
+  },
+}) => {
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title="404: Not Found" />
+      <Styled.h1>Not Found</Styled.h1>
+      <Styled.p>
+        The page you’re trying to visit has moved or doesn’t exist. Please check
+        the URL or use the site navigation to find the content you’re looking
+        for.
+      </Styled.p>
+    </Layout>
+  )
 }
 
 export default NotFoundPage
