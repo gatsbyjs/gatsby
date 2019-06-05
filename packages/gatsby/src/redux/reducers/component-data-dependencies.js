3a6bf9142cc62b3115e3486b07f26a2a4dab95b9
Dustin Schau <dustinschau@gmail.com>
2019-06-05 11:00:00 -0500

    chore: remove testing change

--- packages/gatsby/src/redux/reducers/component-data-dependencies.js
+++ packages/gatsby/src/redux/reducers/component-data-dependencies.js
@@ -3,7 +3,7 @@ const _ = require(`lodash`)
 module.exports = (state = { nodes: {}, connections: {} }, action) => {
   switch (action.type) {
     case `DELETE_CACHE`:
-      return { nodes: state.nodes, connections: state.connections }
+      return { nodes: {}, connections: {} }
     case `CREATE_COMPONENT_DEPENDENCY`:
       if (action.payload.path === ``) {
         return state
