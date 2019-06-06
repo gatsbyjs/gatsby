353e854cb122e4a732a60b06abbd8acc75d7727f
Mike Allanson <michael.allanson@gmail.com>
2019-06-06 17:00:49 +0100

    List PRs that haven't been updated recently (#14556)
    * scoobie doobie dooooo
    
    * Add list of 'lonely' PRs, ones that haven't been updated in at least 30 days
    
    This also splits things up to make a little more testable, adds some basic tests, and replaces moment for date-fns.
    
    * Skip weekends for this job
    
    * Tidy up
    
    * mon-fri, not sun-thurs

---
+++ .github/actions/high-priority-prs/src/__tests__/pr-message.js
@@ -0,0 +1,17 @@
+const { process, maintainers } = require("../process")
+const report = require("../pr-message")
+const fs = require("fs")
+const path = require("path")
+const fixture = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "fixtures", "data.json"), `utf-8`)
+)
+
+test("Creates a correctly formatted Slack message", () => {
+  const now = new Date(`2019-06-05T13:49:58Z`)
+  const queues = process(fixture, now)
+  const slackMessage = report(queues, maintainers, now)
+  expect(slackMessage).toMatchSnapshot()
+
+  // See https://api.slack.com/tools/block-kit-builder
+  // console.log(JSON.stringify(slackMessage, null, 2))
+})
