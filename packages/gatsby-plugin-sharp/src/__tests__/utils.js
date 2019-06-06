b8d4a54fbc2665ce6b6616dfe62dc9f7b4e61a1a
Ward Peeters <ward@coding-tech.com>
2019-06-06 16:16:34 +0200

    fix tests

--- packages/gatsby-plugin-sharp/src/__tests__/utils.js
+++ packages/gatsby-plugin-sharp/src/__tests__/utils.js
@@ -5,24 +5,19 @@ const reporter = require(`gatsby-cli/lib/reporter`)
 const progress = require(`progress`)
 
 describe(`createProgress`, () => {
-  const progressBarHasCorrectApi = bar => {
-    expect(bar).toHaveProperty(`start`, expect.any(Function))
-    expect(bar).toHaveProperty(`tick`, expect.any(Function))
-    expect(bar).toHaveProperty(`done`, expect.any(Function))
-    expect(bar).toHaveProperty(`total`)
-  }
-
   it(`should use createProgress from gatsby-cli when available`, () => {
-    const bar = createProgress(`test`)
+    createProgress(`test`)
     expect(reporter.createProgress).toBeCalled()
     expect(progress).not.toBeCalled()
-    progressBarHasCorrectApi(bar)
   })
 
   it(`should fallback to a local implementation`, () => {
     reporter.createProgress = null
     const bar = createProgress(`test`)
     expect(progress).toHaveBeenCalledTimes(1)
-    progressBarHasCorrectApi(bar)
+    expect(bar).toHaveProperty(`start`, expect.any(Function))
+    expect(bar).toHaveProperty(`tick`, expect.any(Function))
+    expect(bar).toHaveProperty(`done`, expect.any(Function))
+    expect(bar).toHaveProperty(`total`)
   })
 })
--- packages/gatsby-source-filesystem/src/__tests__/utils.js
+++ packages/gatsby-source-filesystem/src/__tests__/utils.js
@@ -32,24 +32,19 @@ describe(`create remote file node`, () => {
 })
 
 describe(`createProgress`, () => {
-  const progressBarHasCorrectApi = bar => {
-    expect(bar).toHaveProperty(`start`, expect.any(Function))
-    expect(bar).toHaveProperty(`tick`, expect.any(Function))
-    expect(bar).toHaveProperty(`done`, expect.any(Function))
-    expect(bar).toHaveProperty(`total`)
-  }
-
   it(`should use createProgress from gatsby-cli when available`, () => {
-    const bar = createProgress(`test`)
+    createProgress(`test`)
     expect(reporter.createProgress).toBeCalled()
     expect(progress).not.toBeCalled()
-    progressBarHasCorrectApi(bar)
   })
 
   it(`should fallback to a local implementation`, () => {
     reporter.createProgress = null
     const bar = createProgress(`test`)
     expect(progress).toHaveBeenCalledTimes(1)
-    progressBarHasCorrectApi(bar)
+    expect(bar).toHaveProperty(`start`, expect.any(Function))
+    expect(bar).toHaveProperty(`tick`, expect.any(Function))
+    expect(bar).toHaveProperty(`done`, expect.any(Function))
+    expect(bar).toHaveProperty(`total`)
   })
 })
