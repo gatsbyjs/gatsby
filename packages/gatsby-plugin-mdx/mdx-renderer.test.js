b9511e6bc20454c500db2a204f80976a4888c1b4
ChristopherBiscardi <chris@christopherbiscardi.com>
2019-06-10 08:54:29 +0800

    remove commented out tests

--- packages/gatsby-plugin-mdx/mdx-renderer.test.js
+++ packages/gatsby-plugin-mdx/mdx-renderer.test.js
@@ -4,14 +4,6 @@ import TestRenderer from "react-test-renderer";
 import MDXRenderer from "./mdx-renderer";
 
 describe("mdx-renderer", () => {
-  test("renders nothing if there are no children", () => {
-    expect(TestRenderer.create(<MDXRenderer />).toJSON()).toEqual(null);
-  });
-
-  test.skip("renders content if function body is passed in", () => {
-    const result = TestRenderer.create(<MDXRenderer>{`return 2`}</MDXRenderer>);
-    expect(result.toJSON()).toEqual("return 2");
-  });
 
   test("renders React elements when scope is provided", () => {
     const result = TestRenderer.create(
