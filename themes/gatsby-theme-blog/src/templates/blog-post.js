96c3ddc683ca63e9c6283d80440658a1cba1488e
Amberley Romo <amberleyjohanna@gmail.com>
2019-06-11 14:57:16 -0500

    incorporate main content into html main tag. Rm "as" when fixed in theme-ui

--- themes/gatsby-theme-blog/src/templates/blog-post.js
+++ themes/gatsby-theme-blog/src/templates/blog-post.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
-import { Styled, css, Flex } from "theme-ui"
+import { Styled, css, Flex, Main, Footer } from "theme-ui"
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
@@ -16,43 +16,49 @@ class BlogPostTemplate extends React.Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title={post.frontmatter.title} description={post.excerpt} />
-        <Styled.h1>{post.frontmatter.title}</Styled.h1>
-        <Styled.p
-          css={css({
-            fontSize: 1,
-            mt: -3,
-            mb: 3,
-          })}
-        >
-          {post.frontmatter.date}
-        </Styled.p>
-        <MDXRenderer>{post.code.body}</MDXRenderer>
-        <Styled.hr />
-        <Bio />
-        <Flex
-          as="ul"
-          css={{
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </Flex>
+        <Main as="main">
+          <Styled.h1>{post.frontmatter.title}</Styled.h1>
+          <Styled.p
+            css={css({
+              fontSize: 1,
+              mt: -3,
+              mb: 3,
+            })}
+          >
+            {post.frontmatter.date}
+          </Styled.p>
+          <MDXRenderer>{post.code.body}</MDXRenderer>
+        </Main>
+        <Footer>
+          <Styled.hr />
+          <Bio />
+          {(previous || next) && (
+            <Flex
+              as="ul"
+              css={{
+                flexWrap: `wrap`,
+                justifyContent: `space-between`,
+                listStyle: `none`,
+                padding: 0,
+              }}
+            >
+              <li>
+                {previous && (
+                  <Link to={previous.fields.slug} rel="prev">
+                    ← {previous.frontmatter.title}
+                  </Link>
+                )}
+              </li>
+              <li>
+                {next && (
+                  <Link to={next.fields.slug} rel="next">
+                    {next.frontmatter.title} →
+                  </Link>
+                )}
+              </li>
+            </Flex>
+          )}
+        </Footer>
       </Layout>
     )
   }
