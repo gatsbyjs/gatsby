dc933ba492d659c50c9fd80a9bb8216c54320d53
FRIN Yvonnick <frin.yvonnick@gmail.com>
2019-06-11 13:29:40 +0200

    :sparkles: Export watch directory logic

--- packages/gatbsy-page-utils/src/index.js
+++ packages/gatbsy-page-utils/src/index.js
@@ -1,9 +1,11 @@
 import validatePath from "./validate-path"
-import createPath from "create-path"
-import ignorePath from "ignore-path"
+import createPath from "./create-path"
+import ignorePath from "./ignore-path"
+import watchDirectory from "./watch-directory"
 
 module.exports = {
   validatePath,
   createPath,
   ignorePath,
+  watchDirectory,
 }
