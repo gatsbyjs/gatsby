a3f6901ad3e0b2b64c5e9c902469075dff11010e
Florian Kissling <21834+fk@users.noreply.github.com>
2019-06-11 23:04:17 +0200

    chore(www): Fix global graphql warning on contributing/events (#14714)

--- www/src/pages/contributing/events.js
+++ www/src/pages/contributing/events.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Helmet } from "react-helmet"
+import { graphql } from "gatsby"
 
 import Layout from "../../components/layout"
 import { itemListContributing } from "../../utils/sidebar/item-list"
