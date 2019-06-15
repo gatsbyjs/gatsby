ccb8e6d8829367141aa93438c74c4d324bcc465d
Florian Kissling <florian.kissling@gmail.com>
2019-06-15 03:48:29 +0200

    Consolidate webfont imports

---
+++ www/src/fonts/futura.js
@@ -0,0 +1,5 @@
+import "./Webfonts/futurapt_book_macroman/stylesheet.css"
+import "./Webfonts/futurapt_bookitalic_macroman/stylesheet.css"
+import "./Webfonts/futurapt_demi_macroman/stylesheet.css"
+import "./Webfonts/futurapt_demiitalic_macroman/stylesheet.css"
+import "./Webfonts/futurapt_bold_macroman/stylesheet.css"
