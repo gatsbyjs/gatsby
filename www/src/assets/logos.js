8761454c2c0de92e7d17904930de81ed9a9ac74c
Florian Kissling <florian.kissling@gmail.com>
2019-06-17 02:49:36 +0200

    Rm `src/used-by` and related logo SVGs

--- www/src/assets/logos.js
+++ www/src/assets/logos.js
@@ -2,16 +2,6 @@ import JSIcon from "./js.svg"
 import GraphQLIcon from "./graphql.svg"
 import WebpackIcon from "./webpack.svg"
 import ReactJSIcon from "./react.svg"
-import FormidableIcon from "./formidable.svg"
-import FabricIcon from "./fabric.svg"
 import NetlifyIcon from "./netlify.svg"
 
-export {
-  JSIcon,
-  GraphQLIcon,
-  WebpackIcon,
-  ReactJSIcon,
-  FormidableIcon,
-  FabricIcon,
-  NetlifyIcon,
-}
+export { JSIcon, GraphQLIcon, WebpackIcon, ReactJSIcon, NetlifyIcon }
