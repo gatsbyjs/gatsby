f265315da899c43a0f25449e94534cc8f0981e01
Alexandre Ramos dos Santos <alexandresantos84@gmail.com>
2019-06-17 19:48:07 -0300

    fix: url comment useStaticQuery

--- starters/default/src/components/layout.js
+++ starters/default/src/components/layout.js
@@ -2,7 +2,7 @@
  * Layout component that queries for data
  * with Gatsby's useStaticQuery component
  *
- * See: https://www.gatsbyjs.org/docs/static-query/
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
 import React from "react"
