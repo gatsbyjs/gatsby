276d595305df294bbd6437626fdf5b22c5ce3e68
dabhitapan <37927312+dabhitapan@users.noreply.github.com>
2019-06-18 08:31:43 +0530

    Update page-2.js

--- starters/default/src/pages/page-2.js
+++ starters/default/src/pages/page-2.js
@@ -8,7 +8,7 @@ const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <p>Welcome to page 2.</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
