c345700516ede88d937159b5d81198e6da8bbffa
dabhitapan <37927312+dabhitapan@users.noreply.github.com>
2019-06-18 08:33:30 +0530

    Update 404.js

--- starters/default/src/pages/404.js
+++ starters/default/src/pages/404.js
@@ -5,7 +5,7 @@ import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
+    <SEO title="404: Page Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
