6c6dc2370cf44b5d9bf80b9f8d189ba8ae128329
Tim Brown <brown.tim.lee@gmail.com>
2019-06-21 04:39:50 -0400

    feat(gatsby-image): Add art direction (#13395)
    * Add optional media key to PropTypes and Typescript declarations
    
    * Add optional fluidImages and fixedImages props
    
    * Add art direction to fixed and fluid images
    
    * Add art direction to base64 and tracedSVG
    
    * Add art direction to noscript image
    
    * Add tests for fixedImages and fluidImages
    
    * Respond to code review
    
    * Use const in tests
    
    * Additinal code review refactor
    
    * Fix e2e tests
    
    * Add README docs
    
    * Fix typo and update wording in README
    
    * Name selectors in e2e test
    
    * Work around SVG bug by encoding spaces
    
    * Fix breaking Placeholder change, respond to code review, and update snapshots
    
    * Use @polarthene's Pastebin
    
    * Update sharp snapshot test
    
    * Reset integration tests
    
    * Move fluidImages & fixedImages into fluid & fixed
    
    * update tests with no media
    
    * cleanup spreadprops
    
    * Add warning if multiple sources with no media were used
    
    * review changes
    
    * fix tests

--- packages/gatsby-image/src/__tests__/index.js
+++ packages/gatsby-image/src/__tests__/index.js
@@ -23,6 +23,46 @@ const fluidShapeMock = {
   base64: `string_of_base64`,
 }
 
+const fixedImagesShapeMock = [
+  {
+    width: 100,
+    height: 100,
+    src: `test_image.jpg`,
+    srcSet: `some srcSet`,
+    srcSetWebp: `some srcSetWebp`,
+    base64: `string_of_base64`,
+  },
+  {
+    width: 100,
+    height: 100,
+    src: `test_image_2.jpg`,
+    srcSet: `some other srcSet`,
+    srcSetWebp: `some other srcSetWebp`,
+    base64: `other_string_of_base64`,
+    media: `only screen and (min-width: 768px)`,
+  },
+]
+
+const fluidImagesShapeMock = [
+  {
+    aspectRatio: 1.5,
+    src: `test_image.jpg`,
+    srcSet: `some srcSet`,
+    srcSetWebp: `some srcSetWebp`,
+    sizes: `(max-width: 600px) 100vw, 600px`,
+    base64: `string_of_base64`,
+  },
+  {
+    aspectRatio: 2,
+    src: `test_image_2.jpg`,
+    srcSet: `some other srcSet`,
+    srcSetWebp: `some other srcSetWebp`,
+    sizes: `(max-width: 600px) 100vw, 600px`,
+    base64: `string_of_base64`,
+    media: `only screen and (min-width: 768px)`,
+  },
+]
+
 const setup = (
   fluid = false,
   props = {},
@@ -51,6 +91,32 @@ const setup = (
   return container
 }
 
+const setupImages = (
+  fluidImages = false,
+  onLoad = () => {},
+  onError = () => {}
+) => {
+  const { container } = render(
+    <Image
+      backgroundColor
+      className={`fixedImage`}
+      style={{ display: `inline` }}
+      title={`Title for the image`}
+      alt={`Alt text for the image`}
+      crossOrigin={`anonymous`}
+      {...fluidImages && { fluid: fluidImagesShapeMock }}
+      {...!fluidImages && { fixed: fixedImagesShapeMock }}
+      onLoad={onLoad}
+      onError={onError}
+      itemProp={`item-prop-for-the-image`}
+      placeholderStyle={{ color: `red` }}
+      placeholderClassName={`placeholder`}
+    />
+  )
+
+  return container
+}
+
 describe(`<Image />`, () => {
   it(`should render fixed size images`, () => {
     const component = setup()
@@ -59,6 +125,16 @@ describe(`<Image />`, () => {
 
   it(`should render fluid images`, () => {
     const component = setup(true)
+    expect(component).toMatchSnapshot()
+  })
+
+  it(`should render multiple fixed image variants`, () => {
+    const component = setupImages()
+    expect(component).toMatchSnapshot()
+  })
+
+  it(`should render multiple fluid image variants`, () => {
+    const component = setupImages(true)
     expect(component).toMatchSnapshot()
   })
 
@@ -91,9 +167,39 @@ describe(`<Image />`, () => {
   })
 
   it(`should have the the "critical" prop set "loading='eager'"`, () => {
+    jest.spyOn(global.console, `log`)
+
     const props = { critical: true }
     const imageTag = setup(false, props).querySelector(`picture img`)
     expect(imageTag.getAttribute(`loading`)).toEqual(`eager`)
+    expect(console.log).toBeCalled()
+  })
+
+  it(`should warn if image variants provided are missing media keys.`, () => {
+    jest.spyOn(global.console, `warn`)
+
+    render(
+      <Image
+        backgroundColor
+        className={`fixedImage`}
+        style={{ display: `inline` }}
+        title={`Title for the image`}
+        alt={`Alt text for the image`}
+        crossOrigin={`anonymous`}
+        fluid={fluidImagesShapeMock.concat({
+          aspectRatio: 2,
+          src: `test_image_3.jpg`,
+          srcSet: `some other srcSet`,
+          srcSetWebp: `some other srcSetWebp`,
+          sizes: `(max-width: 600px) 100vw, 600px`,
+          base64: `string_of_base64`,
+        })}
+        itemProp={`item-prop-for-the-image`}
+        placeholderStyle={{ color: `red` }}
+        placeholderClassName={`placeholder`}
+      />
+    )
+    expect(console.warn).toBeCalled()
   })
 
   it(`should call onLoad and onError image events`, () => {
