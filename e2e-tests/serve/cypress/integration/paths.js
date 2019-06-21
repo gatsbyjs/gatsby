f0df5767234cc247184d10a2c2e5c34c3e507a18
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2019-06-21 15:19:06 +0530

    Add end to end tests

---
+++ e2e-tests/serve/cypress/integration/paths.js
@@ -0,0 +1,22 @@
+/* global cy */
+
+describe(`serve`, () => {
+  it(`works with a static path`, () => {
+    cy.visit(`/static`).waitForRouteChange()
+    cy.get(`h1`).contains(`The greatest Static page of all time`)
+  })
+
+  it(`works with client side only paths`, () => {
+    cy.visit(`/`).waitForRouteChange()
+    cy.get(`h1`).contains(`Index Page`)
+
+    cy.visit(`/12345`).waitForRouteChange()
+    cy.get(`h1`).contains(`Not Found in Index`)
+
+    cy.visit(`/app`).waitForRouteChange()
+    cy.get(`h1`).contains(`App Page`)
+
+    cy.visit(`/app/12345`).waitForRouteChange()
+    cy.get(`h1`).contains(`Not Found in App`)
+  })
+})
---
+++ e2e-tests/serve/src/pages/static.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+const Static = () => <h1>The greatest Static page of all time</h1>
+
+export default Static
