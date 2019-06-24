26dae3ea48c35d7c4401e81184393e20980f9aab
Amberley Romo <amberleyjohanna@gmail.com>
2019-06-24 15:25:58 -0500

    add separate notes theme starter

---
+++ themes/gatsby-starter-notes-theme/gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  __experimentalThemes: [
+    {
+      resolve: `gatsby-theme-notes`,
+      options: {
+        notesPath: `/`,
+      },
+    },
+  ],
+  siteMetadata: {
+    title: `Site title`,
+  },
+}
