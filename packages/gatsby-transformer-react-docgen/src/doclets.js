797136d46d88d718e6f87c7d3001e6301a310497
Sven Liebig <liebigsv@gmail.com>
2019-06-28 18:46:28 +0200

    add priority to fille undefined "type"

--- packages/gatsby-transformer-react-docgen/src/doclets.js
+++ packages/gatsby-transformer-react-docgen/src/doclets.js
@@ -64,7 +64,7 @@ export const applyPropDoclets = prop => {
       value = cleanDocletValue(value)
 
       if (prop.type === undefined) {
-        prop.type = prop.flowType || {}
+        prop.type = { ...prop.tsType } || { ...prop.flowType } || {}
       }
 
       prop.type.name = value
