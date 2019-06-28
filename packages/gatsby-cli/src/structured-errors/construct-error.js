dc2a8acc859d21bc9281ea7c244304dc56193066
Dustin Schau <DSchau@users.noreply.github.com>
2019-06-28 13:45:47 -0500

    fix(gatsby-cli): depend upon @hapi/joi (#15221)

--- packages/gatsby-cli/src/structured-errors/construct-error.js
+++ packages/gatsby-cli/src/structured-errors/construct-error.js
@@ -1,4 +1,4 @@
-const Joi = require(`joi`)
+const Joi = require(`@hapi/joi`)
 const stackTrace = require(`stack-trace`)
 const errorSchema = require(`./error-schema`)
 const { errorMap, defaultError } = require(`./error-map`)
--- packages/gatsby-cli/src/structured-errors/error-schema.js
+++ packages/gatsby-cli/src/structured-errors/error-schema.js
@@ -1,4 +1,4 @@
-const Joi = require(`joi`)
+const Joi = require(`@hapi/joi`)
 
 const Position = Joi.object().keys({
   line: Joi.number(),
