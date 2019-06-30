9b6297470cd2ada0117b355e14703b563a9bda5e
kiwimonk <medicalmasterpiece@gmail.com>
2019-07-01 00:36:11 +0300

    data-explorer.js: Fix Inject Schema

--- packages/gatsby/src/commands/data-explorer.js
+++ packages/gatsby/src/commands/data-explorer.js
@@ -1,28 +1,59 @@
-/* @flow */
+"use strict"
 
 const express = require(`express`)
+
 const graphqlHTTP = require(`express-graphql`)
+const graphqlPlayground = require(`graphql-playground-middleware-express`)
+  .default
+const graphiqlExplorer = require(`gatsby-graphiql-explorer`)
+const { formatError } = require(`graphql`)
+
 const { store } = require(`../redux`)
 const bootstrap = require(`../bootstrap`)
 
-module.exports = async (program: any) => {
+const withResolverContext = require(`../schema/context`)
+
+module.exports = async program => {
   let { port, host } = program
-  port = typeof port === `string` ? parseInt(port, 10) : port
+  port = typeof port === `string` ? parseInt(port, 10) : port // bootstrap to ensure schema is in the store
 
-  // bootstrap to ensure schema is in the store
   await bootstrap(program)
-
-  const schema = store.getState().schema
-
   const app = express()
+
+  const graphqlEndpoint = `/_+graphi?ql`
+
+  if (process.env.GATSBY_GRAPHQL_IDE === `playground`) {
+    app.get(
+      graphqlEndpoint,
+      graphqlPlayground({
+        endpoint: `/___graphql`,
+      }),
+      () => {}
+    )
+  } else {
+    graphiqlExplorer(app, {
+      graphqlEndpoint,
+    })
+  }
+
   app.use(
-    `/`,
-    graphqlHTTP({
-      schema,
-      graphiql: true,
+    graphqlEndpoint,
+    graphqlHTTP(() => {
+      const schema = store.getState().schema
+      return {
+        schema,
+        graphiql: true,
+        context: withResolverContext({}, schema),
+        formatError(err) {
+          return {
+            ...formatError(err),
+            stack: err.stack ? err.stack.split(`\n`) : [],
+          }
+        },
+      }
     })
   )
-
   console.log(`Gatsby data explorer running at`, `http://${host}:${port}`)
   app.listen(port, host)
 }
+//# sourceMappingURL=data-explorer.js.map
