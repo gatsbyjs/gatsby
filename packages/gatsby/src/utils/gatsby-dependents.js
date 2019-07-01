de10db108a50f342e24af91f176541306d959dd3
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2019-07-01 15:55:52 +0530

    Accept program directory as a param

--- packages/gatsby/src/utils/gatsby-dependents.js
+++ packages/gatsby/src/utils/gatsby-dependents.js
@@ -2,11 +2,9 @@ const util = require(`util`)
 const readPackageTree = require(`read-package-tree`)
 const readPackageTreeAsync = util.promisify(readPackageTree)
 
-module.exports = async () => {
-  const allNodeModules = await readPackageTreeAsync(
-    program.directory,
-    () => true
-  )
+// Pass in baseDirectory from store.program.directory
+module.exports = async baseDirectory => {
+  const allNodeModules = await readPackageTreeAsync(baseDirectory, () => true)
   return allNodeModules.children.filter(
     node =>
       (node.package.dependencies && node.package.dependencies[`gatsby`]) ||
