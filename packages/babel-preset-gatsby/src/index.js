a0bccdeb7ae7b8b6e2f911cb619dadf34bdceec2
Ward Peeters <ward@coding-tech.com>
2019-07-01 16:21:47 +0200

    fix tests

--- packages/babel-preset-gatsby/src/index.js
+++ packages/babel-preset-gatsby/src/index.js
@@ -79,7 +79,7 @@ module.exports = function preset(_, options = {}) {
         resolve(`@babel/plugin-transform-runtime`),
         {
           corejs: false,
-          helpers: stage === `develop`,
+          helpers: stage === `develop` || stage === `test`,
           regenerator: true,
           useESModules: stage !== `test`,
           absoluteRuntimePath,
