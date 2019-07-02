e6735f3a15aca2f50549dfe945bf02f121bc43bf
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2019-07-02 23:47:42 +0530

    fix(gatsby): Exclude gatsby itself in gatsby-dependents (#15318)

--- packages/gatsby/src/utils/gatsby-dependents.js
+++ packages/gatsby/src/utils/gatsby-dependents.js
@@ -6,8 +6,10 @@ const rptAsync = promisify(rpt)
 // Returns [Object] with name and path
 module.exports = async () => {
   const { program } = store.getState()
-  const allNodeModules = await rptAsync(program.directory, (node, moduleName) =>
-    /gatsby/.test(moduleName)
+  const allNodeModules = await rptAsync(
+    program.directory,
+    // Include anything that has `gatsby` in its name but not `gatsby` itself
+    (node, moduleName) => /gatsby/.test(moduleName) && moduleName !== `gatsby`
   )
   return allNodeModules.children
 }
