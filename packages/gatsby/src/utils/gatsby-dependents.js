08ca120d21249b36ba10ae46e3a9c830a19abd2d
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2019-07-02 20:43:22 +0530

    feat(gatsby): Switch to name based package filtering for speed (#15308)
    * Switch to name based package filtering for speed
    
    * Naming is hard
    
    * use util.promisify because weird bugs

--- packages/gatsby/src/utils/gatsby-dependents.js
+++ packages/gatsby/src/utils/gatsby-dependents.js
@@ -1,13 +1,13 @@
 import { store } from "../redux"
 import rpt from "read-package-tree"
+import { promisify } from "util"
 
+const rptAsync = promisify(rpt)
 // Returns [Object] with name and path
 module.exports = async () => {
   const { program } = store.getState()
-  const allNodeModules = await rpt(program.directory)
-  return allNodeModules.children.filter(
-    node =>
-      (node.package.dependencies && node.package.dependencies[`gatsby`]) ||
-      (node.package.peerDependencies && node.package.peerDependencies[`gatsby`])
+  const allNodeModules = await rptAsync(program.directory, (node, moduleName) =>
+    /gatsby/.test(moduleName)
   )
+  return allNodeModules.children
 }
