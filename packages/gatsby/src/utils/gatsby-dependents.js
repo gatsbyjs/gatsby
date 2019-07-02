847d16e34c62bc1314ba4cc737aef227ecdd0c53
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2019-07-02 17:56:48 +0530

    Naming is hard

--- packages/gatsby/src/utils/gatsby-dependents.js
+++ packages/gatsby/src/utils/gatsby-dependents.js
@@ -4,8 +4,8 @@ import rpt from "read-package-tree"
 // Returns [Object] with name and path
 module.exports = async () => {
   const { program } = store.getState()
-  const allNodeModules = await rpt(program.directory, (node, kidName) =>
-    /gatsby/.test(kidName)
+  const allNodeModules = await rpt(program.directory, (node, moduleName) =>
+    /gatsby/.test(moduleName)
   )
   return allNodeModules.children
 }
