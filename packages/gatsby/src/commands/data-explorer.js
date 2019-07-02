eca05fc998d1f157312b5d4571bb33a926f621b6
kiwimonk <medicalmasterpiece@gmail.com>
2019-07-02 12:27:10 +0300

    chore(gatsby): Added a data-explorer comments

--- packages/gatsby/src/commands/data-explorer.js
+++ packages/gatsby/src/commands/data-explorer.js
@@ -1,13 +1,11 @@
 "use strict"
 
 const express = require(`express`)
-
 const graphqlHTTP = require(`express-graphql`)
 const graphqlPlayground = require(`graphql-playground-middleware-express`)
   .default
 const graphiqlExplorer = require(`gatsby-graphiql-explorer`)
 const { formatError } = require(`graphql`)
-
 const { store } = require(`../redux`)
 const bootstrap = require(`../bootstrap`)
 const cors = require(`cors`)
@@ -19,9 +17,17 @@ module.exports = async program => {
   port = typeof port === `string` ? parseInt(port, 10) : port // bootstrap to ensure schema is in the store
 
   await bootstrap(program)
+
+  /**
+   * Set up the express app.
+   **/
   const app = express()
 
   app.use(cors())
+
+  /**
+   * Pattern matching all endpoints with graphql or graphiql with 1 or more leading underscores
+   */
   const graphqlEndpoint = `/_+graphi?ql`
 
   if (process.env.GATSBY_GRAPHQL_IDE === `playground`) {
