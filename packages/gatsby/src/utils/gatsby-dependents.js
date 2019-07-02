3800f3fa5eb352e66e43439eab35a4e666bf7e08
Michal Piechowiak <misiek.piechowiak@gmail.com>
2019-07-02 15:44:33 +0200

    use util.promisify because weird bugs

--- packages/gatsby/src/utils/gatsby-dependents.js
+++ packages/gatsby/src/utils/gatsby-dependents.js
@@ -1,10 +1,12 @@
 import { store } from "../redux"
 import rpt from "read-package-tree"
+import { promisify } from "util"
 
+const rptAsync = promisify(rpt)
 // Returns [Object] with name and path
 module.exports = async () => {
   const { program } = store.getState()
-  const allNodeModules = await rpt(program.directory, (node, moduleName) =>
+  const allNodeModules = await rptAsync(program.directory, (node, moduleName) =>
     /gatsby/.test(moduleName)
   )
   return allNodeModules.children
