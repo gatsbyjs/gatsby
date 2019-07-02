555503eb4cbaa0ccc79bf34bf82cf2a06dbd47d4
Amberley Romo <amberleyjohanna@gmail.com>
2019-07-02 08:44:51 -0500

    change how theme colors are overridden

--- themes/gatsby-starter-blog-theme/src/gatsby-theme-blog/gatsby-theme-ui/colors.js
+++ themes/gatsby-starter-blog-theme/src/gatsby-theme-blog/gatsby-theme-ui/colors.js
@@ -1,45 +1,25 @@
-const blue60 = `#007acc`;
-const blue30 = `#66E0FF`;
-const blueGray = `#282c35`;
-const black80 = `#1B1F23`;
-const white = `#fff`;
-const lightWhite = `rgba(255, 255, 255, 0.86)`;
-const opaqueLightYellow = `rgba(255, 229, 100, 0.2)`;
-const opaqueLightWhite = `hsla(0, 0%, 100%, 0.2)`;
-const lightGray = `hsla(0, 0%, 0%, 0.2)`;
+import merge from "lodash.merge"
+import defaultThemeColors from "gatsby-theme-blog/src/gatsby-theme-ui/colors"
 
-export default {
-  text: blueGray,
-  background: white,
-  primary: blue60,
-  secondary: black80,
-  muted: lightGray,
-  highlight: opaqueLightYellow,
-  heading: blueGray,
-  prism: {
-    background: `#011627`,
-    comment: `#809393`,
-    string: `#addb67`,
-    var: `#d6deeb`,
-    number: `#f78c6c`,
-    constant: `#82aaff`,
-    punctuation: `#c792ea`,
-    className: `#ffc98b`,
-    tag: `#ffa7c4`,
-    boolean: `#ff5874`,
-    property: `#80cbc4`,
-    namespace: `#b2ccd6`,
-    highlight: `hsla(207, 95%, 15%, 1)`
-  },
-  modes: {
-    dark: {
-      text: lightWhite,
-      background: blueGray,
-      primary: blue30,
-      secondary: lightWhite,
-      muted: opaqueLightWhite,
-      highlight: blue60,
-      heading: white
-    }
-  }
-};
+/*
+ * Want to change your theme colors?
+ * Try uncommenting the color overrides below
+ * to go from default purple to a blue theme
+ */
+
+// const blue60 = `#007acc`
+// const blue30 = `#66E0FF`
+// const blueGray = `#282c35`
+
+export default merge({}, defaultThemeColors, {
+  // text: blueGray,
+  // primary: blue60,
+  // heading: blueGray,
+  // modes: {
+  //   dark: {
+  //     background: blueGray,
+  //     primary: blue30,
+  //     highlight: blue60,
+  //   },
+  // },
+})
