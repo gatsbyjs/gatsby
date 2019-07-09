0ffbe59ab583bb17c232e31636f5c4b4c725115e
Michal Piechowiak <misiek.piechowiak@gmail.com>
2019-07-09 15:36:42 +0200

    fix(gatsby): restore client-only-paths behaviour (#15457)
    * fix(gatsby): restore client-only-paths behaviour
    
    * adjust tests, add tests for nested routers in user code
    
    * clean up another client-only-path page, this is no longer used
    
    * fix path in test (previous one was removed in cleanup)
    
    * Update packages/gatsby/src/utils/webpack.config.js
    
    Co-Authored-By: Ward Peeters <ward@coding-tech.com>

--- packages/gatsby/cache-dir/production-app.js
+++ packages/gatsby/cache-dir/production-app.js
@@ -1,7 +1,7 @@
 import { apiRunner, apiRunnerAsync } from "./api-runner-browser"
 import React from "react"
 import ReactDOM from "react-dom"
-import { Router, navigate, Location } from "@reach/router"
+import { Router, navigate, Location, BaseContext } from "@reach/router"
 import { ScrollContext } from "gatsby-react-router-scroll"
 import domReady from "@mikaelkristiansson/domready"
 import {
@@ -37,6 +37,25 @@ apiRunnerAsync(`onClientEntry`).then(() => {
     require(`./register-service-worker`)
   }
 
+  // In gatsby v2 if Router is used in page using matchPaths
+  // paths need to contain full path.
+  // For example:
+  //   - page have `/app/*` matchPath
+  //   - inside template user needs to use `/app/xyz` as path
+  // Resetting `basepath`/`baseuri` keeps current behaviour
+  // to not introduce breaking change.
+  // Remove this in v3
+  const RouteHandler = props => (
+    <BaseContext.Provider
+      value={{
+        baseuri: `/`,
+        basepath: `/`,
+      }}
+    >
+      <PageRenderer {...props} />
+    </BaseContext.Provider>
+  )
+
   class LocationHandler extends React.Component {
     render() {
       let { location } = this.props
@@ -53,7 +72,7 @@ apiRunnerAsync(`onClientEntry`).then(() => {
                   location={location}
                   id="gatsby-focus-wrapper"
                 >
-                  <PageRenderer
+                  <RouteHandler
                     path={
                       pageResources.page.path === `/404.html`
                         ? location.pathname
