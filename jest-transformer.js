4c275e47c9cbb60fa93b76c2ec9f84acfb78e726
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2019-07-12 18:02:30 +0530

    Add babel-plugin-dynamic-import-node to babel-preset-gatsby-package

--- jest-transformer.js
+++ jest-transformer.js
@@ -1,5 +1,2 @@
 const babelPreset = require(`babel-preset-gatsby-package`)()
-module.exports = require(`babel-jest`).createTransformer({
-  ...babelPreset,
-  plugins: [...babelPreset.plugins, `babel-plugin-dynamic-import-node`],
-})
+module.exports = require(`babel-jest`).createTransformer(babelPreset)
