a46d0d1d20a98f67313687c452ec8b871f973d85
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2019-07-12 18:43:57 +0530

    remove babel stuff that was added in #15657

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,24 @@
-const path = require(`path`)
-const glob = require(`glob`)
-const fs = require(`fs`)
+const path = require(`path`);
+const glob = require(`glob`);
+const fs = require(`fs`);
 
-const pkgs = glob.sync(`./packages/*`).map(p => p.replace(/^\./, `<rootDir>`))
+const pkgs = glob.sync(`./packages/*`).map(p => p.replace(/^\./, `<rootDir>`));
 
-const reGatsby = /gatsby$/
-const gatsbyDir = pkgs.find(p => reGatsby.exec(p))
-const gatsbyBuildDirs = [`dist`].map(dir => path.join(gatsbyDir, dir))
+const reGatsby = /gatsby$/;
+const gatsbyDir = pkgs.find(p => reGatsby.exec(p));
+const gatsbyBuildDirs = [`dist`].map(dir => path.join(gatsbyDir, dir));
 const builtTestsDirs = pkgs
   .filter(p => fs.existsSync(path.join(p, `src`)))
-  .map(p => path.join(p, `__tests__`))
-const distDirs = pkgs.map(p => path.join(p, `dist`))
+  .map(p => path.join(p, `__tests__`));
+const distDirs = pkgs.map(p => path.join(p, `dist`));
 const ignoreDirs = [`<rootDir>/packages/gatsby-dev-cli/verdaccio`].concat(
   gatsbyBuildDirs,
   builtTestsDirs,
   distDirs
-)
+);
 
-const coverageDirs = pkgs.map(p => path.join(p, `src/**/*.js`))
-const useCoverage = !!process.env.GENERATE_JEST_REPORT
+const coverageDirs = pkgs.map(p => path.join(p, `src/**/*.js`));
+const useCoverage = !!process.env.GENERATE_JEST_REPORT;
 
 module.exports = {
   notify: true,
@@ -31,14 +31,14 @@ module.exports = {
     `<rootDir>/www/`,
     `<rootDir>/dist/`,
     `<rootDir>/node_modules/`,
-    `__tests__/fixtures`,
+    `__tests__/fixtures`
   ],
   transform: {
     "^.+\\.js$": `<rootDir>/jest-transformer.js`,
-    "^.+\\.tsx?$": `<rootDir>/node_modules/ts-jest/preprocessor.js`,
+    "^.+\\.tsx?$": `<rootDir>/node_modules/ts-jest/preprocessor.js`
   },
   moduleNameMapper: {
-    "^highlight.js$": `<rootDir>/node_modules/highlight.js/lib/index.js`,
+    "^highlight.js$": `<rootDir>/node_modules/highlight.js/lib/index.js`
   },
   snapshotSerializers: [`jest-serializer-path`],
   collectCoverage: useCoverage,
@@ -48,11 +48,11 @@ module.exports = {
       lines: 45,
       statements: 44,
       functions: 42,
-      branches: 43,
-    },
+      branches: 43
+    }
   },
   collectCoverageFrom: coverageDirs,
   reporters: [`default`].concat(useCoverage ? `jest-junit` : []),
   testEnvironment: `jest-environment-jsdom-fourteen`,
-  moduleFileExtensions: [`js`, `jsx`, `ts`, `tsx`, `json`],
-}
+  moduleFileExtensions: [`js`, `jsx`, `ts`, `tsx`, `json`]
+};
