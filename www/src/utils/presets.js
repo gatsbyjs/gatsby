7d91354820bbf2d5c86b9adcfe91f015a083ff07
Florian Kissling <florian.kissling@gmail.com>
2019-07-15 17:58:38 +0200

    Add packages/gatsby-design-tokens

--- www/src/utils/presets.js
+++ www/src/utils/presets.js
@@ -1,43 +1,16 @@
-import {
-  breakpoints,
-  mediaQueries,
-  colors,
-  sizes,
-  letterSpacings,
-  lineHeights,
-  radii,
-  shadows,
-  space as spaceTokens,
-  fontSizes as fontSizeTokens,
-  transition,
-  fonts as fontTokens,
-  zIndices,
-  fontWeights,
-} from "./tokens"
-
-let fonts = {}
-for (let fontFamily in fontTokens) {
-  fonts[fontFamily] = fontTokens[fontFamily].join(`, `)
-}
-
-const borders = [0, `1px solid`, `2px solid`]
-const fontSizes = fontSizeTokens.map(token => `${token / 16}rem`)
-const space = spaceTokens.map(token => `${token / 16}rem`)
-
 export {
   breakpoints,
   mediaQueries,
   colors,
   sizes,
-  transition,
-  radii,
-  shadows,
   letterSpacings,
   lineHeights,
+  radii,
+  shadows,
   space,
   fontSizes,
+  transition,
   fonts,
   zIndices,
   fontWeights,
-  borders,
-}
+} from "gatsby-design-tokens"
