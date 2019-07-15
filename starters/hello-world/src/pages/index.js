d566e0c9ab1d58d76a5be5c8a458c361ba9eafa3
Jay Nguyen <iam.nguyen@pm.me>
2019-07-15 12:00:26 +0700

    string changed to backstick quotes

--- starters/hello-world/src/pages/index.js
+++ starters/hello-world/src/pages/index.js
@@ -3,7 +3,7 @@ import React from "react"
 export default () => (
   <div>
     <p> Hello </p>
-    <footer style={{ position: "fixed", bottom: 0 }}>
+    <footer style={{ position: `fixed`, bottom: 0 }}>
       © {new Date().getFullYear()}, Built with {` `}
       <a href="https://www.gatsbyjs.org">Gatsby</a>
     </footer>
