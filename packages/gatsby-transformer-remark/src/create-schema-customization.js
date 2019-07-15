6959c6dfbe345a87c70197ba17fb8b4c7ee20952
stefanprobst <stefanprobst@kolabnow.com>
2019-07-15 12:27:37 +0200

    feat(gatsby-transformer-remark): Allow remark subplugins to modify graphql types owned by parent plugin #15688

---
+++ packages/gatsby-transformer-remark/src/create-schema-customization.js
@@ -0,0 +1,48 @@
+const typeDefs = `
+  type MarkdownHeading {
+    value: String
+    depth: Int
+  }
+
+  enum MarkdownHeadingLevels {
+    h1
+    h2
+    h3
+    h4
+    h5
+    h6
+  }
+
+  enum MarkdownExcerptFormats {
+    PLAIN
+    HTML
+    MARKDOWN
+  }
+
+  type MarkdownWordCount {
+    paragraphs: Int
+    sentences: Int
+    words: Int
+  }
+`
+
+module.exports = (nodeApiArgs, pluginOptions = {}) => {
+  const { plugins = [] } = pluginOptions
+
+  nodeApiArgs.actions.createTypes(typeDefs)
+
+  // This allows subplugins to use Node APIs bound to `gatsby-transformer-remark`
+  // to customize the GraphQL schema. This makes it possible for subplugins to
+  // modify types owned by `gatsby-transformer-remark`.
+  plugins.forEach(plugin => {
+    const resolvedPlugin = require(plugin.resolve)
+    if (typeof resolvedPlugin.createSchemaCustomization === `function`) {
+      resolvedPlugin.createSchemaCustomization(
+        nodeApiArgs,
+        plugin.pluginOptions
+      )
+    }
+  })
+}
+
+module.exports.typeDefs = typeDefs
