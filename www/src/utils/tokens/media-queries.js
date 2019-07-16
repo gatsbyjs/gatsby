e57ee752f8adbf0efc855a96195b31bacdec3926
Florian Kissling <florian.kissling@gmail.com>
2019-07-16 18:27:38 +0200

    Use gatsby-design-tokens, rm `utils/tokens`, rewire
    Order imports in `utils/presets` alphabetically.

--- www/src/utils/tokens/media-queries.js
+++
@@ -1,8 +0,0 @@
-import breakpoints from "./breakpoints"
-
-let mediaQueries = {}
-for (let breakpoint in breakpoints) {
-  mediaQueries[breakpoint] = `@media (min-width: ${breakpoints[breakpoint]})`
-}
-
-export default mediaQueries
