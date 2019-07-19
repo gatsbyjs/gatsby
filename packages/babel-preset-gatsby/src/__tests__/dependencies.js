cafee4945f5b91f04917ef9ebfbcd7260dc1cf24
Ward Peeters <ward@coding-tech.com>
2019-07-19 08:10:52 +0200

    fix test name

--- packages/babel-preset-gatsby/src/__tests__/dependencies.js
+++ packages/babel-preset-gatsby/src/__tests__/dependencies.js
@@ -1,7 +1,7 @@
 const preset = require(`../dependencies`)
 
 describe(`dependencies`, () => {
-  it(`should specify proper presets and plugins when stage is %s`, () => {
+  it(`should specify proper presets and plugins`, () => {
     expect(preset()).toMatchSnapshot()
   })
 })
