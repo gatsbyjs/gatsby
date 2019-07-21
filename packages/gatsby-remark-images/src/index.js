gatsbyjs/gatsby

commit 369b154e92f23e2fdc4b728b431be54079d21a72

gatsby-remark-images: allow user-specified max-width to overwrite presentationWidth
This is useful when you e.g. want to use non-pixel units for max-width
Fix #15578

packages/gatsby-remark-images/src/index.js
@@ -268,12 +268,16 @@ module.exports = (
     `.trim()
     }
 
+    const shouldApplyMaxWidth = !imageCaption && wrapperStyle.indexOf(`max-width:`) !== -1;
+
     rawHTML = `
     <span
       class="${imageWrapperClass}"
       style="position: relative; display: block; margin-left: auto; margin-right: auto; ${
         imageCaption ? `` : wrapperStyle
-      } max-width: ${presentationWidth}px;"
+      } ${
+        shouldApplyMaxWidth ? `max-width: ${presentationWidth}px;` : ``
+      }"
     >
       ${rawHTML}
     </span>

commit 0d12af443f9fba1ed1167391ac2fe1d2957dc97a

Fix linting errors introduced by 369b154

packages/gatsby-remark-images/src/index.js
@@ -268,16 +268,15 @@ module.exports = (
     `.trim()
     }
 
-    const shouldApplyMaxWidth = !imageCaption && wrapperStyle.indexOf(`max-width:`) !== -1;
+    const shouldApplyMaxWidth =
+      !imageCaption && wrapperStyle.indexOf(`max-width:`) !== -1
 
     rawHTML = `
     <span
       class="${imageWrapperClass}"
       style="position: relative; display: block; margin-left: auto; margin-right: auto; ${
         imageCaption ? `` : wrapperStyle
-      } ${
-        shouldApplyMaxWidth ? `max-width: ${presentationWidth}px;` : ``
-      }"
+      } ${shouldApplyMaxWidth ? `max-width: ${presentationWidth}px;` : ``}"
     >
       ${rawHTML}
     </span>