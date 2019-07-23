bc668bd40883f8f79585ff812fe0592a9d3c0136
Samuel Raub <samuel.raub@gmail.com>
2019-07-23 11:33:38 +0200

    fix(gatsby): add jsdoc for matchPath on page Object (#15749)
    * Update public.js
    
    matchPatch property should be documented here in case someone wants to generate client side routes in createPages hook.
    
    * Update public.js
    
    * fix markdown in jsdoc

--- packages/gatsby/src/redux/actions/public.js
+++ packages/gatsby/src/redux/actions/public.js
@@ -91,6 +91,8 @@ const fileOkCache = {}
  * for detailed documentation about creating pages.
  * @param {Object} page a page object
  * @param {string} page.path Any valid URL. Must start with a forward slash
+ * @param {string} page.matchPath Path that Reach Router uses to match the page on the client side.
+ * Also see docs on [matchPath](/docs/gatsby-internals-terminology/#matchpath)
  * @param {string} page.component The absolute path to the component for this page
  * @param {Object} page.context Context data for this page. Passed as props
  * to the component `this.props.pageContext` as well as to the graphql query
