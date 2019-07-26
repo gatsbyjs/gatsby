b8cfe2f83c03305f522503eb92bdbdd0b33aa960
Roman Gusev <rgusev@hwdtech.ru>
2019-07-26 10:15:44 +0600

    chore(starters): Use typographically correct punctuation in default starter 404

--- starters/default/src/pages/404.js
+++ starters/default/src/pages/404.js
@@ -7,7 +7,7 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>You just hit a route that doesn&rsquo;t exist&hellip; the sadness.</p>
   </Layout>
 )
 
