148192138a7afd378a86582b013a1e13e1c3d22c
Roman Gusev <rgusev@hwdtech.ru>
2019-07-26 10:14:20 +0600

    chore(starters): Use typographically correct punctuation in blog starter 404

--- starters/blog/src/pages/404.js
+++ starters/blog/src/pages/404.js
@@ -13,7 +13,9 @@ class NotFoundPage extends React.Component {
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="404: Not Found" />
         <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <p>
+          You just hit a route that doesn&rsquo;t exist&hellip; the sadness.
+        </p>
       </Layout>
     )
   }
