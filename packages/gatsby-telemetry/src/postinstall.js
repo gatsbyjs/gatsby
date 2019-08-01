7724cd639bb341f6f4ef75d530002d80543bd463
Alex Luong <aluong@crimson.ua.edu>
2019-08-01 10:44:01 -0400

    fix(gatsby-telemetry): Don't display analytics notification if opted out (#16258)
    * fix(gatsby-telemetry): remove display analytics notification if opted out
    
    * lint: backtick
    
    * Move checks to EventStorage
    
    * fix(telemetry): don't show telemetry notification when telemetry
    collection is disabled via env flag.
    
    The env check was also implemented deeper just before sending events and
    hence the only glitch was that we showed the notification even though we
    didn't collect the data

--- packages/gatsby-telemetry/src/postinstall.js
+++ packages/gatsby-telemetry/src/postinstall.js
@@ -1,12 +1,12 @@
 const showAnalyticsNotification = require(`./showAnalyticsNotification`)
+const EventStorage = require(`./event-storage.js`)
 
-let enabled = false
 try {
   const ci = require(`ci-info`)
-  const Configstore = require(`configstore`)
-  const config = new Configstore(`gatsby`, {}, { globalConfigPath: true })
-  enabled = config.get(`telemetry.enabled`)
-  if (enabled === undefined && !ci.isCI) {
+  const eventStorage = new EventStorage()
+  const disabled = eventStorage.disabled
+  const enabledInConfig = eventStorage.getConfig(`telemetry.enabled`)
+  if (enabledInConfig === undefined && !disabled && !ci.isCI) {
     showAnalyticsNotification()
   }
 } catch (e) {
