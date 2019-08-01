9d32f3cf45a8155018c4fae0d5ad80f0e195a348
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2019-08-02 00:03:26 +0530

    fix(gatsby-telemetry): Add back fix and fix files in package.j… (#16297)

--- packages/gatsby-telemetry/src/postinstall.js
+++ packages/gatsby-telemetry/src/postinstall.js
@@ -1,12 +1,12 @@
-const showAnalyticsNotification = require(`./showAnalyticsNotification`)
-
 try {
-  const ci = require(`ci-info`)
-  const Configstore = require(`configstore`)
-  const config = new Configstore(`gatsby`, {}, { globalConfigPath: true })
-  const enabledInConfig = config.get(`telemetry.enabled`)
+  const showAnalyticsNotification = require(`./showAnalyticsNotification`)
+  const EventStorage = require(`./event-storage`)
 
-  if (enabledInConfig === undefined && !ci.isCI) {
+  const ci = require(`ci-info`)
+  const eventStorage = new EventStorage()
+  const disabled = eventStorage.disabled
+  const enabledInConfig = eventStorage.getConfig(`telemetry.enabled`)
+  if (enabledInConfig === undefined && !disabled && !ci.isCI) {
     showAnalyticsNotification()
   }
 } catch (e) {
