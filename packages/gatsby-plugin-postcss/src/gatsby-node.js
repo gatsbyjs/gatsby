gatsbyjs/gatsby

commit 88b3158db951a3097b93b0a9b4bec0412417ca8e

feat(gatsby-plugin-postcss): added css-loader options (#10861)
## Description

This commit allows users to define some css-loader options, such as `localIdentName` 

```javascript
{
	resolve: `gatsby-plugin-postcss`,
	options: {
		postCssPlugins: plugins,
		cssOptions: {
			localIdentName: 'mod-[hash:base64:8]'
		}
	}
}
``` 

`cssOptions` is the new bit.

## Related Issues

Some of my class names were getting stupidly long!

packages/gatsby-plugin-postcss/src/gatsby-node.js
@@ -38,6 +38,14 @@ exports.onCreateWebpackConfig = (
   const config = getConfig()
   const cssRules = findCssRules(config)
   const postcssOptions = getOptions(pluginOptions)
+
+  const generateCssLoaderOptions = options =>
+    Object.assign(
+      { importLoaders: 1 },
+      options,
+      pluginOptions.cssLoaderOptions || {}
+    )
+
   const postcssLoader = {
     loader: resolve(`postcss-loader`),
     options: { sourceMap: !isProduction, ...postcssOptions },
@@ -46,15 +54,12 @@ exports.onCreateWebpackConfig = (
     test: CSS_PATTERN,
     use: isSSR
       ? [loaders.null()]
-      : [loaders.css({ importLoaders: 1 }), postcssLoader],
+      : [loaders.css(generateCssLoaderOptions()), postcssLoader],
   }
   const postcssRuleModules = {
     test: MODULE_CSS_PATTERN,
     use: [
-      loaders.css({
-        modules: true,
-        importLoaders: 1,
-      }),
+      loaders.css(generateCssLoaderOptions({ modules: true })),
       postcssLoader,
     ],
   }

commit 4009a4b2c5f3d7091c815966fbdddac6d71076d0

fix(gatsby-plugin-postcss): Fix regression introduced by #10861

packages/gatsby-plugin-postcss/src/gatsby-node.js
@@ -3,22 +3,6 @@ import resolve from "./resolve"
 const CSS_PATTERN = /\.css$/
 const MODULE_CSS_PATTERN = /\.module\.css$/
 
-const getOptions = pluginOptions => {
-  const options = { ...pluginOptions }
-
-  delete options.plugins
-
-  const postcssPlugins = options.postCssPlugins
-
-  if (postcssPlugins) {
-    options.plugins = postcssPlugins
-  }
-
-  delete options.postCssPlugins
-
-  return options
-}
-
 const isCssRules = rule =>
   rule.test &&
   (rule.test.toString() === CSS_PATTERN.toString() ||
@@ -31,20 +15,18 @@ const findCssRules = config =>
 
 exports.onCreateWebpackConfig = (
   { actions, stage, loaders, getConfig },
-  pluginOptions
+  { cssLoaderOptions = {}, postCssPlugins, ...postcssOptions }
 ) => {
   const isProduction = !stage.includes(`develop`)
   const isSSR = stage.includes(`html`)
   const config = getConfig()
   const cssRules = findCssRules(config)
-  const postcssOptions = getOptions(pluginOptions)
 
-  const generateCssLoaderOptions = options =>
-    Object.assign(
-      { importLoaders: 1 },
-      options,
-      pluginOptions.cssLoaderOptions || {}
-    )
+  delete postcssOptions.plugins
+
+  if (postCssPlugins) {
+    postcssOptions.plugins = postCssPlugins
+  }
 
   const postcssLoader = {
     loader: resolve(`postcss-loader`),
@@ -54,12 +36,12 @@ exports.onCreateWebpackConfig = (
     test: CSS_PATTERN,
     use: isSSR
       ? [loaders.null()]
-      : [loaders.css(generateCssLoaderOptions()), postcssLoader],
+      : [loaders.css({ ...cssLoaderOptions, importLoaders: 1 }), postcssLoader],
   }
   const postcssRuleModules = {
     test: MODULE_CSS_PATTERN,
     use: [
-      loaders.css(generateCssLoaderOptions({ modules: true })),
+      loaders.css({ ...cssLoaderOptions, importLoaders: 1, modules: true }),
       postcssLoader,
     ],
   }