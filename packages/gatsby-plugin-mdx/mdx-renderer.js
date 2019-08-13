ac7f4e0252606e21b41eeb717d3ce9a4ecb82b93
Mathieu Dutour <mathieu@dutour.me>
2019-08-13 19:20:20 +0200

    chore(gatsby-plugin-mdx): run prettier (#16575)
    * run prettier on gatsby-plugin-mdx
    
    * Update .prettierignore
    
    * fix lint
    
    * fix lint
    
    * fix lint

--- packages/gatsby-plugin-mdx/mdx-renderer.js
+++ packages/gatsby-plugin-mdx/mdx-renderer.js
@@ -1,6 +1,6 @@
-const React = require("react");
-const { useMDXComponents, mdx } = require("@mdx-js/react");
-const { useMDXScope } = require("./context");
+const React = require(`react`)
+const { useMDXComponents, mdx } = require(`@mdx-js/react`)
+const { useMDXScope } = require(`./context`)
 
 module.exports = function MDXRenderer({
   scope,
@@ -9,37 +9,34 @@ module.exports = function MDXRenderer({
   ...props
 }) {
   if (!children) {
-    throw new Error(`MDXRenderer expected to receive an MDX String in children. Instead, it received: ${children}`)
+    throw new Error(
+      `MDXRenderer expected to receive an MDX String in children. Instead, it received: ${children}`
+    )
   }
 
-  const mdxComponents = useMDXComponents(components);
-  const mdxScope = useMDXScope(scope);
+  const mdxComponents = useMDXComponents(components)
+  const mdxScope = useMDXScope(scope)
 
   // Memoize the compiled component
-  const End = React.useMemo(
-    () => {
-      if (!children) {
-        return null;
-      }
+  const End = React.useMemo(() => {
+    if (!children) {
+      return null
+    }
 
-      const fullScope = {
-        // React is here just in case the user doesn't pass them in
-        // in a manual usage of the renderer
-        React,
-        mdx,
-        ...mdxScope
-      };
+    const fullScope = {
+      // React is here just in case the user doesn't pass them in
+      // in a manual usage of the renderer
+      React,
+      mdx,
+      ...mdxScope,
+    }
 
-      const keys = Object.keys(fullScope);
-      const values = keys.map(key => fullScope[key]);
-      const fn = new Function("_fn", ...keys, `${children}`);
+    const keys = Object.keys(fullScope)
+    const values = keys.map(key => fullScope[key])
+    const fn = new Function(`_fn`, ...keys, `${children}`)
 
-      return fn({}, ...values);
-    },
-    [children, scope]
-  );
+    return fn({}, ...values)
+  }, [children, scope])
 
-  return (
-    React.createElement(End, { components: mdxComponents, ...props })
-  );
-};
+  return React.createElement(End, { components: mdxComponents, ...props })
+}
