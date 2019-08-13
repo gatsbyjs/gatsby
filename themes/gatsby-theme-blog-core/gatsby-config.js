c222872bb3dac51bf6d5b1e3303c96e6672c0fcf
Christopher Biscardi <chris@christopherbiscardi.com>
2019-08-13 15:03:49 -0700

    Gatsby theme blog core (#16166)
    * gatsby-theme-blog-core + starter
    
    json page output
    
    more changes for draft PR
    
    base gatsby-theme-blog on gatsby-theme-blog-core
    
    remove console
    
    fix lint error
    
    Create BlogPost interface implementation
    
    remove old templates
    
    * needs mdx-utils too
    
    * post(s) => post-query
    
    * remove dupe body
    
    * remove format string from date
    
    * remove unused files
    
    * depend on mdx-js/mdx
    
    * READMEs
    
    * correctly parent MdxBlogPosts
    
    * downgrade -core in starter until we really publish
    
    * package-lock
    
    * versioning
    
    * remove title, slug, date
    
    * Update themes/gatsby-starter-blog-theme-core/README.md
    
    Co-Authored-By: John Otander <johnotander@gmail.com>

---
+++ themes/gatsby-theme-blog-core/gatsby-config.js
@@ -0,0 +1,61 @@
+const withDefaults = require(`./utils/default-options`)
+
+module.exports = themeOptions => {
+  const options = withDefaults(themeOptions)
+  const { mdx = true } = themeOptions
+  return {
+    siteMetadata: {
+      title: `Blog Title Placeholder`,
+      author: `Name Placeholder`,
+      description: `Description placeholder`,
+      social: [
+        {
+          name: `Twitter`,
+          url: `https://twitter.com/gatsbyjs`,
+        },
+        {
+          name: `GitHub`,
+          url: `https://github.com/gatsbyjs`,
+        },
+      ],
+    },
+    plugins: [
+      mdx && {
+        resolve: `gatsby-plugin-mdx`,
+        options: {
+          extensions: [`.mdx`, `.md`],
+          gatsbyRemarkPlugins: [
+            {
+              resolve: `gatsby-remark-images`,
+              options: {
+                // should this be configurable by the end-user?
+                maxWidth: 1380,
+                linkImagesToOriginal: false,
+              },
+            },
+            { resolve: `gatsby-remark-copy-linked-files` },
+            { resolve: `gatsby-remark-numbered-footnotes` },
+            { resolve: `gatsby-remark-smartypants` },
+          ],
+          remarkPlugins: [require(`remark-slug`)],
+        },
+      },
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          path: options.contentPath || `content/posts`,
+          name: options.contentPath || `content/posts`,
+        },
+      },
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          path: options.assetPath || `content/assets`,
+          name: options.assetPath || `content/assets`,
+        },
+      },
+      `gatsby-transformer-sharp`,
+      `gatsby-plugin-sharp`,
+    ].filter(Boolean),
+  }
+}
