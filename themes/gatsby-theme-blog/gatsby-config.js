5e1fde351e40974a18c55861909aa2c3eafcfc83
Christopher Biscardi <chris@christopherbiscardi.com>
2019-08-13 19:31:22 -0700

    remove buggy plugin and passthrough options

--- themes/gatsby-theme-blog/gatsby-config.js
+++ themes/gatsby-theme-blog/gatsby-config.js
@@ -1,11 +1,9 @@
 module.exports = options => {
-  const { mdx = true } = options
-
   return {
     plugins: [
       {
         resolve: `gatsby-theme-blog-core`,
-        options: { mdx },
+        options,
       },
       `gatsby-plugin-react-helmet`,
       `gatsby-plugin-twitter`,
