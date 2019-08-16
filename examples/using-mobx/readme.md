27e4864cf83e15eaadb4e225bb0f63f9837fb50d
jonniebigodes <jonniebigodes@hotmail.com>
2019-08-16 23:52:17 +0100

    update example dependencies and code

--- examples/using-mobx/readme.md
+++ examples/using-mobx/readme.md
@@ -6,7 +6,7 @@ Gatsby example site that shows use of mobx.
 
 # Overview
 
-To use mobx in a Gatsby site you'll need to hook in to two of Gatsby's extension points.
+To use mobx in a Gatsby site you'll need to hook into two of Gatsby's extension points.
 
 Once in wrapRootElement which runs during Gatsby's server rendering process, and once in wrapRootElement which is part of Gatsby's browser APIs.
 Check out [./gatsby-ssr.js](./gatsby-ssr.js) and [./gatsby-browser.js](./gatsby-browser.js) to see how this is implemented in this example
@@ -16,3 +16,9 @@ Check out [./gatsby-ssr.js](./gatsby-ssr.js) and [./gatsby-browser.js](./gatsby-
 [Docs](https://mobx.js.org/)
 
 [Examples](https://github.com/mobxjs/mobx-examples)
+
+### Note on mobx-react-devtools
+
+As of now the `mobx-react-devtools` package is deprecated.
+
+If you need access to some form of development tools, you can get a similar experience by installing the browser plugin by following the instructions detailed [here](https://github.com/mobxjs/mobx-devtools).
--- examples/using-mobx/src/components/layout.js
+++ examples/using-mobx/src/components/layout.js
@@ -1,32 +1,35 @@
-import React, { Component } from "react"
+import React from "react"
+import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import Counter from "./Counter"
 
-import DevTools from "mobx-react-devtools"
-
-class DefaultLayout extends Component {
-  render() {
-    return (
+const DefaultLayout = props => {
+  const { children } = props
+  return (
+    <div>
+      <Link to="/">
+        <h3>MobX example</h3>
+      </Link>
       <div>
-        <Link to="/">
-          <h3>MobX example</h3>
-        </Link>
-        <DevTools />
         <Counter />
-        <ul>
-          <li>
-            <Link to="/a/">a</Link>
-          </li>
-          <li>
-            <Link to="/b/">b</Link>
-          </li>
-          <li>
-            <Link to="/c/">c</Link>
-          </li>
-        </ul>
-        {this.props.children}
       </div>
-    )
-  }
+
+      <ul>
+        <li>
+          <Link to="/a/">a</Link>
+        </li>
+        <li>
+          <Link to="/b/">b</Link>
+        </li>
+        <li>
+          <Link to="/c/">c</Link>
+        </li>
+      </ul>
+      {children}
+    </div>
+  )
+}
+DefaultLayout.propTypes = {
+  children: PropTypes.object.isRequired, // eslint-disable-line
 }
 export default DefaultLayout
