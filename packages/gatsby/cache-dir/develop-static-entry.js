d61608ce3b5c1cc506ff965b0533e1822f14f361
gatsbybot <mathews.kyle+gatsbybot@gmail.com>
2019-08-20 07:47:33 +0000

    chore: format

--- packages/gatsby/cache-dir/develop-static-entry.js
+++ packages/gatsby/cache-dir/develop-static-entry.js
@@ -28,11 +28,7 @@ Html = Html && Html.__esModule ? Html.default : Html
 
 export default (pagePath, callback) => {
   let headComponents = [
-    <meta
-      key="environment"
-      name="note"
-      content="environment=development"
-    />,
+    <meta key="environment" name="note" content="environment=development" />,
   ]
   let htmlAttributes = {}
   let bodyAttributes = {}
