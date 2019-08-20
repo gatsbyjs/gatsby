8ea4e9a027f4cc1f803bd49bad4b5df5fa2a78fd
Ward Peeters <ward@coding-tech.com>
2019-08-20 09:21:03 +0200

    fix review comment

--- packages/gatsby/cache-dir/develop-static-entry.js
+++ packages/gatsby/cache-dir/develop-static-entry.js
@@ -29,7 +29,7 @@ Html = Html && Html.__esModule ? Html.default : Html
 export default (pagePath, callback) => {
   let headComponents = [
     <meta
-      key="environment-note"
+      key="environment"
       name="note"
       content="environment=development"
     />,
