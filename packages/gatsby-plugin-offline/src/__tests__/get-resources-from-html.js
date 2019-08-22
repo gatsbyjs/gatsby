0275f1023b755e9c9f4282d1ae506820aebb2d6d
David Bailey <davidbailey00@outlook.com>
2019-08-22 10:25:53 +0100

    oops I forgot to update the arguments

--- packages/gatsby-plugin-offline/src/__tests__/get-resources-from-html.js
+++ packages/gatsby-plugin-offline/src/__tests__/get-resources-from-html.js
@@ -4,6 +4,6 @@ const getResourcesFromHTML = require(`../get-resources-from-html`)
 const htmlPath = path.resolve(`${__dirname}/index.html`)
 
 it(`it extracts resources correctly`, () => {
-  const resources = getResourcesFromHTML(htmlPath)
+  const resources = getResourcesFromHTML(htmlPath, ``)
   expect(resources).toMatchSnapshot()
 })
