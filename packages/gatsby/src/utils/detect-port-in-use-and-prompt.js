6ada08c830e7fdcc1990771afb3440b0deaf2a6e
Jeremy Albright <jeremy.albright@manaollc.com>
2019-08-21 20:50:51 -1000

    readline prompt

--- packages/gatsby/src/utils/detect-port-in-use-and-prompt.js
+++ packages/gatsby/src/utils/detect-port-in-use-and-prompt.js
@@ -1,27 +1,27 @@
 const detectPort = require(`detect-port`)
 const report = require(`gatsby-cli/lib/reporter`)
-// const prompt = require(`./readline-interface`).prompt
-const readlineSync = require(`readline-sync`)
+const prompt = require(`./readline-interface`).prompt
+// const readlineSync = require(`readline-sync`)
 
 const readlinePort = port => {
   const question = `Something is already running at port ${port} \nWould you like to run the app at another port instead? [Y/n] `
   return new Promise(resolve => {
-    // prompt.once(question, answer => {
-    //   resolve(answer.length === 0 || answer.match(/^yes|y$/i))
-    // })
-    const interrupt = () => {
-      process.exit(0)
-    }
-    process.on(`SIGINT`, interrupt)
-    const answer = readlineSync.keyIn(question, {
-      limit: [`y`, `n`, ``],
-      defaultInput: `y`,
-      caseSensitive: false,
-      trueValue: [`y`],
-      falseValue: [`n`],
+    prompt.once(question, answer => {
+      resolve(answer.length === 0 || answer.match(/^yes|y$/i))
     })
-    process.removeListener(`SIGINT`, interrupt)
-    resolve(answer)
+    // const interrupt = () => {
+    //   process.exit(0)
+    // }
+    // process.on(`SIGINT`, interrupt)
+    // const answer = readlineSync.keyIn(question, {
+    //   limit: [`y`, `n`, ``],
+    //   defaultInput: `y`,
+    //   caseSensitive: false,
+    //   trueValue: [`y`],
+    //   falseValue: [`n`],
+    // })
+    // process.removeListener(`SIGINT`, interrupt)
+    // resolve(answer)
   })
 }
 
