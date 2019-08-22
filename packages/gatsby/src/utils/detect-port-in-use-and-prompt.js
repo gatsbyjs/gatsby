57cbbf2ce3fb6db462d484fd0dcf321b5cc57724
Jeremy Albright <jeremy.albright@manaollc.com>
2019-08-21 20:43:04 -1000

    readline module

--- packages/gatsby/src/utils/detect-port-in-use-and-prompt.js
+++ packages/gatsby/src/utils/detect-port-in-use-and-prompt.js
@@ -1,21 +1,38 @@
 const detectPort = require(`detect-port`)
 const report = require(`gatsby-cli/lib/reporter`)
+// const prompt = require(`./readline-interface`).prompt
+const readlineSync = require(`readline-sync`)
 
-const readlinePort = (port, rlInterface) => {
+const readlinePort = port => {
   const question = `Something is already running at port ${port} \nWould you like to run the app at another port instead? [Y/n] `
   return new Promise(resolve => {
-    rlInterface.question(question, answer => {
-      resolve(answer.length === 0 || answer.match(/^yes|y$/i))
+    // prompt.once(question, answer => {
+    //   resolve(answer.length === 0 || answer.match(/^yes|y$/i))
+    // })
+    const interrupt = () => {
+      process.exit(0)
+    }
+    process.on(`SIGINT`, interrupt)
+    const answer = readlineSync.keyIn(question, {
+      limit: [`y`, `n`, ``],
+      defaultInput: `y`,
+      caseSensitive: false,
+      trueValue: [`y`],
+      falseValue: [`n`],
     })
+    process.removeListener(`SIGINT`, interrupt)
+    resolve(answer)
   })
 }
 
-const detectPortInUseAndPrompt = async (port, rlInterface) => {
+const detectPortInUseAndPrompt = async port => {
   let foundPort = port
   const detectedPort = await detectPort(port).catch(err => report.panic(err))
   if (port !== detectedPort) {
-    if (await readlinePort(port, rlInterface)) {
+    if (await readlinePort(port)) {
       foundPort = detectedPort
+    } else {
+      return Promise.reject(`USER_REJECTED`)
     }
   }
   return foundPort
---
+++ packages/gatsby/src/utils/readline-interface.js
@@ -0,0 +1,118 @@
+// /* @flow */
+// const readline = require(`readline`)
+
+// type ReadlineOptions = {
+//   input: NodeJS.ReadStream,
+//   output: NodeJS.WriteStream,
+//   terminal: boolean,
+//   historySize: ?number,
+//   prompt: ?string,
+//   crlfDelay: ?number,
+//   removeHistoryDuplicates: ?boolean,
+//   escapeCodeTimeout: ?number,
+// }
+
+// const defaultIntfcOpts: ReadlineOptions = {
+//   input: process.stdin,
+//   output: process.stdout,
+//   terminal: true,
+// }
+
+// /**
+//  * Spins up and returns a readline interface
+//  * @param {ReadlineOptions} opts
+//  */
+// function create(opts: ?ReadlineOptions) {
+//   const rlOpts = {
+//     ...defaultIntfcOpts,
+//     ...opts,
+//   }
+
+//   const stdin = rlOpts.input
+//   const stdout = rlOpts.output
+
+//   if (!stdin || (stdin !== process.stdin && !stdin.isTTY)) {
+//     throw new Error(`Invalid stream passed`)
+//   }
+//   if (!stdout || (stdout !== process.stdout && !stdout.isTTY)) {
+//     throw new Error(`Invalid output passed`)
+//   }
+
+//   const rl = readline.createInterface({
+//     ...defaultIntfcOpts,
+//     ...opts,
+//   })
+
+//   const isRaw = stdin.isRaw
+//   if (stdin.isTTY) stdin.setRawMode(true)
+
+//   // Save a reference to the original
+//   // close function, so we can call it later
+//   const rlClose = rl.close
+
+//   const close = () => {
+//     if (stdin.isTTY) stdin.setRawMode(isRaw)
+//     rl.pause()
+//     rlClose()
+//   }
+//   // Overloading the close interface, so we
+//   // can inject some custom behavior
+//   rl.close = close
+
+//   // rl.resume()
+
+//   return rl
+// }
+
+// // const ask = rl => (query: any, cb: (answer: any) => void) => {
+// //   rl.setPrompt(query)
+// //   rl.prompt()
+// //   rl.on(`line`, input => {
+// //     cb(input.trim())
+// //   })
+// // }
+
+// const prompt = {
+//   /**
+//    * Create a new readline dedicated to providing the question
+//    * interface
+//    * @param {ReadlineOptions} rlOpts Options to override readline interface
+//    * with
+//    */
+//   new: async (rlOpts: ?ReadlineOptions) => {
+//     const rl = create(rlOpts)
+//     return {
+//       // ask: ask(rl),
+//       ask: rl.question,
+//       done: rl.close,
+//     }
+//   },
+//   /**
+//    * Creates a new readline interface for a one-time prompt, then closes it.
+//    * @param {string} query The question to ask the user
+//    * @param {(answer: any) => void} cb The callback that will receive the user's
+//    * input
+//    * @param {ReadlineOptions} rlOpts Options to override the readline interface
+//    */
+//   once: (
+//     query: string,
+//     cb: (answer: any) => void,
+//     rlOpts: ?ReadlineOptions
+//   ) => {
+//     const rl = create(rlOpts)
+//     rl.question(query, answer => {
+//       rl.close()
+//       cb(answer)
+//     })
+//     // ask(rl)(query, answer => {
+//     //   cb(answer)
+//     //   rl.close()
+//     // })
+//   },
+// }
+
+// module.exports = {
+//   create,
+//   prompt,
+//   // extend features here
+// }
