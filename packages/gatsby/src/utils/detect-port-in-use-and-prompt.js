04ed96cef27eeb85cee65350f0e07c1925f93c5a
Jeremy Albright <jeremy.albright@manaollc.com>
2019-08-22 12:32:15 -1000

    Using `readline-interface.prompt`

--- packages/gatsby/src/utils/detect-port-in-use-and-prompt.js
+++ packages/gatsby/src/utils/detect-port-in-use-and-prompt.js
@@ -1,27 +1,24 @@
 const detectPort = require(`detect-port`)
 const report = require(`gatsby-cli/lib/reporter`)
-const prompt = require(`./readline-interface`).prompt
-// const readlineSync = require(`readline-sync`)
+const { prompt } = require(`./readline-interface`)
 
 const readlinePort = port => {
-  const question = `Something is already running at port ${port} \nWould you like to run the app at another port instead? [Y/n] `
+  const question = `Something is already running at port ${port} \nWould you like to run the app at another port instead? [Y/n]: `
   return new Promise(resolve => {
-    prompt.once(question, answer => {
-      resolve(answer.length === 0 || answer.match(/^yes|y$/i))
-    })
-    // const interrupt = () => {
-    //   process.exit(0)
-    // }
-    // process.on(`SIGINT`, interrupt)
-    // const answer = readlineSync.keyIn(question, {
-    //   limit: [`y`, `n`, ``],
-    //   defaultInput: `y`,
-    //   caseSensitive: false,
-    //   trueValue: [`y`],
-    //   falseValue: [`n`],
-    // })
-    // process.removeListener(`SIGINT`, interrupt)
-    // resolve(answer)
+    prompt.once(
+      question,
+      answer => {
+        resolve(answer)
+      },
+      {
+        single: false,
+        validInput: [`Y`, `n`],
+        defaultValue: `y`,
+        returnBoolean: {
+          trueValue: `y`,
+        },
+      }
+    )
   })
 }
 
