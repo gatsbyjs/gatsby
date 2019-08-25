c70d785f11e45c44c46949b514a6ef8677bf11e0
kingingcole <43896019+kingingcole@users.noreply.github.com>
2019-08-25 19:42:28 +0100

    Update gatsby-node.js

--- starters/blog/gatsby-node.js
+++ starters/blog/gatsby-node.js
@@ -33,7 +33,7 @@ exports.createPages = async ({ graphql, actions }) => {
   }
 
   // Create blog posts pages.
-  const posts = result.data.allMarkdownRemark.edges.filter(post => post.node.frontmatter.published !== false || post.node.frontmatter.published === true)
+  const posts = result.data.allMarkdownRemark.edges.filter( post => post.node.frontmatter.published === true )
 
   posts.forEach((post, index) => {
     const previous = index === posts.length - 1 ? null : posts[index + 1].node
