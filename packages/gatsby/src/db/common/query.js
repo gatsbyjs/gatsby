3e6e45b69e662c5dee794a98160752e4076a3d09
Mikhail Novikov <freiksenet@gmail.com>
2019-08-27 16:32:50 +0300

    Remove extra stuff

--- packages/gatsby/src/db/common/query.js
+++ packages/gatsby/src/db/common/query.js
@@ -1,68 +1,5 @@
 const _ = require(`lodash`)
 
-const getQueryFields = ({ filter, sort, group, distinct }) => {
-  const filterFields = filter ? dropQueryOperators(filter) : {}
-  const sortFields = (sort && sort.fields) || []
-
-  if (group && !Array.isArray(group)) {
-    group = [group]
-  } else if (group == null) {
-    group = []
-  }
-
-  if (distinct && !Array.isArray(distinct)) {
-    distinct = [distinct]
-  } else if (distinct == null) {
-    distinct = []
-  }
-
-  return merge(
-    filterFields,
-    ...sortFields.map(pathToObject),
-    ...group.map(pathToObject),
-    ...distinct.map(pathToObject)
-  )
-}
-
-const mergeObjects = (obj1, obj2) =>
-  Object.keys(obj2).reduce((acc, key) => {
-    const value = obj2[key]
-    if (typeof value === `object` && value && acc[key]) {
-      acc[key] = mergeObjects(acc[key], value)
-    } else {
-      acc[key] = value
-    }
-    return acc
-  }, obj1)
-
-const merge = (...objects) => {
-  const [first, ...rest] = objects.filter(Boolean)
-  return rest.reduce((acc, obj) => mergeObjects(acc, obj), { ...first })
-}
-
-const pathToObject = path => {
-  if (path && typeof path === `string`) {
-    return path.split(`.`).reduceRight((acc, key) => {
-      return { [key]: acc }
-    }, true)
-  }
-  return {}
-}
-
-const dropQueryOperators = filter =>
-  Object.keys(filter).reduce((acc, key) => {
-    const value = filter[key]
-    const k = Object.keys(value)[0]
-    const v = value[k]
-    if (_.isPlainObject(value) && _.isPlainObject(v)) {
-      acc[key] =
-        k === `elemMatch` ? dropQueryOperators(v) : dropQueryOperators(value)
-    } else {
-      acc[key] = true
-    }
-    return acc
-  }, {})
-
 // Converts a nested mongo args object into a dotted notation. acc
 // (accumulator) must be a reference to an empty object. The converted
 // fields will be added to it. E.g
@@ -150,8 +87,6 @@ const liftResolvedFields = (args, resolvedFields) => {
 }
 
 module.exports = {
-  dropQueryOperators,
-  getQueryFields,
   toDottedFields,
   objectToDottedField,
   liftResolvedFields,
