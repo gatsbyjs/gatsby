a166970cd26ad65e7d0786e6aced956e8606ed43
Ward Peeters <ward@coding-tech.com>
2019-08-28 20:50:46 +0200

    keep file, just comment

---
+++ packages/gatsby-plugin-preact/src/gatsby-browser.js
@@ -0,0 +1,6 @@
+// TODO enable preact/debug again when preact devtools is fixed.
+// exports.onClientEntry = () => {
+//   if (process.env.NODE_ENV !== `production`) {
+//     require(`preact/debug`)
+//   }
+// }
