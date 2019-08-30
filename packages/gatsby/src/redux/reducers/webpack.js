3daaf7543fa1310486d89f97f881ff05e3e87c8a
Eddie CooRo <1300100eddiecooro@gmail.com>
2019-08-30 21:35:42 +0430

    feat(gatsby): Add support for webpack multi compiler mode
    Add support for webpack Multi Compiler mode by letting users set
    an array as webpack config using the replaceWebpackConfig API.

--- packages/gatsby/src/redux/reducers/webpack.js
+++ packages/gatsby/src/redux/reducers/webpack.js
@@ -1,18 +1,30 @@
 const merge = require(`webpack-merge`)
 
+function copy(el) {
+  return Array.isArray(el) ? [...el] : { ...el }
+}
+
+function deleteImportantConfigs(config) {
+  const newConfig = copy(config)
+  delete newConfig.entry
+  delete newConfig.output
+  delete newConfig.target
+  delete newConfig.resolveLoaders
+  return newConfig
+}
+
 module.exports = (state = {}, action) => {
   switch (action.type) {
     case `SET_WEBPACK_CONFIG`: {
-      let nextConfig = action.payload
-      delete nextConfig.entry
-      delete nextConfig.output
-      delete nextConfig.target
-      delete nextConfig.resolveLoaders
+      let nextConfig = Array.isArray(action.payload)
+        ? action.payload.map(deleteImportantConfigs)
+        : deleteImportantConfigs(action.payload)
 
       return merge(state, nextConfig)
     }
+
     case `REPLACE_WEBPACK_CONFIG`:
-      return { ...action.payload }
+      return copy(action.payload)
 
     default:
       return state
