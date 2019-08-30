3d2d13141e06a47297c1211eaab765a8ed98e78d
anamritraj <anandamritraj904@gmail.com>
2019-08-30 15:42:49 +0530

    Add success message after installing  gastsby-cli.

---
+++ packages/gatsby-cli/scripts/postinstall.js
@@ -0,0 +1,6 @@
+const createCli = require('../lib/create-cli');
+
+console.log('\033[1;32mSuccess!\n\033[0m');
+console.log('\033[1;36mWelcome to the Gatsby CLI! Please visit \033[7;36mhttps://www.gatsbyjs.org/docs/gatsby-cli/\033[m \033[1;36mfor more information.\033[0m\n');
+
+createCli('--help');
