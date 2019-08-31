8fb0b53e417e6fc33af6f4b55e81909d5421fdd1
djD-REK <53202242+djD-REK@users.noreply.github.com>
2019-08-31 19:32:21 -0400

    Added error #85902 with working Jest snapshot test to warn Windows users that uppercase folders can produce a GraphQL error

--- packages/gatsby/src/query/__tests__/error-parser.js
+++ packages/gatsby/src/query/__tests__/error-parser.js
@@ -8,6 +8,11 @@ it.each([
     `85907`,
   ],
   [`totally vague one`, `foo bar`, `85901`],
+  [
+    `Windows uppercase folders error`,
+    `Error: RelayParser: Encountered duplicated definitions for one or more documents: each document must have a unique name. Duplicated documents:`,
+    `85902`,
+  ],
 ])(`%s`, (_name, message, expectedId) => {
   const structured = errorParser({
     message,
