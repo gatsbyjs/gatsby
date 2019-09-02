433d792da981ea10ba46c4b6fd7f9071ff20f8ad
Michal Piechowiak <misiek.piechowiak@gmail.com>
2019-09-02 12:54:39 +0200

    make createPages snippet richer, so full flow is accounted for

--- packages/gatsby-graphiql-explorer/src/app/snippets.js
+++ packages/gatsby-graphiql-explorer/src/app/snippets.js
@@ -68,11 +68,37 @@ const createPagesQuery = {
   language: `JavaScript`,
   codeMirrorMode: `jsx`,
   options: [],
-  generate: arg => `exports.createPages = async ({ graphql, actions }) => {
+  generate: arg => `const path = require(\`path\`)
+  
+exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions
   const result = await graphql(\`
 ${getQuery(arg, 4)}
   \`)
+
+  if (result.errors) {
+    reporter.panicOnBuild(
+      \`There was error while executing query in gatsby-node.js:\\n\\n\${result.errors
+        .map(e => e.toString())
+        .join(\`\\n\\n\`)}\`
+    )
+    return
+  }
+
+  // Use result.data to create pages, snippet below is sample that need to be adjusted for shape of your query
+  result.data.allData.nodes.forEach(dataNode => {
+    createPage({
+      // set URL of the page
+      path: \`/my/url/\${dataNode.slug}\`,
+      // set react template used to render page
+      component: path.resolve(\`./src/templates/my-template.js\`),
+      // provide variables that can be used in page query to
+      // select node - id is great choice as it is unique
+      context: {
+        id: dataNode.id,
+      },
+    })
+  })
 }
 
 `,
