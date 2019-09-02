88279b5cad27300042f0a3595e4cb3b1fdfb2312
herecydev <herecy@live.co.uk>
2019-09-02 20:24:18 +0100

    Use named components

--- packages/gatsby-graphiql-explorer/src/app/snippets.js
+++ packages/gatsby-graphiql-explorer/src/app/snippets.js
@@ -16,11 +16,13 @@ const pageQuery = {
   generate: arg => `import React from "react"
 import { graphql } from "gatsby"
 
-export default ({ data }) => <pre>{JSON.stringify(data, null, 4)}</pre>
+const ComponentName = ({ data }) => <pre>{JSON.stringify(data, null, 4)}</pre>
 
 export const query = graphql\`
 ${getQuery(arg, 2)}
 \`
+
+export default ComponentName
 
 `,
 }
@@ -33,12 +35,14 @@ const staticHook = {
   generate: arg => `import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
-export default () => {
+const ComponentName = () => {
   const data = useStaticQuery(graphql\`
 ${getQuery(arg, 4)}
   \`)
   return <pre>{JSON.stringify(data, null, 4)}</pre>
 }
+
+export default ComponentName
 
 `,
 }
@@ -51,7 +55,7 @@ const staticQuery = {
   generate: arg => `import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 
-export default () => (
+const ComponentName = () => (
   <StaticQuery
     query={graphql\`
 ${getQuery(arg, 6)}
@@ -60,99 +64,9 @@ ${getQuery(arg, 6)}
   ></StaticQuery>
 )
 
-`,
-}
-
-const getDataNodeName = name =>
-  name.startsWith(`all`) && name.length > 3
-    ? name.substring(3, 4).toLowerCase() + name.substring(4, name.length)
-    : name
-
-const getFieldQuery = (root, enableComments) => {
-  const rootName = root.name.value
-  const dataNodeName = getDataNodeName(rootName)
-  let nodes
-
-  const comment = message => (enableComments ? `\n      // ${message}` : ``)
-
-  if (root.selectionSet) {
-    nodes = root.selectionSet.selections.find(
-      selection => selection.name.value === `nodes`
-    )
-
-    if (nodes && nodes.selectionSet) {
-      let hasId = false
-      let hasSlug = false
-      nodes.selectionSet.selections.forEach(field => {
-        if (field.name.value === `id`) {
-          hasId = true
-        }
-        if (field.name.value === `slug`) {
-          hasSlug = true
-        }
-      })
-
-      return `  result.data.${rootName}.${
-        nodes.name.value
-      }.forEach(${dataNodeName} => {
-    createPage({${comment(`set URL of the page`)}
-      path: ${
-        hasSlug
-          ? `\`/${dataNodeName}/\${${dataNodeName}.slug}/\``
-          : `\`/${dataNodeName}/\${${dataNodeName}.uniquePathField}/\``
-      },${comment(`set react template used to render page`)}
-      component: path.resolve(\`./src/templates/${dataNodeName}.js\`),${comment(
-        `provide variables that can be used in page query`
-      )}
-      context: {
-        ${hasId ? `id: ${dataNodeName}.id,` : ``}
-      },
-    })
-  })`
-    }
-
-    if (enableComments) {
-      return `  // could not find field 'nodes' on ${rootName}\n  // this should probably be queried directly from a page by using the '${pageQuery.name}'`
-    }
-  }
-  return null
-}
-
-const createPagesQuery = {
-  name: `createPages`,
-  language: `JavaScript`,
-  codeMirrorMode: `jsx`,
-  options: [
-    {
-      id: `comments`,
-      label: `Comments`,
-      initial: false,
-    },
-  ],
-  generate: arg => `const path = require(\`path\`)
-  
-exports.createPages = async ({ graphql, actions, reporter }) => {
-  const { createPage } = actions
-  const result = await graphql(\`
-${getQuery(arg, 4)}
-  \`)
-
-  if (result.errors) {
-    reporter.panicOnBuild(
-      \`There was error while executing query in gatsby-node.js:\\n\\n\${result.errors
-        .map(e => e.toString())
-        .join(\`\\n\\n\`)}\`
-    )
-    return
-  }
-
-${arg.operationDataList[0].operationDefinition.selectionSet.selections
-  .map(selection => getFieldQuery(selection, arg.options.comments))
-  .filter(fieldQuery => fieldQuery !== null)
-  .join(`\n\n`)}
-}
+export default ComponentName
 
 `,
 }
 
-export default [pageQuery, staticHook, staticQuery, createPagesQuery]
+export default [pageQuery, staticHook, staticQuery]
