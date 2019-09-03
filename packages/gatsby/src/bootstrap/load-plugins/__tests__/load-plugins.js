21d06d1a35f54db9583df691cd05c30879cf3c8c
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2019-09-04 03:28:58 +0530

    Sanitize paths on Windows

--- packages/gatsby/src/bootstrap/load-plugins/__tests__/load-plugins.js
+++ packages/gatsby/src/bootstrap/load-plugins/__tests__/load-plugins.js
@@ -1,4 +1,5 @@
 const loadPlugins = require(`../index`)
+const slash = require(`slash`)
 
 describe(`Load plugins`, () => {
   /**
@@ -9,6 +10,16 @@ describe(`Load plugins`, () => {
    */
   const replaceFieldsThatCanVary = plugins =>
     plugins.map(plugin => {
+      if (plugin.pluginOptions && plugin.pluginOptions.path) {
+        plugin.pluginOptions = {
+          ...plugin.pluginOptions,
+          path: plugin.pluginOptions.path.replace(
+            slash(process.cwd()),
+            `<PROJECT_ROOT>`
+          ),
+        }
+      }
+
       return {
         ...plugin,
         id: ``,
