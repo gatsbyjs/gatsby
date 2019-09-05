235dbe306c4da9cc3b552a10d689092a63b21ea2
Vishwa Mehta <32997409+mehtavishwa30@users.noreply.github.com>
2019-09-06 00:15:49 +0530

    Docs: Example for sourcing from Wordpress

--- examples/gatsby-sourcing-wordpress/gatsby-config.js
+++ examples/gatsby-sourcing-wordpress/gatsby-config.js
@@ -1,1 +1,17 @@
-
+module.exports = {
+  siteMetadata: {
+    title: ``,
+    subtitle: `Sourcing data from Wordpress`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        baseUrl: `example.wordpress.com`,
+        protocol: `https`,
+        hostingWPCOM: false,
+        useACF: false,
+      },
+    },
+  ],
+}
