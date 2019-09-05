fb70f6406e62965195f9a61622ea9af7c6fad924
Vishwa Mehta <32997409+mehtavishwa30@users.noreply.github.com>
2019-09-06 00:02:37 +0530

    Docs: Example for sourcing from Wordpress

--- examples/gatsby-sourcing-wordpress/gatsby-node.js
+++ examples/gatsby-sourcing-wordpress/gatsby-node.js
@@ -1,1 +1,60 @@
+const path = require(`path`)
+const slash = require(`slash`)
 
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  
+  const result = await graphql(`
+    {
+      allWordpressPage {
+        edges {
+          node {
+            id
+            slug
+            status
+            template
+          }
+        }
+      }
+      allWordpressPost {
+        edges {
+          node {
+            id
+            slug
+            status
+            template
+            format
+          }
+        }
+      }
+    }
+  `)
+
+  if (result.errors) {
+    console.error(result.errors)
+  }
+
+  const { allWordpressPage, allWordpressPost } = result.data
+
+  const pageTemplate = path.resolve(`./src/templates/page.js`)
+  allWordpressPage.edges.forEach(edge => {
+    createPage({
+      path: `/${edge.node.slug}/`,
+      component: slash(pageTemplate),
+      context: {
+        id: edge.node.id,
+      },
+    })
+  })
+
+  const postTemplate = path.resolve(`./src/templates/post.js`)
+  allWordpressPost.edges.forEach(edge => {
+    createPage({
+      path: `/${edge.node.slug}/`,
+      component: slash(postTemplate),
+      context: {
+        id: edge.node.id,
+      },
+    })
+  })
+}
