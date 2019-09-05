e3c29ce2897ad496a070d9aed914476f01303ee1
Ward Peeters <ward@coding-tech.com>
2019-09-05 12:32:39 +0200

    cleanup requires-writer tests

--- packages/gatsby/src/bootstrap/__tests__/requires-writer.js
+++ packages/gatsby/src/bootstrap/__tests__/requires-writer.js
@@ -3,21 +3,17 @@ const requiresWriter = require(`../requires-writer`)
 
 const now = Date.now()
 
-const newMockState = () => {
-  const pages = new Map()
-  pages.set(`path1`, {
-    component: `component1`,
-    componentChunkName: `chunkName1`,
-    matchPath: `matchPath1`,
-    path: `/path1`,
+const generatePagesState = pages => {
+  let state = new Map()
+  pages.forEach(page => {
+    state.set(page.path, {
+      component: ``,
+      componentChunkName: ``,
+      ...page,
+    })
   })
-  pages.set(`path2`, {
-    component: `component2`,
-    componentChunkName: `chunkName2`,
-    path: `/path2`,
-  })
-  const program = { directory: `/dir` }
-  return { pages, program }
+
+  return state
 }
 
 jest.mock(`fs-extra`, () => {
@@ -30,14 +26,42 @@ jest.mock(`fs-extra`, () => {
 const mockFsExtra = require(`fs-extra`)
 
 describe(`requires-writer`, () => {
+  const program = {
+    directory: `/dir`,
+  }
+  let originalDateNow = global.Date.now
+
   beforeEach(() => {
     global.Date.now = () => now
     requiresWriter.resetLastHash()
   })
+
+  afterAll(() => {
+    global.Date.now = originalDateNow
+  })
+
   describe(`writeAll`, () => {
     it(`writes requires files`, async () => {
+      const pages = generatePagesState([
+        {
+          component: `component1`,
+          componentChunkName: `chunkName1`,
+          matchPath: `matchPath1`,
+          path: `/path1`,
+        },
+        {
+          component: `component2`,
+          componentChunkName: `chunkName2`,
+          path: `/path2`,
+        },
+      ])
+
       const spy = jest.spyOn(mockFsExtra, `writeFile`)
-      await requiresWriter.writeAll(newMockState())
+      await requiresWriter.writeAll({
+        pages,
+        program,
+      })
+
       expect(spy).toBeCalledWith(
         joinPath(`/dir`, `.cache`, `match-paths.json.${now}`),
         JSON.stringify([{ path: `/path1`, matchPath: `matchPath1` }], null, 4)
