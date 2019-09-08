09469ce979b48b6db0b224b1b230b2482aadbefd
Charles Assunção <junioassuncaocharles@gmail.com>
2019-09-09 01:28:54 +0200

    feat(gatsby-cli): Welcome message

---
+++ packages/gatsby-cli/scripts/welcome.js
@@ -0,0 +1,7 @@
+const chalk = require(`chalk`);
+
+console.clear();
+
+console.log(chalk.green(`Success!
+Welcome to the Gatsby CLI! Please visit https://www.gatsbyjs.org/docs/gatsby-cli/ for more information.
+`));
