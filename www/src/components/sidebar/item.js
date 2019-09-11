b609de9735bdf136f71ba3119351ad91e08be366
Kevin Miller <kevin@csumb.edu>
2019-09-11 12:07:31 -0300

    Changed item component to function for useEffect

--- www/src/components/sidebar/item.js
+++ www/src/components/sidebar/item.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react"
+import React, { Fragment, useEffect, useState, useRef } from "react"
 
 import Accordion from "./accordion"
 import createLink from "../../utils/sidebar/create-link"
@@ -13,76 +13,75 @@ const isItemActive = (activeItemParents, item) => {
   return false
 }
 
-class Item extends React.PureComponent {
-  constructor(props, context) {
-    super(props, context)
-    this.activeItemRef = React.createRef()
-  }
+const Item = props => {
+  const activeItemRef = useRef(false)
+  const [didScroll, setDidScroll] = useState(false)
 
-  componentDidMount() {
-    if (this.activeItemRef.current) {
-      this.activeItemRef.current.scrollIntoView({
+  useEffect(() => {
+    if (!didScroll && activeItemRef.current) {
+      activeItemRef.current.scrollIntoView({
         block: `center`,
       })
+      setDidScroll(true)
     }
-  }
+  })
 
-  render() {
-    const {
-      activeItemLink,
-      activeItemParents,
-      isActive,
-      openSectionHash,
-      item,
-      location,
-      onLinkClick,
-      onSectionTitleClick,
-      ui,
-      isSingle,
-      disableAccordions,
-    } = this.props
+  const {
+    activeItemLink,
+    activeItemParents,
+    isActive,
+    openSectionHash,
+    item,
+    location,
+    onLinkClick,
+    onSectionTitleClick,
+    ui,
+    isSingle,
+    disableAccordions,
+  } = props
 
-    const isParentOfActiveItem = isItemActive(activeItemParents, item)
+  const isParentOfActiveItem = isItemActive(activeItemParents, item)
 
-    return (
-      <Fragment>
-        {item.items ? (
-          <Accordion
-            activeItemLink={activeItemLink}
-            activeItemParents={activeItemParents}
-            createLink={createLink}
-            isActive={
-              isActive ||
-              item.link === location.pathname ||
-              isParentOfActiveItem ||
-              item.disableAccordions
-            }
-            isParentOfActiveItem={isParentOfActiveItem}
-            item={item}
-            location={location}
-            onLinkClick={onLinkClick}
-            openSectionHash={openSectionHash}
-            onSectionTitleClick={onSectionTitleClick}
-            isSingle={isSingle}
-            disableAccordions={disableAccordions}
-          />
-        ) : (
-          <li
-            ref={item.link === location.pathname ? this.activeItemRef : false}
-          >
-            {createLink({
-              isActive: item.link === activeItemLink.link,
-              item,
-              location,
-              onLinkClick,
-              ui,
-              level: item.level,
-            })}
-          </li>
-        )}
-      </Fragment>
-    )
-  }
+  return (
+    <Fragment>
+      {item.items ? (
+        <Accordion
+          activeItemLink={activeItemLink}
+          activeItemParents={activeItemParents}
+          createLink={createLink}
+          isActive={
+            isActive ||
+            item.link === location.pathname ||
+            isParentOfActiveItem ||
+            item.disableAccordions
+          }
+          isParentOfActiveItem={isParentOfActiveItem}
+          item={item}
+          location={location}
+          onLinkClick={onLinkClick}
+          openSectionHash={openSectionHash}
+          onSectionTitleClick={onSectionTitleClick}
+          isSingle={isSingle}
+          disableAccordions={disableAccordions}
+        />
+      ) : (
+        <li
+          ref={
+            item.link === location.pathname ? activeItemRef : { current: false }
+          }
+        >
+          {createLink({
+            isActive: item.link === activeItemLink.link,
+            item,
+            location,
+            onLinkClick,
+            ui,
+            level: item.level,
+          })}
+        </li>
+      )}
+    </Fragment>
+  )
 }
 
 export default Item
