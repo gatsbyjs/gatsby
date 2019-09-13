ccab80b3852a2de500fac881e7db307963158464
Vishwa Mehta <32997409+mehtavishwa30@users.noreply.github.com>
2019-09-13 22:26:29 +0530

    Restore comments

--- examples/using-wordpress/gatsby-config.js
+++ examples/using-wordpress/gatsby-config.js
@@ -7,6 +7,10 @@ module.exports = {
     {
       resolve: `gatsby-source-wordpress`,
       options: {
+        /*
+         * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
+         * Example : 'dev-gatbsyjswp.pantheonsite.io' or 'www.example-site.com'	
+         */
         baseUrl: `example.wordpress.com`,
         protocol: `https`,
         hostingWPCOM: false,
