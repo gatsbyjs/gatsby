6bf620110f66fc4a9f9a982c7c38f32d166316b2
Kevin Miller <keveemiller@gmail.com>
2019-09-17 08:23:58 -0300

    Semantic example
    Co-Authored-By: Marcy Sutton <marcy@gatsbyjs.com>

--- examples/recipe-sourcing-contentful/gatsby-config.js
+++ examples/recipe-sourcing-contentful/gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Gatsby Contentful example`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
