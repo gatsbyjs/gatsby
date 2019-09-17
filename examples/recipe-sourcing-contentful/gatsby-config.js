3fcf567d9566f63d2419875ef6bbe56f8a099f07
Kevin Miller <kevin@csumb.edu>
2019-09-17 08:38:41 -0300

    Removed unneeded files from example, news in index

--- examples/recipe-sourcing-contentful/gatsby-config.js
+++ examples/recipe-sourcing-contentful/gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Gatsby Contentful Recipe`,
+    description: `Example Gatsby site sourcing data from Contentful.`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -12,30 +12,5 @@ module.exports = {
         accessToken: `nCFHCSL-z72KyOzRKIPSVpoyRClSugzKsNndVjXk8_A`,
       },
     },
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
--- examples/recipe-sourcing-contentful/src/pages/index.js
+++ examples/recipe-sourcing-contentful/src/pages/index.js
@@ -1,22 +1,39 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <Link to="/news">View example news page</Link>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Contentful data source example</h1>
+    <ul>
+      {data.allContentfulNews.edges.map(({ node }, index) => (
+        <li key={index}>
+          <h2>{node.title}</h2>
+          <p>
+            <strong>{node.createdAt}</strong>
+            {` — `}
+            {node.body.body}
+          </p>
+        </li>
+      ))}
+    </ul>
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  {
+    allContentfulNews(sort: { fields: [updatedAt] }) {
+      edges {
+        node {
+          title
+          body {
+            body
+          }
+          createdAt(formatString: "MMMM DD YYYY")
+        }
+      }
+    }
+  }
+`
