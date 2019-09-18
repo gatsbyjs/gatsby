003d82bcc87fa753fe101d84457c96b42f1f68af
Kevin Miller <kevin@csumb.edu>
2019-09-17 22:09:02 -0300

    Use page context

--- examples/recipe-sourcing-contentful/src/templates/blog-post.js
+++ examples/recipe-sourcing-contentful/src/templates/blog-post.js
@@ -2,19 +2,17 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 
-const BlogPostTemplate = ({ pageContext }) => {
-  return (
-    <Layout>
-      <h1>{pageContext.title}</h1>
-      <div
-        dangerouslySetInnerHTML={{
-          __html:
-            pageContext.childContentfulBlogPostBodyTextNode.childMarkdownRemark
-              .html,
-        }}
-      />
-    </Layout>
-  )
-}
+const BlogPostTemplate = ({ pageContext }) => (
+  <Layout>
+    <h1>{pageContext.title}</h1>
+    <div
+      dangerouslySetInnerHTML={{
+        __html:
+          pageContext.childContentfulBlogPostBodyTextNode.childMarkdownRemark
+            .html,
+      }}
+    />
+  </Layout>
+)
 
 export default BlogPostTemplate
