f7959e9c8927739a0b7f640b2f06ae780a8dc44f
Matthew Miller <mnmiller1@me.com>
2019-09-18 13:41:44 +1000

    Revert "Remove the redundant undefined check"
    This reverts commit 901d462f41e4d2e2493d8617420bc6a7193057ab.

--- packages/gatsby/src/query/index.js
+++ packages/gatsby/src/query/index.js
@@ -72,7 +72,11 @@ const popNodeQueries = state => {
 
     // Find components that depend on this node so are now dirty.
     if (state.componentDataDependencies.nodes.has(node.id)) {
-      state.componentDataDependencies.nodes.get(node.id).forEach(dirtyIds)
+      state.componentDataDependencies.nodes.get(node.id).forEach(n => {
+        if (n) {
+          dirtyIds.add(n)
+        }
+      })
     }
 
     // Find connections that depend on this node so are now invalid.
