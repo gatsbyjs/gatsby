1dcad51e38c931ad3c1ff9a0a98d80d1a96dc96f
Ward Peeters <ward@coding-tech.com>
2019-09-24 10:05:01 +0200

    add isGlobally install check to postinstall

--- packages/gatsby-cli/scripts/postinstall.js
+++ packages/gatsby-cli/scripts/postinstall.js
@@ -1,16 +1,19 @@
-const chalk = require('chalk');
+const chalk = require(`chalk`);
 
 const showSuccessMessage = () => {
-    console.log(chalk.green('Success!\n'));
-    console.log(chalk.cyan('Welcome to the Gatsby CLI! Please visit ') + 
-        chalk.bgCyan('https://www.gatsbyjs.org/docs/gatsby-cli/') + 
-        chalk.cyan(' for more information.\n'));
+  console.log(chalk.green(`Success!\n`));
+  console.log(chalk.cyan(`Welcome to the Gatsby CLI! Please visit `) +
+    chalk.bgCyan(`https://www.gatsbyjs.org/docs/gatsby-cli/`) +
+    chalk.cyan(` for more information.\n`));
 }
 
-try{
-    const createCli = require('../lib/create-cli');
+try {
+  // check if it's a global installation of gatsby-cli
+  const npmArgs = JSON.parse(process.env[`npm_config_argv`]);
+  if (npmArgs.cooked && npmArgs.cooked.includes(`--global`)) {
+    const createCli = require(`../lib/create-cli`);
     showSuccessMessage();
-    createCli('--help');
-}catch(e){
-    showSuccessMessage();
+    createCli(`--help`);
+  }
+} catch (e) {
 }
