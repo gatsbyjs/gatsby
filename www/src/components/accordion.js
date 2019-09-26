0c05500672e892c0cd38ef90e52cd4c66b31a12f
Jae Bach Hardie <j@limina.net>
2019-09-26 22:56:48 +0100

    added some explanation/documentation to the accordion component

--- www/src/components/accordion.js
+++ www/src/components/accordion.js
@@ -4,11 +4,23 @@ import ChevronSvg from "./sidebar/chevron-svg"
 
 const AccordionContext = React.createContext()
 
+/**
+ * Makes it so only one child Details component can be open at any given time.
+ * Use it to group multiple Details/Summary blocks that are mutually exclusive,
+ * like different options for how to do the same thing or the same instructions
+ * for different operating systems.
+ */
 export const Accordion = ({ children }) => {
   const [childState, setChildState] = useState([])
+  // A ref is used to keep track of what ids have already been assigned
+  // because state update batching means that multiple calls to registerChild
+  // might see the same childState
   const lastRegisteredId = useRef(0)
   const accordionContextValue = useMemo(() => {
     return {
+      // Called by a child to add itself to the shared state array
+      // It gets passed a ref so that the child has a way to store the id
+      // it will need to retrieve the state associated with it.
       registerChild: ref => {
         ref.current = ++lastRegisteredId.current
         setChildState(previousState => {
@@ -17,7 +29,11 @@ export const Accordion = ({ children }) => {
           return newState
         })
       },
+      // Called by a child to retrieve its state from context.
       getExpandedState: id => !!childState[id],
+      // Called by a child to toggle its open/closed state. Closes all registered
+      // children other than the one calling it, thus implementing the desired
+      // "accordion" behaviour.
       toggleExpandedState: id =>
         setChildState(previousState =>
           previousState.map((isOpen, index) => {
@@ -38,8 +54,18 @@ export const Accordion = ({ children }) => {
   )
 }
 
+/**
+ * Registers and fetches open/closed state with the AccordionContext.
+ * This is extracted into a hook to limit the scope of the slightly convoluted
+ * ref handoff process. Before initialization it returns null for both state
+ * and callback which means the Details/Summary components fall back to their
+ * default browser behaviour if React isn't loaded.
+ */
 const useAccordionState = () => {
   const accordionContext = useContext(AccordionContext)
+  // The id is saved in a ref to avoid unnecessary re-renders. Modifying
+  // context will already re-run this hook, we don't need it to run more
+  // times than that.
   const id = useRef()
   useEffect(() => {
     if (accordionContext && !id.current) {
@@ -63,6 +89,10 @@ const DetailsContext = React.createContext({
   toggle: () => void 0,
 })
 
+/**
+ * Hides all children except a Summary until it is interacted with. Useful
+ * for decluttering a page by hiding optional information.
+ */
 export const Details = props => {
   const [open, toggle] = useAccordionState()
   const contextValue = useMemo(() => {
@@ -79,6 +109,11 @@ export const Details = props => {
 
 const bulletSize = 8
 
+/**
+ * Displays a title for a Details block when rendered as a child of it.
+ * Having a heading inside a Summary is both legal and encouraged to give
+ * the document structure.
+ */
 export const Summary = ({ children, ...props }) => {
   const { toggle } = useContext(DetailsContext)
   return (
