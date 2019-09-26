fdd57d30c8b27f9b6c8abf1e151ac8eb8ede1894
Matthias Gemperli <hello@matthiasgemperli.ch>
2019-09-26 23:09:16 +0200

    fix(gatsby-source-contentful): Take space id into account during node creation (#17923)
    * Take space id into account when creating nodes and assets
    
    * Refactor get space request
    
    * Fix tests

--- packages/gatsby-source-contentful/src/fetch.js
+++ packages/gatsby-source-contentful/src/fetch.js
@@ -24,10 +24,12 @@ module.exports = async ({ syncToken, reporter, pluginConfig }) => {
   // {'locale': value} } so we need to get the space and its default local.
   //
   // We'll extend this soon to support multiple locales.
+  let space
   let locales
   let defaultLocale = `en-US`
   try {
     console.log(`Fetching default locale`)
+    space = await client.getSpace()
     locales = await client.getLocales().then(response => response.items)
     defaultLocale = _.find(locales, { default: true }).code
     locales = locales.filter(pluginConfig.get(`localeFilter`))
@@ -119,6 +121,7 @@ ${formatPluginOptionsForCLI(pluginConfig.getOriginalPluginOptions(), errors)}`)
     contentTypeItems,
     defaultLocale,
     locales,
+    space,
   }
 
   return result
