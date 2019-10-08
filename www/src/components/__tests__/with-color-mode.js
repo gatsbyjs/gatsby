7e533d587e25703d52f356a873d2eadd369398f3
Thomas Wang <iothomaswang@gmail.com>
2019-10-08 16:11:57 -0700

    add jest official workaround mock

---
+++ www/src/components/__tests__/with-color-mode.js
@@ -0,0 +1,9 @@
+window.matchMedia = jest.fn().mockImplementation(query => {
+  return {
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+  }
+})
