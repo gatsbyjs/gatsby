dd8dfeaf8b78f8eaa86506480a7b117977c77723
Thomas Wang <iothomaswang@gmail.com>
2019-10-08 15:48:49 -0700

    implement prefers-color-scheme: dark

--- www/src/components/with-color-mode.js
+++ www/src/components/with-color-mode.js
@@ -2,7 +2,11 @@ import { useColorMode } from "theme-ui"
 import React from "react"
 
 const withColorMode = Component => props => {
-  const colorMode = useColorMode()
+  const [colorMode, setColorMode] = useColorMode()
+  const prefersDark =
+    window && window.matchMedia(`(prefers-color-scheme: dark)`).matches
+
+  setColorMode(prefersDark ? `dark` : `light`)
 
   return <Component colorMode={colorMode} {...props} />
 }
