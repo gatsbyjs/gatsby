0fdda938c6b22e39a48d499f9e70ae104bdbb9fb
Florian Kissling <21834+fk@users.noreply.github.com>
2019-10-08 17:31:28 +0200

    feat(www): Add gatsby-plugin-theme-ui, dark theme MVP (#18027)
    * First cut (not the deepest)
    
    * Second cut 🔪
    
    * Missed .token fix when merging, format
    
    * Moar ✂️
    
    * 😳🗡
    
    * (@￣ρ￣@)
    
    * Bump gatsby, -image, -plugin-mdx
    
    * Fix a couple things
    
    * merge aftercare
    
    * MOAR
    
    * Well well
    
    * As if I knew
    
    * merge aftercare
    
    * Minor stuff
    
    * Fix merge d'oh, reduce logo size from 36 to 24px
    
    * Format 🤷‍♂️
    
    * Missed pragma, basic fixes for layer model tabs UI
    
    * Undo blog post changes
    
    * Mv logo-inverted
    
    * Lint
    
    * Lint moar
    
    * Lint d'oh
    
    * Moar d'oh
    
    * And another one
    
    * 🔪
    
    * ✂️
    
    * 🗡
    
    * 🥳
    
    * 🎉
    
    * 🔨
    
    * Fix tests
    
    @DSchau would appreciate a look 🙏
    
    * Fix hex2rgba choking on theme-ui's CSS var
    
    * One more time
    
    * No need to opt-in to `useCustomProperties` anymore
    
    * Fix homepage logo banner colors for dark mode
    
    * 🧹
    
    * Tidy avatar component, use it in `template-contributor-page`
    
    * Fix gatsby-design-tokens / theme-ui's colors.text conflict
    
    text.header -> heading
    text.primary -> text
    text.secondary -> textMuted
    
    * Pro forma, currently unused: adjust `colors.muted`
    
    * fonts.header -> fonts.heading; minor fixes
    
    * text.primary -> text
    
    * Fix diagram item heading color
    
    * Newsletter tokens, dark mode fixes
    
    * A few fixes for `/features`
    
    * theme-ui fontWeights
    
    * Rm `utils/guidelines/theme`
    
    * Rm unused import
    
    * D'oh
    
    * Meh
    
    * Couple homepage fixes for dark mode
    
    * Fix homepage section icons
    
    * Moar MVP fixes
    
    * Consolidate 3️⃣ skip-nav-links + styles in component
    
    * Fix share-menu, ditch its themes
    
    * MdLaunch instead of FaExtLink icon for Starter details deps
    
    * Fix "All Starters" link color
    
    * Cosmetics
    
    * Mixed bag
    
    - generalize `themedInput` styles, nudge `:focus` state UI
    - fix and improve a couple things irt `email-capture-form`
    - first bits irt `feedback-form` and dark theme
    -
    
    * 💤
    
    * 🙄
    
    * 🧹
    
    * 🧹
    
    * (mobile nav) icons x dark theme
    
    - remove linear gradients from icons, remove assets/icons/svg-defs
    - add icon color definitions to theme, add dark variant
    - realign icons
      - bump stroke width to 1.5
      - fix some weird shapes, remove some
      - adjusted color scheme / states
    - bump mobile nav item font size
    
    * Fix "Featured Sites" gradient overlay
    
    … not sure why this isn't a `mask`. I'm too old.
    
    * 🧹
    
    * 🙊
    
    * 🛍 `button` and moar mixed bag
    
    - give theme-ui variants a shot, use it for button _sizes_ 🙄 (tiny, small, large)
    - add theme-ui color variables for buttons (sloppy)
    - customCSS -> overrideCSS for consistency
    - no orange for the "Visit demo" button on starter detail pages
    - fix some "share menu" weirdness
    - use `<Button>` for showcase and starter detail "Visit site" and "Visit demo" links
    - remove dead code in `showcase-details`, remove related icon `showcase-feather.svg`
    - use starter `screenshot` component for both starters and showcase
    - follow theme-ui.com/recipes/gatsby-link irt handling active state for main navigation items
    - improve showcase and starter detail layout
    
    * Starters v2 badge x dark theme, cosmetics
    
    * 🍤
    
    * showcase/starter sidebar filters
    
    * Rm `tiny` button variant
    
    * Mobile nav final rinse
    
    * MVP fixes for `guidelines/logo`
    
    * 🙄
    
    * Starter list once more 😴
    
    * 🦖
    
    * 🏴‍☠️
    
    * Hack docsearch 🥳
    
    * 🙈
    
    * Fix secondary button background for dark theme
    
    * Align dark mode toggle button with "social nav icons"
    
    Thanks again @johnletey for pointing this out! 🤗
    
    This also fixes the `MoonMask" animation, brings back the default `:focus` outline, and roughly compensates for the additional whitespace of this "icon button" in relation to its "social icon" siblings.
    
    * 🧹

--- www/src/components/homepage/homepage-logo-banner.js
+++ www/src/components/homepage/homepage-logo-banner.js
@@ -1,36 +1,37 @@
-import React from "react"
+/** @jsx jsx */
+import { jsx } from "theme-ui"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 import styled from "@emotion/styled"
+import { mediaQueries } from "gatsby-design-tokens"
 
-import { colors, space, mediaQueries } from "../../utils/presets"
 import { Name } from "./homepage-section"
 
 const Section = styled(`section`)`
+  border-bottom: 1px solid ${p => p.theme.colors.ui.border};
   overflow: hidden;
-  padding: ${space[5]} 0;
+  padding: ${p => p.theme.space[5]} 0;
   width: 100%;
-  border-bottom: 1px solid ${colors.ui.border.subtle};
 
   ${mediaQueries.xl} {
-    margin: -1px 0;
-    padding: ${space[5]} 0;
+    margin-top: -1px;
+    margin-bottom: -1px;
   }
 
   ${mediaQueries.xxl} {
-    padding: ${space[7]} 0;
+    padding: ${p => p.theme.space[7]} 0;
   }
 `
 
 const Title = styled(`header`)`
-  padding-right: ${space[6]};
-  padding-left: ${space[6]};
+  padding-right: ${p => p.theme.space[6]};
+  padding-left: ${p => p.theme.space[6]};
   ${mediaQueries.md} {
     max-width: 30rem;
   }
 
   ${mediaQueries.lg} {
-    margin-left: ${space[9]};
+    margin-left: ${p => p.theme.space[9]};
   }
 
   ${mediaQueries.xl} {
@@ -49,13 +50,13 @@ const LogoGroup = styled(`div`)`
   display: grid;
   grid-auto-flow: column;
   grid-auto-columns: auto;
-  grid-gap: ${space[8]};
+  grid-gap: ${p => p.theme.space[8]};
   align-items: center;
   overflow-x: scroll;
-  padding-left: ${space[3]};
-  padding-bottom: ${space[1]};
+  padding-left: ${p => p.theme.space[3]};
+  padding-bottom: ${p => p.theme.space[4]};
   ${mediaQueries.xxl} {
-    padding-bottom: ${space[3]};
+    padding-bottom: ${p => p.theme.space[6]};
   }
   &::-webkit-scrollbar {
     display: none;
@@ -76,7 +77,7 @@ const HomepageLogoBanner = () => {
           node {
             base
             childImageSharp {
-              fixed(quality: 75, height: 36) {
+              fixed(quality: 75, height: 24) {
                 ...GatsbyImageSharpFixed_tracedSVG
               }
             }
@@ -97,7 +98,6 @@ const HomepageLogoBanner = () => {
             alt={`${image.base.split(`.`)[0]}`}
             fixed={image.childImageSharp.fixed}
             key={image.base}
-            style={{ opacity: 0.5 }}
           />
         ))}
       </LogoGroup>
--- www/src/components/navigation.js
+++ www/src/components/navigation.js
@@ -1,80 +1,95 @@
-import React from "react"
+/** @jsx jsx */
+import { jsx } from "theme-ui"
+import { useColorMode } from "theme-ui"
 import { Link } from "gatsby"
 import GithubIcon from "react-icons/lib/go/mark-github"
 import TwitterIcon from "react-icons/lib/fa/twitter"
+
 import SearchForm from "../components/search-form"
 import DiscordIcon from "../components/discord"
 import logo from "../assets/logo.svg"
-import { rhythm } from "../utils/typography"
-import {
-  colors,
-  space,
-  fontSizes,
-  transition,
-  mediaQueries,
-  sizes,
-  fonts,
-  zIndices,
-} from "../utils/presets"
+import logoInverted from "../assets/logo-inverted.svg"
+import { mediaQueries } from "../gatsby-plugin-theme-ui"
 import { breakpointGutter } from "../utils/styles"
+import DarkModeToggle from "../components/dark-mode-toggle"
 
 // what we need to nudge down the navItems to sit
 // on the baseline of the logo's wordmark
 const navItemTopOffset = `0.4rem`
-const navItemHorizontalSpacing = space[2]
+// theme-ui values
+const navItemHorizontalSpacing = 2
 
-const iconColor = colors.lilac
-
-const assignActiveStyles = ({ isPartiallyCurrent }) =>
-  isPartiallyCurrent ? { style: styles.navItem.active } : {}
+const navItemStyles = {
+  borderBottom: `2px solid transparent`,
+  color: `navigation.linkDefault`,
+  display: `block`,
+  fontSize: 3,
+  lineHeight: t => `calc(${t.sizes.headerHeight} - ${navItemTopOffset})`,
+  position: `relative`,
+  textDecoration: `none`,
+  zIndex: 1,
+  "&:hover, &:focus": { color: `navigation.linkHover` },
+}
 
 const NavItem = ({ linkTo, children }) => (
-  <li css={styles.li}>
-    <Link to={linkTo} getProps={assignActiveStyles} css={styles.navItem}>
+  <li
+    sx={{
+      display: `block`,
+      m: 0,
+      mx: navItemHorizontalSpacing,
+    }}
+  >
+    <Link
+      to={linkTo}
+      activeClassName="active"
+      sx={{
+        ...navItemStyles,
+        "&.active": {
+          borderBottomColor: `lilac`,
+          color: `lilac`,
+        },
+      }}
+    >
       {children}
     </Link>
   </li>
 )
 
+const SocialNavItem = ({ href, title, children }) => (
+  <a
+    href={href}
+    title={title}
+    sx={{
+      ...navItemStyles,
+      color: `navigation.socialLink`,
+      px: navItemHorizontalSpacing,
+    }}
+  >
+    {children}
+  </a>
+)
+
 const Navigation = ({ pathname }) => {
+  const [colorMode] = useColorMode()
   const isHomepage = pathname === `/`
-  const isBlog = pathname === `/blog/` || pathname.indexOf(`/blog/page/`) === 0
-
-  const socialIconsStyles = {
-    ...styles.navItem,
-    ...styles.socialIconItem,
-  }
-
-  const SocialNavItem = ({ href, title, children, overrideCSS }) => (
-    <a
-      href={href}
-      title={title}
-      css={{
-        ...socialIconsStyles,
-        fontSize: fontSizes[2],
-        ...overrideCSS,
-      }}
-    >
-      {children}
-    </a>
-  )
 
   return (
     <header
-      css={{
-        backgroundColor: `rgba(255,255,255,0.975)`,
-        position: `relative`,
-        height: sizes.headerHeight,
+      sx={{
+        bg: `navigation.background`,
+        height: `headerHeight`,
         left: 0,
+        px: `env(safe-area-inset-left)`,
+        position: `relative`,
         right: 0,
-        top: sizes.bannerHeight,
-        zIndex: zIndices.navigation,
+        top: t => t.sizes.bannerHeight,
+        zIndex: `navigation`,
         // use this to test if the header items are properly aligned to the logo
         // wordmark
         // "&:before": {
         //   content: `''`,
         //   position: `absolute`,
-        //   bottom: `1.25rem`,
+        //   bottom: `1.5rem`,
         //   left: 0,
         //   right: 0,
         //   width: `100%`,
@@ -83,36 +98,50 @@ const Navigation = ({ pathname }) => {
         //   background: `red`,
         // },
         [breakpointGutter]: {
-          backgroundColor: isBlog ? colors.ui.background : false,
-          position: isHomepage || isBlog ? `absolute` : `fixed`,
+          position: isHomepage ? `absolute` : `fixed`,
         },
-        paddingLeft: `env(safe-area-inset-left)`,
-        paddingRight: `env(safe-area-inset-right)`,
       }}
     >
       <div
-        css={{
-          ...styles.containerInner,
+        sx={{
+          alignItems: `center`,
+          display: `flex`,
+          fontFamily: `header`,
+          height: `100%`,
+          margin: `0 auto`,
+          px: 6,
+          position: `relative`,
+          width: `100%`,
           "&:after": {
-            content: `''`,
-            position: `absolute`,
+            bg: isHomepage ? `transparent` : `ui.border`,
             bottom: 0,
-            left: 0,
-            right: 0,
+            content: `''`,
             height: 1,
+            left: 0,
+            position: `absolute`,
+            right: 0,
             zIndex: -1,
-            background: isHomepage ? `transparent` : colors.ui.border.subtle,
           },
         }}
       >
         <Link
           to="/"
-          css={styles.logoLink}
+          sx={{
+            alignItems: `center`,
+            color: `inherit`,
+            display: `flex`,
+            flexShrink: 0,
+            mr: 3,
+            textDecoration: `none`,
+          }}
           aria-label="Gatsby, Back to homepage"
         >
           <img
-            src={logo}
-            css={styles.logo}
+            src={colorMode === `light` ? logo : logoInverted}
+            sx={{
+              height: `logo`,
+              m: 0,
+            }}
             alt="Gatsby Logo"
             aria-hidden="true"
           />
@@ -120,9 +149,35 @@ const Navigation = ({ pathname }) => {
         <nav
           className="navigation"
           aria-label="Primary Navigation"
-          css={styles.navContainer}
+          sx={{
+            display: `none`,
+            [mediaQueries.md]: {
+              alignSelf: `flex-end`,
+              display: `flex`,
+              flexGrow: 1,
+              flexShrink: 1,
+              m: 0,
+              minWidth: 0,
+              mr: `auto`,
+            },
+          }}
         >
-          <ul css={styles.ulContainer}>
+          <ul
+            sx={{
+              display: `none`,
+              [mediaQueries.md]: {
+                alignSelf: `flex-end`,
+                display: `flex`,
+                listStyle: `none`,
+                m: 0,
+                maskImage: t =>
+                  `linear-gradient(to right, transparent, white ${
+                    t.space[1]
+                  }, white 98%, transparent)`,
+                overflowX: `auto`,
+              },
+            }}
+          >
             <NavItem linkTo="/docs/">Docs</NavItem>
             <NavItem linkTo="/tutorial/">Tutorials</NavItem>
             <NavItem linkTo="/plugins/">Plugins</NavItem>
@@ -130,121 +185,57 @@ const Navigation = ({ pathname }) => {
             <NavItem linkTo="/blog/">Blog</NavItem>
             <NavItem linkTo="/showcase/">Showcase</NavItem>
             <NavItem linkTo="/contributing/">Contributing</NavItem>
+            {/* <NavItem linkTo="/starters/">Starters</NavItem> */}
           </ul>
         </nav>
-        <div css={styles.searchAndSocialContainer}>
-          <SearchForm
-            key="SearchForm"
-            iconColor={iconColor}
-            offsetVertical="-0.2175rem"
-          />
+        <SearchForm key="SearchForm" offsetVertical={navItemTopOffset} />
+        <div
+          sx={{
+            alignSelf: `flex-end`,
+            display: `flex`,
+          }}
+        >
           <SocialNavItem
             href="https://github.com/gatsbyjs/gatsby"
             title="GitHub"
           >
-            <GithubIcon style={{ verticalAlign: `text-top` }} />
+            <GithubIcon style={{ verticalAlign: `middle` }} />
           </SocialNavItem>
           <div
-            css={{
+            sx={{
               display: `none`,
-              [mediaQueries.lg]: { display: `flex` },
+              [mediaQueries.xl]: { display: `flex` },
             }}
           >
-            <SocialNavItem href="https://gatsby.dev/discord" title="Discord">
-              <DiscordIcon overrideCSS={{ verticalAlign: `text-top` }} />
-            </SocialNavItem>
             <SocialNavItem href="https://twitter.com/gatsbyjs" title="Twitter">
-              <TwitterIcon style={{ verticalAlign: `text-top` }} />
+              <TwitterIcon style={{ verticalAlign: `middle` }} />
             </SocialNavItem>
+            <SocialNavItem href="https://gatsby.dev/discord" title="Discord">
+              <DiscordIcon
+                overrideCSS={{
+                  verticalAlign: `middle`,
+                  position: `relative`,
+                  top: `0.05em`,
+                }}
+              />
+            </SocialNavItem>
+          </div>
+          <div
+            sx={{
+              ...navItemStyles,
+              color: `navigation.socialLink`,
+              ml: 2,
+              "&:hover": {
+                color: `navigation.linkHover`,
+              },
+            }}
+          >
+            <DarkModeToggle />
           </div>
         </div>
       </div>
     </header>
   )
-}
-
-const styles = {
-  li: {
-    display: `block`,
-    margin: 0,
-    marginLeft: navItemHorizontalSpacing,
-    marginRight: navItemHorizontalSpacing,
-  },
-  navContainer: {
-    display: `none`,
-    [mediaQueries.md]: {
-      alignSelf: `flex-end`,
-      display: `flex`,
-      marginLeft: space[6],
-    },
-  },
-  ulContainer: {
-    display: `none`,
-    [mediaQueries.md]: {
-      alignSelf: `flex-end`,
-      display: `flex`,
-      flexGrow: 1,
-      margin: 0,
-      listStyle: `none`,
-      maskImage: `linear-gradient(to right, transparent, white ${rhythm(
-        1 / 8
-      )}, white 98%, transparent)`,
-      overflowX: `auto`,
-    },
-  },
-  containerInner: {
-    margin: `0 auto`,
-    paddingLeft: space[6],
-    paddingRight: space[6],
-    fontFamily: fonts.header,
-    display: `flex`,
-    alignItems: `center`,
-    width: `100%`,
-    height: `100%`,
-    position: `relative`,
-  },
-  navItem: {
-    fontSize: fontSizes[3],
-    borderBottom: `2px solid transparent`,
-    color: colors.text.primary,
-    display: `block`,
-    WebkitFontSmoothing: `antialiased`,
-    lineHeight: `calc(${sizes.headerHeight} - ${navItemTopOffset})`,
-    position: `relative`,
-    textDecoration: `none`,
-    top: 0,
-    transition: `color ${transition.speed.default} ${transition.curve.default}`,
-    zIndex: 1,
-    "&:hover": {
-      color: colors.gatsby,
-    },
-    active: {
-      borderBottomColor: colors.lilac,
-      color: colors.lilac,
-    },
-  },
-  socialIconItem: {
-    color: iconColor,
-    paddingLeft: navItemHorizontalSpacing,
-    paddingRight: navItemHorizontalSpacing,
-  },
-  searchAndSocialContainer: {
-    alignSelf: `flex-end`,
-    display: `flex`,
-    marginLeft: `auto`,
-  },
-  logo: {
-    height: space[6],
-    margin: 0,
-  },
-  logoLink: {
-    alignItems: `center`,
-    color: `inherit`,
-    display: `flex`,
-    flexShrink: 0,
-    marginRight: space[3],
-    textDecoration: `none`,
-  },
 }
 
 export default Navigation
