ba84bc5fd415af7836ad82d0bc9411f0bb07de50
Thomas Wang <iothomaswang@gmail.com>
2019-10-08 14:53:23 -0700

    height to minHeight for ecosystem board

--- www/src/components/ecosystem/ecosystem-board.js
+++ www/src/components/ecosystem/ecosystem-board.js
@@ -41,7 +41,7 @@ class EcosystemBoard extends Component {
           [mediaQueries.md]: {
             flexDirection: `row`,
             flexWrap: `wrap`,
-            height: t =>
+            minHeight: t =>
               `calc(100vh - (${t.sizes.bannerHeight} + ${t.sizes.headerHeight} + 1px))`,
             pt: 7,
             px: 4,
