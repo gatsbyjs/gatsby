679660339e5438deac3ba0acb963b52183ba1a81
Thomas Wang <iothomaswang@gmail.com>
2019-10-08 17:04:44 -0700

    fix test

--- www/src/components/__tests__/with-color-mode.js
+++ www/src/components/__tests__/with-color-mode.js
@@ -1,9 +1,13 @@
-window.matchMedia = jest.fn().mockImplementation(query => {
-  return {
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(),
-    removeListener: jest.fn(),
-  }
+describe(`Test`, () => {
+  beforeAll(() => {
+    window.matchMedia = jest.fn().mockImplementation(query => {
+      return {
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(),
+        removeListener: jest.fn(),
+      }
+    })
+  })
 })
