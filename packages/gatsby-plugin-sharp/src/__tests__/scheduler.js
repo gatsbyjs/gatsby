3cba4028704ae86d2f97948421ee49a18b16d591
emilpalsson <emil@emilpalsson.se>
2019-10-09 17:08:54 +0200

    Fix setJobToProcess test

--- packages/gatsby-plugin-sharp/src/__tests__/scheduler.js
+++ packages/gatsby-plugin-sharp/src/__tests__/scheduler.js
@@ -2,6 +2,11 @@ const { setJobToProcess } = require(`../scheduler`)
 
 describe(`setJobToProcess`, () => {
   it(`allows brackets in paths`, () => {
+    let deferred = {}
+    deferred.promise = new Promise((resolve, reject) => {
+      deferred.resolve = resolve
+      deferred.reject = reject
+    })
     const toProcess = {}
     const job = {
       args: {},
@@ -9,7 +14,7 @@ describe(`setJobToProcess`, () => {
       contentDigest: `8675309jenny`,
       outputPath: `myoutputpath/1234/file[new].jpg`,
     }
-    setJobToProcess(toProcess, job)
+    setJobToProcess(toProcess, job, deferred)
     expect(toProcess).toMatchSnapshot()
   })
 })
