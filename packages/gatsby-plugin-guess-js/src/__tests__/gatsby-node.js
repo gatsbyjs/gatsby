edc2dd72cb48cb00bb6f2362d0ec7a913b1f56b1
Ward Peeters <ward@coding-tech.com>
2019-10-10 08:57:01 +0200

    move to preInit to delete pluginOptions which actually works

--- packages/gatsby-plugin-guess-js/src/__tests__/gatsby-node.js
+++ packages/gatsby-plugin-guess-js/src/__tests__/gatsby-node.js
@@ -1,17 +1,42 @@
-import { onPreBootstrap } from "../gatsby-node"
+import { onPreInit, onPreBootstrap } from "../gatsby-node"
+import { GuessPlugin } from "guess-webpack"
 
 jest.mock(`guess-webpack`)
 
 describe(`gatsby-plugin-guess-js`, () => {
-  describe(`onPreBootstrap`, () => {
+  describe(`onPreInit`, () => {
     it(`should delete jwt pluginOptions`, () => {
       const pluginOptions = {
         jwt: `mykeys`,
       }
 
-      onPreBootstrap({}, pluginOptions)
+      onPreInit({}, pluginOptions)
 
       expect(pluginOptions).not.toHaveProperty(`jwt`)
+    })
+  })
+
+  describe(`onPreBootstrap`, () => {
+    it(`should still have a jwt token to be used in jwt`, () => {
+      const pluginOptions = {
+        jwt: `mykeys`,
+        // period: {
+        //   start: `2019-10-09`,
+        //   end: `2019-10-10`,
+        // },
+        GAViewID: `1234`,
+      }
+
+      onPreInit({}, pluginOptions)
+      onPreBootstrap({}, pluginOptions)
+
+      expect(GuessPlugin).toHaveBeenNthCalledWith(
+        1,
+        expect.objectContaining({
+          jwt: `mykeys`,
+          GA: `1234`,
+        })
+      )
     })
   })
 })
