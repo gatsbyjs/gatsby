5540d13f38b206969746e976b8283e90439afa08
Jeremy Albright <jeremy.albright@manaollc.com>
2019-10-15 12:31:10 -1000

    Update program.host to reflect server certificate

--- packages/gatsby/src/commands/develop.ts
+++ packages/gatsby/src/commands/develop.ts
@@ -421,6 +421,7 @@ module.exports = async (program: IProgram): Promise<void> => {
 
     program.ssl = await getSslCert({
       name: sslHost,
+      caFile: program[`ca-file`],
       certFile: program[`cert-file`],
       keyFile: program[`key-file`],
       directory: program.directory,
