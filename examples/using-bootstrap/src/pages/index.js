baf5cae989d87cca9ad9efe646229bdc531e686c
chunza2542 <chunza2542@hotmail.com>
2019-10-19 18:44:35 +0700

    Format Code

--- examples/using-bootstrap/src/pages/index.js
+++ examples/using-bootstrap/src/pages/index.js
@@ -1,16 +1,22 @@
 import React from "react"
-import {Button, Container, Card, Col, Row} from "react-bootstrap";
+import { Button, Container, Card, Col, Row } from "react-bootstrap"
 
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css"
 
 class IndexPage extends React.Component {
   render() {
     return (
       <>
-        <Container style={{margin: "50px auto"}}>
+        <Container style={{ margin: `50px auto` }}>
           <h1>Hello World, Gatsby</h1>
-          <p>The example of using Gatsby with Bootstrap. You can see all components here <a href="https://react-bootstrap.netlify.com" target="_blank">React Bootstrap</a></p>
-          <br/>
+          <p>
+            The example of using Gatsby with Bootstrap. You can see all
+            components here{` `}
+            <a href="https://react-bootstrap.netlify.com" target="_blank">
+              React Bootstrap
+            </a>
+          </p>
+          <br />
 
           <Row>
             <Col>
@@ -18,8 +24,8 @@ class IndexPage extends React.Component {
                 <Card.Body>
                   <Card.Title>Card Title</Card.Title>
                   <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
                   </Card.Text>
                   <Button variant="primary">Go somewhere</Button>
                 </Card.Body>
@@ -30,8 +36,8 @@ class IndexPage extends React.Component {
                 <Card.Body>
                   <Card.Title>Card Title</Card.Title>
                   <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
                   </Card.Text>
                   <Button variant="primary">Go somewhere</Button>
                 </Card.Body>
@@ -42,8 +48,8 @@ class IndexPage extends React.Component {
                 <Card.Body>
                   <Card.Title>Card Title</Card.Title>
                   <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
                   </Card.Text>
                   <Button variant="primary">Go somewhere</Button>
                 </Card.Body>
