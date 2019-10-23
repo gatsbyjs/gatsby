23d7a28b001f46b85a7b65c61284ce23cb27fe85
Ward Peeters <ward@coding-tech.com>
2019-10-23 09:12:20 +0200

    chore: add tests

---
+++ packages/gatsby-plugin-sharp/src/__tests__/worker.js
@@ -0,0 +1,45 @@
+jest.mock(`../process-file`)
+
+const worker = require(`../worker`)
+const { processFile } = require(`../process-file`)
+
+describe(`worker`, () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it(`should call processFile with the right arguments`, async () => {
+    processFile.mockImplementation((file, contentDigest, transforms) =>
+      transforms.map(transform => Promise.resolve(transform))
+    )
+    const job = {
+      inputPath: `inputpath/file.jpg`,
+      contentDigest: `1234`,
+      transforms: [
+        {
+          outputPath: `myoutputpath/1234/file.jpg`,
+          args: {
+            width: 100,
+            height: 100,
+          },
+        },
+      ],
+      pluginOptions: {},
+    }
+
+    await worker(job)
+
+    expect(processFile).toHaveBeenCalledTimes(1)
+    expect(processFile).toHaveBeenCalledWith(
+      job.inputPath,
+      job.contentDigest,
+      job.transforms.map(transform => {
+        return {
+          outputPath: transform.outputPath,
+          args: transform.transforms,
+        }
+      }),
+      job.pluginOptions
+    )
+  })
+})
