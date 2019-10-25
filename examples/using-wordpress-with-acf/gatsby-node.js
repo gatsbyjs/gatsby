1504031c683fd90f202f5ed9b2b3ece27b7b8a51
Vishwa Mehta <32997409+mehtavishwa30@users.noreply.github.com>
2019-10-25 20:59:25 +0530

    docs(recipes): using wordpress example (#19014)
    * Create post.js
    
    * Create package.json
    
    * Create gatsby-node.js
    
    * Create gatsby-config.js
    
    * Create index.js
    
    * Docs: Example for sourcing from Wordpress
    
    * Docs: Example for sourcing from Wordpress
    
    * Docs: Example for sourcing from Wordpress
    
    * Docs: Example for sourcing from Wordpress
    
    * Create page.js
    
    * Docs: Example for sourcing from Wordpress
    
    * Docs: Example for sourcing from Wordpress
    
    * Docs: Example for sourcing from Wordpress
    
    * Fixed closing tag errors
    
    * Fixed closing tag errors
    
    * Fixed closing tag errors
    
    * chore: format
    
    * Update examples/gatsby-sourcing-wordpress/gatsby-config.js
    
    Co-Authored-By: Marcy Sutton <marcy@gatsbyjs.com>
    
    * Update examples/gatsby-sourcing-wordpress/package.json
    
    Co-Authored-By: Marcy Sutton <marcy@gatsbyjs.com>
    
    * Update examples/gatsby-sourcing-wordpress/src/templates/post.js
    
    Co-Authored-By: Marcy Sutton <marcy@gatsbyjs.com>
    
    * Update examples/gatsby-sourcing-wordpress/src/templates/page.js
    
    Co-Authored-By: Marcy Sutton <marcy@gatsbyjs.com>
    
    * Update examples/gatsby-sourcing-wordpress/src/templates/page.js
    
    Co-Authored-By: Marcy Sutton <marcy@gatsbyjs.com>
    
    * Update examples/gatsby-sourcing-wordpress/src/templates/post.js
    
    Co-Authored-By: Marcy Sutton <marcy@gatsbyjs.com>
    
    * chore: format
    
    * Update folder name and readme
    
    * Revert "Update folder name and readme"
    
    This reverts commit 5f7481e7a889d3eef6cf00e1e6fcd7f2d5d349b5.
    
    * rename examples
    
    * Update examples/using-wordpress-with-acf/README.md
    
    Co-Authored-By: Marcy Sutton <marcy@gatsbyjs.com>
    
    * Update examples/using-wordpress-with-acf/README.md
    
    Co-Authored-By: Marcy Sutton <marcy@gatsbyjs.com>
    
    * Update examples/using-wordpress-with-acf/gatsby-config.js
    
    Co-Authored-By: Marcy Sutton <marcy@gatsbyjs.com>
    
    * Update examples/using-wordpress-with-acf/package.json
    
    Co-Authored-By: Marcy Sutton <marcy@gatsbyjs.com>
    
    * Update examples/using-wordpress-with-acf/package.json
    
    Co-Authored-By: Marcy Sutton <marcy@gatsbyjs.com>
    
    * Restore comments
    
    * Restore comments
    
    * chore: format
    
    * fix linting issues

--- examples/using-wordpress-with-acf/gatsby-node.js
+++ examples/using-wordpress-with-acf/gatsby-node.js
@@ -9,11 +9,11 @@ const slash = require(`slash`)
 // Will create pages for WordPress posts (route : /post/{slug})
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
+
   // The “graphql” function allows us to run arbitrary
   // queries against the local Gatsby GraphQL schema. Think of
   // it like the site has a built-in database constructed
   // from the fetched data that you can run queries against.
-
   const result = await graphql(`
     {
       allWordpressPage {
@@ -42,7 +42,7 @@ exports.createPages = async ({ graphql, actions }) => {
 
   // Check for any errors
   if (result.errors) {
-    console.error(result.errors)
+    throw new Error(result.errors)
   }
 
   // Access query results via object destructuring
