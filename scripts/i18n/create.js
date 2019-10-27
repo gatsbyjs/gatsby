6a65a850181c0ce27f4835a1bf0b196ebe17a2a9
Nat Alison <tesseralis@gmail.com>
2019-10-27 12:20:56 -0700

    update create script

--- scripts/i18n/create.js
+++ scripts/i18n/create.js
@@ -133,7 +133,7 @@ Please refer to the [Translation Progress Issue](${transRepo.url}/issues/1) to s
 
 Useful Links:
 
-* [Style Guide](/style-guide)
+* [Style Guide](/style-guide.md)
 * [Gatsby Translation Guide](https://www.gatsbyjs.org/contributing/gatsby-docs-translation-guide/)
 
 (Feel free to translate this document and add any content you feel would be useful to contributors).
