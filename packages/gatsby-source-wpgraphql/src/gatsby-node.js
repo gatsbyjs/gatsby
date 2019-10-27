1c7a927cd3f065bef0c0f7becf7bb8a96420a4c4
Tyler Barnes <tylerdbarnes@gmail.com>
2019-10-27 09:52:47 -0700

    move create content nodes to source nodes dir

--- packages/gatsby-source-wpgraphql/src/gatsby-node.js
+++ packages/gatsby-source-wpgraphql/src/gatsby-node.js
@@ -1,4 +1,4 @@
-const sourceNodes = require(`./gatsby-node/create-content-nodes`)
+const sourceNodes = require(`./gatsby-node/source-nodes`)
 const createPages = require(`./gatsby-node/create-pages`)
 
 module.exports = {
