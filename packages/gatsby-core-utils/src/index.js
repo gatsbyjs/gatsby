d97bee6c50cffafb7e72de0771b026dec2c2b888
Robin Cussol <RobinCsl@users.noreply.github.com>
2019-10-28 17:44:06 +0100

    feat(gatsby-core-utils): Add isCI and getCIName (#19039)
    Fixes https://github.com/gatsbyjs/gatsby/issues/19018

--- packages/gatsby-core-utils/src/index.js
+++ packages/gatsby-core-utils/src/index.js
@@ -3,3 +3,5 @@ exports.joinPath = require(`./path`).joinPath
 exports.isNodeInternalModulePath = require(`./path`).isNodeInternalModulePath
 exports.cpuCoreCount = require(`./cpu-core-count`)
 exports.urlResolve = require(`./url`).resolve
+exports.isCI = require(`./ci`).isCI
+exports.getCIName = require(`./ci`).getCIName
