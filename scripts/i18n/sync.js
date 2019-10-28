1a6cd363d96a17d94374dcfd69cd09c136ab1a92
Nat Alison <tesseralis@gmail.com>
2019-10-28 15:02:29 -0700

    WIP sync script

---
+++ scripts/i18n/sync.js
@@ -0,0 +1,48 @@
+const log4js = require(`log4js`)
+const shell = require(`shelljs`)
+
+let logger = log4js.getLogger(`sync`)
+
+const host = "https://github.com"
+const owner = "gatsbyjs"
+// Repo to be used as basis for translations
+const sourceRepo = "gatsby-i18n-source"
+
+const sourceRepoUrl = `${host}/${owner}/${sourceRepo}.git`
+
+const dirsToCopy = [
+  "docs/docs",
+  "docs/tutorial",
+  "docs/contributing",
+  "docs/accessibility-statement.md",
+]
+
+function cloneOrUpdateRepo(repoName, repoUrl) {
+  if (shell.ls(repoName).code !== 0) {
+    logger.debug(`cloning ${repoName}`)
+    shell.exec(`git clone ${repoUrl}`)
+  } else {
+    // if the repo already exists, pull from it
+    shell.cd(repoName)
+    shell.exec(`git pull`)
+    shell.cd(`..`)
+  }
+}
+
+// Copy over contents of origin repo (gatsby) to source repo (gatsby-source-i18n)
+async function updateSourceRepo() {
+  if (shell.cd(cacheDir).code !== 0) {
+    logger.debug(`creating ${cacheDir}`)
+    shell.mkdir(cacheDir)
+    shell.cd(cacheDir)
+  }
+  cloneOrUpdateRepo(sourceRepo, sourceRepoUrl)
+  // delete old content
+  shell.rm("-rf", `${sourceRepo}/docs/*`)
+  // repopulate content
+  dirsToCopy.forEach(dir => {
+    shell.cp("-r", `../../../${dir}`, `${sourceRepo}/docs`)
+  })
+  // push
+  shell.exec("git push origin master")
+}
