6c327072935625e3586b69b3a842c54355af3150
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2019-10-30 16:43:35 +0530

    Add CircleCI job and check if there are changes before commit

--- scripts/i18n/update-source.js
+++ scripts/i18n/update-source.js
@@ -1,15 +1,19 @@
 const log4js = require(`log4js`)
 const shell = require(`shelljs`)
+const path = require(`path`)
 
 let logger = log4js.getLogger(`update-source`)
 
-const host = `https://github.com`
+const protocol = `https://`
+const host = `github.com`
 const cacheDir = `.cache`
 const owner = `gatsbyjs`
 // Repo to be used as basis for translations
 const sourceRepo = `gatsby-i18n-source`
 
-const sourceRepoUrl = `${host}/${owner}/${sourceRepo}.git`
+const sourceRepoUrl = `${protocol}${process.env.GITHUB_API_TOKEN}@${host}/${owner}/${sourceRepo}.git`
+
+const originRepo = process.env.PWD
 
 const dirsToCopy = [
   `docs/docs`,
@@ -19,36 +23,42 @@ const dirsToCopy = [
 ]
 
 function cloneOrUpdateRepo(repoName, repoUrl) {
+  shell.cd(path.join(originRepo, `..`))
   if (shell.ls(repoName).code !== 0) {
-    logger.debug(`cloning ${repoName}`)
-    shell.exec(`git clone ${repoUrl}`)
+    logger.debug(`Cloning ${repoName}`)
+    shell.exec(`git clone --quiet --depth 1 ${repoUrl} > /dev/null`)
   } else {
     // if the repo already exists, pull from it
     shell.cd(repoName)
     shell.exec(`git pull origin master`)
-    shell.cd(`..`)
   }
 }
 
-// Copy over contents of origin repo (gatsby) to source repo (gatsby-source-i18n)
-// FIXME make sure the main repo is updated before we do this
+// Copy over contents of origin repo (gatsby) to the source repo (gatsby-source-i18n)
 async function updateSourceRepo() {
   if (shell.cd(cacheDir).code !== 0) {
-    logger.debug(`creating ${cacheDir}`)
+    logger.debug(`Creating ${cacheDir}`)
     shell.mkdir(cacheDir)
     shell.cd(cacheDir)
   }
   cloneOrUpdateRepo(sourceRepo, sourceRepoUrl)
   shell.cd(sourceRepo)
-  // delete old content
+  // Delete old content
   shell.rm(`-rf`, `docs/*`)
-  // repopulate content
+  // Repopulate content
   dirsToCopy.forEach(dir => {
-    shell.cp(`-r`, `../../../../${dir}`, `docs`)
+    shell.cp(`-r`, path.join(originRepo, dir), `docs`)
   })
-  shell.exec(`git add *`)
-  shell.exec(`git commit -m 'Update gatsby monorepo'`)
-  // push to source repo
+
+  // Check if there are any changes to commit
+  if (shell.exec(`git status --porcelain`).stdout.length) {
+    shell.exec(`git add .`)
+    if (shell.exec(`git commit -m 'Update from gatsbyjs/gatsby'`).code !== 0) {
+      logger.debug(`Git commit failed`)
+      shell.exit(1)
+    }
+  }
+  // Push to source repo
   shell.exec(`git push origin master`)
 }
 
