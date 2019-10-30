ea496e83ddc9377465f1d40a2adaa86b6b1e0a29
Nat Alison <tesseralis@gmail.com>
2019-10-29 18:40:33 -0700

    WIP sync

--- scripts/i18n/sync.js
+++ scripts/i18n/sync.js
@@ -26,12 +26,13 @@ function cloneOrUpdateRepo(repoName, repoUrl) {
   } else {
     // if the repo already exists, pull from it
     shell.cd(repoName)
-    shell.exec(`git pull`)
+    shell.exec(`git pull origin master`)
     shell.cd(`..`)
   }
 }
 
 // Copy over contents of origin repo (gatsby) to source repo (gatsby-source-i18n)
+// FIXME make sure the main repo is updated before we do this
 async function updateSourceRepo() {
   if (shell.cd(cacheDir).code !== 0) {
     logger.debug(`creating ${cacheDir}`)
@@ -50,7 +51,39 @@ async function updateSourceRepo() {
 }
 
 async function syncTranslationRepo(code) {
-  const transRepoName = `${host}/${owner}/${repoBase}-${code}`
+  const transRepoName = `${repoBase}-${code}`
+  const transRepoUrl = `${host}/${owner}/${transRepoName}`
+  cloneOrUpdateRepo(transRepoName, transRepoUrl)
+  shell.cd(transRepoName)
+
+  // pull from the source
+  shell.exec(`git remote add source ${sourceRepoUrl}`)
+  const output = shell.exec(`git pull source master`).stdout
+  if (output.includes("Already up to date.")) {
+    logger.info(`We are already up to date with source.`)
+    // teardownAndExit()
+    return
+  }
+  const lines = output.split("\n")
+
+  // Commit all merge conflicts
+  const conflictLines = lines.filter(line => line.startsWith("CONFLICT"))
+  const conflictFiles = conflictLines.map(line =>
+    line.substr(line.lastIndexOf(" ") + 1)
+  )
+
+  // FIXME merge all conflicts
+
+  // If no conflicts, merge directly into master
+  if (conflictFiles.length === 0) {
+    logger.info("No conflicts found. Committing directly to master.")
+    shell.exec(`git checkout ${defaultBranch}`)
+    shell.exec(`git merge ${syncBranch}`)
+    shell.exec(`git push origin ${defaultBranch}`)
+    return
+  }
+
+  // FIXME create a PR
 }
 
 updateSourceRepo()
