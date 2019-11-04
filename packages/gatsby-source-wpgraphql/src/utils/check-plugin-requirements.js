5367d149875d38fda86c5476d04cad000797b3c0
Tyler Barnes <tylerdbarnes@gmail.com>
2019-11-04 13:57:45 -0600

    improve error handling to make sure the url returns 200, WPGatsby is installed, and WPGraphQL is installed

--- packages/gatsby-source-wpgraphql/src/utils/check-plugin-requirements.js
+++ packages/gatsby-source-wpgraphql/src/utils/check-plugin-requirements.js
@@ -4,19 +4,18 @@ const isWpGatsby = async (_, pluginOptions) =>
   fetchGraphql({
     url: pluginOptions.url,
     query: `
-      {
-        isWpGatsby
-      }
-    `,
+        {
+          isWpGatsby
+        }
+      `,
   })
 
 const checkPluginRequirements = async (helpers, pluginOptions) => {
   const fetchIsWpGatsby = await isWpGatsby(helpers, pluginOptions)
-  // @todo add separate checks for WPGraphQL and WPGatsby
-  // check headers for JSON
+
   if (!fetchIsWpGatsby.data || !fetchIsWpGatsby.data.isWpGatsby) {
     console.error(
-      `[gatsby-source-wpgraphql] - Couldn't connect to your WordPress site. Make sure your URL is correct and WP-GraphQL and WP-Gatsby are active.`
+      `[gatsby-source-wpgraphql] - Couldn't connect to WPGatsby, please install it :)`
     )
     process.exit()
   }
--- packages/gatsby-source-wpgraphql/src/utils/fetch-graphql.js
+++ packages/gatsby-source-wpgraphql/src/utils/fetch-graphql.js
@@ -1,10 +1,29 @@
 const fetch = require(`isomorphic-fetch`)
 
-const fetchGraphql = async ({ url, query, variables = {} }) =>
-  (await fetch(url, {
+const fetchGraphql = async ({ url, query, variables = {} }) => {
+  const response = await fetch(url, {
     method: `POST`,
     headers: { "Content-Type": `application/json` },
     body: JSON.stringify({ query, variables }),
-  })).json()
+  })
+
+  if (response.status !== 200) {
+    console.error(`[gatsby-source-wpgraphql] Couldn't connect to ${url}`)
+    process.exit()
+  }
+
+  const contentType = response.headers.get(`content-type`)
+
+  if (!contentType.includes(`application/json;`)) {
+    console.error(
+      `[gatsby-source-wpgraphql] Unable to connect to WPGraphQL.
+        Double check that your WordPress URL is correct and WPGraphQL is installed.
+        ${url}`
+    )
+    process.exit()
+  }
+
+  return response.json()
+}
 
 module.exports = fetchGraphql
