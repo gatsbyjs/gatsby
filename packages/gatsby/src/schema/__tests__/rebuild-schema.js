7f50feb85c5cc085f0f71728e705a96727083e8d
Mikhail Novikov <freiksenet@gmail.com>
2019-11-05 15:21:44 +0200

    Added nested extension test

--- packages/gatsby/src/schema/__tests__/rebuild-schema.js
+++ packages/gatsby/src/schema/__tests__/rebuild-schema.js
@@ -700,6 +700,75 @@ describe(`build and update schema for other types`, () => {
     `)
   })
 
+  it(`applies automatic extensions to inferred fields`, async () => {
+    const node = {
+      id: `NewType1`,
+      internal: { type: `NewType`, contentDigest: `0` },
+      date: `2019-09-09`,
+      nested: {
+        date: `2019-09-09`,
+      },
+    }
+    const newSchema = await addNodeAndRebuild(node)
+    const print = typePrinter(newSchema)
+    expect(print(`NewType`)).toMatchInlineSnapshot(`
+      "type NewType implements Node {
+        id: ID!
+        parent: Node
+        children: [Node!]!
+        internal: Internal!
+        date(
+          \\"\\"\\"
+          Format the date using Moment.js' date tokens, e.g. \`date(formatString: \\"YYYY
+          MMMM DD\\")\`. See https://momentjs.com/docs/#/displaying/format/ for
+          documentation for different tokens.
+          \\"\\"\\"
+          formatString: String
+
+          \\"\\"\\"Returns a string generated with Moment.js' \`fromNow\` function\\"\\"\\"
+          fromNow: Boolean
+
+          \\"\\"\\"
+          Returns the difference between this date and the current time. Defaults to
+          \\"milliseconds\\" but you can also pass in as the measurement \\"years\\",
+          \\"months\\", \\"weeks\\", \\"days\\", \\"hours\\", \\"minutes\\", and \\"seconds\\".
+          \\"\\"\\"
+          difference: String
+
+          \\"\\"\\"Configures the locale Moment.js will use to format the date.\\"\\"\\"
+          locale: String
+        ): Date
+        nested: NewTypeNested
+      }"
+    `)
+
+    expect(print(`NewTypeNested`)).toMatchInlineSnapshot(`
+      "type NewTypeNested {
+        date(
+          \\"\\"\\"
+          Format the date using Moment.js' date tokens, e.g. \`date(formatString: \\"YYYY
+          MMMM DD\\")\`. See https://momentjs.com/docs/#/displaying/format/ for
+          documentation for different tokens.
+          \\"\\"\\"
+          formatString: String
+
+          \\"\\"\\"Returns a string generated with Moment.js' \`fromNow\` function\\"\\"\\"
+          fromNow: Boolean
+
+          \\"\\"\\"
+          Returns the difference between this date and the current time. Defaults to
+          \\"milliseconds\\" but you can also pass in as the measurement \\"years\\",
+          \\"months\\", \\"weeks\\", \\"days\\", \\"hours\\", \\"minutes\\", and \\"seconds\\".
+          \\"\\"\\"
+          difference: String
+
+          \\"\\"\\"Configures the locale Moment.js will use to format the date.\\"\\"\\"
+          locale: String
+        ): Date
+      }"
+    `)
+  })
+
   it(`should report error when conflicting changes`, async () => {
     // TODO
   })
