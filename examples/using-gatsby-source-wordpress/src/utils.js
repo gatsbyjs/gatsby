1fca7021c220d00181aec35573daddd33f88447f
Tyler Barnes <tylerdbarnes@gmail.com>
2019-11-04 18:47:44 -0600

    clean up example site

--- examples/using-gatsby-source-wordpress/src/utils.js
+++
@@ -1,7 +0,0 @@
-const dateformat = require(`dateformat`)
-
-exports.makeBlogPath = ({ createdAt, slug }) => {
-  const date = new Date(createdAt)
-  const formattedDate = dateformat(date, `yyyy-mm-dd`)
-  return `/${formattedDate}-${slug}`
-}
