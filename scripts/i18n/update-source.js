059c2f198039b536acb8a9469bc7aa26bcd7c478
Nat Alison <tesseralis@gmail.com>
2019-11-06 13:39:55 -0800

    feat(scripts/i18n): Script to update `gatsby-i18n-source` from the monorepo (#19133)
    * WIP update-source script
    
    * update README with more context
    
    * update README
    
    * Add CircleCI job and check if there are changes before commit (#19141)
    
    * Add CircleCI job and check if there are changes before commit
    
    * Update .circleci/config.yml
    
    * Update config.yml
    
    * add some commit suggestions
    
    * add some commit suggestions
    
    * more fixes
    
    * more fixes
    
    * more changes
    
    * ensure the script works locally
    
    * use better cache dir
    
    * Update scripts/i18n/update-source.js
    
    Co-Authored-By: Michal Piechowiak <misiek.piechowiak@gmail.com>
    
    * factor out the repo branch name

---
+++ scripts/i18n/update-source.js
@@ -0,0 +1,83 @@
+const log4js = require(`log4js`)
+const shell = require(`shelljs`)
+const path = require(`path`)
+
+let logger = log4js.getLogger(`update-source`)
+logger.level = `info`
+
+const protocol = `https://`
+const host = `github.com`
+const cacheDir = path.join(__dirname, `.cache`)
+const owner = `gatsbyjs`
+// Repo to be used as basis for translations
+const sourceRepo = `gatsby-i18n-source`
+
+// const branch = `master`
+const branch = `test-update` // test branch
+
+const sourceRepoUrl = `${protocol}${process.env.GITHUB_API_TOKEN}@${host}/${owner}/${sourceRepo}.git`
+
+const gatsbyMonorepoPath = path.join(__dirname, `..`, `..`)
+
+const pathsToCopy = [
+  `docs/docs`,
+  `docs/tutorial`,
+  `docs/contributing`,
+  `docs/accessibility-statement.md`,
+]
+
+function cloneOrUpdateRepo(repoName, repoUrl) {
+  if (shell.ls(repoName).code !== 0) {
+    logger.debug(`Cloning ${repoName}`)
+    const { code } = shell.exec(
+      `git clone --quiet --depth 1 ${repoUrl} > /dev/null`
+    )
+    // If cloning fails for whatever reason, we need to exit immediately
+    // or we might accidentally push to the monorepo
+    if (code !== 0) {
+      process.exit(1)
+    }
+    shell.cd(repoName)
+    shell.exec(`git checkout ${branch}`)
+  } else {
+    // if the repo already exists, pull from it
+    shell.cd(repoName)
+    shell.exec(`git checkout ${branch}`)
+    shell.exec(`git pull origin ${branch}`)
+  }
+}
+
+// Copy over contents of origin repo (gatsby) to the source repo (gatsby-source-i18n)
+// TODO make sure the main repo is updated before we do this
+async function updateSourceRepo() {
+  if (shell.cd(cacheDir).code !== 0) {
+    logger.debug(`Creating ${cacheDir}`)
+    shell.mkdir(cacheDir)
+    shell.cd(cacheDir)
+  }
+  cloneOrUpdateRepo(sourceRepo, sourceRepoUrl)
+  // Delete old content
+  logger.info(`Repopulating content`)
+  shell.rm(`-rf`, `docs/*`)
+  // Repopulate content from the monorepo
+  pathsToCopy.forEach(p => {
+    shell.cp(`-r`, path.join(gatsbyMonorepoPath, p), `docs`)
+  })
+
+  // exit if there are no changes to commit
+  if (!shell.exec(`git status --porcelain`).stdout.length) {
+    logger.info(`No changes to commit. Exiting...`)
+    process.exit(0)
+  }
+
+  shell.exec(`git add .`)
+  // TODO use the latest hash & commit message as the message here
+  if (shell.exec(`git commit -m 'Update from gatsbyjs/gatsby'`).code !== 0) {
+    logger.error(`Failed to commit to ${sourceRepo}`)
+    process.exit(1)
+  }
+  // Push to source repo
+  shell.exec(`git push origin ${branch}`)
+}
+
+updateSourceRepo()
