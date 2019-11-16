8fbc2a48394e61fe4c0e44210a8af6f63b2d24b9
Jeffrey Paul <jp@eeqj.com>
2019-11-16 01:18:29 -0800

    add comment as requested

--- packages/gatsby-telemetry/src/event-storage.js
+++ packages/gatsby-telemetry/src/event-storage.js
@@ -42,8 +42,14 @@ module.exports = class EventStorage {
     this.store = new Store(baseDir)
     this.verbose = isTruthy(process.env.GATSBY_TELEMETRY_VERBOSE)
     this.debugEvents = isTruthy(process.env.GATSBY_TELEMETRY_DEBUG)
+
+    // disable telemetry reporting if the user has specifically disabled
+    // gatsby telemetry via GATSBY_TELEMETRY_DISABLED or if they have
+    // opted out via standardized DO_NOT_TRACK environment variable per
+    // consoledonottrack.com
     this.disabled = isTruthy(process.env.GATSBY_TELEMETRY_DISABLED) ||
      isTruthy(process.env.DO_NOT_TRACK)
+
   }
 
   isTrackingDisabled() {
