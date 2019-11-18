e7aab0e0ecae1286476b57001cef8f912bfb65bf
Ward Peeters <ward@coding-tech.com>
2019-11-18 15:15:00 +0100

    Fix outputDir check

--- packages/gatsby-plugin-sharp/src/index.js
+++ packages/gatsby-plugin-sharp/src/index.js
@@ -1,5 +1,6 @@
 const sharp = require(`./safe-sharp`)
 
+const { existsSync } = require(`fs`)
 const imageSize = require(`probe-image-size`)
 
 const _ = require(`lodash`)
@@ -39,10 +40,6 @@ let { boundActionCreators } = require(`gatsby/dist/redux/actions`)
 exports.setBoundActionCreators = actions => {
   boundActionCreators = actions
 }
-
-// We set the queue to a Map instead of an array to easily search in onCreateDevServer Api hook
-const queue = new Map()
-exports.queue = queue
 
 function queueImageResizing({ file, args = {}, reporter }) {
   const pluginOptions = getPluginOptions()
@@ -107,17 +104,20 @@ function queueImageResizing({ file, args = {}, reporter }) {
     outputPath: outputFilePath,
   }
 
-  queue.set(prefixedSrc, job)
+  let finishedPromise = Promise.resolve()
 
-  // schedule job immediately - this will be changed when image processing on demand is implemented
-  const finishedPromise = scheduleJob(
-    job,
-    boundActionCreators,
-    pluginOptions,
-    reporter
-  ).then(() => {
-    queue.delete(prefixedSrc)
-  })
+  // Check if the output file already exists so we don't redo work.
+  // TODO: Remove this when jobs api is stable, it will have a better check
+  const outputFile = path.join(job.outputDir, job.outputPath)
+  if (!existsSync(outputFile)) {
+    // schedule job immediately - this will be changed when image processing on demand is implemented
+    finishedPromise = scheduleJob(
+      job,
+      boundActionCreators,
+      pluginOptions,
+      reporter
+    )
+  }
 
   return {
     src: prefixedSrc,
