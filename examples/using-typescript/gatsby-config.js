82ef6e40c9be7536cdeb3f275428e7da57b9120d
LekoArts <lekoarts@gmail.com>
2019-11-20 11:57:08 +0100

    using-typescript: replace tslint with eslint, add example for fc component and useStaticQuery

--- examples/using-typescript/gatsby-config.js
+++ examples/using-typescript/gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
     siteName: `Using TypeScript Example`,
+    exampleUrl: `https://github.com/gatsbyjs/gatsby/tree/master/examples/using-typescript`,
   },
   plugins: [
     `gatsby-plugin-typescript`,
