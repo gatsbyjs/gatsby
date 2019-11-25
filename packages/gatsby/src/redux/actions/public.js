a62ecebc07d4453660adff1b1e3e55ff97925153
Michal Piechowiak <misiek.piechowiak@gmail.com>
2019-11-25 14:03:27 +0100

    fix(gatsby): memoize shadowCreatePagePath to fix performance r… (#19774)

--- packages/gatsby/src/redux/actions/public.js
+++ packages/gatsby/src/redux/actions/public.js
@@ -20,7 +20,16 @@ const { getCommonDir } = require(`../../utils/path`)
 const apiRunnerNode = require(`../../utils/api-runner-node`)
 const { trackCli } = require(`gatsby-telemetry`)
 const { getNonGatsbyCodeFrame } = require(`../../utils/stack-trace-utils`)
-const shadowCreatePagePath = require(`../../internal-plugins/webpack-theme-component-shadowing/create-page`)
+
+/**
+ * Memoize function used to pick shadowed page components to avoid expensive I/O.
+ * Ideally, we should invalidate memoized values if there are any FS operations
+ * on files that are in shadowing chain, but webpack currently doesn't handle
+ * shadowing changes during develop session, so no invalidation is not a deal breaker.
+ */
+const shadowCreatePagePath = _.memoize(
+  require(`../../internal-plugins/webpack-theme-component-shadowing/create-page`)
+)
 
 const actions = {}
 const isWindows = platform() === `win32`
