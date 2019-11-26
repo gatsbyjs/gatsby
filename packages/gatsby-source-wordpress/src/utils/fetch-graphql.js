ef7d83209d339875926e16813ca4007c8fca0684
Tyler Barnes <tylerdbarnes@gmail.com>
2019-11-26 15:38:37 -0800

    add ability to rate limit gql fetching & improve error handling for GRAPHQL_DEBUG mode in WP

--- packages/gatsby-source-wordpress/src/utils/fetch-graphql.js
+++ packages/gatsby-source-wordpress/src/utils/fetch-graphql.js
@@ -1,18 +1,20 @@
-import fetch from "isomorphic-fetch"
+// import { dd } from "dumper.js"
+import axios from "axios"
+import rateLimit from "axios-rate-limit"
+
+const http = rateLimit(axios.create(), {
+  maxRPS: 1,
+})
 
 const fetchGraphql = async ({ url, query, errorMap, variables = {} }) => {
-  const response = await fetch(url, {
-    method: `POST`,
-    headers: { "Content-Type": `application/json` },
-    body: JSON.stringify({ query, variables }),
-  })
+  const response = await http.post(url, { query, variables })
 
   if (response.status !== 200) {
     console.error(`[gatsby-source-wordpress] Couldn't connect to ${url}`)
     process.exit()
   }
 
-  const contentType = response.headers.get(`content-type`)
+  const contentType = response.headers[`content-type`]
 
   if (!contentType.includes(`application/json;`)) {
     console.error(
@@ -23,10 +25,16 @@ const fetchGraphql = async ({ url, query, errorMap, variables = {} }) => {
     process.exit()
   }
 
-  const json = await response.json()
+  const json = response.data
 
   if (json.errors) {
     json.errors.forEach(error => {
+      if (error.debugMessage) {
+        console.error(`[gatsby-source-wordpress] Error category: ${error.category}
+  ${error.message}
+  ${error.debugMessage}`)
+      }
+
       if (
         errorMap &&
         errorMap.from &&
@@ -36,14 +44,19 @@ const fetchGraphql = async ({ url, query, errorMap, variables = {} }) => {
         return console.error(`[gatsby-source-wordpress] ${errorMap.to}`)
       }
 
-      return console.error(
-        `[gatsby-source-wordpress] ${error.message} (${error.category})`
-      )
+      if (!error.debugMessage) {
+        console.error(
+          `[gatsby-source-wordpress] ${error.message} (${error.category})`
+        )
+      }
     })
 
     console.error(`[gatsby-source-wordpress] GraphQL vars:`, variables)
     console.error(`[gatsby-source-wordpress] GraphQL query: ${query}`)
-    process.exit()
+
+    if (!json) {
+      process.exit()
+    }
   }
 
   return json
