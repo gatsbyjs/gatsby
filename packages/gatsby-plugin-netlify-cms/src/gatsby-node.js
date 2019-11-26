8137255ae2c1ea36ac440344a4eae8297dfe9e39
Erez Rokah <erezrokah@users.noreply.github.com>
2019-11-27 00:43:55 +0200

    fix(gatsby-plugin-netlify-cms): Mark relevant dependencies as externals (#19575)
    * fix: load netlify dependencies as external ones
    
    * fix: force code splitting for netlify-identity-widget
    
    * fix: ignore netlify-identity-widget from build output when disabled
    
    * fix: use concat instead of flat to support node 10
    
    * refactor: take react/react-dom dependencies from node_modules
    
    * fix: don't add optimization code during dev mode
    
    * refactor: use DefinePlugin instead of window.CMS_MANUAL_INIT
    
    * fix: register styles in development mode

--- packages/gatsby-plugin-netlify-cms/src/gatsby-node.js
+++ packages/gatsby-plugin-netlify-cms/src/gatsby-node.js
@@ -6,6 +6,8 @@ import HtmlWebpackExcludeAssetsPlugin from "html-webpack-exclude-assets-plugin"
 import MiniCssExtractPlugin from "mini-css-extract-plugin"
 // TODO: swap back when https://github.com/geowarin/friendly-errors-webpack-plugin/pull/86 lands
 import FriendlyErrorsPlugin from "@pieh/friendly-errors-webpack-plugin"
+import CopyPlugin from "copy-webpack-plugin"
+import HtmlWebpackTagsPlugin from "html-webpack-tags-plugin"
 
 // Deep mapping function for plain objects and arrays. Allows any value,
 // including an object or array, to be transformed.
@@ -47,19 +49,6 @@ function replaceRule(value) {
     return null
   }
 
-  // Manually swap `style-loader` for `MiniCssExtractPlugin.loader`.
-  // `style-loader` is only used in development, and doesn't allow us to pass
-  // the `styles` entry css path to Netlify CMS.
-  if (
-    typeof value.loader === `string` &&
-    value.loader.includes(`style-loader`)
-  ) {
-    return {
-      ...value,
-      loader: MiniCssExtractPlugin.loader,
-    }
-  }
-
   return value
 }
 
@@ -90,7 +79,7 @@ exports.onCreateDevServer = ({ app, store }, { publicPath = `admin` }) => {
 }
 
 exports.onCreateWebpackConfig = (
-  { store, stage, getConfig, plugins, pathPrefix, loaders, rules },
+  { store, stage, getConfig, plugins, pathPrefix, loaders, rules, actions },
   {
     modulePath,
     customizeWebpackConfig,
@@ -108,13 +97,45 @@ exports.onCreateWebpackConfig = (
   const gatsbyConfig = getConfig()
   const { program } = store.getState()
   const publicPathClean = trim(publicPath, `/`)
+
+  const externals = [
+    {
+      name: `react`,
+      global: `React`,
+      assetDir: `umd`,
+      assetName: `react.production.min.js`,
+    },
+    {
+      name: `react-dom`,
+      global: `ReactDOM`,
+      assetDir: `umd`,
+      assetName: `react-dom.production.min.js`,
+    },
+    {
+      name: `netlify-cms-app`,
+      global: `NetlifyCmsApp`,
+      assetDir: `dist`,
+      assetName: `netlify-cms-app.js`,
+      sourceMap: `netlify-cms-app.js.map`,
+    },
+  ]
+
+  if (enableIdentityWidget) {
+    externals.unshift({
+      name: `netlify-identity-widget`,
+      global: `netlifyIdentity`,
+      assetDir: `build`,
+      assetName: `netlify-identity-widget.js`,
+      sourceMap: `netlify-identity-widget.js.map`,
+    })
+  }
+
   const config = {
     ...gatsbyConfig,
     entry: {
       cms: [
-        manualInit && `${__dirname}/cms-manual-init.js`,
-        `${__dirname}/cms.js`,
-        enableIdentityWidget && `${__dirname}/cms-identity.js`,
+        path.join(__dirname, `cms.js`),
+        enableIdentityWidget && path.join(__dirname, `cms-identity.js`),
       ]
         .concat(modulePath)
         .filter(p => p),
@@ -153,9 +174,10 @@ exports.onCreateWebpackConfig = (
 
       // Use a simple filename with no hash so we can access from source by
       // path.
-      new MiniCssExtractPlugin({
-        filename: `[name].css`,
-      }),
+      stage !== `develop` &&
+        new MiniCssExtractPlugin({
+          filename: `[name].css`,
+        }),
 
       // Auto generate CMS index.html page.
       new HtmlWebpackPlugin({
@@ -178,6 +200,34 @@ exports.onCreateWebpackConfig = (
         __PATH__PREFIX__: pathPrefix,
         CMS_PUBLIC_PATH: JSON.stringify(publicPath),
       }),
+
+      new CopyPlugin(
+        [].concat.apply(
+          [],
+          externals.map(({ name, assetName, sourceMap, assetDir }) =>
+            [
+              {
+                from: path.join(`node_modules`, name, assetDir, assetName),
+                to: assetName,
+              },
+              sourceMap && {
+                from: path.join(`node_modules`, name, assetDir, sourceMap),
+                to: sourceMap,
+              },
+            ].filter(item => item)
+          )
+        )
+      ),
+
+      new HtmlWebpackTagsPlugin({
+        tags: externals.map(({ assetName }) => assetName),
+        append: false,
+      }),
+
+      new webpack.DefinePlugin({
+        CMS_MANUAL_INIT: JSON.stringify(manualInit),
+        PRODUCTION: JSON.stringify(stage !== `develop`),
+      }),
     ].filter(p => p),
 
     // Remove common chunks style optimizations from Gatsby's default
@@ -189,6 +239,11 @@ exports.onCreateWebpackConfig = (
       minimizer: stage === `develop` ? [] : gatsbyConfig.optimization.minimizer,
     },
     devtool: stage === `develop` ? `cheap-module-source-map` : `source-map`,
+    externals: externals.map(({ name, global }) => {
+      return {
+        [name]: global,
+      }
+    }),
   }
 
   if (customizeWebpackConfig) {
@@ -202,6 +257,33 @@ exports.onCreateWebpackConfig = (
       plugins,
     })
   }
+
+  actions.setWebpackConfig({
+    // force code splitting for netlify-identity-widget
+    optimization:
+      stage === `develop`
+        ? {}
+        : {
+            splitChunks: {
+              cacheGroups: {
+                "netlify-identity-widget": {
+                  test: /[\\/]node_modules[\\/](netlify-identity-widget)[\\/]/,
+                  name: `netlify-identity-widget`,
+                  chunks: `all`,
+                  enforce: true,
+                },
+              },
+            },
+          },
+    // ignore netlify-identity-widget when not enabled
+    plugins: enableIdentityWidget
+      ? []
+      : [
+          new webpack.IgnorePlugin({
+            resourceRegExp: /^netlify-identity-widget$/,
+          }),
+        ],
+  })
 
   return new Promise((resolve, reject) => {
     if (stage === `develop`) {
