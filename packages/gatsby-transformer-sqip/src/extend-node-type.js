213c38014b96d96202d9ae5f0c69fc723b8ced69
agin10 <58223893+agin10@users.noreply.github.com>
2019-11-26 22:00:36 +0530

    refactor: use Array.includes (#19805)

--- packages/gatsby-transformer-sqip/src/extend-node-type.js
+++ packages/gatsby-transformer-sqip/src/extend-node-type.js
@@ -195,7 +195,7 @@ async function sqipContentful({ type, cache, store }) {
           file: { contentType },
         } = asset
 
-        if (contentType.indexOf(`image/`) !== 0) {
+        if (contentType.includes(`image/`)) {
           return null
         }
 
