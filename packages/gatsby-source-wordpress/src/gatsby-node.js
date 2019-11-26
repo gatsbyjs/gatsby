cb2345fb4be7691336ef816f5ec28d0077ac74cd
Tyler Barnes <tylerdbarnes@gmail.com>
2019-11-25 18:03:40 -0800

    temp disable page creation

--- packages/gatsby-source-wordpress/src/gatsby-node.js
+++ packages/gatsby-source-wordpress/src/gatsby-node.js
@@ -1,11 +1,11 @@
 import sourceNodes from "./gatsby-node/source-nodes"
-import createPages from "./gatsby-node/create-pages"
+// import createPages from "./gatsby-node/create-pages"
 import createSchemaCustomization from "./gatsby-node/create-schema-customization"
 import createResolvers from "./gatsby-node/create-resolvers"
 
 module.exports = {
+  sourceNodes,
   createSchemaCustomization,
   createResolvers,
-  sourceNodes,
-  createPages,
+  // createPages,
 }
