110081aba954e496ee66b6effbd54f177ab11880
Tyler Barnes <tylerdbarnes@gmail.com>
2019-11-26 16:34:46 -0800

    fetch multiple node lists at once

--- packages/gatsby-source-wordpress/src/gatsby-node/source-nodes/fetch-nodes.js
+++ packages/gatsby-source-wordpress/src/gatsby-node/source-nodes/fetch-nodes.js
@@ -9,61 +9,41 @@ export const fetchWPGQLContentNodes = async ({ queries }, helpers, { url }) => {
   const { reporter } = helpers
   const contentNodeGroups = []
 
-  for (const [fieldName, queryInfo] of Object.entries(queries)) {
-    const { queryString, typeInfo } = queryInfo
+  await Promise.all(
+    Object.entries(queries).map(
+      ([fieldName, queryInfo]) =>
+        new Promise(async resolve => {
+          const { queryString, typeInfo } = queryInfo
 
-    const activity = reporter.activityTimer(
-      `[gatsby-source-wordpress] -> fetching all ${typeInfo.pluralName}`
+          const activity = reporter.activityTimer(
+            `[gatsby-source-wordpress] -> fetching all ${typeInfo.pluralName}`
+          )
+          activity.start()
+          const allNodesOfContentType = await paginatedWpNodeFetch({
+            first: 100,
+            after: null,
+            contentTypePlural: typeInfo.pluralName,
+            contentTypeSingular: typeInfo.singleName,
+            nodeTypeName: typeInfo.nodesTypeName,
+            url,
+            query: queryString,
+            activity,
+            helpers,
+          })
+          activity.end()
+
+          if (allNodesOfContentType && allNodesOfContentType.length) {
+            contentNodeGroups.push({
+              singular: fieldName,
+              plural: fieldName,
+              allNodesOfContentType,
+            })
+          }
+
+          return resolve()
+        })
     )
-    activity.start()
-    const allNodesOfContentType = await paginatedWpNodeFetch({
-      first: 100,
-      after: null,
-      contentTypePlural: typeInfo.pluralName,
-      contentTypeSingular: typeInfo.singleName,
-      nodeTypeName: typeInfo.nodesTypeName,
-      url,
-      query: queryString,
-      activity,
-      helpers,
-    })
-    activity.end()
-
-    if (allNodesOfContentType && allNodesOfContentType.length) {
-      contentNodeGroups.push({
-        singular: fieldName,
-        plural: fieldName,
-        allNodesOfContentType,
-      })
-    }
-  }
-
-  // this fetches multiple endpoints at once
-  // await Promise.all(
-  //   Object.entries(queryStrings).map(
-  //     ([fieldName, query]) =>
-  //       new Promise(async resolve => {
-  //         const allNodesOfContentType = await paginatedWpNodeFetch({
-  //           first: 10,
-  //           after: null,
-  //           contentTypePlural: fieldName,
-  //           contentTypeSingular: fieldName,
-  //           url,
-  //           query,
-  //         })
-
-  //         if (allNodesOfContentType && allNodesOfContentType.length) {
-  //           contentNodeGroups.push({
-  //             singular: fieldName,
-  //             plural: fieldName,
-  //             allNodesOfContentType,
-  //           })
-  //         }
-
-  //         return resolve()
-  //       })
-  //   )
-  // )
+  )
 
   return contentNodeGroups
 }
