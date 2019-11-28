807213673d330b77a8e500f0be6bf4352aa6cb37
Tyler Barnes <tylerdbarnes@gmail.com>
2019-11-27 22:01:56 -0800

    spacing

--- packages/gatsby-source-wordpress/src/gatsby-node/source-nodes/wp-actions/update.js
+++ packages/gatsby-source-wordpress/src/gatsby-node/source-nodes/wp-actions/update.js
@@ -90,10 +90,11 @@ const wpActionUPDATE = async ({
           ) {
             reporter.log(``)
             reporter.info(chalk.bold(`${key} changed`))
-            reporter.log(`${chalk.italic.bold(`from`)}`)
-            reporter.log(value)
-            reporter.log(chalk.italic.bold(`to`))
-            reporter.log(updatedNodeContent[key])
+            reporter.log(``)
+            reporter.log(`${chalk.italic.bold(`    from`)}`)
+            reporter.log(`      ${value}`)
+            reporter.log(chalk.italic.bold(`    to`))
+            reporter.log(`      ${updatedNodeContent[key]}`)
             reporter.log(``)
           }
         })
