8db370dd7173280b3e3f94da4b1695c093e1408c
Tyler Barnes <tylerdbarnes@gmail.com>
2019-11-27 17:51:15 -0800

    add comments to node API's

--- packages/gatsby-source-wordpress/src/gatsby-node.js
+++ packages/gatsby-source-wordpress/src/gatsby-node.js
@@ -3,13 +3,23 @@ import sourceNodes from "./gatsby-node/source-nodes"
 import createSchemaCustomization from "./gatsby-node/create-schema-customization"
 import createResolvers from "./gatsby-node/create-resolvers"
 import onPostBootstrap from "./gatsby-node/on-post-bootstrap"
-// import createPages from "./gatsby-node/create-pages"
 
 module.exports = {
+  // check plugin requirements
+  // do introspection on the WPGQL schema
+  // build gql queries and add to rematch/redux store
   onPreBootstrap,
+
+  // pull queries from redux store and use to fetch all data from WPGQL
+  // or just fetch changed data since the last build
   sourceNodes,
+
+  // use introspection to normalize the WPGQL schema and add to the Gatsby schema
   createSchemaCustomization,
+
+  // fetch and create image file nodes when they're queried for
   createResolvers,
-  // createPages,
+
+  // start the interval refetcher in development mode for real-time data updates
   onPostBootstrap,
 }
