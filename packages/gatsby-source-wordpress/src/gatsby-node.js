9425a2ebcdbe6487738522fe12e34e3530b637b9
Tyler Barnes <tylerdbarnes@gmail.com>
2019-11-27 17:55:53 -0800

    clean up gatsby-node

--- packages/gatsby-source-wordpress/src/gatsby-node.js
+++ packages/gatsby-source-wordpress/src/gatsby-node.js
@@ -1,20 +1,23 @@
-import onPreBootstrap from "./gatsby-node/on-pre-bootstrap"
-import sourceNodes from "./gatsby-node/source-nodes"
-import createSchemaCustomization from "./gatsby-node/create-schema-customization"
-import createResolvers from "./gatsby-node/create-resolvers"
-import onPostBootstrap from "./gatsby-node/on-post-bootstrap"
+import {
+  onPreBootstrap,
+  sourceNodes,
+  createSchemaCustomization,
+  createResolvers,
+  onPostBootstrap,
+} from "./gatsby-node"
 
 module.exports = {
-  // check plugin requirements
-  // do introspection on the WPGQL schema
-  // build gql queries and add to rematch/redux store
+  // 1. check plugin requirements
+  // 2. do introspection on the WPGQL schema
+  // 3. build gql queries and add to rematch/redux store
   onPreBootstrap,
 
-  // pull queries from redux store and use to fetch all data from WPGQL
-  // or just fetch changed data since the last build
+  // 1. pull queries from redux store
+  // 2. fetch all data from WPGQL or just fetch changed data since the last build
   sourceNodes,
 
-  // use introspection to normalize the WPGQL schema and add to the Gatsby schema
+  // 1. introspect WPGQL types
+  // 2. normalize the WPGQL schema and add to the Gatsby schema
   createSchemaCustomization,
 
   // fetch and create image file nodes when they're queried for
---
+++ packages/gatsby-source-wordpress/src/gatsby-node/index.js
@@ -0,0 +1,13 @@
+import onPreBootstrap from "./on-pre-bootstrap"
+import sourceNodes from "./source-nodes"
+import createSchemaCustomization from "./create-schema-customization"
+import createResolvers from "./create-resolvers"
+import onPostBootstrap from "./on-post-bootstrap"
+
+export default {
+  onPreBootstrap,
+  sourceNodes,
+  createSchemaCustomization,
+  createResolvers,
+  onPostBootstrap,
+}
