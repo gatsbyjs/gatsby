f3726dddfbd1a2b4c606721c0003ade72b2da59f
Michal Piechowiak <misiek.piechowiak@gmail.com>
2019-11-29 15:34:21 +0100

    streamline page creator SitePlugin nodes so tests work in multiple envs

--- integration-tests/cache-resilience/gatsby-node.js
+++ integration-tests/cache-resilience/gatsby-node.js
@@ -1,5 +1,6 @@
 const fs = require(`fs`)
 const v8 = require(`v8`)
+const path = require(`path`)
 
 const db = require(`gatsby/dist/db`)
 
@@ -8,25 +9,60 @@ const {
   ON_POST_BOOTSTRAP_FILE_PATH,
 } = require(`./utils/constants`)
 
-const createMap = nodes => {
+const sanitizePageCreatorPluginOptions = options => {
+  if (options && options.path) {
+    return {
+      ...options,
+      path: path.relative(process.cwd(), options.path),
+    }
+  }
+  return options
+}
+
+// copied from packages/gatsby/src/bootstrap/load-plugins/load.js
+// and adjusted a bit to make it work
+const createPluginId = (createNodeId, name, pluginObject) =>
+  createNodeId(
+    name +
+      (pluginObject
+        ? JSON.stringify(
+            sanitizePageCreatorPluginOptions(pluginObject.pluginOptions)
+          )
+        : ``),
+    `Plugin`
+  )
+
+const createMap = (nodes, createNodeId) => {
   const map = new Map()
   nodes.forEach(node => {
+    // HACK - SitePlugin nodes for gatsby-plugin-page-creator will have different id
+    // depending on CWD, which make it impossible to do snapshot testing.
+    // We will generate more stable id replacement here
+    if (
+      node.internal.type === `SitePlugin` &&
+      node.name === `gatsby-plugin-page-creator`
+    ) {
+      node = {
+        ...node,
+        id: createPluginId(createNodeId, node.name, node),
+      }
+    }
     map.set(node.id, node)
   })
   return map
 }
 
-exports.onPreBootstrap = ({ getNodes }) => {
+exports.onPreBootstrap = ({ getNodes, createNodeId }) => {
   fs.writeFileSync(
     ON_PRE_BOOTSTRAP_FILE_PATH,
-    v8.serialize(createMap(getNodes()))
+    v8.serialize(createMap(getNodes(), createNodeId))
   )
 }
 
-exports.onPostBootstrap = async ({ getNodes }) => {
+exports.onPostBootstrap = async ({ getNodes, createNodeId }) => {
   fs.writeFileSync(
     ON_POST_BOOTSTRAP_FILE_PATH,
-    v8.serialize(createMap(getNodes()))
+    v8.serialize(createMap(getNodes(), createNodeId))
   )
 
   await db.saveState()
