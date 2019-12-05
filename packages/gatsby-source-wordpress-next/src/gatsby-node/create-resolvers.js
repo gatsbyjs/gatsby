402875a52e452b7b9095da4df478001b91229056
Tyler Barnes <tylerdbarnes@gmail.com>
2019-12-04 16:32:38 -0800

    remove debugging code

--- packages/gatsby-source-wordpress-next/src/gatsby-node/create-resolvers.js
+++ packages/gatsby-source-wordpress-next/src/gatsby-node/create-resolvers.js
@@ -1,30 +1,15 @@
-import {
-  createRemoteMediaItemNode,
-  getFileNodeMetaBySourceUrl,
-} from "./source-nodes/create-remote-media-item-node"
+import { createRemoteMediaItemNode } from "./source-nodes/create-remote-media-item-node"
 
 export default helpers => {
   helpers.createResolvers({
     WpMediaItem: {
       remoteFile: {
         type: `File`,
-        resolve: (source, _, context) => {
-          const nodeMeta = getFileNodeMetaBySourceUrl(source.sourceUrl)
-
-          if (nodeMeta && nodeMeta.id) {
-            return context.nodeModel.getNodeById({
-              id: nodeMeta.id,
-              type: `File`,
-            })
-          }
-
-          return null
-
-          // return createRemoteMediaItemNode({
-          //   mediaItemNode: source,
-          //   helpers,
-          // })
-        },
+        resolve: mediaItemNode =>
+          createRemoteMediaItemNode({
+            mediaItemNode,
+            helpers,
+          }),
       },
     },
   })
