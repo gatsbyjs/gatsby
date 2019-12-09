0a1dbdf4d8e4acce8c509337cdfe2a5153c47091
Ward Peeters <ward@coding-tech.com>
2019-12-09 09:12:02 +0100

    update gatsby-browser with simplified example

--- examples/using-redux/gatsby-browser.js
+++ examples/using-redux/gatsby-browser.js
@@ -1,9 +1,1 @@
-import React from "react"
-import { Provider } from "react-redux"
-
-import createStore from "./src/state/createStore"
-
-export const wrapRootElement = ({ element }) => {
-  const store = createStore()
-  return <Provider store={store}>{element}</Provider>
-}
+export { wrapWithProvider as wrapRootElement } from "./wrap-with-provider"
