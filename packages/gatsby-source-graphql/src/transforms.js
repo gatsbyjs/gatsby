de1166aa4ce91a4e5dd2a2accbe7ae6ed7724300
yaacovCR <yaacovCR@gmail.com>
2019-12-10 10:27:31 -0500

    use cloneType and healSchema instead of internal methods

--- packages/gatsby-source-graphql/src/transforms.js
+++ packages/gatsby-source-graphql/src/transforms.js
@@ -1,13 +1,6 @@
-const {
-  GraphQLObjectType,
-  GraphQLNonNull,
-  GraphQLSchema,
-} = require(`gatsby/graphql`)
+const { GraphQLObjectType, GraphQLNonNull } = require(`gatsby/graphql`)
 const { visitSchema, VisitSchemaKind } = require(`graphql-tools-fork`)
-const {
-  createResolveType,
-  fieldMapToFieldConfigMap,
-} = require(`graphql-tools-fork`)
+const { cloneType, healSchema } = require(`graphql-tools-fork`)
 
 class NamespaceUnderFieldTransform {
   constructor({ typeName, fieldName, resolver }) {
@@ -18,23 +11,14 @@ class NamespaceUnderFieldTransform {
 
   transformSchema(schema) {
     const query = schema.getQueryType()
-    let newQuery
-    const nestedType = new GraphQLObjectType({
-      name: this.typeName,
-      fields: () =>
-        fieldMapToFieldConfigMap(
-          query.getFields(),
-          createResolveType(typeName => {
-            if (typeName === query.name) {
-              return newQuery
-            } else {
-              return schema.getType(typeName)
-            }
-          }),
-          true
-        ),
-    })
-    newQuery = new GraphQLObjectType({
+
+    const nestedType = new cloneType(query)
+    nestedType.name = this.typeName
+
+    const typeMap = schema.getTypeMap()
+    typeMap[this.typeName] = nestedType
+
+    const newQuery = new GraphQLObjectType({
       name: query.name,
       fields: {
         [this.fieldName]: {
@@ -49,15 +33,9 @@ class NamespaceUnderFieldTransform {
         },
       },
     })
-    const typeMap = schema.getTypeMap()
-    const allTypes = Object.keys(typeMap)
-      .filter(name => name !== query.name)
-      .map(key => typeMap[key])
+    typeMap[query.name] = newQuery
 
-    return new GraphQLSchema({
-      query: newQuery,
-      types: allTypes,
-    })
+    return healSchema(schema)
   }
 }
 
