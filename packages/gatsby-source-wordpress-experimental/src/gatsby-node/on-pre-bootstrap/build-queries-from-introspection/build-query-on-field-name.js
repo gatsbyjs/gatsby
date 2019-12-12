086dad546b26598658c64e616c052ee3b09f0562
Tyler Barnes <tylerdbarnes@gmail.com>
2019-12-12 12:36:03 -0800

    simplify schema introspection transformation code && remove duplicate logic

--- packages/gatsby-source-wordpress-experimental/src/gatsby-node/on-pre-bootstrap/build-queries-from-introspection/build-query-on-field-name.js
+++ packages/gatsby-source-wordpress-experimental/src/gatsby-node/on-pre-bootstrap/build-queries-from-introspection/build-query-on-field-name.js
@@ -1,79 +1,4 @@
-const transformField = ({ field, nodeListTypeNames }) => {
-  if (!field) {
-    return null
-  }
-
-  if (
-    field.args.length &&
-    // remove fields that have required args. They'll cause query errors if ommitted
-    //  and we can't determine how to use those args programatically.
-    field.args.find(arg => arg && arg.type && arg.type.kind === `NON_NULL`)
-  ) {
-    return null
-  }
-
-  // temp remove unions. graphql-query-builder doesn't seem to support them
-  // probably need to do something custom instead of using that package
-  if (field.type && field.type.kind === `UNION`) {
-    return null
-  }
-
-  if (field.type.kind === `LIST` && field.type.ofType.kind !== `SCALAR`) {
-    return null
-  }
-
-  // just pull the node id for lists we have nodes for
-  if (
-    field.type.kind === `LIST` &&
-    nodeListTypeNames.includes(field.type.ofType.name)
-  ) {
-    return {
-      fieldName: field.name,
-      innerFields: [`id`],
-    }
-  }
-
-  // pull the id for connections
-  if (field.type.relationShipField && !field.type.mediaItem) {
-    return {
-      fieldName: field.name,
-      innerFields: [`id`],
-    }
-  }
-
-  // pull the id and sourceUrl for connections to media items
-  if (field.type.relationShipField && field.type.mediaItem) {
-    return {
-      fieldName: field.name,
-      innerFields: [`id`, `sourceUrl`],
-    }
-  }
-
-  // if this field has fields,
-  if (field.type && field.type.fields) {
-    return {
-      fieldName: field.name,
-      innerFields: field.type.fields
-        // time to recurse!
-        .map(field =>
-          transformField({
-            field,
-            nodeListTypeNames,
-          })
-        )
-        // remove null fields that we omitted above
-        .filter(Boolean),
-    }
-  }
-
-  return field.name
-}
-
-export const buildNodesQueryOnFieldName = ({
-  fields,
-  fieldName,
-  nodeListTypeNames,
-}) =>
+export const buildNodesQueryOnFieldName = ({ fields, fieldName }) =>
   buildQuery({
     queryName: `NODE_LIST_QUERY`,
     variables: `$first: Int!, $after: String`,
@@ -86,9 +11,7 @@ export const buildNodesQueryOnFieldName = ({
       },
       {
         fieldName: `nodes`,
-        innerFields: fields
-          .map(field => transformField({ field, nodeListTypeNames }))
-          .filter(Boolean),
+        innerFields: fields,
       },
     ],
   })
@@ -132,17 +55,11 @@ const buildQuery = ({
   }
 `
 
-export const buildNodeQueryOnFieldName = ({
-  fields,
-  fieldName,
-  nodeListTypeNames,
-}) =>
+export const buildNodeQueryOnFieldName = ({ fields, fieldName }) =>
   buildQuery({
     queryName: `SINGLE_CONTENT_QUERY`,
     variables: `$id: ID!`,
     fieldName,
     fieldVariables: `id: $id`,
-    fields: fields
-      .map(field => transformField({ field, nodeListTypeNames }))
-      .filter(Boolean),
+    fields: fields,
   })
--- packages/gatsby-source-wordpress-experimental/src/gatsby-node/on-pre-bootstrap/build-queries-from-introspection/recursively-transform-fields.js
+++ packages/gatsby-source-wordpress-experimental/src/gatsby-node/on-pre-bootstrap/build-queries-from-introspection/recursively-transform-fields.js
@@ -6,6 +6,31 @@ const filterField = ({ field, nodeListTypeNames }) => {
   const fieldType = field.type || {}
   const ofType = fieldType.ofType || {}
 
+  if (!field) {
+    return false
+  }
+
+  if (
+    field.args.length &&
+    // remove fields that have required args. They'll cause query errors if ommitted
+    //  and we can't determine how to use those args programatically.
+    field.args.find(arg => arg && arg.type && arg.type.kind === `NON_NULL`)
+  ) {
+    return false
+  }
+
+  // if we're recursing and we have infinite relational data
+  // for which we have no top-level nodes,
+  // and the level we've recursed to is too far down to have a next level
+  // don't query for this field
+  if (fieldType.kind === `OBJECT` && !fieldType.fields) {
+    return false
+  }
+
+  if (fieldType.kind === `LIST` && ofType.kind !== `SCALAR`) {
+    return null
+  }
+
   if (fieldBlacklist.includes(field.name)) {
     return false
   }
@@ -55,116 +80,46 @@ const filterField = ({ field, nodeListTypeNames }) => {
 const transformField = ({ field, nodeListTypeNames }) => {
   const fieldType = field.type || {}
 
-  // if we're recursing and we have infinite relational data
-  // for which we have no top-level nodes,
-  // and the level we've recursed to is too far down to have a next level
-  // don't query for this field
-  if (fieldType.kind === `OBJECT` && !fieldType.fields) {
-    return null
-  }
+  const isAGatsbyNode = nodeListTypeNames.includes(field.type.name)
+  const isAMediaItemNode = isAGatsbyNode && field.type.name === `MediaItem`
 
-  if (nodeListTypeNames.includes(field.type.name)) {
-    // this is a node from another root node list that we will make a node from
-    // let's just turn this into an ID
-    // to do that we just pull the WPGQL id for now
-    fieldType.relationShipField = true
-
-    // if it's a media item node, we also need the URL,
-    // so mark it as a media item
-    if (field.type.name === `MediaItem`) {
-      fieldType.mediaItem = true
-    }
-
-    if (fieldType.fields) {
-      fieldType.fields = fieldType.fields.filter(
-        innerField =>
-          innerField.name === `id` || innerField.name === `sourceUrl`
-      )
+  // pull the id and sourceUrl for connections to media items
+  if (isAMediaItemNode) {
+    return {
+      fieldName: field.name,
+      innerFields: [`id`, `sourceUrl`],
     }
   }
-  // else if (fieldType.fields && fieldType.fields.find(field => field.)) {
 
-  // }
-  else if (fieldType.fields && fieldType.fields.length) {
-    fieldType.fields = fieldType.fields
-      .filter(innerField =>
-        filterField({
-          field: innerField,
-          parentField: field,
-          nodeListTypeNames,
-        })
-      )
-      .filter(f => !!f)
-      .map(field => transformField({ field, nodeListTypeNames }))
+  // pull the id for connections
+  if (isAGatsbyNode && !isAMediaItemNode) {
+    return {
+      fieldName: field.name,
+      innerFields: [`id`],
+    }
   }
 
-  // // for nested fields that are connections
-  // // but there is no nodes field to get id's from
-  // // we don't want this field
-  // fieldType.fields = fieldType.fields
-  //   ? fieldType.fields.filter(field => {
-  //       if (
-  //         !!field &&
-  //         field.type &&
-  //         field.type.name &&
-  //         field.type.name.includes(`Connection`)
-  //       ) {
-  //         return field.type.fields.find(field => field.name === `nodes`)
-  //       }
-
-  //       return true
-  //     })
-  //   : null
-
-  if (!fieldType.fields || !fieldType.fields.length) {
-    delete fieldType.fields
+  if (fieldType.fields) {
+    return {
+      fieldName: field.name,
+      innerFields: recursivelyTransformFields({
+        fields: field.type.fields,
+        nodeListTypeNames,
+      }),
+    }
   }
-  // else if (
-  //   fieldType.kind !== `NON_NULL` &&
-  //   fieldType.kind !== `SCALAR` &&
-  //   fieldType.kind !== `LIST` &&
-  //   fieldType.kind !== `OBJECT` &&
-  //   fieldType.kind !== `ENUM`
-  // ) {
-  //   dd(field)
-  // }
 
-  // if (field.name === `actionMonitorActions`) {
-  //   dd(field.type.fields.filter(field => field.name !== `pageInfo`))
-  // }
-
-  // if (
-  //   fieldType &&
-  //   fieldType.kind === `LIST` &&
-  //   fieldType.ofType &&
-  //   fieldType.ofType.name &&
-  //   types.find(type => type.name === fieldType.ofType.name)
-  // ) {
-  //   // this is a type that isn't in a root field node list
-  //   // so we need to query it directly instead of creating a node relationship.
-  //   // but we need to grab it's possible fields from the type introspection
-  //   // and add them here since we don't have them
-  //   const type = types.find(type => type.name === fieldType.ofType.name)
-  //   console.log(`here it is!`)
-  //   dd(type)
-  // }
-  return field
+  return field.name
 }
 
-const recursivelyTransformFields = ({ field, fields, nodeListTypeNames }) =>
+const recursivelyTransformFields = ({ fields, nodeListTypeNames }) =>
   fields
-    ? fields
-        .filter(innerField =>
-          filterField({
-            field: innerField,
-            parentField: field,
-            nodeListTypeNames,
-          })
-        )
-        .filter(f => !!f)
-        .map(innerField =>
-          transformField({ field: innerField, nodeListTypeNames })
-        )
-    : null
+    .filter(field =>
+      filterField({
+        field,
+        nodeListTypeNames,
+      })
+    )
+    .map(field => transformField({ field, nodeListTypeNames }))
 
 export default recursivelyTransformFields
