399de91d59bec62622859ea9a57dec5992f88085
Michael Elsdörfer <michael@elsdoerfer.info>
2019-12-19 11:24:06 +0100

    Fix lint errors, add test.

--- packages/gatsby-source-shopify/src/create-client.js
+++ packages/gatsby-source-shopify/src/create-client.js
@@ -3,13 +3,12 @@ import { GraphQLClient } from "graphql-request"
  * Create a Shopify Storefront GraphQL client for the provided name and token.
  */
 export const createClient = (shopName, accessToken) => {
-  let url;
-  if (shopName.indexOf("." > -1)) {
-    url = `https://${shopName}/api/graphql`;
+  let url
+  if (shopName.includes(`.`)) {
+    url = `https://${shopName}/api/graphql`
+  } else {
+    url = `https://${shopName}.myshopify.com/api/graphql`
   }
-  else {
-    url = `https://${shopName}.myshopify.com/api/graphql`;
-  }  
   return new GraphQLClient(url, {
     headers: {
       "X-Shopify-Storefront-Access-Token": accessToken,
