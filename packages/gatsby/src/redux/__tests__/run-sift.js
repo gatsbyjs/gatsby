695a48f5a47081b135bbe505bc3164baca13297f
Simply007 <chrastina.ondra@gmail.com>
2019-12-19 14:43:59 +0100

    add more descriptive input to test

--- packages/gatsby/src/redux/__tests__/run-sift.js
+++ packages/gatsby/src/redux/__tests__/run-sift.js
@@ -184,7 +184,7 @@ if (!process.env.GATSBY_DB_NODES || process.env.GATSBY_DB_NODES === `redux`) {
         ])
       })
       it(`return empty array in case of empty nodes`, async () => {
-        const queryArgs = {}
+        const queryArgs = { filter: {}, sort: {} }
         const resultSingular = await runSift({
           gqlType,
           queryArgs,
