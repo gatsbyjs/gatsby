c591eb774ba355543a2be7d529701cfa5bb56b5f
Michal Piechowiak <misiek.piechowiak@gmail.com>
2019-12-20 14:02:56 +0100

    fix(gatsby): enable static queries in `wrapRootElement` (#19986)
    * move static query provider to root of component tree
    
    Co-Authored-By: Orestis Ioannou <oorestisime@gmail.com>
    
    * fix lint
    
    * Try cleaning up the code and add e2e test
    
    * use default export (esm/cjs export/import mixing)
    
    * adjust wrapping component a bit, so we can write assertion easier
    
    * add assertion
    
    * rename json-store
    
    * don't destructure unused variable
    
    * Add same test to production-runtime e2e tests for test parity
    
    Co-authored-by: Orestis Ioannou <oorestisime@gmail.com>

--- packages/gatsby/cache-dir/root.js
+++ packages/gatsby/cache-dir/root.js
@@ -9,7 +9,7 @@ import {
 } from "./navigation"
 import { apiRunner } from "./api-runner-browser"
 import loader from "./loader"
-import JSONStore from "./json-store"
+import { PageQueryStore, StaticQueryStore } from "./query-result-store"
 import EnsureResources from "./ensure-resources"
 
 import { reportError, clearError } from "./error-overlay-handler"
@@ -48,7 +48,7 @@ const RouteHandler = props => (
       basepath: `/`,
     }}
   >
-    <JSONStore {...props} />
+    <PageQueryStore {...props} />
   </BaseContext.Provider>
 )
 
@@ -91,7 +91,7 @@ class LocationHandler extends React.Component {
     let custom404
     if (real404PageResources) {
       custom404 = (
-        <JSONStore {...this.props} pageResources={real404PageResources} />
+        <PageQueryStore {...this.props} pageResources={real404PageResources} />
       )
     }
 
@@ -130,4 +130,4 @@ const WrappedRoot = apiRunner(
   }
 ).pop()
 
-export default () => WrappedRoot
+export default () => <StaticQueryStore>{WrappedRoot}</StaticQueryStore>
