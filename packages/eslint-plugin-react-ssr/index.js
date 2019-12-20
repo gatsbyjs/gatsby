f6501fe0d25655cc8ee56f5af7f4b07e3b2fc3d1
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2019-12-20 15:20:43 +0530

    wip

--- packages/eslint-plugin-react-ssr/index.js
+++ packages/eslint-plugin-react-ssr/index.js
@@ -12,28 +12,40 @@ const rules = {
           if (unavailableConstants.includes(node.name)) {
             // const ancestors = context.getAncestors(node)
             const scope = context.getScope(node); // console.log(ancestors, scope)
-            // Class methods
-            // if (!exclusions.includes(scope.block.parent.key.name)) {
-            //   context.report({
-            //     node,
-            //     message: `Cannot use browser global ${
-            //       node.name
-            //     } during SSR unless it is in ${exclusions.join()}`,
-            //   })
-            // }
-            // Hooks
-            // console.log(scope.block.parent.callee)
 
-            if (scope.block.parent.callee && !exclusions.includes(scope.block.parent.callee.name)) {
-              context.report({
-                node,
-                message: `Cannot use browser global ${node.name} during SSR unless it is in ${exclusions.join()}`
-              });
-            } // Class methods
-            // Call expressions for Hooks
-            // Top level
-            // Render
-            // Conditionals
+            /* Class Methods like componentDidMount, componentWillMount
+             * and componentWillUnmount
+             */
+
+            if (scope.block.parent.key) {
+              console.log(`Class`);
+
+              if (!exclusions.includes(scope.block.parent.key.name)) {
+                context.report({
+                  node,
+                  message: `Cannot use browser global ${node.name} during SSR`
+                });
+              }
+            } // Hooks
+            else if (scope.block.parent.callee) {
+                console.log(`Hooks`);
+
+                if (!exclusions.includes(scope.block.parent.callee.name)) {
+                  context.report({
+                    node,
+                    message: `Cannot use browser global ${node.name} during SSR`
+                  });
+                  return;
+                }
+              } // Everything else
+              else {
+                  console.log(`Everything else`);
+                  context.report({
+                    node,
+                    message: `Cannot use browser global ${node.name} during SSR`
+                  });
+                } // TODO: Handle Top Level
+            // TODO: Conditionals
 
           }
         }
--- packages/eslint-plugin-react-ssr/src/index.js
+++ packages/eslint-plugin-react-ssr/src/index.js
@@ -9,34 +9,44 @@ export const rules = {
             // const ancestors = context.getAncestors(node)
             const scope = context.getScope(node)
             // console.log(ancestors, scope)
-            // Class methods
-            // if (!exclusions.includes(scope.block.parent.key.name)) {
-            //   context.report({
-            //     node,
-            //     message: `Cannot use browser global ${
-            //       node.name
-            //     } during SSR unless it is in ${exclusions.join()}`,
-            //   })
-            // }
+
+            /* Class Methods like componentDidMount, componentWillMount
+             * and componentWillUnmount
+             */
+            if (scope.block.parent.key) {
+              console.log(`Class`)
+              if (!exclusions.includes(scope.block.parent.key.name)) {
+                context.report({
+                  node,
+                  message: `Cannot use browser global ${node.name} during SSR`,
+                })
+              }
+            }
+
             // Hooks
-            // console.log(scope.block.parent.callee)
-            if (
-              scope.block.parent.callee &&
-              !exclusions.includes(scope.block.parent.callee.name)
-            ) {
+            else if (scope.block.parent.callee) {
+              console.log(`Hooks`)
+
+              if (!exclusions.includes(scope.block.parent.callee.name)) {
+                context.report({
+                  node,
+                  message: `Cannot use browser global ${node.name} during SSR`,
+                })
+                return
+              }
+            }
+
+            // Everything else
+            else {
+              console.log(`Everything else`)
               context.report({
                 node,
-                message: `Cannot use browser global ${
-                  node.name
-                } during SSR unless it is in ${exclusions.join()}`,
+                message: `Cannot use browser global ${node.name} during SSR`,
               })
             }
 
-            // Class methods
-            // Call expressions for Hooks
-            // Top level
-            // Render
-            // Conditionals
+            // TODO: Handle Top Level
+            // TODO: Conditionals
           }
         },
       }
