8cf90fba202fce765bb2b8976e2fc3c681c0604a
Francesco Di Lorenzo <fradilorenzo92@gmail.com>
2019-12-30 18:08:50 +0100

    fix(starters/blog): Remove gatsby-plugin-offline

--- starters/blog/gatsby-config.js
+++ starters/blog/gatsby-config.js
@@ -66,7 +66,6 @@ module.exports = {
         icon: `content/assets/gatsby-icon.png`,
       },
     },
-    `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-typography`,
