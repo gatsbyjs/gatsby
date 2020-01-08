b530c87d940d466d8eb6893ed318de3378f16f5b
Nicholas Capo <nicholas.capo@gmail.com>
2020-01-08 15:30:23 -0600

    feat(benchmarks): add circleci job
    Co-authored-by: Dustin Schau <dustinschau@gmail.com>

---
+++ scripts/build-benchmark.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -eux
+
+set -e
+set -u
+set -x
+
+BENCHMARKS=(
+  # create-pages
+  # image-processing
+  markdown_id
+  markdown_slug
+  markdown_table
+  # plugin-manifest
+  # query
+)
+
+SIZES=(
+    1000
+    5000
+   10000
+   25000
+  # 100000
+)
+
+cd benchmarks
+for benchmark in "${BENCHMARKS[@]}"; do
+    cd "${benchmark}"
+    for num in "${SIZES[@]}"; do
+      NUM_PAGES=${num} npm install
+      NUM_PAGES=${num} ./node_modules/.bin/gatsby build
+    done
+    cd ..
+done
