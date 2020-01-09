ed587d47a977326195df235218f44b3eb5a64079
Ward Peeters <ward@coding-tech.com>
2020-01-09 08:58:34 +0100

    forgot to update jobs

--- packages/gatsby/src/redux/reducers/jobsv2.js
+++ packages/gatsby/src/redux/reducers/jobsv2.js
@@ -6,29 +6,30 @@ module.exports = (
     case `CREATE_JOB_V2`: {
       const { job, plugin } = action.payload
 
-      state.stale.set(job.contentDigest, {
+      state.incomplete.set(job.contentDigest, {
         job,
         plugin,
       })
-      state.active.add(job.contentDigest)
 
       return state
     }
 
     case `END_JOB_V2`: {
       const { job, result } = action.payload
-      state.stale.delete(job.contentDigest)
-      state.active.delete(job.contentDigest)
+      state.incomplete.delete(job.contentDigest)
       // inputPaths is used to make sure the job is still necessary
-      state.done.set(job.contentDigest, { result, inputPaths: job.inputPaths })
+      state.complete.set(job.contentDigest, {
+        result,
+        inputPaths: job.inputPaths,
+      })
 
       return state
     }
 
     case `REMOVE_STALE_JOB_V2`: {
       const { contentDigest } = action.payload
-      state.stale.delete(contentDigest)
-      state.done.delete(contentDigest)
+      state.incomplete.delete(contentDigest)
+      state.complete.delete(contentDigest)
 
       return state
     }
