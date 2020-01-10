c759a7d6bd4c0ebfc4b783cc11686a9b4427cd41
Michael <184316+muescha@users.noreply.github.com>
2020-01-10 18:48:59 +0100

    fix(gatsby): fix error cannot read referencePath of undefined (#20517)
    * fix error cannot read referencePath of undefined
    
    * fix import path

--- packages/gatsby/src/utils/api-runner-node.js
+++ packages/gatsby/src/utils/api-runner-node.js
@@ -22,7 +22,7 @@ const { emitter, store } = require(`../redux`)
 const getPublicPath = require(`./get-public-path`)
 const { getNonGatsbyCodeFrameFormatted } = require(`./stack-trace-utils`)
 const { trackBuildError, decorateEvent } = require(`gatsby-telemetry`)
-const { default: errorParser } = require(`./api-runner-node-error-parser`)
+const { default: errorParser } = require(`./api-runner-error-parser`)
 
 // Bind action creators per plugin so we can auto-add
 // metadata to actions they create.
