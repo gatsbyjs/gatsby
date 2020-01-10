06457c4db47134eb43f119ae452922da5e0c27c3
Josh Comeau <joshwcomeau@gmail.com>
2020-01-10 18:30:39 -0500

    WIP - post

--- docs/blog/2020-01-08-git-workflows/Breakout.js
+++ docs/blog/2020-01-08-git-workflows/Breakout.js
@@ -1,25 +1,20 @@
 import React from "react"
-import { jsx } from "theme-ui"
 
-import { breakpointGutter } from "../../../www/src/utils/styles"
-
-const Breakout = ({ children }) => {
-  return (
-    <div
-      css={{
-        width: "98vw",
-        position: "relative",
-        left: "50%",
-        right: "50%",
-        marginLeft: "-49vw",
-        marginRight: "-49vw",
-        display: "grid",
-        placeContent: "center",
-      }}
-    >
-      {children}
-    </div>
-  )
-}
+const Breakout = ({ children }) => (
+  <div
+    css={{
+      width: `98vw`,
+      position: `relative`,
+      left: `50%`,
+      right: `50%`,
+      marginLeft: `-49vw`,
+      marginRight: `-49vw`,
+      display: `grid`,
+      placeContent: `center`,
+    }}
+  >
+    {children}
+  </div>
+)
 
 export default Breakout
