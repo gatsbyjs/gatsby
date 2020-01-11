5f19b34c016f2dc5e5236855f0b5a5872a5f8341
Blaine Kasten <blainekasten@gmail.com>
2020-01-11 13:13:49 -0600

    fix buildHTML reference

--- packages/gatsby/src/commands/build.js
+++ packages/gatsby/src/commands/build.js
@@ -2,7 +2,7 @@
 
 const path = require(`path`)
 const report = require(`gatsby-cli/lib/reporter`)
-const buildHTML = require(`./build-html`)
+import buildHTML from "./build-html"
 const buildProductionBundle = require(`./build-javascript`)
 const bootstrap = require(`../bootstrap`)
 const apiRunnerNode = require(`../utils/api-runner-node`)
