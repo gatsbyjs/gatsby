c2a11bf2edf77395b3763ae0e2ab2351cc8b55d6
LekoArts <lekoarts@gmail.com>
2020-01-21 17:01:33 +0100

    Remove WIP .gif stuff

--- packages/gatsby-transformer-sharp/src/on-node-create.js
+++ packages/gatsby-transformer-sharp/src/on-node-create.js
@@ -7,20 +7,10 @@ const supportedExtensions = {
   tiff: true,
 }
 
-module.exports = async function onCreateNode({
-  node,
-  actions,
-  createNodeId,
-  reporter,
-}) {
+module.exports = async function onCreateNode({ node, actions, createNodeId }) {
   const { createNode, createParentChildLink } = actions
 
   if (!supportedExtensions[node.extension]) {
-    if (node.extension === `gif`) {
-      reporter.warn(
-        `The file "${node.absolutePath}" wasn't processed by gatsby-transformer-sharp as "${node.extension}" is currently not supported.\n\nPlease use "publicURL" in your query and a normal img tag in your React component instead.\nOtherwise you'll receive "null" for your "childImageSharp" query. You could also check for a truthy value of "childImageSharp" before using e.g. gatsby-image.`
-      )
-    }
     return
   }
 
