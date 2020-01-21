cd1437ff9cc042b6f9781be1eca623d6ff3aa656
LekoArts <lekoarts@gmail.com>
2020-01-21 12:22:04 +0100

    WIP: Warn on using .gif in image sharp

--- packages/gatsby-transformer-sharp/src/on-node-create.js
+++ packages/gatsby-transformer-sharp/src/on-node-create.js
@@ -7,10 +7,20 @@ const supportedExtensions = {
   tiff: true,
 }
 
-module.exports = async function onCreateNode({ node, actions, createNodeId }) {
+module.exports = async function onCreateNode({
+  node,
+  actions,
+  createNodeId,
+  reporter,
+}) {
   const { createNode, createParentChildLink } = actions
 
   if (!supportedExtensions[node.extension]) {
+    if (node.extension === `gif`) {
+      reporter.warn(
+        `The file "${node.absolutePath}" wasn't processed by gatsby-transformer-sharp as "${node.extension}" is currently not supported.\n\nPlease use "publicURL" in your query and a normal img tag in your React component instead.\nOtherwise you'll receive "null" for your "childImageSharp" query. You could also check for a truthy value of "childImageSharp" before using e.g. gatsby-image.`
+      )
+    }
     return
   }
 
