f1fcae7fba2877e182d4b85e468d19a0e855624b
Florian Kissling <florian.kissling@gmail.com>
2020-01-26 05:15:04 +0100

    Add transition.default, .curve.fastOutLinearIn, .speed.faster, .slower
    … first step to bring stuff in line with `gatsby-interface`

--- packages/gatsby-design-tokens/src/transition.js
+++ packages/gatsby-design-tokens/src/transition.js
@@ -1,10 +1,14 @@
 export default {
+  default: `250ms cubic-bezier(0.4, 0, 0.2, 1)`,
   curve: {
     default: `cubic-bezier(0.4, 0, 0.2, 1)`,
+    fastOutLinearIn: `cubic-bezier(0.4, 0, 1, 1)`,
   },
   speed: {
-    default: `250ms`,
+    faster: `50ms`,
     fast: `100ms`,
+    default: `250ms`,
     slow: `350ms`,
+    slower: `1000ms`,
   },
 }
