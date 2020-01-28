3dbb4473d2cd035e236b7d17b17bcef00fbdc41f
Tyler Barnes <tylerdbarnes@gmail.com>
2020-01-28 13:27:06 -0800

    fix post_save and post trash firing twice, and add menu and menuItem actions

--- packages/wp-gatsby/src/ActionMonitor/ActionMonitor.php
+++ packages/wp-gatsby/src/ActionMonitor/ActionMonitor.php
@@ -17,13 +17,57 @@ class ActionMonitor
     function __construct()
     {
         add_action(
-            'init',
-            function () {
-                $this->initPostType();
-                $this->monitorActions();
-                $this->registerGraphQLFields();
-            }
+          'init',
+          function () {
+              $this->initPostType();
+          }
         );
+        $this->registerGraphQLFields();
+        $this->monitorActions();
+    }
+
+    // $args = $action_type, $title, $status, $node_id, $relay_id, $graphql_single_name, $graphql_plural_name
+    function insertNewAction($args) {
+      $action_monitor_post = \wp_insert_post(
+            [
+                'post_title'    => "[{$args['action_type']}] - {$args['title']}",
+                'post_type'     => 'action_monitor',
+                'post_status'   => 'publish',
+            ]
+      );
+
+      if ($action_monitor_post !== 0) {
+          \update_post_meta(
+              $action_monitor_post,
+              'action_type',
+              $args['action_type']
+          );
+          \update_post_meta(
+              $action_monitor_post,
+              'referenced_node_status',
+              $args['status'], // menus don't have post status. This is for Gatsby
+          );
+          \update_post_meta(
+              $action_monitor_post,
+              'referenced_node_id',
+              $args['node_id']
+          );
+          \update_post_meta(
+              $action_monitor_post,
+              'referenced_node_relay_id',
+              $args['relay_id']
+          );
+          \update_post_meta(
+              $action_monitor_post,
+              'referenced_node_single_name',
+              $args['graphql_single_name']
+          );
+          \update_post_meta(
+              $action_monitor_post,
+              'referenced_node_plural_name',
+              $args['graphql_plural_name']
+          );
+      }
     }
 
     /**
@@ -31,7 +75,72 @@ class ActionMonitor
      */
     function monitorActions()
     {
+        // Post / Page actions
         add_action('save_post', [$this, 'savePost'], 10, 2);
+
+        // Menu actions
+        add_action( 'wp_update_nav_menu', function( $menu_id ) { 
+          $this->saveMenu( $menu_id, 'UPDATE' ); 
+        } );
+
+        add_action( 'wp_create_nav_menu', function( $menu_id ) { 
+          $this->saveMenu( $menu_id, 'CREATE' ); 
+        } );
+        add_action( 'wp_delete_nav_menu', [ $this, 'deleteMenu' ], 10, 1 );
+    }
+
+    function deleteMenu( $menu_id ) {
+      error_log(print_r('delete menu', true)); 
+
+      $global_relay_id = Relay::toGlobalId(
+            'Menu',
+            $menu_id
+        );
+
+      $this->insertNewAction([
+        'action_type' => 'DELETE',
+        'title' => "Menu #${menu_id}",
+        'status' => 'trash', // menus don't have post status. This is for Gatsby
+        'node_id' => $menu_id,
+        'relay_id' => $global_relay_id,
+        'graphql_single_name' => 'menu',
+        'graphql_plural_name' => 'menus',
+      ]);
+    }
+
+    /**
+     * On save menus (created/updated)
+     */
+    function saveMenu( $menu_id, $action_type ) {
+      if (
+        did_action('wp_update_nav_menu') > 1 || 
+        did_action('wp_create_nav_menu') > 1
+      ) {
+        return $menu_id;
+      }
+
+      // Get a menu object
+      $menu_object = wp_get_nav_menu_object( $menu_id );
+
+      // Bail if not a menu object
+      if ( empty( $menu_object ) ) {
+        return $menu_id;
+      }
+
+      $global_relay_id = Relay::toGlobalId(
+            'Menu',
+            $menu_id
+        );
+
+      $this->insertNewAction([
+        'action_type' => $action_type,
+        'title' => $menu_object->name,
+        'status' => 'publish', // menus don't have post status. This is for Gatsby
+        'node_id' => $menu_id,
+        'relay_id' => $global_relay_id,
+        'graphql_single_name' => 'menu',
+        'graphql_plural_name' => 'menus',
+      ]);
     }
 
     /**
@@ -39,6 +148,10 @@ class ActionMonitor
      */
     function savePost($post_id, $post)
     {
+        if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+            return $post_id;
+        }
+
         if ($post->post_status === 'auto-draft') {
             return $post_id;
         }
@@ -51,9 +164,31 @@ class ActionMonitor
             return $post_id;
         }
 
-        if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
-            return $post_id;
+        if (did_action('save_post') > 1) {
+          return $post_id;
         }
+
+        $post_type_object = \get_post_type_object($post->post_type);
+
+        $title = $post->post_title ?? '';
+        $global_relay_id = '';
+        $global_relay_id = Relay::toGlobalId(
+            $post_type_object->name,
+            absint($post_id)
+        );
+
+        $referenced_node_single_name 
+          = $post_type_object->graphql_single_name ?? null;
+        $referenced_node_plural_name
+          = $post_type_object->graphql_plural_name ?? null;
+
+        if ($post->post_type === 'nav_menu_item') {
+            $global_relay_id = Relay::toGlobalId( 'nav_menu_item', $post_id );
+            $title = "MenuItem #$post_id";
+            $referenced_node_single_name = 'menuItem';
+            $referenced_node_plural_name = 'menuItems';
+        }
+
 
         //
         // add a check here to make sure this is a post type available in WPGQL
@@ -64,20 +199,27 @@ class ActionMonitor
 
         $action_type = null;
 
+        // this post meta helps determine if this is a new post or not
+        // since the 3rd argument in the save_post hook "$update" always
+        // returns true unless using wp_insert_post(), which we're not
+        $update = get_post_meta($post_id, '__update', true);
+
+
+        if ($post->post_status === 'trash' && !$update) {
+            // this post has already been trashed, Gutenberg just fires post_save 2x on trash. It happens in separate threads so did_action('post_save') doesn't increment. :(
+            return $post_id;
+        } 
+
         if ($post->post_status === 'trash') {
             $action_type = 'DELETE';
             // when we delete a post, Gatsby deletes it
             // that means when we untrash a post, it's a new
-            // node for Gatsby. If WP think of untrashing as an update
+            // node for Gatsby. If WP thinks of untrashing as an update
             // rather than a create, Gatsby will error.
             // So set __update to false, when next time this post
             // is untrashed, we'll record it as a new post.
             update_post_meta($post_id, '__update', false);
         } else {
-            // this post meta helps determine if this is a new post or not
-            // since the 3rd argument in the save_post hook "$update" always
-            // returns true unless using wp_insert_post(), which we're not
-            $update = get_post_meta($post_id, '__update', true);
             if (!$update) {
                 update_post_meta($post_id, '__update', true);
             }
@@ -85,52 +227,16 @@ class ActionMonitor
             $action_type = $update ? 'UPDATE' : 'CREATE';
         }
 
-        $post_type_object = \get_post_type_object($post->post_type);
-        $global_relay_id = Relay::toGlobalId(
-            $post_type_object->name,
-            absint($post_id)
-        );
 
-        $action_monitor_post = \wp_insert_post(
-            [
-                'post_title'    => "[$action_type] - {$post->post_title}",
-                'post_type'     => 'action_monitor',
-                'post_status'   => 'publish',
-            ]
-        );
-
-        if ($action_monitor_post !== 0) {
-            \update_post_meta(
-                $action_monitor_post,
-                'action_type',
-                $action_type
-            );
-            \update_post_meta(
-                $action_monitor_post,
-                'referenced_post_status',
-                $post->post_status
-            );
-            \update_post_meta(
-                $action_monitor_post,
-                'referenced_post_id',
-                $post_id
-            );
-            \update_post_meta(
-                $action_monitor_post,
-                'referenced_post_relay_id',
-                $global_relay_id
-            );
-            \update_post_meta(
-                $action_monitor_post,
-                'referenced_post_single_name',
-                $post_type_object->graphql_single_name
-            );
-            \update_post_meta(
-                $action_monitor_post,
-                'referenced_post_plural_name',
-                $post_type_object->graphql_plural_name
-            );
-        }
+        $this->insertNewAction([
+          'action_type' => $action_type,
+          'title' => $title,
+          'status' => $post->post_status,
+          'node_id' => $post_id,
+          'relay_id' => $global_relay_id,
+          'graphql_single_name' => $referenced_node_single_name,
+          'graphql_plural_name' => $referenced_node_plural_name,
+        ]);
     }
 
     function registerPostGraphQLFields() {
@@ -163,12 +269,12 @@ class ActionMonitor
                             'WP_Gatsby'
                         ),
                         'resolve' => function ($post) {
-                            $referenced_post_status = get_post_meta(
+                            $referenced_node_status = get_post_meta(
                                 $post->ID,
-                                'referenced_post_status',
+                                'referenced_node_status',
                                 true
                             );
-                            return $referenced_post_status ?? null;
+                            return $referenced_node_status ?? null;
                         }
                     ]
                 );
@@ -182,12 +288,12 @@ class ActionMonitor
                             'WP_Gatsby'
                         ),
                         'resolve' => function ($post) {
-                            $referenced_post_id = get_post_meta(
+                            $referenced_node_id = get_post_meta(
                                 $post->ID,
-                                'referenced_post_id',
+                                'referenced_node_id',
                                 true
                             );
-                            return $referenced_post_id ?? null;
+                            return $referenced_node_id ?? null;
                         }
                     ]
                 );
@@ -201,12 +307,12 @@ class ActionMonitor
                             'WP_Gatsby'
                         ),
                         'resolve' => function ($post) {
-                            $referenced_post_relay_id = get_post_meta(
+                            $referenced_node_relay_id = get_post_meta(
                                 $post->ID,
-                                'referenced_post_relay_id',
+                                'referenced_node_relay_id',
                                 true
                             );
-                            return $referenced_post_relay_id ?? null;
+                            return $referenced_node_relay_id ?? null;
                         }
                     ]
                 );
@@ -220,12 +326,12 @@ class ActionMonitor
                             'WP_Gatsby'
                         ),
                         'resolve' => function ($post) {
-                            $referenced_post_single_name = get_post_meta(
+                            $referenced_node_single_name = get_post_meta(
                                 $post->ID,
-                                'referenced_post_single_name',
+                                'referenced_node_single_name',
                                 true
                             );
-                            return $referenced_post_single_name ?? null;
+                            return $referenced_node_single_name ?? null;
                         }
                     ]
                 );
@@ -239,12 +345,12 @@ class ActionMonitor
                             'WP_Gatsby'
                         ),
                         'resolve' => function ($post) {
-                            $referenced_post_plural_name = get_post_meta(
+                            $referenced_node_plural_name = get_post_meta(
                                 $post->ID,
-                                'referenced_post_plural_name',
+                                'referenced_node_plural_name',
                                 true
                             );
-                            return $referenced_post_plural_name ?? null;
+                            return $referenced_node_plural_name ?? null;
                         }
                     ]
                 );
