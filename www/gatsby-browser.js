3526072f1fc7175110b1bfb733863c84e44b4bbb
madalynrose <madalyn@gatsbyjs.com>
2020-01-31 16:47:46 -0500

    remove irrelevant changes

--- www/gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-exports.onRouteUpdate = ({ location, prevLocation }) => {
-  if (prevLocation !== null) {
-    const skipLink = document.querySelector("[data-reach-skip-link]")
-    if (skipLink) {
-      skipLink.focus()
-    }
-  }
-}
