910d18ce860f2af73a5f3d20e5e504c100972d8f
madalynrose <madalyn@gatsbyjs.com>
2020-01-31 15:21:05 -0500

    change query selector for skip link

--- www/gatsby-browser.js
+++ www/gatsby-browser.js
@@ -1,6 +1,6 @@
 exports.onRouteUpdate = ({ location, prevLocation }) => {
   if (prevLocation !== null) {
-    const skipLink = document.querySelector("#reach-skip-nav")
+    const skipLink = document.querySelector("[data-reach-skip-link]")
     if (skipLink) {
       skipLink.focus()
     }
