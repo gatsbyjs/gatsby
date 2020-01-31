c8e6c90f70772f701d1a0230fc40c9eea7b38269
Tyler Barnes <tylerdbarnes@gmail.com>
2020-01-31 10:38:23 -0800

    remove child node logic. not needed "nodes should be atomic"

--- packages/gatsby-source-wordpress-experimental/src/steps/source-nodes/create-nodes/create-nodes.js
+++ packages/gatsby-source-wordpress-experimental/src/steps/source-nodes/create-nodes/create-nodes.js
@@ -1,4 +1,3 @@
-import flattenDeep from "lodash/flattenDeep"
 import execall from "execall"
 import fetchReferencedMediaItemsAndCreateNodes from "../fetch-nodes/fetch-referenced-media-items"
 import urlToPath from "~/utils/url-to-path"
@@ -12,56 +11,6 @@ import {
 } from "~/steps/create-schema-customization/helpers"
 
 // const imgSrcRemoteFileRegex = /(?:src=\\")((?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[A-Z0-9+&@#/%=~_|$])\.(?:jpeg|jpg|png|gif|ico|pdf|doc|docx|ppt|pptx|pps|ppsx|odt|xls|psd|mp3|m4a|ogg|wav|mp4|m4v|mov|wmv|avi|mpg|ogv|3gp|3g2|svg|bmp|tif|tiff|asf|asx|wm|wmx|divx|flv|qt|mpe|webm|mkv|txt|asc|c|cc|h|csv|tsv|ics|rtx|css|htm|html|m4b|ra|ram|mid|midi|wax|mka|rtf|js|swf|class|tar|zip|gz|gzip|rar|7z|exe|pot|wri|xla|xlt|xlw|mdb|mpp|docm|dotx|dotm|xlsm|xlsb|xltx|xltm|xlam|pptm|ppsm|potx|potm|ppam|sldx|sldm|onetoc|onetoc2|onetmp|onepkg|odp|ods|odg|odc|odb|odf|wp|wpd|key|numbers|pages))(?=\\"| |\.)/gim
-
-const recursivelySearchForIds = ([key, value]) => {
-  if (!key || !value) {
-    return null
-  }
-
-  if (key === `id`) {
-    return value
-  }
-
-  if (typeof value === `string` || typeof value === `number`) {
-    return null
-  }
-
-  if (Array.isArray(value)) {
-    // loop through each value of the array. If it's an object recurse on it's fields
-    // if it's anything else skip it.
-    value.map(innerValue => {
-      if (innerValue === null) {
-        return null
-      }
-
-      if (typeof innerValue === `object`) {
-        return Object.values(innerValue).map(recursivelySearchForIds)
-      }
-
-      return null
-    })
-  } else if (typeof value === `object`) {
-    return Object.entries(value).map(recursivelySearchForIds)
-  }
-
-  return null
-}
-
-const findNodeChildNodeIds = node => {
-  const childNodeIds = [
-    ...new Set(
-      flattenDeep(Object.entries(node).map(recursivelySearchForIds)).filter(
-        id => id !== node.id && !!id
-      )
-    ),
-  ]
-
-  if (!childNodeIds.length) {
-    return null
-  }
-
-  return childNodeIds
-}
 
 export const createGatsbyNodesFromWPGQLContentNodes = async ({
   wpgqlNodesByContentType,
@@ -108,8 +57,6 @@ export const createGatsbyNodesFromWPGQLContentNodes = async ({
         }
       }
 
-      const childNodeIds = findNodeChildNodeIds(node)
-
       const remoteNode = {
         ...node,
         id: node.id,
@@ -118,10 +65,6 @@ export const createGatsbyNodesFromWPGQLContentNodes = async ({
           contentDigest: createContentDigest(node),
           type: buildTypeName(node.type),
         },
-      }
-
-      if (childNodeIds) {
-        remoteNode.children = childNodeIds
       }
 
       const typeSettings = getTypeSettingsByType({
