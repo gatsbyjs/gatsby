57744cd5a1fe62c1fa5745fa097507959910c9d3
Florian Kissling <florian.kissling@gmail.com>
2020-02-06 16:11:28 +0100

    Add gatsbyjs.org theme
    Both themes don't tree-shake yet, so no agadoo.
    This enables
    
    - `import { theme as default } from "gatsby-design-tokens/dist/theme-gatsbyjs-org"` in `www/src/gatsby-plugin-theme-ui/index`
    - directly import individual token groups with `import { mediaQueries } from "gatsby-design-tokens/dist/theme-gatsbyjs-org"`

--- packages/gatsby-design-tokens/src/font-weights.js
+++ packages/gatsby-design-tokens/src/font-weights.js
@@ -2,6 +2,6 @@ export default {
   body: 400,
   semiBold: 600,
   bold: 700,
-  heading: 700,
   extraBold: 800,
+  heading: 700,
 }
--- packages/gatsby-design-tokens/src/theme.js
+++ packages/gatsby-design-tokens/src/theme.js
@@ -1,92 +1,106 @@
-import { default as breakpoints } from "./breakpoints"
-import { default as colors } from "./colors"
-import { default as fonts } from "./fonts"
-import { default as fontSizes } from "./font-sizes"
-import { default as fontWeights } from "./font-weights"
-import { default as letterSpacings } from "./letter-spacings"
-import { default as lineHeights } from "./line-heights"
-import { default as mediaQueries } from "./media-queries"
-import { default as space } from "./space"
-import { default as radii } from "./radii"
-import { default as transition } from "./transition"
-import { default as shadows } from "./shadows"
-import { default as zIndices } from "./z-indices"
+import { default as breakpointsTokens } from "./breakpoints"
+import { default as colorsTokens } from "./colors"
+import { default as fontsTokens } from "./fonts"
+import { default as fontSizesTokens } from "./font-sizes"
+import { default as fontWeightsTokens } from "./font-weights"
+import { default as letterSpacingsTokens } from "./letter-spacings"
+import { default as lineHeightsTokens } from "./line-heights"
+import { default as mediaQueriesTokens } from "./media-queries"
+import { default as spaceTokens } from "./space"
+import { default as radiiTokens } from "./radii"
+import { default as transitionTokens } from "./transition"
+import { default as shadowsTokens } from "./shadows"
+import { default as zIndicesTokens } from "./z-indices"
 
 // gatsby-theme-ui-theme-default
 // provides a theme-ui theme using gatsby-design-tokens
 
 const pxToRem = (value, fontSizeInPX = 16) => value / fontSizeInPX + `rem`
 
+// let's tweak some values then, shall we?
+
 // breakpoints
 // theme-ui requires an array
 let bp = []
-for (const b in breakpoints) {
-  bp.push(breakpoints[b])
+for (const b in breakpointsTokens) {
+  bp.push(breakpointsTokens[b])
 }
 
 // colors
 // extend colors with theme-ui required keys
 // https://theme-ui.com/theme-spec#color
 const c = {
-  ...colors,
+  ...colorsTokens,
   // Body foreground color
   // overwrite what's currently in _colors_ from _gatsby-design-tokens_
   // also see _heading_ key below
-  text: colors.grey[80], // colors.text.primary
+  text: colorsTokens.grey[80], // colors.text.primary
   // Body background color
-  background: colors.white,
+  background: colorsTokens.white,
   // Primary brand color for links, buttons, etc.
-  primary: colors.gatsby,
+  primary: colorsTokens.gatsby,
   // A secondary brand color for alternative styling
-  secondary: colors.purple[40],
+  secondary: colorsTokens.purple[40],
   // A contrast color for emphasizing UI
-  accent: colors.orange[60],
+  accent: colorsTokens.orange[60],
   // A faint color for backgrounds, borders, and accents that do not require high contrast with the background color
-  muted: colors.grey[5],
+  muted: colorsTokens.grey[5],
   // end Theme-UI required keys
+
   // gatsby-design-tokens has the following in colors.text,
   // which conflicts with theme-ui's default color _text_
   // making text.header and text.secondary available as
   // _heading_ and _textMuted_ resolves that
-  heading: colors.text.header, // text.header
-  textMuted: colors.text.secondary, // text.secondary
+  heading: colorsTokens.text.header, // text.header
+  textMuted: colorsTokens.text.secondary, // text.secondary
 }
 
 // fonts
 // theme-ui requires a CSS string
 let f = {}
-for (const fontFamily in fonts) {
-  f[fontFamily] = fonts[fontFamily].join(`, `)
+for (const fontFamily in fontsTokens) {
+  f[fontFamily] = fontsTokens[fontFamily].join(`, `)
 }
 
-// we are targeting web, and choose to deliver thos as _rem_, not px
+// we are targeting web, and choose to deliver these as _rem_, not px
 // fontSizes
+const fs = fontSizesTokens.map(token => pxToRem(token))
 // space
+const s = spaceTokens.map(token => pxToRem(token))
 
-export default {
+export const theme = {
   breakpoints: bp,
   colors: c,
   fonts: f,
-  fontSizes: fontSizes.map(token => pxToRem(token)),
-  space: space.map(token => pxToRem(token)),
-  fontWeights: fontWeights,
-  letterSpacings: letterSpacings,
-  lineHeights: lineHeights,
-  mediaQueries: mediaQueries,
-  radii: radii,
-  shadows: shadows,
-  transition: transition,
-  zIndices: zIndices,
+  fontSizes: fs,
+  space: s,
+  fontWeights: fontWeightsTokens,
+  letterSpacings: letterSpacingsTokens,
+  lineHeights: lineHeightsTokens,
+  mediaQueries: mediaQueriesTokens,
+  radii: radiiTokens,
+  shadows: shadowsTokens,
+  transition: transitionTokens,
+  zIndices: zIndicesTokens,
 }
 
-// // individual exports
-// // breakpoints as array of pixel values (as opposed to object)
-// export { bp as breakpoints }
-// // colors extended with theme-ui required values
-// export { c as colors }
-// // fonts as CSS string (as opposed to array of font names)
-// export { f as fonts }
-// // fontSizes as `rem` (as opposed to pixels)
-// export { fs as fontSizes }
-// // space as `rem` (as opposed to pixels)
-// export { s as space }
+// individual exports
+// breakpoints as array of pixel values (as opposed to object)
+export { bp as breakpoints }
+// colors extended with theme-ui required values
+export { c as colors }
+// fonts as CSS string (as opposed to array of font names)
+export { f as fonts }
+// fontSizes as `rem` (as opposed to pixels)
+export { fs as fontSizes }
+// space as `rem` (as opposed to pixels)
+export { s as space }
+// pass through unmodified tokens
+export { fontWeightsTokens as fontWeights }
+export { letterSpacingsTokens as letterSpacings }
+export { lineHeightsTokens as lineHeights }
+export { mediaQueriesTokens as mediaQueries }
+export { radiiTokens as radii }
+export { transitionTokens as transition }
+export { shadowsTokens as shadows }
+export { zIndicesTokens as zIndices }
