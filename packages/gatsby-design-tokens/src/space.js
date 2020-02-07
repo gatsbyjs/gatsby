79caddc468a0bddaaef8e7ea905d8aec20014bc8
Florian Kissling <florian.kissling@gmail.com>
2020-02-07 05:37:18 +0100

    Too clever

--- packages/gatsby-design-tokens/src/space.js
+++ packages/gatsby-design-tokens/src/space.js
@@ -2,9 +2,8 @@ import preval from "preval.macro"
 
 const s = preval`
   const space = [0, 4, 8, 12, 16, 20, 24, 32, 40, 48, 56, 64, 72]
-  let spacePx = space.map(t => t + "px")
-  let spaceRem = space.map(t => t / 16 + "rem")
-  spacePx[0] = spaceRem[0] = 0
+  const spacePx = space.map(t => t + "px")
+  const spaceRem = space.map(t => t / 16 + "rem")
 
   module.exports = { space, spacePx, spaceRem }
 `
