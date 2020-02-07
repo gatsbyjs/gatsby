2941e0eb16df7c06fc8381b3b0ab6f199a3563c2
Srikanth Sethumadhavan <ohmsree@gmail.com>
2020-02-07 20:46:52 +0530

    fixed a silly copy paste error

--- packages/gatsby-plugin-google-tagmanager/src/gatsby-browser.js
+++ packages/gatsby-plugin-google-tagmanager/src/gatsby-browser.js
@@ -2,7 +2,7 @@ exports.onRouteUpdate = ({ location, prevLocation }, pluginOptions) => {
   if (
     (process.env.NODE_ENV === `production` ||
       pluginOptions.includeInDevelopment) &&
-    prevLocation·&&·prevLocation.pathname·!==·location.pathname
+    prevLocation && prevLocation.pathname !== location.pathname
   ) {
     // wrap inside a timeout to ensure the title has properly been changed
     setTimeout(() => {
