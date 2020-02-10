c90501426c25b68965c466dd6ee2e7eafd8a1cd4
Nat Alison <tesseralis@gmail.com>
2020-02-10 13:17:46 -0800

    feat(www): Convert API pages to MDX (#20763)
    * Progress snapshot with working frontmatter for jsdoc and apicalls. Ported some api docs to MDX, but they still need to be verified. More testing is needed.
    
    * Converted remaining api doc pages to MDX. The API MDX temple now injects data directly to be used within components of hte page, all MDX files have been adjusted. This exposes the headings for translation.
    
    * Hide all document for `typedef`. Added @private annotation to `deleteNodes` doc.
    
    * The adjustments to api-node-helpers-docs.js put what should be documented in the root of the file, which is consistent with the other doc files.
    
    * The jsdoc parameter now takes a list of strings referencing specific files, not a regexp. Updated mdx to new convention.
    
    * Added apicalls values for stub documents that should have github links. Perhaps someday they can be added with the suggested names. Fixed issue where types were not shown on top level items in `node-api-helpers.mdx`. They were a side-effect of how the api jsdoc was written/queried, should they not exist?
    
    * Changed headings over api document lists to their actual text from the old docs.
    
    * Found more docs with `showTopLevelSignatures={true}`
    
    * Removed unnecessary commented code and a debugging `console.log`
    
    * update mdx pages
    
    * simplify everything!
    
    * whoops
    
    * move utility file to the same page
    
    * Update www/gatsby-node.js
    
    Co-Authored-By: Michal Piechowiak <misiek.piechowiak@gmail.com>
    
    * Update docs/docs/node-api-helpers.md
    
    Co-Authored-By: Michal Piechowiak <misiek.piechowiak@gmail.com>
    
    * Update docs/docs/actions.md
    
    Co-Authored-By: Michal Piechowiak <misiek.piechowiak@gmail.com>
    
    * use javascript:title
    
    * add custom descriptions
    
    Co-authored-by: Bailey Swartz <53988640+sever1an@users.noreply.github.com>
    Co-authored-by: Michal Piechowiak <misiek.piechowiak@gmail.com>

--- www/gatsby-node.js
+++ www/gatsby-node.js
@@ -18,15 +18,6 @@ const {
 const { getPrevAndNext } = require(`./src/utils/get-prev-and-next.js`)
 const { localizedPath } = require(`./src/utils/i18n.js`)
 const yaml = require(`js-yaml`)
-const docLinksData = yaml.load(
-  fs.readFileSync(`./src/data/sidebars/doc-links.yaml`)
-)
-const tutorialLinksData = yaml.load(
-  fs.readFileSync(`./src/data/sidebars/tutorial-links.yaml`)
-)
-const contributingLinksData = yaml.load(
-  fs.readFileSync(`./src/data/sidebars/contributing-links.yaml`)
-)
 const ecosystemFeaturedItems = yaml.load(
   fs.readFileSync(`./src/data/ecosystem/featured-items.yaml`)
 )
@@ -89,6 +80,7 @@ exports.createPages = ({ graphql, actions, reporter }) => {
 
   return new Promise((resolve, reject) => {
     const docsTemplate = path.resolve(`src/templates/template-docs-markdown.js`)
+    const apiTemplate = path.resolve(`src/templates/template-api-markdown.js`)
     const blogPostTemplate = path.resolve(`src/templates/template-blog-post.js`)
     const blogListTemplate = path.resolve(`src/templates/template-blog-list.js`)
     const tagTemplate = path.resolve(`src/templates/tags.js`)
@@ -134,6 +126,8 @@ exports.createPages = ({ graphql, actions, reporter }) => {
                 publishedAt
                 issue
                 tags
+                jsdoc
+                apiCalls
               }
             }
           }
@@ -358,17 +352,41 @@ exports.createPages = ({ graphql, actions, reporter }) => {
         if (!slug) return
 
         if (!_.includes(slug, `/blog/`)) {
-          createPage({
-            path: localizedPath(locale, node.fields.slug),
-            component: slash(
-              node.fields.package ? localPackageTemplate : docsTemplate
-            ),
-            context: {
-              slug: node.fields.slug,
-              locale,
-              ...getPrevAndNext(node.fields.slug),
-            },
-          })
+          const prevAndNext = getPrevAndNext(node.fields.slug)
+
+          if (node.frontmatter.jsdoc) {
+            // API template
+            createPage({
+              path: localizedPath(locale, node.fields.slug),
+              component: slash(apiTemplate),
+              context: {
+                slug: node.fields.slug,
+                jsdoc: node.frontmatter.jsdoc,
+                apiCalls: node.frontmatter.apiCalls,
+                ...prevAndNext,
+              },
+            })
+          } else if (node.fields.package) {
+            // Local package template
+            createPage({
+              path: `${node.fields.slug}`,
+              component: slash(localPackageTemplate),
+              context: {
+                slug: node.fields.slug,
+              },
+            })
+          } else {
+            // Docs template
+            createPage({
+              path: localizedPath(locale, node.fields.slug),
+              component: slash(docsTemplate),
+              context: {
+                slug: node.fields.slug,
+                locale,
+                ...prevAndNext,
+              },
+            })
+          }
         }
       })
 
