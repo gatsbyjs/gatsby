bf2d0cecc5360357b031c9e32c1b20c37577893e
Nat Alison <tesseralis@gmail.com>
2020-02-10 19:44:07 -0800

    inherit from layout in ecosystem page and fix weird styling

--- www/src/components/layout/layout-with-heading.js
+++ www/src/components/layout/layout-with-heading.js
@@ -1,49 +1,22 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui"
 import PropTypes from "prop-types"
-import { Helmet } from "react-helmet"
 
-import Banner from "../banner"
 import PageHeading from "./page-heading"
-import Navigation from "../navigation"
-import MobileNavigation from "../navigation-mobile"
+import BaseLayout from "../layout"
 import { mediaQueries } from "../../gatsby-plugin-theme-ui"
-import SkipNavLink from "../skip-nav-link"
-
-// Import Futura PT typeface
-import "../../assets/fonts/futura"
 
 const LayoutWithHeading = props => {
-  const {
-    children,
-    location: { pathname },
-    pageTitle = ``,
-    pageIcon,
-  } = props
-
-  const isHomepage = pathname === `/`
+  const { children, location, pageTitle = ``, pageIcon } = props
 
   return (
-    <div className={` ${isHomepage ? `isHomepage` : ``}`}>
-      <Helmet>
-        <title>{pageTitle ? `${pageTitle} | GatsbyJS` : `GatsbyJS`}</title>
-        <meta name="twitter:site" content="@gatsbyjs" />
-        <meta name="og:type" content="website" />
-        <meta name="og:site_name" content="GatsbyJS" />
-        <link rel="canonical" href={`https://gatsbyjs.org${pathname}`} />
-        <html lang="en" />
-      </Helmet>
-      <SkipNavLink />
-      <Banner />
-      <Navigation pathname={props.location.pathname} />
+    <BaseLayout location={location}>
       <div
         sx={{
-          pt: t => t.sizes.bannerHeight,
           pb: t => t.fontSizes[10],
 
           [mediaQueries.md]: {
             ml: t => t.sizes.pageHeadingDesktopWidth,
-            pt: t => `calc(${t.sizes.bannerHeight} + ${t.sizes.headerHeight})`,
             pb: 0,
           },
         }}
@@ -51,8 +24,7 @@ const LayoutWithHeading = props => {
         {pageTitle && <PageHeading title={pageTitle} icon={pageIcon} />}
         {children}
       </div>
-      <MobileNavigation />
-    </div>
+    </BaseLayout>
   )
 }
 
