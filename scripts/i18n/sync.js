c1729f76942a20b518ba3e86f965dc7ff7670a64
Nat Alison <tesseralis@gmail.com>
2020-02-11 16:03:21 -0800

    Create another pull request with non-conflicts

--- scripts/i18n/sync.js
+++ scripts/i18n/sync.js
@@ -51,6 +51,62 @@ async function getRepository(owner, name) {
   )
   return repository
 }
+async function createPullRequest(input) {
+  const { createPullRequest } = await graphql(
+    `
+      mutation($input: CreatePullRequestInput!) {
+        createPullRequest(input: $input) {
+          pullRequest {
+            id
+            number
+          }
+        }
+      }
+    `,
+    {
+      headers: {
+        authorization: `token ${process.env.GITHUB_BOT_AUTH_TOKEN}`,
+        accept: `application/vnd.github.shadow-cat-preview+json`,
+      },
+      input,
+    }
+  )
+  return createPullRequest
+}
+
+function conflictPRBody(conflictFiles, comparisonUrl) {
+  return `
+Sync conflicts with the source repo. Please update the translations based on updated source content.
+
+<details ${conflictFiles.length <= 10 ? `open` : ``}>
+<summary>The following ${
+    conflictFiles.length
+  } files have conflicts:</summary><br />
+
+${conflictFiles.map(file => `* [ ] ${file}`).join(`\n`)}
+</details>
+
+Once all the commits have been fixed, mark this pull request as "Ready for review" and merge it in!
+
+See all changes since the last sync here:
+
+${comparisonUrl}
+
+NOTE: Do *NOT* squash-merge this pull request. The sync script requires a ref to the source repo in order to work correctly.
+  `
+}
+
+function syncPRBody(conflictPRNumber) {
+  return `
+Sync all non-conflicting files with the source repo.
+This PR contains all updates that do not cause any conflicts and can be merged immediately.
+
+Pull Request #${conflictPRNumber} was created to resolve conflicts during merge.
+Please resolve the conflicts in that PR and merge it in as well.
+
+NOTE: Do *NOT* squash-merge this pull request. The sync script requires a ref to the source repo in order to work correctly.
+  `
+}
 
 async function syncTranslationRepo(code) {
   logger = log4js.getLogger(`sync:` + code)
@@ -83,9 +139,9 @@ async function syncTranslationRepo(code) {
   const comparisonUrl = `${sourceRepoUrl}/compare/${shortBaseHash}..${shortHash}`
 
   // Check out a new branch
-  const syncBranch = `sync-${shortHash}`
-  if (shell.exec(`git checkout ${syncBranch}`).code !== 0) {
-    shell.exec(`git checkout -b ${syncBranch}`)
+  const conflictBranch = `conflicts-${shortHash}`
+  if (shell.exec(`git checkout ${conflictBranch}`).code !== 0) {
+    shell.exec(`git checkout -b ${conflictBranch}`)
   }
 
   // pull from the source
@@ -106,7 +162,10 @@ async function syncTranslationRepo(code) {
   const conflictFiles = conflictLines.map(line =>
     line.substr(line.lastIndexOf(` `) + 1)
   )
-  // Add all the conflicts
+  // Do a soft reset and unstage non-conflicted files
+  shell.exec(`git reset`)
+
+  // Add all the conflicts as-is
   shell.exec(`git add ${conflictFiles.join(` `)}`)
 
   const removedLines = lines.filter(line =>
@@ -125,76 +184,52 @@ async function syncTranslationRepo(code) {
   // if (conflictFiles.length === 0) {
   //   logger.info(`No conflicts found. Committing directly to master.`)
   //   shell.exec(`git checkout master`)
-  //   shell.exec(`git merge ${syncBranch}`)
+  //   shell.exec(`git merge ${conflictBranch}`)
   //   shell.exec(`git push origin master`)
   //   process.exit(0)
   // }
 
-  // Do a soft reset and add the conflict files as-is
-  // shell.exec(`git reset`)
-  // // clean out the rest of the changed files
-  // shell.exec(`git checkout -- .`)
-  // shell.exec(`git clean -fd`)
+  // clean out the rest of the changed files
+  shell.exec(`git checkout -- .`)
+  shell.exec(`git clean -fd`)
 
   // Commit the conflicts into the new branch and push it
   shell.exec(`git commit -m "Commit git conflicts"`)
-  shell.exec(`git push -u origin ${syncBranch}`)
+  shell.exec(`git push -u origin ${conflictBranch}`)
 
   // TODO if there is already an existing PR, don't create a new one
 
   const repository = await getRepository(owner, transRepoName)
 
-  const body = `
-Sync with the source repo. Please update the translations based on updated source content.
+  const { number } = await createPullRequest({
+    repositoryId: repository.id,
+    baseRefName: `master`,
+    headRefName: conflictBranch,
+    title: `(sync) Resolve conflicts with ${sourceRepo} @ ${shortHash}`,
+    body: conflictPRBody(conflictFiles, comparisonUrl),
+    maintainerCanModify: true,
+    draft: true,
+  })
 
-<details ${conflictFiles.length <= 10 ? `open` : ``}>
-<summary>The following ${
-    conflictFiles.length
-  } files have conflicts:</summary><br />
+  // if we successfully publish the PR, pull again and create a new PR --
+  // this time taking *only* the non-conflicting files
+  shell.exec(`git checkout master`)
 
-${conflictFiles.map(file => `* ${file}`).join(`\n`)}
-</details>
+  const syncBranch = `sync-${shortHash}`
+  if (shell.exec(`git checkout ${syncBranch}`).code !== 0) {
+    shell.exec(`git checkout -b ${syncBranch}`)
+  }
+  shell.exec(`git pull source master --no-edit --strategy-option ours`)
+  shell.exec(`git push -u origin ${syncBranch}`)
 
-Once all the commits have been fixed, mark this pull request as "Ready for review" and merge it in!
-
-See all changes since the last sync here:
-
-${comparisonUrl}
-  `
-
-  // create a new draft PR
-  await graphql(
-    `
-      mutation($input: CreatePullRequestInput!) {
-        createPullRequest(input: $input) {
-          pullRequest {
-            id
-          }
-        }
-      }
-    `,
-    {
-      headers: {
-        authorization: `token ${process.env.GITHUB_BOT_AUTH_TOKEN}`,
-        accept: `application/vnd.github.shadow-cat-preview+json`,
-      },
-      input: {
-        repositoryId: repository.id,
-        baseRefName: `master`,
-        headRefName: syncBranch,
-        title: `Sync with ${sourceRepo} @ ${shortHash}`,
-        body,
-        maintainerCanModify: true,
-        draft: true,
-      },
-    }
-  )
-
-  // if we successfully publish the PR
-  // pull again, taking the translated version and push to master
-  // shell.exec(`git reset --hard`)
-  // shell.exec(`git checkout master`)
-  // shell.exec(`git pull source master --strategy-option ours`)
+  await createPullRequest({
+    repositoryId: repository.id,
+    baseRefName: `master`,
+    headRefName: syncBranch,
+    title: `(sync) Sync with ${sourceRepo} @ ${shortHash}`,
+    body: syncPRBody(number),
+    maintainerCanModify: true,
+  })
   // shell.exec(`git push origin master`)
 }
 
