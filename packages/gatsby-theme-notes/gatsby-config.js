064317ceddd3980500a5148ed287f17cfd4168b7
Laurie Barth <laurie@LauriesrkLaptop.fios-router.home>
2020-02-13 13:12:52 -0500

    Still check mdx flag so we don't break existing theme usage

--- packages/gatsby-theme-notes/gatsby-config.js
+++ packages/gatsby-theme-notes/gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = options => {
-  const { mdxOtherwiseConfigured = false, mdxLayouts = {} } = options
+  const { mdxOtherwiseConfigured = false, mdx: legacyConfigureMdxFlag = true, mdxLayouts = {} } = options
 
   return {
     siteMetadata: {
@@ -8,7 +8,7 @@ module.exports = options => {
       siteUrl: `http://example.com/`,
     },
     plugins: [
-      !mdxOtherwiseConfigured && {
+      (!mdxOtherwiseConfigured && legacyConfigureMdxFlag) && {
         resolve: `gatsby-plugin-mdx`,
         options: {
           extensions: [`.md`, `.mdx`],
