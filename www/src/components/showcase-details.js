ff4cb3c7a870a9c5c1b8fee88c841be902d69d96
Nat Alison <tesseralis@gmail.com>
2020-02-14 12:12:03 -0800

    Remove extra query fields in showcase-details query

--- www/src/components/showcase-details.js
+++ www/src/components/showcase-details.js
@@ -103,8 +103,6 @@ function usePrevAndNextSite(item) {
         }
       ) {
         nodes {
-          id
-          url
           title
           fields {
             slug
@@ -155,7 +153,7 @@ function ShowcaseModal({ children, location }) {
       modalNextLink={
         <>
           <Img
-            key={nextSite.id}
+            key={nextSite.fields.slug}
             sx={styles.prevNextImage}
             backgroundColor
             fixed={{
@@ -198,7 +196,7 @@ function ShowcaseModal({ children, location }) {
       modalPreviousLink={
         <>
           <Img
-            key={previousSite.id}
+            key={previousSite.fields.slug}
             sx={styles.prevNextImage}
             backgroundColor
             fixed={{
