f7d18032c606813b3532eafb996505713a0f5133
Michal Piechowiak <misiek.piechowiak@gmail.com>
2020-02-15 22:27:26 +0100

    we don't want to assert broken behavior - we want to assert correct one, that just means we need to fix runtime

--- e2e-tests/production-runtime/cypress/integration/resource-loading-resilience.js
+++ e2e-tests/production-runtime/cypress/integration/resource-loading-resilience.js
@@ -24,91 +24,54 @@ function assertOnNavigate(
   cy.getTestElement(`dom-marker`).contains(assertShouldBe)
 }
 
-const runTests = (
-  blockedPath = `Default`,
-  options = { alternate: false, link: false, stay: false }
-) => {
-  const shouldLink = options.alternate && options.link
-
-  it(`Loads index - ${blockedPath}`, () => {
-    const currentPath = `/`
-    const assertShouldBe =
-      blockedPath === currentPath && shouldLink ? `404` : `index`
+const runTests = (testNameSuffix = `Unknown scenario`) => {
+  it(`Loads index - ${testNameSuffix}`, () => {
     cy.visit(`/`).waitForAPIorTimeout(`onRouteUpdate`, waitForAPIOptions)
-    cy.getTestElement(`dom-marker`).contains(assertShouldBe)
+    cy.getTestElement(`dom-marker`).contains(`index`)
   })
 
-  it(`Navigates to second page - ${blockedPath}`, () => {
-    const currentPath = `/`
-    const toPath = `/page-2/`
-    const isCurrentBlocked = currentPath === blockedPath
-    const toPathIsBlocked = toPath === blockedPath
-    if (isCurrentBlocked && options.alternate) {
-      assertOnNavigate(null, `not`, `/page-2/`, `404`)
-    } else if (toPathIsBlocked && options.stay) {
-      assertOnNavigate(`page2`, `not`, `/page-2/`, `index`)
-    } else {
-      const [assertShouldBe, locationAssert] =
-        toPath === blockedPath && shouldLink
-          ? [`404`, `not`]
-          : [`page-2`, `equal`]
-      assertOnNavigate(`page2`, locationAssert, `/page-2/`, assertShouldBe)
-    }
+  it(`Navigates to second page - ${testNameSuffix}`, () => {
+    cy.getTestElement(`page2`).click()
+    cy.waitForAPIorTimeout(`onRouteUpdate`, waitForAPIOptions)
+
+    cy.location(`pathname`).should(`equal`, `/page-2/`)
+    cy.getTestElement(`dom-marker`).contains(`page-2`)
   })
 
-  it(`Navigates to 404 page - ${blockedPath}`, () => {
-    cy.get(`[data-testid="dom-marker"]`)
-      .invoke(`text`)
-      .then(currentDom => {
-        const currentPath = `/page-2/`
-        const toPath = `/404.html`
-        const isCurrentBlocked = currentPath === blockedPath
-        if (currentDom.includes(`page-2`)) {
-          if (isCurrentBlocked && options.alternate) {
-            assertOnNavigate(null, `not`, `/page-3/`, `404`)
-          } else {
-            const [assertShouldBe, locationAssert] =
-              toPath === blockedPath && options.safeNotFound
-                ? [`page-2`, `not`]
-                : [`404`, `equal`]
-            assertOnNavigate(`404`, locationAssert, `/page-3/`, assertShouldBe)
-          }
-        } else {
-          assertOnNavigate(null, `not`, `/page-3/`, currentDom)
-        }
-      })
-  })
-
-  it(`Loads 404 - ${blockedPath}`, () => {
-    cy.visit(`/page-3/`, {
-      failOnStatusCode: false,
-    })
-      .waitForAPIorTimeout(`onRouteUpdate`, waitForAPIOptions)
-      .location(`pathname`)
-      .should(`equal`, `/page-3/`)
+  it(`Navigates to 404 page - ${testNameSuffix}`, () => {
+    cy.getTestElement(`404`).click()
+    cy.waitForAPIorTimeout(`onRouteUpdate`, waitForAPIOptions)
+    cy.location(`pathname`).should(`equal`, `/page-3/`)
     cy.getTestElement(`dom-marker`).contains(`404`)
   })
 
-  it(`Can navigate from 404 to index - ${blockedPath}`, () => {
-    const toPath = `/`
-    if (toPath === blockedPath && options.stay) {
-      assertOnNavigate(`index`, `equal`, `/page-3/`, `404`)
-    } else {
-      const [assertShouldBe, locationAssert] =
-        toPath === blockedPath && shouldLink
-          ? [`404`, `equal`]
-          : [`index`, `not`]
-      assertOnNavigate(`index`, locationAssert, `/`, assertShouldBe)
-    }
+  it(`Loads 404 - ${testNameSuffix}`, () => {
+    cy.visit(`/page-3/`, {
+      failOnStatusCode: false,
+    }).waitForAPIorTimeout(`onRouteUpdate`, waitForAPIOptions)
+
+    cy.location(`pathname`).should(`equal`, `/page-3/`)
+    cy.getTestElement(`dom-marker`).contains(`404`)
+  })
+
+  it(`Can navigate from 404 to index - ${testNameSuffix}`, () => {
+    cy.getTestElement(`index`).click()
+    cy.waitForAPIorTimeout(`onRouteUpdate`, waitForAPIOptions)
+
+    cy.location(`pathname`).should(`equal`, `/`)
+    cy.getTestElement(`dom-marker`).contains(`index`)
   })
 }
 
-describe(`Every resources available`, () => {
-  it(`Restore resources`, () => {
-    cy.task(`restoreAllBlockedResources`)
-  })
-  runTests()
-})
+const getTestNameSuffix = (scenario, args) => {
+  if (scenario === `blockAssetsForChunk` && args.chunk === `app`) {
+    return `Blocked "app" chunk`
+  } else if (scenario === `blockAssetsForPage`) {
+    return `Blocked "${args.filter}" for "${args.pagePath}"`
+  }
+
+  return undefined
+}
 
 const runBlockedScenario = (scenario, args) => {
   describe(`Block resources`, () => {
@@ -119,15 +82,10 @@ const runBlockedScenario = (scenario, args) => {
         })
       })
     })
-    runTests(args.pagePath, args.options)
+
+    runTests(getTestNameSuffix(scenario, args))
   })
 }
-
-describe(`Missing top level resources`, () => {
-  describe(`Deleted app chunk assets`, () => {
-    runBlockedScenario(`blockAssetsForChunk`, { chunk: `app` })
-  })
-})
 
 const runSuiteForPage = (label, pagePath) => {
   describe(`Missing "${label}" resources`, () => {
@@ -135,48 +93,41 @@ const runSuiteForPage = (label, pagePath) => {
       runBlockedScenario(`blockAssetsForPage`, {
         pagePath,
         filter: `page-data`,
-        options: {
-          alternate: true,
-          link: true,
-          safeNotFound: true,
-        },
       })
     })
     describe(`Missing "${label}" page page-template asset`, () => {
       runBlockedScenario(`blockAssetsForPage`, {
         pagePath,
         filter: `page-template`,
-        options: {
-          alternate: false,
-          link: true,
-          stay: true && !Cypress.env(`TEST_PLUGIN_OFFLINE`),
-          safeNotFound: true && !Cypress.env(`TEST_PLUGIN_OFFLINE`),
-        },
       })
     })
     describe(`Missing "${label}" page extra assets`, () => {
       runBlockedScenario(`blockAssetsForPage`, {
         pagePath,
         filter: `extra`,
-        options: {
-          alternate: false,
-          link: false,
-        },
       })
     })
     describe(`Missing all "${label}" page assets`, () => {
       runBlockedScenario(`blockAssetsForPage`, {
         pagePath,
         filter: `all`,
-        options: {
-          alternate: false || Cypress.env(`TEST_PLUGIN_OFFLINE`),
-          link: false || Cypress.env(`TEST_PLUGIN_OFFLINE`),
-          safeNotFound: false || Cypress.env(`TEST_PLUGIN_OFFLINE`),
-        },
       })
     })
   })
 }
+
+describe(`Every resources available`, () => {
+  it(`Restore resources`, () => {
+    cy.task(`restoreAllBlockedResources`)
+  })
+  runTests(`Every resource available`)
+})
+
+describe(`Missing top level resources`, () => {
+  describe(`Deleted app chunk assets`, () => {
+    runBlockedScenario(`blockAssetsForChunk`, { chunk: `app` })
+  })
+})
 
 runSuiteForPage(`Index`, `/`)
 runSuiteForPage(`Page-2`, `/page-2/`)
