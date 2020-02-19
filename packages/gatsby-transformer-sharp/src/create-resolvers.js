b1febe03f621c40203dab4862cd58260db4d08ec
LekoArts <lekoarts@gmail.com>
2020-02-19 15:21:10 +0100

    fix: Shorten the warning

--- packages/gatsby-transformer-sharp/src/create-resolvers.js
+++ packages/gatsby-transformer-sharp/src/create-resolvers.js
@@ -7,7 +7,7 @@ module.exports = ({ createResolvers, reporter }) => {
         resolve: (parent, args, context, info) => {
           if (!supportedExtensions[parent.extension]) {
             reporter.warn(
-              `You can't use childImageSharp together with .${parent.extension} files. The childImageSharp portion of the query in this file will return null:\n${context.componentPath}\n\nIf you want to display ${parent.name}.${parent.extension} on your site, use publicURL and a normal img tag instead.\nIf the files can be in multiple formats, you could check for a falsy value on childImageSharp and conditionally use e.g. gatsby-image.`
+              `You can't use childImageSharp together with ${parent.name}.${parent.extension} — use publicURL instead. The childImageSharp portion of the query in this file will return null:\n${context.componentPath}`
             )
           }
           return info.originalResolver(parent, args, context, info)
