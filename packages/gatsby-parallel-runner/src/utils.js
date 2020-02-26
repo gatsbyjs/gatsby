92d73e1579d7d5d496a9e9acc010f3a3ff165740
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2020-02-25 23:55:43 -0500

    fix node 8 compatibility for gatsby-parallel-runner

--- packages/gatsby-parallel-runner/src/utils.js
+++ packages/gatsby-parallel-runner/src/utils.js
@@ -1,13 +1,17 @@
 const path = require(`path`)
-const { readdir } = require(`fs-extra`)
+const fs = require(`fs-extra`)
 
 exports.resolveProcessors = async function() {
   const processorDir = path.join(__dirname, `processors`)
-  const pathNames = await readdir(processorDir, {
-    withFileTypes: true,
-  })
-  return pathNames
-    .filter(dir => dir.isDirectory())
+  const pathNames = await fs.readdir(processorDir)
+  const dirs = await Promise.all(
+    pathNames.map(async name => {
+      const stat = await fs.stat(path.join(processorDir, name))
+      return { stat, name }
+    })
+  )
+  return dirs
+    .filter(dir => dir.stat.isDirectory())
     .map(dir => {
       return {
         path: path.join(processorDir, dir.name),
