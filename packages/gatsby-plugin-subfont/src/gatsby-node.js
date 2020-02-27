32d9dc2be66ba087c5573eb7bda1661807013d9c
Jang Whemoon <palindrom615@gmail.com>
2020-02-27 09:32:44 +0000

    gatsby-plugin-subfong: Refactor default option

--- packages/gatsby-plugin-subfont/src/gatsby-node.js
+++ packages/gatsby-plugin-subfont/src/gatsby-node.js
@@ -4,17 +4,13 @@ const subfont = require(`subfont`)
 exports.onPostBuild = async ({ store }, options) => {
   const root = path.join(store.getState().program.directory, `public`)
 
-  const urlPaths = [`/`]
-
   await subfont(
     {
-      root: `file://${root}`,
+      root,
       inPlace: true,
       inlineCss: true,
       silent: true,
-      inputFiles: urlPaths.map(currentPath =>
-        path.join(root, currentPath, `index.html`)
-      ),
+      inputFiles: [path.join(root, `index.html`)],
       ...options,
     },
     console
