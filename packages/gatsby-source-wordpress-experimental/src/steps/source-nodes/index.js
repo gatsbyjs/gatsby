99be5ab462724b871f92ee8ae76a43e0f8f15b17
Tyler Barnes <tylerdbarnes@gmail.com>
2020-02-27 12:07:21 -0800

    add verbose debug messages related to cache

--- packages/gatsby-source-wordpress-experimental/src/steps/source-nodes/index.js
+++ packages/gatsby-source-wordpress-experimental/src/steps/source-nodes/index.js
@@ -5,13 +5,32 @@ import { LAST_COMPLETED_SOURCE_TIME } from "~/constants"
 import { createContentTypeNodes } from "~/steps/get-content-types"
 import store from "~/store"
 import fetchAndCreateNonNodeRootFields from "./create-nodes/fetch-and-create-non-node-root-fields"
+import { formatLogMessage } from "~/utils/format-log-message"
 
-const sourceNodes = async helpers => {
+const sourceNodes = async (helpers, pluginOptions) => {
   const { cache } = helpers
 
   const lastCompletedSourceTime = await cache.get(LAST_COMPLETED_SOURCE_TIME)
 
   const { schemaWasChanged } = store.getState().remoteSchema
+
+  if (!lastCompletedSourceTime && pluginOptions.verbose) {
+    helpers.reporter.log(``)
+    helpers.reporter.info(
+      formatLogMessage(`No previous builds detected, fetching all data`)
+    )
+    helpers.reporter.log(``)
+  }
+
+  if (lastCompletedSourceTime && schemaWasChanged && pluginOptions.verbose) {
+    helpers.reporter.log(``)
+    helpers.reporter.info(
+      formatLogMessage(
+        `Schema was changed since the last build, fetching all data`
+      )
+    )
+    helpers.reporter.log(``)
+  }
 
   const fetchEverything = !lastCompletedSourceTime || schemaWasChanged
 
