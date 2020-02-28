2d60d5e99367a6dc80f34580835c2d9f9b8a2314
Stuart Rayson <stuart.a.rayson@gmail.com>
2020-02-28 11:08:29 +0000

    check html suffix function

--- packages/gatsby/src/utils/page-html.js
+++ packages/gatsby/src/utils/page-html.js
@@ -1,8 +1,11 @@
 const fs = require(`fs-extra`)
 const path = require(`path`)
 
+const checkForHtmlSuffix = pagePath => !/\.(html?)$/i.test(pagePath)
+
 const remove = async ({ publicDir }, pagePath) => {
   const filePath = getPageHtmlFilePath(publicDir, pagePath)
+
   return fs.remove(filePath)
 }
 
@@ -10,7 +13,7 @@ const remove = async ({ publicDir }, pagePath) => {
 const getPageHtmlFilePath = (dir, outputPath) => {
   let outputFileName = outputPath.replace(/^(\/|\\)/, ``) // Remove leading slashes for webpack-dev-server
 
-  if (!/\.(html?)$/i.test(outputFileName)) {
+  if (checkForHtmlSuffix(outputPath)) {
     outputFileName = path.join(outputFileName, `index.html`)
   }
 
