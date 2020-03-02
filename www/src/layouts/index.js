822055b7a512fc0b6419efacf39f5a93562a0295
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2020-03-02 12:57:22 -0800

    reorganize i18n stuff

--- www/src/layouts/index.js
+++ www/src/layouts/index.js
@@ -1,20 +1,13 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui"
-import { I18nProvider } from "@lingui/react"
 import BaseLayout from "../components/layout"
-import { getLocaleAndBasePath } from "../utils/i18n"
 
-export default function Layout({ location, children }) {
-  if (location.state && location.state.isModal) {
-    return children
-  }
-  const { locale } = getLocaleAndBasePath(location.pathname)
-  const catalog = require(`../data/locales/${locale}/messages.js`)
+import { I18nProvider } from "../components/I18nContext"
+
+export default function Layout({ location, pageContext, children }) {
   return (
-    <I18nProvider language={locale} catalogs={{ [locale]: catalog }}>
-      <BaseLayout location={location} locale={locale}>
-        {children}
-      </BaseLayout>
+    <I18nProvider locale={pageContext.locale}>
+      <BaseLayout location={location}>{children}</BaseLayout>
     </I18nProvider>
   )
 }
