0a8f162274933585e08c800dc150e349312bc449
Tyler Barnes <tylerdbarnes@gmail.com>
2020-03-02 14:44:48 -0800

    bump version

--- packages/wp-gatsby/wp-gatsby.php
+++ packages/wp-gatsby/wp-gatsby.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WP Gatsby
  * Description: Optimize your WordPress site to be a source for Gatsby site(s).
- * Version: 0.0.1
+ * Version: 0.1.1
  * Author: GatsbyJS, Jason Bahl, Tyler Barnes
  * Author URI: https://gatsbyjs.org
  * Text Domain: wp-gatsby
