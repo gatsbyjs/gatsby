a5b409bc17e5a30b214d4b888346a93200d166dc
Benedikt Rötsch <opensource@axe312.dev>
2020-03-03 14:30:25 +0100

    add queue for downloading to avoid request timeouts on gatsby cloud

--- packages/gatsby-transformer-video/src/ffmpeg.js
+++ packages/gatsby-transformer-video/src/ffmpeg.js
@@ -21,6 +21,7 @@ import profileGif from "./profiles/gif"
 export default class FFMPEG {
   constructor({ rootDir, cacheDir, ffmpegPath, ffprobePath, profiles }) {
     this.queue = new PQueue({ concurrency: 1 })
+    this.downloadQueue = new PQueue({ concurrency: 2 })
     this.cacheDir = cacheDir
     this.rootDir = rootDir
     this.profiles = profiles
@@ -106,14 +107,17 @@ export default class FFMPEG {
       const { ext } = parse(fileName)
 
       // Download video from Contentful for further processing
-      const fileNode = await createRemoteFileNode({
-        url: `https:${url}`,
-        store,
-        cache,
-        createNode,
-        createNodeId,
-        ext,
-      })
+      const fileNode = await this.downloadQueue.add(() =>
+        createRemoteFileNode({
+          url: `https:${url}`,
+          store,
+          cache,
+          createNode,
+          createNodeId,
+          ext,
+        })
+      )
+
       path = fileNode.absolutePath
       contentDigest = fileNode.internal.contentDigest
     }
