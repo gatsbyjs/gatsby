gatsbyjs/gatsby

commit fda78f1f9e6ed0af75fa9bca19c5cf0ad283889e

refractor(www): Use `nodes` in gatsbyjs.org code instead of `edges.node` (#21792)

www/src/views/starter-library/filtered-starters.js
@@ -41,7 +41,7 @@ export default class FilteredStarterLibrary extends Component {
     })
   resetFilters = () => this.props.setURLState({ c: [], d: [], v: [], s: `` })
   showMoreSites = starters => {
-    let showAll =
+    const showAll =
       this.state.sitesToShow + 15 > starters.length ? starters.length : false
     this.setState({
       sitesToShow: showAll ? showAll : this.state.sitesToShow + 15,
@@ -77,8 +77,8 @@ export default class FilteredStarterLibrary extends Component {
     )
 
     // stopgap for missing gh data (#8763)
-    let starters = data.allStartersYaml.edges.filter(
-      ({ node: starter }) => starter.fields && starter.fields.starterShowcase
+    let starters = data.allStartersYaml.nodes.filter(
+      starter => starter.fields && starter.fields.starterShowcase
     )
 
     if (urlState.s.length > 0) {
@@ -116,7 +116,7 @@ export default class FilteredStarterLibrary extends Component {
               data={Array.from(
                 count(
                   starters.map(
-                    ({ node }) =>
+                    node =>
                       node.fields &&
                       node.fields.starterShowcase.gatsbyMajorVersion.map(
                         str => str[1]
@@ -129,9 +129,7 @@ export default class FilteredStarterLibrary extends Component {
             />
             <LHSFilter
               heading="Categories"
-              data={Array.from(
-                count(starters.map(({ node: starter }) => starter.tags))
-              )}
+              data={Array.from(count(starters.map(starter => starter.tags)))}
               filters={filtersCategory}
               setFilters={setFiltersCategory}
               sortRecent={urlState.sort === `recent`}
@@ -141,7 +139,7 @@ export default class FilteredStarterLibrary extends Component {
               data={Array.from(
                 count(
                   starters.map(
-                    ({ node: starter }) =>
+                    starter =>
                       starter.fields &&
                       starter.fields.starterShowcase.gatsbyDependencies.map(
                         str => str[0]
@@ -171,7 +169,7 @@ export default class FilteredStarterLibrary extends Component {
               filters={filters}
               label="Gatsby Starter"
               items={starters}
-              edges={starters}
+              nodes={starters}
               what="size"
             />
             <div
@@ -259,12 +257,12 @@ export default class FilteredStarterLibrary extends Component {
 // utility functions
 
 function count(arrays) {
-  let counts = new Map()
+  const counts = new Map()
 
-  for (let categories of arrays) {
+  for (const categories of arrays) {
     if (!categories) continue
 
-    for (let category of categories) {
+    for (const category of categories) {
       if (!counts.has(category)) {
         counts.set(category, 0)
       }
@@ -300,11 +298,12 @@ function filterByDependencies(list, categories) {
 
 function filterByVersions(list, versions) {
   let starters = list
+
   starters = starters.filter(
-    ({ node }) =>
-      node.fields &&
+    ({ fields }) =>
+      fields &&
       isSuperset(
-        node.fields.starterShowcase.gatsbyMajorVersion.map(c => c[1]),
+        fields.starterShowcase.gatsbyMajorVersion.map(c => c[1]),
         versions
       )
   )
@@ -312,7 +311,7 @@ function filterByVersions(list, versions) {
 }
 
 function isSuperset(set, subset) {
-  for (var elem of subset) {
+  for (const elem of subset) {
     if (!set.includes(elem)) {
       return false
     }

www/gatsby-config.js
@@ -287,20 +287,18 @@ module.exports = {
                     fileAbsolutePath: { regex: "/docs.blog/" }
                   }
                 ) {
-                  edges {
-                    node {
-                      html
-                      frontmatter {
-                        title
-                        date
-                        author {
-                          id
-                        }
+                  nodes {
+                    html
+                    frontmatter {
+                      title
+                      date
+                      author {
+                        id
                       }
-                      fields {
-                        excerpt
-                        slug
-                      }
+                    }
+                    fields {
+                      excerpt
+                      slug
                     }
                   }
                 }
@@ -321,7 +319,7 @@ module.exports = {
               }
             },
             serialize: ({ query: { site, allMdx } }) =>
-              allMdx.edges.map(({ node }) => {
+              allMdx.nodes.map(node => {
                 return {
                   title: node.frontmatter.title,
                   description: node.fields.excerpt,

www/src/components/diagram.js
@@ -204,16 +204,14 @@ const Diagram = () => (
     query={graphql`
       query StaticHostsQuery {
         allStaticHostsYaml {
-          edges {
-            node {
-              title
-              url
-            }
+          nodes {
+            title
+            url
           }
         }
       }
     `}
-    render={({ allStaticHostsYaml: { edges: staticHosts } }) => (
+    render={({ allStaticHostsYaml: { nodes: staticHosts } }) => (
       <section
         className="Diagram"
         sx={{
@@ -308,7 +306,7 @@ const Diagram = () => (
           >
             <ItemTitle>Web Hosting</ItemTitle>
             <ItemDescription>
-              {staticHosts.map(({ node: staticHost }, index) => (
+              {staticHosts.map((staticHost, index) => (
                 <Fragment key={staticHost.url}>
                   {index > 0 && `, `}
                   <ItemDescriptionLink to={staticHost.url}>

www/src/components/features/evaluation-table.js
@@ -53,8 +53,8 @@ class EvaluationTable extends Component {
                     [].concat([
                       <SectionHeaderBottom
                         options={options}
-                        display={row.node.Subcategory}
-                        category={row.node.Subcategory}
+                        display={row.Subcategory}
+                        category={row.Subcategory}
                         key={`section-header-${s}-bottom-${i}`}
                       />,
                       // table row with the name of the feature and corresponding scores
@@ -81,7 +81,7 @@ class EvaluationTable extends Component {
                             }}
                             id={
                               j === 0
-                                ? row.node.Feature.toLowerCase()
+                                ? row.Feature.toLowerCase()
                                     .split(` `)
                                     .join(`-`)
                                 : undefined
@@ -92,7 +92,7 @@ class EvaluationTable extends Component {
                               })
                             }}
                           >
-                            {renderCell(row.node[nodeProperty], j)}
+                            {renderCell(row[nodeProperty], j)}
                           </td>
                         ))}
                       </tr>,
@@ -117,7 +117,7 @@ class EvaluationTable extends Component {
                           {
                             <span
                               dangerouslySetInnerHTML={{
-                                __html: row.node.Description,
+                                __html: row.Description,
                               }}
                             />
                           }

www/src/components/features/simple-evaluation-table.js
@@ -12,7 +12,7 @@ const SimpleEvaluationTable = ({ title, headers, data }) => (
     <table>
       <tbody>
         <SectionHeaderTop columnHeaders={headers.map(h => h.display)} />
-        {data.map(({ node }, idx) => (
+        {data.map((node, idx) => (
           <tr key={`feature-row-${idx}`}>
             {headers.map((header, i) => (
               <td

www/src/components/homepage/homepage-logo-banner.js
@@ -73,13 +73,11 @@ const HomepageLogoBanner = () => {
         }
         sort: { fields: publicURL }
       ) {
-        edges {
-          node {
-            base
-            childImageSharp {
-              fixed(quality: 75, height: 24) {
-                ...GatsbyImageSharpFixed_tracedSVG
-              }
+        nodes {
+          base
+          childImageSharp {
+            fixed(quality: 75, height: 24) {
+              ...GatsbyImageSharpFixed_tracedSVG
             }
           }
         }
@@ -93,7 +91,7 @@ const HomepageLogoBanner = () => {
         <Name>Trusted by</Name>
       </Title>
       <LogoGroup>
-        {data.allFile.edges.map(({ node: image }) => (
+        {data.allFile.nodes.map(image => (
           <Img
             alt={`${image.base.split(`.`)[0]}`}
             fixed={image.childImageSharp.fixed}

www/src/pages/creators/agencies.js
@@ -14,10 +14,8 @@ export default AgenciesPage
 export const pageQuery = graphql`
   query {
     allCreatorsYaml(filter: { type: { eq: "agency" } }) {
-      edges {
-        node {
-          ...CreatorData
-        }
+      nodes {
+        ...CreatorData
       }
     }
   }

www/src/pages/creators/companies.js
@@ -14,10 +14,8 @@ export default CompaniesPage
 export const pageQuery = graphql`
   query {
     allCreatorsYaml(filter: { type: { eq: "company" } }) {
-      edges {
-        node {
-          ...CreatorData
-        }
+      nodes {
+        ...CreatorData
       }
     }
   }

commit 14427cd9c755d78557735c88e924f52fa6c6f77e

fix(www): starters is already a node
The `starters` array used to be an array of edges and is "now" an array of nodes so accessing `.node` is no longer necessary. So dropping the access should make it work again.

Fixes #21942

Bug was introduced by #21792

www/src/views/starter-library/filtered-starters.js
@@ -82,8 +82,8 @@ export default class FilteredStarterLibrary extends Component {
     )
 
     if (urlState.s.length > 0) {
-      starterNodes = starterNodes.filter(starter =>
-        JSON.stringify(starter.node)
+      starterNodes = starterNodes.filter(node =>
+        JSON.stringify(node)
           .toLowerCase()
           .includes(urlState.s.toLowerCase())
       )