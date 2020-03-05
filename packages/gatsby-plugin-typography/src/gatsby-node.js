67f950713907ffa2b5620709f1fdb62cd3dd1067
Js-Brecht <myrlin1@gmail.com>
2020-03-05 07:37:07 -1000

    use `cache.directory`

--- packages/gatsby-plugin-typography/src/gatsby-node.js
+++ packages/gatsby-plugin-typography/src/gatsby-node.js
@@ -2,12 +2,9 @@ const fs = require(`fs`)
 const path = require(`path`)
 const os = require(`os`)
 
-const getCachePath = directory =>
-  path.join(directory, `.cache`, `caches`, `gatsby-plugin-typography`)
-
 // Write out a typography module to .cache.
 
-exports.onPreBootstrap = ({ store }, pluginOptions) => {
+exports.onPreBootstrap = ({ store, cache }, pluginOptions) => {
   const program = store.getState().program
 
   let module
@@ -26,7 +23,7 @@ const typography = new Typography()
 module.exports = typography`
   }
 
-  const dir = getCachePath(program.directory)
+  const dir = cache.directory
 
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir)
@@ -35,16 +32,13 @@ module.exports = typography`
   fs.writeFileSync(`${dir}/typography.js`, module)
 }
 
-exports.onCreateWebpackConfig = ({ actions, plugins: { define }, store }) => {
-  const cache = path.join(
-    getCachePath(store.getState().program.directory),
-    `typography.js`
-  )
+exports.onCreateWebpackConfig = ({ actions, plugins: { define }, cache }) => {
+  const cacheDir = path.join(cache.directory, `typography.js`)
   const { setWebpackConfig } = actions
   setWebpackConfig({
     plugins: [
       define({
-        __TYPOGRAPHY_PLUGIN_CACHE_ENDPOINT__: JSON.stringify(cache),
+        __TYPOGRAPHY_PLUGIN_CACHE_ENDPOINT__: JSON.stringify(cacheDir),
       }),
     ],
   })
