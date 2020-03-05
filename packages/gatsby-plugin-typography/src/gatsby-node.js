fd76d022854b7062116cca5621ef82112420aaee
Jeremy Albright <myrlin1@gmail.com>
2020-03-04 20:53:46 -1000

    proxy typography resolution through user cache

--- packages/gatsby-plugin-typography/src/gatsby-node.js
+++ packages/gatsby-plugin-typography/src/gatsby-node.js
@@ -2,6 +2,9 @@ const fs = require(`fs`)
 const path = require(`path`)
 const os = require(`os`)
 
+const getCachePath = directory =>
+  path.join(directory, `.cache`, `caches`, `gatsby-plugin-typography`)
+
 // Write out a typography module to .cache.
 
 exports.onPreBootstrap = ({ store }, pluginOptions) => {
@@ -23,11 +26,27 @@ const typography = new Typography()
 module.exports = typography`
   }
 
-  const dir = `${__dirname}/.cache`
+  // const dir = `${__dirname}/.cache`
+  const dir = getCachePath(program.directory)
 
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir)
   }
 
   fs.writeFileSync(`${dir}/typography.js`, module)
+}
+
+exports.onCreateWebpackConfig = ({ actions, plugins: { define }, store }) => {
+  const cache = path.join(
+    getCachePath(store.getState().program.directory),
+    `typography.js`
+  )
+  const { setWebpackConfig } = actions
+  setWebpackConfig({
+    plugins: [
+      define({
+        __TYPOGRAPHY_PLUGIN_CACHE_ENDPOINT__: JSON.stringify(cache),
+      }),
+    ],
+  })
 }
---
+++ packages/gatsby-plugin-typography/src/typography.js
@@ -0,0 +1,2 @@
+// eslint-disable-next-line no-undef
+module.exports = require(__TYPOGRAPHY_PLUGIN_CACHE_ENDPOINT__)
