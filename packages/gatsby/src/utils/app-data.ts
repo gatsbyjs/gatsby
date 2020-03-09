b56c12727dc1a68f2e792a92d3bd4ca28f986f60
kawamataryou <ba068082@gmail.com>
2020-03-09 10:59:49 +0900

    chore(gatsby): fix promise to void

--- packages/gatsby/src/utils/app-data.ts
+++ packages/gatsby/src/utils/app-data.ts
@@ -3,10 +3,11 @@ import path from "path"
 
 const APP_DATA_JSON = `app-data.json`
 
-export const write = (publicDir: string, hash: string): Promise<void> =>
+export const write = (publicDir: string, hash: string): void => {
   fs.outputJson(path.join(publicDir, `page-data`, APP_DATA_JSON), {
     webpackCompilationHash: hash,
   })
+}
 
 export const exists = (publicDir: string): boolean =>
   fs.pathExistsSync(path.join(publicDir, `page-data`, APP_DATA_JSON))
