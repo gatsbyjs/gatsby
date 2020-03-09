c095a1287b897d22eaac052b41af0c5fd2112628
Michaël De Boey <info@michaeldeboey.be>
2020-03-09 17:02:19 +0100

    Cleanup app-data

--- packages/gatsby/src/utils/app-data.ts
+++ packages/gatsby/src/utils/app-data.ts
@@ -1,13 +1,13 @@
-import fs from "fs-extra"
 import path from "path"
+
+import { outputJson, pathExistsSync } from "fs-extra"
 
 const APP_DATA_JSON = `app-data.json`
 
-export const write = (publicDir: string, hash: string): void => {
-  fs.outputJson(path.join(publicDir, `page-data`, APP_DATA_JSON), {
+export const write = (publicDir: string, hash: string): Promise<void> =>
+  outputJson(path.join(publicDir, `page-data`, APP_DATA_JSON), {
     webpackCompilationHash: hash,
   })
-}
 
 export const exists = (publicDir: string): boolean =>
-  fs.pathExistsSync(path.join(publicDir, `page-data`, APP_DATA_JSON))
+  pathExistsSync(path.join(publicDir, `page-data`, APP_DATA_JSON))
