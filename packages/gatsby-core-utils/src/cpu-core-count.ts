ca9c84930258d0151f36142527644bb35fe3bf05
Matt Kane <matt@gatsbyjs.com>
2020-03-10 17:19:21 +0000

    Move jsdocs from .d.ts into src files

--- packages/gatsby-core-utils/src/cpu-core-count.ts
+++ packages/gatsby-core-utils/src/cpu-core-count.ts
@@ -1,6 +1,12 @@
 import { getPhysicalCpuCount } from "./physical-cpu-count"
 
-export const cpuCoreCount = (ignoreEnvVar: boolean): number => {
+/**
+ * Calculate CPU core count
+ *
+ * @param ignoreEnvVar Ignore the 'GATSBY_CPU_COUNT' env var to calculate the requested type of CPU cores
+ * @return Count of the requested type of CPU cores. Defaults to number of physical cores or 1
+ */
+export const cpuCoreCount = (ignoreEnvVar?: boolean): number => {
   try {
     let coreCount = getPhysicalCpuCount() || 1
 
