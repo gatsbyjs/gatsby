8f661eb5f3623cdf302e19a6ea2a6d3ef09ac972
Mohamed Ouyizme <mohouyizme@gmail.com>
2020-03-10 12:52:23 +0100

    refactor(www): Update react-icons from 2.2.7 to 3.x.x (latest) (#21860)
    * Upgrade to react-icons@3
    
    * add source map explorer output plugin
    
    * remove no longer used file
    
    * all react icons should be vertically aligned by default
    
    * Revert "add source map explorer output plugin"
    
    This reverts commit 4e3811f36eefd4fc310f4df51ba063a749f22bec.
    
    Co-authored-by: Michal Piechowiak <misiek.piechowiak@gmail.com>

--- www/src/components/layout.js
+++ www/src/components/layout.js
@@ -3,6 +3,7 @@ import { jsx } from "theme-ui"
 import React from "react"
 
 import { Global } from "@emotion/core"
+import { IconContext } from "react-icons"
 
 import { globalStyles } from "../utils/styles/global"
 import { breakpointGutter } from "../utils/styles"
@@ -18,28 +19,31 @@ export const LocaleContext = React.createContext(defaultLang)
 
 export default function DefaultLayout({ location, locale, children }) {
   return (
-    <LocaleContext.Provider value={locale || defaultLang}>
-      <Global styles={globalStyles} />
-      <SiteMetadata pathname={location.pathname} locale={locale} />
-      <SkipNavLink />
-      <Banner />
-      <Navigation pathname={location.pathname} />
-      <div
-        className={`main-body docSearch-content`}
-        sx={{
-          px: `env(safe-area-inset-left)`,
-          pt: t => t.sizes.bannerHeight,
-          // make room for the mobile navigation
-          pb: t => t.sizes.headerHeight,
-          [breakpointGutter]: {
-            pt: t => `calc(${t.sizes.bannerHeight} + ${t.sizes.headerHeight})`,
-            pb: 0,
-          },
-        }}
-      >
-        {children}
-      </div>
-      <MobileNavigation />
-    </LocaleContext.Provider>
+    <IconContext.Provider value={{ style: { verticalAlign: "middle" } }}>
+      <LocaleContext.Provider value={locale || defaultLang}>
+        <Global styles={globalStyles} />
+        <SiteMetadata pathname={location.pathname} locale={locale} />
+        <SkipNavLink />
+        <Banner />
+        <Navigation pathname={location.pathname} />
+        <div
+          className={`main-body docSearch-content`}
+          sx={{
+            px: `env(safe-area-inset-left)`,
+            pt: t => t.sizes.bannerHeight,
+            // make room for the mobile navigation
+            pb: t => t.sizes.headerHeight,
+            [breakpointGutter]: {
+              pt: t =>
+                `calc(${t.sizes.bannerHeight} + ${t.sizes.headerHeight})`,
+              pb: 0,
+            },
+          }}
+        >
+          {children}
+        </div>
+        <MobileNavigation />
+      </LocaleContext.Provider>
+    </IconContext.Provider>
   )
 }
