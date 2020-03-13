9bcc4c35674f67dad97650994669096d416f03c7
Samuel Overton Jr <samovertonjr@gmail.com>
2020-03-13 00:04:57 -0500

    Removed comment for eslint

--- www/src/components/localized-link.js
+++ www/src/components/localized-link.js
@@ -1,5 +1,4 @@
 import React from "react"
-// eslint-disable-next-line no-restricted-imports
 import { Link } from "gatsby"
 import { LocaleContext } from "./layout"
 import { localizedPath } from "../utils/i18n"
