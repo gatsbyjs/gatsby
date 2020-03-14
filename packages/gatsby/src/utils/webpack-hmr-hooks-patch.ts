2a988b3ef6a342ff8f7fc7df403409a3a3b22321
Daiki Ihara <sasurau4@gmail.com>
2020-03-15 02:05:31 +0900

    chore(gatsby): migrate webpack-hmr-hooks-patch to TypeScript

---
+++ packages/gatsby/src/utils/webpack-hmr-hooks-patch.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/ban-ts-ignore */
+
+// @ts-ignore: This file assume run at browsers.
+const originalFetch = global.fetch
+// @ts-ignore
+delete global.fetch
+
+export * from "react-hot-loader/webpack"
+
+// @ts-ignore
+global.fetch = originalFetch
