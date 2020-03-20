258d734ac3bdf1d14da7399d9d71e07c76c49e31
Teo Dragovic <teo.dragovic@gmail.com>
2020-03-20 12:29:03 +0100

    Enable Preact in development

--- packages/gatsby-plugin-preact/src/gatsby-node.js
+++ packages/gatsby-plugin-preact/src/gatsby-node.js
@@ -1,16 +1,19 @@
-exports.onCreateWebpackConfig = ({ stage, actions }) => {
-  // Requiring the server version of React-dom is hardcoded right now
-  // in the development server. So we'll just avoid loading Preact there
-  // for now.
-  if (stage !== `develop`) {
-    actions.setWebpackConfig({
-      resolve: {
-        alias: {
-          react: `preact/compat`,
-          "react-dom": `preact/compat`,
-          "react-dom/server": `preact/compat`,
-        },
+
+exports.onPreInit = () => {
+  // Setting this variable replaces react-hot-loader with
+  // [fast-refresh](https://reactnative.dev/docs/next/fast-refresh)
+  // and resolves conflicts with running Preact in development.
+  process.env.GATSBY_HOT_LOADER='fast-refresh';
+}
+
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        react: `preact/compat`,
+        "react-dom": `preact/compat`,
+        "react-dom/server": `preact/compat`,
       },
-    })
-  }
+    },
+  })
 }
