4ca629f47147b78323730c16abb9373157fc3a0c
Kirill Vasiltsov <38713361+jlkiri@users.noreply.github.com>
2020-03-21 02:32:40 +0900

    feat(www): useActiveHash hook for highlighting links in Docs'… (#21762)
    * Create useActiveHash hook
    
    * Change hashlist to idlist
    
    * Remove console.log
    
    * Hook cleanup
    
    * Style link based on hash
    
    * Add traversal option
    
    * Move hooks to its own file
    
    * Improve hook
    
    * Add comments, remove console.log
    
    * Revert hash remove logic
    
    * Remove unused colors
    
    * Revert "Remove unused colors"
    
    This reverts commit 8160ad8618b8de61c2d64764831607c0b3ecccfc.
    
    * Remove unused colors
    
    * Refactor getHeadingIds
    
    * Switch to getElementById
    
    * Handle cases where url is absent
    
    * Do not update URL hash
    
    * Disable active link behavior on mobile
    
    * Handle mobile with media query
    
    * Move function
    
    * Rename toc to items, add depth prop
    
    * Highlight subheadings
    
    * Add tests
    
    * update recursion so tableOfContentsDepth is respected and something is always highlighted in ToC
    
    Co-authored-by: Kyle Gill <kylerobertgill@gmail.com>

--- www/src/components/docs-table-of-contents.js
+++ www/src/components/docs-table-of-contents.js
@@ -3,9 +3,41 @@ import { jsx } from "theme-ui"
 import Link from "./localized-link"
 import { Trans } from "@lingui/macro"
 import {
-  colors,
   mediaQueries,
+  breakpoints,
 } from "gatsby-design-tokens/dist/theme-gatsbyjs-org"
+import { useEffect, useState } from "react"
+import { useActiveHash } from "../hooks/use-active-hash"
+
+const getHeadingIds = (
+  toc,
+  traverseFullDepth = true,
+  depth,
+  recursionDepth = 1
+) => {
+  const idList = []
+  const hashToId = str => str.slice(1)
+
+  if (toc) {
+    for (const item of toc) {
+      // Sometimes url does not exist on item. See #19851
+      if (item.url) {
+        idList.push(hashToId(item.url))
+      }
+
+      // Only traverse sub-items if specified (they are not displayed in ToC)
+      // recursion depth should only go up to 6 headings deep and may come in as
+      // undefined if not set in the tableOfContentsDepth frontmatter field
+      if (item.items && traverseFullDepth && recursionDepth < (depth || 6)) {
+        idList.push(
+          ...getHeadingIds(item.items, true, depth, recursionDepth + 1)
+        )
+      }
+    }
+  }
+
+  return idList
+}
 
 function isUnderDepthLimit(depth, maxDepth) {
   if (maxDepth === null) {
@@ -18,55 +50,72 @@ function isUnderDepthLimit(depth, maxDepth) {
 
 // depth and maxDepth are used to figure out how many bullets deep to render in the ToC sidebar, if no
 // max depth is set via the tableOfContentsDepth field in the frontmatter, all headings will be rendered
-function createItems(items, location, depth, maxDepth) {
+function createItems(items, location, depth, maxDepth, activeHash, isDesktop) {
   return (
     items &&
-    items.map((item, index) => (
-      <li
-        sx={{ [mediaQueries.xl]: { fontSize: 1 } }}
-        key={location.pathname + (item.url || depth + `-` + index)}
-      >
-        {item.url && (
-          <Link
-            sx={{
-              "&&": {
-                color: `textMuted`,
-                border: 0,
-                transition: t =>
-                  `all ${t.transition.speed.fast} ${t.transition.curve.default}`,
-                ":hover": {
-                  color: `link.color`,
-                  borderBottom: t => `1px solid ${t.colors.link.hoverBorder}`,
+    items.map((item, index) => {
+      const isActive = isDesktop && item.url === `#${activeHash}`
+      return (
+        <li
+          data-testid={item.url || ``}
+          sx={{ [mediaQueries.xl]: { fontSize: 1 } }}
+          key={location.pathname + (item.url || depth + `-` + index)}
+        >
+          {item.url && (
+            <Link
+              sx={{
+                "&&": {
+                  color: isActive ? `link.color` : `textMuted`,
+                  border: 0,
+                  borderBottom: t =>
+                    isActive
+                      ? `1px solid ${t.colors.link.hoverBorder}`
+                      : `none`,
+                  transition: t =>
+                    `all ${t.transition.speed.fast} ${t.transition.curve.default}`,
+                  ":hover": {
+                    color: `link.color`,
+                    borderBottom: t => `1px solid ${t.colors.link.hoverBorder}`,
+                  },
                 },
-              },
-            }}
-            getProps={({ href, location }) =>
-              location && location.href && location.href.includes(href)
-                ? {
-                    style: {
-                      color: colors.link.color,
-                      borderBottom: `1px solid ${colors.link.hoverBorder}`,
-                    },
-                  }
-                : null
-            }
-            to={location.pathname + item.url}
-          >
-            {item.title}
-          </Link>
-        )}
-        {item.items && isUnderDepthLimit(depth, maxDepth) && (
-          <ul sx={{ color: `textMuted`, listStyle: `none`, ml: 5 }}>
-            {createItems(item.items, location, depth + 1, maxDepth)}
-          </ul>
-        )}
-      </li>
-    ))
+              }}
+              to={location.pathname + item.url}
+            >
+              {item.title}
+            </Link>
+          )}
+          {item.items && isUnderDepthLimit(depth, maxDepth) && (
+            <ul sx={{ color: `textMuted`, listStyle: `none`, ml: 5 }}>
+              {createItems(
+                item.items,
+                location,
+                depth + 1,
+                maxDepth,
+                activeHash,
+                isDesktop
+              )}
+            </ul>
+          )}
+        </li>
+      )
+    })
   )
 }
 
-function TableOfContents({ page, location }) {
-  return page.tableOfContents.items ? (
+function TableOfContents({ items, depth, location }) {
+  const [isDesktop, setIsDesktop] = useState(false)
+  const activeHash = useActiveHash(getHeadingIds(items, true, depth))
+
+  useEffect(() => {
+    const isDesktopQuery = window.matchMedia(`(min-width: ${breakpoints[4]})`) // 1200px
+    setIsDesktop(isDesktopQuery.matches)
+
+    const updateIsDesktop = e => setIsDesktop(e.matches)
+    isDesktopQuery.addListener(updateIsDesktop)
+    return () => isDesktopQuery.removeListener(updateIsDesktop)
+  }, [])
+
+  return items ? (
     <nav
       sx={{
         mb: [8, null, null, null, null, 0],
@@ -98,12 +147,7 @@ function TableOfContents({ page, location }) {
           m: 0,
         }}
       >
-        {createItems(
-          page.tableOfContents.items,
-          location,
-          1,
-          page.frontmatter.tableOfContentsDepth
-        )}
+        {createItems(items, location, 1, depth, activeHash, isDesktop)}
       </ul>
     </nav>
   ) : null
