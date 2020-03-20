5ff71eaba711bef5f203071bc724daf640533087
Matt Kane <matt@gatsbyjs.com>
2020-03-20 11:00:30 +0000

    Await start of webpack server

--- packages/gatsby/src/services/start-webpack-server.ts
+++ packages/gatsby/src/services/start-webpack-server.ts
@@ -29,59 +29,62 @@ export async function startWebpackServer({ program }): Promise<any> {
   })
 
   let isFirstCompile = true
+
+  return new Promise((resolve, reject) => {
+    compiler.hooks.done.tapAsync(`print gatsby instructions`, async function(
+      stats,
+      done
+    ) {
+      // We have switched off the default Webpack output in WebpackDevServer
+      // options so we are going to "massage" the warnings and errors and present
+      // them in a readable focused way.
+      const messages = formatWebpackMessages(stats.toJson({}, true))
+      const urls = prepareUrls(
+        program.ssl ? `https` : `http`,
+        program.host,
+        program.port
+      )
+      const isSuccessful = !messages.errors.length
+
+      if (isSuccessful && isFirstCompile) {
+        printInstructions(
+          program.sitePackageJson.name || `(Unnamed package)`,
+          urls
+        )
+        printDeprecationWarnings()
+        if (program.open) {
+          try {
+            await openurl(urls.localUrlForBrowser)
+          } catch {
+            console.log(
+              `${chalk.yellow(
+                `warn`
+              )} Browser not opened because no browser was found`
+            )
+          }
+        }
+      }
+
+      isFirstCompile = false
+
+      if (webpackActivity) {
+        reportWebpackWarnings(stats)
+
+        if (!isSuccessful) {
+          const errors = structureWebpackErrors(
+            `develop`,
+            stats.compilation.errors
+          )
+          webpackActivity.panicOnBuild(errors)
+        }
+        webpackActivity.end()
+        webpackActivity = null
+      }
+
+      done()
+      resolve({ compiler })
+    })
+  })
   // "done" event fires when Webpack has finished recompiling the bundle.
   // Whether or not you have warnings or errors, you will get this event.
-  compiler.hooks.done.tapAsync(`print gatsby instructions`, function(
-    stats,
-    done
-  ) {
-    // We have switched off the default Webpack output in WebpackDevServer
-    // options so we are going to "massage" the warnings and errors and present
-    // them in a readable focused way.
-    const messages = formatWebpackMessages(stats.toJson({}, true))
-    const urls = prepareUrls(
-      program.ssl ? `https` : `http`,
-      program.host,
-      program.port
-    )
-    const isSuccessful = !messages.errors.length
-
-    if (isSuccessful && isFirstCompile) {
-      printInstructions(
-        program.sitePackageJson.name || `(Unnamed package)`,
-        urls
-      )
-      printDeprecationWarnings()
-      if (program.open) {
-        Promise.resolve(openurl(urls.localUrlForBrowser)).catch(() =>
-          console.log(
-            `${chalk.yellow(
-              `warn`
-            )} Browser not opened because no browser was found`
-          )
-        )
-      }
-    }
-
-    isFirstCompile = false
-
-    if (webpackActivity) {
-      reportWebpackWarnings(stats)
-
-      if (!isSuccessful) {
-        const errors = structureWebpackErrors(
-          `develop`,
-          stats.compilation.errors
-        )
-        webpackActivity.panicOnBuild(errors)
-      }
-      webpackActivity.end()
-      webpackActivity = null
-    }
-
-    done()
-  })
-  return {
-    compiler,
-  }
 }
