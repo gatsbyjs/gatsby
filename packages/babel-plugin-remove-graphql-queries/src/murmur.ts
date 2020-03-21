a201eaa3f2cd8fc851387f974d26b0399de5ef44
Zach Taylor <zachtylr04@gmail.com>
2020-03-20 20:47:04 -0500

    type every function's input and output

--- packages/babel-plugin-remove-graphql-queries/src/murmur.ts
+++ packages/babel-plugin-remove-graphql-queries/src/murmur.ts
@@ -1,14 +1,16 @@
 // murmurhash2 via https://gist.github.com/raycmorgan/588423
 
-module.exports = (str, seed) => {
-  let m = 0x5bd1e995
-  let r = 24
-  let h = seed ^ str.length
-  let length = str.length
-  let currentIndex = 0
+
+
+export const murmurhash = (str: string, seed: number): number => {
+  let m: number = 0x5bd1e995
+  let r: number = 24
+  let h: number = seed ^ str.length
+  let length: number = str.length
+  let currentIndex: number = 0
 
   while (length >= 4) {
-    let k = UInt32(str, currentIndex)
+    let k: number = UInt32(str, currentIndex)
 
     k = Umul32(k, m)
     k ^= k >>> r
@@ -46,7 +48,7 @@ module.exports = (str, seed) => {
   return h >>> 0
 }
 
-function UInt32(str, pos) {
+function UInt32(str: string, pos: number): number {
   return (
     str.charCodeAt(pos++) +
     (str.charCodeAt(pos++) << 8) +
@@ -55,11 +57,11 @@ function UInt32(str, pos) {
   )
 }
 
-function UInt16(str, pos) {
+function UInt16(str: string, pos: number): number {
   return str.charCodeAt(pos++) + (str.charCodeAt(pos++) << 8)
 }
 
-function Umul32(n, m) {
+function Umul32(n: number, m: number): number {
   n = n | 0
   m = m | 0
   let nlo = n & 0xffff
