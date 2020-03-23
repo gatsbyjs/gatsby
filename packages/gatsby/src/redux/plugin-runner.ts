e6885bdb5111b982de47ca68a5a803d88cc5b803
Blaine Kasten <blainekasten@gmail.com>
2020-03-23 14:23:17 -0500

    Improve typing and reduce module side effects

--- packages/gatsby/src/redux/plugin-runner.ts
+++ packages/gatsby/src/redux/plugin-runner.ts
@@ -1,13 +1,51 @@
-// Invoke plugins for certain actions.
-
+import { Span } from "opentracing"
 import { emitter } from "./index"
 import apiRunnerNode from "../utils/api-runner-node"
+import { ActivityTracker } from "../../"
 
-emitter.on(`CREATE_PAGE`, action => {
-  const page = action.payload
-  apiRunnerNode(
-    `onCreatePage`,
-    { page, traceId: action.traceId, parentSpan: action.parentSpan },
-    { pluginSource: action.plugin.name, activity: action.activity }
-  )
-})
+type Plugin = any // TODO
+
+// This might make sense to live somewhere else
+interface ICreatePageAction {
+  graphql<TData, TVariables = any>(
+    query: string,
+    variables?: TVariables
+  ): Promise<{
+    errors?: any
+    data?: TData
+  }>
+  traceId: "initial-createPages"
+  waitForCascadingActions: boolean
+  parentSpan: Span
+  activity: ActivityTracker
+  type: `CREATE_PAGE`
+  contextModified: boolean
+  plugin: Plugin
+  payload: {
+    internalComponentName: string
+    path: string
+    matchPath: string | undefined
+    component: string
+    componentChunkName: string
+    isCreatedByStatefulCreatePages: boolean
+    context: {
+      slug: string
+      id: string
+    }
+    updatedAt: number
+    pluginCreator__NODE: string
+    pluginCreatorId: string
+    componentPath: string
+  }
+}
+
+export const startPluginRunner = () => {
+  emitter.on(`CREATE_PAGE`, (action: ICreatePageAction) => {
+    const page = action.payload
+    apiRunnerNode(
+      `onCreatePage`,
+      { page, traceId: action.traceId, parentSpan: action.parentSpan },
+      { pluginSource: action.plugin.name, activity: action.activity }
+    )
+  })
+}
