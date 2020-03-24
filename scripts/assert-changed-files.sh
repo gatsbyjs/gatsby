ec352607a04c9978963c1cab5648f126123481bb
wardpeet <ward@coding-tech.com>
2020-03-24 10:52:02 +0100

    remove git config values

--- scripts/assert-changed-files.sh
+++ scripts/assert-changed-files.sh
@@ -17,6 +17,9 @@ if [ "$IS_CI" = true ]; then
     MERGE_SUCCESS=0
     git merge --abort
   fi
+
+  git config --unset user.name
+  git config --unset user.email
 fi
 
 FILES_COUNT="$(git diff-tree --no-commit-id --name-only -r "$CIRCLE_BRANCH" origin/master | grep -E "$GREP_PATTERN" -c)"
