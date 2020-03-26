1a096500e03aae35a33612585b61d48e99c57b06
Peter van der Zee <github-public@qfox.nl>
2020-03-26 15:46:03 +0100

    use local cache for data+types if possible

--- packages/gatsby-source-contentful/src/fetch.js
+++ packages/gatsby-source-contentful/src/fetch.js
@@ -1,14 +1,30 @@
+const path = require(`path`)
+const v8 = require(`v8`)
+const fs = require(`fs`)
+
 const contentful = require(`contentful`)
 const _ = require(`lodash`)
 const chalk = require(`chalk`)
 const normalize = require(`./normalize`)
 const { formatPluginOptionsForCLI } = require(`./plugin-options`)
 
+// Path to read/write files fetched from remote so we can skip it
+const REMOTE_CACHE_FOLDER = process.env.GATSBY_REMOTE_CACHE ?? ``
+const CACHE_DATA_FILE = path.join(REMOTE_CACHE_FOLDER, `sync_data`)
+const CACHE_TYPE_FILE = path.join(REMOTE_CACHE_FOLDER, `content_types`)
+
 module.exports = async ({ syncToken, reporter, pluginConfig }) => {
   // Fetch articles.
   console.time(`Fetch Contentful data`)
 
   console.log(`Starting to fetch data from Contentful`)
+  if (REMOTE_CACHE_FOLDER) {
+    console.log(
+      `Will use \`` +
+        REMOTE_CACHE_FOLDER +
+        `\` as the cache folder to read from if available, or write to after fetching`
+    )
+  }
 
   const pageLimit = pluginConfig.get(`pageLimit`)
   const contentfulClientOptions = {
@@ -29,7 +45,6 @@ module.exports = async ({ syncToken, reporter, pluginConfig }) => {
   let locales
   let defaultLocale = `en-US`
   try {
-    console.log(`Fetching default locale`)
     space = await client.getSpace()
     let contentfulLocales = await client
       .getLocales()
@@ -84,23 +99,103 @@ Used options:
 ${formatPluginOptionsForCLI(pluginConfig.getOriginalPluginOptions(), errors)}`)
   }
 
+  // Three cases, both for the data and the types cache payloads:
+  // - We may not be using the local cache at all: fetch it now
+  // - The local cache may not exist: fetch it now, cache it
+  // - The cache exists and we still want to fetch it:
+  //    - Fetch it now, but
+  //    - Start building immediately
+  //    - When the data is received, match it against the cache
+  //    - If the cache turns out to be stale, restart the build
+
+  if (REMOTE_CACHE_FOLDER) {
+    // Make sure the path exists
+    fs.mkdirSync(REMOTE_CACHE_FOLDER, { recursive: true })
+  }
+
   let currentSyncData
   try {
-    let query = syncToken
-      ? { nextSyncToken: syncToken }
-      : { initial: true, limit: pageLimit }
-    currentSyncData = await client.sync(query)
+    if (REMOTE_CACHE_FOLDER && fs.existsSync(CACHE_DATA_FILE)) {
+      try {
+        currentSyncData = v8.deserialize(fs.readFileSync(CACHE_DATA_FILE))
+        console.log(`Should have loaded data from \`` + CACHE_DATA_FILE + `\``)
+      } catch (e) {
+        console.warn(
+          `Failed to read or deserialize local contentful data cache from \`` +
+            CACHE_DATA_FILE +
+            `\`, going to fetch it from remote. Error: ` +
+            e
+        )
+      }
+    }
+
+    if (!currentSyncData) {
+      let query = syncToken
+        ? { nextSyncToken: syncToken }
+        : { initial: true, limit: pageLimit }
+      currentSyncData = await client.sync(query)
+
+      if (CACHE_DATA_FILE) {
+        try {
+          console.log(
+            `Updating the contentful data cache in \`` + CACHE_DATA_FILE + `\``
+          )
+          // Note: this object may contain cycles so JSON.stringify is unsafe
+          fs.writeFileSync(CACHE_DATA_FILE, v8.serialize(currentSyncData))
+        } catch (e) {
+          console.warn(
+            `Failed to serialize or write data to specified cache file: \`` +
+              CACHE_DATA_FILE +
+              `\`. Error: ` +
+              e
+          )
+        }
+      }
+    }
   } catch (e) {
-    reporter.panic(`Fetching contentful data failed`, e)
+    reporter.panic(`Fetching contentful data failed:` + e)
   }
 
   // We need to fetch content types with the non-sync API as the sync API
   // doesn't support this.
   let contentTypes
   try {
-    contentTypes = await pagedGet(client, `getContentTypes`, pageLimit)
+    if (REMOTE_CACHE_FOLDER && fs.existsSync(CACHE_TYPE_FILE)) {
+      try {
+        contentTypes = v8.deserialize(fs.readFileSync(CACHE_TYPE_FILE))
+        console.log(`Should have loaded types from \`` + CACHE_TYPE_FILE + `\``)
+      } catch (e) {
+        console.warn(
+          `Failed to read or deserialize local contentful type cache from \`` +
+            CACHE_TYPE_FILE +
+            `\`, going to fetch it from remote. Error: ` +
+            e
+        )
+      }
+    }
+
+    if (!contentTypes) {
+      contentTypes = await pagedGet(client, `getContentTypes`, pageLimit)
+
+      if (CACHE_TYPE_FILE) {
+        try {
+          console.log(
+            `Updating the contentful type cache in \`` + CACHE_TYPE_FILE + `\``
+          )
+          // Note: this object may contain cycles so JSON.stringify is unsafe
+          fs.writeFileSync(CACHE_TYPE_FILE, v8.serialize(contentTypes))
+        } catch (e) {
+          console.warn(
+            `Failed to serialize or write types to specified cache file: \`` +
+              CACHE_TYPE_FILE +
+              `\`. Error: ` +
+              e
+          )
+        }
+      }
+    }
   } catch (e) {
-    console.log(`error fetching content types`, e)
+    reporter.panic(`Fetching contentful types failed`, e)
   }
   console.log(`contentTypes fetched`, contentTypes.items.length)
 
@@ -117,7 +212,7 @@ ${formatPluginOptionsForCLI(pluginConfig.getOriginalPluginOptions(), errors)}`)
     currentSyncData,
     contentTypeItems,
     defaultLocale,
-    locales,
+    locales: locales && locales.slice && locales.slice(0, 1), // HACK: only use english
     space,
   }
 
