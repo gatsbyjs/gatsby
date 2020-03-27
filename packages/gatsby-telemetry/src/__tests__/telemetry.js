94ae1850869887fd3cd863eb50602a128f8cba2c
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2020-03-27 22:36:04 +0100

    test(telemetry): just rephrased the test description

--- packages/gatsby-telemetry/src/__tests__/telemetry.js
+++ packages/gatsby-telemetry/src/__tests__/telemetry.js
@@ -15,7 +15,7 @@ describe(`Telemetry`, () => {
     expect(eventStore.mock.instances[0].addEvent).toHaveBeenCalledTimes(1)
   })
 
-  it(`Doesn't add event to store if telemetry is turned off`, () => {
+  it(`Doesn't add event to store if telemetry tracking is turned off`, () => {
     telemetry.trackingEnabled = false
     telemetry.trackActivity(`demo`)
     expect(eventStore.mock.instances[0].addEvent).not.toHaveBeenCalled()
