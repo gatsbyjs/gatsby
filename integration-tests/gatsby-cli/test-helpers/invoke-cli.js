cd042767cfc238158eb3610676386b926abc3567
Blaine Kasten <blainekasten@gmail.com>
2020-03-27 12:09:49 -0500

    try cross-spawn

--- integration-tests/gatsby-cli/test-helpers/invoke-cli.js
+++ integration-tests/gatsby-cli/test-helpers/invoke-cli.js
@@ -1,22 +1,25 @@
-import child_process from "child_process"
+import { spawn } from "cross-spawn"
 import { join } from "path"
 
 export function invokeCli(...args) {
+  const child = spawn(join(__dirname, "./gatsby-cli.js"), args, {
+    cwd: join(__dirname, "../execution-folder"),
+  })
+
+  const logs = []
+
+  child.stdout.setEncoding("utf8")
+  child.stdout.on("data", data => {
+    logs.push(data.toString())
+  })
+
+  child.stderr.setEncoding("utf8")
+  child.stderr.on("data", data => {
+    logs.push(data.toString())
+  })
+
   return new Promise(resolve => {
-    child_process.execFile(
-      join(__dirname, "./gatsby-cli.js"),
-      args,
-      {
-        cwd: join(__dirname, "../execution-folder"),
-      },
-      (error, stdout, stderr) => {
-        const logs = [stdout, stderr].join("")
-        if (error) {
-          console.error(error)
-          return resolve([1, logs])
-        }
-        resolve([0, logs])
-      }
-    )
+    child.on("close", code => resolve([code, logs.join("")]))
+    child.on("exit", code => resolve([code, logs.join("")]))
   })
 }
