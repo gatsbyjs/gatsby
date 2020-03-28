0abbad967c61d90678d631380c6965f65cca51f0
sunecosuri <sunecosuri@gmail.com>
2020-03-29 05:26:17 +0900

    Convert showAnalyticsNotification to typescript

---
+++ packages/gatsby-telemetry/src/showAnalyticsNotification.ts
@@ -0,0 +1,26 @@
+import boxen, { BorderStyle } from 'boxen'
+
+const defaultConfig = {
+  padding: 1,
+  borderColor: `blue`,
+  borderStyle: BorderStyle.Double,
+}
+
+const defaultMessage =
+  `Gatsby collects anonymous usage analytics\n` +
+  `to help improve Gatsby for all users.\n` +
+  `\n` +
+  `If you'd like to opt-out, you can use \`gatsby telemetry --disable\`\n` +
+  `To learn more, checkout https://gatsby.dev/telemetry`
+
+/**
+ * Analytics notice for the end-user
+ * @param {Object} config - The configuration that boxen accepts. https://github.com/sindresorhus/boxen#api
+ * @param {string} message - Message shown to the end-user
+ */
+export const showAnalyticsNotification = (
+  config = defaultConfig,
+  message = defaultMessage
+) => {
+  console.log(boxen(message, config))
+}
