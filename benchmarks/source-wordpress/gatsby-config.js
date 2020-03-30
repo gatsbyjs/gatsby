50d7dc9af6c33b7877ac1e82445793e085421c2f
Peter van der Zee <209817+pvdz@users.noreply.github.com>
2020-03-30 18:46:55 +0200

    feat(benchmarks): add WordPress benchmark (#22662)

---
+++ benchmarks/source-wordpress/gatsby-config.js
@@ -0,0 +1,32 @@
+require("dotenv").config({
+  path: `.env`,
+})
+
+module.exports = {
+  siteMetadata: {
+    siteTitle: `Gatsby WordPress Build Benchmark`,
+  },
+  plugins: [
+    `gatsby-plugin-benchmark-reporting`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-wordpress-experimental`,
+      options: {
+        url: process.env.WPGRAPHQL_URL,
+        type: {
+          Post: {
+            limit: process.env.NODE_ENV === `development` ? 50 : false,
+          },
+        },
+      },
+    },
+  ],
+}
