f02403f6c7a1083eda3aca6007be6b5f4bc368d2
John Otander <johnotander@gmail.com>
2020-04-01 15:52:35 -0700

    Make config slightly more robust

--- packages/gatsby/src/recipes/providers/gatsby/plugin.test.js
+++ packages/gatsby/src/recipes/providers/gatsby/plugin.test.js
@@ -1,11 +1,14 @@
-// const plugin = require(`./plugin`)
 const fs = require(`fs`)
 const path = require(`path`)
 
+const { addPluginToConfig } = require(`./plugin`)
+
+const CONFIG_PATH = path.join(__dirname, `./fixtures/gatsby-config.js`)
+
 test(`babel plugin to get list of plugins and their options!`, () => {
-  const configSrc = fs.readFileSync(
-    path.join(__dirname, `./fixtures/sample-gatsby-config.js`),
-    `utf-8`
-  )
-  console.log({ configSrc })
+  const src = fs.readFileSync(CONFIG_PATH, `utf8`)
+
+  const result = addPluginToConfig(src, `gatsby-plugin-foo`)
+
+  expect(result).toMatch(`gatsby-plugin-foo`)
 })
