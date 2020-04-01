3d491409310f32bc2deb83944803643674dc5e2f
Kyle Mathews <mathews.kyle@gmail.com>
2020-04-01 15:46:33 -0700

    prettier again

--- packages/gatsby/src/commands/recipes.ts
+++ packages/gatsby/src/commands/recipes.ts
@@ -21,7 +21,7 @@ module.exports = async (program: IProgram): Promise<void> => {
   subprocess.stdout.on("data", data => {
     if (!started) {
       const runRecipe = require(`../recipes/index`)
-      runRecipe({recipe, projectRoot: program.directory})
+      runRecipe({ recipe, projectRoot: program.directory })
       started = true
     }
   })
