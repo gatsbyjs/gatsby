4fa121820c36ced30e11f88064db53c47d2a6c8d
Phumrapee Limpianchop <git@rayriffy.com>
2020-04-06 23:24:00 +0700

    fix: caling default export from ts

--- packages/gatsby/src/bootstrap/index.js
+++ packages/gatsby/src/bootstrap/index.js
@@ -28,7 +28,7 @@ process.on(`unhandledRejection`, (reason, p) => {
   report.panic(reason)
 })
 
-const createGraphqlRunner = require(`./graphql-runner`)
+const createGraphqlRunner = require(`./graphql-runner`).default
 const { extractQueries } = require(`../query/query-watcher`)
 const requiresWriter = require(`./requires-writer`)
 const { writeRedirects } = require(`./redirects-writer`)
