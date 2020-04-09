0c6b2ed07d37f1b4c44fea0e1533d9809d006252
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2020-04-08 18:54:27 -0700

    add accessibility and functionality

--- www/src/components/language-dropdown.js
+++ www/src/components/language-dropdown.js
@@ -1,4 +1,5 @@
 /** @jsx jsx */
+import React from "react"
 import { jsx } from "theme-ui"
 import { MdTranslate } from "react-icons/md"
 import { Link } from "gatsby"
@@ -9,6 +10,7 @@ import { useLocale } from "./I18nContext"
 import { langs, getLocaleAndBasePath, localizedPath } from "../utils/i18n"
 
 const navItemTopOffset = `0.4rem`
+// FIXME this is copied from navigation.js
 const navItemStyles = {
   borderBottom: `2px solid transparent`,
   color: `navigation.linkDefault`,
@@ -59,21 +61,67 @@ function LangLink({ code, name, localName, current, pathname }) {
 
 export default function LanguageDropdown({ pathname }) {
   const locale = useLocale()
+  const [isOpen, setIsOpen] = React.useState(false)
+  const buttonRef = React.useRef(null)
+  const dropdownListRef = React.useRef(null)
+
+  function wrapKeyHandler(e) {
+    if (e.key === "Escape" && isOpen) {
+      setIsOpen(false)
+      buttonRef.current.focus()
+    }
+  }
+
+  function clickOutsideHandler(e) {
+    if (
+      buttonRef.current.contains(e.target) ||
+      dropdownListRef.current.contains(e.target)
+    ) {
+      return
+    }
+    setIsOpen(false)
+  }
+
+  React.useEffect(() => {
+    if (isOpen) {
+      document.addEventListener("mouseup", clickOutsideHandler)
+      dropdownListRef.current.querySelector("a").focus()
+    } else {
+      document.removeEventListener("mouseup", clickOutsideHandler)
+    }
+
+    return () => {
+      document.removeEventListener("mouseup", clickOutsideHandler)
+    }
+  }, [isOpen])
+
   return (
-    <div sx={{ position: `relative` }}>
+    <div
+      onKeyUp={wrapKeyHandler}
+      onMouseEnter={() => setIsOpen(true)}
+      onMouseLeave={() => setIsOpen(false)}
+      sx={{ position: `relative` }}
+    >
       <button
+        ref={buttonRef}
+        onClick={() => setIsOpen(!isOpen)}
+        aria-haspopup="true"
+        aria-controls="language-dropdown"
         sx={{
           border: 0,
           background: `none`,
           cursor: `pointer`,
-          outline: `none`, // FIXME enable when tabbing
           ...navItemStyles,
         }}
       >
         <MdTranslate /> Languages
       </button>
       <ul
+        id="language-dropdown"
+        ref={dropdownListRef}
+        tabIndex="-1"
         sx={{
+          display: isOpen ? `block` : `none`,
           position: `absolute`,
           right: 0,
           margin: 0,
