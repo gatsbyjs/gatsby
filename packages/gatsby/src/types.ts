5037e9d427b585158b40b94ea5ce441bc3044928
Phumrapee Limpianchop <git@rayriffy.com>
2020-04-09 19:30:58 +0700

    chore(gatsby): Migrate graphql-runner into TypeScript (#22860)
    * feat: typed graphql-runner
    
    * fix: anything else context can also be boolean (graphql-runner.ts:L55)
    
    * refactor: format
    
    * fix: caling default export from ts
    
    * fix: defining default in test suite
    
    * cleanup PR
    
    * fix ci
    
    * fix lint again
    
    * fix ci again
    
    * update snapshot
    
    Co-authored-by: Blaine Kasten <blainekasten@gmail.com>

--- packages/gatsby/src/types.ts
+++ packages/gatsby/src/types.ts
@@ -2,7 +2,7 @@ export interface IMatch {
   id: string
   context: {
     sourceMessage: string
-    [key: string]: string
+    [key: string]: string | boolean
   }
   error?: Error | undefined
   [key: string]: unknown
