1006515804e11863bffdb984243e735c1b016999
Tyler Barnes <tylerdbarnes@gmail.com>
2020-04-10 17:55:50 -0700

    flip things around on their head. Gatsby asks if it's requirements are met rather than the other way around

--- packages/wp-gatsby/src/Schema/SiteMeta.php
+++ packages/wp-gatsby/src/Schema/SiteMeta.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace WPGatsby\Schema;
+
+use Composer\Semver\Semver;
+
 /**
  * Adds info about the current site
  */
@@ -18,139 +21,125 @@ class SiteMeta
         );
     }
 
+    private static function isVersionARange( $version ) {
+      $version_without_periods = str_replace( '.', '', $version );
+      $version_without_numbers_and_periods = preg_replace(
+        '/[0-9]+/',
+        '',
+        $version_without_periods
+      );
+
+      return $version_without_numbers_and_periods !== '';
+    }
+
     /**
      * Registers site meta fields...
      */
     function register() {
 
-	    register_graphql_object_type( 'WPGatsbyCompatiblity', [
+	    register_graphql_object_type( 'WPGatsbySatisfies', [
+		    'description' => __( 'Check compatibility with WPGatsby and WPGraphQL.' ),
+		    'fields' => [
+			    'wpGQL' => [
+				    'type' => 'Boolean',
+				    'description' => __( 'Whether the provided version range requirement for WPGraphQL is met by this WP instance.', 'wp-gatsby' ),
+			    ],
+			    'wpGatsby' => [
+				    'type' => 'Boolean',
+				    'description' => __( 'Whether the provided version range requirement for WPGatsby is met by this WP instance.', 'wp-gatsby' ),
+			    ],
+		    ]
+	    ] );
+
+	    register_graphql_object_type( 'WPGatsbyCompatibility', [
 		    'description' => __( 'Check compatibility with a given version of gatsby-source-wordpress and the WordPress source site.' ),
 		    'fields' => [
-			    'isCompatible' => [
-				    'type' => 'Boolean',
-				    'description' => __( 'Whether the provided version of the gatsby-source-wordpress plugin is compatible with the WordPress server', 'wp-gatsby' ),
-			    ],
-			    'messages' => [
-				    'type' => [ 'list_of' => 'String' ],
-				    'description' => __( 'If the provided version of gatsby-source-wordpress is incompatible, theses messages will provide further information.', 'wp-gatsby'),
+			    'satisfies' => [
+				    'type' => 'WPGatsbySatisfies',
 			    ],
 		    ]
 	    ] );
 
 	    register_graphql_field( 'RootQuery', 'wpGatsbyCompatibility', [
 		    'description' => __( 'Information about the compatibility of the WordPress server with a provided version of gatsby-source-wordpress.', 'wp-gatsby' ),
-		    'type' => 'WPGatsbyCompatiblity',
+		    'type' => 'WPGatsbyCompatibility',
 		    'args' => [
-			    'version' => [
+			    'wpGQLVersionRange' => [
 				    'type' => [ 'non_null' => 'String' ],
-				    'description' => __( 'The version of the gatsby-source-wordPress plugin to check compatibility with', 'wp-gatsby' ),
+				    'description' => __( 'The semver version range of WPGraphQL that the requester wants to check compatibility with', 'wp-gatsby' ),
+			    ],
+			    'wpGatsbyVersionRange' => [
+				    'type' => [ 'non_null' => 'String' ],
+				    'description' => __( 'The semver version range of WPGatsby that the requester wants to check compatibility with', 'wp-gatsby' ),
 			    ],
 		    ],
 		    'resolve' => function( $root, $args, $context, $info ) {
 
-			    $is_compatible = true;
-			    $messages = [];
-			    $version = isset( $args['version'] ) ? $args['version'] : null;
+          $wpgql_version_range = $args['wpGQLVersionRange'] ?? null;
+          $wpgatsby_version_range = $args['wpGatsbyVersionRange'] ?? null;
 
-			    if ( empty( $version ) ) {
-					$is_compatible = false;
-					$messages[] = __( 'No version was provided for the compatibility check. Please provide a valid version of the gatsby-source-wordpress plugin.', 'wp-gatsby' );
+
+			    if ( ! $wpgql_version_range ) {
+					  throw new \Exception(
+              __(
+                'No WPGraphQL version was provided for the compatibility check. Please provide a semver version range for checking compatibility with WPGraphQL.',
+                'wp-gatsby'
+              )
+            );
 			    }
 
-			    // @todo: Update path when this moves away from Tyler's fork
-			    $remote_compatibility = wp_remote_get( 'https://raw.githubusercontent.com/TylerBarnes/gatsby/feat/source-wordpress-v4/packages/wp-gatsby/compatibility.json' );
+			    if ( ! $wpgatsby_version_range ) {
+					  throw new \Exception(
+              __(
+                'No WPGatsby version was provided for the compatibility check. Please provide a semver version range for checking compatibility with WPGatsby.',
+                'wp-gatsby'
+              )
+            );
+          }
 
-			    /**
-			     * If the remote file cannot be fetched and returned properly, fallback to the local file in the installed version of WPGatsby
-			     */
-			    if ( is_wp_error( $remote_compatibility ) || 200 !== $remote_compatibility['response']['code'] || empty( $remote_compatibility['body'] ) )  {
-				    $compatibility_file = file_get_contents( WPGATSBY_PLUGIN_DIR . 'compatibility.json' );
-				    $compatibility = ! empty( $compatibility_file ) ? json_decode( $compatibility_file, true ) : null;
-			    } else {
-				    $compatibility = json_decode( $remote_compatibility['body'], true );
-			    }
+          if ( ! self::isVersionARange( $wpgql_version_range ) ) {
+            throw new \Exception(
+              'The provided WPGraphQL version is not a range. '
+              . $wpgql_version_range
+            );
+          }
 
-			    /**
-			     * If compatibility info cannot be determined
-			     */
-			    if ( empty( $compatibility ) || ! isset( $compatibility[ $version ] ) ) {
+          if ( ! self::isVersionARange( $wpgatsby_version_range ) ) {
+            throw new \Exception(
+              'The provided WPGatsby version is not a range. '
+              . $wpgatsby_version_range
+            );
+          }
 
-					$is_compatible = false;
-					$messages[] = sprintf( __( 'The compatibility for version %s could not be determined', 'wp-gatsby' ), $version );
+          /**
+           * Get the versions of WPGraphQL and WPGatsby
+           */
+          $installed_wpgraphql_version = defined( 'WPGRAPHQL_VERSION' )
+            ? WPGRAPHQL_VERSION
+            : null;
 
-			    } else {
+          $installed_wpgatsby_version = defined( 'WPGATSBY_VERSION' )
+            ? WPGATSBY_VERSION
+            : null;
 
-				    /**
-				     * Get the versions of WPGraphQL and WPGatsby
-				     */
-				    $installed_wpgraphql_version = defined( 'WPGRAPHQL_VERSION' ) ? WPGRAPHQL_VERSION : null;
-				    $installed__wpgatsby_version = defined( 'WPGATSBY_VERSION' ) ? WPGATSBY_VERSION : null;
-			    	$version_compat = $compatibility[ $version ];
+          $wpgql_is_satisfied = Semver::satisfies(
+            $installed_wpgraphql_version,
+            $wpgql_version_range
+          );
 
-			    	// If wp-graphql isn't specified for a specific version
-			    	if ( ! isset( $version_compat['wp-graphql'] ) ) {
-					    $is_compatible = false;
-						$messages[] = sprintf(__( 'The version of WPGraphQL compatible with gatsby-source-wordpress v%s could not be determined', 'wp-gatsby' ), $version );
-				    }
-
-				    // If wp-gatsby isn't specified for as specific version
-				    if ( ! isset( $version_compat['wp-gatsby'] ) ) {
-					    $is_compatible = false;
-					    $messages[] = sprintf(__( 'The version of WPGatsby compatible with gatsby-source-wordpress v%s could not be determined', 'wp-gatsby' ), $version );
-				    }
-
-				    // If active WPGraphQL is lower than compatible min
-				    if ( ! isset( $version_compat['wp-graphql']['min'] ) ) {
-					    $is_compatible = false;
-					    $messages[] = sprintf(__( 'The minimum required version of WPGraphQL compatible with gatsby-source-wordpress v%s could not be determined', 'wp-gatsby' ), $version );
-				    } else {
-					    if ( ! version_compare( $installed_wpgraphql_version, $version_compat['wp-graphql']['min'], '>=' ) ) {
-						    $is_compatible = false;
-						    $messages[] = sprintf(__( 'The installed version of WPGraphQL (v%1$s) is lower than the required minimum version (v%2$s)', 'wp-gatsby' ), $installed_wpgraphql_version, $version_compat['wp-graphql']['min'] );
-					    }
-				    }
-
-				    // If active WPGraphQL is higher than compatible max
-				    if ( ! isset( $version_compat['wp-gatsby']['max'] ) ) {
-					    $is_compatible = false;
-					    $messages[] = sprintf(__( 'The max required version of WPGraphQL compatible with gatsby-source-wordpress v%s could not be determined', 'wp-gatsby' ), $version );
-				    } else {
-					    if ( ! version_compare( $installed_wpgraphql_version, $version_compat['wp-graphql']['max'], '<=' ) ) {
-						    $is_compatible = false;
-						    $messages[] = sprintf(__( 'The installed version of WPGraphQL (v%1$s) is higher than the max version (v%2$s) compatible with gatsby-source-wordpress %3$s', 'wp-gatsby' ), $installed_wpgraphql_version, $version_compat['wp-graphql']['max'], $version );
-					    }
-				    }
-
-				    // If active WPGatsby is lower than compatible min
-				    if ( ! isset( $version_compat['wp-gatsby']['min'] ) ) {
-					    $is_compatible = false;
-					    $messages[] = sprintf(__( 'The minimum required version of WPGatsby compatible with gatsby-source-wordpress v%s could not be determined', 'wp-gatsby' ), $version );
-				    } else {
-					    if ( ! version_compare( $installed__wpgatsby_version, $version_compat['wp-gatsby']['min'], '>=' ) ) {
-						    $is_compatible = false;
-						    $messages[] = sprintf(__( 'The installed version of WPGatsby (v%1$s) is lower than the required minimum version (v%2$s)', 'wp-gatsby' ), $installed__wpgatsby_version, $version_compat['wp-graphql']['min'] );
-					    }
-				    }
-
-				    // If active WPGatsby is higher than compatible max
-				    if ( ! isset( $version_compat['wp-gatsby']['max'] ) ) {
-					    $is_compatible = false;
-					    $messages[] = sprintf(__( 'The max required version of WPGatsby compatible with gatsby-source-wordpress v%s could not be determined', 'wp-gatsby' ), $version );
-				    } else {
-					    if ( ! version_compare( $installed__wpgatsby_version, $version_compat['wp-gatsby']['max'], '<=' ) ) {
-						    $is_compatible = false;
-						    $messages[] = sprintf(__( 'The installed version of WPGatsby (v%1$s) is higher than the max version (v%2$s) compatible with gatsby-source-wordpress %3$s', 'wp-gatsby' ), $installed__wpgatsby_version, $version_compat['wp-graphql']['max'], $version );
-					    }
-				    }
-
-			    }
+          $wpgatsby_is_satisfied = Semver::satisfies(
+            $installed_wpgatsby_version,
+            $wpgatsby_version_range
+          );
 
 			    /**
 			     * Return the payload
 			     */
 			    return [
-			    	'isCompatible' => $is_compatible,
-				    'messages' => false === $is_compatible ? $messages : null,
+			    	'satisfies' => [
+              'wpGQL' => $wpgql_is_satisfied,
+              'wpGatsby' => $wpgatsby_is_satisfied,
+            ],
 			    ];
 
 		    }
