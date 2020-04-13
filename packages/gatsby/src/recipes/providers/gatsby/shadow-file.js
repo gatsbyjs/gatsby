50258e24ab7e6ae3c1b5e6a1f1d8f6ded56f91df
John Otander <johnotander@gmail.com>
2020-04-13 11:13:15 -0600

    feat(recipes): Implement shadow file resource and test it

--- packages/gatsby/src/recipes/providers/gatsby/shadow-file.js
+++ packages/gatsby/src/recipes/providers/gatsby/shadow-file.js
@@ -1,11 +1,22 @@
 const path = require(`path`)
 const fs = require(`fs-extra`)
-const mkdirp = require(`mkdirp`)
+const Joi = require('@hapi/joi')
+
+const resourceSchema = require('../resource-schema')
+const getDiff = require(`../utils/get-diff`)
+const fileExists = filePath => fs.existsSync(filePath)
+
+const relativePathForShadowedFile = ({ theme, filePath }) => {
+  // TODO: Support windows
+  const [_src, ...filePathParts] = filePath.split('/')
+  const relativePath = path.join('src', theme, path.join(...filePathParts))
+  return relativePath
+}
 
 const create = async ({ root }, { theme, path: filePath }) => {
-  console.log(`hi ShadowFile create 2`, { theme, filePath })
+  const id = relativePathForShadowedFile({ filePath, theme })
+
   const relativePathInTheme = filePath.replace(theme + `/`, ``)
-  console.log(`boo`)
   const fullFilePathToShadow = path.join(
     root,
     `node_modules`,
@@ -13,44 +24,98 @@ const create = async ({ root }, { theme, path: filePath }) => {
     relativePathInTheme
   )
 
-  console.log({ relativePathInTheme, fullFilePathToShadow })
-
   const contents = await fs.readFile(fullFilePathToShadow, `utf8`)
 
-  const fullPath = path.join(root, filePath)
-  const { dir } = path.parse(fullPath)
+  const fullPath = path.join(root, id)
 
-  await mkdirp(dir)
+  await fs.ensureFile(fullPath)
   await fs.writeFile(fullPath, contents)
+
+  const result = await read({ root }, id)
+  return result
 }
 
-const read = async ({ root }, { theme, path: filePath }) => {
-  const relativePathInTheme = filePath.replace(theme + `/`, ``)
-  const fullFilePathToShadow = path.join(
-    root,
-    `node_modules`,
+const read = async ({ root }, id) => {
+  // TODO: Support windows
+  const [_src, theme, ..._filePathParts] = id.split('/')
+
+  const fullPath = path.join(root, id)
+
+  if (!fileExists(fullPath)) {
+    return undefined
+  }
+
+  const contents = await fs.readFile(fullPath, `utf8`)
+
+  const resource = {
+    id,
     theme,
-    relativePathInTheme
-  )
+    path: id,
+    contents
+  }
 
-  const contents = await fs.readFile(fullFilePathToShadow, `utf8`)
-  return contents
+  resource._message = message(resource)
+
+  return resource
 }
 
-const destroy = async ({ root }, { path: filePath }) => {
-  const fullPath = path.join(root, filePath)
-  await fs.unlink(fullPath)
+const destroy = async ({ root }, { id }) => {
+  const resource = await read({ root }, id)
+  await fs.unlink(path.join(root, id))
+  return resource
 }
+
+const schema = {
+  theme: Joi.string(),
+  path: Joi.string(),
+  contents: Joi.string(),
+  ...resourceSchema
+}
+module.exports.schema = schema
+module.exports.validate = resource =>
+  Joi.validate(resource, schema, { abortEarly: false })
 
 module.exports.create = create
 module.exports.update = create
 module.exports.read = read
 module.exports.destroy = destroy
 
-module.exports.plan = (_, { theme, path: filePath }) => {
+const message = resource => `Shadowed ${resource.id || resource.path} from ${resource.theme}`
+
+module.exports.plan = async ({ root }, { theme, path: filePath, id }) => {
+  if (!id) {
+    const [_src, ...filePathParts] = filePath.split('/')
+    id = path.join('src', theme, path.join(...filePathParts))
+  }
+
+  const currentResource = await read({ root }, id) || {}
+
+  const [_src, _theme, ...shadowPathParts] = id.split('/')
+  const fullFilePathToShadow = path.join(
+    root,
+    `node_modules`,
+    theme,
+    'src',
+    path.join(...shadowPathParts)
+  )
+
+  const newContents = await fs.readFile(fullFilePathToShadow, 'utf8')
+  const newResource = {
+    id,
+    theme,
+    path: filePath,
+    contents: newContents
+  }
+
+  const diff = await getDiff(currentResource.contents, newContents)
+
   return {
-    currentState: [],
-    newState: [filePath],
+    id,
+    theme,
+    path: filePath,
+    diff,
+    currentState: currentResource,
+    newState: newResource,
     describe: `Shadow ${filePath} from the theme ${theme}`,
   }
 }
--- packages/gatsby/src/recipes/providers/gatsby/shadow-file.test.js
+++ packages/gatsby/src/recipes/providers/gatsby/shadow-file.test.js
@@ -1,25 +1,37 @@
-const pkgDir = require(`pkg-dir`)
-const os = require(`os`)
 const path = require(`path`)
-const uuid = require(`uuid`)
-const fs = require(`fs-extra`)
+const rimraf = require(`rimraf`)
 
 const shadowFile = require(`./shadow-file`)
 const resourceTestHelper = require(`../resource-test-helper`)
 
-// Make temp directory to run tests in.
-const root = path.join(os.tmpdir(), uuid.v4())
-fs.mkdirSync(root)
+const root = path.join(__dirname, 'fixtures')
 
-// TODO finish this. Shadow File isn't yet updated.
+const cleanup = () => {
+  rimraf.sync(path.join(root, 'src'))
+}
+
+beforeEach(() => {
+  cleanup()
+})
+
+afterEach(() => {
+  cleanup()
+})
+
 describe(`Shadow File resource`, () => {
   test(`e2e shadow file resource test`, async () => {
     await resourceTestHelper({
       resourceModule: shadowFile,
       resourceName: `ShadowFile`,
       context: { root },
-      // initialObject: { path: `file.txt`, content },
-      // partialUpdate: { content: content + `1` },
+      initialObject: {
+        theme: 'gatsby-theme-blog',
+        path: `src/components/author.js`
+      },
+      partialUpdate: {
+        theme: 'gatsby-theme-blog',
+        path: `src/components/author.js`
+      }
     })
   })
 })
