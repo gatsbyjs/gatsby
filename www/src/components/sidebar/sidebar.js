4adcda27a2a27a70600d29ef81c00b53d20a7ae3
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2020-04-13 18:35:03 -0700

    WIP turn the sidebar into a (more efficient) function component)

--- www/src/components/sidebar/sidebar.js
+++ www/src/components/sidebar/sidebar.js
@@ -47,6 +47,212 @@ const getOpenItemHash = (itemList, state) => {
   return false
 }
 
+function _getOpenItemHash(itemList, activeItemLink, activeItemParents) {
+  let result = {}
+  for (let item of itemList) {
+    if (item.items) {
+      result[item.title] =
+        isItemActive(activeItemParents, item) ||
+        activeItemLink.title === item.title
+      result = { ...result, ..._getOpenItemHash(item.items, state) }
+    }
+  }
+  return result
+}
+
+function readLocalStorage(key) {
+  if (hasLocalStorage) {
+    return JSON.parse(localStorage.getItem(`gatsbyjs:sidebar:${key}`))
+  }
+  return {}
+}
+
+function writeLocalStorage(key, state) {
+  if (hasLocalStorage) {
+    localStorage.setItem(`gatsbyjs:sidebar:${key}`, JSON.stringify(state))
+  }
+}
+
+function Sidebar({
+  i18n,
+  title,
+  closeSidebar,
+  itemList,
+  location,
+  position,
+  activeItemHash,
+  disableExpandAll,
+  disableAccordions,
+}) {
+  // TODO initialize state
+  const isSingle = itemList.filter(item => item.level === 0).length === 1
+  const scrollRef = React.useRef(null)
+
+  // Set the scroll position if one is provided
+  React.useEffect(() => {
+    if (scrollRef.current && position) {
+      scrollRef.current.scrollTop = position
+    }
+  }, [position])
+
+  const activeItemLink = getActiveItem(itemList, location, activeItemHash)
+  const activeItemParents = getActiveItemParents(
+    itemList,
+    activeItemLink,
+    []
+  ).map(link => link.title)
+
+  // Get the hash where the only open items are
+  // the hierarchy defined in props
+  const derivedHash = React.useMemo(() => {
+    return _getOpenItemHash(itemList, activeItemLink, activeItemParents)
+  }, [itemList, activeItemLink, activeItemParents])
+
+  // Merge hash in local storage and the derived hash from props
+  const initialHash = React.useMemo(() => {
+    const { openSectionHash } = readLocalStorage(sidebarKey)
+    for (const [key, isOpen] of Object.entries(derivedHash)) {
+      if (isOpen) {
+        openSectionHash[key] = true
+      }
+    }
+    return openSectionHash
+  }, [derivedHash])
+
+  const [openSectionHash, setOpenSectionHash] = React.useState(initialHash)
+  const [expandAll, setExpandAll] = React.useState(false)
+
+  // Write to local storage whenever the open section hash changes
+  React.useEffect(() => {
+    writeLocalStorage(sidebarKey, { openSectionHash })
+  }, [openSectionHash])
+
+  // Update the expandAll variable when the hash changes
+  React.useEffect(() => {
+    setExpandAll(Object.values(openSectionHash).every(isOpen => isOpen))
+  }, [openSectionHash])
+
+  function toggleSection(item) {
+    const newOpenSectionHash = {
+      ...openSectionHash,
+      [item.title]: !openSectionHash[item.title],
+    }
+
+    setOpenSectionHash(newOpenSectionHash)
+  }
+
+  function toggleExpandAll() {
+    if (expandAll) {
+      setOpenSectionHash(derivedHash)
+      setExpandAll(false)
+    } else {
+      const newOpenSectionHash = {}
+      for (const key of Object.keys(openSectionHash)) {
+        newOpenSectionHash[key] = true
+      }
+      setOpenSectionHash(newOpenSectionHash)
+      setExpandAll(false)
+    }
+  }
+
+  return (
+    <section
+      aria-label={i18n._(t`Secondary Navigation`)}
+      id="SecondaryNavigation"
+      className="docSearch-sidebar"
+      sx={{ height: `100%` }}
+    >
+      {!disableExpandAll && (
+        <header
+          sx={{
+            alignItems: `center`,
+            bg: `background`,
+            borderColor: `ui.border`,
+            borderRightStyle: `solid`,
+            borderRightWidth: `1px`,
+            display: `flex`,
+            height: `sidebarUtilityHeight`,
+            pl: 4,
+            pr: 6,
+          }}
+        >
+          <ExpandAllButton onClick={toggleExpandAll} expandAll={expandAll} />
+        </header>
+      )}
+      <nav
+        ref={scrollRef}
+        sx={{
+          WebkitOverflowScrolling: `touch`,
+          bg: `background`,
+          border: 0,
+          display: `block`,
+          overflowY: `auto`,
+          transition: t =>
+            `opacity ${t.transition.speed.default} ${t.transition.curve.default}`,
+          zIndex: 10,
+          borderRightWidth: `1px`,
+          borderRightStyle: `solid`,
+          borderColor: `ui.border`,
+          height: t =>
+            disableExpandAll
+              ? `100%`
+              : `calc(100% - ${t.sizes.sidebarUtilityHeight})`,
+          [mediaQueries.md]: {
+            top: t => `calc(${t.sizes.headerHeight} + ${t.sizes.bannerHeight})`,
+          },
+        }}
+      >
+        <h3
+          sx={{
+            color: `textMuted`,
+            px: 6,
+            fontSize: 1,
+            pt: 6,
+            margin: 0,
+            fontWeight: `body`,
+            textTransform: `uppercase`,
+            letterSpacing: `tracked`,
+          }}
+        >
+          {title}
+        </h3>
+        <ul
+          sx={{
+            m: 0,
+            py: 4,
+            fontSize: 1,
+            bg: `background`,
+            "& li": {
+              m: 0,
+              listStyle: `none`,
+            },
+            "& > li:last-child > span:before": {
+              display: `none`,
+            },
+          }}
+        >
+          {itemList.map((item, index) => (
+            <Item
+              activeItemLink={activeItemLink}
+              activeItemParents={activeItemParents}
+              isActive={item.link === location.pathname}
+              isExpanded={openSectionHash[item.title]}
+              item={item}
+              key={index}
+              location={location}
+              onLinkClick={closeSidebar}
+              onSectionTitleClick={toggleSection}
+              openSectionHash={openSectionHash}
+              isSingle={isSingle}
+              disableAccordions={disableAccordions}
+            />
+          ))}
+        </ul>
+      </nav>
+    </section>
+  )
+}
+
 class SidebarBody extends Component {
   constructor(props, context) {
     super(props, context)
