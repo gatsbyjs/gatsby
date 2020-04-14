b610ee1b04302ce91576e39c882c64fb59c0bc9a
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2020-04-14 12:37:33 -0700

    more test

--- www/src/components/sidebar/__tests__/sidebar.js
+++ www/src/components/sidebar/__tests__/sidebar.js
@@ -1,18 +1,51 @@
 import React from "react"
 import { render } from "@testing-library/react"
+import { ThemeProvider } from "theme-ui"
 
 import Sidebar from "../sidebar"
+import { I18nProvider } from "../../I18nContext"
+import theme from "../../../gatsby-plugin-theme-ui"
+
+const itemList = [
+  { title: "Item 1", link: "/item-1/" },
+  {
+    title: "Section 1",
+    link: "/section-1/",
+    items: [
+      { title: "Subitem 1", link: "/section-1/subitem-1/" },
+      { title: "Subitem 2", link: "/section-1/subitem-2/" },
+    ],
+  },
+  {
+    title: "Section 2",
+    link: "/section-2/",
+    items: [
+      { title: "Subitem 1", link: "/section-2/subitem-1/" },
+      { title: "Subitem 2", link: "/section-2/subitem-2/" },
+    ],
+  },
+]
 
 describe("sidebar", () => {
-  it("opens sections with active items and local storage", () => {
-    render(<Sidebar />)
-    //
+  describe("initialization", () => {
+    it("opens sections with active items", () => {
+      //
+    })
+
+    it("opens sections that are open in local storage", () => {
+      //
+    })
   })
 
   describe("toggle section", () => {
     it("opens the section if it is not open", () => {
-      render(<Sidebar />)
-      //
+      render(
+        <ThemeProvider theme={theme}>
+          <I18nProvider locale="en">
+            <Sidebar itemList={itemList} />
+          </I18nProvider>
+        </ThemeProvider>
+      )
     })
 
     it("closes the section if it is already opened", () => {
