7be6372a5426a4dc84c7211471dcd9d28b26cbc2
Laurie Barth <laurie@LauriesrkLaptop.fios-router.home>
2020-04-15 10:27:44 -0400

    Initial implementation of theme-ui override, currently incompatable

--- packages/gatsby-theme-blog/gatsby-config.js
+++ packages/gatsby-theme-blog/gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = options => {
+  let {disableThemeUiStyling = false} = options
   return {
     plugins: [
       {
--- packages/gatsby-theme-blog/src/gatsby-plugin-theme-ui/index.js
+++ packages/gatsby-theme-blog/src/gatsby-plugin-theme-ui/index.js
@@ -4,16 +4,27 @@ import colors from "./colors"
 import styles from "./styles"
 import prism from "./prism"
 
-export default merge(typography, {
-  initialColorMode: `light`,
-  colors,
-  fonts: {
-    heading: `Montserrat, sans-serif`,
-    monospace: `Consolas, Menlo, Monaco, source-code-pro, Courier New, monospace`,
-  },
-  sizes: {
-    container: 672,
-  },
-  styles,
-  prism,
-})
+import useBlogThemeConfig from "../hooks/configOptions"
+
+const ThemeUiSettings = () => {
+  const blogThemeConfig = useBlogThemeConfig()
+
+  return (
+    !blogThemeConfig.disableThemeUiStyling &&
+    merge(typography, {
+      initialColorMode: `light`,
+      colors,
+      fonts: {
+        heading: `Montserrat, sans-serif`,
+        monospace: `Consolas, Menlo, Monaco, source-code-pro, Courier New, monospace`,
+      },
+      sizes: {
+        container: 672,
+      },
+      styles,
+      prism,
+    })
+  )
+}
+
+export default ThemeUiSettings
