5ee4000ba49d2cc338081b3f96dc094a85a0b6fc
Miles Crighton <miles.crighton@gmail.com>
2020-04-16 13:39:18 +0100

    Added highlight line test

--- packages/gatsby-remark-prismjs/src/__tests__/add-line-numbers.js
+++ packages/gatsby-remark-prismjs/src/__tests__/add-line-numbers.js
@@ -14,4 +14,19 @@ describe(`returns the line numbers container`, () => {
         `</span>`
     )
   })
+  it(`should return as many <span></span> children as there are code lines and highlight classes`, () => {
+    //Expect 6 internal spans
+    const highlightedCode =
+      `line1\n` +
+      `<span class=\\"gatsby-highlight-code-line\\"></span>` +
+      `line2\n` +
+      ` <span class=\\"gatsby-highlight-code-line\\"></span>` +
+      ` line3\n` +
+      `line4`
+    expect(addLineNumbers(highlightedCode)).toEqual(
+      `<span aria-hidden="true" class="line-numbers-rows" style="white-space: normal; width: auto; left: 0;">` +
+        `<span></span><span></span><span></span><span></span><span></span><span></span>` +
+        `</span>`
+    )
+  })
 })
