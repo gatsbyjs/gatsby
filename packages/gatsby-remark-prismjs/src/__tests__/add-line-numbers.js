b3d8cfc9f80cd369b0c9c5e0791d1778b6480443
Michal Piechowiak <misiek.piechowiak@gmail.com>
2020-04-17 02:46:27 +0200

    test: explicitely count line number rows instead of trying to match on pretty confusing html

--- packages/gatsby-remark-prismjs/src/__tests__/add-line-numbers.js
+++ packages/gatsby-remark-prismjs/src/__tests__/add-line-numbers.js
@@ -1,4 +1,5 @@
 const addLineNumbers = require(`../add-line-numbers`)
+const cheerio = require(`cheerio`)
 
 describe(`returns the line numbers container`, () => {
   it(`should return the <span> container with the right classes`, () => {
@@ -23,10 +24,12 @@ describe(`returns the line numbers container`, () => {
       ` <span class=\\"gatsby-highlight-code-line\\"></span>` +
       ` line3\n` +
       `line4`
-    expect(addLineNumbers(highlightedCode)).toEqual(
-      `<span aria-hidden="true" class="line-numbers-rows" style="white-space: normal; width: auto; left: 0;">` +
-        `<span></span><span></span><span></span><span></span><span></span><span></span>` +
-        `</span>`
-    )
+
+    const highlightedCodeWithLineNumbers = addLineNumbers(highlightedCode)
+
+    const $ = cheerio.load(highlightedCodeWithLineNumbers)
+    const numberOfLineNumbers = $(`.line-numbers-rows > span`).length
+
+    expect(numberOfLineNumbers).toEqual(4 + 2)
   })
 })
