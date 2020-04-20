9c8163ee9c1be6810eeec255f1a1c78652dc76b6
Kyle Mathews <mathews.kyle@gmail.com>
2020-04-20 11:28:55 -0600

    Update packages/gatsby-recipes/src/providers/utils/get-diff.js
    Co-Authored-By: John Otander <johnotander@gmail.com>

--- packages/gatsby-recipes/src/providers/utils/get-diff.js
+++ packages/gatsby-recipes/src/providers/utils/get-diff.js
@@ -1,11 +1,6 @@
 const diff = require(`jest-diff`).default
 const chalk = require(`chalk`)
 
-// console.log(process.env.NODE_ENV)
-// if (process.env.NODE_ENV === `test`) {
-// process.env.FORCE_COLOR = 0
-// }
-
 module.exports = async (oldVal, newVal) => {
   const options = {
     aAnnotation: `Original`,
