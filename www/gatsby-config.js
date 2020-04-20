8bdf66c023749a7918a69a7f0e986958d94f9bec
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2020-04-20 16:07:53 -0700

    add note about why we use path aliases

--- www/gatsby-config.js
+++ www/gatsby-config.js
@@ -90,6 +90,8 @@ module.exports = {
       resolve: `gatsby-alias-imports`,
       options: {
         aliases: {
+          // Relative paths break translations of the docs, so use an alias instead
+          // https://www.gatsbyjs.org/contributing/docs-and-blog-components/#importing-other-components
           "@components": "src/components",
         },
       },
