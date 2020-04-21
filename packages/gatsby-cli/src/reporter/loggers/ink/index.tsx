8ee74c1f85f75eb7ede2c22eb87eee21ebc9e8c6
Arthur <arthur@mediamonks.com>
2020-04-21 08:30:41 -0700

    chore(gatsby-cli) Migrate remaining files in reporters/logger/ink to Typscript (#22782)
    * Migrate remaining files in gatsby-cli/reporters/logger/ink to TS
    
    * Rename "IProps" to something more contextual
    Extend interface parameter individually instead of using utility
    
    * Clean up after merging the redux master changes
    
    Co-authored-by: Blaine Kasten <blainekasten@gmail.com>

--- packages/gatsby-cli/src/reporter/loggers/ink/index.tsx
+++ packages/gatsby-cli/src/reporter/loggers/ink/index.tsx
@@ -1,16 +1,13 @@
 import React, { useContext } from "react"
 import { render } from "ink"
 import StoreStateContext, { StoreStateProvider } from "./context"
-
 import CLI from "./cli"
 
-const ConnectedCLI = () => {
+const ConnectedCLI: React.FC = (): React.ReactElement => {
   const state = useContext(StoreStateContext)
   const showStatusBar =
-    state.program &&
-    state.program._ &&
-    state.program._[0] === `develop` &&
-    state.program.status === `BOOTSTRAP_FINISHED`
+    state.program?._?.[0] === `develop` &&
+    state.program?.status === `BOOTSTRAP_FINISHED`
 
   return <CLI showStatusBar={Boolean(showStatusBar)} logs={state.logs} />
 }
