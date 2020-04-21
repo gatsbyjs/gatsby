ab983bf25bb584f75d4327d5101d4d75e5e530ea
Mikhail Novikov <freiksenet@gmail.com>
2020-04-21 15:58:10 +0300

    Fix recipe test problems
    * Strip ansi colors on diff during tests
    * Use tmpdir to run fixtures
    * Fix snapshots having wrong fixture formatting

--- packages/gatsby-recipes/src/providers/gatsby/plugin.test.js
+++ packages/gatsby-recipes/src/providers/gatsby/plugin.test.js
@@ -1,24 +1,49 @@
 const fs = require(`fs-extra`)
 const path = require(`path`)
+const tmp = require(`tmp-promise`)
 
 const plugin = require(`./plugin`)
 const { addPluginToConfig, getPluginsFromConfig } = require(`./plugin`)
 const resourceTestHelper = require(`../resource-test-helper`)
 
-const root = path.join(__dirname, `./fixtures/gatsby-starter-blog`)
-const helloWorldRoot = path.join(
+const STARTER_BLOG_FIXTURE = path.join(
+  __dirname,
+  `./fixtures/gatsby-starter-blog`
+)
+const HELLO_WORLD_FIXTURE = path.join(
   __dirname,
   `./fixtures/gatsby-starter-hello-world`
 )
 const name = `gatsby-plugin-foo`
-const configPath = path.join(root, `gatsby-config.js`)
 
 describe(`gatsby-plugin resource`, () => {
+  let tmpDir
+  let starterBlogRoot
+  let helloWorldRoot
+  let configPath
+  beforeAll(async () => {
+    tmpDir = await tmp.dir({
+      unsafeCleanup: true,
+    })
+    starterBlogRoot = path.join(tmpDir.path, `gatsby-starter-blog`)
+    helloWorldRoot = path.join(tmpDir.path, `gatsby-starter-hello-world`)
+    configPath = path.join(helloWorldRoot, `gatsby-config.js`)
+    await fs.ensureDir(starterBlogRoot)
+    await fs.copy(STARTER_BLOG_FIXTURE, starterBlogRoot)
+    await fs.ensureDir(helloWorldRoot)
+    await fs.copy(HELLO_WORLD_FIXTURE, helloWorldRoot)
+  })
+  afterAll(async () => {
+    if (tmpDir) {
+      await tmpDir.cleanup()
+    }
+  })
+
   test(`e2e plugin resource test`, async () => {
     await resourceTestHelper({
       resourceModule: plugin,
       resourceName: `GatsbyPlugin`,
-      context: { root },
+      context: { root: starterBlogRoot },
       initialObject: { id: name, name },
       partialUpdate: { id: name },
     })
