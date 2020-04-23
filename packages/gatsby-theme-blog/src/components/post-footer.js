5297b89c90f317fe00f7124c7b678894b64eefbb
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2020-04-23 08:27:51 -0600

    refactor(gatsby-theme-blog-core): Use more semantic HTML for articles

--- packages/gatsby-theme-blog/src/components/post-footer.js
+++ packages/gatsby-theme-blog/src/components/post-footer.js
@@ -14,30 +14,32 @@ const Footer = ({ previous, next }) => (
     <Styled.hr />
     <Bio />
     {(previous || next) && (
-      <Flex
-        as="ul"
-        css={css({
-          flexWrap: `wrap`,
-          justifyContent: `space-between`,
-          listStyle: `none`,
-          padding: 0,
-        })}
-      >
-        <li>
-          {previous && (
-            <Styled.a as={Link} to={previous.slug} rel="prev">
-              ← {previous.title}
-            </Styled.a>
-          )}
-        </li>
-        <li>
-          {next && (
-            <Styled.a as={Link} to={next.slug} rel="next">
-              {next.title} →
-            </Styled.a>
-          )}
-        </li>
-      </Flex>
+      <nav>
+        <Flex
+          as="ul"
+          css={css({
+            flexWrap: `wrap`,
+            justifyContent: `space-between`,
+            listStyle: `none`,
+            padding: 0,
+          })}
+        >
+          <li>
+            {previous && (
+              <Styled.a as={Link} to={previous.slug} rel="prev">
+                ← {previous.title}
+              </Styled.a>
+            )}
+          </li>
+          <li>
+            {next && (
+              <Styled.a as={Link} to={next.slug} rel="next">
+                {next.title} →
+              </Styled.a>
+            )}
+          </li>
+        </Flex>
+      </nav>
     )}
   </footer>
 )
