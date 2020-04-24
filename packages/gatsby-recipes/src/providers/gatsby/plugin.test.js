aad3716af5c6ecf6131355bc4db9fc9a8f9ae397
John Otander <johnotander@gmail.com>
2020-04-24 14:02:18 -0600

    feat(gatsby-recipes): Allow for basic config objects in plugin resource (#23433)
    * feat(gatsby-recipes): Allow for basic config objects in plugin resource
    
    - Add support for configuration objects (that are JSON serializable)
    - Update joi2gql to support objects as JSON
    - Use a unique, serialized key for composable plugins
    - Break out functionality into smaller modules for testing
    
    * fix(gatsby-recipes): Ensure keys are different than plugin name

--- packages/gatsby-recipes/src/providers/gatsby/plugin.test.js
+++ packages/gatsby-recipes/src/providers/gatsby/plugin.test.js
@@ -61,14 +61,98 @@ describe(`gatsby-plugin resource`, () => {
 
   test(`does not add the same plugin twice by default`, async () => {
     const configSrc = await fs.readFile(configPath, `utf8`)
-    const newConfigSrc = addPluginToConfig(
-      configSrc,
-      `gatsby-plugin-react-helmet`
-    )
+    const newConfigSrc = addPluginToConfig(configSrc, {
+      name: `gatsby-plugin-react-helmet`,
+    })
     const plugins = getPluginsFromConfig(newConfigSrc)
 
     const result = [...new Set(plugins)]
 
     expect(result).toEqual(plugins)
+  })
+
+  // A key isn't required for gatsby plugin, but when you want to distinguish
+  // between multiple of the same plugin, you can specify it to target config changes.
+  test(`validates the gatsby-source-filesystem specifies a key`, async () => {
+    const result = plugin.validate({ name: `gatsby-source-filesystem` })
+
+    expect(result.error).toEqual(
+      `gatsby-source-filesystem requires a key to be set`
+    )
+  })
+
+  test(`validates the gatsby-source-filesystem specifies a key that isn't equal to the name`, async () => {
+    const result = plugin.validate({
+      name: `gatsby-source-filesystem`,
+      key: `gatsby-source-filesystem`,
+    })
+
+    expect(result.error).toEqual(
+      `gatsby-source-filesystem requires a key to be different than the plugin name`
+    )
+  })
+
+  test(`adds multiple gatsby-source-filesystems and reads with key`, async () => {
+    const context = { root: helloWorldRoot }
+    const fooPlugin = {
+      key: `foo-data-sourcing`,
+      name: `gatsby-source-filesystem`,
+      options: {
+        name: `foo`,
+        path: `foo`,
+      },
+    }
+    const barPlugin = {
+      key: `bar-data-sourcing`,
+      name: `gatsby-source-filesystem`,
+      options: {
+        name: `bar`,
+        path: `bar`,
+      },
+    }
+
+    await plugin.create(context, fooPlugin)
+    await plugin.create(context, barPlugin)
+
+    const barResult = await plugin.read(context, barPlugin.key)
+    const fooResult = await plugin.read(context, fooPlugin.key)
+
+    expect(barResult.key).toEqual(barPlugin.key)
+    expect(fooResult.key).toEqual(fooPlugin.key)
+    expect(barResult.options.name).toEqual(barPlugin.options.name)
+    expect(fooResult.options.name).toEqual(fooPlugin.options.name)
+
+    const newBarResult = await plugin.update(context, {
+      ...barResult,
+      options: { path: `new-bar` },
+    })
+
+    expect(newBarResult.key).toEqual(barPlugin.key)
+    expect(newBarResult.options).toEqual({ path: `new-bar` })
+
+    await plugin.destroy(context, barResult)
+    await plugin.destroy(context, fooResult)
+  })
+
+  test(`handles config options as an object`, async () => {
+    const configSrc = await fs.readFile(configPath, `utf8`)
+    const newConfigSrc = addPluginToConfig(configSrc, {
+      name: `gatsby-plugin-foo`,
+      options: {
+        foo: 1,
+        bar: `baz`,
+        baz: `qux`,
+        otherStuff: [
+          {
+            foo: `bar2`,
+            bar: [{ foo: `bar` }],
+          },
+        ],
+      },
+    })
+
+    const result = getPluginsFromConfig(newConfigSrc)
+
+    expect(result).toMatchSnapshot()
   })
 })
