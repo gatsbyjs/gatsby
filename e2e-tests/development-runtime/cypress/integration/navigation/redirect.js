7b6e32f9c2aa0806ed77db35d442432eb6e0fcbf
Michal Piechowiak <misiek.piechowiak@gmail.com>
2020-04-28 01:21:33 +0200

    test case of 404 not presetnt as well

--- e2e-tests/development-runtime/cypress/integration/navigation/redirect.js
+++ e2e-tests/development-runtime/cypress/integration/navigation/redirect.js
@@ -3,15 +3,21 @@ Cypress.on(`window:before:load`, win => {
   spy = cy.spy(win.console, `error`).as(`spyWinConsoleError`)
 })
 
-describe(`redirect`, () => {
+const runTests = () => {
   it(`should redirect page to index page when there is no such page`, () => {
     cy.visit(`/redirect-without-page`).waitForRouteChange()
 
     cy.location(`pathname`).should(`equal`, `/`)
     cy.then(() => {
-      // assertion below is weird (it's not a function), but is correct - need to disable eslint rule for that
-      // eslint-disable-next-line no-unused-expressions
-      expect(spy).not.to.be.called
+      const calls = spy.getCalls()
+
+      const callsAboutRedirectMatchingPage = calls.filter(call => {
+        return call.args[0].includes(
+          "matches both a page and a redirect; this is probably not intentional."
+        )
+      })
+
+      expect(callsAboutRedirectMatchingPage.length).to.equal(0)
     })
   })
 
@@ -20,12 +26,73 @@ describe(`redirect`, () => {
 
     cy.location(`pathname`).should(`equal`, `/`)
     cy.then(() => {
-      // assertion below is weird (it's not a function), but is correct - need to disable eslint rule for that
-      // eslint-disable-next-line no-unused-expressions
-      expect(spy).to.be.called
+      const calls = spy.getCalls()
+
+      const callsAboutRedirectMatchingPage = calls.filter(call => {
+        return call.args[0].includes(
+          "matches both a page and a redirect; this is probably not intentional."
+        )
+      })
+
+      expect(callsAboutRedirectMatchingPage.length).not.to.equal(0)
       expect(spy).to.be.calledWith(
         `The route "/redirect" matches both a page and a redirect; this is probably not intentional.`
       )
     })
+  })
+}
+
+describe(`redirect`, () => {
+  describe("404 is present", () => {
+    before(() => {
+      cy.task(`restoreAllBlockedResources`)
+    })
+
+    // this is sanity check for this group
+    it(`make sure 404 is present`, () => {
+      cy.visit(`/______not_existing_page`).waitForRouteChange()
+      cy.queryByText("Preview custom 404 page").click()
+      cy.queryByText("A custom 404 page wasn't detected", {
+        exact: false,
+      }).should(`not.exist`)
+      cy.queryByText(
+        "You just hit a route that does not exist... the sadness."
+      ).should(`exist`)
+    })
+
+    runTests()
+  })
+
+  describe("no 404", () => {
+    before(() => {
+      cy.task(`restoreAllBlockedResources`)
+
+      cy.task(`blockAssetsForPage`, {
+        pagePath: `/404`,
+        filter: `page-data`,
+      })
+      cy.task(`blockAssetsForPage`, {
+        pagePath: `/404.html`,
+        filter: `page-data`,
+      })
+    })
+
+    after(() => {
+      cy.task(`restoreAllBlockedResources`)
+    })
+
+    it(`make sure 404 is NOT present`, () => {
+      cy.visit(`/______not_existing_page`).waitForRouteChange()
+      cy.queryByText("Preview custom 404 page").click()
+      cy.queryByText("A custom 404 page wasn't detected", {
+        exact: false,
+      }).should(`exist`)
+      cy.queryByText(
+        "You just hit a route that does not exist... the sadness.",
+        { exact: false }
+      ).should(`not.exist`)
+    })
+
+    runTests()
   })
 })
