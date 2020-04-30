51d88e925c40fd626036b6a90fbc9f39153051ab
Michal Piechowiak <misiek.piechowiak@gmail.com>
2020-04-30 02:34:35 +0200

    fix: @typescript-eslint/interface-name-prefix needs "I" prefix

--- packages/babel-preset-gatsby/src/dependencies.ts
+++ packages/babel-preset-gatsby/src/dependencies.ts
@@ -3,7 +3,7 @@
 
 import path from "path"
 
-interface PresetOptions {
+interface IPresetOptions {
   stage?: "build-javascript" | "build-html" | "develop" | "develop-html"
 }
 
@@ -11,7 +11,7 @@ interface PresetOptions {
 // via require.resolve
 // This function has a better inference than would be beneficial to type, and it's relatively easy to grok.
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export default (_, options: PresetOptions = {}) => {
+export default (_, options: IPresetOptions = {}) => {
   const absoluteRuntimePath = path.dirname(
     require.resolve(`@babel/runtime/package.json`)
   )
