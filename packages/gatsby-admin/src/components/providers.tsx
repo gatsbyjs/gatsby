ae32af3affe471d829f08b2b38f934e4c78baf3f
Max Stoiber <contact@mxstbr.com>
2020-05-04 13:31:24 +0200

    Start Recipes GraphQL server when running develop and fetch fresh port in admin

--- packages/gatsby-admin/src/components/providers.tsx
+++ packages/gatsby-admin/src/components/providers.tsx
@@ -1,7 +1,27 @@
 import React from "react"
-import { Provider } from "urql"
-import client from "../urql-client"
+import { Provider, Client } from "urql"
+import { createUrqlClient } from "../urql-client"
 
-export default ({ children }): React.ReactElement => (
-  <Provider value={client}>{children}</Provider>
-)
+export default ({ children }): React.ReactElement => {
+  const [status, setStatus] = React.useState(`loading`)
+  const [client, setClient] = React.useState<Client | null>(null)
+
+  React.useEffect(() => {
+    setStatus(`loading`)
+    fetch(`/___services`)
+      .then(res => res.json())
+      .then(json => {
+        setStatus(`idle`)
+        if (json.recipesgraphqlserver) {
+          setClient(createUrqlClient({ port: json.recipesgraphqlserver.port }))
+        }
+      })
+  }, [])
+
+  if (status === `loading`) return <p>Loading...</p>
+
+  if (client === null)
+    return <p>It looks like no develop process is running.</p>
+
+  return <Provider value={client}>{children}</Provider>
+}
