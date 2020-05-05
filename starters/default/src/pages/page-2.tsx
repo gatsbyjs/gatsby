19332305408e9ce024df79c88d26b18d02ea533c
Blaine Kasten <blainekasten@gmail.com>
2020-05-05 13:43:46 -0500

    feature(gatsby): Support TypeScript by default by compiling TS files (#23547)
    * feature(gatsby): Support TypeScript by default by compiling TS files
    
    * add a test to ensure we arent adding an extra gatsby-plugin-typescript
    
    * Make a TypeScript file in gatsby-starter
    
    * Add TypeScript example to blog starter
    
    * Update Using Typescript example
    
    * update gatsby-plugin-typescript README.md
    
    * Add Basic TypeScript example
    
    * Update packages/gatsby-plugin-typescript/README.md
    
    Co-authored-by: Aisha Blake <aisha@gatsbyjs.com>
    
    * Update packages/gatsby-plugin-typescript/README.md
    
    Co-authored-by: Aisha Blake <aisha@gatsbyjs.com>
    
    * Add more to the TS docs
    
    * Add docs to sidebar
    
    * Use you as the pronoun
    
    * Fix e2e test
    
    Co-authored-by: Aisha Blake <aisha@gatsbyjs.com>

--- starters/default/src/pages/page-2.tsx
+++ starters/default/src/pages/page-2.tsx
@@ -1,14 +1,15 @@
+// Gatsby supports TypeScript natively!
 import React from "react"
-import { Link } from "gatsby"
+import { PageProps, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
+const SecondPage = (props: PageProps) => (
   <Layout>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <p>Welcome to page 2 ({props.path})</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
