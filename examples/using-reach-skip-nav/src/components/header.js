bb6d5bcdff233075c079972d8ceca9e2512dc320
madalynrose <madalyn@gatsbyjs.com>
2020-05-06 12:30:41 -0400

    remove unneccessary image content from example

--- examples/using-reach-skip-nav/src/components/header.js
+++ examples/using-reach-skip-nav/src/components/header.js
@@ -1,5 +1,4 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
 import "./header.css"
 
@@ -31,13 +30,5 @@ const Header = ({ siteTitle }) => (
     </div>
   </header>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
--- examples/using-reach-skip-nav/src/pages/index.js
+++ examples/using-reach-skip-nav/src/pages/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -11,9 +10,6 @@ const IndexPage = () => (
       "Gatsby" in the footer.
     </h1>
     <p>Welcome to your new Gatsby site.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
   </Layout>
 )
 
