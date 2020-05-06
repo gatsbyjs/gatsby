ffe39e799d11a9bfe18ad0e5ff454f04c6063484
Tyler Barnes <tylerdbarnes@gmail.com>
2020-05-06 12:18:07 -0700

    add script to update 1 article title for benchmarking data updates

--- benchmarks/mdx/scripts/data-update.ts
+++ benchmarks/mdx/scripts/data-update.ts
@@ -1,1 +1,26 @@
-// noop for now, but will be created later.
+import faker from "faker"
+import frontMatter from "front-matter"
+import fs from "fs-extra"
+import glob from "glob"
+;(async () => {
+  // get the first article file
+  const [articleFilePath] = glob.sync(`./src/articles/**/*.mdx`)
+  const articleFileContents = await fs.readFile(articleFilePath, `utf8`)
+
+  // parse the frontmatter
+  const { attributes }: any = frontMatter(articleFileContents)
+
+  // generate a new title
+  const title = faker.random.words(Math.floor(Math.random() * 5) + 3)
+
+  // add the new title
+  const updatedArticleFile = articleFileContents.replace(
+    attributes.title,
+    title
+  )
+
+  // write back to the article file
+  await fs.writeFile(articleFilePath, updatedArticleFile, `utf8`)
+
+  console.log(`updated ${articleFilePath} with a new title`)
+})()
