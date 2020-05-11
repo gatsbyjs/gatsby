f166da0a02ad6ff0c1dfde5a5ee06a7fafe670d0
Michal Piechowiak <misiek.piechowiak@gmail.com>
2020-05-11 14:40:00 +0200

    revert stylistic changes

--- packages/gatsby/src/utils/app-data.ts
+++ packages/gatsby/src/utils/app-data.ts
@@ -1,13 +1,12 @@
+import fs from "fs-extra"
 import path from "path"
-
-import { outputJson, pathExistsSync } from "fs-extra"
 
 const APP_DATA_JSON = `app-data.json`
 
 export const write = (publicDir: string, hash: string): Promise<void> =>
-  outputJson(path.join(publicDir, `page-data`, APP_DATA_JSON), {
+  fs.outputJson(path.join(publicDir, `page-data`, APP_DATA_JSON), {
     webpackCompilationHash: hash,
   })
 
 export const exists = (publicDir: string): boolean =>
-  pathExistsSync(path.join(publicDir, `page-data`, APP_DATA_JSON))
+  fs.pathExistsSync(path.join(publicDir, `page-data`, APP_DATA_JSON))
