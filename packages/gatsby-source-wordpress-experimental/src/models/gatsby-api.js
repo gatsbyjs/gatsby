3b17259b9e70ac7d424a69d72548cfbbb50c07a5
Tyler Barnes <tylerdbarnes@gmail.com>
2020-05-13 12:43:27 -0700

    lower default query depth

--- packages/gatsby-source-wordpress-experimental/src/models/gatsby-api.js
+++ packages/gatsby-source-wordpress-experimental/src/models/gatsby-api.js
@@ -31,7 +31,7 @@ const defaultPluginOptions = {
     },
   },
   schema: {
-    queryDepth: 15,
+    queryDepth: 10,
     circularQueryLimit: 4,
     typePrefix: `Wp`,
     timeout: 30 * 1000, // 30 seconds
