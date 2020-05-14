7153d62718514a00a336caca915c8ac63a234e7c
Gergő Gazda <info@gazdagergo.com>
2020-05-14 09:01:43 +0200

    style(plugin-options): delete semicolons

--- packages/gatsby-source-shopify/src/plugin-options.js
+++ packages/gatsby-source-shopify/src/plugin-options.js
@@ -1,93 +1,93 @@
 // created this singalton class as a quick way to reference options anywhere within the plugin
 
 class PluginOptions {
-  #shopName;
+  #shopName
 
-  #accessToken;
+  #accessToken
 
-  #apiVersion;
+  #apiVersion
 
-  #verbose;
+  #verbose
 
-  #paginationSize;
+  #paginationSize
 
-  #includeCollections;
+  #includeCollections
 
-  #downloadImages;
+  #downloadImages
 
-  #defaultImageUrl;
+  #defaultImageUrl
 
   get shopName() {
-    return this.#shopName;
+    return this.#shopName
   }
 
   get accessToken() {
-    return this.#accessToken;
+    return this.#accessToken
   }
 
   get apiVersion() {
-    return this.#apiVersion || '2020-01';
+    return this.#apiVersion || '2020-01'
   }
 
   get verbose() {
-    return this.#verbose || true;
+    return this.#verbose || true
   }
 
   get paginationSize() {
-    return this.#paginationSize || 250;
+    return this.#paginationSize || 250
   }
 
   get includeCollections() {
-    return this.#includeCollections || ['shop', 'content'];
+    return this.#includeCollections || ['shop', 'content']
   }
 
   get downloadImages() {
-    return this.#downloadImages;
+    return this.#downloadImages
   }
 
   get defaultImageUrl() {
-    return this.#defaultImageUrl;
+    return this.#defaultImageUrl
   }
 
   setShopName(shopName) {
-    this.#shopName = shopName;
-    return this;
+    this.#shopName = shopName
+    return this
   }
 
   setAccessToken(accessToken) {
-    this.#accessToken = accessToken;
-    return this;
+    this.#accessToken = accessToken
+    return this
   }
 
   setApiVersion(apiVersion) {
-    this.#apiVersion = apiVersion;
-    return this;
+    this.#apiVersion = apiVersion
+    return this
   }
 
   setVerbose(verbose) {
-    this.#verbose = verbose;
-    return this;
+    this.#verbose = verbose
+    return this
   }
 
   setPaginationSize(paginationSize) {
-    this.#paginationSize = paginationSize;
-    return this;
+    this.#paginationSize = paginationSize
+    return this
   }
 
   setIncludeCollections(includeCollections) {
-    this.#includeCollections = includeCollections;
-    return this;
+    this.#includeCollections = includeCollections
+    return this
   }
 
   setDownloadImages(downloadImages) {
-    this.#downloadImages = downloadImages;
-    return this;
+    this.#downloadImages = downloadImages
+    return this
   }
 
   setDefaultImageUrl(defaultImageUrl) {
-    this.#defaultImageUrl = defaultImageUrl;
-    return this;
+    this.#defaultImageUrl = defaultImageUrl
+    return this
   }
 }
 
-export default new PluginOptions();
+export default new PluginOptions()
