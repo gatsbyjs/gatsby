091a25135ae6ed8c265fc37b03cbfc235cbbc792
Wiput Pootong <wiput.pootong@gmail.com>
2020-05-16 20:15:26 +0700

    refactor: make sanitizeAction clean

--- packages/gatsby-cli/src/reporter/loggers/json/index.ts
+++ packages/gatsby-cli/src/reporter/loggers/json/index.ts
@@ -1,26 +1,28 @@
 import { onLogAction } from "../../redux/index"
-import { ActionsUnion } from "../../redux/types"
+import { ActionsUnion, ISetStatus } from "../../redux/types"
 import stripAnsi from "strip-ansi"
 import _ from "lodash"
 
-onLogAction((action: ActionsUnion) => {
-  const sanitizedAction = {
-    ...action,
-    payload:
-      typeof action.payload !== `string`
-        ? `text` in action.payload && `statusText` in action.payload
-          ? {
-              ...action.payload,
-              text: action.payload.text && stripAnsi(action.payload.text),
-              statusText:
-                action.payload.statusText &&
-                stripAnsi(action.payload.statusText),
-            }
-          : {
-              ...action.payload,
-            }
-        : action.payload,
+const isStringPayload = (action: ActionsUnion): action is ISetStatus =>
+  typeof action.payload === `string`
+
+const sanitizeAction = (action: ActionsUnion): ActionsUnion => {
+  if (isStringPayload(action)) {
+    return action
   }
+
+  if (`text` in action.payload && action.payload.text) {
+    action.payload.text = stripAnsi(action.payload.text)
+  }
+  if (`statusText` in action.payload && action.payload.statusText) {
+    action.payload.statusText = stripAnsi(action.payload.statusText)
+  }
+
+  return action
+}
+
+onLogAction((action: ActionsUnion) => {
+  const sanitizedAction = sanitizeAction(action)
 
   process.stdout.write(JSON.stringify(sanitizedAction) + `\n`)
 })
