1a77053fd958e5af85ecf8acfcc99352d9ceac03
Max Stoiber <contact@mxstbr.com>
2020-05-20 17:09:57 +0200

    Named default export

--- packages/gatsby-recipes/src/index.js
+++ packages/gatsby-recipes/src/index.js
@@ -1,7 +1,9 @@
 export { startGraphQLServer } from "./graphql-server"
 
 // data = { recipe?: string, graphqlPort: number, projectRoot: string }
-export default async data => {
+const recipesHandler = async data => {
   const cli = require(`import-jsx`)(require.resolve(`./cli`))
   return cli(data)
 }
+
+export default recipesHandler
