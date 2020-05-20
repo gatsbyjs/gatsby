130cb42418b9963396d164b9edf24d564f4e0c56
Max Stoiber <contact@mxstbr.com>
2020-05-20 16:20:41 +0200

    Actually edit the right file...

--- packages/gatsby-recipes/src/index.js
+++ packages/gatsby-recipes/src/index.js
@@ -1,7 +1,7 @@
-exports.startGraphQLServer = require(`./graphql-server`).startGraphQLServer
-
 // data = { recipe?: string, graphqlPort: number, projectRoot: string }
 module.exports = async data => {
   const cli = require(`import-jsx`)(require.resolve(`./cli`))
   return cli(data)
 }
+
+module.exports.startGraphQLServer = require(`./graphql-server`).startGraphQLServer
