5112406bb3bb7c1d5b659476e9929c0183d869ab
Max Stoiber <contact@mxstbr.com>
2020-05-20 11:17:21 +0200

    Explicitly import from /index to avoid issues with importing old builds

--- packages/gatsby-cli/src/recipes.ts
+++ packages/gatsby-cli/src/recipes.ts
@@ -1,5 +1,6 @@
 import { trackCli } from "gatsby-telemetry"
-import startGraphQLServer from "gatsby-recipes/dist/graphql"
+// NOTE(@mxstbr): I explicitly import from /index because the file used to live at graphql.js, which means developers with old builds on their local machines will have that old version imported instead of the new one with changes
+import startGraphQLServer from "gatsby-recipes/dist/graphql/index.js"
 
 export async function recipesHandler(
   recipe: string | undefined
