cda72c89c831f7b98da2ae89e3a7b2bbede12f75
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2020-05-22 16:55:26 -0700

    make a dictionary

---
+++ retext-spell.js
@@ -0,0 +1,17 @@
+const fs = require(`fs`)
+
+const errors = fs.readFileSync(`./errors.txt`, `utf-8`)
+const matches = errors.matchAll(/`(.*)` is misspelt/g)
+
+const words = new Set()
+
+for (const match of matches) {
+  words.add(match[1])
+}
+
+const wordList = [...words]
+wordList.sort()
+
+for (const word of wordList) {
+  console.log(word)
+}
