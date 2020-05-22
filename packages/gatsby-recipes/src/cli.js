abedd3c3ac32515f98bc59993a93af95d0d5cc26
Max Stoiber <contact@mxstbr.com>
2020-05-22 08:39:04 +0200

    chore(gatsby): start recipes graphql server during gatsby develop (#23733)
    * Move startGraphQLServer code from cli to recipes
    
    * Make shared startGraphQLServer util use services
    
    * Do not return data of unlocked services as they are no longer running
    
    * Rename variable for clarity
    
    * Explicitly import from /index to avoid issues with importing old builds
    
    * Start recipes GraphQL server on gatsby develop
    
    * Make recipes command match program.directory from develop command
    
    * Use program.directory in recipes handler
    
    * Move graphql server to fix cloud tests
    
    * Force start GraphQL server in develop
    
    * Start Recipes GraphQL server from gatsby-cli to avoid recipes dependency in gatsby package
    
    * Store JSON in GraphQL server lock data
    
    * Proper exports/imports from gatsby-recipes
    
    * rm index.js!
    
    * Actually edit the right file...
    
    * Refactor gatsby-recipes to export an ESM
    
    * Named default export

--- packages/gatsby-recipes/src/cli.js
+++ packages/gatsby-recipes/src/cli.js
@@ -289,7 +289,7 @@ log(
   `======================================= ${new Date().toJSON()}`
 )
 
-module.exports = ({ recipe, graphqlPort, projectRoot }) => {
+module.exports = async ({ recipe, graphqlPort, projectRoot }) => {
   try {
     const GRAPHQL_ENDPOINT = `http://localhost:${graphqlPort}/graphql`
 
@@ -606,7 +606,8 @@ module.exports = ({ recipe, graphqlPort, projectRoot }) => {
     const Recipe = () => <Wrapper />
 
     // Enable experimental mode for more efficient reconciler and renderer
-    render(<Recipe />, { experimental: true })
+    const { waitUntilExit } = render(<Recipe />, { experimental: true })
+    await waitUntilExit()
   } catch (e) {
     log(e)
   }
