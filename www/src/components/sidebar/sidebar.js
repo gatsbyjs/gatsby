e9e17379233e96a06941cb02e670eacae3734bbc
Alexey Pyzhianov <alex.pyzhianov@gmail.com>
2020-05-25 11:44:13 +0300

    fix(www) Fix scroll inside sidebar moving the content
    * Previously scrolling or swiping inside the navigation
    sidebar could result in main content moving. Fixed for
    most browsers with overflow-behavior: contain
    
    Resolves #24255

--- www/src/components/sidebar/sidebar.js
+++ www/src/components/sidebar/sidebar.js
@@ -153,6 +153,7 @@ export default withI18n()(function Sidebar({
             border: 0,
             display: `block`,
             overflowY: `auto`,
+            overscrollBehavior: `contain`,
             transition: t =>
               `opacity ${t.transition.speed.default} ${t.transition.curve.default}`,
             zIndex: 10,
