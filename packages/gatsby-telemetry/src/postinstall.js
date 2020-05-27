cd667b58f636378641cdc4715612207580c84b48
Eyas Sharaiha <eyas@google.com>
2020-05-26 21:38:28 -0400

    Fix postinstall script.

--- packages/gatsby-telemetry/src/postinstall.js
+++ packages/gatsby-telemetry/src/postinstall.js
@@ -1,6 +1,6 @@
 try {
-  const showAnalyticsNotification = require(`./showAnalyticsNotification`)
-  const EventStorage = require(`./event-storage`)
+  const { showAnalyticsNotification } = require(`./notification`)
+  const { EventStorage } = require(`./event-storage`)
 
   const { isCI } = require(`gatsby-core-utils`)
   const eventStorage = new EventStorage()
