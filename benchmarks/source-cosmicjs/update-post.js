4a8ea62f66b4da225c717bcd0dc3d908d05f4e71
jazibsawar <jazibsawar@gmail.com>
2020-05-28 22:34:04 +0300

    chore(cosmic-benchmark): add update post script

---
+++ benchmarks/source-cosmicjs/update-post.js
@@ -0,0 +1,34 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV || 'development'}`,
+})
+
+const Cosmic = require('cosmicjs')
+
+const bucket = Cosmic().bucket({
+  slug: process.env.COSMIC_BUCKET,
+  read_key: process.env.COSMIC_READ_KEY,
+  write_key: process.env.COSMIC_WRITE_KEY
+})
+
+const DATASET = (Number(process.env.GATSBY_COSMIC_DATASET) || 512)
+
+const randomDoc = Math.floor(Math.random() * DATASET)
+
+console.log(`Getting article number ${randomDoc}`)
+
+const run = async () => {
+  const { objects } = await bucket.getObjects({
+    skip: randomDoc,
+    limit: 1,
+    props: 'slug,title'
+  })
+  if (objects && objects.length > 0) {
+    const { slug, title } = objects[0];
+    await bucket.editObject({
+      slug,
+      title: `${title}!`
+    })
+    console.log(`Updated article number ${randomDoc}`)
+  }
+}
+run();
