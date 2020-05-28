76e0e80d5f815b3d9957c084f359462b256ef1cd
Kyle Mathews <mathews.kyle@gmail.com>
2020-05-28 13:00:44 -0700

    WIP

--- packages/gatsby-recipes/src/gui.js
+++ packages/gatsby-recipes/src/gui.js
@@ -6,6 +6,10 @@ const { useState } = require(`react`)
 const MDX = require(`@mdx-js/runtime`).default
 const ansi2HTML = require(`ansi-html`)
 const {
+  CircularProgressbarWithChildren,
+} = require(`react-circular-progressbar`)
+require("react-circular-progressbar/dist/styles.css")
+const {
   createClient,
   useMutation,
   useSubscription,
@@ -289,18 +293,121 @@ const RecipeGui = ({
         log(`stepResources`, state.context.stepResources)
       }
 
+      const Step = ({ state, step, i }) => {
+        const [output, setOutput] = useState({
+          title: ``,
+          body: ``,
+          date: new Date(),
+        })
+
+        const [complete, setComplete] = useState(false)
+        if (output.title !== `` && output.body !== ``) {
+          setTimeout(() => {
+            setComplete(true)
+          }, 0)
+        } else {
+          setTimeout(() => {
+            setComplete(false)
+          }, 0)
+        }
+
+        return (
+          <div
+            key={`step-${i}`}
+            sx={{
+              border: `1px solid tomato`,
+              marginBottom: 4,
+              borderRadius: 20,
+            }}
+          >
+            <div
+              sx={{
+                display: `flex`,
+                // justifyContent: `space-between`,
+                "& > *": {
+                  marginY: 0,
+                },
+                background: `PaleGoldenRod`,
+                borderTopLeftRadius: 20,
+                borderTopRightRadius: 20,
+                padding: 2,
+              }}
+            >
+              <div
+                sx={{
+                  "& > *": {
+                    marginY: 0,
+                  },
+                  display: `flex`,
+                  alignItems: `flex-start`,
+                }}
+              >
+                <div>
+                  <CircularProgressbarWithChildren
+                    value={66}
+                    sx={{ height: `34px`, width: `34px` }}
+                  >
+                    {/* Put any JSX content in here that you'd like. It'll be vertically and horizonally centered. */}
+
+                    <div style={{ fontSize: `18px` }}>
+                      <strong>5/7</strong>
+                    </div>
+                  </CircularProgressbarWithChildren>
+                </div>
+              </div>
+              <div
+                sx={{
+                  // marginTop: 2,
+                  "& > *": {
+                    marginTop: 0,
+                  },
+                }}
+              >
+                <MDX components={components}>{step}</MDX>
+              </div>
+            </div>
+            <div sx={{ padding: 3 }}>
+              <Div>
+                <div>
+                  <label>title</label>
+                </div>
+                <div>
+                  <input
+                    type="text"
+                    onChange={e => {
+                      const newOutput = { ...output, title: e.target.value }
+                      setOutput(newOutput)
+                    }}
+                  />
+                </div>
+                <div>
+                  <label>body</label>
+                </div>
+                <div>
+                  <textarea
+                    onChange={e => {
+                      const newOutput = { ...output, body: e.target.value }
+                      setOutput(newOutput)
+                    }}
+                  />
+                </div>
+                <h4>Proposed changes</h4>
+                <pre>{JSON.stringify(output, null, 2)}</pre>
+              </Div>
+            </div>
+          </div>
+        )
+      }
+
       const PresentStep = ({ step }) => {
         // const isPlan = state.context.plan && state.context.plan.length > 0
         // const isPresetPlanState = state.value === `presentPlan`
         // const isRunningStep = state.value === `applyingPlan`
-
         // console.log(`PresentStep`, { isRunningStep, isPlan, isPresetPlanState })
-
         // if (isRunningStep) {
         // console.log("running step")
         // return null
         // }
-
         // if (!isPlan || !isPresetPlanState) {
         // return (
         // <Div margin-top={1}>
@@ -339,12 +446,6 @@ const RecipeGui = ({
         // Go!
         // </button>
         // </Div>
-
-        return (
-          <Div>
-            <Text margin-bottom={2}>Proposed changes</Text>
-          </Div>
-        )
       }
 
       const RunningStep = ({ state }) => {
@@ -421,9 +522,14 @@ const RecipeGui = ({
 
       return (
         <Wrapper>
-          <Div></Div>
           {state.context.currentStep === 0 && <WelcomeMessage />}
           <br />
+          <div sx={{ width: `100%`, padding: 3, background: `PaleGoldenRod` }}>
+            recipe status
+          </div>
+          <div>
+            <MDX components={components}>{state.context.steps[0]}</MDX>
+          </div>
           <h2>Proposed changes</h2>
           <button>Apply changes</button>
           <div>count {state.context.plan?.length}</div>
@@ -435,29 +541,8 @@ const RecipeGui = ({
             ))}
           </div>
 
-          {state.context.steps.map((step, i) => {
-            return (
-              <div>
-                <h4 sx={{ marginBottom: 3, marginTop: 0 }}>
-                  {i + 1}/{state.context.steps.length}
-                </h4>
-                <div
-                  key={`step-${i}`}
-                  sx={{
-                    border: `1px solid`,
-                    marginBottom: 4,
-                    padding: 3,
-                    borderRadius: 20,
-                    "& > h1": {
-                      marginTop: 0,
-                    },
-                  }}
-                >
-                  <MDX components={components}>{step}</MDX>
-                  <PresentStep step={step} />
-                </div>
-              </div>
-            )
+          {state.context.steps.slice(1).map((step, i) => {
+            return <Step state={state} step={step} i={i} />
           })}
         </Wrapper>
       )
