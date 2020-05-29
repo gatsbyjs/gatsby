8d49b109a1355d302b16172106178123b20100da
Nicholas Duffy <duffn@outlook.com>
2020-05-29 06:53:31 -0600

    Prefix new benchmark environment variables (#24611)

--- benchmarks/source-sanity/update-post.js
+++ benchmarks/source-sanity/update-post.js
@@ -2,21 +2,23 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
-const client = require('@sanity/client')({
-  projectId: process.env.GATSBY_SANITY_PROJECT_ID,
-  dataset: process.env.GATSBY_SANITY_DATASET,
-  token: process.env.GATSBY_SANITY_WRITE_TOKEN,
-  useCdn: false
+const client = require("@sanity/client")({
+  projectId: process.env.BENCHMARK_SANITY_PROJECT_ID,
+  dataset: process.env.BENCHMARK_SANITY_DATASET,
+  token: process.env.BENCHMARK_SANITY_WRITE_TOKEN,
+  useCdn: false,
 })
 
-const randomDoc = Math.floor(Math.random() * (Number(process.env.GATSBY_SANITY_DATASET) || 512))
+const randomDoc = Math.floor(
+  Math.random() * (Number(process.env.BENCHMARK_SANITY_DATASET) || 512)
+)
 
 console.log(`Getting article number ${randomDoc}`)
 
 client
-  .fetch(`*[$randomDoc]`, {randomDoc})
-  .then(({_id, title}) => {
-    client.patch(_id).set({title: title + '!'})
+  .fetch(`*[$randomDoc]`, { randomDoc })
+  .then(({ _id, title }) => {
+    client.patch(_id).set({ title: title + "!" })
   })
   .then(result => console.log(`The "${result.title}" was updated!`))
   .catch(error => console.error(error))
