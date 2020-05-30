99ca51acc6fae50fd04308f35a27d56d0f1c6283
Vince P <ianparulan@gmail.com>
2020-05-30 00:25:37 -0400

    feature: expose gatsby-image apis from contentful
    Add resolveFluid, resolveFixed and resolveResize to the named exports.

--- packages/gatsby-source-contentful/index.js
+++ packages/gatsby-source-contentful/index.js
@@ -1,1 +1,9 @@
-module.exports.schemes = require(`./schemes.js`)
+const schemes = require(`./schemes.js`)
+const { resolveFluid, resolveFixed, resolveResize } = require('./extend-node-type.js')
+
+module.exports = {
+  schemes,
+  resolveFluid,
+  resolveFixed,
+  resolveResize
+}
