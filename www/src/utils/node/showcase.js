6c7e07dac527eb9fdd7ba491b801306c1b28a4fa
Blaine Kasten <blainekasten@gmail.com>
2020-06-02 11:31:16 -0500

    rm

--- www/src/utils/node/showcase.js
+++ www/src/utils/node/showcase.js
@@ -1,42 +1,42 @@
 const slugify = require(`slugify`)
 const url = require(`url`)
-const { getTemplate } = require(`../get-template`)
+// const { getTemplate } = require(`../get-template`)
 
-exports.createPages = async ({ graphql, actions, reporter }) => {
-  // const { createPage } = actions
-  // const showcaseTemplate = getTemplate(`template-showcase-details`)
-  // const { data, errors } = await graphql(`
-  //   query {
-  //     allSitesYaml(filter: { main_url: { ne: null } }) {
-  //       nodes {
-  //         main_url
-  //         fields {
-  //           slug
-  //           hasScreenshot
-  //         }
-  //       }
-  //     }
-  //   }
-  // `)
-  // if (errors) throw errors
-  // data.allSitesYaml.nodes.forEach(node => {
-  //   if (!node.fields) return
-  //   if (!node.fields.slug) return
-  //   if (!node.fields.hasScreenshot) {
-  //     reporter.warn(
-  //       `Site showcase entry "${node.main_url}" seems offline. Skipping.`
-  //     )
-  //     return
-  //   }
-  //   createPage({
-  //     path: `${node.fields.slug}`,
-  //     component: showcaseTemplate,
-  //     context: {
-  //       slug: node.fields.slug,
-  //     },
-  //   })
-  // })
-}
+// exports.createPages = async ({ graphql, actions, reporter }) => {
+// const { createPage } = actions
+// const showcaseTemplate = getTemplate(`template-showcase-details`)
+// const { data, errors } = await graphql(`
+//   query {
+//     allSitesYaml(filter: { main_url: { ne: null } }) {
+//       nodes {
+//         main_url
+//         fields {
+//           slug
+//           hasScreenshot
+//         }
+//       }
+//     }
+//   }
+// `)
+// if (errors) throw errors
+// data.allSitesYaml.nodes.forEach(node => {
+//   if (!node.fields) return
+//   if (!node.fields.slug) return
+//   if (!node.fields.hasScreenshot) {
+//     reporter.warn(
+//       `Site showcase entry "${node.main_url}" seems offline. Skipping.`
+//     )
+//     return
+//   }
+//   createPage({
+//     path: `${node.fields.slug}`,
+//     component: showcaseTemplate,
+//     context: {
+//       slug: node.fields.slug,
+//     },
+//   })
+// })
+// }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
