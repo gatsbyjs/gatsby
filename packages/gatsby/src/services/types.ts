66e571786f9a330c32340d80d856e13e80820b86
Matt Kane <matt@gatsbyjs.com>
2020-06-03 10:13:35 +0100

    Remove unused props from context type

--- packages/gatsby/src/services/types.ts
+++ packages/gatsby/src/services/types.ts
@@ -1,9 +1,4 @@
-import { WebsocketManager } from "../utils/websocket-manager"
-import { AnyEventObject, Actor } from "xstate"
-import { Compiler } from "webpack"
 import { Span } from "opentracing"
-import { Express } from "express"
-import JestWorker from "jest-worker"
 import { IProgram } from "../commands/types"
 import { Store } from "../.."
 import { GraphQLRunner } from "../query/graphql-runner"
@@ -19,23 +14,8 @@ export interface IMutationAction {
 }
 export interface IBuildContext {
   program?: IProgram
-  app?: Express
-  recursionCount: number
-  nodesMutatedDuringQueryRun: boolean
-  firstRun: boolean
-  nodeMutationBatch: IMutationAction[]
-  filesDirty?: boolean
-  runningBatch: IMutationAction[]
-  compiler?: Compiler
-  websocketManager?: WebsocketManager
   store?: Store
   parentSpan?: Span
   graphqlRunner?: GraphQLRunner
-  refresh?: boolean
-  webhookBody?: Record<string, unknown>
   queryIds?: IGroupedQueryIds
-  workerPool?: JestWorker
-  pagesToBuild?: string[]
-  pagesToDelete?: string[]
-  mutationListener?: Actor<any, AnyEventObject>
 }
