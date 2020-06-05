e33b66dde8186bd66c585351571c35d8c091515f
Blaine Kasten <blainekasten@gmail.com>
2020-06-05 08:18:57 -0500

    rm

--- packages/gatsby-plugin-page-creator/src/get-match-path.ts
+++ packages/gatsby-plugin-page-creator/src/get-match-path.ts
@@ -2,13 +2,13 @@ interface IOptionalMatchPath {
   matchPath?: string
 }
 
+// Does the following transformations:
+//   `/foo/[id]/` => `/foo/:id`
+//   `/foo/[...id]/` => `/foo/*id`
+//   `/foo/[...]/` => `/foo/*`
 export function getMatchPath(srcPagesPath: string): IOptionalMatchPath {
   if (srcPagesPath.includes(`[`) === false) return {}
 
-  // Does the following transformations:
-  //   `/foo/[id]/` => `/foo/:id`
-  //   `/foo/[...id]/` => `/foo/*id`
-  //   `/foo/[...]/` => `/foo/*`
   return {
     matchPath: srcPagesPath
       .replace(`[...`, `*`)
