9c2dbb902d89cfa67cab1b32d9c54baae9ec36b0
abhishekjakhar <abhishekjakhar.11@gmail.com>
2020-06-06 21:15:00 +0530

    convert collapsible to function component

--- www/src/views/shared/collapsible.js
+++ www/src/views/shared/collapsible.js
@@ -1,83 +1,72 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui"
-import { Component } from "react"
+import { useState } from "react"
 import { FaAngleDown, FaAngleUp } from "react-icons/fa"
 
-class Collapsible extends Component {
-  state = {
-    collapsed: false,
-  }
+const Collapsible = props => {
+  const { heading, fixed, children } = props
+  const [collapsed, setCollapsed] = useState(false)
 
-  handleClick = () => {
-    this.setState({ collapsed: !this.state.collapsed })
-  }
-
-  render() {
-    const { heading, fixed, children } = this.props
-    const { collapsed } = this.state
-
-    return (
+  return (
+    <div
+      sx={{
+        borderBottom: t => (collapsed ? 0 : `1px solid ${t.colors.ui.border}`),
+        display: collapsed ? false : `flex`,
+        flex: collapsed ? `0 0 auto` : `1 1 auto`,
+        minHeight: fixed ? `${fixed}px` : `initial`,
+        maxHeight: fixed ? `${fixed}px` : `initial`,
+        flexBasis: 0,
+        overflowY: collapsed ? false : `auto`,
+      }}
+    >
       <div
-        sx={{
-          borderBottom: t =>
-            collapsed ? 0 : `1px solid ${t.colors.ui.border}`,
-          display: collapsed ? false : `flex`,
-          flex: collapsed ? `0 0 auto` : `1 1 auto`,
-          minHeight: fixed ? `${fixed}px` : `initial`,
-          maxHeight: fixed ? `${fixed}px` : `initial`,
-          flexBasis: 0,
-          overflowY: collapsed ? false : `auto`,
+        css={{
+          display: `flex`,
+          flexDirection: `column`,
+          minHeight: `100%`,
+          width: `100%`,
         }}
       >
+        <button
+          sx={{
+            alignItems: `center`,
+            color: `textMuted`,
+            cursor: `pointer`,
+            display: `flex`,
+            flexShrink: 0,
+            fontWeight: `body`,
+            fontSize: 1,
+            my: 6,
+            mr: 4,
+            p: 0,
+            letterSpacing: `tracked`,
+            textTransform: `uppercase`,
+            background: `none`,
+            border: `none`,
+            "&:hover": {
+              color: `gatsby`,
+            },
+          }}
+          aria-expanded={!collapsed}
+          onClick={() => setCollapsed(!collapsed)}
+        >
+          {heading}
+          {` `}
+          <span sx={{ ml: `auto` }} css={{ display: `flex` }}>
+            {collapsed ? <FaAngleDown /> : <FaAngleUp />}
+          </span>
+        </button>
         <div
           css={{
-            display: `flex`,
-            flexDirection: `column`,
-            minHeight: `100%`,
-            width: `100%`,
+            display: collapsed ? `none` : `block`,
+            overflowY: `auto`,
           }}
         >
-          <button
-            sx={{
-              alignItems: `center`,
-              color: `textMuted`,
-              cursor: `pointer`,
-              display: `flex`,
-              flexShrink: 0,
-              fontWeight: `body`,
-              fontSize: 1,
-              my: 6,
-              mr: 4,
-              p: 0,
-              letterSpacing: `tracked`,
-              textTransform: `uppercase`,
-              background: `none`,
-              border: `none`,
-              "&:hover": {
-                color: `gatsby`,
-              },
-            }}
-            aria-expanded={!collapsed}
-            onClick={this.handleClick}
-          >
-            {heading}
-            {` `}
-            <span sx={{ ml: `auto` }} css={{ display: `flex` }}>
-              {collapsed ? <FaAngleDown /> : <FaAngleUp />}
-            </span>
-          </button>
-          <div
-            css={{
-              display: collapsed ? `none` : `block`,
-              overflowY: `auto`,
-            }}
-          >
-            {children}
-          </div>
+          {children}
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Collapsible
