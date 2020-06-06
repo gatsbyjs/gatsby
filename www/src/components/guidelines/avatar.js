f118baba965f3f38a5328001ac0c808b08a4e876
abhishekjakhar <abhishekjakhar.11@gmail.com>
2020-06-07 01:35:46 +0530

    convert avatar to theme-ui

--- www/src/components/guidelines/avatar.js
+++ www/src/components/guidelines/avatar.js
@@ -1,16 +1,18 @@
-import styled from "@emotion/styled"
+/** @jsx jsx */
+import { jsx } from "theme-ui"
 
-import { Box } from "./system"
-
-const Avatar = styled(Box)()
-
-Avatar.defaultProps = {
-  bg: `grey.10`,
-  borderRadius: 6,
-  flex: `0 0 auto`,
-  height: `avatar`,
-  lineHeight: `solid`,
-  width: `avatar`,
-}
+const Avatar = ({ ...rest }) => (
+  <div
+    sx={{
+      bg: `grey.10`,
+      borderRadius: 6,
+      flex: `0 0 auto`,
+      height: `avatar`,
+      lineHeight: `solid`,
+      width: `avatar`,
+      ...rest,
+    }}
+  ></div>
+)
 
 export default Avatar
