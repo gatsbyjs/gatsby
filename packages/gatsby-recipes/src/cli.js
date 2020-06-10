99da774293155014e69ad28409e1a5e38fde5ee6
Pedro Filipe <35262512+puzzledbytheweb@users.noreply.github.com>
2020-06-10 21:52:42 +0100

    feat(recipes): add gitlab-ci-cd recipe to cli list (#24501)
    * feat(recipes): add gitlab-ci-cd recipe to cli list
    
    * Update packages/gatsby-recipes/src/cli.js
    
    Co-authored-by: Muescha <184316+muescha@users.noreply.github.com>
    
    Co-authored-by: Muescha <184316+muescha@users.noreply.github.com>
    Co-authored-by: gatsbybot <mathews.kyle+gatsbybot@gmail.com>

--- packages/gatsby-recipes/src/cli.js
+++ packages/gatsby-recipes/src/cli.js
@@ -159,6 +159,10 @@ const RecipesList = ({ setRecipe }) => {
       label: `Add Preact`,
       value: `preact.mdx`,
     },
+    {
+      label: `Add GitLab CI/CD`,
+      value: `gitlab-ci-cd.mdx`,
+    },
   ]
 
   return (
