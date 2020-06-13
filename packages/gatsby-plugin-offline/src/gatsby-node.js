428a003509d22cbdec8567efb758a5b5b52dbdab
nibtime <kai@friedrich.services>
2020-06-13 19:50:50 +0000

    style: fix linting errors

--- packages/gatsby-plugin-offline/src/gatsby-node.js
+++ packages/gatsby-plugin-offline/src/gatsby-node.js
@@ -162,7 +162,9 @@ exports.onPostBuild = (
 
   const idbKeyvalFile = `idb-keyval-iife.min.js`
   const idbKeyvalSource = require.resolve(`idb-keyval/dist/${idbKeyvalFile}`)
-  const idbKeyValVersioned = `idb-keyval-${require("idb-keyval/package.json").version}-iife.min.js`
+  const idbKeyValVersioned = `idb-keyval-${
+    require(`idb-keyval/package.json`).version
+  }-iife.min.js`
   const idbKeyvalDest = `public/${idbKeyValVersioned}`
   fs.createReadStream(idbKeyvalSource).pipe(fs.createWriteStream(idbKeyvalDest))
 
@@ -181,16 +183,19 @@ exports.onPostBuild = (
           )
         fs.writeFileSync(swDest, swText)
       }
-      
-      const prependIdbKeyValVersionedImport = [
-        `/* global importScripts, workbox, idbKeyval */`,
-        `importScripts(\`${idbKeyValVersioned}\`)`,
-      ].join(`\n\n`) + `\n\n`
 
-      const swAppend = prependIdbKeyValVersionedImport + fs
-        .readFileSync(`${__dirname}/sw-append.js`, `utf8`)
-        .replace(/%pathPrefix%/g, pathPrefix)
-        .replace(/%appFile%/g, appFile)
+      const prependIdbKeyValVersionedImport =
+        [
+          `/* global importScripts, workbox, idbKeyval */`,
+          `importScripts(\`${idbKeyValVersioned}\`)`,
+        ].join(`\n\n`) + `\n\n`
+
+      const swAppend =
+        prependIdbKeyValVersionedImport +
+        fs
+          .readFileSync(`${__dirname}/sw-append.js`, `utf8`)
+          .replace(/%pathPrefix%/g, pathPrefix)
+          .replace(/%appFile%/g, appFile)
 
       fs.appendFileSync(`public/sw.js`, `\n` + swAppend)
 
