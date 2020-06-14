2388195f015bcb1b6750beb5eb9f331db3baf9ed
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2020-06-13 21:23:03 -0700

    add remaining words/names to the dictionary

--- scripts/update-dictionary.js
+++ scripts/update-dictionary.js
@@ -1,23 +1,22 @@
+const { execSync } = require(`child_process`)
 const fs = require(`fs`)
 
-const words = fs.readFileSync(`./dictionary.txt`, `utf-8`).split(`\n`)
+const oldWords = fs.readFileSync(`./dictionary.txt`, `utf-8`).split(`\n`)
+
+let errors
+try {
+  execSync(`yarn lint:docs`, { encoding: `utf-8` })
+} catch (e) {
+  errors = e.stderr
+}
+const matches = errors.matchAll(/`(.*)` is misspelt/g)
+
+const newWords = new Set()
+for (const match of matches) {
+  newWords.add(match[1])
+}
+
+const words = [...oldWords, ...newWords]
 words.sort((a, b) => a.localeCompare(b))
-// for (const word of words) {
-//   console.log(word)
-// }
+
 fs.writeFileSync(`./dictionary.txt`, words.join(`\n`))
-// const errors = fs.readFileSync(`./errors.txt`, `utf-8`)
-// const matches = errors.matchAll(/`(.*)` is misspelt/g)
-
-// const words = new Set()
-
-// for (const match of matches) {
-//   words.add(match[1])
-// }
-
-// const wordList = [...words]
-// wordList.sort()
-
-// for (const word of wordList) {
-//   console.log(word)
-// }
