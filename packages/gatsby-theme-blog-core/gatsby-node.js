9720e82acc0afc462bd93f25f657d49b37f2da10
Blaine Kasten <blainekasten@gmail.com>
2020-06-26 08:50:37 -0500

    revert more

--- packages/gatsby-theme-blog-core/gatsby-node.js
+++ packages/gatsby-theme-blog-core/gatsby-node.js
@@ -252,49 +252,49 @@ exports.onCreateNode = async (
 }
 
 // These templates are simply data-fetching wrappers that import components
-// const PostTemplate = require.resolve(`./src/templates/post-query`)
+const PostTemplate = require.resolve(`./src/templates/post-query`)
 const PostsTemplate = require.resolve(`./src/templates/posts-query`)
 
 exports.createPages = async ({ graphql, actions, reporter }, themeOptions) => {
   const { createPage } = actions
   const { basePath } = withDefaults(themeOptions)
 
-  // const result = await graphql(`
-  //   {
-  //     allBlogPost(sort: { fields: [date, title], order: DESC }, limit: 1000) {
-  //       edges {
-  //         node {
-  //           id
-  //           slug
-  //         }
-  //       }
-  //     }
-  //   }
-  // `)
+  const result = await graphql(`
+    {
+      allBlogPost(sort: { fields: [date, title], order: DESC }, limit: 1000) {
+        edges {
+          node {
+            id
+            slug
+          }
+        }
+      }
+    }
+  `)
 
-  // if (result.errors) {
-  //   reporter.panic(result.errors)
-  // }
+  if (result.errors) {
+    reporter.panic(result.errors)
+  }
 
-  // // Create Posts and Post pages.
-  // const { allBlogPost } = result.data
-  // const posts = allBlogPost.edges
+  // Create Posts and Post pages.
+  const { allBlogPost } = result.data
+  const posts = allBlogPost.edges
 
-  // // Create a page for each Post
-  // posts.forEach(({ node: post }, index) => {
-  //   const previous = index === posts.length - 1 ? null : posts[index + 1]
-  //   const next = index === 0 ? null : posts[index - 1]
-  //   const { slug } = post
-  //   createPage({
-  //     path: slug,
-  //     component: PostTemplate,
-  //     context: {
-  //       id: post.id,
-  //       previousId: previous ? previous.node.id : undefined,
-  //       nextId: next ? next.node.id : undefined,
-  //     },
-  //   })
-  // })
+  // Create a page for each Post
+  posts.forEach(({ node: post }, index) => {
+    const previous = index === posts.length - 1 ? null : posts[index + 1]
+    const next = index === 0 ? null : posts[index - 1]
+    const { slug } = post
+    createPage({
+      path: slug,
+      component: PostTemplate,
+      context: {
+        id: post.id,
+        previousId: previous ? previous.node.id : undefined,
+        nextId: next ? next.node.id : undefined,
+      },
+    })
+  })
 
   // // Create the Posts page
   createPage({
