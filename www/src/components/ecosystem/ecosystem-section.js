0488b0ae88f1967468bba7c92baab0c79e8a88d8
Florian Kissling <florian.kissling@gmail.com>
2020-06-27 11:32:10 +0200

    🙃

--- www/src/components/ecosystem/ecosystem-section.js
+++ www/src/components/ecosystem/ecosystem-section.js
@@ -8,118 +8,87 @@ import EcosystemFeaturedItems from "./ecosystem-featured-items"
 import EcosystemFeaturedItem from "./ecosystem-featured-item"
 import { mediaQueries } from "gatsby-design-tokens/dist/theme-gatsbyjs-org"
 
-const EcosystemSectionRoot = props => (
-  <section
-    {...props}
-    sx={{
-      bg: `card.background`,
-      px: 6,
-      mb: 3,
-      [mediaQueries.md]: {
-        boxShadow: `raised`,
-        borderRadius: 2,
-        display: `flex`,
-        flexBasis: t => `calc(50% - ${t.space[5]})`,
-        flexDirection: `column`,
-        flexGrow: 0,
-        mx: 2,
-        mb: 6,
-        maxHeight: `60vh`,
-        p: 6,
-        pb: 0,
-        ":last-child": {
-          flexGrow: 1,
-        },
-      },
-      [mediaQueries.lg]: {
-        flexBasis: t => `calc(33.33% - ${t.space[5]})`,
-        maxHeight: `100%`,
-        ":last-child": {
-          alignSelf: `flex-start`,
-          pb: 6,
-        },
-      },
-      a: {
-        textDecoration: `none`,
-      },
-    }}
-  />
-)
+const ecosystemSectionRootStyles = {
+  bg: `card.background`,
+  px: 6,
+  mb: 3,
+  [mediaQueries.md]: {
+    boxShadow: `raised`,
+    borderRadius: 2,
+    display: `flex`,
+    flexBasis: t => `calc(50% - ${t.space[5]})`,
+    flexDirection: `column`,
+    flexGrow: 0,
+    mx: 2,
+    mb: 6,
+    maxHeight: `60vh`,
+    p: 6,
+    pb: 0,
+    ":last-child": {
+      flexGrow: 1,
+    },
+  },
+  [mediaQueries.lg]: {
+    flexBasis: t => `calc(33.33% - ${t.space[5]})`,
+    maxHeight: `100%`,
+    ":last-child": {
+      alignSelf: `flex-start`,
+      pb: 6,
+    },
+  },
+  a: {
+    textDecoration: `none`,
+  },
+}
 
 export const Header = props => (
   <header {...props} sx={{ alignItems: `flex-start` }} />
 )
 
-const Title = props => (
-  <Heading
-    {...props}
-    as="h1"
-    sx={{
-      alignItems: `center`,
-      color: `heading`,
-      display: `flex`,
-      fontSize: 4,
-      fontWeight: `heading`,
-      lineHeight: `solid`,
-      m: 0,
-      mb: 1,
-      minHeight: t => t.space[7],
-      span: {
-        mr: 1,
-      },
-    }}
-  />
-)
+const titleStyles = {
+  alignItems: `center`,
+  color: `heading`,
+  display: `flex`,
+  fontSize: 4,
+  fontWeight: `heading`,
+  lineHeight: `solid`,
+  m: 0,
+  mb: 1,
+  minHeight: t => t.space[7],
+  span: {
+    mr: 1,
+  },
+}
 
-const Icon = props => (
-  <span
-    {...props}
-    sx={{
-      display: `block`,
-      height: t => t.space[7],
-      width: t => t.space[7],
-    }}
-  />
-)
+const iconStyles = {
+  display: `block`,
+  height: t => t.space[7],
+  width: t => t.space[7],
+}
 
-const SubTitle = props => (
-  <Heading
-    {...props}
-    sx={{
-      color: `lilac`,
-      fontSize: 1,
-      fontWeight: `normal`,
-      letterSpacing: `tracked`,
-      m: 0,
-      mt: 5,
-      textTransform: `uppercase`,
-    }}
-  />
-)
+const subtitleStyles = {
+  color: `lilac`,
+  fontSize: 1,
+  fontWeight: `normal`,
+  letterSpacing: `tracked`,
+  m: 0,
+  mt: 5,
+  textTransform: `uppercase`,
+}
 
-const Description = props => (
-  <p
-    {...props}
-    sx={{
-      color: `text`,
-      fontSize: 2,
-    }}
-  />
-)
+const descriptionStyles = {
+  color: `text`,
+  fontSize: 2,
+}
 
-const Actions = props => (
-  <Flex
-    {...props}
-    sx={{
-      flexWrap: `wrap`,
-      mt: -1,
-      "> a": {
-        my: 1,
-        mr: 2,
-      },
-    }}
-  />
-)
+const actionsStyles = {
+  flexWrap: `wrap`,
+  mt: -1,
+  "> a": {
+    my: 1,
+    mr: 2,
+  },
+}
 
 const EcosystemSection = ({
   title,
@@ -130,14 +99,14 @@ const EcosystemSection = ({
   featuredItems,
   className,
 }) => (
-  <EcosystemSectionRoot className={className}>
+  <section sx={ecosystemSectionRootStyles} className={className}>
     <Header>
-      <Title>
-        {icon && <Icon>{icon}</Icon>}
+      <h1 sx={titleStyles}>
+        {icon && <span sx={iconStyles}>{icon}</span>}
         <span>{title}</span>
-      </Title>
-      <Description>{description}</Description>
-      <Actions>
+      </h1>
+      <p sx={descriptionStyles}>{description}</p>
+      <Flex sx={actionsStyles}>
         {links.map(item => {
           const { to, label, secondary } = item
 
@@ -147,8 +116,8 @@ const EcosystemSection = ({
             </Button>
           )
         })}
-      </Actions>
-      {subTitle && <SubTitle>{subTitle}</SubTitle>}
+      </Flex>
+      {subTitle && <h2 sx={subtitleStyles}>{subTitle}</h2>}
     </Header>
 
     {featuredItems && featuredItems.length > 0 && (
@@ -157,7 +126,7 @@ const EcosystemSection = ({
         itemComponent={EcosystemFeaturedItem}
       />
     )}
-  </EcosystemSectionRoot>
+  </section>
 )
 
 EcosystemSection.propTypes = {
