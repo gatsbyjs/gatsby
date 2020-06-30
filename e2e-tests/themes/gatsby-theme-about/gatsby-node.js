00f799a41bea0876d5ced43ee49ce3927d2a973c
Laurie Barth <laurie@lauriesrklaptop.lan>
2020-06-30 12:46:39 -0400

    make the test work through createPages API

---
+++ e2e-tests/themes/gatsby-theme-about/gatsby-node.js
@@ -0,0 +1,6 @@
+const bioTemplate = require.resolve(`./src/templates/bio.js`)
+
+exports.createPages = async ({ actions }) => {
+    const { createPage } = actions
+    createPage({ path: `/bio`, component: bioTemplate,})
+}
