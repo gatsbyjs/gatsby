ed9771b612217bebaca8f9958676ea4c220e02d7
Matt Kane <matt@gatsbyjs.com>
2020-07-01 15:30:32 +0100

    feat(gatsby): Use state machine for bootstrap in develop (#25305)
    * Move bootstrap into machine
    
    * Add parent span and query extraction
    
    * Add rebuildSchemaWithSitePage
    
    * Use values from context
    
    * Remove logs
    
    * Add redirectListener
    
    * Changes from review
    
    * Changes from review
    
    * Changes from review
    
    * Use assertStore

--- packages/gatsby/src/services/create-pages.ts
+++ packages/gatsby/src/services/create-pages.ts
@@ -1,14 +1,16 @@
-import { IBuildContext } from "./"
-
 import reporter from "gatsby-cli/lib/reporter"
 import apiRunnerNode from "../utils/api-runner-node"
+import { IDataLayerContext } from "../state-machines/data-layer/types"
 import { assertStore } from "../utils/assert-store"
 
 export async function createPages({
   parentSpan,
   gatsbyNodeGraphQLFunction,
   store,
-}: Partial<IBuildContext>): Promise<void> {
+}: Partial<IDataLayerContext>): Promise<{
+  deletedPages: string[]
+  changedPages: string[]
+}> {
   assertStore(store)
   const activity = reporter.activityTimer(`createPages`, {
     parentSpan,
@@ -59,8 +61,8 @@ export async function createPages({
   // )
   // tim.end()
 
-  // return {
-  //   changedPages,
-  //   deletedPages,
-  // }
+  return {
+    changedPages: [],
+    deletedPages: [],
+  }
 }
