2808943badce1de4f9842a42a344be9c0267e09b
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2020-07-03 01:54:54 -0700

    fix this

--- www/src/views/showcase/featured-sites.js
+++ www/src/views/showcase/featured-sites.js
@@ -220,7 +220,7 @@ export default function FeaturedSites({ setFilters, featured }) {
                 },
                 ...featuredSitesCard,
               }}
-              onClick={this.setFilterToFeatured}
+              onClick={setFilterToFeatured}
             >
               <div
                 sx={{
