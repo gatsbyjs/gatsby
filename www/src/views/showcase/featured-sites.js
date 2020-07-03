25aa5b11b8038602c4bd0d467fc1987b0c692511
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2020-07-03 01:17:47 -0700

    Convert featured-sites to function component and simplify some of the styling`

--- www/src/views/showcase/featured-sites.js
+++ www/src/views/showcase/featured-sites.js
@@ -8,10 +8,7 @@ import { useColorMode } from "theme-ui"
 import { screenshot, screenshotHover, withTitleHover } from "../shared/styles"
 import ShowcaseItemCategories from "./showcase-item-categories"
 import { ShowcaseIcon } from "../../assets/icons"
-import {
-  mediaQueries,
-  colors,
-} from "gatsby-design-tokens/dist/theme-gatsbyjs-org"
+import { colors } from "gatsby-design-tokens/dist/theme-gatsbyjs-org"
 import { svgStyles } from "../../utils/styles"
 import Button from "../../components/button"
 import { MdArrowForward as ArrowForwardIcon } from "react-icons/md"
@@ -22,16 +19,9 @@ const featuredSitesCard = {
   flexDirection: `column`,
   flexGrow: 0,
   flexShrink: 0,
-  width: 320,
+  width: [320, null, null, null, 360, 400],
   marginBottom: 9,
-  marginRight: 6,
-  [mediaQueries.xl]: {
-    width: 360,
-    marginRight: 8,
-  },
-  [mediaQueries.xxl]: {
-    width: 400,
-  },
+  marginRight: [6, null, null, null, 8],
 }
 
 const GradientOverlay = () => {
@@ -58,244 +48,219 @@ const GradientOverlay = () => {
   )
 }
 
-class FeaturedSites extends Component {
-  setFilterToFeatured = e => {
+export default function FeaturedSites({ setfilters, featured }) {
+  function setFilterToFeatured(e) {
     e.preventDefault()
-
-    this.props.setFilters(`Featured`)
+    setFilters(`Featured`)
   }
 
-  render() {
-    const { featured, setFilters } = this.props
-
-    return (
-      <section
-        className="featured-sites"
+  return (
+    <section
+      className="featured-sites"
+      sx={{
+        mt: 6,
+        mx: 6,
+        position: `relative`,
+        display: [`none`, null, null, `block`],
+      }}
+    >
+      <div
         sx={{
-          mt: 6,
-          mx: 6,
-          position: `relative`,
-          display: `none`,
-          [mediaQueries.lg]: {
-            display: `block`,
-          },
+          display: `flex`,
+          alignItems: `center`,
+          flexWrap: `wrap`,
         }}
       >
-        <div
+        <h1
           sx={{
-            display: `flex`,
-            alignItems: `center`,
-            flexWrap: `wrap`,
+            fontFamily: `heading`,
+            fontSize: 4,
+            fontWeight: `bold`,
+            ml: 1,
+            mr: 30,
+            my: 0,
           }}
         >
-          <h1
-            sx={{
-              fontFamily: `heading`,
-              fontSize: 4,
-              fontWeight: `bold`,
-              ml: 1,
-              mr: 30,
-              my: 0,
-            }}
-          >
-            Featured Sites
-          </h1>
-          <a
-            href="#showcase"
-            sx={{
-              ...withTitleHover,
-              display: `none`,
-              fontSize: 1,
-              [mediaQueries.sm]: {
-                display: `block`,
+          Featured Sites
+        </h1>
+        <a
+          href="#showcase"
+          sx={{
+            ...withTitleHover,
+            display: [`none`, `block`],
+            fontSize: 1,
+            "&&": {
+              borderBottom: 0,
+              cursor: `pointer`,
+              "&:hover": {
+                color: `link.hoverColor`,
               },
-              "&&": {
-                borderBottom: 0,
-                cursor: `pointer`,
-                "&:hover": {
-                  color: `link.hoverColor`,
-                },
-              },
-            }}
-            onClick={this.setFilterToFeatured}
-          >
-            <span className="title">View all</span>
-            {` `}
-            <ArrowForwardIcon sx={{ verticalAlign: `sub` }} />
-          </a>
+            },
+          }}
+          onClick={this.setFilterToFeatured}
+        >
+          <span className="title">View all</span>
+          {` `}
+          <ArrowForwardIcon sx={{ verticalAlign: `sub` }} />
+        </a>
+        <div
+          css={{
+            alignItems: `center`,
+            display: `flex`,
+            marginLeft: `auto`,
+          }}
+        >
           <div
-            css={{
-              alignItems: `center`,
-              display: `flex`,
-              marginLeft: `auto`,
+            sx={{
+              color: `textMuted`,
+              display: [`none`, null, `block`],
+              fontSize: 1,
+              mr: 4,
             }}
           >
+            Want to get featured?
+          </div>
+          <Button
+            to="https://www.gatsbyjs.org/contributing/site-showcase-submissions/"
+            tag="href"
+            target="_blank"
+            rel="noopener noreferrer"
+            variant="small"
+            icon={<ArrowForwardIcon />}
+          >
+            Submit your Site
+          </Button>
+        </div>
+      </div>
+      <div css={{ position: `relative` }}>
+        <div
+          sx={{
+            borderBottom: t => `1px solid ${t.colors.ui.border}`,
+            display: `flex`,
+            flexShrink: 0,
+            mx: -6,
+            overflowX: `scroll`,
+            pt: 6,
+            px: 6,
+          }}
+        >
+          {featured.slice(0, 9).map(node => (
             <div
+              key={node.id}
               sx={{
-                color: `textMuted`,
-                display: `none`,
-                fontSize: 1,
-                mr: 4,
-                [mediaQueries.md]: {
-                  display: `block`,
-                },
+                ...featuredSitesCard,
+                ...withTitleHover,
               }}
             >
-              Want to get featured?
-            </div>
-            <Button
-              to="https://www.gatsbyjs.org/contributing/site-showcase-submissions/"
-              tag="href"
-              target="_blank"
-              rel="noopener noreferrer"
-              variant="small"
-              icon={<ArrowForwardIcon />}
-            >
-              Submit your Site
-            </Button>
-          </div>
-        </div>
-        <div css={{ position: `relative` }}>
-          <div
-            sx={{
-              borderBottom: t => `1px solid ${t.colors.ui.border}`,
-              display: `flex`,
-              flexShrink: 0,
-              mx: -6,
-              overflowX: `scroll`,
-              pt: 6,
-              px: 6,
-            }}
-          >
-            {featured.slice(0, 9).map(node => (
-              <div
-                key={node.id}
+              <Link
                 sx={{
-                  ...featuredSitesCard,
-                  ...withTitleHover,
-                }}
-              >
-                <Link
-                  sx={{
-                    "&&": {
-                      borderBottom: `none`,
-                      color: `heading`,
-                      fontFamily: `heading`,
-                      fontSize: 3,
-                      fontWeight: `bold`,
-                      transition: t =>
-                        `box-shadow ${t.transition.speed.slow} ${t.transition.curve.default}, transform .3s ${t.transition.curve.default}`,
-                      "&:hover": { ...screenshotHover },
-                    },
-                  }}
-                  to={node.fields.slug}
-                  state={{ isModal: true }}
-                >
-                  {node.childScreenshot && (
-                    <Img
-                      fluid={
-                        node.childScreenshot.screenshotFile.childImageSharp
-                          .fluid
-                      }
-                      alt={node.title}
-                      sx={{ ...screenshot }}
-                    />
-                  )}
-                  <div>
-                    <span className="title">{node.title}</span>
-                  </div>
-                </Link>
-                <div
-                  sx={{
-                    color: `textMuted`,
-                    fontSize: 1,
-                    fontWeight: `body`,
-                    [mediaQueries.lg]: {
-                      marginTop: `auto`,
-                    },
-                  }}
-                >
-                  {node.built_by && (
-                    <div
-                      sx={{ color: `text`, fontFamily: `heading`, fontSize: 2 }}
-                    >
-                      Built by {node.built_by}
-                    </div>
-                  )}
-                  <ShowcaseItemCategories
-                    categories={node.categories}
-                    onCategoryClick={c => setFilters(c)}
-                  />
-                </div>
-              </div>
-            ))}
-            <div sx={{ display: `flex` }}>
-              <a
-                href="#showcase"
-                sx={{
-                  backgroundColor: `card.background`,
-                  borderRadius: 1,
-                  marginRight: t => `${t.space[6]} !important`,
-                  textAlign: `center`,
                   "&&": {
-                    border: 0,
-                    transition: `default`,
-                    "&:hover": {
-                      transform: t => `translateY(-${t.space[1]})`,
-                      boxShadow: `overlay`,
-                    },
+                    borderBottom: `none`,
+                    color: `heading`,
+                    fontFamily: `heading`,
+                    fontSize: 3,
+                    fontWeight: `bold`,
+                    transition: t =>
+                      `box-shadow ${t.transition.speed.slow} ${t.transition.curve.default}, transform .3s ${t.transition.curve.default}`,
+                    "&:hover": { ...screenshotHover },
                   },
-                  ...featuredSitesCard,
                 }}
-                onClick={this.setFilterToFeatured}
+                to={node.fields.slug}
+                state={{ isModal: true }}
               >
-                <div
+                {node.childScreenshot && (
+                  <Img
+                    fluid={
+                      node.childScreenshot.screenshotFile.childImageSharp.fluid
+                    }
+                    alt={node.title}
+                    sx={{ ...screenshot }}
+                  />
+                )}
+                <div>
+                  <span className="title">{node.title}</span>
+                </div>
+              </Link>
+              <div
+                sx={{
+                  color: `textMuted`,
+                  fontSize: 1,
+                  fontWeight: `body`,
+                  mt: [null, null, null, `auto`],
+                }}
+              >
+                {node.built_by && (
+                  <div
+                    sx={{ color: `text`, fontFamily: `heading`, fontSize: 2 }}
+                  >
+                    Built by {node.built_by}
+                  </div>
+                )}
+                <ShowcaseItemCategories
+                  categories={node.categories}
+                  onCategoryClick={c => setFilters(c)}
+                />
+              </div>
+            </div>
+          ))}
+          <div sx={{ display: `flex` }}>
+            <a
+              href="#showcase"
+              sx={{
+                backgroundColor: `card.background`,
+                borderRadius: 1,
+                marginRight: t => `${t.space[6]} !important`,
+                textAlign: `center`,
+                "&&": {
+                  border: 0,
+                  transition: `default`,
+                  "&:hover": {
+                    transform: t => `translateY(-${t.space[1]})`,
+                    boxShadow: `overlay`,
+                  },
+                },
+                ...featuredSitesCard,
+              }}
+              onClick={this.setFilterToFeatured}
+            >
+              <div
+                sx={{
+                  alignItems: `center`,
+                  borderRadius: 1,
+                  display: `flex`,
+                  flexBasis: `100%`,
+                  position: `relative`,
+                }}
+              >
+                <span
                   sx={{
-                    alignItems: `center`,
-                    borderRadius: 1,
-                    display: `flex`,
-                    flexBasis: `100%`,
-                    position: `relative`,
+                    color: `gatsby`,
+                    mx: `auto`,
                   }}
                 >
                   <span
                     sx={{
-                      color: `gatsby`,
+                      display: `block`,
+                      height: [44, null, 64, null, 72],
                       mx: `auto`,
+                      mb: 6,
+                      width: `auto`,
+                      "& svg": {
+                        height: `100%`,
+                        ...svgStyles.active,
+                      },
                     }}
                   >
-                    <span
-                      sx={{
-                        display: `block`,
-                        height: 44,
-                        mx: `auto`,
-                        mb: 6,
-                        width: `auto`,
-                        [mediaQueries.md]: {
-                          height: 64,
-                        },
-                        [mediaQueries.xl]: {
-                          height: 72,
-                        },
-                        "& svg": {
-                          height: `100%`,
-                          ...svgStyles.active,
-                        },
-                      }}
-                    >
-                      <ShowcaseIcon />
-                    </span>
-                    View all Featured Sites
+                    <ShowcaseIcon />
                   </span>
-                </div>
-              </a>
-            </div>
+                  View all Featured Sites
+                </span>
+              </div>
+            </a>
           </div>
-          <GradientOverlay />
         </div>
-      </section>
-    )
-  }
+        <GradientOverlay />
+      </div>
+    </section>
+  )
 }
-
-export default FeaturedSites
