c9e2baeab06da62950ccf2dde198648c3de50e3f
abhishekjakhar <abhishekjakhar.11@gmail.com>
2020-07-05 17:01:20 +0530

    export FuturaParagraph as function declaration

--- www/src/components/futura-paragraph.js
+++ www/src/components/futura-paragraph.js
@@ -1,16 +1,16 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui"
 
-const FuturaParagraph = ({ children }) => (
-  <p
-    sx={{
-      fontFamily: `heading`,
-      fontSize: 3,
-      mb: 0,
-    }}
-  >
-    {children}
-  </p>
-)
-
-export default FuturaParagraph
+export default function FuturaParagraph({ children }) {
+  return (
+    <p
+      sx={{
+        fontFamily: `heading`,
+        fontSize: 3,
+        mb: 0,
+      }}
+    >
+      {children}
+    </p>
+  )
+}
