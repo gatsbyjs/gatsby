e9faa1ee6dc7f3bded174b991e9d320e685f68bf
abhishekjakhar <abhishekjakhar.11@gmail.com>
2020-07-05 16:59:13 +0530

    tidy <Card/> component

--- www/src/components/card.js
+++ www/src/components/card.js
@@ -3,44 +3,38 @@ import { jsx } from "theme-ui"
 
 import { mediaQueries } from "gatsby-design-tokens/dist/theme-gatsbyjs-org"
 
-const Card = ({ children }) => (
-  <div
-    sx={{
-      boxSizing: `border-box`,
-      display: `flex`,
-      transform: `translateZ(0)`,
-      [mediaQueries.md]: {
-        flex: `0 0 auto`,
-        maxWidth: `50%`,
-        boxShadow: t => `0 1px 0 0 ${t.colors.ui.border}`,
-        "&:nth-of-type(5), &:nth-of-type(6)": { boxShadow: `none` },
-        "&:nth-of-type(2n)": {
-          borderLeft: t => `1px solid ${t.colors.ui.border}`,
-        },
-      },
-      [mediaQueries.xl]: {
-        flex: `0 0 auto`,
-        maxWidth: `33.33333333%`,
-        borderLeft: t => `1px solid ${t.colors.ui.border}`,
-        "&:nth-of-type(4)": { boxShadow: `none` },
-        "&:nth-of-type(3n+1)": { borderLeft: 0 },
-      },
-    }}
-  >
-    <div
-      sx={{
-        pt: 8,
-        px: 0,
-        transform: `translateZ(0)`,
-        [mediaQueries.sm]: {
-          px: 8,
-          py: 8,
-        },
-      }}
-    >
-      {children}
-    </div>
-  </div>
-)
+const outerContainerStyles = {
+  display: `flex`,
+  transform: `translateZ(0)`,
+  maxWidth: [null, null, null, `50%`, null, `33.33333333%`],
+  flex: [null, null, null, `0 0 auto`],
+  [mediaQueries.md]: {
+    boxShadow: t => `0 1px 0 0 ${t.colors.ui.border}`,
+    "&:nth-of-type(5), &:nth-of-type(6)": { boxShadow: `none` },
+    "&:nth-of-type(2n)": {
+      borderLeft: 1,
+      borderColor: `ui.border`,
+    },
+  },
+  [mediaQueries.xl]: {
+    borderLeft: 1,
+    borderColor: `ui.border`,
+    "&:nth-of-type(4)": { boxShadow: `none` },
+    "&:nth-of-type(3n+1)": { borderLeft: 0 },
+  },
+}
 
-export default Card
+const innerContainerStyles = {
+  px: [0, null, 8],
+  py: [null, null, 8],
+  pt: 8,
+  transform: `translateZ(0)`,
+}
+
+export default function Card({ children }) {
+  return (
+    <div sx={outerContainerStyles}>
+      <div sx={innerContainerStyles}>{children}</div>
+    </div>
+  )
+}
