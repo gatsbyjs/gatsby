707bd2d037875563eacc8b45cb0ea63b39cf905e
Eddy <eddyvinck95@gmail.com>
2020-07-06 22:16:56 +0200

    Update `tsc --init` to `typescript --init`
    `tsc --init` is outdated

--- starters/default/src/pages/using-typescript.tsx
+++ starters/default/src/pages/using-typescript.tsx
@@ -16,7 +16,7 @@ const UsingTypescript: React.FC<PageProps<DataProps>> = ({ data, path }) => (
     <SEO title="Using TypeScript" />
     <h1>Gatsby supports TypeScript by default!</h1>
     <p>This means that you can create and write <em>.ts/.tsx</em> files for your pages, components etc. Please note that the <em>gatsby-*.js</em> files (like gatsby-node.js) currently don't support TypeScript yet.</p>
-    <p>For type checking you'll want to install <em>typescript</em> via npm and run <em>tsc --init</em> to create a <em>.tsconfig</em> file.</p>
+    <p>For type checking you'll want to install <em>typescript</em> via npm and run <em>typescript --init</em> to create a <em>.tsconfig</em> file.</p>
     <p>You're currently on the page "{path}" which was built on {data.site.buildTime}.</p>
     <p>To learn more, head over to our <a href="https://www.gatsbyjs.org/docs/typescript/">documentation about TypeScript</a>.</p>
     <Link to="/">Go back to the homepage</Link>
