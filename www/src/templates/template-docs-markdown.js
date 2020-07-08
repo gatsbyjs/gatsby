7c9b692595bb2c5a9fe74a65a615eba908fcda9a
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2020-07-07 19:35:42 -0700

    use the thing for prev and next

--- www/src/templates/template-docs-markdown.js
+++ www/src/templates/template-docs-markdown.js
@@ -5,7 +5,7 @@ import { graphql } from "gatsby"
 
 import DocsMarkdownPage from "../components/docs-markdown-page"
 
-function DocsTemplate({ data, location, pageContext: { next, prev } }) {
+function DocsTemplate({ data, location }) {
   const page = data.docPage
 
   return (
