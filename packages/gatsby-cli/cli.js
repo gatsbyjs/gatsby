655230d71ba66a55aaaa175723aabbf9d29b7a59
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2020-07-10 16:25:46 +0530

    Move the Node compaibility check before parsing any code

--- packages/gatsby-cli/cli.js
+++ packages/gatsby-cli/cli.js
@@ -1,3 +1,34 @@
 #!/usr/bin/env node
 
+const semver = require("semver")
+const commonTags = require("common-tags")
+
+const MIN_NODE_VERSION = `10.13.0`
+
+const version = process.version
+
+if (
+  !semver.satisfies(version, `>=${MIN_NODE_VERSION}`, {
+    includePrerelease: true,
+  })
+) {
+  console.error(
+    commonTags.stripIndent(`
+      Gatsby requires Node.js ${MIN_NODE_VERSION} or higher (you have ${version}).
+      Upgrade Node to the latest stable release: https://gatsby.dev/upgrading-node-js
+    `)
+  )
+  process.exit()
+}
+
+if (semver.prerelease(version)) {
+  console.error(
+    commonTags.stripIndent(`
+    You are currently using a prerelease version of Node (${version}), which is not supported.
+    You can use this for testing, but we do not recommend it in production. 
+    Before reporting any bugs, please test with a supported version of Node (>=${MIN_NODE_VERSION}).`)
+  )
+  process.exit()
+}
+
 require(`./lib/index.js`)
