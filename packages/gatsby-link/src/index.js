83926c8ff1df55898ffdeb49183163b5f0bc4270
Blaine Kasten <blainekasten@gmail.com>
2020-07-10 13:57:35 -0500

    fix(gatsby): Support numbers in navigate function (#25611)
    * fix(gatsby): Support numbers in navigate function
    
    * Fix issue to properly support navigating back by number

--- packages/gatsby-link/src/index.js
+++ packages/gatsby-link/src/index.js
@@ -58,6 +58,9 @@ function absolutify(path, current) {
 }
 
 const rewriteLinkPath = (path, relativeTo) => {
+  if (typeof path === `number`) {
+    return path
+  }
   if (!isLocalLink(path)) {
     return path
   }
