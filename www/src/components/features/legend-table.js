035cdd93bbfb7a1d74ed9a48e6d12e8f2a399c92
Duncan Bain <duncan@bain.io>
2020-07-14 22:04:45 +0100

    semantic table, todo: remove header elements from th elements

--- www/src/components/features/legend-table.js
+++ www/src/components/features/legend-table.js
@@ -10,7 +10,6 @@ const legendBallStyle = {
 }
 
 const baseCellStyle = {
-  display: `table-cell`,
   verticalAlign: `middle`,
   textAlign: `center`,
   p: 3,
@@ -38,44 +37,38 @@ const legendExplanationCellStyle = {
 }
 
 const balls = [
-  <div sx={legendBallCellStyle} key={`${legendBallCellStyle}-1`}>
-    <h4 sx={{ m: 0 }}>Icon</h4>
-  </div>,
-  <div sx={legendBallCellStyle} key={`${legendBallCellStyle}-2`}>
+  <td sx={legendBallCellStyle} key={`${legendBallCellStyle}-2`}>
     <EvaluationCell num="3" style={legendBallStyle} />
-  </div>,
-  <div sx={legendBallCellStyle} key={`${legendBallCellStyle}-3`}>
+  </td>,
+  <td sx={legendBallCellStyle} key={`${legendBallCellStyle}-3`}>
     <EvaluationCell num="2" style={legendBallStyle} />
-  </div>,
-  <div sx={legendBallCellStyle} key={`${legendBallCellStyle}-4`}>
+  </td>,
+  <td sx={legendBallCellStyle} key={`${legendBallCellStyle}-4`}>
     <EvaluationCell num="1" style={legendBallStyle} />
-  </div>,
-  <div sx={legendBallCellStyle} key={`${legendBallCellStyle}-5`}>
+  </td>,
+  <td sx={legendBallCellStyle} key={`${legendBallCellStyle}-5`}>
     <EvaluationCell num="0" style={legendBallStyle} />
-  </div>,
+  </td>,
 ]
 
 const legendText = [
-  <div sx={legendExplanationCellStyle} key={`legendExplanationCell-1`}>
-    <h5 sx={{ m: 0 }}>Feature Availability</h5>
-  </div>,
-  <div sx={legendExplanationCellStyle} key={`legendExplanationCell-2`}>
+  <td sx={legendExplanationCellStyle} key={`legendExplanationCell-2`}>
     Excellent (fully available)
-  </div>,
-  <div sx={legendExplanationCellStyle} key={`legendExplanationCell-3`}>
+  </td>,
+  <td sx={legendExplanationCellStyle} key={`legendExplanationCell-3`}>
     Good (partially available, e.g. plugins)
-  </div>,
-  <div sx={legendExplanationCellStyle} key={`legendExplanationCell-4`}>
+  </td>,
+  <td sx={legendExplanationCellStyle} key={`legendExplanationCell-4`}>
     Fair (needs customization or limited)
-  </div>,
-  <div sx={legendExplanationCellStyle} key={`legendExplanationCell-5`}>
+  </td>,
+  <td sx={legendExplanationCellStyle} key={`legendExplanationCell-5`}>
     Poor (not possible)
-  </div>,
+  </td>,
 ]
 
 export default function LegendTable() {
   return (
-    <table>
+    <table sx={{ ...legendTableContainerStyle, width: `100%` }}>
       <caption
         id="legend"
         sx={{
@@ -84,33 +77,66 @@ export default function LegendTable() {
           textTransform: `uppercase`,
           textAlign: `left`,
           marginBottom: `1.5rem`,
+          lineHeight: `1.5rem`,
         }}
       >
         Legend
       </caption>
-      <div
+      <tbody
         sx={{
           display: [`none`, null, `table`],
-          ...legendTableContainerStyle,
-          width: `100%`,
         }}
       >
-        <div sx={{ display: `table-row` }}>{balls}</div>
-        <div sx={{ display: `table-row` }}>{legendText}</div>
-      </div>
-      <div
+        <tr sx={{ display: `table-row` }}>
+          <th
+            sx={legendBallCellStyle}
+            key={`${legendBallCellStyle}-1`}
+            scope="row"
+          >
+            <h4 sx={{ m: 0 }}>Icon</h4>
+          </th>
+          {balls}
+        </tr>
+        <tr sx={{ display: `table-row` }}>
+          <th
+            sx={legendExplanationCellStyle}
+            key={`legendExplanationCell-1`}
+            scope="row"
+          >
+            <h4 sx={{ m: 0 }}>Feature Availability</h4>
+          </th>
+          {legendText}
+        </tr>
+      </tbody>
+      <tbody
         sx={{
           display: [`table`, null, `none`],
           ...legendTableContainerStyle,
         }}
       >
+        <tr>
+          <th
+            sx={legendBallCellStyle}
+            key={`${legendBallCellStyle}-1`}
+            scope="col"
+          >
+            <h4 sx={{ m: 0 }}>Icon</h4>
+          </th>
+          <th
+            sx={legendExplanationCellStyle}
+            key={`legendExplanationCell-1`}
+            scope="col"
+          >
+            <h4 sx={{ m: 0 }}>Feature Availability</h4>
+          </th>
+        </tr>
         {[0, 1, 2, 3, 4].map(i => (
-          <div sx={{ display: `table-row` }} key={i}>
+          <tr sx={{ display: `table-row` }} key={i}>
             {balls[i]}
             {legendText[i]}
-          </div>
+          </tr>
         ))}
-      </div>
+      </tbody>
     </table>
   )
 }
