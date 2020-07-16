c08fa4a6a6a7e8cc7e06ed4e28a8e325fbf96412
webbugt <idoncev@gmail.com>
2020-07-16 14:20:00 +0200

    feat(gatsby-source-filesystem): allow adjusting `createRemoteFileNode` retry/timeout settings via env vars (#24535)
    * (gatsby-source-filesystem) Remote File Node env variables
    
    For developers with awful internet connection (like me), when using createRemoteFileNode, default timeouts of 30000ms are too short. Out of 300 files, it'd fail 1-4 images due to timeout and mess up the build process. 
    Thus, it would be useful to overwrite default remote file download settings (retry limits and timeouts) with .env variables. 
    For my specific case, just doubling all the values solved all issues.
    
    * Update create-remote-file-node.js
    
    removed ";"s
    
    * updated readme
    
    * update
    
    Co-authored-by: Michal Piechowiak <misiek.piechowiak@gmail.com>

--- packages/gatsby-source-filesystem/src/create-remote-file-node.js
+++ packages/gatsby-source-filesystem/src/create-remote-file-node.js
@@ -55,10 +55,10 @@ let totalJobs = 0
  * @param  {Reporter} [options.reporter]
  */
 
-const STALL_RETRY_LIMIT = 3
-const STALL_TIMEOUT = 30000
+const STALL_RETRY_LIMIT = process.env.GATSBY_STALL_RETRY_LIMIT || 3
+const STALL_TIMEOUT = process.env.GATSBY_STALL_TIMEOUT || 30000
 
-const CONNECTION_TIMEOUT = 30000
+const CONNECTION_TIMEOUT = process.env.GATSBY_CONNECTION_TIMEOUT || 30000
 
 /********************
  * Queue Management *
