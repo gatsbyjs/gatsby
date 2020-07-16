3dd4339e269a9710780a9fc054afaf079f119c77
herecydev <herecy@live.co.uk>
2020-07-16 21:03:17 +0100

    Remove description

--- packages/gatsby-recipes/src/providers/npm/package.js
+++ packages/gatsby-recipes/src/providers/npm/package.js
@@ -138,11 +138,8 @@ const read = async ({ root }, id) => {
 
 const schema = {
   name: Joi.string().required(),
-  version: Joi.string().default(`latest`, `Defaults to "latest"`),
-  dependencyType: Joi.string().default(
-    `dependency`,
-    `defaults to regular dependency`
-  ),
+  version: Joi.string().default(`latest`),
+  dependencyType: Joi.string().default(`dependency`),
   description: Joi.string(),
   ...resourceSchema,
 }
