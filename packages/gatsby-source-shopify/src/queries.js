af9dc3fb24f6247af698fe2762425695494ce733
Muescha <184316+muescha@users.noreply.github.com>
2020-07-17 13:44:38 +0200

    inline products query

--- packages/gatsby-source-shopify/src/queries.js
+++ packages/gatsby-source-shopify/src/queries.js
@@ -1,125 +1,3 @@
-export const PRODUCTS_QUERY = `
-  query GetProducts($first: Int!, $after: String) {
-    products(first: $first, after: $after) {
-      pageInfo {
-        hasNextPage
-      }
-      edges {
-        cursor
-        node {
-          availableForSale
-          createdAt
-          description
-          descriptionHtml
-          handle
-          id
-          images(first: 250) {
-            edges {
-              node {
-                id
-                altText
-                originalSrc
-              }
-            }
-          }
-          metafields(first: 250) {
-            edges {
-              node {
-                description
-                id
-                key
-                namespace
-                value
-                valueType
-              }
-            }
-          }
-          onlineStoreUrl
-          options {
-            id
-            name
-            values
-          }
-          priceRange {
-            minVariantPrice {
-              amount
-              currencyCode
-            }
-            maxVariantPrice {
-              amount
-              currencyCode
-            }
-          }
-          productType
-          publishedAt
-          tags
-          title
-          updatedAt
-          variants(first: 250) {
-            edges {
-              node {
-                availableForSale
-                compareAtPrice
-                compareAtPriceV2 {
-                  amount
-                  currencyCode
-                }
-                id
-                image {
-                  altText
-                  id
-                  originalSrc
-                }
-                metafields(first: 250) {
-                  edges {
-                    node {
-                      description
-                      id
-                      key
-                      namespace
-                      value
-                      valueType
-                    }
-                  }
-                }
-                price
-                priceV2 {
-                  amount
-                  currencyCode
-                }
-                requiresShipping
-                selectedOptions {
-                  name
-                  value
-                }
-                sku
-                title
-                weight
-                weightUnit
-                presentmentPrices(first: 250) {
-                  edges {
-                    node {
-                      price {
-                        amount
-                        currencyCode
-                      }
-                      compareAtPrice {
-                        amount
-                        currencyCode
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-          vendor
-        }
-      }
-    }
-  }
-`
-
 export const SHOP_POLICIES_QUERY = `
   query GetPolicies {
     shop {
@@ -279,7 +157,127 @@ export const defaultQueries = {
     }
   }
 `,
-  products: PRODUCTS_QUERY,
+  products: `
+  query GetProducts($first: Int!, $after: String) {
+    products(first: $first, after: $after) {
+      pageInfo {
+        hasNextPage
+      }
+      edges {
+        cursor
+        node {
+          availableForSale
+          createdAt
+          description
+          descriptionHtml
+          handle
+          id
+          images(first: 250) {
+            edges {
+              node {
+                id
+                altText
+                originalSrc
+              }
+            }
+          }
+          metafields(first: 250) {
+            edges {
+              node {
+                description
+                id
+                key
+                namespace
+                value
+                valueType
+              }
+            }
+          }
+          onlineStoreUrl
+          options {
+            id
+            name
+            values
+          }
+          priceRange {
+            minVariantPrice {
+              amount
+              currencyCode
+            }
+            maxVariantPrice {
+              amount
+              currencyCode
+            }
+          }
+          productType
+          publishedAt
+          tags
+          title
+          updatedAt
+          variants(first: 250) {
+            edges {
+              node {
+                availableForSale
+                compareAtPrice
+                compareAtPriceV2 {
+                  amount
+                  currencyCode
+                }
+                id
+                image {
+                  altText
+                  id
+                  originalSrc
+                }
+                metafields(first: 250) {
+                  edges {
+                    node {
+                      description
+                      id
+                      key
+                      namespace
+                      value
+                      valueType
+                    }
+                  }
+                }
+                price
+                priceV2 {
+                  amount
+                  currencyCode
+                }
+                requiresShipping
+                selectedOptions {
+                  name
+                  value
+                }
+                sku
+                title
+                weight
+                weightUnit
+                presentmentPrices(first: 250) {
+                  edges {
+                    node {
+                      price {
+                        amount
+                        currencyCode
+                      }
+                      compareAtPrice {
+                        amount
+                        currencyCode
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+          vendor
+        }
+      }
+    }
+  }
+`,
   shopPolicies: SHOP_POLICIES_QUERY,
   pages: PAGES_QUERY,
 }
