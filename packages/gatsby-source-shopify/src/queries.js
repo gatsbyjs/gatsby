e1be50f64cde0f4584193b702ec41752aff25f5f
Muescha <184316+muescha@users.noreply.github.com>
2020-07-17 13:40:39 +0200

    inline articles query

--- packages/gatsby-source-shopify/src/queries.js
+++ packages/gatsby-source-shopify/src/queries.js
@@ -1,61 +1,3 @@
-export const ARTICLES_QUERY = `
-  query GetArticles($first: Int!, $after: String) {
-    articles(first: $first, after: $after) {
-      pageInfo {
-        hasNextPage
-      }
-      edges {
-        cursor
-        node {
-          author {
-            bio
-            email
-            firstName
-            lastName
-            name
-          }
-          blog {
-            id
-          }
-          comments(first: 250) {
-            edges {
-              node {
-                author {
-                  email
-                  name
-                }
-                content
-                contentHtml
-                id
-              }
-            }
-          }
-          content
-          contentHtml
-          excerpt
-          excerptHtml
-          handle
-          id
-          handle
-          image {
-            altText
-            id
-            src
-          }
-          publishedAt
-          tags
-          title
-          url
-          seo {
-            title
-            description
-          }
-        }
-      }
-    }
-  }
-`
-
 export const BLOGS_QUERY = `
   query GetBlogs($first: Int!, $after: String) {
     blogs(first: $first, after: $after) {
@@ -282,7 +224,63 @@ export const PAGES_QUERY = `
 `
 
 export const defaultQueries = {
-  articles: ARTICLES_QUERY,
+  articles: `
+  query GetArticles($first: Int!, $after: String) {
+    articles(first: $first, after: $after) {
+      pageInfo {
+        hasNextPage
+      }
+      edges {
+        cursor
+        node {
+          author {
+            bio
+            email
+            firstName
+            lastName
+            name
+          }
+          blog {
+            id
+          }
+          comments(first: 250) {
+            edges {
+              node {
+                author {
+                  email
+                  name
+                }
+                content
+                contentHtml
+                id
+              }
+            }
+          }
+          content
+          contentHtml
+          excerpt
+          excerptHtml
+          handle
+          id
+          handle
+          image {
+            altText
+            id
+            src
+          }
+          publishedAt
+          tags
+          title
+          url
+          seo {
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`,
   blogs: BLOGS_QUERY,
   collections: COLLECTIONS_QUERY,
   products: PRODUCTS_QUERY,
