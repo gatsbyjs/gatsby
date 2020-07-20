d93164010d6a831f1c0024379944c708baaa51f8
Blaine Kasten <blainekasten@gmail.com>
2020-07-20 14:33:19 -0500

    get rid of e2e test legacy usage of createPagesForData

--- e2e-tests/production-runtime/src/pages/collection-routing/{Product.name}.js
+++ e2e-tests/production-runtime/src/pages/collection-routing/{Product.name}.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { unstable_createPagesFromData, Link, graphql } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../../components/layout"
 
