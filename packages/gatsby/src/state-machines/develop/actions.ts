cf14989f73f89201630e7a68eeaaa64d464b6a9f
Matt Kane <matt@gatsbyjs.com>
2020-07-22 09:47:47 +0100

    feat(gatsby): Defer node mutation during querying (#25479)
    * Move bootstrap into machine
    
    * Add parent span and query extraction
    
    * Add rebuildSchemaWithSitePage
    
    * Use values from context
    
    * Remove logs
    
    * Add redirectListener
    
    * Changes from review
    
    * Log child state transitions
    
    * Add state machine for query running
    
    * Changes from review
    
    * Changes from review
    
    * Switch to reporter
    
    * Use assertStore
    
    * Remove unused action
    
    * Remove unusued config
    
    * Remove unusued config
    
    * Add gql runner reset
    
    * Handle node mutation queuing and batching in state machine
    
    * Use new pagedata utils
    
    * Use develop queue
    
    * New xstate syntax
    
    * Work-around xstate bug
    
    * Track first run
    
    * Track first run
    
    * Disable --quiet in e2e
    
    * Don't defer node mutation if we're outside the state machine
    
    * Re-quieten e2e
    
    * Listen for query file changes
    
    * Lint
    
    * Handle webhook
    
    * Changes from review
    
    * Fix typings
    
    * Changes from review
    
    * Typefix
    
    * feat(gatsby): Move final parts into develop state machine (#25716)
    
    * Move remaining parts into state machine
    
    * Move top level state machine into state machines dir
    
    * Add machine ids
    
    * Add missing imports
    
    * Resolve api promises
    
    * Remove unused action
    
    * Move logging into helper
    
    * Changes from review
    
    * Manually save db
    
    * Add comments
    
    * Remove first run from query running
    
    * Refactor into separate data layer machines
    
    * Fix condition
    
    Co-authored-by: gatsbybot <mathews.kyle+gatsbybot@gmail.com>

---
+++ packages/gatsby/src/state-machines/develop/actions.ts
@@ -0,0 +1,140 @@
+import {
+  assign,
+  AnyEventObject,
+  ActionFunction,
+  spawn,
+  ActionFunctionMap,
+  DoneEventObject,
+} from "xstate"
+import { Store } from "redux"
+import { IBuildContext, IMutationAction } from "../../services"
+import { actions, boundActionCreators } from "../../redux/actions"
+import { listenForMutations } from "../../services/listen-for-mutations"
+import { DataLayerResult } from "../data-layer"
+import { assertStore } from "../../utils/assert-store"
+import { saveState } from "../../db"
+import reporter from "gatsby-cli/lib/reporter"
+import { ProgramStatus } from "../../redux/types"
+
+/**
+ * These are the deferred redux actions sent from api-runner-node
+ * They may include a `resolve` prop (if they are createNode actions).
+ * If so, we resolve the promise when we're done
+ */
+export const callRealApi = (event: IMutationAction, store?: Store): void => {
+  assertStore(store)
+  const { type, payload, resolve } = event
+  if (type in actions) {
+    // If this is a createNode action then this will be a thunk.
+    // No worries, we just dispatch it like any other
+    const action = actions[type](...payload)
+    const result = store.dispatch(action)
+    // Somebody may be waiting for this
+    if (resolve) {
+      resolve(result)
+    }
+  } else {
+    reporter.log(`Could not dispatch unknown action "${type}`)
+  }
+}
+
+/**
+ * Handler for when we're inside handlers that should be able to mutate nodes
+ * Instead of queueing, we call it right away
+ */
+export const callApi: ActionFunction<IBuildContext, AnyEventObject> = (
+  { store },
+  event
+) => callRealApi(event.payload, store)
+
+/**
+ * Event handler used in all states where we're not ready to process node
+ * mutations. Instead we add it to a batch to process when we're next idle
+ */
+export const addNodeMutation = assign<IBuildContext, AnyEventObject>({
+  nodeMutationBatch: ({ nodeMutationBatch = [] }, { payload }) => {
+    // It's not pretty, but it's much quicker than concat
+    nodeMutationBatch.push(payload)
+    return nodeMutationBatch
+  },
+})
+
+export const assignStoreAndWorkerPool = assign<IBuildContext, DoneEventObject>(
+  (_context, event) => {
+    const { store, workerPool } = event.data
+    return {
+      store,
+      workerPool,
+    }
+  }
+)
+
+const setQueryRunningFinished = async (): Promise<void> => {
+  boundActionCreators.setProgramStatus(
+    ProgramStatus.BOOTSTRAP_QUERY_RUNNING_FINISHED
+  )
+}
+
+export const markQueryFilesDirty = assign<IBuildContext>({
+  queryFilesDirty: true,
+})
+
+export const assignServiceResult = assign<IBuildContext, DoneEventObject>(
+  (_context, { data }): DataLayerResult => data
+)
+
+/**
+ * This spawns the service that listens to the `emitter` for various mutation events
+ */
+export const spawnMutationListener = assign<IBuildContext>({
+  mutationListener: () => spawn(listenForMutations, `listen-for-mutations`),
+})
+
+export const assignServers = assign<IBuildContext, AnyEventObject>(
+  (_context, { data }) => {
+    return {
+      ...data,
+    }
+  }
+)
+
+export const assignWebhookBody = assign<IBuildContext, AnyEventObject>({
+  webhookBody: (_context, { payload }) => payload?.webhookBody,
+})
+
+export const clearWebhookBody = assign<IBuildContext, AnyEventObject>({
+  webhookBody: undefined,
+})
+
+export const finishParentSpan = ({ parentSpan }: IBuildContext): void =>
+  parentSpan?.finish()
+
+export const saveDbState = (): Promise<void> => saveState()
+
+/**
+ * Event handler used in all states where we're not ready to process a file change
+ * Instead we add it to a batch to process when we're next idle
+ */
+// export const markFilesDirty: BuildMachineAction = assign<IBuildContext>({
+//   filesDirty: true,
+// })
+
+export const markNodesDirty = assign<IBuildContext>({
+  nodesMutatedDuringQueryRun: true,
+})
+
+export const buildActions: ActionFunctionMap<IBuildContext, AnyEventObject> = {
+  callApi,
+  markNodesDirty,
+  addNodeMutation,
+  spawnMutationListener,
+  assignStoreAndWorkerPool,
+  assignServiceResult,
+  assignServers,
+  markQueryFilesDirty,
+  assignWebhookBody,
+  clearWebhookBody,
+  finishParentSpan,
+  saveDbState,
+  setQueryRunningFinished,
+}
