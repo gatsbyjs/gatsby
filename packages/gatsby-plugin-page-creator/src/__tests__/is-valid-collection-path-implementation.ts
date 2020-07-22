472d792b0eaa961ee83f4a94c3820b08ff586f29
Blaine Kasten <blainekasten@gmail.com>
2020-07-22 13:06:53 -0500

    fix test

--- packages/gatsby-plugin-page-creator/src/__tests__/is-valid-collection-path-implementation.ts
+++ packages/gatsby-plugin-page-creator/src/__tests__/is-valid-collection-path-implementation.ts
@@ -1,12 +1,21 @@
 import { isValidCollectionPathImplementation } from "../is-valid-collection-path-implementation"
 import reporter from "gatsby-cli/lib/reporter"
+import syspath from "path"
 
 jest.mock(`gatsby-cli/lib/reporter`)
+
+// windows and mac have different seperators, all code is written with unix-like
+// file systems, but the underlying code uses `path.sep`. So when running tests
+// on windows, they would fail without us swapping the seperators.
+const compatiblePath = (filepath: string): string =>
+  filepath.replace(/\//g, syspath.sep)
 
 describe(`isValidCollectionPathImplementation`, () => {
   it(`works`, () => {
     expect(() =>
-      isValidCollectionPathImplementation(`{Model.bar}/{Model.bar}.js`)
+      isValidCollectionPathImplementation(
+        compatiblePath(`{Model.bar}/{Model.bar}.js`)
+      )
     ).not.toThrow()
   })
 
@@ -19,7 +28,7 @@ describe(`isValidCollectionPathImplementation`, () => {
   ])(`%o`, path => {
     const part = path.split(`/`)[2]
 
-    isValidCollectionPathImplementation(path)
+    isValidCollectionPathImplementation(compatiblePath(path))
     expect(reporter.panicOnBuild)
       .toBeCalledWith(`Collection page builder encountered an error parsing the filepath. To use collection paths the schema to follow is {Model.field}. The problematic part is: ${part}.
 filePath: ${path}`)
