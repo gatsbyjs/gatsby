4cc61deff5f996eeebd31a4b5e14e053901a8f5a
Edmund-Adewu <60354014+Edmund-Adewu@users.noreply.github.com>
2020-07-24 18:49:45 -0400

    Revert changes to bio-content.js

--- starters/gatsby-starter-blog-theme/src/gatsby-theme-blog/components/bio-content.js
+++ starters/gatsby-starter-blog-theme/src/gatsby-theme-blog/components/bio-content.js
@@ -7,10 +7,10 @@ import { Styled } from "theme-ui"
 
 export default () => (
   <Fragment>
-    It's me, Winnie! I hope you're safe and sound, folks! What's that? You want to build your own site? Don't be shy. You can place <Styled.a href="http://example.com/">your name</Styled.a>
+    This is where <Styled.a href="http://example.com/">your name</Styled.a>
     {` `}
-    here or wherever you like.
+    goes.
     <br />
-    Now, where did I put my honey?
+    Or whatever, you make the rules.
   </Fragment>
 )
