3c6c2017654fab5cbb0f22fe2b51a4e791ed1919
Jarmo Isotalo <jamo@isotalo.fi>
2020-07-28 00:27:09 +0300

    fix test

--- packages/gatsby-telemetry/src/__tests__/telemetry.ts
+++ packages/gatsby-telemetry/src/__tests__/telemetry.ts
@@ -1,23 +1,23 @@
 jest.mock(`../event-storage`)
-const eventStore = require(`../event-storage`)
+import { EventStorage } from "../event-storage"
 import { AnalyticsTracker } from "../telemetry"
 
 let telemetry
 beforeEach(() => {
-  eventStore.mockReset()
+  EventStorage.mockReset()
   telemetry = new AnalyticsTracker()
 })
 
 describe(`Telemetry`, () => {
   it(`Adds event to store`, () => {
     telemetry.buildAndStoreEvent(`demo`, {})
-    expect(eventStore).toHaveBeenCalledTimes(1)
-    expect(eventStore.mock.instances[0].addEvent).toHaveBeenCalledTimes(1)
+    expect(EventStorage).toHaveBeenCalledTimes(1)
+    expect(EventStorage.mock.instances[0].addEvent).toHaveBeenCalledTimes(1)
   })
 
   it(`Doesn't add event to store if telemetry tracking is turned off`, () => {
     telemetry.trackingEnabled = false
     telemetry.trackActivity(`demo`)
-    expect(eventStore.mock.instances[0].addEvent).not.toHaveBeenCalled()
+    expect(EventStorage.mock.instances[0].addEvent).not.toHaveBeenCalled()
   })
 })
