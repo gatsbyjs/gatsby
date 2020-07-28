cbd91758a87aca43fb8a6d9d46753e1a20beba35
Jarmo Isotalo <jamo@isotalo.fi>
2020-07-28 16:52:26 +0300

    Stupid jest

--- packages/gatsby-telemetry/src/__tests__/telemetry.ts
+++ packages/gatsby-telemetry/src/__tests__/telemetry.ts
@@ -4,7 +4,7 @@ import { AnalyticsTracker } from "../telemetry"
 
 let telemetry
 beforeEach(() => {
-  EventStorage.mockReset()
+  ;(EventStorage as jest.Mock).mockReset()
   telemetry = new AnalyticsTracker()
 })
 
@@ -12,12 +12,16 @@ describe(`Telemetry`, () => {
   it(`Adds event to store`, () => {
     telemetry.buildAndStoreEvent(`demo`, {})
     expect(EventStorage).toHaveBeenCalledTimes(1)
-    expect(EventStorage.mock.instances[0].addEvent).toHaveBeenCalledTimes(1)
+    expect(
+      (EventStorage as jest.Mock).mock.instances[0].addEvent
+    ).toHaveBeenCalledTimes(1)
   })
 
   it(`Doesn't add event to store if telemetry tracking is turned off`, () => {
     telemetry.trackingEnabled = false
     telemetry.trackActivity(`demo`)
-    expect(EventStorage.mock.instances[0].addEvent).not.toHaveBeenCalled()
+    expect(
+      (EventStorage as jest.Mock).mock.instances[0].addEvent
+    ).not.toHaveBeenCalled()
   })
 })
