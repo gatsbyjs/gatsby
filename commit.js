1a3766b772ebbf68777d50c35e93eee0ab20805c
tanya044 <61521631+tanya044@users.noreply.github.com>
2020-07-30 18:03:27 +0530

    Create commit.js

---
+++ commit.js
@@ -0,0 +1,93 @@
+title: Glee
+  main_url: https://glee.traction.one/
+  url: https://glee.traction.one/
+  description: Whether you're a startup, a developer, an artist, a creator, or just a handful of friends building a great product or creating interesting content, Glee makes it easy to collect, track and manage feedback from your users.
+  featured: false
+  categories:
+    - App
+    - Technology
+    - Productivity
+  built_by: Sankarsan Kampa
+  built_by_url: https://traction.one
+- title: Paths.io
+  url: https://paths.io/
+  main_url: https://paths.io/
+  description: >
+    Paths enables a new type of career discovery, in addition to being a better way to find work.
+  categories:
+    - Data
+    - Technology
+    - Landing Page
+  built_by: HiringSolved
+  built_by_url: https://hiringsolved.com/home/
+  featured: false
+- title: Meetup
+  url: https://meetup.com/
+  main_url: https://meetup.com/
+  description: >
+    Meetup is the leading social media platform dedicated to connecting people in person, or online during times of crisis.
+  categories:
+    - Accessibility
+    - App
+    - Community
+    - Event
+    - SEO
+- title: John Kavanagh Portfolio
+  url: https://johnkavanagh.co.uk/
+  main_url: https://johnkavanagh.co.uk/
+  description: >
+    Website and portfolio of John Kavanagh: a freelance front-end web developer in Brighton, UK.
+  categories:
+    - Portfolio
+    - Technology
+    - Web Development
+  built_by: John Kavanagh
+  built_by_url: https://johnkavanagh.co.uk/
+  featured: false
+- title: MongoDB Developer Hub
+  url: https://developer.mongodb.com
+  main_url: https://developer.mongodb.com
+  description: >
+    The MongoDB Developer Hub serves as the central location for all MongoDB developer content, programs, and resources.
+  categories:
+    - Blog
+    - Data
+    - Education
+    - Learning
+    - Programming
+    - Technology
+    - Web Development
+  built_by: MongoDB
+  featured: false
+- title: Devnet
+  main_url: https://devnet.hr/
+  url: https://devnet.hr/
+  description: >
+    Business website for web development company.
+  categories:
+    - Web Development
+    - Agency
+  built_by: Devnet
+  built_by_url: https://devnet.hr
+- title: Leanier.com
+  url: https://leanier.com
+  main_url: https://leanier.com
+  description: >
+    Enterprise SaaS Management for Modern IT
+  categories:
+    - Data
+    - Technology
+     - Business
+   built_by: leaniercode
+   featured: false
+ - title: TanyaV
+   url: https://Tanyav.com
+   main_url: https://TAnyav.com
+   description: A distaction-free platform to consume Islamic content from reputable Youtube Channels
+   categories:
+     - Community
+     - Library
+     - Media
+     - Nonprofit
+     - SEO
+   featured: false
