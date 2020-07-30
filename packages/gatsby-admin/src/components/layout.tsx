752a001aaf1e97102225aba190bdbbd1b7d38fea
Max Stoiber <contact@mxstbr.com>
2020-07-30 10:46:29 +0200

    Add global layout spacing, update nav a little bit

--- packages/gatsby-admin/src/components/layout.tsx
+++ packages/gatsby-admin/src/components/layout.tsx
@@ -1,5 +1,6 @@
 /** @jsx jsx */
-import { jsx, Flex } from "strict-ui"
+import { jsx } from "theme-ui"
+import { Flex } from "strict-ui"
 import Providers from "./providers"
 import Navbar from "./navbar"
 import { Helmet } from "react-helmet"
@@ -10,7 +11,11 @@ const Layout: React.FC<{}> = ({ children }) => (
       <title>Gatsby Admin</title>
       <html lang="en" />
     </Helmet>
-    <Flex gap={0} flexDirection="column">
+    <Flex
+      gap={0}
+      flexDirection="column"
+      sx={{ maxWidth: `76rem`, margin: `0 auto`, px: 8 }}
+    >
       <Navbar />
       <main>{children}</main>
     </Flex>
--- packages/gatsby-admin/src/pages/index.tsx
+++ packages/gatsby-admin/src/pages/index.tsx
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import React from "react"
+import React, { Fragment } from "react"
 import { jsx, Flex } from "strict-ui"
 import { Spinner } from "theme-ui"
 import { useQuery, useMutation } from "urql"
@@ -12,6 +12,7 @@ import {
   HeadingProps,
   Text,
   Badge,
+  Spacer,
 } from "gatsby-interface"
 import PluginSearchBar from "../components/plugin-search"
 
@@ -112,62 +113,65 @@ const Index: React.FC<{}> = () => {
   }
 
   return (
-    <Flex>
-      <Flex flexDirection="column" gap={13} flex="1">
-        <Flex gap={7} flexDirection="column">
-          <Heading as="h1" sx={{ fontWeight: `800` }}>
-            <div
-              sx={{
-                color: `text.secondary`,
-                fontSize: 3,
-                fontWeight: `400`,
-                pb: 2,
-              }}
+    <Fragment>
+      <Spacer size={9} />
+      <Flex>
+        <Flex flexDirection="column" gap={13} flex="1">
+          <Flex gap={7} flexDirection="column">
+            <Heading as="h1" sx={{ fontWeight: `800` }}>
+              <div
+                sx={{
+                  color: `text.secondary`,
+                  fontSize: 3,
+                  fontWeight: `400`,
+                  pb: 2,
+                }}
+              >
+                Welcome to
+              </div>
+              <Flex alignItems="center" gap={3}>
+                <span>Gatsby Admin</span>
+                <Badge tone="NEUTRAL">alpha</Badge>
+              </Flex>
+            </Heading>
+            <Text sx={{ color: `grey.80` }}>
+              Gatsby Admin is your user interface for managing and extending
+              your Gatsby site. Manage your installed plugins, themes, site
+              metadata and more without touching your code editor.
+            </Text>
+          </Flex>
+          <Flex gap={7} flexDirection="column">
+            <Subheading>
+              Installed plugins ({data.allGatsbyPlugin.nodes.length})
+            </Subheading>
+            <Flex
+              as="ul"
+              gap={3}
+              flexDirection="column"
+              sx={{ p: 0, listStyle: `none` }}
             >
-              Welcome to
-            </div>
-            <Flex alignItems="center" gap={3}>
-              <span>Gatsby Admin</span>
-              <Badge tone="NEUTRAL">alpha</Badge>
+              {data.allGatsbyPlugin.nodes.map(plugin => (
+                <InstalledPluginListItem key={plugin.id} plugin={plugin} />
+              ))}
             </Flex>
-          </Heading>
-          <Text sx={{ color: `grey.80` }}>
-            Gatsby Admin is your user interface for managing and extending your
-            Gatsby site. Manage your installed plugins, themes, site metadata
-            and more without touching your code editor.
-          </Text>
-        </Flex>
-        <Flex gap={7} flexDirection="column">
-          <Subheading>
-            Installed plugins ({data.allGatsbyPlugin.nodes.length})
-          </Subheading>
-          <Flex
-            as="ul"
-            gap={3}
-            flexDirection="column"
-            sx={{ p: 0, listStyle: `none` }}
-          >
-            {data.allGatsbyPlugin.nodes.map(plugin => (
-              <InstalledPluginListItem key={plugin.id} plugin={plugin} />
-            ))}
           </Flex>
         </Flex>
-      </Flex>
-      <Flex flexDirection="column" gap={13} flex="1">
-        <Flex flexDirection="column" gap={3}>
-          <Subheading id="plugin-search-label">Search all plugins</Subheading>
-          <Text sx={{ color: `grey.60` }}>
-            One of the best ways to add functionality to a Gatsby site is
-            through our plugin ecosystem. With 2000+ plugins, there’s probably
-            one that does what you need— from SEO to image optimization to data
-            sourcing and much more.
-          </Text>
-          <PluginSearchBar />
-        </Flex>
+        <Flex flexDirection="column" gap={13} flex="1">
+          <Flex flexDirection="column" gap={3}>
+            <Subheading id="plugin-search-label">Search all plugins</Subheading>
+            <Text sx={{ color: `grey.60` }}>
+              One of the best ways to add functionality to a Gatsby site is
+              through our plugin ecosystem. With 2000+ plugins, there’s probably
+              one that does what you need— from SEO to image optimization to
+              data sourcing and much more.
+            </Text>
+            <PluginSearchBar />
+          </Flex>
 
-        <Subheading>Recommended plugins</Subheading>
+          <Subheading>Recommended plugins</Subheading>
+        </Flex>
       </Flex>
-    </Flex>
+    </Fragment>
   )
 }
 
