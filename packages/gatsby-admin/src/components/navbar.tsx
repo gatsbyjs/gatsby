1c5d8e93f0c760d3f36ce1c65a7b0faa4cbb30b9
Max Stoiber <contact@mxstbr.com>
2020-07-31 16:23:32 +0200

    feat(gatsby-admin): new navbar design (#26159)
    * feat(gatsby-admin): new navbar design
    
    * Update yarn.lock
    
    * graphiql

--- packages/gatsby-admin/src/components/navbar.tsx
+++ packages/gatsby-admin/src/components/navbar.tsx
@@ -1,7 +1,18 @@
 /** @jsx jsx */
 import { jsx, Flex } from "strict-ui"
-import { Text, BaseAnchor } from "gatsby-interface"
+import { Text, Button, AnchorButton } from "gatsby-interface"
 import { useQuery } from "urql"
+import { FeedbackForm } from "feedback-fish"
+import externalLinkIcon from "../external-link.svg"
+import graphqlIcon from "../graphql.svg"
+
+function SendFeedbackButton(props): JSX.Element {
+  return (
+    <Button variant="GHOST" size="S" {...props}>
+      Send feedback
+    </Button>
+  )
+}
 
 function Navbar(): JSX.Element {
   const [{ data }] = useQuery({
@@ -24,31 +35,34 @@ function Navbar(): JSX.Element {
         paddingY: 5,
       }}
     >
-      <Flex gap={5} alignItems="center">
-        <Text>Gatsby Admin</Text>
+      <Flex gap={5} alignItems="baseline">
+        <Text sx={{ textTransform: `uppercase`, fontSize: 0 }}>
+          Gatsby Admin
+        </Text>
         {data && data.npmPackageJson && (
-          <div
-            sx={{
-              width: `1px`,
-              height: `16px`,
-              backgroundColor: `blackFade.50`,
-            }}
-          />
-        )}
-        {data && data.npmPackageJson && (
-          <Text sx={{ fontWeight: `bold`, color: `text.primary` }}>
+          <Text sx={{ fontWeight: `bold`, color: `text.primary`, fontSize: 3 }}>
             {data.npmPackageJson.value.replace(/^"|"$/g, ``)}
           </Text>
         )}
       </Flex>
-      <Flex alignItems="center">
-        <BaseAnchor
-          href={`/`}
+      <Flex alignItems="baseline" gap={3}>
+        <FeedbackForm
+          projectId="9502a819990b03"
+          triggerComponent={SendFeedbackButton}
+        />
+        <AnchorButton
+          size="S"
+          href="/___graphql"
           target="_blank"
-          sx={{ color: `grey.60`, textDecoration: `none` }}
+          variant="SECONDARY"
         >
-          Visit site
-        </BaseAnchor>
+          GraphiQL&nbsp;
+          <img src={graphqlIcon} />
+        </AnchorButton>
+        <AnchorButton size="S" href="/" target="_blank">
+          View localhost&nbsp;
+          <img src={externalLinkIcon} />
+        </AnchorButton>
       </Flex>
     </Flex>
   )
