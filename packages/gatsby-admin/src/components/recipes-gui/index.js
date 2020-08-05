57a7f7fcc537453dc809666364fedaaf382e18fc
Laurie Barth <laurie@LauriesrkLaptop.fios-router.home>
2020-08-05 12:26:04 -0400

    remove welcome message

--- packages/gatsby-admin/src/components/recipes-gui/index.js
+++ packages/gatsby-admin/src/components/recipes-gui/index.js
@@ -15,6 +15,7 @@ import { Global } from "@emotion/core"
 
 import fetch from "isomorphic-fetch"
 
+import WelcomeMessage from "./welcome-message"
 import {
   Button,
   ThemeProvider,
@@ -118,28 +119,7 @@ const Diff = ({ resourcePlan, ...props }) => {
   )
 }
 
-const WelcomeMessage = () => (
-  <div
-    sx={{
-      marginTop: 8,
-      marginBottom: 8,
-      background: theme => theme.tones.BRAND.superLight,
-      border: theme => `1px solid ${theme.tones.BRAND.light}`,
-      padding: 4,
-    }}
-  >
-    <Styled.p>
-      Thank you for trying the experimental version of Gatsby Recipes! 🤗
-    </Styled.p>
-    <Styled.p sx={{ margin: 0 }}>
-      Please ask questions, share your recipes, report bugs, and subscribe for
-      updates in our umbrella issue at{` `}
-      <BaseAnchor href="https://github.com/gatsbyjs/gatsby/issues/22991">
-        https://github.com/gatsbyjs/gatsby/issues/22991
-      </BaseAnchor>
-    </Styled.p>
-  </div>
-)
+
 
 const components = {
   Config: () => null,
