029524405bef0c9b212e8de64bf4a74d05156f8d
Laurie Barth <laurie@LauriesrkLaptop.fios-router.home>
2020-08-06 11:29:18 -0400

    use optional chaining

--- packages/gatsby-admin/src/pages/recipe.js
+++ packages/gatsby-admin/src/pages/recipe.js
@@ -2,5 +2,5 @@ import React from "react"
 import GUI from "../components/recipes-gui"
 
 export default function Recipe({ location }) {
-  return <GUI recipe={location.state.name} />
+  return <GUI recipe={location?.state?.name} />
 }
