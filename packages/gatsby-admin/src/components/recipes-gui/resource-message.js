f0cfc0fc1d22aa17e6c0c9e50cfe9b10d416a907
Laurie Barth <laurie@LauriesrkLaptop.fios-router.home>
2020-08-06 16:24:57 -0400

    resource message refactor

--- packages/gatsby-admin/src/components/recipes-gui/resource-message.js
+++ packages/gatsby-admin/src/components/recipes-gui/resource-message.js
@@ -1,4 +1,5 @@
 /** @jsx jsx */
+/* eslint-disable */
 import React from "react"
 import { MdRefresh, MdBrightness1 } from "react-icons/md"
 import { keyframes } from "@emotion/core"
@@ -7,17 +8,17 @@ import { makeResourceId } from "./utils"
 import { jsx } from "theme-ui"
 
 const ResourceMessage = ({ state, resource }) => {
-    let icon = (
-      <MdBrightness1
-        sx={{ height: `10px`, width: `15px`, display: `inline-block` }}
-      />
-    )
-    let message = resource.describe
-  
-    if (state.value === `applyingPlan` && resource.isDone) {
-      icon = <SuccessIcon />
-    } else if (state.value === `applyingPlan`) {
-      const keyframe = keyframes`
+  let icon = (
+    <MdBrightness1
+      sx={{ height: `10px`, width: `15px`, display: `inline-block` }}
+    />
+  )
+  let message = resource.describe
+
+  if (state.value === `applyingPlan` && resource.isDone) {
+    icon = <SuccessIcon />
+  } else if (state.value === `applyingPlan`) {
+    const keyframe = keyframes`
     0% {
       transform: rotate(0);
     }
@@ -25,43 +26,43 @@ const ResourceMessage = ({ state, resource }) => {
       transform: rotate(360deg);
     }
   `
-      icon = (
-        <MdRefresh
-          sx={{
-            display: `inline-block`,
-            animation: `${keyframe} 1s linear infinite`,
+    icon = (
+      <MdRefresh
+        sx={{
+          display: `inline-block`,
+          animation: `${keyframe} 1s linear infinite`,
           height: `15px`,
-            width: `15px`,
+          width: `15px`,
           top: `3px`,
-            position: `relative`,
-          }}
-        />
-      )
-      message = resource.describe
-    } else if (state.value === `done`) {
-      icon = <SuccessIcon height="15px" width="15px" />
-      message = resource._message
-    }
-  
-    return (
-      <>
-        {icon}
-        {` `}
-        <BaseAnchor
-          href={`#${makeResourceId(resource)}`}
-          onClick={e => {
-            e.preventDefault()
-            const target = document.getElementById(e.currentTarget.hash.slice(1))
-            target.scrollIntoView({
-              behavior: `smooth`, // smooth scroll
-              block: `start`, // the upper border of the element will be aligned at the top of the visible part of the window of the scrollable area.
-            })
-          }}
-        >
-          {message}
-        </BaseAnchor>
-      </>
+          position: `relative`,
+        }}
+      />
     )
+    message = resource.describe
+  } else if (state.value === `done`) {
+    icon = <SuccessIcon height="15px" width="15px" />
+    message = resource._message
   }
+
+  return (
+    <>
+      {icon}
+      {` `}
+      <BaseAnchor
+        href={`#${makeResourceId(resource)}`}
+        onClick={e => {
+          e.preventDefault()
+          const target = document.getElementById(e.currentTarget.hash.slice(1))
+          target.scrollIntoView({
+            behavior: `smooth`, // smooth scroll
+            block: `start`, // the upper border of the element will be aligned at the top of the visible part of the window of the scrollable area.
+          })
+        }}
+      >
+        {message}
+      </BaseAnchor>
+    </>
+  )
+}
 
 export default ResourceMessage
