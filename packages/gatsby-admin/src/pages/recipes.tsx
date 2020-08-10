22f73b9b70720fdf00b458052a666ab43c090541
Laurie Barth <laurie@LauriesrkLaptop.fios-router.home>
2020-08-10 10:34:05 -0400

    see if removing use of header in recipes page fixes build error

--- packages/gatsby-admin/src/pages/recipes.tsx
+++ packages/gatsby-admin/src/pages/recipes.tsx
@@ -1,6 +1,5 @@
 /* @jsx jsx */
 import { jsx, Flex } from "strict-ui"
-import { Heading } from "gatsby-interface"
 import recipesList from "gatsby-recipes/src/recipes-list"
 import { Link } from "gatsby"
 
@@ -8,7 +7,7 @@ function Recipes(): JSX.Element {
   return (
     <Flex gap={8} flexDirection="column" sx={{ paddingY: 7, paddingX: 6 }}>
       <Flex gap={6} flexDirection="column">
-        <Heading as="h1">Recipes</Heading>
+        <h1>Recipes</h1>
         <ul sx={{ pl: 0, listStyle: `none` }}>
           {recipesList.map(recipe => (
             <li key={recipe.value}>
@@ -21,7 +20,7 @@ function Recipes(): JSX.Element {
                   borderRadius: 2,
                 }}
               >
-                <Heading as="h3"> {recipe.label}</Heading>
+                <h2> {recipe.label}</h2>
                 <Link to={`/recipe`} state={{ name: recipe.value }}>
                   Recipe GUI for {recipe.value}
                 </Link>
