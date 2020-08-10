419e1a94de2e89f48adcabaa4a38332afd5b48a0
Laurie Barth <laurie@LauriesrkLaptop.fios-router.home>
2020-08-10 10:32:44 -0400

    remove fragment

--- packages/gatsby-admin/src/pages/recipes.tsx
+++ packages/gatsby-admin/src/pages/recipes.tsx
@@ -1,5 +1,4 @@
 /* @jsx jsx */
-import { Fragment } from "react"
 import { jsx, Flex } from "strict-ui"
 import { Heading } from "gatsby-interface"
 import recipesList from "gatsby-recipes/src/recipes-list"
@@ -7,33 +6,31 @@ import { Link } from "gatsby"
 
 function Recipes(): JSX.Element {
   return (
-    <Fragment>
-      <Flex gap={8} flexDirection="column" sx={{ paddingY: 7, paddingX: 6 }}>
-        <Flex gap={6} flexDirection="column">
-          <Heading as="h1">Recipes</Heading>
-          <ul sx={{ pl: 0, listStyle: `none` }}>
-            {recipesList.map(recipe => (
-              <li key={recipe.value}>
-                <Flex
-                  flexDirection="column"
-                  gap={3}
-                  sx={{
-                    backgroundColor: `ui.background`,
-                    padding: 5,
-                    borderRadius: 2,
-                  }}
-                >
-                  <Heading as="h3"> {recipe.label}</Heading>
-                  <Link to={`/recipe`} state={{ name: recipe.value }}>
-                    Recipe GUI for {recipe.value}
-                  </Link>
-                </Flex>
-              </li>
-            ))}
-          </ul>
-        </Flex>
+    <Flex gap={8} flexDirection="column" sx={{ paddingY: 7, paddingX: 6 }}>
+      <Flex gap={6} flexDirection="column">
+        <Heading as="h1">Recipes</Heading>
+        <ul sx={{ pl: 0, listStyle: `none` }}>
+          {recipesList.map(recipe => (
+            <li key={recipe.value}>
+              <Flex
+                flexDirection="column"
+                gap={3}
+                sx={{
+                  backgroundColor: `ui.background`,
+                  padding: 5,
+                  borderRadius: 2,
+                }}
+              >
+                <Heading as="h3"> {recipe.label}</Heading>
+                <Link to={`/recipe`} state={{ name: recipe.value }}>
+                  Recipe GUI for {recipe.value}
+                </Link>
+              </Flex>
+            </li>
+          ))}
+        </ul>
       </Flex>
-    </Fragment>
+    </Flex>
   )
 }
 
