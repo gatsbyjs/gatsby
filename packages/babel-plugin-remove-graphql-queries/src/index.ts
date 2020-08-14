c931045fc846d9c34ef099b57e79e2df1db29874
Zach Taylor <zachtylr04@gmail.com>
2020-08-13 19:42:00 -0500

    Change the way graphql is imported
    Co-authored-by: Vladimir Razuvaev <vladimir.razuvaev@gmail.com>

--- packages/babel-plugin-remove-graphql-queries/src/index.ts
+++ packages/babel-plugin-remove-graphql-queries/src/index.ts
@@ -1,5 +1,5 @@
 /*  eslint-disable new-cap */
-import { graphql } from "gatsby"
+import graphql from "gatsby/graphql"
 import { murmurhash } from "./murmur"
 import nodePath from "path"
 import { NodePath, PluginObj } from "@babel/core"
