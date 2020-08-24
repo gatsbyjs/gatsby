c8e8e32b3ee944dfab6fd33cd4de3a24764e3c1c
Francesc Busquets <francesc@gmail.com>
2020-08-24 08:45:32 +0200

    fix(gatsby-remark-images): Use pathPrefix also with WebP files (#26472)

--- packages/gatsby-remark-images/src/index.js
+++ packages/gatsby-remark-images/src/index.js
@@ -218,8 +218,7 @@ module.exports = (
           { toFormat: `WEBP` },
           // override options if it's an object, otherwise just pass through defaults
           options.withWebp === true ? {} : options.withWebp,
-          pluginOptions,
-          DEFAULT_OPTIONS
+          options
         ),
         reporter,
       })
