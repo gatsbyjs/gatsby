70c2e83680a1ee58405e4cf8c53bb21743d6b9eb
Michal Piechowiak <misiek.piechowiak@gmail.com>
2020-08-26 23:30:51 +0200

    add validation

--- packages/gatsby/src/utils/generate-module-id.ts
+++ packages/gatsby/src/utils/generate-module-id.ts
@@ -1,8 +1,30 @@
+import { isAbsolute } from "path"
 import { generateComponentChunkName } from "./js-chunk-names"
+
+const allowedTypes = [`default`, `named`, `namespace`]
 
 export const generateModuleId = ({
   source,
   type = `default`,
   importName,
-}): string =>
-  `${generateComponentChunkName(source, `module`)}-${type}-${importName || ``}`
+}): string => {
+  if (!allowedTypes.includes(type)) {
+    throw new Error(
+      `Type "${type}" not allowed (allowed types: ${allowedTypes
+        .map(allowedType => `"${allowedType}"`)
+        .join(`, `)}).`
+    )
+  }
+
+  if (type === `named` && !importName) {
+    throw new Error(`Named imports need "importName" argument.`)
+  }
+
+  if (!isAbsolute(source)) {
+    throw new Error(`"source" need to be absolute path.`)
+  }
+
+  return `${generateComponentChunkName(source, `module`)}-${type}-${
+    importName || ``
+  }`
+}
