ad753fdb8ecb84d2d99e1bc1ce972971604b9157
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2020-08-27 17:57:16 +0530

    Fix lint

--- packages/gatsby/src/utils/generic-on-error.ts
+++ packages/gatsby/src/utils/generic-on-error.ts
@@ -1,7 +1,7 @@
 import reporter from "gatsby-cli/lib/reporter"
 
 export const genericOnError = {
-  actions: (ctx, event): void => {
+  actions: (_ctx, event): void => {
     const error = event.data
     reporter.panicOnBuild(error)
   },
