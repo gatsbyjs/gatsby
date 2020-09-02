952ed0e0c2fa0050dbc4e6621c675fbecb911f13
Peter van der Zee <209817+pvdz@users.noreply.github.com>
2020-09-02 17:07:43 +0200

    fix(benchmarks): remove helmet stuff from gabe-markdown (#26764)

--- benchmarks/gabe-markdown/src/pages/404.js
+++ benchmarks/gabe-markdown/src/pages/404.js
@@ -2,7 +2,6 @@ import React from "react"
 import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
 
 class NotFoundPage extends React.Component {
   render() {
@@ -11,7 +10,6 @@ class NotFoundPage extends React.Component {
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="404: Not Found" />
         <h1>Not Found</h1>
         <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
       </Layout>
--- benchmarks/gabe-markdown/src/pages/index.js
+++ benchmarks/gabe-markdown/src/pages/index.js
@@ -3,7 +3,6 @@ import { Link, graphql } from "gatsby"
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
-import SEO from "../components/seo"
 
 class BlogIndex extends React.Component {
   render() {
@@ -13,7 +12,6 @@ class BlogIndex extends React.Component {
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="All posts" />
         <Bio />
         {posts.map(({ node }) => {
           const title = node.frontmatter.title || node.fields.slug
