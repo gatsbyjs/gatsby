78e2c30db654f95f0b8404e00201018e9b44aaa1
gatsbybot <mathews.kyle+gatsbybot@gmail.com>
2020-09-09 20:02:47 +0000

    chore: format

--- packages/gatsby/src/utils/test-require-error.ts
+++ packages/gatsby/src/utils/test-require-error.ts
@@ -16,6 +16,6 @@ export const testRequireError = (moduleName: string, err: any): boolean => {
       `\\$&`
     )}`
   )
-  const firstLine = err.toString().replace(/\\\\/g, `\\`).split(`\n`)[0];
+  const firstLine = err.toString().replace(/\\\\/g, `\\`).split(`\n`)[0]
   return regex.test(firstLine)
 }
