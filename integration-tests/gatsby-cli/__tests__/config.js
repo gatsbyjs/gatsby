6e2665b93505efa4eb5d9ad1f23c7068e133febb
Laurie Barth <laurie@lauriesrklaptop.lan>
2020-09-11 14:29:25 -0400

    make it a one off test like feedback

--- integration-tests/gatsby-cli/__tests__/config.js
+++ integration-tests/gatsby-cli/__tests__/config.js
@@ -3,11 +3,9 @@ import { GatsbyCLI } from "../test-helpers"
 const MAX_TIMEOUT = 2147483647
 jest.setTimeout(MAX_TIMEOUT)
 
-const cwd = `execution-folder/gatsby-default`
-
 describe(`gatsby config`, () => {
   it(`Prints the config`, () => {
-    const [status, logs] = GatsbyCLI.from(cwd).invoke(`config`)
+    const [status, logs] = GatsbyCLI.from(`gatsby-sites/gatsby-build`).invoke(`config`)
 
     logs.should.contain(`Package Manager`)
     logs.should.contain(`Telemetry enabled`)
