3769f7456209d95480a109129d390b877eb40d87
Michael van Engelshoven <michael@van-engelshoven.de>
2020-09-11 08:11:24 +0200

    add generic type to useScrollRestoration hook
    this is required to match the type of `useRef` which requires the target
    element type to be specified.
    
    The hook can be used like the following:
    ```typescript
    const MyComponent: React.FunctionComponent = () => {
      const scrollRestorationProps = useScrollRestoration<HTMLDivElement>(`some-key`)
      return (
        <div
          {...scrollRestorationProps}
          style={{ overflow: `auto` }}
        >
          Test
        </div>
      )
    }
    ```
    
    Fixes: #26458

--- packages/gatsby-react-router-scroll/src/use-scroll-restoration.ts
+++ packages/gatsby-react-router-scroll/src/use-scroll-restoration.ts
@@ -2,17 +2,17 @@ import { ScrollContext } from "./scroll-handler"
 import { useRef, useContext, useLayoutEffect } from "react"
 import { useLocation } from "@reach/router"
 
-interface IScrollRestorationProps {
-  ref: React.MutableRefObject<HTMLElement | undefined>
+interface IScrollRestorationProps<T extends HTMLElement> {
+  ref: React.MutableRefObject<T | null>
   onScroll(): void
 }
 
-export function useScrollRestoration(
+export function useScrollRestoration<T extends HTMLElement>(
   identifier: string
-): IScrollRestorationProps {
+): IScrollRestorationProps<T> {
   const location = useLocation()
   const state = useContext(ScrollContext)
-  const ref = useRef<HTMLElement>()
+  const ref = useRef<T>(null)
 
   useLayoutEffect((): void => {
     if (ref.current) {
