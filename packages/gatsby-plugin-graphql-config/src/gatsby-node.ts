d55729321be5a9cbc50628a1a54d4fb6b3108fb4
Rikki <rikki.schulte@gmail.com>
2020-09-15 18:38:23 -0400

    consolidate to a single plugin, use store emitter

--- packages/gatsby-plugin-graphql-config/src/gatsby-node.ts
+++ packages/gatsby-plugin-graphql-config/src/gatsby-node.ts
@@ -1,28 +1,42 @@
-import * as fs from "fs"
-import * as path from "path"
+import * as fs from "fs-extra"
+import { resolve, join } from "path"
+import { printSchema, GraphQLSchema } from "graphql"
+import type { GatsbyReduxStore } from "gatsby/src/redux"
+import type { IStateProgram } from "gatsby/src/internal"
 
-export function onPostBootstrap({ store }): void {
-  const {
-    program,
-  }: {
-    program: any
-  } = store.getState()
+async function cacheSchema(
+  cacheDirectory: string,
+  schema: GraphQLSchema
+): Promise<void> {
+  try {
+    const schemaSDLString = printSchema(schema, { commentDescriptions: true })
 
-  const cachePath = program.directory && path.join(program.directory, `.cache`)
+    await fs.writeFile(join(cacheDirectory, `schema.graphql`), schemaSDLString)
 
-  if (cachePath && fs.existsSync(cachePath)) {
+    console.log(`[gatsby-plugin-graphql-config] wrote SDL file to .cache`)
+  } catch (err) {
+    console.error(
+      `[gatsby-plugin-graphql-config] failed writing schema file to .cache`
+    )
+    console.error(err)
+  }
+}
+
+async function cacheGraphQLConfig(program: IStateProgram): Promise<void> {
+  try {
+    const base = program.directory
     const configJSONString = JSON.stringify(
       {
-        schema: `.cache/schema.graphql`,
+        schema: resolve(base, `.cache/schema.graphql`),
         documents: [
-          `src/**/**.{ts,js,tsx,jsx,esm}`,
-          `.cache/fragments.graphql`,
+          resolve(base, `src/**/**.{ts,js,tsx,jsx,esm}`),
+          resolve(base, `.cache/fragments.graphql`),
         ],
         extensions: {
           endpoints: {
             default: {
               url: `${program.https ? `https://` : `http://`}${program.host}:${
-                program.p
+                program.port
               }/___graphql`,
             },
           },
@@ -33,8 +47,65 @@ export function onPostBootstrap({ store }): void {
     )
 
     fs.writeFileSync(
-      path.join(cachePath, `graphql.config.json`),
+      resolve(base, `.cache`, `graphql.config.json`),
       configJSONString
     )
+    console.log(`[gatsby-plugin-graphql-config] wrote config file to .cache`)
+  } catch (err) {
+    console.error(
+      `[gatsby-plugin-graphql-config] failed to write config file to .cache`
+    )
+    console.error(err)
   }
+}
+
+const createFragmentCacheHandler = (
+  cacheDirectory: string,
+  store: GatsbyReduxStore
+) => async (): Promise<void> => {
+  try {
+    const currentDefinitions = store.getState().definitions
+
+    const fragmentString = Array.from(currentDefinitions.entries())
+      .filter(([_, def]) => def.isFragment)
+      .map(([_, def]) => `# ${def.filePath}\n${def.printedAst}`)
+      .join(`\n`)
+
+    await fs.writeFile(
+      join(cacheDirectory, `fragments.graphql`),
+      fragmentString
+    )
+
+    console.log(`[gatsby-plugin-graphql-config] wrote fragments file to .cache`)
+  } catch (err) {
+    console.error(
+      `[gatsby-plugin-graphql-config] failed writing fragments file to .cache`
+    )
+    console.error(err)
+  }
+}
+
+export async function onPostBootstrap({
+  store,
+  emitter,
+}: {
+  store: GatsbyReduxStore
+  emitter: any
+}): Promise<void> {
+  const { schema, program } = store.getState()
+
+  const cacheDirectory = resolve(program.directory, `.cache`)
+
+  if (!fs.existsSync(cacheDirectory)) {
+    return
+  }
+
+  await cacheSchema(cacheDirectory, schema)
+
+  await cacheGraphQLConfig(program)
+
+  emitter.on(
+    `SET_DEFINITIONS`,
+    createFragmentCacheHandler(cacheDirectory, store)
+  )
 }
