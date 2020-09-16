ee5cb0b3944d4fa5ce53e47ff2878fd495756efd
Lennart <lekoarts@gmail.com>
2020-09-16 17:00:46 +0200

    Update starters/blog/src/components/bio.js
    Co-authored-by: Matt Kane <matt@gatsbyjs.com>

--- starters/blog/src/components/bio.js
+++ starters/blog/src/components/bio.js
@@ -68,7 +68,7 @@ const Bio = () => {
           Written by <strong>{author.name}</strong> {author?.summary || null}
           {` `}
           <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            You should follow him on Twitter
+            You should follow them on Twitter
           </a>
         </p>
       )}
