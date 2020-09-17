39831288e31ac12238b4500cd52ba601979b7a43
LekoArts <lekoarts@gmail.com>
2020-09-17 09:24:09 +0200

    add safe-guards to markdown files

--- starters/blog/gatsby-node.js
+++ starters/blog/gatsby-node.js
@@ -36,21 +36,25 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
   const posts = result.data.allMarkdownRemark.nodes
 
   // Create blog posts pages
+  // But only if there's at least one markdown file found at "content/blog" (defined in gatsby-config.js)
   // `context` is available in the template as a prop and as a variable in GraphQL
-  posts.forEach((post, index) => {
-    const previous = index === posts.length - 1 ? null : posts[index + 1]
-    const next = index === 0 ? null : posts[index - 1]
 
-    createPage({
-      path: post.fields.slug,
-      component: blogPost,
-      context: {
-        slug: post.fields.slug,
-        previous,
-        next,
-      },
+  if (posts.length > 0) {
+    posts.forEach((post, index) => {
+      const previous = index === posts.length - 1 ? null : posts[index + 1]
+      const next = index === 0 ? null : posts[index - 1]
+
+      createPage({
+        path: post.fields.slug,
+        component: blogPost,
+        context: {
+          slug: post.fields.slug,
+          previous,
+          next,
+        },
+      })
     })
-  })
+  }
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
@@ -72,6 +76,10 @@ exports.createSchemaCustomization = ({ actions }) => {
 
   // Explicitly define the siteMetadata {} object
   // This way those will always be defined even if removed from gatsby-config.js
+
+  // Also explicitly define the Markdown frontmatter
+  // This way the "MarkdownRemark" queries will return `null` even when no
+  // blog posts are stored inside "content/blog" instead of returning an error
   createTypes(`
     type SiteSiteMetadata {
       author: Author
@@ -86,6 +94,21 @@ exports.createSchemaCustomization = ({ actions }) => {
 
     type Social {
       twitter: String
+    }
+
+    type MarkdownRemark implements Node {
+      frontmatter: Frontmatter
+      fields: Fields
+    }
+
+    type Frontmatter {
+      title: String
+      description: String
+      date: Date @dateformat
+    }
+
+    type Fields {
+      slug: String
     }
   `)
 }
--- starters/blog/src/pages/index.js
+++ starters/blog/src/pages/index.js
@@ -10,6 +10,16 @@ const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
   const posts = data.allMarkdownRemark.nodes
 
+  if (posts.length === 0) {
+    return (
+      <Layout location={location} title={siteTitle}>
+        <SEO title="All posts" />
+        <Bio />
+        <p>No blog posts found. Add markdown posts to "content/blog" (or the directory you specified for the "gatsby-source-filesystem" plugin in gatsby-config.js).</p>
+      </Layout>
+    )
+  }
+
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="All posts" />
