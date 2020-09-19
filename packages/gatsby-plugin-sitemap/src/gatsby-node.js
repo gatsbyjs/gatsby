58bd08300fa4d73961137fd5e0faa915d7e9f201
Alex Moon <moonmeister@users.noreply.github.com>
2020-09-18 18:54:12 -0700

    Update packages/gatsby-plugin-sitemap/src/gatsby-node.js
    Co-authored-by: Ward Peeters <ward@coding-tech.com>

--- packages/gatsby-plugin-sitemap/src/gatsby-node.js
+++ packages/gatsby-plugin-sitemap/src/gatsby-node.js
@@ -46,7 +46,7 @@ exports.onPostBuild = async (
   const { data: queryRecords } = await graphql(query)
 
   reporter.verbose(
-    `${reporterPrefix} Query Results` + JSON.stringify(queryRecords, null, 2)
+    `${reporterPrefix} Query Results:\n${JSON.stringify(queryRecords, null, 2)}`
   )
 
   const allPages = resolvePages(queryRecords)
