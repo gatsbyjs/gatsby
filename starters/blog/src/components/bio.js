2e9a38b0c76c5b96a00656ca1d74011180fc1dbf
Lennart <lekoarts@gmail.com>
2020-09-23 15:01:50 +0200

    feat(gatsby-starter-blog): Remove typography.js (#27001)
    Co-authored-by: gatsbybot <mathews.kyle+gatsbybot@gmail.com>

--- starters/blog/src/components/bio.js
+++ starters/blog/src/components/bio.js
@@ -9,14 +9,12 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Image from "gatsby-image"
 
-import { rhythm } from "../utils/typography"
-
 const Bio = () => {
   const data = useStaticQuery(graphql`
     query BioQuery {
       avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
         childImageSharp {
-          fixed(width: 50, height: 50) {
+          fixed(width: 50, height: 50, quality: 95) {
             ...GatsbyImageSharpFixed
           }
         }
@@ -42,22 +40,12 @@ const Bio = () => {
   const avatar = data?.avatar?.childImageSharp?.fixed
 
   return (
-    <div
-      style={{
-        display: `flex`,
-        marginBottom: rhythm(2.5),
-      }}
-    >
+    <div className="bio">
       {avatar && (
         <Image
           fixed={avatar}
           alt={author?.name || ``}
-          style={{
-            marginRight: rhythm(1 / 2),
-            marginBottom: 0,
-            minWidth: 50,
-            borderRadius: `100%`,
-          }}
+          className="bio-avatar"
           imgStyle={{
             borderRadius: `50%`,
           }}
