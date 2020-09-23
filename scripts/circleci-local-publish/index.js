5b5e9241f1662a2a5b83b1969dffe6e403f322a8
Ward Peeters <ward@coding-tech.com>
2020-09-23 08:55:32 +0200

    update script

--- scripts/circleci-local-publish/index.js
+++ scripts/circleci-local-publish/index.js
@@ -2,7 +2,34 @@ const http = require(`http`)
 const { parse: parseUrl } = require(`url`)
 const { spawn, spawnSync } = require(`child_process`)
 
-const [, , preid] = process.argv
+const [, , cmd, preid] = process.argv
+
+// verdaccio is not easily killable on windows
+function killVerdaccio(proc) {
+  proc.kill(`SIGINT`)
+}
+
+function promiseSpawn(shell, options) {
+  const [command, ...args] = shell.split(` `)
+  const proc = spawn(command, args, options)
+
+  return {
+    proc,
+    promise: new Promise(resolve => {
+      proc.on(`error`, err => {
+        console.log(err)
+        resolve(-1)
+      })
+
+      proc.on(`exit`, exitCode => {
+        resolve(exitCode)
+      })
+      proc.on(`close`, exitCode => {
+        resolve(exitCode)
+      })
+    }),
+  }
+}
 
 function waitFor(url, interval = 1000) {
   function exec(url, onDone) {
@@ -34,68 +61,95 @@ function waitFor(url, interval = 1000) {
   })
 }
 
-const proc = spawn(`verdaccio`, [`--config`, `config.yml`], {
-  shell: true,
-  cwd: __dirname,
+process.on(`exit`, code => {
+  console.log(`Process exit event with code: `, code)
 })
+;(async () => {
+  const REGISTRY_URL = `http://127.0.0.1:4873`
 
-proc.stdout.on(`data`, msg => {
-  console.log(msg.toString())
-})
+  let verdaccioProc
+  let verdaccioPromise
 
-proc.stderr.on(`data`, msg => {
-  console.log(msg.toString())
-  process.exit(-1)
-})
+  try {
+    const verdaccio = promiseSpawn(`verdaccio --config config.yml`, {
+      shell: true,
+      cwd: __dirname,
+    })
 
-waitFor(`http://127.0.0.1:4873`, 1000)
-  .then(
-    () =>
-      new Promise(resolve => {
-        const lerna = spawn(
-          `yarn`,
-          [
-            `lerna`,
-            `publish`,
-            `--registry`,
-            `http://127.0.0.1:4873`,
-            `--canary`,
-            `--preid`,
-            preid,
-            `--dist-tag`,
-            preid,
-            `--force-publish`,
-            `--ignore-scripts`,
-            `--yes`,
-          ],
-          {
-            shell: true,
-          }
-        )
+    verdaccioProc = verdaccio.proc
+    verdaccioPromise = verdaccio.promise
 
-        lerna.stdout.on(`data`, msg => {
-          console.log(msg.toString())
+    verdaccioProc.stdout.on(`data`, msg => {
+      console.log(msg.toString())
+    })
+
+    verdaccioProc.stderr.on(`data`, msg => {
+      console.log(msg.toString())
+    })
+
+    verdaccioPromise.then(exitCode => {
+      if (exitCode === -1) {
+        killVerdaccio(verdaccioProc)
+
+        return new Promise(() => {
+          setTimeout(() => {
+            process.exit(-1)
+          }, 1000)
         })
+      }
 
-        lerna.stderr.on(`data`, msg => {
-          console.log(msg.toString())
-          resolve(-1)
-        })
+      return Promise.resolve()
+    })
 
-        lerna.on(`exit`, exitCode => {
-          resolve(exitCode)
-        })
+    await waitFor(REGISTRY_URL, 1000)
+
+    if (cmd === `publish`) {
+      const { proc: lernaProc, promise: lernaPromise } = promiseSpawn(
+        `yarn lerna publish --registry ${REGISTRY_URL} --canary --preid ${preid} --dist-tag ${preid} --force-publish --ignore-scripts --yes`,
+        {
+          shell: true,
+        }
+      )
+
+      lernaProc.stdout.on(`data`, msg => {
+        console.log(msg.toString())
       })
-  )
-  .then(exitCode => {
-    proc.kill(`SIGINT`)
 
-    if (exitCode === 0) {
-      process.exit(0)
-      return
+      lernaProc.stderr.on(`data`, msg => {
+        console.log(msg.toString())
+      })
+
+      await lernaPromise
+
+      spawnSync(`git`, [`checkout`, `../..`])
     }
 
-    spawnSync(`git`, [`checkout`, `../..`])
+    if (cmd === `install`) {
+      const { proc: installProc, promise: installPromise } = promiseSpawn(
+        `${
+          process.execArgv.includes(`--pm npm`) ? `npm` : `yarn`
+        } --registry ${REGISTRY_URL}`,
+        {
+          shell: true,
+        }
+      )
 
-    process.exit(0)
-  })
+      installProc.stdout.on(`data`, msg => {
+        console.log(msg.toString())
+      })
+
+      installProc.stderr.on(`data`, msg => {
+        console.log(msg.toString())
+      })
+
+      await installPromise
+      console.log(`installation done`)
+    }
+  } catch (err) {
+    process.exitCode = -1
+  } finally {
+    killVerdaccio(verdaccioProc)
+  }
+
+  process.exit(process.exitCode || 0)
+})()
