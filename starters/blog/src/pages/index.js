077477c3050f57e6ccc7b8cccd7571c7b75326ee
Lennart <lekoarts@gmail.com>
2020-09-28 11:23:45 +0200

    revert import change

--- starters/blog/src/pages/index.js
+++ starters/blog/src/pages/index.js
@@ -1,5 +1,5 @@
-import { graphql, Link } from "gatsby"
 import React from "react"
+import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
