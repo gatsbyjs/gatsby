ebab5068e73769e729e82b5a983e7f4c4ffb2dd0
Max Stoiber <contact@mxstbr.com>
2020-09-29 10:30:28 +0200

    Add ability to skip async validation rules

--- packages/gatsby-plugin-utils/src/__tests__/index.ts
+++ packages/gatsby-plugin-utils/src/__tests__/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import Joi from "joi"
 import { validateOptionsSchema } from "../"
 
@@ -9,13 +10,42 @@ it(`validates a basic schema`, async () => {
   const validOptions = {
     str: `is a string`,
   }
-  expect(validateOptionsSchema(pluginSchema, validOptions)).toEqual(
+  expect(await validateOptionsSchema(pluginSchema, validOptions)).toEqual(
     validOptions
   )
 
-  expect(() =>
+  const invalid = () =>
     validateOptionsSchema(pluginSchema, {
       str: 43,
     })
-  ).toThrowErrorMatchingInlineSnapshot(`"\\"str\\" must be a string"`)
+
+  expect(invalid()).rejects.toThrowErrorMatchingInlineSnapshot(
+    `"\\"str\\" must be a string"`
+  )
+})
+
+it(`asynchronously validates the external validation rules`, () => {
+  const failingAsyncValidationRule = async () => {
+    throw new Error(`This failed for some unknown reason.`)
+  }
+
+  const schema = Joi.object({}).external(failingAsyncValidationRule)
+
+  const invalid = () => validateOptionsSchema(schema, {})
+
+  expect(invalid()).rejects.toThrowErrorMatchingInlineSnapshot(
+    `"This failed for some unknown reason. (value)"`
+  )
+})
+
+it(`does not validate async external validation rules`, async () => {
+  const failingAsyncValidationRule = async () => {
+    throw new Error(`This failed for some unknown reason.`)
+  }
+
+  const schema = Joi.object({}).external(failingAsyncValidationRule)
+
+  const invalid = () => validateOptionsSchema(schema, {}, false)
+
+  expect(await invalid()).toMatchInlineSnapshot(`Object {}`)
 })
--- packages/gatsby-plugin-utils/src/validate.ts
+++ packages/gatsby-plugin-utils/src/validate.ts
@@ -1,12 +1,27 @@
-import { Schema } from "joi"
+import { BaseValidationOptions, Schema } from "joi"
 
-export function validateOptionsSchema<Options = object>(
+const validationOptions: BaseValidationOptions = {
+  // Show all errors at once, rather than only the first one every time
+  abortEarly: false,
+  cache: true,
+  // Strip unknown values, this makes sure plugin authors define an extensive schema
+  // TODO: Discuss whether we want this, maybe not a great developer experience...
+  stripUnknown: true,
+}
+
+export async function validateOptionsSchema<Options = object>(
   pluginSchema: Schema,
-  pluginOptions: Options
-): Options {
-  const result = pluginSchema.validate(pluginOptions)
-  // TODO: Better error system
-  if (result.error) throw result.error
+  pluginOptions: Options,
+  externalRules: boolean = true
+): Promise<Options> {
+  if (!externalRules) {
+    const result = pluginSchema.validate(pluginOptions, {
+      ...validationOptions,
+      externals: false,
+    })
+    if (result.error) throw result.error
+    return result.value
+  }
 
-  return result.value
+  return await pluginSchema.validateAsync(pluginOptions, validationOptions)
 }
