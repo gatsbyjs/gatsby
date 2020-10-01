821c9ec74c20cbe5ae1e59f9b4aa2c52f238a80b
Ward Peeters <ward@coding-tech.com>
2020-10-01 11:54:10 +0200

    update cypress test

--- e2e-tests/development-runtime/cypress/integration/hot-reloading/hooks.js
+++ e2e-tests/development-runtime/cypress/integration/hot-reloading/hooks.js
@@ -1,15 +1,37 @@
 const COUNT_ID = `count`
 
 describe(`hot-reloading hooks`, () => {
+  const amount = 100
+
   beforeEach(() => {
     cy.visit(`/hooks`).waitForRouteChange()
   })
 
+  afterEach(() => {
+    cy.exec(
+      `npm run update -- --file src/pages/hooks.js --replacements "count + ${amount}:count + 1" --exact`
+    )
+  })
+
   it(`can update component`, () => {
-    const amount = 100
+    const deferred = {}
+    deferred.promise = new Promise((resolve, reject) => {
+      deferred.resolve = resolve
+      deferred.reject = reject
+    })
+
+    cy.on("log:changed", e => {
+      if (e.name === "xhr" && e.consoleProps.URL.includes("hot-update")) {
+        deferred.resolve()
+      }
+    })
+
     cy.exec(
       `npm run update -- --file src/pages/hooks.js --replacements "count + 1:count + ${amount}" --exact`
     )
+
+    cy.log(deferred.promise)
+    cy.wrap(null).then(() => deferred.promise)
 
     cy.getTestElement(`increment`).click()
 
