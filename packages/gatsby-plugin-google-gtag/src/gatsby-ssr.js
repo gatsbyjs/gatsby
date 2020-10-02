7e4a81a5737501b0526023279efa5d0ee5b576e1
gemanepa <contact@gemanepa.com>
2020-10-01 22:52:58 -0300

    fix(gatsby-plugin-google-gtag): Separate preconnect and dns-prefetch fallback to prevent Safari cancelling preconnect

--- packages/gatsby-plugin-google-gtag/src/gatsby-ssr.js
+++ packages/gatsby-plugin-google-gtag/src/gatsby-ssr.js
@@ -11,8 +11,13 @@ exports.onRenderBody = (
   // Lighthouse recommends pre-connecting to google analytics
   setHeadComponents([
     <link
-      rel="preconnect dns-prefetch"
+      rel="preconnect"
       key="preconnect-google-analytics"
+      href="https://www.google-analytics.com"
+    />,
+    <link
+      rel="dns-prefetch"
+      key="dns-prefetch-google-analytics"
       href="https://www.google-analytics.com"
     />,
   ])
