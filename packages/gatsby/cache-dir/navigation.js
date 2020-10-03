b27c1c7ade13acfdd0e8d95c805fa96af65fc275
Will Mayger <will.mayger@theweblancer.com>
2020-10-03 14:29:49 +0100

    fixing ordering issue by using shouldComponentUpdate

--- packages/gatsby/cache-dir/navigation.js
+++ packages/gatsby/cache-dir/navigation.js
@@ -207,19 +207,19 @@ class RouteUpdates extends React.Component {
     onRouteUpdate(this.props.location, null)
   }
 
-  componentDidUpdate(prevProps, prevState, shouldFireRouteUpdate) {
-    if (shouldFireRouteUpdate) {
-      onRouteUpdate(this.props.location, prevProps.location)
-    }
-  }
-
-  getSnapshotBeforeUpdate(prevProps) {
+  shouldComponentUpdate(prevProps) {
     if (this.props.location.pathname !== prevProps.location.pathname) {
       onPreRouteUpdate(this.props.location, prevProps.location)
       return true
     }
 
     return false
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.location.pathname !== prevProps.location.pathname) {
+      onRouteUpdate(this.props.location, prevProps.location)
+    }
   }
 
   render() {
