f6e1156d137bfee2a1aa9e790299abc9c1052e66
Matt Kane <matt@gatsbyjs.com>
2020-10-06 14:36:54 +0100

    Remove hook import

--- packages/gatsby-plugin-image/src/index.browser.ts
+++ packages/gatsby-plugin-image/src/index.browser.ts
@@ -2,4 +2,3 @@ export { GatsbyImage } from "./components/gatsby-image.browser"
 export { Placeholder } from "./components/placeholder"
 export { MainImage } from "./components/main-image"
 export { StaticImage } from "./components/static-image"
-// export { useGatsbyImage } from './hooks';
