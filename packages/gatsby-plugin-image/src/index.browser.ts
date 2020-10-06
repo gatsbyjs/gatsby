e58139662c8009a5b3f3f31e3d43e8d3d4895a13
Matt Kane <matt@gatsbyjs.com>
2020-10-06 14:36:23 +0100

    Export correctly from browser entry point

--- packages/gatsby-plugin-image/src/index.browser.ts
+++ packages/gatsby-plugin-image/src/index.browser.ts
@@ -1,4 +1,5 @@
 export { GatsbyImage } from "./components/gatsby-image.browser"
 export { Placeholder } from "./components/placeholder"
 export { MainImage } from "./components/main-image"
+export { StaticImage } from "./components/static-image"
 // export { useGatsbyImage } from './hooks';
