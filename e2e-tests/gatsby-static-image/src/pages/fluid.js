c97041c30aeaf53d3f30776d9c9f467b20ff9231
Laurie <laurie@gatsbyjs.com>
2020-10-07 17:00:25 -0400

    remove unused imports, use valid fluid setting

--- e2e-tests/gatsby-static-image/src/pages/fluid.js
+++ e2e-tests/gatsby-static-image/src/pages/fluid.js
@@ -1,5 +1,4 @@
 import * as React from "react"
-import { graphql } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
@@ -7,16 +6,16 @@ import Layout from "../components/layout"
 const FluidPage = () => (
   <Layout>
     <span data-testid="image-fluid">
-    <StaticImage src="../images/citrus-fruits.jpg" fluid alt="Citrus fruits" />
+    <StaticImage src="../images/citrus-fruits.jpg" fluid="true" alt="Citrus fruits" />
     </span>
     <span data-testid="image-fluid-png">
-    <StaticImage src="../images/gatsby-icon.png" fluid alt="Gatsby icon" />
+    <StaticImage src="../images/gatsby-icon.png" fluid="true" alt="Gatsby icon" />
     </span>
     <span data-testid="image-fluid-relative">
-    <StaticImage src="../../content/relative.jpg" fluid alt="Citrus fruits" />
+    <StaticImage src="../../content/relative.jpg" fluid="true" alt="Citrus fruits" />
     </span>
     <span data-testid="invalid-image">
-    <StaticImage src="./does-not-exist.jpg" fluid />
+    <StaticImage src="./does-not-exist.jpg" fluid="true" />
     </span>
 
   </Layout>
--- e2e-tests/gatsby-static-image/src/pages/traced.js
+++ e2e-tests/gatsby-static-image/src/pages/traced.js
@@ -1,5 +1,4 @@
 import * as React from "react"
-import { graphql } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
