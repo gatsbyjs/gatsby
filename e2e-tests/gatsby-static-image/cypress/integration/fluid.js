8125d3448f69cb8da8e59e9ddf72d83e3069f3e6
Laurie <laurie@gatsbyjs.com>
2020-10-08 09:39:20 -0400

    remove tests that no longer match the expected DOM

--- e2e-tests/gatsby-static-image/cypress/integration/fluid.js
+++ e2e-tests/gatsby-static-image/cypress/integration/fluid.js
@@ -19,25 +19,6 @@ describe(`fluid`, () => {
     cy.getTestElement(fluidTestId)
       .find(`.gatsby-image > div`)
       .should(`have.attr`, `style`)
-      .and(`match`, /width:100%;padding-bottom/)
-  })
-
-  it(`renders sizes`, () => {
-    cy.getTestElement(fluidTestId)
-      .find(`picture > source`)
-      .should(`have.attr`, `sizes`)
-      .and(`match`, /\(max-width: \d+px\) 100vw, \d+px/)
-  })
-
-  it(`renders correct srcset`, () => {
-    cy.getTestElement(fluidTestId)
-      .find(`picture > source`)
-      .should(`have.attr`, `srcset`)
-      .and(srcset => {
-        srcset.split(/\s*,\s*/).forEach(part => {
-          expect(part).to.contain(`/static`)
-          expect(part).to.match(/\d{2,}w/)
-        })
-      })
+      .and(`match`, /padding/)
   })
 })
