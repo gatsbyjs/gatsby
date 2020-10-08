b64f89e4fc22fdfbe8c7edf670a4b25d757db7a3
Will Mayger <will@mayger.dev>
2020-10-08 09:52:13 +0100

    fix(gatsby): route lifecycle ordering (#27261)
    * fixing ordering issue by using shouldComponentUpdate
    
    * creating cypress command to check order of actions called
    
    * enabling second page to be able to log lifecycle actions
    
    * writing e2e test to ensure the issue has been fixed and will not return
    
    * removing un-needed findIndex

--- e2e-tests/development-runtime/cypress/support/commands.js
+++ e2e-tests/development-runtime/cypress/support/commands.js
@@ -11,6 +11,39 @@ Cypress.Commands.add(`lifecycleCallCount`, action =>
     )
 )
 
+// This command is to find out that the lifecycle methods have
+// been called in the expected order, not to check the exact order.
+// It checks an array and makes sure that each item in the array is called at some point after the last.
+// It will return a boolean value if the order has been called correctly according to what has been passed in.
+Cypress.Commands.add(`lifecycleCallOrder`, expectedActionCallOrder =>
+  cy.window().then(win => {
+    const actions = win.___PageComponentLifecycleCallsLog
+    const expectedActionCallOrderLength = expectedActionCallOrder.length
+    const actionsLength = actions.length
+
+    if (expectedActionCallOrderLength > actionsLength) {
+      return false
+    }
+    
+    let prevActionIndex = -1
+    for (let i = 0; i < actionsLength; i += 1) {
+      const nextActionIndex = prevActionIndex + 1
+
+      // if the next action is found in the correct order
+      if (actions[i].action === expectedActionCallOrder[nextActionIndex]) {
+        prevActionIndex = nextActionIndex
+      }
+    }
+
+    // if not all actions have been found then it has failed
+    if (prevActionIndex !== expectedActionCallOrderLength - 1) {
+      return false
+    }
+
+    return true
+  })
+)
+
 Cypress.Commands.add(`assertRouterWrapperFocus`, (shouldBeFocused = true) =>
   cy
     .focused()
