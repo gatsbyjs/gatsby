eb1bb982fdc53f995201ea0345b5ee080cc0196d
mfrachet <marvin@gatsbyjs.com>
2020-10-13 09:51:25 +0200

    Removing comments

--- packages/gatsby-plugin-netlify/src/__tests__/gatsby-node.js
+++ packages/gatsby-plugin-netlify/src/__tests__/gatsby-node.js
@@ -1,16 +1,6 @@
 import { testPluginOptionsSchema } from "gatsby-plugin-utils"
 import { pluginOptionsSchema } from "../gatsby-node"
 
-// options: {
-//   headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
-//   allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
-//   mergeSecurityHeaders: true, // boolean to turn off the default security headers
-//   mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
-//   mergeCachingHeaders: true, // boolean to turn off the default caching headers
-//   transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
-//   generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
-// },
-
 describe(`gatsby-node.js`, () => {
   it(`should provide meaningful errors when fields are invalid`, () => {
     const expectedErrors = [
