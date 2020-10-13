bb967fbae7648c6ed01dff489a67c70aa990c968
Laurie <laurie@gatsbyjs.com>
2020-10-13 09:00:39 -0400

    listen to experimental flag

--- packages/gatsby-plugin-typescript/src/gatsby-node.js
+++ packages/gatsby-plugin-typescript/src/gatsby-node.js
@@ -35,16 +35,20 @@ function onCreateWebpackConfig({ actions, loaders }) {
   })
 }
 
-exports.pluginOptionsSchema = ({ Joi }) =>
-  Joi.object({
-    isTSX: Joi.boolean().description(`Enables jsx parsing.`).default(true),
-    jsxPragma: Joi.string()
-      .description(`Replace the function used when compiling JSX expressions.`)
-      .default(`React`),
-    allExtensions: Joi.boolean()
-      .description(`Indicates that every file should be parsed as TS or TSX.`)
-      .default(false),
-  })
+if (process.env.GATSBY_EXPERIMENTAL_PLUGIN_OPTION_VALIDATION) {
+  exports.pluginOptionsSchema = ({ Joi }) =>
+    Joi.object({
+      isTSX: Joi.boolean().description(`Enables jsx parsing.`).default(true),
+      jsxPragma: Joi.string()
+        .description(
+          `Replace the function used when compiling JSX expressions.`
+        )
+        .default(`React`),
+      allExtensions: Joi.boolean()
+        .description(`Indicates that every file should be parsed as TS or TSX.`)
+        .default(false),
+    })
+}
 
 exports.resolvableExtensions = resolvableExtensions
 exports.onCreateBabelConfig = onCreateBabelConfig
