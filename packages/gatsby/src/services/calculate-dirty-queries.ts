3dadbdde10d80b2d2f7b0394134cf10673189afc
Vladimir Razuvaev <vladimir.razuvaev@gmail.com>
2020-10-16 05:17:07 +0700

    Cleanup

--- packages/gatsby/src/services/calculate-dirty-queries.ts
+++ packages/gatsby/src/services/calculate-dirty-queries.ts
@@ -1,33 +1,16 @@
-import {
-  calcInitialDirtyQueryIds,
-  calcDirtyQueryIds,
-  groupQueryIds,
-} from "../query"
+import { calcDirtyQueryIds, groupQueryIds } from "../query"
 import { IGroupedQueryIds } from "./"
 import { IQueryRunningContext } from "../state-machines/query-running/types"
 import { assertStore } from "../utils/assert-store"
 
 export async function calculateDirtyQueries({
   store,
-  firstRun,
 }: Partial<IQueryRunningContext>): Promise<{
   queryIds: IGroupedQueryIds
 }> {
   assertStore(store)
-
   const state = store.getState()
-
-  const newDirtyQueryIds: Array<string> = []
-  state.queries.trackedQueries.forEach((query, queryId) => {
-    if (query.dirty !== 0) {
-      newDirtyQueryIds.push(queryId)
-    }
-  })
-
-  console.log(`Dirty queries:`, newDirtyQueryIds)
-
-  const queryIds = firstRun
-    ? calcInitialDirtyQueryIds(state)
-    : calcDirtyQueryIds(state)
+  const queryIds = calcDirtyQueryIds(state)
+  console.log(`Dirty queries: `, queryIds)
   return { queryIds: groupQueryIds(queryIds) }
 }
