0f3a5b62fe1c37228e906178c5981e3c032f1523
Vladimir Razuvaev <vladimir.razuvaev@gmail.com>
2020-10-16 04:59:34 +0700

    remove page-component machine (as we track query state in `queries` reducer now)

---
+++ packages/gatsby/src/redux/reducers/__tests__/queries.ts
@@ -0,0 +1,7 @@
+describe(`bootstrap`, () => {
+  it.todo(`handles not running queries during bootstrap`)
+  it.todo(`won't run queries if the page component has a JS error`)
+  it.todo(`won't queue extra query if new page is created in bootstrap`)
+  it.todo(`will queue query if new page is created after bootstrap`)
+  it.todo(`will queue query when page context is changed`)
+})
