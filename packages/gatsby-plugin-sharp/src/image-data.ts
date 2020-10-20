050450db0dbe6ffc26bfa21beeb0fc7a07f245d6
Matt Kane <matt@gatsbyjs.com>
2020-10-20 17:05:03 +0100

    Use new resizing functions

--- packages/gatsby-plugin-sharp/src/image-data.ts
+++ packages/gatsby-plugin-sharp/src/image-data.ts
@@ -1,54 +1,161 @@
 /* eslint-disable no-unused-expressions */
 import { ISharpGatsbyImageData } from "gatsby-plugin-image"
-import { GatsbyCache } from "gatsby"
+import { GatsbyCache, Node } from "gatsby"
 import { Reporter } from "gatsby-cli/lib/reporter/reporter"
-import { fixed, fluid, traceSVG } from "."
-
+import { rgbToHex, calculateImageSizes, getSrcSet, getSizes } from "./utils"
+import { traceSVG, getImageSizeAsync, base64, batchQueueImageResizing } from "."
+import type { SharpInstance } from "sharp"
+import sharp from "./safe-sharp"
+import { createTransformObject } from "./plugin-options"
 export interface ISharpGatsbyImageArgs {
   layout?: "fixed" | "fluid" | "constrained"
   placeholder?: "tracedSVG" | "dominantColor" | "blurred" | "none"
   tracedSVGOptions?: Record<string, unknown>
+  width?: number
+  height?: number
+  maxWidth?: number
+  maxHeight?: number
+  fit?: "contain" | "cover" | "fill" | "inside" | "outside"
   [key: string]: unknown
+}
+export type FileNode = Node & {
+  absolutePath?: string
+}
+
+export interface IImageMetadata {
+  width: number
+  height: number
+  format: string
+  density?: string
+  dominantColor?: string
+}
+
+const metadataCache = new Map<string, IImageMetadata>()
+
+export async function getImageMetadata(
+  file: FileNode,
+  getDominantColor?: boolean
+): Promise<IImageMetadata> {
+  if (!getDominantColor) {
+    // If we don't need the dominant color we can use the cheaper size function
+    const { width, height, type } = await getImageSizeAsync(file)
+    return { width, height, format: type }
+  }
+  let metadata = metadataCache.get(file.internal.contentDigest)
+  if (metadata && process.env.NODE_ENV !== `test`) {
+    return metadata
+  }
+  const pipeline: SharpInstance = sharp(file.absolutePath)
+
+  const { width, height, density, format } = await pipeline.metadata()
+
+  const { dominant } = await pipeline.stats()
+  // Fallback in case sharp doesn't support dominant
+  const dominantColor = dominant
+    ? rgbToHex(dominant.r, dominant.g, dominant.b)
+    : `#000000`
+
+  metadata = { width, height, density, format, dominantColor }
+  metadataCache.set(file.internal.contentDigest, metadata)
+  return metadata
+}
+
+export interface IImageDataProps {
+  file: FileNode
+  args: ISharpGatsbyImageArgs
+  cache: GatsbyCache
+  reporter: Reporter
+}
+
+export interface IImageDataArgs {
+  file: FileNode
+  args: ISharpGatsbyImageArgs
+  cache: GatsbyCache
+  reporter: Reporter
+}
+
+export function getOutputAspectRatio(
+  {
+    width,
+    height,
+    maxWidth,
+    maxHeight,
+    layout = `fixed`,
+    fit = `cover`,
+  }: ISharpGatsbyImageArgs,
+  metadata: IImageMetadata
+): number {
+  // These maintain the input aspect ratio
+  if (fit === `inside` || fit === `outside`) {
+    return metadata.width / metadata.height
+  }
+
+  // If there's a width and height set, then calculate from that
+  if (height && width && layout === `fixed`) {
+    return width / height
+  }
+
+  // If there's a maxWidth and maxHeight, then we can get aspect ratio from there
+  if (maxWidth && maxHeight && layout !== `fixed`) {
+    return maxWidth / maxHeight
+  }
+  // Otherwise we use the aspect ratio of the source image
+  return metadata.width / metadata.height
 }
 
 export async function generateImageData({
   file,
-  args: {
+  args,
+  reporter,
+  cache,
+}: IImageDataArgs): Promise<ISharpGatsbyImageData | undefined> {
+  const {
     layout = `fixed`,
     placeholder = `dominantColor`,
     tracedSVGOptions = {},
-    ...args
-  },
-  reporter,
-  cache,
-}: {
-  file: string
-  args: ISharpGatsbyImageArgs
-  cache: GatsbyCache
-  reporter: Reporter
-}): Promise<ISharpGatsbyImageData | undefined> {
-  const isResponsive = layout !== `fixed`
+  } = args
 
-  // TODO: remove this and replace with:
-  // const pipeline = sharp(file)
-  const resize = isResponsive ? fluid : fixed
-  // TODO: actually call-out to get the base64, if needed
-  if (placeholder !== `blurred`) {
-    args.base64 = false
-  }
-  // TODO: Do the actual dominantColor calculation here
+  const metadata = await getImageMetadata(file, placeholder === `dominantColor`)
 
-  if (placeholder === `dominantColor`) {
-    args.dominantColor = true
-  }
+  const imageSizes: {
+    sizes: Array<number>
+    presentationWidth: number
+    presentationHeight: number
+    aspectRatio: number
+  } = calculateImageSizes({
+    file,
+    ...args,
+    imgDimensions: metadata,
+    reporter,
+  })
 
-  // TODO: use pipeline.metadata() to get source image sizes
-  // then use calculateImageSizes() to get output sizes
-  // then do everything else in fluid() except the size calculations
+  const transforms = imageSizes.sizes.map(outputWidth => {
+    const width = Math.round(outputWidth)
+    const transform = createTransformObject({
+      ...args,
+      width,
+      height: Math.round(width * imageSizes.aspectRatio),
+      toFormat: args.toFormat || metadata.format,
+    })
+    return transform
+  })
 
-  const imageData = await resize({ file, args, reporter, cache })
+  const images = batchQueueImageResizing({
+    file,
+    transforms,
+    reporter,
+  })
 
-  if (!imageData) {
+  const srcSet = getSrcSet(images)
+  const sizes = args.sizes || getSizes(imageSizes.presentationWidth)
+
+  const primaryIndex = imageSizes.sizes.findIndex(
+    size => size === imageSizes.presentationWidth
+  )
+
+  const primaryImage = images[primaryIndex]
+
+  if (!images?.length) {
     return undefined
   }
   const imageProps: ISharpGatsbyImageData = {
@@ -56,33 +163,50 @@ export async function generateImageData({
     placeholder: undefined,
     images: {
       fallback: {
-        src: imageData.src,
-        srcSet: imageData.srcSet,
-        sizes: imageData.sizes,
+        src: primaryImage.src,
+        srcSet,
+        sizes,
       },
       sources: [],
     },
   }
 
-  imageData.aspectRatio = imageData.aspectRatio || 1
+  if (args.webP) {
+    const transforms = imageSizes.sizes.map(outputWidth => {
+      const width = Math.round(outputWidth)
+      const transform = createTransformObject({
+        ...args,
+        width,
+        height: Math.round(width * imageSizes.aspectRatio),
+        toFormat: `webp`,
+      })
+      return transform
+    })
 
-  switch (layout) {
-    case `fixed`:
-      imageProps.width = imageData.width
-      imageProps.height = imageData.height
-      break
+    const webpImages = batchQueueImageResizing({
+      file,
+      transforms,
+      reporter,
+    })
+    const webpSrcSet = getSrcSet(webpImages)
 
-    case `fluid`:
-      imageProps.width = 1
-      imageProps.height = 1 / imageData.aspectRatio
-      break
-
-    case `constrained`:
-      imageProps.width = args.maxWidth || imageData.presentationWidth || 1
-      imageProps.height = (imageProps.width || 1) / imageData.aspectRatio
+    imageProps.images.sources?.push({
+      srcSet: webpSrcSet,
+      type: `image/webp`,
+      sizes,
+    })
   }
 
-  if (placeholder === `tracedSVG`) {
+  if (placeholder === `blurred`) {
+    const { src: fallback } = await base64({
+      file,
+      args: { ...args, width: args.base64Width, height: args.base64Height },
+      reporter,
+    })
+    imageProps.placeholder = {
+      fallback,
+    }
+  } else if (placeholder === `tracedSVG`) {
     const fallback: string = await traceSVG({
       file,
       args: tracedSVGOptions,
@@ -93,27 +217,26 @@ export async function generateImageData({
     imageProps.placeholder = {
       fallback,
     }
-  } else if (imageData.base64) {
-    imageProps.placeholder = {
-      fallback: imageData.base64,
-    }
-  } else if (placeholder === `dominantColor`) {
-    imageProps.backgroundColor = imageData.dominantColor
+  } else if (metadata.dominantColor) {
+    imageProps.backgroundColor = metadata.dominantColor
   }
 
-  if (args.webP) {
-    const webp = await resize({
-      file,
-      args: { ...args, base64: false, dominantColor: false, toFormat: `webp` },
-      reporter,
-      cache,
-    })
+  primaryImage.aspectRatio = primaryImage.aspectRatio || 1
 
-    imageProps.images.sources?.push({
-      srcSet: webp.srcSet,
-      type: `image/webp`,
-      sizes: imageData.sizes,
-    })
+  switch (layout) {
+    case `fixed`:
+      imageProps.width = primaryImage.width
+      imageProps.height = primaryImage.height
+      break
+
+    case `fluid`:
+      imageProps.width = 1
+      imageProps.height = 1 / primaryImage.aspectRatio
+      break
+
+    case `constrained`:
+      imageProps.width = args.maxWidth || primaryImage.width || 1
+      imageProps.height = (imageProps.width || 1) / primaryImage.aspectRatio
   }
 
   return imageProps
