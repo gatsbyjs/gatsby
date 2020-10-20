e8da16ce94d0e1f8792b97e68eb843cad7f77bf2
Kyle Mathews <mathews.kyle@gmail.com>
2020-10-20 09:46:37 -0600

    Conditionally generate body str

--- packages/gatsby/cache-dir/develop-static-entry.js
+++ packages/gatsby/cache-dir/develop-static-entry.js
@@ -43,131 +43,157 @@ export default (pagePath, callback) => {
   let preBodyComponents = []
   let postBodyComponents = []
   let bodyProps = {}
-  let htmlStr
 
-  const setHeadComponents = components => {
-    headComponents = headComponents.concat(components)
-  }
-
-  const setHtmlAttributes = attributes => {
-    htmlAttributes = merge(htmlAttributes, attributes)
-  }
-
-  const setBodyAttributes = attributes => {
-    bodyAttributes = merge(bodyAttributes, attributes)
-  }
-
-  const setPreBodyComponents = components => {
-    preBodyComponents = preBodyComponents.concat(components)
-  }
-
-  const setPostBodyComponents = components => {
-    postBodyComponents = postBodyComponents.concat(components)
-  }
-
-  const setBodyProps = props => {
-    bodyProps = merge({}, bodyProps, props)
-  }
-
-  const getHeadComponents = () => headComponents
-
-  const replaceHeadComponents = components => {
-    headComponents = components
-  }
-
-  const replaceBodyHTMLString = body => {
-    bodyHtml = body
-  }
-
-  const getPreBodyComponents = () => preBodyComponents
-
-  const replacePreBodyComponents = components => {
-    preBodyComponents = components
-  }
-
-  const getPostBodyComponents = () => postBodyComponents
-
-  const replacePostBodyComponents = components => {
-    postBodyComponents = components
-  }
-
-  const getPageDataPath = path => {
-    const fixedPagePath = path === `/` ? `index` : path
-    return join(`page-data`, fixedPagePath, `page-data.json`)
-  }
-
-  const getPageData = pagePath => {
-    const pageDataPath = getPageDataPath(pagePath)
-    const absolutePageDataPath = join(process.cwd(), `public`, pageDataPath)
-    const pageDataJson = fs.readFileSync(absolutePageDataPath, `utf8`)
-
-    try {
-      return JSON.parse(pageDataJson)
-    } catch (err) {
-      return null
+  const generateBodyHTML = () => {
+    const setHeadComponents = components => {
+      headComponents = headComponents.concat(components)
     }
-  }
 
-  const pageData = getPageData(pagePath)
+    const setHtmlAttributes = attributes => {
+      htmlAttributes = merge(htmlAttributes, attributes)
+    }
 
-  const componentChunkName = pageData?.componentChunkName
+    const setBodyAttributes = attributes => {
+      bodyAttributes = merge(bodyAttributes, attributes)
+    }
 
-  const createElement = React.createElement
+    const setPreBodyComponents = components => {
+      preBodyComponents = preBodyComponents.concat(components)
+    }
 
-  class RouteHandler extends React.Component {
-    render() {
-      const props = {
-        ...this.props,
-        ...pageData.result,
-        params: {
-          ...grabMatchParams(this.props.location.pathname),
-          ...(pageData.result?.pageContext?.__params || {}),
-        },
-        // pathContext was deprecated in v2. Renamed to pageContext
-        pathContext: pageData.result ? pageData.result.pageContext : undefined,
+    const setPostBodyComponents = components => {
+      postBodyComponents = postBodyComponents.concat(components)
+    }
+
+    const setBodyProps = props => {
+      bodyProps = merge({}, bodyProps, props)
+    }
+
+    const getHeadComponents = () => headComponents
+
+    const replaceHeadComponents = components => {
+      headComponents = components
+    }
+
+    const replaceBodyHTMLString = body => {
+      bodyHtml = body
+    }
+
+    const getPreBodyComponents = () => preBodyComponents
+
+    const replacePreBodyComponents = components => {
+      preBodyComponents = components
+    }
+
+    const getPostBodyComponents = () => postBodyComponents
+
+    const replacePostBodyComponents = components => {
+      postBodyComponents = components
+    }
+
+    const getPageDataPath = path => {
+      const fixedPagePath = path === `/` ? `index` : path
+      return join(`page-data`, fixedPagePath, `page-data.json`)
+    }
+
+    const getPageData = pagePath => {
+      const pageDataPath = getPageDataPath(pagePath)
+      const absolutePageDataPath = join(process.cwd(), `public`, pageDataPath)
+      const pageDataJson = fs.readFileSync(absolutePageDataPath, `utf8`)
+
+      try {
+        return JSON.parse(pageDataJson)
+      } catch (err) {
+        return null
       }
+    }
 
-      const pageElement = createElement(
-        syncRequires.components[componentChunkName],
-        props
-      )
+    const pageData = getPageData(pagePath)
 
-      const wrappedPage = apiRunner(
-        `wrapPageElement`,
-        { element: pageElement, props },
-        pageElement,
-        ({ result }) => {
-          return { element: result, props }
+    const componentChunkName = pageData?.componentChunkName
+
+    const createElement = React.createElement
+
+    class RouteHandler extends React.Component {
+      render() {
+        const props = {
+          ...this.props,
+          ...pageData.result,
+          params: {
+            ...grabMatchParams(this.props.location.pathname),
+            ...(pageData.result?.pageContext?.__params || {}),
+          },
+          // pathContext was deprecated in v2. Renamed to pageContext
+          pathContext: pageData.result
+            ? pageData.result.pageContext
+            : undefined,
         }
-      ).pop()
 
-      return wrappedPage
+        const pageElement = createElement(
+          syncRequires.components[componentChunkName],
+          props
+        )
+
+        const wrappedPage = apiRunner(
+          `wrapPageElement`,
+          { element: pageElement, props },
+          pageElement,
+          ({ result }) => {
+            return { element: result, props }
+          }
+        ).pop()
+
+        return wrappedPage
+      }
     }
-  }
 
-  const routerElement = (
-    <ServerLocation url={`${__BASE_PATH__}${pagePath}`}>
-      <Router id="gatsby-focus-wrapper" baseuri={__BASE_PATH__}>
-        <RouteHandler path="/*" />
-      </Router>
-      <div {...RouteAnnouncerProps} />
-    </ServerLocation>
-  )
+    const routerElement = (
+      <ServerLocation url={`${__BASE_PATH__}${pagePath}`}>
+        <Router id="gatsby-focus-wrapper" baseuri={__BASE_PATH__}>
+          <RouteHandler path="/*" />
+        </Router>
+        <div {...RouteAnnouncerProps} />
+      </ServerLocation>
+    )
 
-  const bodyComponent = apiRunner(
-    `wrapRootElement`,
-    { element: routerElement, pathname: pagePath },
-    routerElement,
-    ({ result }) => {
-      return { element: result, pathname: pagePath }
+    const bodyComponent = apiRunner(
+      `wrapRootElement`,
+      { element: routerElement, pathname: pagePath },
+      routerElement,
+      ({ result }) => {
+        return { element: result, pathname: pagePath }
+      }
+    ).pop()
+
+    if (process.env.GATSBY_EXPERIMENTAL_DEV_SSR) {
+      // Let the site or plugin render the page component.
+      apiRunner(`replaceRenderer`, {
+        bodyComponent,
+        replaceBodyHTMLString,
+        setHeadComponents,
+        setHtmlAttributes,
+        setBodyAttributes,
+        setPreBodyComponents,
+        setPostBodyComponents,
+        setBodyProps,
+        pathname: pagePath,
+        pathPrefix: __PATH_PREFIX__,
+      })
     }
-  ).pop()
 
-  if (process.env.GATSBY_EXPERIMENTAL_DEV_SSR) {
-    // Let the site or plugin render the page component.
-    apiRunner(`replaceRenderer`, {
-      bodyComponent,
-      replaceBodyHTMLString,
+    // If no one stepped up, we'll handle it.
+    if (!bodyHtml) {
+      try {
+        if (process.env.GATSBY_EXPERIMENTAL_DEV_SSR) {
+          bodyHtml = renderToString(bodyComponent)
+        }
+      } catch (e) {
+        // ignore @reach/router redirect errors
+        if (!isRedirect(e)) throw e
+      }
+    }
+
+    apiRunner(`onRenderBody`, {
       setHeadComponents,
       setHtmlAttributes,
       setBodyAttributes,
@@ -175,45 +201,24 @@ export default (pagePath, callback) => {
       setPostBodyComponents,
       setBodyProps,
       pathname: pagePath,
-      pathPrefix: __PATH_PREFIX__,
     })
+
+    apiRunner(`onPreRenderHTML`, {
+      getHeadComponents,
+      replaceHeadComponents,
+      getPreBodyComponents,
+      replacePreBodyComponents,
+      getPostBodyComponents,
+      replacePostBodyComponents,
+      pathname: pagePath,
+    })
+
+    return bodyHtml
   }
-
-  // If no one stepped up, we'll handle it.
-  if (!bodyHtml) {
-    try {
-      if (process.env.GATSBY_EXPERIMENTAL_DEV_SSR) {
-        bodyHtml = renderToString(bodyComponent)
-      }
-    } catch (e) {
-      // ignore @reach/router redirect errors
-      if (!isRedirect(e)) throw e
-    }
-  }
-
-  apiRunner(`onRenderBody`, {
-    setHeadComponents,
-    setHtmlAttributes,
-    setBodyAttributes,
-    setPreBodyComponents,
-    setPostBodyComponents,
-    setBodyProps,
-    pathname: pagePath,
-  })
-
-  apiRunner(`onPreRenderHTML`, {
-    getHeadComponents,
-    replaceHeadComponents,
-    getPreBodyComponents,
-    replacePreBodyComponents,
-    getPostBodyComponents,
-    replacePostBodyComponents,
-    pathname: pagePath,
-  })
 
   let bodyStr = ``
   if (process.env.GATSBY_EXPERIMENTAL_DEV_SSR && pagePath !== `/dev-404/`) {
-    bodyStr = bodyHtml
+    bodyStr = generateBodyHTML()
   }
 
   const htmlElement = React.createElement(Html, {
@@ -230,7 +235,7 @@ export default (pagePath, callback) => {
       <script key={`commons`} src="/commons.js" />,
     ]),
   })
-  htmlStr = renderToStaticMarkup(htmlElement)
+  let htmlStr = renderToStaticMarkup(htmlElement)
   htmlStr = `<!DOCTYPE html>${htmlStr}`
 
   callback(null, htmlStr)
