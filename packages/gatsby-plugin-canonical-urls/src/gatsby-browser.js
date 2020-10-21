146fba3f9f0b1e488e62fd46df698881a86d8b00
Thomas Metten <t.metten@aca-it.be>
2020-10-21 10:12:06 +0200

    Stop using data-baseprotocol and data-basehost
    These values are now parsed from the canonical url's href attribute.

--- packages/gatsby-plugin-canonical-urls/src/gatsby-browser.js
+++ packages/gatsby-plugin-canonical-urls/src/gatsby-browser.js
@@ -1,13 +1,14 @@
+import url from "url"
+
 exports.onRouteUpdate = (
   { location },
   pluginOptions = { stripQueryString: false }
 ) => {
   const domElem = document.querySelector(`link[rel='canonical']`)
   const existingValue = domElem.getAttribute(`href`)
-  const baseProtocol = domElem.getAttribute(`data-baseProtocol`)
-  const baseHost = domElem.getAttribute(`data-baseHost`)
-  if (existingValue && baseProtocol && baseHost) {
-    let value = `${baseProtocol}//${baseHost}${location.pathname}`
+  if (existingValue) {
+    const parsed = url.parse(existingValue)
+    let value = `${parsed.protocol}//${parsed.host}${location.pathname}`
 
     const { stripQueryString } = pluginOptions
 
