78782b66a372b73aaab2cd738670f3fe8ab5ce06
Peter van der Zee <github-public@qfox.nl>
2020-10-23 11:28:44 +0200

    Explicitly splat stats object

--- packages/gatsby-source-filesystem/src/create-file-node.js
+++ packages/gatsby-source-filesystem/src/create-file-node.js
@@ -45,11 +45,6 @@ exports.createFileNode = async (
     }
   }
 
-  stats.atime = stats.atime.toJSON()
-  stats.mtime = stats.mtime.toJSON()
-  stats.ctime = stats.ctime.toJSON()
-  stats.birthtime = stats.birthtime.toJSON()
-
   return {
     // Don't actually make the File id the absolute path as otherwise
     // people will use the id for that and ids shouldn't be treated as
@@ -67,11 +62,31 @@ exports.createFileNode = async (
     ),
     extension: slashedFile.ext.slice(1).toLowerCase(),
     prettySize: prettyBytes(stats.size),
-    modifiedTime: stats.mtime,
-    accessTime: stats.atime,
-    changeTime: stats.ctime,
-    birthTime: stats.birthtime,
+    modifiedTime: stats.mtime.toJSON(),
+    accessTime: stats.atime.toJSON(),
+    changeTime: stats.ctime.toJSON(),
+    birthTime: stats.birthtime.toJSON(),
+    // Note: deprecate splatting the slashedFile object
+    // Note: the object may contain different properties depending on File or Dir
     ...slashedFile,
-    ...stats,
+    // TODO: deprecate copying the entire object
+    // Note: not splatting for perf reasons (make sure Date objects are serialized)
+    dev: stats.dev,
+    mode: stats.mode,
+    nlink: stats.nlink,
+    uid: stats.uid,
+    rdev: stats.rdev,
+    blksize: stats.blksize,
+    ino: stats.ino,
+    size: stats.size,
+    blocks: stats.blocks,
+    atimeMs: stats.atimeMs,
+    mtimeMs: stats.mtimeMs,
+    ctimeMs: stats.ctimeMs,
+    birthtimeMs: stats.birthtimeMs,
+    atime: stats.atime.toJSON(),
+    mtime: stats.mtime.toJSON(),
+    ctime: stats.ctime.toJSON(),
+    birthtime: stats.birthtime.toJSON(),
   }
 }
