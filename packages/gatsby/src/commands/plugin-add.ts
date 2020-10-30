d6449222e6590aebe91cb0f450ae4b68a5507154
Matt Kane <matt@gatsbyjs.com>
2020-10-30 11:49:58 +0000

    feat(gatsby): Add "gatsby plugin add" command

--- packages/gatsby/src/commands/plugin-add.ts
+++ packages/gatsby/src/commands/plugin-add.ts
@@ -1,6 +1,76 @@
-export default async function run({ plugins }: IProgram): Promise<void> {
-  if (!plugins?.length) {
-    console.log(`Please specify a plugin to install`)
+import { NPMPackage, GatsbyPlugin } from "gatsby-recipes"
+import { IProgram } from "./types"
+
+const normalizePluginName = (plugin: string): string => {
+  if (plugin.startsWith(`gatsby-`)) {
+    return plugin
   }
-  console.log(plugins)
+  if (
+    plugin.startsWith(`source-`) ||
+    plugin.startsWith(`transformer-`) ||
+    plugin.startsWith(`plugin-`)
+  ) {
+    return `gatsby-${plugin}`
+  }
+  return `gatsby-plugin-${plugin}`
+}
+
+async function installPluginPackage(
+  plugin: string,
+  root: string,
+  reporter: IProgram["report"]
+): Promise<void> {
+  const installTimer = reporter.activityTimer(`Installing ${plugin}`)
+
+  installTimer.start()
+  reporter.info(`Installing ${plugin}`)
+  try {
+    const result = await NPMPackage.create({ root }, { name: plugin })
+    reporter.info(result._message)
+  } catch (err) {
+    reporter.error(JSON.parse(err)?.message)
+    installTimer.setStatus(`FAILED`)
+  }
+  installTimer.end()
+}
+
+async function installPluginConfig(
+  plugin: string,
+  root: string,
+  reporter: IProgram["report"]
+): Promise<void> {
+  const installTimer = reporter.activityTimer(
+    `Adding ${plugin} to gatsby-config`
+  )
+
+  installTimer.start()
+  reporter.info(`Adding ${plugin}`)
+  try {
+    const result = await GatsbyPlugin.create({ root }, { name: plugin })
+    reporter.info(result._message)
+  } catch (err) {
+    reporter.error(JSON.parse(err)?.message)
+    installTimer.setStatus(`FAILED`)
+  }
+  installTimer.end()
+}
+
+export default async function run({
+  plugins,
+  report,
+  directory,
+}: IProgram & { plugins?: Array<string> }): Promise<void> {
+  if (!plugins?.length) {
+    report.error(`Please specify a plugin to install`)
+    return
+  }
+
+  const pluginList = plugins.map(normalizePluginName)
+
+  await Promise.all(
+    pluginList.map(plugin => installPluginPackage(plugin, directory, report))
+  )
+  await Promise.all(
+    pluginList.map(plugin => installPluginConfig(plugin, directory, report))
+  )
 }
