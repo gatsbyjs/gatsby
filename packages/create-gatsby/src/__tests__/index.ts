3d14984f7414253753cc69325c21c1592e5208bc
Kyle Gill <kylerobertgill@gmail.com>
2020-10-30 11:24:53 -0600

    attempt to add some more tests

--- packages/create-gatsby/src/__tests__/index.ts
+++ packages/create-gatsby/src/__tests__/index.ts
@@ -12,6 +12,7 @@ const tick = (interval = 1): Promise<void> =>
   new Promise(resolve => setTimeout(resolve, interval))
 
 const Keys = {
+  // pulled from https://tldp.org/LDP/abs/html/escapingsection.html
   DOWN: `\x1B\x5B\x42`,
   UP: `\x1B\x5B\x41`,
   ENTER: `\x0D`,
@@ -19,10 +20,18 @@ const Keys = {
 }
 
 describe(`The create-gatsby CLI`, () => {
-  it(`runs`, async () => {
-    // Start mocking stdout
+  beforeEach(() => {
+    // clear stdout buffer
     stdout.start()
-    run()
+    run().catch(() => {})
+    spyOn(process, `exit`)
+  })
+
+  afterEach(() => {
+    stdout.stop()
+  })
+
+  it(`runs`, async () => {
     await tick()
     stdinMock.send(`my-new-site`)
     stdinMock.send(Keys.ENTER)
@@ -62,4 +71,59 @@ describe(`The create-gatsby CLI`, () => {
     )
     await tick(1000)
   })
+
+  it(`displays the plugin for the selected CMS to configure`, async () => {
+    await tick()
+    stdinMock.send(`select-cms`)
+    stdinMock.send(Keys.ENTER)
+    await tick()
+    await stdinMock.send(Keys.DOWN) // WordPress is first in the list
+    await stdinMock.send(Keys.ENTER)
+    await tick()
+    stdout.stop()
+
+    expect(stdout.output).toMatch(`gatsby-source-wordpress`)
+    await tick(1000)
+  })
+
+  it(`displays the plugin for the selected styling solution`, async () => {
+    await tick()
+    stdinMock.send(`select-styling`)
+    stdinMock.send(Keys.ENTER)
+    await tick()
+    await stdinMock.send(Keys.ENTER)
+    await tick()
+    await stdinMock.send(Keys.DOWN) // PostCSS is first in the list
+    await stdinMock.send(Keys.ENTER)
+    await tick()
+    stdout.stop()
+
+    expect(stdout.output).toMatch(`gatsby-plugin-postcss`)
+    await tick(1000)
+  })
+
+  it(`doesnt print steps skipped by user`, async () => {
+    await tick()
+    stdinMock.send(`skip-steps`)
+    stdinMock.send(Keys.ENTER) // skip name
+    await tick()
+    await stdinMock.send(Keys.ENTER) // skip cms step
+    await tick()
+    await stdinMock.send(Keys.ENTER) // skip styling
+    await tick()
+    await stdinMock.send(Keys.ENTER) // skip features
+    // Clear the stdout buffer, as we only want to check final output
+    stdout.start()
+    await tick()
+    stdout.stop()
+
+    // this should always be present
+    expect(stdout.output).toMatch(`Create a new Gatsby site in the folder`)
+    // these steps were skipped
+    expect(stdout.output).not.toMatch(`Install and configure the plugin for`)
+    expect(stdout.output).not.toMatch(`Get you set up to use`)
+    await tick(1000)
+  })
+
+  it.todo(`creates a new project/folder with a gatsby-config`)
 })
