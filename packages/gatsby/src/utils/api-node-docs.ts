4283f80eab5e64efd7be963b9a612254d059ad76
David Landry <david@dmwl.net>
2020-10-30 15:56:06 -0400

    Correct type for `createPage`

--- packages/gatsby/src/utils/api-node-docs.ts
+++ packages/gatsby/src/utils/api-node-docs.ts
@@ -15,7 +15,7 @@ export const resolvableExtensions = true
  * @gatsbyVersion 2.2.0
  * @param {object} $0 See the [documentation for `Node API Helpers` for more details](/docs/node-api-helpers)
  * @param {Actions} $0.actions See the [list of documented actions](/docs/actions)
- * @param {object} $0.actions.createPages [Documentation for this action](/docs/actions/#createPage)
+ * @param {function} $0.actions.createPages [Documentation for this action](/docs/actions/#createPage)
  * @param {function} $0.graphql: Query GraphQL API. See [examples here](/docs/creating-and-modifying-pages/#creating-pages-in-gatsby-nodejs)
  * @param {GatsbyReporter} $0.reporter Log issues. See [GatsbyReporter documentation](/docs/node-api-helpers/#GatsbyReporter) for more details
  *
