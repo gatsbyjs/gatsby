8f8a41979bb88c0caf7ddae91ed840325692b4a8
Max Stoiber <contact@mxstbr.com>
2020-11-02 16:39:09 +0100

    fix(gatsby-transformer-remark): add excerpt_separator to plugin options schema (#27785)
    Closes #27784

--- packages/gatsby-transformer-remark/src/gatsby-node.js
+++ packages/gatsby-transformer-remark/src/gatsby-node.js
@@ -21,6 +21,9 @@ exports.pluginOptionsSchema = function ({ Joi }) {
     gfm: Joi.boolean().description(
       `Activates GitHub Flavored Markdown mode (default: true)`
     ),
+    excerpt_separator: Joi.string().description(
+      `If your Markdown file contains HTML, excerpt will not return a value. In that case, you can set an excerpt_separator to an HTML tag. Edit your Markdown files to include that HTML tag after the text you’d like to appear in the excerpt.`
+    ),
     plugins: Joi.array()
       .items(
         Joi.string(),
