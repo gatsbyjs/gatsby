d01cb3224483d573c12288f98b2196e5fe766ba2
Matt Kane <matt@gatsbyjs.com>
2020-11-02 09:08:26 +0000

    Don't return undefined for CI

--- packages/gatsby-core-utils/src/get-term-program.ts
+++ packages/gatsby-core-utils/src/get-term-program.ts
@@ -1,9 +1,4 @@
-import { isCI } from "."
-
 export function getTermProgram(): string | undefined {
-  if (isCI()) {
-    return undefined
-  }
   const { TERM_PROGRAM, WT_SESSION } = process.env
   if (TERM_PROGRAM) {
     return TERM_PROGRAM
