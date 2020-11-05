27c3083a8dccbab06f72f7259ddf6a79b7043bbd
David Landry <david@dmwl.net>
2020-11-05 05:08:52 -0500

    fix(docs): Update documentation for createPages (#27735)
    * fix(docs): Update documentation for createPages
    
    Update parameters documentation in createPages
    
    * Correct type for `createPage`
    
    * Add return annotation for createPages

--- packages/gatsby/src/utils/api-node-docs.ts
+++ packages/gatsby/src/utils/api-node-docs.ts
@@ -11,6 +11,15 @@ export const resolvableExtensions = true
  * complete so you can query your data in order to create pages.
  *
  * See also [the documentation for the action `createPage`](/docs/actions/#createPage).
+ *
+ * @gatsbyVersion 2.2.0
+ * @param {object} $0 See the [documentation for `Node API Helpers` for more details](/docs/node-api-helpers)
+ * @param {Actions} $0.actions See the [list of documented actions](/docs/actions)
+ * @param {function} $0.actions.createPages [Documentation for this action](/docs/actions/#createPage)
+ * @param {function} $0.graphql: Query GraphQL API. See [examples here](/docs/creating-and-modifying-pages/#creating-pages-in-gatsby-nodejs)
+ * @param {GatsbyReporter} $0.reporter Log issues. See [GatsbyReporter documentation](/docs/node-api-helpers/#GatsbyReporter) for more details
+ * @returns {Promise<void>} No return value required, but the caller will `await` any promise that's returned
+ *
  * @example
  * const path = require(`path`)
  *
