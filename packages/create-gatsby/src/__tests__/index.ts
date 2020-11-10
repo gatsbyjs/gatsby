f4a189fd22b1dc178c857647c1a82fc03ccbaaf4
Laurie <laurie@gatsbyjs.com>
2020-11-10 17:40:25 -0500

    have CI run individual questions and see if we falter

--- packages/create-gatsby/src/__tests__/index.ts
+++ packages/create-gatsby/src/__tests__/index.ts
@@ -19,41 +19,41 @@ describe(`run`, () => {
     jest.setTimeout(10000)
   })
 
-  it(`should not accept bad name`, async () => {
-    enquirer.on(`prompt`, prompt => {
-      prompt.once(`run`, async () => {
-        if (prompt.name === `project`) {
-          await stringInput(prompt, `bad/name`)
-          await prompt.submit()
-          await prompt.keypress(null, { name: `backspace` })
-          await prompt.keypress(null, { name: `backspace` })
-          await prompt.keypress(null, { name: `backspace` })
-          await prompt.keypress(null, { name: `backspace` })
-          await prompt.keypress(null, { name: `backspace` })
-          await prompt.keypress(null, { name: `backspace` })
-          await prompt.keypress(null, { name: `backspace` })
-          await prompt.keypress(null, { name: `backspace` })
-          await stringInput(prompt, `goodname`)
-          await prompt.submit()
-        } else if (prompt.name === `cms`) {
-          await prompt.submit()
-        } else if (prompt.name === `styling`) {
-          await prompt.submit()
-        } else if (prompt.name === `features`) {
-          await prompt.keypress(null, { name: `tab` })
-          await prompt.submit()
-        }
-      })
-    })
+  // it(`should not accept bad name`, async () => {
+  //   enquirer.on(`prompt`, prompt => {
+  //     prompt.once(`run`, async () => {
+  //       if (prompt.name === `project`) {
+  //         await stringInput(prompt, `bad/name`)
+  //         await prompt.submit()
+  //         await prompt.keypress(null, { name: `backspace` })
+  //         await prompt.keypress(null, { name: `backspace` })
+  //         await prompt.keypress(null, { name: `backspace` })
+  //         await prompt.keypress(null, { name: `backspace` })
+  //         await prompt.keypress(null, { name: `backspace` })
+  //         await prompt.keypress(null, { name: `backspace` })
+  //         await prompt.keypress(null, { name: `backspace` })
+  //         await prompt.keypress(null, { name: `backspace` })
+  //         await stringInput(prompt, `goodname`)
+  //         await prompt.submit()
+  //       } else if (prompt.name === `cms`) {
+  //         await prompt.submit()
+  //       } else if (prompt.name === `styling`) {
+  //         await prompt.submit()
+  //       } else if (prompt.name === `features`) {
+  //         await prompt.keypress(null, { name: `tab` })
+  //         await prompt.submit()
+  //       }
+  //     })
+  //   })
 
-    const data = await enquirer.prompt(questions)
-    expect(data).toEqual({
-      project: `goodname`,
-      cms: `none`,
-      styling: `none`,
-      features: [],
-    })
-  })
+  //   const data = await enquirer.prompt(questions)
+  //   expect(data).toEqual({
+  //     project: `goodname`,
+  //     cms: `none`,
+  //     styling: `none`,
+  //     features: [],
+  //   })
+  // })
 
   // fit('should not accept a name that exists', async () => {
   //   enquirer.on('prompt', prompt => {
@@ -100,33 +100,33 @@ describe(`run`, () => {
   //   })
   // })
 
-  it(`should take project name`, async () => {
-    enquirer.on(`prompt`, prompt => {
-      prompt.once(`run`, async () => {
-        if (prompt.name === `project`) {
-          await stringInput(prompt, `my-new-site`)
-          await prompt.submit()
-        } else if (prompt.name === `cms`) {
-          await prompt.submit()
-        } else if (prompt.name === `styling`) {
-          await prompt.submit()
-        } else if (prompt.name === `features`) {
-          await prompt.keypress(null, { name: `tab` })
-          await prompt.submit()
-        }
-      })
-    })
+  // it(`should take project name`, async () => {
+  //   enquirer.on(`prompt`, prompt => {
+  //     prompt.once(`run`, async () => {
+  //       if (prompt.name === `project`) {
+  //         await stringInput(prompt, `my-new-site`)
+  //         await prompt.submit()
+  //       } else if (prompt.name === `cms`) {
+  //         await prompt.submit()
+  //       } else if (prompt.name === `styling`) {
+  //         await prompt.submit()
+  //       } else if (prompt.name === `features`) {
+  //         await prompt.keypress(null, { name: `tab` })
+  //         await prompt.submit()
+  //       }
+  //     })
+  //   })
 
-    const data = await enquirer.prompt(questions)
-    expect(data).toEqual({
-      cms: `none`,
-      features: [],
-      project: `my-new-site`,
-      styling: `none`,
-    })
-  })
+  //   const data = await enquirer.prompt(questions)
+  //   expect(data).toEqual({
+  //     cms: `none`,
+  //     features: [],
+  //     project: `my-new-site`,
+  //     styling: `none`,
+  //   })
+  // })
 
-  it(`should pass a single question`, async () => {
+  it(`should pass the name`, async () => {
     enquirer.on(`prompt`, prompt => {
       prompt.once(`run`, async () => {
         if (prompt.name === `project`) {
@@ -145,86 +145,134 @@ describe(`run`, () => {
   it(`can select a CMS`, async () => {
     enquirer.on(`prompt`, prompt => {
       prompt.once(`run`, async () => {
-        if (prompt.name === `project`) {
-          await stringInput(prompt, `select-cms`)
-          await prompt.submit()
-        } else if (prompt.name === `cms`) {
-          await prompt.keypress(null, { name: `down` })
-          await prompt.keypress(null, { name: `down` })
-          await prompt.submit()
-        } else if (prompt.name === `styling`) {
-          await prompt.submit()
-        } else if (prompt.name === `features`) {
-          await prompt.keypress(null, { name: `tab` })
-          await prompt.submit()
-        }
+        await prompt.keypress(null, { name: `down` })
+        await prompt.keypress(null, { name: `down` })
+        await prompt.submit()
       })
     })
 
-    const data = await enquirer.prompt(questions)
+    const data = await enquirer.prompt(questions[1])
     expect(data).toEqual({
-      project: `select-cms`,
       cms: `gatsby-source-contentful`,
-      styling: `none`,
-      features: [],
     })
   })
 
   it(`can select a styling library`, async () => {
     enquirer.on(`prompt`, prompt => {
       prompt.once(`run`, async () => {
-        if (prompt.name === `project`) {
-          await stringInput(prompt, `select-styling`)
-          await prompt.submit()
-        } else if (prompt.name === `cms`) {
-          await prompt.submit()
-        } else if (prompt.name === `styling`) {
-          await prompt.keypress(null, { name: `down` })
-          await prompt.submit()
-        } else if (prompt.name === `features`) {
-          await prompt.keypress(null, { name: `tab` })
-          await prompt.submit()
-        }
+        await prompt.keypress(null, { name: `down` })
+        await prompt.submit()
       })
     })
 
-    const data = await enquirer.prompt(questions)
+    const data = await enquirer.prompt(questions[2])
     expect(data).toEqual({
-      project: `select-styling`,
-      cms: `none`,
       styling: `gatsby-plugin-postcss`,
-      features: [],
     })
   })
 
   it(`can select additional plugins`, async () => {
     enquirer.on(`prompt`, prompt => {
       prompt.once(`run`, async () => {
-        if (prompt.name === `project`) {
-          await stringInput(prompt, `more-features`)
-          await prompt.submit()
-        } else if (prompt.name === `cms`) {
-          await prompt.submit()
-        } else if (prompt.name === `styling`) {
-          await prompt.submit()
-        } else if (prompt.name === `features`) {
-          await prompt.keypress(null, { name: `down` })
-          await prompt.keypress(null, { name: `space` })
-          await prompt.keypress(null, { name: `down` })
-          await prompt.keypress(null, { name: `down` })
-          await prompt.keypress(null, { name: `space` })
-          await prompt.keypress(null, { name: `tab` })
-          await prompt.submit()
-        }
+        await prompt.keypress(null, { name: `down` })
+        await prompt.keypress(null, { name: `space` })
+        await prompt.keypress(null, { name: `down` })
+        await prompt.keypress(null, { name: `down` })
+        await prompt.keypress(null, { name: `space` })
+        await prompt.keypress(null, { name: `tab` })
+        await prompt.submit()
       })
     })
 
-    const data = await enquirer.prompt(questions)
+    const data = await enquirer.prompt(questions[3])
     expect(data).toEqual({
-      project: `more-features`,
-      cms: `none`,
-      styling: `none`,
       features: [`gatsby-plugin-react-helmet`, `gatsby-plugin-offline`],
     })
   })
+
+  // it(`can select a CMS`, async () => {
+  //   enquirer.on(`prompt`, prompt => {
+  //     prompt.once(`run`, async () => {
+  //       if (prompt.name === `project`) {
+  //         await stringInput(prompt, `select-cms`)
+  //         await prompt.submit()
+  //       } else if (prompt.name === `cms`) {
+  //         await prompt.keypress(null, { name: `down` })
+  //         await prompt.keypress(null, { name: `down` })
+  //         await prompt.submit()
+  //       } else if (prompt.name === `styling`) {
+  //         await prompt.submit()
+  //       } else if (prompt.name === `features`) {
+  //         await prompt.keypress(null, { name: `tab` })
+  //         await prompt.submit()
+  //       }
+  //     })
+  //   })
+
+  //   const data = await enquirer.prompt(questions)
+  //   expect(data).toEqual({
+  //     project: `select-cms`,
+  //     cms: `gatsby-source-contentful`,
+  //     styling: `none`,
+  //     features: [],
+  //   })
+  // })
+
+  // it(`can select a styling library`, async () => {
+  //   enquirer.on(`prompt`, prompt => {
+  //     prompt.once(`run`, async () => {
+  //       if (prompt.name === `project`) {
+  //         await stringInput(prompt, `select-styling`)
+  //         await prompt.submit()
+  //       } else if (prompt.name === `cms`) {
+  //         await prompt.submit()
+  //       } else if (prompt.name === `styling`) {
+  //         await prompt.keypress(null, { name: `down` })
+  //         await prompt.submit()
+  //       } else if (prompt.name === `features`) {
+  //         await prompt.keypress(null, { name: `tab` })
+  //         await prompt.submit()
+  //       }
+  //     })
+  //   })
+
+  //   const data = await enquirer.prompt(questions)
+  //   expect(data).toEqual({
+  //     project: `select-styling`,
+  //     cms: `none`,
+  //     styling: `gatsby-plugin-postcss`,
+  //     features: [],
+  //   })
+  // })
+
+  // it(`can select additional plugins`, async () => {
+  //   enquirer.on(`prompt`, prompt => {
+  //     prompt.once(`run`, async () => {
+  //       if (prompt.name === `project`) {
+  //         await stringInput(prompt, `more-features`)
+  //         await prompt.submit()
+  //       } else if (prompt.name === `cms`) {
+  //         await prompt.submit()
+  //       } else if (prompt.name === `styling`) {
+  //         await prompt.submit()
+  //       } else if (prompt.name === `features`) {
+  //         await prompt.keypress(null, { name: `down` })
+  //         await prompt.keypress(null, { name: `space` })
+  //         await prompt.keypress(null, { name: `down` })
+  //         await prompt.keypress(null, { name: `down` })
+  //         await prompt.keypress(null, { name: `space` })
+  //         await prompt.keypress(null, { name: `tab` })
+  //         await prompt.submit()
+  //       }
+  //     })
+  //   })
+
+  //   const data = await enquirer.prompt(questions)
+  //   expect(data).toEqual({
+  //     project: `more-features`,
+  //     cms: `none`,
+  //     styling: `none`,
+  //     features: [`gatsby-plugin-react-helmet`, `gatsby-plugin-offline`],
+  //   })
+  // })
 })
