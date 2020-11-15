dbd74906c0f0a3133e83047abc2c68cbafe1aa95
Michal Piechowiak <misiek.piechowiak@gmail.com>
2020-11-15 22:18:11 +0100

    refactor(dev-loader): consolidate socket.io and loader caches, remove `getPageData` websocket logic

--- packages/gatsby/cache-dir/dev-loader.js
+++ packages/gatsby/cache-dir/dev-loader.js
@@ -1,20 +1,123 @@
-import { BaseLoader, PageResourceStatus } from "./loader"
+import {
+  BaseLoader,
+  PageResourceStatus,
+  getStaticQueryResults,
+  getPageDataDb,
+  getPageDb,
+} from "./loader"
 import { findPath } from "./find-path"
+import getSocket from "./socketIo"
+import normalizePagePath from "./normalize-page-path"
+
+function didDataChange(a, b) {
+  return JSON.stringify(a) !== JSON.stringify(b)
+}
+
+function mergePageEntry(cachedPage, newPageData) {
+  return {
+    ...cachedPage,
+    payload: {
+      ...cachedPage.payload,
+      json: newPageData.result,
+      page: {
+        ...cachedPage.payload.page,
+        staticQueryResults: newPageData.staticQueryResults,
+      },
+    },
+  }
+}
 
 class DevLoader extends BaseLoader {
   constructor(syncRequires, matchPaths) {
     const loadComponent = chunkName =>
       Promise.resolve(syncRequires.components[chunkName])
     super(loadComponent, matchPaths)
+
+    const socket = getSocket()
+
+    this.notFoundPagePathsInCaches = new Set()
+
+    if (socket) {
+      socket.on(`message`, msg => {
+        if (msg.type === `staticQueryResult`) {
+          const newResult = msg.payload.result
+
+          const cacheKey = msg.payload.id
+          const cachedStaticQueries = getStaticQueryResults()
+          const cachedResult = cachedStaticQueries[cacheKey]
+          if (didDataChange(newResult, cachedResult)) {
+            cachedStaticQueries[cacheKey] = newResult
+            ___emitter.emit(`staticQueryResult`, newResult)
+          }
+        } else if (msg.type === `pageQueryResult`) {
+          const newPageData = msg.payload.result
+
+          const pageDataDbCacheKey = normalizePagePath(msg.payload.id)
+          const cachedPageDataDb = getPageDataDb()
+          const cachedPageData = cachedPageDataDb.get(pageDataDbCacheKey)
+            ?.payload
+
+          if (didDataChange(newPageData, cachedPageData)) {
+            // always update canonical key for pageDataDb
+            cachedPageDataDb.set(pageDataDbCacheKey, {
+              pagePath: pageDataDbCacheKey,
+              payload: newPageData,
+              status: `success`,
+            })
+
+            const cachedPageDb = getPageDb()
+            const cachedPage = cachedPageDb.get(pageDataDbCacheKey)
+            if (cachedPage) {
+              cachedPageDb.set(
+                pageDataDbCacheKey,
+                mergePageEntry(cachedPage, newPageData)
+              )
+            }
+
+            // Additionally if those are query results for "/404.html"
+            // we have to update all paths user wanted to visit, but didn't have
+            // page for it, because we do store them under (normalized) path
+            // user wanted to visit
+            if (pageDataDbCacheKey === `/404.html`) {
+              this.notFoundPagePathsInCaches.forEach(notFoundPath => {
+                const previousPageDataEntry = cachedPageDataDb.get(notFoundPath)
+                if (previousPageDataEntry) {
+                  cachedPageDataDb.set(notFoundPath, {
+                    ...previousPageDataEntry,
+                    payload: newPageData,
+                  })
+                }
+
+                const previousPageEntry = cachedPageDb.get(notFoundPath)
+                if (previousPageEntry) {
+                  cachedPageDb.set(
+                    notFoundPath,
+                    mergePageEntry(previousPageEntry, newPageData)
+                  )
+                }
+              })
+            }
+
+            ___emitter.emit(`pageQueryResult`, newPageData)
+          }
+        }
+      })
+    } else {
+      console.warn(`Could not get web socket`)
+    }
   }
 
   loadPage(pagePath) {
     const realPath = findPath(pagePath)
-    return super.loadPage(realPath).then(result =>
-      require(`./socketIo`)
-        .getPageData(realPath)
-        .then(() => result)
-    )
+    return super.loadPage(realPath).then(result => {
+      const isNotFound = this.pageDb.get(realPath)?.notFound
+
+      if (isNotFound) {
+        this.notFoundPagePathsInCaches.add(realPath)
+      }
+
+      return result
+    })
   }
 
   loadPageDataJson(rawPath) {
@@ -38,7 +141,10 @@ class DevLoader extends BaseLoader {
   }
 
   doPrefetch(pagePath) {
-    return Promise.resolve(require(`./socketIo`).getPageData(pagePath))
+    return super.doPrefetch(pagePath).then(result => {
+      const pageData = result.payload
+      return pageData
+    })
   }
 }
 
