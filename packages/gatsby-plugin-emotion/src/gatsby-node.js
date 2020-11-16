1e82111a9ca9d9637861ee2c5f1885f7045b511e
Paul Kim <kimbaudi@gmail.com>
2020-11-16 12:18:16 -0800

    set autoLabel to `dev-only`

--- packages/gatsby-plugin-emotion/src/gatsby-node.js
+++ packages/gatsby-plugin-emotion/src/gatsby-node.js
@@ -3,7 +3,7 @@ export const onCreateBabelConfig = ({ actions }, pluginOptions) => {
     name: require.resolve(`@emotion/babel-preset-css-prop`),
     options: {
       sourceMap: process.env.NODE_ENV !== `production`,
-      autoLabel: process.env.NODE_ENV !== `production`,
+      autoLabel: `dev-only`,
       ...(pluginOptions ? pluginOptions : {}),
     },
   })
