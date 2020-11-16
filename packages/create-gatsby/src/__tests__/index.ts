d4400f6f3412c1665a75516fae13de10bfd0dfd5
mfrachet <marvin@gatsbyjs.com>
2020-11-16 12:56:53 +0100

    verify timeouts

--- packages/create-gatsby/src/__tests__/index.ts
+++ packages/create-gatsby/src/__tests__/index.ts
@@ -89,7 +89,7 @@ describe(`create-gatsby-app`, () => {
     )
   })
 
-  it(`creates a project with a project name and a CMS`, async () => {
+  it.only(`creates a project with a project name and a CMS`, async () => {
     cli.on(`prompt`, p => {
       p.on(`run`, async () => {
         switch (p.name) {
