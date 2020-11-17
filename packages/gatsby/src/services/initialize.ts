fd18243cefeff3b11622407a29181d9faa9f9234
Michal Piechowiak <misiek.piechowiak@gmail.com>
2020-11-17 17:41:21 +0100

    TIL: process.env.foo = undefined result in 'undefined' string being set and not resetting it

--- packages/gatsby/src/services/initialize.ts
+++ packages/gatsby/src/services/initialize.ts
@@ -166,9 +166,9 @@ export async function initialize({
     if (process.env.gatsby_executing_command !== `develop`) {
       // we don't want to ever have this flag enabled for anything than develop
       // in case someone have this env var globally set
-      process.env.GATSBY_EXPERIMENTAL_QUERY_ON_DEMAND = undefined
+      delete process.env.GATSBY_EXPERIMENTAL_QUERY_ON_DEMAND
     } else if (isCI()) {
-      process.env.GATSBY_EXPERIMENTAL_QUERY_ON_DEMAND = undefined
+      delete process.env.GATSBY_EXPERIMENTAL_QUERY_ON_DEMAND
       reporter.warn(
         `Experimental Query on Demand feature is not available in CI environment. Continuing with regular mode.`
       )
