7e121486d39bf215bac03781af45d9c970986149
Laurie <laurie@gatsbyjs.com>
2020-11-19 11:56:16 -0500

    shouldn't have saved it, oops

--- packages/gatsby-plugin-image/src/transforms/__testfixtures__/gatsby-plugin-image/compat.input.js
+++ packages/gatsby-plugin-image/src/transforms/__testfixtures__/gatsby-plugin-image/compat.input.js
@@ -6,12 +6,3 @@ fixed={data.file.childImageSharp.fixed}
  alt="headshot"/>
 
 //still doing ok?
-
-graphql`constrainedWidth: file(relativePath: {eq: "landscape.jpg"}) {
-    childImageSharp {
-      gatsbyImage(maxWidth: 600, layout: CONSTRAINED) {
-        imageData
-      }
-    }
-  }
-  `
