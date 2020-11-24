72660b54f3220e281b66d7c4f9ba10a91b74f701
Kyle Mathews <mathews.kyle@gmail.com>
2020-11-24 15:07:05 -0800

    load experiments from gatsby-config and properly add/remove

--- packages/gatsby-experiments-cli/src/index.js
+++ packages/gatsby-experiments-cli/src/index.js
@@ -1,23 +1,21 @@
-const React = require("react")
-const { render } = require("ink")
-const MultiSelect = require("ink-multi-select").default
+const React = require(`react`)
+const { render } = require(`ink`)
+const MultiSelect = require(`ink-multi-select`).default
 const { GatsbyExperiments } = require(`gatsby-recipes`)
 const _ = require(`lodash`)
+const { commaListsAnd } = require(`common-tags`)
 
 const experiments = require(`../gatsby/dist/experiments`).activeExperiments
 
 const Demo = () => {
+  const [enabled, setEnabled] = React.useState()
   const handleSubmit = async items => {
-    // `items` = [{ label: 'First', value: 'first' }, { label: 'Third', value: 'third' }]
-    console.log({ items })
     const currentExperiments = await GatsbyExperiments.all({
       root: `/Users/kylemathews/programs/blog`,
     })
-    console.log({ currentExperiments })
 
     const selected = items.map(i => i.value)
     const experimentNames = currentExperiments.map(e => e.id)
-    console.log({ selected, experimentNames })
 
     // Add experiments that are selected but not already in the gatsby-config.js
     const toAdd = _.without(selected, ...experimentNames)
@@ -25,21 +23,43 @@ const Demo = () => {
     // Remove experiments that are no longer selected but _are_ in the gatsby-config.js
     const toRemove = _.without(experimentNames, ...selected)
 
-    console.log({ toAdd, toRemove })
+    let promises = []
 
-    toAdd.forEach(item => {
-      GatsbyExperiments.create(
-        { root: `/Users/kylemathews/programs/blog` },
-        { name: item }
+    promises = promises.concat(
+      toAdd.map(item =>
+        GatsbyExperiments.create(
+          { root: `/Users/kylemathews/programs/blog` },
+          { name: item }
+        )
       )
-    })
-    toRemove.forEach(item => {
-      GatsbyExperiments.destroy(
-        { root: `/Users/kylemathews/programs/blog` },
-        { name: item }
+    )
+    promises = promises.concat(
+      toRemove.map(item =>
+        GatsbyExperiments.destroy(
+          { root: `/Users/kylemathews/programs/blog` },
+          { name: item }
+        )
       )
-    })
+    )
+
+    await Promise.all(promises)
+
+    if (!_.isEmpty(toAdd)) {
+      console.log(commaListsAnd`Enabled the experiments ${toAdd}`)
+    }
+    if (!_.isEmpty(toRemove)) {
+      console.log(commaListsAnd`Removed the experiments ${toRemove}`)
+    }
+
+    process.exit()
   }
+
+  // Get list of current experiments
+  React.useEffect(() => {
+    GatsbyExperiments.all({
+      root: `/Users/kylemathews/programs/blog`,
+    }).then(currentExperiments => setEnabled(currentExperiments))
+  }, [])
 
   const newItems = experiments.map(experiment => {
     const selectItem = {
@@ -49,7 +69,23 @@ const Demo = () => {
     return selectItem
   })
 
-  return <MultiSelect items={newItems} onSubmit={handleSubmit} />
+  if (enabled) {
+    const enabledMap = enabled.map(e => {
+      const item = {
+        value: e.id,
+      }
+      return item
+    })
+    return (
+      <MultiSelect
+        items={newItems}
+        onSubmit={handleSubmit}
+        defaultSelected={enabledMap}
+      />
+    )
+  } else {
+    return null
+  }
 }
 
 render(<Demo />)
