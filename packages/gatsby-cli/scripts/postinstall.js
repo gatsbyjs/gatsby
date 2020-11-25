9e0a25b1c5b789607d9b84e10c3a6e53465a332e
Emmett Kelleher <59432937+noodlesatgithub@users.noreply.github.com>
2020-11-25 14:25:49 +0800

    A better looking postinstall

--- packages/gatsby-cli/scripts/postinstall.js
+++ packages/gatsby-cli/scripts/postinstall.js
@@ -2,9 +2,9 @@ const chalk = require(`chalk`);
 
 const showSuccessMessage = () => {
   console.log(chalk.green(`Success!\n`));
-  console.log(chalk.cyan(`Welcome to the Gatsby CLI! Please visit `) +
+  console.log(chalk.magenta(`Welcome to the Gatsby CLI! Please visit `) +
     chalk.underline(`https://www.gatsbyjs.org/docs/gatsby-cli/`) +
-    chalk.cyan(` for more information.\n`));
+    chalk.magenta(` for more information.\n`));
 }
 
 try {
