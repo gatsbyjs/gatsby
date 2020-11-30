a48ef9bcfdf5e7383725774d2882d20d98dd77c5
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2020-11-30 19:52:35 +0530

    Run a dev server for functions

--- packages/gatsby-plugin-functions/src/gatsby-node.ts
+++ packages/gatsby-plugin-functions/src/gatsby-node.ts
@@ -3,7 +3,14 @@ import glob from "globby"
 import path from "path"
 import webpack from "webpack"
 
-import { ParentSpanPluginArgs, PluginOptions } from "gatsby"
+import { urlResolve } from "gatsby-core-utils"
+
+import {
+  ParentSpanPluginArgs,
+  PluginOptions,
+  CreateDevServerArgs,
+} from "gatsby"
+import { func } from "joi"
 
 export async function onPreInit(
   { reporter }: ParentSpanPluginArgs,
@@ -45,7 +52,7 @@ export async function onPreInit(
       const config = {
         entry: path.join(functionsDirectory, file),
         output: {
-          path: path.join(process.cwd(), `public`, `functions`),
+          path: path.join(process.cwd(), `.cache`, `functions`),
           filename: file.replace(`.ts`, `.js`),
           libraryTarget: `commonjs2`,
         },
@@ -85,4 +92,57 @@ export async function onPreInit(
   )
 
   activity.end()
+}
+
+export async function onCreateDevServer(
+  { reporter, app }: CreateDevServerArgs,
+  { path: functionsDirectoryPath }: PluginOptions
+): Promise<void> {
+  const functionsGlob = `**/*.{js,ts}`
+  const functionsDirectory = path.resolve(
+    process.cwd(),
+    functionsDirectoryPath as string
+  )
+  const files = await glob(functionsGlob, { cwd: functionsDirectory })
+
+  // console.log({ files })
+
+  const knownFunctions = new Map(
+    files.map(file => {
+      return [urlResolve(path.parse(file).dir, path.parse(file).name), file]
+    })
+  )
+  // console.log(app, functionsDirectoryPath)
+
+  app.use((req, res, next) => {
+    const functionName = urlResolve(...req.path.split(`/`).splice(2))
+
+    if (knownFunctions.has(functionName)) {
+      const fn = require(path.join(
+        functionsDirectory,
+        knownFunctions.get(functionName)
+      ))
+
+      return fn(req, res)
+    } else {
+      next()
+    }
+  })
+  // const activity = reporter.activityTimer(`building functions`)
+  // activity.start()
+  // const functionsDirectory = path.resolve(
+  //   process.cwd(),
+  //   functionsDirectoryPath as string
+  // )
+  // const functionsGlob = `**/*.{js,ts}`
+  // // Get initial list of files
+  // const files = await glob(functionsGlob, { cwd: functionsDirectory })
+  // // console.log({ files })
+  // const knownFunctions = new Set(files)
+  // // console.log({ knownFunctions })
+  // // if (![`develop`, `build-javascript`].includes(stage)) {
+  // //   return Promise.resolve()
+  // // }
+  // await fs.ensureDir(path.join(process.cwd(), `.cache`, `functions`))
+  // await fs.emptyDir(path.join(process.cwd(), `.cache`, `functions`))
 }
