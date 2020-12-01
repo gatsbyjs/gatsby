fe83ec924e72230c1cfce0db1517c5f81e8a8a69
Kyle Mathews <mathews.kyle@gmail.com>
2020-12-01 14:03:04 -0800

    feature(gatsby): style the SSR error page (#28416)
    * Add styles & tweak language
    
    * Fix small error noticed w/ source-maps
    
    * fix snapshot
    
    * update snapshot
    
    (cherry picked from commit 239589a2304911cb7c0b3a5736aa8b7f59f016b2)

--- packages/gatsby/src/utils/dev-ssr/develop-html-route.ts
+++ packages/gatsby/src/utils/dev-ssr/develop-html-route.ts
@@ -52,16 +52,112 @@ export const route = ({ app, program, store }): any =>
           column: error.row,
         },
       })
-      let errorHtml = `<title>Develop SSR Error</title><h1>Error<h1>
-        <h2>The page didn't SSR correctly</h2>
+      let errorHtml = `<head>
+       <title>Develop SSR Error</title>
+       <style>
+          * {
+            --gatsby: #663399;
+            --gatsbyLight: #9158ca;
+            --dimmedWhite: rgba(255, 255, 255, 0.8);
+            --white: #ffffff;
+            --black: #000000;
+            --color-ansi-selection: rgba(95, 126, 151, 0.48);
+            --color-ansi-bg: #fafafa;
+            --color-ansi-fg: #545454;
+            --color-ansi-white: #969896;
+            --color-ansi-black: #141414;
+            --color-ansi-blue: #183691;
+            --color-ansi-cyan: #007faa;
+            --color-ansi-green: #008000;
+            --color-ansi-magenta: #795da3;
+            --color-ansi-red: #d91e18;
+            --color-ansi-yellow: #aa5d00;
+            --color-ansi-bright-white: #ffffff;
+            --color-ansi-bright-black: #545454;
+            --color-ansi-bright-blue: #183691;
+            --color-ansi-bright-cyan: #007faa;
+            --color-ansi-bright-green: #008000;
+            --color-ansi-bright-magenta: #795da3;
+            --color-ansi-bright-red: #d91e18;
+            --color-ansi-bright-yellow: #aa5d00;
+            --radii: 5px;
+            --z-index-normal: 5;
+            --z-index-elevated: 10;
+            --space: 1.5em;
+            --space-sm: 1em;
+            --space-lg: 2.5em;
+          }
+          [data-gatsby-overlay="backdrop"] {
+            background: rgba(72, 67, 79, 0.5);
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            height: 100%;
+            width: 100%;
+            z-index: var(--z-index-normal);
+            backdrop-filter: blur(10px);
+          }
+          body {
+            font: 18px/1.5 -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+              Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+              "Segoe UI Symbol" !important;
+            background: var(--color-ansi-bright-white);
+            padding: var(--space);
+            overflow: auto;
+          }
+          h1,
+          h2,
+          h3 {
+            display: flex;
+            align-items: center;
+            color: var(--dimmedWhite);
+            background: var(--gatsby);
+            padding: var(--space);
+            border-top-left-radius: var(--radii);
+            border-top-right-radius: var(--radii);
+          }
+          code {
+            font-family: Consolas, "Andale Mono WT", "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", "Liberation Mono", "Nimbus Mono L", Monaco, "Courier New", Courier, monospace;
+          }
+          pre {
+            margin: 0;
+            color: var(--color-ansi-fg);
+            padding: var(--space-sm);
+            border-radius: var(--radii);
+          }
+          button {
+            cursor: pointer;
+            border: 1px;
+            padding: 10px;
+            background-color: var(--gatsbyLight);
+            color: var(--white);
+            appearance: none;
+            display: inline-flex;
+            align-items: center;
+            justify-content: center;
+            border-radius: var(--radii);
+          }
+        </style>
+       </head>
+        <h1>Error</h1>
+        <h2>The page didn't server render (SSR) correctly</h2>
+        <p style="padding-left: var(--space-sm);">
+          React components in Gatsby must render successfully in the browser and in a
+          node.js environment. When we tried to render your page component in
+          node.js, it errored.
+        </p>
         <ul>
           <li><strong>URL path:</strong> <code>${req.path}</code></li>
           <li><strong>File path:</strong> <code>${error.filename}</code></li>
         </ul>
-        <h3>error message</h3>
-        <p><code>${error.message}</code></p>`
+        <h3>error</h3>
+        <code style="padding: var(--space);padding-left: var(--space-sm);">${error.message}</code>
+        `
+
       if (error.codeFrame) {
-        errorHtml += `<pre style="background:#fdfaf6;padding:8px;">${error.codeFrame}</pre>`
+        errorHtml += `<pre>${error.codeFrame}</pre>`
       }
 
       // Add link to help page
@@ -72,11 +168,14 @@ export const route = ({ app, program, store }): any =>
       // Add skip ssr button
       errorHtml += `
         <h3>Skip SSR</h3>
-        <p>If you don't wish to fix the SSR error at the moment, press the
+        <p style="padding-left: var(--space-sm);">
+        If you don't wish to fix the SSR error at the moment, press the
         button below to reload the page without attempting SSR</p>
-        <p><strong>Note</strong>: this error will show up in when you build your site so must be fixed before then.</p>
-        <p><strong>Caveat</strong>: SSR errors in module scope i.e. outside of your components can't be skipped so will need fixed before you can continue</p>
-        <button onclick='refreshWithQueryString()'>Skip SSR</button>
+        <p style="padding-left: var(--space-sm);">
+        <strong>Note</strong>: this error will show up in when you build your site so must be fixed before then.</p>
+        <p style="padding-left: var(--space-sm);">
+        <strong>Caveat</strong>: SSR errors in module scope i.e. outside of your components can't be skipped so will need fixed before you can continue</p>
+        <button style="margin-left: var(--space-sm);" onclick='refreshWithQueryString()'>Skip SSR</button>
         <script>
           function refreshWithQueryString() {
             if ('URLSearchParams' in window) {
