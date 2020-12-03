856406f6846ba609ca59aede181c9b0ef4a8ad5e
Kyle Mathews <mathews.kyle@gmail.com>
2020-12-03 04:19:44 -0800

    chore(gatsby): specify in description for LAZY_IMAGES min supported version of gatsby-plugin-sharp (#28448)

--- packages/gatsby/src/utils/flags.ts
+++ packages/gatsby/src/utils/flags.ts
@@ -57,7 +57,7 @@ const activeFlags: Array<IFlag> = [
     command: `develop`,
     telemetryId: `LazyImageProcessing`,
     experimental: true,
-    description: `Don't process images during development until they're requested from the browser. Speeds starting the develop server.`,
+    description: `Don't process images during development until they're requested from the browser. Speeds starting the develop server. Requires gatsby-plugin-sharp@2.10.0 or above.`,
     umbrellaIssue: `https://github.com/gatsbyjs/gatsby/discussions/27603`,
   },
   {
