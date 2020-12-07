18e63e87914b071b60394e5739c692edd4eb23a1
Sam Slotsky <sam.slotsky@gatsbyjs.com>
2020-12-07 09:53:17 -0600

    Moar tests

--- e2e-tests/development-runtime/cypress/integration/gatsby-preview/refreshing.js
+++ e2e-tests/development-runtime/cypress/integration/gatsby-preview/refreshing.js
@@ -7,22 +7,48 @@ beforeEach(() => {
 
 after(() => reset())
 
-it(`only updates the data from the matching plugin`, () => {
-  cy
-    .get(`#pinc-data li`)
-    .should(`have.length`, 1)
+describe(`Gatsby Preview (Refreshing)`, () => {
+  it(`displays initial data`, () => {
+    cy
+      .get(`#pinc-data li`)
+      .should(`have.length`, 1)
 
-  cy
-    .get(`#fake-data li`)
-    .should(`have.length`, 1)
+    cy
+      .get(`#fake-data li`)
+      .should(`have.length`, 1)
+  })
 
-  cy.exec(`npm run update:cms-webhook gatsby-source-pinc-data`)
+  it(`updates no data for an unknown plugin`, () => {
+    cy.exec(`npm run update:cms-webhook gatsby-source-unknown`)
 
-  cy
-    .get(`#pinc-data li`)
-    .should(`have.length`, 2)
+    cy
+      .get(`#pinc-data li`)
+      .should(`have.length`, 1)
 
-  cy
-    .get(`#fake-data li`)
-    .should(`have.length`, 1)
+    cy
+      .get(`#fake-data li`)
+      .should(`have.length`, 1)
+  })
+
+  it(`updates the data from the matching plugin`, () => {
+    cy.exec(`npm run update:cms-webhook gatsby-source-pinc-data`)
+
+    cy
+      .get(`#pinc-data li`)
+      .should(`have.length`, 2)
+
+    cy
+      .get(`#fake-data li`)
+      .should(`have.length`, 1)
+
+    cy.exec(`npm run update:cms-webhook gatsby-source-fake-data`)
+
+    cy
+      .get(`#pinc-data li`)
+      .should(`have.length`, 2)
+
+    cy
+      .get(`#fake-data li`)
+      .should(`have.length`, 2)
+  })
 })
