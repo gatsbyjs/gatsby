72c61d64bf7aa56048a133fb1cc46344ddfbc39f
Michal Piechowiak <misiek.piechowiak@gmail.com>
2020-12-08 17:58:46 +0100

    chore(query-on-demand): prerun query for homepage for initial run

--- packages/gatsby/src/commands/develop-process.ts
+++ packages/gatsby/src/commands/develop-process.ts
@@ -132,6 +132,7 @@ module.exports = async (program: IDevelopArgs): Promise<void> => {
     program,
     parentSpan,
     app,
+    pendingQueryRuns: new Set([`/`]),
   })
 
   const service = interpret(machine)
