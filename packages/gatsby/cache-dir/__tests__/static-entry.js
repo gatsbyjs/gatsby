1f9fbdc731184bf880711cf0a3c6388e9c7fbb40
Kyle Mathews <mathews.kyle@gmail.com>
2020-12-07 23:21:21 -0800

    Update tests for signature change

--- packages/gatsby/cache-dir/__tests__/static-entry.js
+++ packages/gatsby/cache-dir/__tests__/static-entry.js
@@ -179,7 +179,7 @@ describe(`develop-static-entry`, () => {
   test(`SSR: onPreRenderHTML can be used to replace headComponents`, done => {
     global.plugins = [fakeStylesPlugin, reverseHeadersPlugin]
 
-    ssrDevelopStaticEntry(`/about/`, false, (_, html) => {
+    ssrDevelopStaticEntry(`/about/`, false, 8000, (_, html) => {
       expect(html).toMatchSnapshot()
       done()
     })
@@ -191,7 +191,7 @@ describe(`develop-static-entry`, () => {
       reverseBodyComponentsPluginFactory(`Post`),
     ]
 
-    ssrDevelopStaticEntry(`/about/`, false, (_, html) => {
+    ssrDevelopStaticEntry(`/about/`, false, 8000, (_, html) => {
       expect(html).toMatchSnapshot()
       done()
     })
@@ -203,14 +203,14 @@ describe(`develop-static-entry`, () => {
       reverseBodyComponentsPluginFactory(`Pre`),
     ]
 
-    ssrDevelopStaticEntry(`/about/`, false, (_, html) => {
+    ssrDevelopStaticEntry(`/about/`, false, 8000, (_, html) => {
       expect(html).toMatchSnapshot()
       done()
     })
   })
 
   test(`SSR: onPreRenderHTML adds metatag note for development environment`, done => {
-    ssrDevelopStaticEntry(`/about/`, false, (_, html) => {
+    ssrDevelopStaticEntry(`/about/`, false, 8000, (_, html) => {
       expect(html).toContain(
         `<meta name="note" content="environment=development"/>`
       )
@@ -221,7 +221,7 @@ describe(`develop-static-entry`, () => {
   test(`SSR: onPreRenderHTML adds metatag note for development environment after replaceHeadComponents`, done => {
     global.plugins = [reverseHeadersPlugin]
 
-    ssrDevelopStaticEntry(`/about/`, false, (_, html) => {
+    ssrDevelopStaticEntry(`/about/`, false, 8000, (_, html) => {
       expect(html).toContain(
         `<meta name="note" content="environment=development"/>`
       )
