248afada82b906d3481aeadc2fa35c3f91a6c529
Michal Piechowiak <misiek.piechowiak@gmail.com>
2020-12-12 00:08:24 +0100

    some tests should be fixed

--- packages/gatsby-cli/src/reporter/loggers/ink/components/spinner.tsx
+++ packages/gatsby-cli/src/reporter/loggers/ink/components/spinner.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Box, Text } from "ink"
-import * as InkSpinner from "ink-spinner"
+import InkSpinner from "ink-spinner"
 
 interface ISpinnerProps {
   text: string
@@ -15,7 +15,7 @@ export function Spinner({ text, statusText }: ISpinnerProps): JSX.Element {
   return (
     <Box>
       <Text>
-        <InkSpinner.default type="dots" /> {label}
+        <InkSpinner type="dots" /> {label}
       </Text>
     </Box>
   )
