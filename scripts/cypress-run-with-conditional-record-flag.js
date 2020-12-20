396071a9cb244dc8f1db2e5a90c98bbaf58ea625
Michal Piechowiak <misiek.piechowiak@gmail.com>
2020-12-20 11:16:09 +0100

    execFile instead of using programmatic api - hopefully less things to maintain

--- scripts/cypress-run-with-conditional-record-flag.js
+++ scripts/cypress-run-with-conditional-record-flag.js
@@ -1,63 +1,22 @@
-const yargs = require(`yargs`)
-const { isCI, createRequireFromPath } = require(`gatsby-core-utils`)
-const cwdRequire = createRequireFromPath(`${process.cwd()}/:internal:`)
+const childProcess = require(`child_process`)
+const path = require(`path`)
+const { isCI } = require(`gatsby-core-utils`)
 
-const cliParser = yargs
-  .strict()
-  .option(`browser`, { type: `string` })
-  .option(`env`, { type: `string`, array: true })
-  .option(`config`, { type: `string`, array: true })
+const shouldRecord =
+  !!process.env.CYPRESS_PROJECT_ID && !!process.env.CYPRESS_RECORD_KEY && isCI()
 
-const args = cliParser.argv
+const cypressBin = path.join(process.cwd(), `node_modules/.bin/cypress`)
 
-if (args._.length > 0) {
-  console.log(`Unknown command (hint there are no commands, just options)`)
-  cliParser.showHelp()
-  process.exit(1)
+// first arg is node binary itself
+// second arg is .js file entry point (as in - path to this file)
+// we only care about extra args
+const cypressArgs = [`run`, ...process.argv.slice(2)]
+
+if (shouldRecord) {
+  cypressArgs.push(`--record`)
 }
 
-// need to parse --env and --config assignments
-function parseAssignments(assignmentsItems) {
-  if (!assignmentsItems) {
-    return undefined
-  }
-
-  const parsed = {}
-  // assignmentsItems will be array of strings like "A=1", "A=1,B=2"
-  assignmentsItems.forEach(assignments => {
-    // assignments will have shape "A=1", "A=1,B=2" etc
-    assignments.split(`,`).forEach(assignment => {
-      // assignment can be "A=1"
-      const [key, value] = assignment.split(`=`)
-      if (!key) {
-        throw new Error(`Assignment "${assignment} doesn't have a key`)
-      }
-      if (!value) {
-        throw new Error(`Assignment "${assignment} doesn't have a value`)
-      }
-
-      parsed[key] = value
-    })
-  })
-
-  return parsed
-}
-
-const cypressArgs = {
-  browser: args.browser,
-  env: parseAssignments(args.env),
-  config: parseAssignments(args.config),
-  record:
-    process.env.CYPRESS_PROJECT_ID && process.env.CYPRESS_RECORD_KEY && isCI(),
-}
-
-const cypress = cwdRequire(`cypress`)
-
-cypress
-  .run(cypressArgs)
-  .then(result => {
-    process.exit(result.totalFailed > 0 ? 1 : 0)
-  })
-  .catch(() => {
-    process.exit(1)
-  })
+childProcess.execFileSync(cypressBin, cypressArgs, {
+  cwd: process.cwd(),
+  stdio: `inherit`,
+})
