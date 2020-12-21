89d23c6244255855c0ee6272c328f575b1025c4f
Kyle Mathews <mathews.kyle@gmail.com>
2020-12-21 12:01:01 -0800

    feature(gatsby): Pause dev-ssr watching between page loads to avoid slowing down regular develop-js HMR (#28394)
    * feature(gatsby): Pause dev-ssr watching between page loads to avoid slowing down regular develop-js HMR
    
    * update snapshot
    
    * Don't double-resolve + add activity for building the SSR bundle
    
    * Add timeout for tests to ensure that dev server has time to bundle SSR + remove activity timers as not helpful
    
    * Update packages/gatsby/src/commands/build-html.ts
    
    Co-authored-by: Michal Piechowiak <misiek.piechowiak@gmail.com>
    
    * fix typo
    
    * Don't resume if nothing has changed
    
    * Update packages/gatsby/src/commands/build-html.ts
    
    Co-authored-by: Michal Piechowiak <misiek.piechowiak@gmail.com>
    
    * Didn't need this
    
    Co-authored-by: Michal Piechowiak <misiek.piechowiak@gmail.com>
    (cherry picked from commit 8ff6245f44882d891bc6f291363019047cbdb55f)

--- packages/gatsby/src/commands/build-html.ts
+++ packages/gatsby/src/commands/build-html.ts
@@ -6,6 +6,7 @@ import telemetry from "gatsby-telemetry"
 import { chunk } from "lodash"
 import webpack from "webpack"
 
+import { emitter } from "../redux"
 import webpackConfig from "../utils/webpack.config"
 import { structureWebpackErrors } from "../utils/webpack-error-utils"
 
@@ -13,6 +14,30 @@ import { IProgram, Stage } from "./types"
 
 type IActivity = any // TODO
 type IWorkerPool = any // TODO
+
+export interface IWebpackWatchingPauseResume extends webpack.Watching {
+  suspend: () => void
+  resume: () => void
+}
+
+let devssrWebpackCompiler: webpack.Compiler
+let devssrWebpackWatcher: IWebpackWatchingPauseResume
+let needToRecompileSSRBundle = true
+export const getDevSSRWebpack = (): Record<
+  IWebpackWatchingPauseResume,
+  webpack.Compiler,
+  needToRecompileSSRBundle
+> => {
+  if (process.env.gatsby_executing_command !== `develop`) {
+    throw new Error(`This function can only be called in development`)
+  }
+
+  return {
+    devssrWebpackWatcher,
+    devssrWebpackCompiler,
+    needToRecompileSSRBundle,
+  }
+}
 
 let oldHash = ``
 let newHash = ``
@@ -34,11 +59,19 @@ const runWebpack = (
       process.env.GATSBY_EXPERIMENTAL_DEV_SSR &&
       stage === `develop-html`
     ) {
-      webpack(compilerConfig).watch(
+      devssrWebpackCompiler = webpack(compilerConfig)
+      devssrWebpackCompiler.hooks.invalid.tap(`ssr file invalidation`, file => {
+        needToRecompileSSRBundle = true
+      })
+      devssrWebpackWatcher = devssrWebpackCompiler.watch(
         {
           ignored: /node_modules/,
         },
         (err, stats) => {
+          needToRecompileSSRBundle = false
+          emitter.emit(`DEV_SSR_COMPILATION_DONE`)
+          devssrWebpackWatcher.suspend()
+
           if (err) {
             return reject(err)
           } else {
