b1506c0a5650fe5a11512009d026b08a0381746f
Kyle Mathews <mathews.kyle@gmail.com>
2020-12-22 09:29:08 -0800

    Stop Jest from chocking on import of css

--- integration-tests/ssr/jest.config.js
+++ integration-tests/ssr/jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   testPathIgnorePatterns: [`/node_modules/`, `__tests__/fixtures`, `.cache`],
+  transform: {
+    "^.+\\.[jt]sx?$": `<rootDir>../../jest-transformer.js`,
+  },
+  moduleNameMapper: {
+    "\\.(css)$": `<rootDir>/__mocks__/styleMock.js`,
+  },
 }
