cd92d1ba39353552e5874ebc02c514077cc28420
Vladimir Razuvaev <vladimir.razuvaev@gmail.com>
2020-12-22 19:12:21 +0700

    Revert "be more defensive"
    This reverts commit e1394864

--- packages/gatsby/src/schema/schema.js
+++ packages/gatsby/src/schema/schema.js
@@ -1060,7 +1060,7 @@ const addInferredChildOfExtension = ({ schemaComposer, typeComposer }) => {
     // to be added in `addConvenienceChildrenFields` method.
     // Also required for proper printing of the `@childOf` directive in the snapshot plugin
     if (!childOfExtension) {
-      childOfExtension = { types: [], mimeTypes: [] }
+      childOfExtension = {}
     }
     if (!childOfExtension.types) {
       childOfExtension.types = []
