6e10b1d1cb7a851c962556668f97ce2dbdd94dcc
Matt Kane <matt@gatsbyjs.com>
2021-01-05 12:21:14 +0000

    Update e2e tests

---
+++ e2e-tests/gatsby-static-image/cypress/integration/constrained.js
@@ -0,0 +1,26 @@
+describe(`constrained`, () => {
+  beforeEach(() => {
+    cy.visit(`/constrained`).waitForRouteChange()
+  })
+
+  it(`renders a spacer svg`, () => {
+    cy.getTestElement(`image-constrained`)
+      .find(`img[role=presentation]`)
+      .should(`have.attr`, `src`)
+      .and(`match`, /svg\+xml/)
+  })
+
+  it(`includes sizes attribute with default width`, () => {
+    cy.getTestElement(`image-constrained`)
+      .find(`[data-main-image]`)
+      .should(`have.attr`, `sizes`)
+      .and(`equal`, `(min-width: 4015px) 4015px, 100vw`)
+  })
+
+  it(`includes sizes attribute with specified width`, () => {
+    cy.getTestElement(`image-constrained-limit`)
+      .find(`[data-main-image]`)
+      .should(`have.attr`, `sizes`)
+      .and(`equal`, `(min-width: 500px) 500px, 100vw`)
+  })
+})
