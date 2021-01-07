5f711fc98c7687640ca3a970602a10fa41b22d84
Lennart <lekoarts@gmail.com>
2021-01-07 09:13:11 +0100

    chore(gatsby): Add `flags` to GatsbyConfig interface (#28887)

--- packages/gatsby/index.d.ts
+++ packages/gatsby/index.d.ts
@@ -185,6 +185,8 @@ export interface GatsbyConfig {
   siteMetadata?: Record<string, unknown>
   /** Plugins are Node.js packages that implement Gatsby APIs. The config file accepts an array of plugins. Some plugins may need only to be listed by name, while others may take options. */
   plugins?: Array<PluginRef>
+  /** You can activate and deactivate current experiments here. These are experimental features that are currently under development and need testing. When opting in to an experiment you'll receive a console message with more information of what it does and a link to an umbrella discussion. */
+  flags?: Record<string, boolean>
   /** It’s common for sites to be hosted somewhere other than the root of their domain. Say we have a Gatsby site at `example.com/blog/`. In this case, we would need a prefix (`/blog`) added to all paths on the site. */
   pathPrefix?: string
   /** In some circumstances you may want to deploy assets (non-HTML resources such as JavaScript, CSS, etc.) to a separate domain. `assetPrefix` allows you to use Gatsby with assets hosted from a separate domain */
