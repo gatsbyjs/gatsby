177d409e03465622a5ef2b16ff93e4017d0684bc
Max Stoiber <contact@mxstbr.com>
2021-01-12 14:27:53 +0100

    Update packages/gatsby-plugin-sharp/src/index.js
    Co-authored-by: Matt Kane <matt@gatsbyjs.com>

--- packages/gatsby-plugin-sharp/src/index.js
+++ packages/gatsby-plugin-sharp/src/index.js
@@ -333,7 +333,7 @@ async function generateBase64({ file, args = {}, reporter }) {
       pipeline = await duotone(options.duotone, options.toFormat, pipeline)
     } else {
       reporter.warn(
-        `To use duotone for ${file.absolutPath}, please specify duotone.highlight and duotone.shadow as the two hex colors you want to use.`
+        `To use duotone for ${file.absolutePath}, please specify duotone.highlight and duotone.shadow as the two hex colors you want to use.`
       )
     }
   }
