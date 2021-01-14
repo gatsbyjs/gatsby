cfc68227044e7e7ed53b41bdd4e21ef31b3fefa7
Benedikt Rötsch <opensource@axe312.dev>
2021-01-14 12:22:23 +0100

    build: upgrade to latest Contentful SDK

--- packages/gatsby-source-contentful/src/fetch.js
+++ packages/gatsby-source-contentful/src/fetch.js
@@ -3,7 +3,6 @@ const _ = require(`lodash`)
 const chalk = require(`chalk`)
 const { formatPluginOptionsForCLI } = require(`./plugin-options`)
 const { CODES } = require(`./report`)
-const { version } = require(`../package.json`)
 
 module.exports = async function contentfulFetch({
   syncToken,
@@ -20,7 +19,7 @@ module.exports = async function contentfulFetch({
     host: pluginConfig.get(`host`),
     environment: pluginConfig.get(`environment`),
     proxy: pluginConfig.get(`proxy`),
-    integration: `gatsby-source-contentful@${version}`,
+    integration: `gatsby-source-contentful`,
     responseLogger: response => {
       function createMetadataLog(response) {
         if (process.env.gatsby_log_level === `verbose`) {
