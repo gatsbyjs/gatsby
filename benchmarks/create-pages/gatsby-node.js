783fe9d7c7eee2888a1a5e8e9a7deb2018f8e78a
Kyle Mathews <mathews.kyle@gmail.com>
2021-01-25 20:26:53 -0800

    cache require.resolve

--- benchmarks/create-pages/gatsby-node.js
+++ benchmarks/create-pages/gatsby-node.js
@@ -1,10 +1,11 @@
 const NUM_PAGES = parseInt(process.env.NUM_PAGES || 5000, 10)
 
+const component = require.resolve(`./src/templates/blank.js`)
 exports.createPages = ({ actions: { createPage } }) => {
   for (let step = 0; step < NUM_PAGES; step++) {
     createPage({
       path: `/path/${step}/`,
-      component: require.resolve(`./src/templates/blank.js`),
+      component,
       context: {
         id: step,
       },
