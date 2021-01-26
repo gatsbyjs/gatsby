557df4f8c6515140126cde668c9983e0d27f5c5b
Vladimir Razuvaev <vladimir.razuvaev@gmail.com>
2021-01-26 19:09:14 +0700

    gatsby-plugin-image: fix incorrect enum value
    GraphQL 15 is stricter with enum values after https://github.com/graphql/graphql-js/pull/1827

--- packages/gatsby-plugin-image/src/resolver-utils.ts
+++ packages/gatsby-plugin-image/src/resolver-utils.ts
@@ -56,14 +56,14 @@ export function getGatsbyImageFieldConfig<TSource, TContext>(
         description: stripIndent`
             The layout for the image.
             FIXED: A static image sized, that does not resize according to the screen width
-            FULL_WIDTH: The image resizes to fit its container. Pass a "sizes" option if it isn't going to be the full width of the screen. 
+            FULL_WIDTH: The image resizes to fit its container. Pass a "sizes" option if it isn't going to be the full width of the screen.
             CONSTRAINED: Resizes to fit its container, up to a maximum width, at which point it will remain fixed in size.
             `,
       },
       width: {
         type: GraphQLInt,
         description: stripIndent`
-        The display width of the generated image for layout = FIXED, and the display width of the largest image for layout = CONSTRAINED.  
+        The display width of the generated image for layout = FIXED, and the display width of the largest image for layout = CONSTRAINED.
         The actual largest image resolution will be this value multiplied by the largest value in outputPixelDensities
         Ignored if layout = FLUID.
         `,
@@ -77,26 +77,26 @@ export function getGatsbyImageFieldConfig<TSource, TContext>(
         type: ImagePlaceholderType,
         defaultValue: `blurred`,
         description: stripIndent`
-            Format of generated placeholder image, displayed while the main image loads. 
+            Format of generated placeholder image, displayed while the main image loads.
             BLURRED: a blurred, low resolution image, encoded as a base64 data URI (default)
-            DOMINANT_COLOR: a solid color, calculated from the dominant color of the image. 
+            DOMINANT_COLOR: a solid color, calculated from the dominant color of the image.
             TRACED_SVG: a low-resolution traced SVG of the image.
             NONE: no placeholder. Set "background" to use a fixed background color.`,
       },
       formats: {
         type: GraphQLList(ImageFormatType),
         description: stripIndent`
-            The image formats to generate. Valid values are AUTO (meaning the same format as the source image), JPG, PNG, WEBP and AVIF. 
+            The image formats to generate. Valid values are AUTO (meaning the same format as the source image), JPG, PNG, WEBP and AVIF.
             The default value is [AUTO, WEBP], and you should rarely need to change this. Take care if you specify JPG or PNG when you do
-            not know the formats of the source images, as this could lead to unwanted results such as converting JPEGs to PNGs. Specifying 
+            not know the formats of the source images, as this could lead to unwanted results such as converting JPEGs to PNGs. Specifying
             both PNG and JPG is not supported and will be ignored. AVIF support is currently experimental.
         `,
-        defaultValue: [`auto`, `webp`],
+        defaultValue: [``, `webp`],
       },
       outputPixelDensities: {
         type: GraphQLList(GraphQLFloat),
         description: stripIndent`
-            A list of image pixel densities to generate. It will never generate images larger than the source, and will always include a 1x image. 
+            A list of image pixel densities to generate. It will never generate images larger than the source, and will always include a 1x image.
             Default is [ 1, 2 ] for fixed images, meaning 1x, 2x, 3x, and [0.25, 0.5, 1, 2] for fluid. In this case, an image with a fluid layout and width = 400 would generate images at 100, 200, 400 and 800px wide
             `,
       },
@@ -104,7 +104,7 @@ export function getGatsbyImageFieldConfig<TSource, TContext>(
         type: GraphQLString,
         defaultValue: ``,
         description: stripIndent`
-            The "sizes" property, passed to the img tag. This describes the display size of the image. 
+            The "sizes" property, passed to the img tag. This describes the display size of the image.
             This does not affect the generated images, but is used by the browser to decide which images to download. You can leave this blank for fixed images, or if the responsive image
             container will be the full width of the screen. In these cases we will generate an appropriate value.
         `,
