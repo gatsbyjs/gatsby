a8b2efafea5210c938ec404b0c44bd66407c90b5
Max Stoiber <contact@mxstbr.com>
2021-01-27 09:47:14 +0100

    react-helmet-async supports fragments!

--- packages/gatsby/cache-dir/head/index.js
+++ packages/gatsby/cache-dir/head/index.js
@@ -5,31 +5,40 @@ import { useLocation } from "@reach/router"
 export const Head = props => {
   const location = useLocation()
 
-  const { title, description, image } = props
+  // This supports passing a gatsby-plugin-image object to the image prop, e.g.
+  // image={getImage(data.metaImage)}
+  const image = props.image?.images?.fallback?.src || props.image
+  const { title, description } = props
 
-  // NOTE(@mxstbr): This code has many ternaries for the same things because
-  // react-helmet doesn't support fragments yet
-  // @see github.com/nfl/react-helmet/issues/342
   return (
     <Helmet>
-      {/* Title */}
-      {title && <title>{title}</title>}
-      {title && <meta name="twitter:title" content={title} />}
-      {title && <meta property="og:title" content={title} />}
-
-      {/* Description */}
-      {description && <meta name="description" content={description} />}
-      {description && <meta name="twitter:description" content={description} />}
-      {description && <meta property="og:description" content={description} />}
-
-      {/* Image */}
-      {image && <meta name="twitter:image" content={image} />}
-      {image && <meta property="og:image" content={image} />}
-
       {/* General meta tags */}
-      <meta name="twitter:card" content="summary_large_image" />
       <meta property="og:type" content="website" />
       <meta property="og:url" content={location.href} />
+
+      {title && (
+        <>
+          <title>{title}</title>
+          <meta name="twitter:title" content={title} />
+          <meta property="og:title" content={title} />
+        </>
+      )}
+
+      {description && (
+        <>
+          <meta name="description" content={description} />
+          <meta name="twitter:description" content={description} />
+          <meta property="og:description" content={description} />
+        </>
+      )}
+
+      {image && (
+        <>
+          <meta name="twitter:image" content={image} />
+          <meta property="og:image" content={image} />
+          <meta name="twitter:card" content="summary_large_image" />
+        </>
+      )}
 
       {/* props.children must be at the end so users can override everything */}
       {props.children}
