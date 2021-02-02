760ae1789904d36bc32bda482544ca94fc001f8e
Tyler Barnes <tylerdbarnes@gmail.com>
2021-02-02 15:38:55 -0800

    temporarily remove starter (it has it's own repo still)

--- starters/gatsby-starter-wordpress-blog/gatsby-browser.js
+++
@@ -1,7 +0,0 @@
-// custom typefaces
-import "typeface-montserrat"
-import "typeface-merriweather"
-// normalize CSS across browsers
-import "./src/normalize.css"
-// custom CSS styles
-import "./src/style.css"
