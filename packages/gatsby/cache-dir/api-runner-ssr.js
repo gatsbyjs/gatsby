2ff695b7d8c7d8a26213baa9abb16461b3564e46
Michal Piechowiak <misiek.piechowiak@gmail.com>
2021-02-08 20:11:35 +0100

    print plugin name that thrown

--- packages/gatsby/cache-dir/api-runner-ssr.js
+++ packages/gatsby/cache-dir/api-runner-ssr.js
@@ -24,11 +24,15 @@ module.exports = (api, args, defaultReturn, argTransform) => {
     if (!plugin.plugin[api]) {
       return undefined
     }
-    const result = plugin.plugin[api](args, plugin.options)
-    if (result && argTransform) {
-      args = argTransform({ args, result })
+    try {
+      const result = plugin.plugin[api](args, plugin.options)
+      if (result && argTransform) {
+        args = argTransform({ args, result })
+      }
+      return result
+    } catch (e) {
+      throw new Error(`Plugin: ${plugin.name}: ${e.message}`)
     }
-    return result
   })
 
   // Filter out undefined results.
