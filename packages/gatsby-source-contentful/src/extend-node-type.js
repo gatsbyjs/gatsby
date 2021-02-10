e567aa82695cf19a7d9eb4c280be767e562c3976
Matt Kane <matt@gatsbyjs.com>
2021-02-10 16:48:53 +0000

    feat(gatsby-plugin-image): Add resolver helper and improve custom hook (#29342)
    * feat(gatsby-plugin-image): Add url builder helper hook
    
    * Unify API
    
    * Refactor
    
    * Fix types
    
    * Add resolver
    
    * Add breakpoints
    
    * Add background color
    
    * lint
    
    * Fix default handling
    
    * Fix import
    
    * Apply suggestions from code review
    
    Co-authored-by: LB <laurie@gatsbyjs.com>
    
    Co-authored-by: gatsbybot <mathews.kyle+gatsbybot@gmail.com>
    Co-authored-by: LB <laurie@gatsbyjs.com>

--- packages/gatsby-source-contentful/src/extend-node-type.js
+++ packages/gatsby-source-contentful/src/extend-node-type.js
@@ -767,9 +767,6 @@ exports.extendNodeType = ({ type, store, reporter }) => {
         type: GraphQLInt,
         defaultValue: 50,
       },
-      backgroundColor: {
-        type: GraphQLString,
-      },
     })
   }
 
