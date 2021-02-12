31e0ca44c6bcc86967e74612c238ecb78405ea0a
Simeon Babev <harveysmurf@abv.bg>
2021-02-12 12:38:56 +0200

    Improve assertion in OutboundLink tests

--- packages/gatsby-plugin-google-gtag/src/__tests__/index.js
+++ packages/gatsby-plugin-google-gtag/src/__tests__/index.js
@@ -2,62 +2,61 @@ import React from "react"
 import { cleanup, fireEvent, render } from "@testing-library/react"
 import { OutboundLink } from "../"
 
-describe(`index.js`, () => {
-  describe(`<OutboundLink />`, () => {
-    afterEach(() => {
-      cleanup()
-      jest.resetAllMocks()
+describe(`<OutboundLink />`, () => {
+  afterEach(() => {
+    cleanup()
+    jest.resetAllMocks()
+  })
+
+  const setup = props => {
+    const utils = render(<OutboundLink {...props}>link</OutboundLink>)
+
+    return Object.assign({}, utils, {
+      link: utils.getByText(`link`),
     })
+  }
 
-    const setup = props => {
-      const utils = render(<OutboundLink {...props}>link</OutboundLink>)
+  it(`matches basic snapshot`, () => {
+    const { container } = setup()
 
-      return Object.assign({}, utils, {
-        link: utils.getByText(`link`),
-      })
-    }
+    expect(container).toMatchSnapshot()
+  })
 
-    it(`matches basic snapshot`, () => {
-      const { container } = setup()
-
-      expect(container).toMatchSnapshot()
+  describe(`tracking`, () => {
+    beforeEach(() => {
+      window.gtag = jest.fn()
     })
+    it(`sends tracking event when clicked`, () => {
+      const { link } = setup()
 
-    describe(`tracking`, () => {
-      beforeEach(() => {
-        window.gtag = jest.fn()
+      fireEvent.click(link)
+
+      expect(window.gtag).toHaveBeenCalledTimes(1)
+    })
+    it(`sends custom tracking event when clicked`, () => {
+      const eventCategory = `eventcategory`
+      const eventAction = `eventaction`
+      const eventLabel = `eventlabel`
+      const eventValue = 55
+      const { link } = setup({
+        eventCategory,
+        eventAction,
+        eventLabel,
+        eventValue,
       })
-      it(`sends tracking event when clicked`, () => {
-        const { link } = setup()
 
-        fireEvent.click(link)
+      fireEvent.click(link)
 
-        expect(window.gtag).toHaveBeenCalled()
-      })
-      it(`sends custom tracking event when clicked`, () => {
-        const eventCategory = `eventcategory`
-        const eventAction = `eventaction`
-        const eventLabel = `eventlabel`
-        const eventValue = 55
-        const { link } = setup({
-          eventCategory,
-          eventAction,
-          eventLabel,
-          eventValue,
+      expect(window.gtag).toHaveBeenCalledTimes(1)
+      expect(window.gtag).toHaveBeenCalledWith(
+        `event`,
+        eventAction,
+        expect.objectContaining({
+          event_category: eventCategory,
+          event_label: eventLabel,
+          event_value: eventValue,
         })
-
-        fireEvent.click(link)
-
-        expect(window.gtag).toHaveBeenCalledWith(
-          `event`,
-          eventAction,
-          expect.objectContaining({
-            event_category: eventCategory,
-            event_label: eventLabel,
-            event_value: eventValue,
-          })
-        )
-      })
+      )
     })
   })
 })
