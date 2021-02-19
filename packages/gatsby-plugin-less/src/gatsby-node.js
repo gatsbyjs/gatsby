f7bb97d3873edc251258b2f1715db3a581510a09
Ward Peeters <ward@coding-tech.com>
2021-02-19 12:22:11 +0100

    update gatsby-plugin-less

--- packages/gatsby-plugin-less/src/gatsby-node.js
+++ packages/gatsby-plugin-less/src/gatsby-node.js
@@ -1,17 +1,14 @@
 import resolve from "./resolve"
 
 exports.onCreateWebpackConfig = (
-  { actions, stage, rules, plugins, loaders },
+  { actions, loaders },
   { cssLoaderOptions = {}, postCssPlugins, loaderOptions, lessOptions }
 ) => {
   const { setWebpackConfig } = actions
-  const PRODUCTION = stage !== `develop`
-  const isSSR = stage.includes(`html`)
 
   const lessLoader = {
     loader: resolve(`less-loader`),
     options: {
-      sourceMap: !PRODUCTION,
       lessOptions: {
         ...lessOptions,
       },
@@ -21,39 +18,28 @@ exports.onCreateWebpackConfig = (
 
   const lessRule = {
     test: /\.less$/,
-    use: isSSR
-      ? [loaders.null()]
-      : [
-          loaders.miniCssExtract(),
-          loaders.css({ ...cssLoaderOptions, importLoaders: 2 }),
-          loaders.postcss({ plugins: postCssPlugins }),
-          lessLoader,
-        ],
+    use: [
+      loaders.miniCssExtract(),
+      loaders.css({ ...cssLoaderOptions, importLoaders: 2 }),
+      loaders.postcss({ plugins: postCssPlugins }),
+      lessLoader,
+    ],
   }
   const lessRuleModules = {
     test: /\.module\.less$/,
     use: [
-      !isSSR && loaders.miniCssExtract({ hmr: false }),
+      loaders.miniCssExtract(),
       loaders.css({ ...cssLoaderOptions, modules: true, importLoaders: 2 }),
       loaders.postcss({ plugins: postCssPlugins }),
       lessLoader,
-    ].filter(Boolean),
+    ],
   }
 
-  let configRules = []
-
-  switch (stage) {
-    case `develop`:
-    case `build-javascript`:
-    case `build-html`:
-    case `develop-html`:
-      configRules = configRules.concat([
-        {
-          oneOf: [lessRuleModules, lessRule],
-        },
-      ])
-      break
-  }
+  const configRules = [
+    {
+      oneOf: [lessRuleModules, lessRule],
+    },
+  ]
 
   setWebpackConfig({
     module: {
