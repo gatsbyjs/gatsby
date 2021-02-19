ff6b3e3650380004730a645e50b8483f78c2d92f
LekoArts <lekoarts@gmail.com>
2021-02-19 10:04:11 +0100

    change close button a bit

--- packages/gatsby/cache-dir/fast-refresh-overlay/components/overlay.js
+++ packages/gatsby/cache-dir/fast-refresh-overlay/components/overlay.js
@@ -4,6 +4,27 @@ function Backdrop() {
   return <div data-gatsby-overlay="backdrop" />
 }
 
+export function VisuallyHidden({ children }) {
+  return (
+    <span
+      style={{
+        border: 0,
+        clip: `rect(0 0 0 0)`,
+        height: `1px`,
+        margin: `-1px`,
+        overflow: `hidden`,
+        padding: 0,
+        position: `absolute`,
+        width: `1px`,
+        whiteSpace: `nowrap`,
+        wordWrap: `normal`,
+      }}
+    >
+      {children}
+    </span>
+  )
+}
+
 export function Overlay({ children }) {
   return (
     <>
@@ -31,18 +52,16 @@ export function Header({ open = undefined, dismiss, children }) {
             Open in editor
           </button>
         )}
-        <button
-          data-gatsby-overlay="header__close-button"
-          aria-label="Close error overlay"
-          onClick={dismiss}
-        >
+        <button data-gatsby-overlay="header__close-button" onClick={dismiss}>
           <svg
+            aria-hidden={true}
             width="24"
             height="24"
             viewBox="0 0 24 24"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
           >
+            <VisuallyHidden>Close</VisuallyHidden>
             <path
               d="M18 6L6 18"
               stroke="currentColor"
