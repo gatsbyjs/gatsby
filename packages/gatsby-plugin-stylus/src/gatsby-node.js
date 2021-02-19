f418fbe22bc732aeaf2cd2b6c8c46c8c7aa77193
Ward Peeters <ward@coding-tech.com>
2021-02-19 12:23:17 +0100

    update gatsby-plugin-stylus

--- packages/gatsby-plugin-stylus/src/gatsby-node.js
+++ packages/gatsby-plugin-stylus/src/gatsby-node.js
@@ -23,57 +23,43 @@
 const resolve = require(`./resolve`)
 
 exports.onCreateWebpackConfig = (
-  { actions, stage, rules, plugins, loaders },
+  { actions, loaders },
   { postCssPlugins, ...stylusOptions }
 ) => {
   const { setWebpackConfig } = actions
-  const PRODUCTION = stage !== `develop`
-  const isSSR = stage.includes(`html`)
 
   const stylusLoader = {
     loader: resolve(`stylus-loader`),
     options: {
-      sourceMap: !PRODUCTION,
       ...stylusOptions,
     },
   }
 
   const stylusRule = {
     test: /\.styl$/,
-    use: isSSR
-      ? [loaders.null()]
-      : [
-          loaders.miniCssExtract(),
-          loaders.css({ importLoaders: 2 }),
-          loaders.postcss({ plugins: postCssPlugins }),
-          stylusLoader,
-        ],
+    use: [
+      loaders.miniCssExtract(),
+      loaders.css({ importLoaders: 2 }),
+      loaders.postcss({ plugins: postCssPlugins }),
+      stylusLoader,
+    ],
   }
 
   const stylusRuleModules = {
     test: /\.module\.styl$/,
     use: [
-      !isSSR && loaders.miniCssExtract({ hmr: false }),
+      loaders.miniCssExtract(),
       loaders.css({ modules: true, importLoaders: 2 }),
       loaders.postcss({ plugins: postCssPlugins }),
       stylusLoader,
-    ].filter(Boolean),
+    ],
   }
 
-  let configRules = []
-
-  switch (stage) {
-    case `develop`:
-    case `build-javascript`:
-    case `build-html`:
-    case `develop-html`:
-      configRules = configRules.concat([
-        {
-          oneOf: [stylusRuleModules, stylusRule],
-        },
-      ])
-      break
-  }
+  const configRules = [
+    {
+      oneOf: [stylusRuleModules, stylusRule],
+    },
+  ]
 
   setWebpackConfig({
     module: {
