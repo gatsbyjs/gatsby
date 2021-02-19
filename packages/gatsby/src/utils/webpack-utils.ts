19bfedf46a138b52464c59b97c14cdf349407296
Ward Peeters <ward@coding-tech.com>
2021-02-19 09:35:32 +0100

    revert wrong changes

--- packages/gatsby/src/utils/webpack-utils.ts
+++ packages/gatsby/src/utils/webpack-utils.ts
@@ -424,6 +424,9 @@ export const createWebpackUtils = (
     } = {}): RuleSetRule => {
       return {
         test: /\.(js|mjs|jsx)$/,
+        resolve: {
+          fullySpecified: false,
+        },
         include: (modulePath: string): boolean => {
           // when it's not coming from node_modules we treat it as a source file.
           if (!vendorRegex.test(modulePath)) {
@@ -517,6 +520,9 @@ export const createWebpackUtils = (
 
       return {
         test: /\.(js|mjs)$/,
+        resolve: {
+          fullySpecified: false,
+        },
         exclude: (modulePath: string): boolean => {
           // If dep is user land code, exclude
           if (!vendorRegex.test(modulePath)) {
