c7cf90ddb24bfb2ddaa39e6f473d37609f2d73c9
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2021-02-21 14:28:30 +0530

    fix(gatsby): add reporter.panic in empty catch in load-themes

--- packages/gatsby/src/bootstrap/load-themes/index.js
+++ packages/gatsby/src/bootstrap/load-themes/index.js
@@ -37,7 +37,7 @@ const resolveTheme = async (
         const { resolve } = resolvePlugin(themeName, rootDir)
         themeDir = resolve
       } catch (localErr) {
-        // catch shouldn't be empty :shrug:
+        reporter.panic(`Failed to resolve ${themeName}`, localErr)
       }
     }
 
