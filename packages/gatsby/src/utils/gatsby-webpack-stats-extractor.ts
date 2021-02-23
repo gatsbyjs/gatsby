170c604ace4cd27b056412cadbbd008d87ec2379
Michal Piechowiak <misiek.piechowiak@gmail.com>
2021-02-23 12:23:03 +0100

    fix(gatsby): re-add prefetch/preload links for child assets (#29693)
    * fix(gatsby): re-add prefetch/preload links for child assets
    
    * test(artifacts): add test for adding <link> tags for webpack's magic comments

--- packages/gatsby/src/utils/gatsby-webpack-stats-extractor.ts
+++ packages/gatsby/src/utils/gatsby-webpack-stats-extractor.ts
@@ -11,6 +11,7 @@ export class GatsbyWebpackStatsExtractor {
     compiler.hooks.done.tapAsync(this.plugin.name, (stats, done) => {
       const assets = {}
       const assetsMap = {}
+      const childAssets = {}
       for (const chunkGroup of stats.compilation.chunkGroups) {
         if (chunkGroup.name) {
           const files: Array<string> = []
@@ -25,11 +26,33 @@ export class GatsbyWebpackStatsExtractor {
                 f.slice(0, chunkGroup.name.length) === chunkGroup.name
             )
             .map(filename => `/${filename}`)
+
+          for (const [rel, childChunkGroups] of Object.entries(
+            chunkGroup.getChildrenByOrders(
+              stats.compilation.moduleGraph,
+              stats.compilation.chunkGraph
+            )
+          )) {
+            if (!(chunkGroup.name in childAssets)) {
+              childAssets[chunkGroup.name] = {}
+            }
+
+            const childFiles = []
+            for (const childChunkGroup of childChunkGroups) {
+              for (const chunk of childChunkGroup.chunks) {
+                childFiles.push(...chunk.files)
+              }
+            }
+
+            childAssets[chunkGroup.name][rel] = childFiles
+          }
         }
       }
+
       const webpackStats = {
         ...stats.toJson({ all: false, chunkGroups: true }),
         assetsByChunkName: assets,
+        childAssetsByChunkName: childAssets,
       }
       fs.writeFile(
         path.join(`public`, `chunk-map.json`),
