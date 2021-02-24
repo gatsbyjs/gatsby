50d6613acb3d44e95783a6ffd214b385941c668e
Michal Piechowiak <misiek.piechowiak@gmail.com>
2021-02-24 16:01:20 +0100

    actually test absolute url cases

--- e2e-tests/production-runtime/cypress/integration/global-styles.js
+++ e2e-tests/production-runtime/cypress/integration/global-styles.js
@@ -4,6 +4,12 @@ describe(`Global style from gatsby-browser.js`, () => {
   beforeEach(() => {
     cy.intercept("/dog-thumbnail.jpg").as("thumbnail")
     cy.intercept("/static/merriweather-latin**.woff2").as("font")
+    cy.intercept("localhost:9000/dog-thumbnail-flip.jpg").as(
+      "absolute-url-without-protocol"
+    )
+    cy.intercept("localhost:9000/dog-thumbnail-dither.jpg").as(
+      "absolute-url-with-protocol"
+    )
     cy.visit(`/global-style/`).waitForRouteChange()
   })
 
@@ -19,22 +25,46 @@ describe(`Global style from gatsby-browser.js`, () => {
     )
   })
 
-  it(`should resolve absolute path in url()`, () => {
-    cy.wait("@thumbnail").should(req => {
-      expect(req.response.statusCode).to.be.gte(200)
+  describe(`should resolve absolute path in url()`, () => {
+    it(`url without host`, () => {
+      cy.wait("@thumbnail").should(req => {
+        expect(req.response.statusCode).to.be.gte(200).and.lt(400)
+      })
+      cy.getTestElement(`global-style-background`).should(
+        `have.css`,
+        `background-image`,
+        `url("http://localhost:9000/dog-thumbnail.jpg")`
+      )
     })
-    cy.getTestElement(`global-style-background`).should(
-      `have.css`,
-      `background-image`,
-      `url("http://localhost:9000/dog-thumbnail.jpg")`
-    )
+
+    it(`url with host and without protocol`, () => {
+      cy.wait("@absolute-url-without-protocol").should(req => {
+        expect(req.response.statusCode).to.be.gte(200).and.lt(400)
+      })
+      cy.getTestElement(`global-style-urlwithoutprotocol`).should(
+        `have.css`,
+        `background-image`,
+        `url("http://localhost:9000/dog-thumbnail-flip.jpg")`
+      )
+    })
+
+    it(`url with host and with protocol`, () => {
+      cy.wait("@absolute-url-with-protocol").should(req => {
+        expect(req.response.statusCode).to.be.gte(200).and.lt(400)
+      })
+      cy.getTestElement(`global-style-fullurl`).should(
+        `have.css`,
+        `background-image`,
+        `url("http://localhost:9000/dog-thumbnail-dither.jpg")`
+      )
+    })
   })
 
   // Service worker is handling requests so this one is cached by previous runs
   if (!Cypress.env(`TEST_PLUGIN_OFFLINE`)) {
     it(`should resolve relative path in url()`, () => {
       cy.wait("@font").should(req => {
-        expect(req.response.statusCode).to.be.gte(200)
+        expect(req.response.statusCode).to.be.gte(200).and.lt(400)
       })
     })
   }
