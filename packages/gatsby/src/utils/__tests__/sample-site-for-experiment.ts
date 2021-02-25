34d1131a318c073b49ce66c02094d98cfde0a614
Matt Kane <matt@gatsbyjs.com>
2021-02-25 10:32:22 +0000

    tests(gatsby): Make experiments test deterministic

--- packages/gatsby/src/utils/__tests__/sample-site-for-experiment.ts
+++ packages/gatsby/src/utils/__tests__/sample-site-for-experiment.ts
@@ -1,11 +1,22 @@
 import sampleSiteForExperiment from "../sample-site-for-experiment"
 
+jest.mock(`gatsby-telemetry/lib/repository-id`)
+import {
+  getRepositoryId,
+  IRepositoryId,
+} from "gatsby-telemetry/lib/repository-id"
+;((getRepositoryId as unknown) as jest.Mock<IRepositoryId>).mockReturnValue({
+  repositoryId: `mockId`,
+})
+
 describe(`sampleSiteForExperiment`, () => {
   it(`returns true or false depending on if they randomly are bucketed in or not`, () => {
     const experiments = [`a`, `b`, `c`, `d`, `e`, `f`, `h`, `i`]
     experiments.forEach(experiment => {
       expect(sampleSiteForExperiment(experiment, 0)).toBeFalsy()
-      expect(sampleSiteForExperiment(experiment, 50)).toMatchSnapshot()
+      expect(sampleSiteForExperiment(experiment, 50)).toMatchSnapshot(
+        experiment
+      )
       expect(sampleSiteForExperiment(experiment, 100)).toBeTruthy()
     })
   })
