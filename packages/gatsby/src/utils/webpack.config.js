cb4e444c0e5b9d00c270bdbf86aee9be78e09970
Matt Kane <matt@gatsbyjs.com>
2021-02-25 17:45:18 +0000

    CHange imports inside gatsby

--- packages/gatsby/src/utils/webpack.config.js
+++ packages/gatsby/src/utils/webpack.config.js
@@ -341,7 +341,7 @@ module.exports = async (
       // Gatsby main router changes it, to keep v2 behaviour.
       // We will need to most likely remove this for v3.
       {
-        test: require.resolve(`@reach/router/es/index`),
+        test: require.resolve(`@gatsbyjs/reach-router/es/index`),
         type: `javascript/auto`,
         use: [{
           loader: require.resolve(`./reach-router-add-basecontext-export-loader`),
