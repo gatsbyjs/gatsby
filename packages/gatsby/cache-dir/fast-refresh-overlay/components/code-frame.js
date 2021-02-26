22da8028395d61fb2bef0b66418ae2c83fe29e6e
Lennart <lekoarts@gmail.com>
2021-02-26 15:47:47 +0100

    fix(gatsby): Update styles & overlay for Fast Refresh (#29797)
    Co-authored-by: gatsbybot <mathews.kyle+gatsbybot@gmail.com>

--- packages/gatsby/cache-dir/fast-refresh-overlay/components/code-frame.js
+++ packages/gatsby/cache-dir/fast-refresh-overlay/components/code-frame.js
@@ -13,22 +13,6 @@ export function CodeFrame({ decoded }) {
     <pre data-gatsby-overlay="pre">
       <code data-gatsby-overlay="pre__code">
         {decoded.map((entry, index) => {
-          // Check if content is "Enter" and render other element that collapses
-          // Otherwise an empty line would be printed
-          if (
-            index === 0 &&
-            entry.content ===
-              `
-`
-          ) {
-            return (
-              <span
-                key={`frame-${index}`}
-                data-gatsby-overlay="pre__code__span__empty"
-              />
-            )
-          }
-
           const style = {
             color: entry.fg ? `var(--color-${entry.fg})` : undefined,
             ...(entry.decoration === `bold`
