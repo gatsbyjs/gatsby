e7e3c7c6b13bdb4a241615cb11d5b132a095a0a4
LekoArts <lekoarts@gmail.com>
2021-03-02 16:12:09 +0100

    update hello world and AVIF

--- starters/default/src/pages/index.js
+++ starters/default/src/pages/index.js
@@ -15,6 +15,7 @@ const IndexPage = () => (
       src="../images/gatsby-astronaut.png"
       width={300}
       quality={95}
+      formats={["AUTO", "WEBP", "AVIF"]}
       alt="A Gatsby astronaut"
       style={{ marginBottom: `1.45rem` }}
     />
