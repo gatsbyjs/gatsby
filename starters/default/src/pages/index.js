90b33478d0d2c8823bbc4773cdf7072057941d87
LekoArts <lekoarts@gmail.com>
2021-03-02 15:00:19 +0100

    quality fix on default

--- starters/default/src/pages/index.js
+++ starters/default/src/pages/index.js
@@ -14,6 +14,7 @@ const IndexPage = () => (
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width="300"
+      quality="95"
       alt="A Gatsby astronaut"
       style={{ marginBottom: `1.45rem` }}
     />
