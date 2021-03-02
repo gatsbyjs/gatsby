fc33ac60989a82fe2e8d696e2ccb319e2f0803c0
Tyler Barnes <tylerdbarnes@gmail.com>
2021-03-02 15:47:24 -0800

    test html link transformations

--- packages/gatsby-source-wordpress/__tests__/process-node.test.js
+++ packages/gatsby-source-wordpress/__tests__/process-node.test.js
@@ -1,6 +1,9 @@
+import execall from "execall"
+
 import {
   getImgSrcRemoteFileMatchesFromNodeString,
   getImgTagMatchesWithUrl,
+  getWpLinkRegex,
 } from "../dist/steps/source-nodes/create-nodes/process-node"
 
 const wpUrl = `wp.fakesite.com`
@@ -22,4 +25,21 @@ test(`HTML image transformation regex matches images`, async () => {
   })
 
   expect(imgTagMatches.length).toBe(3)
+})
+
+test(`HTML link transformation regex matches links`, async () => {
+  const nodeString = `<a href=\\"https://${wpUrl}/wp-content/uploads/2020/01/©SDM-Yep-©Hi-000-Header.jpg\\" />Not a transformable link</a>
+
+  <a href=\\"https://other-site.com/hi\\" />Not a transformable link</a>
+
+  <a href=\\"https://${wpUrl}/page-1\\">
+    page 1
+  </a>
+
+  <a href=\\"https://${wpUrl}/\\">Home</a>`
+
+  const wpLinkRegex = getWpLinkRegex(`https://${wpUrl}`)
+  const matches = execall(wpLinkRegex, nodeString)
+
+  expect(matches.length).toBe(2)
 })
