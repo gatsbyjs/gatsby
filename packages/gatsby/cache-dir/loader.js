96fe114471b11126b286d95767c5842360e8513e
Ward Peeters <ward@coding-tech.com>
2021-03-05 13:36:40 +0100

    Update packages/gatsby/cache-dir/loader.js

--- packages/gatsby/cache-dir/loader.js
+++ packages/gatsby/cache-dir/loader.js
@@ -556,7 +556,7 @@ export const publicLoader = {
   getResourceURLsForPathname: rawPath =>
     instance.getResourceURLsForPathname(rawPath),
   loadPage: rawPath => instance.loadPage(rawPath),
-  // TODO add deprecation to v4 so people use withErrorDetails and than we can remove in v5 and change default behviour
+  // TODO add deprecation to v4 so people use withErrorDetails and then we can remove in v5 and change default behaviour
   loadPageSync: (rawPath, options = {}) =>
     instance.loadPageSync(rawPath, options),
   prefetch: rawPath => instance.prefetch(rawPath),
