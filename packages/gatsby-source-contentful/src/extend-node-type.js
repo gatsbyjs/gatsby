2f599952142b9f2350ef268187839dcbe01ccd74
Matt Kane <matt@gatsbyjs.com>
2021-03-05 11:29:48 +0000

    fix(gatsby-source-contentful): Remove image beta warning (#29980)
    Co-authored-by: gatsbybot <mathews.kyle+gatsbybot@gmail.com>

--- packages/gatsby-source-contentful/src/extend-node-type.js
+++ packages/gatsby-source-contentful/src/extend-node-type.js
@@ -664,9 +664,7 @@ const fluidNodeType = ({ name, getTracedSVG }) => {
   }
 }
 
-let warnedForBeta = false
-
-exports.extendNodeType = ({ type, store, reporter }) => {
+exports.extendNodeType = ({ type, store }) => {
   if (type.name !== `ContentfulAsset`) {
     return {}
   }
@@ -769,14 +767,6 @@ exports.extendNodeType = ({ type, store, reporter }) => {
 
   // gatsby-plugin-image
   const getGatsbyImageData = () => {
-    if (!warnedForBeta) {
-      reporter.warn(
-        stripIndent`
-      Thank you for trying the beta version of the \`gatsbyImageData\` API. Please provide feedback and report any issues at: https://github.com/gatsbyjs/gatsby/discussions/27950`
-      )
-      warnedForBeta = true
-    }
-
     const fieldConfig = getGatsbyImageFieldConfig(resolveGatsbyImageData, {
       jpegProgressive: {
         type: GraphQLBoolean,
