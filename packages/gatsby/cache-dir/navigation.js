38dab684af7475ea17edf5cefddfe62584a4065a
Michal Piechowiak <misiek.piechowiak@gmail.com>
2021-03-05 14:00:26 +0100

    chore: adjust all @reach/router imports to not rely just on webpack alias (#30033)

--- packages/gatsby/cache-dir/navigation.js
+++ packages/gatsby/cache-dir/navigation.js
@@ -5,8 +5,8 @@ import redirects from "./redirects.json"
 import { apiRunner } from "./api-runner-browser"
 import emitter from "./emitter"
 import { RouteAnnouncerProps } from "./route-announcer-props"
-import { navigate as reachNavigate } from "@reach/router"
-import { globalHistory } from "@reach/router/lib/history"
+import { navigate as reachNavigate } from "@gatsbyjs/reach-router"
+import { globalHistory } from "@gatsbyjs/reach-router/lib/history"
 import { parsePath } from "gatsby-link"
 
 // Convert to a map for faster lookup in maybeRedirect()
