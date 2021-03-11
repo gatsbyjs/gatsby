4ec2863767336e8f989eef8b87537504ff2bcb14
Michal Piechowiak <misiek.piechowiak@gmail.com>
2021-03-11 12:47:43 +0100

    fix(gatsby): wokraround some webpack issues causing first save after running gatsby develop to not have any effect

--- packages/gatsby/src/utils/gatsby-webpack-virtual-modules.ts
+++ packages/gatsby/src/utils/gatsby-webpack-virtual-modules.ts
@@ -1,5 +1,6 @@
 import VirtualModulesPlugin from "webpack-virtual-modules"
 import * as path from "path"
+import * as fs from "fs-extra"
 /*
  * This module allows creating virtual (in memory only) modules / files
  * that webpack compilation can access without the need to write module
@@ -45,6 +46,13 @@ export function writeModule(filePath: string, fileContents: string): void {
     // we already have this, no need to cause invalidation
     return
   }
+
+  // workaround webpack marking virtual modules as deleted because those files don't really exist
+  // so we create those files just so watchpack doesn't mark them as initially missing
+  fs.outputFileSync(
+    path.join(process.cwd(), VIRTUAL_MODULES_BASE_PATH, filePath),
+    fileContents
+  )
 
   fileContentLookup[adjustedFilePath] = fileContents
 
