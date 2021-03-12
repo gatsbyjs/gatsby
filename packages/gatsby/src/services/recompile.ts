8c1ce3d386a6e1c2f7e4d118d4b6a2492e4ac919
Vladimir Razuvaev <vladimir.razuvaev@gmail.com>
2021-03-12 20:07:50 +0700

    cleanup

--- packages/gatsby/src/services/recompile.ts
+++ packages/gatsby/src/services/recompile.ts
@@ -17,13 +17,12 @@ export async function recompile({
   return new Promise(resolve => {
     function finish(stats: Stats): void {
       emitter.off(`COMPILATION_DONE`, finish)
-
       resolve(stats)
     }
     emitter.on(`COMPILATION_DONE`, finish)
 
-    // Run re-compilation of aggregated changes
     webpackWatching.resume()
+    // Suspending is just a flag, so it's safe to re-suspend right away
     webpackWatching.suspend()
   })
 }
