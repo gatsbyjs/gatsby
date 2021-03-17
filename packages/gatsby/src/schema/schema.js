05102c51674470a4d8d6aa6a255b5c840eb73cac
Vladimir Razuvaev <vladimir.razuvaev@gmail.com>
2021-03-18 05:44:45 +0700

    fix(gatsby): do not fail on 3rd-party schemas with relay-classic support

--- packages/gatsby/src/schema/schema.js
+++ packages/gatsby/src/schema/schema.js
@@ -770,7 +770,11 @@ const addThirdPartySchemas = ({
   thirdPartySchemas.forEach(schema => {
     const schemaQueryType = schema.getQueryType()
     const queryTC = schemaComposer.createTempTC(schemaQueryType)
-    processThirdPartyTypeFields({ typeComposer: queryTC, schemaQueryType })
+    processThirdPartyTypeFields({
+      typeComposer: queryTC,
+      type: schemaQueryType,
+      schemaQueryType,
+    })
     schemaComposer.Query.addFields(queryTC.getFields())
 
     // Explicitly add the third-party schema's types, so they can be targeted
@@ -785,12 +789,34 @@ const addThirdPartySchemas = ({
         type.name !== `Date` &&
         type.name !== `JSON`
       ) {
+        const isCompositeType =
+          type instanceof GraphQLObjectType ||
+          type instanceof GraphQLInterfaceType
+
+        // Workaround for an edge case typical for Relay Classic-compatible schemas.
+        // For example, GitHub API contains this piece:
+        //   type Query { relay: Query }
+        // And gatsby-source-graphql transforms it to:
+        //   type Query { github: GitHub }
+        //   type GitHub { relay: Query }
+        // The problem:
+        //   schemaComposer.createTC(type) for type `GitHub` will eagerly create type composers
+        //   for all fields (including `relay` and it's type: `Query` of the third-party schema)
+        //   This unexpected `Query` composer messes up with our own Query type composer and produces duplicate types.
+        //   The workaround is to make sure fields of the GitHub type are lazy and are evaluated only when
+        //   this Query type is already replaced with our own root `Query` type (see processThirdPartyTypeFields):
+        if (isCompositeType && typeof type._fields === `object`) {
+          const fields = type._fields
+          type._fields = () => fields
+        }
+        // ^^^ workaround done
         const typeComposer = schemaComposer.createTC(type)
-        if (
-          typeComposer instanceof ObjectTypeComposer ||
-          typeComposer instanceof InterfaceTypeComposer
-        ) {
-          processThirdPartyTypeFields({ typeComposer, schemaQueryType })
+        if (isCompositeType) {
+          processThirdPartyTypeFields({
+            typeComposer,
+            type,
+            schemaQueryType,
+          })
         }
         typeComposer.setExtension(`createdFrom`, `thirdPartySchema`)
         schemaComposer.addSchemaMustHaveType(typeComposer)
@@ -830,15 +856,19 @@ const resetOverriddenThirdPartyTypeFields = ({ typeComposer }) => {
   })
 }
 
-const processThirdPartyTypeFields = ({ typeComposer, schemaQueryType }) => {
+const processThirdPartyTypeFields = ({
+  typeComposer,
+  type,
+  schemaQueryType,
+}) => {
   resetOverriddenThirdPartyTypeFields({ typeComposer })
 
   // Fix for types that refer to Query. Thanks Relay Classic!
+  const fields = type.getFields()
   typeComposer.getFieldNames().forEach(fieldName => {
     // Remove customization that we could have added via `createResolvers`
     // to make it work with schema rebuilding
-    const field = typeComposer.getField(fieldName)
-    const fieldType = field.type.getTypeName()
+    const fieldType = String(fields[fieldName].type)
     if (fieldType.replace(/[[\]!]/g, ``) === schemaQueryType.name) {
       typeComposer.extendField(fieldName, {
         type: fieldType.replace(schemaQueryType.name, `Query`),
