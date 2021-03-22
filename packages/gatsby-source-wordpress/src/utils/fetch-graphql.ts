7cd9cd3e6eed7c430b367d41171f4592161ba09e
Tyler Barnes <tyler@gatsbyjs.com>
2021-03-22 11:45:24 -0700

    fix(gatsby-source-wordpress): improve error message for folks who've never used WP_DEBUG (#30356)
    * improve error message for folks who've never used WP_DEBUG
    
    * add WP.org docs link
    
    * disable eslint error - the escapes here are not useless

--- packages/gatsby-source-wordpress/src/utils/fetch-graphql.ts
+++ packages/gatsby-source-wordpress/src/utils/fetch-graphql.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-useless-escape */
+
 import { IPluginOptions } from "~/models/gatsby-api"
 import { GatsbyReporter } from "./gatsby-types"
 import prettier from "prettier"
@@ -364,7 +366,15 @@ ${getLowerRequestConcurrencyOptionMessage()}`,
 
 Your WordPress server is either overloaded or encountered a PHP error.
 ${errorContext ? `\n${errorContext}\n` : ``}
-Enable WP_DEBUG, WP_DEBUG_LOG, and WPGRAPHQL_DEBUG, run another build and then check your WordPress instance's debug.log file.
+Enable WordPress debugging by adding the following to your wp-config.php file:
+
+define("WP\_DEBUG", true);
+define("WP\_DEBUG\_LOG", true);
+define("GRAPHQL\_DEBUG", true);
+
+(See https://wordpress.org/support/article/debugging-in-wordpress/ for more info)
+
+Then run another build before checking your WordPress instance's debug.log file for errors.
 
 If you don't see any errors in debug.log:
 
