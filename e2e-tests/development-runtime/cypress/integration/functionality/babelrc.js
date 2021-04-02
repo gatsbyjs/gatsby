64812f5d556f5faec37acde01f0a76667c254e70
Michal Piechowiak <misiek.piechowiak@gmail.com>
2021-04-02 16:59:02 +0200

    test(e2e): add editing .babelrc case

--- e2e-tests/development-runtime/cypress/integration/functionality/babelrc.js
+++ e2e-tests/development-runtime/cypress/integration/functionality/babelrc.js
@@ -1,5 +1,13 @@
 const TEST_ELEMENT = `test-element`
 
+after(() => {
+  cy.exec(`npm run reset`)
+})
+
+after(() => {
+  cy.exec(`npm run reset`)
+})
+
 describe(`babelrc`, () => {
   it(`Applies .babelrc`, () => {
     cy.visit(`/babelrc/base/`).waitForRouteChange()
@@ -7,5 +15,25 @@ describe(`babelrc`, () => {
     cy.getTestElement(TEST_ELEMENT)
       .invoke(`text`)
       .should(`eq`, `babel-rc-is-used`)
+  })
+
+  describe(`hot reload`, () => {
+    it(`editing .babelrc`, () => {
+      cy.visit(`/babelrc/edit/`).waitForRouteChange()
+
+      cy.getTestElement(TEST_ELEMENT)
+        .invoke(`text`)
+        .should(`eq`, `babel-rc-initial`)
+
+      cy.exec(
+        `npm run update -- --file src/pages/babelrc/edit/.babelrc --replacements "babel-rc-initial:babel-rc-edited" --exact`
+      )
+
+      cy.waitForHmr()
+
+      cy.getTestElement(TEST_ELEMENT)
+        .invoke(`text`)
+        .should(`eq`, `babel-rc-edited`)
+    })
   })
 })
