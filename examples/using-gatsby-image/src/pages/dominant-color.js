e351588365c32ebf84c06048ba3b2da16e12793c
Matt Kane <matt@gatsbyjs.com>
2021-04-12 12:49:38 +0100

    Port using-gatsby-image site to image plugin (#28489)
    * Moving examples to new plugin
    
    * wip
    
    * wip
    
    * wip
    
    * feat(gatsby-plugin-image): Add useArtDirection hook
    
    * Use hook from package
    
    * wip
    
    * update code and copy
    
    * got stuck ong atsby dev versions
    
    * address comments
    
    * rework
    
    * Fix image crop and alt
    
    * Fix version
    
    Co-authored-by: Laurie <laurie@gatsbyjs.com>

--- examples/using-gatsby-image/src/pages/dominant-color.js
+++ examples/using-gatsby-image/src/pages/dominant-color.js
@@ -5,33 +5,36 @@ import ImageGallery from "../components/image-gallery"
 import PageTitle from "../components/page-title"
 import Layout from "../components/layout"
 
-const BlurUp = ({ data, location }) => (
+const DominantColor = ({ data, location }) => (
   <Layout
     location={location}
     image={getImage(data.coverImage.localFile)}
     imageTitle={`“${data.coverImage.title}” by ${data.coverImage.credit} (via unsplash.com)`}
   >
-    <PageTitle>Blur Up</PageTitle>
+    <PageTitle>Dominant Color Placeholders</PageTitle>
     <GatsbyImage
       image={getImage(data.floatingImage.localFile)}
       alt={`“${data.floatingImage.title}” by ${data.floatingImage.credit} (via unsplash.com)`}
     />
     <p>
-      The Blur Up technique uses progressive loading to make a fast, visually
-      pleasing experience without waiting for a full-resolution image with a
-      blank screen.
+      The default Dominant color technique uses progressive loading to make a
+      fast, visually pleasing experience without waiting for a full-resolution
+      image with a blank screen.
     </p>
     <h2>Progressive Loading with Minimal Effort</h2>
     <p>
-      The magic of Gatsby Image's Blur Up technique means that you can load an
-      image at moderate resolution and not have to bother with creating a small
-      thumbnail yourself. Gatsby Image will automatically create a tiny image
-      from your source image and load it first for quick display while the
-      larger image file is downloaded and displayed. Users first see a blurry
-      lower-resolution image to help with perceived performance, while the
-      larger image downloads and everything works automatically.
+      Dominant color creates a properly sized div with a background color
+      generated based on your specific image. When the page loads, the div will
+      load first for quick display while the larger image file is downloaded and
+      displayed. This provides a nice visual effect and improves page
+      performance while removing the need for layout shifts when the image
+      loads.
     </p>
-    <h2>Unsplash Blurred Gallery</h2>
+    <p>
+      This technique is the default behavior when querying for an image with
+      GraphQL or using StaticImage.
+    </p>
+    <h2>Unsplash Dominant Color Gallery</h2>
     <ImageGallery images={data.galleryImagesCropped.edges} />
     <GatsbyImage
       image={getImage(data.fullWidthImage.localFile)}
@@ -40,7 +43,7 @@ const BlurUp = ({ data, location }) => (
   </Layout>
 )
 
-export default BlurUp
+export default DominantColor
 
 export const query = graphql`
   {
@@ -52,8 +55,8 @@ export const query = graphql`
           gatsbyImageData(
             width: 720
             aspectRatio: 0.5
+            placeholder: DOMINANT_COLOR
             layout: CONSTRAINED
-            placeholder: BLURRED
           )
         }
       }
@@ -63,16 +66,24 @@ export const query = graphql`
       title
       localFile {
         childImageSharp {
-          gatsbyImageData(width: 200, layout: CONSTRAINED, placeholder: BLURRED)
+          gatsbyImageData(
+            width: 200
+            placeholder: DOMINANT_COLOR
+            layout: CONSTRAINED
+          )
         }
       }
     }
-    fullWidthImage: unsplashImagesYaml(title: { eq: "Alien in the forest" }) {
+    fullWidthImage: unsplashImagesYaml(title: { eq: "City from above" }) {
       credit
       title
       localFile {
         childImageSharp {
-          gatsbyImageData(width: 600, layout: CONSTRAINED)
+          gatsbyImageData(
+            width: 480
+            placeholder: DOMINANT_COLOR
+            layout: CONSTRAINED
+          )
         }
       }
     }
@@ -90,7 +101,7 @@ export const query = graphql`
                 width: 380
                 height: 380
                 quality: 70
-                placeholder: BLURRED
+                placeholder: DOMINANT_COLOR
                 layout: CONSTRAINED
               )
             }
