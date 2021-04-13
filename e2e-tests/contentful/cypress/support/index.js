08615b09fa3fb3bd65b71ed181facfe749eea569
Benedikt Rötsch <opensource@axe312.dev>
2021-04-13 08:39:05 +0200

    slightly increase failure threshold

--- e2e-tests/contentful/cypress/support/index.js
+++ e2e-tests/contentful/cypress/support/index.js
@@ -17,10 +17,10 @@ import "gatsby-cypress"
 import { addMatchImageSnapshotCommand } from "cypress-image-snapshot/command"
 
 addMatchImageSnapshotCommand({
-    customDiffDir: `/__diff_output__`,
-    customDiffConfig: {
-        threshold: 0.1
-    },
-    failureThreshold: 0.03,
-    failureThresholdType: `percent`
+  customDiffDir: `/__diff_output__`,
+  customDiffConfig: {
+    threshold: 0.1,
+  },
+  failureThreshold: 0.04,
+  failureThresholdType: `percent`,
 })
