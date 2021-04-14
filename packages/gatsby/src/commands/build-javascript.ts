39850d5aa1e8b72b0775968a97e53660d47daa23
Michal Piechowiak <misiek.piechowiak@gmail.com>
2021-04-14 15:26:11 +0200

    add hooks

--- packages/gatsby/src/commands/build-javascript.ts
+++ packages/gatsby/src/commands/build-javascript.ts
@@ -1,3 +1,4 @@
+import { compile } from "joi"
 import { Span } from "opentracing"
 import webpack, { WebpackError } from "webpack"
 import webpackConfig from "../utils/webpack.config"
@@ -20,6 +21,34 @@ export const buildProductionBundle = async (
   return new Promise((resolve, reject) => {
     const compiler = webpack(compilerConfig)
 
+    compiler.cache.hooks.storeBuildDependencies.tap(`a`, (...args) => {
+      console.log(`storeBuildDependencies`)
+    })
+
+    compiler.cache.hooks.beginIdle.tap(`a`, (...args) => {
+      console.log(`beginIdle`)
+    })
+
+    compiler.cache.hooks.endIdle.tap(`a`, (...args) => {
+      console.log(`endIdle`)
+    })
+
+    // compiler.cache.hooks.get.tap(`a`, (...args) => {
+    //   console.log(`get`)
+    // })
+
+    compiler.cache.hooks.shutdown.tap(`a`, (...args) => {
+      console.log(`shutdown`)
+    })
+
+    compiler.hooks.done.tap(`a`, stats => {
+      console.log(`done`)
+    })
+
+    // compiler.cache.hooks.store.tap(`a`, (...args) => {
+    //   console.log(`store`)
+    // })
+
     compiler.run((err, stats) => {
       // stats can only be empty when an error occurs. Adding it to the if makes typescript happy.
       if (err || !stats) {
@@ -29,7 +58,10 @@ export const buildProductionBundle = async (
       if (stats.hasErrors()) {
         return reject(stats.compilation.errors)
       }
-
+      console.log(`compiler.run finished`)
+      compiler.close((...args) => {
+        console.log(`compiler close`, args)
+      })
       return resolve(stats)
     })
   })
