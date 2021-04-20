4ce1ea5c0d98259341136f22f92db002a7a67589
Tyler Barnes <tylerdbarnes@gmail.com>
2021-04-20 11:57:20 -0700

    make tests pass with correct db

--- integration-tests/gatsby-source-wordpress/docker/wp-cli/start.sh
+++ integration-tests/gatsby-source-wordpress/docker/wp-cli/start.sh
@@ -9,11 +9,9 @@ wp core install \
     --admin_password=secret \
     --admin_email=admin@admin.com
 
-
-wp search-replace 'https://devgatsbyint.wpengine.com' 'http://localhost:8001'
-
 wp user update admin --user_pass="secret"
 wp plugin install custom-post-type-ui --version=$CPT_UI_VERSION
+
 # activate plugins
 wp plugin activate wp-graphql custom-post-type-ui wp-graphql-custom-post-type-ui advanced-custom-fields-pro basic-auth
 
@@ -23,7 +21,6 @@ wp plugin activate wp-gatsby
 
 # this seems to be needed now
 wp core update-db
-
 wp cache flush
 
 # set path rewrite structure
