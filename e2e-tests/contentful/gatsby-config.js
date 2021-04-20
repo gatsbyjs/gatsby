825a5b3f19feb90bb4a5d0b593c704dd73e502ab
Benedikt Rötsch <opensource@axe312.dev>
2021-04-20 12:37:27 +0200

    add hint to credentials

--- e2e-tests/contentful/gatsby-config.js
+++ e2e-tests/contentful/gatsby-config.js
@@ -6,6 +6,9 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
+        // This space is for testing purposes only.
+        // Never store your Contentful credentials in your projects config file.
+        // Use: https://www.gatsbyjs.com/docs/how-to/local-development/environment-variables/
         spaceId: `k8iqpp6u0ior`,
         accessToken: `hO_7N0bLaCJFbu5nL3QVekwNeB_TNtg6tOCB_9qzKUw`,
       },
