a59fbb62e98cb9946360d9e147ced01e37d0a817
Tyler Barnes <tylerdbarnes@gmail.com>
2021-04-21 12:38:11 -0700

    force prune docker in the rebuild script to remove caches

--- integration-tests/gatsby-source-wordpress/docker/rebuild.sh
+++ integration-tests/gatsby-source-wordpress/docker/rebuild.sh
@@ -8,7 +8,8 @@ log 'Rebuilding test docker-compose'
 
 docker-compose rm -fsv && \
 docker volume rm gatsby-source-wordpress_db_data gatsby-source-wordpress_wp_data && \
-docker-compose up --force-recreate --build -V --remove-orphans -d && \
+docker-compose up --force-recreate --build --remove-orphans -d && \
+docker builder prune -f && \
 
 log 'Successfully rebuilt test docker-compose. \n\nContainers are now running in detached mode & next the script will follow logs for wordpress & wp-cli.\nIf you press ctrl/cmd+C to exit the log follow process, the containers will remain running.\nYou can use docker-compose stop to stop them.' && \
 
