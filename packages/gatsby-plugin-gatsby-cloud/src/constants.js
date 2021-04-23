841c0d248fa59df7a045845d59987b093d43112f
Daniel Lew <51924260+DanielSLew@users.noreply.github.com>
2021-04-23 06:25:31 -0400

    feat(gatsby-plugin-gatsby-cloud): add prototype of preview indicator (#30839)
    Co-authored-by: Ward Peeters <ward@coding-tech.com>

--- packages/gatsby-plugin-gatsby-cloud/src/constants.js
+++ packages/gatsby-plugin-gatsby-cloud/src/constants.js
@@ -41,3 +41,5 @@ export const LINK_REGEX = /^(Link: <\/)(.+)(>;.+)/
 export const COMMON_BUNDLES = [`commons`, `app`]
 
 export const PAGE_DATA_DIR = `page-data/`
+
+export const POLLING_INTERVAL = 5000
