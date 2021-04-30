e3bcb71fa59d3c45c14b82cc82e882ee9d5ac224
Kyle Mathews <mathews.kyle@gmail.com>
2021-04-29 19:54:13 -0700

    Remove unnecessary implementation of onCreatePage
    Drops 100k create-pages benchmark from ~8s to 4.5s as it means
    we don't need to invoke `onCreatePage` by default.

--- packages/gatsby/src/internal-plugins/prod-404/gatsby-node.js
+++ packages/gatsby/src/internal-plugins/prod-404/gatsby-node.js
@@ -4,18 +4,25 @@ const { actions } = require(`../../redux/actions`)
 const PROD_404_PAGE_PATH = `/404.html`
 
 let page404 = null
-exports.onCreatePage = ({ page, store, actions }) => {
+emitter.on(`CREATE_PAGE`, action => {
   // Copy /404/ to /404.html as many static site hosts expect
   // site 404 pages to be named this.
   // https://www.gatsbyjs.org/docs/how-to/adding-common-features/add-404-page/
-  if (!page404 && /^\/?404\/?$/.test(page.path)) {
-    actions.createPage({
-      ...page,
-      path: PROD_404_PAGE_PATH,
-    })
-    page404 = page
+  if (!page404 && /^\/?404\/?$/.test(action.payload.path)) {
+    console.log(`found 404`, action)
+    store.dispatch(
+      actions.createPage(
+        {
+          path: PROD_404_PAGE_PATH,
+          component: action.payload.component,
+          context: action.payload.context,
+        },
+        action.plugin
+      )
+    )
+    page404 = true
   }
-}
+})
 
 emitter.on(`DELETE_PAGE`, action => {
   if (page404 && action.payload.path === page404.path) {
