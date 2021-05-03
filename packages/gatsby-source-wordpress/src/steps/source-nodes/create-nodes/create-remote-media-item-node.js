5ea34c085ebaf96a5e9cdac8738bc8d4df82cd61
veryspry <ehlinger.matt@gmail.com>
2021-05-03 17:02:42 -0500

    feat: add warning logs if media items exceed config size or have excluded mime types

--- packages/gatsby-source-wordpress/src/steps/source-nodes/create-nodes/create-remote-media-item-node.js
+++ packages/gatsby-source-wordpress/src/steps/source-nodes/create-nodes/create-remote-media-item-node.js
@@ -232,11 +232,21 @@ export const createRemoteMediaItemNode = async ({
 
   // if this file is larger than maxFileSizeBytes, don't fetch the remote file
   if (fileSize > maxFileSizeBytes) {
+    reporter.warn(
+      formatLogMessage(
+        `Remote media item node not created because its size exceeded the maxFileSizeBytes config option ${mediaItemNode}`
+      )
+    )
     return null
   }
 
   // if this type of file is excluded, don't fetch the remote file
   if (excludeByMimeTypes.includes(mimeType)) {
+    reporter.warn(
+      formatLogMessage(
+        `Remote media item node not created because its mimetype was specified in the "exludeByMimeTypes" config option`
+      )
+    )
     return null
   }
 
