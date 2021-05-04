2905e0567a3dd62201aba6cff80038e5ac61b66a
Michal Piechowiak <misiek.piechowiak@gmail.com>
2021-05-04 12:41:20 +0200

    do not wrap console.log multiple times

--- e2e-tests/development-runtime/cypress/integration/eslint-rules/limited-exports-page-templates.js
+++ e2e-tests/development-runtime/cypress/integration/eslint-rules/limited-exports-page-templates.js
@@ -1,16 +1,14 @@
 describe(`limited-exports-page-templates`, () => {
   it(`should log warning to console for invalid export`, () => {
-    cy.visit(`/eslint-rules/limited-exports-page-templates`, {
-      onBeforeLoad(win) {
-        cy.stub(win.console, "log").as(`consoleLog`)
-      },
-    }).waitForRouteChange()
+    cy.visit(
+      `/eslint-rules/limited-exports-page-templates`
+    ).waitForRouteChange()
 
-    cy.get(`@consoleLog`).should(
+    cy.get(`@hmrConsoleLog`).should(
       `be.calledWithMatch`,
       /15:1  warning  In page templates only a default export of a valid React component and the named export of a page query is allowed./i
     )
-    cy.get(`@consoleLog`).should(
+    cy.get(`@hmrConsoleLog`).should(
       `not.be.calledWithMatch`,
       /17:1  warning  In page templates only a default export of a valid React component and the named export of a page query is allowed./i
     )
--- e2e-tests/development-runtime/cypress/integration/eslint-rules/no-anonymous-exports-page-templates.js
+++ e2e-tests/development-runtime/cypress/integration/eslint-rules/no-anonymous-exports-page-templates.js
@@ -1,24 +1,20 @@
 describe(`no-anonymous-exports-page-templates`, () => {
   it(`should log warning to console for arrow functions`, () => {
-    cy.visit(`/eslint-rules/no-anonymous-exports-page-templates`, {
-      onBeforeLoad(win) {
-        cy.stub(win.console, "log").as(`consoleLog`)
-      },
-    }).waitForRouteChange()
+    cy.visit(
+      `/eslint-rules/no-anonymous-exports-page-templates`
+    ).waitForRouteChange()
 
-    cy.get(`@consoleLog`).should(
+    cy.get(`@hmrConsoleLog`).should(
       `be.calledWithMatch`,
       /Anonymous arrow functions cause Fast Refresh to not preserve local component state./i
     )
   })
   it(`should log warning to console for function declarations`, () => {
-    cy.visit(`/eslint-rules/no-anonymous-exports-page-templates-function`, {
-      onBeforeLoad(win) {
-        cy.stub(win.console, "log").as(`consoleLog`)
-      },
-    }).waitForRouteChange()
+    cy.visit(
+      `/eslint-rules/no-anonymous-exports-page-templates-function`
+    ).waitForRouteChange()
 
-    cy.get(`@consoleLog`).should(
+    cy.get(`@hmrConsoleLog`).should(
       `be.calledWithMatch`,
       /Anonymous function declarations cause Fast Refresh to not preserve local component state./i
     )
