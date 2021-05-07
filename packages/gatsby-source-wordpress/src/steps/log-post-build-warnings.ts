628e7ab60c4bf595842525320793a13b0e2be55d
veryspry <ehlinger.matt@gmail.com>
2021-05-07 15:44:02 -0500

    feat: display help url in warning logs

--- packages/gatsby-source-wordpress/src/steps/log-post-build-warnings.ts
+++ packages/gatsby-source-wordpress/src/steps/log-post-build-warnings.ts
@@ -10,20 +10,23 @@ export const logPostBuildWarnings: Step = async (): Promise<void> => {
     },
   } = store.getState()
 
+  const helpUrl = `https://github.com/gatsbyjs/gatsby/blob/master/packages/gatsby-source-wordpress/docs/debugging-and-troubleshooting.md#media-file-download-skipped`
+
+  const helpText = `Visit ${helpUrl} for more info.`
+
   if (maxFileSizeBytesExceeded > 0) {
-    /**
-     * @todo create README for this
-     */
-    let message = `There were ${maxFileSizeBytesExceeded} files with file sizes that are above the maxFileSizeBytes config option and consequently were not fetched. Visit github.com/docs-url for more info`
-    message = formatLogMessage(message)
+    const message = formatLogMessage(
+      `There were ${maxFileSizeBytesExceeded} files with file sizes that are above the maxFileSizeBytes config option and consequently were not fetched. ${helpText}`
+    )
+
     reporter.warn(message)
   }
 
   if (mimeTypeExcluded > 0) {
-    /**
-     * @todo create README entry for this
-     */
-    const message = `There were ${mimeTypeExcluded} files with mime types that were included in the excludeByMimeTypes config option and consequently were not fetched. Visit github.com/docs-url for more info`
+    const message = formatLogMessage(
+      `There were ${mimeTypeExcluded} files with mime types that were included in the excludeByMimeTypes config option and consequently were not fetched. ${helpText}`
+    )
+
     reporter.warn(message)
   }
 }
