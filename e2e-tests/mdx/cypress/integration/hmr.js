224bccd6bc2812b3dd0fd5664a59bee34faaae97
Michal Piechowiak <misiek.piechowiak@gmail.com>
2021-05-10 11:53:05 +0200

    wait for hmr in mdx/develop

--- e2e-tests/mdx/cypress/integration/hmr.js
+++ e2e-tests/mdx/cypress/integration/hmr.js
@@ -8,12 +8,19 @@ if (Cypress.env("GATSBY_COMMAND") === `develop`) {
   })
 
   it(`Can hot-reload`, () => {
-    cy.visit(`/hmr`).waitForRouteChange()
+    cy.visit(`/hmr`, {
+      onBeforeLoad: win => {
+        cy.spy(win.console, "log").as(`hmrConsoleLog`)
+      },
+    }).waitForRouteChange()
     cy.get(`h2`).invoke(`text`).should(`eq`, `Lorem`)
 
     cy.exec(
       `npm run update -- --file src/pages/hmr.mdx --exact --replacements "Lorem:Ipsum"`
     )
+
+    cy.get(`@hmrConsoleLog`).should(`be.calledWithMatch`, `App is up to date`)
+    cy.wait(1000)
 
     cy.get(`h2`).invoke(`text`).should(`eq`, `Ipsum`)
   })
