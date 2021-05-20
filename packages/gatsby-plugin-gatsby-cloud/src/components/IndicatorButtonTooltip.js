60a98981056e47abf596bd713c466f64ad3281b8
Daniel Lew <51924260+DanielSLew@users.noreply.github.com>
2021-05-20 11:28:41 -0400

    feat(gatsby-plugin-gatsby-cloud): enable preview status indicator and tooltip (#31347)
    Co-authored-by: Ward Peeters <ward@coding-tech.com>

---
+++ packages/gatsby-plugin-gatsby-cloud/src/components/IndicatorButtonTooltip.js
@@ -0,0 +1,38 @@
+import React from "react"
+
+const IndicatorButtonTooltip = ({
+  onClick,
+  tooltipLinkImage,
+  tooltipLink,
+  overrideShowTooltip,
+  showTooltip,
+  tooltipText,
+  tooltipIcon,
+  toolTipOffset,
+  testId,
+}) => (
+  <div
+    onClick={onClick}
+    // toolTipOffset needs to be 40 * button possition (0 indexed) + 12
+    // This will align the tooltip with its correct button
+    // The first button is (40 * 0) + 12
+    // The second button is (40 * 1) + 12 ...
+    style={{ top: `${(toolTipOffset || 0) + 12}px` }}
+    data-gatsby-preview-indicator="tooltip"
+    data-gatsby-preview-indicator-visible={`${
+      overrideShowTooltip || showTooltip
+    }`}
+    data-testid={`${testId}-tooltip`}
+  >
+    {tooltipIcon}
+    {tooltipText}
+    {tooltipLink && (
+      <p data-gatsby-preview-indicator="tooltip-link">{tooltipLink}</p>
+    )}
+    {tooltipLinkImage && (
+      <div data-gatsby-preview-indicator="tooltip-svg">{tooltipLinkImage}</div>
+    )}
+  </div>
+)
+
+export default IndicatorButtonTooltip
