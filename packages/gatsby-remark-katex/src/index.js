62eb2f861de117009742cc31c929305c242f25cd
Boris A. Burkov <vasjaforutube@gmail.com>
2021-05-26 17:11:19 +0300

    #30865 Major fix in gatsby-remark-katex plugin.

--- packages/gatsby-remark-katex/src/index.js
+++ packages/gatsby-remark-katex/src/index.js
@@ -1,22 +1,26 @@
 const visit = require(`unist-util-visit`)
 const katex = require(`katex`)
 const remarkMath = require(`remark-math`)
+const unified = require(`unified`)
+const parse = require(`rehype-parse`)
 
 module.exports = ({ markdownAST }, pluginOptions = {}) => {
+  const parseHtml = unified().use(parse, { fragment: true, position: false })
+
   visit(markdownAST, `inlineMath`, node => {
-    node.type = `html`
-    node.value = katex.renderToString(node.value, {
+    const renderedKatexString = katex.renderToString(node.value, {
       displayMode: false,
       ...pluginOptions,
     })
+    node.data.hChildren = parseHtml.parse(renderedKatexString).children
   })
 
   visit(markdownAST, `math`, node => {
-    node.type = `html`
-    node.value = katex.renderToString(node.value, {
+    const renderedKatexString = katex.renderToString(node.value, {
       displayMode: true,
       ...pluginOptions,
     })
+    node.data.hChildren = parseHtml.parse(renderedKatexString).children
   })
 }
 
