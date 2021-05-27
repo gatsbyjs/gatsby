29dd70d6896f4ab634e87866f7d5bff8c473dc90
Vladimir Razuvaev <vladimir.razuvaev@gmail.com>
2021-05-27 17:01:52 +0700

    fix errors in tests caused by lazy initialization of the store

--- packages/gatsby/src/datastore/index.ts
+++ packages/gatsby/src/datastore/index.ts
@@ -2,6 +2,7 @@ import { setupLmdbStore } from "./lmdb/lmdb-datastore"
 import { setupInMemoryStore } from "./in-memory/in-memory-datastore"
 import { IDataStore } from "./types"
 import { isStrictMode } from "../utils/is-strict-mode"
+import { emitter } from "../redux"
 
 let dataStore: IDataStore
 let isLmdb
@@ -19,3 +20,10 @@ export function getDataStore(): IDataStore {
   }
   return dataStore
 }
+
+// It is possible that the store is not initialized yet when calling `DELETE_CACHE`.
+//  The code below ensures we wipe cache from the proper store
+//  (mostly relevant for tests)
+emitter.on(`DELETE_CACHE`, () => {
+  getDataStore()
+})
