9bef82bbe63bbfe95a027cf0d92ee9b3c8f4fb95
axe312ger <opensource@axe312.dev>
2021-05-27 16:28:34 +0200

    use flexbox instead of grid and use full integer css dimension values

--- e2e-tests/contentful/src/components/grid.js
+++ e2e-tests/contentful/src/components/grid.js
@@ -1,17 +1,23 @@
 import * as React from "react"
 
-const Grid = ({ children, columns, ...rest }) => (
-  <div
-    style={{
-      display: "grid",
-      gridTemplateColumns: "repeat(auto-fill, 240px)",
-      gridGap: "2rem",
-      maxWidth: "240px",
-    }}
-    {...rest}
-  >
-    {children}
-  </div>
-)
+const Grid = ({ children, columns, ...rest }) => {
+  const wrappedChildren = React.Children.map(children, child => (
+    <div style={{ maxWidth: 280, width: "100%" }}>{child}</div>
+  ))
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "space-between",
+      }}
+      {...rest}
+    >
+      {wrappedChildren}
+    </div>
+  )
+}
 
 export default Grid
