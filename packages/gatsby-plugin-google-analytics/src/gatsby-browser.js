b756021a1f0a7f8f943c8ede789acf44be72b240
Ward Peeters <ward@coding-tech.com>
2021-05-31 16:51:24 +0200

    feat(gatsby-plugin-google-analytics): enable core webvitals tracking

--- packages/gatsby-plugin-google-analytics/src/gatsby-browser.js
+++ packages/gatsby-plugin-google-analytics/src/gatsby-browser.js
@@ -1,4 +1,35 @@
+function sendWebVitals() {
+  return import(/* webpackMode: "lazy-once" */ `web-vitals`).then(
+    ({ getLCP, getFID, getCLS }) => {
+      getCLS(sendToGoogleAnalytics)
+      getFID(sendToGoogleAnalytics)
+      getLCP(sendToGoogleAnalytics)
+    }
+  )
+}
+
+function sendToGoogleAnalytics({ name, delta, id }) {
+  window.ga(`send`, `event`, {
+    eventCategory: `Web Vitals`,
+    eventAction: name,
+    // The `id` value will be unique to the current page load. When sending
+    // multiple values from the same page (e.g. for CLS), Google Analytics can
+    // compute a total by grouping on this ID (note: requires `eventLabel` to
+    // be a dimension in your report).
+    eventLabel: id,
+    // Google Analytics metrics must be integers, so the value is rounded.
+    // For CLS the value is first multiplied by 1000 for greater precision
+    // (note: increase the multiplier for greater precision if needed).
+    eventValue: Math.round(name === `CLS` ? delta * 1000 : delta),
+    // Use a non-interaction event to avoid affecting bounce rate.
+    nonInteraction: true,
+    // Use `sendBeacon()` if the browser supports it.
+    transport: `beacon`,
+  })
+}
+
 export const onRouteUpdate = ({ location }, pluginOptions = {}) => {
+  const ga = window.ga
   if (process.env.NODE_ENV !== `production` || typeof ga !== `function`) {
     return null
   }
@@ -16,8 +47,8 @@ export const onRouteUpdate = ({ location }, pluginOptions = {}) => {
     const pagePath = location
       ? location.pathname + location.search + location.hash
       : undefined
-    window.ga(`set`, `page`, pagePath)
-    window.ga(`send`, `pageview`)
+    ga(`set`, `page`, pagePath)
+    ga(`send`, `pageview`)
   }
 
   // Minimum delay for reactHelmet's requestAnimationFrame
@@ -25,4 +56,10 @@ export const onRouteUpdate = ({ location }, pluginOptions = {}) => {
   setTimeout(sendPageView, delay)
 
   return null
+}
+
+export function onClientEntry(_, pluginOptions) {
+  if (!pluginOptions.disableWebVitalsTracking) {
+    sendWebVitals()
+  }
 }
