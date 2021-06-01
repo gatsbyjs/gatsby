c1a622337757c41cdde932ba8147e1937ef67766
Vladimir Razuvaev <vladimir.razuvaev@gmail.com>
2021-06-02 03:43:23 +0700

    add custom iterable implementation

--- packages/gatsby/src/datastore/types.ts
+++ packages/gatsby/src/datastore/types.ts
@@ -9,6 +9,15 @@ export interface ILmdbDatabases {
   nodesByType: Database<NodeId, NodeType>
 }
 
+// Note: this type is compatible with lmdb-store ArrayLikeIterable
+export interface IGatsbyIterable<T> extends Iterable<T> {
+  [Symbol.iterator](): Iterator<T>
+  map<U>(fn: (entry: T) => U): IGatsbyIterable<U>
+  // concat<U>(other: Iterable<U>): Iterable<T | U>
+  filter(predicate: (entry: T) => any): IGatsbyIterable<T>
+  forEach(callback: (entry: T) => any): void
+}
+
 export interface IDataStore {
   getNode(id: string): IGatsbyNode | undefined
   getTypes(): Array<string>
