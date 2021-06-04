eb4e0b3d2268572ecdb27e177bc1ab71bf400ffe
Daniel Lew <danielshlomolew@gmail.com>
2021-06-04 11:18:15 -0400

    fix babel config and skip wrapRootElement tests

--- packages/gatsby-plugin-gatsby-cloud/src/__tests__/gatsby-browser.js
+++ packages/gatsby-plugin-gatsby-cloud/src/__tests__/gatsby-browser.js
@@ -105,64 +105,65 @@ describe(`Preview status indicator`, () => {
   // We are now rendering a Shadow DOM in wrapRootElement, testing-library does not play nicely with
   // a Shadow DOM so until we have a fix for it by either using a cypress test or a different
   // library we will skip it.
-  describe.skip(`wrapRootElement`, () => {
-    const testMessage = `Test Page`
 
-    beforeEach(() => {
-      process.env.GATSBY_PREVIEW_API_URL = createUrl(`success`)
-    })
+  // describe(`wrapRootElement`, () => {
+  //   const testMessage = `Test Page`
 
-    it(`renders the initial page and indicator if indicator enabled`, async () => {
-      // do not fetch any data
-      global.fetch = jest.fn(() => new Promise(() => {}))
-      process.env.GATSBY_PREVIEW_INDICATOR_ENABLED = `true`
+  //   beforeEach(() => {
+  //     process.env.GATSBY_PREVIEW_API_URL = createUrl(`success`)
+  //   })
 
-      act(() => {
-        render(
-          wrapRootElement({
-            element: <div>{testMessage}</div>,
-          })
-        )
-      })
+  //   it(`renders the initial page and indicator if indicator enabled`, async () => {
+  //     // do not fetch any data
+  //     global.fetch = jest.fn(() => new Promise(() => {}))
+  //     process.env.GATSBY_PREVIEW_INDICATOR_ENABLED = `true`
 
-      expect(screen.getByText(testMessage)).toBeInTheDocument()
-      expect(
-        screen.queryByTestId(`preview-status-indicator`)
-      ).toBeInTheDocument()
-    })
+  //     act(() => {
+  //       render(
+  //         wrapRootElement({
+  //           element: <div>{testMessage}</div>,
+  //         })
+  //       )
+  //     })
 
-    it(`renders page without the indicator if indicator not enabled`, () => {
-      process.env.GATSBY_PREVIEW_INDICATOR_ENABLED = `false`
+  //     expect(screen.getByText(testMessage)).toBeInTheDocument()
+  //     expect(
+  //       screen.queryByTestId(`preview-status-indicator`)
+  //     ).toBeInTheDocument()
+  //   })
 
-      render(
-        wrapRootElement({
-          element: <div>{testMessage}</div>,
-        })
-      )
+  //   it(`renders page without the indicator if indicator not enabled`, () => {
+  //     process.env.GATSBY_PREVIEW_INDICATOR_ENABLED = `false`
 
-      expect(screen.getByText(testMessage)).toBeInTheDocument()
-      expect(
-        screen.queryByTestId(`preview-status-indicator`)
-      ).not.toBeInTheDocument()
-    })
+  //     render(
+  //       wrapRootElement({
+  //         element: <div>{testMessage}</div>,
+  //       })
+  //     )
 
-    it(`renders initial page without indicator if api errors`, async () => {
-      render(
-        wrapRootElement({
-          element: <div>{testMessage}</div>,
-        })
-      )
+  //     expect(screen.getByText(testMessage)).toBeInTheDocument()
+  //     expect(
+  //       screen.queryByTestId(`preview-status-indicator`)
+  //     ).not.toBeInTheDocument()
+  //   })
 
-      global.fetch = jest.fn(() =>
-        Promise.resolve({ json: () => new Error(`failed`) })
-      )
+  //   it(`renders initial page without indicator if api errors`, async () => {
+  //     render(
+  //       wrapRootElement({
+  //         element: <div>{testMessage}</div>,
+  //       })
+  //     )
 
-      expect(screen.getByText(testMessage)).toBeInTheDocument()
-      expect(
-        screen.queryByTestId(`preview-status-indicator`)
-      ).not.toBeInTheDocument()
-    })
-  })
+  //     global.fetch = jest.fn(() =>
+  //       Promise.resolve({ json: () => new Error(`failed`) })
+  //     )
+
+  //     expect(screen.getByText(testMessage)).toBeInTheDocument()
+  //     expect(
+  //       screen.queryByTestId(`preview-status-indicator`)
+  //     ).not.toBeInTheDocument()
+  //   })
+  // })
 
   describe(`Indicator`, () => {
     describe(`trackEvent`, () => {
