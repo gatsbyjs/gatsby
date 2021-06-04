d6d926413b51c8da6e388671481978180ccd4789
Daniel Lew <danielshlomolew@gmail.com>
2021-06-04 09:58:36 -0400

    clean up code

--- packages/gatsby-plugin-gatsby-cloud/src/gatsby-browser.js
+++ packages/gatsby-plugin-gatsby-cloud/src/gatsby-browser.js
@@ -28,23 +28,6 @@ const ShadowPortal = ({ children, identifier }) => {
   )
 }
 
-// function PreviewIndicatorRoot() {
-//   const [indicatorRootRef, setIndicatorRootRef] = useState()
-
-//   useEffect(() => {
-//     const indicatorRoot = document.createElement(`div`)
-//     indicatorRoot.id = `gatsby-preview-indicator`
-//     setIndicatorRootRef(indicatorRoot)
-//     document.body.appendChild(indicatorRoot)
-//   }, [])
-
-//   if (!indicatorRootRef) {
-//     return null
-//   }
-
-//   return createPortal(<Indicator />, indicatorRootRef)
-// }
-
 export const wrapRootElement = ({ element }) => {
   if (process.env.GATSBY_PREVIEW_INDICATOR_ENABLED === `true`) {
     return (
