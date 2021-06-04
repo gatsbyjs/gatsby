361a0bb4f603b1ad6e542273327dfcd3bf3a6045
Daniel Lew <danielshlomolew@gmail.com>
2021-06-04 09:57:33 -0400

    render indicator in shadow root

--- packages/gatsby-plugin-gatsby-cloud/src/gatsby-browser.js
+++ packages/gatsby-plugin-gatsby-cloud/src/gatsby-browser.js
@@ -1,30 +1,58 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
 import { createPortal } from "react-dom"
 import Indicator from "./components/Indicator"
 
-function PreviewIndicatorRoot() {
-  const [indicatorRootRef, setIndicatorRootRef] = useState()
+const ShadowPortal = ({ children, identifier }) => {
+  const mountNode = React.useRef(null)
+  const portalNode = React.useRef(null)
+  const shadowNode = React.useRef(null)
+  const [, forceUpdate] = React.useState()
 
-  useEffect(() => {
-    const indicatorRoot = document.createElement(`div`)
-    indicatorRoot.id = `gatsby-preview-indicator`
-    setIndicatorRootRef(indicatorRoot)
-    document.body.appendChild(indicatorRoot)
+  React.useLayoutEffect(() => {
+    const ownerDocument = mountNode.current.ownerDocument
+    portalNode.current = ownerDocument.createElement(identifier)
+    shadowNode.current = portalNode.current.attachShadow({ mode: `open` })
+    ownerDocument.body.appendChild(portalNode.current)
+    forceUpdate({})
+    return () => {
+      if (portalNode.current && portalNode.current.ownerDocument) {
+        portalNode.current.ownerDocument.body.removeChild(portalNode.current)
+      }
+    }
   }, [])
 
-  if (!indicatorRootRef) {
-    return null
-  }
-
-  return createPortal(<Indicator />, indicatorRootRef)
+  return shadowNode.current ? (
+    createPortal(children, shadowNode.current)
+  ) : (
+    <span ref={mountNode} />
+  )
 }
+
+// function PreviewIndicatorRoot() {
+//   const [indicatorRootRef, setIndicatorRootRef] = useState()
+
+//   useEffect(() => {
+//     const indicatorRoot = document.createElement(`div`)
+//     indicatorRoot.id = `gatsby-preview-indicator`
+//     setIndicatorRootRef(indicatorRoot)
+//     document.body.appendChild(indicatorRoot)
+//   }, [])
+
+//   if (!indicatorRootRef) {
+//     return null
+//   }
+
+//   return createPortal(<Indicator />, indicatorRootRef)
+// }
 
 export const wrapRootElement = ({ element }) => {
   if (process.env.GATSBY_PREVIEW_INDICATOR_ENABLED === `true`) {
     return (
       <>
         {element}
-        <PreviewIndicatorRoot />
+        <ShadowPortal identifier="gatsby-preview-indicator">
+          <Indicator />
+        </ShadowPortal>
       </>
     )
   } else {
