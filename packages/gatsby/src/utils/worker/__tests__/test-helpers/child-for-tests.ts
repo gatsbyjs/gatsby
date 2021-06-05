9738f7ab7da2e5c5a765bcd63c3e73194f00a056
Michal Piechowiak <misiek.piechowiak@gmail.com>
2021-06-05 10:07:06 +0200

    add test veryfing config can be loaded and APIs executed in worker

--- packages/gatsby/src/utils/worker/__tests__/test-helpers/child-for-tests.ts
+++ packages/gatsby/src/utils/worker/__tests__/test-helpers/child-for-tests.ts
@@ -1,9 +1,22 @@
 import { getNode } from "../../../../datastore"
+import apiRunner from "../../../api-runner-node"
 
 // re-export all usual methods from production worker
 export * from "../../child"
 
 // additional functions to be able to write assertions that won't be available in production code
+
+// test: datastore
 export function getNodeFromWorker(nodeId: string): ReturnType<typeof getNode> {
   return getNode(nodeId)
+}
+
+// test: config
+export async function runAPI(apiName: string): Promise<any> {
+  return await apiRunner(apiName)
+}
+
+// test: config
+export function getAPIRunResult(): string | undefined {
+  return (global as any).test
 }
