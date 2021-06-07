5a0dfc3250943ddf8dca3f42e66f47a902d6d295
Vladimir Razuvaev <vladimir.razuvaev@gmail.com>
2021-06-08 01:45:29 +0700

    test

--- integration-tests/ssr/test-output.js
+++ integration-tests/ssr/test-output.js
@@ -22,7 +22,7 @@
       // There are many script tag differences
       $(`script`).remove()
       // Only added in production
-      $(`style[data-identity="gatsby-global-css"]`).remove()
+      $(`#gatsby-global-css`).remove()
       // Only added in development
       $(`link[data-identity='gatsby-dev-css']`).remove()
       // Only in prod
