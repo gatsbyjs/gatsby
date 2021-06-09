b9c017529a644bd7361232f70eb0f55d70bc596e
Kyle Mathews <mathews.kyle@gmail.com>
2021-06-09 13:19:41 -0700

    Add sharp example

---
+++ integration-tests/functions/src/api/sharp.ts
@@ -0,0 +1,19 @@
+// import { GatsbyFunctionResponse, GatsbyFunctionRequest } from "gatsby"
+import sharp from "sharp"
+
+export default async function topLevel(req, res) {
+  console.log(req.query)
+  const data = await sharp({
+    create: {
+      width: req.query.width ? parseInt(req.query.width, 10) : 400,
+      height: req.query.height ? parseInt(req.query.height, 10) : 400,
+      channels: 4,
+      background: { r: 255, g: 0, b: 0, alpha: 0.5 },
+    },
+  })
+    .png()
+    .toBuffer()
+
+  res.setHeader("Content-Type", "image/png")
+  res.send(data)
+}
