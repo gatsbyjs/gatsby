fa04e06454a50124774c858fc182b268ad4683a9
Michal Piechowiak <misiek.piechowiak@gmail.com>
2021-06-10 21:32:35 +0200

    actually fix @babel/register

--- packages/gatsby/src/utils/worker/__tests__/test-helpers/wrapper-for-tests.js
+++ packages/gatsby/src/utils/worker/__tests__/test-helpers/wrapper-for-tests.js
@@ -1,8 +1,13 @@
-// spawned process won't use jest config to support TS, so we need to add support ourselves
-require(`@babel/register`)({
-  extensions: [`.js`, `.ts`],
-  configFile: require.resolve(`../../../../../babel.config.js`),
-  ignore: [/node_modules/],
-})
+// this IS executed in process that spawn worker to analyze exports and create functions for them on worker pool object
+// so we want to use `@babel/register` ONLY inside actual worker as otherwise it does mess with jest transformation and tools like `rewire`
+// see `./create-test-worker.ts` file for explanation why this env var is used.
+if (process.env.JEST_WORKER_ID) {
+  // spawned process won't use jest config to support TS, so we need to add support ourselves
+  require(`@babel/register`)({
+    extensions: [`.js`, `.ts`],
+    configFile: require.resolve(`../../../../../babel.config.js`),
+    ignore: [/node_modules/],
+  })
+}
 
 module.exports = require(`./child-for-tests`)
