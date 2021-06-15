1f725a2833bb69d1ecf92c6b64dd36a0911f94bc
Sijin raj <54431616+sijin-raj@users.noreply.github.com>
2021-06-15 11:40:33 +0530

    update-new
    emoji add

--- starters/default/src/pages/404.js
+++ starters/default/src/pages/404.js
@@ -6,8 +6,8 @@ import Seo from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>404: Not Found !!</h1>
+    <p>You just hit a route that doesn&#39;t exist... the sadness ☹️.</p>
   </Layout>
 )
 
