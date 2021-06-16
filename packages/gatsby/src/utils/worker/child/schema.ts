c23c5cb6eaf03e00ef22ee492b336dd3cf071967
LekoArts <lekoarts@gmail.com>
2021-06-16 10:55:28 +0200

    order

--- packages/gatsby/src/utils/worker/child/schema.ts
+++ packages/gatsby/src/utils/worker/child/schema.ts
@@ -15,5 +15,5 @@ export async function buildSchema(): Promise<void> {
 
   await apiRunnerNode(`createSchemaCustomization`)
 
-  await build({ fullMetadataBuild: false })
+  await build({ fullMetadataBuild: false, parentSpan: {} })
 }
