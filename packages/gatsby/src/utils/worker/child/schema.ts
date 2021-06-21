c63bf69fdd71f149d2194694c61f3560599ded65
LekoArts <lekoarts@gmail.com>
2021-06-21 10:29:10 +0200

    run queries in child/schema

--- packages/gatsby/src/utils/worker/child/schema.ts
+++ packages/gatsby/src/utils/worker/child/schema.ts
@@ -1,6 +1,13 @@
-import apiRunnerNode from "../../api-runner-node"
+import {
+  IGroupedQueryIds,
+  runPageQueries,
+  runStaticQueries,
+} from "../../../services"
+import { GraphQLRunner } from "../../../query/graphql-runner"
 import { store } from "../../../redux"
 import { build } from "../../../schema"
+import { getDataStore } from "../../../datastore"
+import apiRunnerNode from "../../api-runner-node"
 import { setState } from "./state"
 
 export function setInferenceMetadata(): void {
@@ -25,4 +32,32 @@ export async function buildSchema(): Promise<void> {
   await apiRunnerNode(`createSchemaCustomization`)
 
   await build({ fullMetadataBuild: false, parentSpan: {} })
+}
+
+export async function runQueries(queryIds: IGroupedQueryIds): Promise<void> {
+  const workerStore = store.getState()
+
+  // If buildSchema() didn't run yet, execute it
+  if (workerStore.schemaCustomization.composer === null) {
+    await buildSchema()
+  }
+
+  const graphqlRunner = new GraphQLRunner(store, {
+    collectStats: true,
+    graphqlTracing: workerStore.program.graphqlTracing,
+  })
+
+  await runStaticQueries({
+    queryIds,
+    store,
+    graphqlRunner,
+  })
+
+  await runPageQueries({
+    queryIds,
+    store,
+    graphqlRunner,
+  })
+
+  await getDataStore().ready()
 }
