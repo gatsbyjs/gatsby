113e43e19b1086e74c55f7092e4a66b7e9fe7c95
Kyle Mathews <mathews.kyle@gmail.com>
2021-06-22 10:39:58 -0700

    feat(gatsby-source-drupal): Disable caching + add http/2 agent (#32012)
    * feat(gatsby-source-drupal): Force revalidation on Drupal API requests
    
    Talking to Gatsby/Drupal users — most set long max-age for the Drupal cache to
    keep their edge http cache as fresh as possible as Drupal can directly purge
    its edge http cache.
    
    But this has the unfortunate side-effect with the recent http client change in
    https://github.com/gatsbyjs/gatsby/pull/31514 that API calls aren't
    revalidated. Meaning that a user could change some content in Drupal and not
    see those updates in their Gatsby site until the Drupal cache expires in the
    Gatsby cache.
    
    This PR removes the `cache-control` header from Drupal API responses so that
    we only can use `etag` for caching which forces revalidation on every request.
    
    * Remove debugging line
    
    * Add http/2 agent
    
    * Disable cache — it's slower than refetch with revalidation
    
    * push don't concat
    
    * consistent return
    
    * Fix statusCode check for got
    
    * Actually this was correct...
    
    * Actually use http/2

--- packages/gatsby-source-drupal/src/gatsby-node.js
+++ packages/gatsby-source-drupal/src/gatsby-node.js
@@ -2,6 +2,7 @@ const got = require(`got`)
 const _ = require(`lodash`)
 const urlJoin = require(`url-join`)
 import HttpAgent from "agentkeepalive"
+const http2wrapper = require(`http2-wrapper`)
 
 const { HttpsAgent } = HttpAgent
 
@@ -18,10 +19,17 @@ const { handleReferences, handleWebhookUpdate } = require(`./utils`)
 const agent = {
   http: new HttpAgent(),
   https: new HttpsAgent(),
+  http2: new http2wrapper.Agent(),
 }
 
 async function worker([url, options]) {
-  return got(url, { agent, ...options })
+  return got(url, {
+    agent,
+    cache: false,
+    request: http2wrapper.auto,
+    http2: true,
+    ...options,
+  })
 }
 
 const requestQueue = require(`fastq`).promise(worker, 20)
@@ -170,10 +178,13 @@ exports.sourceNodes = async (
           headers,
           searchParams: params,
           responseType: `json`,
-          cache,
         },
       ])
 
+      // Fastbuilds returns a -1 if:
+      // - the timestamp has expired
+      // - if old fastbuild logs were purged
+      // - it's been a really long time since you synced so you just do a full fetch.
       if (res.body.status === -1) {
         // The incremental data is expired or this is the first fetch.
         reporter.info(`Unable to pull incremental data changes from Drupal`)
@@ -267,11 +278,11 @@ exports.sourceNodes = async (
         headers,
         searchParams: params,
         responseType: `json`,
-        cache,
       },
     ])
     allData = await Promise.all(
       _.map(res.body.links, async (url, type) => {
+        const dataArray = []
         if (disallowedLinkTypes.includes(type)) return
         if (!url) return
         if (!type) return
@@ -281,7 +292,7 @@ exports.sourceNodes = async (
           entityType => entityType === type
         )
 
-        const getNext = async (url, data = []) => {
+        const getNext = async url => {
           if (typeof url === `object`) {
             // url can be string or object containing href field
             url = url.href
@@ -315,37 +326,33 @@ exports.sourceNodes = async (
                 password: basicAuth.password,
                 headers,
                 responseType: `json`,
-                cache,
               },
             ])
           } catch (error) {
             if (error.response && error.response.statusCode == 405) {
               // The endpoint doesn't support the GET method, so just skip it.
-              return []
+              return
             } else {
               console.error(`Failed to fetch ${url}`, error.message)
               console.log(error)
               throw error
             }
           }
-          data = data.concat(d.body.data)
+          dataArray.push(...d.body.data)
           // Add support for includes. Includes allow entity data to be expanded
           // based on relationships. The expanded data is exposed as `included`
           // in the JSON API response.
           // See https://www.drupal.org/docs/8/modules/jsonapi/includes
           if (d.body.included) {
-            data = data.concat(d.body.included)
+            dataArray.push(...d.body.included)
           }
           if (d.body.links && d.body.links.next) {
-            data = await getNext(d.body.links.next, data)
+            await getNext(d.body.links.next)
           }
-
-          return data
         }
 
-        let data = []
         if (isTranslatable === false) {
-          data = await getNext(url)
+          await getNext(url)
         } else {
           for (let i = 0; i < languageConfig.enabledLanguages.length; i++) {
             let currentLanguage = languageConfig.enabledLanguages[i]
@@ -368,13 +375,13 @@ exports.sourceNodes = async (
             )
             const dataForLanguage = await getNext(joinedUrl)
 
-            data = data.concat(dataForLanguage)
+            dataArray.push(...dataForLanguage)
           }
         }
 
         const result = {
           type,
-          data,
+          data: dataArray,
         }
 
         // eslint-disable-next-line consistent-return
