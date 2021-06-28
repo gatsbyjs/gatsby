e2664d6261bd845ae0523ce5bba9d0db228d6a9d
Timmy Willison <4timmywil@gmail.com>
2021-06-28 15:26:39 -0400

    refactor(gatsby-plugin-remove-trailing-slashes): remove unnecessary Promise wrapper

--- packages/gatsby-plugin-remove-trailing-slashes/src/gatsby-node.js
+++ packages/gatsby-plugin-remove-trailing-slashes/src/gatsby-node.js
@@ -4,13 +4,10 @@ const replacePath = _path => (_path === `/` ? _path : _path.replace(/\/$/, ``))
 exports.onCreatePage = ({ page, actions }) => {
   const { createPage, deletePage } = actions
 
-  return new Promise(resolve => {
-    const oldPage = Object.assign({}, page)
-    page.path = replacePath(page.path)
-    if (page.path !== oldPage.path) {
-      deletePage(oldPage)
-      createPage(page)
-    }
-    resolve()
-  })
+  const oldPage = Object.assign({}, page)
+  page.path = replacePath(page.path)
+  if (page.path !== oldPage.path) {
+    deletePage(oldPage)
+    createPage(page)
+  }
 }
