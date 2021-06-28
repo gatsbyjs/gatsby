68c0c9f677e18230bf7b093c5a0aa7c9385d66c8
Timmy Willison <4timmywil@gmail.com>
2021-06-28 12:07:32 -0400

    fix(gatsby-plugin-remove-trailing-slashes): ensure deletePage has finished before createPage

--- packages/gatsby-plugin-remove-trailing-slashes/src/__tests__/gatsby-node.js
+++ packages/gatsby-plugin-remove-trailing-slashes/src/__tests__/gatsby-node.js
@@ -1,19 +1,21 @@
 const { onCreatePage } = require(`../gatsby-node`)
 
+const mockAsync = () => new Promise(resolve => setTimeout(resolve, 10))
+
 describe(`gatsby-plugin-remove-trailing-slashes`, () => {
   const actions = {
-    createPage: jest.fn(),
-    deletePage: jest.fn(),
+    createPage: jest.fn(mockAsync),
+    deletePage: jest.fn(mockAsync),
   }
 
-  it(`correctly keeps index /`, () => {
-    onCreatePage({ actions, page: { path: `/` } })
+  it(`correctly keeps index /`, async () => {
+    await onCreatePage({ actions, page: { path: `/` } })
     expect(actions.createPage).not.toBeCalled()
     expect(actions.deletePage).not.toBeCalled()
   })
 
-  it(`correctly removes slash and recreated page`, () => {
-    onCreatePage({ actions, page: { path: `/home/` } })
+  it(`correctly removes slash and recreated page`, async () => {
+    await onCreatePage({ actions, page: { path: `/home/` } })
     expect(actions.deletePage).toBeCalledWith({ path: `/home/` })
     expect(actions.createPage).toBeCalledWith({ path: `/home` })
   })
--- packages/gatsby-plugin-remove-trailing-slashes/src/gatsby-node.js
+++ packages/gatsby-plugin-remove-trailing-slashes/src/gatsby-node.js
@@ -1,16 +1,13 @@
 // Replacing '/' would result in empty string which is invalid
 const replacePath = _path => (_path === `/` ? _path : _path.replace(/\/$/, ``))
 
-exports.onCreatePage = ({ page, actions }) => {
+exports.onCreatePage = async ({ page, actions }) => {
   const { createPage, deletePage } = actions
 
-  return new Promise(resolve => {
-    const oldPage = Object.assign({}, page)
-    page.path = replacePath(page.path)
-    if (page.path !== oldPage.path) {
-      deletePage(oldPage)
-      createPage(page)
-    }
-    resolve()
-  })
+  const oldPage = Object.assign({}, page)
+  page.path = replacePath(page.path)
+  if (page.path !== oldPage.path) {
+    await deletePage(oldPage)
+    await createPage(page)
+  }
 }
