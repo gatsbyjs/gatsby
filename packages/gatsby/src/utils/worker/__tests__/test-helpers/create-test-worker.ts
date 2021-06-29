93bcbbfdecc74807bd51bb8841179141438ce158
Michal Piechowiak <misiek.piechowiak@gmail.com>
2021-06-29 13:26:03 +0200

    add comments about usage of JEST_WORKER_ID which is used by tests, even tho we ourselves will be using gatsby-worker

--- packages/gatsby/src/utils/worker/__tests__/test-helpers/create-test-worker.ts
+++ packages/gatsby/src/utils/worker/__tests__/test-helpers/create-test-worker.ts
@@ -10,6 +10,8 @@ export function createTestWorker(): GatsbyTestWorkerPool {
     {
       numWorkers: 1,
       env: {
+        // We are using JEST_WORKER_ID env so that worker use same test database as
+        // jest runner process
         FORCE_TEST_DATABASE_ID: process.env.JEST_WORKER_ID,
         GATSBY_WORKER_POOL_WORKER: `true`,
         NODE_OPTIONS: `--require ${require.resolve(`./ts-register`)}`,
