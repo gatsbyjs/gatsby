43200722bbbfb011c1b96c6228cf3b6523f34c57
Vladimir Razuvaev <vladimir.razuvaev@gmail.com>
2021-06-30 19:06:25 +0700

    refactor(gatsby): simplified materialization a bit (#31882)
    * do not retain nodes when tracking inline objects
    
    * fix tests
    
    * simplify materialization a bit
    
    * add test demonstrating the edge case with querying by interface
    
    Co-authored-by: gatsbybot <mathews.kyle+gatsbybot@gmail.com>

--- packages/gatsby/src/schema/node-model.js
+++ packages/gatsby/src/schema/node-model.js
@@ -75,7 +75,7 @@ class LocalNodeModel {
     this.createPageDependencyActionCreator = createPageDependency
 
     this._rootNodeMap = new WeakMap()
-    this._trackedRootNodes = new Set()
+    this._trackedRootNodes = new WeakSet()
     this._prepareNodesQueues = {}
     this._prepareNodesPromises = {}
     this._preparedNodesCache = new Map()
@@ -280,7 +280,10 @@ class LocalNodeModel {
       nodeTypeNames
     )
 
-    await this.prepareNodes(gqlType, fields, fieldsToResolve, nodeTypeNames)
+    for (const nodeTypeName of nodeTypeNames) {
+      const gqlNodeType = this.schema.getType(nodeTypeName)
+      await this.prepareNodes(gqlNodeType, fields, fieldsToResolve)
+    }
 
     if (materializationActivity) {
       materializationActivity.end()
@@ -391,7 +394,7 @@ class LocalNodeModel {
     return this.trackPageDependencies(first, pageDependencies)
   }
 
-  prepareNodes(type, queryFields, fieldsToResolve, nodeTypeNames) {
+  prepareNodes(type, queryFields, fieldsToResolve) {
     const typeName = type.name
     if (!this._prepareNodesQueues[typeName]) {
       this._prepareNodesQueues[typeName] = []
@@ -405,7 +408,7 @@ class LocalNodeModel {
     if (!this._prepareNodesPromises[typeName]) {
       this._prepareNodesPromises[typeName] = new Promise(resolve => {
         process.nextTick(async () => {
-          await this._doResolvePrepareNodesQueue(type, nodeTypeNames)
+          await this._doResolvePrepareNodesQueue(type)
           resolve()
         })
       })
@@ -414,7 +417,7 @@ class LocalNodeModel {
     return this._prepareNodesPromises[typeName]
   }
 
-  async _doResolvePrepareNodesQueue(type, nodeTypeNames) {
+  async _doResolvePrepareNodesQueue(type) {
     const typeName = type.name
     const queue = this._prepareNodesQueues[typeName]
     this._prepareNodesQueues[typeName] = []
@@ -442,7 +445,8 @@ class LocalNodeModel {
     )
 
     if (!_.isEmpty(actualFieldsToResolve)) {
-      await saveResolvedNodes(nodeTypeNames, async node => {
+      const resolvedNodes = new Map()
+      for (const node of getDataStore().iterateNodesByType(typeName)) {
         this.trackInlineObjectsInRootNode(node)
         const resolvedFields = await resolveRecursive(
           this,
@@ -456,8 +460,14 @@ class LocalNodeModel {
         if (!node.__gatsby_resolved) {
           node.__gatsby_resolved = {}
         }
-        return _.merge(node.__gatsby_resolved, resolvedFields)
-      })
+        resolvedNodes.set(
+          node.id,
+          _.merge(node.__gatsby_resolved, resolvedFields)
+        )
+      }
+      if (resolvedNodes.size) {
+        await saveResolvedNodes(typeName, resolvedNodes)
+      }
       this._preparedNodesCache.set(
         typeName,
         _.merge(
@@ -484,7 +494,7 @@ class LocalNodeModel {
    * @param {Node} node Root Node
    */
   trackInlineObjectsInRootNode(node) {
-    if (!this._trackedRootNodes.has(node.id)) {
+    if (!this._trackedRootNodes.has(node)) {
       addRootNodeToInlineObject(
         this._rootNodeMap,
         node,
@@ -492,7 +502,7 @@ class LocalNodeModel {
         true,
         new Set()
       )
-      this._trackedRootNodes.add(node.id)
+      this._trackedRootNodes.add(node)
     }
   }
 
@@ -940,23 +950,14 @@ const addRootNodeToInlineObject = (
   }
 }
 
-const saveResolvedNodes = async (nodeTypeNames, resolver) => {
-  for (const typeName of nodeTypeNames) {
-    const resolvedNodes = new Map()
-    for (const node of getDataStore().iterateNodesByType(typeName)) {
-      const resolved = await resolver(node)
-      resolvedNodes.set(node.id, resolved)
-    }
-    if (!resolvedNodes.size) continue
-
-    store.dispatch({
-      type: `SET_RESOLVED_NODES`,
-      payload: {
-        key: typeName,
-        nodes: resolvedNodes,
-      },
-    })
-  }
+const saveResolvedNodes = async (typeName, resolvedNodes) => {
+  store.dispatch({
+    type: `SET_RESOLVED_NODES`,
+    payload: {
+      key: typeName,
+      nodes: resolvedNodes,
+    },
+  })
 }
 
 const deepObjectDifference = (from, to) => {
