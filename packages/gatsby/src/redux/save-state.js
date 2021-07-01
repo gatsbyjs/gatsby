85fc2e26be26c9a521e3bcbccdd0a28f6213ef71
abhiaiyer91 <abhiaiyer91@gmail.com>
2021-07-01 06:35:12 -0700

    Track persistence time of save-state

--- packages/gatsby/src/redux/save-state.js
+++ packages/gatsby/src/redux/save-state.js
@@ -1,5 +1,6 @@
 const _ = require(`lodash`)
 const report = require(`gatsby-cli/lib/reporter`)
+const { captureEvent } = require(`gatsby-telemetry`)
 const redux = require(`./`)
 
 let saveInProgress = false
@@ -7,8 +8,14 @@ async function saveState() {
   if (saveInProgress) return
   saveInProgress = true
 
+  const startTime = new Date()
+
   try {
     await redux.saveState()
+
+    const duration = (new Date().getTime() - startTime.getTime()) / 1000
+
+    captureEvent(`EVENT_NAME`, { name: `state_persistence`, duration })
   } catch (err) {
     report.warn(`Error persisting state: ${(err && err.message) || err}`)
   }
