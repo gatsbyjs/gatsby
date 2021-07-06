b177634c7e4cd216ce5f97bf1489252e4d3dc445
Kyle Mathews <mathews.kyle@gmail.com>
2021-07-06 09:07:10 -0700

    feat(gatsby): Enable webpack fs caching for development behind flag with 20% opt-in
    Testing on my blog, it drops the start of the webpack server from 11s -> 6s.
    
    Using fs caching in development doesn't need an explicit persistence step as webpack will write out to the cache
    whenever it's idling (which happens a lot durinv development).

--- packages/gatsby/src/utils/webpack.config.js
+++ packages/gatsby/src/utils/webpack.config.js
@@ -783,8 +783,8 @@ module.exports = async (
   }
 
   if (
-    process.env.GATSBY_EXPERIMENTAL_PRESERVE_WEBPACK_CACHE &&
-    (stage === `build-javascript` || stage === `build-html`)
+    process.env.GATSBY_EXPERIMENTAL_DEV_WEBPACK_CACHE &&
+    (stage === `develop` || stage === `develop-html`)
   ) {
     const cacheLocation = path.join(
       program.directory,
