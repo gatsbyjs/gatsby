072f7d612aa9157b2b0a50704df2c66450a7f190
Michal Piechowiak <misiek.piechowiak@gmail.com>
2021-07-06 11:41:05 +0200

    add uncaughtException and unhandledRejection handlers and signal-exit logs to worker

--- packages/gatsby/src/utils/worker/child/index.ts
+++ packages/gatsby/src/utils/worker/child/index.ts
@@ -1,5 +1,25 @@
+import reporter from "gatsby-cli/lib/reporter"
+import signalExit from "signal-exit"
+
 // Note: this doesn't check for conflicts between module exports
 export { renderHTMLProd, renderHTMLDev } from "./render-html"
 export { setInferenceMetadata, buildSchema } from "./schema"
 export { setQueries, runQueries } from "./queries"
 export { loadConfigAndPlugins } from "./load-config-and-plugins"
+
+process.on(`unhandledRejection`, (reason: unknown) => {
+  reporter.panic((reason as Error) || `Unhandled rejection`)
+})
+
+process.on(`uncaughtException`, function (err) {
+  reporter.panic(err)
+})
+
+signalExit((code, signal) => {
+  reporter.verbose(
+    `Worker ${process.env.GATSBY_WORKER_ID} exitted with ${JSON.stringify({
+      code,
+      signal,
+    })}`
+  )
+})
