dcd07a3b967c6830e05be77cd3633c9d35dc30bd
Lennart <lekoarts@gmail.com>
2021-07-07 13:01:10 +0200

    Restore index.ts

--- packages/gatsby/src/utils/worker/child/index.ts
+++ packages/gatsby/src/utils/worker/child/index.ts
@@ -1,5 +1,3 @@
-import reporter from "gatsby-cli/lib/reporter"
-import signalExit from "signal-exit"
 import { initJobsMessagingInWorker } from "../../jobs/worker-messaging"
 
 initJobsMessagingInWorker()
@@ -9,24 +7,3 @@ export { renderHTMLProd, renderHTMLDev } from "./render-html"
 export { setInferenceMetadata, buildSchema } from "./schema"
 export { setQueries, runQueries } from "./queries"
 export { loadConfigAndPlugins } from "./load-config-and-plugins"
-
-process.on(`unhandledRejection`, (reason: unknown) => {
-  process.stdout.write(`[1] unhandledRejection\n`)
-  reporter.panic((reason as Error) || `Unhandled rejection`)
-})
-
-process.on(`uncaughtException`, function (err) {
-  process.stdout.write(`[1] uncaughtException\n`)
-  reporter.panic(err)
-})
-
-signalExit((code, signal) => {
-  process.stdout.write(
-    `[inside] Worker ${
-      process.env.GATSBY_WORKER_ID
-    } exited with ${JSON.stringify({
-      code,
-      signal,
-    })}\n`
-  )
-})
