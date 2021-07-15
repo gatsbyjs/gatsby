805757889c2a47675c58412360bdd2b62429da93
LekoArts <lekoarts@gmail.com>
2021-07-15 13:49:43 +0200

    dynamic import + test case for object

--- packages/gatsby/src/utils/__tests__/cache-lmdb.ts
+++ packages/gatsby/src/utils/__tests__/cache-lmdb.ts
@@ -1,20 +1,26 @@
 import * as path from "path"
 import fs from "fs-extra"
-import GatsbyCacheLmdb from "../cache-lmdb"
 import { describeWhenLMDB } from "../worker/__tests__/test-helpers"
 
-let cache: GatsbyCacheLmdb | undefined
-
-beforeAll(async () => {
-  cache = new GatsbyCacheLmdb({ name: `__test__` })
-  const fileDir = path.join(
-    process.cwd(),
-    `.cache/caches-lmdb-${process.env.JEST_WORKER_ID}`
-  )
-  await fs.emptyDir(fileDir)
-})
+const complexObject = {
+  key: `value`,
+  another_key: 2,
+  nested: { hello: `world`, foo: `bar`, nested: { super: `duper` } },
+}
 
 describeWhenLMDB(`cache-lmdb`, () => {
+  let cache
+
+  beforeAll(async () => {
+    const { default: GatsbyCacheLmdb } = await import(`../cache-lmdb`)
+    cache = new GatsbyCacheLmdb({ name: `__test__` })
+    const fileDir = path.join(
+      process.cwd(),
+      `.cache/caches-lmdb-${process.env.JEST_WORKER_ID}`
+    )
+    await fs.emptyDir(fileDir)
+  })
+
   it(`it can be instantiated`, () => {
     if (!cache) fail(`cache not instantiated`)
   })
@@ -23,11 +29,19 @@ describeWhenLMDB(`cache-lmdb`, () => {
     expect(cache.set).toEqual(expect.any(Function))
   })
   describe(`set`, () => {
-    it(`resolves to the value it cached`, () =>
-      expect(cache.set(`a`, `b`)).resolves.toBe(`b`))
+    it(`resolves to the value it cached (string)`, () =>
+      expect(cache.set(`string`, `I'm a simple string`)).resolves.toBe(
+        `I'm a simple string`
+      ))
+    it(`resolves to the value it cached (object)`, () =>
+      expect(cache.set(`object`, complexObject)).resolves.toStrictEqual(
+        complexObject
+      ))
   })
   describe(`get`, () => {
-    it(`resolves to the found value`, () =>
-      expect(cache.get(`a`)).resolves.toBe(`b`))
+    it(`resolves to the found value (string)`, () =>
+      expect(cache.get(`string`)).resolves.toBe(`I'm a simple string`))
+    it(`resolves to the found value (object)`, () =>
+      expect(cache.get(`object`)).resolves.toStrictEqual(complexObject))
   })
 })
