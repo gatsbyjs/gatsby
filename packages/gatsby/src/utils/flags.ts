dfd5abc4a7cb47ed2c07c3faa484b4fd62c336a8
Lennart <lekoarts@gmail.com>
2021-07-15 14:57:51 +0200

    Update flags.ts

--- packages/gatsby/src/utils/flags.ts
+++ packages/gatsby/src/utils/flags.ts
@@ -78,7 +78,7 @@ const activeFlags: Array<IFlag> = [
     command: `develop`,
     telemetryId: `FastDev`,
     experimental: false,
-    description: `Enable all experiments aimed at improving develop server start time`,
+    description: `Enable all experiments aimed at improving develop server start time.`,
     includedFlags: [
       `DEV_SSR`,
       `PRESERVE_FILE_DOWNLOAD_CACHE`,
