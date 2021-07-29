9b427936752ec0b107b36331f444366987355fbb
Vladimir Razuvaev <vladimir.razuvaev@gmail.com>
2021-07-29 20:21:49 +0700

    fix(gatsby): correct results for multiple range filters with indexes (#32406)
    * fix(gatsby): correct results when using multiple range filters with indexes
    
    * fix tests
    
    * fix handling of skip/limit with indexes
    
    * fix incorrect limit

--- packages/gatsby/src/datastore/lmdb/query/suggest-index.ts
+++ packages/gatsby/src/datastore/lmdb/query/suggest-index.ts
@@ -19,10 +19,16 @@ interface ISelectIndexArgs {
 type IndexField = [fieldName: string, orderDirection: number]
 type IndexFields = Array<IndexField>
 
+/**
+ * Suggest index fields for this combination of filter and sort.
+ *
+ * Prioritizes sort fields over filter fields when can't use index
+ * for both because sorting is expensive both CPU and memory-wise.
+ */
 export function suggestIndex({
   filter,
   sort,
-  maxFields = 4,
+  maxFields = 6,
 }: ISelectIndexArgs): Array<IndexField> {
   const filterQueries = createDbQueriesFromObject(prepareQueryArgs(filter))
   const filterQueriesThatCanUseIndex = getQueriesThatCanUseIndex(filterQueries)
@@ -37,39 +43,27 @@ export function suggestIndex({
   if (!sortFields.length) {
     return dedupeAndTrim(toIndexFields(filterQueriesThatCanUseIndex), maxFields)
   }
-  const overlap = findOverlappingFields(
-    filterQueriesThatCanUseIndex,
-    sortFields
+
+  // Combined index for filter+sort only makes sense when all prefix fields have `eq` predicate
+  // Same as https://docs.mongodb.com/manual/tutorial/sort-results-with-indexes/#sort-and-non-prefix-subset-of-an-index
+  const sortDirection = sortFields[0][1]
+  const eqFilterQueries = getEqQueries(filterQueriesThatCanUseIndex)
+  const eqFilterFields = toIndexFields(eqFilterQueries, sortDirection)
+
+  // Index prefix should not contain eq filters overlapping with sort fields
+  const overlap = findOverlappingFields(eqFilterQueries, sortFields)
+
+  return dedupeAndTrim(
+    [
+      ...eqFilterFields.filter(([name]) => !overlap.has(name)),
+      ...sortFields,
+      // Still append other filter fields to the tail of the index to leverage additional filtering
+      //  of results using data stored in the index (without loading full node object)
+      //  Note: fields previously listed in eqFilterFields and sortFields will be removed in dedupeAndTrim
+      ...toIndexFields(filterQueriesThatCanUseIndex, sortDirection),
+    ],
+    maxFields
   )
-  if (!overlap.size) {
-    // Filter and sort fields do not overlap.
-    // In this case combined index for filter+sort only makes sense when all filters have `eq` predicate
-    // Same as https://docs.mongodb.com/manual/tutorial/sort-results-with-indexes/#sort-and-non-prefix-subset-of-an-index
-    const eqFields = getFieldsWithEqPredicate(filterQueriesThatCanUseIndex)
-
-    if (eqFields.size === filterQueriesThatCanUseIndex.length) {
-      const eqFilterFields: Array<IndexField> = [...eqFields].map(f => [f, 1])
-      return dedupeAndTrim([...eqFilterFields, ...sortFields], maxFields)
-    }
-    // Single unbound range filter: "lt", "gt", "gte", "lte" (but not "in"): prefer sort fields
-    if (
-      filterQueriesThatCanUseIndex.length === 1 &&
-      getFilterStatement(filterQueriesThatCanUseIndex[0]).comparator !==
-        DbComparator.IN
-    ) {
-      return dedupeAndTrim(sortFields, maxFields)
-    }
-    // Otherwise prefer filter fields
-    return dedupeAndTrim(toIndexFields(filterQueriesThatCanUseIndex), maxFields)
-  }
-
-  // There is an overlap:
-  // First add all non-overlapping filter fields to index prefix, then all sort fields (including overlapping)
-  const filterFields = filterQueriesThatCanUseIndex
-    .map((q): IndexField => [dbQueryToDottedField(q), 1])
-    .filter(([name]) => !overlap.has(name))
-
-  return dedupeAndTrim([...filterFields, ...sortFields], maxFields)
 }
 
 const canUseIndex = new Set([
@@ -131,19 +125,20 @@ function findOverlappingFields(
   return overlap
 }
 
-function getFieldsWithEqPredicate(filterQueries: Array<DbQuery>): Set<string> {
-  return new Set<string>(
-    filterQueries
-      .filter(
-        filterQuery =>
-          getFilterStatement(filterQuery).comparator === DbComparator.EQ
-      )
-      .map(filterQuery => dbQueryToDottedField(filterQuery))
+function getEqQueries(filterQueries: Array<DbQuery>): Array<DbQuery> {
+  return filterQueries.filter(
+    filterQuery =>
+      getFilterStatement(filterQuery).comparator === DbComparator.EQ
   )
 }
 
-function toIndexFields(queries: Array<DbQuery>): IndexFields {
-  return queries.map((q): IndexField => [dbQueryToDottedField(q), 1])
+function toIndexFields(
+  queries: Array<DbQuery>,
+  sortDirection: number = 1
+): IndexFields {
+  return queries.map(
+    (q): IndexField => [dbQueryToDottedField(q), sortDirection]
+  )
 }
 
 function dedupeAndTrim(fields: IndexFields, maxFields: number): IndexFields {
