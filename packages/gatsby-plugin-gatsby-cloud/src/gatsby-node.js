01a334ca073acc9e757aa14d38007a8a6e75b548
abhiaiyer91 <abhiaiyer91@gmail.com>
2021-08-04 08:16:39 -0700

    new canary

--- packages/gatsby-plugin-gatsby-cloud/src/gatsby-node.js
+++ packages/gatsby-plugin-gatsby-cloud/src/gatsby-node.js
@@ -58,7 +58,7 @@ exports.onPostBuild = async (
   const routes = {}
   console.log(`Assembling Routes`)
   for (const [pathname] of pages) {
-    routes[`${pathname.substring(1)}/index.html`] = `SSR`
+    routes[`${pathname.substring(1)}index.html`] = `SSR`
   }
 
   console.log(`Routes Manifest`, routes)
