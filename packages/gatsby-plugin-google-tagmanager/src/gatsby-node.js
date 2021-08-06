e69e0b00d337827b6bc471ab98097e94c3a683d8
Brett Tackaberry <brett.tackaberry@gmail.com>
2021-08-06 12:53:11 -0400

    Update options schema

--- packages/gatsby-plugin-google-tagmanager/src/gatsby-node.js
+++ packages/gatsby-plugin-google-tagmanager/src/gatsby-node.js
@@ -41,4 +41,5 @@ exports.pluginOptionsSchema = ({ Joi }) =>
         `Name of the event that is triggered on every Gatsby route change.`
       ),
     enableWebVitalsTracking: Joi.boolean().default(false),
+    selfHostedOrigin: Joi.string().default("https://www.googletagmanager.com").description(`Self hosted origin.`),
   })
