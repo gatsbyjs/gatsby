133033b5d58e0a3c7e5d544896eee46c3bff9fab
Daniel Lew <danielshlomolew@gmail.com>
2021-08-11 15:04:43 -0400

    add deprecation notice for valueType field

--- packages/gatsby-source-shopify/src/create-schema-customization.ts
+++ packages/gatsby-source-shopify/src/create-schema-customization.ts
@@ -52,13 +52,15 @@ export function createSchemaCustomization(
   { actions, schema }: CreateSchemaCustomizationArgs,
   pluginOptions: ShopifyPluginOptions
 ): void {
-  const includeCollections =
-    pluginOptions.shopifyConnections?.includes(`collections`)
+  const includeCollections = pluginOptions.shopifyConnections?.includes(
+    `collections`
+  )
 
   const includeOrders = pluginOptions.shopifyConnections?.includes(`orders`)
 
-  const includeLocations =
-    pluginOptions.shopifyConnections?.includes(`locations`)
+  const includeLocations = pluginOptions.shopifyConnections?.includes(
+    `locations`
+  )
 
   const name = (name: string): string =>
     `${pluginOptions.typePrefix || ``}${name}`
@@ -73,6 +75,10 @@ export function createSchemaCustomization(
     updatedAt: `Date!`,
     value: `String!`,
     type: `String!`,
+    valueType: {
+      type: `String!`,
+      deprecationReason: `Shopify has deprecated this field`,
+    },
   }
 
   const metafieldInterface = schema.buildInterfaceType({
