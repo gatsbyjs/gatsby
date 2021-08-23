114e3d39695bd96b8c477a413f5927317a19b4cb
Abhi Aiyer <abhiaiyer91@gmail.com>
2021-08-23 06:07:07 -0700

    feat(gatsby-core-utils): Move page-data & HTML utils to package (#32861)
    * Move some utils to core-utils
    
    * Move some utils to core-utils
    
    * Move some utils to core-utils
    
    * Move some utils to core-utils
    
    * moreUtils
    
    * Replace getPageHtmlFilePath in app
    
    * Backticks
    
    * replace fixedPagePath
    
    * Add more imports of fixedPagePath
    
    * change name of getPageHtmlFilePath
    
    * change name of getPageDataPath
    
    * change require
    
    * add deps, add windows test utils, delete unused remove function
    
    * revert cache-dir changes
    
    * add deps
    
    * fix remove function
    
    Co-authored-by: LekoArts <lekoarts@gmail.com>

--- packages/gatsby-core-utils/src/page-html.ts
+++ packages/gatsby-core-utils/src/page-html.ts
@@ -4,11 +4,7 @@ import path from "path"
 const checkForHtmlSuffix = (pagePath: string): boolean =>
   !/\.(html?)$/i.test(pagePath)
 
-// copied from https://github.com/markdalgleish/static-site-generator-webpack-plugin/blob/master/index.js#L161
-export const getPageHtmlFilePath = (
-  dir: string,
-  outputPath: string
-): string => {
+export function generateHtmlPath(dir: string, outputPath: string): string {
   let outputFileName = outputPath.replace(/^(\/|\\)/, ``) // Remove leading slashes for webpack-dev-server
 
   if (checkForHtmlSuffix(outputPath)) {
@@ -18,11 +14,11 @@ export const getPageHtmlFilePath = (
   return path.join(dir, outputFileName)
 }
 
-export const remove = async (
+export async function remove(
   { publicDir }: { publicDir: string },
   pagePath: string
-): Promise<void> => {
-  const filePath = getPageHtmlFilePath(publicDir, pagePath)
+): Promise<void> {
+  const filePath = generateHtmlPath(publicDir, pagePath)
   if (fs.existsSync(filePath)) {
     return await fs.remove(filePath)
   }
