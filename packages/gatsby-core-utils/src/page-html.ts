4f12b2bb8461f2b27ed772b1434e0fe7feaaad0c
LekoArts <lekoarts@gmail.com>
2021-08-23 13:48:01 +0200

    fix remove function

--- packages/gatsby-core-utils/src/page-html.ts
+++ packages/gatsby-core-utils/src/page-html.ts
@@ -1,3 +1,4 @@
+import fs from "fs-extra"
 import path from "path"
 
 const checkForHtmlSuffix = (pagePath: string): boolean =>
@@ -11,4 +12,15 @@ export function generateHtmlPath(dir: string, outputPath: string): string {
   }
 
   return path.join(dir, outputFileName)
+}
+
+export async function remove(
+  { publicDir }: { publicDir: string },
+  pagePath: string
+): Promise<void> {
+  const filePath = generateHtmlPath(publicDir, pagePath)
+  if (fs.existsSync(filePath)) {
+    return await fs.remove(filePath)
+  }
+  return Promise.resolve()
 }
