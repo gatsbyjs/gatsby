1548ffb990726531599dddfaafa5e6842e16dfd8
Dan Kirkham <herecy@live.co.uk>
2021-09-04 23:10:13 +0100

    Fix optional

--- packages/gatsby/src/redux/types.ts
+++ packages/gatsby/src/redux/types.ts
@@ -94,7 +94,7 @@ export interface IGatsbyConfig {
   pathPrefix?: string
   assetPrefix?: string
   mapping?: Record<string, string>
-  jsxRuntime: string
+  jsxRuntime?: string
 }
 
 export interface IGatsbyNode {
