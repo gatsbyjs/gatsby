9f40495f4e452e583bf7ba00ad93353eb736a513
Ward Peeters <ward@coding-tech.com>
2021-09-04 17:50:49 +0200

    fix(gatsby): reduce page-renderer-size

--- packages/gatsby/src/internal-plugins/bundle-optimisations/gatsby-node.js
+++ packages/gatsby/src/internal-plugins/bundle-optimisations/gatsby-node.js
@@ -11,12 +11,24 @@ exports.onCreateWebpackConfig = function onCreateWebpackConfig({
   stage,
   actions,
 }) {
+  const objectAssignStub = path.join(__dirname, `polyfills/object-assign.js`)
+
   if (stage === `build-html` || stage === `develop-html`) {
+    actions.setWebpackConfig({
+      resolve: {
+        alias: {
+          // These files are already polyfilled so these should return in a no-op
+          // Stub Package: object.assign & object-assign
+          "object.assign": objectAssignStub,
+          "object-assign$": objectAssignStub,
+          "@babel/runtime/helpers/extends.js$": objectAssignStub,
+        },
+      },
+    })
     return
   }
 
   const noOp = path.join(__dirname, `polyfills/no-op.js`)
-  const objectAssignStub = path.join(__dirname, `polyfills/object-assign.js`)
   const fetchStub = path.join(__dirname, `polyfills/fetch.js`)
   const whatwgFetchStub = path.join(__dirname, `polyfills/whatwg-fetch.js`)
 
