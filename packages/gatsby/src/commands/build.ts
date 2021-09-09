2f70a826871bcb6a515032035813d83b01ae9b5c
Kyle Mathews <mathews.kyle@gmail.com>
2021-09-08 23:37:27 -0700

    fix(gatsby): fix nesting of tracing spans + add docs for OpenTelemetry (#33098)
    * nest preprocess source under extract queries activity
    
    * Add spans for ast parsing activities
    
    * Nest onPostBuild api call under activity
    
    * Nest all steps to build html renderer under the activity
    
    * Trace persisiting the cache to disk
    
    * Revert anything that's not a bug fix
    
    * Add docs for using OpenTelemetry with Honeycomb
    
    * clarify instructions
    
    * type
    
    * Update docs/docs/performance-tracing.md
    
    Co-authored-by: Ward Peeters <ward@coding-tech.com>
    
    Co-authored-by: Ward Peeters <ward@coding-tech.com>

--- packages/gatsby/src/commands/build.ts
+++ packages/gatsby/src/commands/build.ts
@@ -238,7 +238,11 @@ module.exports = async function build(program: IBuildArgs): Promise<void> {
   let pageRenderer = ``
   let waitForCompilerCloseBuildHtml
   try {
-    const result = await buildRenderer(program, Stage.BuildHTML, buildSpan)
+    const result = await buildRenderer(
+      program,
+      Stage.BuildHTML,
+      buildSSRBundleActivityProgress.span
+    )
     pageRenderer = result.rendererPath
     if (_CFLAGS_.GATSBY_MAJOR === `4` && shouldGenerateEngines()) {
       // for now copy page-render to `.cache` so page-ssr module can require it as a sibling module
@@ -331,7 +335,7 @@ module.exports = async function build(program: IBuildArgs): Promise<void> {
   postBuildActivityTimer.start()
   await apiRunnerNode(`onPostBuild`, {
     graphql: gatsbyNodeGraphQLFunction,
-    parentSpan: buildSpan,
+    parentSpan: postBuildActivityTimer.span,
   })
   postBuildActivityTimer.end()
 
