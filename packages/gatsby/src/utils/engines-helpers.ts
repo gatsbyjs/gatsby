39e02bd0b25a5b3654c9b47a145a319e517bb5e4
Vladimir Razuvaev <vladimir.razuvaev@gmail.com>
2021-09-14 18:43:20 +0700

    fix(gatsby): implement shouldGenerateEngines

--- packages/gatsby/src/utils/engines-helpers.ts
+++ packages/gatsby/src/utils/engines-helpers.ts
@@ -1,4 +1,19 @@
-export function shouldGenerateEngines(): boolean {
-  // TODO: make it smart and actually check if there are any non-ssg pages
-  return true
+import { store } from "../redux"
+import memoize from "memoizee"
+
+// Memoize assuming pages cannot change during the build command
+export const shouldGenerateEngines = memoize(shouldGenerateEnginesImpl)
+
+function shouldGenerateEnginesImpl(): boolean {
+  return process.env.gatsby_executing_command === `build` && hasDynamicPage()
+}
+
+function hasDynamicPage(): boolean {
+  const { pages } = store.getState()
+  for (const page of pages.values()) {
+    if (page.mode !== `SSG`) {
+      return true
+    }
+  }
+  return false
 }
