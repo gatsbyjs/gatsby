d2056927272d33b47d86347e61a3d21635658905
psharma-ii <82900964+psharma-ii@users.noreply.github.com>
2021-09-20 17:23:11 -0400

    (fix) add steps for refetch_ALL
    `REFETCH_ALL` action_type implemented on WPGatsby was not being handled. This takes care of that by adding a conditional for  `REFETCH_ALL`, sources the Node and checks if the sourceNode has `refetchAll` boolean set in addition to other Or conditionals.  To add `refetchAll` GatsbyTypes was modified to add the new property.

--- packages/gatsby-source-wordpress/src/steps/source-nodes/update-nodes/wp-actions/index.js
+++ packages/gatsby-source-wordpress/src/steps/source-nodes/update-nodes/wp-actions/index.js
@@ -6,6 +6,7 @@ import { paginatedWpNodeFetch } from "~/steps/source-nodes/fetch-nodes/fetch-nod
 
 import fetchAndCreateNonNodeRootFields from "~/steps/source-nodes/create-nodes/fetch-and-create-non-node-root-fields"
 import { setHardCachedNodes } from "~/utils/cache"
+import { sourceNodes } from "~/steps/source-nodes"
 
 /**
  * getWpActions
@@ -60,6 +61,9 @@ export const handleWpActions = async api => {
       break
     case `NON_NODE_ROOT_FIELDS`:
       await fetchAndCreateNonNodeRootFields()
+    case `REFETCH_ALL`:
+        helpers.refetchAll = true;
+        await sourceNodes(helpers, {})
   }
 
   await setHardCachedNodes({ helpers })
