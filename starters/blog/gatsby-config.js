06ed2a0ddd942aff4f15dba973f5fb295f1614cf
Lennart <lekoarts@gmail.com>
2021-09-20 10:18:22 +0200

    chore(starters): Make `theme_color` optional (#33252)

--- starters/blog/gatsby-config.js
+++ starters/blog/gatsby-config.js
@@ -116,7 +116,9 @@ module.exports = {
         short_name: `GatsbyJS`,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#663399`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
