24b6f8aacb22c3af7817165098c4315bce049cb0
Ward Peeters <ward@coding-tech.com>
2021-09-23 15:32:12 +0200

    create dummy worker

---
+++ packages/gatsby/src/services/gatsby-worker.js
@@ -0,0 +1,11 @@
+exports.GENERATE_PAGE = ({ paths }) => {
+  // for (const segment of pageQuerySegments) {
+  //   pool.single
+  //     .runQueries({ pageQueryIds: segment, staticQueryIds: [] })
+  //     .then(replayWorkerActions)
+  //     .then(() => {
+  //       activity.tick(segment.length)
+  //     })
+  //     .catch(handleRunQueriesInWorkersQueueError)
+  // }
+}
