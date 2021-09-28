af0ec9fa3379082bf1526d13376638f8186d70b5
Kyle Mathews <mathews.kyle@gmail.com>
2021-09-27 22:45:57 -0700

    remove logs

--- packages/gatsby/src/commands/build-html.ts
+++ packages/gatsby/src/commands/build-html.ts
@@ -245,9 +245,7 @@ const renderHTMLQueue = async (
         await Promise.all(
           Object.entries(renderHTMLResult.previewErrors).map(
             async ([pagePath, error]) => {
-              console.log({ pagePath })
               if (!seenErrors.has(error.stack)) {
-                console.log(`haven't seen`, { pagePath })
                 errorMessages.set(error.stack, {
                   pagePaths: [pagePath],
                 })
@@ -260,24 +258,18 @@ const renderHTMLQueue = async (
                 const errorMessageStr = `${prettyError.stack}${
                   prettyError.codeFrame ? `\n\n${prettyError.codeFrame}\n` : ``
                 }`
-                console.log({ pagePath, errorMessageStr })
 
                 const errorMessage = errorMessages.get(error.stack)
                 errorMessage.errorMessage = errorMessageStr
-                console.log(errorMessage)
                 errorMessages.set(error.stack, errorMessage)
               } else {
                 const errorMessage = errorMessages.get(error.stack)
                 errorMessage.pagePaths.push(pagePath)
-                console.log(errorMessage.pagePaths)
                 errorMessages.set(error.stack, errorMessage)
               }
             }
           )
         )
-        console.log(`hi`)
-
-        console.log(`errorMessages`, errorMessages)
 
         for (const value of errorMessages.values()) {
           const errorMessage = `Preview build error for page path(s):\n\n${value.pagePaths
@@ -289,8 +281,6 @@ const renderHTMLQueue = async (
           })
         }
       }
-
-      console.log(`post isPreview`)
 
       if (stage === `build-html`) {
         const htmlRenderMeta = renderHTMLResult as IRenderHtmlResult
