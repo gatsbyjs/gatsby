3002d652abd6b59791b73a21c188fd042a86f4a7
Ward Peeters <ward@coding-tech.com>
2021-10-12 14:41:25 +0200

    fix(gatsby): remove unused vars in remove api plugin (#33476)

---
+++ packages/gatsby/src/utils/babel/__tests__/fixtures/remove-apis/exports/input.mjs
@@ -0,0 +1,35 @@
+import * as fs from "fs"
+
+const { fetch, Response } = require('node-fetch');
+const unusedReference = fs.readFileSync('./myfile.json');
+const usedReference = 'used reference';
+
+module.exports = function () {
+  const x = new Response({})
+
+  return usedReference
+}
+
+exports.getServerData = async function getServerData() {
+  const data = await fetch('https://example.com');
+  const file = fs.readFileSync('./unknown.tmp.json', 'utf8')
+
+  return {
+    props: {
+      file,
+    }
+  }
+}
+
+exports.config = function config() {
+  return {
+    pageContext: {
+      env: 'test',
+      unusedReference,
+    },
+  }
+}
+
+exports.anotherFunction = function anotherFunction() {
+  return "test"
+}
