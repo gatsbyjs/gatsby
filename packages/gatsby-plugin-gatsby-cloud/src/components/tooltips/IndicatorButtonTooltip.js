922a69e12197e8a2456d87a6cc6933ab880fb508
Leithon English <leithonenglish@gmail.com>
2021-10-14 12:31:58 -0500

    updated styles and removed comments

--- packages/gatsby-plugin-gatsby-cloud/src/components/tooltips/IndicatorButtonTooltip.js
+++ packages/gatsby-plugin-gatsby-cloud/src/components/tooltips/IndicatorButtonTooltip.js
@@ -1,24 +1,27 @@
-import React from "react"
+import React, { useMemo } from "react"
 
 export default function IndicatorButtonTooltip({
   tooltipContent,
   overrideShowTooltip,
   showTooltip,
-  buttonIndex,
   testId,
+  elementRef,
 }) {
+  const elmOffsetTop = useMemo(() => {
+    if (elementRef && elementRef.current) {
+      const elm = elementRef.current
+      return elm.offsetTop
+    }
+    return 0
+  }, [elementRef])
   return (
     <div
-      // The tooltip offset needs to be 40 * button possition (0 indexed) + 12
-      // This will align the tooltip with its correct button
-      // The first button is (40 * 0) + 12
-      // The second button is (40 * 1) + 12 ...
-      style={{ top: `50%`, transform: `translateX(-50%)` }}
       data-gatsby-preview-indicator="tooltip"
       data-gatsby-preview-indicator-visible={`${
         overrideShowTooltip || showTooltip
       }`}
       data-testid={`${testId}-tooltip`}
+      style={{ top: `${elmOffsetTop}px` }}
     >
       {tooltipContent}
     </div>
