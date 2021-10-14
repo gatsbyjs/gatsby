6b4d27c0808b039498e4ee1bf17657aa1774cf95
LekoArts <lekoarts@gmail.com>
2021-10-14 14:29:32 +0200

    fix tests

--- packages/gatsby/src/schema/__tests__/queries.js
+++ packages/gatsby/src/schema/__tests__/queries.js
@@ -15,6 +15,7 @@ jest.mock(`gatsby-cli/lib/reporter`, () => {
     info: jest.fn(),
     warn: jest.fn(),
     error: jest.fn(),
+    verbose: jest.fn(),
     activityTimer: () => {
       return {
         start: jest.fn(),
