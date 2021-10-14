31c79c149109c009fe53b00cbd3a772da4bf60e0
LekoArts <lekoarts@gmail.com>
2021-10-14 09:46:00 +0200

    simplify and update

--- packages/gatsby-transformer-yaml/src/gatsby-node.js
+++ packages/gatsby-transformer-yaml/src/gatsby-node.js
@@ -2,17 +2,6 @@ const jsYaml = require(`js-yaml`)
 const _ = require(`lodash`)
 const path = require(`path`)
 
-const conflictFieldPrefix = `yaml_`
-// restrictedNodeFields from here https://www.gatsbyjs.org/docs/node-interface/
-const restrictedNodeFields = [
-  `children`,
-  `contentful_id`,
-  `fields`,
-  `id`,
-  `internal`,
-  `parent`,
-]
-
 function unstable_shouldOnCreateNode({ node }) {
   return node.internal.mediaType === `text/yaml`
 }
@@ -50,6 +39,9 @@ async function onCreateNode(
         type,
       },
     }
+    if (obj.id) {
+      yamlNode[`yamlId`] = obj.id
+    }
     createNode(yamlNode)
     createParentChildLink({ parent: node, child: yamlNode })
   }
@@ -58,31 +50,6 @@ async function onCreateNode(
 
   const content = await loadNodeContent(node)
   const parsedContent = jsYaml.load(content)
-
-  function normalizeRestrictedFields(obj) {
-    restrictedNodeFields.forEach(restrictedNodeField => {
-      if (Object.keys(obj).includes(restrictedNodeField)) {
-        obj[`${conflictFieldPrefix}${restrictedNodeField}`] =
-          obj[restrictedNodeField]
-        delete obj[restrictedNodeField]
-
-        console.log(
-          `Restricted field found for when parsing yaml object. Field ${restrictedNodeField} value is now found on field ${conflictFieldPrefix}${restrictedNodeField}.`
-        )
-      }
-    })
-  }
-
-  if (_.isArray(parsedContent)) {
-    parsedContent.forEach(obj => {
-      if (_.isPlainObject(obj)) {
-        normalizeRestrictedFields(obj)
-      }
-    })
-  }
-  if (_.isPlainObject(parsedContent)) {
-    normalizeRestrictedFields(parsedContent)
-  }
 
   if (_.isArray(parsedContent)) {
     parsedContent.forEach((obj, i) => {
@@ -95,7 +62,7 @@ async function onCreateNode(
   } else if (_.isPlainObject(parsedContent)) {
     transformObject(
       parsedContent,
-      parsedContent.id ? parsedContent.id : createNodeId(`${node.id} >>> YAML`),
+      createNodeId(`${node.id} >>> YAML`),
       getType({ node, object: parsedContent, isArray: false })
     )
   }
