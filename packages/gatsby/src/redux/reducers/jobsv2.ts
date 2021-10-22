5427bd4431d9b35ae76196f99cebb3af1457f079
Vladimir Razuvaev <vladimir.razuvaev@gmail.com>
2021-10-22 20:58:03 +0700

    Update packages/gatsby/src/redux/reducers/jobsv2.ts
    Co-authored-by: Ward Peeters <ward@coding-tech.com>

--- packages/gatsby/src/redux/reducers/jobsv2.ts
+++ packages/gatsby/src/redux/reducers/jobsv2.ts
@@ -37,7 +37,7 @@ export const jobsV2Reducer = (
 
       const isOutdatedJobsState =
         cleanStateKeys.length !== Object.keys(state).length ||
-        cleanStateKeys.some(key => !state[key])
+        cleanStateKeys.some(key => !Object.prototype.hasOwnProperty.call(state, key);
 
       return action.cacheIsCorrupt || isOutdatedJobsState ? cleanState : state
     }
