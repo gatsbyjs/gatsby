2c9f67cd6dcb2a0ea27371f6842ca00aee4ccaa0
Ashley Hitchcock <hello@ashleyhitchcock.co.uk>
2021-11-17 11:57:43 +0000

    chore(gatsby): Add `getServerData` to PageProps type (#34003)
    Co-authored-by: Lennart <lekoarts@gmail.com>

--- packages/gatsby/index.d.ts
+++ packages/gatsby/index.d.ts
@@ -80,7 +80,8 @@ export const prefetchPathname: (path: string) => void
 export type PageProps<
   DataType = object,
   PageContextType = object,
-  LocationState = WindowLocation["state"]
+  LocationState = WindowLocation["state"],
+  ServerDataType = object
 > = {
   /** The path for this current page */
   path: string
@@ -141,6 +142,8 @@ export type PageProps<
    *   ..
    */
   pageContext: PageContextType
+  /** Data passed into the page via the [getServerData](https://www.gatsbyjs.com/docs/reference/rendering-options/server-side-rendering/) SSR function. */
+  serverData: ServerDataType
 }
 
 export interface PageRendererProps {
