3da0c27ccc3c45028185d1633e7260a2413bbb01
axe312ger <opensource@axe312.dev>
2021-11-18 17:50:42 +0100

    clean up, fix tests, get downloadAssets working again

--- packages/gatsby-source-contentful/src/__tests__/gatsby-node.js
+++ packages/gatsby-source-contentful/src/__tests__/gatsby-node.js
@@ -7,7 +7,6 @@ import { makeId } from "../normalize"
 
 import startersBlogFixture from "../__fixtures__/starter-blog-data"
 import richTextFixture from "../__fixtures__/rich-text-data"
-import restrictedContentTypeFixture from "../__fixtures__/restricted-content-type"
 
 jest.mock(`../fetch`)
 
