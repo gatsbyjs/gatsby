4d966868424d8b1e693a453cb393570d821ac225
Byron Hill <byron@atomicdigital.studio>
2021-11-22 10:49:48 -0700

    Added a setLastBuildTime helper

--- packages/gatsby-source-shopify/src/source-nodes.ts
+++ packages/gatsby-source-shopify/src/source-nodes.ts
@@ -3,19 +3,24 @@ import { SourceNodesArgs } from "gatsby"
 import { eventsApi } from "./events"
 import { shopifyTypes } from "./shopify-types"
 import { createOperations } from "./create-operations"
-import { createNodeId, parseShopifyId } from "./helpers"
 import { makeSourceFromOperation } from "./source-from-operation"
+import {
+  createNodeId,
+  parseShopifyId,
+  getLastBuildTime,
+  setLastBuildTime,
+} from "./helpers"
 
-const getStatus = (gatsbyApi: SourceNodesArgs) =>
-  gatsbyApi.store.getState().status.plugins?.[`gatsby-source-shopify`] || []
-
-interface NodeMap {
+interface INodeMap {
   [key: string]: ShopifyNode
 }
 
-
-function getNodesToDelete(gatsbyApi: SourceNodesArgs, nodeMap: NodeMap, ids: string[]) {
-  const nodesToDelete: string[] = []
+function getNodesToDelete(
+  gatsbyApi: SourceNodesArgs,
+  nodeMap: INodeMap,
+  ids: Array<string>
+): Array<string> {
+  const nodesToDelete: Array<string> = []
 
   for (const id of ids) {
     const node = nodeMap[id]
@@ -27,12 +32,15 @@ function getNodesToDelete(gatsbyApi: SourceNodesArgs, nodeMap: NodeMap, ids: str
       const type = parseShopifyId(node.shopifyId)[1]
       const coupledNodeFields = shopifyTypes[type].coupledNodeFields
 
-      const coupledNodeIdsToDelete: [string?] = []
+      const coupledNodeIdsToDelete: Array<string> = []
 
       if (coupledNodeFields) {
         const coupledNodeIds = coupledNodeFields
           .map((field: string) => node[field])
-          .reduce((acc: string[], value: string[]) => acc.concat(value), [])
+          .reduce(
+            (acc: Array<string>, value: Array<string>) => acc.concat(value),
+            []
+          )
 
         getNodesToDelete(gatsbyApi, nodeMap, coupledNodeIds)
       }
@@ -46,15 +54,14 @@ function getNodesToDelete(gatsbyApi: SourceNodesArgs, nodeMap: NodeMap, ids: str
   return nodesToDelete
 }
 
-
 export async function sourceNodes(
   gatsbyApi: SourceNodesArgs,
   pluginOptions: ShopifyPluginOptions
 ): Promise<void> {
-  const { typePrefix = ``, shopifyConnections: connections = [] } = pluginOptions
+  const { typePrefix = ``, shopifyConnections: connections = [] } =
+    pluginOptions
 
-  const status = getStatus(gatsbyApi)
-  const lastBuildTime = status[`lastBuildTime${typePrefix}`] && new Date(status[`lastBuildTime${typePrefix}`])
+  const lastBuildTime = getLastBuildTime(gatsbyApi, pluginOptions)
 
   if (lastBuildTime !== undefined) {
     gatsbyApi.reporter.info(`Cache is warm, running an incremental build`)
@@ -100,15 +107,17 @@ export async function sourceNodes(
   }
 
   if (lastBuildTime) {
-    console.log('starting building node map');
+    console.log(`starting building node map`)
 
     const nodes = Object.keys(shopifyTypes)
-      .map((type) => gatsbyApi.getNodesByType(`${typePrefix}Shopify${type}`))
+      .map(type => gatsbyApi.getNodesByType(`${typePrefix}Shopify${type}`))
       .reduce((acc, value) => acc.concat(value), [])
 
-    const nodeMap: NodeMap = nodes.reduce((acc, value) => ({ ...acc, [value.id]: value}), {})
+    const nodeMap: INodeMap = nodes.reduce((acc, value) => {
+      return { ...acc, [value.id]: value }
+    }, {})
 
-    console.log('finished building node map');
+    console.log(`finished building node map`)
 
     const { fetchDestroyEventsSince } = eventsApi(pluginOptions)
     const destroyEvents = await fetchDestroyEventsSince(lastBuildTime)
@@ -120,12 +129,16 @@ export async function sourceNodes(
     if (destroyEvents.length) {
       gatsbyApi.reporter.info(`Removing matching nodes from Gatsby`)
 
-      const destroyedNodeIds = destroyEvents.map((event) => {
+      const destroyedNodeIds = destroyEvents.map(event => {
         const shopifyId = `gid://shopify/${event.subject_type}/${event.subject_id}`
         return createNodeId(shopifyId, gatsbyApi, pluginOptions)
       })
 
-      const allDestroyedNodeIds = getNodesToDelete(gatsbyApi, nodeMap, destroyedNodeIds)
+      const allDestroyedNodeIds = getNodesToDelete(
+        gatsbyApi,
+        nodeMap,
+        destroyedNodeIds
+      )
 
       for (const node of nodes) {
         if (!allDestroyedNodeIds.includes(node.id)) {
@@ -136,8 +149,5 @@ export async function sourceNodes(
   }
 
   gatsbyApi.reporter.info(`Finished sourcing nodes, caching last build time`)
-  gatsbyApi.actions.setPluginStatus({
-    ...status,
-    [`lastBuildTime${typePrefix}`]: Date.now(),
-  })
+  setLastBuildTime(gatsbyApi, pluginOptions)
 }
