30ba3250623606ee77b35f2fdff3e6358b3a827d
Michal Piechowiak <misiek.piechowiak@gmail.com>
2021-12-16 19:46:52 +0100

    cast instead of fallback

--- packages/gatsby/src/commands/build.ts
+++ packages/gatsby/src/commands/build.ts
@@ -193,7 +193,7 @@ module.exports = async function build(
           rootDir: program.directory,
           components: state.components,
           staticQueriesByTemplate: state.staticQueriesByTemplate,
-          webpackCompilationHash: webpackCompilationHash ?? ``,
+          webpackCompilationHash: webpackCompilationHash as string, // we set webpackCompilationHash above
           reporter: report,
           isVerbose: program.verbose,
         })
