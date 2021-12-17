ba4dbd9f86e42ed9b6e91464241a872ce1c617bb
LekoArts <lekoarts@gmail.com>
2021-12-17 15:01:16 +0100

    revert create-path

--- packages/gatsby-page-utils/src/__tests__/create-path.ts
+++ packages/gatsby-page-utils/src/__tests__/create-path.ts
@@ -1,39 +1,9 @@
 import { createPath } from "../create-path"
 
-const withoutSlash = `/nested/path`
-const withSlash = `/nested/path/`
-
 describe(`create-path`, () => {
   it(`should create unix paths`, () => {
     const paths = [`b/c/de`, `bee`, `b/d/c/`]
 
     expect(paths.map(p => createPath(p))).toMatchSnapshot()
-  })
-  it(`should handle index page`, () => {
-    expect(createPath(`/index/`)).toEqual(`/`)
-    expect(createPath(`/index`)).toEqual(`/`)
-  })
-  it(`should output with trailing slash in legacy mode`, () => {
-    expect(createPath(withoutSlash)).toEqual(withSlash)
-    expect(createPath(`/index/`)).toEqual(`/`)
-    expect(createPath(`/index`)).toEqual(`/`)
-  })
-  it(`should add trailing slash with always mode`, () => {
-    expect(createPath(withoutSlash, `always`)).toEqual(withSlash)
-    expect(createPath(withSlash, `always`)).toEqual(withSlash)
-    expect(createPath(`/index/`, `always`)).toEqual(`/`)
-    expect(createPath(`/index`, `always`)).toEqual(`/`)
-  })
-  it(`should not add trailing slash with never mode`, () => {
-    expect(createPath(withSlash, `never`)).toEqual(withoutSlash)
-    expect(createPath(withoutSlash, `never`)).toEqual(withoutSlash)
-    expect(createPath(`/index/`, `never`)).toEqual(`/`)
-    expect(createPath(`/index`, `never`)).toEqual(`/`)
-  })
-  it(`should not change anything in ignore mode`, () => {
-    expect(createPath(withSlash, `ignore`)).toEqual(withSlash)
-    expect(createPath(withoutSlash, `ignore`)).toEqual(withoutSlash)
-    expect(createPath(`/index/`, `ignore`)).toEqual(`/`)
-    expect(createPath(`/index`, `ignore`)).toEqual(`/`)
   })
 })
--- packages/gatsby-page-utils/src/create-path.ts
+++ packages/gatsby-page-utils/src/create-path.ts
@@ -1,26 +1,12 @@
 import { parse, posix } from "path"
-import {
-  applyTrailingSlashOption,
-  TrailingSlash,
-} from "./apply-trailing-slash-option"
 
 export function createPath(
   filePath: string,
-  option: TrailingSlash = `legacy`
+  withTrailingSlash: boolean = true
 ): string {
   const { dir, name } = parse(filePath)
   const parsedName = name === `index` ? `` : name
-  const hasHtmlSuffix = parsedName.endsWith(`.html`)
+  const postfix = withTrailingSlash ? `/` : ``
 
-  if (hasHtmlSuffix) {
-    option = `never`
-  }
-
-  const hasTrailing = filePath.endsWith(`/`)
-  const parsedNameWithSlash = `${parsedName}${
-    option === `legacy` || hasTrailing ? `/` : ``
-  }`
-  const postfix = applyTrailingSlashOption(parsedNameWithSlash, option)
-
-  return posix.join(`/`, dir, postfix)
+  return posix.join(`/`, dir, parsedName, postfix)
 }
