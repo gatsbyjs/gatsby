78f9b0ec988e8072a992a7088da239ddaececb86
Ward Peeters <ward@coding-tech.com>
2021-12-17 08:56:23 +0100

    fix tests

--- packages/gatsby-source-contentful/src/__tests__/download-contentful-assets.js
+++ packages/gatsby-source-contentful/src/__tests__/download-contentful-assets.js
@@ -1,5 +1,6 @@
 // @ts-check
 import { downloadContentfulAssets } from "../download-contentful-assets"
+import { createAssetNodes } from "../normalize"
 
 jest.mock(`gatsby-source-filesystem`, () => {
   return {
@@ -22,36 +23,68 @@ const reporter = {
 
 const fixtures = [
   {
-    contentful_id: `idJjXOxmNga8CSnQGEwTw`,
-    node_locale: `en-US`,
-    file: {
-      url: `//images.ctfassets.net/testing/us-image.jpeg`,
+    sys: {
+      id: `idJjXOxmNga8CSnQGEwTw`,
+      type: `Asset`,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
     },
-  },
-  {
-    contentful_id: `idJjXOxmNga8CSnQGEwTw`,
-    node_locale: `fr`,
-    file: {
-      url: `//images.ctfassets.net/testing/fr-image.jpg`,
+    fields: {
+      file: {
+        "en-US": {
+          url: `//images.ctfassets.net/testing/us-image.jpeg`,
+        },
+      },
+    },
+    title: {
+      "en-US": `TundraUS`,
+      fr: `TundraFR`,
     },
   },
 ]
 
 describe(`downloadContentfulAssets`, () => {
   it(`derives unique cache key from node locale and id`, async () => {
+    const createNode = jest.fn(() => Promise.resolve())
+    const createNodeId = jest.fn(id => id)
+    const defaultLocale = `en-US`
+    const locales = [{ code: `en-US` }, { code: `fr`, fallbackCode: `en-US` }]
+    const space = {
+      sys: {
+        id: `1234`,
+      },
+    }
+
     const cache = {
       get: jest.fn(() => Promise.resolve(null)),
       set: jest.fn(() => Promise.resolve(null)),
     }
+
+    const assetNodes = []
+    for (const assetItem of fixtures) {
+      assetNodes.push(
+        ...(await Promise.all(
+          createAssetNodes({
+            assetItem,
+            createNode,
+            createNodeId,
+            defaultLocale,
+            locales,
+            space,
+          })
+        ))
+      )
+    }
+
     await downloadContentfulAssets({
       actions: { touchNode: jest.fn() },
-      assetNodes: fixtures,
+      assetNodes,
       cache,
       assetDownloadWorkers: 50,
       reporter,
     })
 
-    fixtures.forEach(n => {
+    assetNodes.forEach(n => {
       expect(cache.get).toHaveBeenCalledWith(
         `contentful-asset-${n.contentful_id}-${n.node_locale}`
       )
