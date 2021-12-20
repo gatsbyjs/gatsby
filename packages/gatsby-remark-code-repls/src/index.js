865c16ef6d56954b0908a0ae2b6a168bcb5e16f6
Ward Peeters <ward@coding-tech.com>
2021-12-20 08:52:03 +0100

    fix lint

--- packages/gatsby-remark-code-repls/src/index.js
+++ packages/gatsby-remark-code-repls/src/index.js
@@ -22,8 +22,8 @@ const {
 
 function uuidv4() {
   return `xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx`.replace(/[xy]/g, function (c) {
-    var r = (Math.random() * 16) | 0,
-      v = c == `x` ? r : (r & 0x3) | 0x8
+    const r = (Math.random() * 16) | 0
+    const v = c == `x` ? r : (r & 0x3) | 0x8
     return v.toString(16)
   })
 }
@@ -209,7 +209,7 @@ module.exports = (
         )
         verifyMultipleFiles(filesPaths, PROTOCOL_STACKBLITZ)
 
-        let parameters = {
+        const parameters = {
           files: {},
           dependencies: stackblitz.dependencies.reduce((map, dependency) => {
             const { name, fetchSpec } = npa(dependency)
