2d63ea87f7929aaf32850ca2d8f9a575ded486f6
iChenLei <chenleicoder@foxmail.com>
2021-12-31 15:25:56 +0800

    chore: add ssr support

--- examples/using-mobx/wrap-with-provider.js
+++ examples/using-mobx/wrap-with-provider.js
@@ -1,6 +1,9 @@
 import React from "react"
-import { MobXProviderContext } from "mobx-react"
+import { enableStaticRendering, MobXProviderContext } from "mobx-react"
 import CounterStore from "./src/models/CounterModel"
+
+// https://mobx.js.org/react-integration.html#static-rendering
+enableStaticRendering(true)
 
 const App =({ element }) => (
   <MobXProviderContext.Provider value={CounterStore}>{element}</MobXProviderContext.Provider>
