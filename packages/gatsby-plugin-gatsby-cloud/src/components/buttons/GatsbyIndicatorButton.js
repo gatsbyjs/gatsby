247e8540775ec0adc23581e5cfef5b0b183e2c24
Leithon English <leithonenglish@gmail.com>
2022-01-14 10:05:29 -0500

    removed unused code

--- packages/gatsby-plugin-gatsby-cloud/src/components/buttons/GatsbyIndicatorButton.js
+++ packages/gatsby-plugin-gatsby-cloud/src/components/buttons/GatsbyIndicatorButton.js
@@ -1,25 +1,13 @@
 import React from "react"
 import IndicatorButton from "./IndicatorButton"
 import { gatsbyIcon } from "../icons"
-import { BuildStatus } from "../../models/enums"
 
-const getButtonProps = ({ buildStatus, buttonIndex }) => {
+const getButtonProps = ({ buttonIndex }) => {
   const baseProps = {
     testId: `gatsby`,
     buttonIndex,
     iconSvg: gatsbyIcon,
     active: true,
-  }
-
-  const propsByBuildStatus = {
-    [BuildStatus.SUCCESS]: null,
-    [BuildStatus.ERROR]: null,
-    [BuildStatus.BUILDING]: null,
-    [BuildStatus.UPTODATE]: null,
-  }
-  const props = buildStatus ? propsByBuildStatus[buildStatus] : null
-  if (props) {
-    return { ...baseProps, ...props }
   }
   return baseProps
 }
