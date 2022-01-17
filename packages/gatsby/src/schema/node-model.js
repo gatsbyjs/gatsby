0dd2101dd19de0270dde50d02baefc0506e11a41
Michal Piechowiak <misiek.piechowiak@gmail.com>
2022-01-17 20:18:43 +0100

    perf: move id: eq fast path handling to node_model so it's shared between query running strategies

--- packages/gatsby/src/schema/node-model.js
+++ packages/gatsby/src/schema/node-model.js
@@ -267,6 +267,42 @@ class LocalNodeModel {
 
     const nodeTypeNames = toNodeTypeNames(this.schema, gqlType)
 
+    let runQueryActivity
+
+    // check if we can get node by id and skip
+    // more expensive query pipeline
+    if (
+      typeof query?.filter?.id?.eq !== `undefined` &&
+      Object.keys(query.filter).length === 1 &&
+      Object.keys(query.filter.id).length === 1
+    ) {
+      if (tracer) {
+        runQueryActivity = reporter.phantomActivity(`runQuerySimpleIdEq`, {
+          parentSpan: tracer.getParentActivity().span,
+        })
+        runQueryActivity.start()
+      }
+      let nodeFoundById = getDataStore().getNode(query.filter.id.eq)
+
+      // make sure our node is of compatible type
+      if (
+        nodeFoundById &&
+        !nodeTypeNames.includes(nodeFoundById.internal.type)
+      ) {
+        nodeFoundById = null
+      }
+
+      if (runQueryActivity) {
+        runQueryActivity.end()
+      }
+
+      return {
+        gqlType,
+        entries: new GatsbyIterable(nodeFoundById ? [nodeFoundById] : []),
+        totalCount: async () => (nodeFoundById ? 1 : 0),
+      }
+    }
+
     let materializationActivity
     if (tracer) {
       materializationActivity = reporter.phantomActivity(`Materialization`, {
@@ -283,6 +319,7 @@ class LocalNodeModel {
       min: query.min,
       sum: query.sum,
     })
+
     const fieldsToResolve = determineResolvableFields(
       this.schemaComposer,
       this.schema,
@@ -300,7 +337,6 @@ class LocalNodeModel {
       materializationActivity.end()
     }
 
-    let runQueryActivity
     if (tracer) {
       runQueryActivity = reporter.phantomActivity(`runQuery`, {
         parentSpan: tracer.getParentActivity().span,
