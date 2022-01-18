6510687b54ea2f83e5ff0ac2a84190fcfb6ce196
Jude Agboola <marvinjudehk@gmail.com>
2022-01-18 12:16:21 +0100

    revert removal of _gatsby-config.json functionality in gatsby-plugin-gatsby-cloud

--- packages/gatsby-plugin-gatsby-cloud/src/constants.js
+++ packages/gatsby-plugin-gatsby-cloud/src/constants.js
@@ -8,6 +8,7 @@ export const BUILD_CSS_STAGE = `build-css`
 export const HEADERS_FILENAME = `_headers.json`
 export const REDIRECTS_FILENAME = `_redirects.json`
 export const PUBLIC_FUNCTIONS_FILENAME = `_functions.json`
+export const SITE_CONFIG_FILENAME = `_gatsby-config.json`
 export const CACHE_FUNCTIONS_FILENAME = `manifest.json`
 
 export const DEFAULT_OPTIONS = {
