7fc6531025c2945530d1c5aac29ed4b00dc33a94
Ty Hopp <hopp.ty.c@gmail.com>
2022-01-19 14:56:03 +0800

    Never client only splat case

--- e2e-tests/trailing-slash/cypress/integration/always.js
+++ e2e-tests/trailing-slash/cypress/integration/always.js
@@ -72,12 +72,12 @@ describe(`always`, () => {
     cy.getTestElement(`query-param-hash`).click()
     cy.waitForRouteChange().assertRoute(`/page-2/?query_param=hello#anchor`)
   })
-  it(`client-only-simple without slash`, () => {
+  it(`client-only without slash`, () => {
     cy.getTestElement(`client-only-simple-without`).click()
     cy.waitForRouteChange().assertRoute(`/client-only/without/`)
     cy.getTestElement(`title`).should(`have.text`, `without`)
   })
-  it(`client-only-simple with slash`, () => {
+  it(`client-only with slash`, () => {
     cy.getTestElement(`client-only-simple-with`).click()
     cy.waitForRouteChange().assertRoute(`/client-only/with/`)
     cy.getTestElement(`title`).should(`have.text`, `with`)
