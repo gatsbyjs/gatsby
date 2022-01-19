9c8a4302e916402af0bd203799d1a34c4c1423b9
Ty Hopp <hopp.ty.c@gmail.com>
2022-01-19 14:41:45 +0800

    Ignore case client only splat

--- e2e-tests/trailing-slash/cypress/integration/ignore.js
+++ e2e-tests/trailing-slash/cypress/integration/ignore.js
@@ -26,15 +26,15 @@ describe(`ignore`, () => {
     cy.getTestElement(`fs-api-without`).click()
     cy.waitForRouteChange().assertRoute(`/fs-api/without`)
   })
-  it(`fs-api client only splat with slash`, () => {
-    cy.getTestElement(`fs-api-client-only-with`).click()
-    cy.waitForRouteChange().assertRoute(`/fs-api/with/with/`)
-    cy.getTestElement(`title`).should(`have.text`, `with`)
-  })
   it(`fs-api client only splat without slash`, () => {
     cy.getTestElement(`fs-api-client-only-without`).click()
     cy.waitForRouteChange().assertRoute(`/fs-api/without/without`)
     cy.getTestElement(`title`).should(`have.text`, `without`)
+  })
+  it(`fs-api client only splat with slash`, () => {
+    cy.getTestElement(`fs-api-client-only-with`).click()
+    cy.waitForRouteChange().assertRoute(`/fs-api/with/with/`)
+    cy.getTestElement(`title`).should(`have.text`, `with`)
   })
   it(`fs-api-simple with slash`, () => {
     cy.getTestElement(`fs-api-simple-with`).click()
@@ -116,15 +116,25 @@ describe(`ignore (direct visits)`, () => {
       .waitForRouteChange()
       .assertRoute(`/fs-api/without/without`)
   })
-  it(`client-only-simple with`, () => {
+  it(`client-only with`, () => {
     cy.visit(`/client-only/with/`)
       .waitForRouteChange()
       .assertRoute(`/client-only/with/`)
   })
-  it(`client-only-simple without`, () => {
+  it(`client-only without`, () => {
     cy.visit(`/client-only/without`)
       .waitForRouteChange()
       .assertRoute(`/client-only/without`)
+  })
+  it(`client-only-splat with`, () => {
+    cy.visit(`/client-only-splat/with/with/`)
+      .waitForRouteChange()
+      .assertRoute(`/client-only-splat/with/with/`)
+  })
+  it(`client-only-splat without`, () => {
+    cy.visit(`/client-only-splat/without/without`)
+      .waitForRouteChange()
+      .assertRoute(`/client-only-splat/without/without`)
   })
   it(`query-param-hash with`, () => {
     cy.visit(`/page-2/?query_param=hello#anchor`)
