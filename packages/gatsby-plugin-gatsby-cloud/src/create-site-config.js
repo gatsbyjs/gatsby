60ff0ab93c61a8b651484971a38736e65cbfc28f
Lennart <lekoarts@gmail.com>
2022-01-19 11:29:57 +0100

    Update packages/gatsby-plugin-gatsby-cloud/src/create-site-config.js

--- packages/gatsby-plugin-gatsby-cloud/src/create-site-config.js
+++ packages/gatsby-plugin-gatsby-cloud/src/create-site-config.js
@@ -3,7 +3,7 @@ import { SITE_CONFIG_FILENAME } from "./constants"
 
 /*
  *  @deprecated TODO(v5): Will be Remove in V5 since we're now sending config over IPC
- *  see https://github.com/gatsbyjs/gatsby/pull/34411/files#:~:text=process.send(%7B,%7D)
+ *  see https://github.com/gatsbyjs/gatsby/pull/34411
  */
 export default async function createSiteConfig(pluginData, _pluginOptions) {
   const { publicFolder } = pluginData
