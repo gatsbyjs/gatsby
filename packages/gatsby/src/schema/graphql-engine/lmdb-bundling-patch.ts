92799c3563af325f411ea0e88d65961ffc85822d
Michal Piechowiak <misiek.piechowiak@gmail.com>
2022-01-25 17:07:11 +0100

    tmp: patch lmdb so we can bundle it for engines

---
+++ packages/gatsby/src/schema/graphql-engine/lmdb-bundling-patch.ts
@@ -0,0 +1,28 @@
+export default function (source: string): string {
+  let lmdbBinaryLocation
+  try {
+    const nodeGypBuild = require(`node-gyp-build`)
+    const path = require(`path`)
+
+    lmdbBinaryLocation = nodeGypBuild.path(
+      path.dirname(require.resolve(`lmdb`)).replace(`/dist`, ``)
+    )
+  } catch (e) {
+    console.error(`ln`, e)
+    return source
+  }
+
+  return source
+    .replace(
+      `require$1('node-gyp-build')(dirName)`,
+      `require(${JSON.stringify(lmdbBinaryLocation)})`
+    )
+    .replace(
+      `require$2.resolve('./dict/dict.txt')`,
+      `require.resolve('../dict/dict.txt')`
+    )
+    .replace(
+      /fs\.readFileSync\(new URL\('\.\/dict\/dict\.txt',\s*\(typeof\s*document\s*===\s*'undefined'\s*\?\s*new\s*\(require\('u'\s*\+\s*'rl'\)\.URL\)\s*\('file:'\s*\+\s*__filename\).href\s*:\s*\(document\.currentScript\s*&&\s*document\.currentScript\.src\s*\|\|\s*new URL\('index\.cjs',\s*document\.baseURI\)\.href\)\)\.replace\(\/dist\[\\\\\\\/\]index\.cjs\$\/,\s*''\)\)\)/g,
+      `fs.readFileSync(require.resolve('../dict/dict.txt'))`
+    )
+}
