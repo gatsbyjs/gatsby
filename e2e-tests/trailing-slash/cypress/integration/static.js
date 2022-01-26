472b137ecefff60f44ab1378c77db2616abc7441
Michal Piechowiak <misiek.piechowiak@gmail.com>
2022-01-26 14:48:10 +0100

    assetPageVisits -> assertPageVisits

--- e2e-tests/trailing-slash/cypress/integration/static.js
+++ e2e-tests/trailing-slash/cypress/integration/static.js
@@ -1,4 +1,4 @@
-import { assetPageVisits } from "../support/utils/trailing-slash"
+import { assertPageVisits } from "../support/utils/trailing-slash"
 
 const IS_BUILD = Cypress.env(`IS_BUILD`)
 
@@ -7,14 +7,14 @@ const itWhenIsBuild = IS_BUILD ? it : it.skip
 describe(`static directory`, () => {
   describe(`static/something.html`, () => {
     itWhenIsBuild(`visiting directly result in 200`, () => {
-      assetPageVisits([{ path: "/static/something.html", status: 200 }])
+      assertPageVisits([{ path: "/static/something.html", status: 200 }])
 
       cy.visit(`/something.html`).assertRoute(`/something.html`)
     })
 
     it(`adding trailing slash result in 404`, () => {
       // works for build+serve, doesn't work for develop
-      assetPageVisits([{ path: "/something.html/", status: 404 }])
+      assertPageVisits([{ path: "/something.html/", status: 404 }])
 
       cy.visit(`/something.html/`, {
         failOnStatusCode: false,
@@ -26,14 +26,14 @@ describe(`static directory`, () => {
     itWhenIsBuild(
       `visiting without trailing slash redirects to trailing slash`,
       () => {
-        assetPageVisits([{ path: "/nested", status: 200 }])
+        assertPageVisits([{ path: "/nested", status: 200 }])
 
         cy.visit(`/nested`).assertRoute(`/nested/`)
       }
     )
 
     it(`visiting with trailing slash returns 200`, () => {
-      assetPageVisits([{ path: "/nested/", status: 404 }])
+      assertPageVisits([{ path: "/nested/", status: 404 }])
 
       cy.visit(`/nested/`, { failOnStatusCode: false }).assertRoute(`/nested/`)
     })
