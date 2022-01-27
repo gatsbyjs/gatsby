530826bce6230fedde0a6197e9bc0e8f40d1a769
Michal Piechowiak <misiek.piechowiak@gmail.com>
2022-01-27 12:22:58 +0100

    add collection query with sorting and skip/limit

--- benchmarks/memory/gatsby-node.js
+++ benchmarks/memory/gatsby-node.js
@@ -2,6 +2,8 @@ const { cpuCoreCount } = require(`gatsby-core-utils`)
 
 const NUM_NODES = parseInt(process.env.NUM_NODES || 300, 10)
 
+const NUM_KEYS_IN_LARGE_SIZE_OBJ = 1024
+
 exports.sourceNodes = async ({ actions, reporter }) => {
   const contentDigest = Date.now().toString() // make each sourcing mark everything as dirty
 
@@ -10,7 +12,7 @@ exports.sourceNodes = async ({ actions, reporter }) => {
 
   for (let i = 0; i < NUM_NODES; i++) {
     const largeSizeObj = {}
-    for (let j = 1; j <= 1024; j++) {
+    for (let j = 1; j <= NUM_KEYS_IN_LARGE_SIZE_OBJ; j++) {
       largeSizeObj[`key_${j}`] = `x`.repeat(1024)
     }
 
@@ -19,8 +21,9 @@ exports.sourceNodes = async ({ actions, reporter }) => {
       id: `memory-${i}`,
       idClone: `memory-${i}`,
       fooBar: [`foo`, `bar`, `baz`, `foobar`][i % 4],
-      number1: 5,
-      number2: 7,
+      number1: i,
+      number2: NUM_NODES - i,
+      number3: i % 20,
       largeSizeObj,
       largeSizeString: `x`.repeat(1024 * 1024),
       internal: {
@@ -45,10 +48,25 @@ exports.sourceNodes = async ({ actions, reporter }) => {
 }
 
 exports.createSchemaCustomization = ({ actions, schema }) => {
-  actions.createTypes(
+  actions.createTypes([
+    schema.buildObjectType({
+      name: `TestLargeSizeObj`,
+      fields: Object.fromEntries(
+        new Array(NUM_KEYS_IN_LARGE_SIZE_OBJ)
+          .fill(`String`)
+          .map((value, index) => [`key_${index + 1}`, value])
+      ),
+    }),
     schema.buildObjectType({
       name: `Test`,
       fields: {
+        idClone: `String`,
+        fooBar: `String`,
+        number1: `Int`,
+        number2: `Int`,
+        number3: `Int`,
+        largeSizeString: `String`,
+        largeSizeObj: `TestLargeSizeObj`,
         idCloneWithResolver: {
           type: `String`,
           resolve: source => {
@@ -57,8 +75,11 @@ exports.createSchemaCustomization = ({ actions, schema }) => {
         },
       },
       interfaces: ["Node"],
-    })
-  )
+      extensions: {
+        infer: false,
+      },
+    }),
+  ])
 }
 
 const printedMessages = new Set()
@@ -96,6 +117,7 @@ exports.onPreBootstrap = () => {
     `eq_id`, // this should skip node-model and fast filters completely and should be very cheap already
     `eq_field`, // this needs fast filters for eq operator on non-id field
     `eq_field_with_resolver`, // / this needs fast filters for eq operator on non-id field + materialization
+    `ne_field_collection_sort_skip_limit`, // collection query to check code path applying sorting and skip/limit
   ])
   enabledTemplates = new Set(
     process.env.TEMPLATES
@@ -138,8 +160,9 @@ exports.createPages = async ({ actions, graphql }) => {
     console.log(`Creating pages with template "${template}"`)
     let counter = 0
     for (let i = 0; i < repeatCount; i++) {
+      let j = 0
       for (const node of data.allTest.nodes) {
-        const { context } = cb(node)
+        const { context } = cb(node, j)
 
         actions.createPage({
           path: `/${template}/${counter++}`,
@@ -150,6 +173,8 @@ exports.createPages = async ({ actions, graphql }) => {
         if (counter >= minNumOfPagesToSaturateAllWorkers) {
           break
         }
+
+        j++
       }
     }
   }
@@ -180,4 +205,22 @@ exports.createPages = async ({ actions, graphql }) => {
       },
     }
   })
+
+  // allTest(
+  //   filter: { idClone: { ne: $id } }
+  //   sort: { fields: [number3], order: [ASC] }
+  //   limit: 10
+  //   skip: $skip
+  // )
+  createEnoughToSaturate(
+    `ne_field_collection_sort_skip_limit`,
+    (node, index) => {
+      return {
+        context: {
+          id: node.id,
+          skip: Math.max(index, NUM_NODES - 10), // limit is set to 10, so just setting upper bound so queries for last nodes do have 10 items
+        },
+      }
+    }
+  )
 }
