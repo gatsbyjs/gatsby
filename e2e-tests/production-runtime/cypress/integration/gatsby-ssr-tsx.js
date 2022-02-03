8720df7e083ca28042f8702ade494baed57a82a4
LekoArts <lekoarts@gmail.com>
2022-02-03 17:42:26 +0100

    test

--- e2e-tests/production-runtime/cypress/integration/gatsby-ssr-tsx.js
+++ e2e-tests/production-runtime/cypress/integration/gatsby-ssr-tsx.js
@@ -1,9 +1,6 @@
 describe(`gatsby-ssr.tsx`, () => {
   it(`works`, () => {
-    const parentDoc = cy.state('window').parent.document
-    const iframe = parentDoc.querySelector('.iframes-container iframe')
-    iframe.sandbox = ""
-    cy.visit(`/`)
+    cy.visit(`/`).waitForRouteChange()
     cy.get(`.gatsby-ssr-tsx`).should(`have.attr`, `data-content`, `TSX with gatsby-ssr works`)
   })
 })
