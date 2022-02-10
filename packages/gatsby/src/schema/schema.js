000779b935d2f4159f07fe23011a620df14d08c2
Michal Piechowiak <misiek.piechowiak@gmail.com>
2022-02-10 14:22:19 +0100

    move type printing to after types are processed

--- packages/gatsby/src/schema/schema.js
+++ packages/gatsby/src/schema/schema.js
@@ -143,21 +143,6 @@ const updateSchemaComposer = async ({
     parentSpan: parentSpan,
   })
   activity.start()
-  if (!process.env.GATSBY_SKIP_WRITING_SCHEMA_TO_FILE) {
-    await printTypeDefinitions({
-      config: printConfig,
-      schemaComposer,
-      parentSpan: activity.span,
-    })
-    if (enginePrintConfig) {
-      // make sure to print schema that will be used when bundling graphql-engine
-      await printTypeDefinitions({
-        config: enginePrintConfig,
-        schemaComposer,
-        parentSpan: activity.span,
-      })
-    }
-  }
   await addSetFieldsOnGraphQLNodeTypeFields({
     schemaComposer,
     parentSpan: activity.span,
@@ -184,6 +169,21 @@ const updateSchemaComposer = async ({
   })
   await addCustomResolveFunctions({ schemaComposer, parentSpan: activity.span })
   attachTracingResolver({ schemaComposer, parentSpan: activity.span })
+  if (!process.env.GATSBY_SKIP_WRITING_SCHEMA_TO_FILE) {
+    await printTypeDefinitions({
+      config: printConfig,
+      schemaComposer,
+      parentSpan: activity.span,
+    })
+    if (enginePrintConfig) {
+      // make sure to print schema that will be used when bundling graphql-engine
+      await printTypeDefinitions({
+        config: enginePrintConfig,
+        schemaComposer,
+        parentSpan: activity.span,
+      })
+    }
+  }
   activity.end()
 }
 
