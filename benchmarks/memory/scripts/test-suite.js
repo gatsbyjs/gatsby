cd75fc616515ddc19cc5f3b2a8b9a2df3a0e7409
Josh <jcjohnson77@gmail.com>
2022-02-13 21:30:09 -0500

    Add inc suite

--- benchmarks/memory/scripts/test-suite.js
+++ benchmarks/memory/scripts/test-suite.js
@@ -10,6 +10,11 @@ const args = yargs
     default: `suite.csv`,
     describe: `The file to save results to`,
   })
+  .option(`suite`, {
+    default: `incremental`,
+    describe: `Which test suite to run`,
+    choices: ['incremental', 'exhaustive']
+  })
   .option(`show-test-output`, {
     default: ``,
     describe: `Show the output from ./scripts/test.js`,
@@ -26,14 +31,6 @@ const Blue = "\x1b[34m"
 const Magenta = "\x1b[35m"
 const Cyan = "\x1b[36m"
 const White = "\x1b[37m"
-
-const testsPerConfig = 16;
-
-const config = {
-  memory: [`16g`, `8g`, `4g`, `2g`],
-  numNodes: [`100`, `1000`, `10000`],
-  nodeSize: [`1k`, `512k`, `1m`]
-}
 
 const hostExec = (cmd) => {
   console.log(`${Dim}$ ${cmd}${Reset}`)
@@ -82,11 +79,69 @@ async function runTest(memory, numNodes, nodeSize, i) {
   return code
 }
 
-async function executeTestSuites() {
-  for (const memory of config.memory) {
-    for (const numNodes of config.numNodes) {
-      for (const nodeSize of config.nodeSize) {
-        for (const i of Array.from(Array(testsPerConfig).keys())) {
+
+/*
+ * Incremental Test Suite
+ *
+ * Runs tests for each memory config until all tests under the constraints fail.
+ * This sets up nodes of 1m and doubles the amount of nodes for each test.
+ */
+
+const incrementalTestsPerConfig = 16;
+const incrementalConfig = {
+  memory: [`2g`, `4g`, `8g`],
+  numNodesInitial: 100,
+  numNodesIncrement: (previous) => previous + 100,
+  nodeSize: `1m`
+}
+
+async function incremental() {
+  for (const memory of incrementalConfig.memory) {
+    let numNodes = incrementalConfig.numNodesInitial;
+    let allTestsFailed = false;
+
+    while (!allTestsFailed) {
+      // reset allTestsFailed
+      allTestsFailed = true;
+
+      for (let i = 0; i < incrementalTestsPerConfig; i++) {
+        const code = await runTest(memory, numNodes, incrementalConfig.nodeSize, i)
+
+        // if a single test passed, we want to loop again
+        if (code === 0) {
+          allTestsFailed = false;
+        }
+      }
+
+      if (allTestsFailed) {
+        console.log(`\n${Yellow}All tests failed for ${memory} memory, ${numNodes} nodes.${Reset}`)
+      }
+
+      // increment our numNodes for the next run
+      numNodes = incrementalConfig.numNodesIncrement(numNodes)
+    }
+  }
+}
+
+/*
+ * Exhaustive Test Suite
+ * 
+ * Runs N tests for each config in the powerset of the exhaustiveConfig
+ * below, where N is exhaustiveTestsPerConfig.
+ */
+
+const exhaustiveTestsPerConfig = 16;
+const exhaustiveConfig = {
+  memory: [`16g`, `8g`, `4g`, `2g`],
+  numNodes: [`100`, `1000`, `10000`],
+  nodeSize: [`1k`, `512k`, `1m`]
+}
+
+async function exhaustive() {
+  for (const memory of exhaustiveConfig.memory) {
+    for (const numNodes of exhaustiveConfig.numNodes) {
+      for (const nodeSize of exhaustiveConfig.nodeSize) {
+        for (let i = 0; i < exhaustiveTestsPerConfig; i++) {
           await runTest(memory, numNodes, nodeSize, i)
         }
       }
@@ -94,5 +149,10 @@ async function executeTestSuites() {
   }
 }
 
-console.log(`Writing results to ${args.out}`)
-executeTestSuites()
+console.log(`Writing ${args.suite} results to ${args.out}`)
+
+if (args.suite === 'incremental') {
+  incremental()
+} else if (args.suite === 'exhaustive') {
+  exhaustive()
+}
