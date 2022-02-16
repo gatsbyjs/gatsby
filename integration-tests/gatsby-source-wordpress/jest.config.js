ea511dbac0927aebef731fd61d541790042fc802
Ward Peeters <ward@coding-tech.com>
2022-02-16 10:12:02 +0100

    fix jest config wordpress

--- integration-tests/gatsby-source-wordpress/jest.config.js
+++ integration-tests/gatsby-source-wordpress/jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   testPathIgnorePatterns: [`/node_modules/`, `__tests__/fixtures`, `.cache`],
   bail: true,
+  moduleNameMapper: {
+    "^gatsby-core-utils/(.*)$": `gatsby-core-utils/dist/$1`, // Workaround for https://github.com/facebook/jest/issues/9771
+  },
 }
