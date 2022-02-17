0ff9b84e975b2f258d378daab3dda7005c864d2d
Lennart <lekoarts@gmail.com>
2022-02-17 13:42:05 +0100

    Restore gatsby-node.js

--- e2e-tests/themes/gatsby-theme-about/gatsby-node.js
+++ e2e-tests/themes/gatsby-theme-about/gatsby-node.js
@@ -1,5 +1,4 @@
-const path = require("path")
-const bioTemplate = path.resolve(`./src/templates/bio.js`)
+const bioTemplate = require.resolve(`./src/templates/bio.js`)
 
 exports.createPages = async ({ actions }) => {
     const { createPage } = actions
