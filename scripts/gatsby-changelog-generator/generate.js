d2440da072fc0f6d125da328f063b97828cb63a1
Ty Hopp <hopp.ty.c@gmail.com>
2022-02-23 17:19:20 +0800

    chore: Fix changelog generator script failing lint

--- scripts/gatsby-changelog-generator/generate.js
+++ scripts/gatsby-changelog-generator/generate.js
@@ -237,7 +237,7 @@ function addChangelogEntries(packageName, entries, contents) {
   const updatedChangelogParts = [
     header,
     entries.trimRight(),
-    contents.substr(header.length),
+    contents.substr(header.length).trimStart(),
   ]
   fs.writeFileSync(
     changelogPath(packageName),
