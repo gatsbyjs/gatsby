e3753d1a4fc8d2c911637f45848c438afa34cebd
Ward Peeters <ward@coding-tech.com>
2022-02-24 13:25:32 +0100

    fix jest.config

--- jest.config.js
+++ jest.config.js
@@ -48,7 +48,10 @@ module.exports = {
     "^msgpackr$": `<rootDir>/node_modules/msgpackr/dist/node.cjs`,
     "^gatsby-page-utils/(.*)$": `gatsby-page-utils/dist/$1`, // Workaround for https://github.com/facebook/jest/issues/9771
     "^gatsby-core-utils/(.*)$": `gatsby-core-utils/dist/$1`, // Workaround for https://github.com/facebook/jest/issues/9771
-    "^gatsby-plugin-utils/(.*)$": `gatsby-plugin-utils/dist/$1`, // Workaround for https://github.com/facebook/jest/issues/9771
+    "^gatsby-plugin-utils/(.*)$": [
+      `gatsby-plugin-utils/dist/$1`,
+      `gatsby-plugin-utils/$1`,
+    ], // Workaround for https://github.com/facebook/jest/issues/9771
   },
   snapshotSerializers: [`jest-serializer-path`],
   collectCoverageFrom: coverageDirs,
