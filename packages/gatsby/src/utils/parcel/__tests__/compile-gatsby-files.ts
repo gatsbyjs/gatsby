f2bce35acf38715d507f4a83870226b823888347
Michal Piechowiak <misiek.piechowiak@gmail.com>
2022-03-01 14:12:43 +0100

    chore(gatsby-parcel-config): stabilize ts compilation parcel output (#34943)

--- packages/gatsby/src/utils/parcel/__tests__/compile-gatsby-files.ts
+++ packages/gatsby/src/utils/parcel/__tests__/compile-gatsby-files.ts
@@ -13,6 +13,7 @@ import { readFile, remove, pathExists } from "fs-extra"
 const dir = {
   js: `${__dirname}/fixtures/js`,
   ts: `${__dirname}/fixtures/ts`,
+  tsOnlyInLocal: `${__dirname}/fixtures/ts-only-in-local-plugin`,
 }
 
 jest.mock(`@parcel/core`, () => {
@@ -37,6 +38,15 @@ interface IMockedParcel extends Parcel {
   options: unknown
 }
 
+let cwdToRestore
+beforeAll(() => {
+  cwdToRestore = process.cwd()
+})
+
+afterAll(() => {
+  process.chdir(cwdToRestore)
+})
+
 describe(`gatsby file compilation`, () => {
   describe(`constructBundler`, () => {
     it(`should construct Parcel relative to passed directory`, () => {
@@ -60,6 +70,7 @@ describe(`gatsby file compilation`, () => {
   describe(`compileGatsbyFiles`, () => {
     describe(`js files are not touched`, () => {
       beforeAll(async () => {
+        process.chdir(dir.js)
         await remove(`${dir.js}/.cache`)
         await compileGatsbyFiles(dir.js)
       })
@@ -81,6 +92,7 @@ describe(`gatsby file compilation`, () => {
 
     describe(`ts files are compiled`, () => {
       beforeAll(async () => {
+        process.chdir(dir.ts)
         await remove(`${dir.ts}/.cache`)
         await compileGatsbyFiles(dir.ts)
       })
@@ -103,6 +115,32 @@ describe(`gatsby file compilation`, () => {
         )
 
         expect(compiledGatsbyNode).toContain(`I am working!`)
+      })
+    })
+
+    describe(`ts only in local plugin files are compiled and outputted where expected`, () => {
+      beforeAll(async () => {
+        process.chdir(dir.tsOnlyInLocal)
+        await remove(`${dir.tsOnlyInLocal}/.cache`)
+        await compileGatsbyFiles(dir.tsOnlyInLocal)
+      })
+
+      it(`should compile gatsby-config.ts`, async () => {
+        const compiledGatsbyConfig = await readFile(
+          `${dir.tsOnlyInLocal}/.cache/compiled/plugins/gatsby-plugin-local/gatsby-config.js`,
+          `utf-8`
+        )
+
+        expect(compiledGatsbyConfig).toContain(`gatsby-config is working`)
+      })
+
+      it(`should compile gatsby-node.ts`, async () => {
+        const compiledGatsbyNode = await readFile(
+          `${dir.tsOnlyInLocal}/.cache/compiled/plugins/gatsby-plugin-local/gatsby-node.js`,
+          `utf-8`
+        )
+
+        expect(compiledGatsbyNode).toContain(`gatsby-node is working`)
       })
     })
   })
