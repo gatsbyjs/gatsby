da2a1baff310a2c081f0a0a0d4d78caa12a143a5
Lennart <lekoarts@gmail.com>
2022-03-01 11:26:31 +0100

    chore(examples): Update vanilla-extract (#35000)

--- examples/using-vanilla-extract/gatsby-config.ts
+++ examples/using-vanilla-extract/gatsby-config.ts
@@ -1,7 +1,11 @@
-module.exports = {
+import type { GatsbyConfig } from "gatsby"
+
+const config: GatsbyConfig = {
   siteMetadata: {
     title: `using-vanilla-extract`,
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [`gatsby-plugin-vanilla-extract`]
 }
+
+export default config
