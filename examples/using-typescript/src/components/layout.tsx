5f552d4cb2a294cdc9440ed0d38dc0a445b830c3
Lennart <lekoarts@gmail.com>
2022-03-01 03:36:21 +0100

    initial (#34946)

--- examples/using-typescript/src/components/layout.tsx
+++ examples/using-typescript/src/components/layout.tsx
@@ -1,20 +1,5 @@
 import * as React from "react"
 
-import { rhythm } from "../utils/typography"
+const Layout = ({ children }: { children: React.ReactNode }) => <div className="global-wrapper">{children}</div>
 
-const MainLayout: React.FC = ({ children }) => (
-  <div
-    style={{
-      margin: `0 auto`,
-      marginBottom: rhythm(1.5),
-      marginTop: rhythm(1.5),
-      maxWidth: 650,
-      paddingLeft: rhythm(3 / 4),
-      paddingRight: rhythm(3 / 4),
-    }}
-  >
-    {children}
-  </div>
-)
-
-export default MainLayout
+export default Layout
--- examples/using-typescript/src/utils/typography.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const rhythm: (a: number) => number
