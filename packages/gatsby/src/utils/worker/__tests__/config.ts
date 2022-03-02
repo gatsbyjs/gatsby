dd82368ae8b8dbbf92fae753cce38f27ff54c2f8
LekoArts <lekoarts@gmail.com>
2022-03-02 16:36:59 +0100

    fix tests

--- packages/gatsby/src/utils/worker/__tests__/config.ts
+++ packages/gatsby/src/utils/worker/__tests__/config.ts
@@ -9,6 +9,8 @@ jest.mock(`gatsby-telemetry`, () => {
   return {
     decorateEvent: jest.fn(),
     trackCli: jest.fn(),
+    addSiteMeasurement: jest.fn(),
+    isTrackingEnabled: jest.fn(),
   }
 })
 
