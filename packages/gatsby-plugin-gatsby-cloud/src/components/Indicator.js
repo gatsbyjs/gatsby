91b8e8cc2518e1908e1b7a64189c4b4ea798af04
Leithon English <leithonenglish@gmail.com>
2022-03-10 11:40:50 -0500

    canary commit #4

--- packages/gatsby-plugin-gatsby-cloud/src/components/Indicator.js
+++ packages/gatsby-plugin-gatsby-cloud/src/components/Indicator.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useRef } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import IndicatorProvider from "../context/indicatorProvider"
 import { BuildStatus } from "../models/enums"
 import { useTrackEvent, getBuildInfo } from "../utils"
