53984eb7fc2a514c567241d479bc05d92c4d3875
Byron Hill <byron@atomicdigital.studio>
2022-03-14 10:59:36 -0600

    Added a test for source-from-operation

---
+++ packages/gatsby-source-shopify/__tests__/source-from-operation.spec.ts
@@ -0,0 +1,84 @@
+import fetch from "node-fetch"
+
+import { makeSourceFromOperation } from "../src/source-from-operation"
+import * as processBulkResultsModule from "../src/process-bulk-results"
+
+import { makeMockGatsbyApi, mockBulkResults } from "./mocks"
+
+jest.mock(`node-fetch`)
+
+const processBulkResults = jest.spyOn(
+  processBulkResultsModule,
+  `processBulkResults`
+)
+
+const gatsbyApi = makeMockGatsbyApi()
+
+const finishLastOperation = jest.fn()
+
+const completedOperation = jest.fn(() => {
+  return {
+    node: {
+      objectCount: `1`,
+    },
+  }
+})
+
+const operation = {
+  execute: jest.fn(() => {
+    return {
+      bulkOperationRunQuery: {
+        userErrors: [],
+        bulkOperation: { id: `test-id` },
+      },
+    }
+  }),
+  name: `Test Operation`,
+}
+
+const cancelOperationInProgress = jest.fn()
+
+const lastBuildTime = undefined
+
+const priorities = [false, true]
+const types = [`products`, `variants`, `collections`, `orders`, `locations`]
+
+describe(`makeSourceFromOperation`, () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  for (const priority of priorities) {
+    const pluginOptions = { prioritize: priority }
+    for (const type of types) {
+      it(`Returns the correct ${type} when running a ${
+        priority ? `` : `non-`
+      }priority build`, async () => {
+        fetch.mockImplementationOnce(() => {
+          return {
+            body: mockBulkResults(type),
+          }
+        })
+
+        const sourceFromOperation = makeSourceFromOperation(
+          finishLastOperation,
+          completedOperation,
+          cancelOperationInProgress,
+          gatsbyApi,
+          pluginOptions,
+          lastBuildTime
+        )
+
+        await sourceFromOperation(operation)
+
+        expect(cancelOperationInProgress.mock.calls.length).toEqual(
+          priority ? 1 : 0
+        )
+        expect(finishLastOperation.mock.calls.length).toEqual(priority ? 0 : 1)
+        expect(processBulkResults.mock.calls[0][2]).toEqual(
+          require(`./__data__/${type}.json`)
+        )
+      })
+    }
+  }
+})
