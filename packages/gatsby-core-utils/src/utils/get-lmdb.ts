5d29cf537ae4a6d6d0672ec23056a32189abad38
Ward Peeters <ward@coding-tech.com>
2022-03-15 18:24:22 +0100

    fix(gatsby): move package.json check to try catch

--- packages/gatsby-core-utils/src/utils/get-lmdb.ts
+++ packages/gatsby-core-utils/src/utils/get-lmdb.ts
@@ -3,12 +3,12 @@ import importFrom from "import-from"
 import resolveFrom from "resolve-from"
 
 export function getLmdb(): typeof import("lmdb") {
-  const gatsbyPkgRoot = path.dirname(
-    resolveFrom(process.cwd(), `gatsby/package.json`)
-  )
-
   // Try to use lmdb from gatsby if not we use our own version
   try {
+    const gatsbyPkgRoot = path.dirname(
+      resolveFrom(process.cwd(), `gatsby/package.json`)
+    )
+
     return importFrom(gatsbyPkgRoot, `lmdb`) as typeof import("lmdb")
   } catch (err) {
     return require(`lmdb`)
