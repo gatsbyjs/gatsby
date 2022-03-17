274823d284db1713137568d7c2bbd611219c3673
veryspry <ehlinger.matt@gmail.com>
2022-03-17 12:24:18 -0500

    fix: encode url path pieces with base64url instead of base64 so that only url safe chars are used

--- packages/gatsby-plugin-utils/src/polyfill-remote-file/utils/base64.ts
+++ packages/gatsby-plugin-utils/src/polyfill-remote-file/utils/base64.ts
@@ -1,5 +1,5 @@
 export function base64URLEncode(str: string): string {
-  return Buffer.from(str).toString(`base64`)
+  return Buffer.from(str).toString(`base64url`)
 }
 
 export function base64URLDecode(str: string): string {
