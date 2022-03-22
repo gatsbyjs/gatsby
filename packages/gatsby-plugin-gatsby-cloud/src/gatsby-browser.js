3d4b6726a83aac69e83fd410246b9854492aa3d0
Leithon English <leithonenglish@gmail.com>
2022-03-22 12:32:12 -0500

    path updates

--- packages/gatsby-plugin-gatsby-cloud/src/gatsby-browser.js
+++ packages/gatsby-plugin-gatsby-cloud/src/gatsby-browser.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { createPortal } from "react-dom"
-import Indicator from "./components/Indicator/Indicator"
+import { Indicator } from "./components"
 
 const ShadowPortal = ({ children, identifier }) => {
   const mountNode = React.useRef(null)
