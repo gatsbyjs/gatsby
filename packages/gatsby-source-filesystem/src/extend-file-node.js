ef7eecc8f67c662341cfbe135e6b4c87987efa32
ashk001 <keigan.ashall@nbc.ca>
2022-03-24 10:56:23 -0400

    chore: lint errors

--- packages/gatsby-source-filesystem/src/extend-file-node.js
+++ packages/gatsby-source-filesystem/src/extend-file-node.js
@@ -39,15 +39,15 @@ module.exports = ({
           err => {
             // Node.js best practices dictate to copy the file and handle the
             // error without checking if it exists. Otherwise we introduce a
-            // race condition.
-            // 
+            // race condition
+            //
             // It is possible we have valid errors from:
             //   EBUSY: file busy (being transferred)
             //   EOPEN: file open (being transferred)
             //   EEXIST: file exists (already transferred)
             // What would be a true error and where we should panic:
             //    ENOENT: file does not exist
-            if (err && err.code === "ENOENT") {
+            if (err && err.code === `ENOENT`) {
               reporter.panic(
                 {
                   id: prefixId(CODES.MissingResource),
