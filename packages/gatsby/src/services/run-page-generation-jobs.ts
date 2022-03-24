090f5d11fc9342a16e4ddfd2e2983c8ad694f44a
Abhi Aiyer <abhiaiyer91@gmail.com>
2022-03-23 17:17:58 -0700

    Page generation

--- packages/gatsby/src/services/run-page-generation-jobs.ts
+++ packages/gatsby/src/services/run-page-generation-jobs.ts
@@ -6,15 +6,19 @@ import { createJobV2FromInternalJob } from "../redux/actions/internal"
 const pageGenChunkSize =
   Number(process.env.GATSBY_PARALLEL_QUERY_CHUNK_SIZE) || 50
 
-export function runPageGenerationJobs(queryIds): void {
+  interface QueryIds {
+    pageQueryIds: { path: string }[]
+  }
+
+export function runPageGenerationJobs(queryIds: QueryIds): void {
   const pageChunks = chunk(queryIds?.pageQueryIds, pageGenChunkSize)
 
-  pageChunks.forEach(chunk => {
+  pageChunks.forEach((items) => {
     const job = createInternalJob(
       {
         name: `GENERATE_PAGE`,
         args: {
-          paths: chunk?.map(({ path }) => path),
+          paths: items?.map((item) => item?.path),
         },
         inputPaths: [],
         outputDir: __dirname,
