f5820e2f3106342db1d40856a6cb63b618ec1cf5
Ward Peeters <ward@coding-tech.com>
2022-03-28 18:33:01 +0200

    update contentful tests

--- e2e-tests/contentful/cypress/integration/gatsby-image-cdn.js
+++ e2e-tests/contentful/cypress/integration/gatsby-image-cdn.js
@@ -15,28 +15,19 @@ describe(`gatsby-image-cdn urls`, () => {
   it(`creates a proper gatsby image cdn url`, testConfig, () => {
     const type = "gatsby-image-cdn"
 
-    cy.get(`[data-cy="${type}"]`)
-      .find(".gatsby-image-wrapper > picture > img")
-      .each(($el, i) => {
-        cy.wrap($el).should("be.visible")
-        cy.wrap($el)
-          .should("have.attr", "srcset")
-          .and(srcset => {
-            expect(srcset).to.match(/_gatsby\/image/)
+    cy.get(`[data-cy="${type}"] .gatsby-image-wrapper > picture > img`).then(
+      $imgs => {
+        $imgs.each($img => {
+          cy.wrap($el).should("be.visible")
 
-            parseSrcset(srcset).forEach(({ url }) => {
-              const [, , , sourceUrl64, _args64, filename] = url.split(`/`)
-              const sourceUrl = window.atob(sourceUrl64)
-              expect(sourceUrl).to.equal(
-                "https://images.ctfassets.net/k8iqpp6u0ior/3BSI9CgDdAn1JchXmY5IJi/f97a2185b3395591b98008647ad6fd3c/camylla-battani-AoqgGAqrLpU-unsplash.jpg"
-              )
-              expect(filename).to.equal(
-                "camylla-battani-AoqgGAqrLpU-unsplash.jpg"
-              )
-            })
+          const res = await fetch($el.currentSrc, {
+            method: "HEAD",
           })
+          expect(res.ok).to.be.true
 
-        cy.wrap($el).matchImageSnapshot(`${type}-${i}`)
-      })
+          cy.wrap($el).matchImageSnapshot(`${type}-${i}`)
+        })
+      }
+    )
   })
 })
