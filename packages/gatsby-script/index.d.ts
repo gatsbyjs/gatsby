30cc6a1677288f10d3eac9ff4f52e7a7a92e5ac7
tyhopp <hopp.ty.c@gmail.com>
2022-04-08 13:14:40 +0800

    feat(gatsby-script): Package scaffolding

---
+++ packages/gatsby-script/index.d.ts
@@ -0,0 +1,5 @@
+// TODO - Figure out how we want to actually export public types
+export {
+  IScriptProps as GatsbyScriptProps,
+  IScriptStrategy as GatsbyScriptStrategy,
+} from "./src/index"
---
+++ packages/gatsby-script/src/__tests__/index.ts
@@ -0,0 +1,5 @@
+/**
+ * @jest-environment jsdom
+ */
+
+// TODO
