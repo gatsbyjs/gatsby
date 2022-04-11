742458d0e3cf5fc98b12f3b93ea9cde923ce30af
tyhopp <hopp.ty.c@gmail.com>
2022-04-11 16:51:41 +0800

    chore(gatsby-script): Introduce example site

---
+++ examples/using-gatsby-script/gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby"
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `using-gatsby-script`,
+    siteUrl: `https://gatsbyjs.com`,
+  },
+  plugins: [],
+}
+
+export default config
