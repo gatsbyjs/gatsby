2016b75f7922995f3d98d6aea53be9508b3e91cf
tyhopp <hopp.ty.c@gmail.com>
2022-04-18 09:44:53 +0800

    test(gatsby-script): Inline scripts unit tests

--- packages/gatsby-script/src/__tests__/gatsby-script.tsx
+++ packages/gatsby-script/src/__tests__/gatsby-script.tsx
@@ -6,9 +6,16 @@ import React from "react"
 import { render } from "@testing-library/react"
 import { Script, ScriptStrategy } from "../gatsby-script"
 
-const scripts = {
+const scripts: Record<string, string> = {
   react: `https://unpkg.com/react@18/umd/react.development.js`,
+  inline: `console.log('Hello world!')`,
 }
+
+const strategies: Array<ScriptStrategy> = [
+  ScriptStrategy.preHydrate,
+  ScriptStrategy.postHydrate,
+  ScriptStrategy.idle,
+]
 
 describe(`Script`, () => {
   beforeAll(() => {
@@ -64,5 +71,28 @@ describe(`Script`, () => {
     )
     const script = container.parentElement?.querySelector(`script`)
     expect(script?.getAttribute(`async`)).not.toBeNull()
+  })
+
+  it(`should work with inline scripts passed via the dangerouslySetInnerHTML prop`, () => {
+    for (const strategy of strategies) {
+      const { container } = render(
+        <Script
+          strategy={strategy}
+          dangerouslySetInnerHTML={{ __html: scripts.inline }}
+        />
+      )
+      const script = container.parentElement?.querySelector(`script`)
+      expect(script?.textContent).toBe(scripts.inline)
+    }
+  })
+
+  it(`should work with inline scripts passed as template literals`, () => {
+    for (const strategy of strategies) {
+      const { container } = render(
+        <Script strategy={strategy}>{scripts.inline}</Script>
+      )
+      const script = container.parentElement?.querySelector(`script`)
+      expect(script?.textContent).toBe(scripts.inline)
+    }
   })
 })
