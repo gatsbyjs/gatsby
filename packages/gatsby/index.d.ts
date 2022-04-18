7472bbf5341889c420018818a3a81f630884531b
veryspry <ehlinger.matt@gmail.com>
2022-04-18 12:49:35 +0100

    feat: update gatsby index.d.ts doc reference for addGatsbyImageSourceUrl

--- packages/gatsby/index.d.ts
+++ packages/gatsby/index.d.ts
@@ -1279,7 +1279,7 @@ export interface Actions {
     plugin?: ActionPlugin
   ): Promise<unknown>
 
-  /** @todo add docs reference */
+  /** @see https://www.gatsbyjs.com/docs/actions/#addGatsbyImageSourceUrl */
   addGatsbyImageSourceUrl(
     this: void,
     sourceUrl: string,
