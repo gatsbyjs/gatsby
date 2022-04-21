3dede503058e78a7506ea5658c3541847a1d0f5a
Tyler Barnes <tylerdbarnes@gmail.com>
2022-04-21 14:16:27 -0700

    protect against missing plugin options (in tests)

--- packages/gatsby-source-drupal/src/normalize.js
+++ packages/gatsby-source-drupal/src/normalize.js
@@ -30,7 +30,7 @@ const getGatsbyImageCdnFields = async ({
   fileNodesExtendedData,
   reporter,
 }) => {
-  if (!pluginOptions.skipFileDownloads) {
+  if (!pluginOptions?.skipFileDownloads) {
     return {}
   }
 
