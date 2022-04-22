2777d747a675ad53315a9e2ce7936da23ab57c4a
Florian Kissling <florian.kissling@gmail.com>
2022-04-22 22:55:44 +0200

    Bring back `<Header>` + static query
    Did we explicitly define ditching the static query specifically?

--- starters/default/src/components/layout.js
+++ starters/default/src/components/layout.js
@@ -7,12 +7,25 @@
 
 import * as React from "react"
 import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
 
+import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
   return (
     <>
+      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <div
         style={{
           margin: `0 auto`,
