4337719232999789ba67bd2415c0e5123994fbea
Florian Kissling <florian.kissling@gmail.com>
2022-04-23 00:53:05 +0200

    Use `<code>` for inline code (instead  `em`)

--- starters/default/src/pages/using-typescript.tsx
+++ starters/default/src/pages/using-typescript.tsx
@@ -16,13 +16,14 @@ const UsingTypescript: React.FC<PageProps<DataProps>> = ({ data, path }) => (
     <Seo title="Using TypeScript" />
     <h1>Gatsby supports TypeScript by default!</h1>
     <p>
-      This means that you can create and write <em>.ts/.tsx</em> files for your
-      pages, components etc. Please note that the <em>gatsby-*.js</em> files
-      (like gatsby-node.js) currently don't support TypeScript yet.
+      This means that you can create and write <code>.ts/.tsx</code> files for
+      your pages, components etc. Please note that the <code>gatsby-*.js</code>{" "}
+      files (like <code>gatsby-node.js</code>) currently don't support
+      TypeScript yet.
     </p>
     <p>
-      For type checking you'll want to install <em>typescript</em> via npm and
-      run <em>tsc --init</em> to create a <em>tsconfig</em> file.
+      For type checking you'll want to install <code>typescript</code> via npm
+      and run <code>tsc --init</code> to create a <code>tsconfig</code> file.
     </p>
     <p>
       You're currently on the page "{path}" which was built on{" "}
