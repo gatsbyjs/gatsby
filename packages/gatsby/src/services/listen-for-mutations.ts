67a4e14d9c85e3a26cdcf194293a173eecc2863c
LekoArts <lekoarts@gmail.com>
2022-04-27 13:32:37 +0100

    remove unneeded changes

--- packages/gatsby/src/services/listen-for-mutations.ts
+++ packages/gatsby/src/services/listen-for-mutations.ts
@@ -18,21 +18,15 @@ export const listenForMutations: InvokeCallback = (callback: Sender<any>) => {
     callback({ type: `QUERY_RUN_REQUESTED`, payload: event })
   }
 
-  const emitSetSchema = (event: unknown): void => {
-    callback({ type: `SET_SCHEMA`, payload: event })
-  }
-
   emitter.on(`ENQUEUE_NODE_MUTATION`, emitMutation)
   emitter.on(`WEBHOOK_RECEIVED`, emitWebhook)
   emitter.on(`SOURCE_FILE_CHANGED`, emitSourceChange)
   emitter.on(`QUERY_RUN_REQUESTED`, emitQueryRunRequest)
-  emitter.on(`SET_SCHEMA`, emitSetSchema)
 
   return function unsubscribeFromMutationListening(): void {
     emitter.off(`ENQUEUE_NODE_MUTATION`, emitMutation)
     emitter.off(`WEBHOOK_RECEIVED`, emitWebhook)
     emitter.off(`SOURCE_FILE_CHANGED`, emitSourceChange)
     emitter.off(`QUERY_RUN_REQUESTED`, emitQueryRunRequest)
-    emitter.off(`SET_SCHEMA`, emitSetSchema)
   }
 }
