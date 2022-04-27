97caba4ce6a319b4f0f28110f0ee516bc5542cb6
tyhopp <hopp.ty.c@gmail.com>
2022-04-27 23:07:37 +0800

    feat(gatsby-script): Gatsby serve support

--- packages/gatsby/src/commands/serve.ts
+++ packages/gatsby/src/commands/serve.ts
@@ -26,6 +26,7 @@ import { reverseFixedPagePath } from "../utils/page-data"
 import { initTracer } from "../utils/tracer"
 import { configureTrailingSlash } from "../utils/express-middlewares"
 import { getDataStore, detectLmdbStore } from "../datastore"
+import proxy from "express-http-proxy"
 
 process.env.GATSBY_EXPERIMENTAL_LMDB_STORE = `1`
 detectLmdbStore()
@@ -120,6 +121,18 @@ module.exports = async (program: IServeProgram): Promise<void> => {
   const root = path.join(program.directory, `public`)
 
   const app = express()
+
+  // Proxy gatsby-script using off-main-thread strategy
+  app.use(
+    `/__partytown-proxy`,
+    proxy(req => new URL(req.query.url as string).host as string, {
+      proxyReqPathResolver: req => {
+        const { pathname = ``, search = `` } = new URL(req.query.url as string)
+        return pathname + search
+      },
+    })
+  )
+
   // eslint-disable-next-line new-cap
   const router = express.Router()
 
