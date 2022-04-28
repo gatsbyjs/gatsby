f7f9d647204074a4c1df0ab54c20bcbd0e43cf9c
Ward Peeters <ward@coding-tech.com>
2022-04-28 20:31:07 +0200

    Update packages/gatsby-core-utils/src/fetch-remote-file.ts
    Co-authored-by: Tyler Barnes <tylerdbarnes@gmail.com>

--- packages/gatsby-core-utils/src/fetch-remote-file.ts
+++ packages/gatsby-core-utils/src/fetch-remote-file.ts
@@ -49,7 +49,6 @@ export async function fetchRemoteFile(
       if (await fs.pathExists(cachedPath)) {
         // If the cached directory is not part of the public directory, we don't need to copy it
         // as it won't be part of the build.
-        console.log({ cachedPath, downloadPath })
         if (isPublicPath(downloadPath) && cachedPath !== downloadPath) {
           return copyCachedPathToDownloadPath({ cachedPath, downloadPath })
         }
