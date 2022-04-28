22d640ec27bfae39cf31a1a5cc953041f80c6133
LekoArts <lekoarts@gmail.com>
2022-04-28 12:57:44 +0100

    linting

--- packages/gatsby/src/utils/graphql-typegen/utils.ts
+++ packages/gatsby/src/utils/graphql-typegen/utils.ts
@@ -23,7 +23,7 @@ const fieldFilterFromCoordinates =
 
 export function filterPluginSchema(
   schema: GraphQLSchema,
-  typeFilter = (typeName: string): boolean => true
+  typeFilter = (): boolean => true
 ): GraphQLSchema {
   return mapSchema(
     filterSchema({
@@ -66,7 +66,7 @@ export function filterPluginSchema(
 
 export function stabilizeSchema(
   schema: GraphQLSchema,
-  typeFilter = (typeName: string): boolean => true
+  typeFilter = (): boolean => true
 ): GraphQLSchema {
   return lexicographicSortSchema(filterPluginSchema(schema, typeFilter))
 }
