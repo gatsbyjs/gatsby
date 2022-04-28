d6b02fd3e350bf82b96374fd8cc0b02fdff25b25
LekoArts <lekoarts@gmail.com>
2022-04-28 13:05:53 +0100

    types

--- packages/gatsby/src/utils/graphql-typegen/utils.ts
+++ packages/gatsby/src/utils/graphql-typegen/utils.ts
@@ -23,7 +23,7 @@ const fieldFilterFromCoordinates =
 
 export function filterPluginSchema(
   schema: GraphQLSchema,
-  typeFilter = (): boolean => true
+  typeFilter = (_: string): boolean => true
 ): GraphQLSchema {
   return mapSchema(
     filterSchema({
@@ -66,7 +66,7 @@ export function filterPluginSchema(
 
 export function stabilizeSchema(
   schema: GraphQLSchema,
-  typeFilter = (): boolean => true
+  typeFilter = (_: string): boolean => true
 ): GraphQLSchema {
   return lexicographicSortSchema(filterPluginSchema(schema, typeFilter))
 }
