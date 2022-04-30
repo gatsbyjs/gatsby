4d7409907677e2d00319450e28681156f1d84b6d
Tyler Barnes <tylerdbarnes@gmail.com>
2022-04-29 18:50:36 -0700

    add svg image integration test

--- integration-tests/gatsby-source-wordpress/test-fns/gatsby-image.js
+++ integration-tests/gatsby-source-wordpress/test-fns/gatsby-image.js
@@ -6,11 +6,13 @@ const execall = require("execall")
 
 const countGatsbyImgs = string =>
   execall(/[^-]gatsby-image-wrapper[^-]/gim, string).length
+const countGatsbyFiles = string =>
+  execall(/[^-]_gatsby\/file[^-]/gim, string).length
 
 describe(`Gatsby image processing`, () => {
   it(`transforms inline-html images properly`, async () => {
     const {
-      data: { acfPage },
+      data: { acfPage, inlineSvgImageInHtmlFieldPage },
     } = await fetchGraphql({
       url: `http://localhost:8000/__graphql`,
       query: /* GraphQL */ `
@@ -20,10 +22,16 @@ describe(`Gatsby image processing`, () => {
               wysiwygEditorField
             }
           }
+          inlineSvgImageInHtmlFieldPage: wpPage(databaseId: { eq: 11834 }) {
+            content
+          }
         }
       `,
     })
     expect(acfPage.acfPageFields.wysiwygEditorField).toBeTruthy()
     expect(countGatsbyImgs(acfPage.acfPageFields.wysiwygEditorField)).toBe(2)
+
+    expect(inlineSvgImageInHtmlFieldPage?.content).toBeTruthy()
+    expect(countGatsbyFiles(inlineSvgImageInHtmlFieldPage?.content)).toBe(1)
   })
 })
