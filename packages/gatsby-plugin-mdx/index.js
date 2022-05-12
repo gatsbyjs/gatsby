fd7797e2ce852c3c8394309f3b0f556c9a27b88e
axe312ger <opensource@axe312.dev>
2022-05-12 12:13:46 +0200

    initial version with FS pages and frontmatter support

--- packages/gatsby-plugin-mdx/index.js
+++ packages/gatsby-plugin-mdx/index.js
@@ -1,11 +1,2 @@
-/**
- * Welcome to gatsby-plugin-mdx!
- *
- * Start reading in gatsby-node.js
- */
-
-const MDXRenderer = require(`./mdx-renderer`)
-
-module.exports = {
-  MDXRenderer: MDXRenderer,
-}
+// noop
+"use strict";
