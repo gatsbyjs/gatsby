dc054d4f4d0e8c950c5d1db80abb90d0130345bf
Tyler Barnes <tylerdbarnes@gmail.com>
2022-05-13 14:19:32 -0700

    try removing type imports

--- packages/gatsby-core-utils/src/utils/get-store.ts
+++ packages/gatsby-core-utils/src/utils/get-store.ts
@@ -2,15 +2,13 @@ import path from "path"
 import importFrom from "import-from"
 import resolveFrom from "resolve-from"
 
-export function getStore(): typeof import("gatsby/src/redux")["store"] {
+export function getStore(): unknown {
   const gatsbyPkgRoot = path.dirname(
     resolveFrom(process.cwd(), `gatsby/package.json`)
   )
 
-  const { store } = importFrom(
-    gatsbyPkgRoot,
-    `gatsby/dist/redux`
-  ) as typeof import("gatsby/src/redux")
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const { store } = importFrom(gatsbyPkgRoot, `gatsby/dist/redux`) as any
 
   return store
 }
