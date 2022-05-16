a407159ea1a2ea3f79584c60d4ea73b70c7f8a5f
Josh <jcjohnson77@gmail.com>
2022-05-16 10:43:50 -0400

    Do it right

--- e2e-tests/production-runtime/cypress/integration/assets.js
+++ e2e-tests/production-runtime/cypress/integration/assets.js
@@ -16,23 +16,32 @@ describe(`webpack assets`, () => {
         new RegExp(`merriweather-latin-300italic-`, 'i'),
       ]
 
-      // match each one in a list since requests are not deterministic
-      cy.wait("@font").should(req => {
-        for (let i = 0; i < filesNeeded.length; i++) {
-          if (filesNeeded[i].test(req.response.url)) {
-            filesNeeded.splice(i, 1)
-          }
-        }
-      })
+      let totalFiles = filesNeeded.length
 
-      // we should have matched every item in the list
-      expect(filesNeeded).to.be.equal([])
+      // cy.wait enough times to catch all files
+      for (let i = 0; i < totalFiles; i++) {
+
+        // match each one in a list since requests are not deterministic
+        cy.wait("@font").should(req => {
+          let matched = false;
+          for (let i = 0; i < filesNeeded.length; i++) {
+            if (filesNeeded[i].test(req.response.url)) {
+              matched = true;
+              filesNeeded.splice(i, 1)
+            }
+          }
+
+          expect(matched).to.be.equal(true)
+        })
+      }
     })
+
     it(`should load image import`, () => {
       cy.wait("@img-import").should(req => {
         expect(req.response.statusCode).to.be.gte(200).and.lt(400)
       })
     })
+
     it(`should load file import`, () => {
       cy.getTestElement('assets-pdf-import').should('have.attr', 'href').and('match', /\/static\/pdf-example-.*\.pdf/i)
     })
