14b6730d90b06a9af6491d76326d4ecd8af0586e
Tyler Barnes <tylerdbarnes@gmail.com>
2022-05-17 13:43:35 -0700

    use import from to get the gatsby store

--- packages/gatsby-plugin-utils/src/polyfill-remote-file/utils/get-request-headers-for-url.ts
+++ packages/gatsby-plugin-utils/src/polyfill-remote-file/utils/get-request-headers-for-url.ts
@@ -1,12 +1,28 @@
-import { store } from "gatsby/dist/redux"
 import url from "url"
+import path from "path"
+import importFrom from "import-from"
+import resolveFrom from "resolve-from"
+
 import type { Headers } from "got"
+
+export function getStore(): typeof import("gatsby/src/redux")["store"] {
+  const gatsbyPkgRoot = path.dirname(
+    resolveFrom(process.cwd(), `gatsby/package.json`)
+  )
+
+  const { store } = importFrom(
+    gatsbyPkgRoot,
+    `gatsby/dist/redux`
+  ) as typeof import("gatsby/src/redux")
+
+  return store
+}
 
 export function getRequestHeadersForUrl(
   passedUrl: string
 ): Headers | undefined {
   const baseDomain = url.parse(passedUrl).hostname
-  const { requestHeaders } = store.getState()
+  const { requestHeaders } = getStore().getState()
 
-  return requestHeaders?.get(baseDomain)
+  return baseDomain ? requestHeaders?.get(baseDomain) : undefined
 }
