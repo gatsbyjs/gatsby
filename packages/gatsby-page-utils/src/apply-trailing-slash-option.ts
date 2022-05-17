5bcd3ade0ae201ece6ab906147bfcc707af2b667
LekoArts <lekoarts@gmail.com>
2022-05-17 10:17:50 +0200

    initial

--- packages/gatsby-page-utils/src/apply-trailing-slash-option.ts
+++ packages/gatsby-page-utils/src/apply-trailing-slash-option.ts
@@ -6,8 +6,11 @@ export const applyTrailingSlashOption = (
   option: TrailingSlash = `legacy`
 ): string => {
   const hasHtmlSuffix = input.endsWith(`.html`)
+  const hasXmlSuffix = input.endsWith(`.xml`)
+  const hasPdfSuffix = input.endsWith(`.pdf`)
+
   if (input === `/`) return input
-  if (hasHtmlSuffix) {
+  if (hasHtmlSuffix || hasXmlSuffix || hasPdfSuffix) {
     option = `never`
   }
   if (option === `always`) {
