610562a609f403af96821caf0451e1275c9f3371
tyhopp <hopp.ty.c@gmail.com>
2022-05-18 11:17:39 +0800

    Add headers to proxy

--- packages/gatsby/src/internal-plugins/partytown/gatsby-node.ts
+++ packages/gatsby/src/internal-plugins/partytown/gatsby-node.ts
@@ -2,6 +2,7 @@ import path from "path"
 import { copyLibFiles } from "@builder.io/partytown/utils"
 import { CreateDevServerArgs } from "gatsby"
 import { partytownProxyPath, partytownProxy } from "./proxy"
+import reporter from "gatsby-cli/lib/reporter"
 
 /**
  * Copy Partytown library files to public.
@@ -20,15 +21,28 @@ exports.createPages = ({ actions, store }): void => {
   const { createRedirect } = actions
 
   const { config = {} } = store.getState()
-  const { partytownProxiedURLs = [] } = config
+  const { partytownProxiedURLs = [], siteMetadata = {} } = config
 
   for (const host of partytownProxiedURLs) {
     const encodedURL: string = encodeURI(host)
+
+    const headers: Array<string> = [`Referrer-Policy: same-origin`]
+
+    if (siteMetadata?.siteUrl) {
+      headers.push(`Access-Control-Allow-Origin: ${siteMetadata?.siteUrl}`)
+    } else {
+      reporter.warn(
+        `No siteMetadata.siteUrl found in gatsby-config.\nAdd one to ensure the Partytown proxy for your off-main-thread scripts are restricted to your site URL with CORS.\nSee https://gatsby.dev/gatsby-script for more information.`
+      )
+    }
 
     createRedirect({
       fromPath: `${partytownProxyPath}?url=${encodedURL}`,
       toPath: encodedURL,
       statusCode: 200,
+      headers: {
+        [partytownProxyPath]: headers,
+      },
     })
   }
 }
--- packages/gatsby/src/internal-plugins/partytown/proxy.ts
+++ packages/gatsby/src/internal-plugins/partytown/proxy.ts
@@ -8,6 +8,17 @@ export function partytownProxy(
 ): RequestHandler {
   return proxy(req => new URL(req.query.url as string).origin as string, {
     filter: req => partytownProxiedURLs.some(url => req.query?.url === url),
+    userResHeaderDecorator(headers, userReq) {
+      const origin = new URL(userReq.headers.referer as string).origin as string
+
+      if (origin) {
+        headers[`access-control-allow-origin`] = origin
+      }
+
+      headers[`referrer-policy`] = `same-origin`
+
+      return headers
+    },
     proxyReqPathResolver: req => {
       const { pathname = ``, search = `` } = new URL(req.query?.url as string)
       return pathname + search
