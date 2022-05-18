c5c7065183f98c3bc20b156f0ea8ef2af1f757cb
tyhopp <hopp.ty.c@gmail.com>
2022-05-18 11:46:10 +0800

    Optional chaining

--- packages/gatsby/src/internal-plugins/partytown/proxy.ts
+++ packages/gatsby/src/internal-plugins/partytown/proxy.ts
@@ -6,10 +6,11 @@ export const partytownProxyPath = `/__partytown-proxy`
 export function partytownProxy(
   partytownProxiedURLs: Array<string>
 ): RequestHandler {
-  return proxy(req => new URL(req.query.url as string).origin as string, {
+  return proxy(req => new URL(req.query?.url as string)?.origin as string, {
     filter: req => partytownProxiedURLs.some(url => req.query?.url === url),
     userResHeaderDecorator(headers, userReq) {
-      const origin = new URL(userReq.headers.referer as string).origin as string
+      const origin = new URL(userReq.headers?.referer as string)
+        ?.origin as string
 
       if (origin) {
         headers[`access-control-allow-origin`] = origin
