bf48099ead677f134be043a21a239a0e821f2d18
Tyler Barnes <tylerdbarnes@gmail.com>
2022-05-19 15:17:07 -0700

    update warning message

--- packages/gatsby-plugin-utils/src/polyfill-remote-file/utils/get-request-headers-for-url.ts
+++ packages/gatsby-plugin-utils/src/polyfill-remote-file/utils/get-request-headers-for-url.ts
@@ -3,14 +3,23 @@ import url from "url"
 import type { Headers } from "got"
 import type { Store } from "gatsby"
 
+let loggedWarning = false
+
 export function getRequestHeadersForUrl(
   passedUrl: string,
   store: Store
 ): Headers | undefined {
-  if (!store || !(`getState` in store)) {
+  const storeIsMissing = !store || !(`getState` in store)
+
+  if (storeIsMissing && !loggedWarning) {
+    loggedWarning = true
     console.warn(
-      `getRequestHeadersForUrl: argument 2 "store" is not a redux store. Please pass Gatsby's redux store. (for url ${passedUrl})`
+      `Gatsby's redux store is required in the "addRemoteFilePolyfillInterface" and "polyfillImageServiceDevRoutes" API's for image CDN. No store was found when requesting url "${passedUrl}". This will fail your build in Gatsby V5. Upgrade your source plugins and Gatsby packages to the latest versions to resolve this. If you are a source plugin author visit https://gatsby.dev/source-plugins-image-cdn to learn how to make this warning go away.`
     )
+  }
+
+  if (storeIsMissing) {
+    return undefined
   }
 
   const baseDomain = url.parse(passedUrl).hostname
