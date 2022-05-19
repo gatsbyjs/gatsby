497adf76187e22c0ce9a47210f05fe68c039d41e
Tyler Barnes <tylerdbarnes@gmail.com>
2022-05-19 10:52:16 -0700

    remove reporter import

--- packages/gatsby-plugin-utils/src/polyfill-remote-file/utils/get-request-headers-for-url.ts
+++ packages/gatsby-plugin-utils/src/polyfill-remote-file/utils/get-request-headers-for-url.ts
@@ -2,18 +2,15 @@ import url from "url"
 
 import type { Headers } from "got"
 import type { Store } from "gatsby"
-import reporter from "gatsby-cli/lib/reporter"
 
 export function getRequestHeadersForUrl(
   passedUrl: string,
   store: Store
 ): Headers | undefined {
   if (!store || !(`getState` in store)) {
-    reporter.warn(
+    throw new Error(
       `getRequestHeadersForUrl: argument 2 "store" is not a redux store. Please pass Gatsby's redux store. (for url ${passedUrl})`
     )
-
-    return undefined
   }
 
   const baseDomain = url.parse(passedUrl).hostname
