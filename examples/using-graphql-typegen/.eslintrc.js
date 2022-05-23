824fb24b86022c076131bcdaf4cf8d0070c798d1
Lennart <lekoarts@gmail.com>
2022-05-23 09:41:03 +0200

    Apply suggestions from code review
    Co-authored-by: Dimitri POSTOLOV <en3m@ya.ru>

--- examples/using-graphql-typegen/.eslintrc.js
+++ examples/using-graphql-typegen/.eslintrc.js
@@ -1,19 +1,14 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: [
-    "@typescript-eslint"
-  ],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended"
-  ],
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
       processor: '@graphql-eslint/graphql',
-      extends: ['eslint:recommended'],
+      parser: "@typescript-eslint/parser",
+      extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+      ],
       env: {
         es6: true,
       },
