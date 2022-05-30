abcd7cd4cc8c79f007d78cf285b2de008a43c90f
LekoArts <lekoarts@gmail.com>
2022-05-30 14:45:23 +0200

    add doc link

--- starters/gatsby-starter-minimal-ts/gatsby-config.ts
+++ starters/gatsby-starter-minimal-ts/gatsby-config.ts
@@ -5,6 +5,7 @@ const config: GatsbyConfig = {
     siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [],
