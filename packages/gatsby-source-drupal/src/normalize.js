9a3ce9b6098d6818779253b7345e470e484d64ce
Tyler Barnes <tylerdbarnes@gmail.com>
2022-06-06 21:19:48 -0700

    tweak reporting

--- packages/gatsby-source-drupal/src/normalize.js
+++ packages/gatsby-source-drupal/src/normalize.js
@@ -21,6 +21,7 @@ const imageCDNState = {
 exports.imageCDNState = imageCDNState
 
 let four04WarningCount = 0
+let corruptFileWarningCount = 0
 /**
  * This FN takes in node data and returns Gatsby Image CDN fields that should be added to that node. If the input node isn't an image an empty object is returned.
  */
@@ -104,16 +105,19 @@ const getGatsbyImageCdnFields = async ({
     if (e.message.includes(`404`)) {
       if (four04WarningCount < 10) {
         four04WarningCount++
-        reporter.warn(`Drupal file missing for url ${url}`)
+        reporter.warn(`[gatsby-source-drupal] file returns 404: ${url}`)
       }
 
       return {}
     }
 
     if (e.message.includes(`unrecognized file format`)) {
-      reporter.error(
-        `[gatsby-source-drupal] Encountered corrupt file while requesting image dimensions for ${url}`
-      )
+      if (corruptFileWarningCount < 10) {
+        corruptFileWarningCount++
+        reporter.warn(
+          `[gatsby-source-drupal] Encountered corrupt file while requesting image dimensions for ${url}`
+        )
+      }
 
       return {}
     }
