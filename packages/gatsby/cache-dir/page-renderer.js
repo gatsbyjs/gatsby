3d7e8afa676b05171878fd1158038c8c05b64df8
Jude Agboola <marvinjudehk@gmail.com>
2022-06-26 22:01:57 +0400

    use react to render into head instead of parsing and appending resulting child nodes

--- packages/gatsby/cache-dir/page-renderer.js
+++ packages/gatsby/cache-dir/page-renderer.js
@@ -1,98 +1,103 @@
-import React, { createElement } from "react"
+import React, { createElement, useEffect } from "react"
+import { createRoot } from "react-dom/client"
 import PropTypes from "prop-types"
 import { apiRunner } from "./api-runner-browser"
 import { grabMatchParams } from "./find-path"
-import { renderToString } from "react-dom/server"
 import { StaticQueryContext } from "gatsby"
-
 import { VALID_NODE_NAMES } from "./head/constants"
 
+// Calls callback in an effect and renders children
+function Caller({ children, callback }) {
+  useEffect(() => {
+    callback()
+  }, [callback])
+
+  return children
+}
+
 // Renders page
-class PageRenderer extends React.Component {
-  render() {
-    const props = {
-      ...this.props,
-      params: {
-        ...grabMatchParams(this.props.location.pathname),
-        ...this.props.pageResources.json.pageContext.__params,
-      },
+function PageRenderer(props) {
+  const _props = {
+    ...props,
+    params: {
+      ...grabMatchParams(props.location.pathname),
+      ...props.pageResources.json.pageContext.__params,
+    },
+  }
+
+  const preferDefault = m => (m && m.default) || m
+
+  const pageElement = createElement(
+    preferDefault(props.pageResources.component),
+    {
+      ...props,
+      key: props.path || props.pageResources.page.path,
     }
+  )
 
-    const preferDefault = m => (m && m.default) || m
+  const pageComponent = props.pageResources.component
 
-    const pageElement = createElement(
-      preferDefault(this.props.pageResources.component),
-      {
-        ...props,
-        key: this.props.path || this.props.pageResources.page.path,
-      }
-    )
-
-    const pageComponent = this.props.pageResources.component
-
-    if (pageComponent.head) {
-      if (typeof pageComponent.head !== `function`)
-        throw new Error(
-          `Expected "head" export to be a function got "${typeof pageComponent.head}".`
-        )
-
-      const headElement = createElement(
-        StaticQueryContext.Provider,
-        { value: this.props.pageResources.staticQueryResults },
-        createElement(pageComponent.head, props, null)
+  if (pageComponent.head) {
+    if (typeof pageComponent.head !== `function`)
+      throw new Error(
+        `Expected "head" export to be a function got "${typeof pageComponent.head}".`
       )
 
-      // extract head nodes from string
-      const rawString = renderToString(headElement)
-      const parser = new DOMParser()
-      const parsed = parser.parseFromString(rawString, `text/html`)
+    const headElement = createElement(
+      StaticQueryContext.Provider,
+      { value: props.pageResources.staticQueryResults },
+      createElement(pageComponent.head, _props, null)
+    )
 
-      // If DOMParser encounters invalid head elements, it put all the elements in the body
-      const headNodes = [...parsed.head.childNodes, ...parsed.body.childNodes]
+    useEffect(() => {
+      const hiddenRoot = document.createElement(`div`)
+      const root = createRoot(hiddenRoot)
 
-      // Remove previous head nodes
-      const prevHeadNodes = [...document.querySelectorAll(`[data-gatsby-head]`)]
-      prevHeadNodes.forEach(e => e.remove())
+      const callback = () => {
+        // Remove previous head nodes
+        const prevHeadNodes = [
+          ...document.querySelectorAll(`[data-gatsby-head]`),
+        ]
+        prevHeadNodes.forEach(e => e.remove())
 
-      // add attribute to new head nodes while showing warning if it's not a valid node
-      let validHeadNodes = []
+        // add attribute to new head nodes while showing warning if it's not a valid node
+        let validHeadNodes = []
 
-      for (const node of headNodes) {
-        const nodeName = node.nodeName.toLowerCase()
+        for (const node of hiddenRoot.childNodes) {
+          const nodeName = node.nodeName.toLowerCase()
 
-        if (!VALID_NODE_NAMES.includes(nodeName)) {
-          if (process.env.NODE_ENV !== `production`) {
-            if (nodeName !== `script`) {
-              console.warn(
-                `<${nodeName}> is not a valid head element. Please use one of the following: ${VALID_NODE_NAMES.join(
-                  `, `
-                )}`
-              )
-            } else {
-              console.warn(
-                `Do not add scripts here. Please use the <Script> component in your page template instead. For more info see: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-script/`
-              )
+          if (!VALID_NODE_NAMES.includes(nodeName)) {
+            if (process.env.NODE_ENV !== `production`) {
+              const warning =
+                nodeName !== `script`
+                  ? `<${nodeName}> is not a valid head element. Please use one of the following: ${VALID_NODE_NAMES.join(
+                      `, `
+                    )}`
+                  : `Do not add scripts here. Please use the <Script> component in your page template instead. For more info see: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-script/`
+              console.warn(warning)
             }
+          } else {
+            node.setAttribute(`data-gatsby-head`, true)
+            validHeadNodes = [...validHeadNodes, node]
           }
-        } else {
-          node.setAttribute(`data-gatsby-head`, true)
-          validHeadNodes = [...validHeadNodes, node]
         }
-      }
 
-      document.head.append(...validHeadNodes)
-    }
-    const wrappedPage = apiRunner(
-      `wrapPageElement`,
-      { element: pageElement, props },
-      pageElement,
-      ({ result }) => {
-        return { element: result, props }
+        document.head.append(...validHeadNodes)
       }
-    ).pop()
-
-    return wrappedPage
+      // just a hack to call the callback after react has done first render
+      root.render(<Caller callback={callback}>{headElement}</Caller>)
+    }, [headElement])
   }
+  const wrappedPage = apiRunner(
+    `wrapPageElement`,
+    { element: pageElement, props },
+    pageElement,
+    ({ result }) => {
+      return { element: result, props }
+    }
+  ).pop()
+
+  return wrappedPage
 }
 
 PageRenderer.propTypes = {
