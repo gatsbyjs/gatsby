d1deec892ef1ca62ff264227c985197a04bb37db
axe312ger <opensource@axe312.dev>
2022-06-28 15:24:02 +0200

    test: add unit test for chunk name shortening

--- packages/gatsby/src/utils/__tests__/js-chunk-names.ts
+++ packages/gatsby/src/utils/__tests__/js-chunk-names.ts
@@ -32,4 +32,14 @@ describe(`js-chunk-names`, () => {
       generateComponentChunkName(`/src/pages/user/{id}/{name}.js`)
     ).toEqual(`component---src-pages-user-{id}-{name}-js`)
   })
+
+  it(`it shortens to long names`, () => {
+    expect(
+      generateComponentChunkName(
+        `/src/content/lorem-ipsum-dolor-sit-amet-consectetur-adipiscing-elit-sed-non-ex-libero-praesent-ac-neque-id-ex-vehicula-imperdiet-eget-et-dolor-fusce-cursus-neque-in-ipsum-varius-dictum-sed-ac-lectus-faucibus-lobortis-eros-a-lacinia-leo-pellentesque-convallis-volutpat.mdx`
+      )
+    ).toEqual(
+      `component---src-content-lorem-ipsum-dolor-sit-amet-consectetur-adipiscing-elit-sed-non-ex-libero-praesent-ac-neque-id-ex-vehicula-imperdiet-eget-et-dolor-fusce-cursus-neque-in-ipsum-varius-dictum-sed-ac-lectus-fau-k-uksd-fy-2-l-ktc-obj-ckz-cn-1-n-dgn-4-o`
+    )
+  })
 })
