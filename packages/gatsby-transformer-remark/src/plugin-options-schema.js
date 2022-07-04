2a17b1502b54b1f256e9997ab52520bd0c55b15a
Byron Hill <byron@atomicdigital.studio>
2022-07-04 08:13:40 -0600

    Adding descriptions to the new fields

--- packages/gatsby-transformer-remark/src/plugin-options-schema.js
+++ packages/gatsby-transformer-remark/src/plugin-options-schema.js
@@ -10,9 +10,21 @@ module.exports = function pluginOptionsSchema({ Joi }) {
       `If your Markdown file contains HTML, excerpt will not return a value. In that case, you can set an excerpt_separator to an HTML tag. Edit your Markdown files to include that HTML tag after the text you’d like to appear in the excerpt.`
     ),
     contentTypes: Joi.object({
-      resolveType: Joi.alternatives(Joi.string(), Joi.function()).required(),
-      types: Joi.array().items(Joi.string()).unique().required(),
-    }),
+      resolveType: Joi.alternatives(Joi.string(), Joi.function())
+        .required()
+        .description(
+          `A string representing the key that defines the content type of a markdown file or a function that resolves to a content type.`
+        ),
+      types: Joi.array()
+        .items(Joi.string())
+        .unique()
+        .required()
+        .description(
+          `A list of potential types that 'resolveType' can return.`
+        ),
+    }).description(
+      `An object defining optional custom content types to sort markdown files into when creating the data layer.`
+    ),
     plugins:
       _CFLAGS_.GATSBY_MAJOR === `4`
         ? Joi.subPlugins().description(
