e1ca969bb9ea3d8774a579541e0ced88af47b73e
Jude Agboola <marvinjudehk@gmail.com>
2022-07-04 18:58:16 +0100

    add head handler for DEV SSR

--- packages/gatsby/cache-dir/ssr-develop-static-entry.js
+++ packages/gatsby/cache-dir/ssr-develop-static-entry.js
@@ -1,6 +1,6 @@
 /* global BROWSER_ESM_ONLY */
 import React from "react"
-import fs from "fs"
+import fs from "fs-extra"
 import { renderToString, renderToStaticMarkup } from "react-dom/server"
 import { get, merge, isObject, flatten, uniqBy, concat } from "lodash"
 import nodePath from "path"
@@ -10,6 +10,9 @@ import syncRequires from "$virtual/ssr-sync-requires"
 
 import { RouteAnnouncerProps } from "./route-announcer-props"
 import { ServerLocation, Router, isRedirect } from "@reach/router"
+import { headHandlerForSSR } from "./head/head-export-handler-for-ssr"
+import { getStaticQueryResults } from "./loader"
+import path from "path"
 
 // prefer default export if available
 const preferDefault = m => (m && m.default) || m
@@ -22,6 +25,18 @@ const testRequireError = (moduleName, err) => {
   const regex = new RegExp(`Error: Cannot find module\\s.${moduleName}`)
   const firstLine = err.toString().split(`\n`)[0]
   return regex.test(firstLine)
+}
+
+const readStaticQueryContext = async templatePath => {
+  const filePath = path.join(
+    __dirname,
+    `sq-context`,
+    templatePath,
+    `sq-context.json`
+  )
+  const rawSQContext = await fs.readFile(filePath, `utf-8`)
+
+  return JSON.parse(rawSQContext)
 }
 
 let cachedStats
@@ -161,6 +176,16 @@ export default async function staticPage({
     const pageData = getPageData(pagePath)
 
     const { componentChunkName } = pageData
+
+    const pageComponent = await syncRequires.ssrComponents[componentChunkName]
+
+    headHandlerForSSR({
+      pageComponent,
+      setHeadComponents,
+      staticQueryContext: getStaticQueryResults(),
+      pageData,
+      pagePath,
+    })
 
     let scriptsAndStyles = flatten(
       [`commons`].map(chunkKey => {
