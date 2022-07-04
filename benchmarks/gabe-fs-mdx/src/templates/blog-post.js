46a1c2ca434b035dc29cb3d34cbcd3eb3df4340f
Lennart <lekoarts@gmail.com>
2022-07-04 15:07:24 +0200

    Restore blog-post.js

--- benchmarks/gabe-fs-mdx/src/templates/blog-post.js
+++ benchmarks/gabe-fs-mdx/src/templates/blog-post.js
@@ -2,7 +2,7 @@ import React from "react"
 import { graphql, Link } from "gatsby"
 import Layout from "../components/layout_1"
 
-const Article = ({ children, pageContext, data }) => {
+const Article = ({ children, pageContext }) => {
   return (
     <Layout>
       <Link to="/">Go back to index page</Link>
