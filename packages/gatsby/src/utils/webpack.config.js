495efa59255253ae542b3a747597316f08d604aa
Jude Agboola <marvinjudehk@gmail.com>
2022-07-11 14:04:33 +0100

    fix static image issue

--- packages/gatsby/src/utils/webpack.config.js
+++ packages/gatsby/src/utils/webpack.config.js
@@ -367,30 +367,6 @@ module.exports = async (
           },
         },
       },
-      rules.js({
-        modulesThatUseGatsby,
-      }),
-      rules.yaml(),
-      rules.fonts(),
-      rules.images(),
-      rules.media(),
-      rules.miscAssets(),
-
-      // This is a hack that exports one of @reach/router internals (BaseContext)
-      // to export list. We need it to reset basepath and baseuri context after
-      // Gatsby main router changes it, to keep v2 behaviour.
-      // We will need to most likely remove this for v3.
-      {
-        test: require.resolve(`@gatsbyjs/reach-router/es/index`),
-        type: `javascript/auto`,
-        use: [
-          {
-            loader: require.resolve(
-              `./reach-router-add-basecontext-export-loader`
-            ),
-          },
-        ],
-      },
       {
         resourceQuery: /export=default/,
         use: {
@@ -415,6 +391,31 @@ module.exports = async (
           },
         },
       },
+      rules.js({
+        modulesThatUseGatsby,
+      }),
+      rules.yaml(),
+      rules.fonts(),
+      rules.images(),
+      rules.media(),
+      rules.miscAssets(),
+
+      // This is a hack that exports one of @reach/router internals (BaseContext)
+      // to export list. We need it to reset basepath and baseuri context after
+      // Gatsby main router changes it, to keep v2 behaviour.
+      // We will need to most likely remove this for v3.
+      {
+        test: require.resolve(`@gatsbyjs/reach-router/es/index`),
+        type: `javascript/auto`,
+        use: [
+          {
+            loader: require.resolve(
+              `./reach-router-add-basecontext-export-loader`
+            ),
+          },
+        ],
+      },
+
     ]
 
     // Speedup 🏎️💨 the build! We only include transpilation of node_modules on javascript production builds
