0c39ff4f320292d7a8215b5fc0626876f178f01a
LekoArts <lekoarts@gmail.com>
2022-07-15 10:41:24 +0200

    integration-test

--- integration-tests/head-function-export/shared-data/head-function-export.js
+++ integration-tests/head-function-export/shared-data/head-function-export.js
@@ -17,6 +17,7 @@ const data = {
     noscript: `You take romance - I will take Jell-O!`,
     style: `rebeccapurple`,
     link: `/used-by-head-function-export-basic.css`,
+    jsonLD: `{"@context":"https://schema.org","@type":"Organization","url":"https://www.spookytech.com","name":"Spookytechnologies","contactPoint":{"@type":"ContactPoint","telephone":"+5-601-785-8543","contactType":"CustomerSupport"}}`
   },
   queried: {
     base: `http://localhost:8000`,
