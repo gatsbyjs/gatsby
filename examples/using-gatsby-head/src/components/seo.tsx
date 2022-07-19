5e56fc54569ac7dcab99849158799dc0c3780b8b
LekoArts <lekoarts@gmail.com>
2022-07-19 09:08:05 +0200

    update example

--- examples/using-gatsby-head/src/components/seo.tsx
+++ examples/using-gatsby-head/src/components/seo.tsx
@@ -29,7 +29,7 @@ export const SEO: React.FC<React.PropsWithChildren<SEOProps>> = ({ title, descri
       <meta name="twitter:description" content={seo.description} />
       <meta name="twitter:image" content={seo.image} />
       <meta name="twitter:creator" content={seo.twitterUsername} />
-      <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>👤</text></svg>" />
+      <link id="favicon-icon" rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>👤</text></svg>" />
       {children}
     </>
   )
--- examples/using-gatsby-head/src/pages/parks/{Park.name}.tsx
+++ examples/using-gatsby-head/src/pages/parks/{Park.name}.tsx
@@ -20,7 +20,7 @@ export default ParkPage
 
 export const Head: HeadFC<QueryReturn> = ({ data: { park }, location }) => (
   <SEO title={park.name} description={park.description} pathname={location.pathname}>
-    <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>🔥</text></svg>" />
+    <link id="favicon-icon" rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>🔥</text></svg>" />
     <script type="application/ld+json">
       {`
 {
