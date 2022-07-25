1de72ed9275170dff964ebe359f0365e8524f275
pieh <misiek.piechowiak@gmail.com>
2022-07-25 15:50:00 +0200

    wait with proxy until serve is up

--- e2e-tests/path-prefix/scripts/serve.js
+++ e2e-tests/path-prefix/scripts/serve.js
@@ -1,11 +1,24 @@
 const http = require(`http`)
 const httpProxy = require("http-proxy")
+const waitOn = require("wait-on")
 
-const proxy = httpProxy.createProxyServer()
-const server = http.createServer((request, response) =>
-  proxy.web(request, response, { target: "http://localhost:9000" })
+waitOn(
+  {
+    resources: [`http://localhost:9000/blog/`],
+  },
+  function (err) {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+
+    const proxy = httpProxy.createProxyServer()
+    const server = http.createServer((request, response) =>
+      proxy.web(request, response, { target: "http://localhost:9000" })
+    )
+
+    server.listen(9001, () => {
+      console.log(`Assets server running at http://localhost:9001`)
+    })
+  }
 )
-
-server.listen(9001, () => {
-  console.log(`Running at http://localhost:9001`)
-})
