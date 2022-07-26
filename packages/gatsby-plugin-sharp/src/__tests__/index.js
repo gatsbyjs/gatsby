9dc8d491713ea00052da6cf6cf6f2f9285c181c1
LekoArts <lekoarts@gmail.com>
2022-07-26 11:13:44 +0200

    add test

--- packages/gatsby-plugin-sharp/src/__tests__/index.js
+++ packages/gatsby-plugin-sharp/src/__tests__/index.js
@@ -549,6 +549,18 @@ describe(`gatsby-plugin-sharp`, () => {
         }
       `)
     })
+
+    it(`handles really wide aspect ratios for blurred placeholder`, async () => {
+      const result = await base64({
+        file: getFileObject(
+          path.join(__dirname, `images/wide-aspect-ratio.png`)
+        ),
+        args,
+      })
+
+      expect(result.width).toEqual(20)
+      expect(result.height).toEqual(1)
+    })
   })
 
   describe(`tracedSVG`, () => {
