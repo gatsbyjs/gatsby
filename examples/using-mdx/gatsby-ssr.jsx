bb56c2366b85a1da12056271eb74499c2feb6e35
LekoArts <lekoarts@gmail.com>
2022-07-27 07:02:41 +0200

    update

--- examples/using-mdx/gatsby-ssr.jsx
+++ examples/using-mdx/gatsby-ssr.jsx
@@ -1,1 +1,6 @@
-export { wrapPageElement } from "./gatsby-browser"
+import React from "react"
+import { Layout } from "./src/components/layout"
+
+export const wrapPageElement = ({ element, props }) => {
+  return <Layout {...props}>{element}</Layout>
+}
