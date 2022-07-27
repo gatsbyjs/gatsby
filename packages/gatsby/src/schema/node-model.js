1886624fa1fe4000d2db8a42a79ad3958d632c5a
Kyle Mathews <mathews.kyle@gmail.com>
2022-07-27 13:02:39 -0700

    Fix lint

--- packages/gatsby/src/schema/node-model.js
+++ packages/gatsby/src/schema/node-model.js
@@ -858,7 +858,7 @@ function resolveField(
   if (!gqlField?.resolve) {
     return node[fieldName]
   }
-  
+
   // We require this inline as there's a circular dependency from context back to this file.
   // https://github.com/gatsbyjs/gatsby/blob/9d33b107d167e3e9e2aa282924a0c409f6afd5a0/packages/gatsby/src/schema/context.ts#L5
   if (!withResolverContext) {
