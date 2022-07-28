d9ded7ed0adab483cb42233570cfa67c3cd871ba
tyhopp <hopp.ty.c@gmail.com>
2022-07-28 16:00:29 +0800

    Make browser runtime work

--- packages/gatsby/src/internal-plugins/partytown/gatsby-browser.tsx
+++ packages/gatsby/src/internal-plugins/partytown/gatsby-browser.tsx
@@ -1,64 +1,48 @@
-import React, { ReactElement, useState } from "react"
-import type { GatsbySSR } from "gatsby"
-import { Partytown } from "@builder.io/partytown/react"
-import { PartytownContext, ScriptProps } from "gatsby-script"
+import { collectedScriptsByPage } from "gatsby-script"
+import type { GatsbyBrowser } from "gatsby"
+import type { ScriptProps } from "gatsby-script"
+import { partytownSnippet } from "@builder.io/partytown/integration"
 
-interface ICollectedForwardsState {
-  collectedForwards: Set<string>
-  collectedAnyScript: boolean
-}
-
-function PartytownProvider({ children }): ReactElement {
-  const [{ collectedForwards, collectedAnyScript }, setState] =
-    useState<ICollectedForwardsState>({
-      collectedForwards: new Set(),
-      collectedAnyScript: false,
-    })
-
-  return (
-    <PartytownContext.Provider
-      value={{
-        collectScript: (newScript: ScriptProps): void => {
-          let stateShouldChange = false
-          const potentialNewState = {
-            collectedAnyScript,
-            collectedForwards,
-          }
-
-          if (!collectedAnyScript) {
-            potentialNewState.collectedAnyScript = true
-            stateShouldChange = true
-          }
-
-          if (newScript?.forward) {
-            if (Array.isArray(newScript.forward)) {
-              for (const singleForward of newScript.forward) {
-                if (!potentialNewState.collectedForwards.has(singleForward)) {
-                  potentialNewState.collectedForwards.add(singleForward)
-                  stateShouldChange = true
-                }
-              }
-            } else {
-              if (process.env.NODE_ENV === `development`) {
-                console.log(`unexpected shape of forward`, newScript)
-              }
-            }
-          }
-
-          if (stateShouldChange) {
-            setState(potentialNewState)
-          }
-        },
-      }}
-    >
-      {children}
-      {collectedAnyScript && (
-        <Partytown key="partytown" forward={Array.from(collectedForwards)} />
-      )}
-    </PartytownContext.Provider>
+function getForwards(pathname: string): Array<string> {
+  const collectedScripts = collectedScriptsByPage.get(pathname)
+  const forwards = collectedScripts?.flatMap(
+    (script: ScriptProps) => script?.forward || []
   )
+  return forwards
 }
 
-export const wrapRootElement: GatsbySSR[`wrapRootElement`] = ({ element }) => (
-  <PartytownProvider>{element}</PartytownProvider>
-)
+function injectPartytownSnippet(pathname: string): void {
+  const existingSnippet = document.querySelector(`[data-partytown]`)
+
+  if (existingSnippet) {
+    existingSnippet.remove()
+  }
+
+  const forwards = getForwards(pathname)
+
+  const snippet = document.createElement(`script`)
+  snippet.dataset.partytown = ``
+  snippet.innerHTML = partytownSnippet({ forward: forwards })
+  document.head.appendChild(snippet)
+}
+
+export const onInitialClientRender: GatsbyBrowser[`onInitialClientRender`] =
+  (): void => {
+    injectPartytownSnippet(window.location.pathname)
+    collectedScriptsByPage.delete(window.location.pathname)
+  }
+
+export const onRouteUpdate: GatsbyBrowser[`onRouteUpdate`] = ({
+  prevLocation,
+  location,
+}): void => {
+  if (!prevLocation) {
+    return
+  }
+
+  collectedScriptsByPage.delete(window.location.pathname)
+
+  setTimeout(() => {
+    injectPartytownSnippet(location.pathname)
+  }, 0)
+}
