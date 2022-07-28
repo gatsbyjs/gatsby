4d663a91a76663c45894673ebc00c5768bd25032
Benedikt Rötsch <benedikt@hashbite.net>
2022-07-28 15:21:01 +0200

    test(unit): update test for gatsby-remark-* plugin transform

--- packages/gatsby-plugin-mdx/src/__tests__/get-source-plugins-as-remark-plugins.ts
+++ packages/gatsby-plugin-mdx/src/__tests__/get-source-plugins-as-remark-plugins.ts
@@ -1,3 +1,5 @@
+import { createProcessor } from "@mdx-js/mdx"
+
 import { getSourcePluginsAsRemarkPlugins } from "../get-source-plugins-as-remark-plugins"
 import { mockGatsbyApi } from "../__fixtures__/test-utils"
 
@@ -7,13 +9,49 @@ describe(`transform gatsby-remark plugins into regular plugins`, () => {
   it(`skips on non-functionals`, async () => {
     await expect(
       getSourcePluginsAsRemarkPlugins({
-        gatsbyRemarkPlugins: [],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `mocked`,
+          },
+        ],
+
         getNode,
         getNodesByType,
         reporter,
         cache,
         pathPrefix,
       })
-    ).resolves.toMatchInlineSnapshot()
+    ).resolves.toHaveLength(0)
   })
-})
+  it(`transforms source functions into regular plugins`, async () => {
+    const module = jest.fn()
+    const plugins = await getSourcePluginsAsRemarkPlugins({
+      gatsbyRemarkPlugins: [
+        {
+          resolve: `mocked`,
+          module,
+        },
+      ],
+
+      getNode,
+      getNodesByType,
+      reporter,
+      cache,
+      pathPrefix,
+    })
+    expect(plugins).toHaveLength(1)
+    if (plugins === undefined) {
+      throw new Error(`Returned plugins should not be undefined`)
+    }
+    if (typeof plugins[0] !== `function`) {
+      throw new Error(`Returned plugin should a function`)
+    }
+    const remarkPlugin = plugins[0]
+    const processor = createProcessor({
+      remarkPlugins: [remarkPlugin],
+    })
+
+    await processor.process(``)
+
+    expect(module).toHaveBeenCalled()
+  })
