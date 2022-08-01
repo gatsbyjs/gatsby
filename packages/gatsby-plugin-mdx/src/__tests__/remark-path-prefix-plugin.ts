b3079e0ad588ca1b27ddaec3db85a2a901534a85
Benedikt Rötsch <benedikt@hashbite.net>
2022-08-01 15:45:36 +0200

    test(unit): rewrite path prefix test to avoid remark dependency

--- packages/gatsby-plugin-mdx/src/__tests__/remark-path-prefix-plugin.ts
+++ packages/gatsby-plugin-mdx/src/__tests__/remark-path-prefix-plugin.ts
@@ -1,25 +1,39 @@
-import remark from "remark"
+import { createProcessor } from "@mdx-js/mdx"
 
 import { remarkPathPlugin } from "../remark-path-prefix-plugin"
 
 describe(`remark: path prefix`, () => {
   it(`do not touch with empty prefix`, async () => {
-    const processor = remark().use(remarkPathPlugin, {
-      pathPrefix: ``,
+    const processor = createProcessor({
+      remarkPlugins: [
+        [
+          remarkPathPlugin,
+          {
+            pathPrefix: ``,
+          },
+        ],
+      ],
     })
 
     const res = await processor.process(`[Some Link](/some-page/)`)
 
-    expect(res.contents).toBe(`[Some Link](/some-page/)\n`)
+    expect(res.value).toContain(`href: "/some-page/"`)
   })
 
   it(`attach prefix`, async () => {
-    const processor = remark().use(remarkPathPlugin, {
-      pathPrefix: `/some/prefix`,
+    const processor = createProcessor({
+      remarkPlugins: [
+        [
+          remarkPathPlugin,
+          {
+            pathPrefix: `/some/prefix`,
+          },
+        ],
+      ],
     })
 
     const res = await processor.process(`[Some Link](/some-page/)`)
 
-    expect(res.contents).toBe(`[Some Link](/some/prefix/some-page/)\n`)
+    expect(res.value).toContain(`href: "/some/prefix/some-page/"`)
   })
 })
