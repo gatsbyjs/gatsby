c7445a94e2c1d2ad691bd757ef9817ec3debcef8
Lennart <lekoarts@gmail.com>
2022-08-02 13:17:55 +0200

    typo

--- starters/gatsby-starter-minimal-ts/src/pages/index.tsx
+++ starters/gatsby-starter-minimal-ts/src/pages/index.tsx
@@ -139,7 +139,6 @@ const links = [
 const IndexPage = () => {
   return (
     <main style={pageStyles}>
-      <title>Home Page</title>
       <h1 style={headingStyles}>
         Congratulations
         <br />
