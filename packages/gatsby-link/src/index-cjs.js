c43dc0f4362dfe9be466b003e5a9e47b9ad75ccd
pieh <misiek.piechowiak@gmail.com>
2022-08-04 11:31:34 +0200

    make it work (hopefully)

--- packages/gatsby-link/src/index-cjs.js
+++ packages/gatsby-link/src/index-cjs.js
@@ -5,5 +5,6 @@ module.exports = Link.default
 Object.getOwnPropertyNames(Link).forEach(key => {
   Object.defineProperty(module.exports, key, {
     value: Link[key],
+    enumerable: true,
   })
 })
