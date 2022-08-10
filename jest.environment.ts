8484d9b84594c881642181c150191f6558907cdf
Michal Piechowiak <misiek.piechowiak@gmail.com>
2022-08-10 11:52:47 +0200

    2

--- jest.environment.ts
+++ jest.environment.ts
@@ -1,30 +1,20 @@
-/*
-  Jest is resetting `global` between test suites. This makes our global maps that track opened
-  dbs unusable. This custom environment is really just regular Node environment, but it does setup
-  global available in tests with SHARED maps within same process (different processes will use different
-  dbs, so that part is taken care of)
-*/
 const NodeEnvironment = require(`jest-environment-node`)
-
-const GlobalsToPreserve = [`__GATSBY_OPEN_ROOT_LMDBS`, `__GATSBY_OPEN_LMDBS`]
-
-const preservedGlobals = new Map<string, unknown>()
 
 class CustomEnvironment extends NodeEnvironment {
   constructor(config, context) {
     super(config, context)
   }
 
-  async setup(): Promise<void> {
-    await super.setup()
-    for (const globalToCheck of GlobalsToPreserve) {
-      this.global[globalToCheck] = preservedGlobals.get(globalToCheck)
-    }
-  }
-
   async teardown(): Promise<void> {
-    for (const globalToCheck of GlobalsToPreserve) {
-      preservedGlobals.set(globalToCheck, this.global[globalToCheck])
+    // close open lmdbs after running test suite
+    // this prevent dangling open handles that sometimes cause problems
+    // particularly in windows tests (failures to move or delete a db file)
+    if (this.global.__GATSBY_OPEN_ROOT_LMDBS) {
+      for (const rootDb of this.global.__GATSBY_OPEN_ROOT_LMDBS.values()) {
+        await rootDb.clearAsync()
+        await rootDb.close()
+      }
+      this.global.__GATSBY_OPEN_ROOT_LMDBS = undefined
     }
     await super.teardown()
   }
