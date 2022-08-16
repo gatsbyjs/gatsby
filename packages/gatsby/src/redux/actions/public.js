d59e7b61b2c63827fba02c5d9ea96c614cc83344
Francesco Agnoletto <Francesco.agnoletto92@gmail.com>
2022-08-16 19:42:48 +0200

    chore(gatsby): convert sanitize-node to typescript (#36327)
    * Convert sanitize-node
    
    * Convert sanitize-node test
    
    * Change to trigger build
    
    * Change to trigger build
    
    * Change to trigger build
    
    Co-authored-by: Josh Johnson <jcjohnson77@gmail.com>

--- packages/gatsby/src/redux/actions/public.js
+++ packages/gatsby/src/redux/actions/public.js
@@ -15,7 +15,7 @@ const {
 const { splitComponentPath } = require(`gatsby-core-utils/parse-component-path`)
 const { hasNodeChanged } = require(`../../utils/nodes`)
 const { getNode, getDataStore } = require(`../../datastore`)
-const sanitizeNode = require(`../../utils/sanitize-node`)
+import sanitizeNode from "../../utils/sanitize-node"
 const { store } = require(`../index`)
 const { validatePageComponent } = require(`../../utils/validate-page-component`)
 import { nodeSchema } from "../../joi-schemas/joi"
