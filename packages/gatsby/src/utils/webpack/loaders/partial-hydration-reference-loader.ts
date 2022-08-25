7f64f5280140b6685585253d1e647b0b7a7a77fc
tyhopp <hopp.ty.c@gmail.com>
2022-08-25 17:32:23 +0800

    feat(gatsby): Create client references via AST matching

--- packages/gatsby/src/utils/webpack/loaders/partial-hydration-reference-loader.ts
+++ packages/gatsby/src/utils/webpack/loaders/partial-hydration-reference-loader.ts
@@ -2,44 +2,86 @@
 
 import { LoaderDefinitionFunction } from "webpack"
 import url from "url"
+import { parse } from "acorn"
+import { simple as walk } from "acorn-walk"
+import type { ExportNamedDeclaration } from "estree"
+
+function createNewReference(name: string, moduleId: string): string {
+  return `export const ${name} = {
+    $$typeof: Symbol.for('react.module.reference'),
+    filepath: '${moduleId}',
+    name: '${name}'
+  }`
+}
 
 const partialHydrationReferenceLoader: LoaderDefinitionFunction<
   Record<string, unknown>
 > = async function partialHydrationReferenceLoader(content) {
-  // TODO make this check more robust, using acorn ast check
-  if (!content.includes(`"client export"`)) {
+  if (!content.includes(`client export`)) {
     return content
   }
+
+  const references: Array<string> = []
 
   const moduleId = url
     .pathToFileURL(this.resourcePath)
     .href.replace(this.rootContext.replace(/\\/g, `/`), ``)
     .replace(`file:////`, `file://`)
 
-  const source: Array<string> = []
-  // capture  CJS exports
-  const cjsExports = content.matchAll(/exports\.([^ ]+)/g)
-  for (const match of cjsExports) {
-    source.push(`export const ${match[1]} = {
-      $$typeof: MODULE_REFERENCE,
-      filepath: '${moduleId}',
-      name: '${match[1]}'
-    }`)
-  }
+  walk(parse(content, { ecmaVersion: 2020, sourceType: `module` }), {
+    // @ts-ignore Types are mostly right, except for a few cases. Keep for documentation.
+    ExportNamedDeclaration(node: ExportNamedDeclaration) {
+      if (!node.declaration) {
+        return
+      }
 
-  // capture ESM exports
-  const esmExports = content.matchAll(
-    /export (const|let|var|function) ([^ ]+)/g
-  )
-  for (const match of esmExports) {
-    source.push(`export const ${match[2]} = {
-      $$typeof: Symbol.for('react.module.reference'),
-      filepath: '${moduleId}',
-      name: '${match[2]}'
-    }`)
-  }
+      switch (node.declaration?.type) {
+        case `VariableDeclaration`:
+          for (const { id } of node.declaration.declarations || []) {
+            if (id.type === `Identifier` && id.name) {
+              references.push(createNewReference(id.name, moduleId))
+            }
 
-  return source.join(`\n`)
+            if (id.type === `ObjectPattern`) {
+              // @ts-ignore Wrong type
+              for (const { value } of id.properties) {
+                if (value.type === `Identifier` && value.name) {
+                  references.push(createNewReference(value.name, moduleId))
+                }
+              }
+            }
+
+            if (id.type === `ArrayPattern`) {
+              for (const element of id.elements || []) {
+                if (element?.type === `Identifier` && element.name) {
+                  references.push(createNewReference(element.name, moduleId))
+                }
+              }
+            }
+          }
+          break
+        case `FunctionDeclaration`:
+        case `ClassDeclaration`:
+          if (
+            node.declaration.id?.type === `Identifier` &&
+            node.declaration.id.name
+          ) {
+            references.push(
+              createNewReference(node.declaration.id.name, moduleId)
+            )
+          }
+          break
+      }
+    },
+  })
+
+  // TODO: Handle ESM export list
+  // TODO: Handle ESM export default
+  // TODO: Handle ESM export module aggregation
+  // TODO: Handle CJS export list
+  // TODO: Handle CJS export default
+
+  return references.join(`\n`)
 }
 
 module.exports = partialHydrationReferenceLoader
