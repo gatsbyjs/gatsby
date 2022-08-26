6d2125b5300ad5345e9b20b6f0765f82341d185c
tyhopp <hopp.ty.c@gmail.com>
2022-08-26 10:42:09 +0800

    Add placeholders for other ESM export syntax

---
+++ packages/gatsby/src/utils/webpack/loaders/__tests__/fixtures/esm-aggregation.js
@@ -0,0 +1,9 @@
+"client export"
+
+// TODO: Handle, otherwise remove and document if we decide not to support
+
+// export * from "./some-module"
+// export * as a from "some-module"
+// export { b, c } from "some-module"
+// export { d as e, f as g, h } from "some-module"
+// export { default } from "some-module"
