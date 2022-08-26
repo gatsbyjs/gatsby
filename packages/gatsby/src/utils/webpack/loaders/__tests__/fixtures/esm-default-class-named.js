919b35edd5f72c634e9a629ceeae325cfea1e810
tyhopp <hopp.ty.c@gmail.com>
2022-08-26 10:42:09 +0800

    Add placeholders for other ESM export syntax

---
+++ packages/gatsby/src/utils/webpack/loaders/__tests__/fixtures/esm-default-class-named.js
@@ -0,0 +1,5 @@
+"client export"
+
+// TODO: Handle, otherwise remove and document if we decide not to support
+
+// export default class ClassName {}
---
+++ packages/gatsby/src/utils/webpack/loaders/__tests__/fixtures/esm-default-expression.js
@@ -0,0 +1,7 @@
+"client export"
+
+// TODO: Handle, otherwise remove and document if we decide not to support
+
+// const a = 1
+
+// export default a
