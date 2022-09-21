eefd0b4817380f37dcea94499457d9ce0d9aabc0
tyhopp <hopp.ty.c@gmail.com>
2022-09-21 12:45:21 +0800

    Add tests

---
+++ packages/gatsby/src/utils/webpack/utils/__tests__/create-normalized-module-key.ts
@@ -0,0 +1,30 @@
+import path from "path"
+import { createNormalizedModuleKey } from "../create-normalized-module-key"
+
+const projectRoot = path.join(`Users`, `username`, `project`)
+
+it(`should normalize module imports from node_modules`, () => {
+  const nodeModule = path.join(`node_modules`, `package-name`)
+
+  const normalizedModuleKey = createNormalizedModuleKey(
+    path.join(projectRoot, nodeModule, `index.js`),
+    projectRoot
+  )
+
+  expect(normalizedModuleKey).toEqual(`file://${nodeModule}`)
+})
+
+it(`should normalize local module imports`, () => {
+  const localModuleFile = path.join(
+    projectRoot,
+    `src`,
+    `components`,
+    `index.js`
+  )
+
+  const normalizedModuleKey = createNormalizedModuleKey(
+    localModuleFile,
+    projectRoot
+  )
+  expect(normalizedModuleKey).toEqual(localModuleFile)
+})
