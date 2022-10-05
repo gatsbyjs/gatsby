719bd1cf17119712c26d1fdb6034a3ed4bff66c6
Michal Piechowiak <misiek.piechowiak@gmail.com>
2022-10-05 13:09:29 +0200

    fix(partial-hydration): move client components to separate chunks (#36749)

--- packages/gatsby/src/utils/webpack/plugins/partial-hydration.ts
+++ packages/gatsby/src/utils/webpack/plugins/partial-hydration.ts
@@ -30,6 +30,8 @@ class ClientReferenceDependency extends ModuleDependency {
   }
 }
 
+export const PARTIAL_HYDRATION_CHUNK_REASON = `PartialHydration client module`
+
 /**
  * inspiration and code mostly comes from https://github.com/facebook/react/blob/3f70e68cea8d2ed0f53d35420105ae20e22ce428/packages/react-server-dom-webpack/src/ReactFlightWebpackPlugin.js
  */
@@ -173,12 +175,8 @@ export class PartialHydrationPlugin {
         for (const chunk of chunkGraph.getModuleChunksIterable(
           resolvedModule
         )) {
-          for (const group of chunk.groupsIterable) {
-            for (const chunkInGroup of group.chunks) {
-              if (chunkInGroup.id) {
-                chunkIds.add(chunkInGroup.id as string)
-              }
-            }
+          if (chunk.id) {
+            chunkIds.add(chunk.id as string)
           }
         }
 
@@ -267,7 +265,7 @@ export class PartialHydrationPlugin {
           })
         }
 
-        compilation.hooks.optimizeChunkModules.tap(
+        compilation.hooks.optimizeChunks.tap(
           this.name,
           // eslint-disable-next-line @typescript-eslint/no-unused-vars
           () => {
@@ -283,20 +281,58 @@ export class PartialHydrationPlugin {
                 )
               )
 
-              const selectedChunks = Array.from(
-                compilation.chunkGraph.getModuleChunksIterable(clientModule)
-              )
               const chunk = compilation.addChunk(chunkName)
-              chunk.chunkReason = `PartialHydration client module`
-              compilation.chunkGraph.connectChunkAndModule(chunk, clientModule)
+              chunk.chunkReason = PARTIAL_HYDRATION_CHUNK_REASON
 
-              for (const connectedChunk of selectedChunks) {
-                compilation.chunkGraph.disconnectChunkAndModule(
-                  connectedChunk,
+              const handledModules = new Set()
+
+              function moveModuleToChunk(
+                clientModule: webpack.Module,
+                chunk: webpack.Chunk
+              ): void {
+                if (handledModules.has(clientModule)) {
+                  return
+                }
+
+                handledModules.add(clientModule)
+
+                const selectedChunks = Array.from(
+                  compilation.chunkGraph.getModuleChunksIterable(clientModule)
+                )
+
+                for (const connectedChunk of selectedChunks) {
+                  if (connectedChunk.name === `app`) {
+                    return
+                  }
+                }
+
+                compilation.chunkGraph.connectChunkAndModule(
+                  chunk,
                   clientModule
                 )
-                connectedChunk.split(chunk)
+
+                for (const connectedChunk of selectedChunks) {
+                  compilation.chunkGraph.disconnectChunkAndModule(
+                    connectedChunk,
+                    clientModule
+                  )
+                  connectedChunk.split(chunk)
+                }
+
+                const uniqueDependencyModules = new Set(
+                  clientModule.dependencies.map(r =>
+                    compilation.moduleGraph.getModule(r)
+                  )
+                )
+
+                for (const dependencyModule of uniqueDependencyModules) {
+                  if (dependencyModule) {
+                    moveModuleToChunk(dependencyModule, chunk)
+                  }
+                }
               }
+
+              moveModuleToChunk(clientModule, chunk)
             }
           }
         )
