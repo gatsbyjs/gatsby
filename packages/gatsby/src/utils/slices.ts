38fde9460d3ac9e263e7679a804a4e57f481715c
Michal Piechowiak <misiek.piechowiak@gmail.com>
2022-10-06 14:42:40 +0200

    Update packages/gatsby/src/utils/slices.ts
    Co-authored-by: Lennart <lekoarts@gmail.com>

--- packages/gatsby/src/utils/slices.ts
+++ packages/gatsby/src/utils/slices.ts
@@ -1,5 +1,5 @@
 import type { IGatsbySlice } from "../redux/types"
-import { createContentDigest } from "gatsby-core-utils"
+import { createContentDigest } from "gatsby-core-utils/create-content-digest"
 
 export function getSliceId(slice: IGatsbySlice): string {
   return `${slice.componentChunkName}-${createContentDigest(slice.context)}`
