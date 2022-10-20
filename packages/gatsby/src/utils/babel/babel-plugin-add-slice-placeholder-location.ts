7f5b83f88dd236ec40e994840e61df7427ec9706
kathmbeck <katherine.murphy.beck@gmail.com>
2022-10-20 14:15:50 -0700

    remove jsx from comment

--- packages/gatsby/src/utils/babel/babel-plugin-add-slice-placeholder-location.ts
+++ packages/gatsby/src/utils/babel/babel-plugin-add-slice-placeholder-location.ts
@@ -4,7 +4,7 @@ import { ObjectProperty } from "@babel/types"
 import { store } from "../../redux"
 
 /**
- * This is a plugin that finds <Slice> placeholder components and injects the __renderedByLocation prop
+ * This is a plugin that finds Slice placeholder components and injects the __renderedByLocation prop
  * with filename and location in the file where the placeholder was found. This is later used to provide
  * more useful error messages when the user props are invalid showing codeframe where user tries to render it
  * instead of codeframe of the Slice component itself (internals of gatsby) that is not useful for the user.
