506e35db3cebbdcd7fb28499f00537db60734adf
pieh <misiek.piechowiak@gmail.com>
2022-10-20 07:18:48 +0200

    update jsdoc description of new babel plugin to match what it's doing instead of description that was copy&paste from StaticImage plugin

--- packages/gatsby/src/utils/babel/babel-plugin-add-slice-placeholder-location.ts
+++ packages/gatsby/src/utils/babel/babel-plugin-add-slice-placeholder-location.ts
@@ -4,8 +4,10 @@ import { ObjectProperty } from "@babel/types"
 import { store } from "../../redux"
 
 /**
- * This is a plugin that finds StaticImage components and injects the image props into the component.
- * These props contain the image URLs etc, and were created earlier in the build process
+ * This is a plugin that finds <Slice> placeholder components and injects the __renderedBylocation prop
+ * with filename and location in the file where the placeholder was found. This is later used to provide
+ * more useful error messages when the user props are invalid showing codeframe where user tries to render it
+ * instead of codeframe of the Slice component itself (internals of gatsby) that is not useful for the user.
  */
 
 export default function addSlicePlaceholderLocation(
