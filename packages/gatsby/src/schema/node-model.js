23eebf558d137f9db48289d1c459801c1ceecba0
LekoArts <lekoarts@gmail.com>
2022-10-21 15:07:18 +0200

    update internals & tests

--- packages/gatsby/src/schema/node-model.js
+++ packages/gatsby/src/schema/node-model.js
@@ -54,14 +54,6 @@ export interface NodeModel {
     { ids: Array<string>, type?: TypeOrTypeName },
     pageDependencies?: PageDependencies
   ): Array<any>;
-  getAllNodes(
-    { type?: TypeOrTypeName },
-    pageDependencies?: PageDependencies
-  ): Array<any>;
-  runQuery(
-    args: QueryArguments,
-    pageDependencies?: PageDependencies
-  ): Promise<any>;
   getTypes(): Array<string>;
   trackPageDependencies<nodeOrNodes: Node | Node[]>(
     result: nodeOrNodes,
@@ -188,71 +180,6 @@ class LocalNodeModel {
     }
 
     return wrapNodes(this.trackPageDependencies(result, pageDependencies))
-  }
-
-  /**
-   * Get all nodes in the store, or all nodes of a specified type. Note that
-   * this adds connectionType tracking by default if type is passed.
-   *
-   * @deprecated Since version 4.0 - Use nodeModel.findAll() instead
-   * @param {Object} args
-   * @param {(string|GraphQLOutputType)} [args.type] Optional type of the nodes
-   * @param {PageDependencies} [pageDependencies]
-   * @returns {Node[]}
-   */
-  getAllNodes(args, pageDependencies = {}) {
-    // TODO(v5): Remove API
-    reportOnce(
-      `nodeModel.getAllNodes() is deprecated. Use nodeModel.findAll() with an empty query instead.`
-    )
-    const { type = `Node` } = args || {}
-
-    let result
-    if (type === `Node`) {
-      result = getNodes()
-    } else {
-      const nodeTypeNames = toNodeTypeNames(this.schema, type)
-      const nodesByType = nodeTypeNames.map(typeName =>
-        getNodesByType(typeName)
-      )
-      const nodes = [].concat(...nodesByType)
-      result = nodes.filter(Boolean)
-    }
-
-    if (result) {
-      result.forEach(node => this.trackInlineObjectsInRootNode(node))
-    }
-
-    if (typeof pageDependencies.connectionType === `undefined`) {
-      pageDependencies.connectionType =
-        typeof type === `string` ? type : type.name
-    }
-
-    return wrapNodes(this.trackPageDependencies(result, pageDependencies))
-  }
-
-  /**
-   * Get nodes of a type matching the specified query.
-   *
-   * @deprecated Since version 4.0 - Use nodeModel.findAll() or nodeModel.findOne() instead
-   * @param {Object} args
-   * @param {Object} args.query Query arguments (`filter` and `sort`)
-   * @param {(string|GraphQLOutputType)} args.type Type
-   * @param {boolean} [args.firstOnly] If true, return only first match
-   * @param {PageDependencies} [pageDependencies]
-   * @returns {Promise<Node[]>}
-   */
-  async runQuery(args, pageDependencies = {}) {
-    // TODO(v5): Remove API
-    reportOnce(
-      `nodeModel.runQuery() is deprecated. Use nodeModel.findAll() or nodeModel.findOne() instead.`
-    )
-    if (args.firstOnly) {
-      return this.findOne(args, pageDependencies)
-    }
-    const { skip, limit, ...query } = args.query
-    const result = await this.findAll({ ...args, query }, pageDependencies)
-    return Array.from(result.entries)
   }
 
   async _query(args) {
@@ -627,20 +554,6 @@ class ContextualNodeModel {
 
   getNodesByIds(args, pageDependencies) {
     return this.nodeModel.getNodesByIds(
-      args,
-      this._getFullDependencies(pageDependencies)
-    )
-  }
-
-  getAllNodes(args, pageDependencies) {
-    return this.nodeModel.getAllNodes(
-      args,
-      this._getFullDependencies(pageDependencies)
-    )
-  }
-
-  runQuery(args, pageDependencies) {
-    return this.nodeModel.runQuery(
       args,
       this._getFullDependencies(pageDependencies)
     )
