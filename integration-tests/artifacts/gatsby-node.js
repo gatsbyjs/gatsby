76dbd41c47776fac3de43410c12e302fba6f6dbc
pieh <misiek.piechowiak@gmail.com>
2022-10-21 13:45:57 +0200

    add base setup for slice testing - shared layout slice + author bio case (no tests yet)

--- integration-tests/artifacts/gatsby-node.js
+++ integration-tests/artifacts/gatsby-node.js
@@ -139,6 +139,53 @@ exports.sourceNodes = ({
     label: `Node created in run ${runNumber}`,
   })
 
+  createNodeHelper(`SliceBlogPost`, {
+    id: "blog-1",
+    authorId: `kylem`,
+    title: "What is Lorem Ipsum?",
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+    slug: "blog-1",
+  })
+
+  createNodeHelper(`SliceBlogPost`, {
+    id: "blog-2",
+    authorId: `joshj`,
+    title: "Why do we use it?",
+    content:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+    slug: "blog-2",
+  })
+
+  createNodeHelper(`SliceBlogPost`, {
+    id: "blog-3",
+    authorId: `joshj`,
+    title: "Where does it come from?",
+    content: `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+    The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.`,
+    slug: "blog-3",
+  })
+
+  createNodeHelper(`SliceBlogPostAuthor`, {
+    name: `Kyle Mathews`,
+    id: `kylem`,
+    summary: `who lives and works in San Francisco building useful things.`,
+    twitter: `kylemathews`,
+  })
+
+  createNodeHelper(`SliceBlogPostAuthor`, {
+    name: `Josh Johnson`,
+    id: `joshj`,
+    summary: `who lives and works in Michigan building neat things.`,
+    twitter: `0xJ05H`,
+  })
+
+  createNodeHelper(`SliceLayoutMetadata`, {
+    id: `slice-metadata`,
+    title: runNumber <= 2 ? `Gatsby Slice Test` : `Gatsby Slice Test (changed)`,
+  })
+
   for (const prevNode of previouslyCreatedNodes.values()) {
     if (!currentlyCreatedNodes.has(prevNode.id)) {
       actions.deleteNode({ node: prevNode })
@@ -204,6 +251,64 @@ exports.createPages = async ({ actions, graphql }) => {
           : require.resolve(`./src/templates/deps-page-query`),
       context: {
         id: depPageQueryNode.id,
+      },
+    })
+  }
+
+  actions.createSlice({
+    id: `layout`,
+    component: require.resolve(`./src/components/slices-layout`),
+  })
+
+  const authorResults = await graphql(
+    `
+      {
+        allSliceBlogPostAuthor {
+          nodes {
+            id
+          }
+        }
+      }
+    `
+  )
+
+  for (const author of authorResults.data.allSliceBlogPostAuthor.nodes) {
+    actions.createSlice({
+      id: `bio--${author.id}`,
+      component: require.resolve(`./src/components/slices-bio.js`),
+      context: {
+        id: author.id,
+      },
+    })
+  }
+
+  const blogResults = await graphql(
+    `
+      {
+        allSliceBlogPost {
+          nodes {
+            id
+            slug
+            authorId
+          }
+        }
+      }
+    `
+  )
+
+  for (const post of blogResults.data.allSliceBlogPost.nodes) {
+    actions.createPage({
+      path: `/slices/${post.slug}`,
+      component: require.resolve(
+        `./src/templates/page-using-slices-details.js`
+      ),
+      context: {
+        id: post.id,
+      },
+      slices: {
+        // Instruct this blog page to use the matching bio slice
+        // Any time the "bio" alias is seen, it'll use the "bio--${authorId}" slice
+        bio: `bio--${post.authorId}`,
       },
     })
   }
