dfb134cc81923453c37a1076ad5c783f678e0799
LekoArts <lekoarts@gmail.com>
2022-11-04 10:27:49 +0100

    hello-world

--- starters/blog/gatsby-config.js
+++ starters/blog/gatsby-config.js
@@ -1,4 +1,10 @@
 /**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
+
+/**
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
