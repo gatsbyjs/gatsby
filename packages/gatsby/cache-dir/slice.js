b109aa28247df85ee27520031b9d7b28db4c3f84
Michal Piechowiak <misiek.piechowiak@gmail.com>
2022-11-07 13:33:11 +0100

    fix: drop `__renderedByLocation` prop when calculating slice props hashes and don't expose it to slice component

--- packages/gatsby/cache-dir/slice.js
+++ packages/gatsby/cache-dir/slice.js
@@ -13,6 +13,7 @@ export function Slice(props) {
       sliceName: props.alias,
     }
     delete internalProps.alias
+    delete internalProps.__renderedByLocation
 
     const slicesContext = useContext(SlicesContext)
 
