16d91f3aa507c0f20af3ab797c90b4fb5011cf0f
Lennart <lekoarts@gmail.com>
2022-11-08 10:30:07 +0100

    chore: Update starters to Gatsby 5 (#36963)

--- starters/hello-world/gatsby-config.js
+++ starters/hello-world/gatsby-config.js
@@ -1,10 +1,12 @@
 /**
  * Configure your Gatsby site with this file.
  *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
  */
 
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
 module.exports = {
-  /* Your site config here */
   plugins: [],
 }
