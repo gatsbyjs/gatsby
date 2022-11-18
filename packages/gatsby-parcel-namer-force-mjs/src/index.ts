b0ca8158d5b7bbe946ab1569a7887975fe72d1a6
LekoArts <lekoarts@gmail.com>
2022-11-18 13:16:18 +0100

    fix typo
    Co-authored-by: pieh <misiek.piechowiak@gmail.com>

--- packages/gatsby-parcel-namer-force-mjs/src/index.ts
+++ packages/gatsby-parcel-namer-force-mjs/src/index.ts
@@ -1,7 +1,6 @@
 import { Namer } from "@parcel/plugin"
 import type { FilePath, Namer as NamerOpts } from "@parcel/types"
-// @ts-ignore - No types available
-import gatsbyParcelNamerRelativeToCwd from "@gatsbyjs/gatsby-parcel-namer-relative-to-cwd"
+import gatsbyParcelNamerRelativeToCwd from "@gatsbyjs/parcel-namer-relative-to-cwd"
 import * as path from "path"
 
 const CONFIG = Symbol.for(`parcel-plugin-config`)
