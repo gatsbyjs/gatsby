7a7a139ebba8075e5838643c219ee8fe6e2615b9
LekoArts <lekoarts@gmail.com>
2022-12-05 10:28:19 +0100

    update more packages and fix linting issues

--- packages/gatsby-source-wordpress/src/steps/source-nodes/create-nodes/create-local-file-node.js
+++ packages/gatsby-source-wordpress/src/steps/source-nodes/create-nodes/create-local-file-node.js
@@ -233,6 +233,7 @@ export const createLocalFileNode = async ({
   mediaItemUrl = ensureSrcHasHostname({ wpUrl, src: mediaItemUrl })
 
   const { excludeByMimeTypes, maxFileSizeBytes } =
+    // eslint-disable-next-line no-unsafe-optional-chaining
     pluginOptions.type?.MediaItem?.localFile
 
   // if this file is larger than maxFileSizeBytes, don't fetch the remote file
