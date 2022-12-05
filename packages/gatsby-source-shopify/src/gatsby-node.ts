fbbfe236a5011a8703aae9c5255d3df82d145bbc
LekoArts <lekoarts@gmail.com>
2022-12-05 09:25:20 +0100

    initial

--- packages/gatsby-source-shopify/src/gatsby-node.ts
+++ packages/gatsby-source-shopify/src/gatsby-node.ts
@@ -1,5 +1,4 @@
-import { NodePluginArgs } from "gatsby"
-
+import type { GatsbyNode } from "gatsby"
 import { ERROR_MAP } from "./error-map"
 
 export { sourceNodes } from "./source-nodes"
@@ -7,28 +6,8 @@ export { createResolvers } from "./create-resolvers"
 export { pluginOptionsSchema } from "./plugin-options-schema"
 export { createSchemaCustomization } from "./create-schema-customization"
 
-const initializePlugin = ({ reporter }: NodePluginArgs): void => {
+export const onPluginInit: GatsbyNode["onPluginInit"] = ({
+  reporter,
+}): void => {
   reporter.setErrorMap(ERROR_MAP)
-}
-
-let coreSupportsOnPluginInit: `unstable` | `stable` | undefined
-
-try {
-  const { isGatsbyNodeLifecycleSupported } = require(`gatsby-plugin-utils`)
-  if (isGatsbyNodeLifecycleSupported(`onPluginInit`)) {
-    coreSupportsOnPluginInit = `stable`
-  } else if (isGatsbyNodeLifecycleSupported(`unstable_onPluginInit`)) {
-    coreSupportsOnPluginInit = `unstable`
-  }
-} catch (e) {
-  console.error(`Could not check if Gatsby supports onPluginInit lifecycle`)
-}
-
-if (coreSupportsOnPluginInit === `unstable`) {
-  // need to conditionally export otherwise it throws an error for older versions
-  exports.unstable_onPluginInit = initializePlugin
-} else if (coreSupportsOnPluginInit === `stable`) {
-  exports.onPluginInit = initializePlugin
-} else {
-  exports.onPreInit = initializePlugin
 }
