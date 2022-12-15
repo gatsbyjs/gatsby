79c2884bae87c600fccbcb442f99220b555730ad
Lennart <lekoarts@gmail.com>
2022-12-15 08:37:35 +0100

    fix(gatsby-transformer-remark): Disallow JS frontmatter by default (#37244)
    (cherry picked from commit 77b8ccdf764790d027c75b6f62bab15a30e30a2b)

--- packages/gatsby-transformer-remark/src/__tests__/gatsby-node.js
+++ packages/gatsby-transformer-remark/src/__tests__/gatsby-node.js
@@ -7,32 +7,43 @@ describe(`gatsby-node.js`, () => {
       `"footnotes" must be a boolean`,
       `"gfm" must be a boolean`,
       `"plugins" must be an array`,
+      `"jsFrontmatterEngine" must be a boolean`,
     ]
 
-    const { errors } = await testPluginOptionsSchema(pluginOptionsSchema, {
-      footnotes: `this should be a boolean`,
-      gfm: `this should be a boolean`,
-      plugins: `this should be an array`,
-    })
+    const { errors, isValid } = await testPluginOptionsSchema(
+      pluginOptionsSchema,
+      {
+        footnotes: `this should be a boolean`,
+        gfm: `this should be a boolean`,
+        plugins: `this should be an array`,
+        jsFrontmatterEngine: `this should be a boolean`,
+      }
+    )
 
+    expect(isValid).toBe(false)
     expect(errors).toEqual(expectedErrors)
   })
 
   it(`should validate the schema`, async () => {
-    const { isValid } = await testPluginOptionsSchema(pluginOptionsSchema, {
-      footnotes: false,
-      gfm: false,
-      plugins: [
-        `gatsby-remark-copy-linked-files`,
-        {
-          resolve: `gatsby-remark-images`,
-          options: {
-            maxWidth: 756,
+    const { isValid, errors } = await testPluginOptionsSchema(
+      pluginOptionsSchema,
+      {
+        footnotes: false,
+        gfm: false,
+        plugins: [
+          `gatsby-remark-copy-linked-files`,
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 756,
+            },
           },
-        },
-      ],
-    })
+        ],
+        jsFrontmatterEngine: true,
+      }
+    )
 
     expect(isValid).toBe(true)
+    expect(errors).toEqual([])
   })
 })
