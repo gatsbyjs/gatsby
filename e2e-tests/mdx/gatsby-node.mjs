ecaac51d8ae59eb694dd68c3010348f3d099c7c8
LekoArts <lekoarts@gmail.com>
2023-01-03 14:45:33 +0100

    try more fixes

--- e2e-tests/mdx/gatsby-node.mjs
+++ e2e-tests/mdx/gatsby-node.mjs
@@ -1,9 +1,8 @@
-const fs = require(`fs-extra`)
-const path = require(`path`)
+import * as fs from "fs-extra"
+import * as path from "path"
+import slugify from "@sindresorhus/slugify"
 
-const slugify = require(`@sindresorhus/slugify`)
-
-exports.onPostBuild = async ({ graphql }) => {
+export const onPostBuild = async ({ graphql }) => {
   const results = await graphql(`
     {
       mdx(slug: { eq: "html" }) {
@@ -19,7 +18,7 @@ exports.onPostBuild = async ({ graphql }) => {
   )
 }
 
-exports.createSchemaCustomization = ({ actions }) => {
+export const createSchemaCustomization = ({ actions }) => {
   const { createTypes } = actions
 
   createTypes(`
@@ -31,8 +30,9 @@ exports.createSchemaCustomization = ({ actions }) => {
     }
   `)
 }
+
 // Add custom fields to our MDX nodes
-exports.onCreateNode = async ({ node, actions, getNode, reporter, cache }) => {
+export const onCreateNode = async ({ node, actions, getNode, reporter, cache }) => {
   const { createNodeField } = actions
   if (node.internal.type === `Mdx`) {
     // Set slug based on frontmatter, fall back to filename
