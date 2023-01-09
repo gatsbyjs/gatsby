29f1656c915b846b22135c4afab3874a59f3421d
Tyler Barnes <tylerdbarnes@gmail.com>
2023-01-09 15:29:30 -0800

    add normalization code for langCode as def

--- packages/gatsby-source-drupal/src/plugin-options.ts
+++ packages/gatsby-source-drupal/src/plugin-options.ts
@@ -19,18 +19,24 @@ type Options = {
 }
 
 const mutateOptions = (options: Options) => {
-  // Support renamed language codes in Drupal
-  options?.languageConfig?.enabledLanguages?.forEach?.((lang, index) => {
-    if (typeof lang === `object`) {
-      // move the as langcode of the complex code to the enabled languages array
-      options!.languageConfig!.enabledLanguages!.push(lang.as)
-      // then move the complex lang-code to a different array
-      options!.languageConfig!.complexEnabledLanguages ||= []
-      options!.languageConfig!.complexEnabledLanguages.push(lang)
-      // and remove it from enabledLanguages
-      delete options!.languageConfig!.enabledLanguages![index]
-    }
-  })
+  if (options?.languageConfig?.enabledLanguages?.length) {
+    // Support renamed language codes in Drupal
+    options.languageConfig.enabledLanguages.forEach(lang => {
+      if (typeof lang === `object`) {
+        // move the as langcode of the complex code to the enabled languages array
+        options!.languageConfig!.enabledLanguages!.push(lang.as)
+        // then move the complex lang-code to a different array
+        options!.languageConfig!.complexEnabledLanguages ||= []
+        options!.languageConfig!.complexEnabledLanguages.push(lang)
+      }
+    })
+
+    // since we moved all the object enabled languages to a new array, we can remove them from enabledLanguages
+    options.languageConfig.enabledLanguages =
+      options.languageConfig.enabledLanguages.filter(
+        lang => typeof lang === `string`
+      )
+  }
 
   return options
 }
