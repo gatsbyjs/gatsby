97626421f4adf0b993de53471d5927a647907a00
Lennart <lekoarts@gmail.com>
2023-01-11 13:28:28 +0100

    fix(gatsby): Support ESM-only `gatsby-remark-*` plugins (#37440)

---
+++ integration-tests/esm-in-gatsby-files/__tests__/fixtures/plugins/gatsby-remark-cjs/index.js
@@ -0,0 +1,5 @@
+console.info(`gatsby-remark-cjs-gatsby-config-js`)
+
+module.exports = ({ markdownAST }, pluginOptions) => {
+  return markdownAST
+}
---
+++ integration-tests/esm-in-gatsby-files/__tests__/fixtures/plugins/gatsby-remark-esm/index.js
@@ -0,0 +1,5 @@
+console.info(`gatsby-remark-esm-gatsby-config-mjs`)
+
+export default ({ markdownAST }, pluginOptions) => {
+  return markdownAST
+}
