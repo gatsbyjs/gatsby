7b42d6a5d80afd5704cc7f2ea09f067fa4a64fb4
LekoArts <lekoarts@gmail.com>
2023-01-11 16:18:57 +0100

    use importGatsbyPlugin util

--- packages/gatsby/src/bootstrap/load-plugins/validate.ts
+++ packages/gatsby/src/bootstrap/load-plugins/validate.ts
@@ -20,6 +20,7 @@ import {
 } from "./types"
 import { resolvePlugin } from "./resolve-plugin"
 import { preferDefault } from "../prefer-default"
+import { importGatsbyPlugin } from "../../utils/import-gatsby-plugin"
 
 interface IApi {
   version?: string
@@ -213,8 +214,7 @@ async function validatePluginsOptions(
       let gatsbyNode
       try {
         const resolvedPlugin = resolvePlugin(plugin, rootDir)
-        gatsbyNode = require(resolvedPlugin.resolvedCompiledGatsbyNode ??
-          `${resolvedPlugin.resolve}/gatsby-node`)
+        gatsbyNode = await importGatsbyPlugin(resolvedPlugin, `gatsby-node`)
       } catch (err) {
         gatsbyNode = {}
       }
