3a586c4aa46720e76c192cceeee317c4b50d168a
pieh <misiek.piechowiak@gmail.com>
2023-02-15 19:06:46 +0100

    fix e2e test setup

--- e2e-tests/contentful/src/pages/ssr.js
+++ e2e-tests/contentful/src/pages/ssr.js
@@ -3,12 +3,16 @@ import Layout from "../components/layout"
 
 const SSRPage = ({ serverData }) => (
   <Layout>
-    <div data-cy-id="getserverdata-result">{serverData}</div>
+    <div data-cy-id="getserverdata-result">{serverData.test}</div>
   </Layout>
 )
 
 export default SSRPage
 
 export function getServerData() {
-  return "getServerData used in contentful E2E test"
+  return {
+    props: {
+      test: "getServerData used in contentful E2E test",
+    },
+  }
 }
