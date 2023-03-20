220dbb94a00e64d582fd5811d7420e6c93a9a2ff
rogermparent <rogermparent@gmail.com>
2023-03-20 13:17:42 -0400

    Add more test cases

--- packages/gatsby-plugin-page-creator/src/__tests__/path-utils.ts
+++ packages/gatsby-plugin-page-creator/src/__tests__/path-utils.ts
@@ -2,8 +2,13 @@ import { pagesGlob } from "../path-utils"
 import micromatch from "micromatch"
 
 describe(`pagesGlob`, () => {
-  it(`will match files with collection routes in their or any parent directory's name.`, async () => {
-    const pathPatterns = [`a/{b}/c.tsx`, `a/{b}.tsx`]
+  it(`will match files with collection routes in any segment of their file path.`, async () => {
+    const pathPatterns = [
+      `{a.slug}.tsx`,
+      `a/{b.slug}.tsx`,
+      `a/{b.slug}/c.tsx`,
+      `a/{b.slug}/c/d.tsx`,
+    ]
     expect(micromatch(pathPatterns, pagesGlob)).toHaveLength(
       pathPatterns.length
     )
