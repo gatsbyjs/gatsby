1b06ef7a69c38334083eba399fb20a1a7ebe5eb9
kathmbeck <katherine.murphy.beck@gmail.com>
2023-04-03 10:00:56 -0700

    exit build early for invalid redirect

--- packages/gatsby/src/bootstrap/redirects-writer.ts
+++ packages/gatsby/src/bootstrap/redirects-writer.ts
@@ -8,15 +8,59 @@ import { IRedirect } from "../redux/types"
 let lastHash: string | null = null
 let bootstrapFinished = false
 
+function normalizePathPrefix(pathPrefix: string): string {
+  const newPathPrefix = pathPrefix.endsWith(`/`)
+    ? pathPrefix.slice()
+    : `${pathPrefix}/`
+
+  // strip trailing `/`
+  return newPathPrefix.replace(/\/$/, ``)
+}
+
+export function ensureLeadingSlash(path: string): string {
+  if (!path.startsWith(`/`) && !path.startsWith(`http`)) {
+    return `/` + path
+  }
+  return path
+}
+
+function normalizePath(path: string, pathPrefix: string | null): string {
+  let normalizedPath
+
+  if (pathPrefix && !path.startsWith(`http`)) {
+    const newPathPrefix = normalizePathPrefix(pathPrefix)
+    normalizedPath = path.replace(newPathPrefix, ``)
+  } else {
+    normalizedPath = path
+  }
+
+  return ensureLeadingSlash(normalizedPath)
+}
+
 export const writeRedirects = async (): Promise<void> => {
   bootstrapFinished = true
 
-  const { program, redirects, pages } = store.getState()
+  const { program, redirects, pages, config } = store.getState()
+  const { pathPrefix } = config
 
   const redirectMatchingPageWarnings: Array<string> = []
   const browserRedirects: Array<IRedirect> = []
 
   for (const redirect of redirects) {
+    let normalizedFromPath = normalizePath(
+      redirect.fromPath,
+      pathPrefix ? pathPrefix : null
+    )
+
+    try {
+      normalizedFromPath = decodeURIComponent(normalizedFromPath)
+    } catch (error) {
+      reporter.panicOnBuild(
+        `Invalid redirect for redirect with fromPath - ${redirect.fromPath}`,
+        error
+      )
+    }
+
     const alternativePath = redirect.fromPath.endsWith(`/`)
       ? redirect.fromPath.slice(0, -1)
       : redirect.fromPath + `/`
