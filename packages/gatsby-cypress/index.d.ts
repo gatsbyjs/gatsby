99775b6e6fc9ed3c8402149f1e67a44553d75845
LekoArts <lekoarts@gmail.com>
2023-04-28 12:22:35 +0200

    use subject generic

--- packages/gatsby-cypress/index.d.ts
+++ packages/gatsby-cypress/index.d.ts
@@ -1,9 +1,24 @@
 declare global {
   namespace Cypress {
-    interface Chainable {
-      waitForRouteChange: typeof waitForRouteChange
-      waitForAPI: typeof waitForAPI
-      waitForAPIorTimeout: typeof waitForAPIorTimeout
+    interface Chainable<Subject = any> {
+      /**
+       * Waits for Gatsby to finish the route change, in order to ensure event handlers are properly setup
+       * @example
+       * cy.visit(`/page-2`).waitForRouteChange()
+       */
+      waitForRouteChange: () => Chainable<Subject>
+      /**
+       * Waits for a specific Gatsby API to finish
+       * @example
+       * cy.waitForAPI(`onRouteUpdate`).get(`#element-with-event-handler`).click()
+       */
+      waitForAPI: (lifeCycleName: string) => Chainable<Subject>
+      /**
+       * Waits for a specific Gatsby API to finish. It timeouts if it doesn't finish.
+       * @example
+       * cy.waitForAPIorTimeout(`onRouteUpdate`)
+       */
+      waitForAPIorTimeout: (lifeCycleName: string, options?: WaitForApiOrTimeoutOptions) => Chainable<Subject>
     }
   }
 }
@@ -12,22 +27,4 @@ export interface WaitForApiOrTimeoutOptions {
   timeout?: number
 }
 
-/**
- * Waits for Gatsby to finish the route change, in order to ensure event handlers are properly setup
- * @example
- * cy.visit(`/page-2`).waitForRouteChange()
- */
-export declare const waitForRouteChange: () => Chainable<JQuery>
-/**
- * Waits for a specific Gatsby API to finish
- * @example
- * cy.waitForAPI(`onRouteUpdate`).get(`#element-with-event-handler`).click()
- */
-export declare const waitForAPI: (lifeCycleName: string) => Chainable<JQuery>
-/**
- * Waits for a specific Gatsby API to finish. It timeouts if it doesn't finish.
- * @example
- * cy.waitForAPIorTimeout(`onRouteUpdate`)
- */
-export declare const waitForAPIorTimeout: (lifeCycleName: string, options?: WaitForApiOrTimeoutOptions) => Chainable<JQuery>
 export {}
