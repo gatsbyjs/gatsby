fd78387a685fdf3ad9e00666f4778e3d64d71df3
Tyler Barnes <tylerdbarnes@gmail.com>
2023-05-16 08:37:20 -0700

    add logging and fix inverted modulus check

--- packages/gatsby/src/utils/chunk-stepper.ts
+++ packages/gatsby/src/utils/chunk-stepper.ts
@@ -8,6 +8,7 @@ export function chunkStepper(
   if (typeof runFn !== `function`) {
     throw new Error(`You must pass a function to chunkSteppers first argument`)
   }
+
   if (!Array.isArray(config) || !config.length) {
     throw new Error(
       `You must pass an array of objects to chunkSteppers second argument`
@@ -17,22 +18,22 @@ export function chunkStepper(
   let currentValues = config[0]
   let counter = 0
 
-  const tick = () => {
+  const tick = (): any => {
+    if (counter++ >= currentValues.until) {
+      currentValues = config[config.indexOf(currentValues) + 1]
+      console.log(`changing values to until ${currentValues?.until} every ${currentValues?.every}`)
+    }
+
     if (!currentValues) {
       // we've reached the end of the config
       return
     }
 
-    if (counter++ >= currentValues.until) {
-      currentValues = config[config.indexOf(currentValues) + 1]
-    }
-
-    if (
-      typeof currentValues.every === `number` &&
-      counter % currentValues.every !== 0
-    ) {
+    if (currentValues?.every && counter % currentValues.every === 0) {
       return runFn(counter)
     }
+
+    return
   }
 
   return { tick }
