e48c01c64cf91caa8f77cde62903e41b5fcae48a
Benedikt Rötsch <benedikt@hashbite.net>
2023-05-17 10:17:51 +0200

    rewrite utils.js to ts

--- packages/gatsby-source-contentful/src/utils.ts
+++ packages/gatsby-source-contentful/src/utils.ts
@@ -1,6 +1,6 @@
 // When iterating on tons of objects, we don't want to block the event loop
 // this helper function returns a promise that resolves on the next tick so that the event loop can continue before we continue running blocking code
-export function untilNextEventLoopTick() {
+export function untilNextEventLoopTick(): Promise<null> {
   return new Promise(res => {
     setImmediate(() => {
       res(null)
