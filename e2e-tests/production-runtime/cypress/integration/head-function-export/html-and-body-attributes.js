ccbbda5c6ae4cc9dfcbbf07891e9c74461c6ed55
Lennart <lekoarts@gmail.com>
2023-05-22 09:46:33 +0200

    fix(gatsby): Support inline styles on html/body for Head API (#38098)

--- e2e-tests/production-runtime/cypress/integration/head-function-export/html-and-body-attributes.js
+++ e2e-tests/production-runtime/cypress/integration/head-function-export/html-and-body-attributes.js
@@ -19,8 +19,12 @@ describe(`Html and body attributes`, () => {
 
     cy.get(`body`).should(`have.attr`, `data-foo`, `baz`)
     cy.get(`body`).should(`have.attr`, `class`, `foo`)
+    cy.get(`body`).should(`have.css`, `accentColor`, `rgb(102, 51, 153)`)
+    cy.get(`body`).should(`have.css`, `caretColor`, `rgb(102, 51, 153)`)
     cy.get(`html`).should(`have.attr`, `data-foo`, `bar`)
     cy.get(`html`).should(`have.attr`, `lang`, `fr`)
+    cy.get(`html`).should(`have.css`, `accentColor`, `rgb(102, 51, 153)`)
+    cy.get(`html`).should(`have.css`, `caretColor`, `rgb(102, 51, 153)`)
   })
 
   it(`Page has body and html attributes on client-side navigation`, () => {
@@ -37,8 +41,12 @@ describe(`Html and body attributes`, () => {
 
     cy.get(`body`).should(`have.attr`, `data-foo`, `baz`)
     cy.get(`body`).should(`have.attr`, `class`, `foo`)
+    cy.get(`body`).should(`have.css`, `accentColor`, `rgb(102, 51, 153)`)
+    cy.get(`body`).should(`have.css`, `caretColor`, `rgb(102, 51, 153)`)
     cy.get(`html`).should(`have.attr`, `data-foo`, `bar`)
     cy.get(`html`).should(`have.attr`, `lang`, `fr`)
+    cy.get(`html`).should(`have.css`, `accentColor`, `rgb(102, 51, 153)`)
+    cy.get(`html`).should(`have.css`, `caretColor`, `rgb(102, 51, 153)`)
   })
 
   it(`Body and html attributes are removed on client-side navigation when new page doesn't set them`, () => {
@@ -48,14 +56,22 @@ describe(`Html and body attributes`, () => {
 
     cy.get(`body`).should(`have.attr`, `data-foo`, `baz`)
     cy.get(`body`).should(`have.attr`, `class`, `foo`)
+    cy.get(`body`).should(`have.css`, `accentColor`, `rgb(102, 51, 153)`)
+    cy.get(`body`).should(`have.css`, `caretColor`, `rgb(102, 51, 153)`)
     cy.get(`html`).should(`have.attr`, `data-foo`, `bar`)
     cy.get(`html`).should(`have.attr`, `lang`, `fr`)
+    cy.get(`html`).should(`have.css`, `accentColor`, `rgb(102, 51, 153)`)
+    cy.get(`html`).should(`have.css`, `caretColor`, `rgb(102, 51, 153)`)
 
     cy.visit(headFunctionExportSharedData.page.basic).waitForRouteChange()
 
     cy.get(`body`).should(`not.have.attr`, `data-foo`, `baz`)
     cy.get(`body`).should(`not.have.attr`, `class`, `foo`)
+    cy.get(`body`).should(`not.have.css`, `accentColor`, `rgb(102, 51, 153)`)
+    cy.get(`body`).should(`not.have.css`, `caretColor`, `rgb(102, 51, 153)`)
     cy.get(`html`).should(`not.have.attr`, `data-foo`, `bar`)
     cy.get(`html`).should(`not.have.attr`, `lang`, `fr`)
+    cy.get(`html`).should(`not.have.css`, `accentColor`, `rgb(102, 51, 153)`)
+    cy.get(`html`).should(`not.have.css`, `caretColor`, `rgb(102, 51, 153)`)
   })
 })
