7b05c8935314ec7cc473cb88fb5ce7bb384433cf
LekoArts <lekoarts@gmail.com>
2023-06-12 15:45:52 +0200

    format lambda-handler

--- packages/gatsby-adapter-netlify/src/lambda-handler.ts
+++ packages/gatsby-adapter-netlify/src/lambda-handler.ts
@@ -3,6 +3,28 @@ import type { IFunctionDefinition } from "gatsby/src/utils/adapter/types"
 import fs from "fs-extra"
 import * as path from "path"
 
+interface INetlifyFunctionConfig {
+  externalNodeModules?: Array<string>
+  includedFiles?: Array<string>
+  includedFilesBasePath?: string
+  ignoredNodeModules?: Array<string>
+  nodeBundler?: "esbuild" | "esbuild_zisi" | "nft" | "zisi" | "none"
+  nodeSourcemap?: boolean
+  nodeVersion?: string
+  processDynamicNodeImports?: boolean
+  rustTargetDirectory?: string
+  schedule?: string
+  zipGo?: boolean
+  name?: string
+  generator?: string
+  nodeModuleFormat?: "cjs" | "esm"
+}
+
+interface INetlifyFunctionManifest {
+  config: INetlifyFunctionConfig
+  version: number
+}
+
 async function prepareFunction(
   fun: IFunctionDefinition,
   odbfunctionName?: string
@@ -24,7 +46,7 @@ async function prepareFunction(
 
   await fs.ensureDir(internalFunctionsDir)
 
-  const functionManifest: any = {
+  const functionManifest: INetlifyFunctionManifest = {
     config: {
       includedFiles: fun.requiredFiles.map(file =>
         file.replace(/\[/g, `*`).replace(/]/g, `*`)
@@ -40,19 +62,19 @@ async function prepareFunction(
   )
 
   const handlerSource = /* javascript */ `
-    const Stream = require("stream")
-    const http = require("http")
-    const { Buffer } = require("buffer")
-    const cookie = require("cookie")
-    ${isODB ? `const { builder } = require("@netlify/functions")` : ``}
+const Stream = require("stream")
+const http = require("http")
+const { Buffer } = require("buffer")
+const cookie = require("cookie")
+${isODB ? `const { builder } = require("@netlify/functions")` : ``}
 
-    const preferDefault = m => (m && m.default) || m
+const preferDefault = m => (m && m.default) || m
 
-    const functionModule = require("./../../../${fun.pathToEntryPoint}")
+const functionModule = require("./../../../${fun.pathToEntryPoint}")
 
-    const functionHandler = preferDefault(functionModule)
+const functionHandler = preferDefault(functionModule)
 
-    const createRequestObject = ({ event, context }) => {
+const createRequestObject = ({ event, context }) => {
   const {
     path = "",
     multiValueQueryStringParameters,
@@ -95,144 +117,144 @@ async function prepareFunction(
 }
 
 const createResponseObject = ({ onResEnd }) => {
-    const response = {
-        isBase64Encoded: true,
-        multiValueHeaders: {},
-    };
-    const res = new Stream();
-    Object.defineProperty(res, 'statusCode', {
-        get() {
-            return response.statusCode;
-        },
-        set(statusCode) {
-            response.statusCode = statusCode;
-        },
-    });
-    res.headers = { 'content-type': 'text/html; charset=utf-8' };
-    res.writeHead = (status, headers) => {
-        response.statusCode = status;
-        if (headers) {
-            res.headers = Object.assign(res.headers, headers);
-        }
-        // Return res object to allow for chaining
-        // Fixes: https://github.com/netlify/next-on-netlify/pull/74
-        return res;
-    };
-    res.write = (chunk) => {
-        if (!response.body) {
-            response.body = Buffer.from('');
-        }
-        response.body = Buffer.concat([
-            Buffer.isBuffer(response.body)
-                ? response.body
-                : Buffer.from(response.body),
-            Buffer.isBuffer(chunk) ? chunk : Buffer.from(chunk),
-        ]);
-        return true;
-    };
-    res.setHeader = (name, value) => {
-        res.headers[name.toLowerCase()] = value;
-        return res;
-    };
-    res.removeHeader = (name) => {
-        delete res.headers[name.toLowerCase()];
-    };
-    res.getHeader = (name) => res.headers[name.toLowerCase()];
-    res.getHeaders = () => res.headers;
-    res.hasHeader = (name) => Boolean(res.getHeader(name));
-    res.end = (text) => {
-        if (text)
-            res.write(text);
-        if (!res.statusCode) {
-            res.statusCode = 200;
-        }
-        if (response.body) {
-            response.body = Buffer.from(response.body).toString('base64');
-        }
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore These types are a mess, and need sorting out
-        response.multiValueHeaders = res.headers;
-        res.writeHead(response.statusCode);
-        // Convert all multiValueHeaders into arrays
-        for (const key of Object.keys(response.multiValueHeaders)) {
-            const header = response.multiValueHeaders[key];
-            if (!Array.isArray(header)) {
-                response.multiValueHeaders[key] = [header];
-            }
-        }
-        res.finished = true;
-        res.writableEnded = true;
-        // Call onResEnd handler with the response object
-        onResEnd(response);
-        return res;
-    };
-    // Gatsby Functions additions
-    res.send = (data) => {
-        if (res.finished) {
-            return res;
-        }
-        if (typeof data === 'number') {
-            return res
-                .status(data)
-                .setHeader('content-type', 'text/plain; charset=utf-8')
-                .end(statuses_1.default.message[data] || String(data));
-        }
-        if (typeof data === 'boolean' || typeof data === 'object') {
-            if (Buffer.isBuffer(data)) {
-                res.setHeader('content-type', 'application/octet-Stream');
-            }
-            else if (data !== null) {
-                return res.json(data);
-            }
-        }
-        res.end(data);
-        return res;
-    };
-    res.json = (data) => {
-        if (res.finished) {
-            return res;
-        }
-        res.setHeader('content-type', 'application/json');
-        res.end(JSON.stringify(data));
-        return res;
-    };
-    res.status = (code) => {
-        const numericCode = Number.parseInt(code);
-        if (!Number.isNaN(numericCode)) {
-            response.statusCode = numericCode;
-        }
-        return res;
-    };
-    res.redirect = (statusCodeOrUrl, url) => {
-        let statusCode = statusCodeOrUrl;
-        let Location = url;
-        if (!url && typeof statusCodeOrUrl === 'string') {
-            Location = statusCodeOrUrl;
-            statusCode = 302;
-        }
-        res.writeHead(statusCode, { Location });
-        res.end();
-        return res;
-    };
+  const response = {
+    isBase64Encoded: true,
+    multiValueHeaders: {},
+  };
+  const res = new Stream();
+  Object.defineProperty(res, 'statusCode', {
+    get() {
+      return response.statusCode;
+    },
+    set(statusCode) {
+      response.statusCode = statusCode;
+    },
+  });
+  res.headers = { 'content-type': 'text/html; charset=utf-8' };
+  res.writeHead = (status, headers) => {
+    response.statusCode = status;
+    if (headers) {
+      res.headers = Object.assign(res.headers, headers);
+    }
+    // Return res object to allow for chaining
+    // Fixes: https://github.com/netlify/next-on-netlify/pull/74
     return res;
+  };
+  res.write = (chunk) => {
+    if (!response.body) {
+      response.body = Buffer.from('');
+    }
+    response.body = Buffer.concat([
+      Buffer.isBuffer(response.body)
+        ? response.body
+        : Buffer.from(response.body),
+      Buffer.isBuffer(chunk) ? chunk : Buffer.from(chunk),
+    ]);
+    return true;
+  };
+  res.setHeader = (name, value) => {
+    res.headers[name.toLowerCase()] = value;
+    return res;
+  };
+  res.removeHeader = (name) => {
+    delete res.headers[name.toLowerCase()];
+  };
+  res.getHeader = (name) => res.headers[name.toLowerCase()];
+  res.getHeaders = () => res.headers;
+  res.hasHeader = (name) => Boolean(res.getHeader(name));
+  res.end = (text) => {
+    if (text)
+      res.write(text);
+    if (!res.statusCode) {
+      res.statusCode = 200;
+    }
+    if (response.body) {
+      response.body = Buffer.from(response.body).toString('base64');
+    }
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore These types are a mess, and need sorting out
+    response.multiValueHeaders = res.headers;
+    res.writeHead(response.statusCode);
+    // Convert all multiValueHeaders into arrays
+    for (const key of Object.keys(response.multiValueHeaders)) {
+      const header = response.multiValueHeaders[key];
+      if (!Array.isArray(header)) {
+        response.multiValueHeaders[key] = [header];
+      }
+    }
+    res.finished = true;
+    res.writableEnded = true;
+    // Call onResEnd handler with the response object
+    onResEnd(response);
+    return res;
+  };
+  // Gatsby Functions additions
+  res.send = (data) => {
+    if (res.finished) {
+      return res;
+    }
+    if (typeof data === 'number') {
+      return res
+        .status(data)
+        .setHeader('content-type', 'text/plain; charset=utf-8')
+        .end(statuses_1.default.message[data] || String(data));
+    }
+    if (typeof data === 'boolean' || typeof data === 'object') {
+      if (Buffer.isBuffer(data)) {
+        res.setHeader('content-type', 'application/octet-Stream');
+      }
+      else if (data !== null) {
+        return res.json(data);
+      }
+    }
+    res.end(data);
+    return res;
+  };
+  res.json = (data) => {
+    if (res.finished) {
+      return res;
+    }
+    res.setHeader('content-type', 'application/json');
+    res.end(JSON.stringify(data));
+    return res;
+  };
+  res.status = (code) => {
+    const numericCode = Number.parseInt(code);
+    if (!Number.isNaN(numericCode)) {
+      response.statusCode = numericCode;
+    }
+    return res;
+  };
+  res.redirect = (statusCodeOrUrl, url) => {
+    let statusCode = statusCodeOrUrl;
+    let Location = url;
+    if (!url && typeof statusCodeOrUrl === 'string') {
+      Location = statusCodeOrUrl;
+      statusCode = 302;
+    }
+    res.writeHead(statusCode, { Location });
+    res.end();
+    return res;
+  };
+  return res;
 };
 
-    const handler = async (event, context) => {
-      const req = createRequestObject({ event, context })
+const handler = async (event, context) => {
+  const req = createRequestObject({ event, context })
 
-      return new Promise(async resolve => {
-        try {
-          const res = createResponseObject({ onResEnd: resolve })
-          await functionHandler(req, res)
-        } catch(error) {
-          console.error("Error executing " + event.path, error)
-          resolve({ statusCode: 500 })
-        }
-      })
+  return new Promise(async resolve => {
+    try {
+      const res = createResponseObject({ onResEnd: resolve })
+      await functionHandler(req, res)
+    } catch(error) {
+      console.error("Error executing " + event.path, error)
+      resolve({ statusCode: 500 })
     }
+  })
+}
 
-    exports.handler = ${isODB ? `builder(handler)` : `handler`}
-    `
+exports.handler = ${isODB ? `builder(handler)` : `handler`}
+`
 
   await fs.writeFile(
     path.join(internalFunctionsDir, `${functionId}.js`),
