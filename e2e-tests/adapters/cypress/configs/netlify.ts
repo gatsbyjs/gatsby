8c7d99e507d461760c2df55bfed4b739175a622a
LekoArts <lekoarts@gmail.com>
2023-06-14 09:26:33 +0200

    cypress: remove viewPort configs

--- e2e-tests/adapters/cypress/configs/netlify.ts
+++ e2e-tests/adapters/cypress/configs/netlify.ts
@@ -4,8 +4,6 @@ export default defineConfig({
   e2e: {
     baseUrl: `http://localhost:8888`,
     projectId: `4enh4m`,
-    viewportWidth: 1440,
-    viewportHeight: 900,
     videoUploadOnPasses: false,
   },
 })
