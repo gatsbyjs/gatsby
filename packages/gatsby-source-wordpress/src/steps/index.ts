b9413808c0dd7fe6d44487e41a3776608aa842fe
Matt Kane <m@mk.gg>
2023-06-15 10:26:07 +0100

    chore(gatsby-source-wordpress): remove warning about multiple plugin instances (#38241)

--- packages/gatsby-source-wordpress/src/steps/index.ts
+++ packages/gatsby-source-wordpress/src/steps/index.ts
@@ -8,7 +8,6 @@ export { setImageNodeIdCache } from "~/steps/set-image-node-id-cache"
 export { startPollingForContentUpdates } from "~/steps/source-nodes/update-nodes/content-update-interval"
 export { checkIfSchemaHasChanged } from "~/steps/ingest-remote-schema/diff-schemas"
 export { setErrorMap } from "~/steps/set-error-map"
-export { tempPreventMultipleInstances } from "~/steps/temp-prevent-multiple-instances"
 
 export { onPreExtractQueriesInvokeLeftoverPreviewCallbacks } from "./preview/cleanup"
 
