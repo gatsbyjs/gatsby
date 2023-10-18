de27dc7ef4f7e019814eaad5bdebdba68f104bf8
Michal Piechowiak <misiek.piechowiak@gmail.com>
2023-10-18 17:13:50 +0200

    test: clear browser cache for interception tests

--- e2e-tests/adapters/cypress/e2e/basics.cy.ts
+++ e2e-tests/adapters/cypress/e2e/basics.cy.ts
@@ -1,44 +1,48 @@
 import { title } from "../../constants"
 
-describe('Basics', () => {
+describe("Basics", () => {
   beforeEach(() => {
     cy.intercept("/gatsby-icon.png").as("static-folder-image")
     cy.intercept("/static/astro-**.png").as("img-import")
 
-    cy.visit('/').waitForRouteChange()
+    // clear browser cache before each test - otherwise permanently cached assets (like img-import)
+    // would be intercepted only on very first test
+    cy.wrap(
+      Cypress.automation("remote:debugger:protocol", {
+        command: "Network.clearBrowserCache",
+      })
+    )
+
+    cy.visit("/").waitForRouteChange()
   })
 
-  it('should display index page', () => {
-    cy.get('h1').should('have.text', title)
-    cy.title().should('eq', 'Adapters E2E')
+  it("should display index page", () => {
+    cy.get("h1").should("have.text", title)
+    cy.title().should("eq", "Adapters E2E")
   })
   // If this test fails, run "gatsby build" and retry
   it('should serve assets from "static" folder', () => {
     cy.wait("@static-folder-image").should(req => {
-      expect(req.response.statusCode).to.be.gte(200).and.lt(400)
+      expect(req.response.statusCode).to.be.eq(200)
     })
 
-    cy.get('[alt="Gatsby Monogram Logo"]').should('be.visible')
+    cy.get('[alt="Gatsby Monogram Logo"]').should("be.visible")
   })
-  it('should serve assets imported through webpack', () => {
+  it("should serve assets imported through webpack", () => {
     cy.wait("@img-import").should(req => {
-      expect(req.response.statusCode).to.be.gte(200).and.lt(400)
+      expect(req.response.statusCode).to.be.eq(200)
     })
 
-    cy.get('[alt="Gatsby Astronaut"]').should('be.visible')
+    cy.get('[alt="Gatsby Astronaut"]').should("be.visible")
   })
   it(`should show custom 404 page on invalid URL`, () => {
     cy.visit(`/non-existent-page`, {
       failOnStatusCode: false,
     })
 
-    cy.get('h1').should('have.text', 'Page not found')
+    cy.get("h1").should("have.text", "Page not found")
   })
-  it('should apply CSS', () => {
-    cy.get(`h1`).should(
-      `have.css`,
-      `color`,
-      `rgb(21, 21, 22)`
-    )
+  it("should apply CSS", () => {
+    cy.get(`h1`).should(`have.css`, `color`, `rgb(21, 21, 22)`)
   })
 })
