d09b7917b2d4e8f9b2709de05b1551f514388111
Michal Piechowiak <misiek.piechowiak@gmail.com>
2023-10-27 10:44:48 +0200

    streamline file moving logic

--- packages/gatsby-adapter-netlify/src/pretty-urls.ts
+++ packages/gatsby-adapter-netlify/src/pretty-urls.ts
@@ -1,22 +1,12 @@
 import fastq from "fastq"
 import fs from "fs-extra"
 
-function generateFilePathForStaticRoute(
-  routePath: string,
-  shouldUsePrettyUrl: boolean
-): string {
-  if (shouldUsePrettyUrl) {
-    if (routePath.endsWith(`.html`)) {
-      return routePath
-    } else if (routePath === `/`) {
-      return `/index.html`
-    } else if (routePath.endsWith(`/`)) {
-      return `${routePath}index.html`
-    } else {
-      return `${routePath}.html`
-    }
+function generatePrettyUrlFilePath(routePath: string): string {
+  if (routePath.endsWith(`/`)) {
+    return `${routePath}index.html`
+  } else {
+    return `${routePath}.html`
   }
-  return routePath
 }
 
 interface IMoveTask {
@@ -32,9 +22,9 @@ export function createStaticAssetsPathHandler(): {
   const moveQueue = fastq<void, IMoveTask, void>(async (task, cb) => {
     try {
       if (task.keepOriginalFile) {
-        await fs.copy(task.from, task.to)
+        await fs.copy(task.from, task.to, { overwrite: true })
       } else {
-        await fs.move(task.from, task.to)
+        await fs.move(task.from, task.to, { overwrite: true })
       }
       cb(null, undefined)
     } catch (error) {
@@ -43,17 +33,19 @@ export function createStaticAssetsPathHandler(): {
   }, 2)
 
   function ensureStaticAssetPath(filePath: string, routePath: string): string {
-    const shouldUsePrettyUrl = filePath.endsWith(`.html`)
+    const shouldUsePrettyUrl =
+      filePath.endsWith(`.html`) && !routePath.endsWith(`.html`)
 
-    const expectedPath = `public${generateFilePathForStaticRoute(
-      routePath,
-      shouldUsePrettyUrl
-    )}`
+    const expectedPath = `public${
+      shouldUsePrettyUrl ? generatePrettyUrlFilePath(routePath) : routePath
+    }`
 
     if (expectedPath !== filePath) {
       moveQueue.push({
         from: filePath,
         to: expectedPath,
+        // 404.html should stay in root of PUBLISH_DIR to be used as custom 404 page
+        // both 404.html and 500.html should stay in root of PUBLISH_DIR to be bundled correctly for SSR/DSG
         keepOriginalFile:
           filePath === `public/404.html` || filePath === `public/500.html`,
       })
