2505005a4197ad2561d78ad74c2eb3fcbfd9f1fa
Michal Piechowiak <misiek.piechowiak@gmail.com>
2023-11-13 15:06:36 +0100

    feat(gatsby,gatsby-adapter-netlify): support pathPrefix and trailingSlash options (#38666)
    * initial wip
    
    * prefix all the things
    
    * lambda handler stripping path prefix
    
    * test: adjust e2e setup to also run variant with path prefix and no trailing slashes
    
    * chore: use queue for file moving to limit concurrency
    
    * tmp: don't clean up deploys while debugging things
    
    * test: added variant variables to deploy title
    
    * fix TS
    
    * fix unit tests
    
    * try different cypress group
    
    * try different cypress group2
    
    * maybe fix passing cypress env?
    
    * fix ssr path_prefix
    
    * keep 404/500 status pages in original place
    
    * fix typo
    
    * fix assertion?
    
    * streamline file moving logic
    
    * cache (and restore) publishdir locally too
    
    * add pretty-url unit tests
    
    * handle dynamic paths when generating pretty url file names
    
    * update intercepting glob to handle path prefix
    
    * update intercepting glob to handle path prefix 2
    
    * restore automatic deploys deletion
    
    * test: jest ensure we mount files that we test filepaths for
    
    * handle path prefix in header rules
    
    * drop debug helpers
    
    * first check if local
    
    * drop debug log
    
    * handle external redirects when pathPrefix is used. Thanks @techfg
    
    * make placeholder syntax consistent
    
    * update comment
    
    * move dynamic route path normalization to its own function
    
    (cherry picked from commit 63e57cf3dd96083d219c09741a751206bb715a53)

--- e2e-tests/adapters/cypress/e2e/headers.cy.ts
+++ e2e-tests/adapters/cypress/e2e/headers.cy.ts
@@ -1,5 +1,7 @@
 import { WorkaroundCachedResponse } from "../utils/dont-cache-responses-in-browser"
 
+const PATH_PREFIX = Cypress.env(`PATH_PREFIX`) || ``
+
 describe("Headers", () => {
   const defaultHeaders = {
     "x-xss-protection": "1; mode=block",
@@ -73,23 +75,38 @@ describe("Headers", () => {
   }
 
   beforeEach(() => {
-    cy.intercept("/", WorkaroundCachedResponse).as("index")
-    cy.intercept("routes/ssr/static", WorkaroundCachedResponse).as("ssr")
-    cy.intercept("routes/dsg/static", WorkaroundCachedResponse).as("dsg")
+    cy.intercept(PATH_PREFIX + "/", WorkaroundCachedResponse).as("index")
+    cy.intercept(
+      PATH_PREFIX + "/routes/ssr/static",
+      WorkaroundCachedResponse
+    ).as("ssr")
+    cy.intercept(
+      PATH_PREFIX + "/routes/dsg/static",
+      WorkaroundCachedResponse
+    ).as("dsg")
 
-    cy.intercept("**/page-data.json", WorkaroundCachedResponse).as("page-data")
-    cy.intercept("**/app-data.json", WorkaroundCachedResponse).as("app-data")
-    cy.intercept("**/slice-data/*.json", WorkaroundCachedResponse).as(
-      "slice-data"
-    )
-    cy.intercept("**/page-data/sq/d/*.json", WorkaroundCachedResponse).as(
-      "static-query-result"
-    )
+    cy.intercept(
+      PATH_PREFIX + "/**/page-data.json",
+      WorkaroundCachedResponse
+    ).as("page-data")
+    cy.intercept(
+      PATH_PREFIX + "/**/app-data.json",
+      WorkaroundCachedResponse
+    ).as("app-data")
+    cy.intercept(
+      PATH_PREFIX + "/**/slice-data/*.json",
+      WorkaroundCachedResponse
+    ).as("slice-data")
+    cy.intercept(
+      PATH_PREFIX + "/**/page-data/sq/d/*.json",
+      WorkaroundCachedResponse
+    ).as("static-query-result")
 
-    cy.intercept("/static/astro-**.png", WorkaroundCachedResponse).as(
-      "img-webpack-import"
-    )
-    cy.intercept("*.js", WorkaroundCachedResponse).as("js")
+    cy.intercept(
+      PATH_PREFIX + "/static/astro-**.png",
+      WorkaroundCachedResponse
+    ).as("img-webpack-import")
+    cy.intercept(PATH_PREFIX + "/**/*.js", WorkaroundCachedResponse).as("js")
   })
 
   it("should contain correct headers for index page", () => {
