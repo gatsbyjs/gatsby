e3365ab01eebb6c17bbae7fcb7e74040a0c4f260
João Antunes <me@jgantunes.com>
2023-11-21 14:48:31 +0000

    fix: re-install lmdb when detecting the absence of @LMDB prebuilt packages (#38691)
    * fix: re-install lmdb when detecting the absence of @lmdb prebuilt packages
    
    * chore: set the correct root lmdb and add further comments
    
    * chore: linter
    
    * chore: linting
    
    * chore: make the install check for reliable and install @lmdb instead of lmdb
    
    * fix: no need to touch the patch, we can leverage the force binary flag
    
    * chore: add integration test for lmdb fix
    
    * chore: add CCI config for new integration test
    
    * add integration_tests_esm_in_gatsby_files to jobs
    
    * fix: integration test for lmdb-regeneration
    
    * fix: cli path in test
    
    * chore: actually add lmdb's dist directory
    
    * fix: back to the npmrc and just rm -rf @lmdb modules
    
    ---------
    
    Co-authored-by: Michal Piechowiak <misiek.piechowiak@gmail.com>

---
+++ integration-tests/lmdb-regeneration/__tests__/index.js
@@ -0,0 +1,59 @@
+const path = require(`path`)
+const execa = require(`execa`)
+const mod = require("module")
+const fs = require(`fs-extra`)
+
+jest.setTimeout(100000)
+
+const rootPath = path.resolve(__dirname, "../")
+
+describe(`Lmdb regeneration`, () => {
+  test(`gatbsy build detects lmdb setup built from source and installs pre-buit package`, async () => {
+    const lmdbNodeModulesPath = path.resolve(rootPath, "node_modules", "lmdb")
+    // Make sure we clear out the current `@lmdb` optional dependencies
+    const pathsToRemove = [
+      path.resolve(rootPath, "node_modules", "@lmdb"),
+      path.resolve(rootPath, "node_modules", "gatsby", "node_modules", "@lmdb"),
+    ]
+    for (let path of pathsToRemove) {
+      fs.rmSync(path, { force: true, recursive: true })
+    }
+    // Check the lmdb instance we have installed does have a binary built from source since we need it to reproduce the fix we're trying to test
+    // If this check fails then it means our fixture is wrong and we're relying on an lmdb instance with prebuilt binaries
+    const builtFromSource = fs.existsSync(
+      path.resolve(lmdbNodeModulesPath, "build", "Release", "lmdb.node")
+    )
+    expect(builtFromSource).toEqual(true)
+
+    const options = {
+      stderr: `inherit`,
+      stdout: `inherit`,
+      cwd: rootPath,
+    }
+    const gatsbyBin = path.resolve(rootPath, `node_modules`, `gatsby`, `cli.js`)
+    await execa(gatsbyBin, [`build`], options)
+
+    // lmdb module with prebuilt binaries for our platform
+    const lmdbPackage = `@lmdb/lmdb-${process.platform}-${process.arch}`
+
+    // If the fix worked correctly we should have installed the prebuilt binary for our platform under our `.cache` directory
+    const lmdbRequire = mod.createRequire(
+      path.resolve(rootPath, ".cache", "internal-packages", "package.json")
+    )
+    expect(() => {
+      lmdbRequire.resolve(lmdbPackage)
+    }).not.toThrow()
+
+    // The resulting query-engine bundle should not contain the binary built from source
+    const binaryBuiltFromSource = path.resolve(
+      rootPath,
+      ".cache",
+      "query-engine",
+      "assets",
+      "build",
+      "Release",
+      "lmdb.node"
+    )
+    expect(fs.existsSync(binaryBuiltFromSource)).toEqual(false)
+  })
+})
