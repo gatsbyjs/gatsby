c4903b990c84348b9b903aee143f6321955aa301
Benedikt Rötsch <benedikt@hashbite.net>
2023-12-06 11:50:21 +0100

    fix(gatsby-source-contentful): don't apply parent node links to child nodes

--- packages/gatsby-source-contentful/src/source-nodes.ts
+++ packages/gatsby-source-contentful/src/source-nodes.ts
@@ -519,13 +519,13 @@ export const sourceNodes: GatsbyNode["sourceNodes"] =
 
           // memory cached nodes are mutated during back reference checks
           // so we need to carry over the changes to the updated node
-          if (node.__memcache) {
-            for (const key of Object.keys(node)) {
+          if (nodeToUpdateOriginal.__memcache) {
+            for (const key of Object.keys(nodeToUpdateOriginal)) {
               if (!key.endsWith(`___NODE`)) {
                 continue
               }
 
-              newNode[key] = node[key]
+              newNode[key] = nodeToUpdateOriginal[key]
             }
           }
 
