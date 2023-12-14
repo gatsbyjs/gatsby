8181977127913c78fd9b622f1b40b74ee646923e
Michal Piechowiak <misiek.piechowiak@gmail.com>
2023-12-14 18:38:13 +0100

    fix: use different method of figuring out wether to enable .cache resolver plugin

--- packages/gatsby/src/utils/webpack/plugins/cache-folder-resolver.ts
+++ packages/gatsby/src/utils/webpack/plugins/cache-folder-resolver.ts
@@ -1,24 +1,70 @@
 import Resolver from "enhanced-resolve/lib/Resolver"
+import mod from "module"
 
 interface IRequest {
   request?: string
   path: string
 }
 
-type ProcessWithPNP = NodeJS.ProcessVersions & { pnp?: string }
-
 /**
  * To support PNP we have to make sure dependencies resolved from the .cache folder should be resolved from the gatsby package directory
  */
 export class CacheFolderResolver {
   private requestsFolder: string
+  private isEnabled = false
 
   constructor(requestsFolder: string) {
     this.requestsFolder = requestsFolder
+
+    const cacheDirReq = mod.createRequire(requestsFolder)
+
+    let isEverythingResolvableFromCacheDir = true
+    let isEverythingResolvableFromGatsbyPackage = true
+
+    // Hardcoded list of gatsby deps used in gatsby browser and ssr runtimes
+    // instead of checking if we use Yarn PnP (via `process.versions.pnp`),
+    // we check if we can resolve the external deps from the cache-dir folder
+    // to know if we need to enable this plugin so we also cover pnpm
+    // It might be good idea to always enable it overall, but to limit potential
+    // regressions we only enable it if we are sure we need it.
+    const modulesToCheck = [
+      `prop-types`,
+      `lodash/isEqual`,
+      `mitt`,
+      `shallow-compare`,
+      `@gatsbyjs/reach-router`,
+      `gatsby-react-router-scroll`,
+      `react-server-dom-webpack`,
+      `gatsby-link`,
+    ]
+    for (const cacheDirDep of modulesToCheck) {
+      try {
+        cacheDirReq.resolve(cacheDirDep)
+      } catch {
+        // something is not resolable from the cache folder, so we should not enable this plugin
+        isEverythingResolvableFromCacheDir = false
+      }
+
+      try {
+        require.resolve(cacheDirDep)
+      } catch {
+        // something is not resolable from the gatsby package
+        isEverythingResolvableFromGatsbyPackage = false
+      }
+    }
+
+    // we only enable this plugin if we are unable to resolve cache-dir deps from .cache folder
+    // and we can resolve them from gatsby package
+    if (
+      !isEverythingResolvableFromCacheDir &&
+      isEverythingResolvableFromGatsbyPackage
+    ) {
+      this.isEnabled = true
+    }
   }
 
   apply(resolver: Resolver): void {
-    if (!(process.versions as ProcessWithPNP).pnp) {
+    if (!this.isEnabled) {
       return
     }
 
