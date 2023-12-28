cfd7b29643c7bdfcfff223766cca8ef9af147f24
Benedikt Rötsch <benedikt@hashbite.net>
2023-12-28 13:28:10 +0100

    fix: further improve rich text types

--- packages/gatsby-source-contentful/src/rich-text.ts
+++ packages/gatsby-source-contentful/src/rich-text.ts
@@ -2,38 +2,32 @@ import {
   documentToReactComponents,
   Options,
 } from "@contentful/rich-text-react-renderer"
-import {
-  AssetHyperlink,
-  AssetLinkBlock,
-  Document,
-  EntryHyperlink,
-  EntryLinkBlock,
-  EntryLinkInline,
-} from "@contentful/rich-text-types"
+import { Document } from "@contentful/rich-text-types"
 import React from "react"
 import type { IContentfulEntry, IContentfulAsset } from "./types/contentful"
 
 interface IContentfulRichTextLinksAssets {
-  block: Array<IContentfulAsset>
-  hyperlink: Array<IContentfulAsset>
+  block?: Array<IContentfulAsset>
+  hyperlink?: Array<IContentfulAsset>
 }
 
 interface IContentfulRichTextLinksEntries {
-  inline: Array<IContentfulEntry>
-  block: Array<IContentfulEntry>
-  hyperlink: Array<IContentfulEntry>
+  inline?: Array<IContentfulEntry>
+  block?: Array<IContentfulEntry>
+  hyperlink?: Array<IContentfulEntry>
 }
 
 interface IContentfulRichTextLinks {
-  assets: IContentfulRichTextLinksAssets
-  entries: IContentfulRichTextLinksEntries
+  assets?: IContentfulRichTextLinksAssets
+  entries?: IContentfulRichTextLinksEntries
 }
 
-type AssetBlockMap = Map<string, AssetLinkBlock>
-type AssetHyperlinkMap = Map<string, AssetHyperlink>
-type EntryBlockMap = Map<string, EntryLinkBlock>
-type EntryInlineMap = Map<string, EntryLinkInline>
-type EntryHyperlinkMap = Map<string, EntryHyperlink>
+type AssetBlockMap = Map<string, Partial<IContentfulAsset>>
+type AssetHyperlinkMap = Map<string, Partial<IContentfulAsset>>
+type EntryBlockMap = Map<string, Partial<IContentfulEntry>>
+type EntryInlineMap = Map<string, Partial<IContentfulEntry>>
+type EntryHyperlinkMap = Map<string, Partial<IContentfulEntry>>
+
 interface IMakeOptions {
   assetBlockMap: AssetBlockMap
   assetHyperlinkMap: AssetHyperlinkMap
@@ -48,24 +42,24 @@ export function renderRichText(
     json,
     links,
   }: {
-    json: Document
-    links: IContentfulRichTextLinks
+    json: unknown
+    links?: unknown
   },
-  makeOptions: MakeOptions | Options
+  makeOptions?: MakeOptions | Options
 ): React.ReactNode {
   const options =
     typeof makeOptions === `function`
-      ? makeOptions(generateLinkMaps(links))
+      ? makeOptions(generateLinkMaps(links as IContentfulRichTextLinks))
       : makeOptions
 
-  return documentToReactComponents(json, options)
+  return documentToReactComponents(json as Document, options)
 }
 
 /**
  * Helper function to simplify Rich Text rendering. Based on:
  * https://www.contentful.com/blog/2021/04/14/rendering-linked-assets-entries-in-contentful/
  */
-function generateLinkMaps(links): {
+function generateLinkMaps(links: IContentfulRichTextLinks): {
   assetBlockMap: AssetBlockMap
   assetHyperlinkMap: AssetHyperlinkMap
   entryBlockMap: EntryBlockMap
@@ -73,28 +67,38 @@ function generateLinkMaps(links): {
   entryHyperlinkMap: EntryHyperlinkMap
 } {
   const assetBlockMap = new Map()
-  for (const asset of links?.assets.block || []) {
-    assetBlockMap.set(asset.sys.id, asset)
+  if (links?.assets?.block) {
+    for (const asset of links.assets.block) {
+      assetBlockMap.set(asset.sys.id, asset)
+    }
   }
 
   const assetHyperlinkMap = new Map()
-  for (const asset of links?.assets.hyperlink || []) {
-    assetHyperlinkMap.set(asset.sys.id, asset)
+  if (links?.assets?.hyperlink) {
+    for (const asset of links.assets.hyperlink) {
+      assetHyperlinkMap.set(asset.sys.id, asset)
+    }
   }
 
   const entryBlockMap = new Map()
-  for (const entry of links?.entries.block || []) {
-    entryBlockMap.set(entry.sys.id, entry)
+  if (links?.entries?.block) {
+    for (const entry of links.entries.block) {
+      entryBlockMap.set(entry.sys.id, entry)
+    }
   }
 
   const entryInlineMap = new Map()
-  for (const entry of links?.entries.inline || []) {
-    entryInlineMap.set(entry.sys.id, entry)
+  if (links?.entries?.inline) {
+    for (const entry of links.entries.inline) {
+      entryInlineMap.set(entry.sys.id, entry)
+    }
   }
 
   const entryHyperlinkMap = new Map()
-  for (const entry of links?.entries.hyperlink || []) {
-    entryHyperlinkMap.set(entry.sys.id, entry)
+  if (links?.entries?.hyperlink) {
+    for (const entry of links.entries.hyperlink) {
+      entryHyperlinkMap.set(entry.sys.id, entry)
+    }
   }
 
   return {
