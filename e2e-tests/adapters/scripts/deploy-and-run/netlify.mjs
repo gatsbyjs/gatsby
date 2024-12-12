08620b4e87cf434de7dc9abf7ca44639b094a24e
Michal Piechowiak <misiek.piechowiak@gmail.com>
2024-12-12 17:25:19 +0100

    Update netlify.mjs

--- e2e-tests/adapters/scripts/deploy-and-run/netlify.mjs
+++ e2e-tests/adapters/scripts/deploy-and-run/netlify.mjs
@@ -1,8 +1,6 @@
 // @ts-check
 import { execa } from "execa"
 
-// trigger-ci-2
-
 // only set NETLIFY_SITE_ID from E2E_ADAPTERS_NETLIFY_SITE_ID if it's set
 if (process.env.E2E_ADAPTERS_NETLIFY_SITE_ID) {
   process.env.NETLIFY_SITE_ID = process.env.E2E_ADAPTERS_NETLIFY_SITE_ID
