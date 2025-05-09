3daa45356cddbaf904ae983b2c419237a529ba83
Matthew <matthewaureliustjoa@gmail.com>
2025-05-09 15:54:26 -0400

    fix(gatsby-link): Fix external URL detection in isLocalLink function

--- packages/gatsby-link/src/is-local-link.js
+++ packages/gatsby-link/src/is-local-link.js
@@ -1,13 +1,13 @@
-// Copied from https://github.com/sindresorhus/is-absolute-url/blob/3ab19cc2e599a03ea691bcb8a4c09fa3ebb5da4f/index.js
-const ABSOLUTE_URL_REGEX = /^[a-zA-Z][a-zA-Z\d+\-.]*?:/
-const isAbsolute = path => ABSOLUTE_URL_REGEX.test(path)
+export function isLocalLink(path) {
+  // Handle null/undefined case
+  if (!path) return false
 
-export const isLocalLink = path => {
-  if (typeof path !== `string`) {
-    return undefined
-    // TODO(v5): Re-Add TypeError
-    // throw new TypeError(`Expected a \`string\`, got \`${typeof path}\``)
+  // Check for protocol-containing URLs
+  // This will match things like https://, http://, mailto:, tel:, etc.
+  if (/^(?:[a-z+]+:)?\/\//i.test(path)) {
+    return false
   }
 
-  return !isAbsolute(path)
+  // If it's not a protocol-based URL, it's likely a local link
+  return true
 }
