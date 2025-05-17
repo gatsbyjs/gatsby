feae011125af709453fc6cfcb4d763f4f58f02f6
mac <2099930591@qq.com>
2025-05-17 13:40:34 +0800

    fix the external URL detected as local link bug

--- packages/gatsby-link/src/is-local-link.js
+++ packages/gatsby-link/src/is-local-link.js
@@ -1,21 +1,13 @@
-// Recognize absolute URLs like https://, http://, mailto:, etc.
-const ABSOLUTE_URL_REGEX = /^[a-zA-Z][a-zA-Z\d+\-.]*:\/\//;
+const ABSOLUTE_URL_REGEX = /^[a-zA-Z][a-zA-Z\d+\-.]*:\/\//
 
-/**
- * Check if a path is an absolute URL (external).
- */
 function isAbsolute(path) {
-  return ABSOLUTE_URL_REGEX.test(path);
+  return ABSOLUTE_URL_REGEX.test(path)
 }
 
-/**
- * Check if a path is a local (internal) link.
- * Returns true if the path is relative to the site, false otherwise.
- */
 export function isLocalLink(path) {
-  if (typeof path !== 'string') {
-    throw new TypeError(`Expected a string, got ${typeof path}`);
+  if (typeof path !== `string`) {
+    throw new TypeError(`Expected a string, got ${typeof path}`)
   }
 
-  return !isAbsolute(path);
+  return !isAbsolute(path)
 }
