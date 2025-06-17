8b3ad732c0a9669a8e9d4617640c048215ec999a
Michal Piechowiak <misiek.piechowiak@gmail.com>
2025-06-17 16:47:33 +0200

    tmp: handle adjustments in sharp so webpack bundles correct binaries

--- packages/gatsby/src/schema/graphql-engine/sharp-bundling-patch.ts
+++ packages/gatsby/src/schema/graphql-engine/sharp-bundling-patch.ts
@@ -1,6 +1,23 @@
-export default function (this: any, source: string): string {
+import { LoaderDefinitionFunction } from "webpack"
+
+const webpackPatchSharp: LoaderDefinitionFunction = function webpackPatchSharp(
+  source: string
+): string {
+  // eslint-disable-next-line @babel/no-invalid-this
+  if (source && this?.resourcePath?.endsWith(`/lib/sharp.js`)) {
+    // ref https://github.com/lovell/sharp/issues/4380
+    source = source.replace(
+      `sharp = require(path)`,
+      // todo: this should not hardcode the platform specific package name
+      // and instead figure out correct one for deployment platform
+      `sharp = require('@img/sharp-linux-x64/sharp.node')`
+    )
+  }
+
   return source?.replace(
     `versions = require(\`../vendor/\${versions.vips}/\${platformAndArch}/versions.json\`);`,
     ``
   )
 }
+
+export default webpackPatchSharp
