d2f2bc1d9915efec64e3ce7b5a30acb4bb2e7c11
Philippe Serhal <philippe.serhal@netlify.com>
2025-11-10 17:15:56 -0500

    fix(deps): match latest estree types exactly
    For some reason the yarn install was being nondeterministic and we'd
    sometimes end up with 1.0.7 being resolved here and other times 1.0.1.
    
    The former had a new required `attributes` property and the latter had
    no such type, so we'd end up with a type error when it resolved to the
    former (and if we made this change only, we'd have type errors if it
    resolved to the latter). So just bump to 1.0.7+ and add the property.

--- packages/gatsby-plugin-mdx/src/gatsby-layout-loader.ts
+++ packages/gatsby-plugin-mdx/src/gatsby-layout-loader.ts
@@ -99,6 +99,7 @@ const gatsbyLayoutLoader: LoaderDefinition = async function (
         type: `Literal`,
         value: slash(mdxPath),
       },
+      attributes: [],
     })
 
     let hasClassicReactImport = false
@@ -236,6 +237,7 @@ const gatsbyLayoutLoader: LoaderDefinition = async function (
           type: `Literal`,
           value: `react`,
         },
+        attributes: [],
       })
     }
 
