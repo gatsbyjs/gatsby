ef84a467b402f1192eb4588dd69bf64d58da5c12
Hrishikesh Kokate <hrishi5200@gmail.com>
2026-07-10 23:35:50 +0530

    disable domain deletion

--- e2e-tests/adapters/scripts/deploy-and-run/netlify.mjs
+++ e2e-tests/adapters/scripts/deploy-and-run/netlify.mjs
@@ -57,7 +57,10 @@ const deployResults = await execa(
   ],
   {
     reject: false,
-    env: { ...env, NO_COLOR: "1" },
+    env: {
+      ...env,
+      NO_COLOR: "1"
+    },
   }
 )
 
@@ -97,7 +100,7 @@ try {
     stdio: `inherit`,
   })
 } finally {
-  if (!env.GATSBY_TEST_SKIP_CLEANUP) {
+  /* if (!env.GATSBY_TEST_SKIP_CLEANUP) {
     log(`Deleting project with deploy_id ${deployId}`)
 
     const deleteResponse = await execa("npx", [
@@ -115,5 +118,5 @@ try {
     }
 
     log(`Successfully deleted project with deploy_id ${deployId}`)
-  }
+    } */
 }
