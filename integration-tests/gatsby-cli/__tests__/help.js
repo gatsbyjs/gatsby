62bd6779d6580d03ce4ee30b8b11d06fd46dc29a
Emmett Hoolahan <emmetthoolahan@gmail.com>
2026-09-06 11:15:33 +0800

    test(gatsby-cli): cover global options without a command

--- integration-tests/gatsby-cli/__tests__/help.js
+++ integration-tests/gatsby-cli/__tests__/help.js
@@ -1,11 +1,21 @@
 import { GatsbyCLI } from "../test-helpers"
 
+const expectHelp = args => {
+  const [status, logs] = GatsbyCLI.from(`gatsby-sites/gatsby-build`).invoke(
+    args
+  )
+
+  logs.should.contain(`Usage: gatsby <command> [options]`)
+  logs.should.contain(`gatsby develop`)
+  expect(status).toBe(0)
+}
+
 describe(`gatsby`, () => {
   it(`shows help when called without a command`, () => {
-    const [status, logs] = GatsbyCLI.from(`gatsby-sites/gatsby-build`).invoke([])
+    expectHelp([])
+  })
 
-    logs.should.contain(`Usage: gatsby <command> [options]`)
-    logs.should.contain(`gatsby develop`)
-    expect(status).toBe(0)
+  it(`shows help when called with global options but no command`, () => {
+    expectHelp([`--verbose`])
   })
 })
